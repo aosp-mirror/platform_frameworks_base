3f9de8b6caa3fe9119b363c64e79d13bb72aec76
Dirk Dougherty <nobody@android.com>
2009-05-22 16:04:18 -0700

    AI 149211: Doc updates for Android 1.5 SDK, r2.   BUG=1790234
    Automated import of CL 149211

--- Android.mk
+++ Android.mk
@@ -309,7 +309,7 @@ web_docs_sample_code_flags := \
 # most current Android platform version included in the SDK package. 
 framework_docs_SDK_VERSION :=  1.5
 # release version for SDK (ie "Release x")
-framework_docs_SDK_REL_ID :=   1
+framework_docs_SDK_REL_ID :=   2
 framework_docs_SDK_CURRENT_DIR := $(framework_docs_SDK_VERSION)_r$(framework_docs_SDK_REL_ID)
 
 framework_docs_LOCAL_DROIDDOC_OPTIONS += \
