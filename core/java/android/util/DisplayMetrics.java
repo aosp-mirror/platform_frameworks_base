2a578ae518ff3d8a2d4768b3d190e4702509e82c
David 'Digit' Turner <digit@google.com>
2009-06-18 04:30:32 +0200

    Allow the qemu.sf.lcd_density property to override the value of ro.sf.lcd_density
    ro.sf.lcd_density is usually defined in the build.prop file which is parsed by init
    before anything else. Since its name begins with "ro.", this property is write-once
    and cannot later be modified, e.g. in /system/etc/init.goldfish.sh.
    
    In other words, you cannot use "emulator -prop ro.sf.lcd_density=<value>", since
    it is impossible to override the value defined in build.prop
    
    This patch modifies the system to recognize "qemu.sf.lcd_density" as an override
    value, which can be set with "emulator -prop qemu.sf.lcd_density=<value>", forcing
    a specific density.
    
    A later patch will allow the emulator to automatically set this property depending
    on AVD hardware configuration settings.

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -37,8 +37,7 @@ public class DisplayMetrics {
      * The device's density.
      * @hide
      */
-    public static final int DEVICE_DENSITY = SystemProperties.getInt("ro.sf.lcd_density",
-            DEFAULT_DENSITY);
+    public static final int DEVICE_DENSITY = getDeviceDensity();
 
     /**
      * The absolute width of the display in pixels.
@@ -160,5 +159,14 @@ public class DisplayMetrics {
         return "DisplayMetrics{density=" + density + ", width=" + widthPixels +
             ", height=" + heightPixels + ", scaledDensity=" + scaledDensity +
             ", xdpi=" + xdpi + ", ydpi=" + ydpi + "}";
+    }
+
+    private static int getDeviceDensity() {
+        // qemu.sf.lcd_density can be used to override ro.sf.lcd_density
+        // when running in the emulator, allowing for dynamic configurations.
+        // The reason for this is that ro.sf.lcd_density is write-once and is
+        // set by the init process when it parses build.prop before anything else.
+        return SystemProperties.getInt("qemu.sf.lcd_density",
+                SystemProperties.getInt("ro.sf.lcd_density", DEFAULT_DENSITY));
     }
 }
