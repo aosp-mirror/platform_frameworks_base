2debd56b5b24d7f4c385f64bfcaf23d00d4d986a
Tom Taylor <tomtaylor@google.com>
2009-07-14 11:27:20 -0700

    Add new gtalk values
    Add new gservices strings for various picture related urls for GTalk.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2820,7 +2820,7 @@ public final class Settings {
                 "gtalk_nosync_heartbeat_ping_interval_ms";
 
         /**
-         * The maximum heartbeat interval used while on the WIFI network. 
+         * The maximum heartbeat interval used while on the WIFI network.
          */
         public static final String GTALK_SERVICE_WIFI_MAX_HEARTBEAT_INTERVAL_MS =
                 "gtalk_wifi_max_heartbeat_ping_interval_ms";
@@ -2891,7 +2891,38 @@ public final class Settings {
          */
         public static final String PUSH_MESSAGING_REGISTRATION_URL =
                 "push_messaging_registration_url";
-        
+
+        /**
+         * This is gdata url to lookup album and picture info from picasa web.
+         */
+        public static final String GTALK_PICASA_ALBUM_URL =
+            "gtalk_picasa_album_url";
+
+        /**
+         * This is the url to lookup picture info from flickr.
+         */
+        public static final String GTALK_FLICKR_PHOTO_INFO_URL =
+            "gtalk_flickr_photo_info_url";
+
+        /**
+         * This is the url to lookup an actual picture from flickr.
+         */
+        public static final String GTALK_FLICKR_PHOTO_URL =
+            "gtalk_flickr_photo_url";
+
+        /**
+         * This is the gdata url to lookup info on a youtube video.
+         */
+        public static final String GTALK_YOUTUBE_VIDEO_URL =
+            "gtalk_youtube_video_url";
+
+
+        /**
+         * This is the url for getting the app token for server-to-device data messaging.
+         */
+        public static final String DATA_MESSAGE_GET_APP_TOKEN_URL =
+                "data_messaging_get_app_token_url";
+
         /**
          * Enable use of ssl session caching.
          * 'db' - save each session in a (per process) database
