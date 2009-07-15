ab3ef104cd507c06eac34fc436b39340f6e9d680
Karl Rosaen <krosaen@google.com>
2009-07-15 10:09:56 -0700

    Run background threads associated with filtering at background priority.
    This improves the typing responsiveness in the browser a lot, and should
    help out in all the other cases in the UI where we use a filter thread.

--- core/java/android/widget/Filter.java
+++ core/java/android/widget/Filter.java
@@ -85,7 +85,8 @@ public abstract class Filter {
     public final void filter(CharSequence constraint, FilterListener listener) {
         synchronized (mLock) {
             if (mThreadHandler == null) {
-                HandlerThread thread = new HandlerThread(THREAD_NAME);
+                HandlerThread thread = new HandlerThread(
+                        THREAD_NAME, android.os.Process.THREAD_PRIORITY_BACKGROUND);
                 thread.start();
                 mThreadHandler = new RequestHandler(thread.getLooper());
             }
