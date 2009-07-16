abccd7eaa52d0b8579ba3d1c954dfe7529945c0f
Wink Saville <wink@google.com>
2009-07-16 15:56:22 -0700

    DO NOT MERGE. Implement IMSI function for CDMA phones
    Includes support to get the MCC and MNC from system properties, since
    they are known to be unreliable from the network.
    
    This is known to conflict with master and will be
    added by hand so as not to break the build.

--- telephony/java/com/android/internal/telephony/cdma/RuimRecords.java
+++ telephony/java/com/android/internal/telephony/cdma/RuimRecords.java
@@ -123,19 +123,10 @@ public final class RuimRecords extends IccRecords {
 
         adnCache.reset();
 
-        phone.setSystemProperty(TelephonyProperties.PROPERTY_ICC_OPERATOR_NUMERIC, null);
-        phone.setSystemProperty(TelephonyProperties.PROPERTY_ICC_OPERATOR_ISO_COUNTRY, null);
-
         // recordsRequested is set to false indicating that the SIM
         // read requests made so far are not valid. This is set to
         // true only when fresh set of read requests are made.
         recordsRequested = false;
-    }
-
-    /** Returns null if RUIM is not yet ready */
-    public String getIMSI_M() {
-        // TODO(Moto): mImsi is not initialized, fix.
-        return mImsi;
     }
 
     public String getMdnNumber() {
