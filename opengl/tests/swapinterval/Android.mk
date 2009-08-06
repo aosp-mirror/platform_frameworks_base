bb99ffb4db008441e6ac4236d8a48b6e2b0c01d9
Mathias Agopian <mathias@google.com>
2009-08-05 17:38:49 -0700

    added a gl swapinterval test

---
+++ opengl/tests/swapinterval/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	swapinterval.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+    libEGL \
+    libGLESv1_CM \
+    libui
+
+LOCAL_MODULE:= test-opengl-swapinterval
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_EXECUTABLE)
--- opengl/tests/swapinterval/swapinterval.cpp
+++ opengl/tests/swapinterval/swapinterval.cpp
@@ -85,95 +85,38 @@ int main(int argc, char** argv)
      
      printf("w=%d, h=%d\n", w, h);
      
-     glBindTexture(GL_TEXTURE_2D, 0);
-     glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-     glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-     glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-     glTexParameterx(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-     glTexEnvx(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
-     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
      glDisable(GL_DITHER);
      glEnable(GL_BLEND);
-     glEnable(GL_TEXTURE_2D);
-     glColor4f(1,1,1,1);
-
-     uint32_t* t32 = (uint32_t*)malloc(512*512*4); 
-     for (int y=0 ; y<512 ; y++) {
-         for (int x=0 ; x<512 ; x++) {
-             int u = x-256;
-             int v = y-256;
-             if (u*u+v*v < 256*256) {
-                 t32[x+y*512] = 0x10FFFFFF;
-             } else {
-                 t32[x+y*512] = 0x20FF0000;
-             }
-         }
-     }
-
-     const GLfloat vertices[4][2] = {
-             { 0,  0 },
-             { 0,  h },
-             { w,  h },
-             { w,  0 }
-     };
-
-     const GLfloat texCoords[4][2] = {
-             { 0,  0 },
-             { 0,  1 },
-             { 1,  1 },
-             { 1,  0 }
-     };
-
-     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 512, 512, 0, GL_RGBA, GL_UNSIGNED_BYTE, t32);
 
      glViewport(0, 0, w, h);
-     glMatrixMode(GL_PROJECTION);
-     glLoadIdentity();
      glOrthof(0, w, 0, h, 0, 1);
-
-     glEnableClientState(GL_VERTEX_ARRAY);
-     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-     glVertexPointer(2, GL_FLOAT, 0, vertices);
-     glTexCoordPointer(2, GL_FLOAT, 0, texCoords);
 
      eglSwapInterval(dpy, 1);
 
      glClearColor(1,0,0,0);
      glClear(GL_COLOR_BUFFER_BIT);
-     glDrawArrays(GL_TRIANGLE_FAN, 0, 4); 
      eglSwapBuffers(dpy, surface);
+
+
+     int time = 10;
+     printf("screen should flash red/green quickly for %d s...\n", time);
      
-
-     nsecs_t times[32];
-
-     for (int c=1 ; c<32 ; c++) {
+     int c = 0;
+     nsecs_t start = systemTime();
+     nsecs_t t;
+     do {
+         glClearColor(1,0,0,0);
          glClear(GL_COLOR_BUFFER_BIT);
-         for (int i=0 ; i<c ; i++) {
-             glDrawArrays(GL_TRIANGLE_FAN, 0, 4); 
-         }
          eglSwapBuffers(dpy, surface);
-     }
-
-
-     //     for (int c=31 ; c>=1 ; c--) {
-     int j=0;
-     for (int c=1 ; c<32 ; c++) {
+         glClearColor(0,1,0,0);
          glClear(GL_COLOR_BUFFER_BIT);
-         nsecs_t now = systemTime();
-         for (int i=0 ; i<c ; i++) {
-             glDrawArrays(GL_TRIANGLE_FAN, 0, 4); 
-         }
          eglSwapBuffers(dpy, surface);
-         nsecs_t t = systemTime() - now;
-         times[j++] = t;
-     }
-
-     for (int c=1, j=0 ; c<32 ; c++, j++) {
-         nsecs_t t = times[j];
-         printf("%lld\t%d\t%f\n", t, c, (double(t)/c)/1000000.0);
-     }
-
-
+         t = systemTime() - start;
+         c += 2;
+     } while (int(ns2s(t))<=time);
+     
+     double p =  (double(t) / c) / 1000000000.0;
+     printf("refresh-rate is %f fps (%f ms)\n", 1.0f/p, p*1000.0);
        
      eglTerminate(dpy);
      
