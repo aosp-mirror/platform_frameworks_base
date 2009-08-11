4523b76803e1269171003a950fa3d5237ea2cdcb
Mathias Agopian <mathias@google.com>
2009-08-10 21:13:06 -0700

    enable HW acceleration for the Dim Surface on 8k devices

--- libs/surfaceflinger/Android.mk
+++ libs/surfaceflinger/Android.mk
@@ -24,6 +24,9 @@ LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES -DEGL_EGLEXT_PROTOTYPES
 ifeq ($(TARGET_BOARD_PLATFORM), msm7k)
 	LOCAL_CFLAGS += -DDIM_WITH_TEXTURE
 endif
+ifeq ($(TARGET_BOARD_PLATFORM), qsd8k)
+	LOCAL_CFLAGS += -DDIM_WITH_TEXTURE
+endif
 
 # need "-lrt" on Linux simulator to pick up clock_gettime
 ifeq ($(TARGET_SIMULATOR),true)
