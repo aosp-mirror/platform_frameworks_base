3f8f9da85f4189bfef68cf13a1b643674b5e0726
Marco Nelissen <marcone@google.com>
2009-08-12 09:41:07 -0700

    Fix the simulator build by removing librs_jni and libRS from it. Since libacc isn't 64-bit clean (it casts pointers to integers), and so can't be built for the gHardy simulator, everything that depends on it can't be built for the simulator either, which means removing libRS and librs_jni.

--- graphics/jni/Android.mk
+++ graphics/jni/Android.mk
@@ -1,4 +1,10 @@
 
+# libRS needs libacc, which isn't 64-bit clean, and so can't be built
+# for the simulator on gHardy, and therefore libRS needs to be excluded
+# from the simulator as well, and so in turn librs_jni needs to be
+# excluded.
+ifneq ($(TARGET_SIMULATOR),true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -36,3 +42,4 @@ LOCAL_REQUIRED_MODULES := libRS
 
 include $(BUILD_SHARED_LIBRARY)
 
+endif #simulator
