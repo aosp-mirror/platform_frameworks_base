56f67d21459ad3f136c73c8932904d4a495989c0
Fred Quintana <fredq@google.com>
2009-08-28 14:36:42 -0700

    add the ability to specify yieldpoints in a ContentProviderOperation

--- core/java/android/content/OperationApplicationException.java
+++ core/java/android/content/OperationApplicationException.java
@@ -21,16 +21,34 @@ package android.content;
  * constraints.
  */
 public class OperationApplicationException extends Exception {
+    private final int mNumSuccessfulYieldPoints;
+
     public OperationApplicationException() {
         super();
+        mNumSuccessfulYieldPoints = 0;
     }
     public OperationApplicationException(String message) {
         super(message);
+        mNumSuccessfulYieldPoints = 0;
     }
     public OperationApplicationException(String message, Throwable cause) {
         super(message, cause);
+        mNumSuccessfulYieldPoints = 0;
     }
     public OperationApplicationException(Throwable cause) {
         super(cause);
+        mNumSuccessfulYieldPoints = 0;
+    }
+    public OperationApplicationException(int numSuccessfulYieldPoints) {
+        super();
+        mNumSuccessfulYieldPoints = numSuccessfulYieldPoints;
+    }
+    public OperationApplicationException(String message, int numSuccessfulYieldPoints) {
+        super(message);
+        mNumSuccessfulYieldPoints = numSuccessfulYieldPoints;
+    }
+
+    public int getNumSuccessfulYieldPoints() {
+        return mNumSuccessfulYieldPoints;
     }
 }
