44b283dc27ed8ba5a946d809489f29b4ae88cb15
Kenny Root <kenny@the-b.org>
2009-09-01 19:03:11 -0500

    Fix %p vs %s typo in aapt message
    When trying to print an xmltree or xmlstrings from aapt, the error
    message if the resource didn't exist erroneously printed a pointer
    instead of a string.
    
    Change-Id: I317bbbdc1200e0f10922e80a36e41a22b2d50d0d

--- tools/aapt/Command.cpp
+++ tools/aapt/Command.cpp
@@ -396,7 +396,7 @@ int doDump(Bundle* bundle)
             ResXMLTree tree;
             asset = assets.openNonAsset(resname, Asset::ACCESS_BUFFER);
             if (asset == NULL) {
-                fprintf(stderr, "ERROR: dump failed because resource %p found\n", resname);
+                fprintf(stderr, "ERROR: dump failed because resource %s found\n", resname);
                 goto bail;
             }
 
@@ -422,7 +422,7 @@ int doDump(Bundle* bundle)
             ResXMLTree tree;
             asset = assets.openNonAsset(resname, Asset::ACCESS_BUFFER);
             if (asset == NULL) {
-                fprintf(stderr, "ERROR: dump failed because resource %p found\n", resname);
+                fprintf(stderr, "ERROR: dump failed because resource %s found\n", resname);
                 goto bail;
             }
 
