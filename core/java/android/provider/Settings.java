c15255a979c9fc70bc251aefdcd9c1074b458d49
Amith Yamasani <yamasani@google.com>
2009-09-23 15:33:19 -0700

    Add a PRIVACY_SETTINGS action to launch the new Privacy settings screen.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -148,6 +148,20 @@ public final class Settings {
             "android.settings.SECURITY_SETTINGS";
 
     /**
+     * Activity Action: Show settings to allow configuration of privacy options.
+     * <p>
+     * In some cases, a matching Activity may not exist, so ensure you
+     * safeguard against this.
+     * <p>
+     * Input: Nothing.
+     * <p>
+     * Output: Nothing.
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_PRIVACY_SETTINGS =
+            "android.settings.PRIVACY_SETTINGS";
+
+    /**
      * Activity Action: Show settings to allow configuration of Wi-Fi.
 
      * <p>
