c8217638be7e39161fa31bfe240a64a563690dbf
Hung-ying Tyan <tyanh@google.com>
2009-09-30 16:57:51 +0800

    Fix stopping all vpn daemons before connect and more.
    * move DaemonHelper out from VpnService to VpnDaemons for better
      managing native daemons.
    * check connectivity and dns less frequently to save battery.

--- packages/VpnServices/src/com/android/server/vpn/L2tpIpsecService.java
+++ packages/VpnServices/src/com/android/server/vpn/L2tpIpsecService.java
@@ -31,9 +31,10 @@ class L2tpIpsecService extends VpnService<L2tpIpsecProfile> {
     protected void connect(String serverIp, String username, String password)
             throws IOException {
         L2tpIpsecProfile p = getProfile();
+        VpnDaemons daemons = getDaemons();
+
         // IPSEC
-        DaemonProxy ipsec = startDaemon(IPSEC);
-        ipsec.sendCommand(serverIp, L2tpService.L2TP_PORT,
+        DaemonProxy ipsec = daemons.startIpsecForL2tp(serverIp,
                 Credentials.USER_PRIVATE_KEY + p.getUserCertificate(),
                 Credentials.USER_CERTIFICATE + p.getUserCertificate(),
                 Credentials.CA_CERTIFICATE + p.getCaCertificate());
@@ -42,15 +43,8 @@ class L2tpIpsecService extends VpnService<L2tpIpsecProfile> {
         sleep(2000); // 2 seconds
 
         // L2TP
-        MtpdHelper.sendCommand(this, L2tpService.L2TP_DAEMON, serverIp,
-                L2tpService.L2TP_PORT,
+        daemons.startL2tp(serverIp,
                 (p.isSecretEnabled() ? p.getSecretString() : null),
                 username, password);
-    }
-
-    @Override
-    protected void stopPreviouslyRunDaemons() {
-        stopDaemon(IPSEC);
-        stopDaemon(MtpdHelper.MTPD);
     }
 }
