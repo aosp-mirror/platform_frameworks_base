0a9d46b7e741ccf7272bb69a4eef3c1f2adc91b7
Xavier Ducrohet <xav@android.com>
2009-10-06 09:58:08 -0700

    Add a test to make sure the replaced classes in layoutlib are correct.
    Change-Id: I9641635153c12e2c0a23583f7d094767533fc683

--- tools/layoutlib/bridge/tests/com/android/layoutlib/bridge/NinePatchTest.java
+++ tools/layoutlib/bridge/tests/com/android/layoutlib/bridge/NinePatchTest.java
@@ -7,20 +7,21 @@ import java.net.URL;
 import junit.framework.TestCase;
 
 public class NinePatchTest extends TestCase {
-    
+
     private NinePatch mPatch;
 
     @Override
     protected void setUp() throws Exception {
-        URL url = this.getClass().getClassLoader().getResource("button.9.png");
+        URL url = this.getClass().getClassLoader().getResource(
+                "/com/android/layoutlib/testdata/button.9.png");
 
         mPatch = NinePatch.load(url, false /* convert */);
     }
-    
+
     public void test9PatchLoad() throws Exception {
         assertNotNull(mPatch);
     }
-    
+
     public void test9PatchMinSize() {
         int[] padding = new int[4];
         mPatch.getPadding(padding);
@@ -28,8 +29,8 @@ public class NinePatchTest extends TestCase {
         assertEquals(3, padding[1]);
         assertEquals(13, padding[2]);
         assertEquals(4, padding[3]);
-        assertEquals(38, mPatch.getWidth());
-        assertEquals(27, mPatch.getHeight());
+        assertEquals(36, mPatch.getWidth());
+        assertEquals(25, mPatch.getHeight());
     }
 
 }
