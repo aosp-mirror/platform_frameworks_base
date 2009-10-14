3d0726b4d8a30e57852b70f1dcf4d8d20508cd1e
Nick Pelly <npelly@google.com>
2009-10-14 11:46:18 -0700

    Remove STOPSHIP comment.
    Change-Id: I2492bffe57bded65fdbef5cf98e22cef3937e537
    DrNo: eastham
    Bug: 2089423
    Joke: Why don't anteaters get sick? Because they're full of anty-bodies!

--- core/jni/android_server_BluetoothEventLoop.cpp
+++ core/jni/android_server_BluetoothEventLoop.cpp
@@ -735,8 +735,7 @@ static DBusHandlerResult event_filter(DBusConnection *conn, DBusMessage *msg,
         return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
     }
 
-    // STOPSHIP: Change to LOGV
-    LOGE("%s: Received signal %s:%s from %s", __FUNCTION__,
+    LOGV("%s: Received signal %s:%s from %s", __FUNCTION__,
         dbus_message_get_interface(msg), dbus_message_get_member(msg),
         dbus_message_get_path(msg));
 
