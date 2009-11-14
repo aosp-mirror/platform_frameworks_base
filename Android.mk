613438086cb68a4e0f48ddd51238f3d9a054ac9d
Dan Egnor <egnor@google.com>
2009-11-13 17:14:26 -0800

    Remove external/protobuf from the build, now that it's been moved to vendor/google/libraries.

--- Android.mk
+++ Android.mk
@@ -523,7 +523,6 @@ include $(BUILD_DROIDDOC)
 ext_dirs := \
 	../../external/apache-http/src \
 	../../external/gdata/src \
-	../../external/protobuf/src \
 	../../external/tagsoup/src
 
 ext_src_files := $(call all-java-files-under,$(ext_dirs))
