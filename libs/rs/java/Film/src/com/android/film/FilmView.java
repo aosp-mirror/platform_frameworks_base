7efde12a03b8d56c9344d5586d76336806638f11
Jason Sams <rjsams@android.com>
2009-11-25 15:12:01 -0800

    Fix film

--- libs/rs/java/Film/src/com/android/film/FilmView.java
+++ libs/rs/java/Film/src/com/android/film/FilmView.java
@@ -52,7 +52,7 @@ public class FilmView extends RSSurfaceView {
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
         super.surfaceChanged(holder, format, w, h);
         if (mRS == null) {
-            mRS = createRenderScript(false);
+            mRS = createRenderScript(true);
             mRS.contextSetSurface(w, h, holder.getSurface());
             mRender = new FilmRS();
             mRender.init(mRS, getResources(), w, h);
