b0caf946b7ae9be519c0cede935ea2ecb44f9bd0
Andreas Huber <andih@google.com>
2009-12-03 11:39:54 -0800

    Squashed commit of the following:
    commit 543e192bf2ae13f573dc4c4e53b239ed4ea00e81
    Author: Andreas Huber <andih@google.com>
    Date:   Thu Dec 3 11:33:57 2009 -0800
    
        stagefright now acts as the OMX Master, vendors supply their own plugins through libstagefrighthw. In OpenCore-enabled builds we now sit on top of PVMaster...
    
    commit 3cbfdbd9cecadbb77b63125c62883bf1065884fe
    Author: Andreas Huber <andih@google.com>
    Date:   Wed Dec 2 12:39:07 2009 -0800
    
        More OMX infrastructure, stagefright is now taking over the OMX Master, multiplexing all contributing OMX implementations under a common interface.

---
+++ media/libstagefright/omx/OMXPVCodecsPlugin.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2009 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OMX_PV_CODECS_PLUGIN_H_
+
+#define OMX_PV_CODECS_PLUGIN_H_
+
+#include <media/stagefright/OMXPluginBase.h>
+
+namespace android {
+
+struct OMXPVCodecsPlugin : public OMXPluginBase {
+    OMXPVCodecsPlugin();
+    virtual ~OMXPVCodecsPlugin();
+
+    virtual OMX_ERRORTYPE makeComponentInstance(
+            const char *name,
+            const OMX_CALLBACKTYPE *callbacks,
+            OMX_PTR appData,
+            OMX_COMPONENTTYPE **component);
+
+    virtual OMX_ERRORTYPE enumerateComponents(
+            OMX_STRING name,
+            size_t size,
+            OMX_U32 index);
+
+private:
+    OMXPVCodecsPlugin(const OMXPVCodecsPlugin &);
+    OMXPVCodecsPlugin &operator=(const OMXPVCodecsPlugin &);
+};
+
+}  // namespace android
+
+#endif  // OMX_PV_CODECS_PLUGIN_H_
