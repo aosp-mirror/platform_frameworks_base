ef340357cefeee452403a77c2b6c1aa01c239d6b
Justin Mattson <jmatt@google.com>
2010-01-13 21:05:46 -0800

    NEW_API: Declare an ACTION_SEARCH_SETTINGS constant in android.provider.Settings so that apps can cause this system setting group to be opened directly.
    The Settings app already responds to this action it just was not exposed in the platform API.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -436,6 +436,20 @@ public final class Settings {
     public static final String ACTION_MEMORY_CARD_SETTINGS =
             "android.settings.MEMORY_CARD_SETTINGS";
 
+    /**
+     * Activity Action: Show settings for global search.
+     * <p>
+     * In some cases, a matching Activity may not exist, so ensure you
+     * safeguard against this.
+     * <p>
+     * Input: Nothing.
+     * <p>
+     * Output: Nothing
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_SEARCH_SETTINGS =
+        "android.search.action.SEARCH_SETTINGS";
+
     // End of Intent actions for Settings
 
     private static final String JID_RESOURCE_PREFIX = "android";
