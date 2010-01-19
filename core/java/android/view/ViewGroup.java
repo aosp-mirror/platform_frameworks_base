bc9fdc92603112b3462024c8d0096b4eca3001c3
Romain Guy <romainguy@android.com>
2010-01-19 13:27:48 -0800

    Make sure the drawing cache is invalidated when reattaching a view. Bug #2373677

--- core/java/android/view/ViewGroup.java
+++ core/java/android/view/ViewGroup.java
@@ -2268,7 +2268,7 @@ public abstract class ViewGroup extends View implements ViewParent, ViewManager 
         addInArray(child, index);
 
         child.mParent = this;
-        child.mPrivateFlags = (child.mPrivateFlags & ~DIRTY_MASK) | DRAWN;
+        child.mPrivateFlags = (child.mPrivateFlags & ~DIRTY_MASK & ~DRAWING_CACHE_VALID) | DRAWN;
 
         if (child.hasFocus()) {
             requestChildFocus(child, child.findFocus());
