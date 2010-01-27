3b16892c6bc6b804133163db24ffc5ff54b9a8fb
Ken Shirriff <kens@google.com>
2010-01-27 12:27:28 -0800

    Fix ExtendedProperties EntityIterator.
    The iterator was using the wrong cursor.
    
    bug 2399618

--- core/java/android/provider/Calendar.java
+++ core/java/android/provider/Calendar.java
@@ -744,9 +744,10 @@ public final class Calendar {
                 try {
                     while (subCursor.moveToNext()) {
                         ContentValues extendedValues = new ContentValues();
-                        extendedValues.put(ExtendedProperties.NAME, cursor.getString(COLUMN_NAME));
+                        extendedValues.put(ExtendedProperties.NAME,
+                                subCursor.getString(COLUMN_NAME));
                         extendedValues.put(ExtendedProperties.VALUE,
-                                cursor.getString(COLUMN_VALUE));
+                                subCursor.getString(COLUMN_VALUE));
                         entity.addSubValue(ExtendedProperties.CONTENT_URI, extendedValues);
                     }
                 } finally {
