3433d3c90e53d926e2047182c59cf596a88b5264
Neal Nguyen <tommyn@google.com>
2010-02-02 17:09:00 -0800

    Modifying Services Tests XML file so it builds with the right permissions.

--- services/tests/servicestests/Android.mk
+++ services/tests/servicestests/Android.mk
@@ -11,5 +11,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_JAVA_LIBRARIES := android.test.runner services
 LOCAL_PACKAGE_NAME := FrameworksServicesTests
 
+LOCAL_CERTIFICATE := platform
+
 include $(BUILD_PACKAGE)
 
