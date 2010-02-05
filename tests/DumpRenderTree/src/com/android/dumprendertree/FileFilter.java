cdb4ef4cdb4b3fb729d619c830e169ba056d3905
Steve Block <steveblock@google.com>
2010-02-05 10:31:15 +0000

    Sorts the lists of tests in DumpRenderTree in preparation for cleaning up the lists.

--- tests/DumpRenderTree/src/com/android/dumprendertree/FileFilter.java
+++ tests/DumpRenderTree/src/com/android/dumprendertree/FileFilter.java
@@ -81,138 +81,88 @@ public class FileFilter {
         "platform",  // platform specific
         "http/wml",
     };
-        
+
     static final String [] ignoreTestList = {
-        // RegExp is exponatal
-        "fast/regex/test1.html",
-        "fast/regex/slow.html",
-        // RegExp is too large, causing OOM
-        "fast/js/regexp-charclass-crash.html",
-        // The Android browser has no notion of private browsing.
-        "storage/private-browsing-readonly.html",
-        "storage/domstorage/localstorage/private-browsing-affects-storage.html",
-        "storage/domstorage/sessionstorage/private-browsing-affects-storage.html",
-        // Android layout tests are stored in "layout_tests". The following two
-        // tests expect "LayoutTests" in their output.
-        "storage/domstorage/localstorage/iframe-events.html",
-        "storage/domstorage/sessionstorage/iframe-events.html",
-        // We do not support multi touch events.
-        "fast/events/touch/basic-multi-touch-events.html",
-        // below tests (failed or crashes) are filtered out temporarily due to prioritizing
         "editing/selection/move-left-right.html",
+        "fast/events/touch/basic-multi-touch-events.html", // We do not support multi touch events.
+        "fast/js/regexp-charclass-crash.html", // RegExp is too large, causing OOM
+        "fast/regex/test1.html", // RegExp is exponential
+        "fast/regex/slow.html", // RegExp is exponential
+        "storage/domstorage/localstorage/iframe-events.html", // Expects test to be in LayoutTests
+        "storage/domstorage/localstorage/private-browsing-affects-storage.html", // No notion of private browsing.
+        "storage/domstorage/sessionstorage/iframe-events.html", // Expects test to be in LayoutTests
+        "storage/domstorage/sessionstorage/private-browsing-affects-storage.html", // No notion of private browsing.
+        "storage/private-browsing-readonly.html", // No notion of private browsing.
     };
-    
+
     static void fillIgnoreResultSet() {
-        // need test plugin
-        ignoreResultList.add("fast/dom/Window/Plug-ins.html");
-        // pixel depth
-        ignoreResultList.add("fast/dom/Window/window-screen-properties.html");
-        // missing space in textrun, ok as text is wrapped. ignore. #714933
-        ignoreResultList.add("fast/events/onload-webkit-before-webcore.html");
-        // missing support for textInputController.makeAttributedString()
-        ignoreResultList.add("fast/forms/attributed-strings.html");
-        // charset convert. #516936 ignore, won't fix
-        ignoreResultList.add("fast/forms/form-data-encoding-2.html");
-        // charset convert. #516936 ignore, won't fix
-        ignoreResultList.add("fast/forms/form-data-encoding.html");
-        // execCommand "insertText" not supported
-        ignoreResultList.add("fast/forms/input-appearance-maxlength.html");
-        // Copy&Paste commands not supported
-        ignoreResultList.add("fast/forms/input-truncate-newline.html");
-        ignoreResultList.add("fast/forms/textarea-paste-newline.html");
-        // requires eventSender.mouseMoveTo, mouseDown & mouseUp and 
-        // abs. position of mouse to select a word. ignore, won't fix #716583
-        ignoreResultList.add("fast/forms/onselect-textarea.html");
-        // requires eventSender.mouseMoveTo, mouseDown & mouseUp and 
-        // abs. position of mouse to select a word. ignore, won't fix #716583
-        ignoreResultList.add("fast/forms/onselect-textfield.html");
-        // not implemented queryCommandEnabled:BackColor, Undo & Redo
-        ignoreResultList.add("fast/forms/plaintext-mode-1.html");
-        // Our text areas are a little thinner than Apples. Also RTL test failes
-        ignoreResultList.add("fast/forms/textarea-appearance-wrap.html");
-        // Our text areas are a little thinner than Apples
-        ignoreResultList.add("fast/forms/textarea-hard-linewrap.html");
-        // screen width&height are different
-        ignoreResultList.add("fast/frames/frameElement-widthheight.html");
-        ignoreResultList.add("fast/frames/frame-js-url-clientWidth.html");
-        //  requires JS test API, textInputController
-        ignoreResultList.add("fast/text/attributed-substring-from-range.html"); 
-        ignoreResultList.add("fast/text/attributed-substring-from-range-001.html");
-        // will not fix #619707
-        ignoreResultList.add("fast/css/case-transform.html");
-        // different platform defaults for font and different screen size
-        ignoreResultList.add("fast/css/computed-style.html");
-        // different screen size result in extra spaces in Apple compared to us
-        ignoreResultList.add("fast/dom/Element/offsetLeft-offsetTop-body-quirk.html");
-        // xslt and xpath elements missing from property list
-        ignoreResultList.add("fast/dom/Window/window-properties.html");
-        // requires textInputController.characterIndexForPoint
-        ignoreResultList.add("fast/dom/character-index-for-point.html");
-        // requires xpath support
-        ignoreResultList.add("fast/dom/gc-9.html");
-        // requires xslt and xpath support
-        ignoreResultList.add("fast/dom/global-constructors.html");
-        // dynamic plugins not supported
-        ignoreResultList.add("fast/dom/object-embed-plugin-scripting.html");
-        ignoreResultList.add("fast/js/navigator-mimeTypes-length.html");
-        // there is extra spacing in the file due to multiple input boxes
-        // fitting on one line on Apple, ours are wrapped. Space at line ends
-        // are stripped.
-        ignoreResultList.add("fast/dom/tabindex-clamp.html");
-        
-        // requires eventSender.mouseDown(),mouseUp()
-        ignoreResultList.add("fast/dom/Window/window-xy-properties.html");
-        ignoreResultList.add("fast/events/window-events-bubble.html");
-        ignoreResultList.add("fast/events/window-events-bubble2.html");
-        ignoreResultList.add("fast/events/window-events-capture.html");
-        ignoreResultList.add("fast/forms/select-empty-list.html");
-        ignoreResultList.add("fast/replaced/image-map.html");
-        ignoreResultList.add("fast/events/capture-on-target.html");
-        ignoreResultList.add("fast/events/dblclick-addEventListener.html");
-        ignoreResultList.add("fast/events/drag-in-frames.html");
-        ignoreResultList.add("fast/events/drag-outside-window.html");
-        ignoreResultList.add("fast/events/event-sender-mouse-click.html");
-        ignoreResultList.add("fast/events/event-view-toString.html");
-        ignoreResultList.add("fast/events/frame-click-focus.html");
-        ignoreResultList.add("fast/events/input-image-scrolled-x-y.html");
-        ignoreResultList.add("fast/events/anchor-image-scrolled-x-y.html");
-        ignoreResultList.add("fast/events/mouseclick-target-and-positioning.html");
-        ignoreResultList.add("fast/events/mouseover-mouseout.html");
-        ignoreResultList.add("fast/events/mouseover-mouseout2.html");
-        ignoreResultList.add("fast/events/mouseup-outside-button.html");
-        ignoreResultList.add("fast/events/mouseup-outside-document.html");
-        ignoreResultList.add("fast/events/onclick-list-marker.html");
-        ignoreResultList.add("fast/events/ondragenter.html");
-        ignoreResultList.add("fast/forms/drag-into-textarea.html");
-        ignoreResultList.add("fast/forms/input-select-on-click.html");
-        ignoreResultList.add("fast/forms/listbox-onchange.html");
-        ignoreResultList.add("fast/forms/search-cancel-button-mouseup.html");
-        ignoreResultList.add("fast/forms/textarea-scrolled-endline-caret.html");
-        
-        // there is extra spacing in the file due to multiple frame boxes
-        // fitting on one line on Apple, ours are wrapped. Space at line ends
-        // are stripped.
-        ignoreResultList.add("fast/events/iframe-object-onload.html");
-        // eventSender.mouseDown(), mouseUp() and objc API missing
-        ignoreResultList.add("fast/events/mouseup-outside-document.html");
-        ignoreResultList.add("fast/events/objc-keyboard-event-creation.html");
-        ignoreResultList.add("fast/events/objc-event-api.html");
-        // not capturing the console messages
-        ignoreResultList.add("fast/forms/selected-index-assert.html");
-        ignoreResultList.add("fast/parser/script-tag-with-trailing-slash.html");
-        // there is extra spacing as the text areas and input boxes fit next
-        // to each other on Apple, but are wrapped on our screen.
-        ignoreResultList.add("fast/forms/selection-functions.html");
-        // Text selection done differently on our platform. When a inputbox
-        // gets focus, the entire block is selected.
-        ignoreResultList.add("fast/forms/textarea-initial-caret-position.html");
-        ignoreResultList.add("fast/forms/textarea-no-scroll-on-blur.html");
-        // Requires LayoutTests to exist at /tmp/LayoutTests
-        ignoreResultList.add("fast/loader/local-JavaScript-from-local.html");
-        ignoreResultList.add("fast/loader/local-iFrame-source-from-local.html");
-        // extra spacing because iFrames rendered next to each other on Apple
-        ignoreResultList.add("fast/loader/opaque-base-url.html");
-        ignoreResultList.add("fast/text/plain-text-line-breaks.html");
+        ignoreResultList.add("fast/css/case-transform.html"); // will not fix #619707
+        ignoreResultList.add("fast/css/computed-style.html"); // different platform defaults for font and different screen size
+        ignoreResultList.add("fast/dom/Element/offsetLeft-offsetTop-body-quirk.html"); // different screen size result in extra spaces in Apple compared to us
+        ignoreResultList.add("fast/dom/Window/Plug-ins.html"); // need test plugin
+        ignoreResultList.add("fast/dom/Window/window-properties.html"); // xslt and xpath elements missing from property list
+        ignoreResultList.add("fast/dom/Window/window-screen-properties.html"); // pixel depth
+        ignoreResultList.add("fast/dom/Window/window-xy-properties.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/dom/character-index-for-point.html"); // requires textInputController.characterIndexForPoint
+        ignoreResultList.add("fast/dom/gc-9.html"); // requires xpath support
+        ignoreResultList.add("fast/dom/global-constructors.html"); // requires xslt and xpath support
+        ignoreResultList.add("fast/dom/object-embed-plugin-scripting.html"); // dynamic plugins not supported
+        ignoreResultList.add("fast/dom/tabindex-clamp.html"); // there is extra spacing in the file due to multiple input boxes fitting on one line on Apple, ours are wrapped. Space at line ends are stripped.
+        ignoreResultList.add("fast/events/anchor-image-scrolled-x-y.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/capture-on-target.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/dblclick-addEventListener.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/drag-in-frames.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/drag-outside-window.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/event-sender-mouse-click.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/event-view-toString.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/frame-click-focus.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/iframe-object-onload.html"); // there is extra spacing in the file due to multiple frame boxes fitting on one line on Apple, ours are wrapped. Space at line ends are stripped.
+        ignoreResultList.add("fast/events/input-image-scrolled-x-y.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/mouseclick-target-and-positioning.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/mouseover-mouseout.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/mouseover-mouseout2.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/mouseup-outside-button.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/mouseup-outside-document.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/objc-event-api.html"); // eventSender.mouseDown(), mouseUp() and objc API missing
+        ignoreResultList.add("fast/events/objc-keyboard-event-creation.html"); // eventSender.mouseDown(), mouseUp() and objc API missing
+        ignoreResultList.add("fast/events/onclick-list-marker.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/ondragenter.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/onload-webkit-before-webcore.html"); // missing space in textrun, ok as text is wrapped. ignore. #714933
+        ignoreResultList.add("fast/events/window-events-bubble.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/window-events-bubble2.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/events/window-events-capture.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/attributed-strings.html"); // missing support for textInputController.makeAttributedString()
+        ignoreResultList.add("fast/forms/drag-into-textarea.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/form-data-encoding-2.html"); // charset convert. #516936 ignore, won't fix
+        ignoreResultList.add("fast/forms/form-data-encoding.html"); // charset convert. #516936 ignore, won't fix
+        ignoreResultList.add("fast/forms/input-appearance-maxlength.html"); // execCommand "insertText" not supported
+        ignoreResultList.add("fast/forms/input-select-on-click.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/input-truncate-newline.html"); // Copy&Paste commands not supported
+        ignoreResultList.add("fast/forms/listbox-onchange.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/onselect-textarea.html"); // requires eventSender.mouseMoveTo, mouseDown & mouseUp and abs. position of mouse to select a word. ignore, won't fix #716583
+        ignoreResultList.add("fast/forms/onselect-textfield.html"); // requires eventSender.mouseMoveTo, mouseDown & mouseUp and abs. position of mouse to select a word. ignore, won't fix #716583
+        ignoreResultList.add("fast/forms/plaintext-mode-1.html"); // not implemented queryCommandEnabled:BackColor, Undo & Redo
+        ignoreResultList.add("fast/forms/search-cancel-button-mouseup.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/select-empty-list.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/forms/selected-index-assert.html"); // not capturing the console messages
+        ignoreResultList.add("fast/forms/selection-functions.html"); // there is extra spacing as the text areas and input boxes fit next to each other on Apple, but are wrapped on our screen.
+        ignoreResultList.add("fast/forms/textarea-appearance-wrap.html"); // Our text areas are a little thinner than Apples. Also RTL test failes
+        ignoreResultList.add("fast/forms/textarea-hard-linewrap.html"); // Our text areas are a little thinner than Apples
+        ignoreResultList.add("fast/forms/textarea-initial-caret-position.html"); // Text selection done differently on our platform. When a inputbox gets focus, the entire block is selected.
+        ignoreResultList.add("fast/forms/textarea-no-scroll-on-blur.html"); // Text selection done differently on our platform. When a inputbox gets focus, the entire block is selected.
+        ignoreResultList.add("fast/forms/textarea-paste-newline.html"); // Copy&Paste commands not supported
+        ignoreResultList.add("fast/forms/textarea-scrolled-endline-caret.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/frames/frameElement-widthheight.html"); // screen width&height are different
+        ignoreResultList.add("fast/frames/frame-js-url-clientWidth.html"); // screen width&height are different
+        ignoreResultList.add("fast/js/navigator-mimeTypes-length.html"); // dynamic plugins not supported
+        ignoreResultList.add("fast/loader/local-JavaScript-from-local.html"); // Requires LayoutTests to exist at /tmp/LayoutTests
+        ignoreResultList.add("fast/loader/local-iFrame-source-from-local.html"); // Requires LayoutTests to exist at /tmp/LayoutTests
+        ignoreResultList.add("fast/loader/opaque-base-url.html"); // extra spacing because iFrames rendered next to each other on Apple
+        ignoreResultList.add("fast/parser/script-tag-with-trailing-slash.html"); // not capturing the console messages
+        ignoreResultList.add("fast/replaced/image-map.html"); // requires eventSender.mouseDown(),mouseUp()
+        ignoreResultList.add("fast/text/attributed-substring-from-range.html"); //  requires JS test API, textInputController
+        ignoreResultList.add("fast/text/attributed-substring-from-range-001.html"); //  requires JS test API, textInputController
+        ignoreResultList.add("fast/text/plain-text-line-breaks.html"); // extra spacing because iFrames rendered next to each other on Apple
     }
 
     static void fillBugTable() {
