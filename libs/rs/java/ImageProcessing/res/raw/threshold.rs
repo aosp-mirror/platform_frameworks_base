ec2746c33f10d4cd24823d217fa26a4b9f3eab76
Jason Sams <rjsams@android.com>
2010-02-10 15:03:24 -0800

    Fix ImageProcessing example.

--- libs/rs/java/ImageProcessing/res/raw/threshold.rs
+++ libs/rs/java/ImageProcessing/res/raw/threshold.rs
@@ -5,8 +5,8 @@ struct color_s {
     char a;
 };
 
-void filter(struct color_s *in, struct color_s *out, struct vec3_s *luminanceVector) {
-    struct vec3_s pixel;
+void filter(struct color_s *in, struct color_s *out, struct vecF32_3_s *luminanceVector) {
+    struct vecF32_3_s pixel;
     pixel.x = (in->r & 0xFF) / 255.0f;
     pixel.y = (in->g & 0xFF) / 255.0f;
     pixel.z = (in->b & 0xFF) / 255.0f;
@@ -22,10 +22,12 @@ void filter(struct color_s *in, struct color_s *out, struct vec3_s *luminanceVec
 }
 
 void main() {
+    int t = uptimeMillis();
+
     struct color_s *in = (struct color_s *) InPixel;
     struct color_s *out = (struct color_s *) OutPixel;
-    
-    struct vec3_s luminanceVector;
+
+    struct vecF32_3_s luminanceVector;
     luminanceVector.x = 0.2125f;
     luminanceVector.y = 0.7154f;
     luminanceVector.z = 0.0721f;
@@ -39,6 +41,9 @@ void main() {
         in++;
         out++;
     }
+
+    t= uptimeMillis() - t;
+    debugI32("Filter time", t);
 
     sendToClient(&count, 1, 4, 0);
 }
