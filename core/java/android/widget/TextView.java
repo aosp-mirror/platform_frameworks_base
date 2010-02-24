7a36788f4a69b3ddaf8fb8e250e1a13bee7238fa
Steve Zeigler <sfzeigler@gmail.com>
2010-02-23 16:39:08 -0800

    These changes add access to some status values in widgets listed below:
    widget/CheckedTextView.java:  report if the item is checked or not.
    
    widget/CompoundButton.java:  report if the item is checked or not.
    
    widget/ProgressBar.java:  isIndeterminate(), getProgress(), getSecondaryProgress(), and getMax() report what
    sliders and progress bars are showing
    
    widget/TextView.java: report the current selection:  getSelectionStart() and getSelectionEnd()

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -5726,6 +5726,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     /**
      * Convenience for {@link Selection#getSelectionStart}.
      */
+    @ViewDebug.ExportedProperty
     public int getSelectionStart() {
         return Selection.getSelectionStart(getText());
     }
@@ -5733,6 +5734,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     /**
      * Convenience for {@link Selection#getSelectionEnd}.
      */
+    @ViewDebug.ExportedProperty
     public int getSelectionEnd() {
         return Selection.getSelectionEnd(getText());
     }
