746c4c44645e022ee9b49acbb4a1bde3c95a7a45
Dianne Hackborn <hackbod@google.com>
2010-02-24 20:03:34 -0800

    Update to reflect android-common no longer in framework.

--- policy/Android.mk
+++ policy/Android.mk
@@ -6,6 +6,8 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
             $(call all-subdir-java-files)
+            
+LOCAL_STATIC_JAVA_LIBRARIES += android-common
 
 LOCAL_MODULE := android.policy_phone
 LOCAL_UNINSTALLABLE_MODULE := true
