61701ced749232165a96112f579ab82ba5040e38
James Dong <jdong@google.com>
2010-02-25 18:41:46 -0800

    Don't allow MediaRecorder.setParameter() to be invoked in the following states: error, prepared or recording
    bug - 2362412

--- media/libmedia/mediarecorder.cpp
+++ media/libmedia/mediarecorder.cpp
@@ -365,6 +365,15 @@ status_t MediaRecorder::setParameters(const String8& params) {
         return INVALID_OPERATION;
     }
 
+    bool isInvalidState = (mCurrentState &
+                           (MEDIA_RECORDER_PREPARED |
+                            MEDIA_RECORDER_RECORDING |
+                            MEDIA_RECORDER_ERROR));
+    if (isInvalidState) {
+        LOGE("setParameters is called in an invalid state: %d", mCurrentState);
+        return INVALID_OPERATION;
+    }
+
     status_t ret = mMediaRecorder->setParameters(params);
     if (OK != ret) {
         LOGE("setParameters(%s) failed: %d", params.string(), ret);
