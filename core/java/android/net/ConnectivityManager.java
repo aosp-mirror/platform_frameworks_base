8214deb542392f48b6c3fdc377fdf976c0b17a32
jsh <jsh@google.com>
2010-03-11 15:04:43 -0800

    Add definitions for WiMAX and iDen network types.
    b/2509185
    
    Change-Id: Icf07ce857874e200712218713edbdbb47e646297

--- core/java/android/net/ConnectivityManager.java
+++ core/java/android/net/ConnectivityManager.java
@@ -184,9 +184,15 @@ public class ConnectivityManager
      * will route over this interface if a default route exists.
      */
     public static final int TYPE_MOBILE_HIPRI = 5;
-    /** {@hide} */
+    /**
+     * The Default WiMAX data connection.  When active, all data traffic
+     * will use this connection by default.  Should not coexist with other
+     * default connections.
+     */
+    public static final int TYPE_WIMAX       = 6;
+    /** {@hide} TODO: Need to adjust this for WiMAX. */
     public static final int MAX_RADIO_TYPE   = TYPE_WIFI;
-    /** {@hide} */
+    /** {@hide} TODO: Need to adjust this for WiMAX. */
     public static final int MAX_NETWORK_TYPE = TYPE_MOBILE_HIPRI;
 
     public static final int DEFAULT_NETWORK_PREFERENCE = TYPE_WIFI;
