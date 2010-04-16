e577d72a3be20e589437271f0e5ca474fab38124
Roman Nurik <romannurik@google.com>
2010-04-16 15:57:34 -0700

    Revamp resources listing, part 1 (frameworks/base)
    - Revamp resource index page with big icons for resource types and a category listing.
    - Update resource TOC to show resource types and topics, instead of actual resource links.
    - Add resource browser/search results page (browser.html?tag=foo).
    - Add resources-data.js file containing metadata and links to all resources.
    
    Change-Id: Ieca87c6e42dda3c609824b71fb8a638abb11850c

--- docs/html/resources/resources_toc.cs
+++ docs/html/resources/resources_toc.cs
@@ -1,5 +1,56 @@
 <ul>
   <li>
+    <h2><span class="en">Technical Resources</span>
+    </h2>
+    <ul>
+      <li class="toggle-list">
+        <div><a href="<?cs var:toroot ?>resources/browser.html?tag=sample">
+            <span class="en">Sample Code</span>
+            <span class="de" style="display:none">Beispielcode</span>
+            <span class="es" style="display:none">Código de ejemplo</span>
+            <span class="fr" style="display:none">Exemple de code</span>
+            <span class="it" style="display:none">Codice di esempio</span>
+            <span class="ja" style="display:none">サンプル コード</span>
+            <span class="zh-CN" style="display:none"></span>
+            <span class="zh-TW" style="display:none"></span>
+          </a></div>
+        <ul id="devdoc-nav-sample-list">
+          <li><a href="<?cs var:toroot ?>resources/samples/get.html">
+                <span class="en">Getting the Samples</span>
+              </a></li>
+        </ul>
+      </li>
+      <li class="toggle-list">
+        <div><a href="<?cs var:toroot ?>resources/browser.html?tag=article">
+               <span class="en">Articles</span>
+             </a></div>
+        <ul id="devdoc-nav-article-list">
+        </ul>
+      </li>
+      <li class="toggle-list">
+        <div><a href="<?cs var:toroot ?>resources/browser.html?tag=tutorial">
+               <span class="en">Tutorials</span>
+               <span class="de" style="display:none">Lernprogramme</span>
+               <span class="es" style="display:none">Tutoriales</span>
+               <span class="fr" style="display:none">Didacticiels</span>
+               <span class="it" style="display:none">Esercitazioni</span>
+               <span class="ja" style="display:none">チュートリアル</span>
+               <span class="zh-CN" style="display:none"></span>
+               <span class="zh-TW" style="display:none"></span>
+             </a></div>
+        <ul id="devdoc-nav-tutorial-list">
+        </ul>
+      </li>
+      <li class="toggle-list">
+        <div><a href="<?cs var:toroot ?>resources/topics.html">
+               <span class="en">Topics</span>
+             </a></div>
+        <ul id="devdoc-nav-topic-list">
+        </ul>
+      </li>
+    </ul>
+  </li>
+  <li>
     <h2><span class="en">Community</span>
                <span style="display:none" class="de"></span>
                <span style="display:none" class="es">Comunidad</span>
@@ -34,241 +85,6 @@
   </li><?cs
   /if
 ?>
-
-  <li>
-    <h2><span class="en">Technical Articles</span>
-    </h2>
-    <ul>
-      <li class="toggle-list">
-        <div><a href="<?cs var:toroot ?>resources/articles/index.html">
-               <span class="en">List of Articles</span>
-             </a></div>
-        <ul>
-        <li><a href="<?cs var:toroot ?>resources/articles/avoiding-memory-leaks.html">
-                <span class="en">Avoiding Memory Leaks</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/backward-compatibility.html">
-                <span class="en">Backward Compatibility</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/can-i-use-this-intent.html">
-                <span class="en">Can I Use this Intent?</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/creating-input-method.html">
-                <span class="en">Creating an Input Method</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/drawable-mutations.html">
-                <span class="en">Drawable Mutations</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/faster-screen-orientation-change.html">
-                <span class="en">Faster Screen Orientation Change</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/future-proofing.html">
-                <span class="en">Future-Proofing Your Apps</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/gestures.html">
-                <span class="en">Gestures</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/glsurfaceview.html">
-                <span class="en">Introducing GLSurfaceView</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/layout-tricks-reuse.html">
-                <span class="en">Layout Tricks: Reusing </span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/layout-tricks-efficiency.html">
-                <span class="en">Layout Tricks: Efficiency</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/layout-tricks-stubs.html">
-                <span class="en">Layout Tricks: ViewStubs </span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/layout-tricks-merge.html">
-                <span class="en">Layout Tricks: Merging </span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/listview-backgrounds.html">
-                <span class="en">ListView Backgrounds</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/live-folders.html">
-                <span class="en">Live Folders</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/live-wallpapers.html">
-                <span class="en">Live Wallpapers</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/on-screen-inputs.html">
-                <span class="en">Onscreen Input Methods</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/painless-threading.html">
-                <span class="en">Painless Threading</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/qsb.html">
-                <span class="en">Quick Search Box</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/speech-input.html">
-                <span class="en">Speech Input</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/touch-mode.html">
-                <span class="en">Touch Mode</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/track-mem.html">
-                <span class="en">Tracking Memory Allocations</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/ui-1.5.html">
-                <span class="en">UI Framework Changes in Android 1.5</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/ui-1.6.html">
-                <span class="en">UI Framework Changes in Android 1.6</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/timed-ui-updates.html">
-                <span class="en">Updating the UI from a Timer</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/tts.html">
-                <span class="en">Using Text-to-Speech</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/contacts.html">
-                <span class="en">Using the Contacts API</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/using-webviews.html">
-                <span class="en">Using WebViews</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/wikinotes-linkify.html">
-                <span class="en">WikiNotes: Linkify your Text!</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/wikinotes-intents.html">
-                <span class="en">WikiNotes: Routing Intents</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/window-bg-speed.html">
-                <span class="en">Window Backgrounds &amp; UI Speed</span>
-                </a></li>
-        <li><a href="<?cs var:toroot ?>resources/articles/zipalign.html">
-                <span class="en">Zipalign: An Easy Optimization</span>
-                </a></li>
-        </ul>
-      </li>
-    </ul>
-   </li>
-
-  <li>
-    <h2><span class="en">Tutorials</span>
-               <span class="de" style="display:none">Lernprogramme</span>
-               <span class="es" style="display:none">Tutoriales</span>
-               <span class="fr" style="display:none">Didacticiels</span>
-               <span class="it" style="display:none">Esercitazioni</span>
-               <span class="ja" style="display:none">チュートリアル</span>
-               <span class="zh-CN" style="display:none"></span>
-               <span class="zh-TW" style="display:none"></span>
-    </h2>
-    <ul>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/hello-world.html">
-            <span class="en">Hello World</span>
-          </a></li>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/views/index.html">
-            <span class="en">Hello Views</span>
-          </a></li>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/localization/index.html">
-            <span class="en">Hello Localization</span>
-          </a></li>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/testing/helloandroid_test.html">
-            <span class="en">Hello Testing</span></a>
-            <span class="new">new!</span>
-      </li>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/notepad/index.html">
-            <span class="en">Notepad Tutorial</span>
-          </a></li>
-      <li><a href="<?cs var:toroot ?>resources/tutorials/testing/activity_test.html">
-            <span class="en">Activity Testing</span></a>
-            <span class="new">new!</span>
-      </li>
-    </ul>
-  </li>
-
-
-  <li>
-    <h2><span class="en">Sample Code</span>
-               <span class="de" style="display:none">Beispielcode</span>
-               <span class="es" style="display:none">Código de ejemplo</span>
-               <span class="fr" style="display:none">Exemple de code</span>
-               <span class="it" style="display:none">Codice di esempio</span>
-               <span class="ja" style="display:none">サンプル コード</span>
-               <span class="zh-CN" style="display:none"></span>
-               <span class="zh-TW" style="display:none"></span>
-    </h2>
-    <ul>
-      <li><a href="<?cs var:toroot ?>resources/samples/get.html">
-            <span class="en">Getting the Samples</span>
-          </a></li>
-      <li class="toggle-list">
-        <div><a href="<?cs var:toroot ?>resources/samples/index.html">
-               <span class="en">List of Samples</span>
-             </a></div>
-        <ul>
-          <li><a href="<?cs var:toroot ?>resources/samples/ApiDemos/index.html">
-                <span class="en">API Demos</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/BackupRestore/index.html">
-                <span class="en">Backup and Restore</span>
-              </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/BluetoothChat/index.html">
-                <span class="en">Bluetooth Chat</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/BusinessCard/index.html">
-                <span class="en">Business Card</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/ContactManager/index.html">
-                <span class="en">Contact Manager</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/Home/index.html">
-                <span class="en">Home</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/JetBoy/index.html">
-                <span class="en">JetBoy</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/CubeLiveWallpaper/index.html">
-                <span class="en">Live Wallpaper</span>
-                </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/LunarLander/index.html">
-                <span class="en">Lunar Lander</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/MultiResolution/index.html">
-                <span class="en">Multiple Resolutions</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/NotePad/index.html">
-                <span class="en">Note Pad</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/SampleSyncAdapter/index.html">
-                <span class="en">Sample Sync Adapter</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/SearchableDictionary/index.html">
-                <span class="en">Searchable Dictionary v2</span>
-              </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/Snake/index.html">
-                <span class="en">Snake</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/SoftKeyboard/index.html">
-                <span class="en">Soft Keyboard</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/Spinner/index.html">
-                <span class="en">Spinner</span>
-                </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/SpinnerTest/index.html">
-                <span class="en">SpinnerTest</span>
-                </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/TicTacToeLib/index.html">
-                <span class="en">TicTacToeLib</span>
-                </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/TicTacToeMain/index.html">
-                <span class="en">TicTacToeMain</span>
-                </a> <span class="new">new!</span></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/Wiktionary/index.html">
-                <span class="en">Wiktionary</span>
-              </a></li>
-          <li><a href="<?cs var:toroot ?>resources/samples/WiktionarySimple/index.html">
-                <span class="en">Wiktionary (Simplified)</span>
-              </a></li>
-        </ul>
-      </li>
-    </ul>
-  </li>
-
-
-
   <li>
     <h2><span class="en">More</span>
     </h2>
@@ -297,8 +113,6 @@
      </li>
     </ul>
   </li>
-
-
 </ul>
 
 <script type="text/javascript">
