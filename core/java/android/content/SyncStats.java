20c640e0f4c11f56cb02abaac0a25fe84ebfa487
Fred Quintana <fredq@google.com>
2010-04-26 17:38:56 -0700

    Add JavaDoc for SyncResult http://b/2420252
    Change-Id: I88be5232d54959f6ec3480c36751a8fb7dc369fd

--- core/java/android/content/SyncStats.java
+++ core/java/android/content/SyncStats.java
@@ -20,17 +20,77 @@ import android.os.Parcelable;
 import android.os.Parcel;
 
 /**
- * @hide
+ * Used to record various statistics about the result of a sync operation. The SyncManager
+ * gets access to these via a {@link SyncResult} and uses some of them to determine the
+ * disposition of the sync. See {@link SyncResult} for further dicussion on how the
+ * SyncManager uses these values.
  */
 public class SyncStats implements Parcelable {
+    /**
+     * The SyncAdapter was unable to authenticate the {@link android.accounts.Account}
+     * that was specified in the request. The user needs to take some action to resolve
+     * before a future request can expect to succeed. This is considered a hard error.
+     */
     public long numAuthExceptions;
+
+    /**
+     * The SyncAdapter had a problem, most likely with the network connectivity or a timeout
+     * while waiting for a network response. The request may succeed if it is tried again
+     * later. This is considered a soft error.
+     */
     public long numIoExceptions;
+
+    /**
+     * The SyncAdapter had a problem with the data it received from the server or the storage
+     * later. This problem will likely repeat if the request is tried again. The problem
+     * will need to be cleared up by either the server or the storage layer (likely with help
+     * from the user). If the SyncAdapter cleans up the data itself then it typically won't
+     * increment this value although it may still do so in order to record that it had to
+     * perform some cleanup. E.g., if the SyncAdapter received a bad entry from the server
+     * when processing a feed of entries, it may choose to drop the entry and thus make
+     * progress and still increment this value just so the SyncAdapter can record that an
+     * error occurred. This is considered a hard error.
+     */
     public long numParseExceptions;
+
+    /**
+     * The SyncAdapter detected that there was an unrecoverable version conflict when it
+     * attempted to update or delete a version of a resource on the server. This is expected
+     * to clear itself automatically once the new state is retrieved from the server,
+     * though it may remain until the user intervenes manually, perhaps by clearing the
+     * local storage and starting over frmo scratch. This is considered a hard error.
+     */
     public long numConflictDetectedExceptions;
+
+    /**
+     * Counter for tracking how many inserts were performed by the sync operation, as defined
+     * by the SyncAdapter.
+     */
     public long numInserts;
+
+    /**
+     * Counter for tracking how many updates were performed by the sync operation, as defined
+     * by the SyncAdapter.
+     */
     public long numUpdates;
+
+    /**
+     * Counter for tracking how many deletes were performed by the sync operation, as defined
+     * by the SyncAdapter.
+     */
     public long numDeletes;
+
+    /**
+     * Counter for tracking how many entries were affected by the sync operation, as defined
+     * by the SyncAdapter.
+     */
     public long numEntries;
+
+    /**
+     * Counter for tracking how many entries, either from the server or the local store, were
+     * ignored during the sync operation. This could happen if the SyncAdapter detected some
+     * unparsable data but decided to skip it and move on rather than failing immediately.
+     */
     public long numSkippedEntries;
 
     public SyncStats() {
@@ -75,6 +135,9 @@ public class SyncStats implements Parcelable {
         return sb.toString();
     }
 
+    /**
+     * Reset all the counters to 0.
+     */
     public void clear() {
         numAuthExceptions = 0;
         numIoExceptions = 0;
