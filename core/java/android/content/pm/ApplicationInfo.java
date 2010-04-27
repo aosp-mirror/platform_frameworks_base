3202d380226043fa665df3c92252f791f8c52d55
Dianne Hackborn <hackbod@google.com>
2010-04-26 17:51:34 -0700

    Fix issue #2631417: Expose FLAG_EXTERNAL_STORAGE
    Also update 8.xml to match the current API.
    
    Change-Id: I668fe56f6c156c2e8daa458baa7410fdb8ebef52

--- core/java/android/content/pm/ApplicationInfo.java
+++ core/java/android/content/pm/ApplicationInfo.java
@@ -252,20 +252,22 @@ public class ApplicationInfo extends PackageItemInfo implements Parcelable {
     public static final int FLAG_RESTORE_ANY_VERSION = 1<<17;
 
     /**
+     * Value for {@link #flags}: Set to true if the application is
+     * currently installed on external/removable/unprotected storage.  Such
+     * applications may not be available if their storage is not currently
+     * mounted.  When the storage it is on is not available, it will look like
+     * the application has been uninstalled (its .apk is no longer available)
+     * but its persistent data is not removed.
+     */
+    public static final int FLAG_EXTERNAL_STORAGE = 1<<19;
+
+    /**
      * Value for {@link #flags}: Set to true if the application has been
      * installed using the forward lock option.
      *
      * {@hide}
      */
-    public static final int FLAG_FORWARD_LOCK = 1<<19;
-
-    /**
-     * Value for {@link #flags}: Set to true if the application is
-     * currently installed on the sdcard.
-     *
-     * {@hide}
-     */
-    public static final int FLAG_EXTERNAL_STORAGE = 1<<20;
+    public static final int FLAG_FORWARD_LOCK = 1<<20;
 
     /**
      * Value for {@link #flags}: Set to true if the application is
