0c388fd29cf685a722ca862fe1af472b1c7e2a1f
Jean-Baptiste Queru <jbq@google.com>
2010-05-03 09:50:38 -0700

    Adapt to new location of libcore
    Change-Id: Ie4fb163fb9f04a96f8412a996d43321accb28cc8

--- Android.mk
+++ Android.mk
@@ -272,7 +272,7 @@ fwbase_dirs_to_document := \
 # as "final" in the official SDK APIs.
 fwbase_dirs_to_document += core/config/sdk
 
-# These are relative to dalvik/libcore
+# These are relative to libcore
 # Intentionally not included from libcore:
 #     icu openssl suncompat support
 libcore_to_document := \
@@ -312,7 +312,7 @@ non_base_dirs := \
 dirs_to_document := \
 	$(fwbase_dirs_to_document) \
 	$(non_base_dirs) \
-	$(addprefix ../../dalvik/libcore/, $(libcore_to_document))
+	$(addprefix ../../libcore/, $(libcore_to_document))
 
 html_dirs := \
 	$(FRAMEWORKS_BASE_SUBDIRS) \
