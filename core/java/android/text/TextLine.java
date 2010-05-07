0b9d2ca6b6fc4574898cfff6acdee821d4723a96
Doug Felt <dougfelt@google.com>
2010-05-06 17:10:54 -0700

    Fix selection off-by-one bug.
    The wrong value was used to measure the width of unidirectional text.
    
    Change-Id: I57752c111641ca1cc951e0647b60c79f5a9493ad

--- core/java/android/text/TextLine.java
+++ core/java/android/text/TextLine.java
@@ -295,10 +295,10 @@ class TextLine {
 
         if (!mHasTabs) {
             if (mDirections == Layout.DIRS_ALL_LEFT_TO_RIGHT) {
-                return measureRun( 0, 0, target, mLen, false, fmi);
+                return measureRun( 0, 0, offset, mLen, false, fmi);
             }
             if (mDirections == Layout.DIRS_ALL_RIGHT_TO_LEFT) {
-                return measureRun(0, 0, target, mLen, true, fmi);
+                return measureRun(0, 0, offset, mLen, true, fmi);
             }
         }
 
