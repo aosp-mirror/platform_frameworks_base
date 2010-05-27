acea08d20ff34248814e14f3e3bafc86825c2d72
Leon Scroggins <scroggo@google.com>
2010-05-27 15:09:32 -0400

    Prevent a null pointer exception.
    Bug 2721268
    
    Change-Id: I3a9e838ed3f2f9dccdf7b4bf42a0dbad58e62e02

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -3459,7 +3459,9 @@ public class WebView extends AbsoluteLayout
      *  @param  end     End of selection.
      */
     /* package */ void setSelection(int start, int end) {
-        mWebViewCore.sendMessage(EventHub.SET_SELECTION, start, end);
+        if (mWebViewCore != null) {
+            mWebViewCore.sendMessage(EventHub.SET_SELECTION, start, end);
+        }
     }
 
     @Override
