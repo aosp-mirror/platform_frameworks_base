a34c9ca30ea25d0c4d8ae51da7858c4ea58b7f25
Kenny Root <kroot@google.com>
2010-06-01 13:23:53 -0700

    Add more error checking for ndc
    In NativeDaemonConnector.doCommand() calls, there was inconsistent error
    checking. This change adds error checking for every call and makes it so
    that any call to .doCommand() that gets an error code won't cause the
    code to hang forever.
    
    Change-Id: If714282b6642f278fb8137f652af1a012670253b

--- services/java/com/android/server/NativeDaemonConnector.java
+++ services/java/com/android/server/NativeDaemonConnector.java
@@ -128,12 +128,11 @@ final class NativeDaemonConnector implements Runnable {
                                     Slog.e(TAG, String.format(
                                             "Error handling '%s'", event), ex);
                                 }
-                            } else {
-                                try {
-                                    mResponseQueue.put(event);
-                                } catch (InterruptedException ex) {
-                                    Slog.e(TAG, "Failed to put response onto queue", ex);
-                                }
+                            }
+                            try {
+                                mResponseQueue.put(event);
+                            } catch (InterruptedException ex) {
+                                Slog.e(TAG, "Failed to put response onto queue", ex);
                             }
                         } catch (NumberFormatException nfe) {
                             Slog.w(TAG, String.format("Bad msg (%s)", event));
