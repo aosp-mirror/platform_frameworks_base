3e168335b3422008908ac6973bb36078fd979f35
Romain Guy <romainguy@google.com>
2010-06-22 13:29:14 -0700

    Fix simulator build again.
    Change-Id: Ifd204d64eaa4c356422e3363ec0a6e6aa61c52dc

--- libs/hwui/Android.mk
+++ libs/hwui/Android.mk
@@ -1,6 +1,3 @@
-# Does not build for the simulator
-ifneq ($(TARGET_SIMULATOR), true)
-
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -23,5 +20,3 @@ LOCAL_MODULE:= libhwui
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif #simulator
--- libs/hwui/Matrix.cpp
+++ libs/hwui/Matrix.cpp
@@ -18,6 +18,7 @@
 
 #include <math.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <utils/Log.h>
 
