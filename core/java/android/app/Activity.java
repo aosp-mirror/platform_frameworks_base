3c80a4a044865bdf1289c7896baffa1c082d835c
Dianne Hackborn <hackbod@google.com>
2010-06-29 19:20:40 -0700

    Implement default key handling for native code.
    The native code now maintains a list of all keys that may use
    default handling.  If the app finishes one of these keys
    without handling it, the key will be passed back off to Java
    for default treatment.
    
    Change-Id: I6a842a0d728eeafa4de7142fae573f8c11099e18

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -39,7 +39,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
-import android.os.Looper;
 import android.os.RemoteException;
 import android.text.Selection;
 import android.text.SpannableStringBuilder;
