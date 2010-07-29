8dc16c27e23dae5bdf65fd2a2ced97c2f645a633
Mike Lockwood <lockwood@android.com>
2010-07-29 11:39:55 -0400

    Fix typo in GPS mode logic.
    MS-Assisted support should not be required for using MS-Based mode.
    
    Change-Id: Ia6ee219674de3e9cc13190b753f8776305a4fa90
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -892,7 +892,7 @@ public class GpsLocationProvider implements LocationProviderInterface {
                     Settings.Secure.ASSISTED_GPS_ENABLED, 1) != 0) {
                 if (singleShot && hasCapability(GPS_CAPABILITY_MSA)) {
                     mPositionMode = GPS_POSITION_MODE_MS_ASSISTED;
-                } else if (hasCapability(GPS_CAPABILITY_MSA)) {
+                } else if (hasCapability(GPS_CAPABILITY_MSB)) {
                     mPositionMode = GPS_POSITION_MODE_MS_BASED;
                 }
             }
