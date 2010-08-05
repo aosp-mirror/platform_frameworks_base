d7b421b4de68d803252a5d9b3cb0106731217ad7
Kenny Root <kroot@google.com>
2010-08-05 08:40:00 -0700

    Fix up PackageManager tests
    PackageManagerTests was trying to mount and unmount the media without
    waiting for the correct state to arrive. It was proceeding with any
    state transition.
    
    Some tests had their own volume mounting and unmounting code that was
    flawed, so switch it to the more robust mountMedia() and unmountMedia()
    implementations.
    
    Change-Id: If464927bb09dd15cb040c6cce195a6ad8452de95

--- core/tests/coretests/src/android/os/storage/StorageListener.java
+++ core/tests/coretests/src/android/os/storage/StorageListener.java
@@ -21,21 +21,24 @@ import android.util.Log;
 public class StorageListener extends StorageEventListener {
     private static final boolean localLOGV = true;
 
-    public static final String TAG="StorageListener";
+    public static final String TAG = "StorageListener";
 
-    String oldState;
-    String newState;
-    String path;
+    private String mTargetState;
     private boolean doneFlag = false;
+
+    public StorageListener(String targetState) {
+        mTargetState = targetState;
+    }
+
     @Override
     public void onStorageStateChanged(String path, String oldState, String newState) {
         if (localLOGV) Log.i(TAG, "Storage state changed from " + oldState + " to " + newState);
+
         synchronized (this) {
-            this.oldState = oldState;
-            this.newState = newState;
-            this.path = path;
-            doneFlag = true;
-            notifyAll();
+            if (mTargetState.equals(newState)) {
+                doneFlag = true;
+                notifyAll();
+            }
         }
     }
 
