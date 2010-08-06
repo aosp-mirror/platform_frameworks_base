5723dfe8c87d0887b8926f43637ed300466890d2
Mike Lockwood <lockwood@android.com>
2010-08-06 10:06:26 -0400

    GPS: Don't set XTRA download pending flag at boot time.
    XTRA data downloads are now strictly on demand from the GPS engine.
    Also fix typo in handleDownloadXtraData()
    
    Change-Id: Ied1a6e2e62134add4d965326aae909c86f834682
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- services/java/com/android/server/location/GpsLocationProvider.java
+++ services/java/com/android/server/location/GpsLocationProvider.java
@@ -177,7 +177,7 @@ public class GpsLocationProvider implements LocationProviderInterface {
     // flags to trigger NTP or XTRA data download when network becomes available
     // initialized to true so we do NTP and XTRA when the network comes up after booting
     private boolean mInjectNtpTimePending = true;
-    private boolean mDownloadXtraDataPending = true;
+    private boolean mDownloadXtraDataPending = false;
 
     // true if GPS is navigating
     private boolean mNavigating;
@@ -495,7 +495,7 @@ public class GpsLocationProvider implements LocationProviderInterface {
     }
 
     private void handleDownloadXtraData() {
-        if (!mDownloadXtraDataPending) {
+        if (!mNetworkAvailable) {
             // try again when network is up
             mDownloadXtraDataPending = true;
             return;
