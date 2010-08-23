2703a42d16af0e62da1bba02b6c935d98debf936
Gilles Debunne <debunne@google.com>
2010-08-23 15:14:03 -0700

    When an EditText gains focus by tapping, move the insertion point where tapped.
    The previous behavior was to move cursor at the end of the first line,
    which feels weird.
    
    Change-Id: I5a72f9871ed79ee2c521698ea642ba126537f4f9

--- core/java/android/text/method/ArrowKeyMovementMethod.java
+++ core/java/android/text/method/ArrowKeyMovementMethod.java
@@ -319,25 +319,9 @@ public class ArrowKeyMovementMethod implements MovementMethod {
 
     public void onTakeFocus(TextView view, Spannable text, int dir) {
         if ((dir & (View.FOCUS_FORWARD | View.FOCUS_DOWN)) != 0) {
-            Layout layout = view.getLayout();
-
-            if (layout == null) {
-                /*
-                 * This shouldn't be null, but do something sensible if it is.
-                 */
+            if (view.getLayout() == null) {
+                // This shouldn't be null, but do something sensible if it is.
                 Selection.setSelection(text, text.length());
-            } else {
-                /*
-                 * Put the cursor at the end of the first line, which is
-                 * either the last offset if there is only one line, or the
-                 * offset before the first character of the second line
-                 * if there is more than one line.
-                 */
-                if (layout.getLineCount() == 1) {
-                    Selection.setSelection(text, text.length());
-                } else {
-                    Selection.setSelection(text, layout.getLineStart(1) - 1);
-                }
             }
         } else {
             Selection.setSelection(text, text.length());
