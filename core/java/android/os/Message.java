a334e7c72408c4e2805f2427a35d841a60adefc4
Wink Saville <wink@google.com>
2010-08-24 10:56:30 -0700

    Allow reliable detection of a message that is in use.
    Because the standard Looper.loop code calls Message#recycle it is
    imperative that Handler#handleMessage code not attempt to resue
    a message it receives. If allowed to do so it will cause bugs that
    could be difficult to diagnois.
    
    This change adds Message#flags and uses one bit to reliably detect
    a message is in use and throws an error in MessageQueue#enqueueMessage.
    This allows early detection of this bug.
    
    Note: This is not new functionality, but the current implementation does
    not detect messages that are in use because it uses Message#when != 0
    as the detection mechanism. The problem is that a Message#when value of 0
    is valid value used to place a message at the front of the queue and is
    thus unreliable.
    
    Another option is to change the setting of Message#when in Message#enqueueMessage
    so that it is never 0, although that does change subtly a publicly accessible
    field.
    
    Yet another option would be to use other fields but all candidates have
    similar problems as when in that they are publicly accessible or even
    settable such as Message#target.
    
    Change-Id: I040d6e546376f7b1ed1e4daa0d5644cce8bf333a

--- core/java/android/os/Message.java
+++ core/java/android/os/Message.java
@@ -73,7 +73,18 @@ public final class Message implements Parcelable {
      * receiver.
      */
     public Messenger replyTo;
-    
+
+    /** If set message is in use */
+    /*package*/ static final int FLAG_IN_USE = 1;
+
+    /** Flags reserved for future use (All are reserved for now) */
+    /*package*/ static final int FLAGS_RESERVED = ~FLAG_IN_USE;
+
+    /** Flags to clear in the copyFrom method */
+    /*package*/ static final int FLAGS_TO_CLEAR_ON_COPY_FROM = FLAGS_RESERVED | FLAG_IN_USE;
+
+    /*package*/ int flags;
+
     /*package*/ long when;
     
     /*package*/ Bundle data;
@@ -253,6 +264,7 @@ public final class Message implements Parcelable {
      * target/callback of the original message.
      */
     public void copyFrom(Message o) {
+        this.flags = o.flags & ~FLAGS_TO_CLEAR_ON_COPY_FROM;
         this.what = o.what;
         this.arg1 = o.arg1;
         this.arg2 = o.arg2;
@@ -350,6 +362,7 @@ public final class Message implements Parcelable {
     }
 
     /*package*/ void clearForRecycle() {
+        flags = 0;
         what = 0;
         arg1 = 0;
         arg2 = 0;
@@ -359,6 +372,14 @@ public final class Message implements Parcelable {
         target = null;
         callback = null;
         data = null;
+    }
+
+    /*package*/ boolean isInUse() {
+        return ((flags & FLAG_IN_USE) == FLAG_IN_USE);
+    }
+
+    /*package*/ void markInUse() {
+        flags |= FLAG_IN_USE;
     }
 
     /** Constructor (but the preferred way to get a Message is to call {@link #obtain() Message.obtain()}).
@@ -453,4 +474,3 @@ public final class Message implements Parcelable {
         replyTo = Messenger.readMessengerOrNullFromParcel(source);
     }
 }
-
