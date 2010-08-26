382872e3fa45fb1433976b7bda4b26bbc2072f71
Joe Onorato <joeo@google.com>
2010-08-26 14:46:01 -0400

    Continue trying to make the turn off quickly after call feature work.
    The previous implementation worked if the set timeout is short enough, which mine was.  This one
    carries the remaining amount of timeout override forward through all of the countdown steps.
    
    Bug: 2844990
    Change-Id: I040df22f9f9ddf98c355ac6845b7624e95f84f33

--- services/java/com/android/server/PowerManagerService.java
+++ services/java/com/android/server/PowerManagerService.java
@@ -1029,11 +1029,10 @@ class PowerManagerService extends IPowerManager.Stub
     // If they gave a timeoutOverride it is the number of seconds
     // to screen-off.  Figure out where in the countdown cycle we
     // should jump to.
-    private void setTimeoutLocked(long now, long timeoutOverride, int nextState) {
+    private void setTimeoutLocked(long now, final long originalTimeoutOverride, int nextState) {
+        long timeoutOverride = originalTimeoutOverride;
         if (mBootCompleted) {
             synchronized (mLocks) {
-                mHandler.removeCallbacks(mTimeoutTask);
-                mTimeoutTask.nextState = nextState;
                 long when = 0;
                 if (timeoutOverride <= 0) {
                     switch (nextState)
@@ -1084,6 +1083,12 @@ class PowerManagerService extends IPowerManager.Stub
                             + " timeoutOverride=" + timeoutOverride
                             + " nextState=" + nextState + " when=" + when);
                 }
+
+                mHandler.removeCallbacks(mTimeoutTask);
+                mTimeoutTask.nextState = nextState;
+                mTimeoutTask.remainingTimeoutOverride = timeoutOverride > 0
+                        ? (originalTimeoutOverride - timeoutOverride)
+                        : -1;
                 mHandler.postAtTime(mTimeoutTask, when);
                 mNextTimeout = when; // for debugging
             }
@@ -1099,6 +1104,7 @@ class PowerManagerService extends IPowerManager.Stub
     private class TimeoutTask implements Runnable
     {
         int nextState; // access should be synchronized on mLocks
+        long remainingTimeoutOverride;
         public void run()
         {
             synchronized (mLocks) {
@@ -1119,11 +1125,11 @@ class PowerManagerService extends IPowerManager.Stub
                 {
                     case SCREEN_BRIGHT:
                         if (mDimDelay >= 0) {
-                            setTimeoutLocked(now, SCREEN_DIM);
+                            setTimeoutLocked(now, remainingTimeoutOverride, SCREEN_DIM);
                             break;
                         }
                     case SCREEN_DIM:
-                        setTimeoutLocked(now, SCREEN_OFF);
+                        setTimeoutLocked(now, remainingTimeoutOverride, SCREEN_OFF);
                         break;
                 }
             }
@@ -2054,6 +2060,7 @@ class PowerManagerService extends IPowerManager.Stub
                         + " mUserState=0x" + Integer.toHexString(mUserState)
                         + " mWakeLockState=0x" + Integer.toHexString(mWakeLockState)
                         + " mProximitySensorActive=" + mProximitySensorActive
+                        + " timeoutOverride=" + timeoutOverride
                         + " force=" + force);
             }
             // ignore user activity if we are in the process of turning off the screen
