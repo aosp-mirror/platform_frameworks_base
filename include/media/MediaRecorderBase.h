b33a5aea130b025f30966828562fcba56f25b265
Nipun Kwatra <nkwatra@google.com>
2010-08-26 17:20:53 -0700

    Added setAuxiliaryOutputFile to MediaRecorder and JNI
    added setAuxiliaryOutputFile to allow setting of an auxiliary output file.
    Also added the JNI support.
    
    Change-Id: I8c3335192bd6f7fcbfdfc8552cfd0848f2ad2a5d

--- include/media/MediaRecorderBase.h
+++ include/media/MediaRecorderBase.h
@@ -40,6 +40,7 @@ struct MediaRecorderBase {
     virtual status_t setPreviewSurface(const sp<Surface>& surface) = 0;
     virtual status_t setOutputFile(const char *path) = 0;
     virtual status_t setOutputFile(int fd, int64_t offset, int64_t length) = 0;
+    virtual status_t setOutputFileAuxiliary(int fd) {return INVALID_OPERATION;}
     virtual status_t setParameters(const String8& params) = 0;
     virtual status_t setListener(const sp<IMediaRecorderClient>& listener) = 0;
     virtual status_t prepare() = 0;
