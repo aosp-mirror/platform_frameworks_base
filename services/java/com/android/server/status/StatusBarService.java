43a87c5c874f1b8fbf582ff522f10051a6434906
Tony Wu <Tony_Wu@acer.com.tw>
2010-10-07 17:53:43 +0800

    Fix sometimes mExpandedView will appear while screen transforming.
    While watching YouTube and receiving a call, after hang up the call,
    mExpandedView may flash when screen transform from Phone to YouTube.
    
    Change-Id: Ib25a0cd1086c278b91a2b34e21744a3c432a8f35

--- services/java/com/android/server/status/StatusBarService.java
+++ services/java/com/android/server/status/StatusBarService.java
@@ -289,6 +289,7 @@ public class StatusBarService extends IStatusBar.Stub
         mScrollView = (ScrollView)expanded.findViewById(R.id.scroll);
         mNotificationLinearLayout = expanded.findViewById(R.id.notificationLinearLayout);
 
+        mExpandedView.setVisibility(View.GONE);
         mOngoingTitle.setVisibility(View.GONE);
         mLatestTitle.setVisibility(View.GONE);
         
@@ -970,6 +971,7 @@ public class StatusBarService extends IStatusBar.Stub
         mExpandedDialog.getWindow().setAttributes(mExpandedParams);
         mExpandedView.requestFocus(View.FOCUS_FORWARD);
         mTrackingView.setVisibility(View.VISIBLE);
+        mExpandedView.setVisibility(View.VISIBLE);
         
         if (!mTicking) {
             setDateViewVisibility(true, com.android.internal.R.anim.fade_in);
@@ -1054,6 +1056,7 @@ public class StatusBarService extends IStatusBar.Stub
         mExpandedParams.flags &= ~WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
         mExpandedDialog.getWindow().setAttributes(mExpandedParams);
         mTrackingView.setVisibility(View.GONE);
+        mExpandedView.setVisibility(View.GONE);
 
         if ((mDisabled & StatusBarManager.DISABLE_NOTIFICATION_ICONS) == 0) {
             setNotificationIconVisibility(true, com.android.internal.R.anim.fade_in);
