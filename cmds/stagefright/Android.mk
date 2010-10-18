ac2a88061dc9a1ade73b3e76a033238a8be4cb98
Jean-Baptiste Queru <jbq@google.com>
2010-10-18 08:40:53 -0700

    Remove one more opencore include path
    Bug: 3101573
    Change-Id: Id60845e4c46523e910504fe6e707f8247a3b6102

--- cmds/stagefright/Android.mk
+++ cmds/stagefright/Android.mk
@@ -61,7 +61,7 @@ LOCAL_SHARED_LIBRARIES := \
 LOCAL_C_INCLUDES:= \
 	$(JNI_H_INCLUDE) \
 	frameworks/base/media/libstagefright \
-	$(TOP)/external/opencore/extern_libs_v2/khronos/openmax/include
+	$(TOP)/frameworks/base/include/media/stagefright/openmax
 
 LOCAL_CFLAGS += -Wno-multichar
 
