120a4594855951ed5eb185fdfc19bf98efef3ba2
Jeff Brown <jeffbrown@google.com>
2010-10-27 18:43:51 -0700

    Drop all dispatcher state when dispatcher is disabled.
    This patch makes the dispatcher drop all of its state when it is
    disabled (when the screen turns off).  This ensures that the dispatcher
    does not get stuck thinking a pointer is still down if the screen
    turned off while the user was touching the display (such as a fat touch
    while hitting the power button).
    
    Bug: 3098344
    Change-Id: If50ef5804870aa1acd3179fd4b40e3cda58dd39d

--- libs/ui/InputDispatcher.cpp
+++ libs/ui/InputDispatcher.cpp
@@ -1302,6 +1302,9 @@ Failed:
     }
 
 Unresponsive:
+    // Reset temporary touch state to ensure we release unnecessary references to input channels.
+    mTempTouchState.reset();
+
     nsecs_t timeSpentWaitingForApplication = getTimeSpentWaitingForApplicationLocked(currentTime);
     updateDispatchStatisticsLocked(currentTime, entry,
             injectionResult, timeSpentWaitingForApplication);
@@ -2586,8 +2589,12 @@ void InputDispatcher::setInputDispatchMode(bool enabled, bool frozen) {
         AutoMutex _l(mLock);
 
         if (mDispatchEnabled != enabled || mDispatchFrozen != frozen) {
-            if (mDispatchFrozen && ! frozen) {
+            if (mDispatchFrozen && !frozen) {
                 resetANRTimeoutsLocked();
+            }
+
+            if (mDispatchEnabled && !enabled) {
+                resetAndDropEverythingLocked("dispatcher is being disabled");
             }
 
             mDispatchEnabled = enabled;
@@ -2606,6 +2613,21 @@ void InputDispatcher::setInputDispatchMode(bool enabled, bool frozen) {
         // Wake up poll loop since it may need to make new input dispatching choices.
         mLooper->wake();
     }
+}
+
+void InputDispatcher::resetAndDropEverythingLocked(const char* reason) {
+#if DEBUG_FOCUS
+    LOGD("Resetting and dropping all events (%s).", reason);
+#endif
+
+    synthesizeCancelationEventsForAllConnectionsLocked(InputState::CANCEL_ALL_EVENTS, reason);
+
+    resetKeyRepeatLocked();
+    releasePendingEventLocked();
+    drainInboundQueueLocked();
+    resetTargetsLocked();
+
+    mTouchState.reset();
 }
 
 void InputDispatcher::logDispatchStateLocked() {
