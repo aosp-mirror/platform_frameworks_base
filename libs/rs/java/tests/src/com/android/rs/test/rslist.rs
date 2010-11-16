baa19bc59cd15e8a224cfae10454dc970baac23f
Stephen Hines <srhines@google.com>
2010-11-15 17:09:02 -0800

    Remove WARs for zero-init + destructors.
    Change-Id: I919db554f3f6fa1fb027402298602a16fba4a235

--- libs/rs/java/tests/src/com/android/rs/test/rslist.rs
+++ libs/rs/java/tests/src/com/android/rs/test/rslist.rs
@@ -47,7 +47,7 @@ int root(int launchID) {
     rsgBindFont(gFont);
     color(0.2, 0.2, 0.2, 0);
 
-    rs_allocation listAlloc = {0};
+    rs_allocation listAlloc;
     rsSetObject(&listAlloc, rsGetAllocation(gList));
     int allocSize = rsAllocationGetDimX(listAlloc);
 
@@ -103,7 +103,6 @@ int root(int launchID) {
         }
         currentYPos += itemHeight;
     }
-    rsClearObject(&listAlloc);
 
     return 10;
 }
