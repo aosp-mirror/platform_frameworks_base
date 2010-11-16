44ab8453e1c4c46790f792a46d026fa1017d8cfe
Chris Tate <ctate@google.com>
2010-11-16 15:10:49 -0800

    Permission fix: don't require BACKUP perm for self-restores
    The public API is not supposed to require the BACKUP permission in order
    for an application to restore its own last-known-good backup data.  However,
    as currently implemented, BackupManager.requestRestore() [the public API
    in question] depends on private Backup Manager methods that *do* enforce
    that permission.  The net result is that the method cannot be successfully
    used by third party applications: it will throw an exception if attempted.
    This CL restructures the permission checking involved.
    
    First, the underlying beginRestoreSession() operation can now be passed a
    'null' transport name; if this is done, then the restore session is begun
    on whatever the currently-active transport is.  Looking up the name of the
    active transport is one of the permission-guarded actions that was required
    with the initial implementation.
    
    Second, a package name can now be passed to beginRestoreSession().  If
    this is done, then the restore session can only be used to perform a
    single-package restore of that one application.  The BACKUP permission is
    not required if the caller is tying the restore to its own package name.
    
    In combination, these changes permit BackupManager.requestRestore() to
    function without the calling app needing to hold any special permission.
    The no-permission case is intentionally quite narrow:  the caller must
    hold the permission unless they both (a) pass 'null' for the transport
    name, thereby accepting whatever the currently active transport is, and
    (b) pass their own package name to restrict the restore session only
    to their own app.
    
    External bug http://code.google.com/p/android/issues/detail?id=10094
    Internal bug 3197202
    
    Change-Id: Ibc9d652323f2da03727d850f991b4096af6520d2

--- core/java/android/app/backup/BackupManager.java
+++ core/java/android/app/backup/BackupManager.java
@@ -138,8 +138,8 @@ public class BackupManager {
         if (sService != null) {
             RestoreSession session = null;
             try {
-                String transport = sService.getCurrentTransport();
-                IRestoreSession binder = sService.beginRestoreSession(transport);
+                IRestoreSession binder = sService.beginRestoreSession(mContext.getPackageName(),
+                        null);
                 session = new RestoreSession(mContext, binder);
                 result = session.restorePackage(mContext.getPackageName(), observer);
             } catch (RemoteException e) {
@@ -163,8 +163,8 @@ public class BackupManager {
         checkServiceBinder();
         if (sService != null) {
             try {
-                String transport = sService.getCurrentTransport();
-                IRestoreSession binder = sService.beginRestoreSession(transport);
+                // All packages, current transport
+                IRestoreSession binder = sService.beginRestoreSession(null, null);
                 session = new RestoreSession(mContext, binder);
             } catch (RemoteException e) {
                 Log.w(TAG, "beginRestoreSession() couldn't connect");
