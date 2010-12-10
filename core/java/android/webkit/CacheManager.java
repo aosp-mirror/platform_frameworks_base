0f868ca036ed6dcd835dfb2d8f8842f344d4f5ff
Iain Merrick <husky@google.com>
2010-12-10 16:17:54 +0000

    Disable CacheManager when using the Chrome HTTP stack.
    Currently, cacheDisabled() reports false, but no files are ever
    added to the cache. This CL makes the framework more honest and
    reports that the cache is disabled.
    
    BUG=3270236
    
    Change-Id: I13c0291ac295d965f71f1e70dbdebc74b95ddce0

--- core/java/android/webkit/CacheManager.java
+++ core/java/android/webkit/CacheManager.java
@@ -181,6 +181,11 @@ public final class CacheManager {
             removeAllCacheFiles();
             mClearCacheOnInit = false;
         }
+        // If we're using the Chrome HTTP stack, disable the cache.
+        // Chrome has its own cache, and we don't provide programmatic access to it.
+        if (JniUtil.useChromiumHttpStack()) {
+            setCacheDisabled(true);
+        }
     }
     
     /**
@@ -232,6 +237,10 @@ public final class CacheManager {
         mDisabled = disabled;
         if (mDisabled) {
             removeAllCacheFiles();
+        }
+        if (!mDisabled && JniUtil.useChromiumHttpStack()) {
+            Log.w(LOGTAG, "CacheManager enabled, but it will not work as "
+                    + "expected because the Chrome HTTP stack is in use.");
         }
     }
 
