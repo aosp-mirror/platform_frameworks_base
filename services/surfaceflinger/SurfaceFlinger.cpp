a6b8c1c3bd1d9b84bbeb1820c0239742f50d3edb
Mathias Agopian <mathias@google.com>
2010-12-15 14:41:59 -0800

    workaround [3201922] display not on: log full of gralloc errors
    while we're waiting for the real fix in the gralloc/gpu driver,
    this workaround should resolve the issue.
    
    we make sure to call compositionComplete after everytime we do
    composition with the GPU (even for the screenshot case), which is
    where the buffer locks are released.
    
    Change-Id: I3cb5ad67d48c81a23100172bab77e86a70e29152

--- services/surfaceflinger/SurfaceFlinger.cpp
+++ services/surfaceflinger/SurfaceFlinger.cpp
@@ -395,6 +395,7 @@ bool SurfaceFlinger::threadLoop()
         logger.log(GraphicLog::SF_REPAINT_DONE, index);
     } else {
         // pretend we did the post
+        hw.compositionComplete();
         unlockClients();
         usleep(16667); // 60 fps period
     }
@@ -2184,6 +2185,9 @@ status_t SurfaceFlinger::captureScreenImplLocked(DisplayID dpy,
     glBindFramebufferOES(GL_FRAMEBUFFER_OES, 0);
     glDeleteRenderbuffersOES(1, &tname);
     glDeleteFramebuffersOES(1, &name);
+
+    hw.compositionComplete();
+
     return result;
 }
 
