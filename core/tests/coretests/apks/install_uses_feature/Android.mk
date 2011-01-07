1683afac148f5bce186f7af7a6a8ea6294ecd379
Kenny Root <kroot@google.com>
2011-01-07 14:27:50 -0800

    Fix check for required features
    The required features check was in an null check for usesLibraries, so
    it wasn't checking for required features when an application did not use
    a library.
    
    Bug: 3331713
    Change-Id: I96d366ba00d4962b84f3796be97e2b622eb331a5

---
+++ core/tests/coretests/apks/install_uses_feature/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_PACKAGE_NAME := FrameworkCoreTests_install_uses_feature
+
+include $(BUILD_PACKAGE)
+
