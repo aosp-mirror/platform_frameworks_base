Neoteric-OS/android_frameworks_base

JPtextinput: Dont break emoji characters when cutting strings.
In the LengthFilter make sure that we do not cut the
string so a last emoji character gets broken and replaced
by some garbage character.

We need to check if the last char is the high surrogate and
if so drop the high surrogate also.

Change-Id: I210e7e41aa0761ecbb2b32e3ebf680a04382c287

core/java/android/text/InputFilter.java
@@ -88,7 +88,14 @@ public interface InputFilter
             } else if (keep >= end - start) {
                 return null; // keep original
             } else {
-                return source.subSequence(start, start + keep);
+                int keepEnd = start + keep;
+                if (Character.isHighSurrogate(source.charAt(keepEnd - 1))) {
+                    --keepEnd;
+                    if (keepEnd == start) {
+                        return "";
+                    }
+                }
+                return source.subSequence(start, keepEnd);
             }
         }
 

aosp-mirror/platform_frameworks_base

core/java/android/text/InputFilter.java
@@ -88,7 +88,14 @@ public interface InputFilter
             } else if (keep >= end - start) {
                 return null; // keep original
             } else {
-                return source.subSequence(start, start + keep);
+                keep += start;
+                if (Character.isHighSurrogate(source.charAt(keep - 1))) {
+                    --keep;
+                    if (keep == start) {
+                        return "";
+                    }
+                }
+                return source.subSequence(start, keep);
             }
         }
 