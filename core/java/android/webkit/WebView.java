7170bb6391f233ef09fdbfd2ea9d1dd77f306e68
Cary Clark <cary@android.com>
2011-01-12 10:12:15 -0500

    add null check to copy selection
    This prevents a toast from appearing
    if copyselection is done initially.
    
    bug:3339016
    Change-Id: If6f91b8f3794b3807fb3319fc542838abd431c64

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -4955,7 +4955,7 @@ public class WebView extends AbsoluteLayout
     public boolean copySelection() {
         boolean copiedSomething = false;
         String selection = getSelection();
-        if (selection != "") {
+        if (selection != null && selection != "") {
             if (DebugFlags.WEB_VIEW) {
                 Log.v(LOGTAG, "copySelection \"" + selection + "\"");
             }
