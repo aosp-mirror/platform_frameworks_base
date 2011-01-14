076b770c2a9a72af768e41738e9e1b282acd4669
Shih-wei Liao <sliao@google.com>
2011-01-14 06:21:28 -0800

    bcc interface fixed.
    Change-Id: I81349750e58f119a9c2ad596f376c7e39d5f0d4e

--- libs/rs/Android.mk
+++ libs/rs/Android.mk
@@ -122,6 +122,7 @@ LOCAL_SHARED_LIBRARIES += libz libcutils libutils libEGL libGLESv1_CM libGLESv2 
 LOCAL_STATIC_LIBRARIES := libdex libft2
 
 LOCAL_C_INCLUDES += external/freetype/include external/zlib dalvik
+LOCAL_C_INCLUDES += frameworks/compile/libbcc/include
 
 LOCAL_LDLIBS := -lpthread -ldl
 LOCAL_MODULE:= libRS
