39bc6197baa7482dd070878a63992e8212965a4d
Jeff Brown <jeffbrown@google.com>
2011-01-18 18:22:48 -0800

    Never drag scroll views with no children. (DO NOT MERGE)
    Fixes a regression from Froyo.  Previously, when a scroll view
    had no children, its onTouchEvent would return because the scroll
    view would only start dragging if the user touched one of its children.
    In Gingerbread, the user can drag from anywhere within the scroll
    view, not just by touching a child.  However, it makes no sense
    to drag a scroll view that has no children so an empty scroll view
    should just ignore touches like any other empty view group would.
    
    This change fixes applications that for some reason or other happen
    to have empty scroll views in strange places.
    
    Bug: 3246230
    Change-Id: I76c6136d3cd74968983033014d60deec1718ce30

--- core/java/android/widget/HorizontalScrollView.java
+++ core/java/android/widget/HorizontalScrollView.java
@@ -502,8 +502,10 @@ public class HorizontalScrollView extends FrameLayout {
 
         switch (action & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN: {
-                final float x = ev.getX();
-                mIsBeingDragged = true;
+                mIsBeingDragged = getChildCount() != 0;
+                if (!mIsBeingDragged) {
+                    return false;
+                }
 
                 /*
                  * If being flinged and user touches, stop the fling. isFinished
@@ -514,7 +516,7 @@ public class HorizontalScrollView extends FrameLayout {
                 }
 
                 // Remember where the motion event started
-                mLastMotionX = x;
+                mLastMotionX = ev.getX();
                 mActivePointerId = ev.getPointerId(0);
                 break;
             }
