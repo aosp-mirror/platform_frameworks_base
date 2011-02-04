3515a33599b728707beec3eeeb1122dd1b642934
Glenn Kasten <gkasten@google.com>
2011-02-03 16:55:26 -0800

    Bug 3366668 Use BinderService template
    Change-Id: I93d7f3fc9dc9b6a365723d8a51a73a5aabdb4f93

--- services/audioflinger/AudioPolicyService.cpp
+++ services/audioflinger/AudioPolicyService.cpp
@@ -479,13 +479,6 @@ status_t AudioPolicyService::onTransact(
 
 
 // ----------------------------------------------------------------------------
-void AudioPolicyService::instantiate() {
-    defaultServiceManager()->addService(
-            String16("media.audio_policy"), new AudioPolicyService());
-}
-
-
-// ----------------------------------------------------------------------------
 // AudioPolicyClientInterface implementation
 // ----------------------------------------------------------------------------
 
