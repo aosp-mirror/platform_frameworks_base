dfe515e49ae8a0275012b56a4a79d2dfcc017db2
Jesse Wilson <jessewilson@google.com>
2011-02-10 19:06:09 -0800

    Remove LruCache.setMaxSize().
    Dynamically changing a max cache size is clumsy; almost everyone
    should set this when they create the cache.
    
    Fix SQLiteDatabase to copy entries into a new cache when the size
    is changed. In pratice this will always be immediately after the
    SQLiteDatabase is created. Since the cache field is no longer
    final, change the guard on the cache field to the SQLiteDatabase
    instance itself.
    
    Change-Id: I4e325f06edc551636723568a52770c0982e2d945

--- core/java/android/util/LruCache.java
+++ core/java/android/util/LruCache.java
@@ -155,25 +155,6 @@ public class LruCache<K, V> {
     }
 
     /**
-     * Sets the maximum size of this cache. Decreasing the maximum size may
-     * evict entries from this cache.
-     *
-     * @hide
-     *
-     * @param maxSize for caches that do not override {@link #sizeOf}, this is
-     *     the maximum number of entries in the cache. For all other caches,
-     *     this is the maximum sum of the sizes of the entries in this cache.
-     */
-    public synchronized final void setMaxSize(int maxSize) {
-        if (maxSize <= 0) {
-            throw new IllegalArgumentException("maxSize <= 0");
-        }
-
-        trimToSize(maxSize);
-        this.maxSize = maxSize;
-    }
-
-    /**
      * Called for entries that have reached the tail of the least recently used
      * queue and are be removed. The default implementation does nothing.
      */
