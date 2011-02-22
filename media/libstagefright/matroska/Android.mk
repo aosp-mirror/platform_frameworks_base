8433f75b07cc3c5fb477c7585c1add375c2a4ffc
James Dong <jdong@google.com>
2011-02-22 15:12:15 -0800

    Remove mkvparser code from /frameworks/base
    bug - 3322129
    
    Change-Id: I94dca6234ce57c9a32290aa7ac4885b0a2f566a9

--- media/libstagefright/matroska/Android.mk
+++ media/libstagefright/matroska/Android.mk
@@ -2,11 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:=                 \
-        MatroskaExtractor.cpp     \
-        mkvparser.cpp             \
+        MatroskaExtractor.cpp
 
 LOCAL_C_INCLUDES:= \
-	$(JNI_H_INCLUDE) \
+        $(JNI_H_INCLUDE) \
+        $(TOP)/external/libvpx/mkvparser \
         $(TOP)/frameworks/base/include/media/stagefright/openmax \
 
 LOCAL_CFLAGS += -Wno-multichar
