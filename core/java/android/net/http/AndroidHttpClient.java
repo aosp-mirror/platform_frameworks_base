9add122296d3b252bdb1b377326870e4413a68d9
Alon Albert <aalbert@google.com>
2011-02-23 13:55:11 -0800

    DO NOT MERGE - Increasing timeout to 60 sec This was already done in HC branch in this CL: https://android-git.corp.google.com/w/?p=platform/frameworks/base.git;a=commit;h=d39fd5a97c6f56794a6ed7ac1dfb0bbf585becf1 This is also related to bug http://b/issue?id=3402863 I first considered increasing the timeout specificaly for our GData client but when I saw we already accepted a CL for HC that did this at a lower level, I decided to do that instead.
    Bug: 3402863
    Change-Id: I7ef2968ecbab3cc0d698a408f2c643c83d333e2f

--- core/java/android/net/http/AndroidHttpClient.java
+++ core/java/android/net/http/AndroidHttpClient.java
@@ -108,8 +108,8 @@ public final class AndroidHttpClient implements HttpClient {
         HttpConnectionParams.setStaleCheckingEnabled(params, false);
 
         // Default connection and socket timeout of 20 seconds.  Tweak to taste.
-        HttpConnectionParams.setConnectionTimeout(params, 20 * 1000);
-        HttpConnectionParams.setSoTimeout(params, 20 * 1000);
+        HttpConnectionParams.setConnectionTimeout(params, 60 * 1000);
+        HttpConnectionParams.setSoTimeout(params, 60 * 1000);
         HttpConnectionParams.setSocketBufferSize(params, 8192);
 
         // Don't handle redirects -- return them to the caller.  Our code
