27555315629ffce59a19bd03ba51a8323cc864b0
Mike Lockwood <lockwood@android.com>
2011-02-27 16:49:23 -0800

    USB accessory support library
    This provides a mechanism for developing applications to work with
    USB accessories in versions of android prior to the introduction
    of the android.hardware.UsbManager APIs.
    
    Applications should link against the com.google.android.usb library
    to use this support.
    
    Change-Id: I48ff80467fd9a6667aa84a758eca754132f546b9
    Signed-off-by: Mike Lockwood <lockwood@android.com>

---
+++ libs/usb/tests/AccessoryChat/Android.mk
@@ -0,0 +1,15 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := tests
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_PACKAGE_NAME := AccessoryChatGB
+
+LOCAL_JAVA_LIBRARIES := com.google.android.usb
+
+# Force an old SDK version to make sure we aren't using newer UsbManager APIs
+LOCAL_SDK_VERSION := 8
+
+include $(BUILD_PACKAGE)
