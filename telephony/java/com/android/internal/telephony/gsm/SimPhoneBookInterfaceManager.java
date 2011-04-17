d7784628368404ff53caf080640cd9ff32775fd3
David Sobreira Marques <dpsmarques@gmail.com>
2011-04-17 12:46:50 -0300

    Fixing concurrency issue on IccPhoneBookInterfaceManager.
    All the reads/updates methods are synchronous calls that rely
    on an unique lock object in order to wait for the asynchronous
    simcard operations to complete and return appropriate results
    
    Concurent calls to these methods will cause errors when one
    completed operation will unlock all waiting calls generating
    inconsistent results on some of the method calls.
    
    Change-Id: I8b87004ac039bcb971b8369f7640281f1bf9eb35
    Signed-off-by: David Sobreira Marques <dpsmarques@gmail.com>

--- telephony/java/com/android/internal/telephony/gsm/SimPhoneBookInterfaceManager.java
+++ telephony/java/com/android/internal/telephony/gsm/SimPhoneBookInterfaceManager.java
@@ -16,6 +16,8 @@
 
 package com.android.internal.telephony.gsm;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import android.os.Message;
 import android.util.Log;
 
@@ -56,14 +58,11 @@ public class SimPhoneBookInterfaceManager extends IccPhoneBookInterfaceManager {
             recordSize = new int[3];
 
             //Using mBaseHandler, no difference in EVENT_GET_SIZE_DONE handling
-            Message response = mBaseHandler.obtainMessage(EVENT_GET_SIZE_DONE);
+            AtomicBoolean status = new AtomicBoolean(false);
+            Message response = mBaseHandler.obtainMessage(EVENT_GET_SIZE_DONE, status);
 
             phone.getIccFileHandler().getEFLinearRecordSize(efid, response);
-            try {
-                mLock.wait();
-            } catch (InterruptedException e) {
-                logd("interrupted while trying to load from the SIM");
-            }
+            waitForResult(status);
         }
 
         return recordSize;
