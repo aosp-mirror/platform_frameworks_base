fba19ff5e8135878a1d87ed696e0a89cc776f37e
Irfan Sheriff <isheriff@google.com>
2011-04-28 10:47:12 -0700

    DO NOT MERGE Define Protocol class
    Adds a common namespace for communication across
    different state machines
    
    Change-Id: Ib6a29715c651371f91a950348a8525275b6026a0

---
+++ core/java/com/android/internal/util/Protocol.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util;
+
+/**
+ * This class defines Message.what base addresses for various protocols that are recognized
+ * to be unique by any {@link com.android.internal.util.Statemachine} implementation. This
+ * allows for interaction between different StateMachine implementations without a conflict
+ * of message codes.
+ *
+ * As an example, all messages in {@link android.net.wifi.WifiStateMachine} will have message
+ * codes with Message.what starting at Protocol.WIFI + 1 and less than or equal to Protocol.WIFI +
+ * Protocol.MAX_MESSAGE
+ *
+ * {@hide}
+ */
+public class Protocol {
+    public static final int MAX_MESSAGE     =  0x0000FFFF;
+
+    public static final int BASE_WIFI       =  0x00010000;
+    public static final int BASE_DHCP       =  0x00020000;
+    //TODO: define all used protocols
+}
