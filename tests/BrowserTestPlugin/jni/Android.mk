1048b7e63e41c3b6044edddf5e581bb6fb0f3b39
Steve Block <steveblock@google.com>
2011-05-12 16:31:13 +0100

    Fix WebCore include paths due to merge to WebKit r75315
    The WebCore directory has moved inside a new Source directory.
    
    Change-Id: Idb72a6e80f8df9d8685698b259ca5c305a3c0a5d

--- tests/BrowserTestPlugin/jni/Android.mk
+++ tests/BrowserTestPlugin/jni/Android.mk
@@ -32,12 +32,14 @@ LOCAL_SRC_FILES := \
 	PluginObject.cpp \
 	event/EventPlugin.cpp \
 
+WEBCORE_PATH := external/webkit/Source/WebCore
+
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH) \
 	$(LOCAL_PATH)/event \
-	external/webkit/WebCore/bridge \
-	external/webkit/WebCore/plugins \
-	external/webkit/WebCore/platform/android/JavaVM \
+	$(WEBCORE_PATH)/bridge \
+	$(WEBCORE_PATH)/plugins \
+	$(WEBCORE_PATH)/platform/android/JavaVM \
 	external/webkit/WebKit/android/plugins
 
 LOCAL_CFLAGS += -fvisibility=hidden 
