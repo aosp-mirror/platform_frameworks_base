d39d1affe82cb8c21d32baaa5fbb2d6afb806f8e
Derek Sollenberger <djsollen@google.com>
2011-05-16 13:09:42 -0400

    Updates resulting from the Skia merge (revision 1327)
    Change-Id: I2a8f5869dbe95bb594f2ba5d7278f9b330e6f17a

--- core/jni/android/graphics/NinePatchImpl.cpp
+++ core/jni/android/graphics/NinePatchImpl.cpp
@@ -116,9 +116,9 @@ void NinePatch_Draw(SkCanvas* canvas, const SkRect& bounds,
         paint = &defaultPaint;
     }
     
-    // if our canvas is GL, draw this as a mesh, which will be faster than
-    // in parts (which is faster for raster)
-    if (canvas && canvas->getViewport(NULL)) {
+    // if our SkCanvas were back by GL we should enable this and draw this as
+    // a mesh, which will be faster in most cases.
+    if (false) {
         SkNinePatch::DrawMesh(canvas, bounds, bitmap,
                               chunk.xDivs, chunk.numXDivs,
                               chunk.yDivs, chunk.numYDivs,
