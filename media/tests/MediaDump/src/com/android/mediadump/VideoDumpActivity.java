ea0bad0574451212591841ba84f477ecc216003a
Huahui Wu <hwu@google.com>
2011-05-18 15:02:16 -0700

    b/4452171 Dumping video playbacks to files.
    MediaDump: a tool app to dump video playback into raw files and
     a viewer to display the dumped files.
    
    Change-Id: I7bf116e38bb1f9e85d5a1680ae92b5b72bc10ea8

---
+++ media/tests/MediaDump/src/com/android/mediadump/VideoDumpActivity.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.mediadump;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
+
+/**
+ * A media tool to play a video and dump the screen display
+ * into raw RGB files. Check VideoDumpView for tech details.
+ */
+public class VideoDumpActivity extends Activity {
+
+    private Context context;
+
+    private View mainView;
+    private VideoDumpView mVideoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = this;
+
+        mainView = createView();
+        setContentView(mainView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoView.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mVideoView.onResume();
+    }
+
+    private View createView() {
+        mVideoView = new VideoDumpView(this);
+        mVideoView.setMediaController(new MediaController(context));
+
+        LinearLayout mainLayout = new LinearLayout(this);
+        mainLayout.addView(mVideoView, new LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT));
+
+        return mainLayout;
+    }
+
+    protected void onStop() {
+        if (mVideoView != null) {
+            if (mVideoView.isPlaying()) {
+                mVideoView.stopPlayback();
+            }
+        }
+        super.onStop();
+    }
+}
+
