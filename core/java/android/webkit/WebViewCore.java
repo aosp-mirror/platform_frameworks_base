1d87cbf6029838da9f979a2a879fde4993730f08
Russell Brenner <russellbrenner@google.com>
2011-05-24 13:34:39 -0700

    DO NOT MERGE-Refire the redraw event if webkit wasn't ready
    If webkit wasn't in a drawing mood, post a WEBKIT_DRAW event to
    assure that the update is not lost.
    
    bug: 4474358
    Change-Id: Ib0c4cedb10f58821f95c439824c30043a906f8b8

--- core/java/android/webkit/WebViewCore.java
+++ core/java/android/webkit/WebViewCore.java
@@ -2016,7 +2016,8 @@ final class WebViewCore {
         if (DebugFlags.WEB_VIEW_CORE) Log.v(LOGTAG, "webkitDraw start");
         draw.mBaseLayer = nativeRecordContent(draw.mInvalRegion, draw.mContentSize);
         if (draw.mBaseLayer == 0) {
-            if (DebugFlags.WEB_VIEW_CORE) Log.v(LOGTAG, "webkitDraw abort");
+            if (DebugFlags.WEB_VIEW_CORE) Log.v(LOGTAG, "webkitDraw abort, resending draw message");
+            mEventHub.sendMessage(Message.obtain(null, EventHub.WEBKIT_DRAW));
             return;
         }
         webkitDraw(draw);
