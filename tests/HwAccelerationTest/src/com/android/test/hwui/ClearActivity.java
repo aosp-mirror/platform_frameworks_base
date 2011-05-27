f09ef51889f75289b041f9e9f949b7b82ed5b686
Romain Guy <romainguy@google.com>
2011-05-27 11:43:46 -0700

    Fix issue with drawColor(color, Mode.Clear)
    Change-Id: I486b24a5b609c6f8adb0276037ddb24af2b888b2

--- tests/HwAccelerationTest/src/com/android/test/hwui/ClearActivity.java
+++ tests/HwAccelerationTest/src/com/android/test/hwui/ClearActivity.java
@@ -51,7 +51,7 @@ public class ClearActivity extends Activity {
             mClearPaint = new Paint();
             mClearPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
             mClearPaint.setAntiAlias(true);
-            mClearPaint.setColor(0xff00ff00);
+            mClearPaint.setColor(0x0000ff00);
             mClearPaint.setStrokeWidth(15.0f);
             mClearPaint.setStyle(Paint.Style.FILL);
             mClearPaint.setTextSize(32.0f);
