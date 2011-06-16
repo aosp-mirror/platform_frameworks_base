b07124082e32d51be12d21bfb26147ab0617bbc9
Fabrice Di Meglio <fdimeglio@google.com>
2011-06-16 16:18:11 -0700

    Update BiDiTest app for testing View padding
    Change-Id: I7b956bd1d16cc4d27f33595f09f14f5d4e17e469

---
+++ tests/BiDiTests/src/com/android/bidi/BiDiTestViewPadding.java
@@ -0,0 +1,17 @@
+// Copyright 2011 Google Inc. All Rights Reserved.
+package com.android.bidi;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BiDiTestViewPadding extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.view_padding, container, false);
+    }
+}
