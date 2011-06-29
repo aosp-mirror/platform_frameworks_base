6e64e0c61720244894dbd1fd6b36394adc63b2de
Huahui Wu <hwu@google.com>
2011-06-29 14:21:58 -0700

    b/3302378 Passes touch pressures, size and timestamps to webkit.
    Related CL in WebKit:
      https://android-git.corp.google.com/g/#change,118422
    
    Change-Id: Ibf209131a6d9ca8186c461b0c5b6286e5f669d0d

--- core/java/android/webkit/WebViewCore.java
+++ core/java/android/webkit/WebViewCore.java
@@ -577,8 +577,10 @@ public final class WebViewCore {
     private native void nativeTouchUp(int touchGeneration,
             int framePtr, int nodePtr, int x, int y);
 
-    private native boolean nativeHandleTouchEvent(int action, int[] idArray,
-            int[] xArray, int[] yArray, int count, int actionIndex, int metaState);
+    private native boolean nativeHandleTouchEvent(int action, long eventTime,
+            int[] idArray, int[] xArray, int[] yArray, float[] pressureArray,
+            int[] touchMajorArray, int[] touchMinorArray, float[] orientationArray,
+            int count, int actionIndex, int metaState);
 
     private native void nativeUpdateFrameCache();
 
@@ -833,16 +835,21 @@ public final class WebViewCore {
 
     static class TouchEventData {
         int mAction;
-        int[] mIds;  // Ids of the touch points
+        long mEventTime;  // Time (in ms) this event was generated.
+        int[] mIds;  // Ids of the touch points.
         Point[] mPoints;
-        Point[] mPointsInView;  // the point coordinates in view axis.
-        int mActionIndex;  // Associated pointer index for ACTION_POINTER_DOWN/UP
+        Point[] mPointsInView;  // Point coordinates in view axis.
+        float[] mPressures;  // Pressures of the touch points.
+        int[] mTouchMajor;  // Length of the major axis of the touch area.
+        int[] mTouchMinor;  // Length of the minor axis of the touch area.
+        float[] mOrientation;  // The orientation of the touch area.
+        int mActionIndex;  // Associated pointer index for ACTION_POINTER_DOWN/UP.
         int mMetaState;
         boolean mReprocess;
-        MotionEvent mMotionEvent;
+        MotionEvent mMotionEvent;  // Only used for multi-touch.
         int mNativeLayer;
         Rect mNativeLayerRect = new Rect();
-        long mSequence;
+        long mSequence;  // For queuing the events.
         boolean mNativeResult;
     }
 
@@ -1351,8 +1358,11 @@ public final class WebViewCore {
                                 nativeScrollLayer(ted.mNativeLayer,
                                         ted.mNativeLayerRect);
                             }
-                            ted.mNativeResult = nativeHandleTouchEvent(ted.mAction, ted.mIds,
-                                    xArray, yArray, count, ted.mActionIndex, ted.mMetaState);
+                            ted.mNativeResult = nativeHandleTouchEvent(
+                                    ted.mAction, ted.mEventTime, ted.mIds,
+                                    xArray, yArray, ted.mPressures,
+                                    ted.mTouchMajor, ted.mTouchMinor, ted.mOrientation,
+                                    count, ted.mActionIndex, ted.mMetaState);
                             Message.obtain(
                                     mWebView.mPrivateHandler,
                                     WebView.PREVENT_TOUCH_ID,
