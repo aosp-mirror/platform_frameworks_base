3c728fe61dcf49354ea761a3e4ef6032ec271630
Jim Miller <jaggies@google.com>
2011-07-07 20:53:52 -0700

    Fix 4689527: Improved curation for Recents + misc cleanup
    This sets a threshhold for both the curation animation as well as the
    snap back animation.  It now limits these to 500ms and 250ms, respectively.
    
    Cleaned up misc log spew.
    
    Updated after review.
    
    Change-Id: Ie11a2fc4556b396257c33c3ebd7513c4c220ff95

---
+++ packages/SystemUI/src/com/android/systemui/recent/Constants.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2011 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.recent;
+
+public class Constants {
+    static final int MAX_ESCAPE_ANIMATION_DURATION = 500; // in ms
+    static final float FADE_CONSTANT = 0.5f; // unitless
+    static final int SNAP_BACK_DURATION = 250; // in ms
+    static final int ESCAPE_VELOCITY = 100; // speed of item required to "curate" it in dp/s
+}
