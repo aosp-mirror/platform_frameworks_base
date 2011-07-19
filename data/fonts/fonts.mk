359d052a9a56e0b5b23d39fc8bcea4b71459ffb2
Russell Brenner <russellbrenner@google.com>
2011-07-19 10:42:46 -0700

    Include DroidSansEthiopic-Regular as fallback font
    Font file was previously added to source tree with another CL:
    
        https://android-git.corp.google.com/g/117734
    
    Change-Id: I03d33d1f6af950c57093efaf78f36a12bcf82689

--- data/fonts/fonts.mk
+++ data/fonts/fonts.mk
@@ -18,6 +18,7 @@ PRODUCT_COPY_FILES := \
     frameworks/base/data/fonts/DroidSans.ttf:system/fonts/DroidSans.ttf \
     frameworks/base/data/fonts/DroidSans-Bold.ttf:system/fonts/DroidSans-Bold.ttf \
     frameworks/base/data/fonts/DroidSansArabic.ttf:system/fonts/DroidSansArabic.ttf \
+    frameworks/base/data/fonts/DroidSansEthiopic-Regular.ttf:system/fonts/DroidSansEthiopic-Regular.ttf \
     frameworks/base/data/fonts/DroidSansHebrew-Regular.ttf:system/fonts/DroidSansHebrew-Regular.ttf \
     frameworks/base/data/fonts/DroidSansHebrew-Bold.ttf:system/fonts/DroidSansHebrew-Bold.ttf \
     frameworks/base/data/fonts/DroidSansThai.ttf:system/fonts/DroidSansThai.ttf \
