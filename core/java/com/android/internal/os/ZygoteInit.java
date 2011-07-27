e5168930975618a2b2b3f556c6c162a8cf6c9e34
Jeff Sharkey <jsharkey@android.com>
2011-07-27 16:30:00 -0700

    Grant AID_NET_BW_STATS to system service.
    NetworkManagementService is reading stats /proc/ files, which are now
    being protected by GID.
    
    Bug: 5087530
    Change-Id: I29b35a97ab621999544b6acc8ad4cf72ee053b79

--- core/java/com/android/internal/os/ZygoteInit.java
+++ core/java/com/android/internal/os/ZygoteInit.java
@@ -474,7 +474,7 @@ public class ZygoteInit {
         String args[] = {
             "--setuid=1000",
             "--setgid=1000",
-            "--setgroups=1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1018,3001,3002,3003",
+            "--setgroups=1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1018,3001,3002,3003,3006",
             "--capabilities=130104352,130104352",
             "--runtime-init",
             "--nice-name=system_server",
