97c3311bb84c7a0a75f3c42c8c781ca836c7a5c6
Svetoslav Ganov <svetoslavganov@google.com>
2011-07-28 12:45:06 -0700

    Updaing the database version
    Change-Id: Iff671a152ac8c96d4136f0f268167c1b60842bb5

--- packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
+++ packages/SettingsProvider/src/com/android/providers/settings/DatabaseHelper.java
@@ -63,7 +63,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     // database gets upgraded properly. At a minimum, please confirm that 'upgradeVersion'
     // is properly propagated through your change.  Not doing so will result in a loss of user
     // settings.
-    private static final int DATABASE_VERSION = 67;
+    private static final int DATABASE_VERSION = 68;
 
     private Context mContext;
 
