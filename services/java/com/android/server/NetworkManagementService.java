05355c3eb69ef2cd8d08260be059be006a2883b9
Jeff Sharkey <jsharkey@android.com>
2011-08-09 14:44:19 -0700

    Enable network stats module by default.
    Bug: 5138315
    Change-Id: Id4cffdc126bf14a005fc2d3aece87ea036a51999

--- services/java/com/android/server/NetworkManagementService.java
+++ services/java/com/android/server/NetworkManagementService.java
@@ -188,10 +188,9 @@ class NetworkManagementService extends INetworkManagementService.Stub {
 
         // only enable bandwidth control when support exists, and requested by
         // system setting.
-        // TODO: eventually migrate to be always enabled
         final boolean hasKernelSupport = new File("/proc/net/xt_qtaguid/ctrl").exists();
         final boolean shouldEnable =
-                Settings.Secure.getInt(mContext.getContentResolver(), NETSTATS_ENABLED, 0) != 0;
+                Settings.Secure.getInt(mContext.getContentResolver(), NETSTATS_ENABLED, 1) != 0;
 
         mBandwidthControlEnabled = false;
         if (hasKernelSupport && shouldEnable) {
