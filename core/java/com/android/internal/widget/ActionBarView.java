9e58df5d40e345a3b5ade8bebae6bc61421ca06a
Adam Powell <adamp@google.com>
2011-08-15 17:06:49 -0700

    Fix bug 5049406 - ActionBar UP with no home icon, tap highlight looks broken
    Measure the action bar title layout to consume the full height of the bar.
    
    Change-Id: Iaf0521c5c80b5bd7ceb7f756e50248f6a27fc1fc

--- core/java/com/android/internal/widget/ActionBarView.java
+++ core/java/com/android/internal/widget/ActionBarView.java
@@ -695,7 +695,8 @@ public class ActionBarView extends AbsActionBarView {
     private void initTitle() {
         if (mTitleLayout == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            mTitleLayout = (LinearLayout) inflater.inflate(R.layout.action_bar_title_item, null);
+            mTitleLayout = (LinearLayout) inflater.inflate(R.layout.action_bar_title_item,
+                    this, false);
             mTitleView = (TextView) mTitleLayout.findViewById(R.id.action_bar_title);
             mSubtitleView = (TextView) mTitleLayout.findViewById(R.id.action_bar_subtitle);
             mTitleUpView = (View) mTitleLayout.findViewById(R.id.up);
@@ -724,8 +725,7 @@ public class ActionBarView extends AbsActionBarView {
             mTitleLayout.setEnabled(titleUp);
         }
 
-        addView(mTitleLayout, new LayoutParams(LayoutParams.WRAP_CONTENT,
-                LayoutParams.MATCH_PARENT));
+        addView(mTitleLayout);
         if (mExpandedActionView != null) {
             // Don't show while in expanded mode
             mTitleLayout.setVisibility(GONE);
@@ -820,7 +820,8 @@ public class ActionBarView extends AbsActionBarView {
             boolean showTitle = mTitleLayout != null && mTitleLayout.getVisibility() != GONE &&
                     (mDisplayOptions & ActionBar.DISPLAY_SHOW_TITLE) != 0;
             if (showTitle) {
-                availableWidth = measureChildView(mTitleLayout, availableWidth, childSpecHeight, 0);
+                availableWidth = measureChildView(mTitleLayout, availableWidth,
+                        MeasureSpec.makeMeasureSpec(mContentHeight, MeasureSpec.EXACTLY), 0);
                 leftOfCenter = Math.max(0, leftOfCenter - mTitleLayout.getMeasuredWidth());
             }
 
