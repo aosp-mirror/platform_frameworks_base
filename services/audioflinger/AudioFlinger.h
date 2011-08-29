2d95dfbf4d284780035ac868651402bc39caac32
Eric Laurent <elaurent@google.com>
2011-08-29 12:42:48 -0700

    Audioflinger: reverse logic of BT NREC indication
    The interpretation of BT NREC by AudioFlinger to enable
    or disable AEC and NS was wrong: NREC to ON (default) means
    the phone (Audio Gateway) must enable local AEC and NS.
    
    Change-Id: I88a264e7fc9831c43bbace4f6b585baec73f2006

--- services/audioflinger/AudioFlinger.h
+++ services/audioflinger/AudioFlinger.h
@@ -210,7 +210,7 @@ public:
 
                 uint32_t    getMode() { return mMode; }
 
-                bool        btNrec() { return mBtNrec; }
+                bool        btNrecIsOff() { return mBtNrecIsOff; }
 
 private:
                             AudioFlinger();
@@ -1389,7 +1389,7 @@ private:
                 DefaultKeyedVector< pid_t, sp<NotificationClient> >    mNotificationClients;
                 volatile int32_t                    mNextUniqueId;
                 uint32_t                            mMode;
-                bool                                mBtNrec;
+                bool                                mBtNrecIsOff;
 
                 Vector<AudioSessionRef*> mAudioSessionRefs;
 };
