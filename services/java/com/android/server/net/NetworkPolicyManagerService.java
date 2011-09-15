8ca953da93eb8679574abba535f75f72d50016f2
Jeff Sharkey <jsharkey@android.com>
2011-09-14 19:56:11 -0700

    Change data usage warning notification wording.
    Bug: 5308710
    Change-Id: Ic807640a031a3edb03897634e638473d5a039ecc

--- services/java/com/android/server/net/NetworkPolicyManagerService.java
+++ services/java/com/android/server/net/NetworkPolicyManagerService.java
@@ -572,8 +572,7 @@ public class NetworkPolicyManagerService extends INetworkPolicyManager.Stub {
         switch (type) {
             case TYPE_WARNING: {
                 final CharSequence title = res.getText(R.string.data_usage_warning_title);
-                final CharSequence body = res.getString(R.string.data_usage_warning_body,
-                        Formatter.formatFileSize(mContext, policy.warningBytes));
+                final CharSequence body = res.getString(R.string.data_usage_warning_body);
 
                 builder.setSmallIcon(R.drawable.ic_menu_info_details);
                 builder.setTicker(title);
