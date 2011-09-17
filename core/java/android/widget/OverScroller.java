b0c71eb9f50ce06327fa6bb6219f0970e04fd856
Jeff Brown <jeffbrown@google.com>
2011-09-16 21:40:49 -0700

    Can't stop the fling! Bug: 5335420
    Fixed a bug in VelocityTracker where the output velocity was
    not being set to zero when not available.
    
    Added a condition to ensure that the velocity is at least
    the minimum fling velocity before continuing.  If not, then
    the user is trying to stop the fling and scroll more precisely.
    
    Change-Id: I36634b0c3f7a9a09cf20c33f71d41163a8e33eed

--- core/java/android/widget/OverScroller.java
+++ core/java/android/widget/OverScroller.java
@@ -532,7 +532,7 @@ public class OverScroller {
         final int dx = mScrollerX.mFinal - mScrollerX.mStart;
         final int dy = mScrollerY.mFinal - mScrollerY.mStart;
         return !isFinished() && Math.signum(xvel) == Math.signum(dx) &&
-        Math.signum(yvel) == Math.signum(dy);
+                Math.signum(yvel) == Math.signum(dy);
     }
 
     static class SplineOverScroller {
