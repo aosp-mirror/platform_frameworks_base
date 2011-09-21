44b471231c131f0a319f98202c2c55c06a0bb3d7
Mike Lockwood <lockwood@android.com>
2011-09-21 13:03:40 -0400

    PhoneWindowManager: Disable boot progress dialog on headless builds
    Change-Id: I1ac3475bade29a460ebb48a4962edd72daeb6637
    Signed-off-by: Mike Lockwood <lockwood@android.com>

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3363,6 +3363,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
 
     /** {@inheritDoc} */
     public void showBootMessage(final CharSequence msg, final boolean always) {
+        if (mHeadless) return;
         mHandler.post(new Runnable() {
             @Override public void run() {
                 if (mBootMsgDialog == null) {
