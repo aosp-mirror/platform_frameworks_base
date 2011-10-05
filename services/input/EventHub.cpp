4130554b452a8462a83f97051240f1aec8590650
Jeff Brown <jeffbrown@google.com>
2011-10-05 11:14:13 -0700

    Fix line wrapping.
    Change-Id: I41199d1145d2435eee046a7e5006dcdce0986821

--- services/input/EventHub.cpp
+++ services/input/EventHub.cpp
@@ -648,8 +648,9 @@ size_t EventHub::getEvents(int timeoutMillis, RawEvent* buffer, size_t bufferSiz
                         sizeof(struct input_event) * capacity);
                 if (readSize == 0 || (readSize < 0 && errno == ENODEV)) {
                     // Device was removed before INotify noticed.
-                    ALOGW("could not get event, removed? (fd: %d size: %d bufferSize: %d capacity: %d errno: %d)\n",
-                         device->fd, readSize, bufferSize, capacity, errno);
+                    ALOGW("could not get event, removed? (fd: %d size: %d bufferSize: %d "
+                            "capacity: %d errno: %d)\n",
+                            device->fd, readSize, bufferSize, capacity, errno);
                     deviceChanged = true;
                     closeDeviceLocked(device);
                 } else if (readSize < 0) {
