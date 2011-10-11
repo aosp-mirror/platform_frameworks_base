89b8a2161ab6ad0211df305da1eebd6227a217e6
Jeff Sharkey <jsharkey@android.com>
2011-10-11 11:58:11 -0700

    Update internal state when iface is removed.
    Bug: 5433432
    Change-Id: I7eb0ffe3ae07f42d5c47fe4082f490478c5099bf

--- services/java/com/android/server/NetworkManagementService.java
+++ services/java/com/android/server/NetworkManagementService.java
@@ -238,6 +238,11 @@ public class NetworkManagementService extends INetworkManagementService.Stub
      * Notify our observers of an interface removal.
      */
     private void notifyInterfaceRemoved(String iface) {
+        // netd already clears out quota and alerts for removed ifaces; update
+        // our sanity-checking state.
+        mActiveAlertIfaces.remove(iface);
+        mActiveQuotaIfaces.remove(iface);
+
         for (INetworkManagementEventObserver obs : mObservers) {
             try {
                 obs.interfaceRemoved(iface);
