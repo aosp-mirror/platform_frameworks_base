5f839f8ee426cc42af3ee0eaf5236f2f6930dd15
Daniel Sandler <dsandler@android.com>
2011-10-12 01:45:26 -0400

    Fix obsolete Honeycomb platlogo.
    Bug: 5438131
    Change-Id: I848bcc8e4d6cdd28c1ffaa08b7fcceb87242932a

--- core/java/com/android/internal/app/PlatLogoActivity.java
+++ core/java/com/android/internal/app/PlatLogoActivity.java
@@ -17,32 +17,79 @@
 package com.android.internal.app;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Vibrator;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 public class PlatLogoActivity extends Activity {
     Toast mToast;
+    ImageView mContent;
+    Vibrator mZzz = new Vibrator();
+    int mCount;
+    final Handler mHandler = new Handler();
+
+    Runnable mSuperLongPress = new Runnable() {
+        public void run() {
+            mCount++;
+            mZzz.vibrate(50 * mCount);
+            final float scale = 1f + 0.25f * mCount * mCount;
+            mContent.setScaleX(scale);
+            mContent.setScaleY(scale);
+
+            if (mCount <= 3) {
+                mHandler.postDelayed(mSuperLongPress, ViewConfiguration.getLongPressTimeout());
+            } else {
+                try {
+                    startActivity(new Intent(Intent.ACTION_MAIN)
+                        .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                            | Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+                        .setClassName("com.android.systemui","com.android.systemui.Nyandroid"));
+                } catch (ActivityNotFoundException ex) {
+                    android.util.Log.e("PlatLogoActivity", "Couldn't find platlogo screensaver.");
+                }
+                finish();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        mToast = Toast.makeText(this, "REZZZZZZZ...", Toast.LENGTH_SHORT);
+        mToast = Toast.makeText(this, "Android 4.0: Ice Cream Sandwich", Toast.LENGTH_SHORT);
 
-        ImageView content = new ImageView(this);
-        content.setImageResource(com.android.internal.R.drawable.platlogo);
-        content.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+        mContent = new ImageView(this);
+        mContent.setImageResource(com.android.internal.R.drawable.platlogo);
+        mContent.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
+
+        mContent.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int action = event.getAction();
+                if (action == MotionEvent.ACTION_DOWN) {
+                    mContent.setPressed(true);
+                    mHandler.removeCallbacks(mSuperLongPress);
+                    mCount = 0;
+                    mHandler.postDelayed(mSuperLongPress, 2*ViewConfiguration.getLongPressTimeout());
+                } else if (action == MotionEvent.ACTION_UP) {
+                    if (mContent.isPressed()) {
+                        mContent.setPressed(false);
+                        mHandler.removeCallbacks(mSuperLongPress);
+                        mToast.show();
+                    }
+                }
+                return true;
+            }
+        });
         
-        setContentView(content);
-    }
-
-    @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        if (ev.getAction() == MotionEvent.ACTION_UP) {
-            mToast.show();
-        }
-        return super.dispatchTouchEvent(ev);
+        setContentView(mContent);
     }
 }
