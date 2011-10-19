5ac84423a28fea4924eb1c389d647defe1c1b93c
Gilles Debunne <debunne@google.com>
2011-10-19 09:35:58 -0700

    Typo in ViewRootImpl
    Change-Id: I4a5acfb091ce05f56cdbaa0a56809f377cbf9b39

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -281,7 +281,7 @@ public final class ViewRootImpl extends Handler implements ViewParent,
 
     final AccessibilityManager mAccessibilityManager;
 
-    AccessibilityInteractionController mAccessibilityInteractionContrtoller;
+    AccessibilityInteractionController mAccessibilityInteractionController;
 
     AccessibilityInteractionConnectionManager mAccessibilityInteractionConnectionManager;
 
@@ -3490,10 +3490,10 @@ public final class ViewRootImpl extends Handler implements ViewParent,
             throw new IllegalStateException("getAccessibilityInteractionController"
                     + " called when there is no mView");
         }
-        if (mAccessibilityInteractionContrtoller == null) {
-            mAccessibilityInteractionContrtoller = new AccessibilityInteractionController();
+        if (mAccessibilityInteractionController == null) {
+            mAccessibilityInteractionController = new AccessibilityInteractionController();
         }
-        return mAccessibilityInteractionContrtoller;
+        return mAccessibilityInteractionController;
     }
 
     private int relayoutWindow(WindowManager.LayoutParams params, int viewVisibility,
