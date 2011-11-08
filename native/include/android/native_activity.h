dd00e734f335f13b4a9e24a27e280b6112d919a5
David 'Digit' Turner <digit@google.com>
2011-11-08 14:18:11 +0100

    NDK: ANativeActivity: Fix documentation for 'clazz' member.
    As discussed on the android-ndk forum, it turns out that the
    member named 'clazz' is really a global reference to the NativeActivity
    instance. As such, it's really a handle to a VM object, not a VM class.
    
    Ideally, we would rename it to 'activity', but this cannot be done
    without breaking NDK source compatibility.
    
    Change-Id: I82ca1549b35346a3eacf9e84c4c836387fc883a6

--- native/include/android/native_activity.h
+++ native/include/android/native_activity.h
@@ -60,7 +60,14 @@ typedef struct ANativeActivity {
     JNIEnv* env;
 
     /**
-     * The NativeActivity Java class.
+     * The NativeActivity object handle.
+     *
+     * IMPORTANT NOTE: This member is mis-named. It should really be named
+     * 'activity' instead of 'clazz', since it's a reference to the
+     * NativeActivity instance created by the system for you.
+     *
+     * We unfortunately cannot change this without breaking NDK
+     * source-compatibility.
      */
     jobject clazz;
 
