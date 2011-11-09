77c500c9a1f763b31fb5a03c803b3523fcb72310
James Dong <jdong@google.com>
2011-11-09 00:48:56 -0800

    Add support for retrieving location information in mp4/3gpp files
    Change-Id: Ib8427704ef9ee5a4fa8fe1818c8a62d77b4ea687
    related-to-bug: 5560253

--- include/media/mediametadataretriever.h
+++ include/media/mediametadataretriever.h
@@ -54,6 +54,7 @@ enum {
     METADATA_KEY_BITRATE         = 20,
     METADATA_KEY_TIMED_TEXT_LANGUAGES      = 21,
     METADATA_KEY_IS_DRM          = 22,
+    METADATA_KEY_LOCATION        = 23,
 
     // Add more here...
 };
