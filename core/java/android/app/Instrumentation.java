f434e37945c752a3f92fe6e9941805d612159bdb
Svetoslav Ganov <svetoslavganov@google.com>
2011-11-11 16:50:21 -0800

    Added missing comments in Instrumentation#sendStringSync
    Change-Id: Ib20c5f9763c4ff97dec02fbb79721da93e964bc7

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -838,6 +838,11 @@ public class Instrumentation {
 
         if (events != null) {
             for (int i = 0; i < events.length; i++) {
+                // We have to change the time of an event before injecting it because
+                // all KeyEvents returned by KeyCharacterMap.getEvents() have the same
+                // time stamp and the system rejects too old events. Hence, it is
+                // possible for an event to become stale before it is injected if it
+                // takes too long to inject the preceding ones.
                 sendKeySync(KeyEvent.changeTimeRepeat(events[i], SystemClock.uptimeMillis(), 0));
             }
         }
