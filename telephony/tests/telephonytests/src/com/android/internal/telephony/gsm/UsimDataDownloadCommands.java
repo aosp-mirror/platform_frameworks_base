2149014ed3dd99fb5f03321f96917ce72a7985de
Alex Yakavenka <ayakaven@quicinc.com>
2011-12-09 11:41:30 -0800

    Telephony: Fix get_imsi and icc_io
    Make get_imsi and icc_io requests to follow ril v6 interface
    
    Change-Id: Ifdbdcfd943f985c759359e59ced82b67195f4163

--- telephony/tests/telephonytests/src/com/android/internal/telephony/gsm/UsimDataDownloadCommands.java
+++ telephony/tests/telephonytests/src/com/android/internal/telephony/gsm/UsimDataDownloadCommands.java
@@ -612,4 +612,13 @@ class UsimDataDownloadCommands extends BaseCommands {
     @Override
     public void getVoiceRadioTechnology(Message response) {
     }
+
+    @Override
+    public void getIMSIForApp(String aid, Message result) {
+    }
+
+    @Override
+    public void iccIOForApp(int command, int fileid, String path, int p1, int p2, int p3,
+            String data, String pin2, String aid, Message response) {
+    }
 }
