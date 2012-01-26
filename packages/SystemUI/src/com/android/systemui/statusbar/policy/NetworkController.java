62a1eb8972f6eb1133060ae96142abcd57d347dd
Daniel Sandler <dsandler@android.com>
2012-01-26 10:22:23 -0500

    Suppress mobile-out-of-service label if wifi connected.
    Carrier name is shown if
     - in service and connected to mobile data
     - in service and connected to wifi
    
    Carrier name is blanked if out of service and connected to
    wifi.
    
    "No internet connection" is shown if out of service without
    any other route.
    
    Bug: 5903914
    Change-Id: I59d4d0c90e4153d73c38f7f0e150becf7e021a5b

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -892,13 +892,24 @@ public class NetworkController extends BroadcastReceiver {
             mDataSignalIconId = mPhoneSignalIconId = 0;
             mobileLabel = "";
         } else {
-            // We want to show the carrier name even if data is not being routed over that link, so
-            // we look only at the service state here.
-            mobileLabel = hasService() 
-                ? mNetworkName
-                : context.getString(R.string.status_bar_settings_signal_meter_disconnected);
-            if (DEBUG) {
-                mobileLabel += "yyyyYYYYyyyyYYYY";
+            // We want to show the carrier name if in service and either:
+            //   - We are connected to mobile data, or
+            //   - We are not connected to mobile data, as long as the *reason* packets are not
+            //     being routed over that link is that we have better connectivity via wifi.
+            // If data is disconnected for some other reason but wifi is connected, we show nothing.
+            // Otherwise (nothing connected) we show "No internet connection".
+
+            if (mDataConnected) {
+                mobileLabel = mNetworkName;
+            } else if (mWifiConnected) {
+                if (hasService()) {
+                    mobileLabel = mNetworkName;
+                } else {
+                    mobileLabel = "";
+                }
+            } else {
+                mobileLabel 
+                    = context.getString(R.string.status_bar_settings_signal_meter_disconnected);
             }
 
             // Now for things that should only be shown when actually using mobile data.
