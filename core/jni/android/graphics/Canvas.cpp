889a3fa6ab9710104af60db5f73d69f253ddf254
Derek Sollenberger <derek@android.com>
2012-01-31 14:08:22 -0500

    Skia API changes as a result of an update to the Skia library.
    These changes are required to work with r3022 of Skia
    
    Change-Id: Ib7cebeb2eba6790bb38edfc2397b311cf419e17c

--- core/jni/android/graphics/Canvas.cpp
+++ core/jni/android/graphics/Canvas.cpp
@@ -67,7 +67,7 @@ public:
     static void freeCaches(JNIEnv* env, jobject) {
         // these are called in no particular order
         SkImageRef_GlobalPool::SetRAMUsed(0);
-        SkGraphics::SetFontCacheUsed(0);
+        SkGraphics::PurgeFontCache();
     }
     
     static jboolean isOpaque(JNIEnv* env, jobject jcanvas) {
