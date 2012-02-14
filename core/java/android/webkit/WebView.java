7e2346d51a4511879d92a73fd35c30c71d0f3003
Teng-Hui Zhu <ztenghui@google.com>
2012-02-14 11:22:14 -0800

    Update the scale while updating the drawGL functor
    bug:6008955
    
    webkit change:
    https://android-git.corp.google.com/g/#/c/165734/
    
    Change-Id: I5ba7998a6b8f33b44582b0158ba966054aaad2a8

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -6029,7 +6029,7 @@ public class WebView extends AbsoluteLayout
         calcOurContentVisibleRectF(mVisibleContentRect);
         nativeUpdateDrawGLFunction(mGLViewportEmpty ? null : mGLRectViewport,
                 mGLViewportEmpty ? null : mViewRectViewport,
-                mVisibleContentRect);
+                mVisibleContentRect, getScale());
     }
 
     /**
@@ -9815,7 +9815,7 @@ public class WebView extends AbsoluteLayout
     private native int      nativeGetDrawGLFunction(int nativeInstance, Rect rect,
             Rect viewRect, RectF visibleRect, float scale, int extras);
     private native void     nativeUpdateDrawGLFunction(Rect rect, Rect viewRect,
-            RectF visibleRect);
+            RectF visibleRect, float scale);
     private native void     nativeExtendSelection(int x, int y);
     private native int      nativeFindAll(String findLower, String findUpper,
             boolean sameAsLastSearch);
