04a3a4350e320b65bda9fae6500250c4880e4bf2
Ying Wang <wangying@google.com>
2012-02-28 12:53:19 -0800

    Disable ProGuard for FakeOemFeatures to fix build.
    Change-Id: If0ccf16712bf43adb5fe490906be2a13b484f726

--- packages/FakeOemFeatures/Android.mk
+++ packages/FakeOemFeatures/Android.mk
@@ -8,6 +8,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := FakeOemFeatures
 LOCAL_CERTIFICATE := platform
 
+LOCAL_PROGUARD_ENABLED := disabled
+
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
