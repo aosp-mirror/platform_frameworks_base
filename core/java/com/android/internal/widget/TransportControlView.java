1f0eac9951c7167ee9f07251d1ef60805b96bffd
Jim Miller <jaggies@google.com>
2012-03-01 16:48:54 -0800

    Fix 5958806: Allow play button to be hidden in TransportControlView
    Change-Id: I92e79cb1e33abea76f283eea389dad17d1d7c162

--- core/java/com/android/internal/widget/TransportControlView.java
+++ core/java/com/android/internal/widget/TransportControlView.java
@@ -314,7 +314,7 @@ public class TransportControlView extends FrameLayout implements OnClickListener
         final int flags = mTransportControlFlags;
         setVisibilityBasedOnFlag(mBtnPrev, flags, RemoteControlClient.FLAG_KEY_MEDIA_PREVIOUS);
         setVisibilityBasedOnFlag(mBtnNext, flags, RemoteControlClient.FLAG_KEY_MEDIA_NEXT);
-        setVisibilityBasedOnFlag(mBtnPrev, flags,
+        setVisibilityBasedOnFlag(mBtnPlay, flags,
                 RemoteControlClient.FLAG_KEY_MEDIA_PLAY
                 | RemoteControlClient.FLAG_KEY_MEDIA_PAUSE
                 | RemoteControlClient.FLAG_KEY_MEDIA_PLAY_PAUSE
