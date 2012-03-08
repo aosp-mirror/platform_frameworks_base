8c79817dc6459baa2c79db88e3c3e4d5c07d6a61
Mathias Agopian <mathias@google.com>
2012-03-07 19:36:08 -0800

    remove files that moved to frameworks/native
    Change-Id: I140d291e520097b1148930f736823650e08488f7

--- opengl/tools/glgen/stubs/jsr239/glGetString.cpp
+++
@@ -1,5 +0,0 @@
-/* const GLubyte * glGetString ( GLenum name ) */
-static jstring android_glGetString(JNIEnv *_env, jobject, jint name) {
-    const char* chars = (const char*) glGetString((GLenum) name);
-    return _env->NewStringUTF(chars);
-}
