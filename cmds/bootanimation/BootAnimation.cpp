13c2fc8ee57f2a6edcfe2b7fae122de7b0752d6a
Ricardo Cerqueira <github@cerqueira.org>
2012-03-14 19:25:27 +0000

    bootanimation: don't cache textures
    if they're expected to use a lot of VRAM
    
    bootanimation cached all the textures generated until the animation
    was completed.
    For Hi-res animations with lots of frames (like the new CM9 anim on
    xhdpi devices), this implied using large amounts of video RAM, more
    than most devices actually have available, causing the animation to
    stall (and in some cases, gralloc to lock up); so if an animation is
    expected to use a lot of RAM (rough estimation based on frame size
    and number of frames), disable cache entirely and generate the textures
    on demand
    
    Android 5.0:
    Also apply partially "bootanimation: performance enhancements" from
    @turl:
    TARGET_BOOTANIMATION_TEXTURE_CACHE allows maintainers to
    force texture cache on/off.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Ie6ce64f1c22f1000aa35150b10fbc24b13f9e70f
    
    Conflicts:
    	cmds/bootanimation/BootAnimation.cpp
    
    Conflicts:
    	cmds/bootanimation/BootAnimation.cpp

--- cmds/bootanimation/BootAnimation.cpp
+++ cmds/bootanimation/BootAnimation.cpp
@@ -583,6 +583,17 @@ bool BootAnimation::movie()
     for (size_t i=0 ; i<pcount ; i++) {
         const Animation::Part& part(animation.parts[i]);
         const size_t fcount = part.frames.size();
+
+#ifdef BOARD_USES_TEXTURE_CACHE
+#ifdef NO_TEXTURE_CACHE
+        const int useTextureCache = 0;
+#else
+        const int useTextureCache = (
+            (animation.width * animation.height * fcount) >
+            48 * 1024 * 1024) ? 1 : 0;
+#endif
+#endif
+
         glBindTexture(GL_TEXTURE_2D, 0);
 
         for (int r=0 ; !part.count || r<part.count ; r++) {
@@ -605,7 +616,11 @@ bool BootAnimation::movie()
                 const Animation::Frame& frame(part.frames[j]);
                 nsecs_t lastFrame = systemTime();
 
+#ifdef BOARD_USES_TEXTURE_CACHE
+                if (r > 0 && !useTextureCache) {
+#else
                 if (r > 0) {
+#endif
                     glBindTexture(GL_TEXTURE_2D, frame.tid);
                 } else {
                     if (part.count != 1) {
@@ -650,6 +665,10 @@ bool BootAnimation::movie()
                 }
 
                 checkExit();
+#ifdef BOARD_USES_TEXTURE_CACHE
+                if (useTextureCache)
+                    glDeleteTextures(1, &frame.tid);
+#endif
             }
 
             usleep(part.pause * ns2us(frameDuration));
