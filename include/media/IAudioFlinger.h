4acee00352b6c420f235c9b35010849ceaa9fc14
Glenn Kasten <gkasten@google.com>
2012-03-19 12:07:35 -0700

    Add TRACK_FAST for IAudioFlinger::createTrack
    Currently not implemented by client or server
    
    Change-Id: Ib11dda57db3eeb871bcc7b546e340078776875f5

--- include/media/IAudioFlinger.h
+++ include/media/IAudioFlinger.h
@@ -45,8 +45,9 @@ public:
 
     // or-able bits shared by createTrack and openRecord, but not all combinations make sense
     enum {
-        TRACK_DEFAULT = 0,
-        TRACK_TIMED   = 1,
+        TRACK_DEFAULT = 0,  // client requests a default AudioTrack
+        TRACK_TIMED   = 1,  // client requests a TimedAudioTrack
+        TRACK_FAST    = 2,  // client requests a fast AudioTrack
     };
     typedef uint32_t track_flags_t;
 
