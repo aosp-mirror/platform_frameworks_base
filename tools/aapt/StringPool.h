80a6b33be276eb6c808fce13debb4bb7a489cdad
Jeff Brown <jeffbrown@google.com>
2012-03-19 12:02:10 -0700

    Fix build break on glibc, for real.
    Change-Id: I8bbec237229b05f96c708d41f3c4da17e2a90e2b

--- tools/aapt/StringPool.h
+++ tools/aapt/StringPool.h
@@ -139,7 +139,11 @@ public:
     const Vector<size_t>* offsetsForString(const String16& val) const;
 
 private:
+#ifdef __GLIBC__
+    static int config_sort(const void* lhs, const void* rhs, void* state);
+#else
     static int config_sort(void* state, const void* lhs, const void* rhs);
+#endif
 
     const bool                              mUTF8;
 
