9a5f56563f2a46ea6d1991dd1819bb9240f2dab1
John Reck <jreck@google.com>
2012-03-20 16:01:26 -0700

    Don't contentInvalidate when changing a WebView property
    Change-Id: I629671f1edffed945395eef182eeb6e8617e32de

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -1638,7 +1638,7 @@ public class WebSettingsClassic extends WebSettings {
      */
     public void setProperty(String key, String value) {
         if (mWebView.nativeSetProperty(key, value)) {
-            mWebView.contentInvalidateAll();
+            mWebView.invalidate();
         }
     }
 
