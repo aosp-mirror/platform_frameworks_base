d10ca466db0bccf7ec76b7181662ca6b6e682171
Glenn Kasten <gkasten@google.com>
2012-03-19 17:36:25 -0700

    Add libmedia_native
    Change-Id: I3ac357c78fb89f108d15c6e5b9fa317de0e9fb9a

--- services/audioflinger/Android.mk
+++ services/audioflinger/Android.mk
@@ -15,6 +15,7 @@ LOCAL_C_INCLUDES := \
     $(call include-path-for, audio-effects) \
     $(call include-path-for, audio-utils)
 
+# FIXME keep libmedia_native but remove libmedia after split
 LOCAL_SHARED_LIBRARIES := \
     libaudioutils \
     libcommon_time_client \
@@ -22,6 +23,7 @@ LOCAL_SHARED_LIBRARIES := \
     libutils \
     libbinder \
     libmedia \
+    libmedia_native \
     libhardware \
     libhardware_legacy \
     libeffects \
