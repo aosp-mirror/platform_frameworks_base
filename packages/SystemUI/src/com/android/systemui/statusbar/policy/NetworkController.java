3d30590bc4874de32f7b04fa3e5db1f95f71de1d
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-04-03 10:43:54 +0700

    SystemUI: Fix data activity overlay not being removed when data is disabled
    If mobile data connection is disabled while there is active data
    activity, the data activity overlay will not be removed and will
    be stuck there. This patch fixes that.
    
    Change-Id: I4cc1a14bb2e2d85687752657608e5989adfe9af1

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -968,6 +968,8 @@ public class NetworkController extends BroadcastReceiver {
                 combinedActivityIconId = mMobileActivityIconId;
                 combinedSignalIconId = mDataSignalIconId; // set by updateDataIcon()
                 mContentDescriptionCombinedSignal = mContentDescriptionDataType;
+            } else {
+                mMobileActivityIconId = 0;
             }
         }
 
