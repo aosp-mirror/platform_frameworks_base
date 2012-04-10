96ac720117979ab0507d8760f49076a0a1e3bfb8
bxu10X <bxu10X@intel.com>
2012-04-10 16:52:36 +0800

    [Audio]Press camera button during Http live Audio playing, then return Browser, DUT display abnormal.
    When long press camera key, dispatchKeyEvent function will add control surface after the control view hide,
    actually, we needn't show control view after we press camera key.
    
    Change-Id: I45175c4eb97b3f13469cd9d8bacb295c9c5803dd
    Author: Bin Xu <bxu10X@intel.com>
    Signed-off-by: Bin Xu <bxu10X@intel.com>
    Singed-off-by: Shuo Gao <shuo.gao@intel.com>
    Signed-off-by: Bruce Beare <bruce.j.beare@intel.com>
    Signed-off-by: Jack Ren <jack.ren@intel.com>
    Author-tracking-BZ: 28344

--- core/java/android/widget/MediaController.java
+++ core/java/android/widget/MediaController.java
@@ -477,7 +477,8 @@ public class MediaController extends FrameLayout {
             return true;
         } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
                 || keyCode == KeyEvent.KEYCODE_VOLUME_UP
-                || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE) {
+                || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE
+                || keyCode == KeyEvent.KEYCODE_CAMERA) {
             // don't show the controls for volume adjustment
             return super.dispatchKeyEvent(event);
         } else if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_MENU) {
