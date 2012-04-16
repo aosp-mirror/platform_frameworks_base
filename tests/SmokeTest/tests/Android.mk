8debcfa2904fd1e2fc6389bc49575f3fb2b251b3
Brett Chabot <brettchabot@android.com>
2012-04-16 10:35:32 -0700

    Build Smoke tests against the SDK.
    Done so the new 'launch all apps' test can be utilized on older platforms.
    
    Change-Id: Ie74ce258ceb372e78a0634a633bbb19dcd8e2779

--- tests/SmokeTest/tests/Android.mk
+++ tests/SmokeTest/tests/Android.mk
@@ -4,8 +4,6 @@ include $(CLEAR_VARS)
 # We only want this apk build for tests.
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_JAVA_LIBRARIES := android.test.runner
-
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
@@ -16,6 +14,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := SmokeTest
 
 LOCAL_INSTRUMENTATION_FOR := SmokeTestApp
+
+LOCAL_SDK_VERSION := 8
 
 include $(BUILD_PACKAGE)
 
