4bedba49fe6989387fd04499b175af56e068da28
Sreenidhi T <nidhit@broadcom.com>
2012-04-18 04:24:33 -0700

    Change done in BluetoothAdapter to use the same API for interacting with the Bluetooth Service. setName was being done using mService while getName was using mManagerService. As a result, the device name was not being updated in Localadapter getname call. Modified to use mService in both cases.
    Change-Id: I2e900a782df11fd511319fbb42d94324e50ae89b

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -584,7 +584,7 @@ public final class BluetoothAdapter {
      */
     public String getName() {
         try {
-            return mManagerService.getName();
+            return mService.getName();
         } catch (RemoteException e) {Log.e(TAG, "", e);}
         return null;
     }
