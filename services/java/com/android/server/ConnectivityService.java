2473a4bae16b2713753dd80f8356ad18fce43207
Robert Greenwalt <rgreenwalt@google.com>
2012-04-23 18:00:37 -0700

    Stop complaining about failure to remove routes
    We often get told a network has gone away after the interface is downed
    and the routes are auto-removed.  Change the log to VDBUG so standard
    builds don't have the noise.
    
    bug:2856577
    Change-Id: If12ec3ed3b646ff6ef89b3778d037cfc478c0d25

--- services/java/com/android/server/ConnectivityService.java
+++ services/java/com/android/server/ConnectivityService.java
@@ -1341,7 +1341,7 @@ private NetworkStateTracker makeWimaxStateTracker() {
                         mNetd.removeRoute(ifaceName, r);
                     } catch (Exception e) {
                         // never crash - catch them all
-                        if (DBG) loge("Exception trying to remove a route: " + e);
+                        if (VDBG) loge("Exception trying to remove a route: " + e);
                         return false;
                     }
                 } else {
@@ -1353,7 +1353,7 @@ private NetworkStateTracker makeWimaxStateTracker() {
                     mNetd.removeSecondaryRoute(ifaceName, r);
                 } catch (Exception e) {
                     // never crash - catch them all
-                    if (DBG) loge("Exception trying to remove a route: " + e);
+                    if (VDBG) loge("Exception trying to remove a route: " + e);
                     return false;
                 }
             }
