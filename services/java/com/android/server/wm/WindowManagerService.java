e15ccb93add99ebb9cd7aec03a04faa37f45b39d
Svetoslav Ganov <svetoslavganov@google.com>
2012-05-16 15:48:55 -0700

    Changing the interaction model of the touch explorer.
    1. Now the user have to double tap to activate the last
       item. If the last touched window is not active because
       it does not take input focus the click on the last
       touch explored location. Othewise the click is on the
       accessibility focus location.
    
    bug:5932640
    
    Change-Id: Ibb7b97262a7c5f2f94abef429e02790fdc91a8dd

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -6561,6 +6561,16 @@ public class WindowManagerService extends IWindowManager.Stub
         sendScreenStatusToClients();
     }
 
+    public IBinder getFocusedWindowClientToken() {
+        synchronized (mWindowMap) {
+            WindowState windowState = getFocusedWindowLocked();
+            if (windowState != null) {
+                return windowState.mClient.asBinder();
+            }
+            return null;
+        }
+    }
+
     private WindowState getFocusedWindow() {
         synchronized (mWindowMap) {
             return getFocusedWindowLocked();
