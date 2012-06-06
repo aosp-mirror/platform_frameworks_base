462a7284b52503f1d7f2b3b9c9e9397d0c40d0ed
Svetoslav Ganov <svetoslavganov@google.com>
2012-06-05 21:10:00 -0700

    Removing leftover code to sync accessibility and input focus.
    1. We have decided to let the accessibility services take care
       for syncing accessibility and input focus. Some apps may
       move input focus when a given view takes input focus which
       dragging accessibility focus can lead to a loop in the
       focus traversal. This change removes some leftover sync
       code.
    
    bug:6616861
    
    Change-Id: I57cb44e315a386d13596794d3767b559e9ee3d99

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -6296,13 +6296,11 @@ public class View implements Drawable.Callback, Drawable.Callback2, KeyEvent.Cal
     private void requestAccessibilityFocusFromHover() {
         if (includeForAccessibility() && isActionableForAccessibility()) {
             requestAccessibilityFocus();
-            requestFocusNoSearch(View.FOCUS_DOWN, null);
         } else {
             if (mParent != null) {
                 View nextFocus = mParent.findViewToTakeAccessibilityFocusFromHover(this, this);
                 if (nextFocus != null) {
                     nextFocus.requestAccessibilityFocus();
-                    nextFocus.requestFocusNoSearch(View.FOCUS_DOWN, null);
                 }
             }
         }
