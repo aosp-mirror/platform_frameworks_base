983119ab22a18c743e4084dff27f35e3f490dd34
Svetoslav Ganov <svetoslavganov@google.com>
2012-07-03 21:04:10 -0700

    AccessibilityNodeInfo bounds in screen incorrect if application scale not one.
    1. If the application does not accommodate different screen density the
       system applies an application scale equal to the device density over
       the a default density. The AccessibilityNodeInfo coordinates were not
       reported after applying the compatibility scale, therefore the bounds
       in parent and screen were not as perceived by the user.
    
    bug:6764586
    
    Change-Id: Iae2d6ea81049364194c7cb09df2240b5eda3d939

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -4820,10 +4820,14 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      */
     void onInitializeAccessibilityNodeInfoInternal(AccessibilityNodeInfo info) {
         Rect bounds = mAttachInfo.mTmpInvalRect;
+        final float applicationScale = mAttachInfo.mApplicationScale;
+
         getDrawingRect(bounds);
+        bounds.scale(applicationScale);
         info.setBoundsInParent(bounds);
 
         getBoundsOnScreen(bounds);
+        bounds.scale(applicationScale);
         info.setBoundsInScreen(bounds);
 
         ViewParent parent = getParentForAccessibility();
