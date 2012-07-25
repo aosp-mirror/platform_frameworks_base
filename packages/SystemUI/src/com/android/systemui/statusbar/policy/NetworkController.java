4df2c7fa44a71f351ac9aab5eb9c6909d76e9419
Daniel Sandler <dsandler@android.com>
2012-07-25 12:31:22 -0400

    Fix NPE on some phones at startup.
    Bug: 6877589
    Change-Id: I65ff37b4cf6b59a72a7aa665f252f5fb34f3b183

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkController.java
@@ -917,6 +917,7 @@ public class NetworkController extends BroadcastReceiver {
         String wifiLabel = "";
         String mobileLabel = "";
         int N;
+        final boolean emergencyOnly = (mServiceState != null && mServiceState.isEmergencyOnly());
 
         if (!mHasMobileDataFeature) {
             mDataSignalIconId = mPhoneSignalIconId = 0;
@@ -932,8 +933,8 @@ public class NetworkController extends BroadcastReceiver {
 
             if (mDataConnected) {
                 mobileLabel = mNetworkName;
-            } else if (mConnected || mServiceState.isEmergencyOnly()) {
-                if (hasService() || mServiceState.isEmergencyOnly()) {
+            } else if (mConnected || emergencyOnly) {
+                if (hasService() || emergencyOnly) {
                     // The isEmergencyOnly test covers the case of a phone with no SIM
                     mobileLabel = mNetworkName;
                 } else {
