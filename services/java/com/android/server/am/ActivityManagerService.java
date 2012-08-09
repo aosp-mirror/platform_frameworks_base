f958b321fa6038fdc40b930c38313d467319f88f
Zoran Marceta <marcetaz@google.com>
2012-08-09 20:27:12 +0900

    ActivityStack handler now use ActivityManager looper instead of ServerThread looper
    Bug: 6957716
    Change-Id: I36feb45ef7a85885ab812092c7b28e91886a4821

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -1342,7 +1342,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         context.setTheme(android.R.style.Theme_Holo);
         m.mContext = context;
         m.mFactoryTest = factoryTest;
-        m.mMainStack = new ActivityStack(m, context, true);
+        m.mMainStack = new ActivityStack(m, context, true, thr.mLooper);
         
         m.mBatteryStatsService.publish(context);
         m.mUsageStatsService.publish(context);
@@ -1363,6 +1363,7 @@ public final class ActivityManagerService extends ActivityManagerNative
     
     static class AThread extends Thread {
         ActivityManagerService mService;
+        Looper mLooper;
         boolean mReady = false;
 
         public AThread() {
@@ -1380,6 +1381,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
             synchronized (this) {
                 mService = m;
+                mLooper = Looper.myLooper();
                 notifyAll();
             }
 
