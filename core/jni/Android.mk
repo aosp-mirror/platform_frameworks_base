bdd23ae9f5ea2b3e0720e76711345cad0fa8d6dd
Kenny Root <kroot@google.com>
2012-08-16 10:43:47 -0700

    Use libcore Posix class for StatFs implementation
    Remove some JNI and duplicated functionality and use libcore's Posix
    class for the statfs function instead.
    
    Change-Id: Ic1e161dc10c18c2c6ee81d895a0efd8910086dbf

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -67,7 +67,6 @@ LOCAL_SRC_FILES:= \
 	android_os_ParcelFileDescriptor.cpp \
 	android_os_Parcel.cpp \
 	android_os_SELinux.cpp \
-	android_os_StatFs.cpp \
 	android_os_SystemClock.cpp \
 	android_os_SystemProperties.cpp \
 	android_os_Trace.cpp \
