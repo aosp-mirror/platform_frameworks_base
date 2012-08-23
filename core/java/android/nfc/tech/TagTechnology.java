4470f50273f85beb0d340e6bede9955459ae3aa2
doug yeager <doug@simplytapp.com>
2012-08-23 10:24:41 -0500

    card emulation patch port to JB
    Change-Id: I73fe9aa30830951ea5b9d44e9a9bf83cb720e567

--- core/java/android/nfc/tech/TagTechnology.java
+++ core/java/android/nfc/tech/TagTechnology.java
@@ -158,6 +158,24 @@ public interface TagTechnology extends Closeable {
     public static final int NFC_BARCODE = 10;
 
     /**
+     * This technology is an instance of {@link IsoPcdA}.
+     * <p>Support for this technology type is optional. If a stack doesn't support this technology
+     * type tags using it must still be discovered and present the lower level radio interface
+     * technologies in use.
+     * @hide
+     */
+    public static final int ISO_PCD_A = 11;
+
+    /**
+     * This technology is an instance of {@link IsoPcdB}.
+     * <p>Support for this technology type is optional. If a stack doesn't support this technology
+     * type tags using it must still be discovered and present the lower level radio interface
+     * technologies in use.
+     * @hide
+     */
+    public static final int ISO_PCD_B = 12;
+
+    /**
      * Get the {@link Tag} object backing this {@link TagTechnology} object.
      * @return the {@link Tag} backing this {@link TagTechnology} object.
      */
