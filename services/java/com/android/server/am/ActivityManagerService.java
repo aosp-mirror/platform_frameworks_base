4ee364982e783b4a2eac6e05f81d167c6121e110
Vairavan Srinivasan <vairav@codeaurora.org>
2012-09-03 17:09:58 -0700

    Am: Clean mLaunchingProviders of all providers started by the client
    Launching providers is removed from mLaunchingProviders when the client
    process dies and works as long as the client process had requested just
    one provider and not otherwise. Fix is to make sure mLaunchingProviders
    is updated to remove all providers launched by the client process.
    
    Change-Id: Ib4b8b44b6a102762a6558f65fb2e6dbf11d725ea

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -10950,7 +10950,9 @@ public final class ActivityManagerService extends ActivityManagerNative
                     restart = true;
                 } else {
                     removeDyingProviderLocked(app, cpr, true);
+                    // cpr should have been removed from mLaunchingProviders
                     NL = mLaunchingProviders.size();
+                    i--;
                 }
             }
         }
