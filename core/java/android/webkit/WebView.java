42d66983bc3c80c9a868d51e30f0faf07aa0c429
John Reck <jreck@google.com>
2012-09-14 15:19:41 -0700

    DO NOT MERGE Improve focal point tracking
    Change-Id: I2c37b6c0a2fec26cb6fddb00100fe03805101057

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -4362,7 +4362,7 @@ public class WebView extends AbsoluteLayout
 
         // A multi-finger gesture can look like a long press; make sure we don't take
         // long press actions if we're scaling.
-        final ScaleGestureDetector detector = mZoomManager.getMultiTouchGestureDetector();
+        final ScaleGestureDetector detector = mZoomManager.getScaleGestureDetector();
         if (detector != null && detector.isInProgress()) {
             return false;
         }
@@ -6319,7 +6319,7 @@ public class WebView extends AbsoluteLayout
                     // Scaling implies free-roaming movement. Note this is only ever a question
                     // if mZoomManager.supportsPanDuringZoom() is true.
                     final ScaleGestureDetector detector =
-                      mZoomManager.getMultiTouchGestureDetector();
+                      mZoomManager.getScaleGestureDetector();
                     mAverageAngle = calculateDragAngle(deltaX, deltaY);
                     if (detector == null || !detector.isInProgress()) {
                         // if it starts nearly horizontal or vertical, enforce it
@@ -6628,28 +6628,40 @@ public class WebView extends AbsoluteLayout
         }
 
         final ScaleGestureDetector detector =
-            mZoomManager.getMultiTouchGestureDetector();
+            mZoomManager.getScaleGestureDetector();
 
-        // A few apps use WebView but don't instantiate gesture detector.
-        // We don't need to support multi touch for them.
-        if (detector == null) return;
+        int action = ev.getActionMasked();
+        final boolean pointerUp = action == MotionEvent.ACTION_POINTER_UP;
+        final boolean configChanged =
+            action == MotionEvent.ACTION_POINTER_UP ||
+            action == MotionEvent.ACTION_POINTER_DOWN;
+        final int skipIndex = pointerUp ? ev.getActionIndex() : -1;
 
-        float x = ev.getX();
-        float y = ev.getY();
+        // Determine focal point
+        float sumX = 0, sumY = 0;
+        final int count = ev.getPointerCount();
+        for (int i = 0; i < count; i++) {
+            if (skipIndex == i) continue;
+            sumX += ev.getX(i);
+            sumY += ev.getY(i);
+        }
+        final int div = pointerUp ? count - 1 : count;
+        float x = sumX / div;
+        float y = sumY / div;
 
-        if (mPreventDefault != PREVENT_DEFAULT_YES) {
+        if (configChanged) {
+            mLastTouchX = Math.round(x);
+            mLastTouchY = Math.round(y);
+            mLastTouchTime = ev.getEventTime();
+            cancelLongPress();
+            mPrivateHandler.removeMessages(SWITCH_TO_LONGPRESS);
+        }
+
+        if (mPreventDefault != PREVENT_DEFAULT_YES && detector != null) {
             detector.onTouchEvent(ev);
-
             if (detector.isInProgress()) {
-                if (DebugFlags.WEB_VIEW) {
-                    Log.v(LOGTAG, "detector is in progress");
-                }
                 mLastTouchTime = ev.getEventTime();
-                x = detector.getFocusX();
-                y = detector.getFocusY();
 
-                cancelLongPress();
-                mPrivateHandler.removeMessages(SWITCH_TO_LONGPRESS);
                 if (!mZoomManager.supportsPanDuringZoom()) {
                     return;
                 }
@@ -6660,14 +6672,9 @@ public class WebView extends AbsoluteLayout
             }
         }
 
-        int action = ev.getActionMasked();
         if (action == MotionEvent.ACTION_POINTER_DOWN) {
             cancelTouch();
             action = MotionEvent.ACTION_DOWN;
-        } else if (action == MotionEvent.ACTION_POINTER_UP) {
-            // set mLastTouchX/Y to the remaining points for multi-touch.
-            mLastTouchX = Math.round(x);
-            mLastTouchY = Math.round(y);
         } else if (action == MotionEvent.ACTION_MOVE) {
             // negative x or y indicate it is on the edge, skip it.
             if (x < 0 || y < 0) {
