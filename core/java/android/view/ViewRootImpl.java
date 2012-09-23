3b84206bba19ae2bc9decef6a88f97951c2a205c
Dianne Hackborn <hackbod@google.com>
2012-09-23 13:29:44 -0700

    Fix issue #7184877: Calendar Locksceen Widget crashes and forces phone to reboot
    Don't kill the system uid if we are running out of RAM.
    
    Change-Id: Ie1818a3241fc80d4dfa19f8e8bdad22d164d7baa

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -52,6 +52,7 @@ import android.os.RemoteException;
 import android.os.SystemClock;
 import android.os.SystemProperties;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.util.AndroidRuntimeException;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -1493,7 +1494,8 @@ public final class ViewRootImpl implements ViewParent,
                             } catch (Surface.OutOfResourcesException e) {
                                 Log.e(TAG, "OutOfResourcesException initializing HW surface", e);
                                 try {
-                                    if (!mWindowSession.outOfMemory(mWindow)) {
+                                    if (!mWindowSession.outOfMemory(mWindow) &&
+                                            Process.myUid() != Process.SYSTEM_UID) {
                                         Slog.w(TAG, "No processes killed for memory; killing self");
                                         Process.killProcess(Process.myPid());
                                     }
