5bcb55186ebda12d9e4308043898f7aa3ac5c952
Doug Zongker <dougz@google.com>
2012-09-24 12:24:54 -0700

    fix argument parser for global settings URLs
    Make content://settings/global/setting_name URLs work like system and
    secure URLs.
    
    Bug: 7212535
    Change-Id: I33e388a0cc80309453714eab726ce45b3f8fef73

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -252,7 +252,8 @@ public class SettingsProvider extends ContentProvider {
                 if (!DatabaseHelper.isValidTable(this.table)) {
                     throw new IllegalArgumentException("Bad root path: " + this.table);
                 }
-                if (TABLE_SYSTEM.equals(this.table) || TABLE_SECURE.equals(this.table)) {
+                if (TABLE_SYSTEM.equals(this.table) || TABLE_SECURE.equals(this.table) ||
+                    TABLE_GLOBAL.equals(this.table)) {
                     this.where = Settings.NameValueTable.NAME + "=?";
                     this.args = new String[] { url.getPathSegments().get(1) };
                 } else {
