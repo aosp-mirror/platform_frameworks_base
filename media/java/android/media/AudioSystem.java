c203ecfd9f6c356542fcb10bc85b27b98b170ddf
Jean-Michel Trivi <jmtrivi@google.com>
2012-10-11 10:13:50 -0700

    Use device availability to start WFD audio routing
    Don't use the force_use mechanism to start WFD audio routing,
     but rather only the availability of the remote submix audio
     device. This is matched by a change in the audio policy
     manager.
    
    Bug 7318180
    
    Change-Id: I756f032d8ca0375c3dbeddb7c321261c497539bc

--- media/java/android/media/AudioSystem.java
+++ media/java/android/media/AudioSystem.java
@@ -360,9 +360,8 @@ public class AudioSystem
     public static final int FORCE_ANALOG_DOCK = 8;
     public static final int FORCE_DIGITAL_DOCK = 9;
     public static final int FORCE_NO_BT_A2DP = 10;
-    public static final int FORCE_REMOTE_SUBMIX = 11;
-    public static final int FORCE_SYSTEM_ENFORCED = 12;
-    private static final int NUM_FORCE_CONFIG = 13;
+    public static final int FORCE_SYSTEM_ENFORCED = 11;
+    private static final int NUM_FORCE_CONFIG = 12;
     public static final int FORCE_DEFAULT = FORCE_NONE;
 
     // usage for setForceUse, must match AudioSystem::force_use
