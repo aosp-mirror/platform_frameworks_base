72898e982867ff11ff6494ad71898176e30127a9
Fabrice Di Meglio <fdimeglio@google.com>
2012-10-25 11:22:39 -0700

    FIx bug #7414801 Should make private and final View.TEXT_DIRECTION_DEFAULT and View.TEXT_ALIGNMENT_DEFAULT constants
    - made them private and final
    - fixed comment
    
    Change-Id: Ia1d22329edb7ac26e7bcd0489770a99ad41c4f0e

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -1864,7 +1864,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
     /**
      * Default horizontal layout direction.
-     * @hide
      */
     private static final int LAYOUT_DIRECTION_DEFAULT = LAYOUT_DIRECTION_INHERIT;
 
@@ -1914,7 +1913,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
     /**
      * Default text direction is inherited
      */
-    public static int TEXT_DIRECTION_DEFAULT = TEXT_DIRECTION_INHERIT;
+    private static final int TEXT_DIRECTION_DEFAULT = TEXT_DIRECTION_INHERIT;
 
     /**
      * Bit shift to get the horizontal layout direction. (bits after LAYOUT_DIRECTION_RESOLVED)
@@ -2024,7 +2023,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
     /**
      * Default text alignment is inherited
      */
-    public static int TEXT_ALIGNMENT_DEFAULT = TEXT_ALIGNMENT_GRAVITY;
+    private static final int TEXT_ALIGNMENT_DEFAULT = TEXT_ALIGNMENT_GRAVITY;
 
     /**
       * Bit shift to get the horizontal layout direction. (bits after DRAG_HOVERED)
@@ -3224,7 +3223,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         mContext = context;
         mResources = context != null ? context.getResources() : null;
         mViewFlags = SOUND_EFFECTS_ENABLED | HAPTIC_FEEDBACK_ENABLED;
-        // Set layout and text direction defaults
+        // Set some flags defaults
         mPrivateFlags2 =
                 (LAYOUT_DIRECTION_DEFAULT << PFLAG2_LAYOUT_DIRECTION_MASK_SHIFT) |
                 (TEXT_DIRECTION_DEFAULT << PFLAG2_TEXT_DIRECTION_MASK_SHIFT) |
