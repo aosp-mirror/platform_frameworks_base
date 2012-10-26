1957d281ea123e4925e51fa5ad22ce239ef2a07d
Fabrice Di Meglio <fdimeglio@google.com>
2012-10-25 17:42:39 -0700

    Fix bug #7419054 TextView Drawables resolution is broken in RTL mode
    - check layout direction previous value in the onResolveDrawables(int) callback
    - dont do any Drawables resolution if we cannot resolve the layout direction
    - also remove unnecessary call to resolveRtlPropertiesIfNeeded() in ViewGroup when
      adding a child as the call to resolveRtlPropertiesIfNeeded() will be done into
      the measure() call itself later
    
    Change-Id: I62237af3d307dfea203f7f2865551d1c61a0e0b8

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -14198,11 +14198,13 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * @hide
      */
     protected void resolveDrawables() {
-        if (mBackground != null) {
-            mBackground.setLayoutDirection(getLayoutDirection());
+        if (canResolveLayoutDirection()) {
+            if (mBackground != null) {
+                mBackground.setLayoutDirection(getLayoutDirection());
+            }
+            mPrivateFlags2 |= PFLAG2_DRAWABLE_RESOLVED;
+            onResolveDrawables(getLayoutDirection());
         }
-        mPrivateFlags2 |= PFLAG2_DRAWABLE_RESOLVED;
-        onResolveDrawables(getLayoutDirection());
     }
 
     /**
