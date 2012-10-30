3dffb8cb691ccd3187cec193223d53b32339ed8b
Victoria Lease <violets@google.com>
2012-10-30 10:44:14 -0700

    clarify Geofence.createCircle() param ranges
    This commit adds the valid ranges to the latitude/longitude
    parameters in Geofence.createCircle()'s javadoc.
    
    Bug: 7172696
    Change-Id: Iff6e3c3723d3fd9b6393bbc827ec5755c0d034af

--- location/java/android/location/Geofence.java
+++ location/java/android/location/Geofence.java
@@ -38,8 +38,8 @@ public final class Geofence implements Parcelable {
     /**
      * Create a circular geofence (on a flat, horizontal plane).
      *
-     * @param latitude latitude in degrees
-     * @param longitude longitude in degrees
+     * @param latitude latitude in degrees, between -90 and +90 inclusive
+     * @param longitude longitude in degrees, between -180 and +180 inclusive
      * @param radius radius in meters
      * @return a new geofence
      * @throws IllegalArgumentException if any parameters are out of range
