43d84518f2380e45659fbb31455479ef018b9329
John Spurlock <jspurlock@google.com>
2012-11-09 10:27:33 -0500

    Disable navbar searchlight if search assist not available.
    Bug:7506441
    Change-Id: I55c740d4c3a45b9f7fdfa38346d1bc3fb1299153

--- core/java/android/app/SearchManager.java
+++ core/java/android/app/SearchManager.java
@@ -858,6 +858,9 @@ public class SearchManager
      */
     public Intent getAssistIntent(Context context, int userHandle) {
         try {
+            if (mService == null) {
+                return null;
+            }
             ComponentName comp = mService.getAssistIntent(userHandle);
             if (comp == null) {
                 return null;
