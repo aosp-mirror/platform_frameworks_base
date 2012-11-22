952af4176ff6fc8643aae6c91b4c7e74cec44ece
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-11-22 06:02:18 +0000

    telephony: SignalStrengh: Don't calculate a LTE snr if the srq is invalid
    If the previous values are invalid and signal quality is -1, chances are there's
    no LTE signal at all, so don't calculate the SNR and mark it invalid as well.
    
    This fixes the strength indicator in GSM devices where it was being skewed
    by the ghost LTE value
    
    Change-Id: I070573401c141aa99941a332ee256246697abb90

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -369,7 +369,7 @@ public class SignalStrength implements Parcelable {
         mLteSignalStrength = (mLteSignalStrength >= 0) ? mLteSignalStrength : 99;
         mLteRsrp = ((mLteRsrp >= 44) && (mLteRsrp <= 140)) ? -mLteRsrp : SignalStrength.INVALID;
         mLteRsrq = ((mLteRsrq >= 3) && (mLteRsrq <= 20)) ? -mLteRsrq : SignalStrength.INVALID;
-        mLteRssnr = ((mLteRssnr >= -200) && (mLteRssnr <= 300)) ? mLteRssnr
+        mLteRssnr = ((mLteRssnr >= -200) && (mLteRssnr <= 300) && !(mLteRsrq == SignalStrength.INVALID && mLteRssnr == -1)) ? mLteRssnr
                 : SignalStrength.INVALID;
         // Cqi no change
         if (DBG) log("Signal after validate=" + this);
