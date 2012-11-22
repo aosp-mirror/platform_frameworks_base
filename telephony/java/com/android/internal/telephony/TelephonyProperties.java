23f1b4afaa238963f9b9aeec760644870d5a50c7
dhacker29 <davidhackerdvm@gmail.com>
2012-11-21 20:18:27 -0600

    TelephonyProperties: Alpha ID fix for OPEN_CHANNEL and other p-commands (1/2)
    p-cmds that support the below mentioned scenarios:
    - SETUP CALL
    - OPEN CHANNEL
    - SEND DTMF
    - SEND SMS
    - SEND SS
    - SEND USSD
    - LAUNCH BROWSER
    - PLAY TONE
    - SELECT ITEM
    
    Senarios handled :
    - NO ALPHA ID : Introduce a new configurable sys prop
      "persist.atel.noalpha.usrcnf".
       --  Allow display of default UI confirmation if prop is set to true
       --  No UI confirmation to be show if prop is set to false
    - NULL ALPHA ID (len = 0 ) :
       -- NO UI confirmation to be displayed
    
    Cherry-picked from CAF commit 5ea0d64186ca08abc0857aae1f83de969393be3b
    Without this SimToolkit will pop up asking for user response instead
    of recieveing response from framework.
    
    Change-Id: I012707285ee3a93119c2e2956ec599d631db10d5
    Signed-off-by: dhacker29 <davidhackerdvm@gmail.com>
    
    Conflicts:
    
    	telephony/java/com/android/internal/telephony/TelephonyProperties.java

--- telephony/java/com/android/internal/telephony/TelephonyProperties.java
+++ telephony/java/com/android/internal/telephony/TelephonyProperties.java
@@ -197,4 +197,10 @@ public interface TelephonyProperties
      * Set to true to indicates support for simultaneous voice and data.
      */
     static final String PROPERTY_SVDATA = "ro.config.svlte1x";
+
+    /**
+     * Property to control alpha ID display for proactive commands
+     * Type: boolean ( true = alpha display enabled, false = alpha display disabled)
+     */
+    static final String PROPERTY_ALPHA_USRCNF = "persist.atel.noalpha.usrcnf";
 }
