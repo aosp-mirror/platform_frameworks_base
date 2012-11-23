70f1a2cfa974d743a4562b05eac8e4438fe82e21
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-23 15:48:20 +0700

    PackageManager: fix app lib migration for device with /datadata
    For devices using /datadata, dataPathString will point
    to /datadata while nativeLibraryDir will point to /data/data.
    Thus, compare to /data/data directly to avoid problems.
    
    Change-Id: I11149cf9cc5a573cc85513e12d3569b514f5ffb5

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -4569,7 +4569,10 @@ public class PackageManagerService extends IPackageManager.Stub {
                          * Update native library dir if it starts with
                          * /data/data
                          */
-                        if (nativeLibraryDir.getPath().startsWith(dataPathString)) {
+                        // For devices using /datadata, dataPathString will point
+                        // to /datadata while nativeLibraryDir will point to /data/data.
+                        // Thus, compare to /data/data directly to avoid problems.
+                        if (nativeLibraryDir.getPath().startsWith("/data/data")) {
                             setInternalAppNativeLibraryPath(pkg, pkgSetting);
                             nativeLibraryDir = new File(pkg.applicationInfo.nativeLibraryDir);
                         }
