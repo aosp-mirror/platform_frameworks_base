8103890a59de6ed4abaedaad80e66666ea59f9b5
Dianne Hackborn <hackbod@google.com>
2012-11-26 17:04:09 -0800

    Improve debugging for issue #7586414: AlarmManager wakelocks held
    In alarm manager, print a summary of the top 10 alarms by time
    being executed.  Keep track of execution time (and wake count) of
    each type of alarm for each application so this can be printed in
    the summary (and used to compute the top 10 alarms).  Rework how
    the alarm summary stats are tracked so that we don't need to hold
    on to the full Intent for each stat and can get the Intent information
    at the time the alarm is sent rather than waiting for whatever Intent
    comes back in the result.
    
    Also in the battery stats: sort the kernel wake locks by time, add
    a new section showing all partial wake locks across all applications
    sorted by time.
    
    Finally a new LocalLog class that is used by AlarmManager to log
    important warning messages, so these can also be later found in
    its dumpsys output.
    
    Change-Id: Icc07810053e60fb623a49937e696819cb8352b06

---
+++ core/java/com/android/internal/util/LocalLog.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.util;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import android.util.Slog;
+
+/**
+ * Helper class for logging serious issues, which also keeps a small
+ * snapshot of the logged events that can be printed later, such as part
+ * of a system service's dumpsys output.
+ * @hide
+ */
+public class LocalLog {
+    private final String mTag;
+    private final int mMaxLines = 20;
+    private final ArrayList<String> mLines = new ArrayList<String>(mMaxLines);
+
+    public LocalLog(String tag) {
+        mTag = tag;
+    }
+
+    public void w(String msg) {
+        synchronized (mLines) {
+            Slog.w(mTag, msg);
+            if (mLines.size() >= mMaxLines) {
+                mLines.remove(0);
+            }
+            mLines.add(msg);
+        }
+    }
+
+    public boolean dump(PrintWriter pw, String header, String prefix) {
+        synchronized (mLines) {
+            if (mLines.size() <= 0) {
+                return false;
+            }
+            if (header != null) {
+                pw.println(header);
+            }
+            for (int i=0; i<mLines.size(); i++) {
+                if (prefix != null) {
+                    pw.print(prefix);
+                }
+                pw.println(mLines.get(i));
+            }
+            return true;
+        }
+    }
+}
