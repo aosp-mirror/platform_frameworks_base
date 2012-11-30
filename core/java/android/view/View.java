cb5f9db47a471620061ccb6a574afc209c610dd2
Fabrice Di Meglio <fdimeglio@google.com>
2012-11-30 15:32:56 -0800

    Fix bug #7651552 resetRtlProperties() is called way too often
    - remove unnecessary calls to resetRtlProperties().
    - now reset of RTL properties will only be done when adding a View
    (and no more when removing it)
    
    Change-Id: I0d42128c9f7df6085fb92bb5af5c9bd4d1ba88a3

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -11854,8 +11854,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
 
         mCurrentAnimation = null;
 
-        resetRtlProperties();
-        onRtlPropertiesChanged(LAYOUT_DIRECTION_DEFAULT);
         resetAccessibilityStateChanged();
     }
 
