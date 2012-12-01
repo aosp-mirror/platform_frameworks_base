8bc230f7ccee45dbf1c765363ea9ff0ace63852a
PrimeDirective <activethrasher00@gmail.com>
2012-12-01 15:33:29 -0500

    libaudioeffect_jni GREF leak fix
    - excessive jni reference in AudioEffects
    - global references created during native setup are not deleted
    - global references are dleted in setup failure and during native releases
    
    Changeset 2:
        -fixed whitespace, and rebased on jb42
    
    Change-Id: I3ff54cc3b0c5394e978421dad3a7a257551f776e

--- media/jni/audioeffect/android_media_Visualizer.cpp
+++ media/jni/audioeffect/android_media_Visualizer.cpp
@@ -376,6 +376,8 @@ setup_failure:
     }
 
     if (lpVisualizer) {
+		env->DeleteGlobalRef(lpJniStorage->mCallbackData.visualizer_class);
+        env->DeleteGlobalRef(lpJniStorage->mCallbackData.visualizer_ref);
         delete lpVisualizer;
     }
     env->SetIntField(thiz, fields.fidNativeVisualizer, 0);
@@ -404,6 +406,9 @@ static void android_media_visualizer_native_finalize(JNIEnv *env,  jobject thiz)
     visualizerJniStorage* lpJniStorage = (visualizerJniStorage *)env->GetIntField(
         thiz, fields.fidJniData);
     if (lpJniStorage) {
+		// delete global refs created in native_setup
+        env->DeleteGlobalRef(lpJniStorage->mCallbackData.visualizer_class);
+        env->DeleteGlobalRef(lpJniStorage->mCallbackData.visualizer_ref);
         ALOGV("deleting pJniStorage: %x\n", (int)lpJniStorage);
         delete lpJniStorage;
     }
