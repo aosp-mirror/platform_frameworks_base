188315cf8b44fb59da2d37c1d54bbc70ee3acb4e
Scott Main <smain@google.com>
2013-01-03 18:02:21 -0800

    docs: fix broken links and add new sitemap text file
    Change-Id: If0f7967a65a6e3a444a565a2e8229a04a5265f56

--- docs/html/google/google_toc.cs
+++ docs/html/google/google_toc.cs
@@ -1,9 +1,15 @@
-<?cs # Table of contents for Dev Guide.
-
-       For each document available in translation, add an localized title to this TOC.
-       Do not add localized title for docs not available in translation.
-       Below are template spans for adding localized doc titles. Please ensure that
-       localized titles are added in the language order specified below.
+<?cs #########################################################
+     ########                                  ###############
+     ########            ATTENTION             ###############
+     ########                                  ###############
+     #########################################################
+     
+     IF YOU MAKE CHANGES TO THIS FILE, YOU MUST GENERATE THE
+     GMS REFERENCE DOCS, BECAUSE THEY ARE NOT INCLUDED IN THE
+     DOCS BUILD RULE.
+     
+     #########################################################
+     #########################################################
 ?>
 
 <ul id="nav">
