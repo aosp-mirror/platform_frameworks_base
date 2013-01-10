9d08143fa72ad87572a7d5c714f379262a73e1b2
Satoshi Kataoka <satok@google.com>
2013-01-09 23:44:28 -0800

    Revert "Fix a bug where disabled auxilialy IME is unexpectedly re-enabled"
    This reverts commit 32b812054cce27d1c70b53ba8ac729c7186b105e
    
    Bug: 7976890
    
    Change-Id: I75ab60734153719b199cf7281d23f5eb1ad2d1bc

--- services/java/com/android/server/InputMethodManagerService.java
+++ services/java/com/android/server/InputMethodManagerService.java
@@ -782,9 +782,6 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         if (!isSystemIme(imi)) {
             return false;
         }
-        if (imi.isAuxiliaryIme()) {
-            return false;
-        }
         if (imi.getIsDefaultResourceId() != 0) {
             try {
                 Resources res = context.createPackageContext(
@@ -806,9 +803,6 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
 
     private static boolean isSystemImeThatHasEnglishSubtype(InputMethodInfo imi) {
         if (!isSystemIme(imi)) {
-            return false;
-        }
-        if (imi.isAuxiliaryIme()) {
             return false;
         }
         return containsSubtypeOf(imi, ENGLISH_LOCALE.getLanguage());
@@ -2862,9 +2856,6 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
         List<Pair<String, ArrayList<String>>> enabledInputMethodsList = mSettings
                 .getEnabledInputMethodsAndSubtypeListLocked();
 
-        if (DEBUG) {
-            Slog.d(TAG, (enabled ? "Enable " : "Disable ") + id);
-        }
         if (enabled) {
             for (Pair<String, ArrayList<String>> pair: enabledInputMethodsList) {
                 if (pair.first.equals(id)) {
