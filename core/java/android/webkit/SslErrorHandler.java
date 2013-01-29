393fa83424822fca7a105a690d09d796f2241626
Selim Gurun <sgurun@google.com>
2013-01-28 16:30:46 -0800

    Fix public comments for SslErrorHandler.
    Fix documentation.
    
    Change-Id: I56a485d2ed3b338f9acb6fa8d7e42cba88ad6291

--- core/java/android/webkit/SslErrorHandler.java
+++ core/java/android/webkit/SslErrorHandler.java
@@ -19,9 +19,11 @@ package android.webkit;
 import android.os.Handler;
 
 /**
- * SslErrorHandler: class responsible for handling SSL errors.
- * This class is passed as a parameter to BrowserCallback.displaySslErrorDialog
- * and is meant to receive the user's response.
+ * Represents a request for handling an SSL error. Instances of this class are
+ * created by the WebView and passed to
+ * {@link WebViewClient#onReceivedSslError}. The host application must call
+ * either {@link #proceed} or {@link #cancel} to set the WebView's response
+ * to the request.
  */
 public class SslErrorHandler extends Handler {
 
