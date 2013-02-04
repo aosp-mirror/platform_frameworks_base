8b58c52bf4cc276165b1857eb4087eabde7b6477
Kenny Root <kroot@google.com>
2013-02-04 14:47:23 -0800

    AndroidKeyStore: fix tests
    Change-Id: I65fd8ba27af57ea8fd27c8e08c9c1201f32c494d

--- keystore/java/android/security/KeyStore.java
+++ keystore/java/android/security/KeyStore.java
@@ -243,7 +243,7 @@ public class KeyStore {
      */
     public long getmtime(String key) {
         try {
-            return mBinder.getmtime(key);
+            return mBinder.getmtime(key) * 1000L;
         } catch (RemoteException e) {
             Log.w(TAG, "Cannot connect to keystore", e);
             return -1L;
