5b08a2dc780a26e1557cfb4806de91f9f6bfb855
Jason Sams <jsams@google.com>
2013-02-08 11:22:17 -0800

    Add FieldPacker contructor that accepts external storage.
    Change-Id: I9c796f925802b202eae7e0fe1750a10140ef4a47

--- graphics/java/android/renderscript/FieldPacker.java
+++ graphics/java/android/renderscript/FieldPacker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 The Android Open Source Project
+ * Copyright (C) 2013 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,12 @@ public class FieldPacker {
         mPos = 0;
         mLen = len;
         mData = new byte[len];
+    }
+
+    public FieldPacker(byte[] data) {
+        mPos = 0;
+        mLen = data.length;
+        mData = data;
     }
 
     public void align(int v) {
