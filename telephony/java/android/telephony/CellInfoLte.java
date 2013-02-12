e2c508121825d37c12669c642b6c43e1bf84b566
Wink Saville <wink@google.com>
2013-02-12 10:47:32 -0800

    Turn off debug in CellInfoLte.
    Change-Id: Icf3ea5dad9eef71f644de2292db0b0d3960ced9d

--- telephony/java/android/telephony/CellInfoLte.java
+++ telephony/java/android/telephony/CellInfoLte.java
@@ -26,7 +26,7 @@ import android.telephony.Rlog;
 public final class CellInfoLte extends CellInfo implements Parcelable {
 
     private static final String LOG_TAG = "CellInfoLte";
-    private static final boolean DBG = true;
+    private static final boolean DBG = false;
 
     private CellIdentityLte mCellIdentityLte;
     private CellSignalStrengthLte mCellSignalStrengthLte;
