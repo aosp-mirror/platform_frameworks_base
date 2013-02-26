217fd2903d09d40cabcdade9f2a162dc6513f800
jungheang.lee <jungheang.lee@lge.com>
2013-02-26 16:53:22 +0900

    Fixed the problem ime invisible status despite being the icon that appears in the statusbar.
    [Reproduce the Path]
    Precondition : Set "Auto-rotate screen" option to ENABLE
    
    Calendar App lunch -> Rotate device to "Landscape" -> Touch "+" button(Add new Schedule)
    -> Check the ime icons of the Status bar
    
    Ime invisible status despite being the icon that appears in the statusbar.
    Displays an icon in the status bar when the ime was actually visible state is modified to check.
    
    Change-Id: If103ab909c5bfa6391eb51a696fb8b8f0b18808c

--- core/java/android/inputmethodservice/InputMethodService.java
+++ core/java/android/inputmethodservice/InputMethodService.java
@@ -424,7 +424,7 @@ public class InputMethodService extends AbstractInputMethodService {
                 showWindow(true);
             }
             // If user uses hard keyboard, IME button should always be shown.
-            boolean showing = onEvaluateInputViewShown();
+            boolean showing = isInputViewShown();
             mImm.setImeWindowStatus(mToken, IME_ACTIVE | (showing ? IME_VISIBLE : 0),
                     mBackDisposition);
             if (resultReceiver != null) {
