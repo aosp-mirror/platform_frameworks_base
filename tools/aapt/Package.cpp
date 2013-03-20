3c7d9cad982825a21a67e45512bfb6de68b9a089
Amit Shekhar <ashekhar@codeaurora.org>
2013-03-20 14:47:38 -0700

    aapt: Support for mxmf file playback
    mxmf files couldn't be played.
    Added .mxmf file ext to kNoCompressExt array
    
    CRs-Fixed: 465437
    Change-Id: I8eb50e09d4a3b0dcf5d60869dc2d69ca924c8dc2

--- tools/aapt/Package.cpp
+++ tools/aapt/Package.cpp
@@ -31,7 +31,7 @@ static const char* kNoCompressExt[] = {
     ".jpg", ".jpeg", ".png", ".gif",
     ".wav", ".mp2", ".mp3", ".ogg", ".aac",
     ".mpg", ".mpeg", ".mid", ".midi", ".smf", ".jet",
-    ".rtttl", ".imy", ".xmf", ".mp4", ".m4a",
+    ".rtttl", ".imy", ".xmf", ".mxmf", ".mp4", ".m4a",
     ".m4v", ".3gp", ".3gpp", ".3g2", ".3gpp2",
     ".amr", ".awb", ".wma", ".wmv"
 };
