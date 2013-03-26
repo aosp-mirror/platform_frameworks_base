edf6f4b49f6e77c349f5055372ce381b74f12efb
Chet Haase <chet@google.com>
2013-03-26 07:55:30 -0700

    Make adding views specific to a ViewGroup's overlay
    Adding views to views (possible with the new Overlay API) is weird.
    This change moves the view-management facilities of Overlay to a subclass
    that is specific to the overlay returned from ViewGroup.getOverlay().
    So now you can add drawables to all view overlays, but only add/remove
    views to/from the overlay returned from ViewGroup.getOverlay().
    
    Also, the previous approach of using an interface for Overlay was
    changed to classes for both ViewOverlay and ViewGroupOverlay.
    
    Finally, this change makes not handling touch correctly the proper,
    and documented, behavior of overlay views. There are various tricky issues
    to sort out with input in overlays (including click handling as well as focus)
    and we don't want developers starting to use overlays as some kind of general
    container hierarchy, so we're purposely constraining overlays to have visual-only
    behavior.
    
    Issue #8459085 Overlay needs to handle touch correctly
    
    Change-Id: I207b8dbf528f87c92369d270d8b0a6556826d207

--- core/java/android/view/ViewOverlay.java
+++ core/java/android/view/ViewOverlay.java
@@ -23,215 +23,286 @@ import android.graphics.drawable.Drawable;
 import java.util.ArrayList;
 
 /**
- * ViewOverlay is a container that View uses to host all objects (views and
- * drawables) that are added to its "overlay", gotten through
- * {@link View#getOverlay()}. Views and drawables are added to the overlay
- * via the add/remove methods in this class. These views and drawables are
- * drawn whenever the view itself is drawn; first the view draws its own
- * content (and children, if it is a ViewGroup), then it draws its overlay
- * (if it has one).
+ * An overlay is an extra layer that sits on top of a View (the "host view")
+ * which is drawn after all other content in that view (including children,
+ * if the view is a ViewGroup). Interaction with the overlay layer is done
+ * by adding and removing drawables.
  *
- * Besides managing and drawing the list of drawables, this class serves
- * two purposes:
- * (1) it noops layout calls because children are absolutely positioned and
- * (2) it forwards all invalidation calls to its host view. The invalidation
- * redirect is necessary because the overlay is not a child of the host view
- * and invalidation cannot therefore follow the normal path up through the
- * parent hierarchy.
+ * <p>An overlay requested from a ViewGroup is of type {@link ViewGroupOverlay},
+ * which also supports adding and removing views.</p>
  *
- * @hide
+ * @see View#getOverlay() View.getOverlay()
+ * @see ViewGroup#getOverlay() ViewGroup.getOverlay()
+ * @see ViewGroupOverlay
  */
-class ViewOverlay extends ViewGroup implements Overlay {
+public class ViewOverlay {
 
     /**
-     * The View for which this is an overlay. Invalidations of the overlay are redirected to
-     * this host view.
+     * The actual container for the drawables (and views, if it's a ViewGroupOverlay).
+     * All of the management and rendering details for the overlay are handled in
+     * OverlayViewGroup.
      */
-    View mHostView;
+    OverlayViewGroup mOverlayViewGroup;
 
-    /**
-     * The set of drawables to draw when the overlay is rendered.
-     */
-    ArrayList<Drawable> mDrawables = null;
-
-    ViewOverlay(Context context, View host) {
-        super(context);
-        mHostView = host;
-        mParent = mHostView.getParent();
+    ViewOverlay(Context context, View hostView) {
+        mOverlayViewGroup = new OverlayViewGroup(context, hostView);
     }
 
-    @Override
+    /**
+     * Used internally by View and ViewGroup to handle drawing and invalidation
+     * of the overlay
+     * @return
+     */
+    ViewGroup getOverlayView() {
+        return mOverlayViewGroup;
+    }
+
+    /**
+     * Adds a Drawable to the overlay. The bounds of the drawable should be relative to
+     * the host view. Any drawable added to the overlay should be removed when it is no longer
+     * needed or no longer visible.
+     *
+     * @param drawable The Drawable to be added to the overlay. This drawable will be
+     * drawn when the view redraws its overlay.
+     * @see #remove(Drawable)
+     */
     public void add(Drawable drawable) {
-        if (mDrawables == null) {
-            mDrawables = new ArrayList<Drawable>();
-        }
-        if (!mDrawables.contains(drawable)) {
-            // Make each drawable unique in the overlay; can't add it more than once
-            mDrawables.add(drawable);
-            invalidate(drawable.getBounds());
-            drawable.setCallback(this);
-        }
+        mOverlayViewGroup.add(drawable);
     }
 
-    @Override
+    /**
+     * Removes the specified Drawable from the overlay.
+     *
+     * @param drawable The Drawable to be removed from the overlay.
+     * @see #add(Drawable)
+     */
     public void remove(Drawable drawable) {
-        if (mDrawables != null) {
-            mDrawables.remove(drawable);
-            invalidate(drawable.getBounds());
-            drawable.setCallback(null);
-        }
+        mOverlayViewGroup.remove(drawable);
     }
 
-    @Override
-    public void invalidateDrawable(Drawable drawable) {
-        invalidate(drawable.getBounds());
-    }
-
-    @Override
-    public void add(View child) {
-        int deltaX = 0;
-        int deltaY = 0;
-        if (child.getParent() instanceof ViewGroup) {
-            ViewGroup parent = (ViewGroup) child.getParent();
-            if (parent != mHostView) {
-                // Moving to different container; figure out how to position child such that
-                // it is in the same location on the screen
-                int[] parentLocation = new int[2];
-                int[] hostViewLocation = new int[2];
-                parent.getLocationOnScreen(parentLocation);
-                mHostView.getLocationOnScreen(hostViewLocation);
-                child.offsetLeftAndRight(parentLocation[0] - hostViewLocation[0]);
-                child.offsetTopAndBottom(parentLocation[1] - hostViewLocation[1]);
-            }
-            parent.removeView(child);
-        }
-        super.addView(child);
-    }
-
-    @Override
-    public void remove(View view) {
-        super.removeView(view);
-    }
-
-    @Override
+    /**
+     * Removes all content from the overlay.
+     */
     public void clear() {
-        removeAllViews();
-        mDrawables.clear();
+        mOverlayViewGroup.clear();
     }
 
     boolean isEmpty() {
-        if (getChildCount() == 0 && (mDrawables == null || mDrawables.size() == 0)) {
-            return true;
-        }
-        return false;
+        return mOverlayViewGroup.isEmpty();
     }
 
-    @Override
-    protected void dispatchDraw(Canvas canvas) {
-        super.dispatchDraw(canvas);
-        final int numDrawables = (mDrawables == null) ? 0 : mDrawables.size();
-        for (int i = 0; i < numDrawables; ++i) {
-            mDrawables.get(i).draw(canvas);
-        }
-    }
-
-    @Override
-    protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        // Noop: children are positioned absolutely
-    }
-
-    /*
-     The following invalidation overrides exist for the purpose of redirecting invalidation to
-     the host view. The overlay is not parented to the host view (since a View cannot be a parent),
-     so the invalidation cannot proceed through the normal parent hierarchy.
-     There is a built-in assumption that the overlay exactly covers the host view, therefore
-     the invalidation rectangles received do not need to be adjusted when forwarded to
-     the host view.
+    /**
+     * OverlayViewGroup is a container that View and ViewGroup use to host
+     * drawables and views added to their overlays  ({@link ViewOverlay} and
+     * {@link ViewGroupOverlay}, respectively). Drawables are added to the overlay
+     * via the add/remove methods in ViewOverlay, Views are added/removed via
+     * ViewGroupOverlay. These drawable and view objects are
+     * drawn whenever the view itself is drawn; first the view draws its own
+     * content (and children, if it is a ViewGroup), then it draws its overlay
+     * (if it has one).
+     *
+     * <p>Besides managing and drawing the list of drawables, this class serves
+     * two purposes:
+     * (1) it noops layout calls because children are absolutely positioned and
+     * (2) it forwards all invalidation calls to its host view. The invalidation
+     * redirect is necessary because the overlay is not a child of the host view
+     * and invalidation cannot therefore follow the normal path up through the
+     * parent hierarchy.</p>
+     *
+     * @see View#getOverlay()
+     * @see ViewGroup#getOverlay()
      */
+    static class OverlayViewGroup extends ViewGroup {
 
-    @Override
-    public void invalidate(Rect dirty) {
-        super.invalidate(dirty);
-        if (mHostView != null) {
-            mHostView.invalidate(dirty);
+        /**
+         * The View for which this is an overlay. Invalidations of the overlay are redirected to
+         * this host view.
+         */
+        View mHostView;
+
+        /**
+         * The set of drawables to draw when the overlay is rendered.
+         */
+        ArrayList<Drawable> mDrawables = null;
+
+        OverlayViewGroup(Context context, View hostView) {
+            super(context);
+            mHostView = hostView;
+            mAttachInfo = mHostView.mAttachInfo;
+            mRight = hostView.getWidth();
+            mBottom = hostView.getHeight();
         }
-    }
 
-    @Override
-    public void invalidate(int l, int t, int r, int b) {
-        super.invalidate(l, t, r, b);
-        if (mHostView != null) {
-            mHostView.invalidate(l, t, r, b);
-        }
-    }
+        public void add(Drawable drawable) {
+            if (mDrawables == null) {
 
-    @Override
-    public void invalidate() {
-        super.invalidate();
-        if (mHostView != null) {
-            mHostView.invalidate();
-        }
-    }
-
-    @Override
-    void invalidate(boolean invalidateCache) {
-        super.invalidate(invalidateCache);
-        if (mHostView != null) {
-            mHostView.invalidate(invalidateCache);
-        }
-    }
-
-    @Override
-    void invalidateViewProperty(boolean invalidateParent, boolean forceRedraw) {
-        super.invalidateViewProperty(invalidateParent, forceRedraw);
-        if (mHostView != null) {
-            mHostView.invalidateViewProperty(invalidateParent, forceRedraw);
-        }
-    }
-
-    @Override
-    protected void invalidateParentCaches() {
-        super.invalidateParentCaches();
-        if (mHostView != null) {
-            mHostView.invalidateParentCaches();
-        }
-    }
-
-    @Override
-    protected void invalidateParentIfNeeded() {
-        super.invalidateParentIfNeeded();
-        if (mHostView != null) {
-            mHostView.invalidateParentIfNeeded();
-        }
-    }
-
-    public void invalidateChildFast(View child, final Rect dirty) {
-        if (mHostView != null) {
-            // Note: This is not a "fast" invalidation. Would be nice to instead invalidate using DL
-            // properties and a dirty rect instead of causing a real invalidation of the host view
-            int left = child.mLeft;
-            int top = child.mTop;
-            if (!child.getMatrix().isIdentity()) {
-                child.transformRect(dirty);
+                mDrawables = new ArrayList<Drawable>();
             }
-            dirty.offset(left, top);
-            mHostView.invalidate(dirty);
-        }
-    }
-
-    @Override
-    public ViewParent invalidateChildInParent(int[] location, Rect dirty) {
-        if (mHostView != null) {
-            dirty.offset(location[0], location[1]);
-            if (mHostView instanceof ViewGroup) {
-                location[0] = 0;
-                location[1] = 0;
-                super.invalidateChildInParent(location, dirty);
-                return ((ViewGroup) mHostView).invalidateChildInParent(location, dirty);
-            } else {
-                invalidate(dirty);
+            if (!mDrawables.contains(drawable)) {
+                // Make each drawable unique in the overlay; can't add it more than once
+                mDrawables.add(drawable);
+                invalidate(drawable.getBounds());
+                drawable.setCallback(this);
             }
         }
-        return null;
+
+        public void remove(Drawable drawable) {
+            if (mDrawables != null) {
+                mDrawables.remove(drawable);
+                invalidate(drawable.getBounds());
+                drawable.setCallback(null);
+            }
+        }
+
+        public void add(View child) {
+            if (child.getParent() instanceof ViewGroup) {
+                ViewGroup parent = (ViewGroup) child.getParent();
+                if (parent != mHostView) {
+                    // Moving to different container; figure out how to position child such that
+                    // it is in the same location on the screen
+                    int[] parentLocation = new int[2];
+                    int[] hostViewLocation = new int[2];
+                    parent.getLocationOnScreen(parentLocation);
+                    mHostView.getLocationOnScreen(hostViewLocation);
+                    child.offsetLeftAndRight(parentLocation[0] - hostViewLocation[0]);
+                    child.offsetTopAndBottom(parentLocation[1] - hostViewLocation[1]);
+                }
+                parent.removeView(child);
+            }
+            super.addView(child);
+        }
+
+        public void remove(View view) {
+            super.removeView(view);
+        }
+
+        public void clear() {
+            removeAllViews();
+            mDrawables.clear();
+        }
+
+        boolean isEmpty() {
+            if (getChildCount() == 0 &&
+                    (mDrawables == null || mDrawables.size() == 0)) {
+                return true;
+            }
+            return false;
+        }
+
+        @Override
+        public void invalidateDrawable(Drawable drawable) {
+            invalidate(drawable.getBounds());
+        }
+
+        @Override
+        protected void dispatchDraw(Canvas canvas) {
+            super.dispatchDraw(canvas);
+            final int numDrawables = (mDrawables == null) ? 0 : mDrawables.size();
+            for (int i = 0; i < numDrawables; ++i) {
+                mDrawables.get(i).draw(canvas);
+            }
+        }
+
+        @Override
+        protected void onLayout(boolean changed, int l, int t, int r, int b) {
+            // Noop: children are positioned absolutely
+        }
+
+        /*
+         The following invalidation overrides exist for the purpose of redirecting invalidation to
+         the host view. The overlay is not parented to the host view (since a View cannot be a
+         parent), so the invalidation cannot proceed through the normal parent hierarchy.
+         There is a built-in assumption that the overlay exactly covers the host view, therefore
+         the invalidation rectangles received do not need to be adjusted when forwarded to
+         the host view.
+         */
+
+        @Override
+        public void invalidate(Rect dirty) {
+            super.invalidate(dirty);
+            if (mHostView != null) {
+                mHostView.invalidate(dirty);
+            }
+        }
+
+        @Override
+        public void invalidate(int l, int t, int r, int b) {
+            super.invalidate(l, t, r, b);
+            if (mHostView != null) {
+                mHostView.invalidate(l, t, r, b);
+            }
+        }
+
+        @Override
+        public void invalidate() {
+            super.invalidate();
+            if (mHostView != null) {
+                mHostView.invalidate();
+            }
+        }
+
+        @Override
+        void invalidate(boolean invalidateCache) {
+            super.invalidate(invalidateCache);
+            if (mHostView != null) {
+                mHostView.invalidate(invalidateCache);
+            }
+        }
+
+        @Override
+        void invalidateViewProperty(boolean invalidateParent, boolean forceRedraw) {
+            super.invalidateViewProperty(invalidateParent, forceRedraw);
+            if (mHostView != null) {
+                mHostView.invalidateViewProperty(invalidateParent, forceRedraw);
+            }
+        }
+
+        @Override
+        protected void invalidateParentCaches() {
+            super.invalidateParentCaches();
+            if (mHostView != null) {
+                mHostView.invalidateParentCaches();
+            }
+        }
+
+        @Override
+        protected void invalidateParentIfNeeded() {
+            super.invalidateParentIfNeeded();
+            if (mHostView != null) {
+                mHostView.invalidateParentIfNeeded();
+            }
+        }
+
+        public void invalidateChildFast(View child, final Rect dirty) {
+            if (mHostView != null) {
+                // Note: This is not a "fast" invalidation. Would be nice to instead invalidate
+                // using DisplayList properties and a dirty rect instead of causing a real
+                // invalidation of the host view
+                int left = child.mLeft;
+                int top = child.mTop;
+                if (!child.getMatrix().isIdentity()) {
+                    child.transformRect(dirty);
+                }
+                dirty.offset(left, top);
+                mHostView.invalidate(dirty);
+            }
+        }
+
+        @Override
+        public ViewParent invalidateChildInParent(int[] location, Rect dirty) {
+            if (mHostView != null) {
+                dirty.offset(location[0], location[1]);
+                if (mHostView instanceof ViewGroup) {
+                    location[0] = 0;
+                    location[1] = 0;
+                    super.invalidateChildInParent(location, dirty);
+                    return ((ViewGroup) mHostView).invalidateChildInParent(location, dirty);
+                } else {
+                    invalidate(dirty);
+                }
+            }
+            return null;
+        }
     }
+
 }
