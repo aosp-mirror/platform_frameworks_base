cd604992aadc08b028797638e12bb6e5436826ae
Vimal Puthanveed <vimalp@codeaurora.org>
2013-04-12 14:07:50 -0700

    frameworks/base: Fix to remove exception in tonegenerator
    - Framework reboot happens, if tonegenerator fails to create
      a track in audioflinger.
    - The cause for framework reboot is, an exception thrown by
      tonegenerator if fails to init.
    - Fix is to remove the exception and delete the tonegenerator
      instance.
    CRs-fixed:466491
    
    Change-Id: Ie7b87f787d8a885cc70be9af4ba9d04ecd691cc2

--- core/jni/android_media_ToneGenerator.cpp
+++ core/jni/android_media_ToneGenerator.cpp
@@ -44,7 +44,7 @@ static jboolean android_media_ToneGenerator_startTone(JNIEnv *env, jobject thiz,
     ToneGenerator *lpToneGen = (ToneGenerator *)env->GetIntField(thiz,
             fields.context);
     if (lpToneGen == NULL) {
-        jniThrowRuntimeException(env, "Method called after release()");
+        ALOGE("lpToneGen is NULL");
         return false;
     }
 
@@ -59,7 +59,7 @@ static void android_media_ToneGenerator_stopTone(JNIEnv *env, jobject thiz) {
 
     ALOGV("ToneGenerator lpToneGen: %x", (unsigned int)lpToneGen);
     if (lpToneGen == NULL) {
-        jniThrowRuntimeException(env, "Method called after release()");
+        ALOGE("lpToneGen is NULL");
         return;
     }
     lpToneGen->stopTone();
@@ -97,8 +97,8 @@ static void android_media_ToneGenerator_native_setup(JNIEnv *env, jobject thiz,
 
     if (!lpToneGen->isInited()) {
         ALOGE("ToneGenerator init failed");
-        jniThrowRuntimeException(env, "Init failed");
-        delete lpToneGen;
+        if (lpToneGen)
+            delete lpToneGen;
         return;
     }
 
