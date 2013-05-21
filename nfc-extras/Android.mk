13e3f1ca242996a2e65cdc89ab9e3230d1bd1d2b
Ying Wang <wangying@google.com>
2013-05-21 12:38:22 -0700

    Clean up dist files.
    Change-Id: I921267de67aaab81dbde53751bbea392a862b62b

--- nfc-extras/Android.mk
+++ nfc-extras/Android.mk
@@ -9,6 +9,3 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE:= com.android.nfc_extras
 
 include $(BUILD_JAVA_LIBRARY)
-
-# put the classes.jar, with full class files instead of classes.dex inside, into the dist directory
-$(call dist-for-goals, droidcore, $(full_classes_jar):com.android.nfc_extras.jar)
