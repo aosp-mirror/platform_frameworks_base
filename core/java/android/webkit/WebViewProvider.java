db2302513b7271d0e5736d7fa1044ad09c742217
Ben Murdoch <benm@google.com>
2013-06-29 01:02:48 +0100

    Revert "Plumb View.getAccessibilityNodeProvider though WebViewProvider."
    This reverts commit 150cf1c15b23bb30be8b7267a5e8bc326d0f50cc.

--- core/java/android/webkit/WebViewProvider.java
+++ core/java/android/webkit/WebViewProvider.java
@@ -32,7 +32,6 @@ import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
-import android.view.accessibility.AccessibilityNodeProvider;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.webkit.WebView.HitTestResult;
@@ -279,8 +278,6 @@ public interface WebViewProvider {
     // the remainder on the methods below.
     interface ViewDelegate {
         public boolean shouldDelayChildPressedState();
-
-        public AccessibilityNodeProvider getAccessibilityNodeProvider();
 
         public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info);
 
