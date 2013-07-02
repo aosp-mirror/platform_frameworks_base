c16b0d0061108685e74b40d77ad801fe0304bc42
Vinit Deshapnde <vinitd@google.com>
2013-07-01 17:10:28 -0700

    Temporarily disable 'Scan Always' mode
    This change disables 'Scan Always' by ignoring the setting; and
    hard coding value to 'false'. The UI will change based on whatever
    is in settings; but underneath we won't enable scans when WiFi is
    off.
    
    Change-Id: Ibb1401207b6675e609e979ea23b63177814f762c

--- services/java/com/android/server/wifi/WifiController.java
+++ services/java/com/android/server/wifi/WifiController.java
@@ -379,6 +379,9 @@ class WifiController extends StateMachine {
 
         @Override
         public void enter() {
+
+            if (DBG) logd("Going to disabled without scan state");
+
             mWifiStateMachine.setSupplicantRunning(false);
             // Supplicant can't restart right away, so not the time we switched off
             mDisabledTimestamp = SystemClock.elapsedRealtime();
@@ -496,6 +499,9 @@ class WifiController extends StateMachine {
 
         @Override
         public void enter() {
+
+            if (DBG) logd("Enabling disabled with scan state");
+
             mWifiStateMachine.setSupplicantRunning(true);
             mWifiStateMachine.setOperationalMode(WifiStateMachine.SCAN_ONLY_WITH_WIFI_OFF_MODE);
             mWifiStateMachine.setDriverStart(true);
