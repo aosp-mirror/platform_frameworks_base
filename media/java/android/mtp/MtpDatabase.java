3ed02535fdc3c752346c40b57962905b7072e693
jangwon.lee <jangwon.lee@lge.com>
2013-07-22 19:52:48 +0900

    When deleting a db file, Context.deleteDatabase() has to be called instead of File.delete() to guarantee all db related files like journal, wal, shm, etc are deleted together.
    Signed-off-by: jangwon.lee<jangwon.lee@lge.com>
    
    Change-Id: I3c3d7dfa13ffbe709ab7d8a5c7cfe6cfa2864fea

--- media/java/android/mtp/MtpDatabase.java
+++ media/java/android/mtp/MtpDatabase.java
@@ -218,7 +218,7 @@ public class MtpDatabase {
                 if (c != null) c.close();
                 if (db != null) db.close();
             }
-            databaseFile.delete();
+            context.deleteDatabase(devicePropertiesName);
         }
     }
 
