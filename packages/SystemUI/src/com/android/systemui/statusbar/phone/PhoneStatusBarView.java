a5baf891024888eefdd1d1f6ed1e2923f0600681
John Spurlock <jspurlock@google.com>
2013-07-25 13:15:42 -0400

    Remove obsolete optimization.
    Reverts JB optimization I7ae978caa1f28932ee56f65484b18330a4406b80
    made obsolete by I2f9b0591d44599b07bd83f03c4e09e6dd98e1448 in MR1.
    
    Change-Id: I8a738624f9a902648e317f2eac25fd56c3b47a46

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -142,7 +142,7 @@ public class PhoneStatusBarView extends PanelBar {
     @Override
     public void onPanelPeeked() {
         super.onPanelPeeked();
-        mBar.makeExpandedVisible(true);
+        mBar.makeExpandedVisible();
     }
 
     @Override
