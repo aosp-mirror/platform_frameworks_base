f1da84706e2ef16a27f89f3b15cbe95f59145b1d
XpLoDWilD <xplodwild@cyanogenmod.org>
2013-07-28 15:48:15 +0200

    hwui: Don't discard framebuffer on exynos4
    The layers gets empty/not cleaned and redrawn on top of the preview,
    causing beautiful glitch on homescreen, and drawing black instead
    of transparency.
    
    Change-Id: I633b104224c73c625f1193ee3dde41823cddc5bf

--- libs/hwui/LayerRenderer.cpp
+++ libs/hwui/LayerRenderer.cpp
@@ -338,7 +338,7 @@ void LayerRenderer::destroyLayerDeferred(Layer* layer) {
 }
 
 void LayerRenderer::flushLayer(Layer* layer) {
-#ifdef GL_EXT_discard_framebuffer
+#if defined(GL_EXT_discard_framebuffer) && !defined(EXYNOS4_ENHANCEMENTS)
     GLuint fbo = layer->getFbo();
     if (layer && fbo) {
         // If possible, discard any enqueud operations on deferred
