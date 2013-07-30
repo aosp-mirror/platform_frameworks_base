35d2e33136b607b94230b9e77da29878a530decb
Selim Gurun <sgurun@google.com>
2013-07-30 16:31:12 -0700

    Update cleanspec
    Fix build break in master-chromium branch due to a forgotten cleanspec.
    
    Change-Id: If377c53bf3a82bff775f968ad8eb270e3c666837

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -165,6 +165,7 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framew
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/framework-res_intermediates)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates)
 $(call add-clean-step, rm -rf $(HOST_OUT)/obj/STATIC_LIBRARIES/libandroidfw_intermediates/import_includes)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
