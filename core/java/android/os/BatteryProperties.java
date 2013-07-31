df89ca3308544cfdd1ca12b22eac4a1d16e04410
Todd Poynor <toddpoynor@google.com>
2013-07-30 20:33:27 -0700

    batteryservice: add battery current now and charge counter data
    Available for retrieval via future APIs if desired.  Dumped by dumpsys.
    Not added to batterystats or ACTION_BATTERY_CHANGED intents at this point.
    
    Also fixes a formatting problem in the existing dumpsys output for voltage.
    
    Change-Id: I5320b19035914256fb872c13095c09c648dd522a

--- core/java/android/os/BatteryProperties.java
+++ core/java/android/os/BatteryProperties.java
@@ -30,6 +30,8 @@ public class BatteryProperties implements Parcelable {
     public boolean batteryPresent;
     public int batteryLevel;
     public int batteryVoltage;
+    public int batteryCurrentNow;
+    public int batteryChargeCounter;
     public int batteryTemperature;
     public String batteryTechnology;
 
@@ -47,6 +49,8 @@ public class BatteryProperties implements Parcelable {
         batteryPresent = p.readInt() == 1 ? true : false;
         batteryLevel = p.readInt();
         batteryVoltage = p.readInt();
+        batteryCurrentNow = p.readInt();
+        batteryChargeCounter = p.readInt();
         batteryTemperature = p.readInt();
         batteryTechnology = p.readString();
     }
@@ -60,6 +64,8 @@ public class BatteryProperties implements Parcelable {
         p.writeInt(batteryPresent ? 1 : 0);
         p.writeInt(batteryLevel);
         p.writeInt(batteryVoltage);
+        p.writeInt(batteryCurrentNow);
+        p.writeInt(batteryChargeCounter);
         p.writeInt(batteryTemperature);
         p.writeString(batteryTechnology);
     }
