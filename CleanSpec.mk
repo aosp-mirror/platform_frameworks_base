5839d17223e30335179e82d2a7c55d5dc39c0119
Ben Murdoch <benm@google.com>
2013-07-31 10:44:01 +0100

    Add missing cleanspec.
    Change-Id: Ida9f3c7335a84b126f67535ff658b056a59247a9

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -166,6 +166,7 @@ $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/framework-res_in
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates)
 $(call add-clean-step, rm -rf $(HOST_OUT)/obj/STATIC_LIBRARIES/libandroidfw_intermediates/import_includes)
 $(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework_intermediates)
+$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/framework-base_intermediates)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
