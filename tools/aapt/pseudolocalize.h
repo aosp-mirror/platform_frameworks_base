9f6a119c8aa276432ece4fe2118bd8a3c9b1067e
Mike Lockwood <lockwood@google.com>
2013-08-28 09:44:17 -0700

    Move frameworks/base/tools/ to frameworks/tools/
    Change-Id: I3ffafdab27cc4aca256c3a5806b630795b75d5c8

--- tools/aapt/pseudolocalize.h
+++
@@ -1,9 +0,0 @@
-#ifndef HOST_PSEUDOLOCALIZE_H
-#define HOST_PSEUDOLOCALIZE_H
-
-#include <string>
-
-std::string pseudolocalize_string(const std::string& source);
-
-#endif // HOST_PSEUDOLOCALIZE_H
-
--- tools/preload/loadclass/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_MODULE_TAGS := tests
-
-LOCAL_MODULE := loadclass
-
-include $(BUILD_JAVA_LIBRARY)
