f91d7f58bfdd30f50e1ada33cbdcb30dc9f2ec13
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-09-01 16:24:14 +0700

    PackageManager: allow build-time disabling of components
    Allow components to be specified as disabled at build time
    (applied on boot).
    
    This allows stock OTA components to be marked as disabled in
    CM builds.
    
    Change-Id: I6e4499cc40a779792a5ea97a10137399dad7d69f
    
    Conflicts:
    	core/res/res/values/symbols.xml

--- services/java/com/android/server/pm/PackageManagerService.java
+++ services/java/com/android/server/pm/PackageManagerService.java
@@ -1444,6 +1444,21 @@ public class PackageManagerService extends IPackageManager.Stub {
                 mSettings.readDefaultPreferredAppsLPw(this, 0);
             }
 
+            // Disable components marked for disabling at build-time
+            for (String name : mContext.getResources().getStringArray(
+                    com.android.internal.R.array.config_disabledComponents)) {
+                ComponentName cn = ComponentName.unflattenFromString(name);
+                Slog.v(TAG, "Disabling " + name);
+                String className = cn.getClassName();
+                PackageSetting pkgSetting = mSettings.mPackages.get(cn.getPackageName());
+                if (pkgSetting == null || pkgSetting.pkg == null
+                        || !pkgSetting.pkg.hasComponentClassName(className)) {
+                    Slog.w(TAG, "Unable to disable " + name);
+                    continue;
+                }
+                pkgSetting.disableComponentLPw(className, UserHandle.USER_OWNER);
+            }
+
             // can downgrade to reader
             mSettings.writeLPr();
 
