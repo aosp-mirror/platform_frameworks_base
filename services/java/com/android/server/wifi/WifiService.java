8c461c6fc6e060cb868e1492cc833c19fdeea0c2
Yuhao Zheng <yuhaozheng@google.com>
2013-09-16 12:43:00 -0700

    Fix Settings app crash while enabling tethering
    While enabling Portable Wifi hotspot in Settings, a null WifiConfiguration
    is provided. This null config is on purpose and meanful, and it should bypass
    the validation test to avoid the NullPointerException.
    
    Bug: 10780414
    
    Change-Id: Ic507ecae463946c925b3d5fb5a1d185b37db410f

--- services/java/com/android/server/wifi/WifiService.java
+++ services/java/com/android/server/wifi/WifiService.java
@@ -582,7 +582,8 @@ public final class WifiService extends IWifiManager.Stub {
      */
     public void setWifiApEnabled(WifiConfiguration wifiConfig, boolean enabled) {
         enforceChangePermission();
-        if (wifiConfig.isValid()) {
+        // null wifiConfig is a meaningful input for CMD_SET_AP
+        if (wifiConfig == null || wifiConfig.isValid()) {
             mWifiController.obtainMessage(CMD_SET_AP, enabled ? 1 : 0, 0, wifiConfig).sendToTarget();
         } else {
             Slog.e(TAG, "Invalid WifiConfiguration");
