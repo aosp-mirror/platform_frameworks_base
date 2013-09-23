bc1b0488cd74d6630fd6c0e90742bd3dcc8c4c71
Riaz Rahaman <riazr@codeaurora.org>
2013-09-23 14:01:10 +0530

    frameworks/base: media: add support for DIVX files
    Change-Id: I6e922c5d6e17ee590606268af97b7894f35d99ae
    
    Conflicts:
    	media/java/android/media/MediaFile.java

--- media/java/android/media/MediaFile.java
+++ media/java/android/media/MediaFile.java
@@ -79,8 +79,9 @@ public class MediaFile {
     
     // More video file types
     public static final int FILE_TYPE_MP2PS   = 200;
+    public static final int FILE_TYPE_DIVX    = 201;
     private static final int FIRST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
-    private static final int LAST_VIDEO_FILE_TYPE2 = FILE_TYPE_MP2PS;
+    private static final int LAST_VIDEO_FILE_TYPE2 = FILE_TYPE_DIVX;
 
     // Image file types
     public static final int FILE_TYPE_JPEG    = 31;
@@ -250,6 +251,7 @@ public class MediaFile {
         addFileType("ZIP", FILE_TYPE_ZIP, "application/zip");
         addFileType("MPG", FILE_TYPE_MP2PS, "video/mp2p");
         addFileType("MPEG", FILE_TYPE_MP2PS, "video/mp2p");
+        addFileType("DIVX", FILE_TYPE_DIVX, "video/divx");
         addFileType("QCP", FILE_TYPE_QCP, "audio/qcelp");
         addFileType("AC3", FILE_TYPE_AC3, "audio/ac3");
         addFileType("EC3", FILE_TYPE_EC3, "audio/eac3");
