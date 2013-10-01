ee3e1603fdbbe24b0cbf3cb85c456696cf3b7941
Jeff Sharkey <jsharkey@android.com>
2013-10-01 15:32:13 -0700

    Fix animations, padding in RTL mode.
    Show directory animations coming in from left-side when in RTL
    language.  Also fix NinePatchDrawable to correctly mirror its padding
    when auto-mirrored, and fix InsetDrawable to propagate the layout
    direction to the wrapped Drawable.
    
    Bug: 10987190, 11030793
    Change-Id: I1213802a07d0c4ced93438df1e6ddf5aed3df677

--- packages/DocumentsUI/src/com/android/documentsui/DirectoryView.java
+++ packages/DocumentsUI/src/com/android/documentsui/DirectoryView.java
@@ -40,7 +40,7 @@ public class DirectoryView extends FrameLayout {
     public void setBackground(Drawable background) {
         final Rect rect = new Rect();
         background.getPadding(rect);
-        final InsetDrawable inset = new InsetDrawable(background, -rect.left, 0, 0, 0);
+        final InsetDrawable inset = new InsetDrawable(background, -rect.left, 0, -rect.right, 0);
         super.setBackground(inset);
     }
 
