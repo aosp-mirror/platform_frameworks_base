4dfd8b48ffa8c23ba01a368d1943f5411e23fba0
Dirk Dougherty <ddougherty@google.com>
2013-10-11 16:09:40 -0700

    Doc change: Update Play for Edu docs for full launch.
    Change-Id: I50a1bfa6087383a5e7a10051e9a73ada96980297

--- docs/html/distribute/distribute_toc.cs
+++ docs/html/distribute/distribute_toc.cs
@@ -91,6 +91,7 @@
       <li><a href="<?cs var:toroot ?>distribute/googleplay/edu/about.html">About</a></li>
       <li><a href="<?cs var:toroot ?>distribute/googleplay/edu/start.html">Get Started</a></li>
       <li><a href="<?cs var:toroot ?>distribute/googleplay/edu/guidelines.html">Guidelines</a></li>
+      <li><a href="<?cs var:toroot ?>distribute/googleplay/edu/faq.html">FAQ</a></li>
       <li><a href="<?cs var:toroot ?>distribute/googleplay/edu/contact.html">Sign Up</a></li>
     </ul>  
   </li>
