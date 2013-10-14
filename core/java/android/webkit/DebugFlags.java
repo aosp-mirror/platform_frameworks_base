94c0057d67c2e0a4b88a4f735388639210260d0e
Torne (Richard Coles) <torne@google.com>
2013-10-14 16:28:11 +0100

    Delete the old WebView.
    Delete all the Java classes used only by the old WebView implementation,
    and also sections of common classes that were only needed for the old
    WebView.
    
    Bug: 10427705
    Change-Id: I02549a71104b35d86d99058c71f43e054730ec7d

--- core/java/android/webkit/DebugFlags.java
+++ core/java/android/webkit/DebugFlags.java
@@ -35,22 +35,4 @@ public class DebugFlags {
     public static final boolean URL_UTIL = false;
     public static final boolean WEB_SYNC_MANAGER = false;
 
-    // TODO: Delete these when WebViewClassic is moved
-    public static final boolean BROWSER_FRAME = false;
-    public static final boolean CACHE_MANAGER = false;
-    public static final boolean CALLBACK_PROXY = false;
-    public static final boolean COOKIE_MANAGER = false;
-    public static final boolean FRAME_LOADER = false;
-    public static final boolean J_WEB_CORE_JAVA_BRIDGE = false;// HIGHLY VERBOSE
-    public static final boolean LOAD_LISTENER = false;
-    public static final boolean MEASURE_PAGE_SWAP_FPS = false;
-    public static final boolean NETWORK = false;
-    public static final boolean SSL_ERROR_HANDLER = false;
-    public static final boolean STREAM_LOADER = false;
-    public static final boolean WEB_BACK_FORWARD_LIST = false;
-    public static final boolean WEB_SETTINGS = false;
-    public static final boolean WEB_VIEW = false;
-    public static final boolean WEB_VIEW_CORE = false;
-
-
 }
