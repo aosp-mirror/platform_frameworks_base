faaa3654d1fbbcc73554a53bb11f0169853a94df
Jim Miller <jaggies@google.com>
2013-10-15 13:46:51 -0700

    Fix possible crash in System UI
    Fixes bug 11215581
    
    Change-Id: Ic76ad783729316539e8e57feb098d6e823b4ecdf

--- packages/Keyguard/src/com/android/keyguard/PagedView.java
+++ packages/Keyguard/src/com/android/keyguard/PagedView.java
@@ -1553,6 +1553,9 @@ public abstract class PagedView extends ViewGroup implements ViewGroup.OnHierarc
             if (mTouchState == TOUCH_STATE_SCROLLING) {
                 final int activePointerId = mActivePointerId;
                 final int pointerIndex = ev.findPointerIndex(activePointerId);
+
+                if (pointerIndex == -1) return true;
+
                 final float x = ev.getX(pointerIndex);
                 final VelocityTracker velocityTracker = mVelocityTracker;
                 velocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
