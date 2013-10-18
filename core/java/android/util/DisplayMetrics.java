413100d247ec9674b63b2175ecbafecb90a2df14
Dianne Hackborn <hackbod@google.com>
2013-10-17 17:28:09 -0700

    Fix issue #11256132: Add density bucket for all real numbers between 0 and ∞...
    Well, how about 400.  400 is a real number.
    
    Change-Id: I29ac61b7d629d582c7b68367365a7f81fcf679a2

--- core/java/android/util/DisplayMetrics.java
+++ core/java/android/util/DisplayMetrics.java
@@ -67,6 +67,14 @@ public class DisplayMetrics {
     public static final int DENSITY_XHIGH = 320;
 
     /**
+     * Intermediate density for screens that sit somewhere between
+     * {@link #DENSITY_XHIGH} (320dpi) and {@link #DENSITY_XXHIGH} (480 dpi).
+     * This is not a density that applications should target, instead relying
+     * on the system to scale their {@link #DENSITY_XXHIGH} assets for them.
+     */
+    public static final int DENSITY_400 = 400;
+
+    /**
      * Standard quantized DPI for extra-extra-high-density screens.  Applications
      * should not generally worry about this density; relying on XHIGH graphics
      * being scaled up to it should be sufficient for almost all cases.
