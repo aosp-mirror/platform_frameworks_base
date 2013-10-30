86b3fa6090e78ff795a9ceaed531f8f549114deb
Wei Wang <weiwa@google.com>
2013-10-29 21:05:37 -0700

    BLE peripheral mode (3/4): Add peripheral mode API.
    Change-Id: Id9d2f566b6d9ed0fffe73b67efad2e3d045360b4
    
    Conflicts:
    	core/java/android/bluetooth/BluetoothAdapter.java
    	core/java/android/bluetooth/BluetoothGatt.java
    
    Conflicts:
    	core/java/android/bluetooth/BluetoothAdapter.java

--- core/java/android/bluetooth/BluetoothGattServer.java
+++ core/java/android/bluetooth/BluetoothGattServer.java
@@ -537,7 +537,7 @@ public final class BluetoothGattServer implements BluetoothProfile {
         try {
             mService.beginServiceDeclaration(mServerIf, service.getType(),
                 service.getInstanceId(), service.getHandles(),
-                new ParcelUuid(service.getUuid()));
+                new ParcelUuid(service.getUuid()), service.isAdvertisePreferred());
 
             List<BluetoothGattService> includedServices = service.getIncludedServices();
             for (BluetoothGattService includedService : includedServices) {
