8db45b844b5482cf61839404c0f6847e053de817
Svetoslav Ganov <svetoslavganov@google.com>
2013-10-30 20:52:04 -0700

    Update the PdfDocument docs with unsupported operations.
    The PDF generating canvas we use does not yet support all
    drawing operations. This change adds docs which operations
    are not yet supported.
    
    Change-Id: I8d5ee8c1e2a473ba783b5e62f23ab90a54bbf106

--- graphics/java/android/graphics/pdf/PdfDocument.java
+++ graphics/java/android/graphics/pdf/PdfDocument.java
@@ -391,6 +391,31 @@ public class PdfDocument {
         /**
          * Gets the {@link Canvas} of the page.
          *
+         * <p>
+         * <strong>Note: </strong> There are some draw operations that are
+         * not yet supported by the canvas returned by this method. More
+         * specifically:
+         * <ul>
+         * <li>{@link Canvas#clipPath(android.graphics.Path)
+         *     Canvas.clipPath(android.graphics.Path)}</li>
+         * <li>All flavors of {@link Canvas#drawText(String, float, float,
+         *     android.graphics.Paint) Canvas.drawText(String, float, float,
+         *     android.graphics.Paint)}</li>
+         * <li>All flavors of {@link Canvas#drawPosText(String, float[],
+         *     android.graphics.Paint) Canvas.drawPosText(String, float[],
+         *     android.graphics.Paint)}</li>
+         * <li>{@link Canvas#drawVertices(android.graphics.Canvas.VertexMode, int,
+         *     float[], int, float[], int, int[], int, short[], int, int,
+         *     android.graphics.Paint) Canvas.drawVertices(
+         *     android.graphics.Canvas.VertexMode, int, float[], int, float[],
+         *     int, int[], int, short[], int, int, android.graphics.Paint)}</li>
+         * <li>{@link android.graphics.PorterDuff.Mode#SRC_ATOP PorterDuff.Mode SRC},
+         *     {@link android.graphics.PorterDuff.Mode#DST_ATOP PorterDuff.DST_ATOP},
+         *     {@link android.graphics.PorterDuff.Mode#XOR PorterDuff.XOR},
+         *     {@link android.graphics.PorterDuff.Mode#ADD PorterDuff.ADD}</li>
+         * <li>Perspective transforms</li>
+         * </ul>
+         *
          * @return The canvas if the page is not finished, null otherwise.
          *
          * @see PdfDocument#finishPage(Page)
