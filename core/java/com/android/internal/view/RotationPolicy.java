f1a72d3cf1413c561b3ea26215d0f9a877646cb0
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:48:55 +0000

    Add support for HW Rotation Lock
    Forward ported from jellybean
    
    Change-Id: I75fb9fa33541eb6acedd62a67e74977b36768390
    
    Conflicts:
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/SystemServer.java
    
    Conflicts:
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml

--- core/java/com/android/internal/view/RotationPolicy.java
+++ core/java/com/android/internal/view/RotationPolicy.java
@@ -75,7 +75,9 @@ public final class RotationPolicy {
         return isRotationLockToggleSupported(context) &&
                 Settings.System.getIntForUser(context.getContentResolver(),
                         Settings.System.HIDE_ROTATION_LOCK_TOGGLE_FOR_ACCESSIBILITY, 0,
-                        UserHandle.USER_CURRENT) == 0;
+                        UserHandle.USER_CURRENT) == 0 &&
+                !context.getResources().getBoolean(com.android
+                        .internal.R.bool.config_hasRotationLockSwitch);
     }
 
     /**
--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -145,6 +145,7 @@ class ServerThread {
         WindowManagerService wm = null;
         BluetoothManagerService bluetooth = null;
         DockObserver dock = null;
+        RotationSwitchObserver rotateSwitch = null;
         UsbService usb = null;
         SerialService serial = null;
         TwilightService twilight = null;
@@ -342,6 +343,9 @@ class ServerThread {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting core service", e);
         }
+
+        boolean hasRotationLock = context.getResources().getBoolean(com.android
+                .internal.R.bool.config_hasRotationLockSwitch);
 
         DevicePolicyManagerService devicePolicy = null;
         StatusBarManagerService statusBar = null;
@@ -661,6 +665,16 @@ class ServerThread {
                 }
             }
 
+            try {
+                if (hasRotationLock) {
+                    Slog.i(TAG, "Rotation Switch Observer");
+                    // Listen for switch changes
+                    rotateSwitch = new RotationSwitchObserver(context);
+                }
+            } catch (Throwable e) {
+                reportWtf("starting RotationSwitchObserver", e);
+            }
+
             if (!disableNonCoreServices) {
                 try {
                     Slog.i(TAG, "USB Service");
@@ -898,6 +912,7 @@ class ServerThread {
         final NetworkPolicyManagerService networkPolicyF = networkPolicy;
         final ConnectivityService connectivityF = connectivity;
         final DockObserver dockF = dock;
+        final RotationSwitchObserver rotateSwitchF = rotateSwitch;
         final UsbService usbF = usb;
         final TwilightService twilightF = twilight;
         final UiModeManagerService uiModeF = uiMode;
@@ -968,6 +983,11 @@ class ServerThread {
                     if (dockF != null) dockF.systemReady();
                 } catch (Throwable e) {
                     reportWtf("making Dock Service ready", e);
+                }
+                try {
+                    if (rotateSwitchF != null) rotateSwitchF.systemReady();
+                } catch (Throwable e) {
+                    reportWtf("making Rotation Switch Service ready", e);
                 }
                 try {
                     if (usbF != null) usbF.systemReady();
