89cf17cc963e4f33967a6da05a241345f1f6aa12
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:00:31 +0000

    Make "SD Card removed" notification dismissible if the removed SD Card is not the primary storage
    The Galaxy S uses the "external SD Card" as the extended storage
    and it the notification should be dismissible as it's okay not
    to have it.
    
    Additionally, this patch also not hide the USB storage mount dialog
    if the removed storage is not the primary storage
    
    Adapted for JB 4.2
    
    Change-Id: Ic752a3da917e0d7736a619fb68f07905106987fb
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java

--- packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
+++ packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java
@@ -121,6 +121,7 @@ public class StorageNotification extends SystemUI {
     }
 
     private void onStorageStateChangedAsync(String path, String oldState, String newState) {
+        boolean isPrimary = mStorageManager.getPrimaryVolume().getPath().equals(path);
         if (DEBUG) Log.i(TAG, String.format(
                 "Media {%s} state changed from {%s} -> {%s}", path, oldState, newState));
         if (newState.equals(Environment.MEDIA_SHARED)) {
@@ -222,25 +223,25 @@ public class StorageNotification extends SystemUI {
         } else if (newState.equals(Environment.MEDIA_REMOVED)) {
             /*
              * Storage has been removed. Show nomedia media notification,
-             * and disable UMS notification regardless of connection state.
+             * and disable UMS notification if the removed storage is the primary storage.
              */
             setMediaStorageNotification(
                     com.android.internal.R.string.ext_media_nomedia_notification_title,
                     com.android.internal.R.string.ext_media_nomedia_notification_message,
                     com.android.internal.R.drawable.stat_notify_sdcard_usb,
-                    true, false, null);
-            updateUsbMassStorageNotification(false);
+                    true, !isPrimary, null);
+            updateUsbMassStorageNotification(isPrimary ? false : mUmsAvailable);
         } else if (newState.equals(Environment.MEDIA_BAD_REMOVAL)) {
             /*
              * Storage has been removed unsafely. Show bad removal media notification,
-             * and disable UMS notification regardless of connection state.
+             * and disable UMS notification if the removed storage is the primary storage.
              */
             setMediaStorageNotification(
                     com.android.internal.R.string.ext_media_badremoval_notification_title,
                     com.android.internal.R.string.ext_media_badremoval_notification_message,
                     com.android.internal.R.drawable.stat_sys_warning,
                     true, true, null);
-            updateUsbMassStorageNotification(false);
+            updateUsbMassStorageNotification(isPrimary ? false : mUmsAvailable);
         } else {
             Log.w(TAG, String.format("Ignoring unknown state {%s}", newState));
         }
