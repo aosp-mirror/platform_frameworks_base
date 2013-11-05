618b071b220b5b0c25a046d370919b448f630614
Yamit Mehta <ymehta@codeaurora.org>
2013-11-05 15:36:35 +0530

    audio: Fix for volume level mismatch for AVRCP headset
    -The volume level sent to AVRCP module from AudioService
     is incorrect.
    -The volume level need to scale down for AVRCP module.
    -Divide volume level by 10 to match AVRCP module volume
     levels.
    
    Change-Id: Ieb8146797d0b552311b1a4782c45aea1be73e492
    CRs-Fixed: 561649

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -1030,7 +1030,7 @@ public class AudioService extends IAudioService.Stub {
                 (flags & AudioManager.FLAG_BLUETOOTH_ABS_VOLUME) == 0) {
                 synchronized (mA2dpAvrcpLock) {
                     if (mA2dp != null && mAvrcpAbsVolSupported) {
-                        mA2dp.setAvrcpAbsoluteVolume(index);
+                        mA2dp.setAvrcpAbsoluteVolume(index / 10);
                     }
                 }
             }
