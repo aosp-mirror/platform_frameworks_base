697d9f2f30dfaf87fbebdd182e47fc552d345ce5
Sailesh Nepal <sail@google.com>
2013-11-12 13:26:27 -0800

    Add ThirdPartyCall APIs to master
    These are APIs for the new ThirdPartyPhone feature.
    
    Note, these APIs can't actually be used yet on master for two reasons:
      - initiating a call from a 3rd party app isn't possible yet because
        the TelephonyManager APIs aren't being added to master.
      - the TeleService implementation of these APIs aren't being added to
        master.
    
    Also, these APIs will be removed and the final ones will be added
    once they're ready to be merged into master.
    
    Change-Id: Ie783290451da448a011f813983e55b12047b5d99

--- Android.mk
+++ Android.mk
@@ -268,6 +268,9 @@ LOCAL_SRC_FILES += \
 	telephony/java/com/android/internal/telephony/IPhoneStateListener.aidl \
 	telephony/java/com/android/internal/telephony/IPhoneSubInfo.aidl \
 	telephony/java/com/android/internal/telephony/ITelephony.aidl \
+	telephony/java/com/android/internal/telephony/IThirdPartyCallListener.aidl \
+	telephony/java/com/android/internal/telephony/IThirdPartyCallProvider.aidl \
+	telephony/java/com/android/internal/telephony/IThirdPartyCallService.aidl \
 	telephony/java/com/android/internal/telephony/ISms.aidl \
 	telephony/java/com/android/internal/telephony/ITelephonyRegistry.aidl \
 	telephony/java/com/android/internal/telephony/IWapPushManager.aidl \
