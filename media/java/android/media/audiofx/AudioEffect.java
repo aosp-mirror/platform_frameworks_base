f5065505ee8a29824dee0b89e3be5538650364fa
xplodwild <me@xplod.fr>
2013-11-15 17:45:40 +0100

    Add a stereo widener effect [3/3]
    Change-Id: Iab2035c5601394b060e7e77e85ad85aca6e4c89f

--- media/java/android/media/audiofx/AudioEffect.java
+++ media/java/android/media/audiofx/AudioEffect.java
@@ -96,6 +96,11 @@ public class AudioEffect {
      */
     public static final UUID EFFECT_TYPE_VIRTUALIZER = UUID
             .fromString("37cc2c00-dddd-11db-8577-0002a5d5c51b");
+    /**
+     * UUID for stereowide effect
+     */
+    public static final UUID EFFECT_TYPE_STEREOWIDE = UUID
+            .fromString("37cc2c00-dddd-11db-8577-0002a5d5c51c");
 
     /**
      * UUIDs for effect types not covered by OpenSL ES.
