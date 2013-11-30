e006ad16d581f7b327be479af6835714c2d1d580
Rashmi Ramanna <rashmi@codeaurora.org>
2013-11-30 16:44:53 +0530

    Close sockets on wpa_supplicant termination.
    Framework does not close the sockets on supplicant
    termination, leading to leakage of file descriptors.
    
    Thus close the socket on supplicant termination.
    
    Change-Id: I50aefc6d60b3794288564f403a42258442e224df
    CRs-Fixed: 576276

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -2329,6 +2329,7 @@ public class WifiStateMachine extends StateMachine {
         * or when the driver is hung. Ensure supplicant is stopped here.
         */
         mWifiMonitor.killSupplicant(mP2pSupported);
+        mWifiNative.closeSupplicantConnection();
         sendSupplicantConnectionChangedBroadcast(false);
         setWifiState(WIFI_STATE_DISABLED);
     }
