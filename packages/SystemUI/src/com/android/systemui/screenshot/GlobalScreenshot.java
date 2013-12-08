cedee5479d9e2678fa22c3a2722b97c09f6f5e76
PlayfulGod <playfulgod@gmail.com>
2013-12-08 14:17:10 -0500

    fix for screenshot animation glitch in portrait
    edits by Stefano Semeraro aka Suxsem.
    
    Change-Id: I7a03a24c5b1bfdfe616f940b2bd7be42091f45a6

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -464,20 +464,23 @@ class GlobalScreenshot {
             return;
         }
 
+        Bitmap ss = Bitmap.createBitmap(mDisplayMetrics.widthPixels,
+                mDisplayMetrics.heightPixels, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(ss);
+
         if (requiresRotation) {
             // Rotate the screenshot to the current orientation
-            Bitmap ss = Bitmap.createBitmap(mDisplayMetrics.widthPixels,
-                    mDisplayMetrics.heightPixels, Bitmap.Config.ARGB_8888);
-            Canvas c = new Canvas(ss);
             c.translate(ss.getWidth() / 2, ss.getHeight() / 2);
             c.rotate(degrees);
             c.translate(-dims[0] / 2, -dims[1] / 2);
-            c.drawBitmap(mScreenBitmap, 0, 0, null);
-            c.setBitmap(null);
-            // Recycle the previous bitmap
-            mScreenBitmap.recycle();
-            mScreenBitmap = ss;
         }
+
+        c.drawBitmap(mScreenBitmap, 0, 0, null);
+        c.setBitmap(null);
+
+        // Recycle the previous bitmap
+        mScreenBitmap.recycle();
+        mScreenBitmap = ss;
 
         // Optimizations
         mScreenBitmap.setHasAlpha(false);
