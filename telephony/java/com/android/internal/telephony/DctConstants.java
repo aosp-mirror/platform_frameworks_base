a4400472d99625ecf2affaab5d9e2a02cb33cd6f
Chris Holmes <chris@chris123nt.com>
2013-12-14 19:15:28 +0000

    Revert "Telephony: Add data rat constant"
    See http://review.cyanogenmod.org/#/c/55832/ for reasons.
    
    This reverts commit 60f79613897f74cacceafad7bcd7260fe25bb1e5.
    
    Change-Id: I7eea322dec1cbb3df33e4ee10d4e73fde0c6e934

--- telephony/java/com/android/internal/telephony/DctConstants.java
+++ telephony/java/com/android/internal/telephony/DctConstants.java
@@ -97,7 +97,6 @@ public class DctConstants {
     public static final int CMD_ENABLE_MOBILE_PROVISIONING = BASE + 37;
     public static final int CMD_IS_PROVISIONING_APN = BASE + 38;
     public static final int EVENT_PROVISIONING_APN_ALARM = BASE + 39;
-    public static final int EVENT_DATA_RAT_CHANGED = BASE + 40;
 
     /***** Constants *****/
 
