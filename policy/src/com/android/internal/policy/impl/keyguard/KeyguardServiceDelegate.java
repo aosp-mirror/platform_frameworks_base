291d6c991e34cd934ede428b8e87d8c988a73029
Chirayu Desai <cdesai@cyanogenmod.org>
2013-12-15 13:48:02 +0530

    KeyguardServiceDelegate: disable DEBUG
    Change-Id: Ia5476563f6ed7f9e3175233994676c038b3a8f67

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceDelegate.java
@@ -34,7 +34,7 @@ public class KeyguardServiceDelegate {
     public static final String KEYGUARD_CLASS = "com.android.keyguard.KeyguardService";
 
     private static final String TAG = "KeyguardServiceDelegate";
-    private static final boolean DEBUG = true;
+    private static final boolean DEBUG = false;
     protected KeyguardServiceWrapper mKeyguardService;
     private View mScrim; // shown if keyguard crashes
     private KeyguardState mKeyguardState = new KeyguardState();
