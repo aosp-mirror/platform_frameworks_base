57cf560719e5cbe4ed52e54bb0556e6f79bbe061
Stephen Hines <srhines@google.com>
2013-12-18 16:21:30 -0800

    DO NOT MERGE - Fix improper declaration/use of pointer.
    https://code.google.com/p/android/issues/detail?id=61547
    
    This variable is shadowing the outer "ptr" declaration, resulting in a
    potential NULL pointer being passed to the message API.
    
    Change-Id: If96bfae8d5e874e12597182678a180ba137b78da

--- graphics/jni/android_renderscript_RenderScript.cpp
+++ graphics/jni/android_renderscript_RenderScript.cpp
@@ -335,7 +335,7 @@ nContextSendMessage(JNIEnv *_env, jobject _this, RsContext con, jint id, jintArr
     jint len = 0;
     if (data) {
         len = _env->GetArrayLength(data);
-        jint *ptr = _env->GetIntArrayElements(data, NULL);
+        ptr = _env->GetIntArrayElements(data, NULL);
     }
     LOG_API("nContextSendMessage, con(%p), id(%i), len(%i)", con, id, len);
     rsContextSendMessage(con, id, (const uint8_t *)ptr, len * sizeof(int));
