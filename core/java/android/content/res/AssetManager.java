a0545dd54fdab5b284b509b7121515f15f4d728a
Ashok Bhat <ashok.bhat@arm.com>
2014-01-14 10:52:35 +0000

    AArch64: Add AssetInputStream.getNativeAsset
    This patch adds AssetInputStream.getNativeAsset. This function
    returns asset pointer, which can then be passed to JNI functions.
    Note that this function is hidden and not part of Android API.
    
    This function will eventually replace getAssetInt,
    once all the classes that use getAssetInt are changed
    to use getNativeAsset instead.
    
    Change-Id: I2a2b2e6864f1f57ae2b3a10c45559b3635266ed5
    Signed-off-by: Ashok Bhat <ashok.bhat@arm.com>

--- core/java/android/content/res/AssetManager.java
+++ core/java/android/content/res/AssetManager.java
@@ -540,6 +540,12 @@ public final class AssetManager {
         public final int getAssetInt() {
             return mAsset;
         }
+        /**
+         * @hide
+         */
+        public final long getNativeAsset() {
+            return mAsset;
+        }
         private AssetInputStream(int asset)
         {
             mAsset = asset;
