ad6ed950dbfa152c193dd7e49c369d9e831f1591
Mårten Kongstad <marten.kongstad@sonymobile.com>
2014-01-31 14:47:44 +0100

    Runtime resource overlay, iteration 2, test cases
    Add automated test cases for runtime resource overlay, iteration 2.
    
    The test cases are a mixture of 'adb shell' commands and regular
    instrumentation tests. The device is rebooted between tests to setup
    different overlay scenarios for framework-res.apk.
    
    To verify Runtime resoure overlay, iteration 2, run
        $ frameworks/base/core/tests/overlaytests/testrunner.py
    
    For a list of supported options, run
        $ frameworks/base/core/tests/overlaytests/testrunner.py --help
    
    Change-Id: I692aa1a7ad073efd116b24f9ec7f197dfd65dfef

--- core/tests/overlaytests/OverlayTest/Android.mk
+++ core/tests/overlaytests/OverlayTest/Android.mk
@@ -5,6 +5,10 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_PACKAGE_NAME := OverlayTest
 
+LOCAL_DEX_PREOPT := false
+
+LOCAL_MODULE_PATH := $(TARGET_OUT)/app
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 include $(BUILD_PACKAGE)
