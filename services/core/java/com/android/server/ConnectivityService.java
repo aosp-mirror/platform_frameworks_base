7468bb7156c72b5175604f2d5de5d3b2afa2f97b
Jake Hamby <jhamby@google.com>
2014-02-06 14:43:50 -0800

    Remove unneeded new RIL command.
    Remove the recently added RIL_REQUEST_SET_RADIO_MODE command and update
    the definition of the RIL_REQUEST_NV_RESET_CONFIG parameter.
    Also remove some accidentally added debug log lines.
    
    Bug: 12864208
    Change-Id: I6f035d6900c9fcb1427bad62057d7b4a1d3cd99c

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -169,9 +169,9 @@ public class ConnectivityService extends IConnectivityManager.Stub {
     private static final String TAG = "ConnectivityService";
 
     private static final boolean DBG = true;
-    private static final boolean VDBG = true;
+    private static final boolean VDBG = false;
 
-    private static final boolean LOGD_RULES = true;
+    private static final boolean LOGD_RULES = false;
 
     // TODO: create better separation between radio types and network types
 
@@ -4504,7 +4504,6 @@ public class ConnectivityService extends IConnectivityManager.Stub {
          * @param seconds
          */
         private static void sleep(int seconds) {
-            log("XXXXX sleeping for " + seconds + " sec");
             long stopTime = System.nanoTime() + (seconds * 1000000000);
             long sleepTime;
             while ((sleepTime = stopTime - System.nanoTime()) > 0) {
@@ -4513,7 +4512,6 @@ public class ConnectivityService extends IConnectivityManager.Stub {
                 } catch (InterruptedException ignored) {
                 }
             }
-            log("XXXXX returning from sleep");
         }
 
         private static void log(String s) {
