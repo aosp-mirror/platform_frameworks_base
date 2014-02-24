26459b21bbd2889e716e8c7b676a7de67ad40f45
Danny Baumann <dannybaumann@web.de>
2014-02-24 23:36:48 +0700

    base: Factor out preference-in-storage check to overridable method.
    Change-Id: I64966e15e0e92a2058809ead2c694a9badae881b

--- core/java/android/preference/Preference.java
+++ core/java/android/preference/Preference.java
@@ -1575,6 +1575,14 @@ public class Preference implements Comparable<Preference> {
         mDefaultValue = defaultValue;
     }
 
+    /**
+     * Returns whether the preference can be found in persistent storage
+     * @hide
+     */
+    protected boolean isPersisted() {
+        return getSharedPreferences().contains(mKey);
+    }
+
     private void dispatchSetInitialValue() {
         if (getPreferenceDataStore() != null) {
             onSetInitialValue(true, mDefaultValue);
@@ -1583,7 +1591,7 @@ public class Preference implements Comparable<Preference> {
 
         // By now, we know if we are persistent.
         final boolean shouldPersist = shouldPersist();
-        if (!shouldPersist || !getSharedPreferences().contains(mKey)) {
+        if (!shouldPersist || !isPersisted()) {
             if (mDefaultValue != null) {
                 onSetInitialValue(false, mDefaultValue);
             }
