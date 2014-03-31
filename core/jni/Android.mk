973b4663b0b5ee62006522bf4742af076096e548
Narayan Kamath <narayan@google.com>
2014-03-31 13:41:26 +0100

    Move zygote startup logic to the frameworks.
    The Zygote class is now in com.android.internal.os. It is
    responsible for the vast majority of work before and after
    the call to fork(). It calls back into the Runtime via
    the new dalvik.system.ZygoteHooks class to allow the Runtime
    to perform pre fork cleanup and post fork initialization.
    
    The native code in Zygote.cpp is a direct and straightforward
    port of the existing code in art. Most differences are
    superficial, for example :
    - We use C style logging (ALOGE) instead of stream based
      logging.
    - We call env->FatalError() instead of using LOG(FATAL)
    
    Change-Id: Ia101fb2af12d23894fe57e4134d2bc6d142e5059

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -149,7 +149,8 @@ LOCAL_SRC_FILES:= \
 	android_content_res_ObbScanner.cpp \
 	android_content_res_Configuration.cpp \
 	android_animation_PropertyValuesHolder.cpp \
-	com_android_internal_net_NetworkStatsFactory.cpp
+	com_android_internal_net_NetworkStatsFactory.cpp \
+	com_android_internal_os_Zygote.cpp
 
 LOCAL_C_INCLUDES += \
 	$(JNI_H_INCLUDE) \
