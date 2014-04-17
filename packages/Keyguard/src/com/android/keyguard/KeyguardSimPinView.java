3f4ed0f5c3bfd1afdab70030b96d5786753dd077
Jorim Jaggi <jjaggi@google.com>
2014-04-17 20:17:30 +0200

    Fix not being able to unlock SIM PIN.
    Bug: 14120902
    Change-Id: I812d9679000242eabea617ea75cf8355d16926b2

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
@@ -87,7 +87,7 @@ public class KeyguardSimPinView extends KeyguardAbsKeyInputView
 
     @Override
     protected int getPasswordTextViewId() {
-        return R.id.pinEntry;
+        return R.id.simPinEntry;
     }
 
     @Override
