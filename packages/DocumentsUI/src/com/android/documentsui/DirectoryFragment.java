9bc30e4d078a1e8d25b3e22fd1efb9440eef1191
Felix Ableitner <me@nutomic.com>
2014-04-21 22:18:34 +0200

    Replaced custom strings with Android defaults
    PS2: also replaced menu_cut/copy/paste with strings from android.R.string
    
    Change-Id: I7695b283faa66c6997fad372b31de2c678d222d0
    Signed-off-by: iceandfire <arhamjamal@gmail.com>
    
    Conflicts:
    	packages/DocumentsUI/res/values-es/custom_strings.xml
    	packages/DocumentsUI/res/values-fr/custom_strings.xml
    	packages/DocumentsUI/res/values-it/custom_strings.xml

--- packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
+++ packages/DocumentsUI/src/com/android/documentsui/DirectoryFragment.java
@@ -642,12 +642,12 @@ public class DirectoryFragment extends Fragment {
         // Open a confirmation dialog
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 new DeleteFilesTask(docs.toArray(new DocumentInfo[0])).executeOnExecutor(getCurrentExecutor());
             }
         });
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+        builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 // User cancelled the dialog, ignore actions
             }
