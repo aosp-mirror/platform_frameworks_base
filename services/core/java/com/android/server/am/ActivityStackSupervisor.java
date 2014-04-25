2e46e65e9dafe1f6a911efa213fcb0d105394b09
Craig Mautner <cmautner@google.com>
2014-04-25 03:01:40 +0000

    Too soon.
    Change-Id: I1abb1d4841e4e6342c3b861d33ae22e22ed0a161

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -705,10 +705,6 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 profileFile, profileFd, userId);
 
         ActivityContainer container = (ActivityContainer)iContainer;
-        if (container != null && (aInfo.flags & ActivityInfo.FLAG_ALLOW_EMBEDDED) == 0) {
-            throw new SecurityException(
-                    "Activities launched in ActivityContainers must set allowEmbedded=\"true\"");
-        }
         synchronized (mService) {
             int callingPid;
             if (callingUid >= 0) {
