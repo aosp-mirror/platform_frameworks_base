9054d94cbac888c3a52427f54420cde2460c0c3b
Jorim Jaggi <jjaggi@google.com>
2014-05-09 22:47:28 +0200

    Move Carrier label into universal status bar header.
    Also introduces a different height for the header area on keyguard,
    as it looks better with less padding when no background is here (and
    is closer to the mocks).
    
    Change-Id: Ie9b94dcf823c3e4af83a22f5fae15d9033e146fd

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -34,9 +34,13 @@ public class StatusBarHeaderView extends RelativeLayout {
     private View mBackground;
     private ViewGroup mSystemIconsContainer;
     private View mDateTime;
+    private View mKeyguardCarrierText;
 
     private int mCollapsedHeight;
     private int mExpandedHeight;
+    private int mKeyguardHeight;
+
+    private boolean mKeyguardShowing;
 
     public StatusBarHeaderView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -48,6 +52,7 @@ public class StatusBarHeaderView extends RelativeLayout {
         mBackground = findViewById(R.id.background);
         mSystemIconsContainer = (ViewGroup) findViewById(R.id.system_icons_container);
         mDateTime = findViewById(R.id.datetime);
+        mKeyguardCarrierText = findViewById(R.id.keyguard_carrier_text);
         loadDimens();
     }
 
@@ -55,10 +60,12 @@ public class StatusBarHeaderView extends RelativeLayout {
         mCollapsedHeight = getResources().getDimensionPixelSize(R.dimen.status_bar_header_height);
         mExpandedHeight = getResources().getDimensionPixelSize(
                 R.dimen.status_bar_header_height_expanded);
+        mKeyguardHeight = getResources().getDimensionPixelSize(
+                R.dimen.status_bar_header_height_keyguard);
     }
 
     public int getCollapsedHeight() {
-        return mCollapsedHeight;
+        return mKeyguardShowing ? mKeyguardHeight : mCollapsedHeight;
     }
 
     public int getExpandedHeight() {
@@ -66,11 +73,29 @@ public class StatusBarHeaderView extends RelativeLayout {
     }
 
     public void setExpanded(boolean expanded) {
-        if (expanded != mExpanded) {
-            ViewGroup.LayoutParams lp = getLayoutParams();
-            lp.height = expanded ? mExpandedHeight : mCollapsedHeight;
+        mExpanded = expanded;
+        updateHeights();
+    }
+
+    private void updateHeights() {
+        int height;
+        if (mExpanded) {
+            height = mExpandedHeight;
+        } else if (mKeyguardShowing) {
+            height = mKeyguardHeight;
+        } else {
+            height = mCollapsedHeight;
+        }
+        ViewGroup.LayoutParams lp = getLayoutParams();
+        if (lp.height != height) {
+            lp.height = height;
             setLayoutParams(lp);
-            mExpanded = expanded;
+        }
+        int systemIconsContainerHeight = mKeyguardShowing ? mKeyguardHeight : mCollapsedHeight;
+        lp = mSystemIconsContainer.getLayoutParams();
+        if (lp.height != systemIconsContainerHeight) {
+            lp.height = systemIconsContainerHeight;
+            mSystemIconsContainer.setLayoutParams(lp);
         }
     }
 
@@ -97,12 +122,15 @@ public class StatusBarHeaderView extends RelativeLayout {
     }
 
     public void setKeyguardShowing(boolean keyguardShowing) {
+        mKeyguardShowing = keyguardShowing;
         mBackground.setVisibility(keyguardShowing ? View.INVISIBLE : View.VISIBLE);
         mDateTime.setVisibility(keyguardShowing ? View.INVISIBLE : View.VISIBLE);
+        mKeyguardCarrierText.setVisibility(keyguardShowing ? View.VISIBLE : View.GONE);
         if (keyguardShowing) {
             setZ(0);
         } else {
             setTranslationZ(0);
         }
+        updateHeights();
     }
 }
