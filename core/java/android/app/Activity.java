4d1a434cbd1dbf03f568ecfc10b2f89affc00192
Winson Chung <winsonc@google.com>
2014-05-09 16:42:00 -0700

    Fixing crash in Activity.onApplyTheme().

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -3499,9 +3499,8 @@ public class Activity extends ContextThemeWrapper
         }
 
         // Get the primary color and update the RecentsActivityValues for this activity
-        Resources.Theme tmpTheme = getTheme();
-        if (tmpTheme != null) {
-            TypedArray a = tmpTheme.obtainStyledAttributes(com.android.internal.R.styleable.Theme);
+        if (theme != null) {
+            TypedArray a = theme.obtainStyledAttributes(com.android.internal.R.styleable.Theme);
             int colorPrimary = a.getColor(com.android.internal.R.styleable.Theme_colorPrimary, 0);
             a.recycle();
             if (colorPrimary != 0) {
