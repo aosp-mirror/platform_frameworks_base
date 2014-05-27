2c2a3017624678ee65d92feb51340f542a1d244c
Jinsuk Kim <jinsukkim@google.com>
2014-05-27 17:23:27 +0900

    DO NOT MERGE: Fix a bug using wrong addresses for CEC logical address allocation
    Change-Id: I41da586c16dca4f325d147bb3abe5d4e09f7b91c

--- services/core/java/com/android/server/hdmi/HdmiCecController.java
+++ services/core/java/com/android/server/hdmi/HdmiCecController.java
@@ -196,7 +196,7 @@ final class HdmiCecController {
         for (int i = 0; i < NUM_LOGICAL_ADDRESS; ++i) {
             int curAddress = (startAddress + i) % NUM_LOGICAL_ADDRESS;
             if (curAddress != HdmiCec.ADDR_UNREGISTERED
-                    && deviceType == HdmiCec.getTypeFromAddress(i)) {
+                    && deviceType == HdmiCec.getTypeFromAddress(curAddress)) {
                 if (!sendPollMessage(curAddress, RETRY_COUNT_FOR_LOGICAL_ADDRESS_ALLOCATION)) {
                     logicalAddress = curAddress;
                     break;
