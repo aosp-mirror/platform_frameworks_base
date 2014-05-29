609cb91c9cbb94c792e1fe50f6d762f498d6bcc8
Kazuki Nakayama <kazuki.x.nakayama@sonymobile.com>
2014-05-29 16:05:34 +0900

    Reset killedByAm flag at attachApplicationLocked
    ActivityManager reuses a process record object that killed
    by him under some situation. That reused process record inherits
    a killedByAm flag unexpectedly.
    The killedByAm flag must be reset otherwise ActivityManager can't
    judge the process can be killed or not.
    
    Change-Id: If95137d91939cc44882ad2813131bcde0edd0c1b

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -6098,6 +6098,7 @@ public final class ActivityManagerService extends ActivityManagerNative
         app.hasShownUi = false;
         app.debugging = false;
         app.cached = false;
+        app.killedByAm = false;
 
         mHandler.removeMessages(PROC_START_TIMEOUT_MSG, app);
 
