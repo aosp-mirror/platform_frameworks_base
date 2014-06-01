377801463ef75d7c14f3ef6a346d8ddccb2a4045
Adam Powell <adamp@google.com>
2014-06-01 13:31:00 -0700

    Change parameter name for Activity#setActionBar
    Feedback from API review
    
    Bug 14998253
    
    Change-Id: Id3b87a8071d43de49847e93fd6229d48364bf55e

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -2075,15 +2075,15 @@ public class Activity extends ContextThemeWrapper
      * <p>In order to use a Toolbar within the Activity's window content the application
      * must not request the window feature {@link Window#FEATURE_ACTION_BAR FEATURE_ACTION_BAR}.</p>
      *
-     * @param actionBar Toolbar to set as the Activity's action bar
+     * @param toolbar Toolbar to set as the Activity's action bar
      */
-    public void setActionBar(@Nullable Toolbar actionBar) {
+    public void setActionBar(@Nullable Toolbar toolbar) {
         if (getActionBar() instanceof WindowDecorActionBar) {
             throw new IllegalStateException("This Activity already has an action bar supplied " +
                     "by the window decor. Do not request Window.FEATURE_ACTION_BAR and set " +
                     "android:windowActionBar to false in your theme to use a Toolbar instead.");
         }
-        mActionBar = new ToolbarActionBar(actionBar);
+        mActionBar = new ToolbarActionBar(toolbar);
     }
     
     /**
