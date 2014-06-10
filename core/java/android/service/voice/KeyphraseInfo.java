22968950b814e66a6aa119ea92ae648884cbe0d9
Sandeep Siddhartha <sansid@google.com>
2014-06-10 12:32:53 -0700

    Activity for enrollment
    - Make the enrollment attrs public
    - Add a dummy activity for enrollment
    
    - Manually tested that the meta-data is correctly read by the
      VoiceInteractionService when the enrollment application is under
      /system/priv-app
    
    Change-Id: I36676ed8ffc919109031c26bac047d0c51a77e13

--- core/java/android/service/voice/KeyphraseInfo.java
+++ core/java/android/service/voice/KeyphraseInfo.java
@@ -19,4 +19,9 @@ public class KeyphraseInfo {
             this.supportedLocales.add(locale);
         }
     }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", keyphrase=" + keyphrase + ", supported-locales=" + supportedLocales;
+    }
 }
---
+++ tests/VoiceEnrollment/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_PACKAGE_NAME := VoiceEnrollment
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRIVILEGED_MODULE := true
+
+include $(BUILD_PACKAGE)
