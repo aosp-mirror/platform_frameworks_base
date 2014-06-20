42c9f5e5f73a4c65270f334d1bf77101211da574
John Reck <jreck@google.com>
2014-06-20 09:59:56 -0700

    Add negative guard
     Bug: 15631600
    
    Change-Id: Idd7740f58876e73694fafb8ef55ebaff511f6dca

--- libs/hwui/Interpolator.cpp
+++ libs/hwui/Interpolator.cpp
@@ -112,6 +112,10 @@ float LUTInterpolator::interpolate(float input) {
     int i1 = (int) ipart;
     int i2 = MathUtils::min(i1 + 1, mSize - 1);
 
+    LOG_ALWAYS_FATAL_IF(i1 < 0 || i2 < 0, "negatives in interpolation!"
+            " i1=%d, i2=%d, input=%f, lutpos=%f, size=%zu, values=%p, ipart=%f, weight=%f",
+            i1, i2, input, lutpos, mSize, mValues, ipart, weight);
+
     float v1 = mValues[i1];
     float v2 = mValues[i2];
 
