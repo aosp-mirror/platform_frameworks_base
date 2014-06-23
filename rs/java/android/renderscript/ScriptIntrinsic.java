1c8da1cccedc7194cc02edbdf26249bc3550195c
Jason Sams <jsams@google.com>
2014-06-23 15:15:08 -0700

    Add check and throw for failed Intrinsic init.
    Change-Id: Icd53b3edd8e887d3692e5815dc0c38cd68b7aeb1

--- rs/java/android/renderscript/ScriptIntrinsic.java
+++ rs/java/android/renderscript/ScriptIntrinsic.java
@@ -27,5 +27,8 @@ package android.renderscript;
 public abstract class ScriptIntrinsic extends Script {
     ScriptIntrinsic(long id, RenderScript rs) {
         super(id, rs);
+        if (id == 0) {
+            throw new RSRuntimeException("Loading of ScriptIntrinsic failed.");
+        }
     }
 }
