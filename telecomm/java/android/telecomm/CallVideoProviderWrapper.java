f759b5bb0bd41fc33df8b43ee9608fd421792353
Andrew Lee <anwlee@google.com>
2014-06-27 10:54:44 -0700

    Add wrapper to provide method calls for binder to InCallUI.
    Bug: 15893156
    Change-Id: I6b12c0e8c915e184bb469b5934865242f9fde00d

---
+++ telecomm/java/android/telecomm/CallVideoProviderWrapper.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package android.telecomm;
+
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import com.android.internal.telecomm.ICallVideoProvider;
+
+
+public class CallVideoProviderWrapper implements IBinder.DeathRecipient {
+    private final ICallVideoProvider mCallVideoProvider;
+
+    CallVideoProviderWrapper(ICallVideoProvider callVideoProvider) throws RemoteException {
+        mCallVideoProvider = callVideoProvider;
+        mCallVideoProvider.asBinder().linkToDeath(this, 0);
+    }
+
+    @Override
+    public void binderDied() {
+        mCallVideoProvider.asBinder().unlinkToDeath(this, 0);
+    }
+
+    /**
+     * Sets the camera to be used for video recording in a video call, using the binder.
+     *
+     * @param cameraId The id of the camera.
+     */
+    public void setCamera(String cameraId) throws RemoteException {
+        mCallVideoProvider.setCamera(cameraId);
+    };
+}
