f9499b36e689dee5cec2cb7bb0b7d21f7c302d84
Fabrice Di Meglio <fdimeglio@google.com>
2014-07-02 17:28:20 -0700

    Add PreferenceFragment styling
    - add the capability to specify a specific layout for PreferenceFragment
    
    Change-Id: I2fa36b8fa8110e6cbc8006e1d000dc90ae6a5f0b

--- core/java/android/preference/PreferenceFragment.java
+++ core/java/android/preference/PreferenceFragment.java
@@ -20,6 +20,7 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -110,6 +111,8 @@ public abstract class PreferenceFragment extends Fragment implements
     private boolean mHavePrefs;
     private boolean mInitDone;
 
+    private int mLayoutResId = com.android.internal.R.layout.preference_list_fragment;
+
     /**
      * The starting request code given out to preference framework.
      */
@@ -159,8 +162,18 @@ public abstract class PreferenceFragment extends Fragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
-        return inflater.inflate(com.android.internal.R.layout.preference_list_fragment, container,
-                false);
+
+        TypedArray a = getActivity().obtainStyledAttributes(null,
+                com.android.internal.R.styleable.PreferenceFragment,
+                com.android.internal.R.attr.preferenceFragmentStyle,
+                0);
+
+        mLayoutResId = a.getResourceId(com.android.internal.R.styleable.PreferenceFragment_layout,
+                mLayoutResId);
+
+        a.recycle();
+
+        return inflater.inflate(mLayoutResId, container, false);
     }
 
     @Override
