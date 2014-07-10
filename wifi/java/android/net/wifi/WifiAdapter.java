ebb530619569dae465e68a4e53e4cdc749ae3727
Vinit Deshpande <vinitd@google.com>
2014-07-09 22:58:21 -0700

    Fix name mismatch in WifiAdapter
    Change-Id: I895a2b7a8aa07a14f54543d0f86ad4a8e7660051

--- wifi/java/android/net/wifi/WifiAdapter.java
+++ wifi/java/android/net/wifi/WifiAdapter.java
@@ -23,21 +23,21 @@ import android.os.Parcelable;
 public class WifiAdapter implements Parcelable {
 
     /* Keep this list in sync with wifi_hal.h */
-    private static final int WIFI_FEATURE_INFRA            = 0x0001;    // Basic infrastructure mode
-    private static final int WIFI_FEATURE_INFRA_5G         = 0x0002;    // Support for 5 GHz Band
-    private static final int WIFI_FEATURE_PASSPOINT        = 0x0004;    // Support for GAS/ANQP
-    private static final int WIFI_FEATURE_WIFI_DIRECT      = 0x0008;    // Wifi-Direct
-    private static final int WIFI_FEATURE_MOBILE_HOTSPOT   = 0x0010;    // Soft AP
-    private static final int WIFI_FEATURE_SCANNER          = 0x0020;    // WifiScanner APIs
-    private static final int WIFI_FEATURE_NAN              = 0x0040;    // Neighbor Awareness Networking
-    private static final int WIFI_FEATURE_D2D_RTT          = 0x0080;    // Device-to-device RTT
-    private static final int WIFI_FEATURE_D2AP_RTT         = 0x0100;    // Device-to-AP RTT
-    private static final int WIFI_FEATURE_BATCH_SCAN       = 0x0200;    // Batched Scan (deprecated)
-    private static final int WIFI_FEATURE_PNO              = 0x0400;    // Preferred network offload
-    private static final int WIFI_FEATURE_ADDITIONAL_STA   = 0x0800;    // Support for two STAs
-    private static final int WIFI_FEATURE_TDLS             = 0x1000;    // Tunnel directed link setup
-    private static final int WIFI_FEATURE_TDLS_OFFCHANNEL  = 0x2000;    // Support for TDLS off channel
-    private static final int WIFI_FEATURE_EPR              = 0x4000;    // Enhanced power reporting
+    public static final int WIFI_FEATURE_INFRA            = 0x0001;  // Basic infrastructure mode
+    public static final int WIFI_FEATURE_INFRA_5G         = 0x0002;  // Support for 5 GHz Band
+    public static final int WIFI_FEATURE_PASSPOINT        = 0x0004;  // Support for GAS/ANQP
+    public static final int WIFI_FEATURE_P2P              = 0x0008;  // Wifi-Direct
+    public static final int WIFI_FEATURE_MOBILE_HOTSPOT   = 0x0010;  // Soft AP
+    public static final int WIFI_FEATURE_SCANNER          = 0x0020;  // WifiScanner APIs
+    public static final int WIFI_FEATURE_NAN              = 0x0040;  // Neighbor Awareness Networking
+    public static final int WIFI_FEATURE_D2D_RTT          = 0x0080;  // Device-to-device RTT
+    public static final int WIFI_FEATURE_D2AP_RTT         = 0x0100;  // Device-to-AP RTT
+    public static final int WIFI_FEATURE_BATCH_SCAN       = 0x0200;  // Batched Scan (deprecated)
+    public static final int WIFI_FEATURE_PNO              = 0x0400;  // Preferred network offload
+    public static final int WIFI_FEATURE_ADDITIONAL_STA   = 0x0800;  // Support for two STAs
+    public static final int WIFI_FEATURE_TDLS             = 0x1000;  // Tunnel directed link setup
+    public static final int WIFI_FEATURE_TDLS_OFFCHANNEL  = 0x2000;  // Support for TDLS off channel
+    public static final int WIFI_FEATURE_EPR              = 0x4000;  // Enhanced power reporting
 
     private String name;
     private int    supportedFeatures;
@@ -64,7 +64,7 @@ public class WifiAdapter implements Parcelable {
     }
 
     public boolean isWifiDirectSupported() {
-        return isFeatureSupported(WIFI_FEATURE_WIFI_DIRECT);
+        return isFeatureSupported(WIFI_FEATURE_P2P);
     }
 
     public boolean isMobileHotstpoSupported() {
