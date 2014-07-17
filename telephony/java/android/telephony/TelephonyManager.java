3099c831441e861328cd4965011c854e060ce56a
Steven Liu <stevenliu@motorola.com>
2014-07-17 10:57:01 -0500

    Add API invokeOemRilRequestRaw
    this API is used by system app, app could communicate with RIL
    with it.
    
    Change-Id: I5397c1cf2b108d9dc3a9694b1f071dc60bb5b341
    
    Conflicts:
    	telephony/java/com/android/internal/telephony/ITelephony.aidl

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3308,4 +3308,23 @@ public class TelephonyManager {
         }
         return false;
     }
+
+    /**
+     * Returns the result and response from RIL for oem request
+     *
+     * @param oemReq the data is sent to ril.
+     * @param oemResp the respose data from RIL.
+     * @return negative value request was not handled or get error
+     *         0 request was handled succesfully, but no response data
+     *         positive value success, data length of response
+     * @hide
+     */
+    public int invokeOemRilRequestRaw(byte[] oemReq, byte[] oemResp) {
+        try {
+            return getITelephony().invokeOemRilRequestRaw(oemReq, oemResp);
+        } catch (RemoteException ex) {
+        } catch (NullPointerException ex) {
+        }
+        return -1;
+    }
 }
--- telephony/java/com/android/internal/telephony/ITelephony.aidl
+++ telephony/java/com/android/internal/telephony/ITelephony.aidl
@@ -724,5 +724,16 @@ interface ITelephony {
      * @return true if the operation was executed correctly.
      */
     boolean setOperatorBrandOverride(String iccId, String brand);
+
+    /**
+     * Returns the result and response from RIL for oem request
+     *
+     * @param oemReq the data is sent to ril.
+     * @param oemResp the respose data from RIL.
+     * @return negative value request was not handled or get error
+     *         0 request was handled succesfully, but no response data
+     *         positive value success, data length of response
+     */
+    int invokeOemRilRequestRaw(in byte[] oemReq, out byte[] oemResp);
 }
 
