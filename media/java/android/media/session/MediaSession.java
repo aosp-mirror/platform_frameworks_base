ca58ddf7c82dd0857de0c3d49d7eb87a842ee4ce
RoboErik <epastern@google.com>
2014-07-17 18:26:21 -0700

    Fix crash when calling MediaSession callback
    bug:16311395
    Change-Id: Ice52d2da63933af881dd95fa8c6f59a6fe27e0df

--- media/java/android/media/session/MediaSession.java
+++ media/java/android/media/session/MediaSession.java
@@ -154,7 +154,7 @@ public final class MediaSession {
         if (TextUtils.isEmpty(tag)) {
             throw new IllegalArgumentException("tag cannot be null or empty");
         }
-        mCbStub = new CallbackStub();
+        mCbStub = new CallbackStub(this);
         MediaSessionManager manager = (MediaSessionManager) context
                 .getSystemService(Context.MEDIA_SESSION_SERVICE);
         try {
@@ -769,7 +769,7 @@ public final class MediaSession {
     public static class CallbackStub extends ISessionCallback.Stub {
         private WeakReference<MediaSession> mMediaSession;
 
-        public void setMediaSession(MediaSession session) {
+        public CallbackStub(MediaSession session) {
             mMediaSession = new WeakReference<MediaSession>(session);
         }
 
