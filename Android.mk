50aca23bd7f51a3cf32a1f7e32238cc1e26ca1a0
Andrew Lee <anwlee@google.com>
2014-07-22 16:41:54 -0700

    Add VideoCall and VideoCall.Listener as an inner classes of Call.
    Bug: 16494880
    Bug: 16495203
    Change-Id: Ife3b6723374ea5fbbff4927c4f9168d075be21e1

--- Android.mk
+++ Android.mk
@@ -349,8 +349,8 @@ LOCAL_SRC_FILES += \
 	media/java/android/media/tv/ITvInputServiceCallback.aidl \
 	media/java/android/media/tv/ITvInputSession.aidl \
 	media/java/android/media/tv/ITvInputSessionCallback.aidl \
-	telecomm/java/com/android/internal/telecomm/ICallVideoProvider.aidl \
-	telecomm/java/com/android/internal/telecomm/ICallVideoClient.aidl \
+	telecomm/java/com/android/internal/telecomm/IVideoCallCallback.aidl \
+	telecomm/java/com/android/internal/telecomm/IVideoCallProvider.aidl \
 	telecomm/java/com/android/internal/telecomm/IConnectionService.aidl \
 	telecomm/java/com/android/internal/telecomm/IConnectionServiceAdapter.aidl \
 	telecomm/java/com/android/internal/telecomm/IInCallAdapter.aidl \
