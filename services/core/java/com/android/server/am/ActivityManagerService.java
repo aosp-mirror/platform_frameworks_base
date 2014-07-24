fd7a8dab687dff9e8b8eae5c2315e1a692eb7e5d
Sachin Sharma <sachsh@codeaurora.org>
2014-07-24 11:57:27 -0700

    Creating sperate files for ANRs traces
    If an ANR comes just after the framework reboot, the traces for the
    framework reboot will get over-written with the ANR traces. So, We
    are missing the traces for the framework reboot which makes debugging
    the framework reboot difficult.
    Even for multiple ANRs in different apps, we will get the traces for
    the last ANR.
    Most of the time traces show that the call is stuck in some native call
    and we do not have any info about the native call stack becuase we do
    not collect the tombstones for teh ANR.
    This patch will create one trace file for each application in case of ANR
    and the filename would be traces_<app_name>.txt and the traces for
    framework reboot will be stored in traces.txt
    
    Change-Id: I5b78ce62b26f25d3dc14097e2988ba12c2d77896

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -5072,6 +5072,23 @@ public final class ActivityManagerService extends ActivityManagerNative
                     annotation != null ? "ANR " + annotation : "ANR",
                     info.toString());
 
+            String tracesPath = SystemProperties.get("dalvik.vm.stack-trace-file", null);
+            if (tracesPath != null && tracesPath.length() != 0) {
+                File traceRenameFile = new File(tracesPath);
+                String newTracesPath;
+                int lpos = tracesPath.lastIndexOf (".");
+                if (-1 != lpos)
+                    newTracesPath = tracesPath.substring (0, lpos) + "_" + app.processName + tracesPath.substring (lpos);
+                else
+                    newTracesPath = tracesPath + "_" + app.processName;
+                traceRenameFile.renameTo(new File(newTracesPath));
+
+                Process.sendSignal(app.pid, 6);
+                SystemClock.sleep(1000);
+                Process.sendSignal(app.pid, 6);
+                SystemClock.sleep(1000);
+            }
+
             // Bring up the infamous App Not Responding dialog
             Message msg = Message.obtain();
             HashMap<String, Object> map = new HashMap<String, Object>();
