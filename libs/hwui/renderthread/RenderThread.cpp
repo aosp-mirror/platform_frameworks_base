738ec3aace180018560998d1c2cdeb9ddde5fbfa
Chris Craik <ccraik@google.com>
2014-07-25 18:25:02 +0000

    Revert "Dump RenderThread stack on unresponsive"
    bug:16563871
    bug:16565900
    bug:16555847
    bug:16551643
    
    This reverts commit ca66e06b9db6e6c921662886e4b7ddd02ac92280.
    
    Change-Id: I23e8d4eaf828b1b298126ba5f36e4e8e7451706a

--- libs/hwui/renderthread/RenderThread.cpp
+++ libs/hwui/renderthread/RenderThread.cpp
@@ -20,7 +20,6 @@
 
 #include <gui/DisplayEventReceiver.h>
 #include <utils/Log.h>
-#include <pthread.h>
 
 #include "../RenderState.h"
 #include "CanvasContext.h"
@@ -137,7 +136,6 @@ public:
 };
 
 RenderThread::RenderThread() : Thread(true), Singleton<RenderThread>()
-        , mThreadId(0)
         , mNextWakeup(LLONG_MAX)
         , mDisplayEventReceiver(0)
         , mVsyncRequested(false)
@@ -246,7 +244,6 @@ void RenderThread::requestVsync() {
 }
 
 bool RenderThread::threadLoop() {
-    mThreadId = pthread_self();
     initThreadLocals();
 
     int timeoutMillis = -1;
@@ -289,16 +286,6 @@ void RenderThread::queue(RenderTask* task) {
     if (mNextWakeup && task->mRunAt < mNextWakeup) {
         mNextWakeup = 0;
         mLooper->wake();
-    }
-}
-
-void RenderThread::queueAndWait(RenderTask* task, Condition& signal, Mutex& lock) {
-    static nsecs_t sTimeout = milliseconds(500);
-    queue(task);
-    status_t err = signal.waitRelative(lock, sTimeout);
-    if (CC_UNLIKELY(err != NO_ERROR)) {
-        ALOGE("Timeout waiting for RenderTherad! err=%d", err);
-        nukeFromOrbit();
     }
 }
 
@@ -352,10 +339,6 @@ RenderTask* RenderThread::nextTask(nsecs_t* nextWakeup) {
         *nextWakeup = mNextWakeup;
     }
     return next;
-}
-
-void RenderThread::nukeFromOrbit() {
-    pthread_kill(mThreadId, SIGABRT);
 }
 
 } /* namespace renderthread */
