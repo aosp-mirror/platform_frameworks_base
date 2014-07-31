d60cb8b65058df61f6bcb5a3d6c92788d36e89a4
Shashank Mittal <mittals@codeaurora.org>
2014-07-31 15:32:29 -0700

    USB: Don't add adb function when usb disconnected
    When USB is disconnected, "adb" functions will be added even in
    "charging" mode. This breaks "charging" mode's design.
    
    Change-Id: I7ab62194a0acb97620e33da4839e0f6c87f60917

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -482,7 +482,8 @@ public class UsbDeviceManager {
             // with OEM specific mode.
             if (functions != null && makeDefault && !needsOemUsbOverride()) {
 
-                if (mAdbEnabled) {
+                if (!UsbManager.USB_FUNCTION_CHARGING.equals(functions)
+                        && mAdbEnabled) {
                     functions = addFunction(functions, UsbManager.USB_FUNCTION_ADB);
                 } else {
                     functions = removeFunction(functions, UsbManager.USB_FUNCTION_ADB);
@@ -514,7 +515,8 @@ public class UsbDeviceManager {
                 // Override with bootmode specific usb mode if needed
                 functions = processOemUsbOverride(functions);
 
-                if (mAdbEnabled) {
+                if (!UsbManager.USB_FUNCTION_CHARGING.equals(functions)
+                        && mAdbEnabled) {
                     functions = addFunction(functions, UsbManager.USB_FUNCTION_ADB);
                 } else {
                     functions = removeFunction(functions, UsbManager.USB_FUNCTION_ADB);
