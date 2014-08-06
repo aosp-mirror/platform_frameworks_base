465acf3f3f5111b58025d6272f2fd734c0c69390
Nicolas Prevot <nprevot@google.com>
2014-08-06 17:03:25 +0100

    Marking the methods to get Profile/Device Owner with @SystemApi.
    BUG: 16860239
    
    Change-Id: I60126a8112a57e836cae3bcd9b62ebf50385f5ec

--- core/java/android/app/admin/DevicePolicyManager.java
+++ core/java/android/app/admin/DevicePolicyManager.java
@@ -2157,6 +2157,7 @@ public class DevicePolicyManager {
     }
 
     /** @hide */
+    @SystemApi
     public String getDeviceOwner() {
         if (mService != null) {
             try {
@@ -2365,6 +2366,7 @@ public class DevicePolicyManager {
      * owner has been set for that user.
      * @throws IllegalArgumentException if the userId is invalid.
      */
+    @SystemApi
     public ComponentName getProfileOwner() throws IllegalArgumentException {
         return getProfileOwnerAsUser(Process.myUserHandle().getIdentifier());
     }
