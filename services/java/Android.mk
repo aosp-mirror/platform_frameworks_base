b2bec3c955ff7a9ad61eaeed91fa9d317ede5697
GabrielDuque <gabrielferrazduque@gmail.com>
2014-08-26 22:03:08 -0300

    Update Android.mk

--- services/java/Android.mk
+++ services/java/Android.mk
@@ -7,7 +7,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
             $(call all-subdir-java-files) \
 	    com/android/server/EventLogTags.logtags \
-	    com/android/server/am/EventLogTags.logtags
+	    com/android/server/am/EventLogTags.logtags\
 
 LOCAL_MODULE:= services
 
