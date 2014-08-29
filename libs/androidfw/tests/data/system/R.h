9d9cc6233c7035c32d3b8622aa5dc7d413193dc7
Adam Lesinski <adamlesinski@google.com>
2014-08-29 14:10:04 -0700

    Add test to ensure themes get copied from separate resource tables
    Change-Id: I94e9966cf1e9d0e7e6c7daa0606a87bb0f67705e

---
+++ libs/androidfw/tests/data/system/R.h
@@ -0,0 +1,23 @@
+#ifndef __ANDROID_R_H
+#define __ANDROID_R_H
+
+namespace android {
+namespace R {
+
+namespace attr {
+    enum {
+        background  = 0x01010000, // default
+        foreground  = 0x01010001, // default
+    };
+}
+
+namespace style {
+    enum {
+        Theme_One      = 0x01020000,   // default
+    };
+}
+
+} // namespace R
+} // namespace android
+
+#endif // __ANDROID_R_H
