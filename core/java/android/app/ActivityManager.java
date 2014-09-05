a4e102ee580282dc7abeb22f4a025813e53b9431
Dianne Hackborn <hackbod@google.com>
2014-09-04 22:52:27 -0700

    Work on issue #17357238: Recents is often slow if not used in a while
    Add a new activity attribute, resumeWhilePausing, that allows an
    activity specifying it to immediately start running without waiting
    for the previous activity to pause.  The recents activity is updated
    to use this.
    
    The implementation of this is ultimately fairly simple -- if we are
    in the path of resuming such an activity, and find that we first need
    to pause the existing activity, then within the activity manager we
    do the regular pause flow but act like it has immediately finished
    pausing right then so that we can immediately go on to the resume.
    To make this clean, we tell the activity when asking it to pause that
    it should not come back and tell us it is done, because we aren't in
    any way waiting for it.
    
    One potentially important change I needed to make here is the pause
    callback no longer provides the saved persistent state, because we
    now can't count on that callback happening.  I don't think there was
    really any utility in this anyway -- all modern apps will have their
    save state flow happen as part of stopping, not pausing, so we'll
    only capture that saved state when the stop is reported back anyway.
    And since we do send the saved state back when stopping, it would
    always blow away whatever we had gotten at the pause.
    
    Finally, update the documentation for AppTask.startActivity(), and
    fix the implementation handling that to be cleaner -- we need to
    deal with inTask first before getting in to "oh noes add NEW_TASK
    if this isn't coming from a calling activity" flow.
    
    Change-Id: Ia1da0fac90d7bdbaafdda2e34850d795ce17a39f

--- core/java/android/app/ActivityManager.java
+++ core/java/android/app/ActivityManager.java
@@ -2656,17 +2656,24 @@ public class ActivityManager {
 
         /**
          * Start an activity in this task.  Brings the task to the foreground.  If this task
-         * is not currently active (that is, its id < 0), then the activity being started
-         * needs to be started as a new task and the Intent's ComponentName must match the
-         * base ComponenentName of the recent task entry.  Otherwise, the activity being
-         * started must <b>not</b> be launched as a new task -- not through explicit intent
-         * flags nor implicitly as the singleTask or singleInstance launch modes.
+         * is not currently active (that is, its id < 0), then a new activity for the given
+         * Intent will be launched as the root of the task and the task brought to the
+         * foreground.  Otherwise, if this task is currently active and the Intent does not specify
+         * an activity to launch in a new task, then a new activity for the given Intent will
+         * be launched on top of the task and the task brought to the foreground.  If this
+         * task is currently active and the Intent specifies {@link Intent#FLAG_ACTIVITY_NEW_TASK}
+         * or would otherwise be launched in to a new task, then the activity not launched but
+         * this task be brought to the foreground and a new intent delivered to the top
+         * activity if appropriate.
          *
-         * <p>See {@link Activity#startActivity(android.content.Intent, android.os.Bundle)
-         * Activity.startActivity} for more information.</p>
+         * <p>In other words, you generally want to use an Intent here that does not specify
+         * {@link Intent#FLAG_ACTIVITY_NEW_TASK} or {@link Intent#FLAG_ACTIVITY_NEW_DOCUMENT},
+         * and let the system do the right thing.</p>
          *
          * @param intent The Intent describing the new activity to be launched on the task.
          * @param options Optional launch options.
+         *
+         * @see Activity#startActivity(android.content.Intent, android.os.Bundle)
          */
         public void startActivity(Context context, Intent intent, Bundle options) {
             ActivityThread thread = ActivityThread.currentActivityThread();
