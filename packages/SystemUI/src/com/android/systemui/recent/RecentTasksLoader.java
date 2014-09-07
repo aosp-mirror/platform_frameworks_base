fdf169cbea5c59c6f74c25c6c529d8a19c6cb78c
Amith Yamasani <yamasani@google.com>
2014-09-07 15:45:57 -0700

    Use correct API for badging
    Bug: 17414778
    Change-Id: I7c0321d9435b0787a90cd62be80862069ba2d790

--- packages/SystemUI/src/com/android/systemui/recent/RecentTasksLoader.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentTasksLoader.java
@@ -203,8 +203,7 @@ public class RecentTasksLoader implements View.OnTouchListener {
         Drawable icon = getFullResIcon(td.resolveInfo, pm);
         if (td.userId != UserHandle.myUserId()) {
             // Need to badge the icon
-            icon = mContext.getPackageManager().getUserBadgedDrawableForDensity(icon,
-                    new UserHandle(td.userId), null, 0);
+            icon = mContext.getPackageManager().getUserBadgedIcon(icon, new UserHandle(td.userId));
         }
         if (DEBUG) Log.v(TAG, "Loaded bitmap for task "
                 + td + ": " + thumbnail);
