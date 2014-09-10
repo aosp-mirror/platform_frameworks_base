c9d7150680e288f848178fc6c44776c61ddc8548
Jeff Sharkey <jsharkey@android.com>
2014-09-10 11:23:15 -0700

    Manage mode shouldn't block for other providers.
    When loading DocumentsUI in management mode (for Downloads), we
    normally focus on a single authority and let the other providers
    keep loading async.  However, when one of the providers kicks a
    change notification, we end up blocking the main thread.
    
    Simple fix is to move the blocking into the AsyncTask.
    
    Bug: 17452691
    Change-Id: Ie5212ad825b7df719690b29941c9ae9ba2cabe4b

--- packages/DocumentsUI/src/com/android/documentsui/RootsCache.java
+++ packages/DocumentsUI/src/com/android/documentsui/RootsCache.java
@@ -116,9 +116,6 @@ public class RootsCache {
      * Gather roots from storage providers belonging to given package name.
      */
     public void updatePackageAsync(String packageName) {
-        // Need at least first load, since we're going to be using previously
-        // cached values for non-matching packages.
-        waitForFirstLoad();
         new UpdateTask(packageName).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
@@ -182,6 +179,12 @@ public class RootsCache {
         @Override
         protected Void doInBackground(Void... params) {
             final long start = SystemClock.elapsedRealtime();
+
+            if (mFilterPackage != null) {
+                // Need at least first load, since we're going to be using
+                // previously cached values for non-matching packages.
+                waitForFirstLoad();
+            }
 
             mTaskRoots.put(mRecentsRoot.authority, mRecentsRoot);
 
