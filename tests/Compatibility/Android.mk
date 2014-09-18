e87cc9a2f29567747c0ea92756f2b579550016c7
wsmlby <wsmlby@google.com>
2014-09-18 13:33:18 -0700

    Add leanback support to AppCompatiblityTest
    Change-Id: Ia9da14b8551c4ec5834a047f0792a5a213a3ac04

--- tests/Compatibility/Android.mk
+++ tests/Compatibility/Android.mk
@@ -18,12 +18,12 @@ include $(CLEAR_VARS)
 # We only want this apk build for tests.
 LOCAL_MODULE_TAGS := tests
 
+LOCAL_JAVA_LIBRARIES := android.test.runner
 # Include all test java files.
 LOCAL_SRC_FILES := \
 	$(call all-java-files-under, src)
 
 
-LOCAL_SDK_VERSION := 8
 LOCAL_PACKAGE_NAME := AppCompatibilityTest
 
 include $(BUILD_PACKAGE)
