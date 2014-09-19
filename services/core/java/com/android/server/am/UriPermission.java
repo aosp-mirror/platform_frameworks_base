29b2516012cf69022ebe33409223095fb3c99405
Jeff Sharkey <jsharkey@android.com>
2014-09-19 09:14:34 -0700

    Only revoke ownerless grants when unprivileged.
    Recently we relaxed revokeUriPermission() to allow apps to revoke
    Uri permissions that had been granted to them, but this uncovered
    bugs in apps that had been relying on the previous no-op behavior.
    
    To mitigate this, only revoke ownerless Uri permissions when in the
    unprivileged state; an active owner indicates that another component
    of the calling app still needs the permission.
    
    Bug: 17554268
    Change-Id: Icc412933b29041ffb699d20136a623440ecc71ec

--- services/core/java/com/android/server/am/UriPermission.java
+++ services/core/java/com/android/server/am/UriPermission.java
@@ -180,7 +180,7 @@ final class UriPermission {
     /**
      * @return if mode changes should trigger persisting.
      */
-    boolean revokeModes(int modeFlags) {
+    boolean revokeModes(int modeFlags, boolean includingOwners) {
         final boolean persistable = (modeFlags & Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION) != 0;
         modeFlags &= (Intent.FLAG_GRANT_READ_URI_PERMISSION
                 | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
@@ -193,7 +193,7 @@ final class UriPermission {
                 persistedModeFlags &= ~Intent.FLAG_GRANT_READ_URI_PERMISSION;
             }
             globalModeFlags &= ~Intent.FLAG_GRANT_READ_URI_PERMISSION;
-            if (mReadOwners != null) {
+            if (mReadOwners != null && includingOwners) {
                 ownedModeFlags &= ~Intent.FLAG_GRANT_READ_URI_PERMISSION;
                 for (UriPermissionOwner r : mReadOwners) {
                     r.removeReadPermission(this);
@@ -207,7 +207,7 @@ final class UriPermission {
                 persistedModeFlags &= ~Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
             }
             globalModeFlags &= ~Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
-            if (mWriteOwners != null) {
+            if (mWriteOwners != null && includingOwners) {
                 ownedModeFlags &= ~Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
                 for (UriPermissionOwner r : mWriteOwners) {
                     r.removeWritePermission(this);
