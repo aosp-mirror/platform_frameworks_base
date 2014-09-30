dbfbb17512fe6a5b3c7198d60b6a149969174a71
Jeff Hao <jeffhao@google.com>
2014-09-30 14:41:31 -0700

    Backport of ordering apps for boot dexopt.
    This is a squashed commit of the following changes:
    
    1. Order apps by priority when performing boot dexopt.
       (cherry picked from commit 65cde7d42d741c7d9aa2714a397b7333f688ab55)
    
    2. Improve priority ordering of apps when performing boot dexopt.
       Added core apps and updated system apps.
       (cherry picked from commit 272bf3a274daff62995caf05da338c1f2a73dae3)
    
    3. Stop boot dexopt when low on memory.
       (cherry picked from commit 1d892dcb6b0ff3a50cc63e387667dc29baf1014f)
    
    Bug: 17641843
    Change-Id: Ie32f1c21047d3462aaf728f7633fecf647ba2b47

--- core/java/android/content/pm/PackageInfo.java
+++ core/java/android/content/pm/PackageInfo.java
@@ -235,6 +235,9 @@ public class PackageInfo implements Parcelable {
     public int installLocation = INSTALL_LOCATION_INTERNAL_ONLY;
 
     /** @hide */
+    public boolean coreApp;
+
+    /** @hide */
     public boolean requiredForAllUsers;
 
     /** @hide */
@@ -293,6 +296,7 @@ public class PackageInfo implements Parcelable {
         dest.writeTypedArray(reqFeatures, parcelableFlags);
         dest.writeTypedArray(featureGroups, parcelableFlags);
         dest.writeInt(installLocation);
+        dest.writeInt(coreApp ? 1 : 0);
         dest.writeInt(requiredForAllUsers ? 1 : 0);
         dest.writeString(restrictedAccountType);
         dest.writeString(requiredAccountType);
@@ -337,6 +341,7 @@ public class PackageInfo implements Parcelable {
         reqFeatures = source.createTypedArray(FeatureInfo.CREATOR);
         featureGroups = source.createTypedArray(FeatureGroupInfo.CREATOR);
         installLocation = source.readInt();
+        coreApp = source.readInt() != 0;
         requiredForAllUsers = source.readInt() != 0;
         restrictedAccountType = source.readString();
         requiredAccountType = source.readString();
