76716c5a180aa471c6973ca7aa03c7f2da677823
Svetoslav Ganov <svetoslavganov@google.com>
2014-10-03 20:34:37 +0000

    Revert "TouchExplorer computes incorrectly the click location."
    This reverts commit 851a5059a47cbf76e530c9d050a677cb6e3f8657 as
    it creates a regression. Let us revert this and correctly fix the
    issue the original change was trying to address.
    
    bug:17789608
    
    Change-Id: I8abb1a61d5310430e839e4ef60e7ca5cc0cbdd80

--- services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
+++ services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -764,6 +764,29 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
                 .getAccessibilityFocusClickPointInScreenNotLocked(outPoint);
     }
 
+    /**
+     * Gets the bounds of the active window.
+     *
+     * @param outBounds The output to which to write the bounds.
+     */
+    boolean getActiveWindowBounds(Rect outBounds) {
+        // TODO: This should be refactored to work with accessibility
+        // focus in multiple windows.
+        IBinder token;
+        synchronized (mLock) {
+            final int windowId = mSecurityPolicy.mActiveWindowId;
+            token = mGlobalWindowTokens.get(windowId);
+            if (token == null) {
+                token = getCurrentUserStateLocked().mWindowTokens.get(windowId);
+            }
+        }
+        mWindowManagerService.getWindowFrame(token, outBounds);
+        if (!outBounds.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
     int getActiveWindowId() {
         return mSecurityPolicy.getActiveWindowId();
     }
@@ -3205,6 +3228,13 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
                     point.offset((int) -spec.offsetX, (int) -spec.offsetY);
                     point.x = (int) (point.x * (1 / spec.scale));
                     point.y = (int) (point.y * (1 / spec.scale));
+                }
+
+                // Make sure the point is within the window.
+                Rect windowBounds = mTempRect;
+                getActiveWindowBounds(windowBounds);
+                if (!windowBounds.contains(point.x, point.y)) {
+                    return false;
                 }
 
                 // Make sure the point is within the screen.
