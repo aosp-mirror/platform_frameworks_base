7053919a3a55ad1b58e6db701afda18850037732
Deepanshu Gupta <deepanshu@google.com>
2014-10-15 15:57:40 -0700

    Add Optimized Line breaking to LayoutLib
    Change-Id: I308a7d07d98ddd7747f16e06bcffcfd14d667534

---
+++ tools/layoutlib/bridge/src/android/text/Primitive.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.text;
+
+import com.android.annotations.NonNull;
+
+// Based on the native implementation of Primitive in
+// frameworks/base/core/jni/android_text_StaticLayout.cpp revision b808260
+public class Primitive {
+    public final @NonNull PrimitiveType type;
+    public final int location;
+    // The following fields don't make sense for all types.
+    // Box and Glue have width only.
+    // Penalty has both width and penalty.
+    // Word_break has penalty only.
+    public final float width;
+    public final float penalty;
+
+    /**
+     * Use {@code PrimitiveType#getNewPrimitive()}
+     */
+    private Primitive(@NonNull PrimitiveType type, int location, float width, float penalty) {
+        this.type = type;
+        this.location = location;
+        this.width = width;
+        this.penalty = penalty;
+    }
+
+    public static enum PrimitiveType {
+        /**
+         * Something with a constant width that is to be typeset - like a character.
+         */
+        BOX,
+        /**
+         * Blank space with fixed width.
+         */
+        GLUE,
+        /**
+         * Aesthetic cost indicating how desirable breaking at this point will be. A penalty of
+         * {@link #PENALTY_INFINITY} means a forced non-break, whereas a penalty of negative
+         * {@code #PENALTY_INFINITY} means a forced break.
+         * <p/>
+         * Currently, it only stores penalty with values 0 or -infinity.
+         */
+        PENALTY,
+        /**
+         * For tabs - variable width space.
+         */
+        VARIABLE,
+        /**
+         * Possible breakpoints within a word. Think of this as a high cost {@link #PENALTY}.
+         */
+        WORD_BREAK;
+
+        public Primitive getNewPrimitive(int location) {
+            assert this == VARIABLE;
+            return new Primitive(this, location, 0f, 0f);
+        }
+
+        public Primitive getNewPrimitive(int location, float value) {
+            assert this == BOX || this == GLUE || this == WORD_BREAK;
+            if (this == BOX || this == GLUE) {
+                return new Primitive(this, location, value, 0f);
+            } else {
+                return new Primitive(this, location, 0f, value);
+            }
+        }
+
+        public Primitive getNewPrimitive(int location, float width, float penalty) {
+            assert this == PENALTY;
+            return new Primitive(this, location, width, penalty);
+        }
+
+        // forced non-break, negative infinity is forced break.
+        public static final float PENALTY_INFINITY = 1e7f;
+    }
+}
+
--- tools/layoutlib/bridge/src/android/text/StaticLayout_Delegate.java
+++ tools/layoutlib/bridge/src/android/text/StaticLayout_Delegate.java
@@ -1,55 +1,99 @@
 package android.text;
 
+import com.android.annotations.NonNull;
 import com.android.tools.layoutlib.annotations.LayoutlibDelegate;
 
-import java.text.CharacterIterator;
-import java.util.Arrays;
-import java.util.Locale;
+import android.text.StaticLayout.LineBreaks;
+import android.text.Primitive.PrimitiveType;
 
-import com.ibm.icu.lang.UCharacter;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ibm.icu.text.BreakIterator;
 import com.ibm.icu.util.ULocale;
 import javax.swing.text.Segment;
 
 /**
  * Delegate that provides implementation for native methods in {@link android.text.StaticLayout}
- *
- * Through the layoutlib_create tool, selected methods of Handler have been replaced
- * by calls to methods of the same name in this delegate class.
- *
+ * <p/>
+ * Through the layoutlib_create tool, selected methods of Handler have been replaced by calls to
+ * methods of the same name in this delegate class.
  */
 public class StaticLayout_Delegate {
 
-    /**
-     * Fills the recycle array with positions that are suitable to break the text at. The array
-     * must be terminated by '-1'.
-     */
+    private static final char CHAR_SPACE     = 0x20;
+    private static final char CHAR_TAB       = 0x09;
+    private static final char CHAR_NEWLINE   = 0x0A;
+    private static final char CHAR_ZWSP      = 0x200B;  // Zero width space.
+
     @LayoutlibDelegate
-    /*package*/ static int[] nLineBreakOpportunities(String locale, char[] text, int length,
-            int[] recycle) {
-        BreakIterator iterator = BreakIterator.getLineInstance(new ULocale(locale));
-        Segment segment = new Segment(text, 0, length);
-        iterator.setText(segment);
-        if (recycle == null) {
-            // Because 42 is the answer to everything.
-            recycle = new int[42];
+    /*package*/ static int nComputeLineBreaks(String locale, char[] inputText, float[] widths,
+            int length, float firstWidth, int firstWidthLineCount, float restWidth,
+            int[] variableTabStops, int defaultTabStop, boolean optimize, LineBreaks recycle,
+            int[] recycleBreaks, float[] recycleWidths, boolean[] recycleFlags, int recycleLength) {
+
+        // compute all possible breakpoints.
+        BreakIterator it = BreakIterator.getLineInstance(new ULocale(locale));
+        it.setText(new Segment(inputText, 0, length));
+        // average word length in english is 5. So, initialize the possible breaks with a guess.
+        List<Integer> breaks = new ArrayList<Integer>((int) Math.ceil(length / 5d));
+        int loc;
+        it.first();
+        while ((loc = it.next()) != BreakIterator.DONE) {
+            breaks.add(loc);
         }
-        int breakOpp = iterator.first();
-        recycle[0] = breakOpp;
-        //noinspection ConstantConditions
-        assert BreakIterator.DONE == -1;
-        for (int i = 1; breakOpp != BreakIterator.DONE; ++i) {
-            if (i >= recycle.length) {
-                recycle = doubleSize(recycle);
-            }
-            assert (i < recycle.length);
-            breakOpp = iterator.next();
-            recycle[i] = breakOpp;
+
+        LineWidth lineWidth = new LineWidth(firstWidth, firstWidthLineCount, restWidth);
+        TabStops tabStopCalculator = new TabStops(variableTabStops, defaultTabStop);
+        List<Primitive> primitives = computePrimitives(inputText, widths, length, breaks);
+        LineBreaker lineBreaker;
+        if (optimize) {
+            lineBreaker = new OptimizingLineBreaker(primitives, lineWidth, tabStopCalculator);
+        } else {
+            lineBreaker = new GreedyLineBreaker(primitives, lineWidth, tabStopCalculator);
         }
-        return recycle;
+        lineBreaker.computeBreaks(recycle);
+        return recycle.breaks.length;
     }
 
-    private static int[] doubleSize(int[] array) {
-        return Arrays.copyOf(array, array.length * 2);
+    /**
+     * Compute metadata each character - things which help in deciding if it's possible to break
+     * at a point or not.
+     */
+    @NonNull
+    private static List<Primitive> computePrimitives(@NonNull char[] text, @NonNull float[] widths,
+            int length, @NonNull List<Integer> breaks) {
+        // Initialize the list with a guess of the number of primitives:
+        // 2 Primitives per non-whitespace char and approx 5 chars per word (i.e. 83% chars)
+        List<Primitive> primitives = new ArrayList<Primitive>(((int) Math.ceil(length * 1.833)));
+        int breaksSize = breaks.size();
+        int breakIndex = 0;
+        for (int i = 0; i < length; i++) {
+            char c = text[i];
+            if (c == CHAR_SPACE || c == CHAR_ZWSP) {
+                primitives.add(PrimitiveType.GLUE.getNewPrimitive(i, widths[i]));
+            } else if (c == CHAR_TAB) {
+                primitives.add(PrimitiveType.VARIABLE.getNewPrimitive(i));
+            } else if (c != CHAR_NEWLINE) {
+                while (breakIndex < breaksSize && breaks.get(breakIndex) < i) {
+                    breakIndex++;
+                }
+                Primitive p;
+                if (widths[i] != 0) {
+                    if (breakIndex < breaksSize && breaks.get(breakIndex) == i) {
+                        p = PrimitiveType.PENALTY.getNewPrimitive(i, 0, 0);
+                    } else {
+                        p = PrimitiveType.WORD_BREAK.getNewPrimitive(i, 0);
+                    }
+                    primitives.add(p);
+                }
+
+                primitives.add(PrimitiveType.BOX.getNewPrimitive(i, widths[i]));
+            }
+        }
+        // final break at end of everything
+        primitives.add(
+                PrimitiveType.PENALTY.getNewPrimitive(length, 0, -PrimitiveType.PENALTY_INFINITY));
+        return primitives;
     }
 }
