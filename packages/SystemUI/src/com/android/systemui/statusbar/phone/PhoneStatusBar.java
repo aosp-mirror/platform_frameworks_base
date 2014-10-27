952d5d8d8a82ddd21ca4f119ec75a4c82fec3f79
Jason Monk <jmonk@google.com>
2014-10-27 15:28:22 -0400

    Add hotspot icon to status bar
    Bug: 17318034
    Change-Id: I291fe6bf7ac8c2faffd37cfda436ef050c1c5ca5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -584,7 +584,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         addNavigationBar();
 
         // Lastly, call to the icon policy to install/update all the icons.
-        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController);
+        mIconPolicy = new PhoneStatusBarPolicy(mContext, mCastController, mHotspotController);
         mSettingsObserver.onChange(false); // set up
 
         mHeadsUpObserver.onChange(true); // set up
