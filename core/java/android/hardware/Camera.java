705e47e64188bc8a5ae9752d844b7a7706a004ef
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-08 04:28:02 +0000

    Don't throw exceptions when value pairs have spaces in them
    "(1,2)" worked, "(1, 2)" crapped out. Fix it.
    
    Change-Id: I71262909280f88d930898a2bcf37d379add9fdea

--- core/java/android/hardware/Camera.java
+++ core/java/android/hardware/Camera.java
@@ -4088,6 +4088,7 @@ public class Camera {
             splitter.setString(str);
             int index = 0;
             for (String s : splitter) {
+                s = s.replaceAll("\\s","");
                 output[index++] = Integer.parseInt(s);
             }
         }
