eaaf0512fcbf03b926cdbbbb7f54df474ecf861a
Svet Ganov <svetoslavganov@google.com>
2014-11-26 04:09:27 -0800

    Crash in print spooler when back pressed before content update complted.
    We are updating the printed content asynchronously. If the user cancels
    printing before the first update is complete the spooler components are
    in a destroyed state but we attempt to handle the completed update resulting
    in a crash. Now if printing is cancelled we ignore the update result.
    
    bug:18525491
    
    Change-Id: I1ef47a17de19896cba2c7a1dd2bfc205065e0a5a

--- packages/PrintSpooler/src/com/android/printspooler/ui/PrintActivity.java
+++ packages/PrintSpooler/src/com/android/printspooler/ui/PrintActivity.java
@@ -487,6 +487,10 @@ public class PrintActivity extends Activity implements RemotePrintDocument.Updat
                 requestCreatePdfFileOrFinish();
             } break;
 
+            case STATE_PRINT_CANCELED: {
+                updateOptionsUi();
+            } break;
+
             default: {
                 updatePrintPreviewController(document.changed);
 
