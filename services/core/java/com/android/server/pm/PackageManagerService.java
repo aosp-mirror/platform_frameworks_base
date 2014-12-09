a98d3462bef2e8fd4e0800b35cbafea0d50741a0
Jeff Sharkey <jsharkey@android.com>
2014-12-08 16:41:04 -0800

    Consistent result when signatures don't match.
    INSTALL_FAILED_UPDATE_INCOMPATIBLE is a better description of the
    real underlying cause when signatures don't match.
    
    Bug: 18413784
    Change-Id: Ib81591df7d0d79f6725a4fa7300ae4e20958f004

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -10199,13 +10199,13 @@ public class PackageManagerService extends IPackageManager.Stub {
                 // default to original signature matching
                 if (compareSignatures(oldPackage.mSignatures, pkg.mSignatures)
                     != PackageManager.SIGNATURE_MATCH) {
-                    res.setError(INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES,
+                    res.setError(INSTALL_FAILED_UPDATE_INCOMPATIBLE,
                             "New package has a different signature: " + pkgName);
                     return;
                 }
             } else {
                 if(!checkUpgradeKeySetLP(ps, pkg)) {
-                    res.setError(INSTALL_PARSE_FAILED_INCONSISTENT_CERTIFICATES,
+                    res.setError(INSTALL_FAILED_UPDATE_INCOMPATIBLE,
                             "New package not signed by keys specified by upgrade-keysets: "
                             + pkgName);
                     return;
