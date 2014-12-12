e99793d144b1d8291b3ad82f6656957e7ce75dde
Yigit Boyar <yboyar@google.com>
2014-12-12 14:26:25 -0800

    add samples, upload gradle files

---
+++ tools/data-binding/samples/BindingDemo/app/src/main/java/com/android/example/bindingdemo/MainActivity.java
@@ -0,0 +1,210 @@
+package com.android.example.bindingdemo;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.android.example.bindingdemo.generated.BR;
+import com.android.example.bindingdemo.generated.ListItemBinder;
+import com.android.example.bindingdemo.generated.MainActivityBinder;
+import com.android.example.bindingdemo.vo.User;
+import com.android.example.bindingdemo.vo.Users;
+import com.android.databinding.library.DataBinder;
+import com.android.databinding.library.Observable;
+import com.android.databinding.library.ObservableHelper;
+import com.android.databinding.library.ObservableListener;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity implements Observable {
+    UserAdapter tkAdapter;
+    UserAdapter robotAdapter;
+    MainActivityBinder dataBinder;
+    User selected;
+    ObservableHelper helper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        helper = new ObservableHelper(this);
+        dataBinder = DataBinder.createBinder(MainActivityBinder.class, this, R.layout.main_activity, null);
+        setContentView(dataBinder.getRoot());
+        dataBinder.getRobotList().setHasFixedSize(true);
+        dataBinder.getToolkittyList().setHasFixedSize(true);
+        tkAdapter = new UserAdapter(Users.toolkities);
+        robotAdapter = new UserAdapter(Users.robots);
+        dataBinder.getToolkittyList().setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        dataBinder.getRobotList().setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        dataBinder.setActivity(this);
+        dataBinder.rebindDirty();
+    }
+
+    public UserAdapter getTkAdapter() {
+        return tkAdapter;
+    }
+
+    public UserAdapter getRobotAdapter() {
+        return robotAdapter;
+    }
+
+    public User getSelected() {
+        return selected;
+    }
+
+    private void setSelected(User selected) {
+        if (selected == this.selected) {
+            return;
+        }
+        this.selected = selected;
+        helper.fireChange(BR.selected);
+    }
+
+    public View.OnClickListener onSave = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (selected == null) {
+                return;
+            }
+            selected.setName(dataBinder.getSelectedName().getText().toString());
+            selected.setLastName(dataBinder.getSelectedLastname().getText().toString());
+        }
+    };
+
+    public View.OnClickListener onUnselect = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            setSelected(null);
+        }
+    };
+
+    public View.OnClickListener onDelete = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (selected == null) {
+                return;
+            }
+            if (selected.getGroup() == User.TOOLKITTY) {
+                tkAdapter.remove(selected);
+                selected.setGroup(User.ROBOT);
+                robotAdapter.add(selected);
+                dataBinder.getRobotList().smoothScrollToPosition(robotAdapter.getItemCount() - 1);
+            } else {
+                tkAdapter.add(selected);
+                dataBinder.getToolkittyList().smoothScrollToPosition(tkAdapter.getItemCount() - 1);
+                selected.setGroup(User.TOOLKITTY);
+                robotAdapter.remove(selected);
+            }
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void register(ObservableListener observableListener) {
+        helper.register(observableListener);
+    }
+
+    @Override
+    public void unRegister(ObservableListener observableListener) {
+        helper.unRegister(observableListener);
+    }
+
+    public class UserAdapter extends DataBoundAdapter<ListItemBinder> implements View.OnClickListener, Observable {
+        List<User> userList = new ArrayList<>();
+        ObservableHelper mHelper;
+        public UserAdapter(User[] toolkities) {
+            super(R.layout.list_item, ListItemBinder.class);
+            mHelper = new ObservableHelper(this);
+            userList.addAll(Arrays.asList(toolkities));
+        }
+
+        @Override
+        public DataBoundViewHolder<ListItemBinder> onCreateViewHolder(ViewGroup viewGroup, int type) {
+            DataBoundViewHolder<ListItemBinder> vh = super.onCreateViewHolder(viewGroup, type);
+            vh.dataBinder.setClickListener(this);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(DataBoundViewHolder<ListItemBinder> vh, int index) {
+            vh.dataBinder.setUser(userList.get(index));
+            vh.dataBinder.rebindDirty();
+        }
+
+        @Override
+        public int getItemCount() {
+            return userList.size();
+        }
+
+        public void add(User user) {
+            if (userList.contains(user)) {
+                return;
+            }
+            userList.add(user);
+            notifyItemInserted(userList.size() - 1);
+            mHelper.fireChange("itemCount");
+        }
+
+        public void remove(User user) {
+            int i = userList.indexOf(user);
+            if (i < 0) {
+                return;
+            }
+            userList.remove(i);
+            notifyItemRemoved(i);
+            mHelper.fireChange("itemCount");
+        }
+
+        @Override
+        public void onClick(View v) {
+            RecyclerView.LayoutParams lp = (RecyclerView.LayoutParams) v.getLayoutParams();
+            final int pos = lp.getViewPosition();
+            if (pos > -1 && pos < userList.size()) {
+                v.requestFocus();
+                setSelected(userList.get(pos));
+            } else {
+                setSelected(null);
+            }
+        }
+
+        @Override
+        public void register(ObservableListener observableListener) {
+            mHelper.register(observableListener);
+        }
+
+        @Override
+        public void unRegister(ObservableListener observableListener) {
+            mHelper.unRegister(observableListener);
+        }
+    }
+}
