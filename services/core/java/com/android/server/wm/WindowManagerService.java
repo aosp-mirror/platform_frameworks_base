dc36e99b5a71b56a4def6928e66f8c954069e667
Martin Wallgren <martin.wallgren@sonymobile.com>
2015-01-07 14:59:47 +0100

    Don't enable screen until keyguard is ready
    The home screen was shown before the keyguard was ready. Allowing the
    user to navigate home without unlocking the phone.
    
    WindowManagerService calls checkWaitingForWindowsLocked to determine
    when it is safe to enable the screen. But checkWaitingForWindowsLocked
    only check if the Keyguard is ready if there is a Window of type
    TYPE_STATUS_BAR.
    
    If there is no statusbar window, the WindowManagerService assumed that
    the keyguard was ready.
    
    This change will assume that the keyguard is NOT ready until there is a
    statusbar window and mPolicy.isKeyguardDrawnLw() return true.
    
    Downside to this patch is that it will not prevent home screen from
    being shown before keyguard if we have displayed any boot messages
    during boot. At the moment we only show boot messages during first boot
    and after upgrades.
    
    Change-Id: Ia3da149568f966c9d753f658d939946dfa2f3b46

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5715,7 +5715,7 @@ public class WindowManagerService extends IWindowManager.Stub
         boolean wallpaperEnabled = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_enableWallpaperService)
                 && !mOnlyCore;
-        boolean haveKeyguard = true;
+        boolean haveKeyguard = false;
         // TODO(multidisplay): Expand to all displays?
         final WindowList windows = getDefaultWindowListLocked();
         final int N = windows.size();
