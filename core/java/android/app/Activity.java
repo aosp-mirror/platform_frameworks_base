bd769a15abfcb23b16daaed10c09029bbb978eef
Jose Lima <joselima@google.com>
2015-01-28 10:43:15 -0800

    Fix crash caused by disabling the Options panel on TVs
       Address more cases where calls to getPanelState may cause a crash when the
    options panel is disabled on a platform.
    
    Bug: 19178531
    Bug: 18780696
    Change-Id: Ib72bb8483e636181788ed3919c4cb9e99a94b7b1

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -3230,7 +3230,9 @@ public class Activity extends ContextThemeWrapper
      * Programmatically closes the most recently opened context menu, if showing.
      */
     public void closeContextMenu() {
-        mWindow.closePanel(Window.FEATURE_CONTEXT_MENU);
+        if (mWindow.hasFeature(Window.FEATURE_CONTEXT_MENU)) {
+            mWindow.closePanel(Window.FEATURE_CONTEXT_MENU);
+        }
     }
 
     /**
