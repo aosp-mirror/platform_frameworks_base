f8512ce08174426dfbba96e8108e5a64332f1e71
Alan Viverette <alanv@google.com>
2015-02-05 15:58:56 -0800

    Mutate scroll bar drawables
    Bug: 19285726
    Change-Id: I4bb596433c1fa1cc4e2fa53d0cdae992f9add858

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -12872,7 +12872,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
                         Interpolator.Result.FREEZE_END) {
                     cache.state = ScrollabilityCache.OFF;
                 } else {
-                    cache.scrollBar.setAlpha(Math.round(values[0]));
+                    cache.scrollBar.mutate().setAlpha(Math.round(values[0]));
                 }
 
                 // This will make the scroll bars inval themselves after
@@ -12882,7 +12882,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
             } else {
                 // We're just on -- but we may have been fading before so
                 // reset alpha
-                cache.scrollBar.setAlpha(255);
+                cache.scrollBar.mutate().setAlpha(255);
             }
 
 
