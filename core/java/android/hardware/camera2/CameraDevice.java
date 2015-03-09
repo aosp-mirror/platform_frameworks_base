35286a2268f93b88b16bfaa2ea5def1c9e36a7f2
Yin-Chia Yeh <yinchiayeh@google.com>
2015-03-09 16:53:14 -0700

    Camera: add @hide API for setting camera rotation
    Only adding API header. Haven't filled in implementation.
    
    Change-Id: I99a1c84d194dd20562845a0f566dd10ddb3041b7

--- core/java/android/hardware/camera2/CameraDevice.java
+++ core/java/android/hardware/camera2/CameraDevice.java
@@ -18,6 +18,7 @@ package android.hardware.camera2;
 
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.graphics.ImageFormat;
+import android.hardware.camera2.params.OutputConfiguration;
 import android.os.Handler;
 import android.view.Surface;
 
@@ -377,6 +378,20 @@ public abstract class CameraDevice implements AutoCloseable {
      * @see StreamConfigurationMap#getOutputSizes(Class)
      */
     public abstract void createCaptureSession(List<Surface> outputs,
+            CameraCaptureSession.StateCallback callback, Handler handler)
+            throws CameraAccessException;
+
+    /**
+     * <p>Create a new camera capture session by providing the target output set of Surfaces and
+     * its corresponding surface configuration to the camera device.</p>
+     *
+     * @see #createCaptureSession
+     * @see OutputConfiguration
+     *
+     * @hide
+     */
+    public abstract void createCaptureSessionByOutputConfiguration(
+            List<OutputConfiguration> outputConfigurations,
             CameraCaptureSession.StateCallback callback, Handler handler)
             throws CameraAccessException;
 
