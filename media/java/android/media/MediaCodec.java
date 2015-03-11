d30f04568e5b23fc3793f25b0340a07780872699
Surajit Podder <spodder@codeaurora.org>
2015-03-11 19:37:35 +0530

    MediaCodec: Update cached buffers in async mode
    Update mCachedOutputBuffers when CB_OUTPUT_FORMAT_CHANGE
    event is received in async mode.
    This is needed to ensure the cached buffer properties
    are updated when being compared with buffers of new
    resolution received after the format change.
    
    Change-Id: I16bc92489d72f771f217443eedfce6f15371fa45

--- media/java/android/media/MediaCodec.java
+++ media/java/android/media/MediaCodec.java
@@ -401,6 +401,9 @@ final public class MediaCodec {
 
                 case CB_OUTPUT_FORMAT_CHANGE:
                 {
+                    synchronized(mBufferLock) {
+                        cacheBuffers(false /* input */);
+                    }
                     mCallback.onOutputFormatChanged(mCodec,
                             new MediaFormat((Map<String, Object>) msg.obj));
                     break;
