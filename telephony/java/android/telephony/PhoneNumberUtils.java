e75e3ba83a3e2574a29b8c55e037062b564f08f2
Sandeep Kunta <skunta@codeaurora.org>
2015-03-12 18:56:30 +0530

    Optimize IncallUI delay for voice calls
    In current implementation detectCountry method is called from telephony
    as many times as 34 for one MO voice call, causing increasing delay to appear
    incallui for user.  This fix ensures that there is only one call to
    detectCountry method per one voice call thus optimizing delay.
    
    CRs-Fixed: 803069
    Change-Id: Ia6e832476c7f7be2750f1c2d9a4c83f728c2131e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- telephony/java/android/telephony/PhoneNumberUtils.java
+++ telephony/java/android/telephony/PhoneNumberUtils.java
@@ -26,6 +26,7 @@ import android.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Country;
 import android.location.CountryDetector;
 import android.net.Uri;
 import android.os.PersistableBundle;
@@ -108,6 +109,7 @@ public class PhoneNumberUtils {
     private static final String BCD_EF_ADN_EXTENDED = "*#,N;";
     private static final String BCD_CALLED_PARTY_EXTENDED = "*#abc";
 
+    private static Country sCountryDetector = null;
     /*
      * global-phone-number = ["+"] 1*( DIGIT / written-sep )
      * written-sep         = ("-"/".")
@@ -2177,18 +2179,37 @@ public class PhoneNumberUtils {
     private static boolean isLocalEmergencyNumberInternal(int subId, String number,
                                                           Context context,
                                                           boolean useExactMatch) {
-        String countryIso;
-        CountryDetector detector = (CountryDetector) context.getSystemService(
-                Context.COUNTRY_DETECTOR);
-        if (detector != null && detector.detectCountry() != null) {
-            countryIso = detector.detectCountry().getCountryIso();
-        } else {
+        String countryIso = getCountryIso(context);
+        Rlog.w(LOG_TAG, "isLocalEmergencyNumberInternal" + countryIso);
+        if (countryIso == null) {
             Locale locale = context.getResources().getConfiguration().locale;
             countryIso = locale.getCountry();
             Rlog.w(LOG_TAG, "No CountryDetector; falling back to countryIso based on locale: "
                     + countryIso);
         }
         return isEmergencyNumberInternal(subId, number, countryIso, useExactMatch);
+    }
+
+    private static String getCountryIso(Context context) {
+        Rlog.w(LOG_TAG, "getCountryIso " + sCountryDetector);
+        if (sCountryDetector == null) {
+            CountryDetector detector = (CountryDetector) context.getSystemService(
+                Context.COUNTRY_DETECTOR);
+            if (detector != null) {
+                sCountryDetector = detector.detectCountry();
+            }
+        }
+
+        if (sCountryDetector == null) {
+            return null;
+        } else {
+            return sCountryDetector.getCountryIso();
+        }
+    }
+
+    /** @hide */
+    public static void resetCountryDetectorInfo() {
+        sCountryDetector = null;
     }
 
     /**
