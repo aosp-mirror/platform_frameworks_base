dd47168f3d0f932394778058214908759a97971c
Michal Bendowski <bendowski@google.com>
2015-03-12 09:34:11 -0700

    Add a unit test to the sample project.

---
+++ tools/data-binding/samples/BindingDemo/app/src/test/java/com/android/example/bindingdemo/vo/UnitTest.java
@@ -0,0 +1,39 @@
+package com.android.example.bindingdemo.vo;
+
+import android.binding.OnPropertyChangedListener;
+
+import com.android.example.bindingdemo.R;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+public class UnitTest {
+
+    private User testUser;
+
+    @Before
+    public void setUp() throws Exception {
+        testUser = new User("Ted", "Tester", R.drawable.george, User.ROBOT);
+    }
+
+    @Test
+    public void settersWorkFineOnTheJvm() throws Exception {
+        assertEquals("Ted", testUser.getName());
+        testUser.setName("Tom");
+        assertEquals("Tom", testUser.getName());
+    }
+
+    @Test
+    public void listeners() throws Exception {
+        OnPropertyChangedListener mockListener = mock(OnPropertyChangedListener.class);
+        testUser.addOnPropertyChangedListener(mockListener);
+        testUser.setName("Tom");
+        verify(mockListener).onPropertyChanged(testUser, android.binding.BR.name);
+        verifyNoMoreInteractions(mockListener);
+    }
+}
