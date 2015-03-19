05199e83a460683e2af06212b0dc7310647e8502
Adam Lesinski <adamlesinski@google.com>
2015-03-19 14:37:11 -0700

    Set the appliance (tv/watch) in the configuration earlier
    Some UI is displayed (boot/optimizing) before the configuration
    has been completely set. This noteably happens for watches and
    TVs because UIModeManagerService hasn't started yet.
    
    UIModeManagerService is started earlier, and its dependency
    TwilightService is loaded during the "all system services ready"
    boot phase.
    
    b/18388692
    
    Change-Id: Iabdf8ac3eb04ebcb44779d752e19042769cc65c6

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -561,6 +561,10 @@ public final class SystemServer {
             }
         }
 
+        // We start this here so that we update our configuration to set watch or television
+        // as appropriate.
+        mSystemServiceManager.startService(UiModeManagerService.class);
+
         try {
             mPackageManagerService.performBootDexOpt();
         } catch (Throwable e) {
@@ -836,8 +840,6 @@ public final class SystemServer {
             }
 
             mSystemServiceManager.startService(TwilightService.class);
-
-            mSystemServiceManager.startService(UiModeManagerService.class);
 
             mSystemServiceManager.startService(JobSchedulerService.class);
 
