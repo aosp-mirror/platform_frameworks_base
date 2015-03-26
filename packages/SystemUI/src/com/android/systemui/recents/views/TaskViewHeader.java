5c143e7ead06aa7016e786cf1c9b8bd969a9c72c
Skuhne <skuhne@google.com>
2015-03-26 16:57:12 -0700

    Unopened tasks should show the fullscreen item in recents
    BUG: 19947524
    Change-Id: I83d2143f312ed016264c8bc54fda31720100ff1f

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskViewHeader.java
@@ -219,7 +219,7 @@ public class TaskViewHeader extends FrameLayout {
         Rect display = mSsp.getWindowRect();
         Rect taskRect = mSsp.getTaskBounds(t.key.stackId);
         int resId = R.drawable.star;
-        if (display.equals(taskRect)) {
+        if (display.equals(taskRect) || taskRect.isEmpty()) {
             resId = R.drawable.vector_drawable_place_fullscreen;
         } else {
             boolean top = display.top == taskRect.top;
