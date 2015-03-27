31697500a7013d306b17d6c09179e2499e96b5e1
Pavel Zhamaitsiak <pavelz@google.com>
2015-03-27 09:24:47 -0700

    WiFi Calling: Show different operator name string in Data usage dialog.
    Bug: 19950657
    Change-Id: I9999d58e78154ed2d6d824973c40011875de15ef

--- telephony/java/com/android/internal/telephony/TelephonyIntents.java
+++ telephony/java/com/android/internal/telephony/TelephonyIntents.java
@@ -319,6 +319,7 @@ public class TelephonyIntents {
     public static final String EXTRA_PLMN       = "plmn";
     public static final String EXTRA_SHOW_SPN   = "showSpn";
     public static final String EXTRA_SPN        = "spn";
+    public static final String EXTRA_DATA_SPN   = "spnData";
 
     /**
      * <p>Broadcast Action: It indicates one column of a subinfo record has been changed
