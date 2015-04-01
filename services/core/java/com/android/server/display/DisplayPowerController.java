ded72f05c6f008414b7142bd6300c75977a14105
Danny Baumann <dannybaumann@web.de>
2015-04-01 08:44:37 +0200

    base: Increase brightness faster if ambient light is brightening quickly.
    The point of doing this is when going from indoors to bright sunlight,
    the brightness adjustment currently leaves one with looking at an
    unreadable display for a couple of seconds. Increase responsiveness in
    that scenario to ramp up brightness faster.
    
    Change-Id: Idf8ca8ebcdcea06746b0c4f3ab94d2ddddafb32e

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -324,6 +324,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         }
         long brighteningLightDebounce = resources.getInteger(
                 com.android.internal.R.integer.config_autoBrightnessBrighteningLightDebounce);
+        long brighteningLightFastDebounce = resources.getInteger(
+                com.android.internal.R.integer.config_autoBrightnessBrighteningLightFastDebounce);
         long darkeningLightDebounce = resources.getInteger(
                 com.android.internal.R.integer.config_autoBrightnessDarkeningLightDebounce);
         boolean autoBrightnessResetAmbientLuxAfterWarmUp = resources.getBoolean(
@@ -388,7 +390,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                         handler.getLooper(), sensorManager, screenAutoBrightnessSpline,
                         lightSensorWarmUpTimeConfig, screenBrightnessRangeMinimum,
                         mScreenBrightnessRangeMaximum, dozeScaleFactor, lightSensorRate,
-                        initialLightSensorRate, brighteningLightDebounce, darkeningLightDebounce,
+                        initialLightSensorRate, brighteningLightDebounce,
+                        brighteningLightFastDebounce ,darkeningLightDebounce,
                         autoBrightnessResetAmbientLuxAfterWarmUp, ambientLightHorizon,
                         autoBrightnessAdjustmentMaxGamma, mUseActiveDozeLightSensorConfig,
                         useNewSensorSamplesForDoze, luxLevels);
