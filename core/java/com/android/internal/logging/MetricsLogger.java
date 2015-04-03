7af10188c0525d5a3ab8a1883ce621fe4d333070
John Spurlock <jspurlock@google.com>
2015-04-03 15:10:31 -0400

    Zen: Changes to support new automation settings sub-section.
    Bug: 20064962
    Change-Id: I1da5bd1f8ef52a070b8225da4b761979f290df77

--- core/java/com/android/internal/logging/MetricsLogger.java
+++ core/java/com/android/internal/logging/MetricsLogger.java
@@ -40,6 +40,7 @@ public class MetricsLogger implements MetricsConstants {
 
     public static final int MANAGE_PERMISSIONS = 142;
     public static final int NOTIFICATION_ZEN_MODE_PRIORITY = 143;
+    public static final int NOTIFICATION_ZEN_MODE_AUTOMATION = 144;
 
     public static void visible(Context context, int category) throws IllegalArgumentException {
         if (Build.IS_DEBUGGABLE && category == VIEW_UNKNOWN) {
