85757fceb727f76e42956aec67b1152231ab4ee9
Amith Yamasani <yamasani@google.com>
2015-04-03 13:31:18 -0700

    Turn off a debug flag
    Change-Id: I5d7edc2838341754dd092410e6e21787a3454834

--- services/core/java/com/android/server/am/ActivityManagerDebugConfig.java
+++ services/core/java/com/android/server/am/ActivityManagerDebugConfig.java
@@ -73,7 +73,7 @@ class ActivityManagerDebugConfig {
     static final boolean DEBUG_URI_PERMISSION = DEBUG_ALL || false;
     static final boolean DEBUG_USER_LEAVING = DEBUG_ALL || false;
     static final boolean DEBUG_VISIBILITY = DEBUG_ALL || false;
-    static final boolean DEBUG_USAGE_STATS = DEBUG_ALL || true;
+    static final boolean DEBUG_USAGE_STATS = DEBUG_ALL || false;
 
     static final String POSTFIX_BACKUP = (APPEND_CATEGORY_NAME) ? "_Backup" : "";
     static final String POSTFIX_BROADCAST = (APPEND_CATEGORY_NAME) ? "_Broadcast" : "";
