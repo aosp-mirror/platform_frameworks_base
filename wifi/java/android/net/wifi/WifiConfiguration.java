1ac50cb32c08388540f53833c04a02f13fed0824
Lorenzo Colitti <lorenzo@google.com>
2015-04-06 17:19:19 +0900

    Add an UNKNOWN_UID constant to WifiConfiguration.
    This value will be used to indicate that one of the various UIDs
    tracked stored in the WifiConfiguration is unknown. Currently the
    code just uses "-1" everywhere for this purpose; a constant is
    clearer.
    
    Bug: 20081183
    Change-Id: Id865da9284f642587eed11f67685e5b12ce772c2

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -194,6 +194,9 @@ public class WifiConfiguration implements Parcelable {
     /** @hide */
     public static final int DISABLED_BY_WIFI_MANAGER                        = 5;
 
+    /** @hide */
+    public static final int UNKNOWN_UID = -1;
+
     /**
      * The ID number that the supplicant uses to identify this
      * network configuration entry. This must be passed as an argument
@@ -1131,7 +1134,9 @@ public class WifiConfiguration implements Parcelable {
         if (creatorName != null) sbuf.append(" cname=" + creatorName);
         if (lastUpdateUid != 0) sbuf.append(" luid=" + lastUpdateUid);
         if (lastUpdateName != null) sbuf.append(" lname=" + lastUpdateName);
-        sbuf.append("userApproved=" + userApprovedAsString(userApproved));
+        sbuf.append(" lcuid=" + lastConnectUid);
+        sbuf.append(" userApproved=" + userApprovedAsString(userApproved));
+        sbuf.append(" noInternetAccessExpected=" + noInternetAccessExpected);
 
         if (this.lastConnected != 0) {
             sbuf.append('\n');
