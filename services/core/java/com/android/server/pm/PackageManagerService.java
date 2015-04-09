53f35f4a20f69a61b83b88b666e85277d07e9de0
Fabrice Di Meglio <fdimeglio@google.com>
2015-04-09 13:07:50 -0700

    Fix bug #20128771 All apps disappeared from app drawer after AAZ08 OTA
    - relax a bit Intent resolution. We should still include Apps that
    do not support Web domains :-) so in that context add in the result
    list the Apps that are with a status INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED
    
    Change-Id: Ibad7b7f577552ac75bba256bb387a75b83524958

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4022,7 +4022,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 if (ps != null) {
                     // Try to get the status from User settings first
                     int status = getDomainVerificationStatusLPr(ps, userId);
-                    if (status == INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_ALWAYS) {
+                    if (status == INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_ALWAYS ||
+                            status == INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_UNDEFINED) {
                         result.add(info);
                     } else if (status == INTENT_FILTER_DOMAIN_VERIFICATION_STATUS_NEVER) {
                         neverList.add(info);
