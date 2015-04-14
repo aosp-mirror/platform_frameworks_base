30982c778d969859a2f04d07ef2835ba34ebd728
Selim Gurun <sgurun@google.com>
2015-04-14 14:20:45 -0700

    Enable onProvideVirtualAssistStructure API
    Bug: b/19771298
    
    onProvideVirtualAssistStructure API was hardwired to call the super
    method prevent any crashes when assist gesture is used. Enable it.
    
    Change-Id: I333f9f024ffb34af6a2cfa7e4b66ae233d73eb41

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -2427,9 +2427,7 @@ public class WebView extends AbsoluteLayout
 
     @Override
     public void onProvideVirtualAssistStructure(ViewAssistStructure structure) {
-        super.onProvideVirtualAssistStructure(structure);
-        // TODO: enable when chromium backend lands.
-        // mProvider.getViewDelegate().onProvideVirtualAssistStructure(structure);
+        mProvider.getViewDelegate().onProvideVirtualAssistStructure(structure);
     }
 
     /** @hide */
