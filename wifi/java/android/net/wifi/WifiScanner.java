85607190c958d89999edad95c765ce6b2d4449c8
Vinit Deshpande <vinitd@google.com>
2015-04-21 15:59:53 -0700

    Turn of WifiScanner's debug logging
    Bug: 20416721
    Change-Id: I48051dbc0bb6e58279ea5ca5dce90d837f1df4e6

--- wifi/java/android/net/wifi/WifiScanner.java
+++ wifi/java/android/net/wifi/WifiScanner.java
@@ -722,7 +722,7 @@ public class WifiScanner {
     /* private members and methods */
 
     private static final String TAG = "WifiScanner";
-    private static final boolean DBG = true;
+    private static final boolean DBG = false;
 
     /* commands for Wifi Service */
     private static final int BASE = Protocol.BASE_WIFI_SCANNER;
