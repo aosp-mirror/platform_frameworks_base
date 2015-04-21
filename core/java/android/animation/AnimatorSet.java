b1e80d826b860203cc966d56584f9d83f5636bc0
ztenghui <ztenghui@google.com>
2015-04-21 13:00:08 -0700

    Reset the Node status when clone.
    b/20457797
    
    Change-Id: I5f1b0e2238fdc4a69a9c09c1e6e0272f7ede3685

--- core/java/android/animation/AnimatorSet.java
+++ core/java/android/animation/AnimatorSet.java
@@ -1094,7 +1094,8 @@ public final class AnimatorSet extends Animator {
         public Node clone() {
             try {
                 Node node = (Node) super.clone();
-                node.animation = (Animator) animation.clone();
+                node.animation = animation.clone();
+                node.done = false;
                 return node;
             } catch (CloneNotSupportedException e) {
                throw new AssertionError();
