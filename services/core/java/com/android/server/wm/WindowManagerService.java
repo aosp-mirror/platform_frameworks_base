80b1f64280829e7d483302c23518e9d937e7340a
Craig Mautner <cmautner@google.com>
2015-04-22 10:59:09 -0700

    Fix bad crop on clip reveal animation
    For non-fullscreen apps the dimensions of the app window must be used
    to set up the animation.
    
    Fixes bug 18392184.
    
    Change-Id: Ia1681e4a2cb74be2f820cb76ddc7c651a5e4aab6

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -3385,6 +3385,7 @@ public class WindowManagerService extends IWindowManager.Stub
             WindowState win = atoken.findMainWindow();
             Rect containingFrame = new Rect(0, 0, width, height);
             Rect contentInsets = new Rect();
+            Rect appFrame = new Rect(0, 0, width, height);
             boolean isFullScreen = true;
             if (win != null) {
                 if (win.mContainingFrame != null) {
@@ -3392,6 +3393,9 @@ public class WindowManagerService extends IWindowManager.Stub
                 }
                 if (win.mContentInsets != null) {
                     contentInsets.set(win.mContentInsets);
+                }
+                if (win.mFrame != null) {
+                    appFrame.set(win.mFrame);
                 }
                 isFullScreen =
                         ((win.mSystemUiVisibility & SYSTEM_UI_FLAGS_LAYOUT_STABLE_FULLSCREEN) ==
@@ -3406,8 +3410,8 @@ public class WindowManagerService extends IWindowManager.Stub
                 enter = false;
             }
             Animation a = mAppTransition.loadAnimation(lp, transit, enter, width, height,
-                    mCurConfiguration.orientation, containingFrame, contentInsets, isFullScreen,
-                    isVoiceInteraction);
+                    mCurConfiguration.orientation, containingFrame, contentInsets, appFrame,
+                    isFullScreen, isVoiceInteraction);
             if (a != null) {
                 if (DEBUG_ANIM) {
                     RuntimeException e = null;
