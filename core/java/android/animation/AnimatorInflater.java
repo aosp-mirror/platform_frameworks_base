7eaccbfbc25b772659536c096f6341927c38c470
Doris Liu <tianliu@google.com>
2015-04-22 23:18:38 +0000

    Remove distance based animation duration API
    Bug: 20500107
    
    This reverts commit 0084e370955cfa1efbf8ab447ac5e71a5529f5d3.
    
    Change-Id: I57f1f3a22c5adfefd966447050b100a50e7141b2

--- core/java/android/animation/AnimatorInflater.java
+++ core/java/android/animation/AnimatorInflater.java
@@ -70,13 +70,6 @@ public class AnimatorInflater {
     private static final int VALUE_TYPE_COLOR       = 3;
     private static final int VALUE_TYPE_UNDEFINED   = 4;
 
-    /**
-     * Enum values used in XML attributes to indicate the duration scale hint.
-     */
-    private static final int HINT_NO_SCALE                  = 0;
-    private static final int HINT_PROPORTIONAL_TO_SCREEN    = 1;
-    private static final int HINT_DEFINED_IN_DP             = 2;
-
     private static final boolean DBG_ANIMATOR_INFLATER = false;
 
     // used to calculate changing configs for resource references
@@ -698,9 +691,6 @@ public class AnimatorInflater {
                 int ordering = a.getInt(R.styleable.AnimatorSet_ordering, TOGETHER);
                 createAnimatorFromXml(res, theme, parser, attrs, (AnimatorSet) anim, ordering,
                         pixelSize);
-                final int hint = a.getInt(R.styleable.AnimatorSet_durationScaleHint,
-                        HINT_NO_SCALE);
-                anim.setDurationScaleHint(hint, res);
                 a.recycle();
             } else if (name.equals("propertyValuesHolder")) {
                 PropertyValuesHolder[] values = loadValues(res, theme, parser,
@@ -1065,9 +1055,6 @@ public class AnimatorInflater {
             anim.setInterpolator(interpolator);
         }
 
-        final int hint = arrayAnimator.getInt(R.styleable.Animator_durationScaleHint,
-                HINT_NO_SCALE);
-        anim.setDurationScaleHint(hint, res);
         arrayAnimator.recycle();
         if (arrayObjectAnimator != null) {
             arrayObjectAnimator.recycle();
--- core/java/android/animation/AnimatorSet.java
+++ core/java/android/animation/AnimatorSet.java
@@ -519,7 +519,6 @@ public final class AnimatorSet extends Animator {
 
         for (Node node : mNodes) {
             node.animation.setAllowRunningAsynchronously(false);
-            copyDurationScaleInfoTo(node.animation);
         }
 
         if (mDuration >= 0) {
