b4004dfc84b7c51519645020ea9431b051f5db62
Alan Viverette <alanv@google.com>
2015-04-29 16:55:42 -0700

    ActionBar SearchView's default hint shouldn't override SearchableInfo
    Moves the queryHint to defaultQueryHint, specifies the override order
    as queryHint > SearchableInfo > defaultQueryHint.
    
    Cleans up annotations and comments for several related methods.
    
    Bug: 20614122
    Change-Id: Ib58ec309e6814cd512df147d789ec0cd546018af

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -374,24 +374,30 @@ public abstract class Context {
     }
 
     /**
-     * Return a localized string from the application's package's
+     * Returns a localized string from the application's package's
      * default string table.
      *
      * @param resId Resource id for the string
+     * @return The string data associated with the resource, stripped of styled
+     *         text information.
      */
+    @NonNull
     public final String getString(@StringRes int resId) {
         return getResources().getString(resId);
     }
 
     /**
-     * Return a localized formatted string from the application's package's
+     * Returns a localized formatted string from the application's package's
      * default string table, substituting the format arguments as defined in
      * {@link java.util.Formatter} and {@link java.lang.String#format}.
      *
      * @param resId Resource id for the format string
-     * @param formatArgs The format arguments that will be used for substitution.
+     * @param formatArgs The format arguments that will be used for
+     *                   substitution.
+     * @return The string data associated with the resource, formatted and
+     *         stripped of styled text information.
      */
-
+    @NonNull
     public final String getString(@StringRes int resId, Object... formatArgs) {
         return getResources().getString(resId, formatArgs);
     }
