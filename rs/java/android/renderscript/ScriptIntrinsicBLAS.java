cecc00aba1012d4f19bc78fcd12ddcbccdd49182
Miao Wang <miaowang@google.com>
2015-04-29 18:14:55 -0700

    [RenderScript] L2 BLAS, fix element type in ZHER
    Change-Id: I99f9f9ff8942522d6581ff60b9a2d75e33e66ff6

--- rs/java/android/renderscript/ScriptIntrinsicBLAS.java
+++ rs/java/android/renderscript/ScriptIntrinsicBLAS.java
@@ -960,7 +960,7 @@ public final class ScriptIntrinsicBLAS extends ScriptIntrinsic {
     }
     public void ZHER(@Uplo int Uplo, double alpha, Allocation X, int incX, Allocation A) {
         // same as SYR
-        int N = validateSYR(Element.F64(mRS), Uplo, X, incX, A);
+        int N = validateSYR(Element.F64_2(mRS), Uplo, X, incX, A);
         mRS.nScriptIntrinsicBLAS_Z(getID(mRS), RsBlas_zher, 0, 0, 0, Uplo, 0, 0, N, 0, alpha, 0, X.getID(mRS), 0, 0, 0, A.getID(mRS), incX, 0, 0, 0);
     }
     public void ZHPR(@Uplo int Uplo, double alpha, Allocation X, int incX, Allocation Ap) {
