6b53fd28e5de1ef5263b4cffaf7c5b6d99005e76
longyu.huang <longyu.huang@ck-telecom.com>
2015-05-06 01:46:23 -0700

    optimize wallpaper load,avoid show black wallpaper.
    [Preconditions]
    open auto-rotate
    
    [Procedures]
    1.enter Contacts app, and rotate 90 degrees to the right
    2.press power key to lock screen,and unlock
    3.rotare 90 degrees to the left and exit Contacts app
    4.the wallpaper will be black first,then show the really wallpaper

--- packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
+++ packages/SystemUI/src/com/android/systemui/ImageWallpaper.java
@@ -236,7 +236,9 @@ public class ImageWallpaper extends WallpaperService {
                     Log.d(TAG, "Visibility changed to visible=" + visible);
                 }
                 mVisible = visible;
-                drawFrame();
+                if (visible) {
+                    drawFrame();
+                }
             }
         }
 
