0e6cac26585b04dc9a357b03cab09d93404819f1
Jason Monk <jmonk@google.com>
2015-05-13 16:01:16 -0400

    Add default icon for permissions
    Bug: 21119477
    Change-Id: Ie982a1b42d2195ab79c33cf87756efb3bd9eab29

--- packages/SettingsLib/src/com/android/settingslib/applications/PermissionsInfo.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/PermissionsInfo.java
@@ -22,7 +22,6 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.PermissionGroupInfo;
 import android.content.pm.PermissionInfo;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.UserHandle;
@@ -80,7 +79,8 @@ public class PermissionsInfo {
                 if (info.protectionLevel == PermissionInfo.PROTECTION_DANGEROUS) {
                     group = new PermissionGroup();
                     // TODO: Add default permission icon.
-                    group.icon = info.icon != 0 ? info.loadIcon(mPm) : new ShapeDrawable();
+                    group.icon = info.icon != 0 ? info.loadIcon(mPm) : mContext.getDrawable(
+                            com.android.internal.R.drawable.ic_perm_device_info);
                     group.name = info.name;
                     group.label = info.loadLabel(mPm).toString();
                     mGroups.add(group);
@@ -103,7 +103,8 @@ public class PermissionsInfo {
             for (PermissionGroupInfo groupInfo : groups) {
                 PermissionGroup group = new PermissionGroup();
                 // TODO: Add default permission icon.
-                group.icon = groupInfo.icon != 0 ? groupInfo.loadIcon(mPm) : new ShapeDrawable();
+                group.icon = groupInfo.icon != 0 ? groupInfo.loadIcon(mPm) : mContext.getDrawable(
+                        com.android.internal.R.drawable.ic_perm_device_info);
                 group.name = groupInfo.name;
                 group.label = groupInfo.loadLabel(mPm).toString();
                 synchronized (mGroups) {
