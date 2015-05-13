46d168fb6b50f0bb093f5c0428c8287f2e8d2d08
Todd Kennedy <toddke@google.com>
2015-05-13 11:13:58 -0700

    Don't negate saving state
    In the original implementation, we would only save the state if the
    activity was "!isFinishing()". That was abstracted into a method
    that returned whether we should save state. However, the "!" wasn't
    removed. D'oh.
    
    Bug: 20860094
    Change-Id: I8827cb265f2e7fb911f5d110c56e464aa092bfe7

--- core/java/android/app/FragmentManager.java
+++ core/java/android/app/FragmentManager.java
@@ -945,7 +945,7 @@ final class FragmentManagerImpl extends FragmentManager implements LayoutInflate
                         if (f.mView != null) {
                             // Need to save the current view state if not
                             // done already.
-                            if (!mHost.onShouldSaveFragmentState(f) && f.mSavedViewState == null) {
+                            if (mHost.onShouldSaveFragmentState(f) && f.mSavedViewState == null) {
                                 saveFragmentViewState(f);
                             }
                         }
