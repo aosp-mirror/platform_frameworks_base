d2aac451ba562e066112c5b01e0ebcb6c46375c4
Wale Ogunwale <ogunwale@google.com>
2015-05-14 12:20:53 -0700

    Revert "Update IME focus when the active view is losing the focus."
    This reverts commit 97c381304207013fd95c7806df4dcca0c69006c0.
    
    This causes issue with the right IME window getting focus.
    
    Bug: 21144633
    Change-Id: I4c75b6e7dd87c10f008444d2059164b52a8f4335

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -2014,6 +2014,7 @@ public final class ViewRootImpl implements ViewParent,
                 mLastWasImTarget = imTarget;
                 InputMethodManager imm = InputMethodManager.peekInstance();
                 if (imm != null && imTarget) {
+                    imm.startGettingWindowFocus(mView);
                     imm.onWindowFocus(mView, mView.findFocus(),
                             mWindowAttributes.softInputMode,
                             !mHasHadWindowFocus, mWindowAttributes.flags);
@@ -3320,6 +3321,10 @@ public final class ViewRootImpl implements ViewParent,
 
                     InputMethodManager imm = InputMethodManager.peekInstance();
                     if (mView != null) {
+                        if (hasWindowFocus && imm != null && mLastWasImTarget &&
+                                !isInLocalFocusMode()) {
+                            imm.startGettingWindowFocus(mView);
+                        }
                         mAttachInfo.mKeyDispatchState.reset();
                         mView.dispatchWindowFocusChanged(hasWindowFocus);
                         mAttachInfo.mTreeObserver.dispatchOnWindowFocusChange(hasWindowFocus);
