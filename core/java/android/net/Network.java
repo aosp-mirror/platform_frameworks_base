e1a6cf2caa628a02032be9b26462958f3769bd70
Erik Kline <ek@google.com>
2015-05-14 15:40:07 +0900

    Add a test that Network#getNetworkHandle() behaves sanely.
    Additionally:
        - make zero more obvious for debugging, rather than emitting
          some inscrutable magic value.
    
    Bug: 19537384
    Change-Id: Iac9a3297a0dda1ba3d69fd01cf6de81f01fd837e

--- core/java/android/net/Network.java
+++ core/java/android/net/Network.java
@@ -378,6 +378,9 @@ public class Network implements Parcelable {
         //
         // The HANDLE_MAGIC value MUST be kept in sync with the corresponding
         // value in the native/android/net.c NDK implementation.
+        if (netId == 0) {
+            return 0L;  // make this zero condition obvious for debugging
+        }
         final long HANDLE_MAGIC = 0xfacade;
         return (((long) netId) << 32) | HANDLE_MAGIC;
     }
