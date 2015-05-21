76976fae6fbe2f30ba209575557da153e29be33b
Jae Seo <jaeseo@google.com>
2015-05-20 21:51:16 -0700

    TIF: Build the list of available TV inputs as the current user
    Bug: 21313985
    Change-Id: I8aa881570915d14d63e4ac0b8034044f8736443b

--- services/core/java/com/android/server/tv/TvInputManagerService.java
+++ services/core/java/com/android/server/tv/TvInputManagerService.java
@@ -288,9 +288,10 @@ public final class TvInputManagerService extends SystemService {
 
         if (DEBUG) Slog.d(TAG, "buildTvInputList");
         PackageManager pm = mContext.getPackageManager();
-        List<ResolveInfo> services = pm.queryIntentServices(
+        List<ResolveInfo> services = pm.queryIntentServicesAsUser(
                 new Intent(TvInputService.SERVICE_INTERFACE),
-                PackageManager.GET_SERVICES | PackageManager.GET_META_DATA);
+                PackageManager.GET_SERVICES | PackageManager.GET_META_DATA,
+                userId);
         List<TvInputInfo> inputList = new ArrayList<TvInputInfo>();
         for (ResolveInfo ri : services) {
             ServiceInfo si = ri.serviceInfo;
