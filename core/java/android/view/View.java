b010291ace13947fcc76b1a64eaa67880e15f0a6
Filip Gruszczynski <gruszczy@google.com>
2015-06-03 10:32:26 -0700

    Fix NPE when requesting outsets for detached view.
    Outsets are used during measure/layout pass, but this can be called on a
    view that is not currently attached.
    
    Bug: 21602590
    Change-Id: I23e3acc45ca4bf7684d8913f839e29e8e9e94d78
    (cherry picked from commit e660d4e96a2e5ac35f2dd6e2b43c588dd7e28358)

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -6672,7 +6672,11 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
      * @hide
      */
     public void getOutsets(Rect outOutsetRect) {
-        outOutsetRect.set(mAttachInfo.mOutsets);
+        if (mAttachInfo != null) {
+            outOutsetRect.set(mAttachInfo.mOutsets);
+        } else {
+            outOutsetRect.setEmpty();
+        }
     }
 
     /**
@@ -13566,7 +13570,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
             }
         }
 
-        onDetachedFromWindow();
+        onDetachedFromWindows();
         onDetachedFromWindowInternal();
 
         ListenerInfo li = mListenerInfo;
