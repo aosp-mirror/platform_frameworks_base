1b41cc9567ebe7fc2217357895d1ac17aa6d3bf6
Aravind Akella <aakella@google.com>
2015-06-10 15:21:03 -0700

    Use LOCATION_HARDWARE permission for data injection.
    Change-Id: I59c1e1e97a6a0457f6fc2984b214647814895cf2

--- core/java/android/hardware/SensorManager.java
+++ core/java/android/hardware/SensorManager.java
@@ -1570,7 +1570,7 @@ public abstract class SensorManager {
      * Significant Motion, Step Counter etc.
      *
      * The tests which call this API need to have {@code
-     * android.permission.HARDWARE_TEST} permission which isn't
+     * android.permission.LOCATION_HADWARE} permission which isn't
      * available for third party applications.
      *
      * @param enable True to set the HAL in DATA_INJECTION mode.
@@ -1600,7 +1600,7 @@ public abstract class SensorManager {
      * the HAL is already in data injection mode.
      *
      * The tests which call this API need to have {@code
-     * android.permission.HARDWARE_TEST} permission which isn't
+     * android.permission.LOCATION_HARDWARE} permission which isn't
      * available for third party applications.
      *
      * @param sensor The sensor to inject.
