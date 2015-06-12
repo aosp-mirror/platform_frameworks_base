3dcb9945fd0d9e8ccea41eb7cc2b5c7a13bf5185
Chet Haase <chet@google.com>
2015-06-12 13:25:27 +0100

    Remove View.getBackgroundColor()
    This method was added earlier in M - removing it now based on API
    Council feedback.
    
    Issue #21572678  API Review: android.view.View.getBackgroundColor()
    
    Change-Id: I04fad49c3c377218d0fb708dbeaea609aca46ce8

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -17175,21 +17175,6 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
     }
 
     /**
-     * If the view has a ColorDrawable background, returns the color of that
-     * drawable.
-     *
-     * @return The color of the ColorDrawable background, if set, otherwise 0.
-     * @hide
-     */
-    @ColorInt
-    public int getBackgroundColor() {
-        if (mBackground instanceof ColorDrawable) {
-            return ((ColorDrawable) mBackground).getColor();
-        }
-        return 0;
-    }
-
-    /**
      * Set the background to a given resource. The resource should refer to
      * a Drawable object or 0 to remove the background.
      * @param resid The identifier of the resource.
