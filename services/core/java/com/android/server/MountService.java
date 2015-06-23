8101ee6884e9fe954aeaf3c08afd01ab1714f306
Todd Kennedy <toddke@google.com>
2015-06-23 13:35:28 -0700

    Handle volume benchmark timeout
    If a volume benchmark operation times out, we don't want to show
    a cryptic toast message. Instead, we return a very large integer
    [eg Long.MAX_INT]. The storage wizard can then use this value
    to show an appropriate dialog if it chooses.
    
    Bug: 21376364
    Change-Id: I3d97336e19c93511cfff2cbdb2f07ab033a1143d

--- services/core/java/com/android/server/MountService.java
+++ services/core/java/com/android/server/MountService.java
@@ -1491,6 +1491,8 @@ class MountService extends IMountService.Stub
         try {
             final NativeDaemonEvent res = mConnector.execute("volume", "benchmark", volId);
             return Long.parseLong(res.getMessage());
+        } catch (NativeDaemonTimeoutException e) {
+            return Long.MAX_VALUE;
         } catch (NativeDaemonConnectorException e) {
             throw e.rethrowAsParcelableException();
         }
