16add46c5c4575ae28af9ef421eee0a76b07fa77
David Friedman <dmail@google.com>
2015-06-30 23:36:33 -0700

    Docs: Adding localized TOC for Preview docs.
    This change is associated with Bug 21789313 and CL 718631.
    
    Change-Id: Ifb5db3d80fe01cc3f99000b812f9570777c39228

--- docs/html/preview/preview_toc.cs
+++ docs/html/preview/preview_toc.cs
@@ -1,9 +1,17 @@
 <ul id="nav">
 
   <li class="nav-section">
-    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/overview.html">
-      Program Overview</a></div>
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/overview.html"
+   es-lang="Información general del programa"
+   ja-lang="プログラム概要"
+   ko-lang="프로그램 개요"
+   pt-br-lang="Visão geral do programa"
+   ru-lang="Обзор программы"
+   zh-cn-lang="计划概览"
+   zh-tw-lang="程式總覽">Program Overview
+      </a></div>
   </li>
+
 
   <li class="nav-section">
     <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/download.html">
@@ -25,11 +33,25 @@
   </li>
 
   <li class="nav-section">
-    <div class="nav-section-header"><a href="<?cs var:toroot ?>preview/api-overview.html">
-      API Overview</a></div>
+    <div class="nav-section-header"><a href="<?cs var:toroot ?>preview/api-overview.html"
+      es-lang="Información general de la API"
+      ja-lang="API の概要"
+      ko-lang="API 개요"
+      pt-br-lang="Visão geral da API"
+      ru-lang="Обзор API-интерфейсов"
+      zh-cn-lang="API 概览"
+      zh-tw-lang="API 總覽">API Overview
+      </a></div>
       <ul>
-        <li><a href="<?cs var:toroot ?>preview/features/runtime-permissions.html">
-          Permissions</a></li>
+        <li><a href="<?cs var:toroot ?>preview/features/runtime-permissions.html"
+      es-lang="Permisos"
+      ja-lang="パーミッション"
+      ko-lang="권한"
+      pt-br-lang="Permissões"
+      ru-lang="Разрешения"
+      zh-cn-lang="权限"
+      zh-tw-lang="權限">Permissions
+          </a></li>
         <li><a href="<?cs var:toroot ?>preview/features/app-linking.html">
           App Links</a></li>
         <li><a href="<?cs var:toroot ?>preview/backup/index.html">
@@ -38,8 +60,15 @@
   </li>
 
   <li class="nav-section">
-    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/behavior-changes.html">
-      Behavior Changes</a></div>
+    <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/behavior-changes.html"
+      es-lang="Cambios en los comportamientos"
+      ja-lang="動作の変更点"
+      ko-lang="동작 변경"
+      pt-br-lang="Mudanças de comportamento"
+      ru-lang="Изменения в работе"
+      zh-cn-lang="行为变更"
+      zh-tw-lang="行為變更">Behavior Changes
+       </a></div>
   </li>
   <li class="nav-section">
     <div class="nav-section-header empty"><a href="<?cs var:toroot ?>preview/samples.html">
