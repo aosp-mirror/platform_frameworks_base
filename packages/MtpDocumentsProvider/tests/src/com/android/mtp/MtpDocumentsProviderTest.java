d5152429e11d3a831a7993dc67ddf69f69bd3912
Daichi Hirono <hirono@google.com>
2015-07-15 13:31:51 +0900

    Add openDevice and closeDevice methods to MtpDocumentsProvider.
    These methods will be called in the following CL from the service that handles
    USB attach and detach intent.
    
    BUG=20274999
    
    Change-Id: I7b3c658afc5750d6a2713c07f40c59b26dcd1460

--- packages/MtpDocumentsProvider/tests/src/com/android/mtp/MtpDocumentsProviderTest.java
+++ packages/MtpDocumentsProvider/tests/src/com/android/mtp/MtpDocumentsProviderTest.java
@@ -16,13 +16,92 @@
 
 package com.android.mtp;
 
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.test.AndroidTestCase;
+import android.test.mock.MockContentResolver;
 import android.test.suitebuilder.annotation.SmallTest;
+
+import java.io.IOException;
 
 @SmallTest
 public class MtpDocumentsProviderTest extends AndroidTestCase {
-    public void testBasic() {
+    public void testOpenAndCloseDevice() throws Exception {
+        final ContentResolver resolver = new ContentResolver();
         final MtpDocumentsProvider provider = new MtpDocumentsProvider();
-        assertNotNull(provider);
+        provider.onCreateForTesting(new MtpManagerMock(getContext()), resolver);
+
+        provider.openDevice(MtpManagerMock.SUCCESS_DEVICE_ID);
+        assertEquals(1, resolver.changeCount);
+        provider.closeDevice(MtpManagerMock.SUCCESS_DEVICE_ID);
+        assertEquals(2, resolver.changeCount);
+
+        provider.openDevice(MtpManagerMock.FAILURE_DEVICE_ID);
+        assertEquals(2, resolver.changeCount);
+        provider.closeDevice(MtpManagerMock.FAILURE_DEVICE_ID);
+        assertEquals(2, resolver.changeCount);
+    }
+
+    public void testCloseAllDevices() {
+        final ContentResolver resolver = new ContentResolver();
+        final MtpDocumentsProvider provider = new MtpDocumentsProvider();
+        provider.onCreateForTesting(new MtpManagerMock(getContext()), resolver);
+
+        provider.closeAllDevices();
+        assertEquals(0, resolver.changeCount);
+
+        provider.openDevice(MtpManagerMock.SUCCESS_DEVICE_ID);
+        assertEquals(1, resolver.changeCount);
+
+        provider.closeAllDevices();
+        assertEquals(2, resolver.changeCount);
+    }
+
+    private static class MtpManagerMock extends MtpManager {
+        final static int SUCCESS_DEVICE_ID = 1;
+        final static int FAILURE_DEVICE_ID = 2;
+
+        private boolean opened = false;
+
+        MtpManagerMock(Context context) {
+            super(context);
+        }
+
+        @Override
+        void openDevice(int deviceId) throws IOException {
+            if (deviceId == SUCCESS_DEVICE_ID) {
+                opened = true;
+            } else {
+                throw new IOException();
+            }
+        }
+
+        @Override
+        void closeDevice(int deviceId) throws IOException {
+            if (opened && deviceId == SUCCESS_DEVICE_ID) {
+                opened = false;
+            } else {
+                throw new IOException();
+            }
+        }
+
+        @Override
+        int[] getOpenedDeviceIds() {
+            if (opened) {
+                return new int[] { SUCCESS_DEVICE_ID };
+            } else {
+                return new int[0];
+            }
+        }
+    }
+
+    private static class ContentResolver extends MockContentResolver {
+        int changeCount = 0;
+
+        @Override
+        public void notifyChange(Uri uri, ContentObserver observer, boolean syncToNetwork) {
+            changeCount++;
+        }
     }
 }
