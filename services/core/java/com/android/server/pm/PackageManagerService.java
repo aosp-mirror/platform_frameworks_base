9dacbf6fd4b602f3abe9b1a347690d474c54f9a7
Benjamin Franz <bfranz@google.com>
2015-07-30 15:50:16 +0100

    Add missing return statement
    Bug: 22844272
    Change-Id: I4b37b28adb1d1b6d0d1235dd68fbfad6c3121c54

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -9587,6 +9587,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                 killApplication(packageName, UserHandle.getUid(userId, pkgSetting.appId),
                         "hiding pkg");
                 sendApplicationHiddenForUser(packageName, pkgSetting, userId);
+                return true;
             }
         } finally {
             Binder.restoreCallingIdentity(callingId);
