aa4f4bf6ce721a60b518fa42159cc4fe06c9e012
Paul Soulos <psoulos@google.com>
2015-08-04 11:59:45 -0700

    Fix doc typo for default wearable notification size.
    Change-Id: Ic3428278eee0b541de3e1b355692401170d1ab3b

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -4607,7 +4607,7 @@ public class Notification implements Parcelable
          * Size value for use with {@link #setCustomSizePreset} to show this notification with
          * default sizing.
          * <p>For custom display notifications created using {@link #setDisplayIntent},
-         * the default is {@link #SIZE_LARGE}. All other notifications size automatically based
+         * the default is {@link #SIZE_MEDIUM}. All other notifications size automatically based
          * on their content.
          */
         public static final int SIZE_DEFAULT = 0;
