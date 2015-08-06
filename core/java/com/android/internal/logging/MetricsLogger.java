a35c96eb90f3be5980797b160da3780f2b8f35e6
Chris Wren <cwren@android.com>
2015-08-06 14:53:32 -0400

    add metrics to assistant
      long press
      screenshot enabled
      context enabled
    
    Bug: 22949579
    Change-Id: Iac178207fa7149cda03c9a817a0d1450e46e1c59

--- core/java/com/android/internal/logging/MetricsLogger.java
+++ core/java/com/android/internal/logging/MetricsLogger.java
@@ -28,6 +28,7 @@ import android.view.View;
 public class MetricsLogger implements MetricsConstants {
     // Temporary constants go here, to await migration to MetricsConstants.
     // next value is 239;
+    public static final int ACTION_ASSIST_LONG_PRESS = 239;
 
     public static void visible(Context context, int category) throws IllegalArgumentException {
         if (Build.IS_DEBUGGABLE && category == VIEW_UNKNOWN) {
