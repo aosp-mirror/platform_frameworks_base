f9273e185b186984bfcfe7994de60d51cb4eefc4
Roozbeh Pournader <roozbeh@google.com>
2015-08-07 18:30:37 -0700

    Remove legacy font configuration files.
    They were no longer in use by frameworks. They were never part of a
    public API and were kept in the system just in case a legacy app read
    them. They had also been deprecated for three releases, which should
    have given everybody enough time to switch.
    
    Bug: 21785593
    Change-Id: Ib80bcea92e45b608388455ef576bbfde647f77db

--- data/fonts/fonts.mk
+++ data/fonts/fonts.mk
@@ -15,8 +15,6 @@
 # Warning: this is actually a product definition, to be inherited from
 
 PRODUCT_COPY_FILES := \
-    frameworks/base/data/fonts/system_fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/system_fonts.xml \
-    frameworks/base/data/fonts/fallback_fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fallback_fonts.xml \
     frameworks/base/data/fonts/fonts.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/fonts.xml
 
 PRODUCT_PACKAGES := \
