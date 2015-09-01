d68e8cb51bc41bdc7567ddcee92fb80d9b47204e
Md Haque <haque@google.com>
2015-09-01 12:00:34 -0700

    Adding recyclerview and activity transition to UiBench
    Change-Id: Ief31c6ed7f6b798893256f79b41e8a6ccbc3f3c7

--- tests/UiBench/Android.mk
+++ tests/UiBench/Android.mk
@@ -11,17 +11,20 @@ LOCAL_SRC_FILES := $(call all-java-files-under,src)
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     frameworks/support/v7/appcompat/res \
-    frameworks/support/v7/cardview/res
+    frameworks/support/v7/cardview/res \
+    frameworks/support/v7/recyclerview/res
 
 LOCAL_AAPT_FLAGS := \
     --auto-add-overlay \
     --extra-packages android.support.v7.appcompat \
-    --extra-packages android.support.v7.cardview
+    --extra-packages android.support.v7.cardview \
+    --extra-packages android.support.v7.recyclerview
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
     android-support-v7-appcompat \
-    android-support-v7-cardview
+    android-support-v7-cardview \
+    android-support-v7-recyclerview
 
 LOCAL_PACKAGE_NAME := UiBench
 
--- tests/UiBench/src/com/android/test/uibench/listview/CompatListActivity.java
+++ tests/UiBench/src/com/android/test/uibench/listview/CompatListActivity.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.test.uibench;
+package com.android.test.uibench.listview;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
