acddf2b34965da479efe46bd87871234ac6a5aab
Joe LaPenna <jlapenna@google.com>
2015-09-04 12:52:42 -0700

    Do not wtf when the systemui is not present.
    Not all devices, like wearables actually have a system ui.
    
    BUG: 23822226
    
    Change-Id: Ibc2d6c3b8db1de668b9cbced3efcec5a7a39f20b

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -273,7 +273,8 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
             sysUiUid = mContext.getPackageManager().getPackageUid("com.android.systemui",
                     UserHandle.USER_OWNER);
         } catch (PackageManager.NameNotFoundException e) {
-            Log.wtf(TAG, "Unable to resolve SystemUI's UID.", e);
+            // Some platforms, such as wearables do not have a system ui.
+            Log.w(TAG, "Unable to resolve SystemUI's UID.", e);
         }
         mSystemUiUid = sysUiUid;
     }
