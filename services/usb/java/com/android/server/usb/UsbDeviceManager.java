e753361508a51ecea4bc2c84ffba68fd5a8cdd4b
Gus Prevas <kprevas@google.com>
2015-09-09 16:10:54 -0400

    Adds config flag for USB notification.
    This change modifies UsbDeviceManager such that the ongoing
    system notification for USB charging state is controlled by a
    config flag.
    
    Bug: 23409719
    Change-Id: I2ef24fe74923170a6e8dd02375b973b4025281e4

--- services/usb/java/com/android/server/usb/UsbDeviceManager.java
+++ services/usb/java/com/android/server/usb/UsbDeviceManager.java
@@ -233,7 +233,8 @@ public class UsbDeviceManager {
         final StorageManager storageManager = StorageManager.from(mContext);
         final StorageVolume primary = storageManager.getPrimaryVolume();
         massStorageSupported = primary != null && primary.allowMassStorage();
-        mUseUsbNotification = !massStorageSupported;
+        mUseUsbNotification = !massStorageSupported && mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_usbChargingMessage);
 
         // make sure the ADB_ENABLED setting value matches the current state
         try {
