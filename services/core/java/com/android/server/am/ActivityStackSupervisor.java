13a9b16dc7a331687084dd3722b3134f0d928cb1
Wale Ogunwale <ogunwale@google.com>
2015-09-22 21:38:51 -0700

    Only place new task in focus stack if it is full screen stack.
    The fullscreen stack is the only stack that can contain any task
    regardless of if the task is resizeable or not. So, we don't want
    to place any random new task in a non-fullscreen stack that has
    focus as you can end up with issue like a non-resizeable task
    ending up in the freeform stack and gets resized and the window
    decor.
    
    Bug: 23626353
    Change-Id: Id2f151449f612b4f81ff85b42c375254871f6ec2

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -1803,8 +1803,11 @@ public final class ActivityStackSupervisor implements DisplayListener {
                 return container.mStack;
             }
 
-            if (mFocusedStack != mHomeStack && (!newTask ||
-                    mFocusedStack.mActivityContainer.isEligibleForNewTasks())) {
+            // The fullscreen stack is the only stack that can contain any task regardless of if
+            // the task is resizeable or not. So, we let the task go in the fullscreen task if it
+            // is the focus stack.
+            if (mFocusedStack.mStackId == FULLSCREEN_WORKSPACE_STACK_ID
+                    && (!newTask || mFocusedStack.mActivityContainer.isEligibleForNewTasks())) {
                 if (DEBUG_FOCUS || DEBUG_STACK) Slog.d(TAG_FOCUS,
                         "computeStackFocus: Have a focused stack=" + mFocusedStack);
                 return mFocusedStack;
@@ -1823,7 +1826,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
             }
 
             // If there is no suitable dynamic stack then we figure out which static stack to use.
-            int stackId = task != null ? task.getLaunchStackId() :
+            final int stackId = task != null ? task.getLaunchStackId() :
                         bounds != null ? FREEFORM_WORKSPACE_STACK_ID :
                                          FULLSCREEN_WORKSPACE_STACK_ID;
             stack = getStack(stackId, CREATE_IF_NEEDED, ON_TOP);
