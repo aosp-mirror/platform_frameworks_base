7353d32d42fd89ad3f181ac895810d0eb1e4910e
Chet Kener <Cl3Kener@gmail.com>
2015-10-06 11:26:15 -0400

    appt: Fix Compile
    * We were missing this function
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- tools/aapt/Main.cpp
+++ tools/aapt/Main.cpp
@@ -224,7 +224,10 @@ void usage(void)
         "   --no-version-vectors\n"
         "       Do not automatically generate versioned copies of vector XML resources.\n"
         "   --private-symbols\n"
-        "       Java package name to use when generating R.java for private resources.\n",
+        "       Java package name to use when generating R.java for private resources.\n"
+        "   --compress\n"
+        "       Specifies if aapt should prefer compression or not.\n"
+        "       Possible values are 0(Stored) and 1(Deflated).\n",
         gDefaultIgnoreAssets);
 }
 
@@ -714,6 +717,23 @@ int main(int argc, char* const argv[])
                         goto bail;
                     }
                     bundle.setPrivateSymbolsPackage(String8(argv[0]));
+                } else if (strcmp(cp, "-compress") == 0) {
+                    argc--;
+                    argv++;
+                    if (!argc) {
+                        fprintf(stderr, "ERROR: No argument supplied for '--compress' option\n");
+                        wantUsage = true;
+                        goto bail;
+                    }
+                    if (strcmp(argv[0], "0") == 0) {
+                        bundle.setCompressionMethod(ZipEntry::kCompressStored);
+                    } else if (strcmp(argv[0], "1") == 0) {
+                        bundle.setCompressionMethod(ZipEntry::kCompressDeflated);
+                    } else {
+                        fprintf(stderr, "ERROR: Invalid argument supplied for '--compress' option\n");
+                        wantUsage = true;
+                        goto bail;
+                    }
                 } else {
                     fprintf(stderr, "ERROR: Unknown option '-%s'\n", cp);
                     wantUsage = true;
