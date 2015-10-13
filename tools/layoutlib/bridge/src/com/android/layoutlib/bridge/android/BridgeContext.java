e8a04cd5a68a5e5aa78a4f40b3269c86cc3f152f
Ian Pedowitz <ijpedowitz@google.com>
2015-10-12 19:19:09 -0700

    Fix Build
    Mimicking what was done for bdbd2d9c1878432eca6933e06c980663e2579d85
    
    Change-Id: I5aef245b7a426edaa90e7e237acd4608b431c073

--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgeContext.java
@@ -73,6 +73,7 @@ import android.os.Looper;
 import android.os.Parcel;
 import android.os.PowerManager;
 import android.os.RemoteException;
+import android.os.ResultReceiver;
 import android.os.UserHandle;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -1129,6 +1130,11 @@ public final class BridgeContext extends Context {
                 @Override
                 public boolean unlinkToDeath(DeathRecipient recipient, int flags) {
                     return false;
+                }
+
+                @Override
+                public void shellCommand(FileDescriptor in, FileDescriptor out, FileDescriptor err,
+                  String[] args, ResultReceiver resultReceiver) {
                 }
             };
         }
