c0f39c1ece72a05c796f7ba30b7a2b5b580d5025
Matthew Williams <mjwilliams@google.com>
2015-10-16 12:01:31 -0700

    Sync extras bundle comparison can throw NPE
    BUG: 23591205
    Change-Id: Ic6404c0befe70c34b078e0eae6a627826173d82c
    (cherry picked from commit 9ad2c8403354a985258c098681067e74b9e2f638)

--- services/core/java/com/android/server/content/SyncManager.java
+++ services/core/java/com/android/server/content/SyncManager.java
@@ -100,6 +100,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Random;
 import java.util.Set;
 
@@ -3194,7 +3195,7 @@ public class SyncManager {
             if (!smaller.containsKey(key)) {
                 return false;
             }
-            if (!bigger.get(key).equals(smaller.get(key))) {
+            if (!Objects.equals(bigger.get(key), smaller.get(key))) {
                 return false;
             }
         }
@@ -3202,7 +3203,6 @@ public class SyncManager {
     }
 
     /**
-     * TODO: Get rid of this when we separate sync settings extras from dev specified extras.
      * @return true if the provided key is used by the SyncManager in scheduling the sync.
      */
     private static boolean isSyncSetting(String key) {
