2ee19c7f46ab488f19380f4c70904a1ac00fee9a
Todd Kennedy <toddke@google.com>
2015-10-19 15:31:51 -0700

    Retain fragment loaders
    Remove the partial fix [it did not work for child fragment managers]
    and replace with a more general fix that works with all fragments.
    
    Bug: 23838271
    Change-Id: I88b465f6a06a6ad627b9651b9e2eea41fae08972

--- core/java/android/app/Fragment.java
+++ core/java/android/app/Fragment.java
@@ -460,9 +460,6 @@ public class Fragment implements ComponentCallbacks2, OnCreateContextMenuListene
     // If set this fragment is being retained across the current config change.
     boolean mRetaining;
 
-    // If set this fragment's loaders are being retained across the current config change.
-    boolean mRetainLoader;
-
     // If set this fragment has menu items to contribute.
     boolean mHasMenu;
 
@@ -2404,7 +2401,7 @@ public class Fragment implements ComponentCallbacks2, OnCreateContextMenuListene
                 mLoaderManager = mHost.getLoaderManager(mWho, mLoadersStarted, false);
             }
             if (mLoaderManager != null) {
-                if (mRetainLoader) {
+                if (mHost.getRetainLoaders()) {
                     mLoaderManager.doRetain();
                 } else {
                     mLoaderManager.doStop();
