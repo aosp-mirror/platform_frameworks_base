02ed01297d4c12b07570397e4a664c785a687853
riddle_hsu <riddle_hsu@htc.com>
2015-10-20 16:00:15 +0800

    MediaSession: Use Handler for sending a pending intent
    When PendingIntent.send() is called with a null handler,
    onSendFinished() will be called inside of the send() method.
    Therefore in this case, if a thread holds a lock before
    calling send(), and onSendFinished() also requires the lock,
    a deadlock could happen.
    
    Bug: 25653801
    Change-Id: I71d9f708fc3d52b7b5713426ad6140cbf6e336ef

--- services/core/java/com/android/server/media/MediaSessionService.java
+++ services/core/java/com/android/server/media/MediaSessionService.java
@@ -967,7 +967,7 @@ public class MediaSessionService extends SystemService implements Monitor {
                         if (user.mLastMediaButtonReceiver != null) {
                             user.mLastMediaButtonReceiver.send(getContext(),
                                     needWakeLock ? mKeyEventReceiver.mLastTimeoutId : -1,
-                                    mediaButtonIntent, mKeyEventReceiver, null);
+                                    mediaButtonIntent, mKeyEventReceiver, mHandler);
                         } else {
                             mediaButtonIntent.setComponent(user.mRestoredMediaButtonReceiver);
                             getContext().sendBroadcastAsUser(mediaButtonIntent,
