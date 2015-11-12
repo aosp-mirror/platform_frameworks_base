fe9823f64cb41e8aed8ab61417acb57f6c588e0c
Filip Gruszczynski <gruszczy@google.com>
2015-11-12 14:09:26 -0800

    Fix recents to freeform animation.
    We should use freeform animation spec not only when the task is already
    freeform, but also when it is entering freeform.
    
    Bug: 25669457
    Change-Id: I30cba79a149183b0a8b9e1e4751a215cef6a490f

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsTransitionHelper.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsTransitionHelper.java
@@ -270,7 +270,7 @@ public class RecentsTransitionHelper {
         int taskCount = tasks.size();
         for (int i = taskCount - 1; i >= 0; i--) {
             Task t = tasks.get(i);
-            if (t.isFreeformTask()) {
+            if (t.isFreeformTask() || targetStackId == FREEFORM_WORKSPACE_STACK_ID) {
                 TaskView tv = stackView.getChildViewForTask(t);
                 if (tv == null) {
                     // TODO: Create a different animation task rect for this case (though it should
