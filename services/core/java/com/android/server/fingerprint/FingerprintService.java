1e24c428efa7b0836154fa909bdcf2d3e7210462
Amith Yamasani <yamasani@google.com>
2015-11-20 09:44:08 -0800

    Fix missing observer reply callbacks
    This was causing upto 2 seconds in user switching delay.
    
    Bug: 25813588
    Change-Id: Icf23b021f134a8ccfa2dae470af02b2da7454acc

--- services/core/java/com/android/server/fingerprint/FingerprintService.java
+++ services/core/java/com/android/server/fingerprint/FingerprintService.java
@@ -1139,6 +1139,12 @@ public class FingerprintService extends SystemService implements IBinder.DeathRe
                     public void onUserSwitching(int newUserId, IRemoteCallback reply) {
                         mHandler.obtainMessage(MSG_USER_SWITCHING, newUserId, 0 /* unused */)
                                 .sendToTarget();
+                        if (reply != null) {
+                            try {
+                                reply.sendResult(null);
+                            } catch (RemoteException e) {
+                            }
+                        }
                     }
                     @Override
                     public void onUserSwitchComplete(int newUserId) throws RemoteException {
