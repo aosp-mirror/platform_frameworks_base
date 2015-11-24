a21136c347d1ef34ec65283a39760a039d1d77d9
Filip Gruszczynski <gruszczy@google.com>
2015-11-24 14:43:31 -0800

    Don't preserve the window on client side when pinning activity.
    We were both enabling window replacement in WMS and window preserving on
    the client side. This doesn't work together, because window replacement
    depends on the fact that the client won't preserve the window (it needs
    to receive remove and add window operations).
    
    Bug: 25854235
    Change-Id: I0625df6fbe4c29bf14f364d764de3bf63dc90961

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -3412,7 +3412,7 @@ public final class ActivityStackSupervisor implements DisplayListener {
             pinnedStack.moveActivityToStack(r);
         }
 
-        resizeStackLocked(PINNED_STACK_ID, bounds, PRESERVE_WINDOWS, true);
+        resizeStackLocked(PINNED_STACK_ID, bounds, !PRESERVE_WINDOWS, true);
 
         // The task might have already been running and its visibility needs to be synchronized with
         // the visibility of the stack / windows.
