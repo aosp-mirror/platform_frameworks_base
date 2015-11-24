3f6a3917329b0caed7f6d0c677fbdbd5aca09b44
Tom Marshall <tdm@cyngn.com>
2015-11-24 15:28:43 -0800

    storage: Do not notify for volumes on non-removable disks
    Change-Id: Ia353ab43fbeba42d7d9c21d926f638f550a3dbbe

--- core/java/android/os/storage/DiskInfo.java
+++ core/java/android/os/storage/DiskInfo.java
@@ -50,6 +50,8 @@ public class DiskInfo implements Parcelable {
     public static final int FLAG_DEFAULT_PRIMARY = 1 << 1;
     public static final int FLAG_SD = 1 << 2;
     public static final int FLAG_USB = 1 << 3;
+    public static final int FLAG_EMMC = 1 << 4;
+    public static final int FLAG_NON_REMOVABLE = 1 << 5;
 
     public final String id;
     @UnsupportedAppUsage
@@ -150,6 +152,10 @@ public class DiskInfo implements Parcelable {
     @UnsupportedAppUsage
     public boolean isUsb() {
         return (flags & FLAG_USB) != 0;
+    }
+
+    public boolean isNonRemovable() {
+        return (flags & FLAG_NON_REMOVABLE) != 0;
     }
 
     @Override
