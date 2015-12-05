0894319162a2a1f4260805938d4807c4ffcbcb0f
Yohei Yukawa <yukawa@google.com>
2015-12-04 16:16:47 -0800

    Add subtypeId for SpellCheckerSubtype.
    What this CL actually does is just copying the existing concept
    "subtypeId" from InputMethodSubtype to SpellCheckerSubtype.
    
    To recap, the underlying problem is that the system has stored only the
    return value of SpellCheckerSubtype#hashCode() to track the set of
    enabled subtypes, and SpellCheckerSubtype#hashCode() has been
    implemented as Arrays.hashCode(new Object[] {locale, extraValue}), which
    is problematic because:
      - Spell checker developers cannot change "locale" and/or 'extraValue'
        if they want to keep enabled subtypes enabled.
      - Android Framework developers cannot change the hash function even
        when new fields are added if they want to keep enabled subtypes
        enabled.
    InputMethodSubtype has had the same issue, and what we did was
    introducing a concept "subtypeId", which allows IME developers to
    specify the return value of #hashCode().
    
    For instance, suppose that a subtype X has already been used in
    production with the following attributes:
      - locale: "tl_PH"
      - extraValues: "key1=value1,key2=value2"
    
    With "subtypeId", you can change the attributes of subtype X without
    losing the enabled state of subtype X on devices as follows.
      - locale: "fil_PH"
      - extraValues: "key1=value1,key2=value2,key3=value3"
      - subtypeId: Arrays.hashCode(new Object[] {
                "tl_PH", "key1=value1,key2=value2"})
    
    This CL also deprecates existing public constructor of
    SpellCheckerSubtype, which was probably published as a public API by
    mistake.  Note that the constructor of SpellCheckerInfo class is @hide.
    Also there is no public API that receives SpellCheckerSubtype object
    instantiated by developers with custom data.  Making developers to be
    able to instantiate SpellCheckerSubtype does not make sense right now.
    
    Bug: 11736916
    Bug: 22858221
    Change-Id: I98f05c1e9421c47a93769bc4a6fe11b678bc2509

--- core/tests/coretests/src/android/view/textservice/SpellCheckerSubtypeTest.java
+++ core/tests/coretests/src/android/view/textservice/SpellCheckerSubtypeTest.java
@@ -29,12 +29,15 @@ import static android.test.MoreAsserts.assertNotEqual;
  * TODO: Most of part can be, and probably should be, moved to CTS.
  */
 public class SpellCheckerSubtypeTest extends InstrumentationTestCase {
+    private static final int SUBTYPE_SUBTYPE_ID_NONE = 0;
     private static final String SUBTYPE_SUBTYPE_LOCALE_STRING_A = "en_GB";
     private static final int SUBTYPE_NAME_RES_ID_A = 0x12345;
     private static final String SUBTYPE_EXTRA_VALUE_A = "Key1=Value1,Key2=Value2";
+    private static final int SUBTYPE_SUBTYPE_ID_A = 42;
     private static final String SUBTYPE_SUBTYPE_LOCALE_STRING_B = "en_IN";
     private static final int SUBTYPE_NAME_RES_ID_B = 0x54321;
     private static final String SUBTYPE_EXTRA_VALUE_B = "Key3=Value3,Key4=Value4";
+    private static final int SUBTYPE_SUBTYPE_ID_B = -42;
 
     private static int defaultHashCodeAlgorithm(String locale, String extraValue) {
         return Arrays.hashCode(new Object[] {locale, extraValue});
@@ -55,10 +58,9 @@ public class SpellCheckerSubtypeTest extends InstrumentationTestCase {
     }
 
     @SmallTest
-    public void testSubtype() throws Exception {
+    public void testSubtypeWithNoSubtypeId() throws Exception {
         final SpellCheckerSubtype subtype = new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A,
-                SUBTYPE_SUBTYPE_LOCALE_STRING_A, SUBTYPE_EXTRA_VALUE_A);
-
+                SUBTYPE_SUBTYPE_LOCALE_STRING_A, SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE);
         assertEquals(SUBTYPE_NAME_RES_ID_A, subtype.getNameResId());
         assertEquals(SUBTYPE_SUBTYPE_LOCALE_STRING_A, subtype.getLocale());
         assertEquals("Value1", subtype.getExtraValueOf("Key1"));
@@ -78,6 +80,26 @@ public class SpellCheckerSubtypeTest extends InstrumentationTestCase {
         assertEquals(
                 defaultHashCodeAlgorithm(SUBTYPE_SUBTYPE_LOCALE_STRING_A, SUBTYPE_EXTRA_VALUE_A),
                 clonedSubtype.hashCode());
+    }
+
+    public void testSubtypeWithSubtypeId() throws Exception {
+        final SpellCheckerSubtype subtype = new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A,
+                SUBTYPE_SUBTYPE_LOCALE_STRING_A, SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A);
+
+        assertEquals(SUBTYPE_NAME_RES_ID_A, subtype.getNameResId());
+        assertEquals(SUBTYPE_SUBTYPE_LOCALE_STRING_A, subtype.getLocale());
+        assertEquals("Value1", subtype.getExtraValueOf("Key1"));
+        assertEquals("Value2", subtype.getExtraValueOf("Key2"));
+        // Similar to "SubtypeId" in InputMethodSubtype, "SubtypeId" in SpellCheckerSubtype enables
+        // developers to specify a stable and consistent ID for each subtype.
+        assertEquals(SUBTYPE_SUBTYPE_ID_A, subtype.hashCode());
+
+        final SpellCheckerSubtype clonedSubtype = cloneViaParcel(subtype);
+        assertEquals(SUBTYPE_NAME_RES_ID_A, clonedSubtype.getNameResId());
+        assertEquals(SUBTYPE_SUBTYPE_LOCALE_STRING_A, clonedSubtype.getLocale());
+        assertEquals("Value1", clonedSubtype.getExtraValueOf("Key1"));
+        assertEquals("Value2", clonedSubtype.getExtraValueOf("Key2"));
+        assertEquals(SUBTYPE_SUBTYPE_ID_A, clonedSubtype.hashCode());
     }
 
     @SmallTest
@@ -130,5 +152,54 @@ public class SpellCheckerSubtypeTest extends InstrumentationTestCase {
                         SUBTYPE_EXTRA_VALUE_A),
                 new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
                         SUBTYPE_EXTRA_VALUE_B));
+
+        // If subtype ID is 0 (== SUBTYPE_SUBTYPE_ID_NONE), we keep the same behavior.
+        assertEquals(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE));
+        assertNotEqual(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_B, SUBTYPE_SUBTYPE_LOCALE_STRING_B,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE));
+        assertNotEqual(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_B,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE));
+        assertNotEqual(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_NONE),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_B, SUBTYPE_SUBTYPE_ID_NONE));
+
+        // If subtype ID is not 0, we test the equality based only on the subtype ID.
+        assertEquals(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A));
+        assertEquals(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_B, SUBTYPE_SUBTYPE_LOCALE_STRING_B,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A));
+        assertEquals(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_B,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A));
+        assertEquals(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_B, SUBTYPE_SUBTYPE_ID_A));
+        assertNotEqual(
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_A),
+                new SpellCheckerSubtype(SUBTYPE_NAME_RES_ID_A, SUBTYPE_SUBTYPE_LOCALE_STRING_A,
+                        SUBTYPE_EXTRA_VALUE_A, SUBTYPE_SUBTYPE_ID_B));
     }
 }
