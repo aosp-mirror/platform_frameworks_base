91ec9298d90ea6c279decdb52fae2fa5d28813d0
zhangshuxiao <zhangshuxiao@xiaomi.com>
2016-01-13 10:35:01 +0800

    When wifi ssid is empty, return WifiSsid.NONE
    Issue 199610
    
    When WifistateMachine enter Roaming State, WifiInfo
    getSSID will return 0x, statusbar show "0x", because
    roaming State do not update WifiInfo mWifiSsid.
    
    Change-Id: I241f8e9c11dc16db26cadde133043745f2f19573
    Signed-off-by: zhangshuxiao <zhangshuxiao@xiaomi.com>

--- wifi/java/android/net/wifi/WifiInfo.java
+++ wifi/java/android/net/wifi/WifiInfo.java
@@ -320,7 +320,8 @@ public class WifiInfo implements Parcelable {
             if (!TextUtils.isEmpty(unicode)) {
                 return "\"" + unicode + "\"";
             } else {
-                return mWifiSsid.getHexString();
+                String hex = mWifiSsid.getHexString();
+                return (hex != null) ? hex : WifiSsid.NONE;
             }
         }
         return WifiSsid.NONE;
