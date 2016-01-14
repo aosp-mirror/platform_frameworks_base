5c74bb313fbe3fb479f9dc4296e46e9586faf946
Lianwei Wang <lianwei.wang@gmail.com>
2016-01-13 18:42:35 -0800

    close file in PersistentDataBlockService jni
    The opened file should be closed.
    
    Change-Id: I8a43b338c5c9eb0a5b1b5329eec767c6395bc7eb
    Signed-off-by: Lianwei Wang <lianwei.wang@gmail.com>

--- services/core/jni/com_android_server_PersistentDataBlockService.cpp
+++ services/core/jni/com_android_server_PersistentDataBlockService.cpp
@@ -84,7 +84,11 @@ namespace android {
         if (fd < 0)
             return 0;
 
-        return get_block_device_size(fd);
+        const uint64_t size = get_block_device_size(fd);
+
+        close(fd);
+
+        return size;
     }
 
     static int com_android_server_PersistentDataBlockService_wipe(JNIEnv *env, jclass, jstring jpath) {
@@ -94,7 +98,11 @@ namespace android {
         if (fd < 0)
             return 0;
 
-        return wipe_block_device(fd);
+        const int ret = wipe_block_device(fd);
+
+        close(fd);
+
+        return ret;
     }
 
     static const JNINativeMethod sMethods[] = {
