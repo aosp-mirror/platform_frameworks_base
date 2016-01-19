9fb54aedc36a000498c8c0590c54e398d8eda654
Matthew Williams <mjwilliams@google.com>
2016-01-19 23:04:04 +0000

    Redact Account info from getCurrentSyncs
    BUG:26094635
    If the caller to ContentResolver#getCurrentSyncs does not hold the
    GET_ACCOUNTS permission, return a SyncInfo object that does not
    contain any Account information.
    
    Change-Id: I5628ebe1f56c8e3f784aaf1b3281e6b829d19314
    (cherry picked from commit b63057e698a01dafcefc7ba09b397b0336bba43d)
    (cherry picked from commit f39549e389edf62f43bacd95a1b8580d75ad77ed)

--- services/core/java/com/android/server/content/SyncStorageEngine.java
+++ services/core/java/com/android/server/content/SyncStorageEngine.java
@@ -1459,15 +1459,23 @@ public class SyncStorageEngine extends Handler {
     }
 
     /**
-     * @return a copy of the current syncs data structure. Will not return
-     * null.
+     * @param userId Id of user to return current sync info.
+     * @param canAccessAccounts Determines whether to redact Account information from the result.
+     * @return a copy of the current syncs data structure. Will not return null.
      */
-    public List<SyncInfo> getCurrentSyncsCopy(int userId) {
+    public List<SyncInfo> getCurrentSyncsCopy(int userId, boolean canAccessAccounts) {
         synchronized (mAuthorities) {
             final List<SyncInfo> syncs = getCurrentSyncsLocked(userId);
             final List<SyncInfo> syncsCopy = new ArrayList<SyncInfo>();
             for (SyncInfo sync : syncs) {
-                syncsCopy.add(new SyncInfo(sync));
+                SyncInfo copy;
+                if (!canAccessAccounts) {
+                    copy = SyncInfo.createAccountRedacted(
+                        sync.authorityId, sync.authority, sync.startTime);
+                } else {
+                    copy = new SyncInfo(sync);
+                }
+                syncsCopy.add(copy);
             }
             return syncsCopy;
         }
