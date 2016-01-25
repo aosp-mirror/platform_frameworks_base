1052a1cb478fa3902fb73b6bb832268e761862a3
riddle_hsu <riddle_hsu@htc.com>
2016-01-25 19:09:14 +0800

    Fix race condition of job extras.
    When writing to xml, bundle will unparcel and clear mParcelledData.
    Other thread may execute startJob(JobParameters) that uses the same
    bundle, then it may occur NPE in writeToParcelInner.
    
    Change-Id: I460d89019745b72dd56b78cc7bc4fb0c2bd16d49

--- services/core/java/com/android/server/job/JobStore.java
+++ services/core/java/com/android/server/job/JobStore.java
@@ -294,7 +294,8 @@ public class JobStore {
                     addAttributesToJobTag(out, jobStatus);
                     writeConstraintsToXml(out, jobStatus);
                     writeExecutionCriteriaToXml(out, jobStatus);
-                    writeBundleToXml(jobStatus.getExtras(), out);
+                    // Only unparcel our own copy because others may use it.
+                    writeBundleToXml(new PersistableBundle(jobStatus.getExtras()), out);
                     out.endTag(null, "job");
                 }
                 out.endTag(null, "job-info");
