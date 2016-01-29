a481b48dcfadc0aeaabeed0d0e35c4117840365d
Adrian Roos <roosa@google.com>
2016-01-29 09:21:58 -0800

    Fix NPE when logging a WTF in unknown process
    Bug: 26873463
    Change-Id: I548c10b43f5784152b9227926aa37170fd9fd78f

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12970,9 +12970,11 @@ public final class ActivityManagerService extends ActivityManagerNative
 
         final StringBuilder sb = new StringBuilder(1024);
         appendDropBoxProcessHeaders(process, processName, sb);
-        sb.append("Foreground: ")
-                .append(process.isInterestingToUserLocked() ? "Yes" : "No")
-                .append("\n");
+        if (process != null) {
+            sb.append("Foreground: ")
+                    .append(process.isInterestingToUserLocked() ? "Yes" : "No")
+                    .append("\n");
+        }
         if (activity != null) {
             sb.append("Activity: ").append(activity.shortComponentName).append("\n");
         }
