6b7f0f30619fd4468844abfce4d34232707cd4a2
Ritesh Reddy <riteshr@google.com>
2016-02-01 13:47:48 +0000

    Revert "Enabled NetworkPolicy backup and restore."
    This reverts commit 9f548b00dcdd89e8d557223b8b99269ca7362c2e.
    
    Change-Id: If0081fa34333339e36f9e8b9a34a2e3a84e4638f

--- packages/SettingsProvider/test/Android.mk
+++ packages/SettingsProvider/test/Android.mk
@@ -5,10 +5,7 @@ include $(CLEAR_VARS)
 # Note we statically link SettingsState to do some unit tests.  It's not accessible otherwise
 # because this test is not an instrumentation test. (because the target runs in the system process.)
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
-    ../src/com/android/providers/settings/SettingsState.java \
-    ../src/com/android/providers/settings/WiFiConfigurationSerializer.java \
-    ../src/com/android/providers/settings/NetworkPolicySerializer.java
-
+    ../src/com/android/providers/settings/SettingsState.java
 
 LOCAL_PACKAGE_NAME := SettingsProviderTest
 
