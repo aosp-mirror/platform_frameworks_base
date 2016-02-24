5c2f839971aaac2bc7cfd98a61e0eb0e04340a9e
Adam Powell <adamp@google.com>
2016-02-23 16:06:42 -0800

    Un-hide string set persistence methods in Preference
    6 years is enough time for API approval.
    
    Bug 27324328
    
    Change-Id: I5d5af0fc291250cd9ea5b50cd5031378475eacec

--- core/java/android/preference/Preference.java
+++ core/java/android/preference/Preference.java
@@ -1494,11 +1494,9 @@ public class Preference implements Comparable<Preference> {
      * @return True if the Preference is persistent. (This is not whether the
      *         value was persisted, since we may not necessarily commit if there
      *         will be a batch commit later.)
-     * @see #getPersistedString(Set)
-     * 
-     * @hide Pending API approval
+     * @see #getPersistedStringSet(Set)
      */
-    protected boolean persistStringSet(Set<String> values) {
+    public boolean persistStringSet(Set<String> values) {
         if (shouldPersist()) {
             // Shouldn't store null
             if (values.equals(getPersistedStringSet(null))) {
@@ -1527,10 +1525,8 @@ public class Preference implements Comparable<Preference> {
      * @return The value from the SharedPreferences or the default return
      *         value.
      * @see #persistStringSet(Set)
-     * 
-     * @hide Pending API approval
      */
-    protected Set<String> getPersistedStringSet(Set<String> defaultReturnValue) {
+    public Set<String> getPersistedStringSet(Set<String> defaultReturnValue) {
         if (!shouldPersist()) {
             return defaultReturnValue;
         }
