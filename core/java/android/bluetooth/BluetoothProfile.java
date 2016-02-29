7c2539e2a50ec3dcdb1c9420f9215bba438b21b6
Bryce Lee <brycelee@google.com>
2016-02-29 14:59:07 -0800

    DO NOT MERGE ANYWHERE Add way to query for supported Bluetooth profiles.
    Currently there is no way to get the profiles supported by the Bluetooth
    adapter. Asking for a profile proxy of an unsupported profile does not
    fail and can lead to code indefinitely waiting for the proxy response. This
    new code will allow for checking the supported profiles before asking for
    the proxies.
    
    Bug: 26451648
    Change-Id: Ie209f08058205eea39513c03b626788f5fed3293
    Signed-off-by: Bryce Lee <brycelee@google.com>

--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -130,6 +130,14 @@ public interface BluetoothProfile {
      */
     public static final int HEADSET_CLIENT = 16;
 
+
+    /**
+     * Max profile ID. This value should be updated whenever a new profile is added to match
+     * the largest value assigned to a profile.
+     * @hide
+     */
+    public static final int MAX_PROFILE_ID = 16;
+
     /**
      * Default priority for devices that we try to auto-connect to and
      * and allow incoming connections for the profile
