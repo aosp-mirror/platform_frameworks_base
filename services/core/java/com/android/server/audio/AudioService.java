598c0c9f659184971e974de6a0184a3000e7900c
Jean-Michel Trivi <jmtrivi@google.com>
2016-03-07 14:13:03 -0800

    Rename AudioRecordConfiguration to AudioRecordingConfiguration
    In package android.media:
    - rename AudioRecordConfiguration to
       AudioRecordingConfiguration to avoid ambiguity with the
       android.media.AudioRecord class
    - rename AudioManager.getActiveRecordConfigurations() to
       getActiveRecordingConfigurations.
    
    Bug 27385560
    
    Change-Id: I5ef404ff36522193990c9b563d4545893529b365

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -58,7 +58,7 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioManagerInternal;
 import android.media.AudioPort;
-import android.media.AudioRecordConfiguration;
+import android.media.AudioRecordingConfiguration;
 import android.media.AudioRoutesInfo;
 import android.media.IAudioFocusDispatcher;
 import android.media.IAudioRoutesObserver;
@@ -6287,8 +6287,8 @@ public class AudioService extends IAudioService.Stub {
         mRecordMonitor.unregisterRecordingCallback(rcdb);
     }
 
-    public AudioRecordConfiguration[] getActiveRecordConfigurations() {
-        return mRecordMonitor.getActiveRecordConfigurations();
+    public AudioRecordingConfiguration[] getActiveRecordingConfigurations() {
+        return mRecordMonitor.getActiveRecordingConfigurations();
     }
 
     //======================
