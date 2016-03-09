df91a67953d1a2fbcbb785e8813189ace4ddd5ee
Sungsoo Lim <sungsoo@google.com>
2016-03-09 15:59:01 +0900

    MediaBrowser: Copy options before calling subscribeInternal()
    If MediaBrowser and MediaBrowserService are on the same process,
    the options object could be shared.
    
    Bug: 27398805
    Change-Id: I61ce63f667e46229662d85cd6f417b104f9d1388

--- media/java/android/media/browse/MediaBrowser.java
+++ media/java/android/media/browse/MediaBrowser.java
@@ -365,7 +365,7 @@ public final class MediaBrowser {
         if (options == null) {
             throw new IllegalArgumentException("options are null");
         }
-        subscribeInternal(parentId, options, callback);
+        subscribeInternal(parentId, new Bundle(options), callback);
     }
 
     /**
@@ -1116,7 +1116,7 @@ public final class MediaBrowser {
                 }
             }
             mCallbacks.add(callback);
-            mOptionsList.add(options == null ? null : new Bundle(options));
+            mOptionsList.add(options);
         }
 
         public boolean removeCallback(Bundle options) {
