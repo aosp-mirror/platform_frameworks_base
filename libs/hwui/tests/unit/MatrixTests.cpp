034a10bf216cdef251928edf72d93668d81515f8
Chris Craik <ccraik@google.com>
2016-03-09 16:03:21 -0800

    Partial revert of 261725fdb2962271c222a049fcdf57bbdc8363c7
    Bug:27534946
    
    Mapping empty rects still must be done to get correct positions for
    line/point rects describing strokable bounds.
    
    Change-Id: I2f9efe543e58eb438b755276585491c1256d6719

--- libs/hwui/tests/unit/MatrixTests.cpp
+++ libs/hwui/tests/unit/MatrixTests.cpp
@@ -21,15 +21,30 @@
 
 using namespace android::uirenderer;
 
-TEST(Matrix, mapRect) {
+TEST(Matrix, mapRect_emptyScaleSkew) {
     // Skew, so we don't hit identity/translate/simple fast paths
-    Matrix4 matrix;
-    matrix.skew(0.1f, 0.1f);
+    Matrix4 scaleMatrix;
+    scaleMatrix.loadScale(10, 10, 1);
+    scaleMatrix.skew(0.1f, 0.1f);
 
     // non-zero empty rect, so sorting x/y would make rect non-empty
-    Rect empty(100, 100, -100, -100);
+    Rect empty(15, 20, 15, 100);
     ASSERT_TRUE(empty.isEmpty());
-    matrix.mapRect(empty);
-    EXPECT_TRUE(empty.isEmpty())
-        << "Empty rect should always remain empty, regardless of mapping.";
+    scaleMatrix.mapRect(empty);
+    EXPECT_EQ(Rect(170, 215, 250, 1015), empty);
+    EXPECT_FALSE(empty.isEmpty())
+        << "Empty 'line' rect doesn't remain empty when skewed.";
+}
+
+TEST(Matrix, mapRect_emptyRotate) {
+    // Skew, so we don't hit identity/translate/simple fast paths
+    Matrix4 skewMatrix;
+    skewMatrix.loadRotate(45);
+
+    // non-zero empty rect, so sorting x/y would make rect non-empty
+    Rect lineRect(0, 100);
+    ASSERT_TRUE(lineRect.isEmpty());
+    skewMatrix.mapRect(lineRect);
+    EXPECT_FALSE(lineRect.isEmpty())
+        << "Empty 'line' rect doesn't remain empty when rotated.";
 }
