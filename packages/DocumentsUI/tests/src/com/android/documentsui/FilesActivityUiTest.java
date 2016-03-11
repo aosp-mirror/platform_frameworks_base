3e63e7de2378055857c329e80862a11d947852c4
Steve McKay <smckay@google.com>
2016-03-11 10:11:52 -0800

    Run all downloads through trampoline.
    This guarantees the retry/delete support in the downloads
        trampoline can do its thing.
    Allow APKs not in downloads to go directly to pakcage
        manager via VIEW intent.
    Add test coverage for retry dialog.
    
    Bug: 27539337
    
    Change-Id: I75c5db8915e24a7648c7990136d39ed86d407637

--- packages/DocumentsUI/tests/src/com/android/documentsui/FilesActivityUiTest.java
+++ packages/DocumentsUI/tests/src/com/android/documentsui/FilesActivityUiTest.java
@@ -19,10 +19,17 @@ package com.android.documentsui;
 import static com.android.documentsui.StubProvider.ROOT_0_ID;
 import static com.android.documentsui.StubProvider.ROOT_1_ID;
 
+import android.app.DownloadManager;
+import android.app.DownloadManager.Request;
+import android.content.Context;
+import android.net.Uri;
 import android.os.RemoteException;
+import android.support.test.uiautomator.Configurator;
+import android.support.test.uiautomator.UiObject;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.Suppress;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 
 import com.android.documentsui.model.RootInfo;
 
@@ -172,5 +179,38 @@ public class FilesActivityUiTest extends ActivityTest<FilesActivity> {
             bots.main.pressKey(KeyEvent.KEYCODE_DPAD_LEFT);
             bots.roots.assertHasFocus();
         }
+    }
+
+    // We don't really need to test the entirety of download support
+    // since downloads is (almost) just another provider.
+    public void testDownload_Queued() throws Exception {
+        DownloadManager dm = (DownloadManager) context.getSystemService(
+                Context.DOWNLOAD_SERVICE);
+        // This downloads ends up being queued (because DNS can't be resolved).
+        // We'll still see an entry in the downloads UI with a "Queued" label.
+        dm.enqueue(new Request(Uri.parse("http://hammychamp.toodles")));
+
+        bots.roots.openRoot("Downloads");
+        bots.directory.assertDocumentsPresent("Queued");
+    }
+
+    public void testDownload_RetryUnsuccessful() throws Exception {
+        DownloadManager dm = (DownloadManager) context.getSystemService(
+                Context.DOWNLOAD_SERVICE);
+        // This downloads fails! But it'll still show up.
+        dm.enqueue(new Request(Uri.parse("http://www.google.com/hamfancy")));
+
+        bots.roots.openRoot("Downloads");
+        UiObject doc = bots.directory.findDocument("Unsuccessful");
+        doc.waitForExists(TIMEOUT);
+
+        int toolType = Configurator.getInstance().getToolType();
+        Configurator.getInstance().setToolType(MotionEvent.TOOL_TYPE_FINGER);
+        doc.click();
+        Configurator.getInstance().setToolType(toolType);
+
+        assertTrue(bots.main.findDownloadRetryDialog().exists());
+
+        device.pressBack(); // to clear the dialog.
     }
 }
