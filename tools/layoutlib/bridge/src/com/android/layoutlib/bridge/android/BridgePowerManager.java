86b66c5ef6f2170c1da74beba550b82a858ea09a
Tony Mantler <nicoya@google.com>
2016-03-18 13:06:56 -0700

    Fix build
    Change-Id: I590db57366a40aa011ee02e6e34e64cd2e7d1115

--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/BridgePowerManager.java
@@ -86,6 +86,11 @@ public class BridgePowerManager implements IPowerManager {
     }
 
     @Override
+    public void rebootSafeMode(boolean confirm, boolean wait) {
+        // pass for now.
+    }
+
+    @Override
     public void shutdown(boolean confirm, String reason, boolean wait) {
         // pass for now.
     }
