98ff0b72f0ac630be2b7fd8ab77f5ee50a1ce26f
Dan Willemsen <dwillemsen@google.com>
2016-03-23 00:42:44 -0700

    Remove custom copy rules, stop using ACP
    Replace some custom rules with BUILD_PREBUILT.
    
    Move tools from ALL_PREBUILTS to BUILD_PREBUILT. All of them are already
    in PRODUCT_PACKAGES in build/target/product/base.mk.
    
    Remove uses of ACP and replace with core build system macros or 'cp'
    
    Change-Id: I529e7c6c50fc64ad36b75bb47142f6bde909d141

--- cmds/svc/Android.mk
+++ cmds/svc/Android.mk
@@ -1,15 +1,16 @@
 # Copyright 2007 The Android Open Source Project
 #
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
-LOCAL_MODULE := svc
+LOCAL_MODULE := svclib
+LOCAL_MODULE_STEM := svc
 include $(BUILD_JAVA_LIBRARY)
 
-
 include $(CLEAR_VARS)
-ALL_PREBUILT += $(TARGET_OUT)/bin/svc
-$(TARGET_OUT)/bin/svc : $(LOCAL_PATH)/svc | $(ACP)
-	$(transform-prebuilt-to-target)
-
+LOCAL_MODULE := svc
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES := svc
+LOCAL_REQUIRED_MODULES := svclib
+include $(BUILD_PREBUILT)
