96e30eba1eaed0e35d6b8cfcd504aed5475556d8
Aga Wronska <agawronska@google.com>
2016-03-23 17:12:33 -0700

    Make pressing 'Back' button open the drawer when in Recents root
    Change-Id: I3c6cbcd3d2d003af1a6459c3bfd193ee45021882
    Fixed: 27230999

--- packages/DocumentsUI/src/com/android/documentsui/FilesActivity.java
+++ packages/DocumentsUI/src/com/android/documentsui/FilesActivity.java
@@ -381,7 +381,7 @@ public class FilesActivity extends BaseActivity {
             }
 
             // Open the Close drawer if it is closed and we're at the top of a root.
-            if (size == 1) {
+            if (size <= 1) {
                 mDrawer.setOpen(true);
                 // Remember so we don't just close it again if back is pressed again.
                 mDrawerLastFiddled = System.currentTimeMillis();
