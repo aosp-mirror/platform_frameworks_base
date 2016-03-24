907c177b04d07d5d84005ac92df9bde6af7c394d
George Mount <mount@google.com>
2016-03-24 16:29:03 -0700

    Start seeked animators without waiting for start delay.
    Bug 24872727
    
    Change-Id: I954efd7113565f9b8f9a569f57960b8d91620758

--- core/java/android/animation/ValueAnimator.java
+++ core/java/android/animation/ValueAnimator.java
@@ -967,7 +967,7 @@ public class ValueAnimator extends Animator implements AnimationHandler.Animatio
         AnimationHandler animationHandler = AnimationHandler.getInstance();
         animationHandler.addAnimationFrameCallback(this, (long) (mStartDelay * sDurationScale));
 
-        if (mStartDelay == 0) {
+        if (mStartDelay == 0 || mSeekFraction >= 0) {
             // If there's no start delay, init the animation and notify start listeners right away
             // to be consistent with the previous behavior. Otherwise, postpone this until the first
             // frame after the start delay.
