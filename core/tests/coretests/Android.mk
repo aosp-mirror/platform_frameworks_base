ba068f48727dee2223b3be3b0359accb949e363d
Ying Wang <wangying@google.com>
2016-03-30 13:58:10 -0700

    Remove support of disabling Jack.
    Also removed obsolete LOCAL_DX_FLAGS.
    
    Bug: 27400061
    Change-Id: I23fe13927fb3580698a8a4b84173e229e037f02e

--- core/tests/coretests/Android.mk
+++ core/tests/coretests/Android.mk
@@ -21,7 +21,6 @@ LOCAL_SRC_FILES := \
 	$(call all-java-files-under, DisabledTestApp/src) \
 	$(call all-java-files-under, EnabledTestApp/src)
 
-LOCAL_DX_FLAGS := --core-library
 LOCAL_AAPT_FLAGS = -0 dat -0 gld
 LOCAL_STATIC_JAVA_LIBRARIES := core-tests-support android-common frameworks-core-util-lib mockwebserver guava littlemock mockito-target
 LOCAL_JAVA_LIBRARIES := android.test.runner conscrypt telephony-common org.apache.http.legacy
