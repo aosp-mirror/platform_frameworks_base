ae4908f41c6e96c973a65effe1dfbb3d0fbbeba2
Alan Viverette <alanv@google.com>
2016-03-31 16:18:27 -0400

    Remove DayNight theme
    Bug: 21854466
    Change-Id: I739872112d0ae457d0d4620f9222206072d54b4b

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -167,7 +167,7 @@ public final class SystemServer {
      * visual content.
      */
     private static final int DEFAULT_SYSTEM_THEME =
-            com.android.internal.R.style.Theme_Material_DayNight_DarkActionBar;
+            com.android.internal.R.style.Theme_Material_Light_DarkActionBar;
 
     private final int mFactoryTestMode;
     private Timer mProfilerSnapshotTimer;
