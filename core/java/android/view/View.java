68be3529e442cdab1bc544d1026144e0b13f0b36
Chris Craik <ccraik@google.com>
2016-04-13 14:46:13 -0700

    Destroy drawing cache when switching layer type
    bug:27099358
    
    Change-Id: Ia308dd817799b6d64a802df211c39efc25c99e7e

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -15741,8 +15741,10 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
             return;
         }
 
-        // Destroy any previous software drawing cache if needed
-        if (mLayerType == LAYER_TYPE_SOFTWARE) {
+        if (layerType != LAYER_TYPE_SOFTWARE) {
+            // Destroy any previous software drawing cache if present
+            // NOTE: even if previous layer type is HW, we do this to ensure we've cleaned up
+            // drawing cache created in View#draw when drawing to a SW canvas.
             destroyDrawingCache();
         }
 
