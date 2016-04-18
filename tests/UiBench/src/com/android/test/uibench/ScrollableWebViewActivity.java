e706ef6b9ea79622d45b5b3536e1a6c1b5474e7e
Chris Craik <ccraik@google.com>
2016-04-18 13:28:52 -0700

    Add WebView scroll activity to UiBench
    bug:28095056
    
    Change-Id: Id1379d5a30835ef7285e729a3632cfef233d73a3

---
+++ tests/UiBench/src/com/android/test/uibench/ScrollableWebViewActivity.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.test.uibench;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class ScrollableWebViewActivity extends AppCompatActivity {
+    static String sHtml = "<body style=\"height:10000px;"
+             + "background:repeating-linear-gradient(white, black 10%)\"/>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        webView.loadData(sHtml, "text/html", null);
+        webView.setVerticalScrollBarEnabled(true);
+        setContentView(webView);
+    }
+}
