53a528bb4f4cdb5a8d07b0d91405fdb2edb62201
Steve McKay <smckay@google.com>
2016-04-26 15:49:50 -0700

    Add archive support to Bug report docs provider.
    Bug: 28386918
    Change-Id: I2270324be3f020d73a68151002f885f70eb1449c

--- packages/Shell/Android.mk
+++ packages/Shell/Android.mk
@@ -5,7 +5,8 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4 \
+        android-support-documents-archive
 
 LOCAL_PACKAGE_NAME := Shell
 LOCAL_CERTIFICATE := platform
