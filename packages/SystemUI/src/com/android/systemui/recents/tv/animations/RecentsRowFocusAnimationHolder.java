01e365b9af1bf87265ac9466c339d1e4c27966e8
Jaewan Kim <jaewan@google.com>
2016-05-03 02:20:11 +0900

    Fix recents row blinking when PIP is focused
    Bug: 28522204
    Change-Id: I916c47a57b30e553f8823ea8d2c3e686fab9e25c

--- packages/SystemUI/src/com/android/systemui/recents/tv/animations/RecentsRowFocusAnimationHolder.java
+++ packages/SystemUI/src/com/android/systemui/recents/tv/animations/RecentsRowFocusAnimationHolder.java
@@ -52,7 +52,9 @@ public class RecentsRowFocusAnimationHolder {
 
         mFocusLoseAnimatorSet = new AnimatorSet();
         mFocusLoseAnimatorSet.playTogether(
-                ObjectAnimator.ofFloat(mView, "alpha", dimAlpha),
+                // Animation doesn't start from the current value (1f) sometimes,
+                // so specify the desired initial value here.
+                ObjectAnimator.ofFloat(mView, "alpha", 1f, dimAlpha),
                 ObjectAnimator.ofFloat(mTitleView, "alpha", 0f));
         mFocusLoseAnimatorSet.setDuration(duration);
         mFocusLoseAnimatorSet.setInterpolator(Interpolators.FAST_OUT_SLOW_IN);
