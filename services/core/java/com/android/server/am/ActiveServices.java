9d97ee2ad26b248a1778c4ccb43859b63b24a7b1
Joe Onorato <joeo@google.com>
2016-05-31 10:49:08 -0700

    Turn this wtf into just a log.
    We're seeing this a bunch, but the overhead from the WTF is concerning.
    So turn it off.  It means that somewhere there is a race in tearing
    down the app, or maybe some place fails to call back in some scenario.
    
    Bug: 28932059
    Change-Id: Ice14ade95bb9377ad622d440fb022953ad51c34c

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -2201,11 +2201,11 @@ public final class ActiveServices {
                     // If the app is null, then it was probably removed because the process died,
                     // otherwise wtf
                     if (r.app != null) {
-                        Slog.wtfStack(TAG, "Service done with onDestroy, but not inDestroying: "
+                        Slog.w(TAG, "Service done with onDestroy, but not inDestroying: "
                                 + r + ", app=" + r.app);
                     }
                 } else if (r.executeNesting != 1) {
-                    Slog.wtfStack(TAG, "Service done with onDestroy, but executeNesting="
+                    Slog.w(TAG, "Service done with onDestroy, but executeNesting="
                             + r.executeNesting + ": " + r);
                     // Fake it to keep from ANR due to orphaned entry.
                     r.executeNesting = 1;
