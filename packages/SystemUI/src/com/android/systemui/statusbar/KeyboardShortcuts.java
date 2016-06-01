3ac0ae076e4baee06804e93adb7b5573d5de67de
Clara Bayarri <clarabayarri@google.com>
2016-06-01 17:28:14 +0100

    Fix Keyboard Shortcut Helper requires pressing META+/ twice
    By using a local boolean we were missing out on when the system dismisses
    the window with the back key.
    
    Bug: 28910518
    Change-Id: I87b21c11830eaf81e3fd9586be6e3a7bb59c8a1f

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyboardShortcuts.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyboardShortcuts.java
@@ -74,7 +74,6 @@ public final class KeyboardShortcuts {
     private static final String TAG = KeyboardShortcuts.class.getSimpleName();
     private static final Object sLock = new Object();
     private static KeyboardShortcuts sInstance;
-    private static boolean sIsShowing;
 
     private final SparseArray<String> mSpecialCharacterNames = new SparseArray<>();
     private final SparseArray<String> mModifierNames = new SparseArray<>();
@@ -136,13 +135,12 @@ public final class KeyboardShortcuts {
                 dismiss();
             }
             getInstance(context).showKeyboardShortcuts(deviceId);
-            sIsShowing = true;
         }
     }
 
     public static void toggle(Context context, int deviceId) {
         synchronized (sLock) {
-            if (sIsShowing) {
+            if (isShowing()) {
                 dismiss();
             } else {
                 show(context, deviceId);
@@ -158,8 +156,12 @@ public final class KeyboardShortcuts {
                 sInstance.dismissKeyboardShortcuts();
                 sInstance = null;
             }
-            sIsShowing = false;
         }
+    }
+
+    private static boolean isShowing() {
+        return sInstance != null && sInstance.mKeyboardShortcutsDialog != null
+                && sInstance.mKeyboardShortcutsDialog.isShowing();
     }
 
     private void loadResources(Context context) {
