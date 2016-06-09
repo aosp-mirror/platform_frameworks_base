43b9a39cbf5ab81ab1a58a0a448f45f25e902ba3
Garfield, Tan <xutan@google.com>
2016-06-09 15:20:32 -0700

    Jarjar common dependencies of test apk and app apk to avoid class conflicts.
    Also remove android-support-v4 dependency from test apk as there is no
    dependency over it in test apk.
    
    Change-Id: I0b459bb831dae6c03adfb4215a8f6a514fc72904
    (cherry picked from commit 41f0641ee657215b3efa07c52698c0c4aa2ca1e4)

--- packages/DocumentsUI/tests/Android.mk
+++ packages/DocumentsUI/tests/Android.mk
@@ -7,8 +7,9 @@ LOCAL_MODULE_TAGS := tests
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_JAVA_LIBRARIES := android-support-v4 android.test.runner
+LOCAL_JAVA_LIBRARIES := android.test.runner
 LOCAL_STATIC_JAVA_LIBRARIES := mockito-target ub-uiautomator espresso-core guava
+LOCAL_JARJAR_RULES := $(LOCAL_PATH)/jarjar-rules.txt
 
 LOCAL_PACKAGE_NAME := DocumentsUITests
 LOCAL_INSTRUMENTATION_FOR := DocumentsUI
