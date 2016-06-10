5076e56ee9314fcc70771642cebbd4ddf44a1996
Willi Ye <williye97@gmail.com>
2016-06-10 14:41:59 +0200

    Solid battery: Bring back pulsing animation
    Change-Id: Ia62b4cf5aaeaca88534cc80a20dd788672b79388

--- packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterDrawable.java
@@ -16,7 +16,10 @@
 
 package com.android.systemui;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
+import android.animation.ValueAnimator;
 import android.annotation.Nullable;
 import android.content.Context;
 import android.content.res.Resources;
@@ -116,6 +119,7 @@ public class BatteryMeterDrawable extends Drawable implements
     private StopMotionVectorDrawable mLevelDrawable;
     private Drawable mBoltDrawable;
     private Drawable mPlusDrawable;
+    private ValueAnimator mAnimator;
 
     private int mTextGravity;
 
@@ -261,6 +265,11 @@ public class BatteryMeterDrawable extends Drawable implements
         if (level < 31) {
             isPctToBeWhiteOrRed = true;
         }
+
+        if (mStyle == BATTERY_STYLE_SOLID) {
+            animateSolidBattery(level, pluggedIn, charging);
+        }
+
         postInvalidate();
     }
 
@@ -347,6 +356,37 @@ public class BatteryMeterDrawable extends Drawable implements
                 }
             }
             return color;
+        }
+    }
+
+    public void animateSolidBattery(int level, boolean pluggedIn, boolean charging) {
+        if (charging) {
+            if (mAnimator != null) mAnimator.cancel();
+
+            final int defaultAlpha = mLevelDrawable.getAlpha();
+            mAnimator = ValueAnimator.ofInt(defaultAlpha, 0, defaultAlpha);
+            mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator animation) {
+                    mLevelDrawable.setAlpha((int) animation.getAnimatedValue());
+                    invalidateSelf();
+                }
+            });
+            mAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mLevelDrawable.setAlpha(defaultAlpha);
+                    mAnimator = null;
+                }
+            });
+            mAnimator.setDuration(2000);
+            mAnimator.start();
         }
     }
 
