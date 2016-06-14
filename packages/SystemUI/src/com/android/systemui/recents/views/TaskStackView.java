edaffc5792ee4e32aaf622bad9278a981a5e51fe
Winson <winsonc@google.com>
2016-06-14 14:48:01 -0700

    Fixing header bar icons.
    Bug: 28402665
    Change-Id: If61a2c1fbfb881a61e67d8bc6f3a79236e01c7fa

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -274,6 +274,9 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
             }
         });
         setImportantForAccessibility(View.IMPORTANT_FOR_ACCESSIBILITY_YES);
+        if (ssp.hasFreeformWorkspaceSupport()) {
+            setWillNotDraw(false);
+        }
 
         mFreeformWorkspaceBackground = (GradientDrawable) getContext().getDrawable(
                 R.drawable.recents_freeform_workspace_bg);
