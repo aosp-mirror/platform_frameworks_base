23d1fdded5fe78024927137ddfb82401fd9e3344
Teng-Hui Zhu <ztenghui@google.com>
2016-06-20 14:42:35 -0700

    Refactor the java side APCT perf test
    Relocate everything into apct-tests/
    Setup the share static java lib for the common code and use it.
    
    b/28980976
    
    Change-Id: I74c80c0b54ad18ee5fc44da43249d3c88fb926e2

--- apct-tests/perftests/graphics/src/android/graphics/perftests/VectorDrawablePerfTest.java
+++ apct-tests/perftests/graphics/src/android/graphics/perftests/VectorDrawablePerfTest.java
@@ -17,12 +17,12 @@
 package android.graphics.perftests;
 
 import android.app.Activity;
-import android.app.Instrumentation;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.VectorDrawable;
 import android.os.Bundle;
 
+import android.perftests.utils.BenchmarkState;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.util.Log;
@@ -37,19 +37,19 @@ import java.io.IOException;
 public class VectorDrawablePerfTest extends ActivityInstrumentationTestCase2<StubActivity> {
 
     private static final String TAG = "PathPerfTest";
-    private static final int REPEAT_TIMES = 200;
     private static final boolean DBG_PERF = false;
 
     private int[] mTestWidths = {1024, 512};
     private int[] mTestHeights = {512, 1024};
 
-    private String KEY_VECTORDRAWABLE_DRAW_TIME = "VectorDrawable_Draw_Time_MicroSec";
+    private String KEY_VECTORDRAWABLE_DRAW_TIME = "VectorDrawable_Draw_Time_NanoSec";
 
     public VectorDrawablePerfTest() {
         super(StubActivity.class);
     }
 
     // Save a bitmap into a PNG, only for debugging purpose.
+    // TODO: move into utility class.
     private void saveBitmapIntoPNG(Bitmap bitmap, int resId) throws IOException {
         // Save the image to the disk.
         FileOutputStream out = null;
@@ -87,14 +87,14 @@ public class VectorDrawablePerfTest extends ActivityInstrumentationTestCase2<Stu
         Bitmap bmp = Bitmap.createBitmap(w, h, conf);
         Canvas canvas = new Canvas(bmp);
 
-        long startTime = System.nanoTime();
-        for (int i = 0; i < REPEAT_TIMES; i++) {
+        BenchmarkState state = new BenchmarkState();
+        int i = 0;
+        while (state.keepRunning()) {
             // Use different width / height each to force the vectorDrawable abandon the cache.
             vd.setBounds(0, 0, mTestWidths[i % 2], mTestHeights[i % 2]);
+            i++;
             vd.draw(canvas);
         }
-        long duration = System.nanoTime() - startTime;
-        long avgDurationMicroSecond = duration / REPEAT_TIMES / 1000;
 
         // Double check the bitmap pixels to make sure we draw correct content.
         int backgroundColor = bmp.getPixel(w / 4, h / 2);
@@ -102,13 +102,6 @@ public class VectorDrawablePerfTest extends ActivityInstrumentationTestCase2<Stu
         assertTrue("The background should be white", backgroundColor == 0xFFFFFFFF);
         assertTrue("The object should be black", objColor == 0xFF000000);
 
-        if (DBG_PERF) {
-            Log.v(TAG, "avg drawing vector drawable in bitmap size " + w + "x" + h + ":"
-                    + avgDurationMicroSecond + "micro seconds");
-        }
-
-        final Bundle status = new Bundle();
-        status.putLong(KEY_VECTORDRAWABLE_DRAW_TIME, avgDurationMicroSecond);
-        getInstrumentation().sendStatus(Activity.RESULT_OK, status);
+        state.sendFullStatusReport(getInstrumentation(), KEY_VECTORDRAWABLE_DRAW_TIME);
     }
 }
