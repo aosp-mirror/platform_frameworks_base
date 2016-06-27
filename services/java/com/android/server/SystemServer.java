7380c321ad23b6cbf0af239fbd2e037f67b08545
Jeff Sharkey <jsharkey@android.com>
2016-06-27 17:08:23 -0600

    Remove "starting apps" boot message.
    The "starting apps" message is only shown for a short period of time
    before we show the lock screen.  Recent boot animations now have a
    spin down phase which is just long enough to result in us flashing
    this message for a very short period of time, which looks janky.
    
    To avoid the janky behavior, remove this message altogether.  Other
    boot messages for dexopt'ing after an OTA remain intact.
    
    Also fix security bug that would allow any app to show a message.
    
    Bug: 29367890
    Change-Id: I77bb65acbe0e5afb4033dc6b83e533e665dce690

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -724,14 +724,6 @@ public final class SystemServer {
         }
         Trace.traceEnd(Trace.TRACE_TAG_SYSTEM_SERVER);
 
-        try {
-            ActivityManagerNative.getDefault().showBootMessage(
-                    context.getResources().getText(
-                            com.android.internal.R.string.android_upgrading_starting_apps),
-                    false);
-        } catch (RemoteException e) {
-        }
-
         if (mFactoryTestMode != FactoryTest.FACTORY_TEST_LOW_LEVEL) {
             if (!disableNonCoreServices) {
                 traceBeginAndSlog("StartLockSettingsService");
