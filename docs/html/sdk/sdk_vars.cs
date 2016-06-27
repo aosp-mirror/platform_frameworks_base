e080bc5bf7f25b4b75f4f701acdaefdbbd1148d3
Dan Albert <danalbert@google.com>
2016-06-27 12:56:37 -0700

    Fix Windows NDK labeling.
    32-bit and 64-bit labels are backward.
    
    Bug: http://b/29523188
    Bug: https://github.com/android-ndk/ndk/issues/127
    Change-Id: I87de0730c54555dc219d170fd600ec23a51decc2

--- docs/html/sdk/sdk_vars.cs
+++ docs/html/sdk/sdk_vars.cs
@@ -7,13 +7,13 @@ set:ndk.linux64_download='android-ndk-r12-linux-x86_64.zip' ?><?cs
 set:ndk.linux64_bytes='755431993' ?><?cs
 set:ndk.linux64_checksum='b7e02dc733692447366a2002ad17e87714528b39' ?><?cs
 
-set:ndk.win64_download='android-ndk-r12-windows-x86.zip' ?><?cs
-set:ndk.win64_bytes='706332762' ?><?cs
-set:ndk.win64_checksum='37fcd7acf6012d0068a57c1524edf24b0fef69c9' ?><?cs
+set:ndk.win32_download='android-ndk-r12-windows-x86.zip' ?><?cs
+set:ndk.win32_bytes='706332762' ?><?cs
+set:ndk.win32_checksum='37fcd7acf6012d0068a57c1524edf24b0fef69c9' ?><?cs
 
-set:ndk.win32_download='android-ndk-r12-windows-x86_64.zip' ?><?cs
-set:ndk.win32_bytes='749444245' ?><?cs
-set:ndk.win32_checksum='80d64a77aab52df867ac55cec1e976663dd3326f'
+set:ndk.win64_download='android-ndk-r12-windows-x86_64.zip' ?><?cs
+set:ndk.win64_bytes='749444245' ?><?cs
+set:ndk.win64_checksum='80d64a77aab52df867ac55cec1e976663dd3326f'
 ?>
 <?cs
 def:size_in_mb(bytes)
