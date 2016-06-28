4f26cb5a36487b3f5b497804faebe16241c15d54
Shunta Sato <Shunta.Sato@sonymobile.com>
2016-06-28 09:29:19 +0900

    Fix: "adb install -s" adaptation for session based install
    Internal implementation of "adb install" has been changed to
    session install. As a result '-s' option doesn't work.
    Size info is required when using '-s' option in session install.
    PackageHelper.fitsOnExternal() returns false and moving app to SD is
    failed when size info is not set.
    
    So set size info when '-s' option is used.
    And change identity on some method because shell doesn't have some
    permission to access to asec.
    
    Bug: 29932779
    Author: Ryuki Nakamura <ryuki.x.nakamura@sonymobile.com>
    Change-Id: I65edcc0f01ceecaaba17792ad329791a9253d603

--- services/core/java/com/android/server/pm/PackageManagerShellCommand.java
+++ services/core/java/com/android/server/pm/PackageManagerShellCommand.java
@@ -30,6 +30,10 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageInstaller;
 import android.content.pm.PackageItemInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageParser;
+import android.content.pm.PackageParser.ApkLite;
+import android.content.pm.PackageParser.PackageLite;
+import android.content.pm.PackageParser.PackageParserException;
 import android.content.pm.ParceledListSlice;
 import android.content.pm.PermissionGroupInfo;
 import android.content.pm.PermissionInfo;
@@ -48,6 +52,7 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.text.TextUtils;
 import android.util.PrintWriterPrinter;
+import com.android.internal.content.PackageHelper;
 import com.android.internal.util.SizedInputStream;
 
 import dalvik.system.DexFile;
@@ -137,11 +142,26 @@ class PackageManagerShellCommand extends ShellCommand {
     private int runInstall() throws RemoteException {
         final PrintWriter pw = getOutPrintWriter();
         final InstallParams params = makeInstallParams();
+        final String inPath = getNextArg();
+        if (params.sessionParams.sizeBytes < 0 && inPath != null) {
+            File file = new File(inPath);
+            if (file.isFile()) {
+                try {
+                    ApkLite baseApk = PackageParser.parseApkLite(file, 0);
+                    PackageLite pkgLite = new PackageLite(null, baseApk, null, null, null);
+                    params.sessionParams.setSize(
+                            PackageHelper.calculateInstalledSize(pkgLite,false, params.sessionParams.abiOverride));
+                } catch (PackageParserException | IOException e) {
+                    pw.println("Error: Failed to parse APK file : " + e);
+                    return 1;
+                }
+            }
+        }
+
         final int sessionId = doCreateSession(params.sessionParams,
                 params.installerPackageName, params.userId);
         boolean abandonSession = true;
         try {
-            final String inPath = getNextArg();
             if (inPath == null && params.sessionParams.sizeBytes == 0) {
                 pw.println("Error: must either specify a package size or an APK file");
                 return 1;
