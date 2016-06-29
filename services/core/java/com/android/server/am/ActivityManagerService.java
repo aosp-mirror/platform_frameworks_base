9536c809dda20bd01c3d5c30df046b271ba2ec4f
Jeff Sharkey <jsharkey@android.com>
2016-06-29 16:00:55 -0600

    Move getRecentTasks() to ParceledListSlice.
    In extreme cases the list of recent tasks can grow beyond the size
    of a single Binder transaction.  This change moves over to
    ParceledListSlice which handles chunking any large results.
    
    Bug: 29635557
    Change-Id: Iaf1227234f5f8c9451f73a6a5c1dc89f2067f05f

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -9077,7 +9077,8 @@ public final class ActivityManagerService extends ActivityManagerNative
     }
 
     @Override
-    public List<ActivityManager.RecentTaskInfo> getRecentTasks(int maxNum, int flags, int userId) {
+    public ParceledListSlice<ActivityManager.RecentTaskInfo> getRecentTasks(int maxNum, int flags,
+            int userId) {
         final int callingUid = Binder.getCallingUid();
         userId = mUserController.handleIncomingUser(Binder.getCallingPid(), callingUid, userId,
                 false, ALLOW_FULL_ONLY, "getRecentTasks", null);
@@ -9093,7 +9094,7 @@ public final class ActivityManagerService extends ActivityManagerNative
 
             if (!isUserRunning(userId, ActivityManager.FLAG_AND_UNLOCKED)) {
                 Slog.i(TAG, "user " + userId + " is still locked. Cannot load recents");
-                return Collections.emptyList();
+                return ParceledListSlice.emptyList();
             }
             mRecentTasks.loadUserRecentsLocked(userId);
 
@@ -9192,7 +9193,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                     maxNum--;
                 }
             }
-            return res;
+            return new ParceledListSlice<>(res);
         }
     }
 
