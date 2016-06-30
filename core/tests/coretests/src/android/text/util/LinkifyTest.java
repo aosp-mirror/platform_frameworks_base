4262e94a18979c862e31afcd20f07a3ef1e247e5
Roozbeh Pournader <roozbeh@google.com>
2016-06-30 16:07:01 -0700

    Set the locale explicitly for tests that depend on it
    Previously, the tests in DateUtilsTest and LinkifyTest assumed the US
    English locale, which would not necessarily be the case when the
    tests were run. Now we save the original locale, set the locale to
    US English, and finally restore the original when the tests are done.
    
    Bug: 29880704
    Change-Id: Ieef59e5fc128b484b0db13f35bfa4372fed650d0

--- core/tests/coretests/src/android/text/util/LinkifyTest.java
+++ core/tests/coretests/src/android/text/util/LinkifyTest.java
@@ -16,21 +16,48 @@
 
 package android.text.util;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.LocaleList;
 import android.support.test.filters.SmallTest;
 import android.test.AndroidTestCase;
 import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * LinkifyTest tests {@link Linkify}.
  */
 public class LinkifyTest extends AndroidTestCase {
 
+    private static final LocaleList LOCALE_LIST_US = new LocaleList(Locale.US);
+    private LocaleList mOriginalLocales;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mOriginalLocales = LocaleList.getDefault();
+        LocaleList.setDefault(LOCALE_LIST_US);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        LocaleList.setDefault(mOriginalLocales);
+        super.tearDown();
+    }
+
+    private Context createUsEnglishContext() {
+        final Configuration overrideConfig = new Configuration();
+        overrideConfig.setLocales(LOCALE_LIST_US);
+        return getContext().createConfigurationContext(overrideConfig);
+    }
+
     @SmallTest
     public void testNothing() throws Exception {
         TextView tv;
 
-        tv = new TextView(getContext());
+        tv = new TextView(createUsEnglishContext());
         tv.setText("Hey, foo@google.com, call 415-555-1212.");
 
         assertFalse(tv.getMovementMethod() instanceof LinkMovementMethod);
@@ -41,7 +68,7 @@ public class LinkifyTest extends AndroidTestCase {
     public void testNormal() throws Exception {
         TextView tv;
 
-        tv = new TextView(getContext());
+        tv = new TextView(createUsEnglishContext());
         tv.setAutoLinkMask(Linkify.ALL);
         tv.setText("Hey, foo@google.com, call 415-555-1212.");
 
@@ -53,7 +80,7 @@ public class LinkifyTest extends AndroidTestCase {
     public void testUnclickable() throws Exception {
         TextView tv;
 
-        tv = new TextView(getContext());
+        tv = new TextView(createUsEnglishContext());
         tv.setAutoLinkMask(Linkify.ALL);
         tv.setLinksClickable(false);
         tv.setText("Hey, foo@google.com, call 415-555-1212.");
