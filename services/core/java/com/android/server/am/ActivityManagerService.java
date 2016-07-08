967fc8dd62be9d29f89e3520ec79d4873df03481
Wei Liu <luciferleo@google.com>
2016-07-08 11:44:20 -0700

    Add BUGREPORT_OPTION_WEAR so that Wear can customize its bugreporting.
    b/30041885
    
    Change-Id: I4ffa7b5c8ddad35e32f0f31ce904848ff40ad734

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12036,6 +12036,9 @@ public final class ActivityManagerService extends ActivityManagerNative
             case ActivityManager.BUGREPORT_OPTION_REMOTE:
                 service = "bugreportremote";
                 break;
+            case ActivityManager.BUGREPORT_OPTION_WEAR:
+                service = "bugreportwear";
+                break;
         }
         if (service == null) {
             throw new IllegalArgumentException("Provided bugreport type is not correct, value: "
