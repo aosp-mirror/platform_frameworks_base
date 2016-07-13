439e5e49fb3278662889d057e578f2e08f347ad5
xiaoli.xb.zuo <xiaoli.xb.zuo@sonymobile.com>
2016-07-13 14:37:58 +0900

    Fix memory leak in MediaScannerConnection
    This patch adds releasing of MediaScannerConnection in ClientProxy
    after disconnecting and releasing of ClientProxy and
    IMediaScannerService in disconnection sequence in
    MediaScannerConnection.
    
    Bug: 30150344
    
    Change-Id: I1e162e4c9a0b7a741ace20480bcf06f3151a93bf

--- media/java/android/media/MediaScannerConnection.java
+++ media/java/android/media/MediaScannerConnection.java
@@ -133,6 +133,10 @@ public class MediaScannerConnection implements ServiceConnection {
                 }
                 try {
                     mContext.unbindService(this);
+                    if (mClient instanceof ClientProxy) {
+                        mClient = null;
+                    }
+                    mService = null;
                 } catch (IllegalArgumentException ex) {
                     if (false) {
                         Log.v(TAG, "disconnect failed: " + ex);
@@ -205,6 +209,7 @@ public class MediaScannerConnection implements ServiceConnection {
         void scanNextPath() {
             if (mNextPath >= mPaths.length) {
                 mConnection.disconnect();
+                mConnection = null;
                 return;
             }
             String mimeType = mMimeTypes != null ? mMimeTypes[mNextPath] : null;
