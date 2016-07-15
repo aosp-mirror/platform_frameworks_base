4c307258ac9f16bbc372af649ce66979dd117f33
Christopher Tate <ctate@google.com>
2016-07-15 10:53:15 -0700

    Turn down restore-time logspam
    Bug 30160827
    
    Change-Id: I14ccf0a31e5a6882bf189c28c79d1c139ec44bcc

--- services/backup/java/com/android/server/backup/BackupManagerService.java
+++ services/backup/java/com/android/server/backup/BackupManagerService.java
@@ -5501,11 +5501,11 @@ public class BackupManagerService {
 
                         // If the policy is satisfied, go ahead and set up to pipe the
                         // data to the agent.
-                        if (DEBUG && okay && mAgent != null) {
+                        if (MORE_DEBUG && okay && mAgent != null) {
                             Slog.i(TAG, "Reusing existing agent instance");
                         }
                         if (okay && mAgent == null) {
-                            if (DEBUG) Slog.d(TAG, "Need to launch agent for " + pkg);
+                            if (MORE_DEBUG) Slog.d(TAG, "Need to launch agent for " + pkg);
 
                             try {
                                 mTargetApp = mPackageManager.getApplicationInfo(pkg, 0);
