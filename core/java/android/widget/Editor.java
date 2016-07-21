133dfc0068d9f14eb9907a4ca77e4e87e8a705e4
Keisuke Kuroyanagi <ksk@google.com>
2016-07-21 18:07:23 +0900

    Fix: Selection created by long tap and drag can be empty.
    Bug: 21302473
    Change-Id: Ia81729a402957d25b40d203af3ce69c166f8dc91

--- core/java/android/widget/Editor.java
+++ core/java/android/widget/Editor.java
@@ -5403,10 +5403,12 @@ public class Editor {
                 // Expanding with start handle.
                 offset = getWordStart(offset);
                 startOffset = getWordEnd(mStartOffset);
+                if (startOffset == offset) {
+                    offset = getNextCursorOffset(offset, false);
+                }
             }
             mLineSelectionIsOn = currLine;
-            Selection.setSelection((Spannable) mTextView.getText(),
-                    startOffset, offset);
+            Selection.setSelection((Spannable) mTextView.getText(), startOffset, offset);
         }
 
         private void updateParagraphBasedSelection(MotionEvent event) {
