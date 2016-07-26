c6fe61c59c5a3a6ae691256c9afdde3820e3dd9e
Jason Monk <jmonk@google.com>
2016-07-26 13:04:50 -0400

    Fix double status bar icons
    Don't allow the status bar icon slot list to be changed because
    it confuses the relationship between StatusBarIconList and
    StatusBarIconController.  Set it in the constructor to enforce
    this.
    
    Change-Id: Ieeea0a9efad88179d1cccc0e5702899333de2e72
    Fixes: 28524184

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -3522,7 +3522,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         repositionNavigationBar();
         updateRowStates();
-        mIconController.defineSlots();
         mScreenPinningRequest.onConfigurationChanged();
         mNetworkController.onConfigurationChanged();
     }
