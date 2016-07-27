40f8b9251831c5f009fd476b1a3d216e126d3ef0
Svetoslav Ganov <svetoslavganov@google.com>
2016-07-26 18:23:01 -0700

    Add WiFi toggle prompts - framework
    If permission review is enabled toggling WiFi on or off
    results in a user prompt to collect a consent. This applies
    only to legacy apps, i.e. ones that don't support runtime
    permissions as they target SDK 22.
    
    bug:28715749
    
    Change-Id: I020601e208c0736005bcabc7b7b77b96eee45b03
    (cherry picked from commit d3663c04a721e0848dd3e9179c84ba585d91839d)

--- wifi/java/android/net/wifi/WifiManager.java
+++ wifi/java/android/net/wifi/WifiManager.java
@@ -19,6 +19,7 @@ package android.net.wifi;
 import android.annotation.SdkConstant;
 import android.annotation.SdkConstant.SdkConstantType;
 import android.annotation.SystemApi;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.DhcpInfo;
@@ -559,6 +560,28 @@ public class WifiManager {
      */
     @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
     public static final String ACTION_PICK_WIFI_NETWORK = "android.net.wifi.PICK_WIFI_NETWORK";
+
+    /**
+     * Activity Action: Show UI to get user approval to enable WiFi.
+     * <p>Input: {@link android.content.Intent#EXTRA_PACKAGE_NAME} string extra with
+     *           the name of the app requesting the action.
+     * <p>Output: Nothing.
+     *
+     * @hide
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_REQUEST_ENABLE = "android.net.wifi.action.REQUEST_ENABLE";
+
+    /**
+     * Activity Action: Show UI to get user approval to disable WiFi.
+     * <p>Input: {@link android.content.Intent#EXTRA_PACKAGE_NAME} string extra with
+     *           the name of the app requesting the action.
+     * <p>Output: Nothing.
+     *
+     * @hide
+     */
+    @SdkConstant(SdkConstantType.ACTIVITY_INTENT_ACTION)
+    public static final String ACTION_REQUEST_DISABLE = "android.net.wifi.action.REQUEST_DISABLE";
 
     /**
      * Internally used Wi-Fi lock mode representing the case were no locks are held.
@@ -1443,7 +1466,7 @@ public class WifiManager {
      */
     public boolean setWifiEnabled(boolean enabled) {
         try {
-            return mService.setWifiEnabled(enabled);
+            return mService.setWifiEnabled(mContext.getOpPackageName(), enabled);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
