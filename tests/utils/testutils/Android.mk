d3868cfc63cd814e43367586f5706f39ec1cd5a9
Mitchell Wills <mwills@google.com>
2016-07-28 17:04:38 -0700

    Don't pull in external/junit through mockito
    android-support-test provides a different version of junit
    
    FIXED=30481239
    
    Change-Id: I6d8f52d5f82b4c9905da8cc4a6ca79216eba9e26

--- tests/utils/testutils/Android.mk
+++ tests/utils/testutils/Android.mk
@@ -25,6 +25,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under,java)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-test \
-    mockito-target
+    mockito-target-minus-junit4
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
