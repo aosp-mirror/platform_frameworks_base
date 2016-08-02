2dd20a61e959f9a0acb0639664e063101b6e3bac
Yigit Boyar <yboyar@google.com>
2016-08-01 17:42:28 -0700

    Target API check for margin params fix
    Prior to N, our widgets were not converting MarginLayoutParams
    properly between ViewGroups. The fix intrudced some issues in
    older apps as the broken conversion code would hide developer
    errors. This CL guards the change with a target API check so
    that we don't affect older apps.
    
    Bug: 30378230
    Change-Id: I215281d261b553c3b4cedcd29ea0a861df809471

--- core/java/android/widget/LinearLayout.java
+++ core/java/android/widget/LinearLayout.java
@@ -1844,13 +1844,14 @@ public class LinearLayout extends ViewGroup {
 
     @Override
     protected LayoutParams generateLayoutParams(ViewGroup.LayoutParams lp) {
-        if (lp instanceof LayoutParams) {
-            return new LayoutParams((LayoutParams) lp);
-        } else if (lp instanceof MarginLayoutParams) {
-            return new LayoutParams((MarginLayoutParams) lp);
-        } else {
-            return new LayoutParams(lp);
+        if (sPreserveMarginParamsInLayoutParamConversion) {
+            if (lp instanceof LayoutParams) {
+                return new LayoutParams((LayoutParams) lp);
+            } else if (lp instanceof MarginLayoutParams) {
+                return new LayoutParams((MarginLayoutParams) lp);
+            }
         }
+        return new LayoutParams(lp);
     }
 
 
