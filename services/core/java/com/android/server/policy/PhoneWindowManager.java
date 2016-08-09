34331b7ca97f6cffe3bd91c8281732cc3379c7e8
Doris Ling <dling@google.com>
2016-08-09 12:02:28 -0700

    Fix fingerprint swipe gesture settings for secondary users.
    Change to use getIntForUser() when reading the secure settings value for
    fingerprint swipe, as PhoneWindowManager always read as user 0 by
    default.
    
    Change-Id: I1d08a970b031f7bce4864c982e791678e1ef221b
    Fixes: 30709756

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -7316,8 +7316,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     private boolean areSystemNavigationKeysEnabled() {
-        return Settings.Secure.getInt(mContext.getContentResolver(),
-                Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED, 0) == 1;
+        return Settings.Secure.getIntForUser(mContext.getContentResolver(),
+                Settings.Secure.SYSTEM_NAVIGATION_KEYS_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
     }
 
     @Override
