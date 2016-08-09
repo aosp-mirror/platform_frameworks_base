6a85d3bddc92f0e25cbea977648cfb4d3beedc96
jackqdyulei <jackqdyulei@google.com>
2016-08-09 14:30:10 -0700

    Change the function "setPolicyLimitBytes" and "setPolicyWarningBytes".
    Bug: 30560513
    
    Change two functions to make sure that "warningBytes <= limitBytes".
    
    Change-Id: I2e1809442f87dfa791c39f6769035a9bf81beb8c

--- packages/SettingsLib/src/com/android/settingslib/NetworkPolicyEditor.java
+++ packages/SettingsLib/src/com/android/settingslib/NetworkPolicyEditor.java
@@ -167,7 +167,7 @@ public class NetworkPolicyEditor {
         return (policy != null) ? policy.warningBytes : WARNING_DISABLED;
     }
 
-    public void setPolicyWarningBytes(NetworkTemplate template, long warningBytes) {
+    private void setPolicyWarningBytesInner(NetworkTemplate template, long warningBytes) {
         final NetworkPolicy policy = getOrCreatePolicy(template);
         policy.warningBytes = warningBytes;
         policy.inferred = false;
@@ -175,12 +175,28 @@ public class NetworkPolicyEditor {
         writeAsync();
     }
 
+    public void setPolicyWarningBytes(NetworkTemplate template, long warningBytes) {
+        long limitBytes = getPolicyLimitBytes(template);
+
+        // If the warningBytes are larger than limitBytes, set the warningBytes to limitBytes
+        warningBytes = Math.min(warningBytes, limitBytes);
+        setPolicyWarningBytesInner(template, warningBytes);
+    }
+
     public long getPolicyLimitBytes(NetworkTemplate template) {
         final NetworkPolicy policy = getPolicy(template);
         return (policy != null) ? policy.limitBytes : LIMIT_DISABLED;
     }
 
+
     public void setPolicyLimitBytes(NetworkTemplate template, long limitBytes) {
+        long warningBytes = getPolicyWarningBytes(template);
+
+        // If the warningBytes are larger than limitBytes, set the warningBytes to limitBytes
+        if (warningBytes > limitBytes) {
+            setPolicyWarningBytesInner(template, limitBytes);
+        }
+
         final NetworkPolicy policy = getOrCreatePolicy(template);
         policy.limitBytes = limitBytes;
         policy.inferred = false;
