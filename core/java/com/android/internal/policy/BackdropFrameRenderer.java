9a7d0a5f8472bcefe21f334dead30e3867908374
Jorim Jaggi <jjaggi@google.com>
2016-08-09 13:47:20 +0200

    Fix navigation bar in seascape in BackdropFrameRenderer
    Change-Id: I922aaf835011b236f4b4d34f249304e335c3318b
    Fixes: 30670517

--- core/java/com/android/internal/policy/BackdropFrameRenderer.java
+++ core/java/com/android/internal/policy/BackdropFrameRenderer.java
@@ -385,7 +385,7 @@ public class BackdropFrameRenderer extends Thread implements Choreographer.Frame
             final int size = DecorView.getNavBarSize(bottomInset, rightInset, leftInset);
             if (DecorView.isNavBarToRightEdge(bottomInset, rightInset)) {
                 mNavigationBarColor.setBounds(width - size, 0, width, height);
-            } else if (DecorView.isNavBarToLeftEdge(bottomInset, rightInset)) {
+            } else if (DecorView.isNavBarToLeftEdge(bottomInset, leftInset)) {
                 mNavigationBarColor.setBounds(0, 0, size, height);
             } else {
                 mNavigationBarColor.setBounds(0, height - size, width, height);
