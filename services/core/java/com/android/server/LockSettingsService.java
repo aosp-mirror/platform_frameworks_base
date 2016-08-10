d7a94a8ebb52fb3e761894b7aa6ae14359c40d09
Jim Miller <jaggies@google.com>
2016-08-10 15:43:17 -0700

    Fix vulnerability in LockSettings service
    Fixes bug 30003944
    
    Change-Id: I8700d4424c6186c8d5e71d2fdede0223ad86904d
    (cherry picked from commit 2d71384a139ae27cbc7b57f06662bf6ee2010f2b)

--- services/core/java/com/android/server/LockSettingsService.java
+++ services/core/java/com/android/server/LockSettingsService.java
@@ -1215,6 +1215,9 @@ public class LockSettingsService extends ILockSettings.Stub {
     private VerifyCredentialResponse doVerifyPattern(String pattern, boolean hasChallenge,
             long challenge, int userId) throws RemoteException {
        checkPasswordReadPermission(userId);
+       if (TextUtils.isEmpty(pattern)) {
+           throw new IllegalArgumentException("Pattern can't be null or empty");
+       }
        CredentialHash storedHash = mStorage.readPatternHash(userId);
        return doVerifyPattern(pattern, storedHash, hasChallenge, challenge, userId);
     }
@@ -1306,6 +1309,9 @@ public class LockSettingsService extends ILockSettings.Stub {
     private VerifyCredentialResponse doVerifyPassword(String password, boolean hasChallenge,
             long challenge, int userId) throws RemoteException {
        checkPasswordReadPermission(userId);
+       if (TextUtils.isEmpty(password)) {
+           throw new IllegalArgumentException("Password can't be null or empty");
+       }
        CredentialHash storedHash = mStorage.readPasswordHash(userId);
        return doVerifyPassword(password, storedHash, hasChallenge, challenge, userId);
     }
