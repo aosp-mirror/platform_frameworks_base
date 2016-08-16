7fe5776c3ed16343191e70dfe10977f2aac01d20
Simao Gomes Viana <xdevs23@outlook.com>
2016-08-16 15:36:32 +0200

    ViewConfiguration: Add support for max/min fling velocity sysproperties
    Change-Id: Ia9ee8a5fdd5063691fc3cf08097f10f80306553e
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -26,6 +26,7 @@ import android.content.res.Resources;
 import android.graphics.Point;
 import android.os.Build;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 import android.provider.Settings;
 import android.util.DisplayMetrics;
 import android.util.SparseArray;
@@ -448,10 +449,49 @@ public class ViewConfiguration {
 
         mDoubleTapTouchSlop = mTouchSlop;
 
-        mMinimumFlingVelocity = res.getDimensionPixelSize(
-                com.android.internal.R.dimen.config_viewMinFlingVelocity);
-        mMaximumFlingVelocity = res.getDimensionPixelSize(
-                com.android.internal.R.dimen.config_viewMaxFlingVelocity);
+        // Modification by xdevs23 for better responsiveness using
+        // system.prop
+        String minFlingVeloProp = "ro.min.fling_velocity", // Min fling prop
+               maxFlingVeloProp = "ro.max.fling_velocity"; // Max fling prop
+        // Get the properties
+        String minFlingVeloSysProp = SystemProperties.get(minFlingVeloProp),
+               maxFlingVeloSysProp = SystemProperties.get(maxFlingVeloProp);
+        boolean isMaxFlingVeloPredefined = false,
+                isMinFlingVeloPredefined = false;
+        int minFlingVeloTmp = 0,
+            maxFlingVeloTmp = 0;
+
+        // Check whether the property values are valid
+        if(minFlingVeloSysProp != null && (!minFlingVeloSysProp.isEmpty()) &&
+            isNumeric(minFlingVeloSysProp)) {
+            minFlingVeloTmp = Integer.parseInt(minFlingVeloSysProp);
+            isMinFlingVeloPredefined = true;
+        }
+
+        if(maxFlingVeloSysProp != null && (!maxFlingVeloSysProp.isEmpty()) &&
+            isNumeric(maxFlingVeloSysProp)) {
+            maxFlingVeloTmp = Integer.parseInt(maxFlingVeloSysProp);
+            isMaxFlingVeloPredefined = true;
+        }
+
+        // Use config values if no prop available or invalid
+        if(!isMinFlingVeloPredefined && minFlingVeloTmp == 0)
+            minFlingVeloTmp = res.getDimensionPixelSize(
+                    com.android.internal.R.dimen.config_viewMinFlingVelocity);
+        if(!isMaxFlingVeloPredefined && maxFlingVeloTmp == 0)
+            maxFlingVeloTmp = res.getDimensionPixelSize(
+                    com.android.internal.R.dimen.config_viewMaxFlingVelocity);
+
+        // Check again for availability, otherwise use default values
+        if(minFlingVeloTmp * maxFlingVeloTmp == 0) {
+            minFlingVeloTmp = MINIMUM_FLING_VELOCITY;
+            maxFlingVeloTmp = MAXIMUM_FLING_VELOCITY;
+        }
+
+        // Assign the final variables
+        mMinimumFlingVelocity = minFlingVeloTmp;
+        mMaximumFlingVelocity = maxFlingVeloTmp;
+
         mGlobalActionsKeyTimeout = res.getInteger(
                 com.android.internal.R.integer.config_globalActionsKeyTimeout);
 
@@ -468,6 +508,18 @@ public class ViewConfiguration {
 
         mScreenshotChordKeyTimeout = res.getInteger(
                 com.android.internal.R.integer.config_screenshotChordKeyTimeout);
+    }
+
+    /**
+     * @hide
+     */
+    public static boolean isNumeric(String string) {
+        try {
+            Integer.parseInt(string);
+        } catch(NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 
     /**
