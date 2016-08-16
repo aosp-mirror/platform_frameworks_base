51dd1e51ecb11af67e4ea1fe41d600c5d7eaaf15
Chris Craik <ccraik@google.com>
2016-08-16 13:35:36 -0700

    Add misc apct perf tests
    Includes test for Log.d(), and System.nanoTime().
    
    Also unifies Android.mk format, and package usage.
    
    Change-Id: I90039eff1c2c2d59afe75c4a808a29b67f3176f3

--- apct-tests/perftests/core/src/android/widget/LayoutPerfTest.java
+++ apct-tests/perftests/core/src/android/widget/LayoutPerfTest.java
@@ -27,7 +27,7 @@ import android.support.test.rule.ActivityTestRule;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.android.core.frameworks.perftests.R;
+import android.core.perftests.R;
 
 import org.junit.Rule;
 import org.junit.Test;
