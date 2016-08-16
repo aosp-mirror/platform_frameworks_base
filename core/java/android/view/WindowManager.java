fea963edeeb60c76465c0d644078def191f41e0f
Chong Zhang <chz@google.com>
2016-08-15 17:14:16 -0700

    Add new window type TYPE_DRAWN_APPLICATION
    This type behaves like a normal TYPE_APPLICATION, except that WM
    will always wait for it to be drawn before starting a transition.
    
    WM always waits for TYPE_BASE_APPLICATION (main window), but for
    TYPE_APPLICATION, it only waits if the window relayouts to visible
    and gets a surface before the main window is drawn. If main window
    itself is ready very fast, transition could start without the other
    window.
    
    bug: 30830849
    Change-Id: Ife71a9812db7c8eba6ee4ead10ce4f31d9e93b40

--- core/java/android/view/WindowManager.java
+++ core/java/android/view/WindowManager.java
@@ -221,6 +221,7 @@ public interface WindowManager extends ViewManager {
          * @see #TYPE_BASE_APPLICATION
          * @see #TYPE_APPLICATION
          * @see #TYPE_APPLICATION_STARTING
+         * @see #TYPE_DRAWN_APPLICATION
          * @see #TYPE_APPLICATION_PANEL
          * @see #TYPE_APPLICATION_MEDIA
          * @see #TYPE_APPLICATION_SUB_PANEL
@@ -244,6 +245,7 @@ public interface WindowManager extends ViewManager {
             @ViewDebug.IntToString(from = TYPE_BASE_APPLICATION, to = "TYPE_BASE_APPLICATION"),
             @ViewDebug.IntToString(from = TYPE_APPLICATION, to = "TYPE_APPLICATION"),
             @ViewDebug.IntToString(from = TYPE_APPLICATION_STARTING, to = "TYPE_APPLICATION_STARTING"),
+            @ViewDebug.IntToString(from = TYPE_DRAWN_APPLICATION, to = "TYPE_DRAWN_APPLICATION"),
             @ViewDebug.IntToString(from = TYPE_APPLICATION_PANEL, to = "TYPE_APPLICATION_PANEL"),
             @ViewDebug.IntToString(from = TYPE_APPLICATION_MEDIA, to = "TYPE_APPLICATION_MEDIA"),
             @ViewDebug.IntToString(from = TYPE_APPLICATION_SUB_PANEL, to = "TYPE_APPLICATION_SUB_PANEL"),
@@ -313,6 +315,13 @@ public interface WindowManager extends ViewManager {
          * In multiuser systems shows on all users' windows.
          */
         public static final int TYPE_APPLICATION_STARTING = 3;
+
+        /**
+         * Window type: a variation on TYPE_APPLICATION that ensures the window
+         * manager will wait for this window to be drawn before the app is shown.
+         * In multiuser systems shows only on the owning user's window.
+         */
+        public static final int TYPE_DRAWN_APPLICATION = 4;
 
         /**
          * End of types of application windows.
