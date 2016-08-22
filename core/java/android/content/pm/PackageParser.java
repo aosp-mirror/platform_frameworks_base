ef8d8ea24ab3b2319ac1eed1c67ebacb0a6c726e
Todd Kennedy <toddke@google.com>
2016-08-22 15:44:00 -0700

    No need to check package name
    Change-Id: I8c403ca2bc38f2c7191b6e9a709286ac7e450ac4
    Fixes: 30958291
    (cherry picked from commit afb5269ad6a507a3230b519b3156ac0bcc5fc3a0)

--- core/java/android/content/pm/PackageParser.java
+++ core/java/android/content/pm/PackageParser.java
@@ -2290,11 +2290,7 @@ public class PackageParser {
             b.append(cls);
             return b.toString().intern();
         }
-        if (c >= 'a' && c <= 'z') {
-            return cls.intern();
-        }
-        outError[0] = "Bad class name " + cls + " in package " + pkg;
-        return null;
+        return cls.intern();
     }
 
     private static String buildCompoundName(String pkg,
@@ -2755,12 +2751,7 @@ public class PackageParser {
         }
 
         if (ai.name != null) {
-            ai.className = buildClassName(pkgName, ai.name, outError);
-            if (ai.className == null) {
-                sa.recycle();
-                mParseError = PackageManager.INSTALL_PARSE_FAILED_MANIFEST_MALFORMED;
-                return false;
-            }
+            ai.className = ai.name;
         }
 
         String manageSpaceActivity = sa.getNonConfigurationString(
