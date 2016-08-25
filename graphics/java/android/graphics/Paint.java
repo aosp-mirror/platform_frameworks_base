8b9ed44f8d559be2590b25cb4b72d2aefd3b3784
Chet Haase <chet@google.com>
2016-08-25 15:35:53 -0700

    Various doc cleanups
    Issue #6439958  More R.attr breakage
    Issue #918412  android.graphics.Paint.FontMetricsInt fields & methods need doc
    Issue #917387  javadoc bug: Canvas.EdgeType has minimal documentation
    Issue #18948460 [DAC-bug] LinearLayout xml layout_weight parameter is missing description
    
    Change-Id: Idb289ca778b3f316923757405460031ce7a5e1e9

--- graphics/java/android/graphics/Paint.java
+++ graphics/java/android/graphics/Paint.java
@@ -1641,10 +1641,27 @@ public class Paint {
      * integers.
      */
     public static class FontMetricsInt {
+        /**
+         * The maximum distance above the baseline for the tallest glyph in
+         * the font at a given text size.
+         */
         public int   top;
+        /**
+         * The recommended distance above the baseline for singled spaced text.
+         */
         public int   ascent;
+        /**
+         * The recommended distance below the baseline for singled spaced text.
+         */
         public int   descent;
+        /**
+         * The maximum distance below the baseline for the lowest glyph in
+         * the font at a given text size.
+         */
         public int   bottom;
+        /**
+         * The recommended additional space to add between lines of text.
+         */
         public int   leading;
 
         @Override public String toString() {
