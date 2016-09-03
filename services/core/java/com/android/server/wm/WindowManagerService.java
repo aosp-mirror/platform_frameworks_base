14af62567400ef46854f44df3bd1ef5b8a2d70f9
Andreas Gampe <agampe@google.com>
2016-09-03 09:48:04 -0700

    WindowManagerService: Fix rename
    Follow-up to 71c510236c92c14efae6ff00e68cf9ad38f51c32. Required
    because of a6cc361e34337c7fe293a56713910faf0f2f302c.
    
    Bug: 30150688
    Test: m
    Change-Id: I68e8951c354a1f590040bffb0259198cf80ae463

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1629,7 +1629,7 @@ public class WindowManagerService extends IWindowManager.Stub
             } else if (type == TYPE_TOAST) {
                 // Apps targeting SDK above N MR1 cannot arbitrary add toast windows.
                 addToastWindowRequiresToken = doesAddToastWindowRequireToken(attrs.packageName,
-                        callingUid, attachedWindow);
+                        callingUid, parentWindow);
                 if (addToastWindowRequiresToken && token.windowType != TYPE_TOAST) {
                     Slog.w(TAG_WM, "Attempted to add a toast window with bad token "
                             + attrs.token + ".  Aborting.");
