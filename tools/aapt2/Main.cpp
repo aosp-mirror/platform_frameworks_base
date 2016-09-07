5cb0530ca444ad070adfa10005bb9cabd4dbeeb7
Adam Lesinski <adamlesinski@google.com>
2016-09-06 17:34:06 -0700

    AAPT2: Update version and readme
    Update version to 2.2 and update readme to reflect added
    support for inline complex XML resources.
    
    Change-Id: If825c96541952e95860d34390c6c36704a24129b

--- tools/aapt2/Main.cpp
+++ tools/aapt2/Main.cpp
@@ -25,7 +25,7 @@ namespace aapt {
 static const char* sMajorVersion = "2";
 
 // Update minor version whenever a feature or flag is added.
-static const char* sMinorVersion = "1";
+static const char* sMinorVersion = "2";
 
 int printVersion() {
     std::cerr << "Android Asset Packaging Tool (aapt) "
