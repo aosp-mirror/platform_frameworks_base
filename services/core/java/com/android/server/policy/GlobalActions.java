3032dbd3e122237b24545fb425f9c3d95f0b7fd5
Marko Man <darkobas@gmail.com>
2016-09-09 21:10:14 +0200

    base: remove telephony check from airplane toggle in globalactions
    Change-Id: Ia87f8e4eeb2f75ba362f6ab02242a4e1d059ae2d

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -1435,7 +1435,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private void onAirplaneModeChanged() {
         // Let the service state callbacks handle the state.
-        if (mHasTelephony) return;
+        // if (mHasTelephony) return;
 
         boolean airplaneModeOn = Settings.Global.getInt(
                 mContext.getContentResolver(),
