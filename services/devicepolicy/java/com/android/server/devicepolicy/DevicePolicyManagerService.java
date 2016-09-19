6ad5f92512462f774a2ff7e59abdf5edbfd215b3
Makoto Onuki <omakoto@google.com>
2016-09-19 14:23:02 -0700

    Fix DPM unit tests, also fix a log message.
    Bug 31446501
    
    Change-Id: I37debbe2f4e983fb8bad026f8dd9bd91b7448dce

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -3010,8 +3010,8 @@ public class DevicePolicyManagerService extends IDevicePolicyManager.Stub {
             throw new IllegalStateException(e);
         }
         if (ai == null) {
-            throw new IllegalStateException("Couldn't find package to remove admin "
-                    + packageName + " " + userHandle);
+            throw new IllegalStateException("Couldn't find package: "
+                    + packageName + " on user " + userHandle);
         }
         return (ai.flags & ApplicationInfo.FLAG_TEST_ONLY) != 0;
     }
