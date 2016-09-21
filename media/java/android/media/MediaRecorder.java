5b168a0c549a164898b5f1568b155d02ccce77cd
Hangyu Kuang <hkuang@google.com>
2016-09-21 16:02:47 -0700

    media: Unhide mpeg2ts format for streaming output format support.
    Test: Run the camera recorder and output correct mpeg2ts stream.
    
    Bug: 21620607
    Change-Id: Ic79758d568e984d55e822d4b6af8716b856dd7da

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -389,8 +389,8 @@ public class MediaRecorder
         /** @hide Stream over a socket, limited to a single stream */
         public static final int OUTPUT_FORMAT_RTP_AVP = 7;
 
-        /** @hide H.264/AAC data encapsulated in MPEG2/TS */
-        public static final int OUTPUT_FORMAT_MPEG2TS = 8;
+        /** H.264/AAC data encapsulated in MPEG2/TS */
+        public static final int MPEG_2_TS = 8;
 
         /** VP8/VORBIS data in a WEBM container */
         public static final int WEBM = 9;
