a7fcb2bc2a65da3743914727adb0df89a4eb0b66
Torne (Richard Coles) <torne@google.com>
2016-09-23 16:41:42 +0100

    Add the webview zygote socket to the zygote whitelist.
    Allow the webview zygote socket to be open at time of fork, so that
    webview renderers can be forked successfully.
    
    Test: WebView functions correctly with multiprocess dev setting enabled
    Bug: 21643067
    Change-Id: I312846433a43a8e4548cb2f98b96b56acf9f5333

--- core/jni/fd_utils-inl.h
+++ core/jni/fd_utils-inl.h
@@ -51,6 +51,7 @@ static const char* kPathWhitelist[] = {
   "/dev/null",
   "/dev/socket/zygote",
   "/dev/socket/zygote_secondary",
+  "/dev/socket/webview_zygote",
   "/sys/kernel/debug/tracing/trace_marker",
   "/system/framework/framework-res.apk",
   "/dev/urandom",
