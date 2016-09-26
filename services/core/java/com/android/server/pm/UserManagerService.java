d91dd06591a761aa9e3eab519a23b40bf2b0dbcc
bohu <bohu@google.com>
2016-09-26 16:20:07 -0700

    Emulator: Fix CTS failure caused by removed MANAGE_USERS permission in Shell
    BUG: 34132936
    BUG: 31648062
    Change-Id: I9679c18c0e3611ebc9779408826f182ea8f1e88b
    (cherry picked from commit 57f47e8490b68678fd123cdc111d5073ec994382)
    (cherry picked from commit 12d23a13bb45cd4f83395f0f45f3382d7571effe)

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -573,7 +573,7 @@ public class UserManagerService extends IUserManager.Stub {
     @Override
     public int[] getProfileIds(int userId, boolean enabledOnly) {
         if (userId != UserHandle.getCallingUserId()) {
-            checkManageUsersPermission("getting profiles related to user " + userId);
+            checkManageOrCreateUsersPermission("getting profiles related to user " + userId);
         }
         final long ident = Binder.clearCallingIdentity();
         try {
