4d04b1a446cb8c3dc5b42ddd58f82a6127521622
Doris Liu <tianliu@google.com>
2016-09-26 15:12:46 -0700

    Allow null target in ObjectAnimator if the target has never been set
    BUG: 31684571
    Test: http://ag/1484115
    
    Change-Id: I867fc3f93ddf70c58ae4387317ac875c5bafbebe

--- core/java/android/animation/ObjectAnimator.java
+++ core/java/android/animation/ObjectAnimator.java
@@ -977,8 +977,9 @@ public final class ObjectAnimator extends ValueAnimator {
     @Override
     void animateValue(float fraction) {
         final Object target = getTarget();
-        if (target == null) {
-            // We lost the target reference, cancel and clean up.
+        if (mTarget != null && target == null) {
+            // We lost the target reference, cancel and clean up. Note: we allow null target if the
+            /// target has never been set.
             cancel();
             return;
         }
