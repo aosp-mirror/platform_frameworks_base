8d1667ea7db0f5bc480570dd6e272283c12288c0
GWX <kagwind@gmail.com>
2016-09-29 13:58:45 +0200

    [1/3] Flip to Mute/Reject Call

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4363,6 +4363,14 @@ public final class Settings {
         public static final String STATUS_BAR_TRACK_TICKER = "status_bar_track_ticker";
 
         /**
+         * Whether flip action during incomming call should mute or dismiss
+         * the call (mute = 0, dismiss = 1, nothing = 2 (default))
+         *
+         * @hide
+         */
+        public static final String CALL_FLIP_ACTION_KEY = "call_flip_action_key";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
