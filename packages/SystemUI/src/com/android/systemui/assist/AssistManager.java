8f326c540e5bed7d214fe3687aaed435fab27a6b
maxwen <max.weninger@gmail.com>
2016-10-11 01:05:46 +0200

    SystemUI: AssistManager - how is this supposed to ever work?
    nobody ever creates mView before
    
    Change-Id: Icc7ddfa420ae44dd0af8d8c8bf5dc93d957f643e

--- packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
+++ packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
@@ -133,6 +133,9 @@ public class AssistManager {
             return;
         }
 
+        if (mView == null) {
+            onConfigurationChanged();
+        }
         final boolean isService = assistComponent.equals(getVoiceInteractorComponentName());
         if (!isService || (!isVoiceSessionRunning() && shouldShowOrb())) {
             showOrb(assistComponent, isService);
