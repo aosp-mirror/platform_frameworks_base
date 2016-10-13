248c3ab8323c774d213df75ac528d4ab96107fe4
Sungsoo <sungsoo@google.com>
2016-10-13 12:24:16 +0900

    DO NOT MERGE) ExifInterface: Provide backward compatibility
    ExifInterface.saveAttribute() didn't throw UnsupportedOperationException
    before. Use IOException instead of UnsupportedOperationException for
    backward compatibility.
    
    Bug: 30936376, Bug: 32068647, Bug: 31319086
    Change-Id: Iacc7b4d91d49edd7bece8f2e738a633a91025eca
    (cherry picked from commit 7ce9243087b04bce719e1f3d10a14b9b26b8ea81)

--- media/java/android/media/ExifInterface.java
+++ media/java/android/media/ExifInterface.java
@@ -1372,8 +1372,7 @@ public class ExifInterface {
      */
     public void saveAttributes() throws IOException {
         if (!mIsSupportedFile) {
-            throw new UnsupportedOperationException(
-                    "ExifInterface only supports saving attributes on JPEG formats.");
+            throw new IOException("ExifInterface only supports saving attributes on JPEG formats.");
         }
         // Keep the thumbnail in memory
         mThumbnailBytes = getThumbnail();
