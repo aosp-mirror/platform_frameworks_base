07bcab787ea7ce65081dffe7da196f872a1be37a
Wale Ogunwale <ogunwale@google.com>
2016-10-14 15:30:09 -0700

    Add windows to window tokens in expected z-order
    Decouple the logic for adding window to a position in the parent
    window token from the position we are adding the window to in the
    window list. The window token now adds the windows in order based
    on the rules the rest of the system is using which makes the code
    a little more straightforward to follow.
    
    Test: bit FrameworksServicesTests:com.android.server.wm.AppWindowTokenTests
    Change-Id: Ic9b724fba02279a0c4e92508d39e5e35171b6d8d

--- services/tests/servicestests/src/com/android/server/wm/AppWindowTokenTests.java
+++ services/tests/servicestests/src/com/android/server/wm/AppWindowTokenTests.java
@@ -29,17 +29,19 @@ import android.view.IWindow;
 import android.view.WindowManager;
 
 import static android.view.WindowManager.LayoutParams.FIRST_SUB_WINDOW;
+import static android.view.WindowManager.LayoutParams.TYPE_APPLICATION;
 import static android.view.WindowManager.LayoutParams.TYPE_APPLICATION_STARTING;
 import static android.view.WindowManager.LayoutParams.TYPE_BASE_APPLICATION;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Tests for the {@link WindowState} class.
  *
- * Build: mmma -j32 frameworks/base/services/tests/servicestests
- * Install: adb install -r out/target/product/$TARGET_PRODUCT/data/app/FrameworksServicesTests/FrameworksServicesTests.apk
- * Run: adb shell am instrument -w -e class com.android.server.wm.AppWindowTokenTests com.android.frameworks.servicestests/android.support.test.runner.AndroidJUnitRunner
+ * Build/Install/Run:
+ *  bit FrameworksServicesTests:com.android.server.wm.AppWindowTokenTests
  */
 @SmallTest
 @Presubmit
@@ -53,6 +55,36 @@ public class AppWindowTokenTests {
     public void setUp() throws Exception {
         final Context context = InstrumentationRegistry.getTargetContext();
         sWm = TestWindowManagerPolicy.getWindowManagerService(context);
+    }
+
+    @Test
+    public void testAddWindow_Order() throws Exception {
+        final TestAppWindowToken token = new TestAppWindowToken();
+
+        assertEquals(0, token.getWindowsCount());
+
+        final WindowState win1 = createWindow(null, TYPE_APPLICATION, token);
+        final WindowState startingWin = createWindow(null, TYPE_APPLICATION_STARTING, token);
+        final WindowState baseWin = createWindow(null, TYPE_BASE_APPLICATION, token);
+        final WindowState win4 = createWindow(null, TYPE_APPLICATION, token);
+
+        token.addWindow(win1);
+        token.addWindow(startingWin);
+        token.addWindow(baseWin);
+        token.addWindow(win4);
+
+        // Should not contain the windows that were added above.
+        assertEquals(4, token.getWindowsCount());
+        assertTrue(token.hasWindow(win1));
+        assertTrue(token.hasWindow(startingWin));
+        assertTrue(token.hasWindow(baseWin));
+        assertTrue(token.hasWindow(win4));
+
+        // The starting window should be on-top of all other windows.
+        assertEquals(startingWin, token.getLastChild());
+
+        // The base application window should be below all other windows.
+        assertEquals(baseWin, token.getFirstChild());
     }
 
     @Test
@@ -92,6 +124,14 @@ public class AppWindowTokenTests {
 
         boolean hasWindow(WindowState w) {
             return mChildren.contains(w);
+        }
+
+        WindowState getFirstChild() {
+            return mChildren.getFirst();
+        }
+
+        WindowState getLastChild() {
+            return mChildren.getLast();
         }
     }
 }
--- services/tests/servicestests/src/com/android/server/wm/WindowTokenTests.java
+++ services/tests/servicestests/src/com/android/server/wm/WindowTokenTests.java
@@ -41,9 +41,8 @@ import static org.mockito.Mockito.mock;
 /**
  * Tests for the {@link WindowState} class.
  *
- * Build: mmma -j32 frameworks/base/services/tests/servicestests
- * Install: adb install -r out/target/product/$TARGET_PRODUCT/data/app/FrameworksServicesTests/FrameworksServicesTests.apk
- * Run: adb shell am instrument -w -e class com.android.server.wm.WindowTokenTests com.android.frameworks.servicestests/android.support.test.runner.AndroidJUnitRunner
+ * Build/Install/Run:
+ *  bit FrameworksServicesTests:com.android.server.wm.WindowTokenTests
  */
 @SmallTest
 @Presubmit
