20f75d60c522ceee69ec4fd623c63dddfde12579
Roman Birg <romanbirg@gmail.com>
2016-10-23 06:43:28 +0200

    base: Utils: Add isPackageInstalled boolean
    Change-Id: Ibadea83b68c012c08e47c3622ba96a37c8ddf1b9

--- core/java/com/android/internal/util/atom/Utils.java
+++ core/java/com/android/internal/util/atom/Utils.java
@@ -18,7 +18,9 @@ package com.android.internal.util.atom;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.hardware.fingerprint.FingerprintManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -33,6 +35,26 @@ public class Utils {
         ConnectivityManager cm = (ConnectivityManager)context.getSystemService(
                 Context.CONNECTIVITY_SERVICE);
         return (cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE) == false);
+    }
+
+    // Check to see if a package is installed
+    public static boolean isPackageInstalled(Context context, String pkg, boolean ignoreState) {
+        if (pkg != null) {
+            try {
+                PackageInfo pi = context.getPackageManager().getPackageInfo(pkg, 0);
+                if (!pi.applicationInfo.enabled && !ignoreState) {
+                    return false;
+                }
+            } catch (NameNotFoundException e) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPackageInstalled(Context context, String pkg) {
+        return isPackageInstalled(context, pkg, true);
     }
 
     // Check to see if device supports the Fingerprint scanner
