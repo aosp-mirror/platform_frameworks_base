e2179925df2ce174d81dd6f7abae626cc859ce00
Seigo Nonaka <nona@google.com>
2016-10-27 18:06:39 +0900

    Make sure the Typeface is loaded before calling measureText.
    This is a clean up CL by resolving TODO in hwui/Typeface.cpp
    
    Usually Typeface is listed in preloaded-classes and loaded during Zygote
    initialization. However, on some devices, class preloading is disabled.
    Paint.measureText can not be called before Typeface static initializer
    and it may not load Typeface class.
    
    To ensure that the font preloading happens in ZygoteInit even on such
    devices, refer Typeface.DEFAULT static field in TextView.preloadFontCache
    in TextView which also happens during zygote initialization.
    
    Bug: 32374752
    Test: Manually done
    Change-Id: I773c154671c170bebad3aa0f9c04eee5a664b849

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -672,6 +672,11 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     public static void preloadFontCache() {
         Paint p = new Paint();
         p.setAntiAlias(true);
+        // Ensure that the Typeface is loaded here.
+        // Typically, Typeface is preloaded by zygote but not on all devices, e.g. Android Auto.
+        // So, sets Typeface.DEFAULT explicitly here for ensuring that the Typeface is loaded here
+        // since Paint.measureText can not be called without Typeface static initializer.
+        p.setTypeface(Typeface.DEFAULT);
         // We don't care about the result, just the side-effect of measuring.
         p.measureText("H");
     }
