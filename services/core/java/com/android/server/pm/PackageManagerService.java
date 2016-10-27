24c4c2e14196d4b78ddbad5a7c319c112188a191
Nicholas Chum <nicholaschum@gmail.com>
2016-10-27 07:08:00 +0200

    OMS7-N: Do not enforce code policy limiting overlay installation [10/11]
    Change-Id: Iea317f3771f25dbfcbf4938e88cace12fd97d7eb

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8032,10 +8032,6 @@ public class PackageManagerService extends IPackageManager.Stub {
             pkg.applicationInfo.privateFlags |= ApplicationInfo.PRIVATE_FLAG_PRIVILEGED;
         }
 
-        if ((policyFlags & PackageParser.PARSE_ENFORCE_CODE) != 0) {
-            enforceCodePolicy(pkg);
-        }
-
         if (mCustomResolverComponentName != null &&
                 mCustomResolverComponentName.getPackageName().equals(pkg.packageName)) {
             setUpCustomResolverActivity(pkg);
