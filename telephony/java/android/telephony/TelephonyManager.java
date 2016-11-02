79a1e01668bdc315140074aa7950cc80b02ca3db
pengzhicai <pengzhicai@xiaomi.com>
2016-11-02 09:27:20 +0800

    Fix String is empty issue
    Maybe the string is empty
    https://code.google.com/p/android/issues/detail?id=226739
    
    Change-Id: I645ae05a8cbe316e29d9710de0bbba64c0c2275d
    Signed-off-by: pengzhicai <pengzhicai@xiaomi.com>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1165,7 +1165,7 @@ public class TelephonyManager {
     private int getPhoneTypeFromProperty(int phoneId) {
         String type = getTelephonyProperty(phoneId,
                 TelephonyProperties.CURRENT_ACTIVE_PHONE, null);
-        if (type == null || type.equals("")) {
+        if (type == null || type.isEmpty()) {
             return getPhoneTypeFromNetworkType(phoneId);
         }
         return Integer.parseInt(type);
@@ -1181,7 +1181,7 @@ public class TelephonyManager {
         // use the system property for default network type.
         // This is a fail safe, and can only happen at first boot.
         String mode = getTelephonyProperty(phoneId, "ro.telephony.default_network", null);
-        if (mode != null) {
+        if (mode != null && !mode.isEmpty()) {
             return TelephonyManager.getPhoneType(Integer.parseInt(mode));
         }
         return TelephonyManager.PHONE_TYPE_NONE;
