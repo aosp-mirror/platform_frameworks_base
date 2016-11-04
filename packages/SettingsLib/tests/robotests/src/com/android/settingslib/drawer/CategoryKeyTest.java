9c9e364ae85c7386df7e46e5bb1e8bcdde7a889c
Fan Zhang <zhfan@google.com>
2016-11-04 12:53:46 -0700

    Add new category for input setting and language setting
    Bug: 32637613
    Bug: 32643833
    Test: RunSettingsLibRoboTests
    
    Change-Id: I3551f3743982372e314dac1c778d0faddbfbea69

--- packages/SettingsLib/tests/robotests/src/com/android/settingslib/drawer/CategoryKeyTest.java
+++ packages/SettingsLib/tests/robotests/src/com/android/settingslib/drawer/CategoryKeyTest.java
@@ -56,9 +56,11 @@ public class CategoryKeyTest {
         allKeys.add(CategoryKey.CATEGORY_SECURITY);
         allKeys.add(CategoryKey.CATEGORY_ACCOUNT);
         allKeys.add(CategoryKey.CATEGORY_SYSTEM);
+        allKeys.add(CategoryKey.CATEGORY_SYSTEM_INPUT);
+        allKeys.add(CategoryKey.CATEGORY_SYSTEM_LANGUAGE);
         // DO NOT REMOVE ANYTHING ABOVE
 
-        assertThat(allKeys.size()).isEqualTo(11);
+        assertThat(allKeys.size()).isEqualTo(13);
     }
 
 }
