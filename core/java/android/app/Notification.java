441f57489428855cc8071686ef22c629dc539d31
George G <kreach3r@users.noreply.github.com>
2016-11-14 14:49:47 +0200

    Notification dynamic colors bool compatible with OMS7 (2/2)
    OMS7 introduced this fine piece of code: https://github.com/SubstratumResources/platform_frameworks_base/blob/n-oms7/core/java/android/app/ResourcesManager.java#L897..#L904
    
    // Resources.getSystem Resources are created on request and aren't tracked by
    // mResourceReferences.
    //
    // If overlays targeting "android" are to be used, we must create the system
    // resources regardless of whether they already exist, since otherwise the
    // information on what overlays to use would be lost. This is wasteful for most
    // applications, so limit this operation to the system user only. (This means
    // Resources.getSystem() will *not* use overlays for applications.)
    
    Replaced deprecated Resources.getSystem() with compatible method.
    
    Change-Id: I02efe27de3cc7067552964ffbaf079f9e9b5bc3e

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -3918,7 +3918,7 @@ public class Notification implements Parcelable
         }
 
         int resolveIconContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_icon_default_color);
             } else {
                 return resolveContrastColor();
@@ -3926,7 +3926,7 @@ public class Notification implements Parcelable
         }
 
         int resolveContrastColor() {
-            if (!Resources.getSystem().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
+            if (!mContext.getResources().getBoolean(R.bool.config_allowNotificationIconTextTinting)) {
                 return mContext.getColor(R.color.notification_text_default_color);
             }
 
