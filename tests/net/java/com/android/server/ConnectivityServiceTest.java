bccd8ad00ab299d77a9231b5999d3f9b0194a10a
Hugo Benichi <hugobenichi@google.com>
2016-11-15 13:42:34 +0900

    DO NOT MERGE: ConnectivityServiceTest: fix testAvoidBadWifiSettings
    This patch introduces an assertEventuallyThat helper function in
    ConnectivityServiceTest which given a boolean function retries until the
    function returns true or until a maximum retry time is reached.
    
    This function is used to fix flakyness of testAvoidBadWifiSetting where
    the Message posted by reevaluate() could reach the Handler's
    MessageQueue after waitForIdle takes effect, resulting in the test to
    fail.
    
    Instead of fixing the flakyness by introdcing hard sleep times,
    assertEventuallyThat is used to reduce the overall test time.
    
    With this change the test has been observed to pass with 100% success
    rate over 50000 invocations.
    
    Test: $ runtest frameworks-net
    Bug: 32561414
    
    (cherry picked from commit 99c8294ef0c7a720364abb5884063e0a9d1bb5fe)
    
    (cherry picked from commit 53d83d51424a496978d2ff2d86845a2284515712)
    
    Change-Id: I432f90a699dadfef37a5d0a69e25050753340964

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -91,6 +91,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.BooleanSupplier;
 
 /**
  * Tests for {@link ConnectivityService}.
@@ -622,7 +623,7 @@ public class ConnectivityServiceTest extends AndroidTestCase {
     }
 
     private class WrappedAvoidBadWifiTracker extends AvoidBadWifiTracker {
-        public boolean configRestrictsAvoidBadWifi;
+        public volatile boolean configRestrictsAvoidBadWifi;
 
         public WrappedAvoidBadWifiTracker(Context c, Handler h, Runnable r) {
             super(c, h, r);
@@ -2180,7 +2181,7 @@ public class ConnectivityServiceTest extends AndroidTestCase {
             tracker.reevaluate();
             mService.waitForIdle();
             String msg = String.format("config=false, setting=%s", values[i]);
-            assertTrue(msg, mService.avoidBadWifi());
+            assertEventuallyTrue(() -> mService.avoidBadWifi(), 50);
             assertFalse(msg, tracker.shouldNotifyWifiUnvalidated());
         }
 
@@ -2189,19 +2190,19 @@ public class ConnectivityServiceTest extends AndroidTestCase {
         Settings.Global.putInt(cr, settingName, 0);
         tracker.reevaluate();
         mService.waitForIdle();
-        assertFalse(mService.avoidBadWifi());
+        assertEventuallyTrue(() -> !mService.avoidBadWifi(), 50);
         assertFalse(tracker.shouldNotifyWifiUnvalidated());
 
         Settings.Global.putInt(cr, settingName, 1);
         tracker.reevaluate();
         mService.waitForIdle();
-        assertTrue(mService.avoidBadWifi());
+        assertEventuallyTrue(() -> mService.avoidBadWifi(), 50);
         assertFalse(tracker.shouldNotifyWifiUnvalidated());
 
         Settings.Global.putString(cr, settingName, null);
         tracker.reevaluate();
         mService.waitForIdle();
-        assertFalse(mService.avoidBadWifi());
+        assertEventuallyTrue(() -> !mService.avoidBadWifi(), 50);
         assertTrue(tracker.shouldNotifyWifiUnvalidated());
     }
 
@@ -2416,6 +2417,17 @@ public class ConnectivityServiceTest extends AndroidTestCase {
         mWiFiNetworkAgent = new MockNetworkAgent(TRANSPORT_WIFI);
         mWiFiNetworkAgent.connect(false);
         networkCallback.assertNoCallback();
+    }
+
+    public void assertEventuallyTrue(BooleanSupplier fn, long maxWaitingTimeMs) throws Exception {
+        long start = SystemClock.elapsedRealtime();
+        while (SystemClock.elapsedRealtime() <= start + maxWaitingTimeMs) {
+            if (fn.getAsBoolean()) {
+                return;
+            }
+            Thread.sleep(10);
+        }
+        assertTrue(fn.getAsBoolean());
     }
 
     private static class TestKeepaliveCallback extends PacketKeepaliveCallback {
