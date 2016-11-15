8a4466959bc42b6133eaddf66f6e6cfe54846664
Diego Perez <diegoperez@google.com>
2016-11-15 12:06:49 +0000

    More layoutlib testing infra fixes
    - Remove ModuleClassLoader as it can be replaced with a URLClassLoader
      for now.
    - Move CustomCalendar and CustomDate to a separate package that can be
      used both by the Bridge tests and the actual test app.
    - Move empty.xml out of the test app so it compiles.
    - Update test app to use the latest build tools and SDK (some attributes
      being used by the app weren't supported in API 21).
    - Update gitignore to remove the new out directory.
    
    Change-Id: Ieb7324d5ae559f9c581771c57f2127cd83909015

---
+++ tools/layoutlib/bridge/tests/res/testApp/MyApplication/src/main/myapplication.widgets/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains custom widgets used to set a specific time for the DayTimePicker during
+ * testing.
+ * The classes here are both used from the Android project and from the Bridge test project.
+ */
+package com.android.layoutlib.test.myapplication.widgets;
