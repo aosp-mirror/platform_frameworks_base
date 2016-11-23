8e7b940f8d08fe6697e274933ba8c06e6363c295
Vladislav Kaznacheev <kaznacheev@google.com>
2016-11-23 15:31:13 -0800

    Add ClipData.addItem(Item, ContentResolver)
    Unlike the existing addItem(Item), this method updates
    the MIME type list in the ClipDescription.
    
    Bug: 28750744
    Test: cts-tradefed ... -m CtsContentTestCases
      --test android.content.cts.ClipboardManagerTest
    Change-Id: Ida0477267d1319a31a738dfd704c0af71928dd2f

--- core/java/android/content/ClipDescription.java
+++ core/java/android/content/ClipDescription.java
@@ -22,6 +22,7 @@ import android.os.PersistableBundle;
 import android.text.TextUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Meta-data describing the contents of a {@link ClipData}.  Provides enough
@@ -89,7 +90,7 @@ public class ClipDescription implements Parcelable {
 
 
     final CharSequence mLabel;
-    final String[] mMimeTypes;
+    private final ArrayList<String> mMimeTypes;
     private PersistableBundle mExtras;
 
     /**
@@ -103,7 +104,7 @@ public class ClipDescription implements Parcelable {
             throw new NullPointerException("mimeTypes is null");
         }
         mLabel = label;
-        mMimeTypes = mimeTypes;
+        mMimeTypes = new ArrayList<String>(Arrays.asList(mimeTypes));
     }
 
     /**
@@ -111,7 +112,7 @@ public class ClipDescription implements Parcelable {
      */
     public ClipDescription(ClipDescription o) {
         mLabel = o.mLabel;
-        mMimeTypes = o.mMimeTypes;
+        mMimeTypes = new ArrayList<String>(o.mMimeTypes);
     }
 
     /**
@@ -155,8 +156,9 @@ public class ClipDescription implements Parcelable {
      * matches the desired MIME type, else false.
      */
     public boolean hasMimeType(String mimeType) {
-        for (int i=0; i<mMimeTypes.length; i++) {
-            if (compareMimeTypes(mMimeTypes[i], mimeType)) {
+        final int size = mMimeTypes.size();
+        for (int i=0; i<size; i++) {
+            if (compareMimeTypes(mMimeTypes.get(i), mimeType)) {
                 return true;
             }
         }
@@ -173,12 +175,13 @@ public class ClipDescription implements Parcelable {
      */
     public String[] filterMimeTypes(String mimeType) {
         ArrayList<String> array = null;
-        for (int i=0; i<mMimeTypes.length; i++) {
-            if (compareMimeTypes(mMimeTypes[i], mimeType)) {
+        final int size = mMimeTypes.size();
+        for (int i=0; i<size; i++) {
+            if (compareMimeTypes(mMimeTypes.get(i), mimeType)) {
                 if (array == null) {
                     array = new ArrayList<String>();
                 }
-                array.add(mMimeTypes[i]);
+                array.add(mMimeTypes.get(i));
             }
         }
         if (array == null) {
@@ -193,14 +196,26 @@ public class ClipDescription implements Parcelable {
      * Return the number of MIME types the clip is available in.
      */
     public int getMimeTypeCount() {
-        return mMimeTypes.length;
+        return mMimeTypes.size();
     }
 
     /**
      * Return one of the possible clip MIME types.
      */
     public String getMimeType(int index) {
-        return mMimeTypes[index];
+        return mMimeTypes.get(index);
+    }
+
+    /**
+     * Add MIME types to the clip description.
+     */
+    void addMimeTypes(String[] mimeTypes) {
+        for (int i=0; i!=mimeTypes.length; i++) {
+            final String mimeType = mimeTypes[i];
+            if (!mMimeTypes.contains(mimeType)) {
+                mMimeTypes.add(mimeType);
+            }
+        }
     }
 
     /**
@@ -229,11 +244,12 @@ public class ClipDescription implements Parcelable {
         if (mMimeTypes == null) {
             throw new NullPointerException("null mime types");
         }
-        if (mMimeTypes.length <= 0) {
+        final int size = mMimeTypes.size();
+        if (size <= 0) {
             throw new IllegalArgumentException("must have at least 1 mime type");
         }
-        for (int i=0; i<mMimeTypes.length; i++) {
-            if (mMimeTypes[i] == null) {
+        for (int i=0; i<size; i++) {
+            if (mMimeTypes.get(i) == null) {
                 throw new NullPointerException("mime type at " + i + " is null");
             }
         }
@@ -275,12 +291,13 @@ public class ClipDescription implements Parcelable {
     /** @hide */
     public boolean toShortStringTypesOnly(StringBuilder b) {
         boolean first = true;
-        for (int i=0; i<mMimeTypes.length; i++) {
+        final int size = mMimeTypes.size();
+        for (int i=0; i<size; i++) {
             if (!first) {
                 b.append(' ');
             }
             first = false;
-            b.append(mMimeTypes[i]);
+            b.append(mMimeTypes.get(i));
         }
         return !first;
     }
@@ -293,13 +310,13 @@ public class ClipDescription implements Parcelable {
     @Override
     public void writeToParcel(Parcel dest, int flags) {
         TextUtils.writeToParcel(mLabel, dest, flags);
-        dest.writeStringArray(mMimeTypes);
+        dest.writeStringList(mMimeTypes);
         dest.writePersistableBundle(mExtras);
     }
 
     ClipDescription(Parcel in) {
         mLabel = TextUtils.CHAR_SEQUENCE_CREATOR.createFromParcel(in);
-        mMimeTypes = in.createStringArray();
+        mMimeTypes = in.createStringArrayList();
         mExtras = in.readPersistableBundle();
     }
 
