f1b75647a3d8b51abdc3064445db4f6e40617cb8
Stephen Chen <stewchen@google.com>
2016-12-06 16:08:52 -0800

    Clarify doc on NETWORK_RECOMMENDATIONS_ENABLED and WIFI_WAKEUP_ENABLED.
    Bug: 32913919
    Test: n/a
    Change-Id: Ied075bdcc1ebb8f0a4bc0d3bf9758fa6719ce0b9

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7445,6 +7445,8 @@ public final class Settings {
 
         /**
          * Value to specify if Wi-Fi Wakeup feature is enabled.
+         *
+         * Type: int (0 for false, 1 for true)
          * @hide
          */
         @SystemApi
@@ -7453,6 +7455,8 @@ public final class Settings {
         /**
          * Value to specify if network recommendations from
          * {@link com.android.server.NetworkScoreService} are enabled.
+         *
+         * Type: int (0 for false, 1 for true)
          * @hide
          */
         @SystemApi
