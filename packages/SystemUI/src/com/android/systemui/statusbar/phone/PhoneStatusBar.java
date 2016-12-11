29aa1290dea039790f45cab521156ec83cc80f1b
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2016-12-11 13:44:45 -0500

    DUI: fix SmartBar edit mode
    Google have set the nav always slippery on 7.1.1
    but when switching from fling to smartbar and starting the edit mode
    we need to take the touch on the navbar buttons
    
    Change-Id: I3771be07dbc43badd7373b1bc916f6d86ecc706f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1677,8 +1677,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
                     | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
                     | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH
-                    | WindowManager.LayoutParams.FLAG_SLIPPERY,
+                    | WindowManager.LayoutParams.FLAG_SPLIT_TOUCH,
                 PixelFormat.TRANSLUCENT);
         // this will allow the navbar to run in an overlay on devices that support this
         if (ActivityManager.isHighEndGfx()) {
