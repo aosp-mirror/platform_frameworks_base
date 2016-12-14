62e5b24d91e56d249aeff7142f82c62eb60ffe15
Nathan Harold <nharold@google.com>
2016-12-14 11:40:03 -0800

    Remove dependency on telephony-common from services/core
    After migrating all the dependencies from frameworks/opt/telephony
    to frameworks/base, the dependency is no longer needed and should
    never have been created in the first place. Happily removed.
    
    Bug: 33414487
    Test: compilation
    Change-Id: I261da4c6b819b3c960ce56d2cab5c80cb34e852e

--- services/core/Android.mk
+++ services/core/Android.mk
@@ -19,7 +19,6 @@ LOCAL_AIDL_INCLUDES += \
 
 LOCAL_JAVA_LIBRARIES := \
     services.net \
-    telephony-common \
     android.hardware.light@2.0-java \
     android.hardware.power@1.0-java \
     android.hardware.tv.cec@1.0-java
