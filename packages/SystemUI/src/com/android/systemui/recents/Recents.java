8e17342610b1f64d87078a4a2068b5324c0aa48c
Manu Cornet <manucornet@google.com>
2016-12-16 16:17:33 -0800

    Delete the separate grid recents activity and related code
    Bug: 32101881
    Test: Made sure everything builds & boots on sw600dp device and checked recents
    Change-Id: I1236a1ec5f5d2f485d9825c39702130bbfc243ca

--- packages/SystemUI/src/com/android/systemui/recents/Recents.java
+++ packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -57,7 +57,6 @@ import com.android.systemui.recents.events.component.ShowUserToastEvent;
 import com.android.systemui.recents.events.ui.RecentsDrawnEvent;
 import com.android.systemui.recents.misc.SystemServicesProxy;
 import com.android.systemui.recents.model.RecentsTaskLoader;
-import com.android.systemui.recents.grid.RecentsGridImpl;
 import com.android.systemui.recents.tv.RecentsTvImpl;
 import com.android.systemui.stackdivider.Divider;
 
@@ -84,7 +83,6 @@ public class Recents extends SystemUI
     static {
         RECENTS_ACTIVITIES.add(RecentsImpl.RECENTS_ACTIVITY);
         RECENTS_ACTIVITIES.add(RecentsTvImpl.RECENTS_TV_ACTIVITY);
-        RECENTS_ACTIVITIES.add(RecentsGridImpl.RECENTS_MOSAIC_ACTIVITY);
     }
 
     // Purely for experimentation
