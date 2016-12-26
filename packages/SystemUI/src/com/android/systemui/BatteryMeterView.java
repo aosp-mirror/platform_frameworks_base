2be47b266215e9e1fc6c12b5645dd8bd2fa2e48d
Alex Cruz <mazdarider23@gmail.com>
2016-12-26 08:39:32 +0100

    Fix battery text gap issue
    Steps to reproduce
    
    1. Set battery to text only
    2. Go into statusbar items (tuner) and switch anything on
    
    See image - https://s28.postimg.org/qsuzgdbnx/image.png
    
    Thanks to @Nobe1976 for reporting it
    
    Change-Id: I12aab0af022d83b650c7395f699a51ae140ade15

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -80,10 +80,7 @@ public class BatteryMeterView extends ImageView implements
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (StatusBarIconController.ICON_BLACKLIST.equals(key)) {
-            ArraySet<String> icons = StatusBarIconController.getIconBlacklist(newValue);
-            setVisibility(icons.contains(mSlotBattery) ? View.GONE : View.VISIBLE);
-        } else if (STATUS_BAR_BATTERY_STYLE.equals(key)) {
+        if (STATUS_BAR_BATTERY_STYLE.equals(key)) {
             updateBatteryStyle(newValue);
         } else if (STATUS_BAR_CHARGE_COLOR.equals(key)) {
             updateBoltColor();
