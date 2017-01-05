7b2b573c4d8b770e63262ec43f5ef1a5013e745d
songjinshi <songjinshi@xiaomi.com>
2017-01-05 15:31:35 +0800

    [PackageManagerService]: Fixes can not boot issue.
    Sometime the bad file foramt will cause the device
    can not be boot, so we need to delete the bad file
    and throw an exception to try reboot.
    
    BTW:
    Remove some duplicate code.
    
    Change-Id: Ic271fc3175e21e5bcde7d23b16e8081ab5f34950
    Signed-off-by: songjinshi <songjinshi@xiaomi.com>

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -2927,18 +2927,15 @@ final class Settings {
                     XmlUtils.skipCurrentTag(parser);
                 }
             }
-
-            str.close();
-
-        } catch (XmlPullParserException e) {
+        } catch (XmlPullParserException | IOException | NumberFormatException e) {
+            mSettingsFilename.delete();
             mReadMessages.append("Error reading: " + e.toString());
             PackageManagerService.reportSettingsProblem(Log.ERROR, "Error reading settings: " + e);
             Slog.wtf(PackageManagerService.TAG, "Error reading package manager settings", e);
-
-        } catch (java.io.IOException e) {
-            mReadMessages.append("Error reading: " + e.toString());
-            PackageManagerService.reportSettingsProblem(Log.ERROR, "Error reading settings: " + e);
-            Slog.wtf(PackageManagerService.TAG, "Error reading package manager settings", e);
+            throw new IllegalStateException("Failed parsing settings file: "
+                    + mSettingsFilename , e);
+        } finally {
+            IoUtils.closeQuietly(str);
         }
 
         // If the build is setup to drop runtime permissions
