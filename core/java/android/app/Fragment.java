b2852cab467ef0c1181a1d5096c43c29fdf6a875
Jason Monk <jmonk@google.com>
2017-01-09 15:12:37 -0500

    Allow FragmentContainer to play a role in fragment construction
    This allows class swapping, dependency injection, and other fun
    stuff.
    
    Test: manual
    Change-Id: I93e17ccf9cebb6f542903d31597b5ea41544e069

--- core/java/android/app/Fragment.java
+++ core/java/android/app/Fragment.java
@@ -102,15 +102,19 @@ final class FragmentState implements Parcelable {
         mSavedFragmentState = in.readBundle();
     }
 
-    public Fragment instantiate(FragmentHostCallback host, Fragment parent,
-            FragmentManagerNonConfig childNonConfig) {
+    public Fragment instantiate(FragmentHostCallback host, FragmentContainer container,
+            Fragment parent, FragmentManagerNonConfig childNonConfig) {
         if (mInstance == null) {
             final Context context = host.getContext();
             if (mArguments != null) {
                 mArguments.setClassLoader(context.getClassLoader());
             }
 
-            mInstance = Fragment.instantiate(context, mClassName, mArguments);
+            if (container != null) {
+                mInstance = container.instantiate(context, mClassName, mArguments);
+            } else {
+                mInstance = Fragment.instantiate(context, mClassName, mArguments);
+            }
 
             if (mSavedFragmentState != null) {
                 mSavedFragmentState.setClassLoader(context.getClassLoader());
