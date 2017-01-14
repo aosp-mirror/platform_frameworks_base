a34ae165d7383ab839f6e7e1392a1aa83c02d652
ezio84 <brabus84@gmail.com>
2017-01-14 13:57:25 +0100

    DUI: Fix navbar edit logspam
    01-14 13:51:31.585  5276  6478 E ActivityManager: Sending non-protected broadcast intent_navbar_edit from system 23657:com.android.settings/1000 pkg com.android.settings
    01-14 13:51:31.585  5276  6478 E ActivityManager: java.lang.Throwable
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.checkBroadcastFromSystem(ActivityManagerService.java:17868)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.broadcastIntentLocked(ActivityManagerService.java:18349)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.broadcastIntent(ActivityManagerService.java:18531)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at android.app.ActivityManagerNative.onTransact(ActivityManagerNative.java:500)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at com.android.server.am.ActivityManagerService.onTransact(ActivityManagerService.java:2799)
    01-14 13:51:31.585  5276  6478 E ActivityManager:       at android.os.Binder.execTransact(Binder.java:565)
    
    thanks @KreAch3R

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4485,7 +4485,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 if (actionName.startsWith("android.net.netmon.lingerExpired")
                         || actionName.startsWith("com.android.server.sip.SipWakeupTimer")
                         || actionName.startsWith("com.android.internal.telephony.data-reconnect")
-                        || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")) {
+                        || actionName.startsWith("android.net.netmon.launchCaptivePortalApp")
+                        || actionName.startsWith("intent_navbar_edit")) {
                     return true;
                 }
             }
