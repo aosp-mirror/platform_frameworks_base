f93861ed8fadce07cf67ec10250cbf072388b7b2
xyyx <xyyx@mail.ru>
2017-01-18 00:48:01 +0100

    Lockscreen charging: "mAh" to "mA"
    Pulled from Nitrogen-OS; mAh is a unit of charge, mA is the unit of current, which is applicable here.
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -228,7 +228,7 @@ public class KeyguardIndicationController {
 
         if (showbatteryInfo) {
             if (mChargingCurrent > 0) {
-                batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mAh";
+                batteryInfo = batteryInfo + (mChargingCurrent / 1000) + "mA";
             }
             if (mChargingVoltage > 0) {
                 batteryInfo = (batteryInfo == "" ? "" : batteryInfo + " · ") +
