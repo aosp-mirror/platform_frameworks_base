83b4f7daba75e73ca9aad6033ed513902dc3b5bb
Adam Lesinski <adamlesinski@google.com>
2017-01-20 13:19:27 -0800

    AAPT: Add support for 'font' resource type
    Change-Id: I5bc459e34715779ab3b1c415f40f592a4ccbb96e
    Test: manual

--- tools/aapt/Resource.cpp
+++ tools/aapt/Resource.cpp
@@ -211,7 +211,7 @@ private:
 bool isValidResourceType(const String8& type)
 {
     return type == "anim" || type == "animator" || type == "interpolator"
-        || type == "transition"
+        || type == "transition" || type == "font"
         || type == "drawable" || type == "layout"
         || type == "values" || type == "xml" || type == "raw"
         || type == "color" || type == "menu" || type == "mipmap";
