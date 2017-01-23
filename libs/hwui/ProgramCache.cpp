7b22321b18a64b1196bcbf83d8c884c078484327
Arun <arun.demeure@imgtec.com>
2017-01-23 11:37:23 +0000

    Optimised hwui shadow shader by using Vec2 alpha
    The original fragment shader has unnecessary arithmetic instructions
    on some GPUs due to using vec2(alpha, 0.5) instead of directly passing
    the Vec2 texture coordinates. By passing the Vec2 directly, it improves
    performance on low-end GPUs by letting them reach their peak fillrate.
    It also optimises the multiplication as RGB are known to always be 0.
    
    Test: manual - visual inspection on fugu (nexus player)
    
    Change-Id: I438ff01f899153ac2e3d62ab13ac371fd2745df1

--- libs/hwui/ProgramCache.cpp
+++ libs/hwui/ProgramCache.cpp
@@ -65,6 +65,8 @@ const char* gVS_Header_Varyings_HasColors =
         "varying vec4 outColors;\n";
 const char* gVS_Header_Varyings_HasVertexAlpha =
         "varying float alpha;\n";
+const char* gVS_Header_Varyings_HasVertexAlphaVec2 =
+        "varying vec2 alphaVec2;\n";
 const char* gVS_Header_Varyings_HasBitmap =
         "varying highp vec2 outBitmapTexCoords;\n";
 const char* gVS_Header_Varyings_HasGradient[6] = {
@@ -123,6 +125,9 @@ const char* gVS_Main_Position =
 
 const char* gVS_Main_VertexAlpha =
         "    alpha = vtxAlpha;\n";
+
+const char* gVS_Main_VertexAlphaVec2 =
+        "    alphaVec2 = vec2(vtxAlpha, 0.5);\n";
 
 const char* gVS_Main_HasRoundRectClip =
         "    roundRectPos = (roundRectInvTransform * transformedPosition).xy;\n";
@@ -246,8 +251,10 @@ const char* gFS_Main_ModulateColor =
 const char* gFS_Main_ApplyVertexAlphaLinearInterp =
         "    fragColor *= alpha;\n";
 const char* gFS_Main_ApplyVertexAlphaShadowInterp =
+        // color.rgb is always 0.0 for shadows - this allows the compiler to optimise it away
+        "    fragColor.rgb = vec3(0.0);\n"
         // map alpha through shadow alpha sampler
-        "    fragColor *= texture2D(baseSampler, vec2(alpha, 0.5)).a;\n";
+        "    fragColor *= texture2D(baseSampler, alphaVec2).a;\n";
 const char* gFS_Main_FetchTexture[2] = {
         // Don't modulate
         "    fragColor = texture2D(baseSampler, outTexCoords);\n",
@@ -500,7 +507,11 @@ String8 ProgramCache::generateVertexShader(const ProgramDescription& description
         shader.append(gVS_Header_Varyings_HasTexture);
     }
     if (description.hasVertexAlpha) {
-        shader.append(gVS_Header_Varyings_HasVertexAlpha);
+        if (description.useShadowAlphaInterp) {
+            shader.append(gVS_Header_Varyings_HasVertexAlphaVec2);
+        } else {
+            shader.append(gVS_Header_Varyings_HasVertexAlpha);
+        }
     }
     if (description.hasColors) {
         shader.append(gVS_Header_Varyings_HasColors);
@@ -523,7 +534,11 @@ String8 ProgramCache::generateVertexShader(const ProgramDescription& description
             shader.append(gVS_Main_OutTexCoords);
         }
         if (description.hasVertexAlpha) {
-            shader.append(gVS_Main_VertexAlpha);
+            if (description.useShadowAlphaInterp) {
+                shader.append(gVS_Main_VertexAlphaVec2);
+            } else {
+                shader.append(gVS_Main_VertexAlpha);
+            }
         }
         if (description.hasColors) {
             shader.append(gVS_Main_OutColors);
@@ -573,7 +588,11 @@ String8 ProgramCache::generateFragmentShader(const ProgramDescription& descripti
         shader.append(gVS_Header_Varyings_HasTexture);
     }
     if (description.hasVertexAlpha) {
-        shader.append(gVS_Header_Varyings_HasVertexAlpha);
+        if (description.useShadowAlphaInterp) {
+            shader.append(gVS_Header_Varyings_HasVertexAlphaVec2);
+        } else {
+            shader.append(gVS_Header_Varyings_HasVertexAlpha);
+        }
     }
     if (description.hasColors) {
         shader.append(gVS_Header_Varyings_HasColors);
