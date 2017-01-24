4fa94da34b0c2b15b68821a694c3eed1e43c058c
Svetoslav Ganov <svetoslavganov@google.com>
2017-01-23 16:36:40 -0800

    Add an accidentally removed call to update libs
    Disabled system package may define a lib which its
    update does not. When the update is uninstalled we
    need to upate the libs so ppl can use the lib in
    the now restored sys package.
    
    Change-Id: I22e6aafde86838fa76aa74eda8dfe2c528f998aa

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -17704,6 +17704,13 @@ public class PackageManagerService extends IPackageManager.Stub {
             return false;
         }
 
+        try {
+            // update shared libraries for the newly re-installed system package
+            updateSharedLibrariesLPr(newPkg, null);
+        } catch (PackageManagerException e) {
+            Slog.e(TAG, "updateAllSharedLibrariesLPw failed: " + e.getMessage());
+        }
+
         prepareAppDataAfterInstallLIF(newPkg);
 
         // writer
