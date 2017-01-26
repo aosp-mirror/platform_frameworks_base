24a49fc02a82b3c9dfeadfd039a553c31e3055aa
Amith Yamasani <yamasani@google.com>
2017-01-26 14:57:20 -0800

    Add NPE check before stopping profiler
    During crash scenarios, stopProfiling() could be called early.
    
    Change-Id: I6ef78a47e3431dd7c41198ec29d40ebef3928c6a
    Fixes: 34744398
    Test: N/A
    (cherry picked from commit b1684439b574652166ec499b178ad317ef5b340d)

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -5029,7 +5029,9 @@ public final class ActivityThread {
      * @hide
      */
     public void stopProfiling() {
-        mProfiler.stopProfiling();
+        if (mProfiler != null) {
+            mProfiler.stopProfiling();
+        }
     }
 
     static final void handleDumpHeap(boolean managed, DumpHeapData dhd) {
