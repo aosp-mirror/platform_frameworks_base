74f36500a9b0c40431953724f228227f92a87db2
Diego Perez <diegoperez@google.com>
2017-02-02 17:32:35 +0000

    Fix NopCanvas initialization
    Reverse the logic since the field wouldn't be initialized to true before
    super is called.
    
    Test: Layoutlib tests
    Change-Id: Ic3affb608bdf3306ada25e2dd986a48f6caae202

--- tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/graphics/NopCanvas.java
+++ tools/layoutlib/bridge/src/com/android/layoutlib/bridge/android/graphics/NopCanvas.java
@@ -31,18 +31,18 @@ import android.graphics.RectF;
  * Canvas implementation that does not do any rendering
  */
 public class NopCanvas extends Canvas {
-    private boolean mIsHardwareAccelerated = true;
+    private boolean mIsInitialized = false;
 
     public NopCanvas() {
         super();
-        // We return true the first time so there are no allocations for the software renderer in
-        // the constructor
-        mIsHardwareAccelerated = false;
+        mIsInitialized = true;
     }
 
     @Override
     public boolean isHardwareAccelerated() {
-        return mIsHardwareAccelerated;
+        // We return true the first time so there are no allocations for the software renderer in
+        // the constructor
+        return !mIsInitialized;
     }
 
     @Override
