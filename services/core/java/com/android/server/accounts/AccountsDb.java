6fe34fcbaee098c4d7600867da46513b61db7483
Dmitry Dementyev <dementyev@google.com>
2017-02-03 17:46:10 -0800

    Fix AccountsDb update.
    Test: manual tests.
    Change-Id: I32881f0b77e79e218020dfa7ef921ddeb95de1b7

--- services/core/java/com/android/server/accounts/AccountsDb.java
+++ services/core/java/com/android/server/accounts/AccountsDb.java
@@ -599,7 +599,7 @@ class AccountsDb implements AutoCloseable {
             if (oldVersion == 2) {
                 // Remove uid based table and replace it with packageName based
                 db.execSQL("DROP TRIGGER IF EXISTS " + TABLE_ACCOUNTS + "DeleteVisibility");
-                db.execSQL("DROP TABLE IF EXISTS " + TABLE_ACCOUNTS);
+                db.execSQL("DROP TABLE IF EXISTS " + TABLE_VISIBILITY);
                 createAccountsVisibilityTable(db);
                 createAccountsDeletionVisibilityCleanupTrigger(db);
                 oldVersion++;
