a690473348c6182fa2f0ef1ef1ba75fec7d7d688
Jean-Michel Trivi <jmtrivi@google.com>
2017-02-04 16:35:25 -0800

    API for app to know what to set in Activity.setVolumeControlStream()
    Stream types are deprecated to describe an audio playback use case.
    But they are used for volume control. This API helps the developer
      go from an AudioAttributes instance used for playback, to a
      stream type used to describe which volume stream type should be
      used when the user presses on the volume keys.
    
    Test: see AudioAttributes cts test
    Bug 21267880
    
    Change-Id: I2b9da5b282e8ed2342c61c14a7f59b874d0ce979

--- media/java/android/media/AudioAttributes.java
+++ media/java/android/media/AudioAttributes.java
@@ -913,13 +913,13 @@ public final class AudioAttributes implements Parcelable {
         }
     }
     /**
-     * @hide
-     * CANDIDATE FOR PUBLIC (or at least SYSTEM) API
      * Returns the stream type matching the given attributes for volume control.
      * Use this method to derive the stream type needed to configure the volume
-     * control slider in an {@link Activity} with {@link Activity#setVolumeControlStream(int)}.
+     * control slider in an {@link android.app.Activity} with
+     * {@link android.app.Activity#setVolumeControlStream(int)}.
      * <BR>Do not use this method to set the stream type on an audio player object
-     * (e.g. {@link AudioTrack}, {@link MediaPlayer}), use <code>AudioAttributes</code> instead.
+     * (e.g. {@link AudioTrack}, {@link MediaPlayer}) as this is deprecated,
+     * use <code>AudioAttributes</code> instead.
      * @param aa non-null AudioAttributes.
      * @return a valid stream type for <code>Activity</code> or stream volume control that matches
      *     the attributes, or {@link AudioManager#USE_DEFAULT_STREAM_TYPE} if there isn't a direct
