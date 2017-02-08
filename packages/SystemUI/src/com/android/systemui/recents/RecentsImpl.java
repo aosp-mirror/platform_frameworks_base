96b7a51de845d7c4f39a1e85669e860f6b6bf3c6
Winson Chung <winsonc@google.com>
2017-02-08 12:20:00 -0800

    Fixing NPE when there is no running activity.
    Bug: 30668904
    Test: Boot the device, kill launcher
    Change-Id: I10236d33c7fbc1d20b5d4b097e0660596188eaf0

--- packages/SystemUI/src/com/android/systemui/recents/RecentsImpl.java
+++ packages/SystemUI/src/com/android/systemui/recents/RecentsImpl.java
@@ -374,6 +374,10 @@ public class RecentsImpl implements ActivityOptions.OnAnimationFinishedListener 
         MutableBoolean isHomeStackVisible = new MutableBoolean(true);
         if (!ssp.isRecentsActivityVisible(isHomeStackVisible)) {
             ActivityManager.RunningTaskInfo runningTask = ssp.getRunningTask();
+            if (runningTask == null) {
+                return;
+            }
+
             RecentsTaskLoader loader = Recents.getTaskLoader();
             sInstanceLoadPlan = loader.createLoadPlan(mContext);
             sInstanceLoadPlan.preloadRawTasks(!isHomeStackVisible.value);
