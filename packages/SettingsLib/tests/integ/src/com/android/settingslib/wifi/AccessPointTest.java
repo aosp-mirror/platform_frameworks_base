9853743705603e0b5214e5995bea8c9267708813
Dave Schaefer <daveschaefer@google.com>
2017-02-08 11:26:08 -0800

    AccessPoint - Add Builder; Add unit tests for compareTo()
    Add a Builder class to make testing easy and clean.
    Add unit tests to enforce the sorting order that we want.
    Refactor some compareTo() internals to use existing methods rather than
    duplicating functionality.
    Refactor isReachable() functionality to a new method.
    Update tests to use Truth assertion library.
    
    Test: Run local AccessPoint unit tests:
    
    adb shell am instrument -w -e class
    com.android.settingslib.wifi.AccessPointTest
    com.android.settingslib/android.support.test.runner.AndroidJUnitRunner
    
    Bug: 35042429
    Change-Id: I7c8a1ca69800f62e1fb13141e58b4e52b2bc3d70

--- packages/SettingsLib/tests/integ/src/com/android/settingslib/wifi/AccessPointTest.java
+++ packages/SettingsLib/tests/integ/src/com/android/settingslib/wifi/AccessPointTest.java
@@ -15,9 +15,8 @@
  */
 package com.android.settingslib.wifi;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
@@ -39,6 +38,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
@@ -59,12 +59,12 @@ public class AccessPointTest {
         final AccessPoint ap = new AccessPoint(InstrumentationRegistry.getTargetContext(), bundle);
         final CharSequence ssid = ap.getSsid();
 
-        assertTrue(ssid instanceof SpannableString);
+        assertThat(ssid instanceof SpannableString).isTrue();
 
         TtsSpan[] spans = ((SpannableString) ssid).getSpans(0, TEST_SSID.length(), TtsSpan.class);
 
-        assertEquals(1, spans.length);
-        assertEquals(TtsSpan.TYPE_TELEPHONE, spans[0].getType());
+        assertThat(spans.length).isEqualTo(1);
+        assertThat(spans[0].getType()).isEqualTo(TtsSpan.TYPE_TELEPHONE);
     }
 
     @Test
@@ -80,11 +80,11 @@ public class AccessPointTest {
         originalAccessPoint.update(configuration, wifiInfo, networkInfo);
         AccessPoint copy = new AccessPoint(mContext, originalAccessPoint);
 
-        assertEquals(originalAccessPoint.getSsid().toString(), copy.getSsid().toString());
-        assertEquals(originalAccessPoint.getBssid(), copy.getBssid());
-        assertSame(originalAccessPoint.getConfig(), copy.getConfig());
-        assertEquals(originalAccessPoint.getSecurity(), copy.getSecurity());
-        assertTrue(originalAccessPoint.compareTo(copy) == 0);
+        assertThat(originalAccessPoint.getSsid().toString()).isEqualTo(copy.getSsid().toString());
+        assertThat(originalAccessPoint.getBssid()).isEqualTo(copy.getBssid());
+        assertThat(originalAccessPoint.getConfig()).isEqualTo(copy.getConfig());
+        assertThat(originalAccessPoint.getSecurity()).isEqualTo(copy.getSecurity());
+        assertThat(originalAccessPoint.compareTo(copy) == 0).isTrue();
     }
 
     @Test
@@ -101,11 +101,93 @@ public class AccessPointTest {
 
         bundle.putParcelableArrayList("key_scanresultcache", scanResults);
         AccessPoint original = new AccessPoint(mContext, bundle);
-        assertEquals(4, original.getRssi());
+        assertThat(original.getRssi()).isEqualTo(4);
         AccessPoint copy = new AccessPoint(mContext, createWifiConfiguration());
-        assertEquals(Integer.MIN_VALUE, copy.getRssi());
+        assertThat(copy.getRssi()).isEqualTo(Integer.MIN_VALUE);
         copy.copyFrom(original);
-        assertEquals(original.getRssi(), copy.getRssi());
+        assertThat(original.getRssi()).isEqualTo(copy.getRssi());
+    }
+
+    @Test
+    public void testCompareTo_GivesActiveBeforeInactive() {
+        AccessPoint activeAp = new TestAccessPointBuilder(mContext).setActive(true).build();
+        AccessPoint inactiveAp = new TestAccessPointBuilder(mContext).setActive(false).build();
+
+        assertSortingWorks(activeAp, inactiveAp);
+    }
+
+    @Test
+    public void testCompareTo_GivesReachableBeforeUnreachable() {
+        AccessPoint nearAp = new TestAccessPointBuilder(mContext).setReachable(true).build();
+        AccessPoint farAp = new TestAccessPointBuilder(mContext).setReachable(false).build();
+
+        assertSortingWorks(nearAp, farAp);
+    }
+
+    @Test
+    public void testCompareTo_GivesSavedBeforeUnsaved() {
+        AccessPoint savedAp = new TestAccessPointBuilder(mContext).setSaved(true).build();
+        AccessPoint notSavedAp = new TestAccessPointBuilder(mContext).setSaved(false).build();
+
+        assertSortingWorks(savedAp, notSavedAp);
+    }
+
+    //TODO: add tests for mRankingScore sort order if ranking is exposed
+
+    @Test
+    public void testCompareTo_GivesHighLevelBeforeLowLevel() {
+        final int highLevel = AccessPoint.SIGNAL_LEVELS - 1;
+        final int lowLevel = 1;
+        assertThat(highLevel).isGreaterThan(lowLevel);
+
+        AccessPoint strongAp = new TestAccessPointBuilder(mContext).setLevel(highLevel).build();
+        AccessPoint weakAp = new TestAccessPointBuilder(mContext).setLevel(lowLevel).build();
+
+        assertSortingWorks(strongAp, weakAp);
+    }
+
+    @Test
+    public void testCompareTo_GivesSsidAlphabetically() {
+
+        final String firstName = "AAAAAA";
+        final String secondName = "zzzzzz";
+
+        AccessPoint firstAp = new TestAccessPointBuilder(mContext).setSsid(firstName).build();
+        AccessPoint secondAp = new TestAccessPointBuilder(mContext).setSsid(secondName).build();
+
+        assertThat(firstAp.getSsidStr().compareToIgnoreCase(secondAp.getSsidStr()) < 0).isTrue();
+        assertSortingWorks(firstAp, secondAp);
+    }
+
+    @Test
+    public void testCompareTo_AllSortingRulesCombined() {
+
+        AccessPoint active = new TestAccessPointBuilder(mContext).setActive(true).build();
+        AccessPoint reachableAndMinLevel = new TestAccessPointBuilder(mContext)
+                .setReachable(true).build();
+        AccessPoint saved = new TestAccessPointBuilder(mContext).setSaved(true).build();
+        AccessPoint highLevelAndReachable = new TestAccessPointBuilder(mContext)
+                .setLevel(AccessPoint.SIGNAL_LEVELS - 1).build();
+        AccessPoint firstName = new TestAccessPointBuilder(mContext).setSsid("a").build();
+        AccessPoint lastname = new TestAccessPointBuilder(mContext).setSsid("z").build();
+
+        ArrayList<AccessPoint> points = new ArrayList<AccessPoint>();
+        points.add(lastname);
+        points.add(firstName);
+        points.add(highLevelAndReachable);
+        points.add(saved);
+        points.add(reachableAndMinLevel);
+        points.add(active);
+
+        Collections.sort(points);
+        assertThat(points.indexOf(active)).isLessThan(points.indexOf(reachableAndMinLevel));
+        assertThat(points.indexOf(reachableAndMinLevel)).isLessThan(points.indexOf(saved));
+        // note: the saved AP will not appear before highLevelAndReachable,
+        // because all APs with a signal level are reachable,
+        // and isReachable() takes higher sorting precedence than isSaved().
+        assertThat(points.indexOf(saved)).isLessThan(points.indexOf(firstName));
+        assertThat(points.indexOf(highLevelAndReachable)).isLessThan(points.indexOf(firstName));
+        assertThat(points.indexOf(firstName)).isLessThan(points.indexOf(lastname));
     }
 
     private WifiConfiguration createWifiConfiguration() {
@@ -114,5 +196,86 @@ public class AccessPointTest {
         configuration.SSID = "ssid";
         configuration.networkId = 123;
         return configuration;
+    }
+
+    /**
+    * Assert that the first AccessPoint appears after the second AccessPoint
+    * once sorting has been completed.
+    */
+    private void assertSortingWorks(AccessPoint first, AccessPoint second) {
+
+        ArrayList<AccessPoint> points = new ArrayList<AccessPoint>();
+
+        // add in reverse order so we can tell that sorting actually changed something
+        points.add(second);
+        points.add(first);
+        Collections.sort(points);
+        assertWithMessage(
+                String.format("After sorting: second AccessPoint should have higher array index "
+                    + "than the first, but found indicies second '%s' and first '%s'.",
+                    points.indexOf(second), points.indexOf(first)))
+            .that(points.indexOf(second)).isGreaterThan(points.indexOf(first));
+    }
+
+    @Test
+    public void testBuilder_setActive() {
+        AccessPoint activeAp = new TestAccessPointBuilder(mContext).setActive(true).build();
+        assertThat(activeAp.isActive()).isTrue();
+
+        AccessPoint inactiveAp = new TestAccessPointBuilder(mContext).setActive(false).build();
+        assertThat(inactiveAp.isActive()).isFalse();
+    }
+
+    @Test
+    public void testBuilder_setReachable() {
+        AccessPoint nearAp = new TestAccessPointBuilder(mContext).setReachable(true).build();
+        assertThat(nearAp.isReachable()).isTrue();
+
+        AccessPoint farAp = new TestAccessPointBuilder(mContext).setReachable(false).build();
+        assertThat(farAp.isReachable()).isFalse();
+    }
+
+    @Test
+    public void testBuilder_setSaved() {
+        AccessPoint savedAp = new TestAccessPointBuilder(mContext).setSaved(true).build();
+        assertThat(savedAp.isSaved()).isTrue();
+
+        AccessPoint newAp = new TestAccessPointBuilder(mContext).setSaved(false).build();
+        assertThat(newAp.isSaved()).isFalse();
+    }
+
+    @Test
+    public void testBuilder_setLevel() {
+        AccessPoint testAp;
+
+        for (int i = 0; i < AccessPoint.SIGNAL_LEVELS; i++) {
+            testAp = new TestAccessPointBuilder(mContext).setLevel(i).build();
+            assertThat(testAp.getLevel()).isEqualTo(i);
+        }
+
+        // numbers larger than the max level should be set to max
+        testAp = new TestAccessPointBuilder(mContext).setLevel(AccessPoint.SIGNAL_LEVELS).build();
+        assertThat(testAp.getLevel()).isEqualTo(AccessPoint.SIGNAL_LEVELS - 1);
+
+        // numbers less than 0 should give level 0
+        testAp = new TestAccessPointBuilder(mContext).setLevel(-100).build();
+        assertThat(testAp.getLevel()).isEqualTo(0);
+    }
+
+    @Test
+    public void testBuilder_settingReachableAfterLevelDoesNotAffectLevel() {
+        int level = 1;
+        assertThat(level).isLessThan(AccessPoint.SIGNAL_LEVELS - 1);
+
+        AccessPoint testAp =
+                new TestAccessPointBuilder(mContext).setLevel(level).setReachable(true).build();
+        assertThat(testAp.getLevel()).isEqualTo(level);
+    }
+
+    @Test
+    public void testBuilder_setSsid() {
+        String name = "AmazingSsid!";
+        AccessPoint namedAp = new TestAccessPointBuilder(mContext).setSsid(name).build();
+        assertThat(namedAp.getSsidStr()).isEqualTo(name);
     }
 }
