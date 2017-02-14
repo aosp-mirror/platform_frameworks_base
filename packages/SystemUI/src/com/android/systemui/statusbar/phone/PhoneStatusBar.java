d92ae44c586f65a9f7eea3ceaf47c33f5ac44b5a
Josh Chasky <jchasky@gmail.com>
2017-02-14 12:59:13 -0500

    Remove custom ticker logic
    -- We're going to put a dialog/toast in Settings for this
    
    Change-Id: Ie6fbf6371388d4a19807e39caa8b0a861dcdbf19

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -581,7 +581,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     mTickerEnabled = Settings.System.getIntForUser(
                             mContext.getContentResolver(),
                             Settings.System.STATUS_BAR_SHOW_TICKER,
-                            1, UserHandle.USER_CURRENT) == 1;
+                            0, UserHandle.USER_CURRENT) == 1;
                     initTickerView();
             }
             update();
@@ -698,16 +698,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mClockLocation = Settings.System.getIntForUser(resolver,
                 Settings.System.STATUSBAR_CLOCK_STYLE, 0, UserHandle.USER_CURRENT);
-
-        boolean headsUpEnabled = Settings.System.getIntForUser(resolver,
-                Settings.System.HEADS_UP_USER_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (headsUpEnabled) {
-            mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_TICKER,
-                    mContext.getResources().getBoolean(R.bool.enable_ticker)
-                    ? 1 : 1, UserHandle.USER_CURRENT) == 1;
-            initTickerView();
-        }
     }
     private OmniSettingsObserver mOmniSettingsObserver = new OmniSettingsObserver(mHandler);
     private int mInteractingWindows;
@@ -1116,15 +1106,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         mCenterClock = (TextView) mStatusBarWindow.findViewById(R.id.center_clock);
 
-        boolean headsUpEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                Settings.System.HEADS_UP_USER_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
-        if (headsUpEnabled) {
-            mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
-                    Settings.System.STATUS_BAR_SHOW_TICKER,
-                    mContext.getResources().getBoolean(R.bool.enable_ticker)
-                    ? 1 : 1, UserHandle.USER_CURRENT) == 1;
-            initTickerView();
-        }
+        mTickerEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                mContext.getResources().getBoolean(R.bool.enable_ticker)
+                        ? 1 : 1, UserHandle.USER_CURRENT) == 1;
+        initTickerView();
 
         // set the initial view visibility
         setAreThereNotifications();
