d8aa2e67e74081a41829c1f1c9b3e818da33fab3
Insaf Latypov <saferif@google.com>
2017-02-20 11:33:48 +0000

    Add host-side version of framework-protos.
    AfwTestHarness needs to use framework-protos from host-side. This is why
    new rule is added to build framework-protos usable from host-side.
    
    Change-Id: Ifa35c5de04483d41e97c5853ea781fc29cef4947

--- proto/Android.mk
+++ proto/Android.mk
@@ -14,3 +14,20 @@ LOCAL_JAVA_LIBRARIES := core-oj core-libart
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk $(LOCAL_PATH)/jarjar-rules.txt
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
+
+# Host-side version of framework-protos
+# ============================================================
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := host-framework-protos
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PROTOC_OPTIMIZE_TYPE := nano
+LOCAL_SRC_FILES:= $(call all-proto-files-under, src)
+
+LOCAL_NO_STANDARD_LIBRARIES := true
+LOCAL_STATIC_JAVA_LIBRARIES := host-libprotobuf-java-nano
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk $(LOCAL_PATH)/jarjar-rules.txt
+
+include $(BUILD_HOST_JAVA_LIBRARY)
