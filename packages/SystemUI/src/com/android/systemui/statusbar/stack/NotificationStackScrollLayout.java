f306d9b498c2c81ad52083a0825e037f2757daf7
Selim Cinek <cinek@google.com>
2017-02-21 11:45:13 -0800

    Fixed some notification leaks
    Since we're now allowing heads-up on the lockscreen
    the addedHeadsUpChildren list would fill up, even
    if animations were not enabled.
    
    This also fixes that the ExpandHelper and the DragDownHelper
    would not realease the view on up.
    
    Change-Id: I8c1b54ff4b034a8f4f2219534a707bbd792b8efa
    Fixes: 35375214

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -2785,7 +2785,7 @@ public class NotificationStackScrollLayout extends ViewGroup
             }
             mNeedsAnimation = true;
         }
-        if (isHeadsUp(child) && !mChangePositionInProgress) {
+        if (isHeadsUp(child) && mAnimationsEnabled && !mChangePositionInProgress) {
             mAddedHeadsUpChildren.add(child);
             mChildrenToAddAnimated.remove(child);
         }
