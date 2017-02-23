34cfdb26a62e114d13749d392d56cabcccc8cdb8
Amin Shaikh <ashaikh@google.com>
2017-02-23 10:08:10 -0800

    Fix failing WifiConfigManagerTest test.
    Bug: 35671673
    Test: runtest frameworks-core frameworks-wifi
    Change-Id: I55ff41464cabfb97d03a0758d73145df808ec2c8

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -1305,6 +1305,7 @@ public class WifiConfiguration implements Parcelable {
             setConnectChoice(source.getConnectChoice());
             setConnectChoiceTimestamp(source.getConnectChoiceTimestamp());
             setHasEverConnected(source.getHasEverConnected());
+            setNotRecommended(source.isNotRecommended());
         }
 
         public void writeToParcel(Parcel dest) {
