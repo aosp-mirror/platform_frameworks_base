4024b6696ce1be3b29194b8dc21e3d1f97776e69
Julia Reynolds <juliacr@google.com>
2017-03-01 09:33:41 -0500

    Reduce flakes.
    Test: runtest systemui-notifications
    Change-Id: I48dc8be786246e6c639a14b16784a394eac49a6b

--- services/tests/notification/src/com/android/server/notification/SnoozeHelperTest.java
+++ services/tests/notification/src/com/android/server/notification/SnoozeHelperTest.java
@@ -77,7 +77,7 @@ public class SnoozeHelperTest {
         verify(mAm, times(1)).setExactAndAllowWhileIdle(
                 anyInt(), captor.capture(), any(PendingIntent.class));
         long actualSnoozedUntilDuration = captor.getValue() - SystemClock.elapsedRealtime();
-        assertTrue(Math.abs(actualSnoozedUntilDuration - 1000) < 2);
+        assertTrue(Math.abs(actualSnoozedUntilDuration - 1000) < 25);
         assertTrue(mSnoozeHelper.isSnoozed(
                 UserHandle.USER_SYSTEM, r.sbn.getPackageName(), r.getKey()));
     }
