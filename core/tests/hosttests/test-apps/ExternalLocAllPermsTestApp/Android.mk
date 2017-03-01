fa6f8a76fb582d1629845e0e23d44fc60fedcb2d
Paul Duffin <paulduffin@google.com>
2017-03-01 13:21:10 +0000

    Prepare for removal of junit and android.test classes from Android API (step 1)
    In preparation for removing junit and android.test classes from
    the Android API this change adds explicit dependencies on junit
    and/or legacy-android-test to ensure that modules will compile
    properly once it is removed.
    
    Care was taken to ensure that any targets that depended,
    directly or indirectly, on android-support-test did not also
    depend on junit or hamcrest as they conflict with the versions
    embedded within android-support-test.
    
    Bug: 30188076
    Test: make checkbuild
    
    (cherry picked from commit 00229bc6cfd74c3221d02af6df07960a0f36af1a)
    
    Change-Id: I7fea20f92a5a92885733bc6ecb83b7276a48fbc2
    Merged-In: Ifc3ee3dc804074a3c572bf2600cfb1fe1b6fa749

--- core/tests/hosttests/test-apps/ExternalLocAllPermsTestApp/Android.mk
+++ core/tests/hosttests/test-apps/ExternalLocAllPermsTestApp/Android.mk
@@ -22,6 +22,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := current
 
+LOCAL_STATIC_JAVA_LIBRARIES := junit
+
 LOCAL_PACKAGE_NAME := ExternalLocAllPermsTestApp
 
 include $(BUILD_PACKAGE)
