9635cf3deefed53950a8c4ed5b01a31204db0693
Selim Cinek <cinek@google.com>
2017-03-03 16:59:03 -0800

    Fixed a bug where the expand icon could show but nothing happens
    Test: add notification with an icon and short text, HUN
    Change-Id: Iefcaf262c5147fd7f580fcf1369e66dc369efc99
    Fixes: 27449651

--- packages/SystemUI/src/com/android/systemui/statusbar/NotificationContentView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/NotificationContentView.java
@@ -1199,10 +1199,10 @@ public class NotificationContentView extends FrameLayout {
         if (mExpandedChild != null && mExpandedChild.getHeight() != 0) {
             if ((!mIsHeadsUp && !mHeadsUpAnimatingAway)
                     || mHeadsUpChild == null || mContainingNotification.isOnKeyguard()) {
-                if (mExpandedChild.getHeight() == mContractedChild.getHeight()) {
+                if (mExpandedChild.getHeight() <= mContractedChild.getHeight()) {
                     expandable = false;
                 }
-            } else if (mExpandedChild.getHeight() == mHeadsUpChild.getHeight()) {
+            } else if (mExpandedChild.getHeight() <= mHeadsUpChild.getHeight()) {
                 expandable = false;
             }
         }
