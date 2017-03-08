22291f84e007ea71b2f1c9fded04e6b84d84f110
Hugo Benichi <hugobenichi@google.com>
2017-03-08 10:10:28 +0900

    Fix ApfTest
    By adding missing native libraries to frameworks-net make file. The list
    of missing libraries was found by adding libhidlbase and then resolving
    new linkage errors iteratively until ApfTest worked.
    
    This is a temporary fix. Ideally only adding libhidlbase should be
    enough.
    
    Test: $ runtest -x frameworks/base/tests/net/java/android/net/apf/ApfTest.java
    Bug: 36012053
    Change-Id: I0eff8b3adbf788ddbfc6ddbf9f74b8da01cdfce4

--- tests/net/Android.mk
+++ tests/net/Android.mk
@@ -40,7 +40,19 @@ LOCAL_JNI_SHARED_LIBRARIES := libframeworksnettestsjni \
     libnetdaidl \
     libui \
     libunwind \
-    libutils
+    libutils \
+    libcrypto \
+    libhidl-gen-utils \
+    libhidlbase \
+    libhidltransport \
+    libpackagelistparser \
+    libpcre2 \
+    libselinux \
+    libtinyxml2 \
+    libvintf \
+    libhwbinder \
+    android.hidl.base@1.0 \
+    android.hidl.token@1.0
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
 
