5d9a8acdac056e8842d010edd26496960f2a7a8d
Jeff Tinker <jtinker@google.com>
2017-03-09 16:10:28 -0800

    Clarify release/close java docs
    bug:25372187
    Change-Id: Ibe13943731e53efa9408b0d6bf105063f3ce4334

--- drm/java/android/drm/DrmManagerClient.java
+++ drm/java/android/drm/DrmManagerClient.java
@@ -275,6 +275,10 @@ public class DrmManagerClient implements AutoCloseable {
      * the {@link DrmManagerClient} object is no longer needed in your
      * application. After this method is called, {@link DrmManagerClient} is no
      * longer usable since it has lost all of its required resource.
+     *
+     * This method was added in API 24. In API versions 16 through 23, release()
+     * should be called instead. There is no need to do anything for API
+     * versions prior to 16.
      */
     @Override
     public void close() {
