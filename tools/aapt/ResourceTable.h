c7614e5d1e8f9139cccf6ab9b78dda16b98f7656
Adam Lesinski <adamlesinski@google.com>
2017-03-16 16:54:23 -0700

    AAPT: Version <adaptive-icon> to v26
    Bug: 35908647
    Change-Id: I307fb2f666cb8519adb9a7f676b87706dc6c683f
    Test: manual

--- tools/aapt/ResourceTable.h
+++ tools/aapt/ResourceTable.h
@@ -203,6 +203,9 @@ public:
     size_t numLocalResources() const;
     bool hasResources() const;
 
+    bool versionForCompat(const Bundle* bundle, const String16& resourceName,
+                          const sp<AaptFile>& file, const sp<XMLNode>& root);
+
     status_t modifyForCompat(const Bundle* bundle);
     status_t modifyForCompat(const Bundle* bundle,
                              const String16& resourceName,
@@ -431,6 +434,10 @@ public:
             mEntries.add(config, entry);
         }
         
+        void removeEntry(const ResTable_config& config) {
+            mEntries.removeItem(config);
+        }
+
         const DefaultKeyedVector<ConfigDescription, sp<Entry> >& getEntries() const { return mEntries; }
     private:
         const String16 mName;
