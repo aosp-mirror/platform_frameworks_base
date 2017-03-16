28c9f85efeae5a0c8922cf46e3930f02629d477d
xyyx <xyyx@mail.ru>
2017-03-16 15:26:27 +0100

    Ability to disable Do not Disturb mode when calling (1/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4401,6 +4401,12 @@ public final class Settings {
         public static final String SCREEN_STATE_ON_DELAY = "screen_state_on_delay";
 
         /**
+         * Use Dnd when call active
+         * @hide
+         */
+        public static final String DND_WHEN_CALL = "dnd_when_call";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
