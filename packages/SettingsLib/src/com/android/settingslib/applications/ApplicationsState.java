6e867d49ee2a05aa6a44be644610751db24cb285
Jeff Sharkey <jsharkey@android.com>
2017-03-18 16:39:24 -0600

    Only query storage stats when package installed.
    Otherwise we're just wasting our time (since the sizes will be zero),
    and we'll get yelled at by an exception.
    
    Test: builds, boots, no longer crashes
    Bug: 36308874
    Change-Id: I113d37e4a9ec00b8cb74321104c1ea1f836aa6f4

--- packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
+++ packages/SettingsLib/src/com/android/settingslib/applications/ApplicationsState.java
@@ -332,7 +332,7 @@ public class ApplicationsState {
         if (DEBUG_LOCKING) Log.v(TAG, "requestSize about to acquire lock...");
         synchronized (mEntriesMap) {
             AppEntry entry = mEntriesMap.get(userId).get(packageName);
-            if (entry != null) {
+            if (entry != null && (entry.info.flags & ApplicationInfo.FLAG_INSTALLED) != 0) {
                 mBackgroundHandler.post(() -> {
                     final StorageStats stats = mStats.queryStatsForPackage(entry.info.volumeUuid,
                             packageName, UserHandle.of(userId));
@@ -964,7 +964,8 @@ public class ApplicationsState {
                         long now = SystemClock.uptimeMillis();
                         for (int i=0; i<mAppEntries.size(); i++) {
                             AppEntry entry = mAppEntries.get(i);
-                            if (entry.size == SIZE_UNKNOWN || entry.sizeStale) {
+                            if ((entry.info.flags & ApplicationInfo.FLAG_INSTALLED) != 0
+                                    && (entry.size == SIZE_UNKNOWN || entry.sizeStale)) {
                                 if (entry.sizeLoadStart == 0 ||
                                         (entry.sizeLoadStart < (now-20*1000))) {
                                     if (!mRunning) {
