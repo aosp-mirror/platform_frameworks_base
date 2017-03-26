082f83bf0d5d1ccc9f3ad63acab8bd67df812749
Jeff Sharkey <jsharkey@android.com>
2017-03-26 14:34:47 -0600

    Send storage broadcasts to manifest receivers.
    Bug: 35316141
    Test: builds, boots
    Change-Id: I50998416f189a7f7662ba59ad5a6b6cacf114cf2

--- services/core/java/com/android/server/StorageManagerService.java
+++ services/core/java/com/android/server/StorageManagerService.java
@@ -714,7 +714,8 @@ class StorageManagerService extends IStorageManager.Stub
                         final Intent intent = new Intent(action,
                                 Uri.fromFile(userVol.getPathFile()));
                         intent.putExtra(StorageVolume.EXTRA_STORAGE_VOLUME, userVol);
-                        intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT);
+                        intent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY_BEFORE_BOOT
+                                | Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND);
                         mContext.sendBroadcastAsUser(intent, userVol.getOwner());
                     }
                     break;
