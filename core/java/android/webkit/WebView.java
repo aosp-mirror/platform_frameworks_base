25bf787ffbc2c878683e824643941829e5b6967a
Felipe Leme <felipeal@google.com>
2017-03-28 15:32:29 -0700

    Created an HtmlInfo object on ViewStructure.
    The ViewStructure typically represents a View, but it it can also be a virtual
    view; in particular, WebView uses virtual views to represent HTML elements.
    
    Although most of the properties of the HTML element maps to properties of
    Android Views, some properties (such as 'name' and 'id' on <INPUT> fields)
    don't, and those are crucial for autofilling web pages.
    
    Rather than trying to artificially map these properties, it's better to create
    a generic representation, for the following reasons:
    
    1. Web standards move in a different velocity than Android APIs
    2. Android APIs cannot be changed easily. Deprecated APIs continue to work,
       and new added APIs don't work in older versions
    3. The data used for autofill is opaque to the Framework - it's only relevant
       to the node producers (like WebView) and consumers (Autofill services).
    
    Also removed the setIdEntry() that was used for the same purpose.
    
    Fixes: 36696757
    Bug: 36718508
    Test: VirtualContainerActivityTest with new checks pass
    
    Change-Id: Ia626bd1f640b0b5861e81a5915504b95029874c9

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -41,7 +41,6 @@ import android.os.StrictMode;
 import android.os.RemoteException;
 import android.print.PrintDocumentAdapter;
 import android.security.KeyChain;
-import android.text.InputType;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.DragEvent;
@@ -2620,49 +2619,55 @@ public class WebView extends AbsoluteLayout
      * understood by the {@link android.service.autofill.AutofillService} implementations:
      *
      * <ol>
-     *   <li>{@link ViewStructure#setClassName(String)} should be use to describe the type of node:
-     *   <ol>
-     *       <li>If the Android SDK provides a similar View, the full-qualified class name of that
-     *           view should be used.
-     *       <li>Otherwise, the class name should be {@code HTML.iframe}.
-     *   </ol>
+     *   <li>If the Android SDK provides a similar View, then should be set with the
+     *   fully-qualified class name of such view.
      *   <li>The W3C autofill field ({@code autocomplete} tag attribute) maps to
      *       {@link ViewStructure#setAutofillHint(String[])}.
      *   <li>The {@code type} attribute of {@code INPUT} tags maps to
      *       {@link ViewStructure#setInputType(int)}.
-     *   <li>The {@code name} attribute maps to {@link ViewStructure#setIdEntry(String)}.
      *   <li>The {@code value} attribute maps to {@link ViewStructure#setText(CharSequence)}.
      *   <li>The {@code placeholder} attribute maps to {@link ViewStructure#setHint(CharSequence)}.
      *   <li>{@link ViewStructure#setDataIsSensitive(boolean)} whould only be called with
      *       {@code true} for form fields whose {@code value} attribute was not pre-loaded.
+     *   <li>Other HTML attributes can be represented through
+     *   {@link ViewStructure#setHtmlInfo(android.view.ViewStructure.HtmlInfo)}.
      * </ol>
      *
      * <p>Example1: an HTML form with 2 fields for username and password.
      *
      * <pre class="prettyprint">
-     *    <input type="text" name="username" value="mr.sparkle" autocomplete="username" placeholder="Email or username">
-     *    <input type="password" name="password" autocomplete="current-password" placeholder="Password">
+     *    <input type="text" name="username" id="user" value="mr.sparkle" autocomplete="username" placeholder="Email or username">
+     *    <input type="password" name="password" id="pass" autocomplete="current-password" placeholder="Password">
      * </pre>
      *
      * <p>Would map to:
      *
      * <pre class="prettyprint">
-     *     ViewStructure username = //structure.newChildForAutofill(...);
+     *     int index = structure.addChildCount(2);
+     *     ViewStructure username = structure.newChild(index);
+     *     username.setAutofillId(structure, 1); // id 1 - first child
      *     username.setClassName("input");
      *     username.setInputType("android.widget.EditText");
      *     username.setAutofillHints("username");
-     *     username.setIdEntry("username");
+     *     username.setHtmlInfo(child.newHtmlInfoBuilder("input")
+     *         .addAttribute("name", "username")
+     *         .addAttribute("id", "user")
+     *         .build());
      *     username.setHint("Email or username");
      *     username.setAutofillType(View.AUTOFILL_TYPE_TEXT);
      *     username.setAutofillValue(AutofillValue.forText("mr.sparkle"));
      *     username.setText("mr.sparkle");
      *     username.setDataIsSensitive(true); // Contains real username, which is sensitive
      *
-     *     ViewStructure password = //structure.newChildForAutofill(...);
+     *     ViewStructure password = structure.newChild(index + 1);
+     *     username.setAutofillId(structure, 2); // id 2 - second child
      *     password.setInputType("android.widget.EditText");
      *     password.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
      *     password.setAutofillHints("current-password");
-     *     password.setIdEntry("password");
+     *     password.setHtmlInfo(child.newHtmlInfoBuilder("input")
+     *         .addAttribute("name", "password")
+     *         .addAttribute("id", "pass")
+     *         .build());
      *     password.setHint("Password");
      *     password.setAutofillType(View.AUTOFILL_TYPE_TEXT);
      *     password.setDataIsSensitive(false); // Value is not set
@@ -2677,9 +2682,11 @@ public class WebView extends AbsoluteLayout
      * <p>Would map to:
      *
      * <pre class="prettyprint">
-     *     ViewStructure iframe = //structure.newChildForAutofill(...);
-     *     iframe.setClassName("HTML.iframe");
-     *     iframe.setUrl("http://example.com/login");
+     *     int index = structure.addChildCount(1);
+     *     ViewStructure iframe = structure.newChildFor(index);
+     *     iframe.setHtmlInfo(child.newHtmlInfoBuilder("iframe")
+     *         .addAttribute("url", "http://example.com/login")
+     *         .build());
      * </pre>
      */
     @Override
