e4f5f79c65cbe666c7f33ddc4ed22bc168f5fe22
Sanket Padawe <sanketpadawe@google.com>
2017-04-03 11:17:22 -0700

    Make some intents public in SubscriptionManager.
    + As per https://docs.google.com/document/d/1K8C7Ly8YVu032FQlwEdkv3ovcTYQCTvy0PaMax5PmZI/edit#heading=h.g6rt8vhyfjj1
    
    Bug: 36572866
    Test: Builds successfully.
    Change-Id: Ibb1d1faae9a002ccb530446e24897955c12076e1
    Merged-in: Ibb1d1faae9a002ccb530446e24897955c12076e1

--- telephony/java/com/android/internal/telephony/TelephonyIntents.java
+++ telephony/java/com/android/internal/telephony/TelephonyIntents.java
@@ -15,6 +15,7 @@
  */
 
 package com.android.internal.telephony;
+import android.content.Intent;
 
 /**
  * The intents that the telephony services broadcast.
@@ -334,9 +335,11 @@ public class TelephonyIntents {
      * <ul>
      *   <li><em>subscription</em> - A int, the current default subscription.</li>
      * </ul>
+     * @deprecated Use {@link Intent#ACTION_DEFAULT_SUBSCRIPTION_CHANGED}
      */
+    @Deprecated
     public static final String ACTION_DEFAULT_SUBSCRIPTION_CHANGED
-            = "android.intent.action.ACTION_DEFAULT_SUBSCRIPTION_CHANGED";
+            = Intent.ACTION_DEFAULT_SUBSCRIPTION_CHANGED;
 
     /**
      * Broadcast Action: The default data subscription has changed.  This has the following
@@ -364,9 +367,11 @@ public class TelephonyIntents {
      * <ul>
      *   <li><em>subscription</em> - A int, the current sms default subscription.</li>
      * </ul>
+     * @deprecated Use {@link Intent#ACTION_DEFAULT_SMS_SUBSCRIPTION_CHANGED}
      */
+    @Deprecated
     public static final String ACTION_DEFAULT_SMS_SUBSCRIPTION_CHANGED
-            = "android.intent.action.ACTION_DEFAULT_SMS_SUBSCRIPTION_CHANGED";
+            = Intent.ACTION_DEFAULT_SMS_SUBSCRIPTION_CHANGED;
 
     /*
      * Broadcast Action: An attempt to set phone radio type and access technology has changed.
