8c792880937fbdeba884bfffe86412b5ed537930
Sundeep Ghuman <sghuman@google.com>
2017-04-04 17:23:29 -0700

    Listen to RSSI_CHANGED_ACTION broadcasts in WifiTracker.
    This solves issues where the phone status bar and connected AP in the
    picker are out of date.
    
    Bug: 35971425
    Test: runtest --path
    frameworks/base/packages/SettingsLib/tests/integ/src/com/and
    roid/settingslib/wifi/WifiTrackerTest.java
    
    Change-Id: I2a7c50de6c754623fd262aac4e9e21d6bd7fc5cf

--- packages/SettingsLib/src/com/android/settingslib/wifi/AccessPoint.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/AccessPoint.java
@@ -985,25 +985,32 @@ public class AccessPoint implements Comparable<AccessPoint> {
         return false;
     }
 
+    /** Attempt to update the AccessPoint and return true if an update occurred. */
     public boolean update(WifiConfiguration config, WifiInfo info, NetworkInfo networkInfo) {
-        boolean reorder = false;
+        boolean updated = false;
+        final int oldLevel = getLevel();
         if (info != null && isInfoForThisAccessPoint(config, info)) {
-            reorder = (mInfo == null);
-            mRssi = info.getRssi();
-            mInfo = info;
-            mNetworkInfo = networkInfo;
-            if (mAccessPointListener != null) {
-                mAccessPointListener.onAccessPointChanged(this);
+            updated = (mInfo == null);
+            if (mRssi != info.getRssi()) {
+                mRssi = info.getRssi();
+                updated = true;
             }
+            mInfo = info;
+            // TODO(b/37289220): compare NetworkInfo states and set updated = true if necessary
+            mNetworkInfo = networkInfo;
         } else if (mInfo != null) {
-            reorder = true;
+            updated = true;
             mInfo = null;
             mNetworkInfo = null;
-            if (mAccessPointListener != null) {
-                mAccessPointListener.onAccessPointChanged(this);
+        }
+        if (updated && mAccessPointListener != null) {
+            mAccessPointListener.onAccessPointChanged(this);
+
+            if (oldLevel != getLevel() /* current level */) {
+                mAccessPointListener.onLevelChanged(this);
             }
         }
-        return reorder;
+        return updated;
     }
 
     void update(WifiConfiguration config) {
