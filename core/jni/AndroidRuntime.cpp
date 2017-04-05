4e01db8c12a497f4380a8e5855de98fbe25686e6
Yifan Hong <elsk@google.com>
2017-04-05 14:42:05 -0700

    Add Java API for libvintf.
    android.os.VintfObject has two methods:
    
    - report: return device info that can be reported to OTA server
    - verify: verify that metadata for a given OTA package is
              compatible.
    
    Test: pass
    Test: adb shell am instrument -w -e class android.os.VintfObjectTest \
    com.android.frameworks.coretests/android.support.test.runner.AndroidJUnitRunner
    
    Bug: 36814503
    
    Change-Id: Iff8fae289eec8ae9cfc327d0d0d36a1cdd5e6800

--- core/jni/AndroidRuntime.cpp
+++ core/jni/AndroidRuntime.cpp
@@ -164,6 +164,7 @@ extern int register_android_os_HwRemoteBinder(JNIEnv *env);
 extern int register_android_os_MessageQueue(JNIEnv* env);
 extern int register_android_os_Parcel(JNIEnv* env);
 extern int register_android_os_SELinux(JNIEnv* env);
+extern int register_android_os_VintfObject(JNIEnv *env);
 extern int register_android_os_seccomp(JNIEnv* env);
 extern int register_android_os_SystemProperties(JNIEnv *env);
 extern int register_android_os_SystemClock(JNIEnv* env);
@@ -1306,6 +1307,7 @@ static const RegJNIRec gRegJNI[] = {
     REG_JNI(register_android_os_HwBlob),
     REG_JNI(register_android_os_HwParcel),
     REG_JNI(register_android_os_HwRemoteBinder),
+    REG_JNI(register_android_os_VintfObject),
     REG_JNI(register_android_nio_utils),
     REG_JNI(register_android_graphics_Canvas),
     REG_JNI(register_android_graphics_Graphics),
