5d6144056493b0d657e74d13de8e751c06510357
Casey Burkhardt <caseyburkhardt@google.com>
2017-04-06 13:46:50 -0700

    Improved multiuser support for A11y button/Magnification
    - Ensures NavigationBarFragment updates Accessibility button state
      based on the settings of the current user.
    - Launches AccessibilityButtonChooserActivity as the current user
    
    Bug: 36652061
    Bug: 36699310
    Bug: 37103953
    Test: Manual
    Change-Id: Ieded484d548fb4631ceb8a873d667dd53fd2fd79

--- services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
+++ services/accessibility/java/com/android/server/accessibility/AccessibilityManagerService.java
@@ -1586,7 +1586,7 @@ public class AccessibilityManagerService extends IAccessibilityManager.Stub {
     private void showAccessibilityButtonTargetSelection() {
         Intent intent = new Intent(AccessibilityManager.ACTION_CHOOSE_ACCESSIBILITY_BUTTON);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        mContext.startActivity(intent);
+        mContext.startActivityAsUser(intent, UserHandle.of(mCurrentUserId));
     }
 
     private void scheduleNotifyClientsOfServicesStateChange(UserState userState) {
