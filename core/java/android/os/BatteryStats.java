0a4e11480b277108234f4dcab980d7c7bd6e49f5
Ahmed ElArabawy <arabawy@google.com>
2017-04-10 12:08:10 -0700

    power hal: Use power HAL API 1.1
    Switch framework to use Power HAL 1.1
    
    Many subsystems (e.g.wifi) could be living on an
    independent power island (sourced from VBatt directly)
    and might even have their own dedicated XTAL to source
    their clocks. Since these SOCs are capable of
    autonomously operating (while the platform is in one
    of the sleep states), they are still drawing power
    from the VBatt. Hence it is critical to understand
    the  (SOC) level low power statistics as well when
    the battery level changes and be able to find any
    correlation in event of unexpected battery drain.
    
    This commit directs framework to use Power HAL 1.1
    
    Bug: 29339696
    Test: Manual
    Change-Id: I0d779365536e84de5ec50b97d50f0162738383da
    Signed-off-by: Ahmed ElArabawy <arabawy@google.com>

--- core/java/android/os/BatteryStats.java
+++ core/java/android/os/BatteryStats.java
@@ -1157,6 +1157,7 @@ public abstract class BatteryStats implements Parcelable {
 
         // Platform-level low power state stats
         public String statPlatformIdleState;
+        public String statSubsystemPowerState;
 
         public HistoryStepDetails() {
             clear();
@@ -1188,6 +1189,7 @@ public abstract class BatteryStats implements Parcelable {
             out.writeInt(statSoftIrqTime);
             out.writeInt(statIdlTime);
             out.writeString(statPlatformIdleState);
+            out.writeString(statSubsystemPowerState);
         }
 
         public void readFromParcel(Parcel in) {
@@ -1209,6 +1211,7 @@ public abstract class BatteryStats implements Parcelable {
             statSoftIrqTime = in.readInt();
             statIdlTime = in.readInt();
             statPlatformIdleState = in.readString();
+            statSubsystemPowerState = in.readString();
         }
     }
 
@@ -5296,6 +5299,10 @@ public abstract class BatteryStats implements Parcelable {
                         pw.print(", PlatformIdleStat ");
                         pw.print(rec.stepDetails.statPlatformIdleState);
                         pw.println();
+
+                        pw.print(", SubsystemPowerState ");
+                        pw.print(rec.stepDetails.statSubsystemPowerState);
+                        pw.println();
                     } else {
                         pw.print(BATTERY_STATS_CHECKIN_VERSION); pw.print(',');
                         pw.print(HISTORY_DATA); pw.print(",0,Dcpu=");
@@ -5331,6 +5338,11 @@ public abstract class BatteryStats implements Parcelable {
                         pw.print(',');
                         if (rec.stepDetails.statPlatformIdleState != null) {
                             pw.print(rec.stepDetails.statPlatformIdleState);
+                        }
+                        pw.println();
+
+                        if (rec.stepDetails.statSubsystemPowerState != null) {
+                            pw.print(rec.stepDetails.statSubsystemPowerState);
                         }
                         pw.println();
                     }
