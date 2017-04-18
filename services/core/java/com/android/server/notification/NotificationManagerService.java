bb1d7d219cc89b6d0779f287f03111956b2de6ae
Julia Reynolds <juliacr@google.com>
2017-04-18 10:30:51 -0400

    Remove log spam
    Intercepted entries are in zen log already.
    
    Change-Id: I16d81113b4a8a116c1f64f00913763f6c2cf3cd4
    Fixes: 37446463
    Test: take a bug report

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -3607,7 +3607,7 @@ public class NotificationManagerService extends SystemService {
         final boolean aboveThreshold =
                 record.getImportance() >= NotificationManager.IMPORTANCE_DEFAULT;
         final boolean canInterrupt = aboveThreshold && !record.isIntercepted();
-        if (DBG || record.isIntercepted())
+        if (DBG)
             Slog.v(TAG,
                     "pkg=" + record.sbn.getPackageName() + " canInterrupt=" + canInterrupt +
                             " intercept=" + record.isIntercepted()
