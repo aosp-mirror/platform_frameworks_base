ef4b303fc27056ae137563bc958eb3efd016972e
Badhri Jagan Sridharan <Badhri@google.com>
2017-04-18 17:56:54 -0700

    Update UsbService to V1_1 usb hal
    This CL adapts Usb service to V1_1 hal.
    V1_1 hal supports reporting audio_adapter accessory
    and debug accessory.
    
    Bug: 36604276
    Test: Manually test inserting an Audio accessory.
    Also tested to made sure that change is compatible with V1_0
    implementations
    Change-Id: I8e44f5e9ae14b0e41965e8d355c99ac42af93f23

--- services/usb/Android.mk
+++ services/usb/Android.mk
@@ -9,6 +9,7 @@ LOCAL_SRC_FILES += \
 
 LOCAL_JAVA_LIBRARIES := services.core
 LOCAL_STATIC_JAVA_LIBRARIES := android.hardware.usb-V1.0-java-static \
+android.hardware.usb-V1.1-java-static \
 android.hidl.manager-V1.0-java-static
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
