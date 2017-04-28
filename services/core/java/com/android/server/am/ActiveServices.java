1b9dfda1632a3666e28d193537da3c3bc8d18751
Makoto Onuki <omakoto@google.com>
2017-04-28 16:27:15 -0700

    bringDownServiceLocked() shouldn't complain when mServicesByName...
    doesn't contain the given record, which is what happens when this method
    is called by bringUpServiceLocked().
    
    Bug 37746449
    Test: bit CtsAppTestCases:ServiceTest
    
    Change-Id: Ie540f634a0a1d2a1f16df382f1ea09a083ffa483

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -2150,7 +2150,10 @@ public final class ActiveServices {
 
         final ServiceMap smap = getServiceMapLocked(r.userId);
         ServiceRecord found = smap.mServicesByName.remove(r.name);
-        if (found != r) {
+
+        // Note when this method is called by bringUpServiceLocked(), the service is not found
+        // in mServicesByName and found will be null.
+        if (found != null && found != r) {
             // This is not actually the service we think is running...  this should not happen,
             // but if it does, fail hard.
             smap.mServicesByName.put(r.name, found);
