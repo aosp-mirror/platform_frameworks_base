130d45966fcb4ff6d0b5e4a88b271e5c41163ae4
zhouzhijie <zhouzhijie@xiaomi.com>
2017-05-18 10:02:59 +0800

    Creat a new notification to avoid the mTetheredNotificationBuilder changed
    Now the method NotificationBuilder.build() is different with m.
    it is not creat new notification. But here it need to creat a
    new notification to avoid the mTetheredNotificationBuilder
    changed. So we use buildInto to replace build
    
    Change-Id: I274ec833de4ade4a778e1937a09d1e6242d92a7f
    Signed-off-by: zhouzhijie <zhouzhijie@xiaomi.com>

--- services/core/java/com/android/server/connectivity/Tethering.java
+++ services/core/java/com/android/server/connectivity/Tethering.java
@@ -747,7 +747,7 @@ public class Tethering extends BaseNetworkObserver implements IControlsTethering
         mLastNotificationId = icon;
 
         notificationManager.notifyAsUser(null, mLastNotificationId,
-                mTetheredNotificationBuilder.build(), UserHandle.ALL);
+                mTetheredNotificationBuilder.buildInto(new Notification()), UserHandle.ALL);
     }
 
     private void clearTetheredNotification() {
