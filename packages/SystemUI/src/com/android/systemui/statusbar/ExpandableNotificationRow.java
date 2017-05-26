55a3e738a695d23982eb2e904f47a3435994a325
Selim Cinek <cinek@google.com>
2017-05-25 18:30:10 -0700

    Fixed a bug where min-priority were in the middle
    The recent intrusiveness could elevate min priority notifications
    temporarily and the reordering was also blocked while looking at it.
    We're now allowing the shade to reorder if a notification updates to
    min-priority.
    
    Test: manual, add intrusive notification, update while looking at the shade to min
    Change-Id: I75b23a7cbaecc8ae941d6680a924c79371c95991
    Fixes: 37515606

--- packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/ExpandableNotificationRow.java
@@ -91,6 +91,7 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
     }
 
     private LayoutListener mLayoutListener;
+    private boolean mLowPriorityStateUpdated;
     private final NotificationInflater mNotificationInflater;
     private int mIconTransformContentShift;
     private int mIconTransformContentShiftNoIcon;
@@ -1119,6 +1120,15 @@ public class ExpandableNotificationRow extends ActivatableNotificationView
         if (mChildrenContainer != null) {
             mChildrenContainer.setIsLowPriority(isLowPriority);
         }
+    }
+
+
+    public void setLowPriorityStateUpdated(boolean lowPriorityStateUpdated) {
+        mLowPriorityStateUpdated = lowPriorityStateUpdated;
+    }
+
+    public boolean hasLowPriorityStateUpdated() {
+        return mLowPriorityStateUpdated;
     }
 
     public boolean isLowPriority() {
