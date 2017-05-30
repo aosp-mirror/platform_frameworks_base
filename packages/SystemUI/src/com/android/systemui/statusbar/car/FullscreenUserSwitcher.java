33d6ce4c56dc63190affccd23bb0fafcb0286100
Rakesh Iyer <rni@google.com>
2017-05-30 11:02:18 -0700

    Fix up user switch flow a little.
    - Remove the long click listeners since the deletion flow wasn't
      very discoverable or good anyway. Deletion is better accomplished
      in settings.
    - The lock screen shows up immediately after user switch and gets
      called to be shown quite a few times in not very deterministic
      order. Now, we replace the user grid view with an infinite progress
      bar while the switch is in progress so it looks more natural.
    - Switch the background to solid black like in the mocks.
    
    Note that this does not fix the case of the lock screen showing up
    briefly when switching using the notification shade. That will be
    cleaned up when we replace that view with the UserGridView.
    
    Test: Rebooted and verified lock screen still works fine.
    Bug: 36454400
    Change-Id: I19ad8e9305d93a069294e910998749f1f0386b5b

--- packages/SystemUI/src/com/android/systemui/statusbar/car/FullscreenUserSwitcher.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/car/FullscreenUserSwitcher.java
@@ -16,6 +16,8 @@
 
 package com.android.systemui.statusbar.car;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.res.Resources;
 import android.os.CountDownTimer;
 import android.view.View;
@@ -31,11 +33,16 @@ import com.android.systemui.statusbar.policy.UserSwitcherController;
  */
 public class FullscreenUserSwitcher {
     private final View mContainer;
+    private final View mParent;
     private final UserGridView mUserGridView;
     private final UserSwitcherController mUserSwitcherController;
     private final ProgressBar mProgressBar;
+    private final ProgressBar mSwitchingUsers;
     private final int mLoginTimeoutMs;
     private final int mAnimUpdateIntervalMs;
+    private final int mShortAnimDuration;
+
+    private boolean mShowing;
 
     private CountDownTimer mTimer;
 
@@ -43,9 +50,15 @@ public class FullscreenUserSwitcher {
             UserSwitcherController userSwitcherController,
             ViewStub containerStub) {
         mUserSwitcherController = userSwitcherController;
-        mContainer = containerStub.inflate();
+        mParent = containerStub.inflate();
+        mContainer = mParent.findViewById(R.id.container);
         mUserGridView = mContainer.findViewById(R.id.user_grid);
         mUserGridView.init(statusBar, mUserSwitcherController);
+        mUserGridView.setUserSelectionListener(record -> {
+            if (!record.isCurrent) {
+                toggleSwitchInProgress(true);
+            }
+        });
 
         PageIndicator pageIndicator = mContainer.findViewById(R.id.user_switcher_page_indicator);
         pageIndicator.setupWithViewPager(mUserGridView);
@@ -54,20 +67,64 @@ public class FullscreenUserSwitcher {
         Resources res = mContainer.getResources();
         mLoginTimeoutMs = res.getInteger(R.integer.car_user_switcher_timeout_ms);
         mAnimUpdateIntervalMs = res.getInteger(R.integer.car_user_switcher_anim_update_ms);
+        mShortAnimDuration = res.getInteger(android.R.integer.config_shortAnimTime);
 
         mContainer.findViewById(R.id.start_driving).setOnClickListener(v -> {
             cancelTimer();
             automaticallySelectUser();
         });
+
+        mSwitchingUsers = mParent.findViewById(R.id.switching_users);
     }
 
     public void onUserSwitched(int newUserId) {
         mUserGridView.onUserSwitched(newUserId);
     }
 
+    private void toggleSwitchInProgress(boolean inProgress) {
+        if (inProgress) {
+            crossFade(mSwitchingUsers, mContainer);
+        } else {
+            crossFade(mContainer, mSwitchingUsers);
+        }
+    }
+
+    private void crossFade(View incoming, View outgoing) {
+        incoming.animate()
+            .alpha(1.0f)
+            .setDuration(mShortAnimDuration)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationStart(Animator animator) {
+                    incoming.setAlpha(0.0f);
+                    incoming.setVisibility(View.VISIBLE);
+                }
+            });
+
+        outgoing.animate()
+            .alpha(0.0f)
+            .setDuration(mShortAnimDuration)
+            .setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    outgoing.setVisibility(View.GONE);
+                }
+            });
+    }
+
     public void show() {
-        mContainer.setVisibility(View.VISIBLE);
+        if (mShowing) {
+            return;
+        }
+        mShowing = true;
+        mParent.setVisibility(View.VISIBLE);
         cancelTimer();
+
+        // This would be the case if we were in the middle of a switch.
+        if (mProgressBar.getVisibility() != View.VISIBLE) {
+            return;
+        }
+
         mTimer = new CountDownTimer(mLoginTimeoutMs, mAnimUpdateIntervalMs) {
             @Override
             public void onTick(long msUntilFinished) {
@@ -85,8 +142,10 @@ public class FullscreenUserSwitcher {
     }
 
     public void hide() {
+        mShowing = false;
         cancelTimer();
-        mContainer.setVisibility(View.GONE);
+        toggleSwitchInProgress(false);
+        mParent.setVisibility(View.GONE);
     }
 
     private void cancelTimer() {
