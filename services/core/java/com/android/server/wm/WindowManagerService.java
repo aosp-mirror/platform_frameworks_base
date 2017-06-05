51d67cf693a6f80001b9ce8038f0fc91c42e14e4
Albert Chaulk <achaulk@google.com>
2017-06-05 19:35:23 -0400

    Add a WM API to query the touch region for the input method window
    This is needed by VR to handle keyboards that display a window larger
    than the interactable region
    
    Bug: 62194867
    Test: Manually with prints
    Change-Id: I7288956ed8da8d24e7c46ffa8f27461f7abbca5e
    (cherry picked from commit 56162a0a51e2aa752a500d941869f1af652eabbf)

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6188,6 +6188,17 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     @Override
+    public Region getCurrentImeTouchRegion() {
+        synchronized (mWindowMap) {
+            final Region r = new Region();
+            if (mInputMethodWindow != null) {
+                mInputMethodWindow.getTouchableRegion(r);
+            }
+            return r;
+        }
+    }
+
+    @Override
     public boolean hasNavigationBar() {
         return mPolicy.hasNavigationBar();
     }
