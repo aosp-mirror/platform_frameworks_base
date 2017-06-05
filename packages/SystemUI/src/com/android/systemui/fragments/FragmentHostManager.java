3edad31233d391eb55ffe688beb1234b5b79be8f
Jason Monk <jmonk@google.com>
2017-06-05 11:33:48 -0400

    Fix up some jank from unnecessary reinflation
    Don't need to re-inflate fragments because of ui mode change.
    
    Test: manual
    Change-Id: I8cb8ff4f64b5c1cb5982d39c6acced682eaa4d41
    Fixes: 62041537

--- packages/SystemUI/src/com/android/systemui/fragments/FragmentHostManager.java
+++ packages/SystemUI/src/com/android/systemui/fragments/FragmentHostManager.java
@@ -49,7 +49,8 @@ public class FragmentHostManager {
     private final HashMap<String, ArrayList<FragmentListener>> mListeners = new HashMap<>();
     private final View mRootView;
     private final InterestingConfigChanges mConfigChanges = new InterestingConfigChanges(
-            ActivityInfo.CONFIG_FONT_SCALE);
+            ActivityInfo.CONFIG_FONT_SCALE | ActivityInfo.CONFIG_LOCALE
+                | ActivityInfo.CONFIG_SCREEN_LAYOUT | ActivityInfo.CONFIG_ASSETS_PATHS);
     private final FragmentService mManager;
     private final PluginFragmentManager mPlugins = new PluginFragmentManager();
 
