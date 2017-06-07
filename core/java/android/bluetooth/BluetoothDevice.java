ba1e26578e1b80c7e8b201e04f24b8689e4ea2ac
Jakub Pawlowski <jpawlowski@google.com>
2017-06-07 13:45:33 -0700

    Bluetooth: fix connectGatt overload invocations
    Test: sl4a GattOverBrEdrTest
    Bug: 62387078
    Change-Id: I5bfb87683e25f8862587e8000eb4c9bb6ff14f99

--- core/java/android/bluetooth/BluetoothDevice.java
+++ core/java/android/bluetooth/BluetoothDevice.java
@@ -1669,7 +1669,7 @@ public final class BluetoothDevice implements Parcelable {
      */
     public BluetoothGatt connectGatt(Context context, boolean autoConnect,
                                      BluetoothGattCallback callback, int transport) {
-        return (connectGatt(context, autoConnect,callback, TRANSPORT_AUTO, PHY_LE_1M_MASK));
+        return (connectGatt(context, autoConnect,callback, transport, PHY_LE_1M_MASK));
     }
 
     /**
@@ -1693,7 +1693,7 @@ public final class BluetoothDevice implements Parcelable {
      */
     public BluetoothGatt connectGatt(Context context, boolean autoConnect,
                                      BluetoothGattCallback callback, int transport, int phy) {
-        return connectGatt(context, autoConnect,callback, TRANSPORT_AUTO, PHY_LE_1M_MASK, null);
+        return connectGatt(context, autoConnect,callback, transport, phy, null);
     }
 
     /**
