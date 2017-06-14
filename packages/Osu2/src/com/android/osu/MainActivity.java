5c65738e890fd958cd08aec4d54163464b8db137
Peter Qiu <zqiu@google.com>
2017-06-14 10:11:12 -0700

    Osu2: setup ProvisionService
    ProvisionService will be started when the OSU app is
    started with "Provision" command.  This service will
    run on a separate thread from the Main thread, to avoid
    blocking on the Main thread.  This service doesn't
    extend IntentService since it requires to perform
    async operations.
    
    Bug: 62388032
    Test: am start -n com.android.osu/.MainActivity --es
          "com.android.osu.extra.COMMAND" "Provision"
    
    Change-Id: I32c4eb53830a6b0404af085da98f9533346203eb

--- packages/Osu2/src/com/android/osu/MainActivity.java
+++ packages/Osu2/src/com/android/osu/MainActivity.java
@@ -17,14 +17,60 @@
 package com.android.osu;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.wifi.hotspot2.OsuProvider;
 import android.os.Bundle;
+import android.util.Log;
 
 /**
  * Main entry point for the OSU (Online Sign-Up) app.
  */
 public class MainActivity extends Activity {
+    private static final String TAG = "OSU_MainActivity";
+    private OsuService mService;
+
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
+
+        Intent intent = getIntent();
+        if (intent == null) {
+            Log.e(TAG, "Intent not provided");
+            finish();
+        }
+
+        if (!intent.hasExtra(Constants.INTENT_EXTRA_COMMAND)) {
+            Log.e(TAG, "Command not provided");
+            finish();
+        }
+
+        String command = intent.getStringExtra(Constants.INTENT_EXTRA_COMMAND);
+        switch (command) {
+            case Constants.COMMAND_PROVISION:
+                if (!startProvisionService(intent.getParcelableExtra(
+                        Constants.INTENT_EXTRA_OSU_PROVIDER))) {
+                    finish();
+                }
+                break;
+            default:
+                Log.e(TAG, "Unknown command: '" + command + "'");
+                finish();
+                break;
+        }
+    }
+
+    /**
+     * Start the {@link ProvisionService} to perform provisioning tasks.
+     *
+     * @return true if service is started
+     */
+    private boolean startProvisionService(OsuProvider provider) {
+        if (provider == null) {
+            Log.e(TAG, "OSU Provider not provided");
+            return false;
+        }
+        mService = new ProvisionService(this, provider);
+        mService.start();
+        return true;
     }
 }
