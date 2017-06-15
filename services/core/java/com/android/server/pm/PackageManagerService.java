a708300441729c9f55d53ee12bf81e3cf8245367
Todd Kennedy <toddke@google.com>
2017-06-15 06:38:09 -0700

    don't block uninstall of unknown packages
    If a package can't be found because it's not installed or it's
    been filtered due to lack of visibility, don't claim the uninstall
    should be blocked.
    
    Change-Id: I932b77cea5749e5964ca626558bef51cdfdfd17b
    Fixes: 62450984
    Test: Manual

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -19364,7 +19364,7 @@ public class PackageManagerService extends IPackageManager.Stub
         synchronized (mPackages) {
             final PackageSetting ps = mSettings.mPackages.get(packageName);
             if (ps == null || filterAppAccessLPr(ps, Binder.getCallingUid(), userId)) {
-                return true;
+                return false;
             }
             return mSettings.getBlockUninstallLPr(userId, packageName);
         }
