e44d2ae8b1ca5498c15858ad71737fbcdbaae6ad
Colin Cross <ccross@android.com>
2017-06-29 10:27:54 -0700

    Add missing dependency to services for desugaring
    services.core depends on android.hidl.manager-V1.0-java, and is then
    combined into services.  Desugaring services may require loading
    any class in services.core into the JVM in order to run
    LambdaMetafactory.  Add android.hidl.manager-V1.0-java to servcices
    too.
    
    Bug: 63078196
    Test: m -j ANDROID_COMPILE_WITH_JACK=false
    Change-Id: I83e7e8efdff9cd662e179240336abebedf859c62

--- services/Android.mk
+++ services/Android.mk
@@ -44,6 +44,9 @@ LOCAL_STATIC_JAVA_LIBRARIES := $(addprefix services.,$(services)) \
     android.hidl.base-V1.0-java-static \
     android.hardware.biometrics.fingerprint-V2.1-java-static
 
+LOCAL_JAVA_LIBRARIES := \
+    android.hidl.manager-V1.0-java
+
 ifeq ($(EMMA_INSTRUMENT_FRAMEWORK),true)
 LOCAL_EMMA_INSTRUMENT := true
 endif
