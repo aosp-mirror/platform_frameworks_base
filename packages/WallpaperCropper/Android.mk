ca34386397eeddc7424d83a996cc554eb846d663
Aurimas Liutikas <aurimas@google.com>
2017-07-10 17:11:00 -0700

    Remove unneeded use of support library in WallpaperCropper.
    Test: make -j WallpaperCropper
    Change-Id: I46b0d173aeb52e8d638493bbecea8fa6b6ba00a9

--- packages/WallpaperCropper/Android.mk
+++ packages/WallpaperCropper/Android.mk
@@ -6,7 +6,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_JAVA_LIBRARIES := telephony-common
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 
 LOCAL_PACKAGE_NAME := WallpaperCropper
 LOCAL_CERTIFICATE := platform
