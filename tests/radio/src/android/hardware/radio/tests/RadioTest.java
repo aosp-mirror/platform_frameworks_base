6390f888de30b7560b2838629011ceb76ff574a1
smaraaosp <smara@google.com>
2017-07-12 16:08:15 -0700

    Added mediumTest annotation to execute on tradefed.
    Test: Executed test locally.
    
    Change-Id: Ib5dcdff1a689f0728fe8d005fa9683ee9120ae13

--- tests/radio/src/android/hardware/radio/tests/RadioTest.java
+++ tests/radio/src/android/hardware/radio/tests/RadioTest.java
@@ -22,6 +22,7 @@ import android.hardware.radio.RadioManager;
 import android.hardware.radio.RadioTuner;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.MediumTest;
 import android.util.Log;
 
 import java.lang.reflect.Constructor;
@@ -54,6 +55,7 @@ import static org.testng.Assert.assertThrows;
  * A test for broadcast radio API.
  */
 @RunWith(AndroidJUnit4.class)
+@MediumTest
 public class RadioTest {
     private static final String TAG = "RadioTest";
 
