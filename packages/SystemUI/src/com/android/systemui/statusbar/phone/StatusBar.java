f7b7bab3dc0bf6b543aaabbc49eac510a3ab6549
Lucas Dupin <dupin@google.com>
2017-07-12 12:09:54 -0700

    Correct navbar colors after rebooting
    System UI visibility flags shouldn't be set based on overlay
    states. Overlays may persist across reboots leading to
    situations where the navbar might be white when the rest of
    the UI is black.
    
    Change-Id: Id3d1e935c1f02a76491a92e521565468602719d5
    Fixes: 63138897
    Test: reboot, swipe up, look at navbar

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4595,11 +4595,13 @@ public class StatusBar extends SystemUI implements DemoMode,
             try {
                 mOverlayManager.setEnabled("com.android.systemui.theme.lightwallpaper",
                         useDarkText, mCurrentUserId);
-                mStatusBarWindowManager.setKeyguardDark(useDarkText);
             } catch (RemoteException e) {
                 Log.w(TAG, "Can't change theme", e);
             }
         }
+
+        // Make sure we have the correct navbar/statusbar colors.
+        mStatusBarWindowManager.setKeyguardDark(useDarkText);
     }
 
     private void updateDozingState() {
