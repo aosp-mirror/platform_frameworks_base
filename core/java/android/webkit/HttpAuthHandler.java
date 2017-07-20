1dd0a4997d62e85c6bd31306dbca42fdb0e9a7f1
Gustav Sennton <gsennton@google.com>
2017-07-20 14:23:26 +0100

    Remove a couple of unused android.webkit methods.
    Test: build angler system image.
    
    Change-Id: I09d7f955bd86facca4e4c9fb8ecc243f70346978

--- core/java/android/webkit/HttpAuthHandler.java
+++ core/java/android/webkit/HttpAuthHandler.java
@@ -61,14 +61,4 @@ public class HttpAuthHandler extends Handler {
      */
     public void proceed(String username, String password) {
     }
-
-    /**
-     * Gets whether the prompt dialog should be suppressed.
-     *
-     * @return whether the prompt dialog should be suppressed
-     * @hide
-     */
-    public boolean suppressDialog() {
-        return false;
-    }
 }
