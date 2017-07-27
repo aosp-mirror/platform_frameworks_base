741a824d7ae3c0f17563e00ea867aceee6669ab7
Adrian Roos <roosa@google.com>
2017-07-27 13:57:35 +0200

    AOD: Fix wakeup transition jank
    No longer do the upwards animation from AOD1, because there is a fade in instead now.
    
    Also, because the DozeService gets finished when waking up, we were disallowing
    animations again. Instead, we now ignore disallowing animations while waking up.
    
    Also don't cancel the NotificationPanelView wakeup animation when another identical
    request comes in.
    
    Also adds tracing for various SystemUI states.
    
    Bug: 63531607
    Test: Wake up from AOD1. Ensure there is no jank or flicker
    Change-Id: I383ffa320f28ac4bf5432e4f4f5c06372717f66f
    (cherry picked from commit 51465ea3b667a4123baf7eebb8f590aeaec2aafe)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4612,6 +4612,7 @@ public class StatusBar extends SystemUI implements DemoMode,
     }
 
     private void updateDozingState() {
+        Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
         boolean animate = !mDozing && mDozeServiceHost.shouldAnimateWakeup();
         mNotificationPanel.setDozing(mDozing, animate);
@@ -5493,6 +5494,11 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         @Override
         public void setAnimateWakeup(boolean animateWakeup) {
+            if (mWakefulnessLifecycle.getWakefulness() == WAKEFULNESS_AWAKE
+                    || mWakefulnessLifecycle.getWakefulness() == WAKEFULNESS_WAKING) {
+                // Too late to change the wakeup animation.
+                return;
+            }
             mAnimateWakeup = animateWakeup;
         }
 
