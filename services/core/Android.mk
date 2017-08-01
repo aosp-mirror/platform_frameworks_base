be4b610d985fa85720f9a9dd76f78eef5acbfae4
Steven Moreland <smoreland@google.com>
2017-08-01 16:34:40 -0700

    services.core: remove use of -static hidl java
    These libraries are being removed. Now, the regular java
    library can be used as both a static and non-static library,
    we are removing these duplicate libraries which cause
    several types problems (such as undefined behavior when
    both the static and non-static libs are loaded).
    
    Bug: 36376126
    Test: boot marlin device
    Merged-In: I6acb6a41fd5c62a3070ada373aed11706c423463
    Change-Id: Ic3a62e7be36b1efca10a9e2e2d0efc430578d061

--- services/core/Android.mk
+++ services/core/Android.mk
@@ -27,8 +27,8 @@ LOCAL_JAVA_LIBRARIES := \
 LOCAL_STATIC_JAVA_LIBRARIES := \
     time_zone_distro \
     time_zone_distro_installer \
-    android.hidl.base-V1.0-java-static \
-    android.hardware.tetheroffload.control-V1.0-java-static \
+    android.hidl.base-V1.0-java \
+    android.hardware.tetheroffload.control-V1.0-java \
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
