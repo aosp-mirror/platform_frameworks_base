40f5ccd1d02726c1bd15aa5015fe28a1474884ae
Tyler Gunn <tgunn@google.com>
2017-08-04 09:27:26 -0700

    Track conference start in system elapsed time.
    This is important so that Telecom can track the duration of the call,
    which is tracked using wall clock time.
    
    Test: Manual, unit test
    Bug: 64068300
    Merged-In: If642d282cd8134060acf6ffe8d81215c394d800c
    Change-Id: If642d282cd8134060acf6ffe8d81215c394d800c
    (cherry picked from commit b2f875bc5468eec359dfbbe8697f94d7734cafed)

--- telecomm/java/android/telecom/ConnectionService.java
+++ telecomm/java/android/telecom/ConnectionService.java
@@ -1405,6 +1405,7 @@ public abstract class ConnectionService extends Service {
                         connection.isRingbackRequested(),
                         connection.getAudioModeIsVoip(),
                         connection.getConnectTimeMillis(),
+                        connection.getConnectElapsedTimeMillis(),
                         connection.getStatusHints(),
                         connection.getDisconnectCause(),
                         createIdList(connection.getConferenceables()),
@@ -1822,6 +1823,7 @@ public abstract class ConnectionService extends Service {
                             null : conference.getVideoProvider().getInterface(),
                     conference.getVideoState(),
                     conference.getConnectTimeMillis(),
+                    conference.getConnectElapsedTime(),
                     conference.getStatusHints(),
                     conference.getExtras());
 
@@ -1887,6 +1889,7 @@ public abstract class ConnectionService extends Service {
                     connection.isRingbackRequested(),
                     connection.getAudioModeIsVoip(),
                     connection.getConnectTimeMillis(),
+                    connection.getConnectElapsedTimeMillis(),
                     connection.getStatusHints(),
                     connection.getDisconnectCause(),
                     emptyList,
