0e743c969f4084d526817f66c12d23b28a9e52b8
Andrzej Ressel <jereksel@gmail.com>
2017-08-09 00:11:16 +0200

    SystemUI: Allow user to add/remove QS with one click
    Demo: https://www.youtube.com/watch?v=SlnRKLa4K7o
    
    Change-Id: I9250fe5fd6a3edf567f1f4d30fd86838f5ef4610

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileAdapter.java
@@ -312,8 +312,13 @@ public class TileAdapter extends RecyclerView.Adapter<Holder> implements TileSta
         holder.mTileView.handleStateChanged(info.state);
         holder.mTileView.setShowAppLabel(position > mEditIndex && !info.isSystem);
 
+        final boolean selectable = mAccessibilityAction == ACTION_NONE || position < mEditIndex;
+        if (!(mAccessibilityManager.isTouchExplorationEnabled() && selectable)) {
+            holder.mTileView.setOnClickListener(v -> move(holder.getAdapterPosition(),
+                    mEditIndex, holder.mTileView));
+        }
+
         if (mAccessibilityManager.isTouchExplorationEnabled()) {
-            final boolean selectable = mAccessibilityAction == ACTION_NONE || position < mEditIndex;
             holder.mTileView.setClickable(selectable);
             holder.mTileView.setFocusable(selectable);
             holder.mTileView.setImportantForAccessibility(selectable
