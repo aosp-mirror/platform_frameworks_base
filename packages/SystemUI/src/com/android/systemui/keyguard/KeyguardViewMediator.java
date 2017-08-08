4dc098aed14cf42a343932d2074025e8caf16feb
Kevin Chyn <kchyn@google.com>
2017-08-08 15:06:28 -0700

    Reset fingerprint lockout upon successful unlock
    FingerprintSettings should not start listening for fingerprints until
    after Pin/Pattern/Pass is entered
    
    Fixes: 63437524
    Fixes: 63739006
    
    Test: open FP settings, touch an invalid finger 5 times
    close FP settings, re-enter FP settings, enter Pin/Pattern/Pass
    touch FP with valid finger, should not be in lockout mode
    
    Change-Id: I9b322248b30f3c4f49414e5bc0693f03a8f2dbe4

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -560,9 +560,6 @@ public class KeyguardViewMediator extends SystemUI {
             }
 
             tryKeyguardDone();
-            if (strongAuth) {
-                mUpdateMonitor.reportSuccessfulStrongAuthUnlockAttempt();
-            }
         }
 
         @Override
@@ -591,9 +588,6 @@ public class KeyguardViewMediator extends SystemUI {
             mStatusBarKeyguardViewManager.startPreHideAnimation(mHideAnimationFinishedRunnable);
             mHandler.sendEmptyMessageDelayed(KEYGUARD_DONE_PENDING_TIMEOUT,
                     KEYGUARD_DONE_PENDING_TIMEOUT_MS);
-            if (strongAuth) {
-                mUpdateMonitor.reportSuccessfulStrongAuthUnlockAttempt();
-            }
             Trace.endSection();
         }
 
@@ -1288,7 +1282,6 @@ public class KeyguardViewMediator extends SystemUI {
                 // Without this, settings is not enabled until the lock screen first appears
                 setShowingLocked(false);
                 hideLocked();
-                mUpdateMonitor.reportSuccessfulStrongAuthUnlockAttempt();
                 return;
             }
         }
