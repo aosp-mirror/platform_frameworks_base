2b86092afed3eed0f37e09ee9b76ce9c2e9b012b
Nathan Harold <nharold@google.com>
2017-08-18 13:38:17 -0700

    Un-hide TelephonyManager.getSignalStrength()
    Expose the API to directly retrieve the latest cached
    signal strength from ServiceStateTracker.
    
    Bug: 36133439
    Test: manual/sl4a
    Change-Id: I36e4a24f19d49da5797d875ccfd10576ca0a68b8

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -6740,7 +6740,6 @@ public class TelephonyManager {
      * Get the most recent SignalStrength information reported by the modem. Due
      * to power saving this information may not always be current.
      * @return the most recent cached signal strength info from the modem
-     * @hide
      */
     @Nullable
     public SignalStrength getSignalStrength() {
