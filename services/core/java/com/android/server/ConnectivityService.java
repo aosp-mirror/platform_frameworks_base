1bb5c0818f0c4fb426e13b65a3ba3db7f36c3d88
Rubin Xu <rubinxu@google.com>
2017-09-05 18:40:49 +0100

    Always add local subnet routes to the interface's routing table
    For some networks such as mobile data connections, its LinkProperties
    does not contain routes for the local subnet so no such route is added
    to the interface's routing table. This can be problematic especially
    if the device is in VPN lockdown mode where there exists high-priority
    PROHIBIT routing rule which in turn blocks the network's default gateway
    route from being added (next hop address hitting the prohibit rule).
    
    We fix this by patching LinkProperties to always include direct connected routes
    when they are received by ConnectivityService. This has the added advantage that
    when apps get LinkProperties, they see the directly connected routes as well.
    
    Bug: 63662962
    Test: runtest frameworks-core -c android.net.LinkPropertiesTest
    Test: runtest frameworks-services -c com.android.server.ConnectivityServiceTest
    Test: Start with device with mobile data, set up ics-OpenVPN in always-on
          lockdown mode. Turn off mobile data then turn it back on, observe
          mobile data connectivity is restored and VPN successfully reconnects.
    Change-Id: I35b614eebccfd22c4a5270f40256f9be1e25abfb

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -4316,11 +4316,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
             int currentScore, NetworkMisc networkMisc) {
         enforceConnectivityInternalPermission();
 
+        LinkProperties lp = new LinkProperties(linkProperties);
+        lp.ensureDirectlyConnectedRoutes();
         // TODO: Instead of passing mDefaultRequest, provide an API to determine whether a Network
         // satisfies mDefaultRequest.
         final NetworkAgentInfo nai = new NetworkAgentInfo(messenger, new AsyncChannel(),
-                new Network(reserveNetId()), new NetworkInfo(networkInfo), new LinkProperties(
-                linkProperties), new NetworkCapabilities(networkCapabilities), currentScore,
+                new Network(reserveNetId()), new NetworkInfo(networkInfo), lp,
+                new NetworkCapabilities(networkCapabilities), currentScore,
                 mContext, mTrackerHandler, new NetworkMisc(networkMisc), mDefaultRequest, this);
         synchronized (this) {
             nai.networkMonitor.systemReady = mSystemReady;
@@ -4630,6 +4632,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
         synchronized (nai) {
             nai.linkProperties = newLp;
         }
+        // msg.obj is already a defensive copy.
+        nai.linkProperties.ensureDirectlyConnectedRoutes();
         if (nai.everConnected) {
             updateLinkProperties(nai, oldLp);
         }
