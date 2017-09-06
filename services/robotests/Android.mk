458d6a87d3b03999b0863da8f6082a1ca56aaddf
Maurice Lam <yukl@google.com>
2017-09-06 14:38:07 -0700

    Remove dependency on platform-system-robolectric
    Dependency on platform-system-robolectric is not needed when mockito
    is not necessary.
    If mockito is needed, add a static dependency on
    mockito-robolectric-prebuilt.
    
    Test: m RunFrameworksServicesRoboTests
    Bug: 38456058
    Change-Id: I60ac070125cf157225247561336288dfe97789c6

--- services/robotests/Android.mk
+++ services/robotests/Android.mk
@@ -45,7 +45,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 # Include the testing libraries (JUnit4 + Robolectric libs).
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    platform-system-robolectric \
     truth-prebuilt
 
 LOCAL_JAVA_LIBRARIES := \
