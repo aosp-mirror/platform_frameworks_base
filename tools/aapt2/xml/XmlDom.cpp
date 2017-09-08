4b8ca8be4d6b0e867f5a421197fa4b5d006fd311
Shane Farmer <safarmer@google.com>
2017-09-08 12:17:05 -0700

    AAPT2: Handle inflating compiled attributes.
    * Added the resource ID pulled from the XML parser into the compiled
    attribattribue. This allows binary attributes such as the version code
    and min SDK values to be available whern flattening the XmlResource back
    into an APK file.
    
    * Only add a compiled value if the attribute value was not a string.
    This allows string values like the package name and version name to be
    flattened back into an APK file.
    
    Before making this change, aapt would segfault when dumping the manifest
    and dumping badging would be missing values. After making this change,
    the manifest and badging are dumping correctly.
    
    Test: ran unit tests
    Test: manually ran optimize against an APK
    Change-Id: I885bb06557b032b9f702e8065d637d359c2b519b

--- tools/aapt2/xml/XmlDom.cpp
+++ tools/aapt2/xml/XmlDom.cpp
@@ -236,16 +236,22 @@ static void CopyAttributes(Element* el, android::ResXMLParser* parser, StringPoo
         attr.name = util::Utf16ToUtf8(StringPiece16(str16, len));
       }
 
+      uint32_t res_id = parser->getAttributeNameResID(i);
+      if (res_id > 0) {
+        attr.compiled_attribute = AaptAttribute(::aapt::Attribute(), {res_id});
+      }
+
       str16 = parser->getAttributeStringValue(i, &len);
       if (str16) {
         attr.value = util::Utf16ToUtf8(StringPiece16(str16, len));
+      } else {
+        android::Res_value res_value;
+        if (parser->getAttributeValue(i, &res_value) > 0) {
+          attr.compiled_value = ResourceUtils::ParseBinaryResValue(
+              ResourceType::kAnim, {}, parser->getStrings(), res_value, out_pool);
+        }
       }
 
-      android::Res_value res_value;
-      if (parser->getAttributeValue(i, &res_value) > 0) {
-        attr.compiled_value = ResourceUtils::ParseBinaryResValue(
-            ResourceType::kAnim, {}, parser->getStrings(), res_value, out_pool);
-      }
 
       el->attributes.push_back(std::move(attr));
     }
