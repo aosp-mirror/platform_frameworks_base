115a5b2850e435512b6911a0cb79913341e41657
Torne (Richard Coles) <torne@google.com>
2017-09-12 18:12:41 -0400

    Don't notify isolated processes about proxy updates.
    ConnectivityManager is not accessible from isolated processes, so
    notifying them causes exception logspam. Since they don't have network
    privileges anyway, just don't notify them about proxy changes.
    
    Change-Id: Ic7280df84d7f54433a03e10e998e90d48da0f562
    Fixes: 64640552
    Test: set proxy on wifi network, disable wifi, launch webview app, enable wifi

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2030,7 +2030,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                 synchronized (ActivityManagerService.this) {
                     for (int i = mLruProcesses.size() - 1 ; i >= 0 ; i--) {
                         ProcessRecord r = mLruProcesses.get(i);
-                        if (r.thread != null) {
+                        // Don't dispatch to isolated processes as they can't access
+                        // ConnectivityManager and don't have network privileges anyway.
+                        if (r.thread != null && !r.isolated) {
                             try {
                                 r.thread.setHttpProxy(host, port, exclList, pacFileUrl);
                             } catch (RemoteException ex) {
