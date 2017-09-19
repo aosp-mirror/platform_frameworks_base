fcd615fad2747e2259eb35d30a5a019ccf2431e7
Michal Karpinski <mkarpinski@google.com>
2017-09-19 10:34:07 +0100

    [RefactoredBMS] Do not perform backup/restore of instant apps
    This CL replicates ag/2256021 in RefactoredBMS.
    
    Test: runtest -p com.android.server.backup frameworks-services
    Bug: 37973765
    Change-Id: Icf615b97025aa09681f9d25b1de3f94e60a45ee4

--- services/backup/java/com/android/server/backup/utils/AppBackupUtils.java
+++ services/backup/java/com/android/server/backup/utils/AppBackupUtils.java
@@ -61,6 +61,11 @@ public class AppBackupUtils {
             return false;
         }
 
+        // 4. it is an "instant" app
+        if (app.isInstantApp()) {
+            return false;
+        }
+
         // Everything else checks out; the only remaining roadblock would be if the
         // package were disabled
         return !appIsDisabled(app, pm);
