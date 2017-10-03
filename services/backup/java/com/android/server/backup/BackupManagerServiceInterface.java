73093a477dbf4422cccf19233e691c5d2fdf21f3
Christopher Tate <ctate@google.com>
2017-10-03 15:27:56 -0700

    Backup OS service now processes unlock off the main thread
    We now perform the slow work of unlocking the primary user on the
    background handler thread rather than on the main thread.  This
    is expected to shave 150ms+ off of our total unlock latency on
    some products.
    
    Bug: 63389800
    Test: manual
    Change-Id: I70be032a64de3270476615fd9dde1b427e19aa91

--- services/backup/java/com/android/server/backup/BackupManagerServiceInterface.java
+++ services/backup/java/com/android/server/backup/BackupManagerServiceInterface.java
@@ -39,6 +39,8 @@ import java.io.PrintWriter;
  */
 public interface BackupManagerServiceInterface {
 
+  void unlockSystemUser();
+
   // Utility: build a new random integer token
   int generateRandomIntegerToken();
 
