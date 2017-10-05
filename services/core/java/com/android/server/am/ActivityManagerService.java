de82ddac5d91cae93e810cdcecf445330ac7cd6c
Vlad Zavidovych <zavidovych@google.com>
2017-10-05 15:37:13 -0700

    Add header to DropBox crash reports to indicate instant app crash
    Bug: 62372653
    Test: runtest -c com.android.server.am.ActivityManagerServiceTest frameworks-services
    Change-Id: I5a3d293d4bd7b54b6fcfe26516e51aff15354ded
    (cherry picked from commit 9cab93555579dd73607dfa4f526ea3a0df456843)

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -14702,6 +14702,9 @@ public class ActivityManagerService extends IActivityManager.Stub
                 }
                 sb.append("\n");
             }
+            if (process.info.isInstantApp()) {
+                sb.append("Instant-App: true\n");
+            }
         }
     }
 
