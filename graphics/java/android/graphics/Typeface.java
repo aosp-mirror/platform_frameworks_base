e34ad1871b9f7da309af0b4e11b93b1be834f38b
Mihai Popa <popam@google.com>
2017-10-12 17:22:33 +0100

    Enable the public TTC index attribute for fonts
    This change adds support for specifying the TTC index on font entries,
    relevant in the context of .ttc font files used to deliver multiple
    fonts in the same file.
    Bug: 37853920
    Test: adb shell am instrument -w -e class android.content.res.FontResourcesParserTest com.android.frameworks.coretests/android.support.test.runner.AndroidJUnitRunner
    
    Change-Id: I3f74e87dec78e1f5a68ccead80b0f637af1aa7a3

--- graphics/java/android/graphics/Typeface.java
+++ graphics/java/android/graphics/Typeface.java
@@ -250,9 +250,9 @@ public class Typeface {
 
         FontFamily fontFamily = new FontFamily();
         for (final FontFileResourceEntry fontFile : filesEntry.getEntries()) {
-            // TODO: Add ttc and variation font support. (b/37853920)
+            // TODO: Add variation font support. (b/37853920)
             if (!fontFamily.addFontFromAssetManager(mgr, fontFile.getFileName(),
-                    0 /* resourceCookie */, false /* isAsset */, 0 /* ttcIndex */,
+                    0 /* resourceCookie */, false /* isAsset */, fontFile.getTtcIndex(),
                     fontFile.getWeight(), fontFile.getItalic(), null /* axes */)) {
                 return null;
             }
