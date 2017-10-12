7daee3914d1c04fe0d66b7b9b55fd45d71b5e419
Bryce Lee <brycelee@google.com>
2017-10-12 13:46:18 -0700

    ActivityStarter cleanup
    This changelist removes individual flags for launch modes and
    VR display state.
    
    Test: go/wm-smoke
    Bug: 64750076
    Change-Id: I301e44b21eca6fd6d12e64e907a13f3cfe2e2c6d

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -2533,7 +2533,6 @@ public class ActivityManagerService extends IActivityManager.Stub
         synchronized (this) {
             mWindowManager = wm;
             mStackSupervisor.setWindowManager(wm);
-            mActivityStarter.setWindowManager(wm);
             mLockTaskController.setWindowManager(wm);
         }
     }
@@ -2775,7 +2774,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         mIntentFirewall = new IntentFirewall(new IntentFirewallInterface(), mHandler);
         mTaskChangeNotificationController =
                 new TaskChangeNotificationController(this, mStackSupervisor, mHandler);
-        mActivityStarter = new ActivityStarter(this, mStackSupervisor);
+        mActivityStarter = new ActivityStarter(this);
         mRecentTasks = new RecentTasks(this, mStackSupervisor);
         mStackSupervisor.setRecentTasks(mRecentTasks);
         mLockTaskController = new LockTaskController(mContext, mStackSupervisor, mHandler);
