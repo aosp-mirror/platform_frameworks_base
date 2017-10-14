a78acfddb063d1a023809fbad4d029abf67db2c3
Suprabh Shukla <suprabh@google.com>
2017-10-13 19:29:36 -0700

    Exempting tempwhitelist apps from doze
    Adding doze exemptions for apps on the tempwhitelist. Any app on the
    temporary whitelist can schedule jobs that will be exempt from doze
    restrictions as long as the app is in the whitelist.
    
    Test: cts-tradefed run cts-dev -m JobScheduler -t \
    android.jobscheduler.cts.TempWhitelistTest
    
    Bug: 64071030
    
    Change-Id: I3846565d927014e73fd1badd34a57226cc788ef3

--- services/core/java/com/android/server/DeviceIdleController.java
+++ services/core/java/com/android/server/DeviceIdleController.java
@@ -1336,6 +1336,10 @@ public class DeviceIdleController extends SystemService
         public int[] getPowerSaveWhitelistUserAppIds() {
             return DeviceIdleController.this.getPowerSaveWhitelistUserAppIds();
         }
+
+        public int[] getPowerSaveTempWhitelistAppIds() {
+            return DeviceIdleController.this.getAppIdTempWhitelistInternal();
+        }
     }
 
     public DeviceIdleController(Context context) {
