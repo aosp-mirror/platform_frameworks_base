96aac9b3754b9f85b3db87435d890c43092be20d
Tobias Thierer <tobiast@google.com>
2017-10-17 20:26:20 +0100

    Framework: Prefer android.system.Os over libcore.io.Libcore.os
    This is a pure refactoring with no a behavior change other than
    that these calls now go through android.system.Os, which immediately
    delegates to Libcore.os.
    
    This is a first step towards separating framework (via
    android.system.Os) vs. libcore (via Libcore.os) clients of these
    low level APIs. Separating these is a prerequisite towards moving
    the API parts of android.system into framework, and moving the
    rest into a different package in libcore.
    
    Test: Treehugger
    Bug: 67901714
    
    Change-Id: Ifd8349ec5416e5693f40dba48fdf2bef651b7d81

--- services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
@@ -35,6 +35,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.UserHandle;
 import android.system.ErrnoException;
+import android.system.Os;
 import android.util.ArraySet;
 import android.util.Log;
 import android.util.Slog;
@@ -232,7 +233,7 @@ public class PackageManagerServiceUtils {
      */
     public static String realpath(File path) throws IOException {
         try {
-            return Libcore.os.realpath(path.getAbsolutePath());
+            return Os.realpath(path.getAbsolutePath());
         } catch (ErrnoException ee) {
             throw ee.rethrowAsIOException();
         }
