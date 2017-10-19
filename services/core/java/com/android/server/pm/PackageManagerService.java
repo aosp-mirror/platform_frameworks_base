4f41e849f1b7d6d037439d60eace1b134165a148
Fyodor Kupolov <fkupolov@google.com>
2017-10-19 11:45:29 -0700

    Fail only if CONTROL_PRIVAPP_PERMISSIONS_ENFORCE
    Previously the error was also thrown if ro.control_privapp_permissions=log
    
    Test: manual
    Bug: 64693550
    Change-Id: Ia87984e2e5902dd885feeb37f676956fa56b421c

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -12428,7 +12428,8 @@ public class PackageManagerService extends IPackageManager.Stub
                             .getPrivAppDenyPermissions(pkg.packageName);
                     final boolean permissionViolation =
                             deniedPermissions == null || !deniedPermissions.contains(perm);
-                    if (permissionViolation) {
+                    if (permissionViolation
+                            && RoSystemProperties.CONTROL_PRIVAPP_PERMISSIONS_ENFORCE) {
                         if (mPrivappPermissionsViolations == null) {
                             mPrivappPermissionsViolations = new ArraySet<>();
                         }
