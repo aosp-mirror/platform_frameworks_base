e9b5381ddf949ba614a55f87190e5e8937abc7c8
Jorim Jaggi <jjaggi@google.com>
2017-10-20 14:33:18 +0200

    Fix issues with TRON app transition logging
    There was a case where a app launch started activity A. A step in
    the UI finished A and started B in the same task. Next time A was
    started it immediately finished itself, putting B at the top of
    the task. Now, this launch was considered aborted because A was
    finished during launching, making it invisible.
    
    We fix this by modifying the logic to only abort the launch if the
    full task is not visible anymore after an activity has been set to
    invisible.
    
    There is another case where an app launches A, finishes A, and then
    launches B. In this case the home gets visible in between so we
    don't consider this an app launch anymore, and correctly cancel
    the logging. However it's still useful to know that this is
    happening to expose it to the developer, so we log any cancel
    events, no matter where they are coming from.
    
    Test: Launch Snapchat, click Sign Up, click home, relaunch
    Snapchat from home
    Test: Open slow app and press home quickly to test cancel.
    
    Change-Id: I211636c97fd6785adb3fe443a8ffa1ae766946a4
    Merged-In: I211636c97fd6785adb3fe443a8ffa1ae766946a4
    Fixes: 67094673
    (cherry picked from commit a9d408214cd73709ff25f235cf6817eaaf7a23d6)

--- services/core/java/com/android/server/am/ActivityStackSupervisor.java
+++ services/core/java/com/android/server/am/ActivityStackSupervisor.java
@@ -606,7 +606,7 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
     public ActivityStackSupervisor(ActivityManagerService service, Looper looper) {
         mService = service;
         mHandler = new ActivityStackSupervisorHandler(looper);
-        mActivityMetricsLogger = new ActivityMetricsLogger(this, mService.mContext);
+        mActivityMetricsLogger = new ActivityMetricsLogger(this, mService.mContext, looper);
         mKeyguardController = new KeyguardController(service, this);
     }
 
@@ -4836,9 +4836,13 @@ public class ActivityStackSupervisor extends ConfigurationContainer implements D
                 mService.mActivityStarter.sendPowerHintForLaunchStartIfNeeded(true /* forceSend */,
                         targetActivity);
                 mActivityMetricsLogger.notifyActivityLaunching();
-                mService.moveTaskToFrontLocked(task.taskId, 0, bOptions, true /* fromRecents */);
-                mActivityMetricsLogger.notifyActivityLaunched(ActivityManager.START_TASK_TO_FRONT,
-                        targetActivity);
+                try {
+                    mService.moveTaskToFrontLocked(task.taskId, 0, bOptions,
+                            true /* fromRecents */);
+                } finally {
+                    mActivityMetricsLogger.notifyActivityLaunched(START_TASK_TO_FRONT,
+                            targetActivity);
+                }
 
                 // If we are launching the task in the docked stack, put it into resizing mode so
                 // the window renders full-screen with the background filling the void. Also only
