2200da1cdc292507947eb198a4252f95b1dfb0fc
Cassie Han <xih@google.com>
2017-11-15 17:27:31 +0000

    Revert "Allow unknown mcc/mnc when constructing a CellIdentity from Parcel."
    Bug: 69349963
    Bug: 69096589
    Bug: 63984327
    
    This reverts commit 993203c7e0d397f193b80c01774b4be925242dae.
    
    Change-Id: I52839aa5991541399651904587aa342a858a5915

--- telephony/java/android/telephony/CellIdentityLte.java
+++ telephony/java/android/telephony/CellIdentityLte.java
@@ -114,11 +114,10 @@ public final class CellIdentityLte implements Parcelable {
         mTac = tac;
         mEarfcn = earfcn;
 
-        // Only allow INT_MAX if unknown string mcc/mnc
         if (mccStr == null || mccStr.matches("^[0-9]{3}$")) {
             mMccStr = mccStr;
-        } else if (mccStr.isEmpty() || mccStr.equals(String.valueOf(Integer.MAX_VALUE))) {
-            // If the mccStr is empty or unknown, set it as null.
+        } else if (mccStr.isEmpty()) {
+            // If the mccStr parsed from Parcel is empty, set it as null.
             mMccStr = null;
         } else {
             throw new IllegalArgumentException("invalid MCC format");
@@ -126,8 +125,8 @@ public final class CellIdentityLte implements Parcelable {
 
         if (mncStr == null || mncStr.matches("^[0-9]{2,3}$")) {
             mMncStr = mncStr;
-        } else if (mncStr.isEmpty() || mncStr.equals(String.valueOf(Integer.MAX_VALUE))) {
-            // If the mncStr is empty or unknown, set it as null.
+        } else if (mncStr.isEmpty()) {
+            // If the mncStr parsed from Parcel is empty, set it as null.
             mMncStr = null;
         } else {
             throw new IllegalArgumentException("invalid MNC format");
