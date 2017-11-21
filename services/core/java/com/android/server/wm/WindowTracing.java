ab25003f6583bfc5a784f606427630c3994ee381
Vishnu Nair <vishnun@google.com>
2017-11-21 07:32:43 -0800

    Disable window tracing on user builds. - Make window trace shell commands fail gracefully in user builds. - Dont start the trace thread on user builds.
    Bug: 64831661
    
    Test: adb shell cmd window tracing start && adb shell cmd window tracing stop && adb bugreport ~/tmp.zip (Tested on user and eng builds)
    
    Change-Id: I14252ddf8e884a714075383bf803e9f0836696a6

--- services/core/java/com/android/server/wm/WindowTracing.java
+++ services/core/java/com/android/server/wm/WindowTracing.java
@@ -16,6 +16,7 @@
 
 package com.android.server.wm;
 
+import static android.os.Build.IS_USER;
 import static com.android.server.wm.proto.WindowManagerTraceFileProto.ENTRY;
 import static com.android.server.wm.proto.WindowManagerTraceFileProto.MAGIC_NUMBER;
 import static com.android.server.wm.proto.WindowManagerTraceFileProto.MAGIC_NUMBER_H;
@@ -62,6 +63,10 @@ class WindowTracing {
     }
 
     void startTrace(PrintWriter pw) throws IOException {
+        if (IS_USER){
+            logAndPrintln(pw, "Error: Tracing is not supported on user builds.");
+            return;
+        }
         synchronized (mLock) {
             logAndPrintln(pw, "Start tracing to " + mTraceFile + ".");
             mWriteQueue.clear();
@@ -83,6 +88,10 @@ class WindowTracing {
     }
 
     void stopTrace(PrintWriter pw) {
+        if (IS_USER){
+            logAndPrintln(pw, "Error: Tracing is not supported on user builds.");
+            return;
+        }
         synchronized (mLock) {
             logAndPrintln(pw, "Stop tracing to " + mTraceFile + ". Waiting for traces to flush.");
             mEnabled = mEnabledLockFree = false;
@@ -150,7 +159,9 @@ class WindowTracing {
     static WindowTracing createDefaultAndStartLooper(Context context) {
         File file = new File("/data/misc/wmtrace/wm_trace.pb");
         WindowTracing windowTracing = new WindowTracing(file);
-        new Thread(windowTracing::loop, "window_tracing").start();
+        if (!IS_USER){
+            new Thread(windowTracing::loop, "window_tracing").start();
+        }
         return windowTracing;
     }
 
