57157fb310d5671e660e30d9c9b7974242f86755
Neil Fuller <nfuller@google.com>
2017-11-24 15:58:38 +0000

    Fix checkapi for OpenJDK 9 builds
    This change explitly hides com.android.okhttp
    from doclava. The public.txt generation was
    previously relying on a package.html in
    com.android to hide all subpackages, which
    doesn't work with javadoc 9.
    
    Test: make droid (with both OpenJDK 8 and OpenJDK 9)
    Bug: 69709687
    Change-Id: If61b69f913be044adb4cd34867459bde6330ea2c

--- Android.mk
+++ Android.mk
@@ -1027,6 +1027,7 @@ framework_docs_LOCAL_DROIDDOC_OPTIONS := \
     -knowntags ./frameworks/base/docs/knowntags.txt \
     -knowntags ./libcore/known_oj_tags.txt \
     -manifest ./frameworks/base/core/res/AndroidManifest.xml \
+    -hidePackage com.android.okhttp \
     -hidePackage com.android.org.conscrypt \
     -since $(SRC_API_DIR)/1.xml 1 \
     -since $(SRC_API_DIR)/2.xml 2 \
