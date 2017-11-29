3e654843d55a292b337eb661389fb44e05a6e7a7
Todd Kennedy <toddke@google.com>
2017-11-29 13:58:53 -0800

    Fix if clauses
    During a refactor, we introduced updated the method used to
    verify signatures. The "&& additionalTest" clauses were added
    to the wrong if statement [off-by-one]. So, we were performing
    a compat signature check when we shouldn't and not throwing
    an exception when we should.
    
    Change-Id: Ie479be5b254b9b13451ab07a07d20a68c7b828ad
    Fixes: 69053002
    Test: cts-tradefed run commandAndExit cts-dev -m CtsAppSecurityHostTestCases -t android.appsecurity.cts.AppSecurityTests#testSharedUidDifferentCerts

--- services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
@@ -579,16 +579,16 @@ public class PackageManagerServiceUtils {
             // Already existing package. Make sure signatures match
             boolean match = compareSignatures(pkgSetting.sharedUser.signatures.mSignatures,
                     parsedSignatures) == PackageManager.SIGNATURE_MATCH;
-            if (!match) {
+            if (!match && compareCompat) {
                 match = matchSignaturesCompat(
                         packageName, pkgSetting.sharedUser.signatures, parsedSignatures);
             }
-            if (!match && compareCompat) {
+            if (!match && compareRecover) {
                 match = matchSignaturesRecover(
                         packageName, pkgSetting.sharedUser.signatures.mSignatures, parsedSignatures);
                 compatMatch |= match;
             }
-            if (!match && compareRecover) {
+            if (!match) {
                 throw new PackageManagerException(INSTALL_FAILED_SHARED_USER_INCOMPATIBLE,
                         "Package " + packageName
                         + " has no signatures that match those in shared user "
