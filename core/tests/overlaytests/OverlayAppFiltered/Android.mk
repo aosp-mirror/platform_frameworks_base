6117fa8b0fc3b8a49672f8b25582e5097c4c47e4
Paul Duffin <paulduffin@google.com>
2017-11-30 16:17:16 +0000

    Switch usages of legacy-test to android.test.base
    The legacy-test library has been deprecated in favour of the
    android.test.base library which is part of the SDK.
    
    The following change descriptions were generated automatically and so
    may be a little repetitive. They are provided to give the reviewer
    enough information to check the comments match what has actually been
    changed and check the reasoning behind the changes.
    
    * core/tests/overlaytests/OverlayAppFiltered/Android.mk
        Removed legacy-test from LOCAL_JAVA_LIBRARIES because the source
        for module 'com.android.overlaytest.filtered_app_overlay' does not
        depend on classes from legacy-test
    
    * core/tests/overlaytests/OverlayAppFirst/Android.mk
        Removed legacy-test from LOCAL_JAVA_LIBRARIES because the source
        for module 'com.android.overlaytest.first_app_overlay' does not
        depend on classes from legacy-test
    
    * core/tests/overlaytests/OverlayAppSecond/Android.mk
        Removed legacy-test from LOCAL_JAVA_LIBRARIES because the source
        for module 'com.android.overlaytest.second_app_overlay' does not
        depend on classes from legacy-test
    
    * core/tests/overlaytests/OverlayTest/Android.mk
        Replaced 'legacy-test' with 'android.test.base' in
        LOCAL_JAVA_LIBRARIES because the source for module 'OverlayTest'
        depends on classes from android.test.base
    
    * core/tests/overlaytests/OverlayTestOverlay/Android.mk
        Removed legacy-test from LOCAL_JAVA_LIBRARIES because the source
        for module 'com.android.overlaytest.overlay' does not depend on
        classes from legacy-test
    
    * test-mock/Android.mk
        Removed legacy-test from LOCAL_JAVA_LIBRARIES because the source
        for module 'repackaged.android.test.mock' does not depend on
        classes from legacy-test
    
    Bug: 30188076
    Test: make checkbuild
    Change-Id: I729412304884cf5da88b05dbe8fbe645ad7b5b15

--- core/tests/overlaytests/OverlayAppFiltered/Android.mk
+++ core/tests/overlaytests/OverlayAppFiltered/Android.mk
@@ -3,8 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := tests
 
-LOCAL_JAVA_LIBRARIES += legacy-test
-
 LOCAL_SDK_VERSION := system_current
 
 LOCAL_PACKAGE_NAME := com.android.overlaytest.filtered_app_overlay
