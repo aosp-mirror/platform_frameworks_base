3ee89770bf34543e72878e577c61ac45c29766c9
Neil Fuller <nfuller@google.com>
2017-12-04 13:54:57 +0000

    Fix doc typo. Improve class javadoc...
    Fix doc typo (value -> valid). Improve class javadoc,
    fix lint error in import ordering.
    
    Test: Build
    Change-Id: I8899b1f568e052a3d152e6c36fca4d6bbf9584f2

--- core/java/android/content/res/XmlResourceParser.java
+++ core/java/android/content/res/XmlResourceParser.java
@@ -16,20 +16,19 @@
 
 package android.content.res;
 
-import org.xmlpull.v1.XmlPullParser;
-
 import android.util.AttributeSet;
+
+import org.xmlpull.v1.XmlPullParser;
 
 /**
  * The XML parsing interface returned for an XML resource.  This is a standard
- * XmlPullParser interface, as well as an extended AttributeSet interface and
- * an additional close() method on this interface for the client to indicate
- * when it is done reading the resource.
+ * {@link XmlPullParser} interface but also extends {@link AttributeSet} and
+ * adds an additional {@link #close()} method for the client to indicate when
+ * it is done reading the resource.
  */
 public interface XmlResourceParser extends XmlPullParser, AttributeSet, AutoCloseable {
     /**
-     * Close this interface to the resource.  Calls on the interface are no
-     * longer value after this call.
+     * Close this parser. Calls on the interface are no longer valid after this call.
      */
     public void close();
 }
