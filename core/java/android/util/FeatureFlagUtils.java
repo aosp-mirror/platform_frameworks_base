ee0e2e9d23511a877c13032725d0dfa383f3aa1d
jackqdyulei <jackqdyulei@google.com>
2017-12-08 09:45:23 -0800

    Add default value for battery app list flag
    Bug: 3331576
    Test: Build
    Change-Id: I3396802c03cf2120f5d1ff5ab9120b7d8949d943

--- core/java/android/util/FeatureFlagUtils.java
+++ core/java/android/util/FeatureFlagUtils.java
@@ -43,6 +43,7 @@ public class FeatureFlagUtils {
         DEFAULT_FLAGS.put("settings_app_info_v2", "false");
         DEFAULT_FLAGS.put("settings_connected_device_v2", "false");
         DEFAULT_FLAGS.put("settings_battery_v2", "false");
+        DEFAULT_FLAGS.put("settings_battery_display_app_list", "false");
     }
 
     /**
