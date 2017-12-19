9addc597e37b15c49dc588fb9a01af2e74ace542
Philip P. Moltmann <moltmann@google.com>
2017-12-19 13:20:22 -0800

    Don't include mockito in frameworks-base-testutils
    So that each test can pick if they want mockito-inline or not.
    
    Test: m -j checkbuild
    Change-Id: I1ac02acebbe5cd01f3032a10932a1c612d8d3a4d

--- tests/utils/testutils/Android.mk
+++ tests/utils/testutils/Android.mk
@@ -24,9 +24,12 @@ LOCAL_MODULE_TAG := tests
 LOCAL_SRC_FILES := $(call all-java-files-under,java)
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-test \
-    mockito-target-minus-junit4
+    android-support-test
 
-LOCAL_JAVA_LIBRARIES := android.test.runner android.test.base android.test.mock
+LOCAL_JAVA_LIBRARIES := \
+    android.test.runner \
+    android.test.base \
+    android.test.mock \
+    mockito-target-minus-junit4 \
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
