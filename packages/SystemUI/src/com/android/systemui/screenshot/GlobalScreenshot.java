21acb224aa29ca2586e64a02ad9aa8cff54f9eee
ezio84 <brabus84@gmail.com>
2017-12-19 14:37:35 +0100

    Screenshot: Append app name to filename
    Author: ezio84 <brabus84@gmail.com>
    Date:   Tue Dec 19 14:37:35 2017 +0100
    
        Screenshot: append app name to filename
    
        Kang Samsung's idea.
    
        Change-Id: Ibc6a52b5e2597e6014a2da6a4211febe17ec02c7
    
    Author: Wang Han <wanghan1995315@gmail.com>
    Date:   Fri May 11 12:47:40 2018 +0200
    
        SystemUI: Don't append app name to file on lockscreen
    
        When device is locked, current implementation will append the current
        app name to file when taking screenshot. This causes information leaks.
    
        To prevent this, we check for isKeyGuardLocked() and fall back not to
        append the app name when taking screenshot on lockscreen.
    
        Change-Id: I04498faf51986e1a3723a32befd97d29e1f3fe58
    
    Author: DroidFreak32 <rushabshah32@gmail.com>
    Date:   Thu Oct 4 11:33:07 2018 +0530
    
        Screenshot: Append app name after screenshot date.
    
        Change-Id: I2ee5d65006ff22dfa381aae50e5757b8e7596e91
    
    Author: ezio84 <brabus84@gmail.com>
    Date:   Sat Jun 16 16:57:19 2018 +0200
    
        GlobalScreenshot: Fix screenshot not saved with some languages
    
        like Virgin Islands English when taking a screenshot of the Settings app
    
        Change-Id: Ic04f66f5813b9597c96835d15c74509c93530a5c
    
    Change-Id: Ic2a42536bf763a0c6f7fb9ac4bbbe84f73723f91

--- packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/GlobalScreenshot.java
@@ -33,6 +33,7 @@ import android.animation.ValueAnimator.AnimatorUpdateListener;
 import android.app.ActivityManager;
 import android.app.ActivityOptions;
 import android.app.ActivityTaskManager;
+import android.app.KeyguardManager;
 import android.app.Notification;
 import android.app.Notification.BigPictureStyle;
 import android.app.NotificationManager;
@@ -46,6 +47,8 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.UserInfo;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -102,6 +105,7 @@ import libcore.io.IoUtils;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -147,6 +151,7 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
     private static final String TAG = "SaveImageInBackgroundTask";
 
     private static final String SCREENSHOT_FILE_NAME_TEMPLATE = "Screenshot_%s.png";
+    private static final String SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME = "Screenshot_%s_%s.png";
     private static final String SCREENSHOT_ID_TEMPLATE = "Screenshot_%s";
     private static final String SCREENSHOT_SHARE_SUBJECT_TEMPLATE = "Screenshot (%s)";
 
@@ -163,6 +168,23 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
     private final boolean mSmartActionsEnabled;
     private final Random mRandom = new Random();
 
+    private static CharSequence getRunningActivityName(Context context) {
+        final ActivityManager am = context.getSystemService(ActivityManager.class);
+        final PackageManager pm = context.getPackageManager();
+
+        List<ActivityManager.RunningTaskInfo> tasks = am.getRunningTasks(1);
+        if (tasks != null && !tasks.isEmpty()) {
+            ActivityManager.RunningTaskInfo top = tasks.get(0);
+            try {
+                ActivityInfo info = pm.getActivityInfo(top.topActivity, 0);
+                return pm.getApplicationLabel(info.applicationInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+            }
+        }
+
+        return null;
+    }
+
     SaveImageInBackgroundTask(Context context, SaveImageInBackgroundData data,
             NotificationManager nManager) {
         Resources r = context.getResources();
@@ -171,7 +193,26 @@ class SaveImageInBackgroundTask extends AsyncTask<Void, Void, Void> {
         mParams = data;
         mImageTime = System.currentTimeMillis();
         String imageDate = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date(mImageTime));
-        mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
+        CharSequence appName = getRunningActivityName(context);
+        boolean onKeyguard = context.getSystemService(KeyguardManager.class).isKeyguardLocked();
+        if (!onKeyguard && appName != null) {
+            String appNameString = appName.toString();
+            try {
+                // With some languages like Virgin Islands English, the Settings app gets a weird
+                // long name and some special voodoo chars, so we convert the string to utf-8 to get
+                // a  char instead, easy to remove it then
+                final String temp = new String(appNameString.getBytes("ISO-8859-15"), "UTF-8");
+                appNameString = temp.replaceAll("[]+", "");
+            } catch (UnsupportedEncodingException e) {
+                // Do nothing
+            }
+            // Now replace all spaces and special chars with an underscore
+            appNameString = appNameString.replaceAll("[\\\\/:*?\"<>|\\s]+", "_");
+            mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE_APPNAME,
+                    imageDate, appNameString);
+        } else {
+            mImageFileName = String.format(SCREENSHOT_FILE_NAME_TEMPLATE, imageDate);
+        }
         mScreenshotId = String.format(SCREENSHOT_ID_TEMPLATE, UUID.randomUUID());
 
         // Initialize screenshot notification smart actions provider.
