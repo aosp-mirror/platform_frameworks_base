0f5bba003bbce3efe558ab3969570a613bf1f386
James Lemieux <jplemieux@google.com>
2018-01-04 17:45:28 -0800

    Adopt robolectric 3.6.1
    Bug: 71596155
    Test: make RunSettingsLibRoboTests
    Change-Id: I0f8e538238c42fed89bc4778a6a717f2eddac9b7

--- services/robotests/Android.mk
+++ services/robotests/Android.mk
@@ -49,7 +49,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 
 LOCAL_JAVA_LIBRARIES := \
     junit \
-    platform-robolectric-3.5.1-prebuilt
+    platform-robolectric-3.6.1-prebuilt
 
 LOCAL_INSTRUMENTATION_FOR := FrameworksServicesLib
 LOCAL_MODULE := FrameworksServicesRoboTests
@@ -74,4 +74,4 @@ LOCAL_TEST_PACKAGE := FrameworksServicesLib
 
 LOCAL_INSTRUMENT_SOURCE_DIRS := $(dir $(LOCAL_PATH))backup/java
 
-include prebuilts/misc/common/robolectric/3.5.1/run_robotests.mk
+include prebuilts/misc/common/robolectric/3.6.1/run_robotests.mk
