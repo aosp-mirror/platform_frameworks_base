55c4e3dcb2d2af4a3192114aa6bd8fa28f3f5bd5
Fan Zhang <zhfan@google.com>
2018-01-16 13:01:35 -0800

    Remove search_v2 flag
    Change-Id: I606111f510d2d44276aac2e71fdc5e87b0e2babe
    Fixes: 69851037
    Test: rebuild

--- core/java/android/util/FeatureFlagUtils.java
+++ core/java/android/util/FeatureFlagUtils.java
@@ -38,7 +38,6 @@ public class FeatureFlagUtils {
     static {
         DEFAULT_FLAGS = new HashMap<>();
         DEFAULT_FLAGS.put("device_info_v2", "true");
-        DEFAULT_FLAGS.put("settings_search_v2", "true");
         DEFAULT_FLAGS.put("settings_app_info_v2", "true");
         DEFAULT_FLAGS.put("settings_connected_device_v2", "true");
         DEFAULT_FLAGS.put("settings_battery_v2", "false");
