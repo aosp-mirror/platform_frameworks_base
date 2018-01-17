861b420bc3282da174523b309b8886fb0d50da58
Bernardo Rufino <brufino@google.com>
2018-01-17 15:04:29 +0000

    Binding on-demand #11: Dump TransportClients
    And adjust logs.
    
    Change-Id: I04fcfb77bac14db36cc30288878bbb65332deb81
    Ref: http://go/br-binding-on-demand
    Bug: 17140907
    Test: adb shell dumpsys backup

--- services/backup/java/com/android/server/backup/TransportManager.java
+++ services/backup/java/com/android/server/backup/TransportManager.java
@@ -42,6 +42,7 @@ import com.android.server.backup.transport.TransportConnectionListener;
 import com.android.server.backup.transport.TransportNotAvailableException;
 import com.android.server.backup.transport.TransportNotRegisteredException;
 
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -630,6 +631,10 @@ public class TransportManager {
     private void checkCanUseTransport() {
         Preconditions.checkState(
                 !Thread.holdsLock(mTransportLock), "Can't call transport with transport lock held");
+    }
+
+    public void dump(PrintWriter pw) {
+        mTransportClientManager.dump(pw);
     }
 
     private static Predicate<ComponentName> fromPackageFilter(String packageName) {
