bionic-linux/android_frameworks_base

Add xfrm mark in IpSecConfig
Bug: 63589600
Test: runtest frameworks-net
Change-Id: I2d38d781b8b31d8bf39fd4e9a7e31509f15a9e16

services/core/java/com/android/server/IpSecService.java
@@ -566,7 +566,9 @@ public class IpSecService extends IIpSecService.Stub {
                                 mResourceId,
                                 mConfig.getSourceAddress(),
                                 mConfig.getDestinationAddress(),
-                                spi);
+                                spi,
+                                mConfig.getXfrmMarkValue(),
+                                mConfig.getXfrmMarkMask());
             } catch (ServiceSpecificException e) {
                 // FIXME: get the error code and throw is at an IOException from Errno Exception
             } catch (RemoteException e) {
@@ -634,7 +636,7 @@ public class IpSecService extends IIpSecService.Stub {
                 mSrvConfig
                         .getNetdInstance()
                         .ipSecDeleteSecurityAssociation(
-                                mResourceId, mSourceAddress, mDestinationAddress, mSpi);
+                                mResourceId, mSourceAddress, mDestinationAddress, mSpi, 0, 0);
             } catch (ServiceSpecificException e) {
                 // FIXME: get the error code and throw is at an IOException from Errno Exception
             } catch (RemoteException e) {
@@ -1153,6 +1155,8 @@ public class IpSecService extends IIpSecService.Stub {
                             c.getDestinationAddress(),
                             (c.getNetwork() != null) ? c.getNetwork().netId : 0,
                             spiRecord.getSpi(),
+                            c.getXfrmMarkValue(),
+                            c.getXfrmMarkMask(),
                             (auth != null) ? auth.getName() : "",
                             (auth != null) ? auth.getKey() : new byte[] {},
                             (auth != null) ? auth.getTruncationLengthBits() : 0,

aosp-mirror/platform_frameworks_base

services/core/java/com/android/server/IpSecService.java
@@ -566,7 +566,9 @@ public class IpSecService extends IIpSecService.Stub {
                                 mResourceId,
                                 mConfig.getSourceAddress(),
                                 mConfig.getDestinationAddress(),
-                                spi);
+                                spi,
+                                mConfig.getMarkValue(),
+                                mConfig.getMarkMask());
             } catch (ServiceSpecificException e) {
                 // FIXME: get the error code and throw is at an IOException from Errno Exception
             } catch (RemoteException e) {
@@ -634,7 +636,7 @@ public class IpSecService extends IIpSecService.Stub {
                 mSrvConfig
                         .getNetdInstance()
                         .ipSecDeleteSecurityAssociation(
-                                mResourceId, mSourceAddress, mDestinationAddress, mSpi);
+                                mResourceId, mSourceAddress, mDestinationAddress, mSpi, 0, 0);
             } catch (ServiceSpecificException e) {
                 // FIXME: get the error code and throw is at an IOException from Errno Exception
             } catch (RemoteException e) {
@@ -1153,6 +1155,8 @@ public class IpSecService extends IIpSecService.Stub {
                             c.getDestinationAddress(),
                             (c.getNetwork() != null) ? c.getNetwork().netId : 0,
                             spiRecord.getSpi(),
+                            c.getMarkValue(),
+                            c.getMarkMask(),
                             (auth != null) ? auth.getName() : "",
                             (auth != null) ? auth.getKey() : new byte[] {},
                             (auth != null) ? auth.getTruncationLengthBits() : 0,