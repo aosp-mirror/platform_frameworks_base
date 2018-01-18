3ddda877e1e1c77198201636e336e1b5ccbdc414
Felipe Leme <felipeal@google.com>
2018-01-18 15:21:36 -0800

    Fixed AUTOFILL_UI_LATENCY metric.
    This metric was using the RESERVED_FOR_LOGBUILDER_COUNTER and hence was not
    being picked up.
    
    Bug: 71863561
    Fixes: 72175011
    
    Test: adb shell logcat -b events | grep sysui | grep 1136
    Test: cts-tradefed run commandAndExit cts-dev -m CtsAutoFillServiceTestCases
    
    Change-Id: I34f701ff5336a413477fd14172d16d8d1e5403ad
    (cherry picked from commit b659adb3f3d13049ca1c08fcec2d419ffd7a386b)

--- services/autofill/java/com/android/server/autofill/Session.java
+++ services/autofill/java/com/android/server/autofill/Session.java
@@ -1397,7 +1397,7 @@ final class Session implements RemoteFillService.FillServiceCallbacks, ViewState
                 mUiLatencyHistory.log(historyLog.toString());
 
                 final LogMaker metricsLog = newLogMaker(MetricsEvent.AUTOFILL_UI_LATENCY)
-                        .setCounterValue((int) duration);
+                        .addTaggedData(MetricsEvent.FIELD_AUTOFILL_DURATION, duration);
                 mMetricsLogger.write(metricsLog);
             }
         }
