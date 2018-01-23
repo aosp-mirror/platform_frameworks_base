e77386e8fbe01b7fbf1a04555d4088796d862f03
Yohei Yukawa <yukawa@google.com>
2018-01-23 10:39:32 -0800

    Introduce InputConnection#reportLanguageHint()
    The goal of this CL is to introduce a way for IMEs to tell
    applications about what languages are likely to be used in the text to
    be committed, for IMEs that support dynamic language switching
    (especially without relying on InputMethodSubtype).
    
    It is already obvious that InputMethodSubtype is not working well for
    this use case.  We need is a simple, easy, and back-portable way that
    can be widely accepted by the ecosystem.
    
    Bug: 7031513
    Test: atest CtsInputMethodTestCases
    Change-Id: Ie86edafd1ed68b58f702116f561fc448fdbb57a8

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -30,6 +30,7 @@ import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Handler;
+import android.os.LocaleList;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.os.StrictMode;
@@ -6025,6 +6026,11 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         @Override
         public boolean commitContent(InputContentInfo inputContentInfo, int flags, Bundle opts) {
             return getTarget().commitContent(inputContentInfo, flags, opts);
+        }
+
+        @Override
+        public void reportLanguageHint(@NonNull LocaleList languageHint) {
+            getTarget().reportLanguageHint(languageHint);
         }
     }
 
