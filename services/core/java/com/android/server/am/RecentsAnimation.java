876a3eeab94c8cf15c86d6af2fae445a85751ad4
Jorim Jaggi <jjaggi@google.com>
2018-01-25 14:39:25 +0100

    Fix issue with flickering with recents animation
    The flicker is caused by executeAppTransition only being called
    after the closing activity has finished pausing, probably because
    it can enter PIP.
    
    We don't need to wait for the activity to finish pausing in this
    case - just execute the transaction and destroy the surfaces
    already!
    
    Test: go/wm-smoke
    Test: Swipe up on PIPable activity with new Launcher APK
    Change-Id: I92efef97b157a13e4670eb0c7e7113afa50302d5

--- services/core/java/com/android/server/am/RecentsAnimation.java
+++ services/core/java/com/android/server/am/RecentsAnimation.java
@@ -153,6 +153,10 @@ class RecentsAnimation implements RecentsAnimationCallbacks {
                 mWindowManager.prepareAppTransition(TRANSIT_NONE, false);
                 mStackSupervisor.ensureActivitiesVisibleLocked(null, 0, false);
                 mStackSupervisor.resumeFocusedStackTopActivityLocked();
+
+                // No reason to wait for the pausing activity in this case, as the hiding of
+                // surfaces needs to be done immediately.
+                mWindowManager.executeAppTransition();
             });
         }
     }
