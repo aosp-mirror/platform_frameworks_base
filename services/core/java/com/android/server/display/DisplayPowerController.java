7bb223428d979ba7882c7e177af2933faaffda45
Michael Wright <michaelwr@google.com>
2018-01-26 00:16:41 +0000

    Use correct brightness when manual brightness is enabled.
    We want to use whatever the current brightness setting value is, not
    whatever the user set last. This means that we'll have a reasonable
    value at first boot and we won't get weird brightness jumps when users
    disable autobrightness.
    
    Fixes: 72508527
    Test: Manual
    
    Change-Id: I4178593f91b231ebcffb6e2104c515e557d975fc

--- services/core/java/com/android/server/display/DisplayPowerController.java
+++ services/core/java/com/android/server/display/DisplayPowerController.java
@@ -839,11 +839,8 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
         }
 
         // Apply manual brightness.
-        // Use the current brightness setting from the request, which is expected
-        // provide a nominal default value for the case where auto-brightness
-        // is not ready yet.
         if (brightness < 0) {
-            brightness = clampScreenBrightness(mLastUserSetScreenBrightness);
+            brightness = clampScreenBrightness(mCurrentScreenBrightnessSetting);
         }
 
 
