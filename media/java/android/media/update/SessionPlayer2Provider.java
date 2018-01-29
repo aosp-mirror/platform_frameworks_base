15fe63424626e5d5fd12363d09c8bb819626ce03
Hyundo Moon <hdmoon@google.com>
2018-01-29 17:06:03 +0900

    MediaSession2/Controller2: Add playlist support
    This CL does the following:
     - Removes PlaylistParams argument from all playlist related methods.
       (i.e. setPlaylist(List, PlaylistParams) becomes setPlaylist(List))
     - Adds methods (to/fromBundle, to/fromBundleList) into MediaItem2.
    
    Bug: 72537268
    Test: Passed MediaSession2Test
    Change-Id: I48ef69ad2728fb7de7efab10f4e077aec552dd0b

--- media/java/android/media/update/SessionPlayer2Provider.java
+++ media/java/android/media/update/SessionPlayer2Provider.java
@@ -42,9 +42,9 @@ public interface SessionPlayer2Provider {
     PlaybackState2 getPlaybackState_impl();
     void setAudioAttributes_impl(AudioAttributes attributes);
     AudioAttributes getAudioAttributes_impl();
-    void setPlaylist_impl(List<MediaItem2> list, PlaylistParams param);
     void addPlaylistItem_impl(int index, MediaItem2 item);
     void removePlaylistItem_impl(MediaItem2 item);
+    void setPlaylist_impl(List<MediaItem2> playlist);
     List<MediaItem2> getPlaylist_impl();
     void setCurrentPlaylistItem_impl(int index);
     void setPlaylistParams_impl(PlaylistParams params);
