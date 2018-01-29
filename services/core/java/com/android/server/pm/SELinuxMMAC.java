60747d28230c5a78e30fc8836946a8a8806ab738
Jeff Vander Stoep <jeffv@google.com>
2018-01-29 12:12:10 -0800

    pm: Apps with shared UID must also share selinux domain
    There are two existing cases where apps that share a sharedUserId
    potentially end up in separate SELinux domains.
    
    1. An app installed in /system/priv-app runs in the priv_app domain.
       An app installed on the /data partition which shares a
       sharedUserId with that priv_app would run in the untrusted_app
       domain (e.g. GTS b/72235911). This issue has existed since
       Android N.
    2. The untrusted_app domain may now deprecate permissions based on
       targetSdkVersion, so apps with sharedUserId may have different
       permissions based on which targetSdkVersion they use. This issue
       has existed since Android O, but is particularly problematic for
       feature "Deprecate world accessible app data" which puts every app
       targeting P+ into its own selinux domain.
    
    This change fixes #1 and adds a temporary workaround to prevent #2.
    
    Test: cts-tradefed run cts -m CtsSelinuxTargetSdkCurrentTestCases
    Test: cts-tradefed run cts -m CtsSelinuxTargetSdk27TestCases
    Test: cts-tradefed run cts -m CtsSelinuxTargetSdk25TestCases
    Bug: 72290969
    Change-Id: I211de05ad6f10b69e3e082cfe977f2dd43d90549

--- services/core/java/com/android/server/pm/SELinuxMMAC.java
+++ services/core/java/com/android/server/pm/SELinuxMMAC.java
@@ -287,7 +287,8 @@ public final class SELinuxMMAC {
      *
      * @param pkg object representing the package to be labeled.
      */
-    public static void assignSeInfoValue(PackageParser.Package pkg) {
+    public static void assignSeInfoValue(PackageParser.Package pkg, boolean isPrivileged,
+            int targetSdkVersion) {
         synchronized (sPolicies) {
             if (!sPolicyRead) {
                 if (DEBUG_POLICY) {
@@ -307,10 +308,11 @@ public final class SELinuxMMAC {
         if (pkg.applicationInfo.targetSandboxVersion == 2)
             pkg.applicationInfo.seInfo += SANDBOX_V2_STR;
 
-        if (pkg.applicationInfo.isPrivilegedApp())
+        if (isPrivileged) {
             pkg.applicationInfo.seInfo += PRIVILEGED_APP_STR;
+        }
 
-        pkg.applicationInfo.seInfo += TARGETSDKVERSION_STR + pkg.applicationInfo.targetSdkVersion;
+        pkg.applicationInfo.seInfo += TARGETSDKVERSION_STR + targetSdkVersion;
 
         if (DEBUG_POLICY_INSTALL) {
             Slog.i(TAG, "package (" + pkg.packageName + ") labeled with " +
