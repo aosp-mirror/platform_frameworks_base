7f50956f8af785568f492259e5f670592b3305be
Amith Yamasani <yamasani@google.com>
2018-01-30 13:42:42 +0000

    Revert "Delay coming out of doze until keyguard dismissed"
    This reverts commit 77ee6507962576e8ddc41ee0209158a799e79519.
    
    Reason for revert: Occasional deadlock b/72684251
    
    Change-Id: I1eb02bf1fefcce66830e1e0ab34a813262ba40d6

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -12788,7 +12788,6 @@ public class ActivityManagerService extends IActivityManager.Stub
             long ident = Binder.clearCallingIdentity();
             try {
                 mKeyguardController.setKeyguardShown(showing, secondaryDisplayShowing);
-                mLocalDeviceIdleController.keyguardShowing(showing);
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
