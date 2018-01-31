e3a08ab5e13e06c33ba868b50ba7ee64380188f4
Peeyush Agarwal <apeeyush@google.com>
2018-01-31 19:07:20 +0000

    Make ambient brightness day stats a system API
    Test: atest android.hardware.display.AmbientBrightnessDayStatsTest
    Bug: 69406079
    Change-Id: I83f476f22a2100d324862cbea2a139eababa9d4e

--- core/tests/coretests/src/android/hardware/display/AmbientBrightnessDayStatsTest.java
+++ core/tests/coretests/src/android/hardware/display/AmbientBrightnessDayStatsTest.java
@@ -16,8 +16,11 @@
 
 package android.hardware.display;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import android.os.Parcel;
 import android.support.test.filters.SmallTest;
@@ -27,20 +30,53 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import java.time.LocalDate;
+import java.util.Arrays;
 
 @SmallTest
 @RunWith(AndroidJUnit4.class)
 public class AmbientBrightnessDayStatsTest {
 
+    private static final LocalDate LOCAL_DATE = LocalDate.now();
+    private static final float[] BUCKET_BOUNDARIES = {0, 1, 10, 100};
+    private static final float[] STATS = {1.3f, 2.6f, 5.8f, 10};
+
+    @Test
+    public void testParamsMustNotBeNull() {
+        assertThrows(NullPointerException.class,
+                () -> new AmbientBrightnessDayStats(null, BUCKET_BOUNDARIES));
+
+        assertThrows(NullPointerException.class,
+                () -> new AmbientBrightnessDayStats(LOCAL_DATE, null));
+
+        assertThrows(NullPointerException.class,
+                () -> new AmbientBrightnessDayStats(null, BUCKET_BOUNDARIES, STATS));
+
+        assertThrows(NullPointerException.class,
+                () -> new AmbientBrightnessDayStats(LOCAL_DATE, null, STATS));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBucketBoundariesMustNotBeEmpty() {
+        new AmbientBrightnessDayStats(LocalDate.now(), new float[]{});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testStatsAndBoundariesMustHaveSameLength() {
+        float[] stats = Arrays.copyOf(STATS, STATS.length + 1);
+        stats[stats.length - 1] = 0;
+        new AmbientBrightnessDayStats(LOCAL_DATE, BUCKET_BOUNDARIES, stats);
+    }
+
     @Test
     public void testAmbientBrightnessDayStatsAdd() {
-        AmbientBrightnessDayStats dayStats = new AmbientBrightnessDayStats(LocalDate.now(),
-                new float[]{0, 1, 10, 100});
+        AmbientBrightnessDayStats dayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES);
         dayStats.log(0, 1);
         dayStats.log(0.5f, 1.5f);
         dayStats.log(50, 12.5f);
         dayStats.log(2000, 1.24f);
         dayStats.log(-10, 0.5f);
+        assertEquals(4, dayStats.getStats().length);
         assertEquals(2.5f, dayStats.getStats()[0], 0);
         assertEquals(0, dayStats.getStats()[1], 0);
         assertEquals(12.5f, dayStats.getStats()[2], 0);
@@ -48,38 +84,12 @@ public class AmbientBrightnessDayStatsTest {
     }
 
     @Test
-    public void testAmbientBrightnessDayStatsEquals() {
-        LocalDate today = LocalDate.now();
-        AmbientBrightnessDayStats dayStats1 = new AmbientBrightnessDayStats(today,
-                new float[]{0, 1, 10, 100});
-        AmbientBrightnessDayStats dayStats2 = new AmbientBrightnessDayStats(today,
-                new float[]{0, 1, 10, 100}, new float[4]);
-        AmbientBrightnessDayStats dayStats3 = new AmbientBrightnessDayStats(today,
-                new float[]{0, 1, 10, 100}, new float[]{1, 3, 5, 7});
-        AmbientBrightnessDayStats dayStats4 = new AmbientBrightnessDayStats(today,
-                new float[]{0, 1, 10, 100}, new float[]{1, 3, 5, 0});
-        assertEquals(dayStats1, dayStats2);
-        assertEquals(dayStats1.hashCode(), dayStats2.hashCode());
-        assertNotEquals(dayStats1, dayStats3);
-        assertNotEquals(dayStats1.hashCode(), dayStats3.hashCode());
-        dayStats4.log(100, 7);
-        assertEquals(dayStats3, dayStats4);
-        assertEquals(dayStats3.hashCode(), dayStats4.hashCode());
-    }
-
-    @Test
-    public void testAmbientBrightnessDayStatsIncorrectInit() {
-        try {
-            new AmbientBrightnessDayStats(LocalDate.now(), new float[]{1, 10, 100},
-                    new float[]{1, 5, 6, 7});
-        } catch (IllegalArgumentException e) {
-            // Expected
-        }
-        try {
-            new AmbientBrightnessDayStats(LocalDate.now(), new float[]{});
-        } catch (IllegalArgumentException e) {
-            // Expected
-        }
+    public void testGetters() {
+        AmbientBrightnessDayStats dayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES, STATS);
+        assertEquals(LOCAL_DATE, dayStats.getLocalDate());
+        assertArrayEquals(BUCKET_BOUNDARIES, dayStats.getBucketBoundaries(), 0);
+        assertArrayEquals(STATS, dayStats.getStats(), 0);
     }
 
     @Test
@@ -100,4 +110,62 @@ public class AmbientBrightnessDayStatsTest {
                 parcel);
         assertEquals(stats, statsAgain);
     }
+
+    @Test
+    public void testAmbientBrightnessDayStatsEquals() {
+        AmbientBrightnessDayStats emptyDayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES);
+        AmbientBrightnessDayStats identicalEmptyDayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES, new float[BUCKET_BOUNDARIES.length]);
+        assertEquals(emptyDayStats, identicalEmptyDayStats);
+        assertEquals(emptyDayStats.hashCode(), identicalEmptyDayStats.hashCode());
+
+        AmbientBrightnessDayStats dayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES, STATS);
+        AmbientBrightnessDayStats identicalDayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES, STATS);
+        assertEquals(dayStats, identicalDayStats);
+        assertEquals(dayStats.hashCode(), identicalDayStats.hashCode());
+
+        assertNotEquals(emptyDayStats, dayStats);
+        assertNotEquals(emptyDayStats.hashCode(), dayStats.hashCode());
+
+        AmbientBrightnessDayStats differentDateDayStats = new AmbientBrightnessDayStats(
+                LOCAL_DATE.plusDays(1), BUCKET_BOUNDARIES, STATS);
+        assertNotEquals(dayStats, differentDateDayStats);
+        assertNotEquals(dayStats.hashCode(), differentDateDayStats.hashCode());
+
+        float[] differentStats = Arrays.copyOf(STATS, STATS.length);
+        differentStats[differentStats.length - 1] += 5f;
+        AmbientBrightnessDayStats differentStatsDayStats = new AmbientBrightnessDayStats(LOCAL_DATE,
+                BUCKET_BOUNDARIES, differentStats);
+        assertNotEquals(dayStats, differentDateDayStats);
+        assertNotEquals(dayStats.hashCode(), differentStatsDayStats.hashCode());
+
+        float[] differentBucketBoundaries = Arrays.copyOf(BUCKET_BOUNDARIES,
+                BUCKET_BOUNDARIES.length);
+        differentBucketBoundaries[differentBucketBoundaries.length - 1] += 100f;
+        AmbientBrightnessDayStats differentBoundariesDayStats = new AmbientBrightnessDayStats(
+                LOCAL_DATE, differentBucketBoundaries, STATS);
+        assertNotEquals(dayStats, differentBoundariesDayStats);
+        assertNotEquals(dayStats.hashCode(), differentBoundariesDayStats.hashCode());
+    }
+
+    private interface ExceptionRunnable {
+        void run() throws Exception;
+    }
+
+    private static void assertThrows(Class<? extends Throwable> exceptionClass,
+            ExceptionRunnable r) {
+        try {
+            r.run();
+        } catch (Throwable e) {
+            assertTrue("Expected exception type " + exceptionClass.getName() + " but got "
+                    + e.getClass().getName(), exceptionClass.isAssignableFrom(e.getClass()));
+            return;
+        }
+        fail("Expected exception type " + exceptionClass.getName()
+                + ", but no exception was thrown");
+    }
+
 }
