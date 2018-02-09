8178408358fe1ef433e6993e0f526ab1fe6a38ba
Bookatz <bookatz@google.com>
2018-02-09 14:26:42 -0800

    STATSD_STARTED broadcast include background
    Allow statsd to tell even receivers in the background
    that it is running.
    
    Test: manual
    Change-Id: I4fe9620eaf846275ee995b90849bf789e78130fd

--- services/core/java/com/android/server/stats/StatsCompanionService.java
+++ services/core/java/com/android/server/stats/StatsCompanionService.java
@@ -783,7 +783,9 @@ public class StatsCompanionService extends IStatsCompanionService.Stub {
         enforceCallingPermission();
         if (DEBUG) Slog.d(TAG, "learned that statsdReady");
         sayHiToStatsd(); // tell statsd that we're ready too and link to it
-        mContext.sendBroadcastAsUser(new Intent(StatsManager.ACTION_STATSD_STARTED),
+        mContext.sendBroadcastAsUser(
+                new Intent(StatsManager.ACTION_STATSD_STARTED)
+                        .addFlags(Intent.FLAG_RECEIVER_INCLUDE_BACKGROUND),
                 UserHandle.SYSTEM,
                 android.Manifest.permission.DUMP);
     }
