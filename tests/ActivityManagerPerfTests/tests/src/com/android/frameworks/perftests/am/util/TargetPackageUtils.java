ebd2ea0daf7d5bbeba8b6444c81b023ea55becec
Arthur Eubanks <aeubanks@google.com>
2018-02-13 10:20:39 -0800

    Start target package by binding to Service, not starting Activity
    This reduces the stddev for a bunch of perf tests by quite a bit
    (mostly the ones that rely on starting the package).
    
    This is because previously the target
    package was made to start running by starting an Activity, which takes
    up CPU cycles.
    
    Now we start a Service and wait for it to report back that its Looper
    queue is idle, then proceed.
    
    This does seem to slightly increase the runtime for some of the tests
    though...
    
    Some test numbers (only tests that have a large difference before and
    after):
    
    Before:
    registeredBroadcast_mean=1920309
    registeredBroadcast_median=1724844
    registeredBroadcast_stddev=559809
    manifestBroadcastRunning_mean=2395693
    manifestBroadcastRunning_median=2063385
    manifestBroadcastRunning_stddev=809836
    contentProviderRunning_mean=668463
    contentProviderRunning_median=588125
    contentProviderRunning_stddev=214116
    bindServiceAllowOomManagement_mean=1952431
    bindServiceAllowOomManagement_median=2017682
    bindServiceAllowOomManagement_stddev=289311
    bindServiceAlreadyBound_mean=2146257
    bindServiceAlreadyBound_median=2022891
    bindServiceAlreadyBound_stddev=614549
    bindServiceRunning_mean=4947541
    bindServiceRunning_median=4710521
    bindServiceRunning_stddev=962094
    startServiceProcessRunningReadUriPermission_mean=1914106
    startServiceProcessRunningReadUriPermission_median=1915209
    startServiceProcessRunningReadUriPermission_stddev=196136
    startServiceProcessRunning_mean=2841122
    startServiceProcessRunning_median=2564792
    startServiceProcessRunning_stddev=684654
    
    After:
    registeredBroadcast_mean=2559587
    registeredBroadcast_median=2579792
    registeredBroadcast_stddev=191377
    manifestBroadcastRunning_mean=2709707
    manifestBroadcastRunning_median=2725365
    manifestBroadcastRunning_stddev=165661
    contentProviderRunning_mean=663058
    contentProviderRunning_median=639062
    contentProviderRunning_stddev=97864
    bindServiceAllowOomManagement_mean=1899179
    bindServiceAllowOomManagement_median=1932553
    bindServiceAllowOomManagement_stddev=184329
    bindServiceAlreadyBound_mean=2023920
    bindServiceAlreadyBound_median=2051614
    bindServiceAlreadyBound_stddev=159993
    bindServiceRunning_mean=6683975
    bindServiceRunning_median=6753699
    bindServiceRunning_stddev=475811
    startServiceProcessRunningReadUriPermission_mean=2388997
    startServiceProcessRunningReadUriPermission_median=2405365
    startServiceProcessRunningReadUriPermission_stddev=244184
    startServiceProcessRunning_mean=3384101
    startServiceProcessRunning_median=3388282
    startServiceProcessRunning_stddev=207333
    
    Test: m ActivityManagerPerfTestsTestApp ActivityManagerPerfTests
    Test: adb install \
    $OUT/data/app/ActivityManagerPerfTestsTestApp/ActivityManagerPerfTestsTestApp.apk
    Test: adb install \
    $OUT/data/app/ActivityManagerPerfTests/ActivityManagerPerfTests.apk
    Test: adb shell am instrument -w \
    com.android.frameworks.perftests.amtests/android.support.test.runner.AndroidJUnitRunner
    
    BUG: 67460485
    Change-Id: Iaaaf7d8db885cc1149e74cb7ce4b47a295ab20e4

--- tests/ActivityManagerPerfTests/tests/src/com/android/frameworks/perftests/am/util/TargetPackageUtils.java
+++ tests/ActivityManagerPerfTests/tests/src/com/android/frameworks/perftests/am/util/TargetPackageUtils.java
@@ -17,10 +17,22 @@
 package com.android.frameworks.perftests.am.util;
 
 import android.app.ActivityManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.ResultReceiver;
 import android.os.SystemClock;
+
+import org.junit.Assert;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class TargetPackageUtils {
     private static final String TAG = TargetPackageUtils.class.getSimpleName();
@@ -28,8 +40,10 @@ public class TargetPackageUtils {
     public static final String PACKAGE_NAME = "com.android.frameworks.perftests.amteststestapp";
     public static final String ACTIVITY_NAME = PACKAGE_NAME + ".TestActivity";
     public static final String SERVICE_NAME = PACKAGE_NAME + ".TestService";
+    private static final String START_SERVICE_NAME = PACKAGE_NAME + ".StartProcessService";
 
     private static final long WAIT_TIME_MS = 100L;
+    private static final long AWAIT_SERVICE_CONNECT_MS = 10000L;
 
     // Cache for test app's uid, so we only have to query it once.
     private static int sTestAppUid = -1;
@@ -37,11 +51,13 @@ public class TargetPackageUtils {
     /**
      * Kills the test package synchronously.
      */
-    public static void killTargetPackage(Context context) {
-        ActivityManager activityManager = context.getSystemService(ActivityManager.class);
+    public static void killTargetPackage(Context context, ServiceConnection serviceConnection) {
+        unbindFromService(context, serviceConnection);
+
+        final ActivityManager activityManager = context.getSystemService(ActivityManager.class);
         activityManager.forceStopPackage(PACKAGE_NAME);
         while (targetPackageIsRunning(context)) {
-            sleep();
+            SystemClock.sleep(WAIT_TIME_MS);
         }
 
         Utils.drainBroadcastQueue();
@@ -50,21 +66,24 @@ public class TargetPackageUtils {
     /**
      * Starts the test package synchronously. It does so by starting an Activity.
      */
-    public static void startTargetPackage(Context context, TimeReceiver timeReceiver) {
-        // "am start-activity -W PACKAGE_NAME/ACTIVITY_CLASS_NAME" still requires a sleep even
-        // though it should be synchronous, so just use Intent instead
+    public static ServiceConnection startTargetPackage(Context context) {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
         final Intent intent = new Intent();
-        intent.putExtras(timeReceiver.createReceiveTimeExtraBinder());
-        intent.setClassName(PACKAGE_NAME, ACTIVITY_NAME);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(intent);
+        intent.putExtra(Intent.EXTRA_RESULT_RECEIVER, new CountDownResultReceiver(countDownLatch));
+        intent.setClassName(PACKAGE_NAME, START_SERVICE_NAME);
+        final ServiceConnection serviceConnection = bindAndWaitForConnectedService(context, intent);
 
-        while (!targetPackageIsRunning(context)) {
-            sleep();
+        try {
+            final boolean targetPackageIdleSuccess = countDownLatch.await(AWAIT_SERVICE_CONNECT_MS,
+                    TimeUnit.MILLISECONDS);
+            Assert.assertTrue("Timeout when waiting for ILooperIdleCallback.Stub.looperIdle()",
+                    targetPackageIdleSuccess);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        // make sure Application has run
-        timeReceiver.getReceivedTimeNs(Constants.TYPE_TARGET_PACKAGE_START);
+
         Utils.drainBroadcastQueue();
+        return serviceConnection;
     }
 
     private static boolean targetPackageIsRunning(Context context) {
@@ -72,10 +91,6 @@ public class TargetPackageUtils {
         final String result = Utils.runShellCommand(
                 String.format("cmd activity get-uid-state %d", uid));
         return !result.contains("(NONEXISTENT)");
-    }
-
-    private static void sleep() {
-        SystemClock.sleep(WAIT_TIME_MS);
     }
 
     private static int getTestAppUid(Context context) {
@@ -88,6 +103,46 @@ public class TargetPackageUtils {
             }
         }
         return sTestAppUid;
+    }
+
+    public static ServiceConnection bindAndWaitForConnectedService(Context context, Intent intent) {
+        return bindAndWaitForConnectedService(context, intent, 0);
+    }
+
+    public static ServiceConnection bindAndWaitForConnectedService(Context context, Intent intent,
+            int bindFlags) {
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final ServiceConnection serviceConnection = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName name, IBinder service) {
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName name) {
+            }
+        };
+
+        final boolean success = context.bindService(intent, serviceConnection,
+                Context.BIND_AUTO_CREATE | bindFlags);
+        Assert.assertTrue("Could not bind to service", success);
+
+        try {
+            final boolean connectedSuccess = countDownLatch.await(AWAIT_SERVICE_CONNECT_MS,
+                    TimeUnit.MILLISECONDS);
+            Assert.assertTrue("Timeout when waiting for ServiceConnection.onServiceConnected()",
+                    connectedSuccess);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        return serviceConnection;
+    }
+
+    public static void unbindFromService(Context context, ServiceConnection serviceConnection) {
+        if (serviceConnection != null) {
+            context.unbindService(serviceConnection);
+        }
     }
 
 }
