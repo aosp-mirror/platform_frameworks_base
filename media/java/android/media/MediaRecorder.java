18ac1ffa3d61d523648396b711afde39bdb4065f
Sachin Mohan Gadag <sgadag@codeaurora.org>
2018-02-14 20:16:42 +0530

    media: Define MPEG-H Encoder Format
    Add entry of MPEG-H Format in encoder
    structure.
    
    CRs-Fixed: 2452080
    Change-Id: I40e63b8828275c4762e5529d67e043c4b60dbd7c
    Signed-off-by: Fawad Shaukat <fshaukat@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- media/java/android/media/MediaRecorder.java
+++ media/java/android/media/MediaRecorder.java
@@ -554,6 +554,8 @@ public class MediaRecorder implements AudioRouting,
         public static final int QCELP = 11;
         /** @hide Linear PCM audio codec */
         public static final int LPCM = 12;
+        /** @hide MPEGH audio codec */
+        public static final int MPEGH = 13;
     }
 
     /**
