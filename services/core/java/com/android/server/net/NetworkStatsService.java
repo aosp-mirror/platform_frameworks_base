92af3dbda1918c8590bd493d01d8a15d65fec44c
Jeff Sharkey <jsharkey@android.com>
2018-02-16 16:28:25 -0700

    Leave NetworkStatsService around during shutdown.
    We've seen system code continue to ask about network statistics
    during a shutdown, so keep our objects around to avoid crashing
    with NPEs.
    
    Test: builds, boots
    Bug: 73323432
    Change-Id: I2654d37a59eb830ff32a2151ae6ad6a999a41677

--- services/core/java/com/android/server/net/NetworkStatsService.java
+++ services/core/java/com/android/server/net/NetworkStatsService.java
@@ -422,13 +422,6 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
         mUidRecorder.forcePersistLocked(currentTime);
         mUidTagRecorder.forcePersistLocked(currentTime);
 
-        mDevRecorder = null;
-        mXtRecorder = null;
-        mUidRecorder = null;
-        mUidTagRecorder = null;
-
-        mXtStatsCached = null;
-
         mSystemReady = false;
     }
 
