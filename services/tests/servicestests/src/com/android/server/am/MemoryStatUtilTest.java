bfcd920858c132da7348494e5fe5e9155e168e64
Rajeev Kumar <rajekumar@google.com>
2018-03-02 22:42:13 +0000

    With build failure fix: Add stats logging in appDiedLocked.
    This reverts commit bcf577a436faffa414d30b3a9c0b0209006b71f7.
    
    Change-Id: I6d4a2aec4faf8ad4722a1c94c48858aebf408674
    
    Test: runtest frameworks-services
    Bug: 73660232

--- services/tests/servicestests/src/com/android/server/am/MemoryStatUtilTest.java
+++ services/tests/servicestests/src/com/android/server/am/MemoryStatUtilTest.java
@@ -16,10 +16,11 @@
 
 package com.android.server.am;
 
-import static com.android.server.am.MemoryStatUtil.parseMemoryStat;
+import static com.android.server.am.MemoryStatUtil.parseMemoryStatFromMemcg;
 import static com.android.server.am.MemoryStatUtil.MemoryStat;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import android.support.test.filters.SmallTest;
 import android.support.test.runner.AndroidJUnit4;
@@ -31,44 +32,44 @@ import org.junit.runner.RunWith;
 @SmallTest
 public class MemoryStatUtilTest {
   private String MEMORY_STAT_CONTENTS = String.join(
-      "\n",
-      "cache 96", // keep different from total_cache to catch reading wrong value
-      "rss 97", // keep different from total_rss to catch reading wrong value
-      "rss_huge 0",
-      "mapped_file 524288",
-      "writeback 0",
-      "swap 95", // keep different from total_rss to catch reading wrong value
-      "pgpgin 16717",
-      "pgpgout 5037",
-      "pgfault 99", // keep different from total_pgfault to catch reading wrong value
-      "pgmajfault 98", // keep different from total_pgmajfault to catch reading wrong value
-      "inactive_anon 503808",
-      "active_anon 46309376",
-      "inactive_file 876544",
-      "active_file 81920",
-      "unevictable 0",
-      "hierarchical_memory_limit 18446744073709551615",
-      "hierarchical_memsw_limit 18446744073709551615",
-      "total_cache 4",
-      "total_rss 3",
-      "total_rss_huge 0",
-      "total_mapped_file 524288",
-      "total_writeback 0",
-      "total_swap 5",
-      "total_pgpgin 16717",
-      "total_pgpgout 5037",
-      "total_pgfault 1",
-      "total_pgmajfault 2",
-      "total_inactive_anon 503808",
-      "total_active_anon 46309376",
-      "total_inactive_file 876544",
-      "total_active_file 81920",
-      "total_unevictable 0");
+          "\n",
+          "cache 96", // keep different from total_cache to catch reading wrong value
+          "rss 97", // keep different from total_rss to catch reading wrong value
+          "rss_huge 0",
+          "mapped_file 524288",
+          "writeback 0",
+          "swap 95", // keep different from total_rss to catch reading wrong value
+          "pgpgin 16717",
+          "pgpgout 5037",
+          "pgfault 99", // keep different from total_pgfault to catch reading wrong value
+          "pgmajfault 98", // keep different from total_pgmajfault to catch reading wrong value
+          "inactive_anon 503808",
+          "active_anon 46309376",
+          "inactive_file 876544",
+          "active_file 81920",
+          "unevictable 0",
+          "hierarchical_memory_limit 18446744073709551615",
+          "hierarchical_memsw_limit 18446744073709551615",
+          "total_cache 4",
+          "total_rss 3",
+          "total_rss_huge 0",
+          "total_mapped_file 524288",
+          "total_writeback 0",
+          "total_swap 5",
+          "total_pgpgin 16717",
+          "total_pgpgout 5037",
+          "total_pgfault 1",
+          "total_pgmajfault 2",
+          "total_inactive_anon 503808",
+          "total_active_anon 46309376",
+          "total_inactive_file 876544",
+          "total_active_file 81920",
+          "total_unevictable 0");
 
 
   @Test
   public void testParseMemoryStat_parsesCorrectValues() throws Exception {
-    MemoryStat stat = parseMemoryStat(MEMORY_STAT_CONTENTS);
+    MemoryStat stat = parseMemoryStatFromMemcg(MEMORY_STAT_CONTENTS);
     assertEquals(stat.pgfault, 1);
     assertEquals(stat.pgmajfault, 2);
     assertEquals(stat.rssInBytes, 3);
@@ -78,18 +79,10 @@ public class MemoryStatUtilTest {
 
   @Test
   public void testParseMemoryStat_emptyMemoryStatContents() throws Exception {
-    MemoryStat stat = parseMemoryStat("");
-    assertEquals(stat.pgfault, 0);
-    assertEquals(stat.pgmajfault, 0);
-    assertEquals(stat.rssInBytes, 0);
-    assertEquals(stat.cacheInBytes, 0);
-    assertEquals(stat.swapInBytes, 0);
+    MemoryStat stat = parseMemoryStatFromMemcg("");
+    assertNull(stat);
 
-    stat = parseMemoryStat(null);
-    assertEquals(stat.pgfault, 0);
-    assertEquals(stat.pgmajfault, 0);
-    assertEquals(stat.rssInBytes, 0);
-    assertEquals(stat.cacheInBytes, 0);
-    assertEquals(stat.swapInBytes, 0);
+    stat = parseMemoryStatFromMemcg(null);
+    assertNull(stat);
   }
 }
