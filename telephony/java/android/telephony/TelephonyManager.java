db6a391957f51c6ed9a768f0c39c61ff7a4f3603
David Kelly <dkelly@afilias.info>
2018-03-12 14:17:24 +0000

    Type Allocation Code & Manufacturer Code
    - Addition of getTypeAllocationCode & getManufacturerCode to
    android.telephony.TelephonyManager.
    
    - The Type Allocation Code is the first eight characters of the IMEI.
    The Type Allocation Code identifies a particular GSM device model.
    
    - The Manufacturer Code is the first eight characters of the MEID.
    The Manufacturer Code identifies the manufacturer of a CDMA device.
    
    - The reasoning behind adding getTypeAllocationCode is to be
    able to obtain the Type Allocation Code without requiring the
    READ_PHONE_STATE permission. Currently in order to obtain the
    Type Allocation Code a substring operation must be performed on
    getImei which is protected by the READ_PHONE_STATE permission.
    
    - The reasoning behind adding getManufacturerCode is to be
    able to obtain the Manufacturer Code without requiring the
    READ_PHONE_STATE permission. Currently in order to obtain the
    Manufacturer Code a substring operation must be performed on
    getMeid which is protected by the READ_PHONE_STATE permission.
    
    - The reasoning that these additional methods do not require the
    READ_PHONE_STATE permission is that neither the Type Allocation
    Code nor the Manufacturer Code can identify a unique device.
    The Type Allocation Code and the Manufacturer Code are analogous
    to other device information such as device model or device
    screen dimensions.
    
    Test: run cts -m CtsTelephonyTestCases
    Bug: 74613795
    Change-Id: I5a586b5a362b39aae13357329efb19eb93f0434c
    Signed-off-by: David Kelly <dkelly@afilias.info>

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1230,6 +1230,33 @@ public class TelephonyManager {
     }
 
     /**
+     * Returns the Type Allocation Code from the IMEI. Return null if Type Allocation Code is not
+     * available.
+     */
+    public String getTypeAllocationCode() {
+        return getTypeAllocationCode(getSlotIndex());
+    }
+
+    /**
+     * Returns the Type Allocation Code from the IMEI. Return null if Type Allocation Code is not
+     * available.
+     *
+     * @param slotIndex of which Type Allocation Code is returned
+     */
+    public String getTypeAllocationCode(int slotIndex) {
+        ITelephony telephony = getITelephony();
+        if (telephony == null) return null;
+
+        try {
+            return telephony.getTypeAllocationCodeForSlot(slotIndex);
+        } catch (RemoteException ex) {
+            return null;
+        } catch (NullPointerException ex) {
+            return null;
+        }
+    }
+
+    /**
      * Returns the MEID (Mobile Equipment Identifier). Return null if MEID is not available.
      *
      * <p>Requires Permission: {@link android.Manifest.permission#READ_PHONE_STATE READ_PHONE_STATE}
@@ -1257,6 +1284,33 @@ public class TelephonyManager {
 
         try {
             return telephony.getMeidForSlot(slotIndex, getOpPackageName());
+        } catch (RemoteException ex) {
+            return null;
+        } catch (NullPointerException ex) {
+            return null;
+        }
+    }
+
+    /**
+     * Returns the Manufacturer Code from the MEID. Return null if Manufacturer Code is not
+     * available.
+     */
+    public String getManufacturerCode() {
+        return getManufacturerCode(getSlotIndex());
+    }
+
+    /**
+     * Returns the Manufacturer Code from the MEID. Return null if Manufacturer Code is not
+     * available.
+     *
+     * @param slotIndex of which Type Allocation Code is returned
+     */
+    public String getManufacturerCode(int slotIndex) {
+        ITelephony telephony = getITelephony();
+        if (telephony == null) return null;
+
+        try {
+            return telephony.getManufacturerCodeForSlot(slotIndex);
         } catch (RemoteException ex) {
             return null;
         } catch (NullPointerException ex) {
