3e2caa1a091e5ce88002587f45022b532c5835a0
chaviw <chaviw@google.com>
2018-03-15 14:23:55 -0700

    Revert "Set options for activity being brought to front."
    This reverts commit df750a3f190ce7ad704971d7f684019780e2741a.
    
    Bug: 74611027

--- services/core/java/com/android/server/am/ActivityStack.java
+++ services/core/java/com/android/server/am/ActivityStack.java
@@ -4594,7 +4594,7 @@ class ActivityStack<T extends StackWindowController> extends ConfigurationContai
             }
             ActivityOptions.abort(options);
         } else {
-            updateTransitLocked(TRANSIT_TASK_TO_FRONT, r, options);
+            updateTransitLocked(TRANSIT_TASK_TO_FRONT, starting, options);
         }
         // If a new task is moved to the front, then mark the existing top activity as supporting
         // picture-in-picture while paused only if the task would not be considered an oerlay on top
