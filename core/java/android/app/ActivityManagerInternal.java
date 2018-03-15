54cff64ec6ef818e270eb39a74d6a58068553d66
Jorim Jaggi <jjaggi@google.com>
2018-03-15 15:51:32 +0100

    Insert correct app transition events for recents anim
    The transition delay will be the latency until all the leashes
    were created etc. Thus, the actual latency is going to be in
    WINDOWS_DRAWN_DELAY.
    
    Also fix an issue where we inadvertently started a transition,
    and then the transition logger was hanging.
    
    Bug: 72967764
    Test: Swipe up from home button, observe eventlog
    Change-Id: I2b1fb7d9d694a629a33653c1fa3d5ed47f53de6b

--- core/java/android/app/ActivityManagerInternal.java
+++ core/java/android/app/ActivityManagerInternal.java
@@ -69,6 +69,13 @@ public abstract class ActivityManagerInternal {
               AppProtoEnums.APP_TRANSITION_SNAPSHOT; // 4
 
     /**
+     * Type for {@link #notifyAppTransitionStarting}: The transition was started because it was a
+     * recents animation and we only needed to wait on the wallpaper.
+     */
+    public static final int APP_TRANSITION_RECENTS_ANIM =
+            AppProtoEnums.APP_TRANSITION_RECENTS_ANIM; // 5
+
+    /**
      * The bundle key to extract the assist data.
      */
     public static final String ASSIST_KEY_DATA = "data";
