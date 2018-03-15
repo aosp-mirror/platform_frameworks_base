a911e847983b98887f610e5a332d1640ab43e7a9
Remi NGUYEN VAN <reminv@google.com>
2018-03-15 11:57:14 +0900

    Add tests for IPv4 of dual stack tethering.
    Also refactoring some Tethering and TetherInterfaceStateMachine calls
    to address testability issues.
    
    This is in preparation of other work to have IPv6-only or 464xlat
    tethering working.
    
    Test: runtest frameworks-net
    Bug: 38218697
    Bug: 64382985
    Bug: 64976379
    Bug: 64995262
    
    Change-Id: I721aca4789ddfbee5a97316aae0b378d79ee2107

--- services/core/java/com/android/server/connectivity/tethering/TetheringDependencies.java
+++ services/core/java/com/android/server/connectivity/tethering/TetheringDependencies.java
@@ -16,8 +16,17 @@
 
 package com.android.server.connectivity.tethering;
 
+import android.content.Context;
+import android.net.INetd;
+import android.net.ip.RouterAdvertisementDaemon;
+import android.net.util.InterfaceParams;
+import android.net.util.NetdService;
 import android.os.Handler;
 import android.net.util.SharedLog;
+
+import com.android.internal.util.StateMachine;
+
+import java.util.ArrayList;
 
 
 /**
@@ -28,5 +37,27 @@ import android.net.util.SharedLog;
 public class TetheringDependencies {
     public OffloadHardwareInterface getOffloadHardwareInterface(Handler h, SharedLog log) {
         return new OffloadHardwareInterface(h, log);
+    }
+
+    public UpstreamNetworkMonitor getUpstreamNetworkMonitor(Context ctx, StateMachine target,
+            SharedLog log, int what) {
+        return new UpstreamNetworkMonitor(ctx, target, log, what);
+    }
+
+    public IPv6TetheringCoordinator getIPv6TetheringCoordinator(
+            ArrayList<TetherInterfaceStateMachine> notifyList, SharedLog log) {
+        return new IPv6TetheringCoordinator(notifyList, log);
+    }
+
+    public RouterAdvertisementDaemon getRouterAdvertisementDaemon(InterfaceParams ifParams) {
+        return new RouterAdvertisementDaemon(ifParams);
+    }
+
+    public InterfaceParams getInterfaceParams(String ifName) {
+        return InterfaceParams.getByName(ifName);
+    }
+
+    public INetd getNetdService() {
+        return NetdService.getInstance();
     }
 }
