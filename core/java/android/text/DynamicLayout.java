313debff3204a507ede554f9042d7fb48d7d1692
Seigo Nonaka <nona@google.com>
2018-03-19 17:04:30 +0000

    Revert "Make PrecomputedText Spannable for supporting selection"
    Crash on Sheets APp
    This reverts commit 80ed5a35a90b62b8070d861b9755e230bd679951.
    
    Bug: 75652829
    Change-Id: I40ddd1b9754e74fbd90d7a029cce9c6a7ede0777
    Test: Manual
    (cherry picked from commit e887f4d2c18040276ee230fdd3dadaa6610a188e)

--- core/java/android/text/DynamicLayout.java
+++ core/java/android/text/DynamicLayout.java
@@ -704,12 +704,7 @@ public class DynamicLayout extends Layout {
         // Spans other than ReplacementSpan can be ignored because line top and bottom are
         // disjunction of all tops and bottoms, although it's not optimal.
         final Paint paint = getPaint();
-        if (text instanceof PrecomputedText) {
-            PrecomputedText precomputed = (PrecomputedText) text;
-            precomputed.getBounds(start, end, mTempRect);
-        } else {
-            paint.getTextBounds(text, start, end, mTempRect);
-        }
+        paint.getTextBounds(text, start, end, mTempRect);
         final Paint.FontMetricsInt fm = paint.getFontMetricsInt();
         return mTempRect.top < fm.top || mTempRect.bottom > fm.bottom;
     }
