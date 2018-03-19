ab715ec20be7d5b16e176d1b1bff1c477c4841bf
Jaewan Kim <jaewan@google.com>
2018-03-19 23:09:00 +0900

    MediaController2: Add fastForward() / rewind()
    Following CLs are included
      - Add MediaController2#fastForward() / rewind()
      - Add MediaSession2.SessionCallback#onFastForward() / rewind()
    
    Bug: 74724709
    Test: Build
    Change-Id: I0dd2b6c21931df937006ae26011ea9538720afee

--- media/java/android/media/update/MediaController2Provider.java
+++ media/java/android/media/update/MediaController2Provider.java
@@ -52,6 +52,8 @@ public interface MediaController2Provider extends TransportControlProvider {
     void playFromSearch_impl(String query, Bundle extras);
     void playFromUri_impl(Uri uri, Bundle extras);
     void playFromMediaId_impl(String mediaId, Bundle extras);
+    void fastForward_impl();
+    void rewind_impl();
 
     void setRating_impl(String mediaId, Rating2 rating);
     void sendCustomCommand_impl(Command command, Bundle args, ResultReceiver cb);
