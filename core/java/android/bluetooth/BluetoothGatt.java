148dd5bf861ad2caac0c53a53ee5154bfa413405
Stanley Tng <stng@google.com>
2018-03-19 12:28:56 -0700

    Add min_ce/max_ce parameters to requestLeConnectionUpdate()
    Add new test parameters, min_ce and max_ce, to the SL4A tests for LE
    Connection-oriented Channel (CoC) feature. This CL passes these 2
    parameters to native stack.
    
    Test: Run cmd: act.py -c $MY_SL4A_CONFIG -tc BleCoc2ConnTest
    Bug: 77528723
    
    Change-Id: I9d3d74f671772014209f8114c2d1b8ba606c54d5

--- core/java/android/bluetooth/BluetoothGatt.java
+++ core/java/android/bluetooth/BluetoothGatt.java
@@ -1514,22 +1514,24 @@ public final class BluetoothGatt implements BluetoothProfile {
      * @return true, if the request is send to the Bluetooth stack.
      * @hide
      */
-    public boolean requestLeConnectionUpdate(int minConnectionInterval,
-                                                 int maxConnectionInterval,
-                                                 int slaveLatency, int supervisionTimeout) {
+    public boolean requestLeConnectionUpdate(int minConnectionInterval, int maxConnectionInterval,
+                                             int slaveLatency, int supervisionTimeout,
+                                             int minConnectionEventLen, int maxConnectionEventLen) {
         if (DBG) {
             Log.d(TAG, "requestLeConnectionUpdate() - min=(" + minConnectionInterval
-                       + ")" + (1.25 * minConnectionInterval)
-                       + "msec, max=(" + maxConnectionInterval + ")"
+                        + ")" + (1.25 * minConnectionInterval)
+                        + "msec, max=(" + maxConnectionInterval + ")"
                         + (1.25 * maxConnectionInterval) + "msec, latency=" + slaveLatency
-                       + ", timeout=" + supervisionTimeout + "msec");
+                        + ", timeout=" + supervisionTimeout + "msec" + ", min_ce="
+                        + minConnectionEventLen + ", max_ce=" + maxConnectionEventLen);
         }
         if (mService == null || mClientIf == 0) return false;
 
         try {
             mService.leConnectionUpdate(mClientIf, mDevice.getAddress(),
-                                               minConnectionInterval, maxConnectionInterval,
-                                               slaveLatency, supervisionTimeout);
+                                        minConnectionInterval, maxConnectionInterval,
+                                        slaveLatency, supervisionTimeout,
+                                        minConnectionEventLen, maxConnectionEventLen);
         } catch (RemoteException e) {
             Log.e(TAG, "", e);
             return false;
