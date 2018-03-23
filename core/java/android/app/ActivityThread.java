055128414ef1d3bb159304e7bc4f294c63c61cac
Andrii Kulian <akulian@google.com>
2018-03-23 17:12:05 +0000

    Revert "Use transaction for local activity relaunch"
    This reverts commit 77d73605979a19e377c97df1c82a2dfd8e3a71cd.
    
    Reason for revert: b/76088057
    
    Bug: 72029061
    Bug: 76088057
    Change-Id: Ib1212740bcd5dd0fe8de87521e716c17b93c30cd
    (cherry picked from commit 46312b3999f2305f42e0e18926ec0ea0067bcd75)

--- core/java/android/app/ActivityThread.java
+++ core/java/android/app/ActivityThread.java
@@ -30,15 +30,12 @@ import android.annotation.Nullable;
 import android.app.assist.AssistContent;
 import android.app.assist.AssistStructure;
 import android.app.backup.BackupAgent;
-import android.app.servertransaction.ActivityLifecycleItem;
 import android.app.servertransaction.ActivityLifecycleItem.LifecycleState;
-import android.app.servertransaction.ActivityRelaunchItem;
 import android.app.servertransaction.ActivityResultItem;
 import android.app.servertransaction.ClientTransaction;
 import android.app.servertransaction.PendingTransactionActions;
 import android.app.servertransaction.PendingTransactionActions.StopInfo;
 import android.app.servertransaction.TransactionExecutor;
-import android.app.servertransaction.TransactionExecutorHelper;
 import android.content.BroadcastReceiver;
 import android.content.ComponentCallbacks2;
 import android.content.ComponentName;
@@ -521,10 +518,6 @@ public final class ActivityThread extends ClientTransactionHandler {
 
         public boolean isPersistable() {
             return activityInfo.persistableMode == ActivityInfo.PERSIST_ACROSS_REBOOTS;
-        }
-
-        public boolean isVisibleFromServer() {
-            return activity != null && activity.mVisibleFromServer;
         }
 
         public String toString() {
@@ -1804,7 +1797,6 @@ public final class ActivityThread extends ClientTransactionHandler {
                         // message is handled.
                         transaction.recycle();
                     }
-                    // TODO(lifecycler): Recycle locally scheduled transactions.
                     break;
             }
             Object obj = msg.obj;
@@ -4731,25 +4723,14 @@ public final class ActivityThread extends ClientTransactionHandler {
             return;
         }
 
+        // TODO(b/73747058): Investigate converting this to use transaction to relaunch.
+        handleRelaunchActivityInner(r, 0 /* configChanges */, null /* pendingResults */,
+                null /* pendingIntents */, null /* pendingActions */, prevState != ON_RESUME,
+                r.overrideConfig, "handleRelaunchActivityLocally");
 
-        // Initialize a relaunch request.
-        final MergedConfiguration mergedConfiguration = new MergedConfiguration(
-                r.createdConfig != null ? r.createdConfig : mConfiguration,
-                r.overrideConfig);
-        final ActivityRelaunchItem activityRelaunchItem = ActivityRelaunchItem.obtain(
-                null /* pendingResults */, null /* pendingIntents */, 0 /* configChanges */,
-                mergedConfiguration, r.mPreserveWindow);
-        // Make sure to match the existing lifecycle state in the end of the transaction.
-        final ActivityLifecycleItem lifecycleRequest =
-                TransactionExecutorHelper.getLifecycleRequestForCurrentState(r);
-        // Schedule the transaction.
-        final ClientTransaction transaction = ClientTransaction.obtain(this.mAppThread, r.token);
-        transaction.addCallback(activityRelaunchItem);
-        transaction.setLifecycleStateRequest(lifecycleRequest);
-        try {
-            mAppThread.scheduleTransaction(transaction);
-        } catch (RemoteException e) {
-            // Local scheduling
+        // Restore back to the previous state before relaunch if needed.
+        if (prevState != r.getLifecycleState()) {
+            mTransactionExecutor.cycleToPath(r, prevState);
         }
     }
 
