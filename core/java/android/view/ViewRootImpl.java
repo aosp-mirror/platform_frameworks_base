33c37cb37c885a18f073a3fb68eba1c3498e812d
akirilov <akirilov@google.com>
2018-03-27 13:08:47 -0700

    RESTRICT AUTOMERGE: Prevent reporting fake package name - framework (backport to nyc-dev)
    Test: added AccessibilityEndToEndTest#testPackageNameCannotBeFaked
          cts-tradefed run cts -m CtsAccessibilityServiceTestCases
          cts-tradefed run cts -m CtsAccessibilityTestCases
    
    Bug: 69981755
    Change-Id: I187e3e9839f654cea9e06e5de93e10e4d1de3109

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -7258,6 +7258,7 @@ public final class ViewRootImpl implements ViewParent,
             if (!registered) {
                 mAttachInfo.mAccessibilityWindowId =
                         mAccessibilityManager.addAccessibilityInteractionConnection(mWindow,
+                                mContext.getPackageName(),
                                 new AccessibilityInteractionConnection(ViewRootImpl.this));
             }
         }
