d2d667bf27779f10e21ea62c3f4202aa87b73f8c
akirilov <akirilov@google.com>
2018-03-27 13:08:47 -0700

    RESTRICT AUTOMERGE: Prevent reporting fake package name - framework (backport to oc-mr1-dev)
    Test: added AccessibilityEndToEndTest#testPackageNameCannotBeFaked
          cts-tradefed run cts -m CtsAccessibilityServiceTestCases
          cts-tradefed run cts -m CtsAccessibilityTestCases
    
    Bug: 69981755
    Change-Id: If3752e106aa7fdee4645dc9852289af471ceff18
    Merged-In: I13304efbee10d1affa087e9c8bc4ec237643283e
    (cherry picked from commit c36db6d473c9988496cd614924ee113b67f7e333)

---
+++ core/java/android/appwidget/AppWidgetManagerInternal.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.appwidget;
+
+import android.annotation.Nullable;
+import android.util.ArraySet;
+
+/**
+ * App widget manager local system service interface.
+ *
+ * @hide Only for use within the system server.
+ */
+public abstract class AppWidgetManagerInternal {
+
+    /**
+     * Gets the packages from which the uid hosts widgets.
+     *
+     * @param uid The potential host UID.
+     * @return Whether the UID hosts widgets from the package.
+     */
+    public abstract @Nullable ArraySet<String> getHostedWidgetPackages(int uid);
+}
