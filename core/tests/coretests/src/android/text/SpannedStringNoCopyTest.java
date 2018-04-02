bd1632355ec72a7a893334a87d103d11ba4c2593
Seigo Nonaka <nona@google.com>
2018-04-02 12:56:56 -0700

    Fix {Spanned|Spannable}StringNoCopyTest test cases
    The meaning of boolean argument was flipped in the last patch set.
    Need to be reversed.
    
    Bug: 77224927
    Test: atest FrameworksCoreTests:android.text
    Change-Id: I8bcd21352e86b29e5f6282641ee4e0bb63bc5da8

--- core/tests/coretests/src/android/text/SpannedStringNoCopyTest.java
+++ core/tests/coretests/src/android/text/SpannedStringNoCopyTest.java
@@ -54,7 +54,7 @@ public class SpannedStringNoCopyTest {
         first.setSpan(new UnderlineSpan(), 0, first.length(), Spanned.SPAN_PRIORITY);
 
         // Do not copy NoCopySpan if specified so.
-        final SpannedString copied = new SpannedString(first, false /* copyNoCopySpan */);
+        final SpannedString copied = new SpannedString(first, true /* ignoreNoCopySpan */);
         final Object[] spans = copied.getSpans(0, copied.length(), Object.class);
         assertNotNull(spans);
         assertEquals(2, spans.length);
@@ -87,7 +87,7 @@ public class SpannedStringNoCopyTest {
 
         // Do not copy NoCopySpan if specified so.
         final SpannedString copied = new SpannedString(
-                new CustomSpanned(first), false /* copyNoCopySpan */);
+                new CustomSpanned(first), true /* ignoreNoCopySpan */);
         final Object[] spans = copied.getSpans(0, copied.length(), Object.class);
         assertNotNull(spans);
         assertEquals(2, spans.length);
