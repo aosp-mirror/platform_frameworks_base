d33f8a1e91fc207c01f8a05c42f06c85758faddb
Jack Yu <jackyu@google.com>
2018-04-13 14:05:32 -0700

    Set country code even when device is not registered on network
    Extends the functionality of getNetworkCountryIso to support
    no SIM/no service case. When no SIM/no service, it returns
    the country code of nearby network (unregistered but cell info
    is available to the device).
    
    Test: Manual
    Bug: 64335394
    Merged-In: Ia32cbc9df9b9adf5d7c24c8b90be20063a94a514
    Change-Id: Ia32cbc9df9b9adf5d7c24c8b90be20063a94a514
    (cherry picked from commit ac36896f1aa90ef21cd23bd213674a01f68f48d3)

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -1810,24 +1810,23 @@ public class TelephonyManager {
     }
 
     /**
-     * Returns the ISO country code equivalent of the current registered
-     * operator's MCC (Mobile Country Code).
+     * Returns the ISO country code equivalent of the MCC (Mobile Country Code) of the current
+     * registered operator, or nearby cell information if not registered.
+     * .
      * <p>
-     * Availability: Only when user is registered to a network. Result may be
-     * unreliable on CDMA networks (use {@link #getPhoneType()} to determine if
-     * on a CDMA network).
+     * Note: Result may be unreliable on CDMA networks (use {@link #getPhoneType()} to determine
+     * if on a CDMA network).
      */
     public String getNetworkCountryIso() {
         return getNetworkCountryIsoForPhone(getPhoneId());
     }
 
     /**
-     * Returns the ISO country code equivalent of the current registered
-     * operator's MCC (Mobile Country Code) of a subscription.
+     * Returns the ISO country code equivalent of the MCC (Mobile Country Code) of the current
+     * registered operator, or nearby cell information if not registered.
      * <p>
-     * Availability: Only when user is registered to a network. Result may be
-     * unreliable on CDMA networks (use {@link #getPhoneType()} to determine if
-     * on a CDMA network).
+     * Note: Result may be unreliable on CDMA networks (use {@link #getPhoneType()} to determine
+     * if on a CDMA network).
      *
      * @param subId for which Network CountryIso is returned
      * @hide
@@ -7024,31 +7023,6 @@ public class TelephonyManager {
         if (SubscriptionManager.isValidPhoneId(phoneId)) {
             setTelephonyProperty(phoneId, TelephonyProperties.PROPERTY_OPERATOR_ISROAMING,
                     isRoaming ? "true" : "false");
-        }
-    }
-
-    /**
-     * Set the ISO country code equivalent of the current registered
-     * operator's MCC (Mobile Country Code).
-     * @param iso the ISO country code equivalent of the current registered
-     * @hide
-     */
-    public void setNetworkCountryIso(String iso) {
-        int phoneId = getPhoneId();
-        setNetworkCountryIsoForPhone(phoneId, iso);
-    }
-
-    /**
-     * Set the ISO country code equivalent of the current registered
-     * operator's MCC (Mobile Country Code).
-     * @param phoneId which phone you want to set
-     * @param iso the ISO country code equivalent of the current registered
-     * @hide
-     */
-    public void setNetworkCountryIsoForPhone(int phoneId, String iso) {
-        if (SubscriptionManager.isValidPhoneId(phoneId)) {
-            setTelephonyProperty(phoneId,
-                    TelephonyProperties.PROPERTY_OPERATOR_ISO_COUNTRY, iso);
         }
     }
 
