7fe3dee3ae1cc47b50c4001cbac56c4fefbcc57b
Yi Jin <jinyithu@google.com>
2018-04-16 16:13:04 -0700

    Add a section flag to bypass failure if a specific path can't be found.
    This is a temporary solution to bypass failure of reading cpu info or 
    battery type in P if the path doesn't exist on some devices. See bug for
    details. However in Q, incidentd should implement reading from the
    common Linux kernel interfaces.
    
    Bug: 78028767
    Bug: 78239764
    Test: atest incidentd_test
    Change-Id: I08ffcb21709efe5478d2ac46121deb5795af1024
    Merged-In: I08ffcb21709efe5478d2ac46121deb5795af1024

--- cmds/incidentd/src/Section.cpp
+++ cmds/incidentd/src/Section.cpp
@@ -149,7 +149,8 @@ DONE:
 }
 
 // ================================================================================
-Section::Section(int i, const int64_t timeoutMs) : id(i), timeoutMs(timeoutMs) {}
+Section::Section(int i, int64_t timeoutMs, bool deviceSpecific)
+    : id(i), timeoutMs(timeoutMs), deviceSpecific(deviceSpecific) {}
 
 Section::~Section() {}
 
@@ -234,8 +235,9 @@ status_t MetadataSection::Execute(ReportRequestSet* requests) const {
 // ================================================================================
 static inline bool isSysfs(const char* filename) { return strncmp(filename, "/sys/", 5) == 0; }
 
-FileSection::FileSection(int id, const char* filename, const int64_t timeoutMs)
-    : Section(id, timeoutMs), mFilename(filename) {
+FileSection::FileSection(int id, const char* filename, const bool deviceSpecific,
+                         const int64_t timeoutMs)
+    : Section(id, timeoutMs, deviceSpecific), mFilename(filename) {
     name = filename;
     mIsSysfs = isSysfs(filename);
 }
@@ -248,7 +250,7 @@ status_t FileSection::Execute(ReportRequestSet* requests) const {
     unique_fd fd(open(mFilename, O_RDONLY | O_CLOEXEC));
     if (fd.get() == -1) {
         ALOGW("FileSection '%s' failed to open file", this->name.string());
-        return -errno;
+        return this->deviceSpecific ? NO_ERROR : -errno;
     }
 
     FdBuffer buffer;
