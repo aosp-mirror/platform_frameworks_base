d58b65cd31531445dd2dd53ae3135a9a85149d8f
Evan Rosky <erosky@google.com>
2018-04-18 15:00:45 -0700

    Don't crash during layout of non-attached but focused view
    Verifies that there is a viewrootimpl before calling its
    methods.
    
    Bug: 77193567
    Test: N/A
    Change-Id: I301c3aede3d0f7e1d2abf6fe5bb3d852f697577b

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -20700,7 +20700,7 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
             if (canTakeFocus()) {
                 // We have a robust focus, so parents should no longer be wanting focus.
                 clearParentsWantFocus();
-            } else if (!getViewRootImpl().isInLayout()) {
+            } else if (getViewRootImpl() == null || !getViewRootImpl().isInLayout()) {
                 // This is a weird case. Most-likely the user, rather than ViewRootImpl, called
                 // layout. In this case, there's no guarantee that parent layouts will be evaluated
                 // and thus the safest action is to clear focus here.
