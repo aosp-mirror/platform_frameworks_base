49ec54250f0cafd962c3c5f7f286875cf499483a
Abodunrinwa Toki <toki@google.com>
2018-04-25 14:07:17 +0100

    Fix floating toolbar menu item spacing.
    We mistakenly changed floating_toolbar_menu_button_side_padding in
    I309c729eb842d9388066bfb43eb18f33dbfe10b8 where in fact what we
    wanted to do was to just change the mIconTextSpacing.
    This cl fixes that.
    
    Change-Id: I1b940eb12d2948f8c167c3f37ed0eba105b9f5ce
    Fixes: 74032743
    Test: built, observed visual look
    Test: bit FrameworksCoreTests:android.widget.TextViewActivityTest
    Test: bit CtsWidgetTestCases:android.widget.cts.TextViewTest

--- core/java/com/android/internal/widget/FloatingToolbar.java
+++ core/java/com/android/internal/widget/FloatingToolbar.java
@@ -452,7 +452,7 @@ public final class FloatingToolbar {
             mLineHeight = context.getResources()
                     .getDimensionPixelSize(R.dimen.floating_toolbar_height);
             mIconTextSpacing = context.getResources()
-                    .getDimensionPixelSize(R.dimen.floating_toolbar_menu_button_side_padding);
+                    .getDimensionPixelSize(R.dimen.floating_toolbar_icon_text_spacing);
 
             // Interpolators
             mLogAccelerateInterpolator = new LogAccelerateInterpolator();
@@ -481,7 +481,7 @@ public final class FloatingToolbar {
             mOverflowButton = createOverflowButton();
             mOverflowButtonSize = measure(mOverflowButton);
             mMainPanel = createMainPanel();
-            mOverflowPanelViewHelper = new OverflowPanelViewHelper(mContext);
+            mOverflowPanelViewHelper = new OverflowPanelViewHelper(mContext, mIconTextSpacing);
             mOverflowPanel = createOverflowPanel();
 
             // Animation. Need views.
@@ -1573,10 +1573,9 @@ public final class FloatingToolbar {
 
             private final Context mContext;
 
-            public OverflowPanelViewHelper(Context context) {
+            public OverflowPanelViewHelper(Context context, int iconTextSpacing) {
                 mContext = Preconditions.checkNotNull(context);
-                mIconTextSpacing = context.getResources()
-                        .getDimensionPixelSize(R.dimen.floating_toolbar_menu_button_side_padding);
+                mIconTextSpacing = iconTextSpacing;
                 mSidePadding = context.getResources()
                         .getDimensionPixelSize(R.dimen.floating_toolbar_overflow_side_padding);
                 mCalculator = createMenuButton(null);
