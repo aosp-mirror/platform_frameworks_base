d25c5f7e71040d7da2c4ad32b134d35e4dca12ac
Gabriele M <moto.falcon.git@gmail.com>
2018-04-27 20:51:42 +0200

    SystemUI: QS: UsbTetherTile: Toggle only when USB is connected
    Pressing the tile when unplugged causes USB tethering to turn on
    as soon as the cable is connected. This can happen long after the
    tile has been pressed, so don't do anything if unplugged.
    
    Change-Id: Ib8bbdc758097ca7af5d4e7c3483c28ba42fb9ce1

--- packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/UsbTetherTile.java
@@ -80,7 +80,9 @@ public class UsbTetherTile extends QSTileImpl<BooleanState> {
 
     @Override
     protected void handleClick() {
-        mConnectivityManager.setUsbTethering(!mUsbTetherEnabled);
+        if (mUsbConnected) {
+            mConnectivityManager.setUsbTethering(!mUsbTetherEnabled);
+        }
     }
 
     @Override
