754e29edd76e2d927619e83c660c0d93e98211c8
Yangster-mac <yanglu@google.com>
2018-05-02 12:23:17 -0700

    Turns DEBUG to false in statsd.
    Test: statsd test
    BUG: b/79161505
    Change-Id: Ic6eee527d625b10aa86b2beb4b4c4fc05b051c7d

--- cmds/statsd/src/subscriber/IncidentdReporter.cpp
+++ cmds/statsd/src/subscriber/IncidentdReporter.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define DEBUG true
+#define DEBUG false
 #include "Log.h"
 
 #include "IncidentdReporter.h"
