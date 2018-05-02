bc90715a1ab9f4752a7f54284f18ef45f68ecb04
Felipe Leme <felipeal@google.com>
2018-05-02 14:57:23 -0700

    Minor refactorings on Autofill tests:
    - Move common code to AbstractAutofillTestCase.
    - Copied existing codes to LoginTest, which does not use @Parameters.
    - Deprecated AutofillPerfTest (it will be removed once LoginTest has some runs).
    - Added some (temporary) no-op tests to help us investigate why some tests are
      missing from the dashboard.
    
    Test: mmma -j ./frameworks/base/apct-tests/perftests/core/ &&  \
      adb install -r $OUT/data/app/CorePerfTests/CorePerfTests.apk && \
      adb shell am instrument -w -e class android.view.autofill.AutofillPerfTest \
      com.android.perftests.core/android.support.test.runner.AndroidJUnitRunner
    
    Bug: 38345816
    
    Change-Id: Iab703205f1e01a1e5413b10c3008aac8efcc5dc2

---
+++ apct-tests/perftests/core/src/android/view/autofill/AbstractAutofillPerfTestCase.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License
+ */
+
+package android.view.autofill;
+
+import android.os.Looper;
+import android.perftests.utils.PerfStatusReporter;
+import android.perftests.utils.SettingsHelper;
+import android.perftests.utils.SettingsStateKeeperRule;
+import android.perftests.utils.ShellHelper;
+import android.view.View;
+import android.perftests.utils.StubActivity;
+import android.provider.Settings;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.InstrumentationRegistry;
+
+import com.android.perftests.core.R;
+
+import java.util.Locale;
+import java.util.Collection;
+import java.util.Arrays;
+
+import org.junit.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Base class for all autofill tests.
+ */
+@LargeTest
+public abstract class AbstractAutofillPerfTestCase {
+
+    @ClassRule
+    public static final SettingsStateKeeperRule mServiceSettingsKeeper =
+            new SettingsStateKeeperRule(InstrumentationRegistry.getTargetContext(),
+                    Settings.Secure.AUTOFILL_SERVICE);
+
+    @Rule
+    public ActivityTestRule<StubActivity> mActivityRule =
+            new ActivityTestRule<StubActivity>(StubActivity.class);
+
+    @Rule
+    public PerfStatusReporter mPerfStatusReporter = new PerfStatusReporter();
+
+    private final int mLayoutId;
+
+    protected AbstractAutofillPerfTestCase(int layoutId) {
+        mLayoutId = layoutId;
+    }
+
+    /**
+     * Prepares the activity so that by the time the test is run it has reference to its fields.
+     */
+    @Before
+    public void prepareActivity() throws Throwable {
+        mActivityRule.runOnUiThread(() -> {
+            assertTrue("We should be running on the main thread",
+                    Looper.getMainLooper().getThread() == Thread.currentThread());
+            assertTrue("We should be running on the main thread",
+                    Looper.myLooper() == Looper.getMainLooper());
+            StubActivity activity = mActivityRule.getActivity();
+            activity.setContentView(mLayoutId);
+            onCreate(activity);
+        });
+    }
+
+    @Before
+    public void resetStaticState() {
+        MyAutofillService.resetStaticState();
+    }
+
+    /**
+     * Initializes the {@link StubActivity} after it was launched.
+     */
+    protected abstract void onCreate(StubActivity activity);
+
+    /**
+     * Uses the {@code settings} binary to set the autofill service.
+     */
+    protected void setService() {
+        SettingsHelper.syncSet(InstrumentationRegistry.getTargetContext(),
+                SettingsHelper.NAMESPACE_SECURE,
+                Settings.Secure.AUTOFILL_SERVICE,
+                MyAutofillService.COMPONENT_NAME);
+    }
+
+    /**
+     * Uses the {@code settings} binary to reset the autofill service.
+     */
+    protected void resetService() {
+        SettingsHelper.syncDelete(InstrumentationRegistry.getTargetContext(),
+                SettingsHelper.NAMESPACE_SECURE,
+                Settings.Secure.AUTOFILL_SERVICE);
+    }
+}
