c6b0d54ba9b5184f89cd610e7d211a110a06fc89
Rohan Shah <shahrk@google.com>
2018-05-10 13:00:54 -0700

    Fix notificationinfo test breakage
    Bind no longer logs unnecessarily - log was moved over to the manager
    already.
    
    Test: Ran test
    Fixes: 79533793
    Change-Id: I1d893935fa93ff7dbb91e922ae2444c4ac42b046

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/NotificationInfoTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/NotificationInfoTest.java
@@ -314,12 +314,11 @@ public class NotificationInfoTest extends SysuiTestCase {
 
     @Test
     public void testLogBlockingHelperCounter_logsForBlockingHelper() throws Exception {
-        // Bind notification logs an event, so this counts as one invocation for the metrics logger.
         mNotificationInfo.bindNotification(mMockPackageManager, mMockINotificationManager,
                 TEST_PACKAGE_NAME, mNotificationChannel, 1, mSbn, null, null, null, false, true,
                 true);
         mNotificationInfo.logBlockingHelperCounter("HowCanNotifsBeRealIfAppsArent");
-        verify(mMetricsLogger, times(2)).count(anyString(), anyInt());
+        verify(mMetricsLogger, times(1)).count(anyString(), anyInt());
     }
 
     @Test
