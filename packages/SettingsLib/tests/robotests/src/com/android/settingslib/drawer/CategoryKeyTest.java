b76cd9709173febca6bc0020d3a391e55534015b
Doris Ling <dling@google.com>
2018-05-11 10:53:57 -0700

    Rename settings category.
    - from night light to night display.
    
    Change-Id: Ib09e1b9299478dc4e8dcee935e29c6886d9dc7b8
    Fixes: 79530682
    Test: make RunSettingsLibRoboTests

--- packages/SettingsLib/tests/robotests/src/com/android/settingslib/drawer/CategoryKeyTest.java
+++ packages/SettingsLib/tests/robotests/src/com/android/settingslib/drawer/CategoryKeyTest.java
@@ -58,7 +58,7 @@ public class CategoryKeyTest {
         allKeys.add(CategoryKey.CATEGORY_SYSTEM_LANGUAGE);
         allKeys.add(CategoryKey.CATEGORY_SYSTEM_DEVELOPMENT);
         allKeys.add(CategoryKey.CATEGORY_GESTURES);
-        allKeys.add(CategoryKey.CATEGORY_NIGHT_LIGHT);
+        allKeys.add(CategoryKey.CATEGORY_NIGHT_DISPLAY);
         // DO NOT REMOVE ANYTHING ABOVE
 
         assertThat(allKeys.size()).isEqualTo(18);
