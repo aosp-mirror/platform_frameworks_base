c345791292c487a792b44a1538ef5164a881d900
shawnlin <shawnlin@google.com>
2018-05-15 16:39:56 +0800

    Fixed notification shelf not fully visible on lockscreen
    Remove the height limitation of mBackgroundBounds.bottom, and allow min
    height of NSSL to be shelf size in keyguard when qs panel fully
    expanded.
    
    Test: runtest systemui
    Change-Id: I74755f8eefa70beec32db2a66a7a425b7fff0276
    Fixes: 79202907

--- packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/stack/NotificationStackScrollLayout.java
@@ -727,7 +727,7 @@ public class NotificationStackScrollLayout extends ViewGroup
     }
 
     private void updateAlgorithmLayoutMinHeight() {
-        mAmbientState.setLayoutMinHeight(mQsExpanded && !onKeyguard() || isHeadsUpTransition()
+        mAmbientState.setLayoutMinHeight(mQsExpanded || isHeadsUpTransition()
                 ? getLayoutMinHeight() : 0);
     }
 
@@ -2451,7 +2451,6 @@ public class NotificationStackScrollLayout extends ViewGroup
             }
             int finalHeight = ExpandableViewState.getFinalActualHeight(lastView);
             int finalBottom = finalTranslationY + finalHeight - lastView.getClipBottomAmount();
-            finalBottom = Math.min(finalBottom, getHeight());
             if (mAnimateNextBackgroundBottom
                     || mBottomAnimator == null && mCurrentBounds.bottom == finalBottom
                     || mBottomAnimator != null && mEndAnimationRect.bottom == finalBottom) {
@@ -2460,7 +2459,6 @@ public class NotificationStackScrollLayout extends ViewGroup
             } else {
                 bottom = (int) (lastView.getTranslationY() + lastView.getActualHeight()
                         - lastView.getClipBottomAmount());
-                bottom = Math.min(bottom, getHeight());
             }
         } else {
             top = mTopPadding;
@@ -2473,7 +2471,7 @@ public class NotificationStackScrollLayout extends ViewGroup
             top = Math.max(0, top);
         }
         mBackgroundBounds.top = top;
-        mBackgroundBounds.bottom = Math.min(getHeight(), Math.max(bottom, top));
+        mBackgroundBounds.bottom = Math.max(bottom, top);
     }
 
     private ActivatableNotificationView getFirstPinnedHeadsUp() {
