c4f66f4f607654611b2227827123e016c57a5729
Adrian Roos <roosa@google.com>
2018-05-22 16:56:35 +0200

    WM: Prevent secondary display focus while keyguard is up
    Fixes an issue where input intended for the keyguard could end up going
    to a different display.
    
    To prevent this, make sure that only the default display can get focused
    when the keyguard is showing.
    
    Change-Id: I6463c44aedca06930d2c9bda7c45ffd93141308c
    Merged-In: I6463c44aedca06930d2c9bda7c45ffd93141308c
    Fixes: 71786287
    Test: atest DisplayContentTests
    (cherry picked from commit 3cd5e3d9bbb3255e874b8fa27d7ed506164905dd)

--- services/tests/servicestests/src/com/android/server/wm/DisplayContentTests.java
+++ services/tests/servicestests/src/com/android/server/wm/DisplayContentTests.java
@@ -313,6 +313,25 @@ public class DisplayContentTests extends WindowTestsBase {
         assertEquals(window1, sWm.mRoot.computeFocusedWindow());
     }
 
+    @Test
+    public void testKeyguard_preventsSecondaryDisplayFocus() throws Exception {
+        final WindowState keyguard = createWindow(null, TYPE_STATUS_BAR,
+                sWm.getDefaultDisplayContentLocked(), "keyguard");
+        assertEquals(keyguard, sWm.mRoot.computeFocusedWindow());
+
+        // Add a window to a second display, and it should be focused
+        final DisplayContent dc = createNewDisplay();
+        final WindowState win = createWindow(null, TYPE_BASE_APPLICATION, dc, "win");
+        assertEquals(win, sWm.mRoot.computeFocusedWindow());
+
+        ((TestWindowManagerPolicy)sWm.mPolicy).keyguardShowingAndNotOccluded = true;
+        try {
+            assertEquals(keyguard, sWm.mRoot.computeFocusedWindow());
+        } finally {
+            ((TestWindowManagerPolicy)sWm.mPolicy).keyguardShowingAndNotOccluded = false;
+        }
+    }
+
     /**
      * This tests setting the maximum ui width on a display.
      */
