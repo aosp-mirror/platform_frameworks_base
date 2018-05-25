2ef26755473d812bab87e0e07ba1714b053c1e3e
Joshua Baxter <jbax@google.com>
2018-05-25 11:25:50 -0700

    docs: fixed typo
    Test: make ds-docs
    
    Bug: 79973165
    Change-Id: I058e1ee49ec29890764a7deac5f8e0d40a61ba7f

--- core/java/android/view/accessibility/AccessibilityRecord.java
+++ core/java/android/view/accessibility/AccessibilityRecord.java
@@ -625,7 +625,7 @@ public class AccessibilityRecord {
     }
 
     /**
-     * Sets the text before a change.
+     * Gets the text before a change.
      *
      * @return The text before the change.
      */
