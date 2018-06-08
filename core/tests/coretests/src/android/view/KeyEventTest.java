91fa08ff1bd85ae41d71e1ed5ee72357c975ee3b
Siarhei Vishniakou <svv@google.com>
2018-06-08 22:49:30 +0100

    Move displayId into InputEvent
    Now both KeyEvent and MotionEvent will contain displayId. This will help
    with dispatching input events to specific displays. There are use cases
    where a particular input device is used for a specific display only, and
    it sends key events to the system. This will help with those usages.
    
    Test: atest view.MotionEventTest view.KeyEventTest
    Bug: 64258305
    Change-Id: I75891037617ed60820d60736216a0d615ab5e3b0

--- core/tests/coretests/src/android/view/KeyEventTest.java
+++ core/tests/coretests/src/android/view/KeyEventTest.java
@@ -16,6 +16,8 @@
 
 package android.view;
 
+import static android.view.Display.INVALID_DISPLAY;
+
 import static org.junit.Assert.assertEquals;
 
 import android.support.test.filters.SmallTest;
@@ -28,12 +30,71 @@ import org.junit.runner.RunWith;
 @RunWith(AndroidJUnit4.class)
 public class KeyEventTest {
 
+    private static final int DOWN_TIME = 50;
+    private static final long EVENT_TIME = 100;
+    private static final int ACTION = KeyEvent.ACTION_DOWN;
+    private static final int KEYCODE = KeyEvent.KEYCODE_0;
+    private static final int REPEAT = 0;
+    private static final int METASTATE = 0;
+    private static final int DEVICE_ID = 0;
+    private static final int SCAN_CODE = 0;
+    private static final int FLAGS = 0;
+    private static final int SOURCE = InputDevice.SOURCE_KEYBOARD;
+    private static final String CHARACTERS = null;
+
     @Test
     public void testObtain() {
-        KeyEvent keyEvent = KeyEvent.obtain(0, 0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_0,
-                0, 0, 0, 0, 0, InputDevice.SOURCE_KEYBOARD, null);
-        assertEquals(KeyEvent.ACTION_DOWN, keyEvent.getAction());
-        assertEquals(KeyEvent.KEYCODE_0, keyEvent.getKeyCode());
-        assertEquals(InputDevice.SOURCE_KEYBOARD, keyEvent.getSource());
+        KeyEvent keyEvent = KeyEvent.obtain(DOWN_TIME, EVENT_TIME, ACTION, KEYCODE, REPEAT,
+                METASTATE, DEVICE_ID, SCAN_CODE, FLAGS, SOURCE, CHARACTERS);
+        assertEquals(DOWN_TIME, keyEvent.getDownTime());
+        assertEquals(EVENT_TIME, keyEvent.getEventTime());
+        assertEquals(ACTION, keyEvent.getAction());
+        assertEquals(KEYCODE, keyEvent.getKeyCode());
+        assertEquals(REPEAT, keyEvent.getRepeatCount());
+        assertEquals(METASTATE, keyEvent.getMetaState());
+        assertEquals(DEVICE_ID, keyEvent.getDeviceId());
+        assertEquals(SCAN_CODE, keyEvent.getScanCode());
+        assertEquals(FLAGS, keyEvent.getFlags());
+        assertEquals(SOURCE, keyEvent.getSource());
+        assertEquals(INVALID_DISPLAY, keyEvent.getDisplayId());
+        assertEquals(CHARACTERS, keyEvent.getCharacters());
+    }
+
+    @Test
+    public void testObtainFromKeyEvent() {
+        KeyEvent keyEvent = KeyEvent.obtain(DOWN_TIME, EVENT_TIME, ACTION, KEYCODE, REPEAT,
+                METASTATE, DEVICE_ID, SCAN_CODE, FLAGS, SOURCE, CHARACTERS);
+        KeyEvent keyEvent2 = KeyEvent.obtain(keyEvent);
+        assertEquals(keyEvent.getDownTime(), keyEvent2.getDownTime());
+        assertEquals(keyEvent.getEventTime(), keyEvent2.getEventTime());
+        assertEquals(keyEvent.getAction(), keyEvent2.getAction());
+        assertEquals(keyEvent.getKeyCode(), keyEvent2.getKeyCode());
+        assertEquals(keyEvent.getRepeatCount(), keyEvent2.getRepeatCount());
+        assertEquals(keyEvent.getMetaState(), keyEvent2.getMetaState());
+        assertEquals(keyEvent.getDeviceId(), keyEvent2.getDeviceId());
+        assertEquals(keyEvent.getScanCode(), keyEvent2.getScanCode());
+        assertEquals(keyEvent.getFlags(), keyEvent2.getFlags());
+        assertEquals(keyEvent.getSource(), keyEvent2.getSource());
+        assertEquals(keyEvent.getDisplayId(), keyEvent2.getDisplayId());
+        assertEquals(keyEvent.getCharacters(), keyEvent2.getCharacters());
+    }
+
+    @Test
+    public void testObtainWithDisplayId() {
+        final int displayId = 5;
+        KeyEvent keyEvent = KeyEvent.obtain(DOWN_TIME, EVENT_TIME, ACTION, KEYCODE, REPEAT,
+                METASTATE, DEVICE_ID, SCAN_CODE, FLAGS, SOURCE, displayId, CHARACTERS);
+        assertEquals(DOWN_TIME, keyEvent.getDownTime());
+        assertEquals(EVENT_TIME, keyEvent.getEventTime());
+        assertEquals(ACTION, keyEvent.getAction());
+        assertEquals(KEYCODE, keyEvent.getKeyCode());
+        assertEquals(REPEAT, keyEvent.getRepeatCount());
+        assertEquals(METASTATE, keyEvent.getMetaState());
+        assertEquals(DEVICE_ID, keyEvent.getDeviceId());
+        assertEquals(SCAN_CODE, keyEvent.getScanCode());
+        assertEquals(FLAGS, keyEvent.getFlags());
+        assertEquals(SOURCE, keyEvent.getSource());
+        assertEquals(displayId, keyEvent.getDisplayId());
+        assertEquals(CHARACTERS, keyEvent.getCharacters());
     }
 }
