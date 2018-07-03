9c1a5d4f5b7b051144498cba5f96f9f5fb467207
Tobias Lindskog <tobias.lindskog@sony.com>
2018-07-03 11:55:25 +0200

    bgdexopt: Let PM handler run between optimizations
    BackgroundDexOptService can take mInstallLock for long periods at a
    time, since ordinary monitors aren't fair.
    
    Two app optimizations back-to-back can prevent another thread from
    getting the lock for more than 10 minutes, even if each call is capped
    at 9.5 min.  If someone running on the Package Manager handler thread
    also needs mInstallLock, they could end up waiting for too long and
    triggering the watchdog.
    
    So make the dexopt service wait for the pending messages in the handler
    queue before optimizing the next package.
    
    Test: Manual, adb shell pm bg-dexopt-job
    Change-Id: I81ae0279c51f6bb48b3d8f6f6cc8b5c77c80b86a

--- services/core/java/com/android/server/pm/BackgroundDexOptService.java
+++ services/core/java/com/android/server/pm/BackgroundDexOptService.java
@@ -46,8 +46,10 @@ import com.android.server.pm.dex.DexoptOptions;
 
 import java.io.File;
 import java.nio.file.Paths;
+import java.lang.Runnable;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
@@ -335,6 +337,14 @@ public class BackgroundDexOptService extends JobService {
         return 0;
     }
 
+    private Semaphore mHandlerFlushCount = new Semaphore(0, true);
+    private Runnable mHandlerFlusher = new Runnable() {
+        @Override
+        public void run() {
+            mHandlerFlushCount.release();
+        }
+    };
+
     private int optimizePackages(PackageManagerService pm, ArraySet<String> pkgs,
             long lowStorageThreshold, boolean isForPrimaryDex) {
         ArraySet<String> updatedPackages = new ArraySet<>();
@@ -349,6 +359,20 @@ public class BackgroundDexOptService extends JobService {
         Log.d(TAG, "Should Downgrade " + shouldDowngrade);
         boolean dex_opt_performed = false;
         for (String pkg : pkgs) {
+            // Wait for the PackageManager handler to process all queued up
+            // items so that we don't constantly interrupt it and grab the
+            // install lock
+            pm.mHandler.post(mHandlerFlusher);
+            boolean interrupted;
+            do {
+                try {
+                    mHandlerFlushCount.acquire();
+                    interrupted = false;
+                } catch (InterruptedException e) {
+                    interrupted = true;
+                }
+            } while (interrupted);
+
             int abort_code = abortIdleOptimizations(lowStorageThreshold);
             if (abort_code == OPTIMIZE_ABORT_BY_JOB_SCHEDULER) {
                 return abort_code;
