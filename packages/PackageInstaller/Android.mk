8943ad6325c890c279b8d600784680ef8d0eb3c2
Philip P. Moltmann <moltmann@google.com>
2018-07-25 12:12:30 -0700

    Split PackageInstaller app into installation and permissions management
    The two components were mostly independant for a long time. Since
    I1e80a3f5e63d02b3859ecf74af21ca4c61f96874 the installation flow does
    not grant any permissions anymore and the last connection between these
    parts was broken.
    
    The new app "com.android.packageinstaller" in
    frameworks/base/packages/PackageInstaller will only handle (side load)
    package installtion and uninstallation.
    
    The exisiting app will be renamed to "com.android.permissioncontroller"
    and only handle permission granting and permission management.
    
    This change does only minimal cleanup cleanup. In particularly it does
    not move any files in the old permissions controller. This is to not
    disturb other features currently in development.
    
    This change set also updates the make files to install the two apps on
    the appropriate devices.
    
    Further the permisson policy xmls need to be updated to point to the
    right packages.
    
    Test: Installed + uninstalled packages
          Granted permissions + managed permissions
    
          GtsPackageInstallTestCases
          GtsNoPermissionTestCases
          GtsNoPermissionTestCases25
          GtsPackageInstallerTapjackingTestCases
          GtsPackageUninstallTestCases
    Change-Id: I2d3796b837fc0049e712c82a990907f305c8febf

--- packages/PackageInstaller/Android.mk
+++ packages/PackageInstaller/Android.mk
@@ -9,42 +9,16 @@ LOCAL_SRC_FILES := \
     $(call all-java-files-under, src)
 
 LOCAL_STATIC_ANDROID_LIBRARIES += \
-    androidx.car_car \
-    androidx.design_design \
-    androidx.transition_transition \
-    androidx.core_core \
-    androidx.media_media \
-    androidx.legacy_legacy-support-core-utils \
-    androidx.legacy_legacy-support-core-ui \
-    androidx.fragment_fragment \
-    androidx.appcompat_appcompat \
-    androidx.preference_preference \
-    androidx.recyclerview_recyclerview \
-    androidx.legacy_legacy-preference-v14 \
-    androidx.leanback_leanback \
-    androidx.leanback_leanback-preference \
-    SettingsLib
+    androidx.leanback_leanback
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     xz-java \
     androidx.annotation_annotation
 
 LOCAL_PACKAGE_NAME := PackageInstaller
+
 LOCAL_CERTIFICATE := platform
-
 LOCAL_PRIVILEGED_MODULE := true
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
-
-# Comment for now unitl all private API dependencies are removed
-# LOCAL_SDK_VERSION := system_current
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
 include $(BUILD_PACKAGE)
-
-ifeq (PackageInstaller,$(LOCAL_PACKAGE_NAME))
-    # Use the following include to make our test apk.
-    ifeq (,$(ONE_SHOT_MAKEFILE))
-        include $(call all-makefiles-under,$(LOCAL_PATH))
-    endif
-endif
