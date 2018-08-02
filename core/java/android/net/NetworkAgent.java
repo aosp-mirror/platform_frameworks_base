64e99ef26d93b851ba9bbcc3983f75fbba64ccd0
Roshan Pius <rpius@google.com>
2018-08-02 10:25:02 -0700

    NetworkAgent: Send primitive integer in score
    |Integer| is not parcelable and hences crashes when the binder call
    crosses a process boundary. Using a primitive integer should suffice
    for sending the value to the server. There are no other usage of
    |EVENT_NETWORK_SCORE_CHANGED| in the codebase.
    
    Bug: 112358948
    Test: Device boots up and able to make wifi connection.
    Change-Id: I4c8fc17f4b803f69a4d882c71a8ce014a194d1ba

--- core/java/android/net/NetworkAgent.java
+++ core/java/android/net/NetworkAgent.java
@@ -372,7 +372,7 @@ public abstract class NetworkAgent extends Handler {
         if (score < 0) {
             throw new IllegalArgumentException("Score must be >= 0");
         }
-        queueOrSendMessage(EVENT_NETWORK_SCORE_CHANGED, new Integer(score));
+        queueOrSendMessage(EVENT_NETWORK_SCORE_CHANGED,  score, 0);
     }
 
     /**
