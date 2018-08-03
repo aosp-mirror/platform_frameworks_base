c50a9bd24d2a243f77e09cc331bf9b10446d436a
Salvador Martinez <dehboxturtle@google.com>
2018-08-03 14:07:44 -0700

    Update severe (5%/1 hour) warning to send through battery saver
    Now that battery saver is sticky this notification never shows
    and we are getting reports from people that the notification
    they were expecting never showed up. It turns out that a lot
    of these were due to people not realizing battery saver was on.
    To remedy the worst case situation (your phone is about to die
    soon but no 15%/3 hour notification came up) we should always
    show the severe warning even if battery saver is on. This only
    affects the hybrid warning, not the legacy percentage based
    version.
    
    Test: unit tests
    Bug: 111596093
    Change-Id: I92453b68b6ee8aad8f862147c877f4e789afc55c
    Merged-In: I92453b68b6ee8aad8f862147c877f4e789afc55c

--- packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
+++ packages/SystemUI/src/com/android/systemui/power/PowerNotificationWarnings.java
@@ -266,9 +266,12 @@ public class PowerNotificationWarnings implements PowerUI.WarningsUI {
                 || mEstimate.estimateMillis < mSevereWarningThreshold) {
             nb.setColor(Utils.getColorAttr(mContext, android.R.attr.colorError));
         }
-        nb.addAction(0,
-                mContext.getString(R.string.battery_saver_start_action),
-                pendingBroadcast(ACTION_START_SAVER));
+
+        if (!mPowerMan.isPowerSaveMode()) {
+            nb.addAction(0,
+                    mContext.getString(R.string.battery_saver_start_action),
+                    pendingBroadcast(ACTION_START_SAVER));
+        }
         nb.setOnlyAlertOnce(!mPlaySound);
         mPlaySound = false;
         SystemUI.overrideNotificationAppName(mContext, nb, false);
