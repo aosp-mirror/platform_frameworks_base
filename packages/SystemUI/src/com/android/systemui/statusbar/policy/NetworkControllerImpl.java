fdcf6469f1cfca1aaa17e548cf960f2053e6f2e7
Malcolm Chen <refuhoo@google.com>
2018-08-09 17:01:35 -0700

    Remove old SubscriptionInfo constructors.
    Bug: 92796390
    Test: build
    Change-Id: Id5ce174b43934e690f6c1682e502b3b7e344a865
    Merged-In: Id5ce174b43934e690f6c1682e502b3b7e344a865

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkControllerImpl.java
@@ -930,7 +930,7 @@ public class NetworkControllerImpl extends BroadcastReceiver
 
     private SubscriptionInfo addSignalController(int id, int simSlotIndex) {
         SubscriptionInfo info = new SubscriptionInfo(id, "", simSlotIndex, "", "", 0, 0, "", 0,
-                null, null, null, "");
+                null, null, null, "", false, null, null);
         MobileSignalController controller = new MobileSignalController(mContext,
                 mConfig, mHasMobileDataFeature, mPhone, mCallbackHandler, this, info,
                 mSubDefaults, mReceiverHandler.getLooper());
