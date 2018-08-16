8f4ec0e9418869145cb41f084ed0d6f8e222f5bd
xyyx <xyyx@mail.ru>
2018-08-15 17:29:02 -0700

    SystemUI: Remove data tile dialog
    Change-Id: Ib5e80e49795ce4413519e854d3f10195eab52e16

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CellularTile.java
@@ -115,11 +115,7 @@ public class CellularTile extends QSTileImpl<SignalState> {
             });
             return;
         }
-        if (mDataController.isMobileDataEnabled()) {
-            maybeShowDisableDialog();
-        } else {
-            mDataController.setMobileDataEnabled(true);
-        }
+        mDataController.setMobileDataEnabled(!mDataController.isMobileDataEnabled());
     }
 
     private void maybeShowDisableDialog() {
