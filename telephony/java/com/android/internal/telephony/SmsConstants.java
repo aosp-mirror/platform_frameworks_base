d0741c56fb73de285d98ac8c580b80ca081f2c32
Mathew Inwood <mathewi@google.com>
2018-08-20 13:06:00 +0100

    Code reformatting for upcoming automated code changes.
    Adding annotations to individual members cannot be easily done when there
    are several enum values defined on a single line. Put each on a line of its
    own.
    
    See go/UnsupportedAppUsage for more context.
    
    Bug: 110868826
    Test: m
    Change-Id: I807a416e234fa92001920b484bbf3eedbf305c41

--- telephony/java/com/android/internal/telephony/SmsConstants.java
+++ telephony/java/com/android/internal/telephony/SmsConstants.java
@@ -58,7 +58,11 @@ public class SmsConstants {
      * See TS 23.038.
      */
     public enum MessageClass{
-        UNKNOWN, CLASS_0, CLASS_1, CLASS_2, CLASS_3;
+        UNKNOWN,
+        CLASS_0,
+        CLASS_1,
+        CLASS_2,
+        CLASS_3;
     }
 
     /**
