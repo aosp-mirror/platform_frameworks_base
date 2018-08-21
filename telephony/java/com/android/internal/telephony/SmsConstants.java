b4936a336b3d8fb45f3d28c9001292009894d59c
Mathew Inwood <mathewi@google.com>
2018-08-21 16:40:35 +0100

    Add @UnsupportedAppUsage annotations
    For packages:
      com.android.internal.telephony
    
    This is an automatically generated CL. See go/UnsupportedAppUsage
    for more details.
    
    Exempted-From-Owner-Approval: Mechanical changes to the codebase
    which have been approved by Android API council and announced on
    android-eng@
    
    Bug: 110868826
    Test: m
    Change-Id: Ib9bb9c8ba23a3305c8b72600fa5fd3109b4f6ee1
    Merged-In: Ia06d9a50c456201dc7ecb960ff1a1563617777a5

--- telephony/java/com/android/internal/telephony/SmsConstants.java
+++ telephony/java/com/android/internal/telephony/SmsConstants.java
@@ -15,6 +15,8 @@
  */
 package com.android.internal.telephony;
 
+import android.annotation.UnsupportedAppUsage;
+
 /**
  * SMS Constants and must be the same as the corresponding
  * deprecated version in SmsMessage.
@@ -58,10 +60,15 @@ public class SmsConstants {
      * See TS 23.038.
      */
     public enum MessageClass{
+        @UnsupportedAppUsage
         UNKNOWN,
+        @UnsupportedAppUsage
         CLASS_0,
+        @UnsupportedAppUsage
         CLASS_1,
+        @UnsupportedAppUsage
         CLASS_2,
+        @UnsupportedAppUsage
         CLASS_3;
     }
 
