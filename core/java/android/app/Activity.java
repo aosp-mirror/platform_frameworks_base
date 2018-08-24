0de0eca77287db74884a21e0c972394de411a3dd
Sergey Vasilinets <sergeyv@google.com>
2018-08-23 19:28:57 -0700

    Deprecate Activity.onStateNotSaved
    This callback is no longer accurate, because it is basically
    onPreResume(), but onSaveInstance isn't related to Pause/Resume
    at all. In addition, this callback was added for framework fragments
    and they are deprecated
    
    bug: 113134425
    Test: no logic changes
    Change-Id: I8f961846b52032d9760d5050a2d8ee5ec57c49cd

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -1349,7 +1349,12 @@ public class Activity extends ContextThemeWrapper
      * to give the activity a hint that its state is no longer saved -- it will generally
      * be called after {@link #onSaveInstanceState} and prior to the activity being
      * resumed/started again.
+     *
+     * @deprecated starting with {@link android.os.Build.VERSION_CODES#P} onSaveInstanceState is
+     * called after {@link #onStop}, so this hint isn't accurate anymore: you should consider your
+     * state not saved in between {@code onStart} and {@code onStop} callbacks inclusively.
      */
+    @Deprecated
     public void onStateNotSaved() {
     }
 
