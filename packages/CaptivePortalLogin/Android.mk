eef9daea9590a27a41aa4514f9c470c6f0ae5d4b
Erik Kline <ek@google.com>
2018-08-29 16:22:46 +0900

    Remove services.net JAR dependency
    Test: as follows
        - built, flashed, booted
        - runtest frameworks-net passes
        - captive portal interaction works as expected in Private DNS
          strict mode
    Bug: 64133961
    Bug: 72345192
    Bug: 73872000
    Bug: 78548486
    Change-Id: Ia2ab2fb15e7898d99d30da20699e0bbd7a5a0964

--- packages/CaptivePortalLogin/Android.mk
+++ packages/CaptivePortalLogin/Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 LOCAL_STATIC_ANDROID_LIBRARIES := androidx.legacy_legacy-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES := services.net
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
