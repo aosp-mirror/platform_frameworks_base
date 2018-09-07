c2da1a22448b6a506af1d5fcd0ac8fac7f089e69
jackqdyulei <jackqdyulei@google.com>
2018-09-07 13:37:32 -0700

    Add feature flag for seamless transfer
    Change-Id: Ic9edff6ee60125794c1db5a8e271919d92cbe570
    Fixes: 114451741
    Test: Build

--- core/java/android/util/FeatureFlagUtils.java
+++ core/java/android/util/FeatureFlagUtils.java
@@ -46,6 +46,7 @@ public class FeatureFlagUtils {
         DEFAULT_FLAGS.put("settings_dynamic_homepage", "false");
         DEFAULT_FLAGS.put("settings_mobile_network_v2", "false");
         DEFAULT_FLAGS.put("settings_data_usage_v2", "false");
+        DEFAULT_FLAGS.put("settings_seamless_transfer", "false");
         DEFAULT_FLAGS.put(HEARING_AID_SETTINGS, "true");
     }
 
