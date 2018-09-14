7605d0fa4e0db375cbf9f43ed16aa894521bb2b1
maxwen <max.weninger@gmail.com>
2018-09-14 17:35:55 -0500

    ShorcutService: Use Magma fingerprint
    * Make sure cached data gets updated
    * No more derped deep shortcuts text and images
    
    Change-Id: I65d743a4991f92739e59b4655dcc086bd36a5401

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -4302,7 +4302,7 @@ public class ShortcutService extends IShortcutService.Stub {
 
     // Injection point.
     String injectBuildFingerprint() {
-        return Build.FINGERPRINT;
+        return Build.MAGMA_FINGERPRINT;
     }
 
     final void wtf(String message) {
