e46a2162f2ec9c131546c32fcf0a1d476a2e6913
Kevin Chyn <kchyn@google.com>
2018-09-20 18:43:01 -0700

    Add timeout parameter to generateChallenge
    Bug: 115925222
    
    Test: builds
    Change-Id: Ie81af1ac0b6445fbf5a5344c5f54135256ee6e3d

--- services/core/java/com/android/server/biometrics/face/FaceService.java
+++ services/core/java/com/android/server/biometrics/face/FaceService.java
@@ -82,6 +82,7 @@ public class FaceService extends BiometricService {
             "com.android.server.biometrics.face.ACTION_LOCKOUT_RESET";
     private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_TIMED = 3;
     private static final int MAX_FAILED_ATTEMPTS_LOCKOUT_PERMANENT = 12;
+    private static final int CHALLENGE_TIMEOUT_SEC = 600; // 10 minutes
 
     private final class FaceAuthClient extends AuthenticationClientImpl {
         public FaceAuthClient(Context context,
@@ -783,7 +784,7 @@ public class FaceService extends BiometricService {
             return 0;
         }
         try {
-            return daemon.generateChallenge().value;
+            return daemon.generateChallenge(CHALLENGE_TIMEOUT_SEC).value;
         } catch (RemoteException e) {
             Slog.e(TAG, "startPreEnroll failed", e);
         }
