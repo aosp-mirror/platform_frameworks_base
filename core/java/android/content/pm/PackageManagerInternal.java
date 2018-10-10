cf134ebfb716199aa20e19db2e9fdf1d7eb263d5
Ricky Wai <rickywai@google.com>
2018-10-10 09:26:32 +0100

    Return app hidden details activity in launcher api
    If a normal app does not have launcher icon, launcher api
    will return app details activity instead, so user will
    be noticed that the app is still installed.
    
    Bug: 111348460
    
    Test: Installed an app without launcher activity, an app icon is being
    shown in launcher allapps, and it forwards user to app details page.
    
    Change-Id: I9c17f5edfdefe19727145e7176d7e113286c997d

--- core/java/android/content/pm/PackageManagerInternal.java
+++ core/java/android/content/pm/PackageManagerInternal.java
@@ -19,6 +19,7 @@ package android.content.pm;
 import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.annotation.UserIdInt;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager.ApplicationInfoFlags;
@@ -203,6 +204,29 @@ public abstract class PackageManagerInternal {
      */
     public abstract PackageInfo getPackageInfo(String packageName,
             @PackageInfoFlags int flags, int filterCallingUid, int userId);
+
+    /**
+     * Return a List of all application packages that are installed on the
+     * device, for a specific user. If flag GET_UNINSTALLED_PACKAGES has been
+     * set, a list of all applications including those deleted with
+     * {@code DONT_DELETE_DATA} (partially installed apps with data directory)
+     * will be returned.
+     *
+     * @param flags Additional option flags to modify the data returned.
+     * @param userId The user for whom the installed applications are to be
+     *            listed
+     * @param callingUid The uid of the original caller app
+     * @return A List of ApplicationInfo objects, one for each installed
+     *         application. In the unlikely case there are no installed
+     *         packages, an empty list is returned. If flag
+     *         {@code MATCH_UNINSTALLED_PACKAGES} is set, the application
+     *         information is retrieved from the list of uninstalled
+     *         applications (which includes installed applications as well as
+     *         applications with data directory i.e. applications which had been
+     *         deleted with {@code DONT_DELETE_DATA} flag set).
+     */
+    public abstract List<ApplicationInfo> getInstalledApplications(
+            @ApplicationInfoFlags int flags, @UserIdInt int userId, int callingUid);
 
     /**
      * Retrieve launcher extras for a suspended package provided to the system in
