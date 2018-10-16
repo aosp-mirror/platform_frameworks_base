5bb4fea967b404399f4916a5f61d34f9329519eb
Chenbo Feng <fengc@google.com>
2018-10-15 17:31:43 -0700

    Move bpf project to system/bpf
    The bpf project now have a new project directory in system/bpf instead
    of inside netd. The network related bpf library is renamed to
    libnetdbpf. Fix the dependency issue in framework to make sure no
    regression.
    
    Test: -m CtsNetTestCases -t android.net.cts.TrafficStatsTest
    Bug: 112334572
    Change-Id: Ibd477bf17d18d516aa520fb1569f4a395ef9abf0

--- services/core/jni/com_android_server_net_NetworkStatsService.cpp
+++ services/core/jni/com_android_server_net_NetworkStatsService.cpp
@@ -30,8 +30,8 @@
 #include <utils/Log.h>
 
 #include "android-base/unique_fd.h"
-#include "bpf/BpfNetworkStats.h"
 #include "bpf/BpfUtils.h"
+#include "netdbpf/BpfNetworkStats.h"
 
 using android::bpf::Stats;
 using android::bpf::hasBpfSupport;
