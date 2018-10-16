cea7deea4c7315e4a11f8d68f6a5bf11e2bb21a7
Lucas Dupin <dupin@google.com>
2018-10-16 12:43:37 -0700

    Restore KeyguardBottomAreaView state
    KeyguardBottomAreaView will be re-inflated when the theme changes,
    it's necessary to also restore some of its internal state, otherwise
    we'll have NPEs.
    
    Test: manual
    Test: atest KeyguardBottomAreaTest
    Fixes: 117121386
    Change-Id: Idac8192877b65f8ad00cd60a2bb0c1ce75476cf1

---
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaTest.kt
@@ -0,0 +1,47 @@
+package com.android.systemui.statusbar.phone
+
+import android.support.test.filters.SmallTest
+import android.testing.AndroidTestingRunner
+import android.testing.TestableLooper
+import android.view.LayoutInflater
+
+import com.android.systemui.R
+import com.android.systemui.SysuiTestCase
+import com.android.systemui.statusbar.KeyguardIndicationController
+
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+
+@SmallTest
+@RunWith(AndroidTestingRunner::class)
+@TestableLooper.RunWithLooper
+class KeyguardBottomAreaTest : SysuiTestCase() {
+
+    @Mock
+    private lateinit var mStatusBar: StatusBar
+    @Mock
+    private lateinit var mKeyguardIndicationController: KeyguardIndicationController
+    private lateinit var mKeyguardBottomArea: KeyguardBottomAreaView
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        mKeyguardBottomArea = LayoutInflater.from(mContext).inflate(
+                R.layout.keyguard_bottom_area, null, false) as KeyguardBottomAreaView
+        mKeyguardBottomArea.setStatusBar(mStatusBar)
+        mKeyguardBottomArea.setKeyguardIndicationController(mKeyguardIndicationController)
+    }
+
+    @Test
+    fun initFrom_doesntCrash() {
+        val other = LayoutInflater.from(mContext).inflate(
+                R.layout.keyguard_bottom_area, null, false) as KeyguardBottomAreaView
+
+        other.initFrom(mKeyguardBottomArea)
+        other.launchVoiceAssist()
+        other.onLongClick(null)
+    }
+}
