6e8c60ccc1bbfb4da31ef4d1055d938cb2d430af
Courtney Goeltzenleuchter <courtneygo@google.com>
2018-10-19 11:19:53 -0600

    Add EGL15: eglCreateImage, eglDestroyImage
    Also change eglCreatePlatformPixmapSurface to throw an
    unsupported extension rather than call the native function
    which will return an error indicating it's unsupported.
    
    Bug: 80297325
    Test: atest CtsGraphicsTestCases:EGL15Test
    Change-Id: I94f2d39678515fdacf9b2fccd1c531365930ad1b

--- opengl/java/android/opengl/EGL15.java
+++ opengl/java/android/opengl/EGL15.java
@@ -146,4 +146,22 @@ public class EGL15 {
         int flags
     );
 
+    // C function EGLImage eglCreateImage ( EGLDisplay dpy, EGLContext context, EGLenum target, EGLClientBuffer buffer, const EGLAttrib *attrib_list )
+
+    public static native EGLImage eglCreateImage(
+        EGLDisplay dpy,
+        EGLContext context,
+        int target,
+        long buffer,
+        long[] attrib_list,
+        int offset
+    );
+
+    // C function EGLBoolean eglDestroyImage ( EGLDisplay dpy, EGLImage image )
+
+    public static native boolean eglDestroyImage(
+        EGLDisplay dpy,
+        EGLImage image
+    );
+
 }
