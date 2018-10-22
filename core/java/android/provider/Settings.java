d4b607ce76c734357d97725cad97c8c3b1439bca
xshu <xshu@google.com>
2018-10-22 15:22:25 -0700

    Pno frequency culling: Settings.Global flag
    Add a flag to enable/disable this feature.
    
    Bug: 64312268
    Test: compile
    Test: First use "adb shell settings put global wifi_pno_frequency_culling_enabled 1" to test turning on the feature.
    Test: Then test "adb shell settings put global
    wifi_pno_frequency_culling_enabled garbage" does not turn the feature
    off.
    Change-Id: Ia6cd0d3c9888d7b2e0513b51fc4ad92c3eb171de

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10276,6 +10276,18 @@ public final class Settings {
         public static final String WIFI_LINK_SPEED_METRICS_ENABLED =
                 "wifi_link_speed_metrics_enabled";
 
+        /**
+         * Setting to enable the PNO frequency culling optimization.
+         * Disabled by default, and setting it to 1 will enable it.
+         * The value is boolean (0 or 1).
+         * @hide
+         */
+        public static final String WIFI_PNO_FREQUENCY_CULLING_ENABLED =
+                "wifi_pno_frequency_culling_enabled";
+
+        private static final Validator WIFI_PNO_FREQUENCY_CULLING_ENABLED_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
        /**
         * The maximum number of times we will retry a connection to an access
         * point for which we have failed in acquiring an IP address from DHCP.
@@ -12676,6 +12688,8 @@ public final class Settings {
             VALIDATORS.put(DEVICE_PROVISIONING_MOBILE_DATA_ENABLED, BOOLEAN_VALIDATOR);
             VALIDATORS.put(REQUIRE_PASSWORD_TO_DECRYPT, BOOLEAN_VALIDATOR);
             VALIDATORS.put(DEVICE_DEMO_MODE, BOOLEAN_VALIDATOR);
+            VALIDATORS.put(WIFI_PNO_FREQUENCY_CULLING_ENABLED,
+                    WIFI_PNO_FREQUENCY_CULLING_ENABLED_VALIDATOR);
         }
 
         /**
