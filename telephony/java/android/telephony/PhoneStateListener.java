fd59a813293ecd604a925dddffb2b391385f4c91
Jordan Liu <jminjie@google.com>
2018-10-23 14:17:39 -0700

    Add CallAttributes and PhoneStateListener method
    CallAttributes changes will not be reported until we have a full modem
    and framework implementation. Registering for one right now is a no-op.
    
    Test: builds
    Bug: 110107501
    Change-Id: I9c246438de407f5553fd90652e481cdc39b8c844

--- telephony/java/android/telephony/PhoneStateListener.java
+++ telephony/java/android/telephony/PhoneStateListener.java
@@ -332,6 +332,18 @@ public class PhoneStateListener {
     @SystemApi
     public static final int LISTEN_CALL_DISCONNECT_CAUSES                  = 0x02000000;
 
+    /**
+     * Listen for changes to the call attributes of a currently active call.
+     * {@more}
+     * Requires Permission: {@link android.Manifest.permission#READ_PRECISE_PHONE_STATE
+     * READ_PRECISE_PHONE_STATE}
+     *
+     * @see #onCallAttributesChanged
+     * @hide
+     */
+    @SystemApi
+    public static final int LISTEN_CALL_ATTRIBUTES_CHANGED                 = 0x04000000;
+
     /*
      * Subscription used to listen to the phone state changes
      * @hide
@@ -679,6 +691,17 @@ public class PhoneStateListener {
     }
 
     /**
+     * Callback invoked when the call attributes changes. Requires
+     * the READ_PRIVILEGED_PHONE_STATE permission.
+     * @param callAttributes the call attributes
+     * @hide
+     */
+    @SystemApi
+    public void onCallAttributesChanged(CallAttributes callAttributes) {
+        // default implementation empty
+    }
+
+    /**
      * Callback invoked when modem radio power state changes. Requires
      * the READ_PRIVILEGED_PHONE_STATE permission.
      * @param state the modem radio power state
@@ -935,6 +958,14 @@ public class PhoneStateListener {
 
             Binder.withCleanCallingIdentity(
                     () -> mExecutor.execute(() -> psl.onRadioPowerStateChanged(state)));
+        }
+
+        public void onCallAttributesChanged(CallAttributes callAttributes) {
+            PhoneStateListener psl = mPhoneStateListenerWeakRef.get();
+            if (psl == null) return;
+
+            Binder.withCleanCallingIdentity(
+                    () -> mExecutor.execute(() -> psl.onCallAttributesChanged(callAttributes)));
         }
 
         public void onPreferredDataSubIdChanged(int subId) {
