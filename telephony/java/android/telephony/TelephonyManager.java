67bbcea7cf453e6facee0f1ee068015ed35ed085
Wileen Chiu <wileenc@quicinc.com>
2018-10-24 12:39:56 +0800

    Fix property setting with failure
    1, Avoid containing split char ',' in original value.
    2, Check value utf-8 length to avoid exceeding the maximum length in
    native layer.
    
    Bug: 118357532
    Change-Id: Ic2f17c474f5fcf6a513641c0f8f7edd0794decb0

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -5545,7 +5545,7 @@ public class TelephonyManager {
         if (value == null) {
             value = "";
         }
-
+        value.replace(',', ' ');
         if (prop != null) {
             p = prop.split(",");
         }
@@ -5571,7 +5571,13 @@ public class TelephonyManager {
             }
         }
 
-        if (propVal.length() > SystemProperties.PROP_VALUE_MAX) {
+        int propValLen = propVal.length();
+        try {
+            propValLen = propVal.getBytes("utf-8").length;
+        } catch (java.io.UnsupportedEncodingException e) {
+            Rlog.d(TAG, "setTelephonyProperty: utf-8 not supported");
+        }
+        if (propValLen > SystemProperties.PROP_VALUE_MAX) {
             Rlog.d(TAG, "setTelephonyProperty: property too long phoneId=" + phoneId +
                     " property=" + property + " value: " + value + " propVal=" + propVal);
             return;
