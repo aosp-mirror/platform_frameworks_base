d7a04c8965ae3a79a369d78a0320027891607032
Fabian Kozynski <kozynski@google.com>
2018-10-29 12:27:42 -0400

    Fixes context for tiles without longClick
    QSTile can declare whether it doesn't support (has no effect) long click
    and then TalkBack will not report it as an available option.
    
    Test: manual & atest
    Change-Id: Iec21181f4308c7c8c4962f191fa0787e053ae944
    Merged-In: Iec21181f4308c7c8c4962f191fa0787e053ae944
    Fixes: 117921787

--- packages/SystemUI/plugin/src/com/android/systemui/plugins/qs/QSTile.java
+++ packages/SystemUI/plugin/src/com/android/systemui/plugins/qs/QSTile.java
@@ -116,6 +116,7 @@ public interface QSTile {
         public boolean isTransient = false;
         public String expandedAccessibilityClassName;
         public SlashState slash;
+        public boolean handlesLongClick = true;
 
         public boolean copyTo(State other) {
             if (other == null) throw new IllegalArgumentException();
@@ -133,7 +134,8 @@ public interface QSTile {
                     || !Objects.equals(other.state, state)
                     || !Objects.equals(other.isTransient, isTransient)
                     || !Objects.equals(other.dualTarget, dualTarget)
-                    || !Objects.equals(other.slash, slash);
+                    || !Objects.equals(other.slash, slash)
+                    || !Objects.equals(other.handlesLongClick, handlesLongClick);
             other.icon = icon;
             other.iconSupplier = iconSupplier;
             other.label = label;
@@ -146,6 +148,7 @@ public interface QSTile {
             other.dualTarget = dualTarget;
             other.isTransient = isTransient;
             other.slash = slash != null ? slash.copy() : null;
+            other.handlesLongClick = handlesLongClick;
             return changed;
         }
 
