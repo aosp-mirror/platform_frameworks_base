de93388430b25225033dbe7dc8f5d02a8c1307e5
Seungho Lee <shiny.lee@samsung.com>
2018-10-31 21:49:09 +0900

    Check if notification is valid before it finally vibrates because it can be canceled as soon as enqeued
    Test: Build and run StatusBarTest/_NotifyBuilder
    Test: Set notification sound and vibration
    Test: Make notification with + button, and cancel it with - button quickly
    TEst: Check notification not vibrating
    
    Change-Id: Ia21f45c165f863ed4143da3707b492c89e58387c
    Signed-off-by: Seungho Lee <shiny.lee@samsung.com>

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -5457,8 +5457,17 @@ public class NotificationManagerService extends SystemService {
                     try {
                         Thread.sleep(waitMs);
                     } catch (InterruptedException e) { }
-                    mVibrator.vibrate(record.sbn.getUid(), record.sbn.getPackageName(),
-                            effect, "Notification (delayed)", record.getAudioAttributes());
+
+                    // Notifications might be canceled before it actually vibrates due to waitMs,
+                    // so need to check the notification still valide for vibrate.
+                    synchronized (mNotificationLock) {
+                        if (mNotificationsByKey.get(record.getKey()) != null) {
+                            mVibrator.vibrate(record.sbn.getUid(), record.sbn.getOpPkg(),
+                                    effect, "Notification (delayed)", record.getAudioAttributes());
+                        } else {
+                            Slog.e(TAG, "No vibration for canceled notification : " + record.getKey());
+                        }
+                    }
                 }).start();
             } else {
                 mVibrator.vibrate(record.sbn.getUid(), record.sbn.getPackageName(),
