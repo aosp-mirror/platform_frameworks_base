c1246e6c364bbfef18d566f6b16f46ca1a5216c5
Inseob Kim <inseob@google.com>
2018-11-08 13:13:54 +0900

    Schematize vold system properties
    Properties accessed across partitions are now schematized and will
    become APIs to make explicit interfaces among partitions.
    
    Bug: 117924132
    Test: m -j
    Change-Id: I8e04f643197b6c8a60cc38c6979e41c5de3469f5
    (cherry picked from commit 0cf5fc1433ef113d33d40b62cfca9ce97de12a4b)

--- core/java/android/os/storage/StorageManager.java
+++ core/java/android/os/storage/StorageManager.java
@@ -55,6 +55,7 @@ import android.os.ServiceManager;
 import android.os.ServiceManager.ServiceNotFoundException;
 import android.os.SystemProperties;
 import android.provider.Settings;
+import android.sysprop.VoldProperties;
 import android.system.ErrnoException;
 import android.system.Os;
 import android.system.OsConstants;
@@ -1491,7 +1492,7 @@ public class StorageManager {
      * framework, so no service needs to check for changes during their lifespan
      */
     public static boolean isBlockEncrypting() {
-        final String state = SystemProperties.get("vold.encrypt_progress", "");
+        final String state = VoldProperties.encrypt_progress().orElse("");
         return !"".equalsIgnoreCase(state);
     }
 
@@ -1507,7 +1508,7 @@ public class StorageManager {
      * framework, so no service needs to check for changes during their lifespan
      */
     public static boolean inCryptKeeperBounce() {
-        final String status = SystemProperties.get("vold.decrypt");
+        final String status = VoldProperties.decrypt().orElse("");
         return "trigger_restart_min_framework".equals(status);
     }
 
