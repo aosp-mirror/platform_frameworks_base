270145406505ea8809769827eb4a45f442c43f20
xyyx <xyyx@mail.ru>
2018-11-24 14:22:25 +0800

    NetworkTraffic: Fix gravity
    TextView.setText() has left gravity by default. It's ugly in statusbar
    
    Change-Id: I7fac570597e925c47df45b788823bd959c972234
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -26,6 +26,7 @@ import android.os.SystemClock;
 import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -121,6 +122,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
                    }
                 // Update view if there's anything new to show
                 if (!output.contentEquals(getText())) {
+                    setGravity(Gravity.RIGHT | Gravity.CENTER_VERTICAL);
                     setText(output);
                 }
                 mTrafficVisible = true;
@@ -361,6 +363,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
         final Resources resources = getResources();
         txtImgPadding = resources.getDimensionPixelSize(R.dimen.net_traffic_txt_img_padding);
         setCompoundDrawablePadding(txtImgPadding);
+        setGravity(Gravity.RIGHT);
         updateTextSize();
     }
 
