5e4840ae72a47b7257318f0bd4e170edbab86137
chen xu <fionaxu@google.com>
2018-11-30 00:35:31 -0800

    update comments for resetRadioConfig and rebootRadio
    This operation is carrier/technology agnostic. Its currently used by
    some cdma carriers e.g, OMADM but it doesn't mean its an no-op for GSM
    carriers
    
    Bug: 119118770
    Test: Build
    Change-Id: Iff31fc0afbb060a590b24bd19dc91cad3889e4fb

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -5346,7 +5346,7 @@ public class TelephonyManager {
 
     /**
      * Rollback modem configurations to factory default except some config which are in whitelist.
-     * Used for device configuration by some CDMA operators.
+     * Used for device configuration by some carriers.
      *
      * <p>Requires Permission:
      * {@link android.Manifest.permission#MODIFY_PHONE_STATE MODIFY_PHONE_STATE} or that the calling
@@ -5373,7 +5373,7 @@ public class TelephonyManager {
     }
 
     /**
-     * Generate a radio modem reset. Used for device configuration by some CDMA operators.
+     * Generate a radio modem reset. Used for device configuration by some carriers.
      *
      * <p>Requires Permission:
      * {@link android.Manifest.permission#MODIFY_PHONE_STATE MODIFY_PHONE_STATE} or that the calling
