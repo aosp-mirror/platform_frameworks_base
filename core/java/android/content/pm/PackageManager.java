dad9b86571eb6bce801209d63b8f18611af5a2bd
Varun Shah <varunshah@google.com>
2018-12-10 20:17:31 -0800

    Moved getWellbeingPackage() from SystemApi to TestApi.
    Changed PackageManager#getWellbeingPackage from System to Test API since
    the related GTS using the API is now being built against the test sdk.
    
    Bug: 119330345
    Test: atest com.google.android.wellbeing.permissionpolicy.gts.WellbeingPermissionPolicyTest
    Change-Id: Id80f52a327f3faa292e7c647cac2b1fa0e03275f

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -6438,7 +6438,7 @@ public abstract class PackageManager {
      *
      * @hide
      */
-    @SystemApi
+    @TestApi
     public String getWellbeingPackageName() {
         throw new UnsupportedOperationException(
                 "getWellbeingPackageName not implemented in subclass");
