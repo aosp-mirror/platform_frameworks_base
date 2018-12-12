d11850cd44dcf1114f53bd0e12569a584d557039
Nandana Dutt <nandana@google.com>
2018-12-12 17:26:57 +0000

    Add a privileged API for capturing and consuming bugreports
    The API is mostly implemented; except for hooking up the listener
    and handling an already running bugreport.
    
    BugreportManager is the handle to the new API exposed to apps.
    
    Generating bugreports requires root privileges. To limit the footprint
    of the root access, the actual bugreport generation in Dumpstate binary,
    is accessed as a oneshot service and dies after it finishes
    running.
    
    System server accesses Dumpstate via a binder interface since it does
    not have root privileges.
    
    Starting a oneshot service is done via setting a system property, which
    needs to be done from system server. BugreportManagerService is the
    new system server service that does this. BugreportManager calls into
    BugreportManagerService via a binder interface, since the former is in
    the app's process.
    
    Both app to system server as well as system server to native service
    calls are via implementations of IDumpstate binder interface.
    
    Bug: 111441001
    Test: builds. Flashed & verified it boots.
    Test: wrote a test client (not included) and verified invoking
    startBugreport works.
    
    Change-Id: I4abeb753388c055c36ae0dd916af1ec8d40b7bf0

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -106,6 +106,7 @@ import com.android.server.net.watchlist.NetworkWatchlistService;
 import com.android.server.notification.NotificationManagerService;
 import com.android.server.oemlock.OemLockService;
 import com.android.server.om.OverlayManagerService;
+import com.android.server.os.BugreportManagerService;
 import com.android.server.os.DeviceIdentifiersPolicyService;
 import com.android.server.os.SchedulingPolicyService;
 import com.android.server.pm.BackgroundDexOptService;
@@ -785,6 +786,11 @@ public final class SystemServer {
         // Manages apk rollbacks.
         traceBeginAndSlog("StartRollbackManagerService");
         mSystemServiceManager.startService(RollbackManagerService.class);
+        traceEnd();
+
+        // Service to capture bugreports.
+        traceBeginAndSlog("StartBugreportManagerService");
+        mSystemServiceManager.startService(BugreportManagerService.class);
         traceEnd();
     }
 
