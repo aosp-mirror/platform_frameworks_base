a9f8e1f7322d8b2c957984bd75e4487aaab0c3f6
Kweku Adams <kwekua@google.com>
2018-12-12 16:06:54 -0800

    Clearing QuotaController data on app/user removal.
    Bug: 117846754
    Bug: 111423978
    Test: atest com.android.server.job.controllers.QuotaControllerTest
    Change-Id: I8e971bf44b1b3351e28a5c8409a0f379a598a4d1

--- services/core/java/com/android/server/job/controllers/StateController.java
+++ services/core/java/com/android/server/job/controllers/StateController.java
@@ -83,6 +83,28 @@ public abstract class StateController {
     public void onConstantsUpdatedLocked() {
     }
 
+    /** Called when a package is uninstalled from the device (not for an update). */
+    public void onAppRemovedLocked(String packageName, int uid) {
+    }
+
+    /** Called when a user is removed from the device. */
+    public void onUserRemovedLocked(int userId) {
+    }
+
+    /**
+     * Called when JobSchedulerService has determined that the job is not ready to be run. The
+     * Controller can evaluate if it can or should do something to promote this job's readiness.
+     */
+    public void evaluateStateLocked(JobStatus jobStatus) {
+    }
+
+    /**
+     * Called when something with the UID has changed. The controller should re-evaluate any
+     * internal state tracking dependent on this UID.
+     */
+    public void reevaluateStateLocked(int uid) {
+    }
+
     protected boolean wouldBeReadyWithConstraintLocked(JobStatus jobStatus, int constraint) {
         // This is very cheap to check (just a few conditions on data in JobStatus).
         final boolean jobWouldBeReady = jobStatus.wouldBeReadyWithConstraint(constraint);
@@ -98,20 +120,6 @@ public abstract class StateController {
         // This is potentially more expensive since JSS may have to query component
         // presence.
         return mService.areComponentsInPlaceLocked(jobStatus);
-    }
-
-    /**
-     * Called when JobSchedulerService has determined that the job is not ready to be run. The
-     * Controller can evaluate if it can or should do something to promote this job's readiness.
-     */
-    public void evaluateStateLocked(JobStatus jobStatus) {
-    }
-
-    /**
-     * Called when something with the UID has changed. The controller should re-evaluate any
-     * internal state tracking dependent on this UID.
-     */
-    public void reevaluateStateLocked(int uid) {
     }
 
     public abstract void dumpControllerStateLocked(IndentingPrintWriter pw,
