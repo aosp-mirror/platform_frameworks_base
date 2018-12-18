a6a9d0f90cd4b573f49ffab3ff8e539b5b3e09ad
KOUSHIK PANUGANTI <kpanuganti@google.com>
2018-12-17 18:14:55 -0800

    Migrate frameworks/base/core/tests/privacytests to androidx.test
    See go/jetpack-test-android-migration
    
    Test: make checkbuild
    Change-Id: I286d611dfbc025061fe999e0238775ddd85456de

--- core/tests/privacytests/src/android/privacy/RapporEncoderTest.java
+++ core/tests/privacytests/src/android/privacy/RapporEncoderTest.java
@@ -22,8 +22,9 @@ import static org.junit.Assert.assertTrue;
 
 import android.privacy.internal.rappor.RapporConfig;
 import android.privacy.internal.rappor.RapporEncoder;
-import android.support.test.filters.SmallTest;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
