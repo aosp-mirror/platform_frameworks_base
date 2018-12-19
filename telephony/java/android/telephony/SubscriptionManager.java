f6169a0996309550b511ff603b482c0c0f983ced
Malcolm Chen <refuhoo@google.com>
2018-12-19 15:56:13 -0800

    Remove mistake of calling iSub twice in getSelectableSubscriptionInfoList
    Don't call getAvailableSubscriptionInfoList twice in
    getSelectableSubscriptionInfoList.
    
    Test: build
    Change-Id: Ie5821b1d8a01834228401ce6bb1e9ae251168760

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -2583,8 +2583,7 @@ public class SubscriptionManager {
         if (availableList == null) {
             return null;
         } else {
-            return getAvailableSubscriptionInfoList().stream()
-                    .filter(subInfo -> !shouldHideSubscription(subInfo))
+            return availableList.stream().filter(subInfo -> !shouldHideSubscription(subInfo))
                     .collect(Collectors.toList());
         }
     }
