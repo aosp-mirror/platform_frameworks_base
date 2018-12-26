55b655c881d992f916c67c9d4465232a653e7772
Lucas Dupin <dupin@google.com>
2018-12-26 11:13:33 -0800

    Delete hollow pill flag
    Bug: 111405682
    Test: compile, enter AOD, unlock
    Change-Id: Iff2054420844099fdbb879ecef9af6ee218383b9

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LockIcon.java
@@ -45,8 +45,6 @@ public class LockIcon extends KeyguardAffordanceView implements OnUserInfoChange
     private static final int STATE_FACE_UNLOCK = 2;
     private static final int STATE_FINGERPRINT = 3;
     private static final int STATE_FINGERPRINT_ERROR = 4;
-    private static final boolean HOLLOW_PILL = SystemProperties
-            .getBoolean("persist.sysui.hollow_pill", false);
 
     private int mLastState = 0;
     private boolean mLastDeviceInteractive;
@@ -222,16 +220,6 @@ public class LockIcon extends KeyguardAffordanceView implements OnUserInfoChange
                 break;
             default:
                 throw new IllegalArgumentException();
-        }
-
-        if (HOLLOW_PILL && deviceInteractive) {
-            switch (state) {
-                case STATE_FINGERPRINT:
-                case STATE_LOCK_OPEN:
-                case STATE_LOCKED:
-                case STATE_FACE_UNLOCK:
-                    iconRes = R.drawable.ic_home_button_outline;
-            }
         }
 
         return mContext.getDrawable(iconRes);
