20491714c2ca0a8fd789220e9a8afba3701f5113
Etan Cohen <etancohen@google.com>
2018-12-30 17:59:59 -0800

    [CS] Add an option to block sensitive network specifier
    Network specifiers are used for 2 purposes:
    
    - As part of network requests to specify more information on the type
      of requested networks.
    - On network agents to specify information about their networks.
    
    The network specifiers of the requests and agents are matched to each
    other. However, the agent network specifier may contain sensitive
    information which we do not want forwarded to any app.
    
    This CL adds an option to strip out this agent network specifier before
    the network capabilities are forwarded to the app.
    
    Bug: 161853197
    Bug: 161370134
    Test: atest ConnectivityServiceTest (frameworks/base/tests/net)
    Test: atest frameworks/base/tests/net
    Test: atest frameworks/opt/net/wifi/tests/wifitests
    Test: atest frameworks/opt/telephony/tests/telephonytests
    Test: atest frameworks/opt/net/ethernet/tests
    Test: atest android.net.cts - some flakiness!
    Test: act.py ThroughputTest
    Test: act.py DataPathTest
    Test: atest SingleDeviceTest (cts)
    Change-Id: I38ed3ff88532ef522ab167c88d87e6e82295ffc5
    Merged-In: If08d312ff814bdde1147518f923199e6349503d5
    (cherry picked from commit 9b1d701af461899a03b046dbf97316abc1465ac3)

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -1424,6 +1424,9 @@ public class ConnectivityService extends IConnectivityManager.Stub
             newNc.setUids(null);
             newNc.setSSID(null);
         }
+        if (newNc.getNetworkSpecifier() != null) {
+            newNc.setNetworkSpecifier(newNc.getNetworkSpecifier().redact());
+        }
         return newNc;
     }
 
@@ -5092,7 +5095,8 @@ public class ConnectivityService extends IConnectivityManager.Stub
         }
         switch (notificationType) {
             case ConnectivityManager.CALLBACK_AVAILABLE: {
-                putParcelable(bundle, new NetworkCapabilities(networkAgent.networkCapabilities));
+                putParcelable(bundle, networkCapabilitiesRestrictedForCallerPermissions(
+                        networkAgent.networkCapabilities, nri.mPid, nri.mUid));
                 putParcelable(bundle, new LinkProperties(networkAgent.linkProperties));
                 break;
             }
