a74079a2424454f20dedee8f1b29fe8bc1522d63
Holly Jiuyu Sun <jiuyu@google.com>
2019-01-09 13:33:14 -0800

    Allow privileged carrier app to update subscription nickname.
    Make EuiccManager#updateSubscriptionNickname as public API to allow
    carrier app to call it.
    Only a privileged app of the given subscription can update the
    subscription nickname.
    Refresh cached subscription list after the nickname is successfully
    updated.
    
    Bug: 122597547
    Test: test on phone
    Change-Id: I63c5ec4366f32735a54adf40b9cc0b47af681736
    Merged-In: I63c5ec4366f32735a54adf40b9cc0b47af681736

--- telephony/java/android/telephony/euicc/EuiccManager.java
+++ telephony/java/android/telephony/euicc/EuiccManager.java
@@ -546,14 +546,13 @@ public class EuiccManager {
     /**
      * Update the nickname for the given subscription.
      *
-     * <p>Requires that the calling app has the
-     * {@link android.Manifest.permission#WRITE_EMBEDDED_SUBSCRIPTIONS} permission. This is for
-     * internal system use only.
+     * <p>Requires that the calling app has carrier privileges according to the metadata of the
+     * profile to be updated, or the
+     * {@code android.Manifest.permission#WRITE_EMBEDDED_SUBSCRIPTIONS} permission.
      *
      * @param subscriptionId the ID of the subscription to update.
      * @param nickname the new nickname to apply.
      * @param callbackIntent a PendingIntent to launch when the operation completes.
-     * @hide
      */
     @RequiresPermission(Manifest.permission.WRITE_EMBEDDED_SUBSCRIPTIONS)
     public void updateSubscriptionNickname(
@@ -564,7 +563,7 @@ public class EuiccManager {
         }
         try {
             getIEuiccController().updateSubscriptionNickname(
-                    subscriptionId, nickname, callbackIntent);
+                    subscriptionId, nickname, mContext.getOpPackageName(), callbackIntent);
         } catch (RemoteException e) {
             throw e.rethrowFromSystemServer();
         }
