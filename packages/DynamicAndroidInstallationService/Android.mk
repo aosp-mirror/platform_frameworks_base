64aa78297fc2e8cb70350fc468cf49a3768af5a1
Po-Chien Hsueh <pchsueh@google.com>
2019-01-12 00:40:02 +0800

    Add DynamicAndroidInstallationService
    DynamicAndroidInstallationService is a framework service to download,
    unzip and write DynamicAndroid images. Apps should interact with it
    using DynamicAndroidClient. Or, developers can use adb am commmands.
    
    Test: build and run on internal target
    Bug: 122015653
    Change-Id: I7c834ed37de52840a407fb140743eda1f2bd82e8

---
+++ packages/DynamicAndroidInstallationService/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, res)
+
+LOCAL_USE_AAPT2 := true
+
+LOCAL_PACKAGE_NAME := DynamicAndroidInstallationService
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+LOCAL_PROGUARD_ENABLED := disabled
+
+
+include $(BUILD_PACKAGE)
