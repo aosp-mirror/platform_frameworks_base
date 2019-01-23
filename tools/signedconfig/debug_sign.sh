b55d7a0a9dfab68a65906429ba4d7d54452347be
Mathew Inwood <mathewi@google.com>
2019-01-23 17:52:39 +0000

    Replace debug key.
    This gives better consistency with the release key. Also improve the
    debug_sign script to use base64, to eliminate issues with escaping and
    newlines.
    
    Bug: 110509075
    Test: atest CtsSignedConfigHostTestCases
    Change-Id: Id8d7b6b5bee3340803d30d3a4a34c7e62f565a93

--- tools/signedconfig/debug_sign.sh
+++ tools/signedconfig/debug_sign.sh
@@ -2,5 +2,5 @@
 # Script to sign data with the debug keys. Outputs base64 for embedding into
 # APK metadata.
 
-openssl dgst -sha256 -sign $(dirname $0)/debug_key.pem  $1 | base64 -w 0
+openssl dgst -sha256 -sign $(dirname $0)/debug_key.pem  <(echo -n "$1" | base64 -d) | base64 -w 0
 echo
