ac1879ab063595d52773e82d1f319ecfcc31c63b
Soonil Nagarkar <sooniln@google.com>
2019-02-01 19:06:18 -0800

    Add TestApi annotations to LocationManager
    Expose some APIs for CTS tests.
    
    Bug: 123766886
    Test: N/A
    Change-Id: I4fe7d05cf13c3cff793803297698452c1bfc247e

--- location/java/android/location/LocationRequest.java
+++ location/java/android/location/LocationRequest.java
@@ -19,6 +19,7 @@ package android.location;
 import android.Manifest;
 import android.annotation.RequiresPermission;
 import android.annotation.SystemApi;
+import android.annotation.TestApi;
 import android.annotation.UnsupportedAppUsage;
 import android.os.Build;
 import android.os.Parcel;
@@ -90,6 +91,7 @@ import android.util.TimeUtils;
  * @hide
  */
 @SystemApi
+@TestApi
 public final class LocationRequest implements Parcelable {
     /**
      * Used with {@link #setQuality} to request the most accurate locations available.
