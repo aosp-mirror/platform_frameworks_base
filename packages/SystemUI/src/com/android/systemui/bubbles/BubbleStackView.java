3d82e68dfdf446a441f125ff76e21653582bb8da
Mady Mellor <madym@google.com>
2019-02-05 13:34:48 -0800

    Rename: Drop 'Container' from BubbleExpandedViewContainer
    Test: atest BubbleControllerTest / it compiles
    Bug: 123543995
    Change-Id: I4ff194966edffd7d314bd9aade1ebb2bb6d2f7d2

--- packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
+++ packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
@@ -94,7 +94,7 @@ public class BubbleStackView extends FrameLayout implements BubbleTouchHandler.F
     private StackAnimationController mStackAnimationController;
     private ExpandedAnimationController mExpandedAnimationController;
 
-    private BubbleExpandedViewContainer mExpandedViewContainer;
+    private BubbleExpandedView mExpandedViewContainer;
 
     private int mBubbleSize;
     private int mBubblePadding;
@@ -173,7 +173,7 @@ public class BubbleStackView extends FrameLayout implements BubbleTouchHandler.F
         mBubbleContainer.setClipChildren(false);
         addView(mBubbleContainer, new FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT));
 
-        mExpandedViewContainer = (BubbleExpandedViewContainer)
+        mExpandedViewContainer = (BubbleExpandedView)
                 LayoutInflater.from(context).inflate(R.layout.bubble_expanded_view,
                         this /* parent */, false /* attachToRoot */);
         mExpandedViewContainer.setElevation(elevation);
@@ -226,7 +226,7 @@ public class BubbleStackView extends FrameLayout implements BubbleTouchHandler.F
     /**
      * Sets the listener to notify when a bubble is blocked.
      */
-    public void setOnBlockedListener(BubbleExpandedViewContainer.OnBubbleBlockedListener listener) {
+    public void setOnBlockedListener(BubbleExpandedView.OnBubbleBlockedListener listener) {
         mExpandedViewContainer.setOnBlockedListener(listener);
     }
 
