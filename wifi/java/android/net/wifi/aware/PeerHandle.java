ea792225c4f2e6f10e60c5b54ecfc53beab5b974
Etan Cohen <etancohen@google.com>
2019-02-08 07:01:54 -0800

    Revert "[AWARE] Make PeerHandle parcelable"
    This reverts commit a36b5c5815e0b3e8aef7ba7866a82773b0945561.
    
    Can't change the behavior of PeerHandle in-place. Will create
    a new class which extends it.
    
    Bug: 122984982
    Test: atest android.net.wifi
    Change-Id: I14da17680fcbf20ece84631896d10cfaa53e33dc

--- wifi/java/android/net/wifi/aware/PeerHandle.java
+++ wifi/java/android/net/wifi/aware/PeerHandle.java
@@ -16,9 +16,6 @@
 
 package android.net.wifi.aware;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Opaque object used to represent a Wi-Fi Aware peer. Obtained from discovery sessions in
  * {@link DiscoverySessionCallback#onServiceDiscovered(PeerHandle, byte[], java.util.List)} or
@@ -36,7 +33,7 @@ import android.os.Parcelable;
  * {@link PublishConfig.Builder#setServiceSpecificInfo(byte[])}, or match filter,
  * {@link PublishConfig.Builder#setMatchFilter(java.util.List)}.
  */
-public final class PeerHandle implements Parcelable {
+public class PeerHandle {
     /** @hide */
     public PeerHandle(int peerId) {
         this.peerId = peerId;
@@ -62,29 +59,4 @@ public final class PeerHandle implements Parcelable {
     public int hashCode() {
         return peerId;
     }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeInt(peerId);
-    }
-
-    public static final Creator<PeerHandle> CREATOR = new Creator<PeerHandle>() {
-        @Override
-        public PeerHandle[] newArray(int size) {
-            return new PeerHandle[size];
-        }
-
-        @Override
-        public PeerHandle createFromParcel(Parcel in) {
-            int peerHandle = in.readInt();
-
-            return new PeerHandle(peerHandle);
-        }
-    };
-
 }
--- wifi/tests/src/android/net/wifi/aware/WifiAwareManagerTest.java
+++ wifi/tests/src/android/net/wifi/aware/WifiAwareManagerTest.java
@@ -1613,24 +1613,4 @@ public class WifiAwareManagerTest {
         assertEquals(cap, rereadCap);
         assertEquals(cap.hashCode(), rereadCap.hashCode());
     }
-
-    // PeerHandle tests
-
-    @Test
-    public void testPeerHandleParcel() {
-        final PeerHandle peerHandle = new PeerHandle(5);
-
-        Parcel parcelW = Parcel.obtain();
-        peerHandle.writeToParcel(parcelW, 0);
-        byte[] bytes = parcelW.marshall();
-        parcelW.recycle();
-
-        Parcel parcelR = Parcel.obtain();
-        parcelR.unmarshall(bytes, 0, bytes.length);
-        parcelR.setDataPosition(0);
-        PeerHandle rereadPeerHandle = PeerHandle.CREATOR.createFromParcel(parcelR);
-
-        assertEquals(peerHandle, rereadPeerHandle);
-        assertEquals(peerHandle.hashCode(), rereadPeerHandle.hashCode());
-    }
 }
