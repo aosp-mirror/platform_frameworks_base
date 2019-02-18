9f05e0ea17b38e3a0453e6f5f30239882c0d60d7
Jeff Sharkey <jsharkey@android.com>
2019-02-18 12:47:25 -0700

    MediaStore API and docs polishing.
    Initial efforts at cleaning up MediaStore docs, including deprecation
    of crufty methods, and annotations to communicate stored units.
    
    Bug: 120429729, 124013019
    Test: atest cts/tests/tests/provider/src/android/provider/cts/MediaStore*
    Change-Id: I87828f19030b6b354472c1b3ce767de166f86134

--- core/java/android/annotation/CurrentTimeSecondsLong.java
+++ core/java/android/annotation/CurrentTimeSecondsLong.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2017 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,14 +26,14 @@ import java.lang.annotation.Target;
 
 /**
  * @memberDoc Value is a non-negative timestamp measured as the number of
- *            milliseconds since 1970-01-01T00:00:00Z.
+ *            seconds since 1970-01-01T00:00:00Z.
  * @paramDoc Value is a non-negative timestamp measured as the number of
- *            milliseconds since 1970-01-01T00:00:00Z.
+ *            seconds since 1970-01-01T00:00:00Z.
  * @returnDoc Value is a non-negative timestamp measured as the number of
- *            milliseconds since 1970-01-01T00:00:00Z.
+ *            seconds since 1970-01-01T00:00:00Z.
  * @hide
  */
 @Retention(SOURCE)
 @Target({METHOD, PARAMETER, FIELD})
-public @interface CurrentTimeMillisLong {
+public @interface CurrentTimeSecondsLong {
 }
