b4469f9e589949beeed37ae9a7b11023a1fb64a8
Makoto Onuki <omakoto@google.com>
2019-02-19 15:55:44 -0800

    Add ADB command to wait for system server crash
    "adb shell svc system-server wait-for-crash" will block until the process dies.
    
    Bug: 124022170
    Test: Manual
    - Run "adb shell svc system-server wait-for-crash" on multiple terminals
    - Make sure open FDs aren't increasing by checking /proc/`pid system_server`/fd
    - adb shell killall system_server
    - All the "adb shell svc system-server wait-for-crash" should finish.
    
    Change-Id: I203d001cd296d506ebf9f5a70b97e05e5769961a

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1484,6 +1484,8 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private static String sTheRealBuildSerial = Build.UNKNOWN;
 
+    private ParcelFileDescriptor[] mLifeMonitorFds;
+
     final class UiHandler extends Handler {
         public UiHandler() {
             super(com.android.server.UiThread.get().getLooper(), null, true);
@@ -18461,5 +18463,25 @@ public class ActivityManagerService extends IActivityManager.Stub
 
     private boolean isOnOffloadQueue(int flags) {
         return (mEnableOffloadQueue && ((flags & Intent.FLAG_RECEIVER_OFFLOAD) != 0));
+    }
+
+    @Override
+    public ParcelFileDescriptor getLifeMonitor() {
+        if (!isCallerShell()) {
+            throw new SecurityException("Only shell can call it");
+        }
+        synchronized (this) {
+            try {
+                if (mLifeMonitorFds == null) {
+                    mLifeMonitorFds = ParcelFileDescriptor.createPipe();
+                }
+                // The returned FD will be closed, but we want to keep our reader open,
+                // so return a dup instead.
+                return mLifeMonitorFds[0].dup();
+            } catch (IOException e) {
+                Slog.w(TAG, "Unable to create pipe", e);
+                return null;
+            }
+        }
     }
 }
