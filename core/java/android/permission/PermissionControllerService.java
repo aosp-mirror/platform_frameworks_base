d895953d3cec7db719a01e3da311a3058eee7c89
Hai Zhang <zhanghai@google.com>
2019-02-28 16:55:30 -0800

    Add PermissionControllerService.isRoleVisible() for app info shortcut.
    The default app shortcut in app info inside Settings needs to know if
    a role is visible, and whether it is visible is controlled logic in
    PermissionController, hence add this API.
    
    Bug: 124452117
    Bug: 124457823
    Test: manual
    Change-Id: I6058dea3bbda8b06d2fb9bab35268397227dd37b

--- core/java/android/permission/PermissionControllerService.java
+++ core/java/android/permission/PermissionControllerService.java
@@ -179,10 +179,19 @@ public abstract class PermissionControllerService extends Service {
      * @param roleName name of the role to check for
      * @param packageName package name of the application to check for
      *
-     * @return whether the application is qualified for the role.
+     * @return whether the application is qualified for the role
      */
     public abstract boolean onIsApplicationQualifiedForRole(@NonNull String roleName,
             @NonNull String packageName);
+
+    /**
+     * Check whether a role should be visible to user.
+     *
+     * @param roleName name of the role to check for
+     *
+     * @return whether the role should be visible to user
+     */
+    public abstract boolean onIsRoleVisible(@NonNull String roleName);
 
     /**
      * Set the runtime permission state from a device admin.
@@ -344,6 +353,18 @@ public abstract class PermissionControllerService extends Service {
             }
 
             @Override
+            public void isRoleVisible(String roleName, RemoteCallback callback) {
+                checkStringNotEmpty(roleName);
+                checkNotNull(callback, "callback");
+
+                enforceCallingPermission(Manifest.permission.MANAGE_ROLE_HOLDERS, null);
+
+                mHandler.sendMessage(obtainMessage(
+                        PermissionControllerService::isRoleVisible,
+                        PermissionControllerService.this, roleName, callback));
+            }
+
+            @Override
             public void setRuntimePermissionGrantStateByDeviceAdmin(String callerPackageName,
                     String packageName, String permission, int grantState,
                     RemoteCallback callback) {
@@ -442,6 +463,13 @@ public abstract class PermissionControllerService extends Service {
         boolean qualified = onIsApplicationQualifiedForRole(roleName, packageName);
         Bundle result = new Bundle();
         result.putBoolean(PermissionControllerManager.KEY_RESULT, qualified);
+        callback.sendResult(result);
+    }
+
+    private void isRoleVisible(@NonNull String roleName, @NonNull RemoteCallback callback) {
+        boolean visible = onIsRoleVisible(roleName);
+        Bundle result = new Bundle();
+        result.putBoolean(PermissionControllerManager.KEY_RESULT, visible);
         callback.sendResult(result);
     }
 
