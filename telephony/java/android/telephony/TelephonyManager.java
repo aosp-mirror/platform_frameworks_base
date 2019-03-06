ceaf9684a56039ffdf07ddfd6a4aee682de030c6
Nazanin Bakhshi <nazaninb@google.com>
2019-03-06 15:31:45 -0800

    update getStaticPhoneCapability to use HAL instead of default value
    Bug: 123359867
    Test: build
    Change-Id: Ic40921679ade1f9be42f2bc3d5170ba201dbab5a
    Merged-In: Ic40921679ade1f9be42f2bc3d5170ba201dbab5a
    (cherry picked from commit 1772247b044795cdad1fa22069040df0bf657f42)

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -9704,7 +9704,7 @@ public class TelephonyManager {
             ITelephony telephony = getITelephony();
             if (telephony != null) {
                 return telephony.getNumberOfModemsWithSimultaneousDataConnections(
-                        getSubId(), mContext.getOpPackageName());
+                        getSubId(), getOpPackageName());
             }
         } catch (RemoteException ex) {
             // This could happen if binder process crashes.
