390f4c03ab15a0543aa3b07bb566571e9394d45a
KOUSHIK PANUGANTI <kpanuganti@google.com>
2019-03-13 16:53:33 -0700

    Migrate frameworks/base/packages/Shell to androidx.test
    See go/jetpack-test-android-migration
    
    Test: mmma frameworks/base/packages/Shell
    Change-Id: I26076b3ad1871dde3ef6b206bf5502648d1a0cbb

--- packages/Shell/tests/Android.mk
+++ packages/Shell/tests/Android.mk
@@ -9,7 +9,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_JAVA_LIBRARIES := android.test.runner android.test.base android.test.mock
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    android-support-test \
+    androidx.test.rules \
     mockito-target-minus-junit4 \
     ub-uiautomator \
     junit \
