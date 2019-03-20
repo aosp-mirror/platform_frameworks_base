569ac173c18b6b63c5e543f585796929435a0eac
Varun Shah <varunshah@google.com>
2019-03-20 11:10:33 -0700

    Added missing permission check to isPackageDeviceAdminOnAnyUser.
    Added a check for the MANAGE_USERS permission to
    PackageManagerService#isPackageDeviceAdminOnAnyUser.
    
    To test that the method is still usable:
    1) Enable virtual storage via: adb shell sm set-virtual-disk true
    2) Follow instructions by clicking on notification to set up virtual storage
    3) Go to Settings -> Apps & notifications -> See all X apps
    4) Click on any non-system app (example Instagram)
    5) Tap Storage and you should see a "Change" button (if not, choose another app)
    6) Tap Change and you should see Internal and Virtual storage options listed
    7) The above step confirms the method is still usable by Settings
    
    Bug: 128599183
    Test: SafetyNet logging (steps listed above)
    Change-Id: I36521fa43daab399e08869647326a7ac32d1e512

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -17666,6 +17666,12 @@ public class PackageManagerService extends IPackageManager.Stub
     @Override
     public boolean isPackageDeviceAdminOnAnyUser(String packageName) {
         final int callingUid = Binder.getCallingUid();
+        if (checkUidPermission(android.Manifest.permission.MANAGE_USERS, callingUid)
+                != PERMISSION_GRANTED) {
+            EventLog.writeEvent(0x534e4554, "128599183", -1, "");
+            throw new SecurityException(android.Manifest.permission.MANAGE_USERS
+                    + " permission is required to call this API");
+        }
         if (getInstantAppPackageName(callingUid) != null
                 && !isCallerSameApp(packageName, callingUid)) {
             return false;
