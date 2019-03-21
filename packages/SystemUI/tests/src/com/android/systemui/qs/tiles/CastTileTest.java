32492eebf9416bd4f615aa2f2fe8b0314b7bc5f4
Narayan Kamath <narayan@google.com>
2019-03-21 18:09:38 +0000

    CastTile: Better handling of multiple active devices.
    With the new MediaProjection based flow for cast, we will have
    a connected MediaRoute active at the same time as a MediaProjection
    session. In order to deal with them correctly, we need to assume
    in a few places that we have more than one active CastDevice. We
    also consider all devices that are connected, regardless of whether
    the given route is selected or not.
    
    Test: Manual
    Test: atest CastControllerImplTest
    Test: atest SystemUITests
    Bug: 128515798
    Change-Id: Ie46798633f69c347ee32e0799d6cb23576122dd9

--- packages/SystemUI/tests/src/com/android/systemui/qs/tiles/CastTileTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/qs/tiles/CastTileTest.java
@@ -14,13 +14,19 @@
 
 package com.android.systemui.qs.tiles;
 
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.assertEquals;
 
+import static org.mockito.ArgumentMatchers.same;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import android.media.MediaRouter;
+import android.media.MediaRouter.RouteInfo;
+import android.media.projection.MediaProjectionInfo;
 import android.service.quicksettings.Tile;
 import android.testing.AndroidTestingRunner;
 import android.testing.TestableLooper;
@@ -33,6 +39,7 @@ import com.android.systemui.SysuiTestCase;
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.qs.QSTileHost;
 import com.android.systemui.statusbar.policy.CastController;
+import com.android.systemui.statusbar.policy.CastController.CastDevice;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.NetworkController;
 
@@ -43,8 +50,9 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RunWith(AndroidTestingRunner.class)
 @TestableLooper.RunWithLooper
@@ -93,7 +101,6 @@ public class CastTileTest extends SysuiTestCase {
         verify(mNetworkController).observe(any(LifecycleOwner.class),
                 signalCallbackArgumentCaptor.capture());
         mCallback = signalCallbackArgumentCaptor.getValue();
-
     }
 
     @Test
@@ -120,33 +127,125 @@ public class CastTileTest extends SysuiTestCase {
         assertEquals(Tile.STATE_UNAVAILABLE, mCastTile.getState().state);
     }
 
-    @Test
-    public void testStateActive_wifiEnabledAndCasting() {
-        CastController.CastDevice device = mock(CastController.CastDevice.class);
-        device.state = CastController.CastDevice.STATE_CONNECTED;
-        Set<CastController.CastDevice> devices = new HashSet<>();
-        devices.add(device);
-        when(mController.getCastDevices()).thenReturn(devices);
-
+    private void enableWifiAndProcessMessages() {
         NetworkController.IconState qsIcon =
                 new NetworkController.IconState(true, 0, "");
         mCallback.setWifiIndicators(true, mock(NetworkController.IconState.class),
                 qsIcon, false,false, "",
                 false, "");
         mTestableLooper.processAllMessages();
+    }
 
+    @Test
+    public void testStateActive_wifiEnabledAndCasting() {
+        CastController.CastDevice device = new CastController.CastDevice();
+        device.state = CastController.CastDevice.STATE_CONNECTED;
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(device);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
         assertEquals(Tile.STATE_ACTIVE, mCastTile.getState().state);
     }
 
     @Test
     public void testStateInactive_wifiEnabledNotCasting() {
-        NetworkController.IconState qsIcon =
-                new NetworkController.IconState(true, 0, "");
-        mCallback.setWifiIndicators(true, mock(NetworkController.IconState.class),
-                qsIcon, false,false, "",
-                false, "");
+        enableWifiAndProcessMessages();
+        assertEquals(Tile.STATE_INACTIVE, mCastTile.getState().state);
+    }
+
+    @Test
+    public void testHandleClick_castDevicePresent() {
+        CastController.CastDevice device = new CastController.CastDevice();
+        device.state = CastDevice.STATE_CONNECTED;
+        device.tag = mock(MediaRouter.RouteInfo.class);
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(device);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
+        mCastTile.handleClick();
         mTestableLooper.processAllMessages();
 
-        assertEquals(Tile.STATE_INACTIVE, mCastTile.getState().state);
+        verify(mActivityStarter, times(1)).postQSRunnableDismissingKeyguard(any());
+    }
+
+    @Test
+    public void testHandleClick_projectionOnly() {
+        CastController.CastDevice device = new CastController.CastDevice();
+        device.state = CastDevice.STATE_CONNECTED;
+        device.tag = mock(MediaProjectionInfo.class);
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(device);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
+        mCastTile.handleClick();
+        mTestableLooper.processAllMessages();
+
+        verify(mController, times(1)).stopCasting(same(device));
+    }
+
+    @Test
+    public void testUpdateState_projectionOnly() {
+        CastController.CastDevice device = new CastController.CastDevice();
+        device.state = CastDevice.STATE_CONNECTED;
+        device.tag = mock(MediaProjectionInfo.class);
+        device.name = "Test Projection Device";
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(device);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
+        assertEquals(Tile.STATE_ACTIVE, mCastTile.getState().state);
+        assertTrue(mCastTile.getState().secondaryLabel.toString().startsWith(device.name));
+    }
+
+    @Test
+    public void testUpdateState_castingAndProjection() {
+        CastController.CastDevice casting = new CastController.CastDevice();
+        casting.state = CastDevice.STATE_CONNECTED;
+        casting.tag = mock(RouteInfo.class);
+        casting.name = "Test Casting Device";
+
+        CastController.CastDevice projection = new CastController.CastDevice();
+        projection.state = CastDevice.STATE_CONNECTED;
+        projection.tag = mock(MediaProjectionInfo.class);
+        projection.name = "Test Projection Device";
+
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(casting);
+        devices.add(projection);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
+
+        // Note here that the tile should be active, and should choose casting over projection.
+        assertEquals(Tile.STATE_ACTIVE, mCastTile.getState().state);
+        assertTrue(mCastTile.getState().secondaryLabel.toString().startsWith(casting.name));
+    }
+
+    @Test
+    public void testUpdateState_connectedAndConnecting() {
+        CastController.CastDevice connecting = new CastController.CastDevice();
+        connecting.state = CastDevice.STATE_CONNECTING;
+        connecting.tag = mock(RouteInfo.class);
+        connecting.name = "Test Casting Device";
+
+        CastController.CastDevice connected = new CastController.CastDevice();
+        connected.state = CastDevice.STATE_CONNECTED;
+        connected.tag = mock(RouteInfo.class);
+        connected.name = "Test Casting Device";
+
+        List<CastDevice> devices = new ArrayList<>();
+        devices.add(connecting);
+        devices.add(connected);
+        when(mController.getCastDevices()).thenReturn(devices);
+
+        enableWifiAndProcessMessages();
+
+        // Tile should be connected and always prefer the connected device.
+        assertEquals(Tile.STATE_ACTIVE, mCastTile.getState().state);
+        assertTrue(mCastTile.getState().secondaryLabel.toString().startsWith(connected.name));
     }
 }
