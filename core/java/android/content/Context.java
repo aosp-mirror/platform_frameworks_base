f178c96cabc8fed228b034e5190edc6c84d54cf8
Amith Yamasani <yamasani@google.com>
2019-03-21 19:40:11 -0700

    Introduce new service bind flag to permit capabilities
    Any process state-based capabilities such as location
    can be allowed to permeate to the bound app, if the app
    has the required permissions as well.
    
    Bug: 128337543
    Test: N/A
    Change-Id: I0b066d2667333fbd65985959e202785641f704fc

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -338,6 +338,14 @@ public abstract class Context {
     public static final int BIND_ADJUST_BELOW_PERCEPTIBLE = 0x0100;
 
     /**
+     * Flag for {@link #bindService}: If binding from an app that has specific capabilities
+     * due to its foreground state such as an activity or foreground service, then this flag will
+     * allow the bound app to get the same capabilities, as long as it has the required permissions
+     * as well.
+     */
+    public static final int BIND_INCLUDE_CAPABILITIES = 0x00001000;
+
+    /**
      * Flag for {@link #bindService}: This flag is intended to be used only by the system to adjust
      * the scheduling policy for IMEs (and any other out-of-process user-visible components that
      * work closely with the top app) so that UI hosted in such services can have the same
