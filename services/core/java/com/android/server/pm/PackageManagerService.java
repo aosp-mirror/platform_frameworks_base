e1f8449bdece70541e2bb3da3f95a508e775281f
Przemyslaw Szczepaniak <pszczepaniak@google.com>
2019-04-04 13:55:55 +0100

    Remove PRODUCT_SERVICES from PackageManagerNative.getLocation.
    It's not used in next android release and its causing issues
    with cherry-picking to AOSP.
    
    Bug: 120483623
    Test: Flashed crosshatch, NNAPI cts tests
    Change-Id: I98fda5751a7b1d2a6f3ad5b58682ad935fc30fa8

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -23383,9 +23383,7 @@ public class PackageManagerService extends IPackageManager.Stub
             }
             return ((appInfo.isSystemApp() ? IPackageManagerNative.LOCATION_SYSTEM : 0)
                     | (appInfo.isVendor() ? IPackageManagerNative.LOCATION_VENDOR : 0)
-                    | (appInfo.isProduct() ? IPackageManagerNative.LOCATION_PRODUCT : 0)
-                    | (appInfo.isProductServices()
-                            ? IPackageManagerNative.LOCATION_PRODUCT_SERVICES : 0));
+                    | (appInfo.isProduct() ? IPackageManagerNative.LOCATION_PRODUCT : 0));
         }
     }
 
