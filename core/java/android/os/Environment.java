45810afbeab450b38b6c8612890a72e042c27b3b
Chad Brubaker <cbrubaker@google.com>
2019-04-08 19:19:48 -0700

    Don't use noteop for preflight checks
    Test: apps don't show as accessing location on start
    Fixes: 130187488
    Change-Id: Iacd7edbadab64f43229b5d6f8056bb076c4f280c

--- core/java/android/os/Environment.java
+++ core/java/android/os/Environment.java
@@ -1148,9 +1148,9 @@ public class Environment {
         final Context context = AppGlobals.getInitialApplication();
         final AppOpsManager appOps = context.getSystemService(AppOpsManager.class);
 
-        final boolean hasLegacy = appOps.noteOpNoThrow(AppOpsManager.OP_LEGACY_STORAGE,
+        final boolean hasLegacy = appOps.checkOpNoThrow(AppOpsManager.OP_LEGACY_STORAGE,
                 context.getApplicationInfo().uid,
-                context.getPackageName()) == AppOpsManager.MODE_ALLOWED;
+                context.getOpPackageName()) == AppOpsManager.MODE_ALLOWED;
 
         // STOPSHIP: only use app-op once permission model has fully landed
         final boolean requestedLegacy = !AppGlobals.getInitialApplication().getApplicationInfo()
