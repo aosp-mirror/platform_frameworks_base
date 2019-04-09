3ec38dc5530db151388879a521d6d3b94679a0de
Benedict Wong <benedictwong@google.com>
2019-04-09 16:29:43 -0700

    Enable checks for MANAGE_TEST_NETWORKS in TestNetworkService
    This commit re-enables enforcement of the MANAGE_TEST_NETWORK
    permission, which is only granted to the shell. CTS tests using this
    permission should use UiAutomation.adoptShellPermissionIdentity() to
    gain access.
    
    Bug: 72950854
    Test: IPsec CTS tests using this passing
    Change-Id: I98573a5c68e45abbbaddef01f6ac74a6a18e26f9

--- services/core/java/com/android/server/TestNetworkService.java
+++ services/core/java/com/android/server/TestNetworkService.java
@@ -325,14 +325,10 @@ class TestNetworkService extends ITestNetworkManager.Stub {
         agent.teardown();
     }
 
-    // STOPSHIP: Change this back to android.Manifest.permission.MANAGE_TEST_NETWORKS
-    private static final String PERMISSION_NAME = "dummy";
+    private static final String PERMISSION_NAME =
+            android.Manifest.permission.MANAGE_TEST_NETWORKS;
 
     public static void enforceTestNetworkPermissions(@NonNull Context context) {
-        // STOPSHIP: Re-enable these checks. Disabled until adoptShellPermissionIdentity() can be
-        //           called from CTS test code.
-        if (false) {
-            context.enforceCallingOrSelfPermission(PERMISSION_NAME, "TestNetworkService");
-        }
+        context.enforceCallingOrSelfPermission(PERMISSION_NAME, "TestNetworkService");
     }
 }
