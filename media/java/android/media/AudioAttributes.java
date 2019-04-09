9bf75facd8b9e7ed775172530f44f2dd1d98831b
Kevin Rocard <krocard@google.com>
2019-04-09 16:25:26 -0700

    Add AudioAttributes.getAllowedCapturePolicy
    Bug: 129948989
    Bug: 128454287
    Test: atest AudioAttributesTest
    Change-Id: Ide4a6c160791ade1b93796ba4d2fca9e3c1f9eed
    Signed-off-by: Kevin Rocard <krocard@google.com>

--- media/java/android/media/AudioAttributes.java
+++ media/java/android/media/AudioAttributes.java
@@ -535,6 +535,23 @@ public final class AudioAttributes implements Parcelable {
     }
 
     /**
+     * Return the capture policy.
+     * @return the capture policy set by {@link Builder#setAllowedCapturePolicy(int)} or
+     *         the default if it was not called.
+     */
+    @CapturePolicy
+    public int getAllowedCapturePolicy() {
+        if ((mFlags & FLAG_NO_SYSTEM_CAPTURE) == FLAG_NO_SYSTEM_CAPTURE) {
+            return ALLOW_CAPTURE_BY_NONE;
+        }
+        if ((mFlags & FLAG_NO_MEDIA_PROJECTION) == FLAG_NO_MEDIA_PROJECTION) {
+            return ALLOW_CAPTURE_BY_SYSTEM;
+        }
+        return ALLOW_CAPTURE_BY_ALL;
+    }
+
+
+    /**
      * Builder class for {@link AudioAttributes} objects.
      * <p> Here is an example where <code>Builder</code> is used to define the
      * {@link AudioAttributes} to be used by a new <code>AudioTrack</code> instance:
