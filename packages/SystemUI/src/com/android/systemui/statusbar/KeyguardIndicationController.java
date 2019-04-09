05904655ba7e7a2cd188ecdf5ebd930ead78a48e
Lucas Dupin <dupin@google.com>
2019-04-09 16:16:15 -0700

    Only show 'scanning' animation on bouncer
    We're trying to minimize how much information the user
    has to parse when authenticating.
    
    Fixes: 130223968
    Test: manually swipe up and down
    Change-Id: I35fa0c417965dd64eefd67ec5a3b7b5076663752

--- packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/KeyguardIndicationController.java
@@ -83,7 +83,7 @@ public class KeyguardIndicationController implements StateListener {
 
     private final int mSlowThreshold;
     private final int mFastThreshold;
-    private LockIcon mLockIcon;
+    private final LockIcon mLockIcon;
     private StatusBarKeyguardViewManager mStatusBarKeyguardViewManager;
 
     private String mRestingIndication;
@@ -685,6 +685,11 @@ public class KeyguardIndicationController implements StateListener {
             if (mVisible) {
                 updateIndication(false);
             }
+        }
+
+        @Override
+        public void onKeyguardBouncerChanged(boolean bouncer) {
+            mLockIcon.setBouncerVisible(bouncer);
         }
     };
 }
