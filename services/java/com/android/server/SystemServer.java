0a069df47fae4f52158de20e4c44a96023a5e618
Tim Murray <timmurray@google.com>
2019-04-10 15:48:37 -0700

    DO NOT MERGE: disable iorap in q
    iorap is not shipping in Q.
    
    Test: boots, works
    bug 72170747
    
    Change-Id: Iabc63f880df24781fa9c6e0f4532a8f37c8d89d5

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -32,7 +32,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageItemInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.PackageManagerInternal;
 import android.content.res.Configuration;
 import android.content.res.Resources.Theme;
 import android.database.sqlite.SQLiteCompatibilityWalFlags;
@@ -158,8 +157,6 @@ import com.android.server.wm.WindowManagerGlobalLock;
 import com.android.server.wm.WindowManagerService;
 
 import dalvik.system.VMRuntime;
-
-import com.google.android.startop.iorap.IorapForwardingService;
 
 import java.io.File;
 import java.io.IOException;
@@ -1071,10 +1068,6 @@ public final class SystemServer {
 
             traceBeginAndSlog("PinnerService");
             mSystemServiceManager.startService(PinnerService.class);
-            traceEnd();
-
-            traceBeginAndSlog("IorapForwardingService");
-            mSystemServiceManager.startService(IorapForwardingService.class);
             traceEnd();
 
             traceBeginAndSlog("SignedConfigService");
