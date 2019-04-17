6c0984a71ceb313fb2b59df4af960b2309bf2ade
Neil Fuller <nfuller@google.com>
2019-04-17 15:54:22 +0100

    Fix incorrect leap year logic
    Fixing an issue for 2100 in SimpleMonthView.
    
    Tweak the logic in DatePickerCalendarDelegate to optimize
    for the common case and save a calculation.
    
    Bug: 28784177
    Test: build only
    Change-Id: I7395ab9f3dc90ac4f7cef1edee6f7bc156f4ab76

--- core/java/android/widget/DatePickerCalendarDelegate.java
+++ core/java/android/widget/DatePickerCalendarDelegate.java
@@ -597,7 +597,7 @@ class DatePickerCalendarDelegate extends DatePicker.AbstractDatePickerDelegate {
             case Calendar.NOVEMBER:
                 return 30;
             case Calendar.FEBRUARY:
-                return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) ? 29 : 28;
+                return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
             default:
                 throw new IllegalArgumentException("Invalid Month");
         }
