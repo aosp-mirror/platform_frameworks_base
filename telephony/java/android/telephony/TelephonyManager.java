6c8e0f3f534fe45b49c8d293326a36966ca379b0
Jordan Liu <jminjie@google.com>
2019-04-22 15:16:10 -0700

    Update getUiccCardsInfo doc
    This method will return info for inactive eUICCs, filtered by carrier
    privilege. (Since we can't read carrier privilege on an inactive eUICC,
    we always assume that the caller does not have carrier privilege).
    
    Bug: 130826215
    Test: manual
    Change-Id: Id89fa48f119004b6c18bd21791396bc6c3df2c95

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -3321,7 +3321,7 @@ public class TelephonyManager {
     }
 
     /**
-     * Gets information about currently inserted UICCs and enabled eUICCs.
+     * Gets information about currently inserted UICCs and eUICCs.
      * <p>
      * Requires that the calling app has carrier privileges (see {@link #hasCarrierPrivileges}).
      * <p>
