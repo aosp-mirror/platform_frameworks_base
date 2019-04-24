7284e4f5b778e51e1f63aa31fc6da8842322e741
Fabian Kozynski <kozynski@google.com>
2019-04-24 09:25:14 -0400

    Notify FSC about app ops in main thread
    Test: build
    Bug: 131188506
    Change-Id: I7c73344d65ac0322c32ad13e6f81ad52c7b629d7

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -596,8 +596,8 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void onActiveStateChanged(int code, int uid, String packageName, boolean active) {
-        mForegroundServiceController.onAppOpChanged(code, uid, packageName, active);
         Dependency.get(MAIN_HANDLER).post(() -> {
+            mForegroundServiceController.onAppOpChanged(code, uid, packageName, active);
             mNotificationListController.updateNotificationsForAppOp(code, uid, packageName, active);
         });
     }
