84a55bd5eb1c82959acccd977470c02ef23deb40
Matt Pietal <mpietal@google.com>
2019-05-01 12:32:47 -0400

    Sharesheet - Make ripples consistent
    There were differences in ripple behavior depending on the list row
    background. Modify this so the ripples propogate against the same
    parent background. Make copy button style match the work profile style.
    
    Bug: 129979196
    Test: Visual inspection
    Change-Id: I51f1b7503c2169bd03f04edecf98927850e6582b

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -2871,10 +2871,10 @@ public class ChooserActivity extends ResolverActivity {
 
             if (startType != lastStartType
                     || rowPosition == getContentPreviewRowCount() + getProfileRowCount()) {
-                row.setBackground(
+                row.setForeground(
                         getResources().getDrawable(R.drawable.chooser_row_layer_list, null));
             } else {
-                row.setBackground(null);
+                row.setForeground(null);
             }
 
             int columnCount = holder.getColumnCount();
