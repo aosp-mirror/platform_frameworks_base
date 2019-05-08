fc5e15c6220d53c77d2dc9b3643c56f717fbfffe
jschung <jschung@google.com>
2019-05-08 11:11:59 +0900

    Disable OPA edge flash animation
    Disable OPA edge flash animation by default.
    Disable OPA home button circle animation by default.
    Same changes are submitted on master branch already.
    Test: Verified changes on Pixel2 XL device
    
    Change-Id: Id5607e1c273cddb760adc5d01d41da0529a7add9
    
    Bug:131767021

--- packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
+++ packages/SystemUI/src/com/android/systemui/assist/AssistManager.java
@@ -167,7 +167,7 @@ public class AssistManager implements ConfigurationChangedReceiver {
     }
 
     protected boolean shouldShowOrb() {
-        return true;
+        return false;
     }
 
     public void startAssist(Bundle args) {
