a0966b5851ecbccc9c7aaa7f4040441e6f70426b
Matt Casey <mrcasey@google.com>
2019-05-15 18:49:37 -0400

    Remove assist status stuff from StatusBar.
    Assist views no longer live in StatusBar.
    
    Bug: 132065047
    Test: Manual test, no expected behavior change.
    Change-Id: I427eedf8c989b7af4f957a6e946919d57b83e827

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -3272,11 +3272,6 @@ public class StatusBar extends SystemUI implements DemoMode,
             }
             return true;
         }
-        if (mStatusBarWindowController.hasAssistActiveSession()) {
-            // Back pressed during an assist session, cancel it.
-            mAssistManager.hideAssist();
-            return true;
-        }
         if (mState != StatusBarState.KEYGUARD && mState != StatusBarState.SHADE_LOCKED) {
             if (mNotificationPanel.canPanelBeCollapsed()) {
                 animateCollapsePanels();
