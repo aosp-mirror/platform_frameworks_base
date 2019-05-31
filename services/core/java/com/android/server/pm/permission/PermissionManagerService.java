9c1e45245c583cbbe02eada97a3447ccfa7e6a75
Philip P. Moltmann <moltmann@google.com>
2019-05-31 08:48:14 -0700

    Reset SYSTEM_FIXED flags for hard restricted perms
    So that incorrectly SYSTEM_FIXED / denied permissions will be able to
    get regrated via the default permission grant policy
    
    This can be removed after all devices have been fixed
    
    Test: Got system into the state with SYSTEM_FIXED / denied permission
          and then silulated an OTA with this change
    Bug: 134069814
    
    Change-Id: I7bc986f0d3e69dd60a017f16867182163fd6eecb
    (cherry picked from commit 0891123822d6173686e9772c3265b72c27b2b901)

--- services/core/java/com/android/server/pm/permission/PermissionManagerService.java
+++ services/core/java/com/android/server/pm/permission/PermissionManagerService.java
@@ -1173,7 +1173,7 @@ public class PermissionManagerService {
                                     }
                                 }
 
-                                if (hardRestricted && !restrictionExempt
+                                if (hardRestricted
                                         && (flags & FLAG_PERMISSION_SYSTEM_FIXED) != 0) {
                                     // Applying a hard restriction implies revoking it. This might
                                     // lead to a system-fixed, revoked permission.
