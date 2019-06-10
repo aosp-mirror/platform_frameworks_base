6e88b5102670ac22e225a8f50e4e596b2aaf09f3
Matt Pietal <mpietal@google.com>
2019-06-10 10:20:15 -0400

    Sharesheet - Hide direct share area for low ram devices
    Make sure to force the item count to 0 to make sure the direct share
    area stays hidden.
    
    Bug: 134776740
    Test: Low ram device, manual
    Change-Id: I598752db5747b1c845495af7ae2f591488799352

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -2476,7 +2476,7 @@ public class ChooserActivity extends ResolverActivity {
         }
 
         public int getServiceTargetCount() {
-            if (isSendAction(getTargetIntent())) {
+            if (isSendAction(getTargetIntent()) && !ActivityManager.isLowRamDeviceStatic()) {
                 return Math.min(mServiceTargets.size(), MAX_SERVICE_TARGETS);
             }
 
