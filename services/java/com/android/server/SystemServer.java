6f5338dd8f90400fe67a97ed13f2479c6bd7237e
Roshan Pius <rpius@google.com>
2019-06-14 14:37:14 -0700

    Mainline wifi stack module
    a) Moved wifi service to a separate APK
    b) Use the IWifiStackConnector to load the wifi stack from
    SystemServer (similar to network stack).
    c) Create a new WifiStackClient interface for system server to interact
    with the wifi stack (under new services/wifi folder). Note: This not planned
    to be updated via wifi-sdk Apex.
    d) Add priv-app permissions for the new wifi stack APK.
    
    Bug: 113174748
    Test: Device boots up & connects to wifi networks, hotspot toggle, etc.
    Test: Will send for regression tests
    Change-Id: I54b3a11ed30668bad5a387370484b2bb0eabca5f
    Merged-In: I54b3a11ed30668bad5a387370484b2bb0eabca5f

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -40,8 +40,8 @@ import android.database.sqlite.SQLiteCompatibilityWalFlags;
 import android.database.sqlite.SQLiteGlobal;
 import android.hardware.display.DisplayManagerInternal;
 import android.net.ConnectivityModuleConnector;
-import android.net.Network;
 import android.net.NetworkStackClient;
+import android.net.wifi.WifiStackClient;
 import android.os.BaseBundle;
 import android.os.Binder;
 import android.os.Build;
@@ -1346,40 +1346,6 @@ public final class SystemServer {
             t.traceEnd();
 
             if (context.getPackageManager().hasSystemFeature(
-                    PackageManager.FEATURE_WIFI)) {
-                // Wifi Service must be started first for wifi-related services.
-                t.traceBegin("StartWifi");
-                mSystemServiceManager.startService(WIFI_SERVICE_CLASS);
-                t.traceEnd();
-                t.traceBegin("StartWifiScanning");
-                mSystemServiceManager.startService(
-                        "com.android.server.wifi.scanner.WifiScanningService");
-                t.traceEnd();
-            }
-
-            if (context.getPackageManager().hasSystemFeature(
-                    PackageManager.FEATURE_WIFI_RTT)) {
-                t.traceBegin("StartRttService");
-                mSystemServiceManager.startService(
-                        "com.android.server.wifi.rtt.RttService");
-                t.traceEnd();
-            }
-
-            if (context.getPackageManager().hasSystemFeature(
-                    PackageManager.FEATURE_WIFI_AWARE)) {
-                t.traceBegin("StartWifiAware");
-                mSystemServiceManager.startService(WIFI_AWARE_SERVICE_CLASS);
-                t.traceEnd();
-            }
-
-            if (context.getPackageManager().hasSystemFeature(
-                    PackageManager.FEATURE_WIFI_DIRECT)) {
-                t.traceBegin("StartWifiP2P");
-                mSystemServiceManager.startService(WIFI_P2P_SERVICE_CLASS);
-                t.traceEnd();
-            }
-
-            if (context.getPackageManager().hasSystemFeature(
                     PackageManager.FEATURE_LOWPAN)) {
                 t.traceBegin("StartLowpan");
                 mSystemServiceManager.startService(LOWPAN_SERVICE_CLASS);
@@ -2183,6 +2149,15 @@ public final class SystemServer {
                 reportWtf("starting Network Stack", e);
             }
             t.traceEnd();
+
+            t.traceBegin("StartWifiStack");
+            try {
+                WifiStackClient.getInstance().start();
+            } catch (Throwable e) {
+                reportWtf("starting Wifi Stack", e);
+            }
+            t.traceEnd();
+
 
             t.traceBegin("MakeLocationServiceReady");
             try {
