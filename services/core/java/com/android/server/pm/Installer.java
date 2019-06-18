2227b5ae102309ce85501b16e82cb51094dadcd8
Calin Juravle <calin@google.com>
2019-06-17 17:13:55 -0700

    Do not clean up ART profiles during OTAs
    Test: manual, upgrade & test for profiles
    Bug: 135299330
    
    (cherry picked from commit 86b6bbd4c593aedb3c5e27995727fb2cb55c604b)
    
    Merged-In: I39d3376fc845ec5f2b474ea6f39a3c534ed2ce69
    Change-Id: I29833113f7d0806733be669cfbc6fe2b206a982f

--- services/core/java/com/android/server/pm/Installer.java
+++ services/core/java/com/android/server/pm/Installer.java
@@ -85,6 +85,9 @@ public class Installer extends SystemService {
     public static final int FLAG_USE_QUOTA = IInstalld.FLAG_USE_QUOTA;
     public static final int FLAG_FORCE = IInstalld.FLAG_FORCE;
 
+    public static final int FLAG_CLEAR_APP_DATA_KEEP_ART_PROFILES =
+            IInstalld.FLAG_CLEAR_APP_DATA_KEEP_ART_PROFILES;
+
     private final boolean mIsolated;
 
     private volatile IInstalld mInstalld;
