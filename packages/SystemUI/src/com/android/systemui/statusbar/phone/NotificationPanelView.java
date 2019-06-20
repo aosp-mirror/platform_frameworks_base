bfa00499c4d9027cf4187d414b1a74f53ea73e3e
Selim Cinek <cinek@google.com>
2019-06-19 18:04:05 -0700

    Not bypassing when the user is expanding quick settings
    Previously we would also bypass when the user would
    look at QS.
    
    Fixes: 134094877
    Test: pull down QS, wait on bypass, nothing happens!
    Change-Id: I0b2ec98f3c361307845c16cc27866c6f3b3d4136
    (cherry picked from commit ae2702d1d6b7f7f0fb05aac68964bc6fb5927df7)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1384,6 +1384,7 @@ public class NotificationPanelView extends PanelView implements
             mStatusBar.setQsExpanded(expanded);
             mNotificationContainerParent.setQsExpanded(expanded);
             mPulseExpansionHandler.setQsExpanded(expanded);
+            mKeyguardBypassController.setQSExpanded(expanded);
         }
     }
 
