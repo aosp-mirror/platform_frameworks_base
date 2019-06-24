cbd5895e1680083c2cf11692f4be9d97ee8be75a
Fabian Kozynski <kozynski@google.com>
2019-06-24 15:00:20 -0400

    Ignore tests in PrivacyItemControllerTest
    Tests are flaking, ignore for now. Marked with STOPSHIP as they need to
    be fixed.
    
    Bug: 135008667
    Test: atest ignores these tests.
    Change-Id: I6b7864089d85aa6c7535dd02321e558be055f99f

--- packages/SystemUI/tests/src/com/android/systemui/privacy/PrivacyItemControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/privacy/PrivacyItemControllerTest.kt
@@ -44,6 +44,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentCaptor
@@ -69,6 +70,7 @@ import org.mockito.MockitoAnnotations
 @RunWith(AndroidTestingRunner::class)
 @SmallTest
 @RunWithLooper
+@Ignore
 class PrivacyItemControllerTest : SysuiTestCase() {
 
     companion object {
