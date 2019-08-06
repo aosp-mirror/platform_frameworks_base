fc8a2bb4c6737604b834aa6283f40d691fa0c81b
dianlujitao <dianlujitao@lineageos.org>
2019-08-06 23:59:53 +0800

    OutdoorModeController: Unconditionally enable auto mode on HWC2
     * On HWC2, setting "Display mode" to "Automatic" and disabling
       "Automatic outdoor mode" at the same time makes no difference with
       setting "Display mode" to "Off" alone. Obviously this is redundant
       and could confuse the user, so simply ignore the "Automatic outdoor
       mode" setting on HWC2 to reduce redundancy.
    
    Change-Id: I6858571a6b9ded3540fd3d936499878f57a7b3d4

--- services/core/java/com/android/server/custom/display/OutdoorModeController.java
+++ services/core/java/com/android/server/custom/display/OutdoorModeController.java
@@ -249,9 +249,9 @@ public class OutdoorModeController extends LiveDisplayFeature {
     }
 
     boolean isAutomaticOutdoorModeEnabled() {
-        return mUseOutdoorMode &&
+        return mUseOutdoorMode && (mNightDisplayAvailable ||
                 getBoolean(Settings.System.DISPLAY_AUTO_OUTDOOR_MODE,
-                           getDefaultAutoOutdoorMode());
+                           getDefaultAutoOutdoorMode()));
     }
 
     boolean getDefaultAutoOutdoorMode() {
