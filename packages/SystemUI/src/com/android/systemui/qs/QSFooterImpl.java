3de37d3a02641c75c251fdb9b734aa7fed6dbb58
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-10 22:48:14 -0300

    SystemUI: Remove build version from qs footer
    Change-Id: I7bb8f1422765d9e09bd50b43fa1e9849fe006344

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -101,14 +101,14 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
 
     private OnClickListener mExpandClickListener;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    /*private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             super.onChange(selfChange, uri);
             setBuildText();
         }
-    };
+    };*/
 
     @Inject
     public QSFooterImpl(@Named(VIEW_CONTEXT) Context context, AttributeSet attrs,
@@ -159,11 +159,11 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
                 oldBottom) -> updateAnimator(right - left));
         setImportantForAccessibility(IMPORTANT_FOR_ACCESSIBILITY_YES);
         updateEverything();
-        setBuildText();
+        //setBuildText();
     }
 
     private void setBuildText() {
-        TextView v = findViewById(R.id.build);
+        /*TextView v = findViewById(R.id.build);
         if (v == null) return;
         if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
             v.setText(mContext.getString(
@@ -173,7 +173,7 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
             v.setVisibility(View.VISIBLE);
         } else {
             v.setVisibility(View.GONE);
-        }
+        }*/
     }
 
     private void updateAnimator(int width) {
@@ -253,16 +253,16 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        mContext.getContentResolver().registerContentObserver(
+        /*mContext.getContentResolver().registerContentObserver(
                 Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                mDeveloperSettingsObserver, UserHandle.USER_ALL);*/
     }
 
     @Override
     @VisibleForTesting
     public void onDetachedFromWindow() {
         setListening(false);
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        //mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
         super.onDetachedFromWindow();
     }
 
