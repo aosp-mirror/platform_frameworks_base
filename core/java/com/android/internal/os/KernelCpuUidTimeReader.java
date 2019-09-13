8827420685593f5e117e807765a0cb15534a2a71
Han Wang <416810799@qq.com>
2019-09-13 14:20:10 +0200

    KernelCpuUidTimeReader: Decrease log level on non-eng builds
     * It is normal that kernel does not fully support this. Don't panic
       users on non-eng build.
    
    Change-Id: I5004d2b7d4031e924dcca7fd481a1b1788cb7dba

--- core/java/com/android/internal/os/KernelCpuUidTimeReader.java
+++ core/java/com/android/internal/os/KernelCpuUidTimeReader.java
@@ -20,6 +20,7 @@ import static com.android.internal.util.Preconditions.checkNotNull;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
+import android.os.Build;
 import android.os.StrictMode;
 import android.os.SystemClock;
 import android.util.IntArray;
@@ -431,7 +432,11 @@ public abstract class KernelCpuUidTimeReader<T> {
                 CharBuffer buf;
                 while ((buf = iter.nextLine()) != null) {
                     if (asLongs(buf, mBuffer) != mBuffer.length) {
-                        Slog.wtf(mTag, "Invalid line: " + buf.toString());
+                        if (Build.IS_ENG) {
+                            Slog.wtf(mTag, "Invalid line: " + buf.toString());
+                        } else {
+                            Slog.w(mTag, "Invalid line: " + buf.toString());
+                        }
                         continue;
                     }
                     final int uid = (int) mBuffer[0];
