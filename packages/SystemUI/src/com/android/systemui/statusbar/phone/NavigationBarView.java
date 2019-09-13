5269f4308111fc7f01566b926db668377652e364
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-13 18:16:31 -0300

    SystemUI: Fix escape text when screen pinned and gestural mode enabled
    Change-Id: Ia09541aa52e7420368c42234e3d84beeb81bee85

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -924,7 +924,7 @@ public class NavigationBarView extends FrameLayout implements
     }
 
     public void showPinningEscapeToast() {
-        mScreenPinningNotify.showEscapeToast(isRecentsButtonVisible());
+        mScreenPinningNotify.showEscapeToast(isRecentsButtonVisible(), isGesturalMode(mNavBarMode));
     }
 
     public boolean isVertical() {
