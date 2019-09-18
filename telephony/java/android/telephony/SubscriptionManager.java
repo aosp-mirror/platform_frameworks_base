d8104ecf5266b59ee84c54eadccc700dc2367090
Jack Yu <jackyu@google.com>
2019-09-18 09:31:28 -0700

    Added slot index version of getNetworkCountryIso
    Added this API to get network country ISO so in SIM
    absent case, we can get the information from the 2nd
    phone.
    
    Bug: 141128426
    Test: CTS
    Merged-In: Ib02e4d145cb39683bcb13d39715003095ad93eef
    Change-Id: Ib02e4d145cb39683bcb13d39715003095ad93eef
    (cherry picked from commit a5fedc5c029b1cfd49ddb68e9cb17c0f0d167118)

--- telephony/java/android/telephony/SubscriptionManager.java
+++ telephony/java/android/telephony/SubscriptionManager.java
@@ -885,6 +885,11 @@ public class SubscriptionManager {
      */
     public static final String EXTRA_SUBSCRIPTION_INDEX = "android.telephony.extra.SUBSCRIPTION_INDEX";
 
+    /**
+     * Integer extra to specify SIM slot index.
+     */
+    public static final String EXTRA_SLOT_INDEX = "android.telephony.extra.SLOT_INDEX";
+
     private final Context mContext;
     private volatile INetworkPolicyManager mNetworkPolicy;
 
@@ -2112,6 +2117,7 @@ public class SubscriptionManager {
         if (VDBG) logd("putPhoneIdAndSubIdExtra: phoneId=" + phoneId + " subId=" + subId);
         intent.putExtra(PhoneConstants.SUBSCRIPTION_KEY, subId);
         intent.putExtra(EXTRA_SUBSCRIPTION_INDEX, subId);
+        intent.putExtra(EXTRA_SLOT_INDEX, phoneId);
         intent.putExtra(PhoneConstants.PHONE_KEY, phoneId);
     }
 
