87f5f3b061d25601d69eaa5329dd30700442ab3b
ganeshi4u <ganeshvarmai4u@gmail.com>
2019-10-01 10:41:33 +0200

    TrafficSB: Pass the animate bool
    - this should fix the indicator acting wonky according to this change
      introduced here
    
    Refer:
    https://github.com/ArrowOS/android_frameworks_base/commit/620ea2c7550ea8817ae055dbf983bf5af525d6a4#diff-b4e0e9525d5c2d3dcd9c31861671714d
    
    Change-Id: I169f8586959a57e1b2b62a241800e107b5092902
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -390,7 +390,7 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     }
 
     @Override
-    public void setVisibleState(int state, boolean mIsEnabled) {
+    public void setVisibleState(int state, boolean animate) {
         if (state == mVisibleState) {
             return;
         }
