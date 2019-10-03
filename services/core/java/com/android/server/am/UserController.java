d34a8950a2db2abff27db991357f413beb4a8089
Felipe Leme <felipeal@google.com>
2019-10-03 10:32:04 -0700

    Fixed wrong traceEnd() call
    Test: echo 'in TH we trust'
    Bug: 133242016
    
    Change-Id: Id0043cb3fd6cdc819d4e443ccf7ee43cb0b60ccd

--- services/core/java/com/android/server/am/UserController.java
+++ services/core/java/com/android/server/am/UserController.java
@@ -1166,7 +1166,7 @@ class UserController implements Handler.Callback {
                     updateStartedUserArrayLU();
                 }
                 needStart = true;
-                t.traceBegin("updateStateStopping");
+                t.traceEnd();
             }
 
             if (uss.state == UserState.STATE_BOOTING) {
