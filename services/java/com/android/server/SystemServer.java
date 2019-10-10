ff948d8a2fb0e98ab6ec59b9ee1712e00c46dbaa
atrost <atrost@google.com>
2019-10-10 19:27:31 +0100

    Add a native aidl API.
    Introduce a platform_compat_native service that just calls the
    platform_compat service.
    The new service is needed as it needs a slightly different (more
    limited, no ApplicationInfo in cpp) aidl API, and a class can only
    extend one stub.
    
    Test: Call the service from dumpsys.cpp (http://aosp/1142055)
    Bug: 138275545
    Change-Id: Ic46cc34b4c1dd4ebc6bcc996fb3f8503607214ac
    Merged-In: Ic46cc34b4c1dd4ebc6bcc996fb3f8503607214ac

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -89,6 +89,7 @@ import com.android.server.broadcastradio.BroadcastRadioService;
 import com.android.server.camera.CameraServiceProxy;
 import com.android.server.clipboard.ClipboardService;
 import com.android.server.compat.PlatformCompat;
+import com.android.server.compat.PlatformCompatNative;
 import com.android.server.connectivity.IpConnectivityMetrics;
 import com.android.server.contentcapture.ContentCaptureManagerInternal;
 import com.android.server.coverage.CoverageService;
@@ -639,8 +640,10 @@ public final class SystemServer {
         // Platform compat service is used by ActivityManagerService, PackageManagerService, and
         // possibly others in the future. b/135010838.
         traceBeginAndSlog("PlatformCompat");
-        ServiceManager.addService(Context.PLATFORM_COMPAT_SERVICE,
-                new PlatformCompat(mSystemContext));
+        PlatformCompat platformCompat = new PlatformCompat(mSystemContext);
+        ServiceManager.addService(Context.PLATFORM_COMPAT_SERVICE, platformCompat);
+        ServiceManager.addService(Context.PLATFORM_COMPAT_NATIVE_SERVICE,
+                new PlatformCompatNative(platformCompat));
         traceEnd();
 
         // Wait for installd to finish starting up so that it has a chance to
