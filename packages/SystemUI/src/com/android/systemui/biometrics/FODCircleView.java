0f2778e2fa80b2a0aa032f689590127abd4520b6
Anushek Prasal <anushekprasal@gmail.com>
2019-10-18 02:29:24 +0530

    FOD: Enable burn-in protection only while dreaming

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -119,16 +119,7 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
             super.onDreamingStateChanged(dreaming);
             mIsDreaming = dreaming;
             mIsInsideCircle = false;
-            if (dreaming) {
-                mBurnInProtectionTimer = new Timer();
-                mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
-            } else if (mBurnInProtectionTimer != null) {
-                mBurnInProtectionTimer.cancel();
-            }
-            if (mIsViewAdded) {
-                resetPosition();
-                invalidate();
-            }
+            setIcon();
         }
 
         @Override
@@ -139,6 +130,7 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
                 mIsDreaming = false;
 	        }
             mIsInsideCircle = false;
+            setIcon();
         }
 
         @Override
@@ -475,7 +467,7 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
                 throw new IllegalArgumentException("Unknown rotation: " + rotation);
         }
 
-        if (mIsDreaming) {
+        if (mIsDreaming && !mIsPulsing) {
             mParams.x += mDreamingOffsetX;
             mParams.y += mDreamingOffsetY;
         }
@@ -514,6 +506,20 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
             mWindowManager.updateViewLayout(this, mParams);
         } catch (IllegalArgumentException e) {
             // do nothing
+        }
+    }
+
+    private void setIcon() {
+        if (mIsDreaming) {
+            mBurnInProtectionTimer = new Timer();
+            mBurnInProtectionTimer.schedule(new BurnInProtectionTask(), 0, 60 * 1000);
+        } else if (mBurnInProtectionTimer != null) {
+            mBurnInProtectionTimer.cancel();
+        }
+
+        if (mIsViewAdded) {
+            resetPosition();
+            invalidate();
         }
     }
 
