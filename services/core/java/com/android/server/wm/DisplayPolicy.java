367b2c9cf00e0e3e547ac64f0a3f484204a9110f
Bernardo Rufino <brufino@google.com>
2019-10-22 11:53:42 +0100

    RESTRICT AUTOMERGE Make toasts non-clickable
    Since enforcement was only on client-side, in Toast class, an app could
    use reflection (or other means) to make the Toast clickable. This is a
    security vulnerability since it allows tapjacking, that is, intercept touch
    events and do stuff like steal PINs and passwords.
    
    This CL brings the enforcement to the system by applying flag
    FLAG_NOT_TOUCHABLE.
    
    Test: atest CtsWindowManagerDeviceTestCases:ToastTest
    Test: Construct app that uses reflection to remove flag FLAG_NOT_TOUCHABLE and
          log click events. Then:
          1) Observe click events are logged without this CL.
          2) Observer click events are not logged with this CL.
    Bug: 128674520
    
    Change-Id: Ic36585bc4f186e0224f5b687c49c0b3d9266838c
    (cherry picked from commit b81f269ae2afb446b9d4a909fc2bcf038af00c41)

--- services/core/java/com/android/server/wm/DisplayPolicy.java
+++ services/core/java/com/android/server/wm/DisplayPolicy.java
@@ -857,6 +857,8 @@ public class DisplayPolicy {
                 if (canToastShowWhenLocked(callingPid)) {
                     attrs.flags |= WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
                 }
+                // Toasts can't be clickable
+                attrs.flags |= WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE;
                 break;
         }
 
