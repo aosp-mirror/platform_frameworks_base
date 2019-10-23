354c9caacd76d23708bfc9cdf962041e19d22964
maxwen <max.weninger@gmail.com>
2019-10-24 00:16:43 +0200

    Keyguard: Add binary custom clock face
    Change-Id: I92589dd5bd0d284645aaffe7ff40b85a5bc42384

---
+++ packages/SystemUI/src/com/android/keyguard/clock/BinaryClock.java
@@ -0,0 +1,202 @@
+/*
+ *  Copyright (C) 2018-2019 The OmniROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.keyguard.clock;
+
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.text.format.DateFormat;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.android.keyguard.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class BinaryClock extends View {
+    private static final String TAG = "BinaryClock";
+    private final Calendar mCalendar = Calendar.getInstance(TimeZone.getDefault());
+    private int mMinutes;
+    private int mHour;
+    private Paint mDotPaint;
+    private Paint mEmptyDotPaint;
+    private Paint mAmbientDotPaint;
+    private Paint mAmbienEmptyDotPaint;
+    private boolean mIsAmbientDisplay;
+    private int mDotSize;
+    private int[][] mDots = new int[4][4];
+    private TimeZone mTimeZone;
+    private String mDescFormat;
+
+    public BinaryClock(Context context) {
+        this(context, null);
+    }
+
+    public BinaryClock(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BinaryClock(Context context, AttributeSet attrs,
+                       int defStyle) {
+        super(context, attrs, defStyle);
+        Resources r = context.getResources();
+
+        mDotPaint = new Paint();
+        mDotPaint.setAntiAlias(true);
+        mDotPaint.setStyle(Paint.Style.FILL);
+        mDotPaint.setColor(r.getColor(R.color.binary_clock_dot_color));
+
+        mEmptyDotPaint = new Paint();
+        mEmptyDotPaint.setAntiAlias(true);
+        mEmptyDotPaint.setStyle(Paint.Style.STROKE);
+        mEmptyDotPaint.setColor(r.getColor(R.color.binary_clock_empty_dot_color));
+
+        mAmbientDotPaint = new Paint();
+        mAmbientDotPaint.setAntiAlias(true);
+        mAmbientDotPaint.setStyle(Paint.Style.FILL);
+        mAmbientDotPaint.setColor(r.getColor(R.color.binary_clock_ambient_dot_color));
+
+        mAmbienEmptyDotPaint = new Paint();
+        mAmbienEmptyDotPaint.setAntiAlias(true);
+        mAmbienEmptyDotPaint.setStyle(Paint.Style.STROKE);
+        mAmbienEmptyDotPaint.setColor(r.getColor(R.color.binary_clock_ambient_empty_dot_color));
+
+        mDescFormat = ((SimpleDateFormat) DateFormat.getTimeFormat(context)).toLocalizedPattern();
+
+        onDensityOrFontScaleChanged();
+    }
+
+    public void onDensityOrFontScaleChanged() {
+        Resources r = getContext().getResources();
+        mDotSize = r.getDimensionPixelSize(R.dimen.binary_clock_dot_size);
+        mDotPaint.setStrokeWidth(r.getDimensionPixelSize(R.dimen.binary_clock_stroke_width));
+        mEmptyDotPaint.setStrokeWidth(r.getDimensionPixelSize(R.dimen.binary_clock_stroke_width));
+        mAmbientDotPaint.setStrokeWidth(r.getDimensionPixelSize(R.dimen.binary_clock_stroke_width));
+        mAmbienEmptyDotPaint.setStrokeWidth(r.getDimensionPixelSize(R.dimen.binary_clock_stroke_width));
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        mCalendar.setTimeZone(mTimeZone != null ? mTimeZone : TimeZone.getDefault());
+        onTimeChanged();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int availableWidth = getWidth();
+        int cellWidth = availableWidth / 4;
+        int availableHeight = getHeight();
+        int cellHeight = availableHeight / 4;
+
+        int yLine = cellHeight / 2;
+
+        for (int y = 3; y >= 0 ; y--) {
+            int xLine = cellWidth / 2;
+            for (int x = 0; x < 4; x++) {
+                if (y >= 2 && x == 0) {
+                    xLine += cellWidth;
+                    continue;
+                }
+                if (mDots[x][y] == 1) {
+                    canvas.drawCircle(xLine, yLine, mDotSize, mIsAmbientDisplay ? mAmbientDotPaint : mDotPaint);
+                } else {
+                    canvas.drawCircle(xLine, yLine, mDotSize, mIsAmbientDisplay ? mAmbienEmptyDotPaint : mEmptyDotPaint);
+                }
+                xLine += cellWidth;
+            }
+            yLine += cellHeight;
+        }
+    }
+
+
+    private void calculateDotMatrix() {
+        int hour0 = (int) (mHour >= 10 ? mHour / 10 : 0);
+        int hour1 = (int) (mHour - hour0 * 10);
+        int minute0 = (int) (mMinutes >= 10 ? mMinutes / 10 : 0);
+        int minute1 = (int) (mMinutes - minute0 * 10);
+
+        mDots = new int[4][4];
+        if (hour0 != 0) {
+            String hour0Bin = Integer.toBinaryString(hour0);
+            for (int i = 0; i < hour0Bin.length(); i++) {
+                mDots[0][hour0Bin.length() - 1 - i] = hour0Bin.charAt(i) == '1' ? 1 : 0;
+            }
+        }
+        if (hour1 != 0) {
+            String hour1Bin = Integer.toBinaryString(hour1);
+            for (int i = 0; i < hour1Bin.length(); i++) {
+                mDots[1][hour1Bin.length() - 1 - i] = hour1Bin.charAt(i) == '1' ? 1 : 0;
+            }
+        }
+        if (minute0 != 0) {
+            String minute0Bin = Integer.toBinaryString(minute0);
+            for (int i = 0; i < minute0Bin.length(); i++) {
+                mDots[2][minute0Bin.length() - 1 - i] = minute0Bin.charAt(i) == '1' ? 1 : 0;
+            }
+        }
+        if (minute1 != 0) {
+            String minute1Bin = Integer.toBinaryString(minute1);
+            for (int i = 0; i < minute1Bin.length(); i++) {
+                mDots[3][minute1Bin.length() - 1 - i] = minute1Bin.charAt(i) == '1' ? 1 : 0;
+            }
+        }
+    }
+
+    public void onTimeChanged() {
+        mCalendar.setTimeInMillis(System.currentTimeMillis());
+        final boolean is24 = DateFormat.is24HourFormat(getContext(), ActivityManager.getCurrentUser());
+        mHour = mCalendar.get(is24 ? Calendar.HOUR_OF_DAY : Calendar.HOUR);
+        mMinutes = mCalendar.get(Calendar.MINUTE);
+        setContentDescription(DateFormat.format(mDescFormat, mCalendar));
+
+        calculateDotMatrix();
+    }
+
+    public void onTimeZoneChanged(TimeZone timeZone) {
+        mTimeZone = timeZone;
+        mCalendar.setTimeZone(timeZone);
+    }
+
+    public void setDark(boolean dark) {
+        if (mIsAmbientDisplay != dark) {
+            mIsAmbientDisplay = dark;
+            invalidate();
+        }
+    }
+
+    public void setTintColor(int color) {
+        mDotPaint.setColor(color);
+        mEmptyDotPaint.setColor(color);
+        invalidate();
+    }
+
+    public void refreshTime() {
+        onTimeChanged();
+        invalidate();
+    }
+}
---
+++ packages/SystemUI/src/com/android/keyguard/clock/BinaryClockController.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.keyguard.clock;
+
+import android.app.WallpaperManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Paint.Style;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextClock;
+
+import com.android.internal.colorextraction.ColorExtractor;
+import com.android.keyguard.R;
+import com.android.systemui.colorextraction.SysuiColorExtractor;
+import com.android.systemui.plugins.ClockPlugin;
+
+import java.util.TimeZone;
+
+/**
+ * Controller for binary clock that can appear on lock screen and AOD.
+ */
+public class BinaryClockController implements ClockPlugin {
+
+    /**
+     * Resources used to get title and thumbnail.
+     */
+    private final Resources mResources;
+
+    /**
+     * LayoutInflater used to inflate custom clock views.
+     */
+    private final LayoutInflater mLayoutInflater;
+
+    /**
+     * Extracts accent color from wallpaper.
+     */
+    private final SysuiColorExtractor mColorExtractor;
+
+    /**
+     * Computes preferred position of clock.
+     */
+    private final SmallClockPosition mClockPosition;
+
+    /**
+     * Renders preview from clock view.
+     */
+    private final ViewPreviewer mRenderer = new ViewPreviewer();
+
+    /**
+     * Custom clock shown on AOD screen and behind stack scroller on lock.
+     */
+    private BinaryClock mBinaryClock;
+    private ClockLayout mBigClockView;
+
+    /**
+     * Small clock shown on lock screen above stack scroller.
+     */
+    private View mView;
+    private TextClock mLockClock;
+
+    /**
+     * Create a BinaryClockController instance.
+     *
+     * @param res Resources contains title and thumbnail.
+     * @param inflater Inflater used to inflate custom clock views.
+     * @param colorExtractor Extracts accent color from wallpaper.
+     */
+    public BinaryClockController(Resources res, LayoutInflater inflater,
+            SysuiColorExtractor colorExtractor) {
+        mResources = res;
+        mLayoutInflater = inflater;
+        mColorExtractor = colorExtractor;
+        mClockPosition = new SmallClockPosition(res);
+    }
+
+    private void createViews() {
+        mBigClockView = (ClockLayout) mLayoutInflater.inflate(R.layout.binary_clock, null);
+        mBinaryClock = mBigClockView.findViewById(R.id.analog_clock);
+
+        mView = mLayoutInflater.inflate(R.layout.digital_clock, null);
+        mLockClock = mView.findViewById(R.id.lock_screen_clock);
+    }
+
+    @Override
+    public void onDestroyView() {
+        mBigClockView = null;
+        mBinaryClock = null;
+        mView = null;
+        mLockClock = null;
+    }
+
+    @Override
+    public String getName() {
+        return "binary";
+    }
+
+    @Override
+    public String getTitle() {
+        return mResources.getString(R.string.clock_title_binary);
+    }
+
+    @Override
+    public Bitmap getThumbnail() {
+        return BitmapFactory.decodeResource(mResources, R.drawable.binary_thumbnail);
+    }
+
+    @Override
+    public Bitmap getPreview(int width, int height) {
+        // Use the big clock view for the preview
+        View view = getBigClockView();
+
+        // Initialize state of plugin before generating preview.
+        setDarkAmount(1f);
+        setTextColor(Color.WHITE);
+        ColorExtractor.GradientColors colors = mColorExtractor.getColors(
+                WallpaperManager.FLAG_LOCK);
+        setColorPalette(colors.supportsDarkText(), colors.getColorPalette());
+        onTimeTick();
+
+        return mRenderer.createPreview(view, width, height);
+    }
+
+    @Override
+    public View getView() {
+        if (mView == null) {
+            createViews();
+        }
+        return mView;
+    }
+
+    @Override
+    public View getBigClockView() {
+        if (mBigClockView == null) {
+            createViews();
+        }
+        return mBigClockView;
+    }
+
+    @Override
+    public int getPreferredY(int totalHeight) {
+        return mClockPosition.getPreferredY();
+    }
+
+    @Override
+    public void setStyle(Style style) {}
+
+    @Override
+    public void setTextColor(int color) {
+        mBinaryClock.setTintColor(color);
+    }
+
+    @Override
+    public void setColorPalette(boolean supportsDarkText, int[] colorPalette) {}
+
+    @Override
+    public void onTimeTick() {
+        mBinaryClock.onTimeChanged();
+        mBigClockView.onTimeChanged();
+        mLockClock.refresh();
+    }
+
+    @Override
+    public void setDarkAmount(float darkAmount) {
+        mClockPosition.setDarkAmount(darkAmount);
+        mBigClockView.setDarkAmount(darkAmount);
+        boolean dark = darkAmount == 1;
+        mBinaryClock.setDark(dark);
+    }
+
+    @Override
+    public void onTimeZoneChanged(TimeZone timeZone) {
+        mBinaryClock.onTimeZoneChanged(timeZone);
+    }
+
+    @Override
+    public boolean shouldShowStatusArea() {
+        return true;
+    }
+}
