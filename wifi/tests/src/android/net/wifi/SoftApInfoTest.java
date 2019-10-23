3fd4b647f78f9ef79dcc9143b98e7a7ce1d428b3
lesl <lesl@google.com>
2019-10-23 10:47:34 +0800

    wifi: Add SoftApInfo Callback support
    Bug: 142752869
    
    Test: unit test, atest frameworks/base/wifi/tests/
    Test: Manual test - checked by log.
    Change-Id: I9fe7a18654721f98026680711685c06add3da264

---
+++ wifi/tests/src/android/net/wifi/SoftApInfoTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.net.wifi;
+
+import android.os.Parcel;
+
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.filters.SmallTest;
+
+import org.junit.Test;
+
+/**
+ * Unit tests for {@link android.net.wifi.SoftApInfo}.
+ */
+@SmallTest
+public class SoftApInfoTest {
+
+    /**
+     * Verifies copy constructor.
+     */
+    @Test
+    public void testCopyOperator() throws Exception {
+        SoftApInfo info = new SoftApInfo();
+        info.setFrequency(2412);
+        info.setBandwidth(SoftApInfo.CHANNEL_WIDTH_20MHZ);
+
+        SoftApInfo copiedInfo = new SoftApInfo(info);
+
+        assertEquals(info, copiedInfo);
+        assertEquals(info.hashCode(), copiedInfo.hashCode());
+    }
+
+    /**
+     * Verifies parcel serialization/deserialization.
+     */
+    @Test
+    public void testParcelOperation() throws Exception {
+        SoftApInfo info = new SoftApInfo();
+        info.setFrequency(2412);
+        info.setBandwidth(SoftApInfo.CHANNEL_WIDTH_20MHZ);
+
+        Parcel parcelW = Parcel.obtain();
+        info.writeToParcel(parcelW, 0);
+        byte[] bytes = parcelW.marshall();
+        parcelW.recycle();
+
+        Parcel parcelR = Parcel.obtain();
+        parcelR.unmarshall(bytes, 0, bytes.length);
+        parcelR.setDataPosition(0);
+        SoftApInfo fromParcel = SoftApInfo.CREATOR.createFromParcel(parcelR);
+
+        assertEquals(info, fromParcel);
+        assertEquals(info.hashCode(), fromParcel.hashCode());
+    }
+
+}
