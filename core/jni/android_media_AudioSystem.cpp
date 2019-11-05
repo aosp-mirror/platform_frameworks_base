4a5db4cd5072c88ddb40551935909ca95b053bd9
Eric Laurent <elaurent@google.com>
2019-11-04 17:22:02 -0800

    AudioService: communicate audio HAL process pids to native audioserver
    Retrieve audio HAL processes pids from service manager and send them
    to AudioFlinger native service. This is needed to dump audio HAL process tombstones
    and because audioserver process does not have the SELinux permission to
    list services.
    
    Bug: 141528385
    Test: Force watchdog and verify tombstone creation
    Change-Id: I3e35b8040779e041b8d8c2e72d1e650d6f8fe818

--- core/jni/android_media_AudioSystem.cpp
+++ core/jni/android_media_AudioSystem.cpp
@@ -2250,6 +2250,20 @@ android_media_AudioSystem_setRttEnabled(JNIEnv *env, jobject thiz, jboolean enab
     return (jint) check_AudioSystem_Command(AudioSystem::setRttEnabled(enabled));
 }
 
+static jint
+android_media_AudioSystem_setAudioHalPids(JNIEnv *env, jobject clazz, jintArray jPids)
+{
+    if (jPids == NULL) {
+        return (jint)AUDIO_JAVA_BAD_VALUE;
+    }
+    pid_t *nPidsArray = (pid_t *)env->GetIntArrayElements(jPids, NULL);
+    std::vector<pid_t> nPids(nPidsArray, nPidsArray + env->GetArrayLength(jPids));
+    status_t status = AudioSystem::setAudioHalPids(nPids);
+    env->ReleaseIntArrayElements(jPids, nPidsArray, 0);
+    jint jStatus = nativeToJavaStatus(status);
+    return jStatus;
+}
+
 // ----------------------------------------------------------------------------
 
 static const JNINativeMethod gMethods[] = {
@@ -2328,6 +2342,7 @@ static const JNINativeMethod gMethods[] = {
                     (void*)android_media_AudioSystem_getHwOffloadEncodingFormatsSupportedForA2DP},
     {"setAllowedCapturePolicy", "(II)I", (void *)android_media_AudioSystem_setAllowedCapturePolicy},
     {"setRttEnabled",       "(Z)I",     (void *)android_media_AudioSystem_setRttEnabled},
+    {"setAudioHalPids",  "([I)I", (void *)android_media_AudioSystem_setAudioHalPids},
 };
 
 static const JNINativeMethod gEventHandlerMethods[] = {
