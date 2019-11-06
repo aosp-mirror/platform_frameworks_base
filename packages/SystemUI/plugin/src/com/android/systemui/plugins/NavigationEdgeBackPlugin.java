202970155d2b99b7524353fcba6e0b1a72b6a214
Andy Wickham <awickham@google.com>
2019-11-06 14:56:26 -0800

    Adds NavigationEdgeBackPlugin to iterate on different Back implementations.
    NavigationBarEdgePanel now implements the new plugin. As part of that,
    some of the UI-specific logic from EdgeBackGestureHandler has been
    moved to NavigationBarEdgePanel (e.g. Dark region detector and logic
    around the arrow position).
    
    Test: Built and manually tested existing back behavior.
    Fixes: 143907351
    Change-Id: I0e01581d6b69e6a8a03a4338be83ed5ff8b2da65

---
+++ packages/SystemUI/plugin/src/com/android/systemui/plugins/NavigationEdgeBackPlugin.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.plugins;
+
+import android.graphics.Point;
+import android.view.MotionEvent;
+import android.view.WindowManager;
+
+import com.android.systemui.plugins.annotations.ProvidesInterface;
+
+/** Plugin to handle navigation edge gestures for Back. */
+@ProvidesInterface(
+        action = NavigationEdgeBackPlugin.ACTION,
+        version = NavigationEdgeBackPlugin.VERSION)
+public interface NavigationEdgeBackPlugin extends Plugin {
+    String ACTION = "com.android.systemui.action.PLUGIN_NAVIGATION_EDGE_BACK_ACTION";
+    int VERSION = 1;
+
+
+    /** Specifies if the UI should be rendered on the left side of the screen. */
+    void setIsLeftPanel(boolean isLeftPanel);
+
+    /** Sets the insets for the gesture handling area. */
+    void setInsets(int leftInset, int rightInset);
+
+    /** Sets the display size. */
+    void setDisplaySize(Point displaySize);
+
+    /** Sets the callback that should be invoked when a Back gesture is detected. */
+    void setBackCallback(BackCallback callback);
+
+    /** Sets the base LayoutParams for the UI. */
+    void setLayoutParams(WindowManager.LayoutParams layoutParams);
+
+    /** Updates the UI based on the motion events passed in device coordinates. */
+    void onMotionEvent(MotionEvent motionEvent);
+
+    /** Callback to let the system react to the detected back gestures. */
+    interface BackCallback {
+        /** Indicates that a Back gesture was recognized and the system should go back. */
+        void triggerBack();
+
+        /** Indicates that the gesture was cancelled and the system should not go back. */
+        void cancelBack();
+    }
+}
