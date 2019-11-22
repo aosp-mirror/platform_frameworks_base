d01bb41b93b24de8df352a6b691a466f6fe6a8fa
Kenny Root <kroot@google.com>
2019-11-22 09:34:03 -0800

    Convert LockSettingsService tests to JUnit4
    In preparation for adding more tests to LockSettingsService, convert the
    existing JUnit3 tests to JUnit4 like the tests in the
    recoverablekeystore subdirectory.
    
    (cherry-picked from commit e7178a2358a457922ecb35c5f418903a3de363ab)
    
    Test: atest FrameworksServicesTests:CachedSyntheticPasswordTests \
                FrameworksServicesTests:LockSettingsServiceTests \
                FrameworksServicesTests:LockSettingsShellCommandTest \
                FrameworksServicesTests:LockSettingsStorageTests \
                FrameworksServicesTests:LockscreenFrpTest \
                FrameworksServicesTests:PasswordSlotManagerTests \
                FrameworksServicesTests:SP800DeriveTests
    Test: atest FrameworksServicesTests:SyntheticPasswordTests \
                FrameworksServicesTests:WeaverBasedSyntheticPasswordTests
    Bug: 145144304
    Merged-In: Iaf280718d4c28966b60c76dda90a4674a24f26c6
    Change-Id: Iaf280718d4c28966b60c76dda90a4674a24f26c6

--- services/tests/servicestests/src/com/android/server/locksettings/WeaverBasedSyntheticPasswordTests.java
+++ services/tests/servicestests/src/com/android/server/locksettings/WeaverBasedSyntheticPasswordTests.java
@@ -3,15 +3,18 @@ package com.android.server.locksettings;
 import android.platform.test.annotations.Presubmit;
 
 import androidx.test.filters.SmallTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
 
 @SmallTest
 @Presubmit
+@RunWith(AndroidJUnit4.class)
 public class WeaverBasedSyntheticPasswordTests extends SyntheticPasswordTests {
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void enableWeaver() throws Exception {
         mSpManager.enableWeaver();
     }
-
 }
