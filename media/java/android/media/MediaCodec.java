bionic-linux/android_frameworks_base

MediaCodec: Release lock before calling onFrameRendered()
Test: atest CtsMediaTestCases -- \
--module-arg CtsMediaTestCases:size:small

Bug: 143672592
Change-Id: I4fb55eb8a90913544a793097c977fee539e09776

media/java/android/media/MediaCodec.java
@@ -1702,20 +1702,23 @@ final public class MediaCodec {
                     break;
                 }
                 case EVENT_FRAME_RENDERED:
+                    EventHandler onFrameRenderedListener = null;
                     synchronized (mListenerLock) {
+                        onFrameRenderedListener = mOnFrameRenderedListener;
+                    }
+                    if (onFrameRenderedListener != null) {
                         Map<String, Object> map = (Map<String, Object>)msg.obj;
                         for (int i = 0; ; ++i) {
                             Object mediaTimeUs = map.get(i + "-media-time-us");
                             Object systemNano = map.get(i + "-system-nano");
-                            if (mediaTimeUs == null || systemNano == null
-                                    || mOnFrameRenderedListener == null) {
+                            if (mediaTimeUs == null || systemNano == null) {
                                 break;
                             }
-                            mOnFrameRenderedListener.onFrameRendered(
+                            onFrameRenderedListener.onFrameRendered(
                                     mCodec, (long)mediaTimeUs, (long)systemNano);
                         }
-                        break;
                     }
+                    break;
                 default:
                 {
                     break;

aosp-mirror/platform_frameworks_base

media/java/android/media/MediaCodec.java
@@ -1702,20 +1702,23 @@ final public class MediaCodec {
                     break;
                 }
                 case EVENT_FRAME_RENDERED:
+                    OnFrameRenderedListener onFrameRenderedListener = null;
                     synchronized (mListenerLock) {
+                        onFrameRenderedListener = mOnFrameRenderedListener;
+                    }
+                    if (onFrameRenderedListener != null) {
                         Map<String, Object> map = (Map<String, Object>)msg.obj;
                         for (int i = 0; ; ++i) {
                             Object mediaTimeUs = map.get(i + "-media-time-us");
                             Object systemNano = map.get(i + "-system-nano");
-                            if (mediaTimeUs == null || systemNano == null
-                                    || mOnFrameRenderedListener == null) {
+                            if (mediaTimeUs == null || systemNano == null) {
                                 break;
                             }
-                            mOnFrameRenderedListener.onFrameRendered(
+                            onFrameRenderedListener.onFrameRendered(
                                     mCodec, (long)mediaTimeUs, (long)systemNano);
                         }
-                        break;
                     }
+                    break;
                 default:
                 {
                     break;