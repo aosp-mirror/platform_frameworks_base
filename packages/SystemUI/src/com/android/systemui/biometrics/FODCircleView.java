0b5de2601abfc1c37fd902e300e5cf2198542851
Marko Man <darkobas@gmail.com>
2019-11-24 21:55:33 +0100

    SystemUI: Fod: Aod content should move only in Y
    Change-Id: I4830eea4740523724e5eba24e22bb4fb6b16591f

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -71,7 +71,6 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
 
     private IFingerprintInscreen mFingerprintInscreenDaemon;
 
-    private int mDreamingOffsetX;
     private int mDreamingOffsetY;
     private int mNavigationBarSize;
 
@@ -486,7 +485,6 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
         }
 
         if (mIsDreaming) {
-            mParams.x += mDreamingOffsetX;
             mParams.y += mDreamingOffsetY;
         }
 
@@ -533,16 +531,8 @@ public class FODCircleView extends ImageView implements OnTouchListener, Configu
             // It is fine to modify the variables here because
             // no other thread will be modifying it
             long now = System.currentTimeMillis() / 1000 / 60;
-            mDreamingOffsetX = (int) (now % (mDreamingMaxOffset * 4));
-            if (mDreamingOffsetX > mDreamingMaxOffset * 2) {
-                mDreamingOffsetX = mDreamingMaxOffset * 4 - mDreamingOffsetX;
-            }
             // Let y to be not synchronized with x, so that we get maximum movement
             mDreamingOffsetY = (int) ((now + mDreamingMaxOffset / 3) % (mDreamingMaxOffset * 2));
-            if (mDreamingOffsetY > mDreamingMaxOffset * 2) {
-                mDreamingOffsetY = mDreamingMaxOffset * 4 - mDreamingOffsetY;
-            }
-            mDreamingOffsetX -= mDreamingMaxOffset;
             mDreamingOffsetY -= mDreamingMaxOffset;
             if (mIsViewAdded) {
                 mHandler.post(() -> resetPosition());
