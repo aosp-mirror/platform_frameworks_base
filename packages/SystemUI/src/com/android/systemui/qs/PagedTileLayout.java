b41b629db7ced6afad38ca7d7652391b22bd37e9
maxwen <max.weninger@gmail.com>
2019-11-27 23:46:26 +0100

    SystemUI: fix qs page num calc algo
    Change-Id: I27fabcd76b00d8549fe919afda864d64379f4882

--- packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
+++ packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -244,12 +244,13 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
 
     private void emptyAndInflateOrRemovePages() {
         final int nTiles = mTiles.size();
-        // We should always have at least one page, even if it's empty.
-        int numPages = Math.max(nTiles / mPages.get(0).maxTiles(), 1);
-
-        // Add one more not full page if needed
-        if (nTiles > numPages * mPages.get(0).maxTiles()) {
-            numPages++;
+        int numPages = 0;
+        if (nTiles <= mPages.get(0).maxTiles()) {
+            numPages = 1;
+        } else {
+            numPages = nTiles / mPages.get(0).maxTiles();
+            // Add one more not full page if needed
+            numPages += numPages * mPages.get(0).maxTiles() > nTiles ? 0 : 1;
         }
 
         final int NP = mPages.size();
@@ -500,7 +501,8 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
         for (int i = 0; i < mPages.size(); i++) {
             mPages.get(i).updateSettings();
         }
-        distributeTiles();
+        mDistributeTiles = true;
+        requestLayout();
     }
 
     @Override
--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -123,7 +123,6 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                 R.layout.qs_paged_tile_layout, this, false);
         mTileLayout.setListening(mListening);
         addView((View) mTileLayout);
-        updateSettings();
 
         mQsTileRevealController = new QSTileRevealController(mContext, this,
                 (PagedTileLayout) mTileLayout);
@@ -138,6 +137,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
         mBrightnessController = new BrightnessController(getContext(),
                 findViewById(R.id.brightness_slider));
         mDumpController = dumpController;
+
+        updateSettings();
     }
 
     protected void addDivider() {
