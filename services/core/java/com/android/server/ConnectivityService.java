408db3293d50e4198a504e22358209848aa329d1
Chalard Jean <jchalard@google.com>
2019-12-03 23:37:28 +0900

    [NS A42] Move writing to the reassignment to the computation
    ...instead of the side effect loop. This is a no-op refactoring.
    
    Test: ConnectivityServiceTest
    Change-Id: I8308d55eaff080efb22a7a43142492545e626cf5

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -6591,9 +6591,13 @@ public class ConnectivityService extends IConnectivityManager.Stub
                 }
                 if (currentNetwork == null || currentNetwork.getCurrentScore() < score) {
                     reassignedRequests.put(nri, newNetwork);
+                    changes.addRequestReassignment(new NetworkReassignment.RequestReassignment(
+                            nri, currentNetwork, newNetwork));
                 }
             } else if (newNetwork == currentNetwork) {
                 reassignedRequests.put(nri, null);
+                changes.addRequestReassignment(new NetworkReassignment.RequestReassignment(
+                        nri, currentNetwork, null));
             }
         }
         return reassignedRequests;
@@ -6634,8 +6638,6 @@ public class ConnectivityService extends IConnectivityManager.Stub
             final NetworkRequestInfo nri = entry.getKey();
             final NetworkAgentInfo previousSatisfier = nri.mSatisfier;
             final NetworkAgentInfo newSatisfier = entry.getValue();
-            changes.addRequestReassignment(new NetworkReassignment.RequestReassignment(
-                    nri, previousSatisfier, newSatisfier));
             updateSatisfiersForRematchRequest(nri, previousSatisfier, newSatisfier, now);
         }
     }
