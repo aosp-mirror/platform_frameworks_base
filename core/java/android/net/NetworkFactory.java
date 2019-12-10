33f921769531968a3ba9bc73fb2410f95868cb8d
Artur Satayev <satayev@google.com>
2019-12-10 17:47:52 +0000

    Use new UnsupportedAppUsage annotation.
    Existing annotations in libcore/ and frameworks/ will deleted after the migration. This also means that any java library that compiles @UnsupportedAppUsage requires a direct dependency on "unsupportedappusage" java_library.
    
    Bug: 145132366
    Test: m && diff unsupportedappusage_index.csv
    Change-Id: I0c336de56bc4a041dc97ff9b7927f62f0b44b457
    Merged-In: I0c336de56bc4a041dc97ff9b7927f62f0b44b457

--- core/java/android/net/NetworkFactory.java
+++ core/java/android/net/NetworkFactory.java
@@ -16,7 +16,7 @@
 
 package android.net;
 
-import android.annotation.UnsupportedAppUsage;
+import android.compat.annotation.UnsupportedAppUsage;
 import android.content.Context;
 import android.os.Build;
 import android.os.Handler;
