abf9e43cb0618a499204bdc9d00341d6174a5c8b
yinchengzhao <yinchengzhao@google.com>
2019-12-13 13:52:31 -0800

    Fix for Telephony Manager API javadoc inconsistencies
    Bug: 144455918
    Test: atest TelephonyManagerTest
    Change-Id: Iac372f04afe210ac7d3edc6fc880ee6a7e614945

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -6692,7 +6692,8 @@ public class TelephonyManager {
      * If the list is longer than the size of EFfplmn, then the file will be written from the
      * beginning of the list up to the file size.
      *
-     * <p>Requires Permission: {@link android.Manifest.permission#READ_PHONE_STATE READ_PHONE_STATE}
+     * <p>Requires Permission: {@link android.Manifest.permission#MODIFY_PHONE_STATE
+     * MODIFY_PHONE_STATE}
      * or that the calling app has carrier privileges (see {@link #hasCarrierPrivileges}).
      *
      * @param fplmns a list of PLMNs to be forbidden.
@@ -6706,7 +6707,7 @@ public class TelephonyManager {
     public int setForbiddenPlmns(@NonNull List<String> fplmns) {
         try {
             ITelephony telephony = getITelephony();
-            if (telephony == null) return 0;
+            if (telephony == null) return -1;
             return telephony.setForbiddenPlmns(
                     getSubId(), APPTYPE_USIM, fplmns, getOpPackageName());
         } catch (RemoteException ex) {
@@ -6715,7 +6716,7 @@ public class TelephonyManager {
             // This could happen before phone starts
             Rlog.e(TAG, "setForbiddenPlmns NullPointerException: " + ex.getMessage());
         }
-        return 0;
+        return -1;
     }
 
     /**
