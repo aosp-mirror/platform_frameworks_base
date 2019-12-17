5aadb8b1376e5b039552d86d69da560c629b4a8f
Cody Kesting <ckesting@google.com>
2019-12-17 12:55:28 -0800

    Add AIDL for registering ConnectivityDiagnosticCallbacks.
    ConnectivityService needs to allow for registering and unregistering
    ConnectivityDiagnosticsCallbacks as part of ConnectivityDiagnostics API.
    These methods will be used by ConnectivityDiagnosticsManager so that
    privileged applications can receive notifications on network events by
    the system.
    
    Bug: 146444622
    Test: compiles
    Change-Id: Id00b30af5692db5eef47d0d948897148578d3b32

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -66,6 +66,7 @@ import android.net.CaptivePortal;
 import android.net.ConnectionInfo;
 import android.net.ConnectivityManager;
 import android.net.ICaptivePortal;
+import android.net.IConnectivityDiagnosticsCallback;
 import android.net.IConnectivityManager;
 import android.net.IDnsResolver;
 import android.net.IIpConnectivityMetrics;
@@ -7306,5 +7307,21 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
             return mTNS;
         }
+    }
+
+    @Override
+    public void registerConnectivityDiagnosticsCallback(
+            @NonNull IConnectivityDiagnosticsCallback callback, @NonNull NetworkRequest request) {
+        // TODO(b/146444622): implement register IConnectivityDiagnosticsCallback functionality
+        throw new UnsupportedOperationException(
+                "registerConnectivityDiagnosticsCallback not yet implemented");
+    }
+
+    @Override
+    public void unregisterConnectivityDiagnosticsCallback(
+            @NonNull IConnectivityDiagnosticsCallback callback) {
+        // TODO(b/146444622): implement register IConnectivityDiagnosticsCallback functionality
+        throw new UnsupportedOperationException(
+                "unregisterConnectivityDiagnosticsCallback not yet implemented");
     }
 }
