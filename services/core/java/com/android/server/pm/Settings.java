1c448eaab91a48abe4868828446c4e81bbda1656
Christopher Tate <ctate@google.com>
2019-12-17 11:21:02 -0800

    Revoke 'always' web handler status when not autoverifying
    If an app has previously used autoVerify to make claims about its status
    re handling web navigation intents, but is updated such that it no
    longer makes those claims, step down its "official handler" status as
    though it had never invoked autoVerify in the first place.
    
    Bug: 146204120
    Test: manual: as described in bug; observe policy before/after via
          'adb shell dumpsys package d'
    Test: atest CtsOsHostTestCases
    Change-Id: I58502d1b32d793aba9aa772fa2ad5ac38acca48a

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -1249,6 +1249,7 @@ public final class Settings {
             return false;
         }
         ps.clearDomainVerificationStatusForUser(userId);
+        ps.setIntentFilterVerificationInfo(null);
         return true;
     }
 
