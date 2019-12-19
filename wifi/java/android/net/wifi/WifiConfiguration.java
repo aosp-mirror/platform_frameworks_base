725e2d267463e8e1ac76284d05c95312ff15290e
David Su <dysu@google.com>
2019-12-18 21:36:21 -0800

    Formalize WifiConfiguration#getIpConfiguration()
    Expose getIpConfiguration() as a @SystemApi for
    Android TV.
    
    Bug: 138801922
    Test: boots up and connects to Wifi
    Test: atest FrameworksWifiApiTests
    Change-Id: Ied83bc02baaba89369aae81981a448856d33140d

--- wifi/java/android/net/wifi/WifiConfiguration.java
+++ wifi/java/android/net/wifi/WifiConfiguration.java
@@ -2394,10 +2394,14 @@ public class WifiConfiguration implements Parcelable {
         return key;
     }
 
-    /** @hide */
-    @UnsupportedAppUsage
+    /**
+     * Get the IpConfiguration object associated with this WifiConfiguration.
+     * @hide
+     */
+    @NonNull
+    @SystemApi
     public IpConfiguration getIpConfiguration() {
-        return mIpConfiguration;
+        return new IpConfiguration(mIpConfiguration);
     }
 
     /**
