1aa5ca3f70eac8ef1ef84e7fad565aa54f75b0f6
Aaron Huang <huangaaron@google.com>
2019-12-31 21:48:45 +0800

    Make Context.NETWORK_POLICY_SERVICE @SystemApi
    Includes Context.NETWORK_POLICY_SERVICE into system API
    so that system apps(including mainline modules) could
    obtain the service.
    
    Bug: 138306002
    Test: FrameworksNetTests
          FrameworksTelephonyTests
    
    Change-Id: I3f751b14e55969952c69b33c97ef86d859cef8b5

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -3918,6 +3918,7 @@ public abstract class Context {
      */
     public static final String NETWORK_STATS_SERVICE = "netstats";
     /** {@hide} */
+    @SystemApi
     public static final String NETWORK_POLICY_SERVICE = "netpolicy";
     /** {@hide} */
     public static final String NETWORK_WATCHLIST_SERVICE = "network_watchlist";
