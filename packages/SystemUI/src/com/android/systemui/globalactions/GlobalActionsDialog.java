49a4cbc76a7d41367cd0f9ea3f2a785e32ef3969
SKULSHADY <anushekprasal@gmail.com>
2020-01-04 19:12:49 +0100

    PowerMenu: Fix Advanced button position on low DPI
    * Also allow marquee like other buttons
    
    Change-Id: I9616e66c698c52167d1419ee10d70861eddbc2e6

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -1450,7 +1450,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
         public View create(
                 Context context, View convertView, ViewGroup parent, LayoutInflater inflater) {
             mContext = context;
-            View v = inflater.inflate(com.android.systemui.R.layout.global_actions_item, parent,
+            View v = inflater.inflate(com.android.systemui.R.layout.global_actions_grid_item, parent,
                     false);
 
             TextView statusView = (TextView) v.findViewById(R.id.status);
@@ -1464,6 +1464,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             TextView messageView = (TextView) v.findViewById(R.id.message);
             if (messageView != null) {
                 messageView.setText(mMessageResid);
+                messageView.setSelected(true); // necessary for marquee to work
             }
             ImageView icon = (ImageView) v.findViewById(R.id.icon);
             if (icon != null) {
