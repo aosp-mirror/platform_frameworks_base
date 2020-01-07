c65494584a2caac31c0772e42da3bd0f5cb292f0
Bill Lin <lbill@google.com>
2020-01-07 15:05:49 +0800

    Add Overlay package for simulate punch hole cutout(1/N)
    Test: adb shell cmd overlay enable --user 0 com.android.internal.display.cutout.emulation.hole
    Test: atest DisplayCutoutTests
    Bug: 145707162
    Change-Id: I5669377a86c003b7bd52d9a9bfa79771209e0e8c

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -26,6 +26,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorPurpleOverlay \
 	DisplayCutoutEmulationCornerOverlay \
 	DisplayCutoutEmulationDoubleOverlay \
+        DisplayCutoutEmulationHoleOverlay \
 	DisplayCutoutEmulationTallOverlay \
 	FontNotoSerifSourceOverlay \
 	IconPackCircularAndroidOverlay \
