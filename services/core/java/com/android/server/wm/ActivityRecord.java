781ad9d5a977ce7c8b353e5396a54a62767f61fb
Daniel Chapin <chapin@google.com>
2020-01-21 21:14:03 +0000

    Revert "Intercepting activites that could cause lock task mode violation in ActivityStartInterceptor."
    This reverts commit 3c91859190911cf36a04a9008ee5cd9f31285ee8.
    
    Reason for revert: b/147878221
    
    Change-Id: I4dded8282b5d01726c695ddd1a8db14f4e3c16fe

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -1633,7 +1633,12 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         requestedVrComponent = (aInfo.requestedVrComponent == null) ?
                 null : ComponentName.unflattenFromString(aInfo.requestedVrComponent);
 
-        lockTaskLaunchMode = getLockTaskLaunchMode(aInfo, options);
+        lockTaskLaunchMode = aInfo.lockTaskLaunchMode;
+        if (info.applicationInfo.isPrivilegedApp()
+                && (lockTaskLaunchMode == LOCK_TASK_LAUNCH_MODE_ALWAYS
+                || lockTaskLaunchMode == LOCK_TASK_LAUNCH_MODE_NEVER)) {
+            lockTaskLaunchMode = LOCK_TASK_LAUNCH_MODE_DEFAULT;
+        }
 
         if (options != null) {
             pendingOptions = options;
@@ -1641,25 +1646,13 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             if (usageReport != null) {
                 appTimeTracker = new AppTimeTracker(usageReport);
             }
-            // Gets launch display id from options. It returns INVALID_DISPLAY if not set.
-            mHandoverLaunchDisplayId = options.getLaunchDisplayId();
-        }
-    }
-
-    static int getLockTaskLaunchMode(ActivityInfo aInfo, @Nullable ActivityOptions options) {
-        int lockTaskLaunchMode = aInfo.lockTaskLaunchMode;
-        if (aInfo.applicationInfo.isPrivilegedApp()
-                && (lockTaskLaunchMode == LOCK_TASK_LAUNCH_MODE_ALWAYS
-                || lockTaskLaunchMode == LOCK_TASK_LAUNCH_MODE_NEVER)) {
-            lockTaskLaunchMode = LOCK_TASK_LAUNCH_MODE_DEFAULT;
-        }
-        if (options != null) {
-            final boolean useLockTask = options.getLockTaskMode();
+            final boolean useLockTask = pendingOptions.getLockTaskMode();
             if (useLockTask && lockTaskLaunchMode == LOCK_TASK_LAUNCH_MODE_DEFAULT) {
                 lockTaskLaunchMode = LOCK_TASK_LAUNCH_MODE_IF_WHITELISTED;
             }
+            // Gets launch display id from options. It returns INVALID_DISPLAY if not set.
+            mHandoverLaunchDisplayId = options.getLaunchDisplayId();
         }
-        return lockTaskLaunchMode;
     }
 
     @Override
