21de9eebb53058ba3ba99b19b2e71faa7ddeb512
Jaewan Kim <jaewan@google.com>
2020-01-22 06:23:44 +0000

    Revert "Adds a new ACCESS_CONTEXT_HUB permission"
    This reverts commit 40428978557d2c2efd79d14d120df6669f9aefcc.
    
    Bug: 135951924
    Reason for revert:
       Breaks PermissionPolicyTest.platformPermissionPolicyIsUnaltered
       in CtsPermission2TestCases
    
    Change-Id: Ie48fca6a0c54e281aae0db6cd9652b49cebbaa5c

--- services/core/java/com/android/server/location/ContextHubClientBroker.java
+++ services/core/java/com/android/server/location/ContextHubClientBroker.java
@@ -16,8 +16,6 @@
 
 package com.android.server.location;
 
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
-
 import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -111,11 +109,6 @@ public class ContextHubClientBroker extends IContextHubClient.Stub
     private AtomicBoolean mIsPendingIntentCancelled = new AtomicBoolean(false);
 
     /*
-     * True if the application creating the client has the ACCESS_CONTEXT_HUB permission.
-     */
-    private final boolean mHasAccessContextHubPermission;
-
-    /*
      * Helper class to manage registered PendingIntent requests from the client.
      */
     private class PendingIntentRequest {
@@ -172,9 +165,6 @@ public class ContextHubClientBroker extends IContextHubClient.Stub
         mCallbackInterface = callback;
         mPendingIntentRequest = new PendingIntentRequest();
         mPackage = mContext.getPackageManager().getNameForUid(Binder.getCallingUid());
-
-        mHasAccessContextHubPermission = context.checkCallingPermission(
-            Manifest.permission.ACCESS_CONTEXT_HUB) == PERMISSION_GRANTED;
     }
 
     /* package */ ContextHubClientBroker(
@@ -188,9 +178,6 @@ public class ContextHubClientBroker extends IContextHubClient.Stub
         mHostEndPointId = hostEndPointId;
         mPendingIntentRequest = new PendingIntentRequest(pendingIntent, nanoAppId);
         mPackage = pendingIntent.getCreatorPackage();
-
-        mHasAccessContextHubPermission = context.checkCallingPermission(
-            Manifest.permission.ACCESS_CONTEXT_HUB) == PERMISSION_GRANTED;
     }
 
     /**
@@ -428,12 +415,10 @@ public class ContextHubClientBroker extends IContextHubClient.Stub
      */
     private void doSendPendingIntent(PendingIntent pendingIntent, Intent intent) {
         try {
-            String requiredPermission = mHasAccessContextHubPermission
-                    ? Manifest.permission.ACCESS_CONTEXT_HUB
-                    : Manifest.permission.LOCATION_HARDWARE;
             pendingIntent.send(
                     mContext, 0 /* code */, intent, null /* onFinished */, null /* Handler */,
-                    requiredPermission, null /* options */);
+                    Manifest.permission.LOCATION_HARDWARE /* requiredPermission */,
+                    null /* options */);
         } catch (PendingIntent.CanceledException e) {
             mIsPendingIntentCancelled.set(true);
             // The PendingIntent is no longer valid
