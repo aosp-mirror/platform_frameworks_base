754493b82f29af4314ee8f6206a2d571a3e99a52
Tyler Gunn <tgunn@google.com>
2020-01-27 10:30:51 -0800

    API Review cleanups.
    - New @SystemApis on Conference and PhoneAccount were missing the required
    permissions annotations.
    - Rename PROPERTY_ASSISTED_DIALING_USED to PROPERTY_ASSISTED_DIALING
    - Standardize get/setConnectionStartElapsedRealtimeMillis method naming
    across Connection and Conference classes.
    - Clarify Conference#sendConferenceEvent API documentation to match the
    docs present for similar method in Connection; include some examples of
    valid event/extras combinations.
    - Update TelecomManager#getDefaultdialerPackage to use UserHandle instead
    of userId.
    - Move Conference#getConnectionStartElapsedRealtimeMillis to public API
    since the setter is already part of the public API.
    
    Test: Run Telecom and Telephony CTS tests.
    Test: Run Telephony unit tests.
    Test: Perform manual single-party-conference regression test to confirm
    that conference behavior does not regress.
    
    Bug: 147301297
    Bug: 148286830
    Bug: 148284863
    Bug: 148284843
    Bug: 148287068
    Bug: 148285484
    Bug: 148285560
    Change-Id: I1f446d81859fa109d74af3661a42a0bd224de5aa

--- telecomm/java/android/telecom/TelecomManager.java
+++ telecomm/java/android/telecom/TelecomManager.java
@@ -819,8 +819,8 @@ public class TelecomManager {
      * automatically add dialing prefixes when placing international calls.
      * <p>
      * Setting this extra on the outgoing call extras will cause the
-     * {@link Connection#PROPERTY_ASSISTED_DIALING_USED} property and
-     * {@link Call.Details#PROPERTY_ASSISTED_DIALING_USED} property to be set on the
+     * {@link Connection#PROPERTY_ASSISTED_DIALING} property and
+     * {@link Call.Details#PROPERTY_ASSISTED_DIALING} property to be set on the
      * {@link Connection}/{@link Call} in question.  When the call is logged to the call log, the
      * {@link android.provider.CallLog.Calls#FEATURES_ASSISTED_DIALING_USED} call feature is set to
      * indicate that assisted dialing was used for the call.
@@ -1412,7 +1412,7 @@ public class TelecomManager {
     /**
      * Used to determine the currently selected default dialer package for a specific user.
      *
-     * @param userId the user id to query the default dialer package for.
+     * @param userHandle the user id to query the default dialer package for.
      * @return package name for the default dialer package or null if no package has been
      *         selected as the default dialer.
      * @hide
@@ -1420,10 +1420,11 @@ public class TelecomManager {
     @SystemApi
     @TestApi
     @RequiresPermission(READ_PRIVILEGED_PHONE_STATE)
-    public @Nullable String getDefaultDialerPackage(int userId) {
+    public @Nullable String getDefaultDialerPackage(@NonNull UserHandle userHandle) {
         try {
             if (isServiceConnected()) {
-                return getTelecomService().getDefaultDialerPackageForUser(userId);
+                return getTelecomService().getDefaultDialerPackageForUser(
+                        userHandle.getIdentifier());
             }
         } catch (RemoteException e) {
             Log.e(TAG, "RemoteException attempting to get the default dialer package name.", e);
