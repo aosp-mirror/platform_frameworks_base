c89c3b8216ebc0a8f38aac29374f0d243d628dc6
Valentin Iftime <valiiftime@google.com>
2020-02-05 14:13:08 +0100

    Check for FEATURE_TELEPHONY before clearing APNs
    Do not clear override APNs for devices without FEATURE_TELEPHONY
    
    Test: atest DeviceOwnerTest
    Bug: 148928926
    Change-Id: I2b6c426c4a88ea7260910b48327a408ab9ee0466

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -8520,6 +8520,9 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
     }
 
     private void clearOverrideApnUnchecked() {
+        if (!mHasTelephonyFeature) {
+            return;
+        }
         // Disable Override APNs and remove them from database.
         setOverrideApnsEnabledUnchecked(false);
         final List<ApnSetting> apns = getOverrideApnsUnchecked();
