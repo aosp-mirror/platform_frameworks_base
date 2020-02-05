0865f2e7b22ceab8d1cc28092b8e361990882c78
Tim Murray <timmurray@google.com>
2020-02-04 22:23:16 -0800

    hwui: remove FatVector
    FatVector now lives in fw/native/libs/ui.
    
    Test: builds, boots, works
    Bug: 149096186
    Change-Id: I09dc2fddd2a238ef47d7853b64b0bfbf74d3f71f

--- core/jni/android/graphics/fonts/Font.cpp
+++ core/jni/android/graphics/fonts/Font.cpp
@@ -33,8 +33,8 @@
 
 #include <hwui/MinikinSkia.h>
 #include <hwui/Typeface.h>
-#include <utils/FatVector.h>
 #include <minikin/FontFamily.h>
+#include <ui/FatVector.h>
 
 #include <memory>
 
@@ -157,7 +157,7 @@ static jlong Font_Builder_build(JNIEnv* env, jobject clazz, jlong builderPtr, jo
     sk_sp<SkData> data(SkData::MakeWithProc(fontPtr, fontSize,
             release_global_ref, reinterpret_cast<void*>(fontRef)));
 
-    uirenderer::FatVector<SkFontArguments::Axis, 2> skiaAxes;
+    FatVector<SkFontArguments::Axis, 2> skiaAxes;
     for (const auto& axis : builder->axes) {
         skiaAxes.emplace_back(SkFontArguments::Axis{axis.axisTag, axis.value});
     }
