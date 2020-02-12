946e06d7e25a73bacb80fc8a2b5fdeb0b340cc8d
Victor Hsieh <victorhsieh@google.com>
2020-02-12 12:11:38 -0800

    Enforce fs-verity support for first_api_level >= R
    This matches the approved CDD requirement.
    
    Test: build
    Bug: 133451372
    Change-Id: I4c0defe08c988ac3c07c6a8352f754d222bd3a5d

--- services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
@@ -577,7 +577,9 @@ public class PackageManagerServiceUtils {
 
     /** Returns true if standard APK Verity is enabled. */
     static boolean isApkVerityEnabled() {
-        return SystemProperties.getInt("ro.apk_verity.mode", FSVERITY_DISABLED) == FSVERITY_ENABLED;
+        return Build.VERSION.FIRST_SDK_INT >= Build.VERSION_CODES.R
+                || SystemProperties.getInt("ro.apk_verity.mode", FSVERITY_DISABLED)
+                        == FSVERITY_ENABLED;
     }
 
     static boolean isLegacyApkVerityEnabled() {
