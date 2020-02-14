3b413dd57e155a911a39529a479de4997023cee9
Suprabh Shukla <suprabh@google.com>
2020-02-13 20:08:07 -0800

    Improve BaseBundle#kindofEquals
    If both BaseBundles are empty, we can infer that without needing to
    unparcel any of them.
    
    Test: atest FrameworksCoreTests:android.os.BundleTest
    
    Bug: 146037505
    Change-Id: I04c28cdd1293227d9887b0c17e178f61328c1959
    Merged-In: I04c28cdd1293227d9887b0c17e178f61328c1959

--- services/core/java/com/android/server/job/controllers/JobStatus.java
+++ services/core/java/com/android/server/job/controllers/JobStatus.java
@@ -1502,11 +1502,11 @@ public final class JobStatus {
                     pw.println();
                 }
             }
-            if (job.getExtras() != null && !job.getExtras().maybeIsEmpty()) {
+            if (job.getExtras() != null && !job.getExtras().isDefinitelyEmpty()) {
                 pw.print(prefix); pw.print("  Extras: ");
                 pw.println(job.getExtras().toShortString());
             }
-            if (job.getTransientExtras() != null && !job.getTransientExtras().maybeIsEmpty()) {
+            if (job.getTransientExtras() != null && !job.getTransientExtras().isDefinitelyEmpty()) {
                 pw.print(prefix); pw.print("  Transient extras: ");
                 pw.println(job.getTransientExtras().toShortString());
             }
@@ -1702,10 +1702,10 @@ public final class JobStatus {
                             job.getTriggerContentMaxDelay());
                 }
             }
-            if (job.getExtras() != null && !job.getExtras().maybeIsEmpty()) {
+            if (job.getExtras() != null && !job.getExtras().isDefinitelyEmpty()) {
                 job.getExtras().writeToProto(proto, JobStatusDumpProto.JobInfo.EXTRAS);
             }
-            if (job.getTransientExtras() != null && !job.getTransientExtras().maybeIsEmpty()) {
+            if (job.getTransientExtras() != null && !job.getTransientExtras().isDefinitelyEmpty()) {
                 job.getTransientExtras().writeToProto(proto, JobStatusDumpProto.JobInfo.TRANSIENT_EXTRAS);
             }
             if (job.getClipData() != null) {
