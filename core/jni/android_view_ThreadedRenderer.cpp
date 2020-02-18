ca0adce78b2287178f24ad3839ea5b4c22e50fb2
Alec Mouri <alecmouri@google.com>
2020-02-18 12:59:41 -0800

    [libandroid_graphics] Add a missing release() call
    RenderProxy acquires its own references to the ANativeWindow and does
    not steal it, so there needs to be an additional ANativeWindow_release
    call in setSurface
    
    Test: builds
    Test: interact with device
    Change-Id: I598f57febf835cda7fb4195b19e03c3723698702

--- core/jni/android_view_ThreadedRenderer.cpp
+++ core/jni/android_view_ThreadedRenderer.cpp
@@ -186,6 +186,7 @@ static void android_view_ThreadedRenderer_setSurface(JNIEnv* env, jobject clazz,
         proxy->setSwapBehavior(SwapBehavior::kSwap_discardBuffer);
     }
     proxy->setSurface(window, enableTimeout);
+    ANativeWindow_release(window);
 }
 
 static jboolean android_view_ThreadedRenderer_pause(JNIEnv* env, jobject clazz,
