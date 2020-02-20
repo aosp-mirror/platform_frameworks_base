be79321d71cd674af4788119c8cdc158c13da5de
lucaslin <lucaslin@google.com>
2020-02-20 16:56:59 +0800

    Remove redundant connected notification
    Remove the old connected notification since there is a new file
    - NetworkStackNotifier.java which will send the connected
    notification when captive portal validated.
    
    Bug: 149883761
    Test: 1. atest FrameworksNetTests
          2. Sign-in a captive portal and see if there is a redundant
          connected notification.
    
    Change-Id: I4ec9d26623af4bfc65156a28a96b982466aede31
    Merged-In: Id11a9b99dd04772a92af8d527104906c47bf64cd

--- tests/net/java/com/android/server/ConnectivityServiceTest.java
+++ tests/net/java/com/android/server/ConnectivityServiceTest.java
@@ -2725,9 +2725,6 @@ public class ConnectivityServiceTest {
 
         // Expect NET_CAPABILITY_VALIDATED onAvailable callback.
         validatedCallback.expectAvailableDoubleValidatedCallbacks(mWiFiNetworkAgent);
-        // Expect no notification to be shown when captive portal disappears by itself
-        verify(mNotificationManager, never()).notifyAsUser(
-                anyString(), eq(NotificationType.LOGGED_IN.eventId), any(), any());
 
         // Break network connectivity.
         // Expect NET_CAPABILITY_VALIDATED onLost callback.
@@ -2789,8 +2786,6 @@ public class ConnectivityServiceTest {
         mWiFiNetworkAgent.mNetworkMonitor.forceReevaluation(Process.myUid());
         validatedCallback.expectAvailableCallbacksValidated(mWiFiNetworkAgent);
         captivePortalCallback.expectCallback(CallbackEntry.LOST, mWiFiNetworkAgent);
-        verify(mNotificationManager, times(1)).notifyAsUser(anyString(),
-                eq(NotificationType.LOGGED_IN.eventId), any(), eq(UserHandle.ALL));
 
         mCm.unregisterNetworkCallback(validatedCallback);
         mCm.unregisterNetworkCallback(captivePortalCallback);
