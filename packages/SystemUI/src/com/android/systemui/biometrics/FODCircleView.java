bfc01f02602341812d96a12ddf9a7a60e013b9d7
TheScarastic <warabhishek@gmail.com>
2020-02-26 10:50:20 -0500

    FODCircleView: adjust dim amount based on changes in brightness
    Change-Id: I76437cc239deb2554d52f005b4b60af5a471565d

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -38,8 +38,9 @@ import android.widget.ImageView;
 
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.keyguard.KeyguardUpdateMonitorCallback;
-import com.android.systemui.R;
 import com.android.systemui.Dependency;
+import com.android.systemui.R;
+import com.android.systemui.tuner.TunerService;
 import com.android.systemui.statusbar.policy.ConfigurationController;
 import com.android.systemui.statusbar.policy.ConfigurationController.ConfigurationListener;
 
@@ -50,7 +51,9 @@ import java.util.NoSuchElementException;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class FODCircleView extends ImageView implements ConfigurationListener {
+public class FODCircleView extends ImageView implements ConfigurationListener, TunerService.Tunable {
+    private final String SCREEN_BRIGHTNESS = "system:" + Settings.System.SCREEN_BRIGHTNESS;
+
     private final int mPositionX;
     private final int mPositionY;
     private final int mSize;
@@ -66,6 +69,7 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
     private int mDreamingOffsetX;
     private int mDreamingOffsetY;
 
+    private int mCurrentBrightness;
     private boolean mIsBouncer;
     private boolean mIsDreaming;
     private boolean mIsShowing;
@@ -180,11 +184,17 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
         mUpdateMonitor = KeyguardUpdateMonitor.getInstance(context);
         mUpdateMonitor.registerCallback(mMonitorCallback);
-
+        Dependency.get(TunerService.class).addTunable(this, SCREEN_BRIGHTNESS);
 
         updateCutoutFlags();
 
         Dependency.get(ConfigurationController.class).addCallback(this);
+    }
+
+    @Override
+    public void onTuningChanged(String key, String newValue) {
+        mCurrentBrightness = newValue != null ?  Integer.parseInt(newValue) : 0;
+        updateDim();
     }
 
     @Override
@@ -376,13 +386,11 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
 
     private void updateDim() {
         if (mIsCircleShowing) {
-            int curBrightness = Settings.System.getInt(getContext().getContentResolver(),
-                    Settings.System.SCREEN_BRIGHTNESS, 100);
             int dimAmount = 0;
 
             IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
             try {
-                dimAmount = daemon.getDimAmount(curBrightness);
+                dimAmount = daemon.getDimAmount(mCurrentBrightness);
             } catch (RemoteException e) {
                 return;
             }
