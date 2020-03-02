a18d7c8ec2764e28345b5355ea86545e9659d858
John Reck <jreck@google.com>
2020-03-02 09:41:33 -0800

    Move Bitmap to AParcel
    Bug: 145227478
    Test: device boots; android.graphics.cts.BitmapTest passes
    Change-Id: I760fa5155549e92c625a28cdc847c8d7b0b178b3

--- libs/hwui/hwui/Bitmap.h
+++ libs/hwui/hwui/Bitmap.h
@@ -71,6 +71,7 @@ public:
     static sk_sp<Bitmap> allocateHardwareBitmap(const SkBitmap& bitmap);
     static sk_sp<Bitmap> allocateHeapBitmap(SkBitmap* bitmap);
     static sk_sp<Bitmap> allocateHeapBitmap(const SkImageInfo& info);
+    static sk_sp<Bitmap> allocateHeapBitmap(size_t size, const SkImageInfo& i, size_t rowBytes);
 
     /* The createFrom factories construct a new Bitmap object by wrapping the already allocated
      * memory that is provided as an input param.
@@ -160,7 +161,6 @@ public:
                        int32_t quality, SkWStream* stream);
 private:
     static sk_sp<Bitmap> allocateAshmemBitmap(size_t size, const SkImageInfo& i, size_t rowBytes);
-    static sk_sp<Bitmap> allocateHeapBitmap(size_t size, const SkImageInfo& i, size_t rowBytes);
 
     Bitmap(void* address, size_t allocSize, const SkImageInfo& info, size_t rowBytes);
     Bitmap(void* address, void* context, FreeFunc freeFunc, const SkImageInfo& info,
