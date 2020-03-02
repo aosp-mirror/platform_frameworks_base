1c0993cf12d34e7611db258982c1854960985545
Hongyi Zhang <hongyiz@google.com>
2020-03-01 18:46:51 -0800

    RescueParty: Add a DeviceConfig killswitch
    1. Add a killswitch DeviceConfig flag in case emergency rollback is
    needed (e.g. PackageWatchdog doesn't behave correctly).
    2. Register the namespace for native flag sync mapper so that we can
    also use the flag to turn off native disaster recovery.
    
    Test: Atest RescueParty & verified on local device
    Bug: 149420506
    Change-Id: I9a0b546c7229c8444947b522585dd2c5ed37a00b

--- services/core/java/com/android/server/RescueParty.java
+++ services/core/java/com/android/server/RescueParty.java
@@ -99,6 +99,8 @@ public class RescueParty {
     private static final String PROP_DISABLE_RESCUE = "persist.sys.disable_rescue";
     private static final String PROP_VIRTUAL_DEVICE = "ro.hardware.virtual_device";
 
+    private static final String DEVICE_CONFIG_DISABLE_FLAG = "disable_rescue_party";
+
     private static final int PERSISTENT_MASK = ApplicationInfo.FLAG_PERSISTENT
             | ApplicationInfo.FLAG_SYSTEM;
 
@@ -112,6 +114,14 @@ public class RescueParty {
         // Check if we're explicitly enabled for testing
         if (SystemProperties.getBoolean(PROP_ENABLE_RESCUE, false)) {
             return false;
+        }
+
+        // We're disabled if the DeviceConfig disable flag is set to true.
+        // This is in case that an emergency rollback of the feature is needed.
+        if (DeviceConfig.getBoolean(
+                DeviceConfig.NAMESPACE_CONFIGURATION, DEVICE_CONFIG_DISABLE_FLAG, false)) {
+            Slog.v(TAG, "Disabled because of DeviceConfig flag");
+            return true;
         }
 
         // We're disabled on all engineering devices
