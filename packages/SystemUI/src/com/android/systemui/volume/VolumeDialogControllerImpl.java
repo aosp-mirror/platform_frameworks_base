d1789c605a98831e14bb95ac7586e171c0113226
Sergey Nikolaienkov <sergeynv@google.com>
2020-03-03 10:32:59 +0100

    Workaround for not initializing StatusBar on TVs
    The fact that StatusBar gets initialized and injected on TVs is causing
    several issues. While we are working on the permanent solution -
    unbinding StatusBar on TVs (bug: 150666950)
    
    Change-Id: I123685dbb49aa6df95f32faeefca9c13123ec527
    Test: make, flash, make sure HDMI-CEC is disabled, use +/- buttons on
    a remote to adjust volume, see the appropriate UI
    Bug: 150663447
    Bug: 146188087
    
    Change-Id: Ifa5ba67db1bdaf6b6ae1f685989e4cd47fa1ac18

--- packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumeDialogControllerImpl.java
@@ -147,7 +147,12 @@ public class VolumeDialogControllerImpl implements VolumeDialogController, Dumpa
     public VolumeDialogControllerImpl(Context context, BroadcastDispatcher broadcastDispatcher,
             Optional<Lazy<StatusBar>> statusBarOptionalLazy) {
         mContext = context.getApplicationContext();
-        mStatusBarOptionalLazy = statusBarOptionalLazy;
+        // TODO(b/150663459): remove this TV workaround once StatusBar is "unbound" on TVs
+        if (mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LEANBACK)) {
+            mStatusBarOptionalLazy = Optional.empty();
+        } else {
+            mStatusBarOptionalLazy = statusBarOptionalLazy;
+        }
         mNotificationManager = (NotificationManager) mContext.getSystemService(
                 Context.NOTIFICATION_SERVICE);
         Events.writeEvent(Events.EVENT_COLLECTION_STARTED);
