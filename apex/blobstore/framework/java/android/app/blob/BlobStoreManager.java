38dae29cbb30d3eb6455af0ee2b70f8f88f07ea1
Sudheer Shanka <sudheersai@google.com>
2020-03-08 11:56:21 -0700

    Expose BlobStoreManager.waitForIdle() as @TestApi.
    Bug: 151035961
    Test: atest --test-mapping cts/hostsidetests/blobstore
    Change-Id: I3632560c5ba9b28b4492d07d847911196b0abc78

--- apex/blobstore/framework/java/android/app/blob/BlobStoreManager.java
+++ apex/blobstore/framework/java/android/app/blob/BlobStoreManager.java
@@ -22,6 +22,7 @@ import android.annotation.IdRes;
 import android.annotation.IntRange;
 import android.annotation.NonNull;
 import android.annotation.SystemService;
+import android.annotation.TestApi;
 import android.content.Context;
 import android.os.ParcelFileDescriptor;
 import android.os.ParcelableException;
@@ -481,6 +482,7 @@ public class BlobStoreManager {
      *
      * @hide
      */
+    @TestApi
     public void waitForIdle(long timeoutMillis) throws InterruptedException, TimeoutException {
         try {
             final CountDownLatch countDownLatch = new CountDownLatch(1);
