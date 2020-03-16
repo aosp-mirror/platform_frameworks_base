9487c9e51331ff75abd45efc5a31a168c36ff55d
Peter Wang <tpwang@google.com>
2020-03-16 04:45:23 -0700

    [Telephony Mainline] Rename getActiveAndHiddenSubscriptionIdList and guard with permission
    Bug: 150878235
    Fix: 150878235
    Test: Build
    Change-Id: I7f3b1f5170e032336d9b39b322c49e2b92573586

--- telephony/common/com/android/internal/telephony/TelephonyPermissions.java
+++ telephony/common/com/android/internal/telephony/TelephonyPermissions.java
@@ -611,7 +611,7 @@ public final class TelephonyPermissions {
     private static boolean checkCarrierPrivilegeForAnySubId(Context context, int uid) {
         SubscriptionManager sm = (SubscriptionManager) context.getSystemService(
                 Context.TELEPHONY_SUBSCRIPTION_SERVICE);
-        int[] activeSubIds = sm.getActiveAndHiddenSubscriptionIdList();
+        int[] activeSubIds = sm.getCompleteActiveSubscriptionIdList();
         for (int activeSubId : activeSubIds) {
             if (getCarrierPrivilegeStatus(context, activeSubId, uid)
                     == TelephonyManager.CARRIER_PRIVILEGE_STATUS_HAS_ACCESS) {
