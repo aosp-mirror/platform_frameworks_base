2cf1d18fe1f234927fd952a9cfa38043c4a59433
Alex Buynytskyy <alexbuy@google.com>
2020-03-17 09:33:45 -0700

    Incremental apps removal on data loader failures.
    Test: adb install --incremental megacity.apk over USB2.0, remove usb connection
    after installed.
    Bug: b/15041101
    
    Change-Id: Ib848640dbe49c76021f7cf544de4413599c2c763

--- services/incremental/IncrementalService.cpp
+++ services/incremental/IncrementalService.cpp
@@ -1248,14 +1248,6 @@ binder::Status IncrementalService::IncrementalDataLoaderListener::onStatusChange
     }
 
     switch (newStatus) {
-        case IDataLoaderStatusListener::DATA_LOADER_NO_CONNECTION: {
-            // TODO(b/150411019): handle data loader connection loss
-            break;
-        }
-        case IDataLoaderStatusListener::DATA_LOADER_CONNECTION_OK: {
-            // TODO(b/150411019): handle data loader connection loss
-            break;
-        }
         case IDataLoaderStatusListener::DATA_LOADER_CREATED: {
             if (startRequested) {
                 incrementalService.startDataLoader(mountId);
@@ -1275,6 +1267,10 @@ binder::Status IncrementalService::IncrementalDataLoaderListener::onStatusChange
             break;
         }
         case IDataLoaderStatusListener::DATA_LOADER_IMAGE_NOT_READY: {
+            break;
+        }
+        case IDataLoaderStatusListener::DATA_LOADER_UNRECOVERABLE: {
+            // Nothing for now. Rely on externalListener to handle this.
             break;
         }
         default: {
