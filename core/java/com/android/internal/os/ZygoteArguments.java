2cd955e79d32832f3eb38227713a5235309f253f
Ricky Wai <rickywai@google.com>
2020-03-17 11:13:49 +0000

    Do not mount whitelisted package in storage data and obb dirs
    We now pass 2 booleans from AM to zygote about:
    - If CE and DE data dirs need to be mounted
    - If storage data and obb dirs need to be mounted
    
    And also, separate whitelisted package from same uid packages, as same
    uid packages do not need to be mounted in storage data and obb dir case,
    it's needed to be mounted for CE and DE data dirs only. Otherwise
    whtelisted packages will also be mounted in storage data and obb dirs,
    which apps should not have access to it.
    
    Bug: 151218156
    Test: atest AdoptableHostTest
    Change-Id: If7c20a7ed3b845d8657c937469161cb7ed3da07f

--- core/java/com/android/internal/os/ZygoteArguments.java
+++ core/java/com/android/internal/os/ZygoteArguments.java
@@ -227,9 +227,20 @@ class ZygoteArguments {
     String[] mPkgDataInfoList;
 
     /**
+     * A list that stores all whitelisted app data info: volume uuid and inode.
+     * Null if it does need to do app data isolation.
+     */
+    String[] mWhitelistedDataInfoList;
+
+    /**
      * @see Zygote#BIND_MOUNT_APP_STORAGE_DIRS
      */
     boolean mBindMountAppStorageDirs;
+
+    /**
+     * @see Zygote#BIND_MOUNT_APP_DATA_DIRS
+     */
+    boolean mBindMountAppDataDirs;
 
     /**
      * Constructs instance and parses args
@@ -452,8 +463,12 @@ class ZygoteArguments {
                 }
             } else if (arg.startsWith(Zygote.PKG_DATA_INFO_MAP)) {
                 mPkgDataInfoList = getAssignmentList(arg);
+            } else if (arg.startsWith(Zygote.WHITELISTED_DATA_INFO_MAP)) {
+                mWhitelistedDataInfoList = getAssignmentList(arg);
             } else if (arg.equals(Zygote.BIND_MOUNT_APP_STORAGE_DIRS)) {
                 mBindMountAppStorageDirs = true;
+            } else if (arg.equals(Zygote.BIND_MOUNT_APP_DATA_DIRS)) {
+                mBindMountAppDataDirs = true;
             } else {
                 break;
             }
