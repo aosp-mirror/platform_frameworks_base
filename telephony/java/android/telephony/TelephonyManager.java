5fe261504641a579ff7f00eab9f85ca3a559e21a
Sarah Chin <sarahchin@google.com>
2020-03-18 13:57:18 -0700

    Remove Mainline APIs for MmsManger and TelephonyManager
    Test: build
    Bug: 144004293
    Bug: 147611125
    Change-Id: I44682d1f33eee02d1dcf57f4de8ce2df0a9b126d
    Merged-In: I44682d1f33eee02d1dcf57f4de8ce2df0a9b126d

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -9628,14 +9628,12 @@ public class TelephonyManager {
      * Powers down the SIM. SIM must be up prior.
      * @hide
      */
-    @SystemApi
     public static final int CARD_POWER_DOWN = 0;
 
     /**
      * Powers up the SIM normally. SIM must be down prior.
      * @hide
      */
-    @SystemApi
     public static final int CARD_POWER_UP = 1;
 
     /**
@@ -9653,7 +9651,6 @@ public class TelephonyManager {
      * is NOT persistent across boots. On reboot, SIM will power up normally.
      * @hide
      */
-    @SystemApi
     public static final int CARD_POWER_UP_PASS_THROUGH = 2;
 
     /**
