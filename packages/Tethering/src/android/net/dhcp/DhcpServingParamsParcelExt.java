Neoteric-OS/android_frameworks_base

Assign specific client address to dhcp server
Bug: 141256482
Test: manual
Change-Id: Ief76c98c843ba5420224cbf0f34464f366c891b7

packages/Tethering/src/android/net/dhcp/DhcpServingParamsParcelExt.java
@@ -18,9 +18,11 @@ package android.net.dhcp;
 
 import static android.net.shared.Inet4AddressUtils.inet4AddressToIntHTH;
 
-import android.annotation.NonNull;
 import android.net.LinkAddress;
 import android.util.ArraySet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.net.Inet4Address;
 import java.util.Collection;
@@ -157,6 +159,16 @@ public class DhcpServingParamsParcelExt extends DhcpServingParamsParcel {
      */
     public DhcpServingParamsParcelExt setMetered(boolean metered) {
         this.metered = metered;
+        return this;
+    }
+
+    /**
+     * Set the client address to tell DHCP server only offer this address.
+     *
+     * <p>If not set, the default value is null.
+     */
+    public DhcpServingParamsParcelExt setClientAddr(@Nullable String clientAddr) {
+        this.clientAddr = clientAddr;
         return this;
     }
 

aosp-mirror/platform_frameworks_base

      atest TetheringTests

packages/Tethering/src/android/net/dhcp/DhcpServingParamsParcelExt.java
@@ -18,9 +18,11 @@ package android.net.dhcp;
 
 import static android.net.shared.Inet4AddressUtils.inet4AddressToIntHTH;
 
-import android.annotation.NonNull;
 import android.net.LinkAddress;
 import android.util.ArraySet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.net.Inet4Address;
 import java.util.Collection;
@@ -157,6 +159,17 @@ public class DhcpServingParamsParcelExt extends DhcpServingParamsParcel {
      */
     public DhcpServingParamsParcelExt setMetered(boolean metered) {
         this.metered = metered;
+        return this;
+    }
+
+    /**
+     * Set the client address to tell DHCP server only offer this address.
+     * The client's prefix length is the same as server's.
+     *
+     * <p>If not set, the default value is null.
+     */
+    public DhcpServingParamsParcelExt setSingleClientAddr(@Nullable Inet4Address clientAddr) {
+        this.clientAddr = clientAddr == null ? 0 : inet4AddressToIntHTH(clientAddr);
         return this;
     }
 