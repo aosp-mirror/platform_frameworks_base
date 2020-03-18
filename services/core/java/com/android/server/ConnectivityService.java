898496365aa1f3601cdbb305004ad0de11ff6bfc
Cody Kesting <ckesting@google.com>
2020-03-18 15:22:12 -0700

    Clean up handling of NetworkCapabilities#administratorUids.
    Update ConnectivityService's check for administrator UIDs to use
    ArrayUtils to check for UID inclusion. Update the NetworkCapabilities
    annotation on the administrator UIDs field to clarify that it is
    NonNull.
    
    Bug: 147903575
    Test: atest FrameworksNetTests
    Change-Id: Id630fe9d76aacdaf038fdaa5360f0327520ee0c3

--- services/core/java/com/android/server/ConnectivityService.java
+++ services/core/java/com/android/server/ConnectivityService.java
@@ -7953,11 +7953,7 @@ public class ConnectivityService extends IConnectivityManager.Stub
 
         // Administrator UIDs also contains the Owner UID
         final int[] administratorUids = nai.networkCapabilities.getAdministratorUids();
-        for (final int uid : administratorUids) {
-            if (uid == callbackUid) return true;
-        }
-
-        return false;
+        return ArrayUtils.contains(administratorUids, callbackUid);
     }
 
     @Override
