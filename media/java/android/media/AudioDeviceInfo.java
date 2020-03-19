7d77d7ea8fcb1f2cf2144715f97a80259c444053
Andy Hung <hunga@google.com>
2020-03-18 17:25:16 -0700

    AudioTrack: Update Tuner API
    Move Tuner specific methods to SystemApi
    to match Tuner Api visibility.
    
    Update return value annotation on getters.
    
    Test: AudioTrackTest#testTunerConfiguration
    Bug: 151178170
    Change-Id: I5461d62db38cd912b5896b9c27fd1fef29306bae

--- media/java/android/media/AudioDeviceInfo.java
+++ media/java/android/media/AudioDeviceInfo.java
@@ -425,9 +425,9 @@ public final class AudioDeviceInfo {
     /**
      * Returns an array of supported encapsulation modes for the device.
      *
-     * The array can include any of
-     * {@link AudioTrack#ENCAPSULATION_MODE_ELEMENTARY_STREAM},
-     * {@link AudioTrack#ENCAPSULATION_MODE_HANDLE}.
+     * The array can include any of the {@code AudioTrack} encapsulation modes,
+     * e.g. {@link AudioTrack#ENCAPSULATION_MODE_NONE},
+     * or {@link AudioTrack#ENCAPSULATION_MODE_ELEMENTARY_STREAM}.
      *
      * @return An array of supported encapsulation modes for the device.  This
      *     may be an empty array if no encapsulation modes are supported.
