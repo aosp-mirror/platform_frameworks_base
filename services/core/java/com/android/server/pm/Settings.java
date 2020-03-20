92561d210d2382880fd6b2fd5a4dfa2d0cc00920
Chris Tate <ctate@android.com>
2020-03-20 18:40:14 +0000

    Revert "Revoke 'always' web handler status when not autoverifying"
    This reverts commit 6cf5f92825df545bd011b7163418f2ea0b337af3.
    
    Reason for revert: Inadvertently broke link handling stickiness even for well behaved apps
    
    Bug: 146204120
    Test: install app that handles web urls; set to 'always' in Settings;
    install same apk again.  Verify that app is still in 'always' state via
    'adb shell dumpsys package d'
    
    Merged-In: I2b108064794b961904811c5d9f54c37dd2c7f482
    Merged-In: If9046cb420961b8ef0333e9f1115eb69fb92242e
    Change-Id: I03a121c0c1284c965bb87ee426eb0376681cf7d8

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -1377,7 +1377,6 @@ final class Settings {
             return false;
         }
         ps.clearDomainVerificationStatusForUser(userId);
-        ps.setIntentFilterVerificationInfo(null);
         return true;
     }
 
