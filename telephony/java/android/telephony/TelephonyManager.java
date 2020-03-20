1c6679a3adf72fa4e2d3b8152ea3641f05b86d3e
Amit Mahajan <amitmahajan@google.com>
2020-03-20 14:39:06 -0700

    Exposing TelephonyManager.isIccLockEnabled as a system API.
    This was recently removed as part of un-exposing telephony
    mainline related APIs (b/146983487), but being added back in R as GMSCore
    already uses it (b/151742706).
    
    Test: TH
    Bug: 151742706
    Bug: 146983487
    Change-Id: If4e8e87c7dfbb261ee3abfec2a4774980c24d0c2

--- telephony/java/android/telephony/TelephonyManager.java
+++ telephony/java/android/telephony/TelephonyManager.java
@@ -13003,6 +13003,7 @@ public class TelephonyManager {
      */
     @WorkerThread
     @RequiresPermission(android.Manifest.permission.READ_PRIVILEGED_PHONE_STATE)
+    @SystemApi
     public boolean isIccLockEnabled() {
         try {
             ITelephony telephony = getITelephony();
