69b897ad07874e57c44095c4b4b81f4615286c5f
Rhed Jao <rhedjao@google.com>
2020-03-24 23:15:46 +0800

    A new setting key to hold targets of the a11y button
    Roll back the changes of ACCESSIBILITY_BUTTON_TARGET_COMPONENT to
    support previous behavior of the a11y button. Also, create another
    setting key to hold shortcut targets of the a11y button which is
    assigned by the user from the settings app.
    
    Bug: 152263232
    Test: atest SettingsProviderTest
    Change-Id: I7a70e8c8495cefe6632efddbb322a323aa1168c7

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -164,7 +164,7 @@ public class SecureSettings {
         Settings.Secure.AWARE_TAP_PAUSE_TOUCH_COUNT,
         Settings.Secure.PEOPLE_STRIP,
         Settings.Secure.ACCESSIBILITY_MAGNIFICATION_MODE,
+        Settings.Secure.ACCESSIBILITY_BUTTON_TARGETS,
         Settings.Secure.ACCESSIBILITY_MAGNIFICATION_CAPABILITY,
-        Settings.Secure.ACCESSIBILITY_BUTTON_LONG_PRESS_TARGETS,
     };
 }
