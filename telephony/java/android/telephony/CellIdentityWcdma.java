f85a2e9220cb523dcb8c05d04a7bcb8b2cfcaf6d
Nathan Harold <nharold@google.com>
2020-03-26 13:08:32 -0700

    Check HIDL Discriminator for CSG Info
    ART throws an exception if an unpopulated safe union element
    is accessed at runtime, so instead we have to check the
    hidl_discriminator to see whether the element is populated
    before trying to access it (and thus we skip the null check).
    
    Bug: 152486447
    Test: atest testCellIdentityLte_1_5_CsgInfo \
            testCellIdentityWcdma_1_5_CsgInfo \
            testCellIdentityTdscdma_1_5_CsgInfo
    Change-Id: I08dc0d2050a9f4d4b574178e1c68a23d015768e4

--- telephony/java/android/telephony/CellIdentityWcdma.java
+++ telephony/java/android/telephony/CellIdentityWcdma.java
@@ -121,8 +121,10 @@ public final class CellIdentityWcdma extends CellIdentity {
         this(cid.base.base.lac, cid.base.base.cid, cid.base.base.psc, cid.base.base.uarfcn,
                 cid.base.base.mcc, cid.base.base.mnc, cid.base.operatorNames.alphaLong,
                 cid.base.operatorNames.alphaShort, cid.additionalPlmns,
-                cid.optionalCsgInfo.csgInfo() != null
-                        ? new ClosedSubscriberGroupInfo(cid.optionalCsgInfo.csgInfo()) : null);
+                cid.optionalCsgInfo.getDiscriminator()
+                        == android.hardware.radio.V1_5.OptionalCsgInfo.hidl_discriminator.csgInfo
+                                ? new ClosedSubscriberGroupInfo(cid.optionalCsgInfo.csgInfo())
+                                        : null);
     }
 
     private CellIdentityWcdma(@NonNull CellIdentityWcdma cid) {
