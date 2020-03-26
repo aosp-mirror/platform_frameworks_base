7cfe82b0ba3a964c9a4bf0ba11ed02d27e77762f
Hui Yu <huiyu@google.com>
2020-03-26 13:25:58 -0700

    Exempt device owner uid from FGS BG restriction.
    Bug: 136219221
    Test: atest cts/tests/app/src/android/app/cts/ActivityManagerFgsBgStartTest.java
    Change-Id: Ic7c592d711e89fcea0ebd7ccc5e33512a0bf03dc

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -638,6 +638,8 @@ public class ActivityManagerService extends IActivityManager.Stub
      */
     String mDeviceOwnerName;
 
+    private int mDeviceOwnerUid = Process.INVALID_UID;
+
     final UserController mUserController;
     @VisibleForTesting
     public final PendingIntentController mPendingIntentController;
@@ -19498,6 +19500,20 @@ public class ActivityManagerService extends IActivityManager.Stub
             synchronized (ActivityManagerService.this) {
                 ActivityManagerService.this.mServices.showWhileInUseDebugToastLocked(
                         uid, op, mode);
+            }
+        }
+
+        @Override
+        public void setDeviceOwnerUid(int uid) {
+            synchronized (ActivityManagerService.this) {
+                mDeviceOwnerUid = uid;
+            }
+        }
+
+        @Override
+        public boolean isDeviceOwner(int uid) {
+            synchronized (ActivityManagerService.this) {
+                return uid >= 0 && mDeviceOwnerUid == uid;
             }
         }
     }
