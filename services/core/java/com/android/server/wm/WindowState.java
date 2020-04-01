6da58ac8d2f98f1cee4dbebc97878315698e037f
Riddle Hsu <riddlehsu@google.com>
2020-04-02 01:04:40 +0800

    Calculate wallpaper offset with associated display info
    If the wallpaper has applied fixed rotation transform, it should
    use the local display info. This change centralizes the access of
    display info to WallpaperController#updateWallpaperOffset, so the
    caller won't pass mismatched display size.
    
    Bug: 152977308
    Test: atest DisplayContentTests#testApplyTopFixedRotationTransform
    Change-Id: If167e82c71d0f039691155a2aad28c2b78c683de

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -1101,7 +1101,6 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
                 }
             }
 
-            final ActivityStack stack = getRootTask();
             layoutDisplayFrame = new Rect(windowFrames.mDisplayFrame);
             windowFrames.mDisplayFrame.set(windowFrames.mContainingFrame);
             layoutXDiff = mInsetFrame.left - windowFrames.mContainingFrame.left;
@@ -1205,8 +1204,7 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
 
         if (mIsWallpaper && (fw != windowFrames.mFrame.width()
                 || fh != windowFrames.mFrame.height())) {
-            dc.mWallpaperController.updateWallpaperOffset(this,
-                    displayInfo.logicalWidth, displayInfo.logicalHeight, false /* sync */);
+            dc.mWallpaperController.updateWallpaperOffset(this, false /* sync */);
         }
 
         // Calculate relative frame
