d5d9e8d4a153d2f7e147e4443312e6f2814565b6
Sergey Nikolaienkov <sergeynv@google.com>
2020-04-01 09:33:44 +0200

    Fix overriding pending ProcessChangeItems
    Append change type to ProcessChangeItem (instead of setting it) after
    enqueuing items, so that we do not overried pending changes if the item
    was pulled from such set.
    
    Bug: 152929851
    Test: manual
    Change-Id: I21a576f4be9b23a5b92ca17d720b8ef4e9a353fb

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -17664,7 +17664,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
             proc.setReportedForegroundServiceTypes(fgServiceTypes);
             ProcessChangeItem item = enqueueProcessChangeItemLocked(proc.pid, proc.info.uid);
-            item.changes = ProcessChangeItem.CHANGE_FOREGROUND_SERVICES;
+            item.changes |= ProcessChangeItem.CHANGE_FOREGROUND_SERVICES;
             item.foregroundServiceTypes = fgServiceTypes;
 
             if (oomAdj) {
