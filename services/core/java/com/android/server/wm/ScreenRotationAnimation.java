333de4e7c30fd524928085bd0128db7266113b8d
Jacky Kao <jackykao@google.com>
2020-04-07 15:01:41 +0800

    Obtain correct screenshot based on device orientation
    When taking the screenshot, passing the width and height
    of the display, and the rotation according to the current
    device orientation.
    
    Bug: 153173726
    Test: a11y CTS & unit tests
    Test: manual test
    Change-Id: I19eea2046da282155719b1dfdf9a3f53ad95e13b

--- services/core/java/com/android/server/wm/ScreenRotationAnimation.java
+++ services/core/java/com/android/server/wm/ScreenRotationAnimation.java
@@ -212,7 +212,7 @@ class ScreenRotationAnimation {
             final Surface surface = mService.mSurfaceFactory.get();
             surface.copyFrom(mScreenshotLayer);
             SurfaceControl.ScreenshotGraphicBuffer gb =
-                    mService.mDisplayManagerInternal.screenshot(displayId);
+                    mService.mDisplayManagerInternal.systemScreenshot(displayId);
             if (gb != null) {
                 Trace.traceBegin(TRACE_TAG_WINDOW_MANAGER,
                         "ScreenRotationAnimation#getMedianBorderLuma");
