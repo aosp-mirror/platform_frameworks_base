a3f2f6c70e18e1ab8dfb90d0689334bc140b1035
Gavin Corkery <gavincorkery@google.com>
2020-04-27 11:20:42 +0100

    Attach logging package to staged rollback IDs
    To ensure consistency in pre-reboot and post-reboot metrics, associate
    the logging package name of a rolled back train with the staged rollback
    id. This uses the assumption that there is at most one logging parent
    (train version package) associated with the rollback. This means that
    there will be only one ROLLBACK_SUCCESS or ROLLBACK_FAILURE log for each
    event.
    
    This change changes the format of the last-staged-rollback-ids file.
    The file now has one line for each rollback id, rather than one
    line containing all ids.
    
    Test: Manual test of logging flow for a train that is configured
         correctly, and configured incorrectly
    Test: atest RollbackUnitTest
    Bug: 154911735
    Change-Id: Ie942fdb9313137c2dc6e696840262dd457558c6d

--- services/tests/servicestests/src/com/android/server/rollback/RollbackUnitTest.java
+++ services/tests/servicestests/src/com/android/server/rollback/RollbackUnitTest.java
@@ -29,6 +29,7 @@ import static org.mockito.Mockito.when;
 import android.content.pm.PackageManagerInternal;
 import android.content.pm.VersionedPackage;
 import android.content.rollback.PackageRollbackInfo;
+import android.util.SparseArray;
 import android.util.SparseIntArray;
 import android.util.SparseLongArray;
 
@@ -430,6 +431,26 @@ public class RollbackUnitTest {
             result.put(pair[0], pair[1]);
         }
         return result;
+    }
+
+    @Test
+    public void readAndWriteStagedRollbackIdsFile() throws Exception {
+        File testFile = File.createTempFile("test", ".txt");
+        RollbackPackageHealthObserver.writeStagedRollbackId(testFile, 2468, null);
+        RollbackPackageHealthObserver.writeStagedRollbackId(testFile, 12345,
+                new VersionedPackage("com.test.package", 1));
+        RollbackPackageHealthObserver.writeStagedRollbackId(testFile, 13579,
+                new VersionedPackage("com.test.package2", 2));
+        SparseArray<String> readInfo =
+                RollbackPackageHealthObserver.readStagedRollbackIds(testFile);
+        assertThat(readInfo.size()).isEqualTo(3);
+
+        assertThat(readInfo.keyAt(0)).isEqualTo(2468);
+        assertThat(readInfo.valueAt(0)).isEqualTo("");
+        assertThat(readInfo.keyAt(1)).isEqualTo(12345);
+        assertThat(readInfo.valueAt(1)).isEqualTo("com.test.package");
+        assertThat(readInfo.keyAt(2)).isEqualTo(13579);
+        assertThat(readInfo.valueAt(2)).isEqualTo("com.test.package2");
     }
 
     private static PackageRollbackInfo newPkgInfoFor(
