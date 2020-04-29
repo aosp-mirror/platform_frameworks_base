2e5f38e0bfe797f3be6ffff5ba070f4dc1b870c1
Andrew Sapperstein <asapperstein@google.com>
2020-04-29 13:16:02 -0700

    Update inner class constructor refs to use proper syntax.
    Previously, we generally required fully qualified names for referring
    to inner class constructors (like #Notification.Builder()) despite that
    not being valid javadoc. Now, we properly support #Builder() syntax and
    the old syntax will error.
    
    Bug: 6963924
    Test: make doc-comment-check-docs
    Change-Id: Ib2e4360493275b79c72487ee1cb173bb5e0fd35f
    Merged-In: Ib2e4360493275b79c72487ee1cb173bb5e0fd35f

--- core/java/android/view/textclassifier/ConversationActions.java
+++ core/java/android/view/textclassifier/ConversationActions.java
@@ -109,7 +109,7 @@ public final class ConversationActions implements Parcelable {
         /**
          * Represents the local user.
          *
-         * @see Builder#ConversationActions.Message.Builder(Person)
+         * @see Builder#Builder(Person)
          */
         @NonNull
         public static final Person PERSON_USER_SELF =
@@ -124,7 +124,7 @@ public final class ConversationActions implements Parcelable {
          * the remote user better, so that the underlying model could differentiate between
          * different remote users.
          *
-         * @see Builder#ConversationActions.Message.Builder(Person)
+         * @see Builder#Builder(Person)
          */
         @NonNull
         public static final Person PERSON_USER_OTHERS =
