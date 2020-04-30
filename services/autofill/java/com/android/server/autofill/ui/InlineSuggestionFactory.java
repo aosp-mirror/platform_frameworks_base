3b4d3caa54b65734f0da72dbcf02ab658747a7b5
Feng Cao <fengcao@google.com>
2020-04-29 18:22:49 -0700

    Reusing the remote inline suggestion view during autofill filtering
    * This gets rid of the flicker in the sample keyboard for most of
      tiem time, during filtering.
    * We introduce a new object InlineFillUi to store the autofill
      inline candidates returned from the autofill provider. Filtering
      is applied internally to ensure a single place managing
      generating the InlineSuggestionsResponse that is sent to the IME.
      This enables us to reuse the backing remote view for filtering.
    * The InlineFillUi is stored in the SessionController.
    * The other classes within the UI package are marked as package
      private.
    * Also implement the reference count to track the right time for
      releasing the remote view.
    
    Test: atest android.autofillservice.cts.inline (sanity test)
    
    Bug: 151467650
    Change-Id: I023bd8cf8acf838088fba2d1d97c0e78d563222e

--- services/autofill/java/com/android/server/autofill/ui/InlineSuggestionFactory.java
+++ services/autofill/java/com/android/server/autofill/ui/InlineSuggestionFactory.java
@@ -17,7 +17,6 @@
 package com.android.server.autofill.ui;
 
 import static com.android.server.autofill.Helper.sDebug;
-import static com.android.server.autofill.Helper.sVerbose;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
@@ -27,11 +26,11 @@ import android.os.IBinder;
 import android.service.autofill.Dataset;
 import android.service.autofill.FillResponse;
 import android.service.autofill.InlinePresentation;
-import android.text.TextUtils;
+import android.util.Pair;
 import android.util.Slog;
+import android.util.SparseArray;
 import android.view.autofill.AutofillId;
 import android.view.autofill.AutofillManager;
-import android.view.autofill.AutofillValue;
 import android.view.inputmethod.InlineSuggestion;
 import android.view.inputmethod.InlineSuggestionInfo;
 import android.view.inputmethod.InlineSuggestionsRequest;
@@ -41,49 +40,34 @@ import android.widget.inline.InlinePresentationSpec;
 import com.android.internal.view.inline.IInlineContentProvider;
 import com.android.server.autofill.RemoteInlineSuggestionRenderService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.regex.Pattern;
 
-public final class InlineSuggestionFactory {
+final class InlineSuggestionFactory {
     private static final String TAG = "InlineSuggestionFactory";
 
-    /**
-     * Callback from the inline suggestion Ui.
-     */
-    public interface InlineSuggestionUiCallback {
-        /**
-         * Callback to autofill a dataset to the client app.
-         */
-        void autofill(@NonNull Dataset dataset);
-
-        /**
-         * Callback to start Intent in client app.
-         */
-        void startIntentSender(@NonNull IntentSender intentSender, @NonNull Intent intent);
+    public static boolean responseNeedAuthentication(@NonNull FillResponse response) {
+        return response.getAuthentication() != null && response.getInlinePresentation() != null;
     }
 
-    /**
-     * Returns a copy of the response, that internally copies the {@link IInlineContentProvider}
-     * so that it's not reused by the remote IME process across different inline suggestions.
-     * See {@link InlineContentProviderImpl} for why this is needed.
-     */
-    @NonNull
-    public static InlineSuggestionsResponse copy(@NonNull InlineSuggestionsResponse response) {
-        final ArrayList<InlineSuggestion> copiedInlineSuggestions = new ArrayList<>();
-        for (InlineSuggestion inlineSuggestion : response.getInlineSuggestions()) {
-            final IInlineContentProvider contentProvider = inlineSuggestion.getContentProvider();
-            if (contentProvider instanceof InlineContentProviderImpl) {
-                copiedInlineSuggestions.add(new
-                        InlineSuggestion(inlineSuggestion.getInfo(),
-                        ((InlineContentProviderImpl) contentProvider).copy()));
-            } else {
-                copiedInlineSuggestions.add(inlineSuggestion);
-            }
-        }
-        return new InlineSuggestionsResponse(copiedInlineSuggestions);
+    public static InlineSuggestion createInlineAuthentication(
+            @NonNull InlineSuggestionsRequest request, @NonNull FillResponse response,
+            @NonNull AutofillId autofillId,
+            @NonNull AutoFillUI.AutoFillUiCallback client, @NonNull Runnable onErrorCallback,
+            @Nullable RemoteInlineSuggestionRenderService remoteRenderService) {
+        final BiConsumer<Dataset, Integer> onClickFactory = (dataset, datasetIndex) -> {
+            client.requestHideFillUi(autofillId);
+            client.authenticate(response.getRequestId(),
+                    datasetIndex, response.getAuthentication(), response.getClientState(),
+                    /* authenticateInline= */ true);
+        };
+        final Consumer<IntentSender> intentSenderConsumer = (intentSender) ->
+                client.startIntentSender(intentSender, new Intent());
+        InlinePresentation inlineAuthentication = response.getInlinePresentation();
+        return createInlineAuthSuggestion(inlineAuthentication,
+                remoteRenderService, onClickFactory, onErrorCallback, intentSenderConsumer,
+                request.getHostInputToken(), request.getHostDisplayId());
     }
 
     /**
@@ -91,33 +75,23 @@ public final class InlineSuggestionFactory {
      * autofill service, potentially filtering the datasets.
      */
     @Nullable
-    public static InlineSuggestionsResponse createInlineSuggestionsResponse(
-            @NonNull InlineSuggestionsRequest request, @NonNull FillResponse response,
-            @Nullable String filterText, @NonNull AutofillId autofillId,
+    public static SparseArray<Pair<Dataset, InlineSuggestion>> createAutofillInlineSuggestions(
+            @NonNull InlineSuggestionsRequest request, int requestId,
+            @NonNull List<Dataset> datasets,
+            @NonNull AutofillId autofillId,
             @NonNull AutoFillUI.AutoFillUiCallback client, @NonNull Runnable onErrorCallback,
             @Nullable RemoteInlineSuggestionRenderService remoteRenderService) {
         if (sDebug) Slog.d(TAG, "createInlineSuggestionsResponse called");
-        final BiConsumer<Dataset, Integer> onClickFactory;
-        if (response.getAuthentication() != null) {
-            onClickFactory = (dataset, datasetIndex) -> {
-                client.requestHideFillUi(autofillId);
-                client.authenticate(response.getRequestId(),
-                        datasetIndex, response.getAuthentication(), response.getClientState(),
-                        /* authenticateInline= */ true);
-            };
-        } else {
-            onClickFactory = (dataset, datasetIndex) -> {
-                client.requestHideFillUi(autofillId);
-                client.fill(response.getRequestId(), datasetIndex, dataset);
-            };
-        }
+        final Consumer<IntentSender> intentSenderConsumer = (intentSender) ->
+                client.startIntentSender(intentSender, new Intent());
+        final BiConsumer<Dataset, Integer> onClickFactory = (dataset, datasetIndex) -> {
+            client.requestHideFillUi(autofillId);
+            client.fill(requestId, datasetIndex, dataset);
+        };
 
-        final InlinePresentation inlineAuthentication =
-                response.getAuthentication() == null ? null : response.getInlinePresentation();
-        return createInlineSuggestionsResponseInternal(/* isAugmented= */ false, request,
-                response.getDatasets(), filterText, inlineAuthentication, autofillId,
-                onErrorCallback, onClickFactory, (intentSender) ->
-                        client.startIntentSender(intentSender, new Intent()), remoteRenderService);
+        return createInlineSuggestionsInternal(/* isAugmented= */ false, request,
+                datasets, autofillId,
+                onErrorCallback, onClickFactory, intentSenderConsumer, remoteRenderService);
     }
 
     /**
@@ -125,16 +99,16 @@ public final class InlineSuggestionFactory {
      * autofill service.
      */
     @Nullable
-    public static InlineSuggestionsResponse createAugmentedInlineSuggestionsResponse(
+    public static SparseArray<Pair<Dataset, InlineSuggestion>>
+            createAugmentedAutofillInlineSuggestions(
             @NonNull InlineSuggestionsRequest request, @NonNull List<Dataset> datasets,
             @NonNull AutofillId autofillId,
-            @NonNull InlineSuggestionUiCallback inlineSuggestionUiCallback,
+            @NonNull InlineFillUi.InlineSuggestionUiCallback inlineSuggestionUiCallback,
             @NonNull Runnable onErrorCallback,
             @Nullable RemoteInlineSuggestionRenderService remoteRenderService) {
         if (sDebug) Slog.d(TAG, "createAugmentedInlineSuggestionsResponse called");
-        return createInlineSuggestionsResponseInternal(/* isAugmented= */ true, request,
-                datasets, /* filterText= */ null, /* inlineAuthentication= */ null,
-                autofillId, onErrorCallback,
+        return createInlineSuggestionsInternal(/* isAugmented= */ true, request,
+                datasets, autofillId, onErrorCallback,
                 (dataset, datasetIndex) ->
                         inlineSuggestionUiCallback.autofill(dataset),
                 (intentSender) ->
@@ -143,29 +117,13 @@ public final class InlineSuggestionFactory {
     }
 
     @Nullable
-    private static InlineSuggestionsResponse createInlineSuggestionsResponseInternal(
+    private static SparseArray<Pair<Dataset, InlineSuggestion>> createInlineSuggestionsInternal(
             boolean isAugmented, @NonNull InlineSuggestionsRequest request,
-            @Nullable List<Dataset> datasets, @Nullable String filterText,
-            @Nullable InlinePresentation inlineAuthentication, @NonNull AutofillId autofillId,
+            @NonNull List<Dataset> datasets, @NonNull AutofillId autofillId,
             @NonNull Runnable onErrorCallback, @NonNull BiConsumer<Dataset, Integer> onClickFactory,
             @NonNull Consumer<IntentSender> intentSenderConsumer,
             @Nullable RemoteInlineSuggestionRenderService remoteRenderService) {
-
-        final ArrayList<InlineSuggestion> inlineSuggestions = new ArrayList<>();
-        if (inlineAuthentication != null) {
-            InlineSuggestion inlineAuthSuggestion = createInlineAuthSuggestion(inlineAuthentication,
-                    remoteRenderService, onClickFactory, onErrorCallback, intentSenderConsumer,
-                    request.getHostInputToken(), request.getHostDisplayId());
-            inlineSuggestions.add(inlineAuthSuggestion);
-
-            return new InlineSuggestionsResponse(inlineSuggestions);
-        }
-
-        if (datasets == null) {
-            Slog.w(TAG, "Datasets should not be null here");
-            return null;
-        }
-
+        SparseArray<Pair<Dataset, InlineSuggestion>> response = new SparseArray<>(datasets.size());
         for (int datasetIndex = 0; datasetIndex < datasets.size(); datasetIndex++) {
             final Dataset dataset = datasets.get(datasetIndex);
             final int fieldIndex = dataset.getFieldIds().indexOf(autofillId);
@@ -179,50 +137,14 @@ public final class InlineSuggestionFactory {
                 Slog.w(TAG, "InlinePresentation not found in dataset");
                 continue;
             }
-            if (!inlinePresentation.isPinned()  // don't filter pinned suggestions
-                    && !includeDataset(dataset, fieldIndex, filterText)) {
-                continue;
-            }
             InlineSuggestion inlineSuggestion = createInlineSuggestion(isAugmented, dataset,
                     datasetIndex,
                     mergedInlinePresentation(request, datasetIndex, inlinePresentation),
                     onClickFactory, remoteRenderService, onErrorCallback, intentSenderConsumer,
                     request.getHostInputToken(), request.getHostDisplayId());
-
-            inlineSuggestions.add(inlineSuggestion);
+            response.append(datasetIndex, Pair.create(dataset, inlineSuggestion));
         }
-        return new InlineSuggestionsResponse(inlineSuggestions);
-    }
-
-    // TODO: Extract the shared filtering logic here and in FillUi to a common method.
-    private static boolean includeDataset(Dataset dataset, int fieldIndex,
-            @Nullable String filterText) {
-        // Show everything when the user input is empty.
-        if (TextUtils.isEmpty(filterText)) {
-            return true;
-        }
-
-        final String constraintLowerCase = filterText.toString().toLowerCase();
-
-        // Use the filter provided by the service, if available.
-        final Dataset.DatasetFieldFilter filter = dataset.getFilter(fieldIndex);
-        if (filter != null) {
-            Pattern filterPattern = filter.pattern;
-            if (filterPattern == null) {
-                if (sVerbose) {
-                    Slog.v(TAG, "Explicitly disabling filter for dataset id" + dataset.getId());
-                }
-                return true;
-            }
-            return filterPattern.matcher(constraintLowerCase).matches();
-        }
-
-        final AutofillValue value = dataset.getFieldValues().get(fieldIndex);
-        if (value == null || !value.isText()) {
-            return dataset.getAuthentication() == null;
-        }
-        final String valueText = value.getTextValue().toString().toLowerCase();
-        return valueText.toLowerCase().startsWith(constraintLowerCase);
+        return response;
     }
 
     private static InlineSuggestion createInlineSuggestion(boolean isAugmented,
