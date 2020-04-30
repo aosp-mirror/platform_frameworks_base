123fba9ffb3c5d2755006cc6b7d480b1a1b01162
Jeff Sharkey <jsharkey@google.com>
2020-04-30 17:28:47 +0000

    Revert "Change to using sysprop for services.core.unboosted"
    This reverts commit dc217b9d20d6e85d84aa8107a6bdef8ef2e05aa5.
    
    Reason for revert:
    
    https://android-build.googleplex.com/builds/submitted/6448376/apps/latest/view/logs/build_error.log
    
    Change-Id: I77fb257c216093c99b27484c2e3ac6da88c34d15

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -195,7 +195,6 @@ import android.provider.DeviceConfig;
 import android.provider.Settings;
 import android.service.vr.IVrManager;
 import android.service.vr.IVrStateCallbacks;
-import android.sysprop.SurfaceFlingerProperties;
 import android.text.format.DateUtils;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -305,9 +304,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -4651,11 +4648,6 @@ public class WindowManagerService extends IWindowManager.Stub
     }
 
     private static boolean queryWideColorGamutSupport() {
-        boolean defaultValue = false;
-        Optional<Boolean> hasWideColorProp = SurfaceFlingerProperties.has_wide_color_display();
-        if (hasWideColorProp.isPresent()) {
-            return hasWideColorProp.get();
-        }
         try {
             ISurfaceFlingerConfigs surfaceFlinger = ISurfaceFlingerConfigs.getService();
             OptionalBool hasWideColor = surfaceFlinger.hasWideColorDisplay();
@@ -4664,18 +4656,11 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         } catch (RemoteException e) {
             // Ignore, we're in big trouble if we can't talk to SurfaceFlinger's config store
-        } catch (NoSuchElementException e) {
-            return defaultValue;
         }
         return false;
     }
 
     private static boolean queryHdrSupport() {
-        boolean defaultValue = false;
-        Optional<Boolean> hasHdrProp = SurfaceFlingerProperties.has_HDR_display();
-        if (hasHdrProp.isPresent()) {
-            return hasHdrProp.get();
-        }
         try {
             ISurfaceFlingerConfigs surfaceFlinger = ISurfaceFlingerConfigs.getService();
             OptionalBool hasHdr = surfaceFlinger.hasHDRDisplay();
@@ -4684,8 +4669,6 @@ public class WindowManagerService extends IWindowManager.Stub
             }
         } catch (RemoteException e) {
             // Ignore, we're in big trouble if we can't talk to SurfaceFlinger's config store
-        } catch (NoSuchElementException e) {
-            return defaultValue;
         }
         return false;
     }
