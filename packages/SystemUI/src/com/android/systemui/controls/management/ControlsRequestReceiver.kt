4429ee6d93b955ec492b618ebf1fffe96f12bf4a
Fabian Kozynski <kozynski@google.com>
2020-05-04 12:44:41 -0400

    Fix redlines and strings in Controls dialog
    Also, make sure that it appears on top of the app
    
    Fixes: 155205745
    Test: manual
    Change-Id: I56ebae4ef6bdf1c89850a4ad70be05b37e3cd0cb

--- packages/SystemUI/src/com/android/systemui/controls/management/ControlsRequestReceiver.kt
+++ packages/SystemUI/src/com/android/systemui/controls/management/ControlsRequestReceiver.kt
@@ -70,6 +70,7 @@ class ControlsRequestReceiver : BroadcastReceiver() {
             ControlsProviderService.EXTRA_CONTROL.let {
                 putExtra(it, intent.getParcelableExtra<Control>(it))
             }
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT)
         }
         activityIntent.putExtra(Intent.EXTRA_USER_ID, context.userId)
 
