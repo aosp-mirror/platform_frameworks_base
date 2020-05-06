6b4b728d8c351b1a4b297be68532aa41fa450939
Amy Zhang <amyjojo@google.com>
2020-05-06 11:54:42 -0700

    Start TunerResourceManager when FEATURE_TUNER is supported
    Test: make services
    Bug: 147380513
    Change-Id: I56201db818018b019dfa7d442e3905fba30806d7

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -1891,6 +1891,10 @@ public final class SystemServer {
                     || mPackageManager.hasSystemFeature(PackageManager.FEATURE_LEANBACK)) {
                 t.traceBegin("StartTvInputManager");
                 mSystemServiceManager.startService(TvInputManagerService.class);
+                t.traceEnd();
+            }
+
+            if (mPackageManager.hasSystemFeature(PackageManager.FEATURE_TUNER)) {
                 t.traceBegin("StartTunerResourceManager");
                 mSystemServiceManager.startService(TunerResourceManagerService.class);
                 t.traceEnd();
