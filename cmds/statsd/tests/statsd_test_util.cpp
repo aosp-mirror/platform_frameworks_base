70a7ee5f605e3994dc66c8150823077f2aba3d96
tsaichristine <tsaichristine@google.com>
2020-05-18 10:05:09 -0700

    Change SimplePredicate InitialValue default to UNKNOWN
    Previous default for InitialValue was FALSE. Currently, no predicates
    configure this field.
    
    Added unit tests to test the initial condition changes in Value and
    Count metrics.
    - ValueMetricE2eTest.TestInitialConditionChanges
    - CountMetricE2eTest.TestInitialConditionChanges
    
    Bug: b/156541904
    Test: m statsd_test && adb sync data && adb shell data/nativetest/statsd_test/statsd_test
    Change-Id: I242bcb0b60455355e699636aae7e620fabc310ef

--- cmds/statsd/tests/statsd_test_util.cpp
+++ cmds/statsd/tests/statsd_test_util.cpp
@@ -169,7 +169,6 @@ AtomMatcher CreateScreenStateChangedAtomMatcher(
     return atom_matcher;
 }
 
-
 AtomMatcher CreateScreenTurnedOnAtomMatcher() {
     return CreateScreenStateChangedAtomMatcher("ScreenTurnedOn",
             android::view::DisplayStateEnum::DISPLAY_STATE_ON);
@@ -335,22 +334,46 @@ State CreateScreenStateWithOnOffMap(int64_t screenOnId, int64_t screenOffId) {
     return state;
 }
 
+State CreateScreenStateWithSimpleOnOffMap(int64_t screenOnId, int64_t screenOffId) {
+    State state;
+    state.set_id(StringToId("ScreenStateSimpleOnOff"));
+    state.set_atom_id(util::SCREEN_STATE_CHANGED);
+
+    auto map = CreateScreenStateSimpleOnOffMap(screenOnId, screenOffId);
+    *state.mutable_map() = map;
+
+    return state;
+}
+
 StateMap_StateGroup CreateScreenStateOnGroup(int64_t screenOnId) {
     StateMap_StateGroup group;
     group.set_group_id(screenOnId);
-    group.add_value(2);
-    group.add_value(5);
-    group.add_value(6);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_ON);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_VR);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_ON_SUSPEND);
     return group;
 }
 
 StateMap_StateGroup CreateScreenStateOffGroup(int64_t screenOffId) {
     StateMap_StateGroup group;
     group.set_group_id(screenOffId);
-    group.add_value(0);
-    group.add_value(1);
-    group.add_value(3);
-    group.add_value(4);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_OFF);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_DOZE);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_DOZE_SUSPEND);
+    return group;
+}
+
+StateMap_StateGroup CreateScreenStateSimpleOnGroup(int64_t screenOnId) {
+    StateMap_StateGroup group;
+    group.set_group_id(screenOnId);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_ON);
+    return group;
+}
+
+StateMap_StateGroup CreateScreenStateSimpleOffGroup(int64_t screenOffId) {
+    StateMap_StateGroup group;
+    group.set_group_id(screenOffId);
+    group.add_value(android::view::DisplayStateEnum::DISPLAY_STATE_OFF);
     return group;
 }
 
@@ -358,6 +381,13 @@ StateMap CreateScreenStateOnOffMap(int64_t screenOnId, int64_t screenOffId) {
     StateMap map;
     *map.add_group() = CreateScreenStateOnGroup(screenOnId);
     *map.add_group() = CreateScreenStateOffGroup(screenOffId);
+    return map;
+}
+
+StateMap CreateScreenStateSimpleOnOffMap(int64_t screenOnId, int64_t screenOffId) {
+    StateMap map;
+    *map.add_group() = CreateScreenStateSimpleOnGroup(screenOnId);
+    *map.add_group() = CreateScreenStateSimpleOffGroup(screenOffId);
     return map;
 }
 
