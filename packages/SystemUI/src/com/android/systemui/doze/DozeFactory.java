0fe4478679a2c2ce6fc72e72bf338baebf75a675
Dave Mankoff <mankoff@google.com>
2020-05-27 10:46:24 -0400

    Use main executor for Doze related stuff.
    Deprecate the unqualified handler provider while we're at it.
    
    Fixes: 157434050
    Test: atest SystemUITests
    Change-Id: I493e86324d3ed28338f3d017e14fe2e362b87c12

--- packages/SystemUI/src/com/android/systemui/doze/DozeFactory.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeFactory.java
@@ -28,6 +28,7 @@ import android.os.Handler;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.systemui.R;
 import com.android.systemui.broadcast.BroadcastDispatcher;
+import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dock.DockManager;
 import com.android.systemui.keyguard.WakefulnessLifecycle;
 import com.android.systemui.plugins.FalsingManager;
@@ -70,7 +71,7 @@ public class DozeFactory {
             DockManager dockManager, @Nullable IWallpaperManager wallpaperManager,
             ProximitySensor proximitySensor,
             DelayedWakeLock.Builder delayedWakeLockBuilder, Handler handler,
-            DelayableExecutor delayableExecutor,
+            @Main DelayableExecutor delayableExecutor,
             BiometricUnlockController biometricUnlockController,
             BroadcastDispatcher broadcastDispatcher, DozeHost dozeHost) {
         mFalsingManager = falsingManager;
