f1347d33e141c5e8e90df2ec4b9322a4a5d9c6cc
Jorim Jaggi <jjaggi@google.com>
2020-05-27 14:05:07 +0200

    Only promote remote animator to top sched group during animation
    When running remote animation or SystemUI has requested top
    priority (for example. when notifacition shade is open on top of
    application), make sure to let it have exclusive top sched group
    access. This reduces contention between app and entity running
    remote animation as remote animator process gets exclusive access
    to certain CPUs.
    
    Test: Manual inspection of trace
    Test: asit/perf/appstartup_non_hermetic_cyclic_dropcache_test
    Bug: 157407136
    Change-Id: I1bc495a1c536656f277a8e4aefc72f88d596c97a

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -1710,6 +1710,12 @@ public class ActivityManagerService extends IActivityManager.Stub
      */
     @Nullable ContentCaptureManagerInternal mContentCaptureService;
 
+    /**
+     * Set of {@link ProcessRecord} that have either {@link ProcessRecord#hasTopUi()} or
+     * {@link ProcessRecord#runningRemoteAnimation} set to {@code true}.
+     */
+    final ArraySet<ProcessRecord> mTopUiOrRunningRemoteAnimApps = new ArraySet<>();
+
     final class UiHandler extends Handler {
         public UiHandler() {
             super(com.android.server.UiThread.get().getLooper(), null, true);
@@ -14738,6 +14744,7 @@ public class ActivityManagerService extends IActivityManager.Stub
 
         mProcessesToGc.remove(app);
         mPendingPssProcesses.remove(app);
+        mTopUiOrRunningRemoteAnimApps.remove(app);
         ProcessList.abortNextPssTime(app.procStateMemTracker);
 
         // Dismiss any open dialogs.
@@ -18524,6 +18531,22 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
 
         return proc;
+    }
+
+    /**
+     * @return {@code true} if {@link #mTopUiOrRunningRemoteAnimApps} set contains {@code app} or when there are no apps
+     *         in this list, an false otherwise.
+     */
+    boolean containsTopUiOrRunningRemoteAnimOrEmptyLocked(ProcessRecord app) {
+        return mTopUiOrRunningRemoteAnimApps.isEmpty() || mTopUiOrRunningRemoteAnimApps.contains(app);
+    }
+
+    void addTopUiOrRunningRemoteAnim(ProcessRecord app) {
+        mTopUiOrRunningRemoteAnimApps.add(app);
+    }
+
+    void removeTopUiOrRunningRemoteAnim(ProcessRecord app) {
+        mTopUiOrRunningRemoteAnimApps.remove(app);
     }
 
     @Override
