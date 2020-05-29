2b71d2b7243810bee5505bd62c30c42a5cb6a1bb
Christopher Tate <ctate@google.com>
2020-05-28 17:49:53 -0700

    Only autoVerify at install for new hosts
    Re-run app link verification at update time only when the set of hosts
    has expanded.  Intentionally revoke verify history when an app stops
    using autoVerify, as a one-time measure to place it back into the
    non-autoverify model for tracking the user's launch preferences.  If the
    app starts using autoVerify again later, it behaves identically to an
    app that has never done so before.
    
    Bug: 151475497
    Bug: 146204120
    Test: described on master CL
    
    Merged-In: I200d85085ce79842a3ed39377d1f75ec381c8991
    Change-Id: Ibaf087946966ad82d60c7b255e3ee75990716b63
    (cherry picked from commit 153de338c182dbdbcbc3b32186cf783805fb7757)

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -1371,7 +1371,8 @@ final class Settings {
         return result;
     }
 
-    boolean removeIntentFilterVerificationLPw(String packageName, int userId) {
+    boolean removeIntentFilterVerificationLPw(String packageName, int userId,
+            boolean alsoResetStatus) {
         PackageSetting ps = mPackages.get(packageName);
         if (ps == null) {
             if (DEBUG_DOMAIN_VERIFICATION) {
@@ -1379,14 +1380,17 @@ final class Settings {
             }
             return false;
         }
-        ps.clearDomainVerificationStatusForUser(userId);
+        if (alsoResetStatus) {
+            ps.clearDomainVerificationStatusForUser(userId);
+        }
+        ps.setIntentFilterVerificationInfo(null);
         return true;
     }
 
     boolean removeIntentFilterVerificationLPw(String packageName, int[] userIds) {
         boolean result = false;
         for (int userId : userIds) {
-            result |= removeIntentFilterVerificationLPw(packageName, userId);
+            result |= removeIntentFilterVerificationLPw(packageName, userId, true);
         }
         return result;
     }
