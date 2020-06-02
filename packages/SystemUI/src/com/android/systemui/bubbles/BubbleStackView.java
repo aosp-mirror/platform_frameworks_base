1bee00878dd36344b39a6cc3841931f2f8ab37b4
Joshua Tsuji <tsuji@google.com>
2020-06-02 15:46:57 -0400

    Add a public method to BubbleController to hide the IME.
    Since the IME focus is given to the virtual display inside the expanded bubble, whatever method Screenshots is using to hide the IME doesn't work.
    
    Test: manual
    Bug: 157756391
    Change-Id: Ic052133c29f269c264f4b2e8dbce29414702e634

--- packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
+++ packages/SystemUI/src/com/android/systemui/bubbles/BubbleStackView.java
@@ -1656,6 +1656,14 @@ public class BubbleStackView extends FrameLayout
         }
     }
 
+    void hideImeFromExpandedBubble() {
+        if (mExpandedBubble != null && mExpandedBubble.getExpandedView() != null) {
+            // Hide the currently expanded bubble's IME if it's visible before switching to a new
+            // bubble.
+            mExpandedBubble.getExpandedView().hideImeIfVisible();
+        }
+    }
+
     private void beforeExpandedViewAnimation() {
         hideFlyoutImmediate();
         updateExpandedBubble();
@@ -2238,11 +2246,7 @@ public class BubbleStackView extends FrameLayout
             Log.d(TAG, "updateExpandedBubble()");
         }
 
-        if (mExpandedBubble != null && mExpandedBubble.getExpandedView() != null) {
-            // Hide the currently expanded bubble's IME if it's visible before switching to a new
-            // bubble.
-            mExpandedBubble.getExpandedView().hideImeIfVisible();
-        }
+        hideImeFromExpandedBubble();
 
         mExpandedViewContainer.removeAllViews();
         if (mIsExpanded && mExpandedBubble != null
