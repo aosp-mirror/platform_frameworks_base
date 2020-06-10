0afc301462126787a106a9dd9c71467ad9ec5eb1
Steve Elliott <steell@google.com>
2020-06-10 16:54:24 -0400

    Tap convo avatar to expand/collapse
    Fixes: 153751368
    Test: manual
    Change-Id: I42b0f05604797faba6ae3769b32bcbbe74d56a4e

--- core/java/com/android/internal/widget/ConversationLayout.java
+++ core/java/com/android/internal/widget/ConversationLayout.java
@@ -1290,8 +1290,10 @@ public class ConversationLayout extends FrameLayout
         if (expandable) {
             mExpandButtonContainer.setVisibility(VISIBLE);
             mExpandButtonInnerContainer.setOnClickListener(onClickListener);
+            mConversationIconContainer.setOnClickListener(onClickListener);
         } else {
             mExpandButtonContainer.setVisibility(GONE);
+            mConversationIconContainer.setOnClickListener(null);
         }
         updateContentEndPaddings();
     }
