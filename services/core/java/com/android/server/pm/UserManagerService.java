6eba5bb73be5798e3693d15b7537ceda003a5657
Alex Buynytskyy <alexbuy@google.com>
2020-06-16 15:44:04 -0700

    Adding internal version of getUsers without permission check.
    It's up to system-server to make sure it's doing the right thing.
    -50ms create time for PM
    
    Bug: 157191740
    Test: adb reboot; adb logcat | grep -e PackageManagerTiming
    Change-Id: I8b64164464fcc6447871cfb94475af7379b72c16

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -751,13 +751,19 @@ public class UserManagerService extends IUserManager.Stub {
     }
 
     public @NonNull List<UserInfo> getUsers(boolean excludeDying) {
-        return getUsers(/*excludePartial= */ true, excludeDying, /* excludePreCreated= */ true);
+        return getUsers(/*excludePartial= */ true, excludeDying, /* excludePreCreated= */
+                true);
     }
 
     @Override
     public @NonNull List<UserInfo> getUsers(boolean excludePartial, boolean excludeDying,
             boolean excludePreCreated) {
         checkManageOrCreateUsersPermission("query users");
+        return getUsersInternal(excludePartial, excludeDying, excludePreCreated);
+    }
+
+    private @NonNull List<UserInfo> getUsersInternal(boolean excludePartial, boolean excludeDying,
+            boolean excludePreCreated) {
         synchronized (mUsersLock) {
             ArrayList<UserInfo> users = new ArrayList<UserInfo>(mUsers.size());
             final int userSize = mUsers.size();
@@ -5039,6 +5045,12 @@ public class UserManagerService extends IUserManager.Stub {
         @Override
         public int[] getUserIds() {
             return UserManagerService.this.getUserIds();
+        }
+
+        @Override
+        public @NonNull List<UserInfo> getUsers(boolean excludeDying) {
+            return UserManagerService.this.getUsersInternal(/*excludePartial= */ true,
+                    excludeDying, /* excludePreCreated= */ true);
         }
 
         @Override
