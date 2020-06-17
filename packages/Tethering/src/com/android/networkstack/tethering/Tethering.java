1c63ba39fab6ac0712556ef35798e67c46423a0c
Nucca Chen <nuccachen@google.com>
2020-06-17 07:03:57 +0000

    [BOT.12] Add unit test for disabling BpfCoordinator by config
    Bug: 150736748
    Test: BpfCoordinatorTest
    Original change: https://android-review.googlesource.com/c/platform/frameworks/base/+/1313802
    Merged-In: Iedb936b7592b6be773d1b84a2498bfc5a440a198
    Change-Id: I298ae39a1fa61b2cf97752aa908aa2d7d0f9783d
    (cherry picked from commit abd6c03d22e71b4afe6c455ef79734a2c9574380)

--- packages/Tethering/src/com/android/networkstack/tethering/Tethering.java
+++ packages/Tethering/src/com/android/networkstack/tethering/Tethering.java
@@ -340,8 +340,7 @@ public class Tethering {
 
                     @NonNull
                     public NetworkStatsManager getNetworkStatsManager() {
-                        return (NetworkStatsManager) mContext.getSystemService(
-                            Context.NETWORK_STATS_SERVICE);
+                        return mContext.getSystemService(NetworkStatsManager.class);
                     }
 
                     @NonNull
@@ -2405,7 +2404,7 @@ public class Tethering {
         final TetherState tetherState = new TetherState(
                 new IpServer(iface, mLooper, interfaceType, mLog, mNetd, mBpfCoordinator,
                              makeControlCallback(), mConfig.enableLegacyDhcpServer,
-                             mConfig.enableBpfOffload, mPrivateAddressCoordinator,
+                             mConfig.isBpfOffloadEnabled(), mPrivateAddressCoordinator,
                              mDeps.getIpServerDependencies()));
         mTetherStates.put(iface, tetherState);
         tetherState.ipServer.start();
