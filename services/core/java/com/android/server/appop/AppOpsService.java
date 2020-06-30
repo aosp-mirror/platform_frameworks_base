63b18ace936417e525f1e6955e9b8ae680537335
Eugene Susla <eugenesusla@google.com>
2020-06-30 11:59:46 -0700

    Resolve package dumpstate as shell uid
    Fixes: 158769948
    Test: adb bugreport, ensure no exceptions from appops
    Change-Id: I68cb90a2150d5eef3f5072e3b28f8737c0ea2a93

--- services/core/java/com/android/server/appop/AppOpsService.java
+++ services/core/java/com/android/server/appop/AppOpsService.java
@@ -6179,6 +6179,7 @@ public class AppOpsService extends IAppOpsService.Stub {
             case "root":
                 return Process.ROOT_UID;
             case "shell":
+            case "dumpstate":
                 return Process.SHELL_UID;
             case "media":
                 return Process.MEDIA_UID;
