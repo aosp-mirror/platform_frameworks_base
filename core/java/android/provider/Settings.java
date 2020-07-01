85eda008b8d37a7f9c11a1e6907bdfbf3119362a
Lincoln Atkinson <latkin@google.com>
2020-07-01 11:25:25 -0700

    Add global settings keys for sysui package and main activity
    Test: manual
    Bug: 155178776
    Change-Id: I5ddb50238eacd485ef3cc39631a1a6a9f97217bf
    (cherry picked from commit 0ae7b7e022e126cc510bb87d3c95beb7d2f65e47)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -14718,6 +14718,22 @@ public final class Settings {
                 "max_sound_trigger_detection_service_ops_per_day";
 
         /**
+         * Setting indicating the name of the Wear OS app package containing the device's sysui.
+         *
+         * @hide
+         */
+        public static final String CLOCKWORK_SYSUI_PACKAGE_NAME =
+                "clockwork_sysui_package";
+
+        /**
+         * Setting indicating the name of the main activity of the Wear OS sysui.
+         *
+         * @hide
+         */
+        public static final String CLOCKWORK_SYSUI_MAIN_ACTIVITY_NAME =
+                "clockwork_sysui_main_activity";
+
+        /**
          * Indicates whether aware is available in the current location.
          * @hide
          */
