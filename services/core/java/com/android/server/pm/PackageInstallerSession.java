2f5eea17c744019c8bfb6226c6cdd015dc4116b9
JW Wang <wangchun@google.com>
2020-07-07 16:45:38 +0800

    Rename some methods (10/n)
    Add the "Locked" suffix to show they must be called within the lock.
    
    Bug: 159663586
    Test: atest StagedInstallTest AtomicInstallTest
    Change-Id: Ie9e5bb08641ab7ad0df3040fd998c93d176e930d

--- services/core/java/com/android/server/pm/PackageInstallerSession.java
+++ services/core/java/com/android/server/pm/PackageInstallerSession.java
@@ -2023,7 +2023,7 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
         }
 
         final File targetFile = new File(stageDir, targetName);
-        resolveAndStageFile(addedFile, targetFile);
+        resolveAndStageFileLocked(addedFile, targetFile);
         mResolvedBaseFile = targetFile;
 
         // Populate package name of the apex session
@@ -2139,7 +2139,7 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
             }
 
             final File targetFile = new File(stageDir, targetName);
-            resolveAndStageFile(addedFile, targetFile);
+            resolveAndStageFileLocked(addedFile, targetFile);
 
             // Base is coming from session
             if (apk.splitName == null) {
@@ -2156,7 +2156,7 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
                 }
                 final File targetDexMetadataFile = new File(stageDir,
                         DexMetadataHelper.buildDexMetadataPathForApk(targetName));
-                resolveAndStageFile(dexMetadataFile, targetDexMetadataFile);
+                resolveAndStageFileLocked(dexMetadataFile, targetDexMetadataFile);
             }
         }
 
@@ -2236,12 +2236,12 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
             // Inherit base if not overridden
             if (mResolvedBaseFile == null) {
                 mResolvedBaseFile = new File(appInfo.getBaseCodePath());
-                resolveInheritedFile(mResolvedBaseFile);
+                resolveInheritedFileLocked(mResolvedBaseFile);
                 // Inherit the dex metadata if present.
                 final File baseDexMetadataFile =
                         DexMetadataHelper.findDexMetadataForFile(mResolvedBaseFile);
                 if (baseDexMetadataFile != null) {
-                    resolveInheritedFile(baseDexMetadataFile);
+                    resolveInheritedFileLocked(baseDexMetadataFile);
                 }
                 baseApk = existingBase;
             }
@@ -2253,12 +2253,12 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
                     final File splitFile = new File(existing.splitCodePaths[i]);
                     final boolean splitRemoved = removeSplitList.contains(splitName);
                     if (!stagedSplits.contains(splitName) && !splitRemoved) {
-                        resolveInheritedFile(splitFile);
+                        resolveInheritedFileLocked(splitFile);
                         // Inherit the dex metadata if present.
                         final File splitDexMetadataFile =
                                 DexMetadataHelper.findDexMetadataForFile(splitFile);
                         if (splitDexMetadataFile != null) {
-                            resolveInheritedFile(splitDexMetadataFile);
+                            resolveInheritedFileLocked(splitDexMetadataFile);
                         }
                     }
                 }
@@ -2359,7 +2359,8 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
         }
     }
 
-    private void resolveAndStageFile(File origFile, File targetFile)
+    @GuardedBy("mLock")
+    private void resolveAndStageFileLocked(File origFile, File targetFile)
             throws PackageManagerException {
         mResolvedStagedFiles.add(targetFile);
         maybeRenameFile(origFile, targetFile);
@@ -2392,7 +2393,8 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
         mResolvedStagedFiles.add(stagedSignature);
     }
 
-    private void resolveInheritedFile(File origFile) {
+    @GuardedBy("mLock")
+    private void resolveInheritedFileLocked(File origFile) {
         mResolvedInheritedFiles.add(origFile);
 
         // Inherit the fsverity signature file if present.
