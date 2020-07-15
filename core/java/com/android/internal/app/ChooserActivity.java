068ddf7b06a81244bb1e52db72e387c70bf724ef
Zhen Zhang <zzhen@google.com>
2020-07-15 11:16:54 -0700

    Scroll the hiding page to top on tab changed in sharesheet
    After the tab changed, ResolverDrawerLayout will lose reference of the
    hidden page which can cause the page not able to scoll back to top.
    Automatically scroll on tab changed to avoid that situation.
    
    Bug: 160886554
    Test: manually test; atest ChooserActivityTest; atest
    ResolverActivityTest
    
    Change-Id: I1a8ba72f6b1dcb9bca19262b97983788001d715b

--- core/java/com/android/internal/app/ChooserActivity.java
+++ core/java/com/android/internal/app/ChooserActivity.java
@@ -3134,6 +3134,7 @@ public class ChooserActivity extends ResolverActivity implements
         // ends up disabled. That's because at some point the old tab's vertical scrolling is
         // disabled and the new tab's is enabled. For context, see b/159997845
         setVerticalScrollEnabled(true);
+        mResolverDrawerLayout.scrollNestedScrollableChildBackToTop();
     }
 
     @Override
