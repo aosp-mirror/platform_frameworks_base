ddca494f4fc0e5d0e48ee2d0b0caaad2dc078252
Zhuoyao Zhang <zhuoyao@google.com>
2020-07-31 17:49:26 +0000

    Revert "Add EBS interface to BatteryController"
    Revert "Add Extreme Battery Saver behavior"
    
    Revert submission 12248919-153940148
    
    Reason for revert: b/162539418
    Reverted Changes:
    Ie6abbbb45:Add EBS interface to BatteryController
    I60bd64e26:Add Extreme Battery Saver behavior
    
    Bug: 162539418
    Change-Id: If264b97788d57d97683cd11ad5607cbdebab118e

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryController.java
@@ -79,13 +79,6 @@ public interface BatteryController extends DemoMode, Dumpable,
     default void setReverseState(boolean isReverse) {}
 
     /**
-     * Returns {@code true} if extreme battery saver is on.
-     */
-    default boolean isExtremeSaverOn() {
-        return false;
-    }
-
-    /**
      * A listener that will be notified whenever a change in battery level or power save mode has
      * occurred.
      */
@@ -98,9 +91,6 @@ public interface BatteryController extends DemoMode, Dumpable,
         }
 
         default void onReverseChanged(boolean isReverse, int level, String name) {
-        }
-
-        default void onExtremeBatterySaverChanged(boolean isExtreme) {
         }
     }
 
