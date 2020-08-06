9a9471290f3d98bd908025159ae993971b3474ef
mincheli <mincheli@google.com>
2020-08-06 16:52:42 +0800

    Adds Magnification settings keys as TestApi
    Bug: 162814440
    Test: atest SettingsProviderTest
    Change-Id: If3ddc65ebbfd679892d89a273a84edac78bc0cb6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -9082,6 +9082,7 @@ public final class Settings {
          * @see#ACCESSIBILITY_MAGNIFICATION_MODE_WINDOW
          * @hide
          */
+        @TestApi
         public static final String ACCESSIBILITY_MAGNIFICATION_MODE =
                 "accessibility_magnification_mode";
 
@@ -9089,12 +9090,14 @@ public final class Settings {
          * Magnification mode value that magnifies whole display.
          * @hide
          */
+        @TestApi
         public static final int ACCESSIBILITY_MAGNIFICATION_MODE_FULLSCREEN = 0x1;
 
         /**
          * Magnification mode value that magnifies magnify particular region in a window
          * @hide
          */
+        @TestApi
         public static final int ACCESSIBILITY_MAGNIFICATION_MODE_WINDOW = 0x2;
 
         /**
@@ -9102,6 +9105,7 @@ public final class Settings {
          * region in a window.
          * @hide
          */
+        @TestApi
         public static final int ACCESSIBILITY_MAGNIFICATION_MODE_ALL = 0x3;
 
         /**
@@ -9113,6 +9117,7 @@ public final class Settings {
          * @see#ACCESSIBILITY_MAGNIFICATION_MODE_ALL
          * @hide
          */
+        @TestApi
         public static final String ACCESSIBILITY_MAGNIFICATION_CAPABILITY =
                 "accessibility_magnification_capability";
 
