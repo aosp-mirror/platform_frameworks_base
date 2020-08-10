4d69266afb895fe375186c0a80316ad3efcfd403
Ivan Chiang <chiangi@google.com>
2020-08-10 16:43:33 +0800

    Revoke the uri permission when the file is deleted
    When the file is deleted, renamed or moved, revoke all uri
    permissions with the file
    
    Bug: 157474195
    Test: manual test with DocumentsUI
    Test: atest DocumentsTest#testAfterMoveDocumentInStorage_revokeUriPermission
    Change-Id: I4ffb183630aadb2d87b0965e8cecf88af15f4534
    Merged-In: I4ffb183630aadb2d87b0965e8cecf88af15f4534
    (cherry picked from commit 9efd606f43abe36f9fcf7f0d1ab0d059c51be514)
    (cherry picked from commit c5c373c25890b9473a93d4ad000a89d134cf2d17)

--- core/java/com/android/internal/content/FileSystemProvider.java
+++ core/java/com/android/internal/content/FileSystemProvider.java
@@ -93,6 +93,14 @@ public abstract class FileSystemProvider extends DocumentsProvider {
         // Default is no-op
     }
 
+    /**
+     * Callback indicating that the given document has been deleted or moved. This gives
+     * the provider a hook to revoke the uri permissions.
+     */
+    protected void onDocIdDeleted(String docId) {
+        // Default is no-op
+    }
+
     @Override
     public boolean onCreate() {
         throw new UnsupportedOperationException(
@@ -243,6 +251,7 @@ public abstract class FileSystemProvider extends DocumentsProvider {
 
         final String afterDocId = getDocIdForFile(after);
         onDocIdChanged(docId);
+        onDocIdDeleted(docId);
         onDocIdChanged(afterDocId);
 
         final File beforeVisibleFile = getFileForDocId(docId, true);
@@ -274,6 +283,7 @@ public abstract class FileSystemProvider extends DocumentsProvider {
 
         final String docId = getDocIdForFile(after);
         onDocIdChanged(sourceDocumentId);
+        onDocIdDeleted(sourceDocumentId);
         onDocIdChanged(docId);
         moveInMediaStore(visibleFileBefore, getFileForDocId(docId, true));
 
@@ -325,6 +335,7 @@ public abstract class FileSystemProvider extends DocumentsProvider {
         }
 
         onDocIdChanged(docId);
+        onDocIdDeleted(docId);
         removeFromMediaStore(visibleFile, isDirectory);
     }
 
