1476c298d53312c61d6e229c82fc7ed238220dc2
Songchun Fan <schfan@google.com>
2020-08-12 18:55:08 -0700

    [pm] ignore old value of abiOverride in pkgSetting when installing
    This reverts a change introduced in ag/10220453, and the correct behavior here should be to use the abiOverride value passed from args.
    
    Test: manual (will add CTS tests)
    BUG: 163888243
    BUG: 162298405
    Change-Id: I93c85b5b8fe94726e450dc467f2cd5aecaa376f9

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -17563,9 +17563,6 @@ public class PackageManagerService extends IPackageManager.Stub
                 synchronized (mLock) {
                     pkgSetting = mSettings.getPackageLPr(pkgName);
                 }
-                String abiOverride =
-                        (pkgSetting == null || TextUtils.isEmpty(pkgSetting.cpuAbiOverrideString)
-                        ? args.abiOverride : pkgSetting.cpuAbiOverrideString);
                 boolean isUpdatedSystemAppFromExistingSetting = pkgSetting != null
                         && pkgSetting.getPkgState().isUpdatedSystemApp();
                 AndroidPackage oldPackage = mPackages.get(pkgName);
@@ -17573,7 +17570,7 @@ public class PackageManagerService extends IPackageManager.Stub
                 final Pair<PackageAbiHelper.Abis, PackageAbiHelper.NativeLibraryPaths>
                         derivedAbi = mInjector.getAbiHelper().derivePackageAbi(parsedPackage,
                         isUpdatedSystemAppFromExistingSetting || isUpdatedSystemAppInferred,
-                        abiOverride);
+                        args.abiOverride);
                 derivedAbi.first.applyTo(parsedPackage);
                 derivedAbi.second.applyTo(parsedPackage);
             } catch (PackageManagerException pme) {
