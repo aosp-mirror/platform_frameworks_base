e49aa726f0687afec8ab6a040bff7016557ba3da
Kweku Adams <kwekua@google.com>
2020-08-14 09:43:06 -0700

    Allow package verifier to hide specific fgs notifications.
    The package verifier performs some system critical work that the user
    doesn't need to be explicitly aware of. This adds a mechanism for the
    verifier to indicate which foreground service notifications should be
    hidden, if possible.
    
    Bug: 164440539
    Test: use test app to confirm requested fgs notifications are hidden
    Change-Id: Ib3eb0b71cc676c145557ade9def98a363e5abebb

--- services/core/java/com/android/server/am/ServiceRecord.java
+++ services/core/java/com/android/server/am/ServiceRecord.java
@@ -104,6 +104,7 @@ final class ServiceRecord extends Binder implements ComponentName.WithComponentN
     boolean whitelistManager; // any bindings to this service have BIND_ALLOW_WHITELIST_MANAGEMENT?
     boolean delayed;        // are we waiting to start this service in the background?
     boolean fgRequired;     // is the service required to go foreground after starting?
+    boolean hideFgNotification; // Hide the fg service notification
     boolean fgWaiting;      // is a timeout for going foreground already scheduled?
     boolean isForeground;   // is service currently in foreground mode?
     int foregroundId;       // Notification ID of last foreground req.
@@ -823,6 +824,9 @@ final class ServiceRecord extends Binder implements ComponentName.WithComponentN
     }
 
     public void postNotification() {
+        if (hideFgNotification) {
+            return;
+        }
         final int appUid = appInfo.uid;
         final int appPid = app.pid;
         if (foregroundId != 0 && foregroundNoti != null) {
@@ -915,7 +919,7 @@ final class ServiceRecord extends Binder implements ComponentName.WithComponentN
                         }
                         if (localForegroundNoti.getSmallIcon() == null) {
                             // Notifications whose icon is 0 are defined to not show
-                            // a notification, silently ignoring it.  We don't want to
+                            // a notification.  We don't want to
                             // just ignore it, we want to prevent the service from
                             // being foreground.
                             throw new RuntimeException("invalid service notification: "
