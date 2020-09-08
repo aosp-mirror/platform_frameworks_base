837fd13486d12383e174674ebce871687237882c
Riddle Hsu <riddlehsu@google.com>
2020-09-08 23:34:15 +0800

    Use current fullscreen opaque window as insets control target
    This fixes stats bar blinking when switching from an app that hides
    status bar to another that shows status bar. It is caused by the
    TopFullscreenOpaqueWindow in DisplayPolicy will be updated according
    to visibility. There may be an intermediate state that the insets
    control target is still the previous window, then the transient
    showing bar will be hidden a while.
    
    It is tempting to make canAffectSystemUiFlags accept the activity
    with mVisibleRequested=true so TopFullscreenOpaqueWindow can be
    updated earlier. But that will break status bar hiding animation
    when switching from an app that shows the bar to another that hides
    the bar.
    
    Also fix:
     - The original insets source is modified when getting insets state
       for client because the default "new InsetsState" will reuse the
       given insets sources.
     - Redundant invocation of updateBarControlTarget when calling
       abortTransient from updateBarControlTarget.
    
    Fixes: 168023253
    Test: InsetsPolicyTest#testControlsForDispatch_topAppHidesStatusBar
    Change-Id: I60f29dba81b0273a189ddd0a48a2e84155177f5b

--- services/core/java/com/android/server/wm/TaskSnapshotSurface.java
+++ services/core/java/com/android/server/wm/TaskSnapshotSurface.java
@@ -237,11 +237,7 @@ class TaskSnapshotSurface implements StartingSurface {
             task.getBounds(taskBounds);
             currentOrientation = topFullscreenOpaqueWindow.getConfiguration().orientation;
             activityType = activity.getActivityType();
-
-            final InsetsPolicy insetsPolicy = topFullscreenOpaqueWindow.getDisplayContent()
-                    .getInsetsPolicy();
-            insetsState =
-                    new InsetsState(insetsPolicy.getInsetsForDispatch(topFullscreenOpaqueWindow));
+            insetsState = topFullscreenOpaqueWindow.getInsetsState();
             mergeInsetsSources(insetsState, topFullscreenOpaqueWindow.getRequestedInsetsState());
         }
         try {
