3e5de04302633a1def9047798f64aecfc0c7efce
timhypeng <timhypeng@google.com>
2020-09-08 16:29:53 +0800

    Add Media Output Dialog for Output Switcher
    -Add MediaOutputBaseDialog to provide common method for different media operations UI
    -Add MediaOutputDialog for showing Bluetooth device
    -Add resources for background image, style and layout
    -Add MediaOutputBaseDialogTest for unit test
    
    Bug: 155822415
    Test: atest MediaOutputBaseDialogTest
    Merged-In: I3086a4049f240870ca1ad870946d6848e500b561
    Change-Id: I3086a4049f240870ca1ad870946d6848e500b561

---
+++ packages/SystemUI/tests/src/com/android/systemui/media/dialog/MediaOutputBaseDialogTest.java
@@ -0,0 +1,212 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.media.dialog;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import android.content.Context;
+import android.media.session.MediaSessionManager;
+import android.os.Bundle;
+import android.testing.AndroidTestingRunner;
+import android.testing.TestableLooper;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.graphics.drawable.IconCompat;
+import androidx.test.filters.SmallTest;
+
+import com.android.settingslib.bluetooth.LocalBluetoothManager;
+import com.android.settingslib.media.MediaDevice;
+import com.android.systemui.R;
+import com.android.systemui.SysuiTestCase;
+import com.android.systemui.plugins.ActivityStarter;
+import com.android.systemui.statusbar.phone.ShadeController;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@SmallTest
+@RunWith(AndroidTestingRunner.class)
+@TestableLooper.RunWithLooper
+public class MediaOutputBaseDialogTest extends SysuiTestCase {
+
+    private static final String TEST_PACKAGE = "test_package";
+
+    // Mock
+    private MediaOutputBaseAdapter mMediaOutputBaseAdapter = mock(MediaOutputBaseAdapter.class);
+
+    private MediaSessionManager mMediaSessionManager = mock(MediaSessionManager.class);
+    private LocalBluetoothManager mLocalBluetoothManager = mock(LocalBluetoothManager.class);
+    private ShadeController mShadeController = mock(ShadeController.class);
+    private ActivityStarter mStarter = mock(ActivityStarter.class);
+
+    private MediaOutputBaseDialogImpl mMediaOutputBaseDialogImpl;
+    private MediaOutputController mMediaOutputController;
+    private int mHeaderIconRes;
+    private IconCompat mIconCompat;
+    private CharSequence mHeaderTitle;
+    private CharSequence mHeaderSubtitle;
+
+    @Before
+    public void setUp() {
+        mMediaOutputController = new MediaOutputController(mContext, TEST_PACKAGE,
+                mMediaSessionManager, mLocalBluetoothManager, mShadeController, mStarter);
+        mMediaOutputBaseDialogImpl = new MediaOutputBaseDialogImpl(mContext,
+                mMediaOutputController);
+        mMediaOutputBaseDialogImpl.onCreate(new Bundle());
+    }
+
+    @Test
+    public void refresh_withIconRes_iconIsVisible() {
+        mHeaderIconRes = 1;
+        mMediaOutputBaseDialogImpl.refresh();
+        final ImageView view = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_icon);
+
+        assertThat(view.getVisibility()).isEqualTo(View.VISIBLE);
+    }
+
+    @Test
+    public void refresh_withIconCompat_iconIsVisible() {
+        mIconCompat = mock(IconCompat.class);
+        mMediaOutputBaseDialogImpl.refresh();
+        final ImageView view = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_icon);
+
+        assertThat(view.getVisibility()).isEqualTo(View.VISIBLE);
+    }
+
+    @Test
+    public void refresh_noIcon_iconLayoutNotVisible() {
+        mHeaderIconRes = 0;
+        mIconCompat = null;
+        mMediaOutputBaseDialogImpl.refresh();
+        final ImageView view = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_icon);
+
+        assertThat(view.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void refresh_checkTitle() {
+        mHeaderTitle = "test_string";
+
+        mMediaOutputBaseDialogImpl.refresh();
+        final TextView titleView = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_title);
+
+        assertThat(titleView.getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(titleView.getText()).isEqualTo(mHeaderTitle);
+    }
+
+    @Test
+    public void refresh_withSubtitle_checkSubtitle() {
+        mHeaderSubtitle = "test_string";
+
+        mMediaOutputBaseDialogImpl.refresh();
+        final TextView subtitleView = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_subtitle);
+
+        assertThat(subtitleView.getVisibility()).isEqualTo(View.VISIBLE);
+        assertThat(subtitleView.getText()).isEqualTo(mHeaderSubtitle);
+    }
+
+    @Test
+    public void refresh_noSubtitle_checkSubtitle() {
+        mMediaOutputBaseDialogImpl.refresh();
+        final TextView subtitleView = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.header_subtitle);
+
+        assertThat(subtitleView.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void refresh_inDragging_notUpdateAdapter() {
+        when(mMediaOutputBaseAdapter.isDragging()).thenReturn(true);
+        mMediaOutputBaseDialogImpl.refresh();
+
+        verify(mMediaOutputBaseAdapter, never()).notifyDataSetChanged();
+    }
+
+    @Test
+    public void refresh_notInDragging_verifyUpdateAdapter() {
+        when(mMediaOutputBaseAdapter.isDragging()).thenReturn(false);
+        mMediaOutputBaseDialogImpl.refresh();
+
+        verify(mMediaOutputBaseAdapter).notifyDataSetChanged();
+    }
+
+    @Test
+    public void refresh_with6Devices_checkBottomPaddingVisibility() {
+        for (int i = 0; i < 6; i++) {
+            mMediaOutputController.mMediaDevices.add(mock(MediaDevice.class));
+        }
+        mMediaOutputBaseDialogImpl.refresh();
+        final View view = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.list_bottom_padding);
+
+        assertThat(view.getVisibility()).isEqualTo(View.GONE);
+    }
+
+    @Test
+    public void refresh_with5Devices_checkBottomPaddingVisibility() {
+        for (int i = 0; i < 5; i++) {
+            mMediaOutputController.mMediaDevices.add(mock(MediaDevice.class));
+        }
+        mMediaOutputBaseDialogImpl.refresh();
+        final View view = mMediaOutputBaseDialogImpl.mDialogView.requireViewById(
+                R.id.list_bottom_padding);
+
+        assertThat(view.getVisibility()).isEqualTo(View.VISIBLE);
+    }
+
+    class MediaOutputBaseDialogImpl extends MediaOutputBaseDialog {
+
+        MediaOutputBaseDialogImpl(Context context, MediaOutputController mediaOutputController) {
+            super(context, mediaOutputController);
+
+            mAdapter = mMediaOutputBaseAdapter;
+        }
+
+        int getHeaderIconRes() {
+            return mHeaderIconRes;
+        }
+
+        IconCompat getHeaderIcon() {
+            return mIconCompat;
+        }
+
+        int getHeaderIconSize() {
+            return 10;
+        }
+
+        CharSequence getHeaderText() {
+            return mHeaderTitle;
+        }
+
+        CharSequence getHeaderSubtitle() {
+            return mHeaderSubtitle;
+        }
+    }
+}
