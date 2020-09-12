d9414beda15c92b33269a816e3c0db362639433b
Felipe Leme <felipeal@google.com>
2020-09-11 18:49:08 -0700

    Fixed PackageManager.readLPw() call.
    It wasn't including pre-created users; as such, apps that were not
    installed on pre-created users got installed on reboot.
    
    To test it:
    
    adb shell pm create-user --pre-create-only
    m ApiDemos
    adb install --user current ~/Downloads/apk/ApiDemos.apk
    adb shell dumpsys package com.example.android.apis |egrep ".*User .*:"|grep installed
    adb shell stop && adb shell start
    adb shell dumpsys package com.example.android.apis |egrep ".*User .*:"|grep installed
    
    Without this fix, the second dumpsys would show the app installed
    for the pre-created user.
    
    Test: manual test (see above)
    Test: atest CtsMultiUserHostTestCases:android.host.multiuser.PreCreateUsersTest
    Bug: 160252062
    
    Change-Id: I46c2ec94a3ab422e3e39b66239c21fb6cbff5a8e

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -1512,6 +1512,7 @@ public final class Settings {
                     return;
                 }
                 str = new FileInputStream(userPackagesStateFile);
+                if (DEBUG_MU) Log.i(TAG, "Reading " + userPackagesStateFile);
             }
             final XmlPullParser parser = Xml.newPullParser();
             parser.setInput(str, StandardCharsets.UTF_8.name());
@@ -1956,9 +1957,13 @@ public final class Settings {
 
             serializer.startTag(null, TAG_PACKAGE_RESTRICTIONS);
 
+            if (DEBUG_MU) Log.i(TAG, "Writing " + userPackagesStateFile);
             for (final PackageSetting pkg : mPackages.values()) {
                 final PackageUserState ustate = pkg.readUserState(userId);
-                if (DEBUG_MU) Log.i(TAG, "  pkg=" + pkg.name + ", state=" + ustate.enabled);
+                if (DEBUG_MU) {
+                    Log.i(TAG, "  pkg=" + pkg.name + ", installed=" + ustate.installed
+                            + ", state=" + ustate.enabled);
+                }
 
                 serializer.startTag(null, TAG_PACKAGE);
                 serializer.attribute(null, ATTR_NAME, pkg.name);
