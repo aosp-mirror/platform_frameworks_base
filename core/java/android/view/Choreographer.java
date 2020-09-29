504c828e214d5e7018e87296a6e1afaedf5a6536
Ady Abraham <adyabr@google.com>
2020-09-29 11:08:26 -0700

    Removing the UnsupportedAppUsage annotation from Choreographer
    Removing the UnsupportedAppUsage annotation according to
    https://googleplex-android-review.git.corp.google.com/c/platform/frameworks/base/+/12517216/11/core/java/android/view/DisplayEventReceiver.java#158
    
    Test: build
    Bug: 162890382
    Bug: 162888881
    Bug: 162890407
    Change-Id: I1bf8edceb52e9f4cf3f24bf9f6b9e3eb8d8ef01b

--- core/java/android/view/Choreographer.java
+++ core/java/android/view/Choreographer.java
@@ -660,8 +660,6 @@ public final class Choreographer {
         ThreadedRenderer.setFPSDivisor(divisor);
     }
 
-    @UnsupportedAppUsage(maxTargetSdk = Build.VERSION_CODES.R, publicAlternatives = "Use {@link "
-            + "#postFrameCallback} instead")
     void doFrame(long frameTimeNanos, int frame, long frameTimelineVsyncId) {
         final long startNanos;
         synchronized (mLock) {
