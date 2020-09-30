50ee8017ea54bc7c2b977e12147727057274f5d9
Patrick Baumann <patb@google.com>
2020-09-30 17:00:13 +0000

    Handling removing <original-package> tag
    This reverts commit I7b9d3c786d653a9b00a553e89d7390bcef45c0f2.
    
    Fixes: 169414761
    Test: Manual
    Test: Add package A
    Test: Verify package A exists
    Test: Remove A, add package B w/ original-package A
    Test: Verify package A exists, but codepath is to package B
    Test: Replace package B w/o original-package A
    Test: Verify package A removed and package B added
    Change-Id: I198ca545f4fea3ae9d2328a845cbd9167eedf8da

--- services/core/java/com/android/server/pm/Settings.java
+++ services/core/java/com/android/server/pm/Settings.java
@@ -476,6 +476,10 @@ public final class Settings {
         return mRenamedPackages.put(pkgName, origPkgName);
     }
 
+    void removeRenamedPackageLPw(String pkgName) {
+        mRenamedPackages.remove(pkgName);
+    }
+
     public boolean canPropagatePermissionToInstantApp(String permName) {
         return mPermissions.canPropagatePermissionToInstantApp(permName);
     }
