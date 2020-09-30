336ad77e71b9564c8ead9a10021876e9c71906fc
Kevin Chyn <kchyn@google.com>
2020-09-30 16:47:48 -0700

    Handle case where ClientMonitors should not be started
    Currently, the only case is when the HAL is null. For this, there are
    two cases:
    
    1) Non-BiometricPrompt operation
    For these, clean up the current client, and do the same for any
    operation that's currently pending. See code comments for more
    details
    
    2) BiometricPrompt operation
    For these, just clean up the current BiometricPrompt operation
    
    Fixes: 165636620
    Test: atest BiometricSchedulerTest
    Test: smoke test: enroll, auth on existing devices
    Change-Id: Iff64d5dba8dc6ebd73f0f9fba233390e76ca8a89

--- services/tests/servicestests/src/com/android/server/biometrics/sensors/BiometricSchedulerTest.java
+++ services/tests/servicestests/src/com/android/server/biometrics/sensors/BiometricSchedulerTest.java
@@ -16,11 +16,21 @@
 
 package com.android.server.biometrics.sensors;
 
+import static junit.framework.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import android.content.Context;
+import android.hardware.biometrics.IBiometricService;
 import android.platform.test.annotations.Presubmit;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.test.InstrumentationRegistry;
 import androidx.test.filters.SmallTest;
 
@@ -40,18 +50,21 @@ public class BiometricSchedulerTest {
     @Mock
     private Context mContext;
     @Mock
-    private ClientMonitor.LazyDaemon<Object> mLazyDaemon;
+    private IBiometricService mBiometricService;
 
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
-        mScheduler = new BiometricScheduler(TAG, null /* gestureAvailabilityTracker */);
+        mScheduler = new BiometricScheduler(TAG, null /* gestureAvailabilityTracker */,
+                mBiometricService);
     }
 
     @Test
     public void testClientDuplicateFinish_ignoredBySchedulerAndDoesNotCrash() {
-        final ClientMonitor<Object> client1 = new TestClientMonitor(mContext, mLazyDaemon);
-        final ClientMonitor<Object> client2 = new TestClientMonitor(mContext, mLazyDaemon);
+        final ClientMonitor.LazyDaemon<Object> nonNullDaemon = () -> mock(Object.class);
+
+        final ClientMonitor<Object> client1 = new TestClientMonitor(mContext, nonNullDaemon);
+        final ClientMonitor<Object> client2 = new TestClientMonitor(mContext, nonNullDaemon);
         mScheduler.scheduleClientMonitor(client1);
         mScheduler.scheduleClientMonitor(client2);
 
@@ -59,7 +72,93 @@ public class BiometricSchedulerTest {
         client1.mCallback.onClientFinished(client1, true /* success */);
     }
 
+    @Test
+    public void testRemovesPendingOperations_whenNullHal_andNotBiometricPrompt() {
+        // Even if second client has a non-null daemon, it needs to be canceled.
+        Object daemon2 = mock(Object.class);
+
+        final ClientMonitor.LazyDaemon<Object> lazyDaemon1 = () -> null;
+        final ClientMonitor.LazyDaemon<Object> lazyDaemon2 = () -> daemon2;
+
+        final TestClientMonitor client1 = new TestClientMonitor(mContext, lazyDaemon1);
+        final TestClientMonitor client2 = new TestClientMonitor(mContext, lazyDaemon2);
+
+        final ClientMonitor.Callback callback1 = mock(ClientMonitor.Callback.class);
+        final ClientMonitor.Callback callback2 = mock(ClientMonitor.Callback.class);
+
+        // Pretend the scheduler is busy so the first operation doesn't start right away. We want
+        // to pretend like there are two operations in the queue before kicking things off
+        mScheduler.mCurrentOperation = new BiometricScheduler.Operation(
+                mock(ClientMonitor.class), mock(ClientMonitor.Callback.class));
+
+        mScheduler.scheduleClientMonitor(client1, callback1);
+        assertEquals(1, mScheduler.mPendingOperations.size());
+        // client1 is pending. Allow the scheduler to start once second client is added.
+        mScheduler.mCurrentOperation = null;
+        mScheduler.scheduleClientMonitor(client2, callback2);
+        waitForIdle();
+
+        assertTrue(client1.wasUnableToStart());
+        verify(callback1).onClientFinished(eq(client1), eq(false) /* success */);
+        verify(callback1, never()).onClientStarted(any());
+
+        assertTrue(client2.wasUnableToStart());
+        verify(callback2).onClientFinished(eq(client2), eq(false) /* success */);
+        verify(callback2, never()).onClientStarted(any());
+
+        assertTrue(mScheduler.mPendingOperations.isEmpty());
+    }
+
+    @Test
+    public void testRemovesOnlyBiometricPromptOperation_whenNullHal() {
+        // Second non-BiometricPrompt client has a valid daemon
+        final Object daemon2 = mock(Object.class);
+
+        final ClientMonitor.LazyDaemon<Object> lazyDaemon1 = () -> null;
+        final ClientMonitor.LazyDaemon<Object> lazyDaemon2 = () -> daemon2;
+
+        final TestClientMonitor client1 =
+                new TestBiometricPromptClientMonitor(mContext, lazyDaemon1);
+        final TestClientMonitor client2 = new TestClientMonitor(mContext, lazyDaemon2);
+
+        final ClientMonitor.Callback callback1 = mock(ClientMonitor.Callback.class);
+        final ClientMonitor.Callback callback2 = mock(ClientMonitor.Callback.class);
+
+        // Pretend the scheduler is busy so the first operation doesn't start right away. We want
+        // to pretend like there are two operations in the queue before kicking things off
+        mScheduler.mCurrentOperation = new BiometricScheduler.Operation(
+                mock(ClientMonitor.class), mock(ClientMonitor.Callback.class));
+
+        mScheduler.scheduleClientMonitor(client1, callback1);
+        assertEquals(1, mScheduler.mPendingOperations.size());
+        // client1 is pending. Allow the scheduler to start once second client is added.
+        mScheduler.mCurrentOperation = null;
+        mScheduler.scheduleClientMonitor(client2, callback2);
+        waitForIdle();
+
+        // Simulate that the BiometricPrompt client's sensor is ready
+        mScheduler.startPreparedClient(client1.getCookie());
+
+        assertTrue(client1.wasUnableToStart());
+        verify(callback1).onClientFinished(eq(client1), eq(false) /* success */);
+        verify(callback1, never()).onClientStarted(any());
+
+        // Client 2 was able to start
+        assertFalse(client2.wasUnableToStart());
+        assertTrue(client2.hasStarted());
+        verify(callback2).onClientStarted(eq(client2));
+    }
+
+    private static class TestBiometricPromptClientMonitor extends TestClientMonitor {
+        public TestBiometricPromptClientMonitor(@NonNull Context context,
+                @NonNull LazyDaemon<Object> lazyDaemon) {
+            super(context, lazyDaemon, 1 /* cookie */);
+        }
+    }
+
     private static class TestClientMonitor extends ClientMonitor<Object> {
+        private boolean mUnableToStart;
+        private boolean mStarted;
 
         public TestClientMonitor(@NonNull Context context, @NonNull LazyDaemon<Object> lazyDaemon) {
             super(context, lazyDaemon, null /* token */, null /* listener */, 0 /* userId */,
@@ -67,14 +166,42 @@ public class BiometricSchedulerTest {
                     0 /* statsAction */, 0 /* statsClient */);
         }
 
+        public TestClientMonitor(@NonNull Context context, @NonNull LazyDaemon<Object> lazyDaemon,
+                int cookie) {
+            super(context, lazyDaemon, null /* token */, null /* listener */, 0 /* userId */,
+                    TAG, cookie, 0 /* sensorId */, 0 /* statsModality */,
+                    0 /* statsAction */, 0 /* statsClient */);
+        }
+
+
         @Override
         public void unableToStart() {
+            assertFalse(mUnableToStart);
+            mUnableToStart = true;
+        }
 
+        @Override
+        public void start(@NonNull Callback callback) {
+            super.start(callback);
+            assertFalse(mStarted);
+            mStarted = true;
         }
 
         @Override
         protected void startHalOperation() {
 
         }
+
+        public boolean wasUnableToStart() {
+            return mUnableToStart;
+        }
+
+        public boolean hasStarted() {
+            return mStarted;
+        }
+    }
+
+    private static void waitForIdle() {
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
     }
 }
