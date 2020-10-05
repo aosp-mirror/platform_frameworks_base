1bebb0e10ebe0ba00707187542af48a66e478443
Danny Lin <danny@kdrag0n.dev>
2020-10-05 12:36:35 -0700

    Add support for app signature spoofing
    This is needed by microG GmsCore to pretend to be the official Google
    Play Services package, because client apps check the package signature
    to make sure it matches Google's official certificate.
    
    This was forward-ported from the Android 10 patch by gudenau:
    https://github.com/microg/android_packages_apps_GmsCore/pull/957
    
    Changes made for Android 11:
      - Updated PackageInfo calls
      - Added new permission to public API surface, needed for
        PermissionController which is now an updatable APEX on 11
      - Added a dummy permission group to allow users to manage the
        permission through the PermissionController UI
        (by Vachounet <vachounet@live.fr>)
      - Updated location provider comment for conciseness
    
    Change-Id: Ied7d6ce0b83a2d2345c3abba0429998d86494a88

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4405,8 +4405,9 @@ public class PackageManagerService extends IPackageManager.Stub
                 });
             }
 
-            PackageInfo packageInfo = PackageInfoUtils.generate(p, gids, flags,
-                    ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps);
+            PackageInfo packageInfo = mayFakeSignature(p, PackageInfoUtils.generate(p, gids, flags,
+                    ps.firstInstallTime, ps.lastUpdateTime, permissions, state, userId, ps),
+                    permissions);
 
             if (packageInfo == null) {
                 return null;
@@ -4440,6 +4441,24 @@ public class PackageManagerService extends IPackageManager.Stub
         } else {
             return null;
         }
+    }
+
+    private PackageInfo mayFakeSignature(AndroidPackage p, PackageInfo pi,
+            Set<String> permissions) {
+        try {
+            if (permissions.contains("android.permission.FAKE_PACKAGE_SIGNATURE")
+                    && p.getTargetSdkVersion() > Build.VERSION_CODES.LOLLIPOP_MR1
+                    && p.getMetaData() != null) {
+                String sig = p.getMetaData().getString("fake-signature");
+                if (sig != null) {
+                    pi.signatures = new Signature[] {new Signature(sig)};
+                }
+            }
+        } catch (Throwable t) {
+            // We should never die because of any failures, this is system code!
+            Log.w("PackageManagerService.FAKE_PACKAGE_SIGNATURE", t);
+        }
+        return pi;
     }
 
     @Override
