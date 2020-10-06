7cdc3bf90fc9205bb771cf1974e65dbc0e5f05af
Jeff Sharkey <jsharkey@android.com>
2020-10-06 14:55:37 -0600

    Upgrade AndroidFrameworkBinderIdentity to fatal.
    We've fixed all outstanding examples of malformed code, so we're
    ready to upgrade this Error Prone detector to break the build to
    ensure that all future code follows best-practices.
    
    Bug: 155703208
    Test: make
    Change-Id: I617b8e7a8f96a4299ed0144bd915b87920f1af87

--- errorprone/tests/java/com/google/errorprone/bugpatterns/android/BinderIdentityCheckerTest.java
+++ errorprone/tests/java/com/google/errorprone/bugpatterns/android/BinderIdentityCheckerTest.java
@@ -89,7 +89,7 @@ public class BinderIdentityCheckerTest {
                         "  }",
                         "  void noFinal() {",
                         "    // BUG: Diagnostic contains:",
-                        "    final long token = Binder.clearCallingIdentity();",
+                        "    long token = Binder.clearCallingIdentity();",
                         "    try {",
                         "      FooService.class.toString();",
                         "    } finally {",
