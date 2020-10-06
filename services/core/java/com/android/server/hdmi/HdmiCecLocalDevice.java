b81216be7f13aaececf9e15389acea34f00542a5
Marvin Ramin <marvinramin@google.com>
2020-10-06 10:26:09 +0200

    Report Features upon initialization
    Upon successfully allocating a logical address, every device should
    broadcast a <Report Features> message.
    
    Bug: 166227212
    Test: atest HdmiCecStartupTest#
    Change-Id: I371f060b2b972a0cdaecc123b61b4f24adefb06f

--- services/core/java/com/android/server/hdmi/HdmiCecLocalDevice.java
+++ services/core/java/com/android/server/hdmi/HdmiCecLocalDevice.java
@@ -18,6 +18,7 @@ package com.android.server.hdmi;
 
 import android.annotation.CallSuper;
 import android.annotation.Nullable;
+import android.hardware.hdmi.HdmiControlManager;
 import android.hardware.hdmi.HdmiDeviceInfo;
 import android.hardware.hdmi.IHdmiControlCallback;
 import android.hardware.input.InputManager;
@@ -560,10 +561,15 @@ abstract class HdmiCecLocalDevice {
     protected abstract List<Integer> getDeviceFeatures();
 
     protected boolean handleGiveFeatures(HdmiCecMessage message) {
-        if (mService.getCecVersion() < Constants.VERSION_2_0) {
+        if (mService.getCecVersion() < HdmiControlManager.HDMI_CEC_VERSION_2_0) {
             return false;
         }
 
+        reportFeatures();
+        return true;
+    }
+
+    protected void reportFeatures() {
         List<Integer> localDeviceTypes = new ArrayList<>();
         for (HdmiCecLocalDevice localDevice : mService.getAllLocalDevices()) {
             localDeviceTypes.add(localDevice.mDeviceType);
@@ -577,7 +583,6 @@ abstract class HdmiCecLocalDevice {
         mService.sendCecCommand(
                 HdmiCecMessageBuilder.buildReportFeatures(mAddress, mService.getCecVersion(),
                         localDeviceTypes, rcProfile, rcFeatures, deviceFeatures));
-        return true;
     }
 
     @ServiceThreadOnly
@@ -789,6 +794,9 @@ abstract class HdmiCecLocalDevice {
     final void handleAddressAllocated(int logicalAddress, int reason) {
         assertRunOnServiceThread();
         mAddress = mPreferredAddress = logicalAddress;
+        if (mService.getCecVersion() >= HdmiControlManager.HDMI_CEC_VERSION_2_0) {
+            reportFeatures();
+        }
         onAddressAllocated(logicalAddress, reason);
         setPreferredAddress(logicalAddress);
     }
