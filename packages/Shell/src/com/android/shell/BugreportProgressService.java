2d38f6e4f04953a07f80984b50d3efefa64cb374
Gavin Corkery <gavincorkery@google.com>
2020-10-20 10:00:11 +0100

    Add immutability flag to PendingIntents
    Explicitly set FLAG_IMMUTABLE for all PendingIntents
    in BugreportProgressService.
    
    Test: Builds
    Bug: 170165227
    Bug: 171830604
    Change-Id: Iae1489b9c8fa323448e60615e99ad96aee1cd1d7

--- packages/Shell/src/com/android/shell/BugreportProgressService.java
+++ packages/Shell/src/com/android/shell/BugreportProgressService.java
@@ -792,7 +792,7 @@ public class BugreportProgressService extends Service {
         intent.setClass(context, BugreportProgressService.class);
         intent.putExtra(EXTRA_ID, info.id);
         return PendingIntent.getService(context, info.id, intent,
-                PendingIntent.FLAG_UPDATE_CURRENT);
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
     }
 
     /**
@@ -1252,7 +1252,7 @@ public class BugreportProgressService extends Service {
                 .setTicker(title)
                 .setContentText(content)
                 .setContentIntent(PendingIntent.getService(mContext, info.id, shareIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT))
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE))
                 .setOnlyAlertOnce(false)
                 .setDeleteIntent(newCancelIntent(mContext, info));
 
