af501bcaf97e9910fcd9c9698722e2661fc2fc63
Tony Mak <tonymak@google.com>
2020-10-22 20:49:53 +0100

    Reduce the latency between the selection span is adjusted and the toolbar is shown
    Results:
    We measure the latency between Editor.startActionModeInternal is called
    and FloatingToolbar.show() is called.
    
    When there is only smart action (url)
    Before: ~150ms
    After ~30ms
    
    When there are 5 smart actions (phone number)
    Before: ~400ms
    After: ~100ms
    
    Before and after videos:
    https://recall.googleplex.com/projects/ea8c4705-96bd-46f0-9f37-786708050727
    
    Fixed a few issues:
    1. updateAssistMenuItems() gets the Icons from TextClassification
       object, calls loadDrawable on them and creates the MenuItem
       objects loadDrawable() is slow, especially if we have a lot of
       smart action icons to load. Even worse, we are calling this
       function 4 times in a row when selecting something! 1 time from
       onCreateActionMode and 3 times from onPrepareActionMode.
       The fix here is to avoid reloading the drawable if it is the same
       text classification object.
    
    2. From SelectionActionModeHelper, we call startActionModeInternal
    before SelectionModifierCursorController.show()
    Internally, SelectionModifierCursorController.show()
    show the two selection handles  by calling startHandle.show() and
    endHandle.show(). Apparently, each handle.show() call invalidates the
    action model right after it is just created! This explains two of the
    unnecessary onPrepareActionModel calls.
    
    The fix is to call SelectionModifierCursorController.show()
    before startActionModeInternal() is called.
    
    3. Editor.startActionModeInternal() does not invoke
    FloatingToolbar.show() right away.
    There are two issues here.
    a) Editor.startActionModeInternal() ends up calling
    FloatingActionModel.repositionToolbar() which hopefully calls
    FloatingToolbar.show(). Sadly, it is not the case
    because mViewRectOnScreen is not set at that time. mViewRectOnScreen
    is set in next onPreDrawCall() call.
    
    b) When mViewRectOnScreen is finally set and calls repositionToolbar()
    again , it still won't call FloatingToolbar.show() immediately
    because we think that the toolbar is moving by comparing the previous
    content rect and the current content rect. They are different because
    the previous one is empty(it wasn't shown before). Becoz we think it is
    moving, we schedule the FloatingToolbar.show() call after 50ms.
    
    To fix a, we now update mViewRectOnScreen() right away wihout waiting
    for the next onPreDraw call().
    To fix b, when the previous content rect is moving, we don't consider
    the toolbar as moving anymore.
    
    Bug: 169043706
    
    Test: atest android.widget.TextViewActivityTest
    Test: atest
    cts/tests/tests/textclassifier/src/android/view/textclassifier/cts/TextViewIntegrationTest.java
    Test: atest
    cts/tests/tests/widget/src/android/widget/cts/TextViewTest.java
    Test: Smart select a phone number and then smart select a link.
          Make sure the smart actions menuitems are updated.
    Test: Click on a smart linkify link. Then dismiss it by tapping outside.
    
    Change-Id: I634b21ac7ed66a14883dc17e03ef006df5b3f223

--- core/java/android/widget/Editor.java
+++ core/java/android/widget/Editor.java
@@ -4105,6 +4105,8 @@ public class Editor {
         private final boolean mHasSelection;
         private final int mHandleHeight;
         private final Map<MenuItem, OnClickListener> mAssistClickHandlers = new HashMap<>();
+        @Nullable
+        private TextClassification mPrevTextClassification;
 
         TextActionModeCallback(@TextActionMode int mode) {
             mHasSelection = mode == TextActionMode.SELECTION
@@ -4255,13 +4257,17 @@ public class Editor {
         }
 
         private void updateAssistMenuItems(Menu menu) {
-            clearAssistMenuItems(menu);
-            if (!shouldEnableAssistMenuItems()) {
-                return;
-            }
             final TextClassification textClassification =
                     getSelectionActionModeHelper().getTextClassification();
+            if (mPrevTextClassification == textClassification) {
+                // Already handled.
+                return;
+            }
+            clearAssistMenuItems(menu);
             if (textClassification == null) {
+                return;
+            }
+            if (!shouldEnableAssistMenuItems()) {
                 return;
             }
             if (!textClassification.getActions().isEmpty()) {
@@ -4290,6 +4296,7 @@ public class Editor {
                         MENU_ITEM_ORDER_SECONDARY_ASSIST_ACTIONS_START + i - 1,
                         MenuItem.SHOW_AS_ACTION_NEVER);
             }
+            mPrevTextClassification = textClassification;
         }
 
         private MenuItem addAssistMenuItem(Menu menu, RemoteAction action, int itemId, int order,
