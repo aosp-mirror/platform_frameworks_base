8f692ef01a8ca95e87af317ff1fa6f0b75f6e616
Riddle Hsu <riddlehsu@google.com>
2020-10-27 22:32:33 +0800

    Finish fixed rotation of recents if it is invisible
    The recents animation may be finished due to device is going to
    sleep, and then the recents activity will be invisible so there
    won't be a transition finish callback due to no visibility change
    when cleaning up recents animation.
    
    So if recents activity was invisible before finishing animation,
    just follow the current display orientation and reset the fixed
    rotation states.
    
    Bug: 171264143
    Test: atest RecentsAnimationControllerTest# \
          testClearFixedRotationLaunchingAppAfterCleanupAnimation
    
    Change-Id: Ic2028b5e1d960d1dcebbc1a56f44f93326335d57
    (cherry picked from commit 9f72932f9d35cebcd128987f88b24293e5c7f1d4)

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -5703,7 +5703,8 @@ class DisplayContent extends WindowContainer<DisplayContent.DisplayChildWindowCo
                 return;
             }
 
-            if (animatingRecents != null && animatingRecents == mFixedRotationLaunchingApp) {
+            if (animatingRecents != null && animatingRecents == mFixedRotationLaunchingApp
+                    && animatingRecents.isVisible()) {
                 // The recents activity should be going to be invisible (switch to another app or
                 // return to original top). Only clear the top launching record without finishing
                 // the transform immediately because it won't affect display orientation. And before
