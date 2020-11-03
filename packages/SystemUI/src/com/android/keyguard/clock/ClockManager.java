775ac020091d6138e6be8fbc3048e98783bc54a5
Danny Lin <danny@kdrag0n.dev>
2020-11-03 13:10:44 -0800

    SystemUI: Restore analog and bubble clock styles
    These will be exposed in ThemePicker.
    
    Change-Id: Ifb83f37ab29b5d0583ae9682c0943cd501aa46ac

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -150,6 +150,8 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new BubbleClockController(res, layoutInflater, colorExtractor));
+        addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
