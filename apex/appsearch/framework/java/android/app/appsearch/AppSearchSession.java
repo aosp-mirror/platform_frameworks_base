b71cb7a596862edf86c29ee0463de196f612dc08
Terry Wang <tytytyww@google.com>
2020-11-03 17:03:56 -0800

    Porting the async AppSearchSession to platform.
    AppSearchSession has different context in platform, it's not directly
    ported from jetpack.
    
    This change switch AppSearch in platform from synchronize to async.
    AppSearch platform Async design doc:
    https://docs.google.com/document/d/1YGrM4s65ZhWy0P1zdI1ssfjWuCctOKxdxUKzUYS5MyY/edit?usp=sharing
    
    Changes included:
    #89b527b: Replace AppSearchManager with a SearchSession.
    
    Bug: 162450968
    Test: presubmit
    Change-Id: I4c7e1b618b6f8eab2087813c32d15339b3daa8ab

---
+++ apex/appsearch/framework/java/android/app/appsearch/AppSearchSession.java
@@ -0,0 +1,312 @@
+/*
+ * Copyright 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.app.appsearch;
+
+import android.annotation.CallbackExecutor;
+import android.annotation.NonNull;
+import android.app.appsearch.exceptions.AppSearchException;
+import android.os.Bundle;
+import android.os.ParcelableException;
+import android.os.RemoteException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.Executor;
+import java.util.function.Consumer;
+
+/**
+ * Represents a connection to an AppSearch storage system where {@link GenericDocument}s can be
+ * placed and queried.
+ * @hide
+ */
+public final class AppSearchSession {
+    private final String mDatabaseName;
+    private final IAppSearchManager mService;
+
+    static void createSearchSession(
+            @NonNull AppSearchManager.SearchContext searchContext,
+            @NonNull IAppSearchManager service,
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull Consumer<AppSearchResult<AppSearchSession>> callback) {
+        AppSearchSession searchSession =
+                new AppSearchSession(searchContext.mDatabaseName, service);
+        searchSession.initialize(executor, callback);
+    }
+
+    // NOTE: No instance of this class should be created or returned except via initialize().
+    // Once the callback.accept has been called here, the class is ready to use.
+    private void initialize(
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull Consumer<AppSearchResult<AppSearchSession>> callback) {
+        try {
+            mService.initialize(new IAppSearchResultCallback.Stub() {
+                public void onResult(AppSearchResult result) {
+                    executor.execute(() -> {
+                        if (result.isSuccess()) {
+                            callback.accept(
+                                    AppSearchResult.newSuccessfulResult(AppSearchSession.this));
+                        } else {
+                            callback.accept(result);
+                        }
+                    });
+                }
+            });
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    private AppSearchSession(@NonNull String databaseName, @NonNull IAppSearchManager service) {
+        mDatabaseName = databaseName;
+        mService = service;
+    }
+
+    /**
+     * Sets the schema will be used by documents provided to the {@link #putDocuments} method.
+     *
+     * <p>The schema provided here is compared to the stored copy of the schema previously supplied
+     * to {@link #setSchema}, if any, to determine how to treat existing documents. The following
+     * types of schema modifications are always safe and are made without deleting any existing
+     * documents:
+     * <ul>
+     *     <li>Addition of new types
+     *     <li>Addition of new
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_OPTIONAL OPTIONAL} or
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_REPEATED REPEATED} properties to a
+     *         type
+     *     <li>Changing the cardinality of a data type to be less restrictive (e.g. changing an
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_OPTIONAL OPTIONAL} property into a
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_REPEATED REPEATED} property.
+     * </ul>
+     *
+     * <p>The following types of schema changes are not backwards-compatible:
+     * <ul>
+     *     <li>Removal of an existing type
+     *     <li>Removal of a property from a type
+     *     <li>Changing the data type ({@code boolean}, {@code long}, etc.) of an existing property
+     *     <li>For properties of {@code Document} type, changing the schema type of
+     *         {@code Document}s of that property
+     *     <li>Changing the cardinality of a data type to be more restrictive (e.g. changing an
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_OPTIONAL OPTIONAL} property into a
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_REQUIRED REQUIRED} property).
+     *     <li>Adding a
+     *         {@link AppSearchSchema.PropertyConfig#CARDINALITY_REQUIRED REQUIRED} property.
+     * </ul>
+     * <p>Supplying a schema with such changes will, by default, result in this call returning an
+     * {@link AppSearchResult} with a code of {@link AppSearchResult#RESULT_INVALID_SCHEMA} and an
+     * error message describing the incompatibility. In this case the previously set schema will
+     * remain active.
+     *
+     * <p>If you need to make non-backwards-compatible changes as described above, you can set the
+     * {@link SetSchemaRequest.Builder#setForceOverride} method to {@code true}. In this case,
+     * instead of returning an {@link AppSearchResult} with the
+     * {@link AppSearchResult#RESULT_INVALID_SCHEMA} error code, all documents which are not
+     * compatible with the new schema will be deleted and the incompatible schema will be applied.
+     *
+     * <p>It is a no-op to set the same schema as has been previously set; this is handled
+     * efficiently.
+     *
+     * @param request The schema update request.
+     * @param executor Executor on which to invoke the callback.
+     * @param callback Callback to receive errors resulting from setting the schema. If the
+     *                 operation succeeds, the callback will be invoked with {@code null}.
+     */
+    public void setSchema(
+            @NonNull SetSchemaRequest request,
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull Consumer<AppSearchResult<Void>> callback) {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(executor);
+        Objects.requireNonNull(callback);
+        List<Bundle> schemaBundles = new ArrayList<>(request.getSchemas().size());
+        for (AppSearchSchema schema : request.getSchemas()) {
+            schemaBundles.add(schema.getBundle());
+        }
+        try {
+            mService.setSchema(mDatabaseName, schemaBundles, request.isForceOverride(),
+                    new IAppSearchResultCallback.Stub() {
+                        public void onResult(AppSearchResult result) {
+                            executor.execute(() -> callback.accept(result));
+                        }
+                    });
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Indexes documents into AppSearch.
+     *
+     * <p>Each {@link GenericDocument}'s {@code schemaType} field must be set to the name of a
+     * schema type previously registered via the {@link #setSchema} method.
+     *
+     * @param request  {@link PutDocumentsRequest} containing documents to be indexed
+     * @param executor Executor on which to invoke the callback.
+     * @param callback Callback to receive pending result of performing this operation. The keys
+     *                 of the returned {@link AppSearchBatchResult} are the URIs of the input
+     *                 documents. The values are {@code null} if they were successfully indexed,
+     *                 or a failed {@link AppSearchResult} otherwise.
+     *                 Or {@link BatchResultCallback#onSystemError} will be invoked with an
+     *                 {@link AppSearchException} if an error occurred in AppSearch initialization
+     *                 or a cause {@link Throwable} if other error occurred in AppSearch service.
+     */
+    public void putDocuments(
+            @NonNull PutDocumentsRequest request,
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull BatchResultCallback<String, Void> callback) {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(executor);
+        Objects.requireNonNull(callback);
+        List<GenericDocument> documents = request.getDocuments();
+        List<Bundle> documentBundles = new ArrayList<>(documents.size());
+        for (int i = 0; i < documents.size(); i++) {
+            documentBundles.add(documents.get(i).getBundle());
+        }
+        try {
+            mService.putDocuments(mDatabaseName, documentBundles,
+                    new IAppSearchBatchResultCallback.Stub() {
+                        public void onResult(AppSearchBatchResult result) {
+                            executor.execute(() -> callback.onResult(result));
+                        }
+
+                        public void onSystemError(ParcelableException exception) {
+                            executor.execute(() -> callback.onSystemError(exception.getCause()));
+                        }
+                    });
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Retrieves {@link GenericDocument}s by URI.
+     *
+     * @param request {@link GetByUriRequest} containing URIs to be retrieved.
+     * @param executor Executor on which to invoke the callback.
+     * @param callback Callback to receive the pending result of performing this operation. The keys
+     *                 of the returned {@link AppSearchBatchResult} are the input URIs. The values
+     *                 are the returned {@link GenericDocument}s on success, or a failed
+     *                 {@link AppSearchResult} otherwise. URIs that are not found will return a
+     *                 failed {@link AppSearchResult} with a result code of
+     *                 {@link AppSearchResult#RESULT_NOT_FOUND}.
+     *                 Or {@link BatchResultCallback#onSystemError} will be invoked with an
+     *                 {@link AppSearchException} if an error occurred in AppSearch initialization
+     *                 or a cause {@link Throwable} if other error occurred in AppSearch service.
+     */
+    public void getByUri(
+            @NonNull GetByUriRequest request,
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull BatchResultCallback<String, GenericDocument> callback) {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(executor);
+        Objects.requireNonNull(callback);
+        try {
+            mService.getDocuments(mDatabaseName, request.getNamespace(),
+                    new ArrayList<>(request.getUris()),
+                    new IAppSearchBatchResultCallback.Stub() {
+                        public void onResult(AppSearchBatchResult result) {
+                            executor.execute(() -> {
+                                AppSearchBatchResult.Builder<String, GenericDocument>
+                                        documentResultBuilder =
+                                        new AppSearchBatchResult.Builder<>();
+
+                                // Translate successful results
+                                for (Map.Entry<String, Bundle> bundleEntry :
+                                        (Set<Map.Entry<String, Bundle>>)
+                                                result.getSuccesses().entrySet()) {
+                                    GenericDocument document;
+                                    try {
+                                        document = new GenericDocument(bundleEntry.getValue());
+                                    } catch (Throwable t) {
+                                        // These documents went through validation, so how could
+                                        // this fail? We must have done something wrong.
+                                        documentResultBuilder.setFailure(
+                                                bundleEntry.getKey(),
+                                                AppSearchResult.RESULT_INTERNAL_ERROR,
+                                                t.getMessage());
+                                        continue;
+                                    }
+                                    documentResultBuilder.setSuccess(
+                                            bundleEntry.getKey(), document);
+                                }
+
+                                // Translate failed results
+                                for (Map.Entry<String, AppSearchResult<Bundle>> bundleEntry :
+                                        (Set<Map.Entry<String, AppSearchResult<Bundle>>>)
+                                                result.getFailures().entrySet()) {
+                                    documentResultBuilder.setFailure(
+                                            bundleEntry.getKey(),
+                                            bundleEntry.getValue().getResultCode(),
+                                            bundleEntry.getValue().getErrorMessage());
+                                }
+                                callback.onResult(documentResultBuilder.build());
+                            });
+                        }
+
+                        public void onSystemError(ParcelableException exception) {
+                            executor.execute(() -> callback.onSystemError(exception.getCause()));
+                        }
+                    });
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    /**
+     * Removes {@link GenericDocument}s from the index by URI.
+     *
+     * @param request Request containing URIs to be removed.
+     * @param executor Executor on which to invoke the callback.
+     * @param callback Callback to receive the pending result of performing this operation. The keys
+     *                 of the returned {@link AppSearchBatchResult} are the input URIs. The values
+     *                 are {@code null} on success, or a failed {@link AppSearchResult} otherwise.
+     *                 URIs that are not found will return a failed {@link AppSearchResult} with a
+     *                 result code of {@link AppSearchResult#RESULT_NOT_FOUND}.
+     *                 Or {@link BatchResultCallback#onSystemError} will be invoked with an
+     *                 {@link AppSearchException} if an error occurred in AppSearch initialization
+     *                 or a cause {@link Throwable} if other error occurred in AppSearch service.
+     */
+    public void removeByUri(
+            @NonNull RemoveByUriRequest request,
+            @NonNull @CallbackExecutor Executor executor,
+            @NonNull BatchResultCallback<String, Void> callback) {
+        Objects.requireNonNull(request);
+        Objects.requireNonNull(executor);
+        Objects.requireNonNull(callback);
+        try {
+            mService.removeByUri(mDatabaseName, request.getNamespace(),
+                    new ArrayList<>(request.getUris()),
+                    new IAppSearchBatchResultCallback.Stub() {
+                        public void onResult(AppSearchBatchResult result) {
+                            executor.execute(() -> callback.onResult(result));
+                        }
+
+                        public void onSystemError(ParcelableException exception) {
+                            executor.execute(() -> callback.onSystemError(exception.getCause()));
+                        }
+                    });
+        } catch (RemoteException e) {
+            throw e.rethrowFromSystemServer();
+        }
+    }
+
+    // TODO(b/162450968) port query() and SearchResults.java to platform.
+    // TODO(b/162450968) port removeByQuery() to platform.
+}
--- apex/appsearch/framework/java/android/app/appsearch/IAppSearchManager.aidl
+++ apex/appsearch/framework/java/android/app/appsearch/IAppSearchManager.aidl
@@ -17,10 +17,12 @@ package android.app.appsearch;
 
 import android.os.Bundle;
 
+import android.app.appsearch.AppSearchBatchResult;
+import android.app.appsearch.AppSearchResult;
+import android.app.appsearch.IAppSearchBatchResultCallback;
+import android.app.appsearch.IAppSearchResultCallback;
 import com.android.internal.infra.AndroidFuture;
 
-parcelable AppSearchResult;
-parcelable AppSearchBatchResult;
 parcelable SearchResults;
 
 /** {@hide} */
@@ -32,14 +34,14 @@ interface IAppSearchManager {
      * @param schemaBundles List of AppSearchSchema bundles.
      * @param forceOverride Whether to apply the new schema even if it is incompatible. All
      *     incompatible documents will be deleted.
-     * @param callback {@link AndroidFuture}&lt;{@link AppSearchResult}&lt;{@link Void}&gt&gt;.
-     *     The results of the call.
+     * @param callback {@link IAppSearchResultCallback#onResult} will be called with an
+     *     {@link AppSearchResult}&lt;{@link Void}&gt;.
      */
     void setSchema(
         in String databaseName,
         in List<Bundle> schemaBundles,
         boolean forceOverride,
-        in AndroidFuture<AppSearchResult> callback);
+        in IAppSearchResultCallback callback);
 
     /**
      * Inserts documents into the index.
@@ -47,16 +49,16 @@ interface IAppSearchManager {
      * @param databaseName  The name of the database where this document lives.
      * @param documentBundes List of GenericDocument bundles.
      * @param callback
-     *     {@link AndroidFuture}&lt;{@link AppSearchBatchResult}&lt;{@link String}, {@link Void}&gt;&gt;.
-     *     If the call fails to start, {@code callback} will be completed exceptionally. Otherwise,
-     *     {@code callback} will be completed with an
+     *     If the call fails to start, {@link IAppSearchBatchResultCallback#onSystemError}
+     *     will be called with the cause throwable. Otherwise,
+     *     {@link IAppSearchBatchResultCallback#onResult} will be called with an
      *     {@link AppSearchBatchResult}&lt;{@link String}, {@link Void}&gt;
      *     where the keys are document URIs, and the values are {@code null}.
      */
     void putDocuments(
         in String databaseName,
         in List<Bundle> documentBundles,
-        in AndroidFuture<AppSearchBatchResult> callback);
+        in IAppSearchBatchResultCallback callback);
 
     /**
      * Retrieves documents from the index.
@@ -65,9 +67,9 @@ interface IAppSearchManager {
      * @param namespace    The namespace this document resides in.
      * @param uris The URIs of the documents to retrieve
      * @param callback
-     *     {@link AndroidFuture}&lt;{@link AppSearchBatchResult}&lt;{@link String}, {@link Bundle}&gt;&gt;.
-     *     If the call fails to start, {@code callback} will be completed exceptionally. Otherwise,
-     *     {@code callback} will be completed with an
+     *     If the call fails to start, {@link IAppSearchBatchResultCallback#onSystemError}
+     *     will be called with the cause throwable. Otherwise,
+     *     {@link IAppSearchBatchResultCallback#onResult} will be called with an
      *     {@link AppSearchBatchResult}&lt;{@link String}, {@link Bundle}&gt;
      *     where the keys are document URIs, and the values are Document bundles.
      */
@@ -75,7 +77,7 @@ interface IAppSearchManager {
         in String databaseName,
         in String namespace,
         in List<String> uris,
-        in AndroidFuture<AppSearchBatchResult> callback);
+        in IAppSearchBatchResultCallback callback);
 
     /**
      * Searches a document based on a given specifications.
@@ -98,9 +100,9 @@ interface IAppSearchManager {
      * @param namespace    Namespace of the document to remove.
      * @param uris The URIs of the documents to delete
      * @param callback
-     *     {@link AndroidFuture}&lt;{@link AppSearchBatchResult}&lt;{@link String}, {@link Void}&gt;&gt;.
-     *     If the call fails to start, {@code callback} will be completed exceptionally. Otherwise,
-     *     {@code callback} will be completed with an
+     *     If the call fails to start, {@link IAppSearchBatchResultCallback#onSystemError}
+     *     will be called with the cause throwable. Otherwise,
+     *     {@link IAppSearchBatchResultCallback#onResult} will be called with an
      *     {@link AppSearchBatchResult}&lt;{@link String}, {@link Void}&gt;
      *     where the keys are document URIs. If a document doesn't exist, it will be reported as a
      *     failure where the {@code throwable} is {@code null}.
@@ -109,7 +111,7 @@ interface IAppSearchManager {
         in String databaseName,
         in String namespace,
         in List<String> uris,
-        in AndroidFuture<AppSearchBatchResult> callback);
+        in IAppSearchBatchResultCallback callback);
 
     /**
      * Removes documents by given query.
@@ -124,4 +126,12 @@ interface IAppSearchManager {
         in String queryExpression,
         in Bundle searchSpecBundle,
         in AndroidFuture<AppSearchResult> callback);
+
+    /**
+     * Creates and initializes AppSearchImpl for the calling app.
+     *
+     * @param callback {@link IAppSearchResultCallback#onResult} will be called with an
+     *     {@link AppSearchResult}&lt;{@link Void}&gt;.
+     */
+    void initialize(in IAppSearchResultCallback callback);
 }
