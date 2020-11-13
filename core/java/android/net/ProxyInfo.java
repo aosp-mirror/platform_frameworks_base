22eea532c51048b6f59beab8b16ba45d00132378
Aaron Huang <huangaaron@google.com>
2020-11-13 23:55:09 +0800

    Rename PacManager to PacProxyInstaller
    Generally, a manager class in Android is used to access
    system services and it should be obtained from Context.
    This class is a bit different from the definition of a
    manager class.
    
    API linter will detect an error if trying to expose a
    class name end with Manager. ProxyTracker will create a
    new instance of this class so this class needs to be
    renamed to avoid API lint error.
    
    Bug: 177035719
    Test: FrameworksNetTests
    Change-Id: I9185d4fb4342bd285a575f0bdd3518b758f37eb6

--- core/java/android/net/ProxyInfo.java
+++ core/java/android/net/ProxyInfo.java
@@ -127,7 +127,7 @@ public class ProxyInfo implements Parcelable {
     }
 
     /**
-     * Only used in PacManager after Local Proxy is bound.
+     * Only used in PacProxyInstaller after Local Proxy is bound.
      * @hide
      */
     public ProxyInfo(@NonNull Uri pacFileUrl, int localProxyPort) {
