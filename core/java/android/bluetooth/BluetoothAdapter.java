95376695238711d657ee81c88ec87d4040f5ad44
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2020-11-17 09:11:17 +0000

    gtbs: Add Generic Telephone Bearer Service support
    Tag: #feature
    Bug: 159786353
    Sponsor: jpawlowski@
    Test: build
    Change-Id: I8264ade4b07df17fc0207437dfdcae7028cc20ff

--- core/java/android/bluetooth/BluetoothAdapter.java
+++ core/java/android/bluetooth/BluetoothAdapter.java
@@ -3086,6 +3086,9 @@ public final class BluetoothAdapter {
             BluetoothCsipSetCoordinator csipSetCoordinator =
                     new BluetoothCsipSetCoordinator(context, listener, this);
             return true;
+        } else if (profile == BluetoothProfile.LE_CALL_CONTROL) {
+            BluetoothLeCallControl tbs = new BluetoothLeCallControl(context, listener);
+            return true;
         } else {
             return false;
         }
@@ -3187,6 +3190,10 @@ public final class BluetoothAdapter {
                 BluetoothCsipSetCoordinator csipSetCoordinator =
                         (BluetoothCsipSetCoordinator) proxy;
                 csipSetCoordinator.close();
+                break;
+            case BluetoothProfile.LE_CALL_CONTROL:
+                BluetoothLeCallControl tbs = (BluetoothLeCallControl) proxy;
+                tbs.close();
                 break;
         }
     }
--- core/java/android/bluetooth/BluetoothProfile.java
+++ core/java/android/bluetooth/BluetoothProfile.java
@@ -240,12 +240,19 @@ public interface BluetoothProfile {
     int LE_AUDIO_BROADCAST = 26;
 
     /**
+     * @hide
+     * Telephone Bearer Service from Call Control Profile
+     *
+     */
+    int LE_CALL_CONTROL = 27;
+
+    /**
      * Max profile ID. This value should be updated whenever a new profile is added to match
      * the largest value assigned to a profile.
      *
      * @hide
      */
-    int MAX_PROFILE_ID = 26;
+    int MAX_PROFILE_ID = 27;
 
     /**
      * Default priority for devices that we try to auto-connect to and
