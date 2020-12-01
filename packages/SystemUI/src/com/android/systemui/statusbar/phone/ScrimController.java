436f8784d177b8c1126b1cc6de62be7cfd799be9
Lucas Dupin <dupin@google.com>
2020-12-01 00:41:28 +0000

    3/N The road to Material NEXT (scrim color events)
    Reload scrim colors when overlays are applied.
    They drive the system theme.
    
    Bug: 173561906
    Test: manual
    Change-Id: I1b6e446e6f67c05f391f48e305f006dbdb39caf5

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimController.java
@@ -236,6 +236,11 @@ public class ScrimController implements ViewTreeObserver.OnPreDrawListener, Dump
             }
 
             @Override
+            public void onOverlayChanged() {
+                ScrimController.this.onThemeChanged();
+            }
+
+            @Override
             public void onUiModeChanged() {
                 ScrimController.this.onThemeChanged();
             }
