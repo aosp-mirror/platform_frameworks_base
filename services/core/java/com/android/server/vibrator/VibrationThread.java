5985ce1b1398a8d4a0ffb0a8411b76e59369798e
Lais Andrade <lsandrade@google.com>
2020-12-01 17:36:58 +0000

    Implement dump and vibrate cmds for Vibrator Manager
    Keep previous vibrations to be reported in the dump method, similar to
    VibratorService.
    
    Implement some vibrate commands to test VibratorManager in the adb
    shell.
    
    Bug: 167946816
    Test: VibratorManagerTest
    Change-Id: Ie7dd89b0e3b2747454b32fe3cc2d8751daae9e19

--- services/core/java/com/android/server/vibrator/VibrationThread.java
+++ services/core/java/com/android/server/vibrator/VibrationThread.java
@@ -116,6 +116,9 @@ public final class VibrationThread extends Thread implements IBinder.DeathRecipi
 
     @Override
     public void binderDied() {
+        if (DEBUG) {
+            Slog.d(TAG, "Binder died, cancelling vibration...");
+        }
         cancel();
     }
 
@@ -146,6 +149,9 @@ public final class VibrationThread extends Thread implements IBinder.DeathRecipi
     /** Notify current vibration that a step has completed on given vibrator. */
     public void vibratorComplete(int vibratorId) {
         synchronized (mLock) {
+            if (DEBUG) {
+                Slog.d(TAG, "Vibration complete reported by vibrator " + vibratorId);
+            }
             if (mCurrentVibrateStep != null) {
                 mCurrentVibrateStep.vibratorComplete(vibratorId);
             }
@@ -467,7 +473,7 @@ public final class VibrationThread extends Thread implements IBinder.DeathRecipi
                     noteVibratorOff();
                 }
                 if (DEBUG) {
-                    Slog.d(TAG, "SingleVibrateStep step done.");
+                    Slog.d(TAG, "SingleVibrateStep done.");
                 }
                 Trace.traceEnd(Trace.TRACE_TAG_VIBRATOR);
             }
