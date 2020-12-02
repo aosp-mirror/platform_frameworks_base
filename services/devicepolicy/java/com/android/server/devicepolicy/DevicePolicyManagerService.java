be8ebd658d8ba1b8e84901c30345b3dbbcc33fa0
Alex Johnston <acjohnston@google.com>
2020-12-02 18:18:44 +0000

    DPMS isCredentialManagementApp add null check
    * Check if policy is null before checking if
      an alias is contained in the policy
    
    Bug: 174666058
    Test: atest android.admin.cts.DevicePolicyManagerTest#testGenerateKeyPair_failIfNotProfileOwner
          atest android.admin.cts.DevicePolicyManagerTest#testSetKeyPairCertificate_failIfNotProfileOwner
    Change-Id: I2e1e0d550cc5131c6042c0aa628b00abe7f04077

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -5839,7 +5839,11 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         // Should include alias in authentication policy
         try (KeyChainConnection connection = KeyChain.bindAsUser(mContext,
                 caller.getUserHandle())) {
-            if (!containsAlias(connection.getService().getCredentialManagementAppPolicy(), alias)) {
+            // The policy will be null if there is no credential management app
+            AppUriAuthenticationPolicy policy =
+                    connection.getService().getCredentialManagementAppPolicy();
+            if (policy == null || policy.getAppAndUriMappings().isEmpty()
+                    || !containsAlias(policy, alias)) {
                 return false;
             }
         } catch (RemoteException | InterruptedException e) {
