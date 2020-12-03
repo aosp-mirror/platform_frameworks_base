9613aa7fc08500830f5f98ffd2047a59453f1ba7
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-12-03 20:06:38 +0100

    LockIcon: refresh icon on overlay changes
    Change-Id: I7a7a30f567098ba32c5a8d823e93242baea2b268
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LockscreenLockIconController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LockscreenLockIconController.java
@@ -145,6 +145,11 @@ public class LockscreenLockIconController {
         private int mDensity;
 
         @Override
+        public void onOverlayChanged() {
+            onThemeChanged();
+        }
+
+        @Override
         public void onThemeChanged() {
             if (mLockIcon == null) {
                 return;
