ac6d791c75a1be5d0842416b6909907357bc00a2
Riddle Hsu <riddlehsu@google.com>
2020-12-10 00:25:02 +0800

    Reland "Do not add display area features for untrusted display"
    Untrusted display (e.g. virtual display created by app, a common
    case is screen recorder) doesn't support system decorations. There
    is nothing to do with additional features.
    
    This saves 31 areas (according to current features) per untrusted
    display. And also reduce the cost of traversing entire hierarchy.
    
    Reland since making DisplayContent#getWindowingLayer() valid by
    commit 941ad3a.
    
    Fixes: 175179353
    Test: DisplayAreaPolicyTests#testEmptyFeaturesOnUntrustedDisplay
    Test: Start Desktop Head Unit (simulation of Auto)
    Change-Id: I3929d17eab6bed3f1358d6782e363120f0edd385

--- services/core/java/com/android/server/wm/DisplayAreaPolicy.java
+++ services/core/java/com/android/server/wm/DisplayAreaPolicy.java
@@ -99,23 +99,41 @@ public abstract class DisplayAreaPolicy {
 
             // Define the features that will be supported under the root of the whole logical
             // display. The policy will build the DisplayArea hierarchy based on this.
-            HierarchyBuilder rootHierarchy = new HierarchyBuilder(root)
-                    // WindowedMagnification should be on the top so that there is only one surface
-                    // to be magnified.
-                    .addFeature(new Feature.Builder(wmService.mPolicy, "WindowedMagnification",
-                            FEATURE_WINDOWED_MAGNIFICATION)
-                            .upTo(TYPE_ACCESSIBILITY_MAGNIFICATION_OVERLAY)
-                            .except(TYPE_ACCESSIBILITY_MAGNIFICATION_OVERLAY)
-                            // Make the DA dimmable so that the magnify window also mirrors the dim
-                            // layer
-                            .setNewDisplayAreaSupplier(DisplayArea.Dimmable::new)
-                            .build())
-                    .addFeature(new Feature.Builder(wmService.mPolicy, "HideDisplayCutout",
-                            FEATURE_HIDE_DISPLAY_CUTOUT)
-                            .all()
-                            .except(TYPE_NAVIGATION_BAR, TYPE_NAVIGATION_BAR_PANEL, TYPE_STATUS_BAR,
-                                    TYPE_NOTIFICATION_SHADE)
-                            .build())
+            final HierarchyBuilder rootHierarchy = new HierarchyBuilder(root);
+            // Set the essential containers (even if the display doesn't support IME).
+            rootHierarchy.setImeContainer(imeContainer).setTaskDisplayAreas(tdaList);
+            if (content.isTrusted()) {
+                // Only trusted display can have system decorations.
+                configureTrustedHierarchyBuilder(rootHierarchy, wmService, content);
+            }
+
+            // Instantiate the policy with the hierarchy defined above. This will create and attach
+            // all the necessary DisplayAreas to the root.
+            return new DisplayAreaPolicyBuilder().setRootHierarchy(rootHierarchy).build(wmService);
+        }
+
+        private void configureTrustedHierarchyBuilder(HierarchyBuilder rootHierarchy,
+                WindowManagerService wmService, DisplayContent content) {
+            // WindowedMagnification should be on the top so that there is only one surface
+            // to be magnified.
+            rootHierarchy.addFeature(new Feature.Builder(wmService.mPolicy, "WindowedMagnification",
+                    FEATURE_WINDOWED_MAGNIFICATION)
+                    .upTo(TYPE_ACCESSIBILITY_MAGNIFICATION_OVERLAY)
+                    .except(TYPE_ACCESSIBILITY_MAGNIFICATION_OVERLAY)
+                    // Make the DA dimmable so that the magnify window also mirrors the dim layer.
+                    .setNewDisplayAreaSupplier(DisplayArea.Dimmable::new)
+                    .build());
+            if (content.isDefaultDisplay) {
+                // Only default display can have cutout.
+                // See LocalDisplayAdapter.LocalDisplayDevice#getDisplayDeviceInfoLocked.
+                rootHierarchy.addFeature(new Feature.Builder(wmService.mPolicy, "HideDisplayCutout",
+                        FEATURE_HIDE_DISPLAY_CUTOUT)
+                        .all()
+                        .except(TYPE_NAVIGATION_BAR, TYPE_NAVIGATION_BAR_PANEL, TYPE_STATUS_BAR,
+                                TYPE_NOTIFICATION_SHADE)
+                        .build());
+            }
+            rootHierarchy
                     .addFeature(new Feature.Builder(wmService.mPolicy, "OneHanded",
                             FEATURE_ONE_HANDED)
                             .all()
@@ -131,13 +149,7 @@ public abstract class DisplayAreaPolicy {
                     .addFeature(new Feature.Builder(wmService.mPolicy, "ImePlaceholder",
                             FEATURE_IME_PLACEHOLDER)
                             .and(TYPE_INPUT_METHOD, TYPE_INPUT_METHOD_DIALOG)
-                            .build())
-                    .setImeContainer(imeContainer)
-                    .setTaskDisplayAreas(tdaList);
-
-            // Instantiate the policy with the hierarchy defined above. This will create and attach
-            // all the necessary DisplayAreas to the root.
-            return new DisplayAreaPolicyBuilder().setRootHierarchy(rootHierarchy).build(wmService);
+                            .build());
         }
     }
 
