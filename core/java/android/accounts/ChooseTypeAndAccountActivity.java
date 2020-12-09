fedbadf57eeeb82813d8fd43aade685659b909c2
Dmitry Dementyev <dementyev@google.com>
2020-12-09 14:58:46 -0800

    Protect account chooser activities against overlay.
    Bug: 172841550
    Test: manual
    Merged-In: I1a16808426934f4a8d12410576d769443e4c2a04
    Merged-In: I3cd5a94386f15cf60a7fe3095b00815e4a6485ae
    Change-Id: I35dc86b5721a4531447a6d99d6c30f23543130cb

--- core/java/android/accounts/ChooseTypeAndAccountActivity.java
+++ core/java/android/accounts/ChooseTypeAndAccountActivity.java
@@ -15,8 +15,6 @@
  */
 package android.accounts;
 
-import com.google.android.collect.Sets;
-
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Intent;
@@ -37,6 +35,8 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.internal.R;
+
+import com.google.android.collect.Sets;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -142,7 +142,9 @@ public class ChooseTypeAndAccountActivity extends Activity
             Log.v(TAG, "ChooseTypeAndAccountActivity.onCreate(savedInstanceState="
                     + savedInstanceState + ")");
         }
-
+        getWindow().addPrivateFlags(
+                android.view.WindowManager.LayoutParams
+                        .PRIVATE_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
         String message = null;
 
         try {
