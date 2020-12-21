7d95185d46ac394ae9a583f4e2f03e0a01ee2f4f
SongFerngWang <songferngwang@google.com>
2020-12-21 16:24:25 +0800

    Improve AllowedNetworkTypesForReason and refactor PreferredNetworkType
    - Add more reasons of allowed network type
      - ALLOWED_NETWORK_TYPES_REASON_USER
      - ALLOWED_NETWORK_TYPES_REASON_CARRIER
    - Add @systemApi for below
      - ALLOWED_NETWORK_TYPES_REASON_USER
      - ALLOWED_NETWORK_TYPES_REASON_POWER
      - ALLOWED_NETWORK_TYPES_REASON_CARRIER
      - setAllowedNetworkTypesForReason API
      - getAllowedNetworkTypesForReason API
      - getAllowedNetworkTypesBitmask API
    - Add Public for below
      - CARRIER_NR_AVAILABILITY_NONE
      - CARRIER_NR_AVAILABILITY_NSA
      - CARRIER_NR_AVAILABILITY_SA
      - KEY_CARRIER_NR_AVAILABILITY_INT
    - @Deprecated API
      - setPreferredNetworkTypeBitmask
      - getPreferredNetworkTypeBitmask
      - setAllowedNetworkType
      - getAllowedNetworkType
    - Add a new way to notify allowedNetworkType at PhoneStateListener
    - Replace KEY_NR_ENABLED_BOOL with KEY_CARRIER_NR_AVAILABILITY_INT
    
    Bug: 161434786
    Test: atest TelephonyManagerTest#testSetAllowedNetworkTypesForReason
    (PASS)
    atest TelephonyManagerTest#\
    testSetAllowedNetworkTypesForReason_moreReason
    (PASS)
    atest PhoneStateListenerTest#\
    testOnAllowedNetworkTypesChangedByRegisterPhoneStateListener
    (PASS)
    
    Change-Id: I3d0c119a0c2a30c56b0c127ba6525f8992b4e9dd

--- services/core/java/com/android/server/TelephonyRegistry.java
+++ services/core/java/com/android/server/TelephonyRegistry.java
@@ -317,6 +317,8 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
 
     private int mDataEnabledReason;
 
+    private Map<Integer, Long> mAllowedNetworkTypesList;
+
     /**
      * Per-phone map of precise data connection state. The key of the map is the pair of transport
      * type and APN setting. This is the cache to prevent redundant callbacks to the listeners.
@@ -623,6 +625,7 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
         mBarringInfo = new ArrayList<>();
         mTelephonyDisplayInfos = new TelephonyDisplayInfo[numPhones];
         mPhysicalChannelConfigs = new ArrayList<>();
+        mAllowedNetworkTypesList = new HashMap<>();
         for (int i = 0; i < numPhones; i++) {
             mCallState[i] =  TelephonyManager.CALL_STATE_IDLE;
             mDataActivity[i] = TelephonyManager.DATA_ACTIVITY_NONE;
@@ -1148,6 +1151,14 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
                         PhoneStateListener.EVENT_DATA_ENABLED_CHANGED)) {
                     try {
                         r.callback.onDataEnabledChanged(mIsDataEnabled, mDataEnabledReason);
+                    } catch (RemoteException ex) {
+                        remove(r.binder);
+                    }
+                }
+                if (events.contains(
+                        PhoneStateListener.EVENT_ALLOWED_NETWORK_TYPE_LIST_CHANGED)) {
+                    try {
+                        r.callback.onAllowedNetworkTypesChanged(mAllowedNetworkTypesList);
                     } catch (RemoteException ex) {
                         remove(r.binder);
                     }
@@ -2383,6 +2394,44 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
                         r.callback.onDataEnabledChanged(enabled, reason);
                     } catch (RemoteException ex) {
                         mRemoveList.add(r.binder);
+                    }
+                }
+            }
+            handleRemoveListLocked();
+        }
+    }
+
+    /**
+     * Notify that the allowed network type has changed.
+     *
+     * @param phoneId the phone id.
+     * @param subId the subId.
+     * @param allowedNetworkTypesList Map associating all allowed network type reasons with reason's
+     *                                allowed network type values.
+     */
+    public void notifyAllowedNetworkTypesChanged(int phoneId, int subId,
+            Map allowedNetworkTypesList) {
+        if (!checkNotifyPermission("notifyAllowedNetworkTypesChanged()")) {
+            return;
+        }
+
+        synchronized (mRecords) {
+            if (validatePhoneId(phoneId)) {
+                mAllowedNetworkTypesList = allowedNetworkTypesList;
+
+                for (Record r : mRecords) {
+                    if (r.matchPhoneStateListenerEvent(
+                            PhoneStateListener.EVENT_ALLOWED_NETWORK_TYPE_LIST_CHANGED)
+                            && idMatch(r.subId, subId, phoneId)) {
+                        try {
+                            if (VDBG) {
+                                log("notifyAllowedNetworkTypesChanged: AllowedNetworkTypesList= "
+                                        + mAllowedNetworkTypesList.toString());
+                            }
+                            r.callback.onAllowedNetworkTypesChanged(mAllowedNetworkTypesList);
+                        } catch (RemoteException ex) {
+                            mRemoveList.add(r.binder);
+                        }
                     }
                 }
             }
