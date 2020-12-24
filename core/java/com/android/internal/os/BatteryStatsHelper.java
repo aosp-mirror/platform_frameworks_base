ef0d81b294aa8c9d554084209037b7b2604406e6
Remi NGUYEN VAN <reminv@google.com>
2020-12-24 17:08:08 +0900

    Note network interfaces based on transport
    Instead of classifying interfaces by network type in BatteryStats,
    classify them based on the transports array provided by the
    NetworkAgent.
    
    Network types are deprecated and transports should be used instead. This
    change allows BatteryStats to stop depending on unstable APIs such as
    isNetworkTypeMobile.
    
    This change also updates nullability annotations in ConnectivityService
    and NetworkAgentInfo to show that the NetworkCapabilities are non-null
    (as provided by the network agent) when calling
    noteNetworkInterfaceTransports.
    
    Bug: 174436414
    Test: atest
          atest ConnectivityServiceTest#testBatteryStatsNetworkType \
              --rerun-until-failure 40
    
    Change-Id: Icc912473d97a42eef73eb953607c9161fdbeb794

--- core/java/com/android/internal/os/BatteryStatsHelper.java
+++ core/java/com/android/internal/os/BatteryStatsHelper.java
@@ -23,7 +23,6 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
 import android.hardware.SensorManager;
-import android.net.ConnectivityManager;
 import android.os.BatteryStats;
 import android.os.BatteryStats.Uid;
 import android.os.Build;
@@ -37,6 +36,7 @@ import android.os.SELinux;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.telephony.TelephonyManager;
 import android.text.format.DateUtils;
 import android.util.ArrayMap;
 import android.util.Log;
@@ -120,12 +120,11 @@ public class BatteryStatsHelper {
     private double mMaxDrainedPower;
 
     public static boolean checkWifiOnly(Context context) {
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(
-                Context.CONNECTIVITY_SERVICE);
-        if (cm == null) {
+        final TelephonyManager tm = context.getSystemService(TelephonyManager.class);
+        if (tm == null) {
             return false;
         }
-        return !cm.isNetworkSupported(ConnectivityManager.TYPE_MOBILE);
+        return !tm.isDataCapable();
     }
 
     @UnsupportedAppUsage
