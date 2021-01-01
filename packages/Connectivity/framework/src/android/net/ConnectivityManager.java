59084d6651179bd2cbc8d6ac813916c1c612d003
James Mattis <jmattis@google.com>
2021-01-01 14:13:35 -0800

    Stubbed setOemNetworkPreference in Connectivity
    Stubbed setOemNetworkPreference() in ConnectivityService and connected
    it to ConnectivityManager.
    
    Bug: 176495254
    Bug: 176494815
    Test: atest FrameworksNetTests
    atest FrameworksNetIntegrationTests
    atest CtsNetTestCasesLatestSdk
    
    Change-Id: Iabad7300a8b058e1edcb0defab8a031d21e6433c

--- packages/Connectivity/framework/src/android/net/ConnectivityManager.java
+++ packages/Connectivity/framework/src/android/net/ConnectivityManager.java
@@ -4871,9 +4871,13 @@ public class ConnectivityManager {
         }
     }
 
-    private void setOemNetworkPreference(@NonNull OemNetworkPreferences preference) {
-        Log.d(TAG, "setOemNetworkPreference called with preference: "
-                + preference.toString());
+    private void setOemNetworkPreference(@NonNull final OemNetworkPreferences preference) {
+        try {
+            mService.setOemNetworkPreference(preference);
+        } catch (RemoteException e) {
+            Log.e(TAG, "setOemNetworkPreference() failed for preference: " + preference.toString());
+            throw e.rethrowFromSystemServer();
+        }
     }
 
     @NonNull
