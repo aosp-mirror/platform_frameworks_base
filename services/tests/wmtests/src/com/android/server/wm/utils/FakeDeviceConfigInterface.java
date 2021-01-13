b4bf83a7733d5334a2df829874684f896df4d3eb
raylinhsu <raylinhsu@google.com>
2021-01-13 13:27:25 +0800

    DO NOT MERGE Revert "Set fixed fps when ambient or display brightness is high"
    This reverts commit 4a0a896accfc86dff48d867ee07239fb8b53c626.
    
    Bug: 176018998
    Test: atest DisplayModeDirectorTest
    Change-Id: Ic9329c59c358742696adbbca9cc2b33f25787eb1

--- services/tests/wmtests/src/com/android/server/wm/utils/FakeDeviceConfigInterface.java
+++ services/tests/wmtests/src/com/android/server/wm/utils/FakeDeviceConfigInterface.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.server.testutils;
+package com.android.server.wm.utils;
 
 import android.annotation.NonNull;
 import android.provider.DeviceConfig;
@@ -22,7 +22,6 @@ import android.util.ArrayMap;
 import android.util.Pair;
 
 import com.android.internal.util.Preconditions;
-import com.android.server.utils.DeviceConfigInterface;
 
 import java.lang.reflect.Constructor;
 import java.util.HashMap;
@@ -117,19 +116,6 @@ public class FakeDeviceConfigInterface implements DeviceConfigInterface {
         }
         try {
             return Long.parseLong(value);
-        } catch (NumberFormatException e) {
-            return defaultValue;
-        }
-    }
-
-    @Override
-    public float getFloat(String namespace, String name, float defaultValue) {
-        String value = getProperty(namespace, name);
-        if (value == null) {
-            return defaultValue;
-        }
-        try {
-            return Float.parseFloat(value);
         } catch (NumberFormatException e) {
             return defaultValue;
         }
