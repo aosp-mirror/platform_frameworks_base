c9522102480db5bf3580f29767d849ca6b7bccd5
Bill Lin <lbill@google.com>
2021-01-13 12:03:16 +0800

    2/ Fix flaky in WMShellFlickerTest legacySplitScreen tests
    b/177374067 indicate OpenAppToLegacySplitScreenTest still flaky
    1) Extend the filter in visibleLayersShownMoreThanOneConsecutiveEntry()
       - Toast window
    2) When going split transition, waitForAppTransitionIdle() at end
    3) Enable navBarLayerIsAlwaysVisible() & statusBarLayerIsAlwaysVisible()
    
    Test: atest com.android.wm.shell.flicker.legacysplitscreen
    Test: Set TEST_REPETITIONS to 100 and run flicker test stressfully
    Bug: 177374067
    Bug: 176923301
    Bug: 176134495
    Change-Id: I1caface176f4af591d5383877c81b786eb23ba58

--- libs/WindowManager/Shell/tests/flicker/src/com/android/wm/shell/flicker/legacysplitscreen/RotateOneLaunchedAppTest.kt
+++ libs/WindowManager/Shell/tests/flicker/src/com/android/wm/shell/flicker/legacysplitscreen/RotateOneLaunchedAppTest.kt
@@ -16,6 +16,7 @@
 
 package com.android.wm.shell.flicker.legacysplitscreen
 
+import android.platform.test.annotations.Presubmit
 import android.view.Surface
 import androidx.test.filters.RequiresDevice
 import com.android.server.wm.flicker.dsl.FlickerBuilder
@@ -42,6 +43,7 @@ import org.junit.runners.Parameterized
  * Test open app to split screen.
  * To run this test: `atest WMShellFlickerTests:RotateOneLaunchedAppTest`
  */
+@Presubmit
 @RequiresDevice
 @RunWith(Parameterized::class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
