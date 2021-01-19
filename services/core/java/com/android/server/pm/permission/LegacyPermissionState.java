9ec7f95b3c1d1db6299d3c159fad5d7a491b5e57
Hai Zhang <zhanghai@google.com>
2021-01-19 12:01:42 -0800

    Store all permissions per-user.
    By putting install permissions into all users when reading the old
    USER_ALL state, and writing them back as how we have been writing
    per-user permissions.
    
    We are allowing roles to control signature permissions, however roles
    are per-user while previously only one install permission state is
    stored for all users, so we should make all permissions stored
    per-user.
    
    This change keeps reusing the old persistence mechanism, and the
    alternative would be creating a new persistence inside
    PermissionManagerService. However, since permission isn't getting
    modularized in S, that would mean yet another form of persistence we
    need to migrate from and another system API to support it, so I don't
    think it's a good trade-off. Meanwhile, even if we manage to create
    such a new persistence for per-user permission state, we are still
    stuck with the legacy persistence for permission definitions inside
    package (writeLegacyPermissionsTEMP) and thus still won't be in a
    totally clean state. And to migrate that as well, we need yet another
    storage class, system API and future migration path, without much
    benefits from doing it besides legacy code looking better, so I
    believe this change is the better approach to address our problem.
    
    Once we modularized permission, we will be able to get rid of the
    writeLegacyPermissionStateTEMP() methods.
    
    Since we are no longer writing install permissions with package, we no
    longer need to write permission state back to PackageSetting before
    Settings.writeLPr().
    
    The dumpsys output is maintained to be the same as before, where the
    install permissions section will print the install permission state for
    user 0.
    
    Bug: 158736025
    Test: presubmit
    Change-Id: I8ebfb9acd354ad906490f3bb2fb9c962bc1c16eb

--- services/core/java/com/android/server/pm/permission/LegacyPermissionState.java
+++ services/core/java/com/android/server/pm/permission/LegacyPermissionState.java
@@ -19,7 +19,6 @@ package com.android.server.pm.permission;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
-import android.os.UserHandle;
 import android.util.ArrayMap;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
@@ -105,28 +104,14 @@ public final class LegacyPermissionState {
     }
 
     /**
-     * Put a install permission state.
-     *
-     * @param permissionState the permission state
-     */
-    public void putInstallPermissionState(@NonNull PermissionState permissionState) {
-        putPermissionState(permissionState, UserHandle.USER_ALL);
-    }
-
-    /**
-     * Put a runtime permission state for a user.
+     * Put a permission state for a user.
      *
      * @param permissionState the permission state
      * @param userId the user ID
      */
-    public void putRuntimePermissionState(@NonNull PermissionState permissionState,
+    public void putPermissionState(@NonNull PermissionState permissionState,
             @UserIdInt int userId) {
         checkUserId(userId);
-        putPermissionState(permissionState, userId);
-    }
-
-    private void putPermissionState(@NonNull PermissionState permissionState,
-            @UserIdInt int userId) {
         UserState userState = mUserStates.get(userId);
         if (userState == null) {
             userState = new UserState();
@@ -157,29 +142,14 @@ public final class LegacyPermissionState {
     }
 
     /**
-     * Get all the install permission states.
-     *
-     * @return the install permission states
-     */
-    @NonNull
-    public Collection<PermissionState> getInstallPermissionStates() {
-        return getPermissionStates(UserHandle.USER_ALL);
-    }
-
-    /**
      * Get all the runtime permission states for a user.
      *
      * @param userId the user ID
      * @return the runtime permission states
      */
     @NonNull
-    public Collection<PermissionState> getRuntimePermissionStates(@UserIdInt int userId) {
+    public Collection<PermissionState> getPermissionStates(@UserIdInt int userId) {
         checkUserId(userId);
-        return getPermissionStates(userId);
-    }
-
-    @NonNull
-    private Collection<PermissionState> getPermissionStates(@UserIdInt int userId) {
         final UserState userState = mUserStates.get(userId);
         if (userState == null) {
             return Collections.emptyList();
@@ -265,6 +235,8 @@ public final class LegacyPermissionState {
         @NonNull
         private final String mName;
 
+        private final boolean mRuntime;
+
         private final boolean mGranted;
 
         private final int mFlags;
@@ -273,17 +245,20 @@ public final class LegacyPermissionState {
          * Create a new instance of this class.
          *
          * @param name the name of the permission
+         * @param runtime whether the permission is runtime
          * @param granted whether the permission is granted
          * @param flags the permission flags
          */
-        public PermissionState(@NonNull String name, boolean granted, int flags) {
+        public PermissionState(@NonNull String name, boolean runtime, boolean granted, int flags) {
             mName = name;
+            mRuntime = runtime;
             mGranted = granted;
             mFlags = flags;
         }
 
         private PermissionState(@NonNull PermissionState other) {
             mName = other.mName;
+            mRuntime = other.mRuntime;
             mGranted = other.mGranted;
             mFlags = other.mFlags;
         }
@@ -296,6 +271,15 @@ public final class LegacyPermissionState {
         @NonNull
         public String getName() {
             return mName;
+        }
+
+        /**
+         * Get whether the permission is a runtime permission.
+         *
+         * @return whether the permission is a runtime permission.
+         */
+        public boolean isRuntime() {
+            return mRuntime;
         }
 
         /**
