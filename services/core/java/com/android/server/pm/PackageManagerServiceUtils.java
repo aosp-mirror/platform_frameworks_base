0ee1e6b15cceebb537b8072294cfaebf4ed44874
Michael Groover <mpgroover@google.com>
2021-01-25 23:55:58 -0800

    Allow APK rollback without rollback capability on previous key
    Currently if an APK rollback is requested after an update to an APK
    that just rotated its signing key the rollback will only be allowed
    if the previous signing key in the lineage has the rollback
    capability granted. This commit allows a rollback to the previously
    installed APK without requiring the rollback capability if the
    rollback API is used. This allows apps to take advantage of APK
    rollbacks without losing the benefit of key rotation by needing to
    set the rollback capability on the previous signing key.
    
    Bug: 175231724
    Test: atest RollbackTest
    Test: atest RollbackManaagerTest
    Test: atest PkgInstallSignatureVerificationTest
    Change-Id: I460ef4e7d4c221509b6442f2e8d3079229ad07c8

--- services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
+++ services/core/java/com/android/server/pm/PackageManagerServiceUtils.java
@@ -621,7 +621,7 @@ public class PackageManagerServiceUtils {
      */
     public static boolean verifySignatures(PackageSetting pkgSetting,
             PackageSetting disabledPkgSetting, PackageParser.SigningDetails parsedSignatures,
-            boolean compareCompat, boolean compareRecover)
+            boolean compareCompat, boolean compareRecover, boolean isRollback)
             throws PackageManagerException {
         final String packageName = pkgSetting.name;
         boolean compatMatch = false;
@@ -653,6 +653,13 @@ public class PackageManagerServiceUtils {
 
             if (!match && isApkVerificationForced(disabledPkgSetting)) {
                 match = matchSignatureInSystem(pkgSetting, disabledPkgSetting);
+            }
+
+            if (!match && isRollback) {
+                // Since a rollback can only be initiated for an APK previously installed on the
+                // device allow rolling back to a previous signing key even if the rollback
+                // capability has not been granted.
+                match = pkgSetting.signatures.mSigningDetails.hasAncestorOrSelf(parsedSignatures);
             }
 
             if (!match) {
