16e72ebab443d676e893aab3ba8f31415e8f9bc0
Nader Jawad <njawad@google.com>
2021-01-25 18:02:07 -0800

    Add RenderEffect support for Shaders as inputs
    Added RenderEffect.createShader to handle creation
    of RenderEffects using shaders as inputs that can be
    chained with other RenderEffects
    
    Fixes: 178536866
    Test: Added example in HwAccelerationTest activity
    Change-Id: I4d9791ad63f3057ffa528918ce2cd083986089e5

--- libs/hwui/jni/RenderEffect.cpp
+++ libs/hwui/jni/RenderEffect.cpp
@@ -115,6 +115,18 @@ static jlong createChainEffect(
     return reinterpret_cast<jlong>(composeFilter.release());
 }
 
+static jlong createShaderEffect(
+    JNIEnv* env,
+    jobject,
+    jlong shaderHandle
+) {
+    auto* shader = reinterpret_cast<const SkShader*>(shaderHandle);
+    sk_sp<SkImageFilter> shaderFilter = SkImageFilters::Shader(
+          sk_ref_sp(shader), nullptr
+    );
+    return reinterpret_cast<jlong>(shaderFilter.release());
+}
+
 static void RenderEffect_safeUnref(SkImageFilter* filter) {
     SkSafeUnref(filter);
 }
@@ -130,7 +142,8 @@ static const JNINativeMethod gRenderEffectMethods[] = {
     {"nativeCreateBitmapEffect", "(JFFFFFFFF)J", (void*)createBitmapEffect},
     {"nativeCreateColorFilterEffect", "(JJ)J", (void*)createColorFilterEffect},
     {"nativeCreateBlendModeEffect", "(JJI)J", (void*)createBlendModeEffect},
-    {"nativeCreateChainEffect", "(JJ)J", (void*)createChainEffect}
+    {"nativeCreateChainEffect", "(JJ)J", (void*)createChainEffect},
+    {"nativeCreateShaderEffect", "(J)J", (void*)createShaderEffect}
 };
 
 int register_android_graphics_RenderEffect(JNIEnv* env) {
