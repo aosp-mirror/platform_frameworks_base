086e935ef280e857165c0566fdbea081ccd856f2
Amy Zhang <amyjojo@google.com>
2021-01-28 14:41:21 -0800

    Improve documation of RestartEvent of Tuner Filter
    Test: make
    Bug: 178498700
    Change-Id: Ia963d3277accc574ff534ac17838d11a65bf861a

--- media/java/android/media/tv/tuner/filter/RestartEvent.java
+++ media/java/android/media/tv/tuner/filter/RestartEvent.java
@@ -19,17 +19,29 @@ package android.media.tv.tuner.filter;
 import android.annotation.SystemApi;
 
 /**
- * An Event that the client would reveice after stopping, reconfiguring and restarting a filter.
+ * An Event that the client would receive after starting a filter. This event is optional to be
+ * received on the newly opened and started filter. It must be received after stopping,
+ * reconfiguring and restarting a Filter to differentiate the valid reconfigured events from the
+ * previous events.
  *
  * <p>After stopping and restarting the filter, the client has to discard all coming events until
  * it receives {@link RestartEvent} to avoid using the events from the previous configuration.
  *
  * <p>Recofiguring must happen after stopping the filter.
  *
+ * @see Filter#stop()
+ * @see Filter#start()
+ * @see Filter#configure(FilterConfiguration)
+ *
  * @hide
  */
 @SystemApi
 public final class RestartEvent extends FilterEvent {
+    /**
+     * The stard id reserved for the newly opened filter's first start event.
+     */
+    public static final int NEW_FILTER_FIRST_START_ID = 0;
+
     private final int mStartId;
 
     // This constructor is used by JNI code only
@@ -38,13 +50,13 @@ public final class RestartEvent extends FilterEvent {
     }
 
     /**
-     * Gets the start id.
+     * Gets the start id sent via the current Restart Event.
      *
-     * <p>An unique ID to mark the start point of receiving the valid filter events after
-     * reconfiguring. It must be sent at least once in the first event after the filter is
-     * restarted.
+     * <p>An unique ID to mark the start point of receiving the valid reconfigured filter events.
+     * The client must receive at least once after the filter is reconfigured and restarted.
      *
-     * <p>0 is reserved for the newly opened filter's first start. It's optional to be received.
+     * <p>{@link #NEW_FILTER_FIRST_START_ID} is reserved for the newly opened filter's first start.
+     * It's optional to be received.
      */
     public int getStartId() {
         return mStartId;
