6a6aa5e23dbcb59840c494603df2ff35f9699f76
Adrian Roos <roosa@google.com>
2021-01-29 12:15:00 +0100

    API: Suppress existing NullableCollections lints
    Bug: 152525509
    Bug: 154763999
    Test: make checkapi
    Change-Id: Iecd0927e6be5496b2fbd1a49396db5439257ffe4

--- core/java/android/content/pm/PackageManager.java
+++ core/java/android/content/pm/PackageManager.java
@@ -4945,7 +4945,7 @@ public abstract class PackageManager {
      *
      * @hide
      */
-    @SuppressWarnings("HiddenAbstractMethod")
+    @SuppressWarnings({"HiddenAbstractMethod", "NullableCollection"})
     @TestApi
     public abstract @Nullable String[] getNamesForUids(int[] uids);
 
