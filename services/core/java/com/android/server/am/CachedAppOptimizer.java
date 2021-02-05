e5430976256a10c7ed727f7c7c96ff0bc1db4cde
Marco Ballesio <balejs@google.com>
2021-02-05 11:32:39 -0800

    CachedAppOptimizer: don't hardcode freezer path
    Test: verified that the freezer is correctly detected
    Bug: 168058155
    Change-Id: Ia594282f62ce934d124fa5f89da75cd58fd6ed78

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -538,6 +538,12 @@ public final class CachedAppOptimizer {
     private static native int getBinderFreezeInfo(int pid);
 
     /**
+     * Returns the path to be checked to verify whether the freezer is supported by this system.
+     * @return absolute path to the file
+     */
+    private static native String getFreezerCheckPath();
+
+    /**
      * Determines whether the freezer is supported by this system
      */
     public static boolean isFreezerSupported() {
@@ -545,7 +551,7 @@ public final class CachedAppOptimizer {
         FileReader fr = null;
 
         try {
-            fr = new FileReader("/sys/fs/cgroup/uid_0/cgroup.freeze");
+            fr = new FileReader(getFreezerCheckPath());
             char state = (char) fr.read();
 
             if (state == '1' || state == '0') {
