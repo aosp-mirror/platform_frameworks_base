27f76dbf6f7caaf4971ff9c99eb57294ba26830d
Elliott Hughes <enh@google.com>
2021-02-05 14:06:08 -0800

    Stop manually mixing /dev/hw_random.
    The kernel has been doing this itself since 2014. We still want the rest
    of this code, though, both for devices without a hw_random device but
    also to transfer entropy from one boot to the next (as described on the
    man page, the link to which I've updated to avoid a redirect).
    
    Also reorder includes to satisfy the upload hook.
    
    Bug: http://b/179086242
    Test: treehugger
    Change-Id: I68ac8e04191b71339708b18eee646a2d65063fd5

--- services/tests/servicestests/src/com/android/server/EntropyMixerTest.java
+++ services/tests/servicestests/src/com/android/server/EntropyMixerTest.java
@@ -34,7 +34,7 @@ public class EntropyMixerTest extends AndroidTestCase {
         assertEquals(0, FileUtils.readTextFile(file, 0, null).length());
 
         // The constructor has the side effect of writing to file
-        new EntropyMixer(getContext(), "/dev/null", file.getCanonicalPath(), "/dev/null");
+        new EntropyMixer(getContext(), "/dev/null", file.getCanonicalPath());
 
         assertTrue(FileUtils.readTextFile(file, 0, null).length() > 0);
     }
