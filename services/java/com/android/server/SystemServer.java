113423619e5c0b8dee5e1b2ea0559cc383f1a224
Chris Wailes <chriswailes@google.com>
2021-02-08 10:00:13 -0800

    Register ART Service with the System Server
    This CL causes the System Server to create and load the ART Local
    Manager.
    
    Test: Build and flash
    Bug: 177273468
    Change-Id: I9734160f1cd4fd9fc363b3af50e27ec8973be557

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -103,6 +103,7 @@ import com.android.internal.util.FrameworkStatsLog;
 import com.android.internal.widget.ILockSettings;
 import com.android.server.am.ActivityManagerService;
 import com.android.server.appbinding.AppBindingService;
+import com.android.server.art.ArtManagerLocal;
 import com.android.server.attention.AttentionManagerService;
 import com.android.server.audio.AudioService;
 import com.android.server.biometrics.AuthService;
@@ -2608,6 +2609,10 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("GameManagerService");
         mSystemServiceManager.startService(GAME_MANAGER_SERVICE_CLASS);
+        t.traceEnd();
+
+        t.traceBegin("ArtManagerLocal");
+        LocalManagerRegistry.addManager(ArtManagerLocal.class, new ArtManagerLocal());
         t.traceEnd();
 
         t.traceBegin("StartBootPhaseDeviceSpecificServicesReady");
