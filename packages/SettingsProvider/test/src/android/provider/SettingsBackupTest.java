27a8746695b0c141d36906338a9d259ed8899609
Michal Olech <molech@google.com>
2021-02-15 14:02:17 +0100

    [CEC Configuration] Move 'hdmi_cec_version' to SharedPreferences
    Test: cts-tradefed
    Bug: 171958586
    Change-Id: I01d374c9a69d23f0d242c0c8ec2364454692ddd4

--- packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
+++ packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
@@ -298,7 +298,6 @@ public class SettingsBackupTest {
                     Settings.Global.GNSS_SATELLITE_BLOCKLIST,
                     Settings.Global.GPRS_REGISTER_CHECK_PERIOD_MS,
                     Settings.Global.HDMI_CEC_SWITCH_ENABLED,
-                    Settings.Global.HDMI_CEC_VERSION,
                     Settings.Global.HDMI_CONTROL_AUTO_DEVICE_OFF_ENABLED,
                     Settings.Global.HDMI_CONTROL_AUTO_WAKEUP_ENABLED,
                     Settings.Global.HDMI_CONTROL_ENABLED,
