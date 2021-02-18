f01a31756f28d3485ea7df7e7ec46fa7b28146fb
Alex Johnston <acjohnston@google.com>
2021-02-18 17:16:57 +0000

    Handle isUsbDataSignalingEnabledForUser PO case
    If there is no DO or COPE PO, then
    usb data signaling is enabled.
    
    Bug: 180547881
    Test: Manual testing
          atest com.android.server.devicepolicy.DevicePolicyManagerTest
    Change-Id: I32a1f73ca403b6c1c38b7c6fa70cc2f0f0830324
    (cherry picked from commit 46149e42b8d5cb57c0ac80cb411d03b6d5815e33)

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -16755,7 +16755,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         synchronized (getLockObject()) {
             final ActiveAdmin admin = getDeviceOwnerOrProfileOwnerOfOrganizationOwnedDeviceLocked(
                     UserHandle.USER_SYSTEM);
-            return admin != null && admin.mUsbDataSignalingEnabled;
+            return admin == null || admin.mUsbDataSignalingEnabled;
         }
     }
 
