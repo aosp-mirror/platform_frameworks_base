c09430c189c16870d4d13105b7449961988f2866
Greg Kaiser <gkaiser@google.com>
2021-02-18 00:29:01 +0000

    Revert "Add a system TextToSpeech implementation that initiates ..."
    Revert "Add CTS tests for TextToSpeech connection related functi..."
    
    Revert submission 13547465-ttsproxy
    
    Reason for revert: Setup wizard crashes due to "Service not registered: android.speech.tts.TextToSpeech$SystemConnection"
    Reverted Changes:
    Id4059e460:Add CTS tests for TextToSpeech connection related ...
    Ie17800bae:Add a system TextToSpeech implementation that init...
    
    Bug: 180519958
    Change-Id: Ie43c485bd4d0f76f8bb0a1dc77d6d85d46ff8377
    Test: Locally reverted this change and no longer see the exception in SUW we saw before.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -314,8 +314,6 @@ public final class SystemServer implements Dumpable {
             "com.android.server.musicrecognition.MusicRecognitionManagerService";
     private static final String SYSTEM_CAPTIONS_MANAGER_SERVICE_CLASS =
             "com.android.server.systemcaptions.SystemCaptionsManagerService";
-    private static final String TEXT_TO_SPEECH_MANAGER_SERVICE_CLASS =
-            "com.android.server.texttospeech.TextToSpeechManagerService";
     private static final String TIME_ZONE_RULES_MANAGER_SERVICE_CLASS =
             "com.android.server.timezone.RulesManagerService$Lifecycle";
     private static final String IOT_SERVICE_CLASS =
@@ -1643,7 +1641,6 @@ public final class SystemServer implements Dumpable {
             startAttentionService(context, t);
             startRotationResolverService(context, t);
             startSystemCaptionsManagerService(context, t);
-            startTextToSpeechManagerService(context, t);
 
             // System Speech Recognition Service
             if (deviceHasConfigString(context,
@@ -2821,13 +2818,6 @@ public final class SystemServer implements Dumpable {
 
         t.traceBegin("StartSystemCaptionsManagerService");
         mSystemServiceManager.startService(SYSTEM_CAPTIONS_MANAGER_SERVICE_CLASS);
-        t.traceEnd();
-    }
-
-    private void startTextToSpeechManagerService(@NonNull Context context,
-            @NonNull TimingsTraceAndSlog t) {
-        t.traceBegin("StartTextToSpeechManagerService");
-        mSystemServiceManager.startService(TEXT_TO_SPEECH_MANAGER_SERVICE_CLASS);
         t.traceEnd();
     }
 
