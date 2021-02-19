6793c1784af802b49e717e01c1e858aaf9665476
Andrew Cheng <chengca@google.com>
2021-02-19 22:14:42 +0000

    Update Companion to new BluetoothConnectionCallback
    Added new disconnect reason to BluetoothConnectionCallback.
    
    Bug: 177668957
    Test: manual, ensure disconnect reasons appear in logcat
    Change-Id: I322e0c4241644a9cf8f4d99c6f3920a8715eb5e2

--- services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
+++ services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
@@ -726,7 +726,9 @@ public class CompanionDeviceManagerService extends SystemService implements Bind
         }
 
         @Override
-        public void onDeviceDisconnected(BluetoothDevice device) {
+        public void onDeviceDisconnected(BluetoothDevice device, @DisconnectReason int reason) {
+            Slog.d(LOG_TAG, device.getAddress() + " disconnected w/ reason: (" + reason + ") "
+                    + BluetoothAdapter.BluetoothConnectionCallback.disconnectReasonText(reason));
             CompanionDeviceManagerService.this.onDeviceDisconnected(device.getAddress());
         }
     }
