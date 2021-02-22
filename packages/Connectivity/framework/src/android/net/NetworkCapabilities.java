f8c3e2eb4edd97106cdbc40125f4d96c425ebe0b
Chiachang Wang <chiachangwang@google.com>
2021-02-22 18:36:38 +0800

    Replace the usage of UidRange
    UidRange is used in a shared way between ConnectivityService
    and VPN through the use of NetworkCapabilities. UidRange will
    be part of the ConnectivityService mainline but Vpn.java will
    stay in the framework. We need a way to replace the APIs using
    UidRange, or to make UidRange system API. The only really
    relevant surface here is NetworkCapabilities#{setUids, getUids}.
    The need for UidRange could be replaced by an integer Range, so
    replace the usage of UidRange by a integer Range in
    NetworkCapabilities#{setUids, getUids} and update the relevant
    callers.
    
    Bug: 172183305
    Test: atest FrameworksNetTests CtsNetTestCasesLatestSdk
    Merged-In: I4e5aec6ef1ea02e038fcd7ed117a3b67b69c5cb9
    Change-Id: Idb7f353788c5779a4fbbd107595e9326b99fe0a8

--- packages/Connectivity/framework/src/android/net/NetworkCapabilities.java
+++ packages/Connectivity/framework/src/android/net/NetworkCapabilities.java
@@ -33,6 +33,7 @@ import android.os.Parcelable;
 import android.os.Process;
 import android.text.TextUtils;
 import android.util.ArraySet;
+import android.util.Range;
 import android.util.proto.ProtoOutputStream;
 
 import com.android.internal.annotations.VisibleForTesting;
@@ -216,7 +217,7 @@ public final class NetworkCapabilities implements Parcelable {
             setTransportInfo(null);
         }
         mSignalStrength = nc.mSignalStrength;
-        setUids(nc.mUids); // Will make the defensive copy
+        mUids = (nc.mUids == null) ? null : new ArraySet<>(nc.mUids);
         setAdministratorUids(nc.getAdministratorUids());
         mOwnerUid = nc.mOwnerUid;
         mUnwantedNetworkCapabilities = nc.mUnwantedNetworkCapabilities;
@@ -1519,9 +1520,8 @@ public final class NetworkCapabilities implements Parcelable {
      * @hide
      */
     public @NonNull NetworkCapabilities setSingleUid(int uid) {
-        final ArraySet<UidRange> identity = new ArraySet<>(1);
-        identity.add(new UidRange(uid, uid));
-        setUids(identity);
+        mUids = new ArraySet<>(1);
+        mUids.add(new UidRange(uid, uid));
         return this;
     }
 
@@ -1530,12 +1530,8 @@ public final class NetworkCapabilities implements Parcelable {
      * This makes a copy of the set so that callers can't modify it after the call.
      * @hide
      */
-    public @NonNull NetworkCapabilities setUids(Set<UidRange> uids) {
-        if (null == uids) {
-            mUids = null;
-        } else {
-            mUids = new ArraySet<>(uids);
-        }
+    public @NonNull NetworkCapabilities setUids(@Nullable Set<Range<Integer>> uids) {
+        mUids = UidRange.fromIntRanges(uids);
         return this;
     }
 
@@ -1544,8 +1540,19 @@ public final class NetworkCapabilities implements Parcelable {
      * This returns a copy of the set so that callers can't modify the original object.
      * @hide
      */
-    public @Nullable Set<UidRange> getUids() {
-        return null == mUids ? null : new ArraySet<>(mUids);
+    public @Nullable Set<Range<Integer>> getUids() {
+        return UidRange.toIntRanges(mUids);
+    }
+
+    /**
+     * Get the list of UIDs this network applies to.
+     * This returns a copy of the set so that callers can't modify the original object.
+     * @hide
+     */
+    public @Nullable Set<UidRange> getUidRanges() {
+        if (mUids == null) return null;
+
+        return new ArraySet<>(mUids);
     }
 
     /**
