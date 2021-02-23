07fedbce463d23fcb433c5ea072069dff83a6d16
Galia Peycheva <galinap@google.com>
2021-02-23 14:20:02 +0100

    Add WM.setForceCrossWindowBlurDisabled TestApi
    Bug: 179990440
    Test: m && atest BlurTests
    Change-Id: Ie393e456f2c55dfde945dc53ecdc83bbcfcdb361

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -5702,6 +5702,11 @@ public class WindowManagerService extends IWindowManager.Stub
         mBlurController.unregisterCrossWindowBlurEnabledListener(listener);
     }
 
+    @Override
+    public void setForceCrossWindowBlurDisabled(boolean disable) {
+        mBlurController.setForceCrossWindowBlurDisabled(disable);
+    }
+
     // -------------------------------------------------------------
     // Internals
     // -------------------------------------------------------------
