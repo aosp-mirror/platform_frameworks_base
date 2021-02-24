171eeee60d2b7a42495660c51aba10510162dc87
Eran Messeri <eranm@google.com>
2021-02-24 15:51:33 +0000

    Implement Device Policy upgrade flow
    Implement an upgrade flow in the DevicePolicyManagerService for
    admin-set policies.
    
    The purpose is to allow graceful, rather than ad-hoc, setting of default
    values for new policies added to DevicePolicyData or ActiveAdmin.
    
    This change also cleans up the lowest of hanging fruits - presence of
    password properties metrics, by upgrading from version 0 (nonexistent)
    to version 1.
    
    The versioning mechanism is meant for handling additional upgrade
    scenarios.
    
    Test: Manual upgrade, flash to a build without a version and check that
    the device boots and the version file is created.
    Test: atest FrameworksServicesTests:PolicyVersionUpgraderTest
    Bug: 181009337
    
    Change-Id: Iac7d345b34beba00d6d5fb10d6fb0a2c745592e0

---
+++ services/devicepolicy/java/com/android/server/devicepolicy/PolicyUpgraderDataProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.server.devicepolicy;
+
+import android.annotation.Nullable;
+import android.app.admin.DeviceAdminInfo;
+import android.content.ComponentName;
+
+import com.android.internal.util.JournaledFile;
+
+import java.util.function.Function;
+
+/**
+ * An interface for providing the {@code PolicyVersionUpgrader} with all the data necessary
+ * to go through the upgrade process.
+ */
+public interface PolicyUpgraderDataProvider {
+    /**
+     * Returns true if the provided {@code userId} is a device owner. May affect some policy
+     * defaults.
+     */
+    boolean isUserDeviceOwner(int userId);
+
+    /**
+     * Returns true if the storage manager indicates file-based encryption is enabled.
+     */
+    boolean storageManagerIsFileBasedEncryptionEnabled();
+
+    /**
+     * Returns the journaled policies file for a given user.
+     */
+    JournaledFile makeDevicePoliciesJournaledFile(int userId);
+
+    /**
+     * Returns the journaled policy version file for a given user.
+     */
+    JournaledFile makePoliciesVersionJournaledFile(int userId);
+
+    /**
+     * Returns the {@code ComponentName} of the owner component for a user.
+     */
+    @Nullable ComponentName getOwnerComponent(int userId);
+
+    /**
+     * Returns a function which provides the component name and device admin info for a given
+     * user.
+     */
+    Function<ComponentName, DeviceAdminInfo> getAdminInfoSupplier(int userId);
+}
