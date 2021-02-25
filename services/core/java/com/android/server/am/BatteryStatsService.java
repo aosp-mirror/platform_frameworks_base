f1f57cc9c5ad39f0faee37449e723f688a18fb29
Chiachang Wang <chiachangwang@google.com>
2021-02-25 16:58:40 +0800

    Have the DataConnectionStat started from BSS
    ConnectivityService is going to becoming a mainline module.
    The DataConnectionStat is used to listen the telephony status
    change and report to BSS. This does not really relate to
    connectivity. DataConnectionStat was created in
    ConnectivityService and use some hidden APIs which is not
    allowed for a mainline module. Thus, move the creation and
    monitor to BSS directly which is more appropriate place from
    its functional perspective.
    
    Also, move the DataConnectionStat out from module scope since
    it does have too much relationship with connectivity module.
    
    Bug: 171183530
    Test: m ; verify the behavior with cellular state change
    Change-Id: I0c2c1b90df7a107ee75e81fd49d39d65678204c0
    Merged-In: I0c2c1b90df7a107ee75e81fd49d39d65678204c0
    (cherry picked from commit 39c303a839e37e143a2e9145e81fe506a6c3a920)

--- services/core/java/com/android/server/am/BatteryStatsService.java
+++ services/core/java/com/android/server/am/BatteryStatsService.java
@@ -85,6 +85,7 @@ import com.android.internal.util.function.pooled.PooledLambda;
 import com.android.net.module.util.NetworkCapabilitiesUtils;
 import com.android.server.LocalServices;
 import com.android.server.Watchdog;
+import com.android.server.connectivity.DataConnectionStats;
 import com.android.server.net.BaseNetworkObserver;
 import com.android.server.pm.UserManagerInternal;
 
@@ -371,6 +372,9 @@ public final class BatteryStatsService extends IBatteryStats.Stub
         }
 
         Watchdog.getInstance().addMonitor(this);
+
+        final DataConnectionStats dataConnectionStats = new DataConnectionStats(mContext, mHandler);
+        dataConnectionStats.startMonitoring();
     }
 
     private final class LocalService extends BatteryStatsInternal {
