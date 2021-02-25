28d0b7b712566f7d4cb6c62f8745f9e39dbfda43
Alex Buynytskyy <alexbuy@google.com>
2021-02-25 09:32:21 -0800

    Remove INSTALLER_V2 permission since the API is finalized in S.
    Bug: 152310230
    Fixes: 152310230
    Test: atest PackageManagerShellCommandTest PackageManagerShellCommandIncrementalTest IncrementalServiceTest PackageManagerServiceTest ChecksumsTest
    Change-Id: I624eb44a455ba65f156064c57787af5903567d96

--- services/core/java/com/android/server/pm/PackageInstallerSession.java
+++ services/core/java/com/android/server/pm/PackageInstallerSession.java
@@ -3547,14 +3547,12 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
 
     @Override
     public DataLoaderParamsParcel getDataLoaderParams() {
-        mContext.enforceCallingOrSelfPermission(Manifest.permission.USE_INSTALLER_V2, null);
         return params.dataLoaderParams != null ? params.dataLoaderParams.getData() : null;
     }
 
     @Override
     public void addFile(int location, String name, long lengthBytes, byte[] metadata,
             byte[] signature) {
-        mContext.enforceCallingOrSelfPermission(Manifest.permission.USE_INSTALLER_V2, null);
         if (!isDataLoaderInstallation()) {
             throw new IllegalStateException(
                     "Cannot add files to non-data loader installation session.");
@@ -3587,7 +3585,6 @@ public class PackageInstallerSession extends IPackageInstallerSession.Stub {
 
     @Override
     public void removeFile(int location, String name) {
-        mContext.enforceCallingOrSelfPermission(Manifest.permission.USE_INSTALLER_V2, null);
         if (!isDataLoaderInstallation()) {
             throw new IllegalStateException(
                     "Cannot add files to non-data loader installation session.");
