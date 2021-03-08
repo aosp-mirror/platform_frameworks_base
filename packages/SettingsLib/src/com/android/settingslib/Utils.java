119100656e4e6ca47d73fdca946e0e0fc009c70a
paulhu <paulhu@google.com>
2021-03-08 10:57:03 +0800

    Replace ConnectivityManager hidden symbols on SettingsLib
    Connectivity is becoming a mainline module in S, so
    ConnectivityManager hidden symbols can not be used for outside
    components. Besides, most Tethering relevant methods or variables
    on CM are migrated to TetheringManager. So replace all these
    methods or variables from ConnectivityManager to TetheringManager
    on SettingsLib.
    
    Bug: 180693313
    Test: atest SettingsLibRoboTests
    Change-Id: I11e317ed0d3aab2628082bbe6b8292fec82109a9

--- packages/SettingsLib/src/com/android/settingslib/Utils.java
+++ packages/SettingsLib/src/com/android/settingslib/Utils.java
@@ -25,6 +25,7 @@ import android.location.LocationManager;
 import android.media.AudioManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkCapabilities;
+import android.net.TetheringManager;
 import android.net.vcn.VcnTransportInfo;
 import android.net.wifi.WifiInfo;
 import android.os.BatteryManager;
@@ -90,10 +91,10 @@ public class Utils {
      * Return string resource that best describes combination of tethering
      * options available on this device.
      */
-    public static int getTetheringLabel(ConnectivityManager cm) {
-        String[] usbRegexs = cm.getTetherableUsbRegexs();
-        String[] wifiRegexs = cm.getTetherableWifiRegexs();
-        String[] bluetoothRegexs = cm.getTetherableBluetoothRegexs();
+    public static int getTetheringLabel(TetheringManager tm) {
+        String[] usbRegexs = tm.getTetherableUsbRegexs();
+        String[] wifiRegexs = tm.getTetherableWifiRegexs();
+        String[] bluetoothRegexs = tm.getTetherableBluetoothRegexs();
 
         boolean usbAvailable = usbRegexs.length != 0;
         boolean wifiAvailable = wifiRegexs.length != 0;
