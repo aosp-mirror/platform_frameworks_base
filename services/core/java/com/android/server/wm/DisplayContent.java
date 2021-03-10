25457d42572d16f0aceef5a3f238181b5028d17f
Riddle Hsu <riddlehsu@google.com>
2021-03-10 12:16:43 +0800

    Support PiP with fixed rotation
    Add 2 cases with rotation change:
    (a) Enter PiP from fullscreen.
        http://recall/-/d6GxE5mVL8Ww2IPnx7Nk0o/hDfmdrhrBFjJNOdi1s66OW
    (b) Launch fullscreen app with the existing PiP.
        http://recall/-/d6GxE5mVL8Ww2IPnx7Nk0o/bUpHZ6Sh98zKZJnRM7oFy9
    
    Steps of (a):
    1. Task#setWindowingMode to PiP. If the next fullscreen activity
       will change display orientation, start fixed rotation on it.
       And start deferring orientation change.
    2. PipTaskOrganizer#onFixedRotationStarted is called to mark it
       will be a special case.
    3. PipTaskOrganizer#onTaskAppeared is called and starts the PiP
       rotation animation by animateResizePip with rotated destination
       bounds from rotated DisplayLayout.
    4. When onPipAnimationEnd, use WCT#scheduleFinishEnterPip to notify
       that the animation is done, so the deferred orientation change
       can continue to update (PinnedTaskController#setEnterPipBounds).
       The end transaction of animation (reset matrix) is deferred
       until fixed rotation is finished. Also freeze the PiP task
       configuration one time, to avoid extra configuration change
       (letterboxed) by rotation change.
    5. The seamless rotation starts, the PiP surface is transformed
       to previous rotation based on the bounds of previous step. So
       the PiP task can show the same orientation as rotated display.
       The frozen flag of PiP task configuration is cleared.
    6. PipTaskOrganizer#onFixedRotationFinished is called. The final
       PiP destination bounds and the deferred transaction of step 4
       will be sent to WM.
    
    Steps of (b):
    1. Fixed rotation happens (PipTaskOrganizer#onFixedRotationStarted)
       when there is an existing PiP. Apply fade-out animation.
    2. PipTaskOrganizer#onMovementBoundsChanged is called to update
       rotated destination bounds.
    3. PipTaskOrganizer#onFixedRotationFinished is called to apply
       fade-in animation with new bounds.
    
    Other changes:
    - WCT#scheduleFinishEnterPip was used to set bounds and notify
      windowing mode change. That is already done by other config
      change oepration of WCT. So this redundant operation is changed
      to be a signal to notify that the PiP animation is done.
    - Ignore calculating letterbox bounds for PiP activity because
      it should fill the task.
    - Add PinnedTaskController#DEFER_ORIENTATION_CHANGE_TIMEOUT_MS
      to avoid using alpha animation after swiping from any task.
    - Consider source rect hint with rotation.
    
    Bug: 165794724
    Bug: 175836469
    Test: DisplayContentTests#testFixedRotationWithPip
          PipAnimationControllerTest#pipTransitionAnimator_rotatedEndValue
    
    Change-Id: I2c26b5d93996193caaf020bd0e2314c8e1789545

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -464,7 +464,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
     private boolean mDeferredRemoval;
 
     final DockedTaskDividerController mDividerControllerLocked;
-    final PinnedTaskController mPinnedTaskControllerLocked;
+    final PinnedTaskController mPinnedTaskController;
 
     final ArrayList<WindowState> mTapExcludedWindows = new ArrayList<>();
     /** A collection of windows that provide tap exclude regions inside of them. */
@@ -1017,7 +1017,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         }
         mWindowCornerRadius = mDisplayPolicy.getWindowCornerRadius();
         mDividerControllerLocked = new DockedTaskDividerController(this);
-        mPinnedTaskControllerLocked = new PinnedTaskController(mWmService, this);
+        mPinnedTaskController = new PinnedTaskController(mWmService, this);
 
         final SurfaceControl.Builder b = mWmService.makeSurfaceBuilder(mSession)
                 .setOpaque(true)
@@ -1578,10 +1578,6 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
             // has it own policy for bounds, the activity bounds based on parent is unknown.
             return false;
         }
-        if (mPinnedTaskControllerLocked.isPipActiveOrWindowingModeChanging()) {
-            // Use normal rotation animation because seamless PiP rotation is not supported yet.
-            return false;
-        }
 
         setFixedRotationLaunchingApp(r, rotation);
         return true;
@@ -1665,6 +1661,10 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
      */
     void continueUpdateOrientationForDiffOrienLaunchingApp() {
         if (mFixedRotationLaunchingApp == null) {
+            return;
+        }
+        if (mPinnedTaskController.shouldDeferOrientationChange()) {
+            // Wait for the PiP animation to finish.
             return;
         }
         // Update directly because the app which will change the orientation of display is ready.
@@ -1837,6 +1837,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
             forAllWindows(w -> {
                 w.seamlesslyRotateIfAllowed(transaction, oldRotation, rotation, rotateSeamlessly);
             }, true /* traverseTopToBottom */);
+            mPinnedTaskController.startSeamlessRotationIfNeeded(transaction);
         }
 
         mWmService.mDisplayManagerInternal.performTraversal(transaction);
@@ -2333,7 +2334,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
     }
 
     PinnedTaskController getPinnedTaskController() {
-        return mPinnedTaskControllerLocked;
+        return mPinnedTaskController;
     }
 
     /**
@@ -2392,12 +2393,11 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
 
     @Override
     public void onConfigurationChanged(Configuration newParentConfig) {
-        // update resources before cascade so that root docked/pinned tasks use the correct info
-        preOnConfigurationChanged();
         final int lastOrientation = getConfiguration().orientation;
         super.onConfigurationChanged(newParentConfig);
         if (mDisplayPolicy != null) {
             mDisplayPolicy.onConfigurationChanged();
+            mPinnedTaskController.onPostDisplayConfigurationChanged();
         }
 
         if (lastOrientation != getConfiguration().orientation) {
@@ -2405,19 +2405,6 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
                     new LogMaker(MetricsEvent.ACTION_PHONE_ORIENTATION_CHANGED)
                             .setSubtype(getConfiguration().orientation)
                             .addTaggedData(MetricsEvent.FIELD_DISPLAY_ID, getDisplayId()));
-        }
-    }
-
-    /**
-     * Updates the resources used by docked/pinned controllers. This needs to be called at the
-     * beginning of a configuration update cascade since the metrics from these resources are used
-     * for bounds calculations.
-     */
-    void preOnConfigurationChanged() {
-        final PinnedTaskController pinnedTaskController = getPinnedTaskController();
-
-        if (pinnedTaskController != null) {
-            getPinnedTaskController().onConfigurationChanged();
         }
     }
 
@@ -2962,7 +2949,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         final boolean imeVisible = imeWin != null && imeWin.isVisible()
                 && imeWin.isDisplayed();
         final int imeHeight = getInputMethodWindowVisibleHeight();
-        mPinnedTaskControllerLocked.setAdjustedForIme(imeVisible, imeHeight);
+        mPinnedTaskController.setAdjustedForIme(imeVisible, imeHeight);
     }
 
     int getInputMethodWindowVisibleHeight() {
@@ -3190,7 +3177,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         }
 
         pw.println();
-        mPinnedTaskControllerLocked.dump(prefix, pw);
+        mPinnedTaskController.dump(prefix, pw);
 
         pw.println();
         mDisplayFrames.dump(prefix, pw);
