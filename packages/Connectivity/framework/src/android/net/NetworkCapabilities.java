d0128901498baf79332da4f439c6d78a33a8ce10
Remi NGUYEN VAN <reminv@google.com>
2021-03-11 20:49:13 +0900

    Remove connectivity dependency on Preconditions
    Preconditions.checkNotNull is deprecated to be replaced by
    Objects.requireNonNull, and other methods can easily be replaced by
    inline checks.
    
    Preconditions is an internal API class that should not be used by
    unbundled jars.
    
    Bug: 177046265
    Test: m
    Change-Id: If14a75439ff332c927dc4114ae0eecb89f53c6c7

--- packages/Connectivity/framework/src/android/net/NetworkCapabilities.java
+++ packages/Connectivity/framework/src/android/net/NetworkCapabilities.java
@@ -34,7 +34,6 @@ import android.util.ArraySet;
 import android.util.proto.ProtoOutputStream;
 
 import com.android.internal.annotations.VisibleForTesting;
-import com.android.internal.util.Preconditions;
 import com.android.net.module.util.CollectionUtils;
 import com.android.net.module.util.NetworkCapabilitiesUtils;
 
@@ -2073,8 +2072,9 @@ public final class NetworkCapabilities implements Parcelable {
     }
 
     private static void checkValidTransportType(@Transport int transport) {
-        Preconditions.checkArgument(
-                isValidTransport(transport), "Invalid TransportType " + transport);
+        if (!isValidTransport(transport)) {
+            throw new IllegalArgumentException("Invalid TransportType " + transport);
+        }
     }
 
     private static boolean isValidCapability(@NetworkCapabilities.NetCapability int capability) {
@@ -2082,8 +2082,9 @@ public final class NetworkCapabilities implements Parcelable {
     }
 
     private static void checkValidCapability(@NetworkCapabilities.NetCapability int capability) {
-        Preconditions.checkArgument(isValidCapability(capability),
-                "NetworkCapability " + capability + "out of range");
+        if (!isValidCapability(capability)) {
+            throw new IllegalArgumentException("NetworkCapability " + capability + "out of range");
+        }
     }
 
     /**
