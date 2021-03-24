5243e14163340de5ef2d60f46c8c90d45e68c7aa
lifr <lifr@google.com>
2021-03-24 18:38:37 +0800

    Add session ID to VpnTransportInfo
    - This will be visible only to apps with the NETWORK_SETTINGS
      permissions (signature), and will be redacted for all other callers.
    - This string is expected to be the same as set by
      VpnService#setSession, and in general, VpnConfig.session. But it
      will be a general API that Vpn.java can call when setting the
      VpnTransportInfo.
    - This string cannot be updated once the VPN NetworkAgent is connected.
    
    Bug: 171872481
    Test: atest ConnectivityServiceTest
          atest VpnTransportInfoTest
          atest android.net.cts.NetworkAgentTest
    Change-Id: I8d09e25b83f7ee8be21ec9c9bd3c72a251f1370d

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -464,7 +464,7 @@ public class Vpn {
                 .addTransportType(NetworkCapabilities.TRANSPORT_VPN)
                 .removeCapability(NetworkCapabilities.NET_CAPABILITY_NOT_VPN)
                 .addCapability(NetworkCapabilities.NET_CAPABILITY_NOT_VCN_MANAGED)
-                .setTransportInfo(new VpnTransportInfo(VpnManager.TYPE_VPN_NONE))
+                .setTransportInfo(new VpnTransportInfo(VpnManager.TYPE_VPN_NONE, null))
                 .build();
 
         loadAlwaysOnPackage();
@@ -528,7 +528,7 @@ public class Vpn {
     private void resetNetworkCapabilities() {
         mNetworkCapabilities = new NetworkCapabilities.Builder(mNetworkCapabilities)
                 .setUids(null)
-                .setTransportInfo(new VpnTransportInfo(VpnManager.TYPE_VPN_NONE))
+                .setTransportInfo(new VpnTransportInfo(VpnManager.TYPE_VPN_NONE, null))
                 .build();
     }
 
@@ -1256,7 +1256,7 @@ public class Vpn {
         capsBuilder.setUids(createUserAndRestrictedProfilesRanges(mUserId,
                 mConfig.allowedApplications, mConfig.disallowedApplications));
 
-        capsBuilder.setTransportInfo(new VpnTransportInfo(getActiveVpnType()));
+        capsBuilder.setTransportInfo(new VpnTransportInfo(getActiveVpnType(), mConfig.session));
 
         // Only apps targeting Q and above can explicitly declare themselves as metered.
         // These VPNs are assumed metered unless they state otherwise.
