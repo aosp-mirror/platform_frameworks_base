a7acb81dd9d4bf49d0dc26b62c936c60e0651730
Dave Mankoff <mankoff@google.com>
2021-03-31 12:06:18 -0400

    Fix ConcurrentModExc in TelephonyListenerManager
    Make a copy of the callbacks before executing them in
    TelephonyListenerManager.
    
    Also, restore running the callbacks to the background thread in
    NetworkControllerImpl.
    
    Fixes: 184019276
    Test: atest SystemUITests
    Change-Id: I7cc1aad4db8eada1e3c9ccc7dd25d05af216f395

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/policy/NetworkControllerDataTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/policy/NetworkControllerDataTest.java
@@ -108,7 +108,7 @@ public class NetworkControllerDataTest extends NetworkControllerBaseTest {
         mConfig.show4gForLte = true;
         mNetworkController = new NetworkControllerImpl(mContext, mMockCm, mMockTm,
                 mTelephonyListenerManager, mMockWm,
-                mMockNsm, mMockSm, mConfig, Looper.getMainLooper(), mCallbackHandler,
+                mMockNsm, mMockSm, mConfig, Looper.getMainLooper(), mFakeExecutor, mCallbackHandler,
                 mock(AccessPointControllerImpl.class),
                 mock(DataUsageController.class), mMockSubDefaults,
                 mock(DeviceProvisionedController.class), mMockBd, mDemoModeController);
