6fbea7cbbdda028ad7361e5b56e6b21da4678a7c
Hans Boehm <hboehm@google.com>
2021-04-01 14:10:31 -0700

    Rename to isIndefiniteThreadSuspensionSafe
    Bug: 181707712
    Test: Build and boot AOSP
    Change-Id: I0d95578cf689223a75fcb132f092805d77b1805b

--- core/java/com/android/internal/os/ZygoteServer.java
+++ core/java/com/android/internal/os/ZygoteServer.java
@@ -547,7 +547,7 @@ class ZygoteServer {
                         try {
                             ZygoteConnection connection = peers.get(pollIndex);
                             boolean multipleForksOK = !isUsapPoolEnabled()
-                                    && ZygoteHooks.indefiniteThreadSuspensionOK();
+                                    && ZygoteHooks.isIndefiniteThreadSuspensionSafe();
                             final Runnable command =
                                     connection.processCommand(this, multipleForksOK);
 
