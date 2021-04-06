62718c2bbf405a555e5f1ccecbc3c0e4b67c2d79
Aaron Huang <huangaaron@google.com>
2021-04-06 11:50:21 +0800

    Expose Context#PAC_PROXY_SERVICE as module lib API
    Bug: 183938000
    Test: make update-api
    Change-Id: I4341e177b61df0311f0a3234f8814a0ae2cf9a1b

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -4165,6 +4165,7 @@ public abstract class Context {
      * @see android.net.PacProxyManager
      * @hide
      */
+    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
     public static final String PAC_PROXY_SERVICE = "pac_proxy";
 
     /**
