0c6aa86ea7c7bca1f3339e7ec0c120e7b8a18eec
Peter_Liang <peterliang@google.com>
2021-04-07 14:16:52 +0800

    Add key ACCESSIBILITY_FLOATING_MENU_MIGRATION_TOOLTIP_PROMPT.
    The key used to show the migration information related to
    the accessibility floating menu.
    
    Bug: 175365399
    Test: atest SettingsProviderTest
    Change-Id: Ifabc412b990c72ad345bab2d5d8539a57bd6ca5d

--- packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
+++ packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
@@ -752,6 +752,7 @@ public class SettingsBackupTest {
                  Settings.Secure.SUPPRESS_DOZE,
                  Settings.Secure.REDUCE_BRIGHT_COLORS_ACTIVATED,
                  Settings.Secure.ACCESSIBILITY_SHOW_WINDOW_MAGNIFICATION_PROMPT,
+                 Settings.Secure.ACCESSIBILITY_FLOATING_MENU_MIGRATION_TOOLTIP_PROMPT,
                  Settings.Secure.UI_TRANSLATION_ENABLED);
 
     @Test
