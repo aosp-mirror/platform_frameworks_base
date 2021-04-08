fecd017a1de50beaa0e9c2e32837774428030668
kholoud mohamed <kholoudm@google.com>
2021-04-08 13:44:04 +0100

    Fix bug in LockSettingsService
    Params for passwordToHistoryHash were
    passed in the wrong order.
    Also removed an used method.
    
    Bug: 183716601
    Test: N/A
    Change-Id: I370f30ba2ad8fa0eba311c5219b8185dccda8e5e

--- services/core/java/com/android/server/locksettings/LockSettingsService.java
+++ services/core/java/com/android/server/locksettings/LockSettingsService.java
@@ -1773,7 +1773,7 @@ public class LockSettingsService extends ILockSettings.Stub {
         } else {
             final byte[] hashFactor = getHashFactor(password, userHandle);
             final byte[] salt = getSalt(userHandle).getBytes();
-            String hash = password.passwordToHistoryHash(hashFactor, salt);
+            String hash = password.passwordToHistoryHash(salt, hashFactor);
             if (hash == null) {
                 Slog.e(TAG, "Compute new style password hash failed, fallback to legacy style");
                 hash = password.legacyPasswordToHash(salt);
