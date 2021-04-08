bc45cab1f2bf7338a4db271fe37102ef998c1b35
yingleiw <yingleiw@google.com>
2021-04-07 18:14:14 -0700

    Use state description of "in progress" for indeterminte ProgressBar
    When a ProgressBar is set to indeterminate, we assign a default
    state description of "in progress" to AccessibilityNodeInfo. We
    only assign default state description to AccessibilityNodeInfo,
    but not to View. If the developer
    sets a custom state description through either View API or
    AccessibilityNodeInfo API, the custom state description will
    be used for both indeterminate and determinate progressbar instead
    of default ones.
    
    We put the "in progress" in state description instead of content
    description to prevent accessibility services like talkback from
    automatically assigning a state description when the state description
    is null or empty.
    
    Fix: b/182582617
    
    Test: tested manually with a material linear indeterminate progressbar
    with talkback. It says "In progress, progress bar".
    Unit tests added.
    
    Change-Id: I24477622bf8992f80a19b8df3a6032bac7f9d5bd

--- core/java/android/widget/ProgressBar.java
+++ core/java/android/widget/ProgressBar.java
@@ -245,8 +245,6 @@ public class ProgressBar extends View {
 
     private boolean mAggregatedIsVisible;
 
-    private CharSequence mCustomStateDescription = null;
-
     private final ArrayList<RefreshData> mRefreshData = new ArrayList<RefreshData>();
 
     private ObjectAnimator mLastProgressAnimator;
@@ -685,6 +683,9 @@ public class ProgressBar extends View {
                 swapCurrentDrawable(mProgressDrawable);
                 stopAnimation();
             }
+
+            notifyViewAccessibilityStateChangedIfNeeded(
+                    AccessibilityEvent.CONTENT_CHANGE_TYPE_UNDEFINED);
         }
     }
 
@@ -1622,18 +1623,21 @@ public class ProgressBar extends View {
     @Override
     @RemotableViewMethod
     public void setStateDescription(@Nullable CharSequence stateDescription) {
-        mCustomStateDescription = stateDescription;
-        if (stateDescription == null) {
-            super.setStateDescription(formatStateDescription(mProgress));
-        } else {
-            super.setStateDescription(stateDescription);
-        }
+        // Assume the previous custom state description is different from default state description.
+        // Otherwise when the argument is null to restore the default state description, we will
+        // send out a state description changed event even though the state description presented to
+        // the user doesn't change. Since mStateDescription in View is private, we can't prevent
+        // this event from sending out.
+        super.setStateDescription(stateDescription);
     }
 
     void onProgressRefresh(float scale, boolean fromUser, int progress) {
         if (AccessibilityManager.getInstance(mContext).isEnabled()
-                && mCustomStateDescription == null) {
-            super.setStateDescription(formatStateDescription(mProgress));
+                && getStateDescription() == null && !isIndeterminate()) {
+            AccessibilityEvent event = AccessibilityEvent.obtain();
+            event.setEventType(AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED);
+            event.setContentChangeTypes(AccessibilityEvent.CONTENT_CHANGE_TYPE_STATE_DESCRIPTION);
+            sendAccessibilityEventUnchecked(event);
         }
     }
 
@@ -2356,7 +2360,15 @@ public class ProgressBar extends View {
                     AccessibilityNodeInfo.RangeInfo.RANGE_TYPE_INT, getMin(), getMax(),
                     getProgress());
             info.setRangeInfo(rangeInfo);
-            info.setStateDescription(formatStateDescription(mProgress));
+        }
+
+        // Only set the default state description when custom state descripton is null.
+        if (getStateDescription() == null) {
+            if (isIndeterminate()) {
+                info.setStateDescription(getResources().getString(R.string.in_progress));
+            } else {
+                info.setStateDescription(formatStateDescription(mProgress));
+            }
         }
     }
 
