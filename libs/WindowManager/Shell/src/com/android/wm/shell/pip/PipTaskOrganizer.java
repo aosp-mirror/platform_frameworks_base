bf5d97ba93c7d7965ab8d21204e3dd89a765b08c
jorgegil@google.com <jorgegil@google.com>
2021-04-09 12:36:18 -0700

    Fix race when rotating the display during an entry into PIP
    Rotating the display while the PIP task had just appeared caused
    a race between the alpha entry animation starting and the rotation
    controller handling that changes the PIP bounds, resulting in
    a bounds mismatch between the surface that was animated and the
    PipBoundsState.mBounds
    
    This CL tracks whether we're still waiting for the alpha entry
    animation to start when a rotation callback comes in, and
    restarts the fade in after adjusting to the rotation.
    
    Bug: 184664409
    Test: In gesure nav, open YT go to fullscreen landscape mode and
    launch another an activity from a notification - verify PIP fades
    in and its bounds match the ones in PipBoundsState
    
    Change-Id: I339d5088fb5f23f08fe8d548b9d02a34c16779d6

--- libs/WindowManager/Shell/src/com/android/wm/shell/pip/PipTaskOrganizer.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/pip/PipTaskOrganizer.java
@@ -98,9 +98,10 @@ public class PipTaskOrganizer implements ShellTaskOrganizer.TaskListener,
     private enum State {
         UNDEFINED(0),
         TASK_APPEARED(1),
-        ENTERING_PIP(2),
-        ENTERED_PIP(3),
-        EXITING_PIP(4);
+        ENTRY_SCHEDULED(2),
+        ENTERING_PIP(3),
+        ENTERED_PIP(4),
+        EXITING_PIP(5);
 
         private final int mStateValue;
 
@@ -261,6 +262,13 @@ public class PipTaskOrganizer implements ShellTaskOrganizer.TaskListener,
 
     public boolean isDeferringEnterPipAnimation() {
         return mState.isInPip() && mWaitForFixedRotation;
+    }
+
+    /**
+     * Returns whether the entry animation is waiting to be started.
+     */
+    public boolean isEntryScheduled() {
+        return mState == State.ENTRY_SCHEDULED;
     }
 
     /**
@@ -481,6 +489,19 @@ public class PipTaskOrganizer implements ShellTaskOrganizer.TaskListener,
         }
     }
 
+    /**
+     * Called when the display rotation handling is skipped (e.g. when rotation happens while in
+     * the middle of an entry transition).
+     */
+    public void onDisplayRotationSkipped() {
+        if (isEntryScheduled()) {
+            // The PIP animation is scheduled to start with the previous orientation's bounds,
+            // re-calculate the entry bounds and restart the alpha animation.
+            final Rect destinationBounds = mPipBoundsAlgorithm.getEntryDestinationBounds();
+            enterPipWithAlphaAnimation(destinationBounds, mEnterAnimationDuration);
+        }
+    }
+
     @VisibleForTesting
     void enterPipWithAlphaAnimation(Rect destinationBounds, long durationMs) {
         // If we are fading the PIP in, then we should move the pip to the final location as
@@ -490,6 +511,7 @@ public class PipTaskOrganizer implements ShellTaskOrganizer.TaskListener,
                 mSurfaceControlTransactionFactory.getTransaction();
         tx.setAlpha(mLeash, 0f);
         tx.apply();
+        mState = State.ENTRY_SCHEDULED;
         applyEnterPipSyncTransaction(destinationBounds, () -> {
             mPipAnimationController
                     .getAnimator(mTaskInfo, mLeash, destinationBounds, 0f, 1f)
