2ac94cb7a27df5962a736937ba658c7de5e7139e
Makoto Onuki <omakoto@google.com>
2021-04-12 11:39:21 -0700

    Remove the FGS exemption list
    Fix: 176844961
    Test: Build / Boot
    Change-Id: I546ceebb64e05739577ed288e9603f0636c6aaff

--- apex/jobscheduler/framework/java/android/os/PowerWhitelistManager.java
+++ apex/jobscheduler/framework/java/android/os/PowerWhitelistManager.java
@@ -176,8 +176,6 @@ public class PowerWhitelistManager {
     /** @hide */
     public static final int REASON_DEVICE_DEMO_MODE = PowerExemptionManager.REASON_DEVICE_DEMO_MODE;
     /** @hide */
-    public static final int REASON_EXEMPTED_PACKAGE = PowerExemptionManager.REASON_EXEMPTED_PACKAGE;
-    /** @hide */
     public static final int REASON_ALLOWLISTED_PACKAGE =
             PowerExemptionManager.REASON_ALLOWLISTED_PACKAGE;
     /** @hide */
@@ -341,7 +339,6 @@ public class PowerWhitelistManager {
             REASON_INSTR_BACKGROUND_FGS_PERMISSION,
             REASON_SYSTEM_ALERT_WINDOW_PERMISSION,
             REASON_DEVICE_DEMO_MODE,
-            REASON_EXEMPTED_PACKAGE,
             REASON_ALLOWLISTED_PACKAGE,
             REASON_APPOP,
             // temp and system allowlist reasons.
