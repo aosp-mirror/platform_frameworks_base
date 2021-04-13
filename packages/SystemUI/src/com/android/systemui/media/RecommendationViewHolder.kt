5b449e1f2f56701b28e29a99c73bd6c5317d2c0b
cecilia <xiaotinghong@google.com>
2021-04-12 20:13:46 -0400

    Rename the Smartspace media recommendation layout to avoid potential mismatch.
    Test: Just renaming the resources, nothing else changed.
    Bug: 185176701
    Change-Id: Ic065090022ac47dd672bc191f0d8a870778e6253

--- packages/SystemUI/src/com/android/systemui/media/RecommendationViewHolder.kt
+++ packages/SystemUI/src/com/android/systemui/media/RecommendationViewHolder.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.systemui.media
 
 import android.view.LayoutInflater
@@ -50,7 +66,10 @@ class RecommendationViewHolder private constructor(itemView: View) {
         @JvmStatic fun create(inflater: LayoutInflater, parent: ViewGroup):
             RecommendationViewHolder {
             val itemView =
-                inflater.inflate(R.layout.smartspace_card_media, parent, /* attachToRoot= */ false)
+                inflater.inflate(
+                    R.layout.media_smartspace_recommendations,
+                    parent,
+                    false /* attachToRoot */)
             // Because this media view (a TransitionLayout) is used to measure and layout the views
             // in various states before being attached to its parent, we can't depend on the default
             // LAYOUT_DIRECTION_INHERIT to correctly resolve the ltr direction.
