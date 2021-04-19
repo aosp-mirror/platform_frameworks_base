985c7eaf4db6c3ae2da6c476127fc48d3fbb93f8
Ying Xu <yinxu@google.com>
2021-04-19 17:54:14 +0000

    Revert "Ignore the WiFi network callbacks if they are not default"
    This reverts commit 302f6cf17f6b2578a4a43b024ce751643352200b.
    
    Reason for revert:
    
    As explained in https://buganizer.corp.google.com/issues/185431894#comment22, the WiFi network and the VPN over that WiFi network are considered different networks, which will cause the WiFi network update to be ignored by the SysUI
    
    Bug: 185431894
    Test: unit tests
    
    Change-Id: I715960ba925be9cd469065c1220b879b4cca882b

--- packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
+++ packages/SettingsLib/src/com/android/settingslib/wifi/WifiStatusTracker.java
@@ -80,9 +80,6 @@ public class WifiStatusTracker {
         @Override
         public void onCapabilitiesChanged(
                 Network network, NetworkCapabilities networkCapabilities) {
-            if (mDefaultNetwork != null && mDefaultNetwork.getNetId() != network.getNetId()) {
-                return;
-            }
             if (!mNetworks.contains(network.getNetId())) {
                 // New network
                 boolean isVcnOverWifi =
@@ -117,9 +114,6 @@ public class WifiStatusTracker {
 
         @Override
         public void onLost(Network network) {
-            if (mDefaultNetwork != null && mDefaultNetwork.getNetId() != network.getNetId()) {
-                return;
-            }
             String log = new StringBuilder()
                     .append(SSDF.format(System.currentTimeMillis())).append(",")
                     .append("onLost: ")
