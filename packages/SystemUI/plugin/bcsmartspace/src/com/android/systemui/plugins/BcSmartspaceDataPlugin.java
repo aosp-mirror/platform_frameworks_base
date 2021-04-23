de36555b9fbd5eb20b35087312e69cafcc38e80f
Matt Pietal <mpietal@google.com>
2021-04-23 07:52:31 -0400

    Smartspace - Support launching intents from lockscreen
    Also integrate the FalsingManager, which is important for determining
    user intent while on the lockscreen.
    
    Fixes: 186219857
    Test: atest KeyguardClockSwitchControllerTest
    
    Change-Id: I22108ec2335bd350fbcaf5d5b90dd3b351011767

--- packages/SystemUI/plugin/bcsmartspace/src/com/android/systemui/plugins/BcSmartspaceDataPlugin.java
+++ packages/SystemUI/plugin/bcsmartspace/src/com/android/systemui/plugins/BcSmartspaceDataPlugin.java
@@ -16,8 +16,12 @@
 
 package com.android.systemui.plugins;
 
+import android.app.PendingIntent;
+import android.app.smartspace.SmartspaceAction;
 import android.app.smartspace.SmartspaceTarget;
+import android.content.Intent;
 import android.os.Parcelable;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.android.systemui.plugins.annotations.ProvidesInterface;
@@ -68,5 +72,33 @@ public interface BcSmartspaceDataPlugin extends Plugin {
          * Range [0.0 - 1.0] when transitioning from Lockscreen to/from AOD
          */
         void setDozeAmount(float amount);
+
+        /**
+         * Overrides how Intents/PendingIntents gets launched. Mostly to support auth from
+         * the lockscreen.
+         */
+        void setIntentStarter(IntentStarter intentStarter);
+
+        /**
+         * When on the lockscreen, use the FalsingManager to help detect errant touches
+         */
+        void setFalsingManager(FalsingManager falsingManager);
+    }
+
+    /** Interface for launching Intents, which can differ on the lockscreen */
+    interface IntentStarter {
+        default void startFromAction(SmartspaceAction action, View v) {
+            if (action.getIntent() != null) {
+                startIntent(v, action.getIntent());
+            } else if (action.getPendingIntent() != null) {
+                startPendingIntent(action.getPendingIntent());
+            }
+        }
+
+        /** Start the intent */
+        void startIntent(View v, Intent i);
+
+        /** Start the PendingIntent */
+        void startPendingIntent(PendingIntent pi);
     }
 }
