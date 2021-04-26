fde81bf22998e0cebf91f7d725bcd58a069d566b
Galia Peycheva <galinap@google.com>
2021-04-26 14:11:09 +0200

    Add TunnelModeStateListener in java+jni
    When tunnel mode is being used (meaning when there
    is a sideband stream in one of the layers in SurfaceFlinger), the
    blur can't be computed over that layer. In that case, SF blurs are
    disabled, which results in a translucent buffer to be drawn.
    
    This CL introduces a listener, which is registered with the
    SurfaceFlinger and receives a callback whenever the tunnel mode
    enabled state changes. This way, the BlurController can properly send
    updates to apps that blurs are disabled/enabled.
    
    Bug: 171457637
    Test: atest TunnelModeStateListenerTest
    Test: atest TunnelModeStateReporterTest
    Change-Id: I6ffba1c342352ad65a32c0efc4ecbc07811245d9

---
+++ core/tests/coretests/src/android/view/TunnelModeEnabledListenerTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.view;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import android.platform.test.annotations.Presubmit;
+
+import androidx.test.filters.MediumTest;
+import androidx.test.runner.AndroidJUnit4;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@MediumTest
+@Presubmit
+public class TunnelModeEnabledListenerTest {
+    private TestableTunnelModeEnabledListener mListener;
+
+    @Before
+    public void init() {
+        mListener = new TestableTunnelModeEnabledListener();
+    }
+
+    @Test
+    public void testRegisterUnregister() {
+        TunnelModeEnabledListener.register(mListener);
+        TunnelModeEnabledListener.unregister(mListener);
+    }
+
+    @Test
+    public void testDispatchUpdatesListener() {
+        TunnelModeEnabledListener.dispatchOnTunnelModeEnabledChanged(mListener, true);
+        assertEquals(true, mListener.mTunnelModeEnabled.get());
+        TunnelModeEnabledListener.dispatchOnTunnelModeEnabledChanged(mListener, false);
+        assertEquals(false, mListener.mTunnelModeEnabled.get());
+    }
+
+    @Test
+    public void testRegisterUpdatesListener() throws Exception {
+        TunnelModeEnabledListener.register(mListener);
+        TimeUnit.SECONDS.sleep(1);
+        assertTrue(mListener.mTunnelModeEnabledUpdated.get());
+        TunnelModeEnabledListener.unregister(mListener);
+    }
+
+    private static class TestableTunnelModeEnabledListener extends TunnelModeEnabledListener {
+        AtomicBoolean mTunnelModeEnabled;
+        AtomicBoolean mTunnelModeEnabledUpdated;
+
+        TestableTunnelModeEnabledListener() {
+            super(Runnable::run);
+            mTunnelModeEnabled = new AtomicBoolean(false);
+            mTunnelModeEnabledUpdated = new AtomicBoolean();
+        }
+
+        @Override
+        public void onTunnelModeEnabledChanged(boolean tunnelModeEnabled) {
+            mTunnelModeEnabled.set(tunnelModeEnabled);
+            mTunnelModeEnabledUpdated.set(true);
+        }
+    }
+
+}
