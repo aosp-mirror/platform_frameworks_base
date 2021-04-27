9bedbd28b8c34feeed0ee373fed5ee7d7d3cc1aa
Alex Johnston <acjohnston@google.com>
2021-04-27 10:33:48 +0100

    DPM Check for unaffiliated secondary users
    * Add authorization check in DPMS for API
      methods to set the private DNS mode and
      the recommended global proxy.
    * When there is a unaffiliated user, it is
      assumed that it is managed separately.
    * These are device-wide policies that affect
      all users, so should only be set when it is
      the same managing entity and all users on
      the device are affiliated.
    
    Bug: 177926343
    Test: atest com.android.server.devicepolicy.DevicePolicyManagerTest
          atest com.android.cts.devicepolicy.DeviceOwnerTest#testProxyPacProxyTest
          atest com.android.cts.devicepolicy.DeviceOwnerTest#testProxyStaticProxyTest
    Change-Id: I119a84988ad3cda42b709dbc6d63acf76b0031d6

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -7232,6 +7232,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         Objects.requireNonNull(who, "ComponentName is null");
         final CallerIdentity caller = getCallerIdentity(who);
         Preconditions.checkCallAuthorization(isDeviceOwner(caller));
+        checkAllUsersAreAffiliatedWithDevice();
         mInjector.binderWithCleanCallingIdentity(
                 () -> mInjector.getConnectivityManager().setGlobalProxy(proxyInfo));
     }
@@ -15709,6 +15710,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         Objects.requireNonNull(who, "ComponentName is null");
         final CallerIdentity caller = getCallerIdentity(who);
         Preconditions.checkCallAuthorization(isDeviceOwner(caller));
+        checkAllUsersAreAffiliatedWithDevice();
         checkCanExecuteOrThrowUnsafe(DevicePolicyManager.OPERATION_SET_GLOBAL_PRIVATE_DNS);
 
         switch (mode) {
