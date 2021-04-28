2de948bbee902514ac30c9d8363067c206d9a7d0
Christopher Tate <ctate@google.com>
2021-04-28 10:12:09 -0700

    Don't defer FGS notification if it's already shown
    Bug: 185523487
    Test: atest CtsAppTestCases:android.app.cts.ServiceTest#testForegroundService_deferredExistingNotification
    Change-Id: I081a3cc8862caf4d53dbd46be1b8b1d4e626ebc0

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -6030,6 +6030,13 @@ public class NotificationManagerService extends SystemService {
         }
 
         @Override
+        public boolean isNotificationShown(String pkg, String tag, int notificationId, int userId) {
+            synchronized (mNotificationLock) {
+                return findNotificationLocked(pkg, tag, notificationId, userId) != null;
+            }
+        }
+
+        @Override
         public void removeForegroundServiceFlagFromNotification(String pkg, int notificationId,
                 int userId) {
             checkCallerIsSystem();
