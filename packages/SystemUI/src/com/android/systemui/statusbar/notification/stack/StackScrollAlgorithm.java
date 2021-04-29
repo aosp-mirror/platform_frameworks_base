3b9c828afd0f6e571ce4bc5537a248b1b0fa5f8d
Lucas Dupin <dupin@google.com>
2021-04-29 23:15:42 +0000

    Revert "Fix notification top clipping"
    This reverts commit 6f97c0a8bf3c58c89d5ccd5ede7e32b6a02c5f83.
    
    Reason for revert: b/186760125
    
    Change-Id: I2675bb52c453c872505bc5ad420bf33d669bf6e1
    (cherry picked from commit 6782bec03d998bf0ca87f6a006832901c5ceb8e3)

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/StackScrollAlgorithm.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/StackScrollAlgorithm.java
@@ -151,7 +151,9 @@ public class StackScrollAlgorithm {
 
     private void updateClipping(StackScrollAlgorithmState algorithmState,
             AmbientState ambientState) {
-        float drawStart = !ambientState.isOnKeyguard() ? ambientState.getStackY() : 0;
+        float drawStart = !ambientState.isOnKeyguard() ? ambientState.getTopPadding()
+                + ambientState.getStackTranslation()
+                : 0;
         float clipStart = 0;
         int childCount = algorithmState.visibleChildren.size();
         boolean firstHeadsUp = true;
@@ -164,7 +166,8 @@ public class StackScrollAlgorithm {
             float newYTranslation = state.yTranslation;
             float newHeight = state.height;
             float newNotificationEnd = newYTranslation + newHeight;
-            boolean isHeadsUp = (child instanceof ExpandableNotificationRow) && child.isPinned();
+            boolean isHeadsUp = (child instanceof ExpandableNotificationRow)
+                    && ((ExpandableNotificationRow) child).isPinned();
             if (mClipNotificationScrollToTop
                     && (!state.inShelf || (isHeadsUp && !firstHeadsUp))
                     && newYTranslation < clipStart
