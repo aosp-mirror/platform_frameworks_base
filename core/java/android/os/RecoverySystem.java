Neoteric-OS/android_frameworks_base

Call apexd's API to allocate space before install non-AB package
When installing a non-AB package, if the OS comtains compressed apexes,
we need to allocate space for these apexes so that they can be properly
decompressed on the next reboot.

Test: adb shell cmd recovery install-package /data/ota_package.zip
Change-Id: Ia40d0614e0e724cfb17e91720ec88a15795bd8ee

core/java/android/os/RecoverySystem.java
@@ -596,6 +596,7 @@ public class RecoverySystem {
         installPackage(context, packageFile, false);
     }
 
+
     /**
      * If the package hasn't been processed (i.e. uncrypt'd), set up
      * UNCRYPT_PACKAGE_FILE and delete BLOCK_MAP_FILE to trigger uncrypt during the
@@ -658,6 +659,7 @@ public class RecoverySystem {
                 filename = "@/cache/recovery/block.map";
             }
 
+
             final String filenameArg = "--update_package=" + filename + "\n";
             final String localeArg = "--locale=" + Locale.getDefault().toLanguageTag() + "\n";
             final String securityArg = "--security\n";
@@ -671,6 +673,14 @@ public class RecoverySystem {
                     Context.RECOVERY_SERVICE);
             if (!rs.setupBcb(command)) {
                 throw new IOException("Setup BCB failed");
+            }
+            try {
+                if (!rs.allocateSpaceForUpdate(packageFile)) {
+                    throw new RuntimeException("Failed to allocate space for update "
+                            + packageFile.getAbsolutePath());
+                }
+            } catch (RemoteException e) {
+                e.rethrowAsRuntimeException();
             }
 
             // Having set up the BCB (bootloader control block), go ahead and reboot
@@ -1389,6 +1399,24 @@ public class RecoverySystem {
         } catch (RemoteException unused) {
         }
         return false;
+    }
+
+    /**
+     * Talks to RecoverySystemService via Binder to allocate space
+     */
+    private boolean allocateSpaceForUpdate(File packageFile) throws RemoteException {
+        return mService.allocateSpaceForUpdate(packageFile.getAbsolutePath());
+    }
+
+    /**
+     * Talks to RecoverySystemService via Binder to allocate space
+     */
+    private long calculateSizeForCompressedApex(File packageFile) {
+        try {
+            return mService.calculateSizeForCompressedApex(packageFile.getAbsolutePath());
+        } catch (RemoteException unused) {
+        }
+        return -1;
     }
 
     /**

aosp-mirror/platform_frameworks_base

core/java/android/os/RecoverySystem.java
@@ -672,6 +672,14 @@ public class RecoverySystem {
             if (!rs.setupBcb(command)) {
                 throw new IOException("Setup BCB failed");
             }
+            try {
+                if (!rs.allocateSpaceForUpdate(packageFile)) {
+                    throw new IOException("Failed to allocate space for update "
+                            + packageFile.getAbsolutePath());
+                }
+            } catch (RemoteException e) {
+                e.rethrowAsRuntimeException();
+            }
 
             // Having set up the BCB (bootloader control block), go ahead and reboot
             PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
@@ -1389,6 +1397,13 @@ public class RecoverySystem {
         } catch (RemoteException unused) {
         }
         return false;
+    }
+
+    /**
+     * Talks to RecoverySystemService via Binder to allocate space
+     */
+    private boolean allocateSpaceForUpdate(File packageFile) throws RemoteException {
+        return mService.allocateSpaceForUpdate(packageFile.getAbsolutePath());
     }
 
     /**