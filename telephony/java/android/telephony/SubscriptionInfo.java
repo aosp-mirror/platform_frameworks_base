873d4bffb26cf2673ddfa949928186c7c99dd564
SongFerngWang <songferngwang@google.com>
2021-05-05 21:33:00 +0800

    [security] SubscriptionGroup is exposed to unprivileged callers
    SubscriptionInfo.mGroupUUID is not cleared in
    conditionallyRemoveIdentifiers if the caller only has READ_PHONE_STATE
    (based on a check to checkReadPhoneState) and not READ_DEVICE_IDENTIFIERS.
    Bug: 181053462
    Test: atest SubscriptionManagerTest
    
    Change-Id: Ic2b62523330dc6e2169ad851715c4ab3da3b29cf
    Merged-In: Ic2b62523330dc6e2169ad851715c4ab3da3b29cf

--- telephony/java/android/telephony/SubscriptionInfo.java
+++ telephony/java/android/telephony/SubscriptionInfo.java
@@ -522,6 +522,13 @@ public class SubscriptionInfo implements Parcelable {
     /**
      * @hide
      */
+    public void clearGroupUuid() {
+        this.mGroupUUID = null;
+    }
+
+    /**
+     * @hide
+     */
     public List<String> getEhplmns() {
         return mEhplmns == null ? Collections.emptyList() : Arrays.asList(mEhplmns);
     }
