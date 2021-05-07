Neoteric-OS/android_frameworks_base

Print human-readable OEM managed states
For NetworkTemplate and NetworkIdentity, print human-readable
representations of the OEM managed state in the classes' respective
toString() functions.

Bug: 180557699
Test: Manual - run atest NetworkTemplateTest NetworkIdentityTest
and verify that the logcat output shows the String representation of the
OEM managed state correctly.

Change-Id: Ia180b911f91f41937ac713e6b3691d82f682e146

core/java/android/net/NetworkTemplate.java
@@ -344,7 +344,7 @@ public class NetworkTemplate implements Parcelable {
             builder.append(", subType=").append(mSubType);
         }
         if (mOemManaged != OEM_MANAGED_ALL) {
-            builder.append(", oemManaged=").append(mOemManaged);
+            builder.append(", oemManaged=").append(getOemManagedName(mOemManaged));
         }
         return builder.toString();
     }
@@ -637,6 +637,19 @@ public class NetworkTemplate implements Parcelable {
                 return "PROXY";
             default:
                 return "UNKNOWN(" + matchRule + ")";
+        }
+    }
+
+    private static String getOemManagedName(int oemManaged) {
+        switch (oemManaged) {
+            case OEM_MANAGED_ALL:
+                return "OEM_MANAGED_ALL";
+            case OEM_MANAGED_NO:
+                return "OEM_MANAGED_NO";
+            case OEM_MANAGED_YES:
+                return "OEM_MANAGED_YES";
+            default:
+                return NetworkIdentity.getOemManagedNames(oemManaged);
         }
     }
 

aosp-mirror/platform_frameworks_base

core/java/android/net/NetworkTemplate.java
@@ -344,7 +344,7 @@ public class NetworkTemplate implements Parcelable {
             builder.append(", subType=").append(mSubType);
         }
         if (mOemManaged != OEM_MANAGED_ALL) {
-            builder.append(", oemManaged=").append(mOemManaged);
+            builder.append(", oemManaged=").append(getOemManagedNames(mOemManaged));
         }
         return builder.toString();
     }
@@ -637,6 +637,19 @@ public class NetworkTemplate implements Parcelable {
                 return "PROXY";
             default:
                 return "UNKNOWN(" + matchRule + ")";
+        }
+    }
+
+    private static String getOemManagedNames(int oemManaged) {
+        switch (oemManaged) {
+            case OEM_MANAGED_ALL:
+                return "OEM_MANAGED_ALL";
+            case OEM_MANAGED_NO:
+                return "OEM_MANAGED_NO";
+            case OEM_MANAGED_YES:
+                return "OEM_MANAGED_YES";
+            default:
+                return NetworkIdentity.getOemManagedNames(oemManaged);
         }
     }
 