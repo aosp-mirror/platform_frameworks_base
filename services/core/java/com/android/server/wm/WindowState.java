bd3409b0ffda8c6580be66b24dd6bb846f510996
wilsonshih <wilsonshih@google.com>
2021-05-11 14:42:45 +0800

    Fix ActivityRecord leak if starting window was transferred.
    Some windows would stuck in animating status because we didn't get the
    correct top activity which contains the starting window, after apply
    animation on this main window it would stuck in animating.
    
    Test: atest AmStartOptionsTests#testDashW_FinishingTop, then check
    there is no WindowState leak after test finish.
    Bug: 184830058
    
    Change-Id: I8f838427168c157732a50fadfccfc9500a3faccc

--- services/core/java/com/android/server/wm/WindowState.java
+++ services/core/java/com/android/server/wm/WindowState.java
@@ -2412,14 +2412,14 @@ class WindowState extends WindowContainer<WindowState> implements WindowManagerP
         }
 
         if (startingWindow && StartingSurfaceController.DEBUG_ENABLE_SHELL_DRAWER) {
-            // cancel the remove starting window animation on shell
+            // Cancel the remove starting window animation on shell. The main window might changed
+            // during animating, checking for all windows would be safer.
             if (mActivityRecord != null) {
-                final WindowState mainWindow =
-                        mActivityRecord.findMainWindow(false/* includeStartingApp */);
-                if (mainWindow != null && mainWindow.isSelfAnimating(0 /* flags */,
-                        ANIMATION_TYPE_STARTING_REVEAL)) {
-                    mainWindow.cancelAnimation();
-                }
+                mActivityRecord.forAllWindows(w -> {
+                    if (w.isSelfAnimating(0, ANIMATION_TYPE_STARTING_REVEAL)) {
+                        w.cancelAnimation();
+                    }
+                }, true);
             }
         }
 
