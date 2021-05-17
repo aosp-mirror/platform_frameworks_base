79cfa8925b294f1c6433c0e7761c567606532736
Fabian Kozynski <kozynski@google.com>
2021-05-17 16:23:15 -0400

    Fix margins between headers and tiles.
    Also:
    * Remove old classes.
    * Change height of tiles.
    * Add back clipping on PagedTileLayout, as the views don't need to go
    outside its bounds.
    * Fix a scrolling issue
    
    Fixes: 187460996
    Fixes: 187966021
    Fixes: 187334034
    Fixes: 188459321
    Test: manual
    Change-Id: Ide520f26982a3f2c8918424cb4c14d3d14da3724

--- packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickQSPanel.java
@@ -18,9 +18,7 @@ package com.android.systemui.qs;
 
 import android.content.Context;
 import android.content.res.Configuration;
-import android.graphics.Rect;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 
@@ -47,19 +45,6 @@ public class QuickQSPanel extends QSPanel {
         super(context, attrs);
         mMaxTiles = Math.min(DEFAULT_MAX_TILES,
                 getResources().getInteger(R.integer.quick_qs_panel_max_columns));
-    }
-
-    @Override
-    void initialize() {
-        super.initialize();
-        applyBottomMargin((View) mRegularTileLayout);
-    }
-
-    private void applyBottomMargin(View view) {
-        int margin = getResources().getDimensionPixelSize(R.dimen.qs_header_tile_margin_bottom);
-        MarginLayoutParams layoutParams = (MarginLayoutParams) view.getLayoutParams();
-        layoutParams.bottomMargin = margin;
-        view.setLayoutParams(layoutParams);
     }
 
     @Override
@@ -186,150 +171,6 @@ public class QuickQSPanel extends QSPanel {
     @Override
     protected QSEvent tileVisibleEvent() {
         return QSEvent.QQS_TILE_VISIBLE;
-    }
-
-    private static class HeaderTileLayout extends TileLayout {
-
-        private Rect mClippingBounds = new Rect();
-
-        HeaderTileLayout(Context context) {
-            super(context);
-            setClipChildren(false);
-            setClipToPadding(false);
-            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT,
-                    LayoutParams.WRAP_CONTENT);
-            lp.gravity = Gravity.CENTER_HORIZONTAL;
-            setLayoutParams(lp);
-        }
-
-        @Override
-        protected void onConfigurationChanged(Configuration newConfig) {
-            super.onConfigurationChanged(newConfig);
-            updateResources();
-        }
-
-        @Override
-        public void onFinishInflate(){
-            super.onFinishInflate();
-            updateResources();
-        }
-
-        private LayoutParams generateTileLayoutParams() {
-            LayoutParams lp = new LayoutParams(mCellWidth, mCellHeight);
-            return lp;
-        }
-
-        @Override
-        protected void addTileView(QSPanelControllerBase.TileRecord tile) {
-            addView(tile.tileView, getChildCount(), generateTileLayoutParams());
-        }
-
-        @Override
-        protected void onLayout(boolean changed, int l, int t, int r, int b) {
-            // We only care about clipping on the right side
-            mClippingBounds.set(0, 0, r - l, 10000);
-            setClipBounds(mClippingBounds);
-
-            calculateColumns();
-
-            for (int i = 0; i < mRecords.size(); i++) {
-                mRecords.get(i).tileView.setVisibility( i < mColumns ? View.VISIBLE : View.GONE);
-            }
-
-            setAccessibilityOrder();
-            layoutTileRecords(mColumns);
-        }
-
-        @Override
-        public boolean updateResources() {
-            mCellWidth = mContext.getResources().getDimensionPixelSize(R.dimen.qs_quick_tile_size);
-            mCellHeight = mCellWidth;
-
-            return false;
-        }
-
-        private boolean calculateColumns() {
-            int prevNumColumns = mColumns;
-            int maxTiles = mRecords.size();
-
-            if (maxTiles == 0){ // Early return during setup
-                mColumns = 0;
-                return true;
-            }
-
-            final int availableWidth = getMeasuredWidth() - getPaddingStart() - getPaddingEnd();
-            final int leftoverWhitespace = availableWidth - maxTiles * mCellWidth;
-
-            if (leftoverWhitespace > 0) {
-                mCellMarginHorizontal = leftoverWhitespace / Math.max(1, maxTiles);
-                mColumns = maxTiles;
-            } else{
-                mColumns = mCellWidth == 0 ? 1 :
-                        Math.min(maxTiles, availableWidth / mCellWidth );
-                // If we can only fit one column, use mCellMarginHorizontal to center it.
-                if (mColumns == 1) {
-                    mCellMarginHorizontal = (availableWidth - mCellWidth) / 2;
-                } else {
-                    mCellMarginHorizontal =
-                            (availableWidth - mColumns * mCellWidth) / mColumns;
-                }
-
-            }
-            return mColumns != prevNumColumns;
-        }
-
-        private void setAccessibilityOrder() {
-            if (mRecords != null && mRecords.size() > 0) {
-                View previousView = this;
-                for (QSPanelControllerBase.TileRecord record : mRecords) {
-                    if (record.tileView.getVisibility() == GONE) continue;
-                    previousView = record.tileView.updateAccessibilityOrder(previousView);
-                }
-            }
-        }
-
-        @Override
-        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-            // Measure each QS tile.
-            for (QSPanelControllerBase.TileRecord record : mRecords) {
-                if (record.tileView.getVisibility() == GONE) continue;
-                record.tileView.measure(exactly(mCellWidth), exactly(mCellHeight));
-            }
-
-            int height = mCellHeight;
-            if (height < 0) height = 0;
-
-            setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec), height);
-        }
-
-        @Override
-        public int getNumVisibleTiles() {
-            return Math.min(mRecords.size(), mColumns);
-        }
-
-        @Override
-        protected int getColumnStart(int column) {
-            if (mColumns == 1) {
-                // Only one column/tile. Use the margin to center the tile.
-                return getPaddingStart() + mCellMarginHorizontal;
-            }
-            return getPaddingStart() + mCellMarginHorizontal / 2
-                    + column *  (mCellWidth + mCellMarginHorizontal);
-        }
-
-        @Override
-        public void setListening(boolean listening, UiEventLogger uiEventLogger) {
-            boolean startedListening = !mListening && listening;
-            super.setListening(listening, uiEventLogger);
-            if (startedListening) {
-                // getNumVisibleTiles() <= mRecords.size()
-                for (int i = 0; i < getNumVisibleTiles(); i++) {
-                    QSTile tile = mRecords.get(i).tile;
-                    uiEventLogger.logWithInstanceId(QSEvent.QQS_TILE_VISIBLE, 0,
-                            tile.getMetricsSpec(), tile.getInstanceId());
-                }
-            }
-        }
     }
 
     static class QQSSideLabelTileLayout extends SideLabelTileLayout {
