32b756cfaea2f11e6df60a0f0b3f417fca534718
Winson Chiu <chiuwinson@google.com>
2021-05-17 19:07:38 +0000

    Revert "Temporary stop-gap for Chrome target SDK issue."
    This reverts commit c6be9fe47041743619d7e9ed7cf95e7c9207f825.
    
    Reason for revert: No longer needed, Chrome targets 30
    
    Bug: 183905675
    
    Change-Id: I7db4fda4f233414330c4daa38a96d088bd56ee09

--- core/java/android/content/pm/parsing/ParsingPackageUtils.java
+++ core/java/android/content/pm/parsing/ParsingPackageUtils.java
@@ -2799,15 +2799,7 @@ public class ParsingPackageUtils {
         }
     }
 
-    @SuppressWarnings("AndroidFrameworkCompatChange")
     private void convertSplitPermissions(ParsingPackage pkg) {
-        // STOPSHIP(b/183905675): REMOVE THIS TERRIBLE, HORRIBLE, NO GOOD, VERY BAD HACK
-        if ("com.android.chrome".equals(pkg.getPackageName())
-                && pkg.getVersionCode() <= 445500399
-                && pkg.getTargetSdkVersion() > Build.VERSION_CODES.R) {
-            pkg.setTargetSdkVersion(Build.VERSION_CODES.R);
-        }
-
         final int listSize = mSplitPermissionInfos.size();
         for (int is = 0; is < listSize; is++) {
             final PermissionManager.SplitPermissionInfo spi = mSplitPermissionInfos.get(is);
