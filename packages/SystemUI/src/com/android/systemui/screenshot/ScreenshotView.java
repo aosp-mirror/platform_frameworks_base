67fd70cdf35b958646a41431bcdb4155686bf2df
Miranda Kephart <mkephart@google.com>
2021-05-19 20:29:32 +0000

    Update screenshot UI to use new Material colors
    Also updates some of the padding/margins and makes the dismiss
    button 32dp wide (tappable size 48dp).
    
    https://screenshot.googleplex.com/6QrPRKuJX7ruAZA (light mode)
    https://screenshot.googleplex.com/6CwbfTCdUU8Jmrx (dark mode)
    
    Bug: 187967953
    Fix: 187967953
    
    Test: manual
    Change-Id: Ice0109633810b4de0aeaae9024fe4257bc2fd4c4

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotView.java
@@ -40,7 +40,6 @@ import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.Insets;
-import android.graphics.Outline;
 import android.graphics.PointF;
 import android.graphics.Rect;
 import android.graphics.Region;
@@ -61,7 +60,6 @@ import android.view.MotionEvent;
 import android.view.TouchDelegate;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewOutlineProvider;
 import android.view.ViewTreeObserver;
 import android.view.WindowInsets;
 import android.view.accessibility.AccessibilityManager;
@@ -108,7 +106,7 @@ public class ScreenshotView extends FrameLayout implements
     private static final long SCREENSHOT_DISMISS_ALPHA_DURATION_MS = 183;
     private static final long SCREENSHOT_DISMISS_ALPHA_OFFSET_MS = 50; // delay before starting fade
     private static final float SCREENSHOT_ACTIONS_START_SCALE_X = .7f;
-    private static final float ROUNDED_CORNER_RADIUS = .05f;
+    private static final float ROUNDED_CORNER_RADIUS = .25f;
     private static final int SWIPE_PADDING_DP = 12; // extra padding around views to allow swipe
 
     private final Interpolator mAccelerateInterpolator = new AccelerateInterpolator();
@@ -129,6 +127,7 @@ public class ScreenshotView extends FrameLayout implements
     private ScreenshotSelectorView mScreenshotSelectorView;
     private View mScreenshotStatic;
     private ImageView mScreenshotPreview;
+    private View mScreenshotPreviewBorder;
     private ImageView mScreenshotFlash;
     private ImageView mActionsContainerBackground;
     private HorizontalScrollView mActionsContainer;
@@ -265,6 +264,9 @@ public class ScreenshotView extends FrameLayout implements
     protected void onFinishInflate() {
         mScreenshotStatic = requireNonNull(findViewById(R.id.global_screenshot_static));
         mScreenshotPreview = requireNonNull(findViewById(R.id.global_screenshot_preview));
+        mScreenshotPreviewBorder = requireNonNull(
+                findViewById(R.id.global_screenshot_preview_border));
+        mScreenshotPreview.setClipToOutline(true);
 
         mActionsContainerBackground = requireNonNull(findViewById(
                 R.id.global_screenshot_actions_container_background));
@@ -278,15 +280,6 @@ public class ScreenshotView extends FrameLayout implements
         mShareChip = requireNonNull(mActionsContainer.findViewById(R.id.screenshot_share_chip));
         mEditChip = requireNonNull(mActionsContainer.findViewById(R.id.screenshot_edit_chip));
         mScrollChip = requireNonNull(mActionsContainer.findViewById(R.id.screenshot_scroll_chip));
-
-        mScreenshotPreview.setClipToOutline(true);
-        mScreenshotPreview.setOutlineProvider(new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                outline.setRoundRect(new Rect(0, 0, view.getWidth(), view.getHeight()),
-                        ROUNDED_CORNER_RADIUS * view.getWidth());
-            }
-        });
 
         int swipePaddingPx = (int) dpToPx(SWIPE_PADDING_DP);
         TouchDelegate previewDelegate = new TouchDelegate(
@@ -467,12 +460,18 @@ public class ScreenshotView extends FrameLayout implements
         mScreenshotFlash.setAlpha(0f);
         mScreenshotFlash.setVisibility(View.VISIBLE);
 
+        ValueAnimator borderFadeIn = ValueAnimator.ofFloat(0, 1);
+        borderFadeIn.setDuration(100);
+        borderFadeIn.addUpdateListener((animation) ->
+                mScreenshotPreviewBorder.setAlpha(animation.getAnimatedFraction()));
+
         if (showFlash) {
             dropInAnimation.play(flashOutAnimator).after(flashInAnimator);
             dropInAnimation.play(flashOutAnimator).with(toCorner);
         } else {
             dropInAnimation.play(toCorner);
         }
+        dropInAnimation.play(borderFadeIn).after(toCorner);
 
         dropInAnimation.addListener(new AnimatorListenerAdapter() {
             @Override
@@ -497,8 +496,8 @@ public class ScreenshotView extends FrameLayout implements
                         finalPos.y - dismissOffset - bounds.height() * cornerScale / 2f);
                 mScreenshotPreview.setScaleX(1);
                 mScreenshotPreview.setScaleY(1);
-                mScreenshotPreview.setX(finalPos.x - bounds.width() * cornerScale / 2f);
-                mScreenshotPreview.setY(finalPos.y - bounds.height() * cornerScale / 2f);
+                mScreenshotPreview.setX(finalPos.x - mScreenshotPreview.getWidth() / 2f);
+                mScreenshotPreview.setY(finalPos.y - mScreenshotPreview.getHeight() / 2f);
                 requestLayout();
 
                 createScreenshotActionsShadeAnimation().start();
@@ -739,6 +738,7 @@ public class ScreenshotView extends FrameLayout implements
         // Clear any references to the bitmap
         mScreenshotPreview.setImageDrawable(null);
         mScreenshotPreview.setVisibility(View.INVISIBLE);
+        mScreenshotPreviewBorder.setAlpha(0);
         mPendingSharedTransition = false;
         mActionsContainerBackground.setVisibility(View.GONE);
         mActionsContainer.setVisibility(View.GONE);
@@ -798,6 +798,7 @@ public class ScreenshotView extends FrameLayout implements
         yAnim.addUpdateListener(animation -> {
             float yDelta = MathUtils.lerp(0, mDismissDeltaY, animation.getAnimatedFraction());
             mScreenshotPreview.setTranslationY(screenshotStartY + yDelta);
+            mScreenshotPreviewBorder.setTranslationY(screenshotStartY + yDelta);
             mDismissButton.setTranslationY(dismissStartY + yDelta);
             mActionsContainer.setTranslationY(yDelta);
             mActionsContainerBackground.setTranslationY(yDelta);
