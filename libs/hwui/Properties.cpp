3f8b9fe50b739a6dd9b0aa35e6b1779c95fd6045
Brett Chabot <brettchabot@google.com>
2021-05-21 08:41:58 -0700

    Backport 'Add ability to disable GL draws'.
    This feature is intended for use in headless test environments,
    where disabling drawing has minimal compatibility impact and can
    save significant resource usage when running UI tests.
    
    This feature is controlled via a new system property
    'debug.hwui.drawing_enabled', and a HardwareRenderer API that can
    override the property value on a per-process basis.
    
    Bug: 201691826
    Test: boot emulator
    Merged-In: I93c21e680382e03342f235dbf58bd7a5c8a6f767
    Change-Id: I391c841e3a0cdcfcc6c7910a316a4dd5e970547d

--- libs/hwui/Properties.cpp
+++ libs/hwui/Properties.cpp
@@ -88,6 +88,9 @@ bool Properties::enableWebViewOverlays = false;
 
 StretchEffectBehavior Properties::stretchEffectBehavior = StretchEffectBehavior::ShaderHWUI;
 
+bool Properties::drawingEnabled = true;
+OverrideDrawingEnabled Properties::overrideDrawingEnabled = OverrideDrawingEnabled::Default;
+
 bool Properties::load() {
     bool prevDebugLayersUpdates = debugLayersUpdates;
     bool prevDebugOverdraw = debugOverdraw;
@@ -140,6 +143,11 @@ bool Properties::load() {
     if (targetCpuTimePercentage <= 0 || targetCpuTimePercentage > 100) targetCpuTimePercentage = 70;
 
     enableWebViewOverlays = base::GetBoolProperty(PROPERTY_WEBVIEW_OVERLAYS_ENABLED, false);
+
+    drawingEnabled = base::GetBoolProperty(PROPERTY_DRAWING_ENABLED, true);
+    if (!drawingEnabled) {
+        enableRTAnimations = false;
+    }
 
     return (prevDebugLayersUpdates != debugLayersUpdates) || (prevDebugOverdraw != debugOverdraw);
 }
@@ -208,6 +216,19 @@ void Properties::overrideRenderPipelineType(RenderPipelineType type, bool inUnit
                                 sRenderPipelineType != type && !inUnitTest,
                         "Trying to change pipeline but it's already set.");
     sRenderPipelineType = type;
+}
+
+void Properties::setDrawingEnabled(bool newDrawingEnabled) {
+    overrideDrawingEnabled =
+            newDrawingEnabled ? OverrideDrawingEnabled::On : OverrideDrawingEnabled::Off;
+    enableRTAnimations = newDrawingEnabled;
+}
+
+bool Properties::isDrawingEnabled() {
+    if (overrideDrawingEnabled == OverrideDrawingEnabled::Default) {
+        return drawingEnabled;
+    }
+    return overrideDrawingEnabled == OverrideDrawingEnabled::On;
 }
 
 }  // namespace uirenderer
--- libs/hwui/jni/android_graphics_HardwareRenderer.cpp
+++ libs/hwui/jni/android_graphics_HardwareRenderer.cpp
@@ -817,6 +817,14 @@ static void android_view_ThreadedRenderer_initDisplayInfo(JNIEnv*, jclass, jint 
     DeviceInfo::setPresentationDeadlineNanos(presentationDeadlineNanos);
 }
 
+static void android_view_ThreadedRenderer_setDrawingEnabled(JNIEnv*, jclass, jboolean enabled) {
+    Properties::setDrawingEnabled(enabled);
+}
+
+static jboolean android_view_ThreadedRenderer_isDrawingEnabled(JNIEnv*, jclass) {
+    return Properties::isDrawingEnabled();
+}
+
 // ----------------------------------------------------------------------------
 // HardwareRendererObserver
 // ----------------------------------------------------------------------------
@@ -953,6 +961,9 @@ static const JNINativeMethod gMethods[] = {
         {"preload", "()V", (void*)android_view_ThreadedRenderer_preload},
         {"isWebViewOverlaysEnabled", "()Z",
          (void*)android_view_ThreadedRenderer_isWebViewOverlaysEnabled},
+        {"nSetDrawingEnabled", "(Z)V", (void*)android_view_ThreadedRenderer_setDrawingEnabled},
+        {"nIsDrawingEnabled", "()Z", (void*)android_view_ThreadedRenderer_isDrawingEnabled},
+
 };
 
 static JavaVM* mJvm = nullptr;
