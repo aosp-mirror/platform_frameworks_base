4e6057e243530ed56358b7c0366886d4d97035cb
Felipe Leme <felipeal@google.com>
2021-05-24 18:10:29 -0700

    Further refactoring on ActionDisabledByAdminDialog classes.
    Bug: 184107103
    Bug: 188836559
    
    Test: m -j RunSettingsLibRoboTests ROBOTEST_FILTER=FinancedDeviceActionDisabledByAdminControllerTest
    Test: m -j RunSettingsLibRoboTests ROBOTEST_FILTER=ManagedDeviceActionDisabledByAdminControllerTest
    Test: m -j RunSettingsLibRoboTests ROBOTEST_FILTER=ActionDisabledLearnMoreButtonLauncherTest
    
    Change-Id: I3c4056d82834b5e2a4afbd958be7c9d0b9af543e

--- packages/SettingsLib/tests/robotests/src/com/android/settingslib/enterprise/ManagedDeviceActionDisabledByAdminControllerTest.java
+++ packages/SettingsLib/tests/robotests/src/com/android/settingslib/enterprise/ManagedDeviceActionDisabledByAdminControllerTest.java
@@ -16,8 +16,12 @@
 
 package com.android.settingslib.enterprise;
 
+import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.ENFORCED_ADMIN;
+import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.ENFORCEMENT_ADMIN_USER_ID;
 import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.LEARN_MORE_ACTION_LAUNCH_HELP_PAGE;
 import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.LEARN_MORE_ACTION_SHOW_ADMIN_POLICIES;
+import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.SUPPORT_MESSAGE;
+import static com.android.settingslib.enterprise.ActionDisabledByAdminControllerTestUtils.URL;
 import static com.android.settingslib.enterprise.FakeDeviceAdminStringProvider.DEFAULT_DISABLED_BY_POLICY_CONTENT;
 import static com.android.settingslib.enterprise.FakeDeviceAdminStringProvider.DEFAULT_DISABLED_BY_POLICY_TITLE;
 import static com.android.settingslib.enterprise.FakeDeviceAdminStringProvider.DISALLOW_ADJUST_VOLUME_TITLE;
@@ -25,15 +29,12 @@ import static com.android.settingslib.enterprise.FakeDeviceAdminStringProvider.D
 import static com.google.common.truth.Truth.assertThat;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.ComponentName;
-import android.os.UserHandle;
+import android.content.Context;
 import android.os.UserManager;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.test.core.app.ApplicationProvider;
 
 import com.android.settingslib.R;
-import com.android.settingslib.RestrictedLockUtils;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -43,23 +44,15 @@ import org.robolectric.android.controller.ActivityController;
 
 @RunWith(RobolectricTestRunner.class)
 public class ManagedDeviceActionDisabledByAdminControllerTest {
-    private static final int ENFORCEMENT_ADMIN_USER_ID = 123;
-    private static final ComponentName ADMIN_COMPONENT =
-            new ComponentName("some.package.name", "some.package.name.SomeClass");
-    private static final String SUPPORT_MESSAGE = "support message";
+
     private static final String RESTRICTION = UserManager.DISALLOW_ADJUST_VOLUME;
-    private static final String URL = "https://testexample.com";
     private static final String EMPTY_URL = "";
-    private static final RestrictedLockUtils.EnforcedAdmin ENFORCED_ADMIN =
-            new RestrictedLockUtils.EnforcedAdmin(
-                    ADMIN_COMPONENT, UserHandle.of(ENFORCEMENT_ADMIN_USER_ID));
     private static final String SUPPORT_TITLE_FOR_RESTRICTION = DISALLOW_ADJUST_VOLUME_TITLE;
 
+    private final Context mContext = ApplicationProvider.getApplicationContext();
     private final Activity mActivity = ActivityController.of(new Activity()).get();
     private final ActionDisabledByAdminControllerTestUtils mTestUtils =
             new ActionDisabledByAdminControllerTestUtils();
-    private final ActionDisabledLearnMoreButtonLauncher mLauncher =
-            mTestUtils.createLearnMoreButtonLauncher();
 
     @Before
     public void setUp() {
@@ -68,68 +61,63 @@ public class ManagedDeviceActionDisabledByAdminControllerTest {
 
     @Test
     public void setupLearnMoreButton_validUrl_negativeButtonSet() {
-        ManagedDeviceActionDisabledByAdminController mController =
-                createController(mLauncher, URL);
-        AlertDialog alertDialog = mTestUtils.createAlertDialog(mController, mActivity);
+        ManagedDeviceActionDisabledByAdminController controller = createController(URL);
 
-        alertDialog.getButton(Dialog.BUTTON_NEUTRAL).performClick();
+        controller.setupLearnMoreButton(mContext);
 
         mTestUtils.assertLearnMoreAction(LEARN_MORE_ACTION_LAUNCH_HELP_PAGE);
     }
 
     @Test
     public void setupLearnMoreButton_noUrl_negativeButtonSet() {
-        ManagedDeviceActionDisabledByAdminController mController =
-                createController(mLauncher, EMPTY_URL);
-        AlertDialog alertDialog = mTestUtils.createAlertDialog(mController, mActivity);
+        ManagedDeviceActionDisabledByAdminController controller = createController(EMPTY_URL);
 
-        alertDialog.getButton(Dialog.BUTTON_NEUTRAL).performClick();
+        controller.setupLearnMoreButton(mContext);
 
         mTestUtils.assertLearnMoreAction(LEARN_MORE_ACTION_SHOW_ADMIN_POLICIES);
     }
 
     @Test
     public void getAdminSupportTitleResource_noRestriction_works() {
-        ManagedDeviceActionDisabledByAdminController mController = createController();
+        ManagedDeviceActionDisabledByAdminController controller = createController();
 
-        assertThat(mController.getAdminSupportTitle(null))
+        assertThat(controller.getAdminSupportTitle(null))
                 .isEqualTo(DEFAULT_DISABLED_BY_POLICY_TITLE);
     }
 
     @Test
     public void getAdminSupportTitleResource_withRestriction_works() {
-        ManagedDeviceActionDisabledByAdminController mController = createController();
+        ManagedDeviceActionDisabledByAdminController controller = createController();
 
-        assertThat(mController.getAdminSupportTitle(RESTRICTION))
+        assertThat(controller.getAdminSupportTitle(RESTRICTION))
                 .isEqualTo(SUPPORT_TITLE_FOR_RESTRICTION);
     }
 
     @Test
     public void getAdminSupportContentString_withSupportMessage_returnsSupportMessage() {
-        ManagedDeviceActionDisabledByAdminController mController = createController();
+        ManagedDeviceActionDisabledByAdminController controller = createController();
 
-        assertThat(mController.getAdminSupportContentString(mActivity, SUPPORT_MESSAGE))
+        assertThat(controller.getAdminSupportContentString(mActivity, SUPPORT_MESSAGE))
                 .isEqualTo(SUPPORT_MESSAGE);
     }
 
     @Test
     public void getAdminSupportContentString_noSupportMessage_returnsDefault() {
-        ManagedDeviceActionDisabledByAdminController mController = createController();
+        ManagedDeviceActionDisabledByAdminController controller = createController();
 
-        assertThat(mController.getAdminSupportContentString(mActivity, /* supportMessage= */ null))
+        assertThat(controller.getAdminSupportContentString(mActivity, /* supportMessage= */ null))
                 .isEqualTo(DEFAULT_DISABLED_BY_POLICY_CONTENT);
     }
 
     private ManagedDeviceActionDisabledByAdminController createController() {
-        return createController(mLauncher, /* url= */ null);
+        return createController(/* url= */ null);
     }
 
-    private ManagedDeviceActionDisabledByAdminController createController(
-            ActionDisabledLearnMoreButtonLauncher buttonHelper, String url) {
+    private ManagedDeviceActionDisabledByAdminController createController(String url) {
         ManagedDeviceActionDisabledByAdminController controller =
                 new ManagedDeviceActionDisabledByAdminController(
-                        buttonHelper,
                         new FakeDeviceAdminStringProvider(url));
+        controller.initialize(mTestUtils.createLearnMoreButtonLauncher());
         controller.updateEnforcedAdmin(ENFORCED_ADMIN, ENFORCEMENT_ADMIN_USER_ID);
         return controller;
     }
