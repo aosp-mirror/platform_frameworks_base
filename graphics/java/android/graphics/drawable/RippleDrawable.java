2a5d63d8bd9ce0d572f944514a93e8ddc695fc64
Lucas Dupin <dupin@google.com>
2021-05-27 16:02:46 +0000

    Revert "Enable dithering on RippleDrawable"
    This reverts commit 1cbfc3b00bd058e8bddd93cd5fc9b4e0b985420e.
    
    Reason for revert: CL was no-op
    Bug: 189281134
    Change-Id: I2c70b51dd1d2f679a8310b1847675360ce415d35

--- graphics/java/android/graphics/drawable/RippleDrawable.java
+++ graphics/java/android/graphics/drawable/RippleDrawable.java
@@ -1193,9 +1193,6 @@ public class RippleDrawable extends LayerDrawable {
             mRipplePaint = new Paint();
             mRipplePaint.setAntiAlias(true);
             mRipplePaint.setStyle(Paint.Style.FILL);
-            if (mState.mRippleStyle == STYLE_PATTERNED) {
-                mRipplePaint.setDither(true);
-            }
         }
 
         final float x = mHotspotBounds.exactCenterX();
