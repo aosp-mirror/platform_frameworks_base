cc44c874cf9945d0190e4b1ceacdc77c103708cf
Patrick Baumann <patb@google.com>
2021-06-09 12:48:49 -0700

    Only update cache for new user on user create
    This change optimizes our handling of user additions to only compute the
    cache for the newly added user and its interactions with existing users.
    Prior to this change, we'd recompute the entire cache on user add.
    
    Test: atest AppsFilterTest AppEnumerationTests
    Fixes: 187853334
    Change-Id: I09cb9f98b34d86ae678373d3f61dd5fdd334cfb7

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -26300,7 +26300,7 @@ public class PackageManagerService extends IPackageManager.Stub
         synchronized (mLock) {
             scheduleWritePackageRestrictionsLocked(userId);
             scheduleWritePackageListLocked(userId);
-            mAppsFilter.onUsersChanged();
+            mAppsFilter.onUserCreated(userId);
         }
     }
 
