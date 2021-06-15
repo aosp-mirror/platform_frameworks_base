a250cbc60fb2e133b62db2a3f01484939bd9f7f2
Emilian Peev <epeev@google.com>
2021-06-15 16:15:54 -0700

    Camera: Switch camera extension proxy service package name
    The current service package name is somewhat generic. To avoid
    any potential name clashes, include the word 'extensions'.
    
    Bug: 191192321
    Test: Camera CTS
    Change-Id: Ib89c455bcadc2c7c888ababd077d4deebe3e8d14

--- packages/services/CameraExtensionsProxy/src/com/android/cameraextensions/CameraExtensionsProxyService.java
+++ packages/services/CameraExtensionsProxy/src/com/android/cameraextensions/CameraExtensionsProxyService.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.camera;
+package com.android.cameraextensions;
 
 import android.app.Service;
 import android.content.Context;
