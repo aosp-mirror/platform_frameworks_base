f460deca543cd3574ac3c0e2e761235a98019f30
wescande <wescande@google.com>
2021-06-18 10:51:19 +0200

    remove getSystemConfigEnabledProfilesForPackage
    Remove hidden api for mainline project.
    This commit is only for
    getSystemConfigEnabledProfilesForPackage
    because this will trigger a SEDenial
    
    Bug: 190440540
    Test: Manual
    Tag: #refactor
    Merged-In: Icb822ab703e7617d2c96275d94f682d30171998f
    Change-Id: Icb822ab703e7617d2c96275d94f682d30171998f

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -81,7 +81,6 @@ import com.android.server.pm.UserRestrictionsUtils;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Locale;
@@ -784,35 +783,6 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
     @Override
     public boolean isHearingAidProfileSupported() {
         return mIsHearingAidProfileSupported;
-    }
-
-    @Override
-    /** @hide */
-    public java.util.List<String> getSystemConfigEnabledProfilesForPackage(String packageName) {
-        if (Binder.getCallingUid() != Process.BLUETOOTH_UID) {
-            Slog.w(TAG, "getSystemConfigEnabledProfilesForPackage(): not allowed for non-bluetooth");
-            return null;
-        }
-
-        SystemConfig systemConfig = SystemConfig.getInstance();
-        if (systemConfig == null) {
-            return null;
-        }
-
-        android.util.ArrayMap<String, Boolean> componentEnabledStates =
-                systemConfig.getComponentsEnabledStates(packageName);
-        if (componentEnabledStates == null) {
-            return null;
-        }
-
-        ArrayList enabledProfiles = new ArrayList<String>();
-        for (Map.Entry<String, Boolean> entry : componentEnabledStates.entrySet()) {
-            if (entry.getValue()) {
-                enabledProfiles.add(entry.getKey());
-            }
-        }
-
-        return enabledProfiles;
     }
 
     private boolean isDeviceProvisioned() {
