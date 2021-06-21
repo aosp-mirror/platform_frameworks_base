b1def70e00e0ece77234f4a8e1b2406d24e31efa
Jeff DeCew <jeffdq@google.com>
2021-06-21 16:37:56 -0400

    Fix a security exception breaking notification inflation.
    Fixes: 188595459
    Test: manual testing that snooze continues to work under basic circumstances.
    Change-Id: Ic1e2dcf3d70d5bc0174254c2f5acba25a459d62b

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -5606,11 +5606,21 @@ public class Notification implements Parcelable
 
             final boolean snoozeEnabled = !hideSnoozeButton
                     && mContext.getContentResolver() != null
-                    && (Settings.Secure.getInt(mContext.getContentResolver(),
-                    Settings.Secure.SHOW_NOTIFICATION_SNOOZE, 0) == 1);
+                    && isSnoozeSettingEnabled();
             if (snoozeEnabled) {
                 big.setViewLayoutMarginDimen(R.id.notification_action_list_margin_target,
                         RemoteViews.MARGIN_BOTTOM, 0);
+            }
+        }
+
+        private boolean isSnoozeSettingEnabled() {
+            try {
+                return Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.SHOW_NOTIFICATION_SNOOZE, 0) == 1;
+            } catch (SecurityException ex) {
+                // Most 3p apps can't access this snooze setting, so their NotificationListeners
+                // would be unable to create notification views if we propagated this exception.
+                return false;
             }
         }
 
