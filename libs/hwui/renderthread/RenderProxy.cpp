f0b6921e894922b28143b281d006dee1285258f6
John Reck <jreck@google.com>
2021-06-25 17:33:33 +0000

    Revert "Delete RenderProxy off of the cleaner thread"
    This reverts commit da3559683482a61d93b06bed6f46349f6f01dc63.
    
    Bug: b/192050906
    
    If too many RenderProxies are destroyed too quickly, we can end up jamming up the CommonPool preventing RenderThread from making progress if it's waiting on CommonPool tasks.
    
    Change-Id: I8c324bc60891440d9b7be67961f8d5f881dc0f03

--- libs/hwui/renderthread/RenderProxy.cpp
+++ libs/hwui/renderthread/RenderProxy.cpp
@@ -26,7 +26,6 @@
 #include "renderthread/CanvasContext.h"
 #include "renderthread/RenderTask.h"
 #include "renderthread/RenderThread.h"
-#include "thread/CommonPool.h"
 #include "utils/Macros.h"
 #include "utils/TimeUtils.h"
 
@@ -41,17 +40,6 @@ RenderProxy::RenderProxy(bool translucent, RenderNode* rootRenderNode,
         return CanvasContext::create(mRenderThread, translucent, rootRenderNode, contextFactory);
     });
     mDrawFrameTask.setContext(&mRenderThread, mContext, rootRenderNode);
-}
-
-void RenderProxy::asyncDelete(RenderProxy* proxy) {
-    if (!proxy) return;
-
-    if (proxy->mContext) {
-        // Use the common pool because ~RenderProxy blocks on calling into RenderThread
-        CommonPool::post([proxy]() { delete proxy; });
-    } else {
-        delete proxy;
-    }
 }
 
 RenderProxy::~RenderProxy() {
