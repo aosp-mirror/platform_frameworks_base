1cafa44cec0d2850effd0884fb59cfb0716e5d3f
rnlee <rnlee@google.com>
2021-06-30 19:34:31 -0700

    Add SF namespace to property mapper.
    Step 3 of go/android-native-flag-api-manual.
    
    Bug: 190769260
    Test: m -j
    Change-Id: I2c1146b0cd6688e3b63556f8a7f464786acf4000

--- services/core/java/com/android/server/am/SettingsToPropertiesMapper.java
+++ services/core/java/com/android/server/am/SettingsToPropertiesMapper.java
@@ -92,6 +92,7 @@ public class SettingsToPropertiesMapper {
         DeviceConfig.NAMESPACE_STATSD_NATIVE,
         DeviceConfig.NAMESPACE_STATSD_NATIVE_BOOT,
         DeviceConfig.NAMESPACE_STORAGE_NATIVE_BOOT,
+        DeviceConfig.NAMESPACE_SURFACE_FLINGER_NATIVE_BOOT,
         DeviceConfig.NAMESPACE_SWCODEC_NATIVE,
         DeviceConfig.NAMESPACE_WINDOW_MANAGER_NATIVE_BOOT,
     };
