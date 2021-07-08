4e09b345527eed31eb53fb34879075f92b61cdc4
Lyn Han <lynhan@google.com>
2021-07-08 16:21:29 -0400

    Do not cancel quick settings expansion onQsIntercept(down)
    ACTION_DOWN won't be followed by ACTION_MOVE for qs button tap so
    - we should not assume DOWN is a qs expansion event
    - we should not call #onQsExpansionStarted on DOWN, which cancels both
    qs expansion and notifications animation downwards.
    
    Fixes: 187685856
    Test: swipe down from (top of lockscreen|open shade)
          tap qs button before qs expansion ends
          => qs expansion and notifications animation *not* frozen
             and complete running as expected
    
    Change-Id: Ib4f3607b4b8bf9abaebcdf1bdaa09e45abe51297

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -1625,7 +1625,6 @@ public class NotificationPanelViewController extends PanelViewController {
                     mView.getParent().requestDisallowInterceptTouchEvent(true);
                 }
                 if (mQsExpansionAnimator != null) {
-                    onQsExpansionStarted();
                     mInitialHeightOnTouch = mQsExpansionHeight;
                     mQsTracking = true;
                     traceQsJank(true /* startTracing */, false /* wasCancelled */);
