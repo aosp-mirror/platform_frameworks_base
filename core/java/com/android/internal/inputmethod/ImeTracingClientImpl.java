a975bfc447b04fe90a5ec9c3d4757810f7d07af1
Yohei Yukawa <yukawa@google.com>
2021-07-09 09:46:58 -0700

    Introduce InputMethodServiceInternal for better abstraction
    This is a mechanical refactoring CL that has no behavior change.
    
    This CL removes the direct dependency on AbstractInputMethodService
    from whenever possible.  As a result, the following classes no longer
    directly depend on AbstractInputMethodService.
    
     * android.inputmethodservice.IInputMethodWrapper
     * android.inputmethodservice.RemoteInputConnection
     * com.android.internal.inputmethod.ImeTracing
     * com.android.internal.inputmethod.ImeTracingClientImpl
     * com.android.internal.inputmethod.ImeTracingServerImpl
    
    This is still a purely mechanical refactoring.  There should be no
    observable behavior change.
    
    Bug: 192412909
    Test: atest CtsInputMethodTestCases
    Test: Manually verified that IME tracing still works
    Change-Id: I2aeeeacd27195ce10059d6590e098a4a969e774d

--- core/java/com/android/internal/inputmethod/ImeTracingClientImpl.java
+++ core/java/com/android/internal/inputmethod/ImeTracingClientImpl.java
@@ -18,7 +18,6 @@ package com.android.internal.inputmethod;
 
 import android.annotation.NonNull;
 import android.annotation.Nullable;
-import android.inputmethodservice.AbstractInputMethodService;
 import android.os.RemoteException;
 import android.os.ServiceManager.ServiceNotFoundException;
 import android.util.Log;
@@ -65,7 +64,7 @@ class ImeTracingClientImpl extends ImeTracing {
     }
 
     @Override
-    public void triggerServiceDump(String where, @NonNull AbstractInputMethodService service,
+    public void triggerServiceDump(String where, @NonNull ServiceDumper dumper,
             @Nullable byte[] icProto) {
         if (!isEnabled() || !isAvailable()) {
             return;
@@ -80,7 +79,7 @@ class ImeTracingClientImpl extends ImeTracing {
 
         try {
             ProtoOutputStream proto = new ProtoOutputStream();
-            service.dumpProtoInternal(proto, icProto);
+            dumper.dumpToProto(proto, icProto);
             sendToService(proto.getBytes(), IME_TRACING_FROM_IMS, where);
         } catch (RemoteException e) {
             Log.e(TAG, "Exception while sending ime-related service dump to server", e);
