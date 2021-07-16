592bacb778489d075341c43ed20029562e6d75fa
Curtis Belmonte <curtislb@google.com>
2021-07-15 17:02:57 -0700

    Require non-null HAT for fingerprint enrollment
    Fingerprint service providers expect to be given a non-null HAT and will
    crash otherwise. Documents that any HAT passed to these providers must be
    non-null to avoid crashing system server. Also adds logging to catch a
    potential source of a null HAT.
    
    Test: Manual
    
    Bug: 193811614
    Change-Id: Ib05541653eab04390e9928ada37bde8d088cbc84

--- services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
+++ services/core/java/com/android/server/biometrics/sensors/fingerprint/aidl/FingerprintProvider.java
@@ -329,9 +329,10 @@ public class FingerprintProvider implements IBinder.DeathRecipient, ServiceProvi
     }
 
     @Override
-    public void scheduleEnroll(int sensorId, @NonNull IBinder token, byte[] hardwareAuthToken,
-            int userId, @NonNull IFingerprintServiceReceiver receiver,
-            @NonNull String opPackageName, @FingerprintManager.EnrollReason int enrollReason,
+    public void scheduleEnroll(int sensorId, @NonNull IBinder token,
+            @NonNull byte[] hardwareAuthToken, int userId,
+            @NonNull IFingerprintServiceReceiver receiver, @NonNull String opPackageName,
+            @FingerprintManager.EnrollReason int enrollReason,
             @NonNull FingerprintStateCallback fingerprintStateCallback) {
         mHandler.post(() -> {
             final int maxTemplatesPerUser = mSensors.get(sensorId).getSensorProperties()
