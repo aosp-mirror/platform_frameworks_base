7cbbc225b1bb37b698e607127dbb832de1e83d4a
Winson Chung <winsonc@google.com>
2021-07-16 15:12:52 -0700

    Workaround for back arrow being stuck on screen
    - Whenever we defer the cleanup of the back gesture, schedule a failsafe
      to clean up the window if it's not handled properly.  We'll enable
      this failsafe for sc-dev and disable it for further investigations
      post sc-dev
    - Dump additional info for the nav bar window so we can narrow down
      the issue post sc-dev
    
    Bug: 190778197
    Test: Can't repro the initial issue, so artificially trigger it to verify failsafe
    Test: adb shell dumpsys activity service SystemUIService
    Change-Id: I0a8f25babe750e6e5ba4c933ab5271a82ef63646

--- packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/gestural/EdgeBackGestureHandler.java
@@ -919,6 +919,9 @@ public class EdgeBackGestureHandler extends CurrentUserTracker
         pw.println("  mGestureLogInsideInsets=" + String.join("\n", mGestureLogInsideInsets));
         pw.println("  mGestureLogOutsideInsets=" + String.join("\n", mGestureLogOutsideInsets));
         pw.println("  mEdgeBackPlugin=" + mEdgeBackPlugin);
+        if (mEdgeBackPlugin != null) {
+            mEdgeBackPlugin.dump(pw);
+        }
     }
 
     private boolean isGestureBlockingActivityRunning() {
