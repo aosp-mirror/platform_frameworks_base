3bbf0ab17e57b48b8e08773225bbb6770b20c599
Hongwei Wang <hwwang@google.com>
2021-07-20 15:11:42 -0700

    Reset leash and content overlay if auto-enter-pip is aborted
    This is to fix the edge case that Launcher has finished the
    auto-enter-pip transition but app for some reason requests
    setAutoEnterEnabled(false).
    
    Video: http://recall/-/aaaaaabFQoRHlzixHdtY/dpmhNEgztWewX4kWNsk3n0
    Bug: 191930787
    Test: swipe up at the end of YT playback, see video
    Change-Id: Ib95a1c046e0243f705b901c9bfabcf96e37cb0e6

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -2151,7 +2151,7 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
                     rootTask.setLastRecentsAnimationTransaction(
                             task.mLastRecentsAnimationTransaction,
                             task.mLastRecentsAnimationOverlay);
-                    task.clearLastRecentsAnimationTransaction();
+                    task.clearLastRecentsAnimationTransaction(false /* forceRemoveOverlay */);
                 }
 
                 // There are multiple activities in the task and moving the top activity should
