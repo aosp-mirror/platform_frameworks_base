018c97979c55fe355cfe6148712f7d53e744114b
Weng Su <wengsu@google.com>
2021-08-10 23:27:11 +0800

    [Provider Model] Enforce DISALLOW_CONFIG_WIFI in Internet Panel
    - Avoid the appearance of WiFi-related strings in sub-title
    
    - Hide all of Wi-Fi related views
    
    - Do not scan Wi-Fi result
    
    Bug: 195669955
    Test: manual test
    atest -c InternetDialogControllerTest \
             InternetDialogTest
    
    Change-Id: I5c19e6a8e52d2c9207053ffb7539d931d1f04799

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
@@ -51,7 +51,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.Space;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -93,6 +92,8 @@ public class InternetDialog extends SystemUIDialog implements
     protected View mDialogView;
     @VisibleForTesting
     protected WifiEntry mConnectedWifiEntry;
+    @VisibleForTesting
+    protected boolean mCanConfigWifi;
 
     private InternetDialogFactory mInternetDialogFactory;
     private SubscriptionManager mSubscriptionManager;
@@ -151,7 +152,8 @@ public class InternetDialog extends SystemUIDialog implements
 
     public InternetDialog(Context context, InternetDialogFactory internetDialogFactory,
             InternetDialogController internetDialogController, boolean canConfigMobileData,
-            boolean aboveStatusBar, UiEventLogger uiEventLogger, @Main Handler handler) {
+            boolean canConfigWifi, boolean aboveStatusBar, UiEventLogger uiEventLogger,
+            @Main Handler handler) {
         super(context, R.style.Theme_SystemUI_Dialog_Internet);
         if (DEBUG) {
             Log.d(TAG, "Init InternetDialog");
@@ -165,6 +167,7 @@ public class InternetDialog extends SystemUIDialog implements
         mTelephonyManager = mInternetDialogController.getTelephonyManager();
         mWifiManager = mInternetDialogController.getWifiManager();
         mCanConfigMobileData = canConfigMobileData;
+        mCanConfigWifi = canConfigWifi;
 
         mLayoutManager = new LinearLayoutManager(mContext) {
             @Override
@@ -247,7 +250,19 @@ public class InternetDialog extends SystemUIDialog implements
         if (DEBUG) {
             Log.d(TAG, "onStart");
         }
-        mInternetDialogController.onStart(this);
+        mInternetDialogController.onStart(this, mCanConfigWifi);
+        if (!mCanConfigWifi) {
+            hideWifiViews();
+        }
+    }
+
+    @VisibleForTesting
+    void hideWifiViews() {
+        setProgressBarVisible(false);
+        mTurnWifiOnLayout.setVisibility(View.GONE);
+        mConnectedWifListLayout.setVisibility(View.GONE);
+        mWifiRecyclerView.setVisibility(View.GONE);
+        mSeeAllLayout.setVisibility(View.GONE);
     }
 
     @Override
@@ -286,9 +301,13 @@ public class InternetDialog extends SystemUIDialog implements
         } else {
             mInternetDialogSubTitle.setText(getSubtitleText());
         }
-        showProgressBar();
         setMobileDataLayout(mInternetDialogController.activeNetworkIsCellular());
 
+        if (!mCanConfigWifi) {
+            return;
+        }
+
+        showProgressBar();
         final boolean isDeviceLocked = mInternetDialogController.isDeviceLocked();
         final boolean isWifiEnabled = mWifiManager.isWifiEnabled();
         updateWifiToggle(isWifiEnabled, isDeviceLocked);
@@ -379,8 +398,8 @@ public class InternetDialog extends SystemUIDialog implements
             return;
         }
         mConnectedWifListLayout.setVisibility(View.VISIBLE);
-        mConnectedWifiTitleText.setText(mInternetDialogController.getInternetWifiTitle());
-        mConnectedWifiSummaryText.setText(mInternetDialogController.getInternetWifiSummary());
+        mConnectedWifiTitleText.setText(mConnectedWifiEntry.getTitle());
+        mConnectedWifiSummaryText.setText(mConnectedWifiEntry.getSummary(false));
         mConnectedWifiIcon.setImageDrawable(
                 mInternetDialogController.getInternetWifiDrawable(mConnectedWifiEntry));
         if (mInternetDialogController.isNightMode()) {
