aca03a172ccbdd7f3f0f2a9a036d4fb0238b5503
Naomi Musgrave <nmusgrave@google.com>
2021-08-13 16:45:31 +0100

    [2/n] Display stack provides DisplayInfos
    For all possible states a device can assume, the DisplayManagerService
    provides the DisplayInfo associated with that layout of the logical
    display. WindowManager applies each possible rotation to these
    DisplayInfo. This, in turn, is used to calculate the possible max
    WindowMetrics on the device.
    
    Done:
    * Display stack builds collection of DisplayInfo, for all
     possible display states (folded, unfolded on jumbo)
    * Display stack pushing set of DisplayInfos to WindowManager
    * WindowManager calculates max bounds for all possible
      (display layouts x rotations)
    
    Not started:
    * WindowManager calculates insets for each rotation
    
    Bug: 181127261
    Test: atest DeviceStateManagerGlobalTest
    Test: atest DeviceStateManagerServiceTest
    Test: atest LogicalDisplayMapperTest
    Change-Id: I3a407262e755cb57c506b7255eb5c067523381d3

--- core/java/android/hardware/display/DisplayManagerInternal.java
+++ core/java/android/hardware/display/DisplayManagerInternal.java
@@ -35,6 +35,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * Display manager local system service interface.
@@ -127,6 +128,14 @@ public abstract class DisplayManagerInternal {
      * returned object must be treated as immutable.
      */
     public abstract DisplayInfo getDisplayInfo(int displayId);
+
+    /**
+     * Returns a set of DisplayInfo, for the states that may be assumed by either the given display,
+     * or any other display within that display's group.
+     *
+     * @param displayId The logical display id to fetch DisplayInfo for.
+     */
+    public abstract Set<DisplayInfo> getPossibleDisplayInfo(int displayId);
 
     /**
      * Returns the position of the display's projection.
