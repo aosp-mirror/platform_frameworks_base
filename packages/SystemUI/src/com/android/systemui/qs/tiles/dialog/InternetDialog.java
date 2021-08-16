903f54b1074d731520452717863822da3d970947
Zoey Chen <zoeychen@google.com>
2021-08-16 12:15:06 +0800

    [Provider Model] Cherry-pick from ag/15243691, ag/15287619, ag/15287620 and ag/15270751
    Move Wi-Fi progress bar position
    
    Add function to launch the Network details settings
    
    - Define the ACTION_WIFI_DETAILS_SETTINGS for launching the Wi-Fi
    network details settings
    
    Launch Wi-Fi detailed when tap the connected Wi-Fi
    
    - Uses the WifiUtils.getWifiDetailsSettingsIntent() to launch Network details settings
    
    
    Sync solution from sc
    
        - ag/15222851: Fix internet panel doesn't show data rat after dds change,
            the internet panel does not listen dds changed.
        - ag/15270340: The "no connection" should be shown in no data cell connection.
        - Add DISALLOW_CONFIG_MOBILE_NETWORKS condition.
    
    Bug: 193029917
    Test: atest InternetDialogControllerTest
    Change-Id: I4ea7aa443659cef091f525e0064a7db4c95522e2
    Merged-In: I4ea7aa443659cef091f525e0064a7db4c95522e2

--- packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/dialog/InternetDialog.java
@@ -55,6 +55,10 @@ import android.widget.Space;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import androidx.annotation.VisibleForTesting;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.android.internal.logging.UiEvent;
 import com.android.internal.logging.UiEventLogger;
 import com.android.settingslib.Utils;
@@ -66,10 +70,6 @@ import com.android.systemui.statusbar.phone.SystemUIDialog;
 import com.android.wifitrackerlib.WifiEntry;
 
 import java.util.List;
-
-import androidx.annotation.VisibleForTesting;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * Dialog for showing mobile network, connected Wi-Fi network and Wi-Fi networks.
@@ -126,6 +126,7 @@ public class InternetDialog extends SystemUIDialog implements
     private int mListMaxHeight;
     private int mDefaultDataSubId = SubscriptionManager.INVALID_SUBSCRIPTION_ID;
     private boolean mIsProgressBarVisible;
+    private boolean mCanConfigMobileData;
 
     private final ViewTreeObserver.OnGlobalLayoutListener mInternetListLayoutListener = () -> {
         // Set max height for list
@@ -137,7 +138,7 @@ public class InternetDialog extends SystemUIDialog implements
     };
 
     public InternetDialog(Context context, InternetDialogFactory internetDialogFactory,
-            InternetDialogController internetDialogController,
+            InternetDialogController internetDialogController, boolean canConfigMobileData,
             boolean aboveStatusBar, UiEventLogger uiEventLogger, @Main Handler handler) {
         super(context, R.style.Theme_SystemUI_Dialog_Internet);
         if (DEBUG) {
@@ -151,6 +152,7 @@ public class InternetDialog extends SystemUIDialog implements
         mDefaultDataSubId = mInternetDialogController.getDefaultDataSubscriptionId();
         mTelephonyManager = mInternetDialogController.getTelephonyManager();
         mWifiManager = mInternetDialogController.getWifiManager();
+        mCanConfigMobileData = canConfigMobileData;
 
         mLayoutManager = new LinearLayoutManager(mContext) {
             @Override
@@ -298,9 +300,7 @@ public class InternetDialog extends SystemUIDialog implements
                                 isChecked, false);
                     }
                 });
-        mConnectedWifListLayout.setOnClickListener(v -> {
-            // TODO(b/191475923): Need to launch the detailed page of Wi-Fi entry.
-        });
+        mConnectedWifListLayout.setOnClickListener(v -> onClickConnectedWifi());
         mSeeAllLayout.setOnClickListener(v -> onClickSeeMoreButton());
         mWiFiToggle.setOnCheckedChangeListener(
                 (buttonView, isChecked) -> {
@@ -319,8 +319,13 @@ public class InternetDialog extends SystemUIDialog implements
             mMobileDataToggle.setChecked(mInternetDialogController.isMobileDataEnabled());
             mMobileNetworkLayout.setVisibility(View.VISIBLE);
             mMobileTitleText.setText(getMobileNetworkTitle());
-            mMobileSummaryText.setText(
-                    Html.fromHtml(getMobileNetworkSummary(), Html.FROM_HTML_MODE_LEGACY));
+            if (!TextUtils.isEmpty(getMobileNetworkSummary())) {
+                mMobileSummaryText.setText(
+                        Html.fromHtml(getMobileNetworkSummary(), Html.FROM_HTML_MODE_LEGACY));
+                mMobileSummaryText.setVisibility(View.VISIBLE);
+            } else {
+                mMobileSummaryText.setVisibility(View.GONE);
+            }
             mSignalIcon.setImageDrawable(getSignalStrengthDrawable());
             int titleColor = isCellularNetwork ? mContext.getColor(
                     R.color.connected_network_primary_color) : Utils.getColorAttrDefaultColor(
@@ -331,6 +336,8 @@ public class InternetDialog extends SystemUIDialog implements
             mMobileTitleText.setTextColor(titleColor);
             mMobileSummaryText.setTextColor(summaryColor);
             mMobileNetworkLayout.setBackground(isCellularNetwork ? mBackgroundOn : null);
+
+            mMobileDataToggle.setVisibility(mCanConfigMobileData ? View.VISIBLE : View.INVISIBLE);
         }
     }
 
@@ -352,6 +359,10 @@ public class InternetDialog extends SystemUIDialog implements
         mWifiSettingsIcon.setColorFilter(
                 mContext.getColor(R.color.connected_network_primary_color));
         mConnectedWifListLayout.setBackground(mBackgroundOn);
+    }
+
+    void onClickConnectedWifi() {
+        mInternetDialogController.launchWifiNetworkDetailsSetting();
     }
 
     void onClickSeeMoreButton() {
