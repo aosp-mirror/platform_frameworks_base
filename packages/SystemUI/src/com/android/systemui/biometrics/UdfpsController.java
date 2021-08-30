2b229d14711971ce6d97e796b66c834ecd427146
Joshua Mccloskey <joshmccloskey@google.com>
2021-08-30 12:07:46 -0700

    Udfps enrollment now focuses on fingerprint region
    Test: Verified manually that the region that accesibility focuses on is
    around the fingerprint sensor.
    Test: Verified in landscape/reverse landscape/portrait & with/without accessibility
    enabled.
    Bug: 195614793
    
    Change-Id: I2be5e5bf657254e843dc863afa56318708ec3411

--- packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/UdfpsController.java
@@ -759,6 +759,7 @@ public class UdfpsController implements DozeReceiver {
                 UdfpsEnrollView enrollView = (UdfpsEnrollView) mInflater.inflate(
                         R.layout.udfps_enroll_view, null);
                 mView.addView(enrollView);
+                enrollView.updateSensorLocation(mSensorProps);
                 return new UdfpsEnrollViewController(
                         enrollView,
                         mServerRequest.mEnrollHelper,
