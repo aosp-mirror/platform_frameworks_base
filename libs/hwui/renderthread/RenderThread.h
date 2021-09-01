432b691184f24e938b50a1c0c9c51815224f2010
Rachel Lee <rnlee@google.com>
2021-09-01 13:21:44 -0700

    HWUI: use public choreographer api
    Bug: 198192636
    Test: atest hwui_unit_tests
    Change-Id: Ic8b833d87d2ba381496cfcf7e21bb9557e9ba85f

--- libs/hwui/renderthread/RenderThread.h
+++ libs/hwui/renderthread/RenderThread.h
@@ -211,7 +211,9 @@ private:
     // corresponding callbacks for each display event type
     static int choreographerCallback(int fd, int events, void* data);
     // Callback that will be run on vsync ticks.
-    static void frameCallback(int64_t frameTimeNanos, void* data);
+    static void extendedFrameCallback(const AChoreographerFrameCallbackData* cbData, void* data);
+    void frameCallback(int64_t vsyncId, int64_t frameDeadline, int64_t frameTimeNanos,
+                       int64_t frameInterval);
     // Callback that will be run whenver there is a refresh rate change.
     static void refreshRateCallback(int64_t vsyncPeriod, void* data);
     void drainDisplayEventQueue();
