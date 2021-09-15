1ecc039415de798b8f80759b57395420e1a65088
Philip Junker <philipjunker@google.com>
2021-09-15 17:21:22 +0200

    Refactor USB permission and confirm dialogs.
    Avoid null description in dialog by falling back to
    UsbDevice#getDeviceName() and UsbAccessory#getManufacturer()
    and UsbAccessory#getModel(). Use config_usbConfirmActivity
    value instead of UsbConfirmActivity.class in UsbResolverActivity.
    
    Bug: 196780010
    Test: Flashed pixel device with custom build containing the change
    Test: Open camera app that requests access to USB camera. Verify that TvUsbPermissionActivity is shown and verify functionality.
    Test: Have an app installed which registers for USB_DEVICE_ATTACHED broadcast in its AndroidManifest. Attach USB camera device and verify that TvUsbConfirmActivity is shown and verify functionality.
    Test: atest UsbPermissionActivityTest
    Change-Id: I89c4176ccd5852f5b11955d65cbf05caf5260946

--- packages/SystemUI/src/com/android/systemui/usb/UsbPermissionActivity.java
+++ packages/SystemUI/src/com/android/systemui/usb/UsbPermissionActivity.java
@@ -16,186 +16,53 @@
 
 package com.android.systemui.usb;
 
-import android.app.AlertDialog;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.PermissionChecker;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.hardware.usb.IUsbManager;
-import android.hardware.usb.UsbAccessory;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbManager;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
-import android.os.ServiceManager;
-import android.os.UserHandle;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.TextView;
-
-import com.android.internal.app.AlertActivity;
-import com.android.internal.app.AlertController;
 import com.android.systemui.R;
 
-public class UsbPermissionActivity extends AlertActivity
-        implements DialogInterface.OnClickListener, CheckBox.OnCheckedChangeListener {
+/**
+ * Dialog shown when a package requests access to a USB device or accessory.
+ */
+public class UsbPermissionActivity extends UsbDialogActivity {
 
-    private static final String TAG = "UsbPermissionActivity";
-
-    private CheckBox mAlwaysUse;
-    private TextView mClearDefaultHint;
-    private UsbDevice mDevice;
-    private UsbAccessory mAccessory;
-    private PendingIntent mPendingIntent;
-    private String mPackageName;
-    private int mUid;
-    private boolean mPermissionGranted;
-    private UsbDisconnectedReceiver mDisconnectedReceiver;
+    private boolean mPermissionGranted = false;
 
     @Override
-    public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
-
-        getWindow().addPrivateFlags(
-                WindowManager.LayoutParams.SYSTEM_FLAG_HIDE_NON_SYSTEM_OVERLAY_WINDOWS);
-        Intent intent = getIntent();
-        mDevice = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-        mAccessory = (UsbAccessory)intent.getParcelableExtra(UsbManager.EXTRA_ACCESSORY);
-        mPendingIntent = (PendingIntent)intent.getParcelableExtra(Intent.EXTRA_INTENT);
-        mUid = intent.getIntExtra(Intent.EXTRA_UID, -1);
-        mPackageName = intent.getStringExtra(UsbManager.EXTRA_PACKAGE);
-        boolean canBeDefault = intent.getBooleanExtra(UsbManager.EXTRA_CAN_BE_DEFAULT, false);
-
-        PackageManager packageManager = getPackageManager();
-        ApplicationInfo aInfo;
-        try {
-            aInfo = packageManager.getApplicationInfo(mPackageName, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            Log.e(TAG, "unable to look up package name", e);
-            finish();
-            return;
-        }
-        String appName = aInfo.loadLabel(packageManager).toString();
-
-        final AlertController.AlertParams ap = mAlertParams;
-        ap.mTitle = appName;
+    protected void onResume() {
+        super.onResume();
+        final int strId;
         boolean useRecordWarning = false;
-        if (mDevice == null) {
-            // Accessory Case
-
-            ap.mMessage = getString(R.string.usb_accessory_permission_prompt, appName,
-                    mAccessory.getDescription());
-            mDisconnectedReceiver = new UsbDisconnectedReceiver(this, mAccessory);
-        } else {
-            boolean hasRecordPermission =
-                    PermissionChecker.checkPermissionForPreflight(
-                            this, android.Manifest.permission.RECORD_AUDIO, -1, aInfo.uid,
-                            mPackageName)
-                            == android.content.pm.PackageManager.PERMISSION_GRANTED;
-            boolean isAudioCaptureDevice = mDevice.getHasAudioCapture();
-            useRecordWarning = isAudioCaptureDevice && !hasRecordPermission;
-
-            int strID = useRecordWarning
+        if (mDialogHelper.isUsbDevice()) {
+            useRecordWarning = mDialogHelper.deviceHasAudioCapture()
+                    && !mDialogHelper.packageHasAudioRecordingPermission();
+            strId = useRecordWarning
                     ? R.string.usb_device_permission_prompt_warn
                     : R.string.usb_device_permission_prompt;
-            ap.mMessage = getString(strID, appName, mDevice.getProductName());
-            mDisconnectedReceiver = new UsbDisconnectedReceiver(this, mDevice);
-
+        } else {
+            // UsbAccessory case
+            strId = R.string.usb_accessory_permission_prompt;
         }
-
-        ap.mPositiveButtonText = getString(android.R.string.ok);
-        ap.mNegativeButtonText = getString(android.R.string.cancel);
-        ap.mPositiveButtonListener = this;
-        ap.mNegativeButtonListener = this;
-
-        // Don't show the "always use" checkbox if the USB/Record warning is in effect
-        if (!useRecordWarning && canBeDefault && (mDevice != null || mAccessory != null)) {
-            // add "open when" checkbox
-            LayoutInflater inflater = (LayoutInflater) getSystemService(
-                    Context.LAYOUT_INFLATER_SERVICE);
-            ap.mView = inflater.inflate(com.android.internal.R.layout.always_use_checkbox, null);
-            mAlwaysUse = (CheckBox) ap.mView.findViewById(com.android.internal.R.id.alwaysUse);
-            if (mDevice == null) {
-                mAlwaysUse.setText(getString(R.string.always_use_accessory, appName,
-                        mAccessory.getDescription()));
-            } else {
-                mAlwaysUse.setText(getString(R.string.always_use_device, appName,
-                        mDevice.getProductName()));
-            }
-            mAlwaysUse.setOnCheckedChangeListener(this);
-
-            mClearDefaultHint = (TextView)ap.mView.findViewById(
-                    com.android.internal.R.id.clearDefaultHint);
-            mClearDefaultHint.setVisibility(View.GONE);
+        setAlertParams(strId);
+        // Only show the "always use" checkbox if there is no USB/Record warning
+        if (!useRecordWarning && mDialogHelper.canBeDefault()) {
+            addAlwaysUseCheckbox();
         }
-
         setupAlert();
     }
 
     @Override
-    public void onDestroy() {
-        IBinder b = ServiceManager.getService(USB_SERVICE);
-        IUsbManager service = IUsbManager.Stub.asInterface(b);
-
-        // send response via pending intent
-        Intent intent = new Intent();
-        try {
-            if (mDevice != null) {
-                intent.putExtra(UsbManager.EXTRA_DEVICE, mDevice);
-                if (mPermissionGranted) {
-                    service.grantDevicePermission(mDevice, mUid);
-                    if (mAlwaysUse != null && mAlwaysUse.isChecked()) {
-                        final int userId = UserHandle.getUserId(mUid);
-                        service.setDevicePackage(mDevice, mPackageName, userId);
-                    }
-                }
-            }
-            if (mAccessory != null) {
-                intent.putExtra(UsbManager.EXTRA_ACCESSORY, mAccessory);
-                if (mPermissionGranted) {
-                    service.grantAccessoryPermission(mAccessory, mUid);
-                    if (mAlwaysUse != null && mAlwaysUse.isChecked()) {
-                        final int userId = UserHandle.getUserId(mUid);
-                        service.setAccessoryPackage(mAccessory, mPackageName, userId);
-                    }
-                }
-            }
-            intent.putExtra(UsbManager.EXTRA_PERMISSION_GRANTED, mPermissionGranted);
-            mPendingIntent.send(this, 0, intent);
-        } catch (PendingIntent.CanceledException e) {
-            Log.w(TAG, "PendingIntent was cancelled");
-        } catch (RemoteException e) {
-            Log.e(TAG, "IUsbService connection failed", e);
+    protected void onPause() {
+        if (isFinishing()) {
+            mDialogHelper.sendPermissionDialogResponse(mPermissionGranted);
         }
-
-        if (mDisconnectedReceiver != null) {
-            unregisterReceiver(mDisconnectedReceiver);
-        }
-        super.onDestroy();
+        super.onPause();
     }
 
-    public void onClick(DialogInterface dialog, int which) {
-        if (which == AlertDialog.BUTTON_POSITIVE) {
-            mPermissionGranted = true;
+    @Override
+    void onConfirm() {
+        mDialogHelper.grantUidAccessPermission();
+        if (isAlwaysUseChecked()) {
+            mDialogHelper.setDefaultPackage();
         }
+        mPermissionGranted = true;
         finish();
-    }
-
-    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-        if (mClearDefaultHint == null) return;
-
-        if(isChecked) {
-            mClearDefaultHint.setVisibility(View.VISIBLE);
-        } else {
-            mClearDefaultHint.setVisibility(View.GONE);
-        }
     }
 }
