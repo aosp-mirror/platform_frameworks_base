324ba75ab6fca2650df8ac3afeae5fa95e8a1ad4
JW Wang <wangchun@google.com>
2021-09-23 15:53:30 +0800

    Migrate to SdkExtensions.getAllExtensionVersion()
    Bug: 200784637
    Test: atest RollbackUnitTest RollbackStoreTest
    Change-Id: If5ff547a397d083c21b1068a1b74e48d147f0a57

--- services/core/java/com/android/server/rollback/RollbackManagerServiceImpl.java
+++ services/core/java/com/android/server/rollback/RollbackManagerServiceImpl.java
@@ -43,7 +43,6 @@ import android.content.rollback.IRollbackManager;
 import android.content.rollback.RollbackInfo;
 import android.content.rollback.RollbackManager;
 import android.os.Binder;
-import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.HandlerExecutor;
@@ -86,6 +85,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -1284,14 +1284,10 @@ class RollbackManagerServiceImpl extends IRollbackManager.Stub implements Rollba
     }
 
     private SparseIntArray getExtensionVersions() {
-        // This list must be updated whenever the current API level is increased, or should be
-        // replaced when we have another way of determining the relevant SDK versions.
-        final int[] relevantSdkVersions = { Build.VERSION_CODES.R, Build.VERSION_CODES.S };
-
-        SparseIntArray result = new SparseIntArray(relevantSdkVersions.length);
-        for (int i = 0; i < relevantSdkVersions.length; i++) {
-            result.put(relevantSdkVersions[i],
-                    SdkExtensions.getExtensionVersion(relevantSdkVersions[i]));
+        Map<Integer, Integer> allExtensionVersions = SdkExtensions.getAllExtensionVersions();
+        SparseIntArray result = new SparseIntArray(allExtensionVersions.size());
+        for (int extension : allExtensionVersions.keySet()) {
+            result.put(extension, allExtensionVersions.get(extension));
         }
         return result;
     }
