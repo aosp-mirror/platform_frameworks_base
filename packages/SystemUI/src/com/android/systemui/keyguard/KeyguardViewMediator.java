82c01c46adb87edee6c2c351624dbd619d0a890f
Matt Pietal <mpietal@google.com>
2021-10-01 11:03:16 -0400

    [DO NOT MERGE] Keyguard - Treat messsages to lock with priority
    When switching users and attempting to lock the device, the sysui main
    thread becomes overwhelmed with events, creating a significant lag
    between the time a message is posted and processed on the main
    thread. This can be dangerous when these events are critical for
    security, such as calls coming from PhoneWindowManager#lockNow() that
    call KeyguardViewMediator#doKeyguardTimeout(). On older devices with
    slower CPUs and less memory, the delay in processing can be
    significant (15 - 30s).
    
    The result of not prioritizing these events leads to a window of time
    where a guest user can switch back to the owner, and gain access to
    the owner's homescreen without needing to unlock the device with the
    owner's credentials.
    
    As a mitigation, prioritize two events originating in two specific
    methods to make sure the device locks as soon as possible as well as
    have the system server preemptively update its local cache.
    
    Bug: 151095871
    Test: Very manual race condition - follow steps listed in bug
    Change-Id: I7585a0a5eeb308e0e32a4f77f581556d883b5cda
    Merged-In: I7585a0a5eeb308e0e32a4f77f581556d883b5cda
    (cherry picked from commit 28c53ab8bca26af58b45625c1ebba8b9051c107d)

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1233,7 +1233,9 @@ public class KeyguardViewMediator extends SystemUI {
     public void doKeyguardTimeout(Bundle options) {
         mHandler.removeMessages(KEYGUARD_TIMEOUT);
         Message msg = mHandler.obtainMessage(KEYGUARD_TIMEOUT, options);
-        mHandler.sendMessage(msg);
+        // Treat these messages with priority - A call to timeout means the device should lock
+        // as soon as possible and not wait for other messages on the thread to process first.
+        mHandler.sendMessageAtFrontOfQueue(msg);
     }
 
     /**
@@ -1420,12 +1422,15 @@ public class KeyguardViewMediator extends SystemUI {
      * @see #handleShow
      */
     private void showLocked(Bundle options) {
-        Trace.beginSection("KeyguardViewMediator#showLocked aqcuiring mShowKeyguardWakeLock");
+        Trace.beginSection("KeyguardViewMediator#showLocked acquiring mShowKeyguardWakeLock");
         if (DEBUG) Log.d(TAG, "showLocked");
         // ensure we stay awake until we are finished displaying the keyguard
         mShowKeyguardWakeLock.acquire();
         Message msg = mHandler.obtainMessage(SHOW, options);
-        mHandler.sendMessage(msg);
+        // Treat these messages with priority - This call can originate from #doKeyguardTimeout,
+        // meaning the device should lock as soon as possible and not wait for other messages on
+        // the thread to process first.
+        mHandler.sendMessageAtFrontOfQueue(msg);
         Trace.endSection();
     }
 
@@ -1584,6 +1589,7 @@ public class KeyguardViewMediator extends SystemUI {
                 case KEYGUARD_TIMEOUT:
                     synchronized (KeyguardViewMediator.this) {
                         doKeyguardLocked((Bundle) msg.obj);
+                        notifyDefaultDisplayCallbacks(mShowing);
                     }
                     break;
                 case DISMISS:
@@ -2154,7 +2160,7 @@ public class KeyguardViewMediator extends SystemUI {
         for (int i = size - 1; i >= 0; i--) {
             IKeyguardStateCallback callback = mKeyguardStateCallbacks.get(i);
             try {
-                callback.onShowingStateChanged(showing);
+                callback.onShowingStateChanged(showing, KeyguardUpdateMonitor.getCurrentUser());
             } catch (RemoteException e) {
                 Slog.w(TAG, "Failed to call onShowingStateChanged", e);
                 if (e instanceof DeadObjectException) {
@@ -2202,7 +2208,7 @@ public class KeyguardViewMediator extends SystemUI {
             mKeyguardStateCallbacks.add(callback);
             try {
                 callback.onSimSecureStateChanged(mUpdateMonitor.isSimPinSecure());
-                callback.onShowingStateChanged(mShowing);
+                callback.onShowingStateChanged(mShowing, KeyguardUpdateMonitor.getCurrentUser());
                 callback.onInputRestrictedStateChanged(mInputRestricted);
                 callback.onTrustedChanged(mUpdateMonitor.getUserHasTrust(
                         KeyguardUpdateMonitor.getCurrentUser()));
