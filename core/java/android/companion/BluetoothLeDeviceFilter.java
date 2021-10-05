9aeb44682680ccd37970ae0c5ad7d053e8cb8384
Etienne Ruffieux <eruffieux@google.com>
2021-10-05 15:07:30 +0000

    Removed Bluetooh ScanFilter hidden API usage.
    Replaced hidden API usage in Companion by public
    methods.
    
    Tag: #feature
    Bug: 200202917
    Test: atest CompanionTests
    Change-Id: Ifc7cdcbf40e23d1c8870e4dcd44772dcae55120a

--- core/java/android/companion/BluetoothLeDeviceFilter.java
+++ core/java/android/companion/BluetoothLeDeviceFilter.java
@@ -75,7 +75,7 @@ public final class BluetoothLeDeviceFilter implements DeviceFilter<ScanResult> {
             String renameSuffix, int renameBytesFrom, int renameBytesLength,
             int renameNameFrom, int renameNameLength, boolean renameBytesReverseOrder) {
         mNamePattern = namePattern;
-        mScanFilter = ObjectUtils.firstNotNull(scanFilter, ScanFilter.EMPTY);
+        mScanFilter = ObjectUtils.firstNotNull(scanFilter, new ScanFilter.Builder().build());
         mRawDataFilter = rawDataFilter;
         mRawDataFilterMask = rawDataFilterMask;
         mRenamePrefix = renamePrefix;
