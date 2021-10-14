4006041d55db50a1db640e7f85accd58d23e71b8
Songchun Fan <schfan@google.com>
2021-10-14 09:12:44 -0700

    Revert "Revert "[pm] remove old stage dirs on low storage""
    This reverts commit b45ebca772a77bf3fd269d0dcfc1a7af7c73861d.
    
    Reason for revert: adding the fix for system to abandon sessions
    
    BUG: 67862680
    Test: manual
    Change-Id: I5b762a29d12f62b516a59a85530386236259308b
    Merged-In: I91170ba399b3a596320b3bd9c8188912e5c4f1be

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -5140,6 +5140,10 @@ public class PackageManagerService extends IPackageManager.Stub
                             InstantAppRegistry.DEFAULT_UNINSTALLED_INSTANT_APP_MIN_CACHE_PERIOD))) {
                 return;
             }
+
+            // 12. Clear temp install session files
+            mInstallerService.freeStageDirs(volumeUuid);
+
         } else {
             try {
                 mInstaller.freeCache(volumeUuid, bytes, 0, 0);
