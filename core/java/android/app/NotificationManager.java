876d03fc2f564b70a4d52445454236935936065a
Daniel Chapin <chapin@google.com>
2021-10-29 20:44:51 +0000

    Revert "Enforce that callers to matchesCallFilter have either li..."
    Revert "Test permissions for matchesCallFilter in NotificationMa..."
    
    Revert submission 16077951-yl-mcfperm
    
    Reason for revert: Droidfood Blocking Bug: b/204523343
    Reverted Changes:
    I8e7c8b699:Enforce that callers to matchesCallFilter have eit...
    I8a05ffa89:Test permissions for matchesCallFilter in Notifica...
    
    Change-Id: Ib5c6814de37cae4260fb35b50a1bfc95143e1b2d

--- core/java/android/app/NotificationManager.java
+++ core/java/android/app/NotificationManager.java
@@ -2579,9 +2579,6 @@ public class NotificationManager {
      * for more information.
      * </p>
      * <p>
-     * Callers of this method must have notification listener access or permission to read contacts.
-     * </p>
-     * <p>
      * NOTE: This method calls into Contacts, which may take some time, and should not be called
      * on the main thread.
      * </p>
