8e47bf7a9fdfff7e83e2e19eaa9f36314fbc7a1b
Chris Li <lihongyu@google.com>
2021-11-03 13:00:15 +0800

    Check app targetSdkVersion when check minWidth/Height for multi window
    Bug: 197654537
    Test: manually with app target S- with large min width/height
    Change-Id: I2dd0902cb255cbe111ebab86dbf59be1a32e0cfc

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -1758,7 +1758,9 @@ class TaskFragment extends WindowContainer<WindowContainer> {
             return false;
         }
 
-        return tda.supportsActivityMinWidthHeightMultiWindow(mMinWidth, mMinHeight);
+        final ActivityRecord rootActivity = getTask().getRootActivity();
+        return tda.supportsActivityMinWidthHeightMultiWindow(mMinWidth, mMinHeight,
+                rootActivity != null ? rootActivity.info : null);
     }
 
     private int getTaskId() {
