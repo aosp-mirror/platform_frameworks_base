8e8e721f61625ae013da5fbae42f7f192083be04
Yan-De Chen <chened@google.com>
2021-11-04 06:33:22 +0000

    Revert "TouchMode (5/n) Implementing onTouchModeChanged"
    This reverts commit 487559898c52a9a3baed8dde6a2d26936bef4918.
    
    Reason for revert: Broken test(b/205063292)
    
    Change-Id: I477ab2ca44571d99f46fe53e38033a6746522c7e

--- core/java/android/view/InputEventReceiver.java
+++ core/java/android/view/InputEventReceiver.java
@@ -174,10 +174,10 @@ public abstract class InputEventReceiver {
      * Called when the display for the window associated with the input channel has entered or
      * exited touch mode.
      *
-     * @param inTouchMode {@code true} if the display showing the window associated with the
+     * @param isInTouchMode {@code true} if the display showing the window associated with the
      *                                  input channel entered touch mode.
      */
-    public void onTouchModeChanged(boolean inTouchMode) {
+    public void onTouchModeChanged(boolean isInTouchMode) {
     }
 
     /**
