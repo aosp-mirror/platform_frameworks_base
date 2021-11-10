d631564700ab8e12ec8009d01e2f9c54c27b5f1e
Matt Pietal <mpietal@google.com>
2021-11-10 08:45:26 -0500

    Keyguard Unfold
    Support subtle x-axis movements of keyguard elements as the device
    hinge is operated.
    
    Bug: 201410717
    Test: KeyguardUnfoldTransitionTest
    
    Change-Id: Ic86b8316bbd07922e3d86a5fe7ff341ff5c7040e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -905,7 +905,7 @@ public class NotificationPanelViewController extends PanelViewController {
         }
 
         mTapAgainViewController.init();
-        mKeyguardUnfoldTransition.ifPresent(u -> u.setup(mNotificationContainerParent));
+        mKeyguardUnfoldTransition.ifPresent(u -> u.setup(mView));
     }
 
     @Override
@@ -1150,7 +1150,7 @@ public class NotificationPanelViewController extends PanelViewController {
         }
         setKeyguardBottomAreaVisibility(mBarState, false);
 
-        mKeyguardUnfoldTransition.ifPresent(u -> u.setup(mNotificationContainerParent));
+        mKeyguardUnfoldTransition.ifPresent(u -> u.setup(mView));
     }
 
     private void attachSplitShadeMediaPlayerContainer(FrameLayout container) {
