efb099367349a687d36b0514744ac01bbd85006b
Hongwei Wang <hwwang@google.com>
2021-11-17 16:05:10 -0800

    Ignore ui mode change caused by Morris overlay in PiP
    When entering PiP from GMM with Morris overlay enabled, there will be a
    display change callback caused by ui mode change that tend to shift the
    PiP window. For PiP, such ui mode change from app is not quite relevant
    and we can simply ignore in PipController.
    
    Bug: 201589034
    Video: http://recall/-/aaaaaabFQoRHlzixHdtY/dmI3KCeKPAH118BRqnhgNO
    Test: enter PiP from GMM with Morris overlay, see video
    Merged-In: I2d72e166fb8de1e503415b5441c03f0685de9573
    Change-Id: I2d72e166fb8de1e503415b5441c03f0685de9573
    (cherry picked from commit 391f372726af62c6d9ba9b221508003c37e8ff8a)
    (cherry picked from commit 166f263128010f6a885efa17a5ebc3aa94d3b3d6)
    Merged-In:I2d72e166fb8de1e503415b5441c03f0685de9573

--- libs/WindowManager/Shell/src/com/android/wm/shell/common/DisplayLayout.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/common/DisplayLayout.java
@@ -83,6 +83,19 @@ public class DisplayLayout {
     private boolean mHasStatusBar = false;
     private int mNavBarFrameHeight = 0;
 
+    /**
+     * Different from {@link #equals(Object)}, this method compares the basic geometry properties
+     * of two {@link DisplayLayout} objects including width, height, rotation, density and cutout.
+     * @return {@code true} if the given {@link DisplayLayout} is identical geometry wise.
+     */
+    public boolean isSameGeometry(@NonNull DisplayLayout other) {
+        return mWidth == other.mWidth
+                && mHeight == other.mHeight
+                && mRotation == other.mRotation
+                && mDensityDpi == other.mDensityDpi
+                && Objects.equals(mCutout, other.mCutout);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
