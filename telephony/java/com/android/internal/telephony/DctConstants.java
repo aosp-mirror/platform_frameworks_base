193815a0cf30e9debca9d97584696131588d237b
Sarah Chin <sarahchin@google.com>
2021-12-09 10:32:27 -0800

    Create EVENT_TRAFFIC_DESCRIPTORS_UPDATED
    Test: atest DataConnectionTest, DcTrackerTest
    Bug: 198473491
    Change-Id: Idc6ef705090192eb7f7d145b10616fb9ac3de074

--- telephony/java/com/android/internal/telephony/DctConstants.java
+++ telephony/java/com/android/internal/telephony/DctConstants.java
@@ -114,6 +114,7 @@ public class DctConstants {
     public static final int EVENT_CARRIER_CONFIG_CHANGED = BASE + 54;
     public static final int EVENT_SIM_STATE_UPDATED = BASE + 55;
     public static final int EVENT_APN_UNTHROTTLED = BASE + 56;
+    public static final int EVENT_TRAFFIC_DESCRIPTORS_UPDATED = BASE + 57;
 
     /***** Constants *****/
 
