1b560d8747beee450dfc14f2696cac5513f97baf
Jerry Chang <chenghsiuchang@google.com>
2021-12-10 18:25:35 +0800

    Integrate launch-to-side split transition to shell transition
    Integrate launch-to-side split transition to shell transition.
    Because there is no guarantee for launching activity to split
    successfully before actually launching it, trigger enter split
    transition after observed new task launching into split.
    
    Bug: 206487881
    Test: atest SplitTransitionTests
    Test: drag to enter split with shell transition works
    Change-Id: Idda7c3b02689753cea1a1fe80264145f1a449f73

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1672,7 +1672,8 @@ class ActivityStarter {
                 }
                 if (newTransition != null) {
                     transitionController.requestStartTransition(newTransition,
-                            mTargetTask, remoteTransition, null /* displayChange */);
+                            mTargetTask == null ? r.getTask() : mTargetTask,
+                            remoteTransition, null /* displayChange */);
                 } else if (started) {
                     // Make the collecting transition wait until this request is ready.
                     transitionController.setReady(r, false);
