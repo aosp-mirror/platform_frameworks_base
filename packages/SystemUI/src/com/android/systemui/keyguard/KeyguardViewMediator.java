ada60a50003d890bd20e03c5340616bcb2ffa533
Josh Tsuji <tsuji@google.com>
2021-12-15 16:06:41 -0500

    Remove check preventing keyguardGoingAwayRunnable from being called if it's already going away.
    This check was added in ag/16441222. The root cause of ag/16441222's bug was that we were not
    handling remote animation cancellations properly, so it fixed that and also added this check to
    prevent extraneous cancellations from happening in the first place.
    
    That check was not necessary to fix the original bug (since we handle cancellations properly now,
    including extraneous ones), and appears to be causing issues with camera CTS tests and at least
    one report of a stuck lock screen. I can't reproduce any of that locally, but given that it was
    not necessary, we should remove it pending investigation.
    
    Bug: 210831136
    Bug: 210732858
    Test: atest SystemUITests
    Change-Id: I78af499016e9c46f881fb7045e9823273162ddcc
    (cherry picked from commit cfdad866881b225f4d139ad517e5e18dd761bf52)
    Merged-In:I78af499016e9c46f881fb7045e9823273162ddcc

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -2093,15 +2093,6 @@ public class KeyguardViewMediator extends SystemUI implements Dumpable,
     private final Runnable mKeyguardGoingAwayRunnable = new Runnable() {
         @Override
         public void run() {
-            // If the keyguard is already going away, or it's about to because we are going to
-            // trigger the going-away remote animation to show the surface behind, don't do it
-            // again. That will cause the current animation to be cancelled unnecessarily.
-            if (mKeyguardStateController.isKeyguardGoingAway()
-                    || mSurfaceBehindRemoteAnimationRequested
-                    || mSurfaceBehindRemoteAnimationRunning) {
-                return;
-            }
-
             Trace.beginSection("KeyguardViewMediator.mKeyGuardGoingAwayRunnable");
             if (DEBUG) Log.d(TAG, "keyguardGoingAway");
             mKeyguardViewControllerLazy.get().keyguardGoingAway();
