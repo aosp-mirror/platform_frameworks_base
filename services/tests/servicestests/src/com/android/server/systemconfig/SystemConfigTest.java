1df9a1e8f5b1c6697f249e7b4b5b5b0fa29a8233
Pedro Loureiro <pedroql@google.com>
2021-12-22 19:42:44 +0000

    Rename updatable-library to apex-library
    Mostly for consistency with the names used in related changes.
    
    Test: atest com.android.server.pm.parsing.library.ApexSharedLibraryUpdaterTest com.android.server.systemconfig.SystemConfigTest
    
    Bug: 191978330
    Change-Id: Ic4ccc0fdca100b576e28bc0918d378cabae9ce61
    Merged-In: Ic4ccc0fdca100b576e28bc0918d378cabae9ce61

--- services/tests/servicestests/src/com/android/server/systemconfig/SystemConfigTest.java
+++ services/tests/servicestests/src/com/android/server/systemconfig/SystemConfigTest.java
@@ -354,13 +354,13 @@ public class SystemConfigTest {
 
     /**
      * Tests that readPermissions works correctly for a library using the new
-     * {@code updatable-library} tag.
+     * {@code apex-library} tag.
      */
     @Test
     public void readPermissions_allowLibs_parsesUpdatableLibrary() throws IOException {
         String contents =
                 "<permissions>\n"
-                        + "    <updatable-library \n"
+                        + "    <apex-library \n"
                         + "        name=\"foo\"\n"
                         + "        file=\"foo.jar\"\n"
                         + "        on-bootclasspath-before=\"10\"\n"
