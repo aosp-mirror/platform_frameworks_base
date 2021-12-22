1a07c53231568f868f16e6f66b8c6228b9d56d94
Xinyi Zhou <zxy@google.com>
2021-12-21 16:48:49 -0800

    Expose Conext.NEARBY_SERVICE as SystemApi
    Change-Id: Ie9fb547e166e6c881dfd04383050375a11f84a9b
    Test: Built successfully
    Bug: 189954300

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -5914,6 +5914,7 @@ public abstract class Context {
      * @see android.nearby.NearbyManager
      * @hide
      */
+    @SystemApi
     public static final String NEARBY_SERVICE = "nearby";
 
     /**
