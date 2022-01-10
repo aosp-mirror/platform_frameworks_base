46753e9a33f6f6d17ca660bc57102bc2b98a813f
Junyu Lai <junyulai@google.com>
2022-01-10 10:35:59 +0000

    [MS48.5]Remove INetworkStatsSession from SummaryForAllUidLoader
    SummaryForAllUidLoader is using INetworkStatsSession hidden APIs, which
    cannot be accessed after moving into the mainline module. So, replace
    and remove those hidden API usages.
    
    Test: atest clockwork-settings-robotests
          make RunSettingsRoboTests -j40
          make RunSettingsLibRoboTests
               ROBOTEST_FILTER=DataUsageControllerTest
          make RunSettingsLibRoboTests
               ROBOTEST_FILTER=NetworkCycleChartDataLoaderTest
          make RunSettingsLibRoboTests
               ROBOTEST_FILTER=NetworkCycleDataForUidLoaderTest
          make RunSettingsLibRoboTests
               ROBOTEST_FILTER=NetworkCycleDataLoaderTest
          make RunSettingsLibRoboTests
               ROBOTEST_FILTER=DataUsageUtilsTest
    Bug: 204830222
    Ignore-AOSP-First: Related API conflict, need master first.
    
      (cherry-picked from ag/16689474)
    
    Change-Id: Iac733a5021d0d79111c0350b694e4ed0ec90e262
    Merged-In: Iac733a5021d0d79111c0350b694e4ed0ec90e262

--- packages/SettingsLib/src/com/android/settingslib/net/SummaryForAllUidLoader.java
+++ packages/SettingsLib/src/com/android/settingslib/net/SummaryForAllUidLoader.java
@@ -16,13 +16,12 @@
 
 package com.android.settingslib.net;
 
+import android.app.usage.NetworkStats;
+import android.app.usage.NetworkStatsManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
-import android.net.INetworkStatsSession;
-import android.net.NetworkStats;
 import android.net.NetworkTemplate;
 import android.os.Bundle;
-import android.os.RemoteException;
 
 /**
  * Framework loader is deprecated, use the compat version instead.
@@ -35,7 +34,7 @@ public class SummaryForAllUidLoader extends AsyncTaskLoader<NetworkStats> {
     private static final String KEY_START = "start";
     private static final String KEY_END = "end";
 
-    private final INetworkStatsSession mSession;
+    private final NetworkStatsManager mNetworkStatsManager;
     private final Bundle mArgs;
 
     public static Bundle buildArgs(NetworkTemplate template, long start, long end) {
@@ -46,9 +45,9 @@ public class SummaryForAllUidLoader extends AsyncTaskLoader<NetworkStats> {
         return args;
     }
 
-    public SummaryForAllUidLoader(Context context, INetworkStatsSession session, Bundle args) {
+    public SummaryForAllUidLoader(Context context, Bundle args) {
         super(context);
-        mSession = session;
+        mNetworkStatsManager = context.getSystemService(NetworkStatsManager.class);
         mArgs = args;
     }
 
@@ -63,12 +62,7 @@ public class SummaryForAllUidLoader extends AsyncTaskLoader<NetworkStats> {
         final NetworkTemplate template = mArgs.getParcelable(KEY_TEMPLATE);
         final long start = mArgs.getLong(KEY_START);
         final long end = mArgs.getLong(KEY_END);
-
-        try {
-            return mSession.getSummaryForAllUid(template, start, end, false);
-        } catch (RemoteException e) {
-            return null;
-        }
+        return mNetworkStatsManager.querySummary(template, start, end);
     }
 
     @Override
