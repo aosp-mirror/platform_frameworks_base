8624015554c37c48d8611be2af7141750275e335
Will <wleshner@google.com>
2022-01-12 16:54:35 -0800

    Periodically jitter dream overlay to prevent burn-in.
    Test: atest DreamOverlayContainerViewControllerTest
    Bug: 213907279
    Change-Id: Id43d8697c5ef032b07c6f5854445cb16f6c820b7

--- packages/SystemUI/src/com/android/systemui/dreams/DreamOverlayContainerViewController.java
+++ packages/SystemUI/src/com/android/systemui/dreams/DreamOverlayContainerViewController.java
@@ -16,8 +16,11 @@
 
 package com.android.systemui.dreams;
 
+import static com.android.systemui.doze.util.BurnInHelperKt.getBurnInOffset;
+
 import android.graphics.Rect;
 import android.graphics.Region;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
@@ -26,6 +29,7 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.systemui.R;
+import com.android.systemui.dagger.qualifiers.Main;
 import com.android.systemui.dreams.dagger.DreamOverlayComponent;
 import com.android.systemui.dreams.dagger.DreamOverlayModule;
 import com.android.systemui.util.ViewController;
@@ -46,6 +50,15 @@ public class DreamOverlayContainerViewController extends ViewController<DreamOve
     // The dream overlay's content view, which is located below the status bar (in z-order) and is
     // the space into which widgets are placed.
     private final ViewGroup mDreamOverlayContentView;
+
+    // The maximum translation offset to apply to the overlay container to avoid screen burn-in.
+    private final int mMaxBurnInOffset;
+
+    // The interval in milliseconds between burn-in protection updates.
+    private final long mBurnInProtectionUpdateInterval;
+
+    // Main thread handler used to schedule periodic tasks (e.g. burn-in protection updates).
+    private final Handler mHandler;
 
     // A hook into the internal inset calculation where we declare the overlays as the only
     // touchable regions.
@@ -81,13 +94,21 @@ public class DreamOverlayContainerViewController extends ViewController<DreamOve
     public DreamOverlayContainerViewController(
             DreamOverlayContainerView containerView,
             @Named(DreamOverlayModule.DREAM_OVERLAY_CONTENT_VIEW) ViewGroup contentView,
-            DreamOverlayStatusBarViewController statusBarViewController) {
+            DreamOverlayStatusBarViewController statusBarViewController,
+            @Main Handler handler,
+            @Named(DreamOverlayModule.MAX_BURN_IN_OFFSET) int maxBurnInOffset,
+            @Named(DreamOverlayModule.BURN_IN_PROTECTION_UPDATE_INTERVAL) long
+                    burnInProtectionUpdateInterval) {
         super(containerView);
         mDreamOverlayContentView = contentView;
         mStatusBarViewController = statusBarViewController;
         mDreamOverlayNotificationsDragAreaHeight =
                 mView.getResources().getDimensionPixelSize(
                         R.dimen.dream_overlay_notifications_drag_area_height);
+
+        mHandler = handler;
+        mMaxBurnInOffset = maxBurnInOffset;
+        mBurnInProtectionUpdateInterval = burnInProtectionUpdateInterval;
     }
 
     @Override
@@ -99,10 +120,12 @@ public class DreamOverlayContainerViewController extends ViewController<DreamOve
     protected void onViewAttached() {
         mView.getViewTreeObserver()
                 .addOnComputeInternalInsetsListener(mOnComputeInternalInsetsListener);
+        mHandler.postDelayed(this::updateBurnInOffsets, mBurnInProtectionUpdateInterval);
     }
 
     @Override
     protected void onViewDetached() {
+        mHandler.removeCallbacks(this::updateBurnInOffsets);
         mView.getViewTreeObserver()
                 .removeOnComputeInternalInsetsListener(mOnComputeInternalInsetsListener);
     }
@@ -122,5 +145,16 @@ public class DreamOverlayContainerViewController extends ViewController<DreamOve
     @VisibleForTesting
     int getDreamOverlayNotificationsDragAreaHeight() {
         return mDreamOverlayNotificationsDragAreaHeight;
+    }
+
+    private void updateBurnInOffsets() {
+        // These translation values change slowly, and the set translation methods are idempotent,
+        // so no translation occurs when the values don't change.
+        mView.setTranslationX(getBurnInOffset(mMaxBurnInOffset * 2, true)
+                - mMaxBurnInOffset);
+        mView.setTranslationY(getBurnInOffset(mMaxBurnInOffset * 2, false)
+                - mMaxBurnInOffset);
+
+        mHandler.postDelayed(this::updateBurnInOffsets, mBurnInProtectionUpdateInterval);
     }
 }
