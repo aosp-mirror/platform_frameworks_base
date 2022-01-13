7617679a6197f210fb0ef92f59e589e6d800df4b
Mariia Sandrikova <mariiasand@google.com>
2022-01-13 15:21:38 +0000

    DO NOT MERGE Revert "[2/n] Camera Compat UI: Add interfaces for client-server..."
    Revert submission 16468379
    
    Reason for revert: Feature development is moving to T.
    
    Reverted Changes:
    Id9b9a8930:[3/n] Camera Compat UI: Add a camera compat contro...
    Id6be4a954:Enable a camera app compat control on Large screen...
    I083aa6718:[2/n] Camera Compat UI: Add interfaces for client-...
    
    Bug: 206602997
    
    Change-Id: I9ad876043fd61f708a8f468fffd1ef371bfa0866

--- libs/WindowManager/Shell/src/com/android/wm/shell/ShellTaskOrganizer.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/ShellTaskOrganizer.java
@@ -458,7 +458,7 @@ public class ShellTaskOrganizer extends TaskOrganizer implements
                 newListener.onTaskInfoChanged(taskInfo);
             }
             notifyLocusVisibilityIfNeeded(taskInfo);
-            if (updated || !taskInfo.equalsForCompatUi(data.getTaskInfo())) {
+            if (updated || !taskInfo.equalsForSizeCompat(data.getTaskInfo())) {
                 // Notify the compat UI if the listener or task info changed.
                 notifyCompatUI(taskInfo, newListener);
             }
@@ -633,7 +633,7 @@ public class ShellTaskOrganizer extends TaskOrganizer implements
         // The task is vanished or doesn't support compat UI, notify to remove compat UI
         // on this Task if there is any.
         if (taskListener == null || !taskListener.supportCompatUI()
-                || !taskInfo.hasCompatUI() || !taskInfo.isVisible) {
+                || !taskInfo.topActivityInSizeCompat || !taskInfo.isVisible) {
             mCompatUI.onCompatInfoChanged(taskInfo.displayId, taskInfo.taskId,
                     null /* taskConfig */, null /* taskListener */);
             return;
