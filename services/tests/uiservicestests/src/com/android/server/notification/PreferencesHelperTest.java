3d04700c231e67c079d679dc508856ff6ef4dcb6
Jason Lee <jasl@google.com>
2022-01-13 06:40:19 +0000

    Revert "Prevent apps from creating blocked channel groups"
    This reverts commit 1d65ccb49a56b426894b6f74ffdef6cd7b58c599.
    
    Reason for revert: Possible culprit of b/214331751
    
    Change-Id: I315a4676a7f9107631f85a7be1cbac977bfbfb8f

--- services/tests/uiservicestests/src/com/android/server/notification/PreferencesHelperTest.java
+++ services/tests/uiservicestests/src/com/android/server/notification/PreferencesHelperTest.java
@@ -3174,19 +3174,6 @@ public class PreferencesHelperTest extends UiServiceTestCase {
     }
 
     @Test
-    public void testIsGroupBlocked_appCannotCreateAsBlocked() throws Exception {
-        NotificationChannelGroup group = new NotificationChannelGroup("id", "name");
-        group.setBlocked(true);
-        mHelper.createNotificationChannelGroup(PKG_N_MR1, UID_N_MR1, group, true);
-        assertFalse(mHelper.isGroupBlocked(PKG_N_MR1, UID_N_MR1, group.getId()));
-
-        NotificationChannelGroup group3 = group.clone();
-        group3.setBlocked(false);
-        mHelper.createNotificationChannelGroup(PKG_N_MR1, UID_N_MR1, group3, true);
-        assertFalse(mHelper.isGroupBlocked(PKG_N_MR1, UID_N_MR1, group.getId()));
-    }
-
-    @Test
     public void testIsGroup_appCannotResetBlock() throws Exception {
         NotificationChannelGroup group = new NotificationChannelGroup("id", "name");
         mHelper.createNotificationChannelGroup(PKG_N_MR1, UID_N_MR1, group, true);
@@ -4719,7 +4706,7 @@ public class PreferencesHelperTest extends UiServiceTestCase {
     public void testGetConversations_noDisabledGroups() {
         NotificationChannelGroup group = new NotificationChannelGroup("a", "a");
         group.setBlocked(true);
-        mHelper.createNotificationChannelGroup(PKG_O, UID_O, group, false);
+        mHelper.createNotificationChannelGroup(PKG_O, UID_O, group, true);
         NotificationChannel parent = new NotificationChannel("parent", "p", 1);
         mHelper.createNotificationChannel(PKG_O, UID_O, parent, true, false);
 
