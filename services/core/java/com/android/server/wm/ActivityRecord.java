103cb7d50ccb403d19c6442eca02b4157cde3040
Mariia Sandrikova <mariiasand@google.com>
2022-01-13 13:55:43 +0000

    DO NOT MERGE Revert "Log the CameraCompatControlEventReported atom."
    This reverts commit 4e5bb88605af9e0943a2c1633fd411389121dd1f.
    
    Reason for revert: Feature development is moving to T.
    
    Bug: 206602997
    Change-Id: Ia5da62807034a1657d65ac1bd15a2fff940d7160

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -1618,8 +1618,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         if (!changed) {
             return;
         }
-        mTaskSupervisor.getActivityMetricsLogger().logCameraCompatControlAppearedEventReported(
-                newCameraCompatControlState, info.applicationInfo.uid);
         if (newCameraCompatControlState == TaskInfo.CAMERA_COMPAT_CONTROL_HIDDEN) {
             mCameraCompatControlClickedByUser = false;
             mCompatCameraControlCallback = null;
@@ -1642,8 +1640,6 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         if (!changed) {
             return;
         }
-        mTaskSupervisor.getActivityMetricsLogger().logCameraCompatControlClickedEventReported(
-                state, info.applicationInfo.uid);
         if (state == TaskInfo.CAMERA_COMPAT_CONTROL_DISMISSED) {
             mCompatCameraControlCallback = null;
             return;
