6c04f9b6da88021675611ce4f504a8337749d8c3
Riddle Hsu <riddlehsu@google.com>
2022-01-18 15:40:25 +0800

    Fix leakage of DisplayContent referenced from TransitionController
    Unlike AppTransition which is associated DisplayContent, the
    TransitionController is a global instance. So it needs to unregister
    the listener when the display is removed.
    
    Bug: 214991741
    Test: Start/stop screen recording multiple times (create virtual
          display). The heap dump of system server should not contain
          many DisplayContent instances.
    (cherry picked from commit 491914c17c869a174f91d0414181d1be11e040a3)
    (cherry picked from commit c3eaf662257919343210b96f5d5aef161df0723e)
    Merged-In: I44e2a794981fc114ada0ce98cd2471adf623d856
    Change-Id: I44e2a794981fc114ada0ce98cd2471adf623d856

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -1067,8 +1067,6 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
 
         mAppTransition = new AppTransition(mWmService.mContext, mWmService, this);
         mAppTransition.registerListenerLocked(mWmService.mActivityManagerAppTransitionNotifier);
-        mTransitionController.registerLegacyListener(
-                mWmService.mActivityManagerAppTransitionNotifier);
         mAppTransition.registerListenerLocked(mFixedRotationTransitionListener);
         mAppTransitionController = new AppTransitionController(mWmService, this);
         mUnknownAppVisibilityController = new UnknownAppVisibilityController(mWmService, this);
