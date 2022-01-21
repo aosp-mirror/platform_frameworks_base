ff7b453ca8e23b4ef75ba2c3f5becaf511cb07d3
Yohei Yukawa <yukawa@google.com>
2022-01-20 21:28:34 -0800

    Minimum impl of nav buttons rendered by IME
    This is the minimum implementation of rendering the back button and
    the IME switcher button in the InputMethodService process.
    
    Note that the feature is still disabled by default behind the flag.
    Newly added UI logic should be running only when the feature flag is
    enabled.
    
     Bug 215545985: Enable canImeRenderGesturalNavButtons() by default
    
    Here is the list of known blockers before enabling the flag.
    
     * Bug 215552600: Subscribe gesture mode change.
     * Bug 215551357: Show IME swicher button only when necessary.
     * Bug 215549533: Support light navigation bar.
     * Bug 215550296: Support floating IME.
     * Bug 215554582: Set "input_method_rounded_corner_content_padding"
                      for each devices.
    
    Also, after enabling the flag by default, we start cleaning up newly
    added code under
    
      android.inputmethodservice.navigationbar.
    
    at Bug 215443343.
    
    Bug: 205803355
    Test: Manually done as follows
      1. Build aosp_coral-userdebug and flash it
      2. Switch to gestural navigation mode.
      3. adb root
      4. adb shell setprop \
           persist.sys.ime.can_render_gestural_nav_buttons true
      5. adb reboot
      6. Tap any edit field to show AOSP Keyboard.
      7. Confirm that the back button on the navbar works.
      8. Confirm that the IME switcher button on the navbar works.
    Change-Id: I3e7e1f83554444131e2765dc159617bb9e2337c7

--- core/java/android/inputmethodservice/navigationbar/ButtonInterface.java
+++ core/java/android/inputmethodservice/navigationbar/ButtonInterface.java
@@ -1,13 +1,11 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
 /*
- * Copyright 2015, The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,11 +13,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
--->
-<resources>
-    <dimen name="chooser_grid_padding">16dp</dimen>
 
-    <!-- Copied from SysUI's @dimen/navigation_key_width for the embedded nav bar in the IME -->
-    <dimen name="input_method_navigation_key_width">80dip</dimen>
+package android.inputmethodservice.navigationbar;
 
-</resources>
+import android.annotation.Nullable;
+import android.graphics.drawable.Drawable;
+
+interface ButtonInterface {
+
+    void setImageDrawable(@Nullable Drawable drawable);
+
+    void setDarkIntensity(float intensity);
+
+    void setDelayTouchFeedback(boolean shouldDelay);
+}
