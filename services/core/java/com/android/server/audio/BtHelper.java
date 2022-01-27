f185c9bcd95c666905d4fbada73ebe4d5ef32c2d
William Escande <wescande@google.com>
2022-01-27 13:33:02 +0100

    API Changes needed for Blueooth apex to build
    Bug: 216476895
    Test: Compile
    Tag: #refactor
    Merged-In: Ia7741309719e1f46c8c18c81b65c8862bca814e1
    Change-Id: Ia7741309719e1f46c8c18c81b65c8862bca814e1

--- services/core/java/com/android/server/audio/BtHelper.java
+++ services/core/java/com/android/server/audio/BtHelper.java
@@ -490,9 +490,7 @@ public class BtHelper {
             return;
         }
         final BluetoothDevice btDevice = deviceList.get(0);
-        final @BluetoothProfile.BtProfileState int state =
-                proxy.getConnectionState(btDevice);
-        if (state == BluetoothProfile.STATE_CONNECTED) {
+        if (proxy.getConnectionState(btDevice) == BluetoothProfile.STATE_CONNECTED) {
             mDeviceBroker.queueOnBluetoothActiveDeviceChanged(
                     new AudioDeviceBroker.BtDeviceChangedData(btDevice, null,
                         new BluetoothProfileConnectionInfo(profile),
