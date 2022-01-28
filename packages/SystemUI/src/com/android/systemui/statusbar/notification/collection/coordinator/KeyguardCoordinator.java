670240dc87f914e73f513e579a54f869e380171c
Beverly <beverlyt@google.com>
2022-01-28 22:06:14 +0000

    Don't show notification private view too early
    Don't show the notification private view  immediately
    after authenticating with a biometric that can bypass
    the lockscreen. We don't want to flash the private notification
    right before we hide the notification shade because it looks
    janky. Instead, wait to update it until after the keyguard
    is done going away.
    
    Test: atest SensitiveContentCoordinatorTest NotificationViewHierarchyManagerTest
    Test: manually test with and w/o the new notification pipeline enabled
      1. add FP for security
      2. Settings > Notifications > Disable "Sensitive notifications"
      3. Post a notification that would be partially hidden as a sensitive
         notification
      4. Authenticate with FP on the LS (repeat 10x) times
      Expected: notifications don't flash their private views when entering
      the device
    Bug: 212835832
    
    Change-Id: I5ce4460043250aad4a5b0b0838e6f3b5ae6feb1d

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/coordinator/KeyguardCoordinator.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/collection/coordinator/KeyguardCoordinator.java
@@ -164,6 +164,8 @@ public class KeyguardCoordinator implements Coordinator {
         }
     }
 
+    // TODO(b/206118999): merge this class with SensitiveContentCoordinator which also depends on
+    // these same updates
     private void setupInvalidateNotifListCallbacks() {
         // register onKeyguardShowing callback
         mKeyguardStateController.addCallback(mKeyguardCallback);
