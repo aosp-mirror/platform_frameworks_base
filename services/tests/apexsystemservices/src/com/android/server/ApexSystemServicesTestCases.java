5a38385882f451215e7561ecfac96cc937394096
Artur Satayev <satayev@google.com>
2022-02-01 13:46:38 +0000

    Revert "Revert "Introduce initOrder for apex-system-services.""
    This reverts commit 1352272eb620f8c52e840340a0d96f6dc019f265.
    
    Reason for revert: rollforward with a fix.
    
    Add waitForBootComplete() after installing test apex and inspecting
    logcat.
    
    Bug: 192880996
    Test: atest ApexSystemServicesTestCases PackageParserTest ApexManagerTestChange-Id: I7a238b439fc25da4ae1ab379cc66e54565dac293
    Change-Id: I0270c24c471d8e7391260b7f731b6c0fb2fa0662

--- services/tests/apexsystemservices/src/com/android/server/ApexSystemServicesTestCases.java
+++ services/tests/apexsystemservices/src/com/android/server/ApexSystemServicesTestCases.java
@@ -37,8 +37,14 @@ import org.junit.rules.RuleChain;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runner.RunWith;
 
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 @RunWith(DeviceJUnit4ClassRunner.class)
 public class ApexSystemServicesTestCases extends BaseHostJUnit4Test {
+
+    private static final int REBOOT_TIMEOUT = 1 * 60 * 1000;
 
     private final InstallUtilsHost mHostUtils = new InstallUtilsHost(this);
     private final TemporaryFolder mTemporaryFolder = new TemporaryFolder();
@@ -67,7 +73,7 @@ public class ApexSystemServicesTestCases extends BaseHostJUnit4Test {
     }
 
     @Test
-    public void noApexSystemServerStartsWithoutApex() throws Exception {
+    public void testNoApexSystemServiceStartsWithoutApex() throws Exception {
         mPreparer.reboot();
 
         assertThat(getFakeApexSystemServiceLogcat())
@@ -75,20 +81,55 @@ public class ApexSystemServicesTestCases extends BaseHostJUnit4Test {
     }
 
     @Test
-    public void apexSystemServerStarts() throws Exception {
+    public void testApexSystemServiceStarts() throws Exception {
         // Pre-install the apex
         String apex = "test_com.android.server.apex";
         mPreparer.pushResourceFile(apex, "/system/apex/" + apex);
         // Reboot activates the apex
         mPreparer.reboot();
 
+        mDevice.waitForBootComplete(REBOOT_TIMEOUT);
+
         assertThat(getFakeApexSystemServiceLogcat())
                 .contains("FakeApexSystemService onStart");
+    }
+
+    @Test
+    public void testInitOrder() throws Exception {
+        // Pre-install the apex
+        String apex = "test_com.android.server.apex";
+        mPreparer.pushResourceFile(apex, "/system/apex/" + apex);
+        // Reboot activates the apex
+        mPreparer.reboot();
+
+        mDevice.waitForBootComplete(REBOOT_TIMEOUT);
+
+        assertThat(getFakeApexSystemServiceLogcat().lines()
+                .map(ApexSystemServicesTestCases::getDebugMessage)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList()))
+                .containsExactly(
+                        // Second service has a higher initOrder and must be started first
+                        "FakeApexSystemService2 onStart",
+                        "FakeApexSystemService onStart"
+                )
+                .inOrder();
     }
 
     private String getFakeApexSystemServiceLogcat() throws DeviceNotAvailableException {
         return mDevice.executeAdbCommand("logcat", "-v", "brief", "-d", "FakeApexSystemService:D",
                 "*:S");
+    }
+
+    private static final Pattern DEBUG_MESSAGE =
+            Pattern.compile("(FakeApexSystemService[0-9]* onStart)");
+
+    private static String getDebugMessage(String logcatLine) {
+        return DEBUG_MESSAGE.matcher(logcatLine)
+                .results()
+                .map(m -> m.group(1))
+                .findFirst()
+                .orElse(null);
     }
 
 }
