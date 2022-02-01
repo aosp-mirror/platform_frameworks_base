f10dcdbb3b56390daa08a80c37b3dbaa7f732787
Dave McCloskey <dlm@google.com>
2022-02-01 00:33:39 +0000

    Revert "Changes to support adding CTS Test TrustTestCases."
    Revert "Add TrustTestCases CTS test with first test for onUserRe..."
    
    Revert submission 16640606-au-cts1
    
    Reason for revert: Test case does not work on CTS. b/217203979
    Reverted Changes:
    I43bffe76c:Changes to support adding CTS Test TrustTestCases....
    Ia0008dd81:Add TrustTestCases CTS test with first test for on...
    
    Change-Id: I9161003d217e624f22d2d212197a365d92aba3c3

--- core/java/android/service/trust/TrustAgentService.java
+++ core/java/android/service/trust/TrustAgentService.java
@@ -21,7 +21,6 @@ import android.annotation.IntDef;
 import android.annotation.NonNull;
 import android.annotation.SdkConstant;
 import android.annotation.SystemApi;
-import android.annotation.TestApi;
 import android.app.Service;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
@@ -311,10 +310,9 @@ public class TrustAgentService extends Service {
      *
      * @see #FLAG_GRANT_TRUST_TEMPORARY_AND_RENEWABLE
      *
-     * TODO(b/213631672): Remove @hide and @TestApi
+     * TODO(b/213631672): Add CTS tests
      * @hide
      */
-    @TestApi
     public void onUserRequestedUnlock() {
     }
 
