55c1473bf2fedaacf7bb8ac068e6f9b1a625b5e0
Vishnu Nair <vishnun@google.com>
2022-02-02 17:13:21 +0000

    Drop input for toast and child surfaces
    Toasts that do not have the trustedOverlay flag should not receive input.
    These windows should not have any children, so force this hierarchy of
    windows to drop all input by setting a flag on the toast window state
    which will apply the DROP_INPUT flag on all windows with an input
    channel. This is to prevent malicious apps from parenting surfaces with
    input channels to the toast window.
    
    Test: show toast and check if input feature flag DROP_INPUT id set via dumpsys
    Bug: b/197296414
    
    Change-Id: I316b76b685ca5030fd8aa91283555efcce4d6994
    Merged-In: I316b76b685ca5030fd8aa91283555efcce4d6994

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1650,6 +1650,7 @@ public class WindowManagerService extends IWindowManager.Stub
 
             win.mToken.addWindow(win);
             displayPolicy.addWindowLw(win, attrs);
+            displayPolicy.setDropInputModePolicy(win, win.mAttrs);
             if (type == TYPE_INPUT_METHOD) {
                 displayContent.setInputMethodWindowLocked(win);
                 imMayMove = false;
