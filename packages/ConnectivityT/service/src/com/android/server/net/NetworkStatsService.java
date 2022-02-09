767c2e79074dac6e012221bba589687446a5bd1c
Frank <lifr@google.com>
2022-02-09 20:37:26 +0800

    [DU03-4]Add NETWORK_STACK Permission check for NetworkStatsService API
    1. getUidStatsForTransport API
    
    Bug: 210066922
    Test: atest BatteryStatsImplTest WifiPowerCalculatorTest
                MobileRadioPowerCalculatorTest NetworkStatsServiceTest
    
    (cherry-picked from ag/16816095)
    
    Change-Id: I022c34b5af87fe3ff6857ea264bac2f7098eaed4
    Merged-In: I022c34b5af87fe3ff6857ea264bac2f7098eaed4

--- packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
+++ packages/ConnectivityT/service/src/com/android/server/net/NetworkStatsService.java
@@ -1123,7 +1123,7 @@ public class NetworkStatsService extends INetworkStatsService.Stub {
 
     @Override
     public NetworkStats getUidStatsForTransport(int transport) {
-        enforceAnyPermissionOf(NetworkStack.PERMISSION_MAINLINE_NETWORK_STACK);
+        PermissionUtils.enforceNetworkStackPermission(mContext);
         try {
             final String[] relevantIfaces =
                     transport == TRANSPORT_WIFI ? mWifiIfaces : mMobileIfaces;
