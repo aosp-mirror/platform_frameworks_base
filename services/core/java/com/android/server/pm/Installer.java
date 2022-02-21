beaf4d8692f8ec6d52aa33629c0d60f69c9aa747
Samiul Islam <samiul@google.com>
2022-02-21 15:29:05 +0000

    Create sdk directory for an app only if app consumes sdk
    Currently, we create sdk directories for all apps. This means during
    boot time, when we create app data directory for system apps, we are now
    creating twice the amount of directories as before. This has significant
    impact on boot time.
    
    In order to avoid the regression, we are now limiting the creation to
    only when app actually consumes an sdk and needs these directories. We
    have introduced a new installd flag: FLAG_STORAGE_SDK, which needs to be
    passed to installd when sdk directory should be created for the app.
    
    Bug: 220095381
    Bug: 217543371
    Test: atest SupplementalProcessStorageHostTest
    Change-Id: I72af7c9460892bf7ac6d908a6a7ca6912167b894

--- services/core/java/com/android/server/pm/Installer.java
+++ services/core/java/com/android/server/pm/Installer.java
@@ -92,6 +92,7 @@ public class Installer extends SystemService {
     public static final int FLAG_STORAGE_DE = IInstalld.FLAG_STORAGE_DE;
     public static final int FLAG_STORAGE_CE = IInstalld.FLAG_STORAGE_CE;
     public static final int FLAG_STORAGE_EXTERNAL = IInstalld.FLAG_STORAGE_EXTERNAL;
+    public static final int FLAG_STORAGE_SDK = IInstalld.FLAG_STORAGE_SDK;
 
     public static final int FLAG_CLEAR_CACHE_ONLY = IInstalld.FLAG_CLEAR_CACHE_ONLY;
     public static final int FLAG_CLEAR_CODE_CACHE_ONLY = IInstalld.FLAG_CLEAR_CODE_CACHE_ONLY;
@@ -190,12 +191,16 @@ public class Installer extends SystemService {
     // We explicitly do NOT set previousAppId because the default value should always be 0.
     // Manually override previousAppId after building CreateAppDataArgs for specific behaviors.
     static CreateAppDataArgs buildCreateAppDataArgs(String uuid, String packageName,
-            int userId, int flags, int appId, String seInfo, int targetSdkVersion) {
+            int userId, int flags, int appId, String seInfo, int targetSdkVersion,
+            boolean usesSdk) {
         final CreateAppDataArgs args = new CreateAppDataArgs();
         args.uuid = uuid;
         args.packageName = packageName;
         args.userId = userId;
         args.flags = flags;
+        if (usesSdk) {
+            args.flags |= FLAG_STORAGE_SDK;
+        }
         args.appId = appId;
         args.seInfo = seInfo;
         args.targetSdkVersion = targetSdkVersion;
