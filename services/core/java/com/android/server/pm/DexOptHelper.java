7c67b428348d50c7f34adcfd5c0d6c80d9eccfec
Nikolas Havrikov <havrikov@google.com>
2022-02-23 16:25:47 +0100

    Replace LinkedList by a more performant collection
    This is a semi-automatic change.
    See https://errorprone.info/bugpattern/JdkObsolete for the rationale.
    
    Test: make
    Bug: 221046110
    Change-Id: I4a32cf96bcdb2bf2d7f7623b6306502d7a57403d

--- services/core/java/com/android/server/pm/DexOptHelper.java
+++ services/core/java/com/android/server/pm/DexOptHelper.java
@@ -72,7 +72,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
@@ -568,7 +567,7 @@ final class DexOptHelper {
             Collection<? extends PackageStateInternal> pkgSettings,
             PackageManagerService packageManagerService,
             boolean debug) {
-        List<PackageStateInternal> result = new LinkedList<>();
+        List<PackageStateInternal> result = new ArrayList<>();
         ArrayList<PackageStateInternal> remainingPkgSettings = new ArrayList<>(pkgSettings);
 
         // First, remove all settings without available packages
