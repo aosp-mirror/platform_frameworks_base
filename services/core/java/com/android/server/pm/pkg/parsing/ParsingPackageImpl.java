3a14d0599a672de2f4d4280dfde83a081009348a
Pedro Loureiro <pedroql@google.com>
2022-02-24 15:23:49 +0000

    Parse maxSdkVersion for APK in apexes
    "Normal" app installs are not affected by this change.
    
    Test: atest apex_apkinapexmaxsdk_tests
    
    Bug: 208239394
    
    Change-Id: Iceb464dcb6fe1e9c26c456be85379ce709753103

--- services/core/java/com/android/server/pm/pkg/parsing/ParsingPackageImpl.java
+++ services/core/java/com/android/server/pm/pkg/parsing/ParsingPackageImpl.java
@@ -383,6 +383,7 @@ public class ParsingPackageImpl implements ParsingPackage, ParsingPackageHidden,
     @Nullable
     private SparseIntArray minExtensionVersions;
     private int minSdkVersion = ParsingUtils.DEFAULT_MIN_SDK_VERSION;
+    private int maxSdkVersion = ParsingUtils.DEFAULT_MAX_SDK_VERSION;
     private int networkSecurityConfigRes;
     @Nullable
     private CharSequence nonLocalizedLabel;
@@ -1308,6 +1309,7 @@ public class ParsingPackageImpl implements ParsingPackage, ParsingPackageHidden,
         dest.writeFloat(this.maxAspectRatio);
         dest.writeFloat(this.minAspectRatio);
         dest.writeInt(this.minSdkVersion);
+        dest.writeInt(this.maxSdkVersion);
         dest.writeInt(this.networkSecurityConfigRes);
         dest.writeCharSequence(this.nonLocalizedLabel);
         dest.writeString(this.permission);
@@ -1456,6 +1458,7 @@ public class ParsingPackageImpl implements ParsingPackage, ParsingPackageHidden,
         this.maxAspectRatio = in.readFloat();
         this.minAspectRatio = in.readFloat();
         this.minSdkVersion = in.readInt();
+        this.maxSdkVersion = in.readInt();
         this.networkSecurityConfigRes = in.readInt();
         this.nonLocalizedLabel = in.readCharSequence();
         this.permission = in.readString();
@@ -2070,6 +2073,11 @@ public class ParsingPackageImpl implements ParsingPackage, ParsingPackageHidden,
     }
 
     @Override
+    public int getMaxSdkVersion() {
+        return maxSdkVersion;
+    }
+
+    @Override
     public int getNetworkSecurityConfigRes() {
         return networkSecurityConfigRes;
     }
@@ -2600,6 +2608,12 @@ public class ParsingPackageImpl implements ParsingPackage, ParsingPackageHidden,
     @Override
     public ParsingPackageImpl setMinSdkVersion(int value) {
         minSdkVersion = value;
+        return this;
+    }
+
+    @Override
+    public ParsingPackageImpl setMaxSdkVersion(int value) {
+        maxSdkVersion = value;
         return this;
     }
 
