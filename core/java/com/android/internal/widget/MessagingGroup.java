88f4dbce894885e1fef1d0871817d2cfad61c763
Steve Elliott <steell@google.com>
2022-03-10 16:16:28 -0500

    Defer MessagingMessage#recycle until bind ends
    See ag/17158486 for context.
    
    Fixes: 216202070
    Test: manual
    Change-Id: I924f917fbf757d2c3866fcd60de2fbfccae2eb6a

--- core/java/com/android/internal/widget/MessagingGroup.java
+++ core/java/com/android/internal/widget/MessagingGroup.java
@@ -262,7 +262,8 @@ public class MessagingGroup extends LinearLayout implements MessagingLinearLayou
         return createdGroup;
     }
 
-    public void removeMessage(MessagingMessage messagingMessage) {
+    public void removeMessage(MessagingMessage messagingMessage,
+            ArrayList<MessagingLinearLayout.MessagingChild> toRecycle) {
         View view = messagingMessage.getView();
         boolean wasShown = view.isShown();
         ViewGroup messageParent = (ViewGroup) view.getParent();
@@ -270,15 +271,14 @@ public class MessagingGroup extends LinearLayout implements MessagingLinearLayou
             return;
         }
         messageParent.removeView(view);
-        Runnable recycleRunnable = () -> {
-            messageParent.removeTransientView(view);
-            messagingMessage.recycle();
-        };
         if (wasShown && !MessagingLinearLayout.isGone(view)) {
             messageParent.addTransientView(view, 0);
-            performRemoveAnimation(view, recycleRunnable);
+            performRemoveAnimation(view, () -> {
+                messageParent.removeTransientView(view);
+                messagingMessage.recycle();
+            });
         } else {
-            recycleRunnable.run();
+            toRecycle.add(messagingMessage);
         }
     }
 
