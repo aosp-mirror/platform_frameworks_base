dd51f02468660bfa611e612fb76748b91481d816
Mady Mellor <madym@google.com>
2022-03-10 20:34:01 +0000

    Enforce statusbarservice for hideCurrentInputMethodForBubbles
    Bug: 192950627
    Test: manual - hide ime for bubbles still works
    Change-Id: I99d2256a1077e247b08588b0a01884568742f9e9

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -1652,6 +1652,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub implements D
 
     @Override
     public void hideCurrentInputMethodForBubbles() {
+        enforceStatusBarService();
         final long token = Binder.clearCallingIdentity();
         try {
             InputMethodManagerInternal.get().hideCurrentInputMethod(
