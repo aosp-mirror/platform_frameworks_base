14913d1d083b631425a540c72dec97a549184fce
kholoud mohamed <kholoudm@google.com>
2022-03-21 15:13:18 +0000

    RESTRICT AUTOMERGE Fixed and reenabled failing DevicePolicyManagerTest
    Also refactored DPMS to stop calling DPM#getString
    
    Fixes: 225415867
    Test: atest com.android.server.devicepolicy.DevicePolicyManagerTest
    Change-Id: Id53bd4c6b3f57d8ac98a31d28eaa27cb98547852

--- services/tests/servicestests/src/com/android/server/devicepolicy/DevicePolicyManagerServiceTestable.java
+++ services/tests/servicestests/src/com/android/server/devicepolicy/DevicePolicyManagerServiceTestable.java
@@ -530,5 +530,10 @@ public class DevicePolicyManagerServiceTestable extends DevicePolicyManagerServi
 
             return true;
         }
+
+        @Override
+        public Context createContextAsUser(UserHandle user) {
+            return context;
+        }
     }
 }
