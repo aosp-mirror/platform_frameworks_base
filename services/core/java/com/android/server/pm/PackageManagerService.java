8cb3e6aac93147287bcb9fa41a284b1745d47c75
Songchun Fan <schfan@google.com>
2022-03-22 22:29:25 +0000

    [AppsFilter] read-only interface for snapshots
    Provides a read-only interface that is used by computer and snapshots.
    It fixes the data conflicts when AppsFilter is changed while a snapshot is
    taken. This requires a watchable class for SparseSetArray.
    
    Test: atest AppsFilterTest
    Test: atest com.android.server.utils.WatcherTest
    Test: m RUN_ERROR_PRONE=true framework services.core |& grep AppsFilter
    BUG: 218411030
    Change-Id: Ib9d13537b6cec911b2a189aea828e9baec650585

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -724,7 +724,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     }
 
     @Watched
-    final AppsFilter mAppsFilter;
+    final AppsFilterImpl mAppsFilter;
 
     final PackageParser2.Callback mPackageParserCallback;
 
@@ -981,7 +981,7 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
         public final InstantAppRegistry instantAppRegistry;
         public final ApplicationInfo androidApplication;
         public final String appPredictionServicePackage;
-        public final AppsFilter appsFilter;
+        public final AppsFilterSnapshot appsFilter;
         public final ComponentResolverApi componentResolver;
         public final PackageManagerService service;
         public final WatchedArrayMap<String, Integer> frozenPackages;
@@ -1433,7 +1433,8 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
                         RuntimePermissionsPersistence.createInstance(),
                         i.getPermissionManagerServiceInternal(),
                         domainVerificationService, lock),
-                (i, pm) -> AppsFilter.create(i, i.getLocalService(PackageManagerInternal.class)),
+                (i, pm) -> AppsFilterImpl.create(i,
+                        i.getLocalService(PackageManagerInternal.class)),
                 (i, pm) -> (PlatformCompat) ServiceManager.getService("platform_compat"),
                 (i, pm) -> SystemConfig.getInstance(),
                 (i, pm) -> new PackageDexOptimizer(i.getInstaller(), i.getInstallLock(),
