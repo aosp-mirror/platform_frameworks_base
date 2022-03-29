882f7c4aba536ecf8660964a8260be64f67752da
Zoey Chen <zoeychen@google.com>
2022-03-29 17:44:43 +0800

    [LeAudio] New QR code scanner UIs and modify local API
    Bug: 214340362
    Test: manual
    Change-Id: I6f654d808dbcb92c99eaad9507b6328a80a0d5f4
    Merged-In: I6f654d808dbcb92c99eaad9507b6328a80a0d5f4

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothBroadcastUtils.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/BluetoothBroadcastUtils.java
@@ -18,7 +18,32 @@ package com.android.settingslib.bluetooth;
 
 public final class BluetoothBroadcastUtils {
 
-    static final String SCHEME_BT_BROADCAST_METADATA = "BT:";
+    /**
+     * The fragment tag specified to FragmentManager for container activities to manage fragments.
+     */
+    public static final String TAG_FRAGMENT_QR_CODE_SCANNER = "qr_code_scanner_fragment";
+
+    /**
+     * Action for launching qr code scanner activity.
+     */
+    public static final String ACTION_BLUETOOTH_LE_AUDIO_QR_CODE_SCANNER =
+            "android.settings.BLUETOOTH_LE_AUDIO_QR_CODE_SCANNER";
+
+    /**
+     * Extra for {@link android.bluetooth.BluetoothDevice}.
+     */
+    public static final String EXTRA_BLUETOOTH_DEVICE_SINK = "bluetooth_device_sink";
+
+    /**
+     * Extra for checking the {@link android.bluetooth.BluetoothLeBroadcastAssistant} should perform
+     * this operation for all coordinated set members throughout one session or not.
+     */
+    public static final String EXTRA_BLUETOOTH_SINK_IS_GROUP = "bluetooth_sink_is_group";
+
+    /**
+     * Bluetooth scheme.
+     */
+    public static final String SCHEME_BT_BROADCAST_METADATA = "BT:";
 
     // BluetoothLeBroadcastMetadata
     static final String PREFIX_BT_ADDRESS_TYPE = "T:";
