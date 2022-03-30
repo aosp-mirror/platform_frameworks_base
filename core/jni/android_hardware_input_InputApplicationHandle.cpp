2b4d25be789ae2c4148d12fa7aa894bd5ff484d3
chaviw <chaviw@google.com>
2022-03-30 11:24:30 -0500

    Add ScopedLocalRef for javaObjectForIBinder
    javaObjectForIBinder creates a Java object that is not getting freed.
    Wrap all javaObjectForIBinder calls with ScopedLocalRef so they're
    automatically cleaned up
    
    Test: Builds
    Bug: 225287201
    Change-Id: Id5970ef8b1ff256ebc404d4242819728f120050d

--- core/jni/android_hardware_input_InputApplicationHandle.cpp
+++ core/jni/android_hardware_input_InputApplicationHandle.cpp
@@ -105,10 +105,11 @@ std::shared_ptr<InputApplicationHandle> android_view_InputApplicationHandle_getH
 
 jobject android_view_InputApplicationHandle_fromInputApplicationInfo(
         JNIEnv* env, gui::InputApplicationInfo inputApplicationInfo) {
-    jobject binderObject = javaObjectForIBinder(env, inputApplicationInfo.token);
+    ScopedLocalRef<jobject> binderObject(env,
+                                         javaObjectForIBinder(env, inputApplicationInfo.token));
     ScopedLocalRef<jstring> name(env, env->NewStringUTF(inputApplicationInfo.name.data()));
     return env->NewObject(gInputApplicationHandleClassInfo.clazz,
-                          gInputApplicationHandleClassInfo.ctor, binderObject, name.get(),
+                          gInputApplicationHandleClassInfo.ctor, binderObject.get(), name.get(),
                           inputApplicationInfo.dispatchingTimeoutMillis);
 }
 
