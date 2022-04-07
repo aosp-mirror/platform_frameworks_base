e40047950b424731b9b1b9d48d9b03b14a3a5cae
wilsonshih <wilsonshih@google.com>
2022-04-07 12:00:31 +0800

    Extends waiting condition for wake up transition.
    While device is about to wake up, add the wait condition for dismiss
    AOD, which can be a condition to release the sleep token.
    
    Also consider the mUnknownAppVisibilityController in AR#isSyncFinished,
    because the visibility can changed before all of unknown conditions
    resolved.
    
    Bug: 225127896
    Test: start showWhenLocked + turnScreenOn activity while screen off,
    finish activity, then start the activity again.
    
    Change-Id: I6cb2df7385091b45b45d49197aaa259216786a79

--- services/core/java/com/android/server/wm/ActivityTaskManagerService.java
+++ services/core/java/com/android/server/wm/ActivityTaskManagerService.java
@@ -64,7 +64,6 @@ import static android.provider.Settings.Global.HIDE_ERROR_DIALOGS;
 import static android.provider.Settings.System.FONT_SCALE;
 import static android.view.Display.DEFAULT_DISPLAY;
 import static android.view.Display.INVALID_DISPLAY;
-import static android.view.WindowManager.TRANSIT_WAKE;
 
 import static com.android.internal.protolog.ProtoLogGroup.WM_DEBUG_CONFIGURATION;
 import static com.android.internal.protolog.ProtoLogGroup.WM_DEBUG_FOCUS;
@@ -6625,15 +6624,6 @@ public class ActivityTaskManagerService extends IActivityTaskManager.Stub {
                 String callingPackage) {
             return ActivityTaskManagerService.this.hasSystemAlertWindowPermission(callingUid,
                     callingPid, callingPackage);
-        }
-
-        @Override
-        public void notifyWakingUp() {
-            synchronized (mGlobalLock) {
-                // Start a transition for waking. This is needed for showWhenLocked activities.
-                getTransitionController().requestTransitionIfNeeded(TRANSIT_WAKE, 0 /* flags */,
-                        null /* trigger */, mRootWindowContainer.getDefaultDisplay());
-            }
         }
 
         @Override
