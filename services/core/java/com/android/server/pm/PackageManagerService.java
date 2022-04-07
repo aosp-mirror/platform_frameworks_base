2c8aeb6ae8a10bc6603b0ca8e3b0039049139b2d
Songchun Fan <schfan@google.com>
2022-04-07 23:24:37 +0000

    [pm] delete unfinished sessions when installer is uninstalled
    A malicious app can create many new sessions without any permission.
    Prior to this CL, those sessions might remain for days even after the
    app has been uninstalled. This change makes PackageInstallerService
    promptly abandon unfinished sessions as soon as the installer package is
    uninstalled.
    
    BUG: 219030339
    Test: atest android.content.pm.cts.InstallSessionCleanupTest
    Change-Id: I72c4a29e031cb47c16c9a0fbafdf68b75c4d6a55

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -7167,4 +7167,10 @@ public class PackageManagerService implements PackageSender, TestUtilityService 
     void notifyInstantAppPackageInstalled(String packageName, int[] newUsers) {
         mInstantAppRegistry.onPackageInstalled(snapshotComputer(), packageName, newUsers);
     }
+
+    void addInstallerPackageName(InstallSource installSource) {
+        synchronized (mLock) {
+            mSettings.addInstallerPackageNames(installSource);
+        }
+    }
 }
