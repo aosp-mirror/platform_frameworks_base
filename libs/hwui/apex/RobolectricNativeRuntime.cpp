55f21e74e9642481dbea73bb3de4449159de4384
Michael Hoisie <hoisie@google.com>
2022-04-12 19:45:20 +0000

    Move graphics JNI registration into hwui
    Previously, the JavaVM global was not being set in GraphicsJNI. This
    caused a segfault when GraphicsJNI::getJNIEnv was called from Font and
    FontFamily JNI, as getJNIEnv contains an assertion that the JavaVM is
    not null.
    
    Move all graphics registration into a new file in hwui that has the
    ability to call GraphicsJNI::setJNIEnv. This more closely resembles the
    initialization structure for layoutlib.
    
    Test: m -j robolectric_native_runtime dist
    
    Change-Id: Idae7d5b51e06affac40ba82c7a739ba05dfbfd74

---
+++ libs/hwui/apex/RobolectricNativeRuntime.cpp
@@ -0,0 +1,114 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <GraphicsJNI.h>
+#include <SkGraphics.h>
+
+#include <unordered_map>
+#include <vector>
+
+#include "Properties.h"
+#include "android/graphics/jni_runtime.h"
+#include "graphics_jni_helpers.h"
+
+using namespace std;
+
+int register_android_graphics_Bitmap(JNIEnv*);
+int register_android_graphics_BitmapFactory(JNIEnv*);
+int register_android_graphics_CreateJavaOutputStreamAdaptor(JNIEnv* env);
+int register_android_graphics_Graphics(JNIEnv* env);
+int register_android_graphics_MaskFilter(JNIEnv* env);
+int register_android_graphics_PathEffect(JNIEnv* env);
+int register_android_graphics_Shader(JNIEnv* env);
+int register_android_graphics_Typeface(JNIEnv* env);
+int register_android_graphics_RenderEffect(JNIEnv* env);
+
+namespace android {
+
+int register_android_graphics_ColorFilter(JNIEnv* env);
+int register_android_graphics_ColorSpace(JNIEnv* env);
+int register_android_graphics_FontFamily(JNIEnv* env);
+int register_android_graphics_Matrix(JNIEnv* env);
+int register_android_graphics_Paint(JNIEnv* env);
+int register_android_graphics_Path(JNIEnv* env);
+int register_android_graphics_PathMeasure(JNIEnv* env);
+int register_android_graphics_Region(JNIEnv* env);
+int register_android_graphics_fonts_Font(JNIEnv* env);
+int register_android_graphics_fonts_FontFamily(JNIEnv* env);
+int register_android_graphics_text_LineBreaker(JNIEnv* env);
+int register_android_graphics_text_MeasuredText(JNIEnv* env);
+int register_android_util_PathParser(JNIEnv* env);
+
+#define REG_JNI(name) \
+    { name }
+struct RegJNIRec {
+    int (*mProc)(JNIEnv*);
+};
+
+static const RegJNIRec graphicsJNI[] = {
+        REG_JNI(register_android_graphics_Bitmap),
+        REG_JNI(register_android_graphics_BitmapFactory),
+        REG_JNI(register_android_graphics_ColorFilter),
+        REG_JNI(register_android_graphics_ColorSpace),
+        REG_JNI(register_android_graphics_CreateJavaOutputStreamAdaptor),
+        REG_JNI(register_android_graphics_FontFamily),
+        REG_JNI(register_android_graphics_Graphics),
+        REG_JNI(register_android_graphics_MaskFilter),
+        REG_JNI(register_android_graphics_Matrix),
+        REG_JNI(register_android_graphics_Paint),
+        REG_JNI(register_android_graphics_Path),
+        REG_JNI(register_android_graphics_PathEffect),
+        REG_JNI(register_android_graphics_PathMeasure),
+        REG_JNI(register_android_graphics_Region),
+        REG_JNI(register_android_graphics_RenderEffect),
+        REG_JNI(register_android_graphics_Shader),
+        REG_JNI(register_android_graphics_Typeface),
+        REG_JNI(register_android_graphics_fonts_Font),
+        REG_JNI(register_android_graphics_fonts_FontFamily),
+        REG_JNI(register_android_graphics_text_LineBreaker),
+        REG_JNI(register_android_graphics_text_MeasuredText),
+        REG_JNI(register_android_util_PathParser),
+};
+
+static int register_jni_procs(const RegJNIRec array[], size_t count, JNIEnv* env) {
+    for (size_t i = 0; i < count; i++) {
+        if (array[i].mProc(env) < 0) {
+            return -1;
+        }
+    }
+    return 0;
+}
+}  // namespace android
+
+using namespace android;
+
+void init_android_graphics() {
+    SkGraphics::Init();
+}
+
+int register_android_graphics_classes(JNIEnv* env) {
+    JavaVM* vm = nullptr;
+    env->GetJavaVM(&vm);
+    GraphicsJNI::setJavaVM(vm);
+
+    if (register_jni_procs(graphicsJNI, NELEM(graphicsJNI), env) < 0) {
+        return JNI_ERR;
+    }
+
+    return 0;
+}
+
+void zygote_preload_graphics() {}
