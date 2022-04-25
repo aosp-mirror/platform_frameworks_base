e736d465ad40f16e51a7f8aad97b3d5c227ba7b9
Sooraj Sasindran <sasindran@google.com>
2022-04-25 18:48:20 +0000

    Revert "Allow device owner to configure preferential network service"
    This reverts commit 787dedce39017bb450b2ed75e9843bb7a3a1046c.
    
    Reason for revert: b/230351564
    
    Change-Id: Id32fdad0d4c31992ca739da681ef7273d6dd2705
    (cherry picked from commit b579517aca640c386277a385598694c2db680872)
    Merged-In: Id32fdad0d4c31992ca739da681ef7273d6dd2705

--- services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
+++ services/devicepolicy/java/com/android/server/devicepolicy/DevicePolicyManagerService.java
@@ -8515,13 +8515,6 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
         }
     }
 
-    private boolean isDeviceOwnerUserId(int userId) {
-        synchronized (getLockObject()) {
-            return mOwners.hasDeviceOwner()
-                    && mOwners.getDeviceOwnerUserId() == userId;
-        }
-    }
-
     private boolean isDeviceOwnerPackage(String packageName, int userId) {
         synchronized (getLockObject()) {
             return mOwners.hasDeviceOwner()
@@ -18267,7 +18260,7 @@ public class DevicePolicyManagerService extends BaseIDevicePolicyManager {
 
     private void updateNetworkPreferenceForUser(int userId,
             List<PreferentialNetworkServiceConfig> preferentialNetworkServiceConfigs) {
-        if (!isManagedProfile(userId) && !isDeviceOwnerUserId(userId)) {
+        if (!isManagedProfile(userId)) {
             return;
         }
         List<ProfileNetworkPreference> preferences = new ArrayList<>();
