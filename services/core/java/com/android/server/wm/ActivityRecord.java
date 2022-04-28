cf56e7e2940eb4ed73169a18fa989fc813b81083
Linus Tufvesson <lus@google.com>
2022-04-28 16:35:56 +0200

    Make Activites touch opaque - DO NOT MERGE
    Block touches from passing through activities by adding a dedicated
    surface that consumes all touches that would otherwise pass through the
    bounds availble to the Activity.
    
    Bug: 194480991
    Test: atest CtsWindowManagerDeviceTestCases:ActivityRecordInputSinkTests
    Test: atest CtsWindowManagerDeviceTestCases:CrossAppDragAndDropTests
    Test: atest CtsWindowManagerDeviceTestCases:PinnedStackTests
    Test: Manually ran through go/wm-smoke (verified pip and splitscreen
    still work)
    Test: Manually verfied that freeform windows work and don't consume
    touches outside their bounds
    
    Change-Id: I01b35e34a63868dead4e728a3d359ae0942302f9
    (cherry picked from commit 22261fa6649f6ec6441646743ad98132fcf47fe0)
    Merged-In: I01b35e34a63868dead4e728a3d359ae0942302f9

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -769,6 +769,13 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
 
     private AppSaturationInfo mLastAppSaturationInfo;
 
+    private final ActivityRecordInputSink mActivityRecordInputSink;
+
+    // Activities with this uid are allowed to not create an input sink while being in the same
+    // task and directly above this ActivityRecord. This field is updated whenever a new activity
+    // is launched from this ActivityRecord. Touches are always allowed within the same uid.
+    int mAllowedTouchUid;
+
     private final ColorDisplayService.ColorTransformController mColorTransformController =
             (matrix, translation) -> mWmService.mH.post(() -> {
                 synchronized (mWmService.mGlobalLock) {
@@ -1713,6 +1720,8 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             createTime = _createTime;
         }
         mAtmService.mPackageConfigPersister.updateConfigIfNeeded(this, mUserId, packageName);
+
+        mActivityRecordInputSink = new ActivityRecordInputSink(this, sourceRecord);
     }
 
     /**
@@ -3549,6 +3558,7 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             destroyImmediately("removeImmediately");
         }
         onRemovedFromDisplay();
+        mActivityRecordInputSink.releaseSurfaceControl();
         super.removeImmediately();
     }
 
@@ -6673,6 +6683,9 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
                 getSyncTransaction().show(mSurfaceControl);
             } else if (!show && mLastSurfaceShowing) {
                 getSyncTransaction().hide(mSurfaceControl);
+            }
+            if (show) {
+                mActivityRecordInputSink.applyChangesToSurfaceIfChanged(getSyncTransaction());
             }
         }
         if (mThumbnail != null) {
