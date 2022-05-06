df399f52e73b939092b73d8f351cb6d965b96475
Ben Lin <linben@google.com>
2022-05-06 16:43:51 -0700

    Move CentralSurface into initialize().
    Not all devices have CentralSurface, so moving off injection into
    initialize parameter.
    
    Cherry-picked from ag/18698975.
    
    Bug: 243819288
    Test: build
    Change-Id: I2debf1f857b397924ddb29b24ea6863421ac3304

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -1421,6 +1421,7 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
         mStackScrollerController.setNotificationActivityStarter(mNotificationActivityStarter);
         mGutsManager.setNotificationActivityStarter(mNotificationActivityStarter);
         mNotificationsController.initialize(
+                this,
                 mPresenter,
                 mNotifListContainer,
                 mStackScrollerController.getNotifStackController(),
