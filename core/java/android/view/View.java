528b0d282438d107429aa5f88b100c4e2d05b333
Nader Jawad <njawad@google.com>
2022-05-10 13:58:36 -0700

    Fix memory leak with RenderNodeAnimator
    Update View logic to cancel all RenderNodeAnimators
    when it is detached from a window.
    Updated HWUI Animation logic to enable a cancellation
    flag to cancel all animators operating on a RenderNode
    whenever the staging parameters are pushed to RenderThread
    
    Fixes: 229136453
    Test: Added core test to RenderNodeAnimatorTests
    Change-Id: Id674e8474757bfc8dfe30394dde29da49d139bfc

--- core/java/android/view/View.java
+++ core/java/android/view/View.java
@@ -21149,6 +21149,11 @@ public class View implements Drawable.Callback, KeyEvent.Callback,
         }
 
         AccessibilityNodeIdManager.getInstance().unregisterViewWithId(getAccessibilityViewId());
+
+        if (mBackgroundRenderNode != null) {
+            mBackgroundRenderNode.forceEndAnimators();
+        }
+        mRenderNode.forceEndAnimators();
     }
 
     private void cleanupDraw() {
