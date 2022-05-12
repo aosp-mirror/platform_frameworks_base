ae445b4c4f76327f47990a076692c82e0f9a2a06
Josh Yang <yzj@google.com>
2022-05-11 17:13:12 -0700

    Add a setting WEAR_ACTIVITY_AUTO_RESUME_TIMEOUT_MAX_RESET_COUNT
    Bug: 196111331
    Test: manual test
    Change-Id: I937fb76aceb47e569ad90ca03289410cdf2ad4c4

--- packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
+++ packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
@@ -656,7 +656,8 @@ public class SettingsBackupTest {
                     Settings.Global.Wearable.COOLDOWN_MODE_ON,
                     Settings.Global.Wearable.CHARGING_SOUNDS_ENABLED,
                     Settings.Global.Wearable.SCREEN_UNLOCK_SOUND_ENABLED,
-                    Settings.Global.Wearable.BEDTIME_MODE);
+                    Settings.Global.Wearable.BEDTIME_MODE,
+                    Settings.Global.Wearable.WEAR_ACTIVITY_AUTO_RESUME_TIMEOUT_MAX_RESET_COUNT);
 
     private static final Set<String> BACKUP_DENY_LIST_SECURE_SETTINGS =
              newHashSet(
