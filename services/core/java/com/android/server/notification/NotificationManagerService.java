239251c1cd9ed4bb629578d5f66246412c889122
Julia Reynolds <juliacr@google.com>
2022-05-16 15:28:24 -0400

    Move service initialization
    Occasionally ILockSettings can fail to be initialized otherwise
    Fixes: 232714129
    Test: boot (and eventually bootstress/reboot-long)
    
    Change-Id: I2f9f9bdba37f4ebfaea56c1a6662f0474ae8a002
    Merged-In: I2f9f9bdba37f4ebfaea56c1a6662f0474ae8a002
    (cherry picked from commit 8e278543bd290d4b6c417758554d6dee93a4fe74)

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -1935,7 +1935,6 @@ public class NotificationManagerService extends SystemService {
         }
     }
 
-    private LockPatternUtils mLockPatternUtils;
     private StrongAuthTracker mStrongAuthTracker;
 
     public NotificationManagerService(Context context) {
@@ -2152,7 +2151,6 @@ public class NotificationManagerService extends SystemService {
                 ServiceManager.getService(Context.PLATFORM_COMPAT_SERVICE));
 
         mUiHandler = new Handler(UiThread.get().getLooper());
-        mLockPatternUtils = new LockPatternUtils(getContext());
         mStrongAuthTracker = new StrongAuthTracker(getContext());
         String[] extractorNames;
         try {
@@ -2629,7 +2627,7 @@ public class NotificationManagerService extends SystemService {
                 bubbsExtractor.setShortcutHelper(mShortcutHelper);
             }
             registerNotificationPreferencesPullers();
-            mLockPatternUtils.registerStrongAuthTracker(mStrongAuthTracker);
+            new LockPatternUtils(getContext()).registerStrongAuthTracker(mStrongAuthTracker);
         } else if (phase == SystemService.PHASE_THIRD_PARTY_APPS_CAN_START) {
             // This observer will force an update when observe is called, causing us to
             // bind to listener services.
