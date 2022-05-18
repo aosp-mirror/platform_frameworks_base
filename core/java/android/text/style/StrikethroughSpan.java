0561027f9a964cd60ac0f6f0b47c047ef261a924
Seigo Nonaka <nona@google.com>
2022-05-18 11:04:51 -0700

    Add descriptive toString method and missing accessors
    Bug: N/A
    Test: compile
    Change-Id: I15411d8bf67dba9745f4f2e02114099612b9a844

--- core/java/android/text/style/StrikethroughSpan.java
+++ core/java/android/text/style/StrikethroughSpan.java
@@ -77,4 +77,9 @@ public class StrikethroughSpan extends CharacterStyle
     public void updateDrawState(@NonNull TextPaint ds) {
         ds.setStrikeThruText(true);
     }
+
+    @Override
+    public String toString() {
+        return "StrikethroughSpan{}";
+    }
 }
