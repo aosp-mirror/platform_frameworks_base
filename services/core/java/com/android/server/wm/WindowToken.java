95ad5b5372e1e803654a647b66904e00748d0d9d
Vaibhav Devmurari <vdevmurari@google.com>
2022-05-19 16:20:39 +0000

    Change Rounded corner overlay to be in Overlay layer
    Rounded corner overlay should be topmost visible window in overlay
    layer. Only Input gesture windows can be higher in hierarchy than
    rounded corner overlay.
    
    Test: Manual
    Bug: 225505986
    Change-Id: I7bf2380edbb8267347a8ded53b0755524f6781f5
    Merged-In: I7bf2380edbb8267347a8ded53b0755524f6781f5
    (cherry picked from commit 0809288dfda6a2d041633a765d5ad9fd9260013f)

--- services/core/java/com/android/server/wm/WindowToken.java
+++ services/core/java/com/android/server/wm/WindowToken.java
@@ -368,7 +368,7 @@ class WindowToken extends WindowContainer<WindowState> {
             super.assignRelativeLayer(t,
                     mDisplayContent.getDefaultTaskDisplayArea().getSplitScreenDividerAnchor(), 1);
         } else if (mRoundedCornerOverlay) {
-            super.assignLayer(t, WindowManagerPolicy.COLOR_FADE_LAYER + 1);
+            super.assignLayer(t, WindowManagerPolicy.SCREEN_DECOR_DISPLAY_OVERLAY_LAYER);
         } else {
             super.assignLayer(t, layer);
         }
@@ -378,7 +378,7 @@ class WindowToken extends WindowContainer<WindowState> {
     SurfaceControl.Builder makeSurface() {
         final SurfaceControl.Builder builder = super.makeSurface();
         if (mRoundedCornerOverlay) {
-            builder.setParent(null);
+            builder.setParent(getDisplayContent().getOverlayLayer());
         }
         return builder;
     }
