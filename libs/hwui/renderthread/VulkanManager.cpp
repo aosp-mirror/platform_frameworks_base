bf3cba0c6cbf67dd4c1e80bc33b406d1d2146b9b
Alec Mouri <alecmouri@google.com>
2022-05-27 22:03:11 +0000

    Make GPU duration metrics more accurate for Vulkan
    In the Vulkan pipeline, the GPU start time was measured to be when
    swapBuffers starts. But the command queue has already been submitted at
    this point, which means that the GPU work would already have begun. This
    means that it's possible to measure a negative time since for very light
    GPU workloads, the GPU fence can fire prior to CPU making it to
    swapBuffers().
    
    To compensate, instead measure from after skia completed submitting GPU
    commands, until the GPU fence fires. Since it is theoretically possible
    for GPU work to complete if the render thread gets descheduled
    immediately after submitting the GPU, we also add some clamping to
    ensure that the duration from command submission -> completion of GPU
    work is nonnegative.
    
    Bug: 230713131
    Bug: 240888213
    Test: atest android.view.cts.FrameMetricsListenerTest
    Change-Id: Ia30b7732eaab71e4e29766f788d5cd94ec63c38a
    Merged-In: Ia30b7732eaab71e4e29766f788d5cd94ec63c38a

--- libs/hwui/renderthread/VulkanManager.cpp
+++ libs/hwui/renderthread/VulkanManager.cpp
@@ -491,7 +491,7 @@ static void destroy_semaphore(void* context) {
     }
 }
 
-void VulkanManager::finishFrame(SkSurface* surface) {
+nsecs_t VulkanManager::finishFrame(SkSurface* surface) {
     ATRACE_NAME("Vulkan finish frame");
     ALOGE_IF(mSwapSemaphore != VK_NULL_HANDLE || mDestroySemaphoreContext != nullptr,
              "finishFrame already has an outstanding semaphore");
@@ -527,6 +527,7 @@ void VulkanManager::finishFrame(SkSurface* surface) {
     GrDirectContext* context = GrAsDirectContext(surface->recordingContext());
     ALOGE_IF(!context, "Surface is not backed by gpu");
     context->submit();
+    const nsecs_t submissionTime = systemTime();
     if (semaphore != VK_NULL_HANDLE) {
         if (submitted == GrSemaphoresSubmitted::kYes) {
             mSwapSemaphore = semaphore;
@@ -555,6 +556,8 @@ void VulkanManager::finishFrame(SkSurface* surface) {
         }
     }
     skiapipeline::ShaderCache::get().onVkFrameFlushed(context);
+
+    return submissionTime;
 }
 
 void VulkanManager::swapBuffers(VulkanSurface* surface, const SkRect& dirtyRect) {
