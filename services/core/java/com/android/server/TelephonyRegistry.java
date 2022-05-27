c947aed9485c227062f8c1979910830100762a80
Jack Yu <jackyu@google.com>
2022-05-26 17:57:37 -0700

    Improved telephony registry logging
    Bug: 232630223
    Test: Manual
    Merged-In: Ifd00798a9add597b06677b857c58b3a946fb7435
    Change-Id: Ifd00798a9add597b06677b857c58b3a946fb7435

--- services/core/java/com/android/server/TelephonyRegistry.java
+++ services/core/java/com/android/server/TelephonyRegistry.java
@@ -2357,9 +2357,8 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
             return;
         }
 
-        if (VDBG) {
-            log("notifyActiveDataSubIdChanged: activeDataSubId=" + activeDataSubId);
-        }
+        log("notifyActiveDataSubIdChanged: activeDataSubId=" + activeDataSubId);
+        mLocalLog.log("notifyActiveDataSubIdChanged: activeDataSubId=" + activeDataSubId);
 
         mActiveDataSubId = activeDataSubId;
         synchronized (mRecords) {
