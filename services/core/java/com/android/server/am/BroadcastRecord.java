facccc241a0154a88045b14a508b9f802aee6905
Christopher Tate <ctate@google.com>
2022-06-27 14:43:27 -0700

    BroadcastOption for "this is an alarm broadcast"
    Instead of a callback/observer mechanism between the alarm subsystem and
    the broadcast delivery subsystem, there will be a direct
    BroadcastOptions tag that the Alarm Manager uses to annotate broadcasts
    that originate from an alarm firing.  Only the OS itself can use this
    BroadcastOptions annotation.
    
    Bug: 161537193
    Test: atest CtsAlarmManagerTestCases
    Test: atest BroadcastOptionsTest
    Test: atest FrameworksMockingServicesTests:AlarmManagerServiceTest
    Change-Id: Ie45fd67f9dd55931a3dc109dc198c118b2a55bce

--- services/core/java/com/android/server/am/BroadcastRecord.java
+++ services/core/java/com/android/server/am/BroadcastRecord.java
@@ -70,6 +70,7 @@ final class BroadcastRecord extends Binder {
     final boolean callerInstantApp; // caller is an Instant App?
     final boolean ordered;  // serialize the send to receivers?
     final boolean sticky;   // originated from existing sticky data?
+    final boolean alarm;    // originated from an alarm triggering?
     final boolean initialSticky; // initial broadcast from register to sticky?
     final int userId;       // user id this broadcast was for
     final String resolvedType; // the resolved data type
@@ -305,6 +306,7 @@ final class BroadcastRecord extends Binder {
         this.allowBackgroundActivityStarts = allowBackgroundActivityStarts;
         mBackgroundActivityStartsToken = backgroundActivityStartsToken;
         this.timeoutExempt = timeoutExempt;
+        alarm = options != null && options.isAlarmBroadcast();
     }
 
     /**
@@ -357,6 +359,7 @@ final class BroadcastRecord extends Binder {
         allowBackgroundActivityStarts = from.allowBackgroundActivityStarts;
         mBackgroundActivityStartsToken = from.mBackgroundActivityStartsToken;
         timeoutExempt = from.timeoutExempt;
+        alarm = from.alarm;
     }
 
     /**
