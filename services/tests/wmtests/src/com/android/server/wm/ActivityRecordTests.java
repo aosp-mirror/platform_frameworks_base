9e6553e0853868c374a042bf61c03e17ddc4efda
Louis Chang <louischang@google.com>
2022-06-28 06:57:09 +0000

    Fix WM presubmit failures
    The changes in SizeConfigurationBuckets in commit 8ec32dd
    was not verified by wm presubmit and therefore broke a few
    wm unit tests.
    
    Bug: 235774601
    Bug: 237348419
    Test: ActivityRecordTests
    Test: SizeConfigurationBucketsTest
    Change-Id: I23299e329c2bf2ca94fc156859993ea31ad3dc75

--- services/tests/wmtests/src/com/android/server/wm/ActivityRecordTests.java
+++ services/tests/wmtests/src/com/android/server/wm/ActivityRecordTests.java
@@ -29,6 +29,8 @@ import static android.app.WindowConfiguration.WINDOWING_MODE_MULTI_WINDOW;
 import static android.app.WindowConfiguration.WINDOWING_MODE_PINNED;
 import static android.content.pm.ActivityInfo.CONFIG_ORIENTATION;
 import static android.content.pm.ActivityInfo.CONFIG_SCREEN_LAYOUT;
+import static android.content.pm.ActivityInfo.CONFIG_SCREEN_SIZE;
+import static android.content.pm.ActivityInfo.CONFIG_SMALLEST_SCREEN_SIZE;
 import static android.content.pm.ActivityInfo.FLAG_SUPPORTS_PICTURE_IN_PICTURE;
 import static android.content.pm.ActivityInfo.LOCK_TASK_LAUNCH_MODE_ALWAYS;
 import static android.content.pm.ActivityInfo.LOCK_TASK_LAUNCH_MODE_DEFAULT;
@@ -181,6 +183,10 @@ import java.util.function.Consumer;
 public class ActivityRecordTests extends WindowTestsBase {
 
     private final String mPackageName = getInstrumentation().getTargetContext().getPackageName();
+
+    private static final int ORIENTATION_CONFIG_CHANGES =
+            CONFIG_ORIENTATION | CONFIG_SCREEN_LAYOUT | CONFIG_SCREEN_SIZE
+                    | CONFIG_SMALLEST_SCREEN_SIZE;
 
     @Before
     public void setUp() throws Exception {
@@ -487,7 +493,7 @@ public class ActivityRecordTests extends WindowTestsBase {
     public void testSetRequestedOrientationUpdatesConfiguration() throws Exception {
         final ActivityRecord activity = new ActivityBuilder(mAtm)
                 .setCreateTask(true)
-                .setConfigChanges(CONFIG_ORIENTATION | CONFIG_SCREEN_LAYOUT)
+                .setConfigChanges(ORIENTATION_CONFIG_CHANGES)
                 .build();
         activity.setState(RESUMED, "Testing");
 
@@ -710,7 +716,7 @@ public class ActivityRecordTests extends WindowTestsBase {
         final ActivityRecord activity = new ActivityBuilder(mAtm)
                 .setCreateTask(true)
                 .setLaunchTaskBehind(true)
-                .setConfigChanges(CONFIG_ORIENTATION | CONFIG_SCREEN_LAYOUT)
+                .setConfigChanges(ORIENTATION_CONFIG_CHANGES)
                 .build();
         final Task task = activity.getTask();
         activity.setState(STOPPED, "Testing");
