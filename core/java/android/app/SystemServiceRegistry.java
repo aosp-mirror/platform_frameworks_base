db259e0e45961a775b25ab08c1609f079a8fc254
Thiébaud Weksteen <tweek@google.com>
2022-06-29 14:02:13 +1000

    Add PermissionEnforcer class
    Defines a new PermissionEnforcer class used to check permission as part
    of the @EnforcePermission AIDL annotation. It is registered as a system
    service to avoid the cost of creating a new object for each new client.
    
    Bug: 234083358
    Test: TH
    Change-Id: I2bd23f62a0d207ddac6bbe69666ced96e77ba16f

--- core/java/android/app/SystemServiceRegistry.java
+++ core/java/android/app/SystemServiceRegistry.java
@@ -172,6 +172,7 @@ import android.os.IThermalService;
 import android.os.IUserManager;
 import android.os.IncidentManager;
 import android.os.PerformanceHintManager;
+import android.os.PermissionEnforcer;
 import android.os.PowerManager;
 import android.os.RecoverySystem;
 import android.os.ServiceManager;
@@ -1363,6 +1364,14 @@ public final class SystemServiceRegistry {
                     public PermissionCheckerManager createService(ContextImpl ctx)
                             throws ServiceNotFoundException {
                         return new PermissionCheckerManager(ctx.getOuterContext());
+                    }});
+
+        registerService(Context.PERMISSION_ENFORCER_SERVICE, PermissionEnforcer.class,
+                new CachedServiceFetcher<PermissionEnforcer>() {
+                    @Override
+                    public PermissionEnforcer createService(ContextImpl ctx)
+                            throws ServiceNotFoundException {
+                        return new PermissionEnforcer(ctx.getOuterContext());
                     }});
 
         registerService(Context.DYNAMIC_SYSTEM_SERVICE, DynamicSystemManager.class,
