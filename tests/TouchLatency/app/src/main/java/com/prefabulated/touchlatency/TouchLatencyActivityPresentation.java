1a4ceb794de1b878d232e60fa6db824bfdba4379
ramindani <ramindani@google.com>
2022-07-26 16:47:24 +0000

    Add support to render app on multiple displays
    BUG: 241152647
    
    Test: Manual Testing
    Change-Id: I912f164b99c34e9b04d73cf37d0a97a19a1afc40

--- tests/TouchLatency/app/src/main/java/com/prefabulated/touchlatency/TouchLatencyActivityPresentation.java
+++ tests/TouchLatency/app/src/main/java/com/prefabulated/touchlatency/TouchLatencyActivityPresentation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,7 @@
 package com.prefabulated.touchlatency;
 
 import android.app.Activity;
-import android.app.ActivityOptions;
-import android.content.Intent;
-import android.hardware.display.DisplayManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Trace;
 import android.view.Display;
 import android.view.Display.Mode;
@@ -30,37 +26,25 @@ import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class TouchLatencyActivity extends Activity {
-    private Mode mDisplayModes[];
+public class TouchLatencyActivityPresentation extends Activity {
+    public static final String DISPLAY_ID = "DISPLAY_ID";
+    private Mode[] mDisplayModes;
     private int mCurrentModeIndex;
-    private DisplayManager mDisplayManager;
-    private final DisplayManager.DisplayListener mDisplayListener =
-            new DisplayManager.DisplayListener() {
-        @Override
-        public void onDisplayAdded(int i) {
-            invalidateOptionsMenu();
-        }
-
-        @Override
-        public void onDisplayRemoved(int i) {
-            invalidateOptionsMenu();
-        }
-
-        @Override
-        public void onDisplayChanged(int i) {
-            invalidateOptionsMenu();
-        }
-    };
+    private int mDisplayId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Trace.beginSection("TouchLatencyActivity onCreate");
+        if (getIntent().hasExtra(DISPLAY_ID)) {
+            mDisplayId = (int) getIntent().getExtras().get(DISPLAY_ID);
+        }
+        Trace.beginSection(
+                "TouchLatencyActivityPresentation::DisplayId::" + mDisplayId + " onCreate");
         setContentView(R.layout.activity_touch_latency);
+
         mTouchView = findViewById(R.id.canvasView);
 
-        configureDisplayListener();
         WindowManager wm = getWindowManager();
         Display display = wm.getDefaultDisplay();
         mDisplayModes = display.getSupportedModes();
@@ -76,8 +60,15 @@ public class TouchLatencyActivity extends Activity {
     }
 
     @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        Trace.beginSection("TouchLatencyActivity onCreateOptionsMenu");
+        Trace.beginSection(
+                "TouchLatencyActivityPresentation::DisplayId:: "
+                        + mDisplayId + "  onCreateOptionsMenu");
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_touch_latency, menu);
         if (mDisplayModes.length > 1) {
@@ -85,7 +76,6 @@ public class TouchLatencyActivity extends Activity {
             Mode currentMode = getWindowManager().getDefaultDisplay().getMode();
             updateDisplayMode(menuItem, currentMode);
         }
-        updateMultiDisplayMenu(menu.findItem(R.id.multi_display));
         Trace.endSection();
         return true;
     }
@@ -96,31 +86,11 @@ public class TouchLatencyActivity extends Activity {
         menuItem.setVisible(true);
     }
 
-    private void updateMultiDisplayMenu(MenuItem item) {
-        item.setVisible(mDisplayManager.getDisplays().length > 1);
-    }
-
-    private void configureDisplayListener() {
-        mDisplayManager = getSystemService(DisplayManager.class);
-        mDisplayManager.registerDisplayListener(mDisplayListener, new Handler());
-    }
-
     public void changeDisplayMode(MenuItem item) {
         Window w = getWindow();
         WindowManager.LayoutParams params = w.getAttributes();
 
         int modeIndex = (mCurrentModeIndex + 1) % mDisplayModes.length;
-        while (modeIndex != mCurrentModeIndex) {
-            // skip modes with different resolutions
-            Mode currentMode = mDisplayModes[mCurrentModeIndex];
-            Mode nextMode = mDisplayModes[modeIndex];
-            if (currentMode.getPhysicalHeight() == nextMode.getPhysicalHeight()
-                    && currentMode.getPhysicalWidth() == nextMode.getPhysicalWidth()) {
-                break;
-            }
-            modeIndex = (modeIndex + 1) % mDisplayModes.length;
-        }
-
         params.preferredDisplayModeId = mDisplayModes[modeIndex].getModeId();
         w.setAttributes(params);
 
@@ -128,23 +98,11 @@ public class TouchLatencyActivity extends Activity {
         mCurrentModeIndex = modeIndex;
     }
 
-    private void changeMultipleDisplays() {
-        Intent intent = new Intent(this, TouchLatencyActivityPresentation.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_LAUNCH_ADJACENT | Intent.FLAG_ACTIVITY_NEW_TASK);
-        ActivityOptions options = ActivityOptions.makeBasic();
-        for (int i = 1; i < mDisplayManager.getDisplays().length; ++i) {
-            // We assume the first display is already displaying the TouchLatencyActivity
-            int displayId = mDisplayManager.getDisplays()[i].getDisplayId();
-            options.setLaunchDisplayId(displayId);
-            intent.putExtra(TouchLatencyActivityPresentation.DISPLAY_ID, displayId);
-            startActivity(intent, options.toBundle());
-        }
-    }
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        Trace.beginSection("TouchLatencyActivity onOptionsItemSelected");
+        Trace.beginSection(
+                "TouchLatencyActivityPresentation::DisplayId::"
+                        + mDisplayId + "  onOptionsItemSelected");
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -160,22 +118,10 @@ public class TouchLatencyActivity extends Activity {
                 changeDisplayMode(item);
                 break;
             }
-            case R.id.multi_display: {
-                changeMultipleDisplays();
-                break;
-            }
         }
 
         Trace.endSection();
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mDisplayManager != null) {
-            mDisplayManager.unregisterDisplayListener(mDisplayListener);
-        }
     }
 
     private TouchLatencyView mTouchView;
