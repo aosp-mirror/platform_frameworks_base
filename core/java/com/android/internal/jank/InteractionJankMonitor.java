24a356607cd3f7acb916a3453aae90c9f73c3db1
Pascal Muetschard <pmuetschard@google.com>
2022-08-17 17:40:30 +0200

    DCL is broken and should not be used.
    Use the acceptable instance holder pattern as a replacement.
    
    Change-Id: I2fb29edd48e00ac36a7fb0907a21ae5ca1d5163f

--- core/java/com/android/internal/jank/InteractionJankMonitor.java
+++ core/java/com/android/internal/jank/InteractionJankMonitor.java
@@ -293,7 +293,10 @@ public class InteractionJankMonitor {
             UIINTERACTION_FRAME_INFO_REPORTED__INTERACTION_TYPE__SHADE_CLEAR_ALL,
     };
 
-    private static volatile InteractionJankMonitor sInstance;
+    private static class InstanceHolder {
+        public static final InteractionJankMonitor INSTANCE =
+            new InteractionJankMonitor(new HandlerThread(DEFAULT_WORKER_NAME));
+    }
 
     private final DeviceConfig.OnPropertiesChangedListener mPropertiesChangedListener =
             this::updateProperties;
@@ -385,15 +388,7 @@ public class InteractionJankMonitor {
      * @return instance of InteractionJankMonitor
      */
     public static InteractionJankMonitor getInstance() {
-        // Use DCL here since this method might be invoked very often.
-        if (sInstance == null) {
-            synchronized (InteractionJankMonitor.class) {
-                if (sInstance == null) {
-                    sInstance = new InteractionJankMonitor(new HandlerThread(DEFAULT_WORKER_NAME));
-                }
-            }
-        }
-        return sInstance;
+        return InstanceHolder.INSTANCE;
     }
 
     /**
