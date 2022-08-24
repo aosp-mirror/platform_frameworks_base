d7b3cdc705b120ec7dadfbb2d16dcabce5128d3d
Nate Myren <ntmyren@google.com>
2022-08-24 16:14:29 +0000

    Revert "Add GET_USAGE_STATS to RUNTIME_AND_APPOP_PERMISSIONS_OPS"
    Revert submission 19682544-usage-stats
    
    Reason for revert: b/243572246
    Bug: 243572246
    Reverted Changes:
    Ib8b1bdff0:Add GET_USAGE_STATS to RUNTIME_AND_APPOP_PERMISSIO...
    I1ac0ca320:Add test for app op permission mappings
    
    Change-Id: I9f2da8d32a88566fc39ad3272fe4caa69d8d5aca

--- core/java/android/app/AppOpsManager.java
+++ core/java/android/app/AppOpsManager.java
@@ -1907,7 +1907,6 @@ public class AppOpsManager {
             OP_SCHEDULE_EXACT_ALARM,
             OP_MANAGE_MEDIA,
             OP_TURN_SCREEN_ON,
-            OP_GET_USAGE_STATS,
     };
 
     /**
