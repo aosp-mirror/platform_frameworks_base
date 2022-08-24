a823b466cc4f69446eb070303a1ee735d40c0c73
Hawkwood Glazier <jglazier@google.com>
2022-08-24 16:24:18 +0000

    [DO NOT MERGE] Additional Tests for DefaultClock
    DefaultClock needed additional units as coverage of much of that class
    was too low. This additionally adds an animation disable flag into
    AnimatableClockView for use by the screenshot test.
    
    Bug: 229771520
    Test: atest DefaultClockProviderTest
    Change-Id: I7d585cedb9b43ac0fb6405e36bec99ee8a246a61

--- packages/SystemUI/shared/src/com/android/systemui/shared/clocks/DefaultClockProvider.kt
+++ packages/SystemUI/shared/src/com/android/systemui/shared/clocks/DefaultClockProvider.kt
@@ -19,6 +19,7 @@ import android.graphics.drawable.Drawable
 import android.icu.text.NumberFormat
 import android.util.TypedValue
 import android.view.LayoutInflater
+import com.android.internal.annotations.VisibleForTesting
 import com.android.systemui.dagger.qualifiers.Main
 import com.android.systemui.plugins.Clock
 import com.android.systemui.plugins.ClockAnimations
@@ -221,7 +222,7 @@ class DefaultClock(
     override fun dump(pw: PrintWriter) = clocks.forEach { it.dump(pw) }
 
     companion object {
-        private const val DOZE_COLOR = Color.WHITE
+        @VisibleForTesting const val DOZE_COLOR = Color.WHITE
         private const val FORMAT_NUMBER = 1234567890
     }
 }
