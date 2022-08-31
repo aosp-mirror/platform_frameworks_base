c0728d68873e3ac0d76242ed53e6617fa6139fba
Eric Laurent <elaurent@google.com>
2022-08-31 18:28:25 +0200

    SpatializerHelper: fix sensor reinitialization
    Force reinitializing the sensors if the headtracking is enabled
    for a given device and headtracker is not available.
    
    Bug: 244430716
    Test: repro steps in bug
    Change-Id: Ic16f5be105ae5a95217bf55c2b454fbbbea54893

--- services/core/java/com/android/server/audio/SpatializerHelper.java
+++ services/core/java/com/android/server/audio/SpatializerHelper.java
@@ -1115,6 +1115,9 @@ public class SpatializerHelper {
                 && ROUTING_DEVICES[0].getAddress().equals(ada.getAddress())) {
             setDesiredHeadTrackingMode(enabled ? mDesiredHeadTrackingModeWhenEnabled
                     : Spatializer.HEAD_TRACKING_MODE_DISABLED);
+            if (enabled && !mHeadTrackerAvailable) {
+                postInitSensors();
+            }
         }
     }
 
