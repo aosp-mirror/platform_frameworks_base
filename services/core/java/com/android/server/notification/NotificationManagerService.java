6fda1c93a25d1012f70c64064a94ad8ee8c24033
Yuri Lin <yurilin@google.com>
2022-09-06 16:14:16 -0400

    Fix system zen rules by using owner package name if caller is system
    Previously were unable to add new zen rules because rules added via the settings pages were getting registered under package "com.android.settings", which then were not considered "system rules". These rules should have package android, so when we can trust the caller (via checking that the caller is system) we should be taking the package name from the owner of the rule.
    
    Bug: 245236706
    Bug: 242537431
    Test: NMSTest; manual
    
    Change-Id: Id69b671592396ac3304862dadbe73de328a8e27a
    Merged-In: Id69b671592396ac3304862dadbe73de328a8e27a
    (cherry picked from commit d9ead5f42c251c5ac16e0c4ef308402e650112f7)
    Merged-In: Id69b671592396ac3304862dadbe73de328a8e27a

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4774,7 +4774,16 @@ public class NotificationManagerService extends SystemService {
             }
             enforcePolicyAccess(Binder.getCallingUid(), "addAutomaticZenRule");
 
-            return mZenModeHelper.addAutomaticZenRule(pkg, automaticZenRule,
+            // If the caller is system, take the package name from the rule's owner rather than
+            // from the caller's package.
+            String rulePkg = pkg;
+            if (isCallingUidSystem()) {
+                if (automaticZenRule.getOwner() != null) {
+                    rulePkg = automaticZenRule.getOwner().getPackageName();
+                }
+            }
+
+            return mZenModeHelper.addAutomaticZenRule(rulePkg, automaticZenRule,
                     "addAutomaticZenRule");
         }
 
