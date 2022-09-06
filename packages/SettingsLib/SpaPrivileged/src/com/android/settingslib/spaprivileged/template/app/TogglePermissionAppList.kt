586abf6b7b84dd4682fad3234b4822597db5d329
Chaohui Wang <chaohuiw@google.com>
2022-09-06 13:37:25 +0800

    Add doc for AppListModel
    Also for TogglePermissionAppListModel.
    
    Bug: 235727273
    Test: TAP
    Change-Id: I955dc5b25c638bed3b4d9932fcd7e5768a03e987

--- packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/TogglePermissionAppList.kt
+++ packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/TogglePermissionAppList.kt
@@ -29,21 +29,51 @@ import com.android.settingslib.spa.widget.preference.PreferenceModel
 import com.android.settingslib.spaprivileged.model.app.AppRecord
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Implement this interface to build an App List which toggles a permission on / off.
+ */
 interface TogglePermissionAppListModel<T : AppRecord> {
     val pageTitleResId: Int
     val switchTitleResId: Int
     val footerResId: Int
 
+    /**
+     * Loads the extra info for the App List, and generates the [AppRecord] List.
+     *
+     * Default is implemented by [transformItem]
+     */
     fun transform(userIdFlow: Flow<Int>, appListFlow: Flow<List<ApplicationInfo>>): Flow<List<T>> =
         appListFlow.asyncMapItem(::transformItem)
 
+    /**
+     * Loads the extra info for one app, and generates the [AppRecord].
+     *
+     * This must be implemented, because when show the App Info page for single app, this will be
+     * used instead of [transform].
+     */
     fun transformItem(app: ApplicationInfo): T
+
+    /**
+     * Filters the [AppRecord] list.
+     *
+     * @return the [AppRecord] list which will be displayed.
+     */
     fun filter(userIdFlow: Flow<Int>, recordListFlow: Flow<List<T>>): Flow<List<T>>
 
+    /**
+     * Gets whether the permission is allowed for the given app.
+     */
     @Composable
     fun isAllowed(record: T): State<Boolean?>
 
+    /**
+     * Gets whether the permission on / off is changeable for the given app.
+     */
     fun isChangeable(record: T): Boolean
+
+    /**
+     * Sets whether the permission is allowed for the given app.
+     */
     fun setAllowed(record: T, newAllowed: Boolean)
 }
 
