b6faae9f7d1a9af3a4c548c5e01f72a53d1404ea
Sarp Misoglu <sarpm@google.com>
2022-09-15 13:37:53 +0100

    Move transport client code back to the main target
    In ag/9433204 this code was separated into its own library for a feature
    that was cancelled. It doesn't make sense to keep it separate anymore.
    
    Test: m
      atest BackupTransportClientTest.java
      atest CtsBackupHostTestCases
    Bug: 210083465
    Change-Id: I6b5a6bb45563c16b359f0281aac2c28bce03a6fd

--- services/backup/java/com/android/server/backup/transport/BackupTransportClient.java
+++ services/backup/java/com/android/server/backup/transport/BackupTransportClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2022 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -376,7 +376,7 @@ public class BackupTransportClient {
         try {
             return future.get(600, TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException
-                | CancellationException e) {
+                 | CancellationException e) {
             Slog.w(TAG, "Failed to get result from transport:", e);
             return null;
         } finally {
