e275759b5422f9a4c2e4f769a1d7e9cf4123f7c1
Chaohui Wang <chaohuiw@google.com>
2022-09-16 19:20:34 +0800

    Add entry item for TogglePermissionAppInfoPage
    To support adding items like "Install unknown apps" to App Settings
    page.
    
    Item will only displayed it's changeable for that app.
    
    Bug: 236346018
    Test: Manual with Settings App
    Change-Id: Icf68e0eab0daf056ab1c30c8cebd4784aeb6a3a9

--- packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
+++ packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/AppInfo.kt
@@ -17,6 +17,8 @@
 package com.android.settingslib.spaprivileged.template.app
 
 import android.content.pm.ApplicationInfo
+import android.content.pm.PackageInfo
+import android.text.BidiFormatter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -25,49 +27,60 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.android.settingslib.spa.framework.compose.rememberDrawablePainter
 import com.android.settingslib.spa.framework.theme.SettingsDimension
 import com.android.settingslib.spa.widget.ui.SettingsBody
 import com.android.settingslib.spa.widget.ui.SettingsTitle
-import com.android.settingslib.spaprivileged.model.app.PackageManagers
+import com.android.settingslib.spaprivileged.R
 import com.android.settingslib.spaprivileged.model.app.rememberAppRepository
 
-@Composable
-fun AppInfo(packageName: String, userId: Int) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                horizontal = SettingsDimension.itemPaddingStart,
-                vertical = SettingsDimension.itemPaddingVertical,
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        val packageInfo =
-            remember { PackageManagers.getPackageInfoAsUser(packageName, userId) } ?: return
-        Box(modifier = Modifier.padding(SettingsDimension.itemPaddingAround)) {
-            AppIcon(app = packageInfo.applicationInfo, size = SettingsDimension.appIconInfoSize)
+class AppInfoProvider(private val packageInfo: PackageInfo) {
+    @Composable
+    fun AppInfo(displayVersion: Boolean = false) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = SettingsDimension.itemPaddingStart,
+                    vertical = SettingsDimension.itemPaddingVertical,
+                ),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(modifier = Modifier.padding(SettingsDimension.itemPaddingAround)) {
+                AppIcon(app = packageInfo.applicationInfo, size = SettingsDimension.appIconInfoSize)
+            }
+            AppLabel(packageInfo.applicationInfo)
+            if (displayVersion) AppVersion()
         }
-        AppLabel(packageInfo.applicationInfo)
-        AppVersion(packageInfo.versionName)
+    }
+
+    @Composable
+    private fun AppVersion() {
+        if (packageInfo.versionName == null) return
+        Spacer(modifier = Modifier.height(4.dp))
+        SettingsBody(packageInfo.versionName)
+    }
+
+    @Composable
+    fun FooterAppVersion() {
+        if (packageInfo.versionName == null) return
+        Divider()
+        Box(modifier = Modifier.padding(SettingsDimension.itemPadding)) {
+            val versionName = BidiFormatter.getInstance().unicodeWrap(packageInfo.versionName)
+            SettingsBody(stringResource(R.string.version_text, versionName))
+        }
     }
 }
 
 @Composable
-private fun AppVersion(versionName: String?) {
-    if (versionName == null) return
-    Spacer(modifier = Modifier.height(4.dp))
-    SettingsBody(versionName)
-}
-
-@Composable
-fun AppIcon(app: ApplicationInfo, size: Dp) {
+internal fun AppIcon(app: ApplicationInfo, size: Dp) {
     val appRepository = rememberAppRepository()
     Image(
         painter = rememberDrawablePainter(appRepository.produceIcon(app).value),
@@ -77,7 +90,7 @@ fun AppIcon(app: ApplicationInfo, size: Dp) {
 }
 
 @Composable
-fun AppLabel(app: ApplicationInfo) {
+internal fun AppLabel(app: ApplicationInfo) {
     val appRepository = rememberAppRepository()
     SettingsTitle(appRepository.produceLabel(app))
 }
