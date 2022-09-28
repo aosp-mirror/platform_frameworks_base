4565e99d60f668a5a7b7c53c297de045efe47746
Louis Chang <louischang@google.com>
2022-09-28 06:46:29 +0000

    [RESTRICT AUTOMERGE] Trim the activity info of another uid if no privilege
    The activity info could be from another uid which is different
    from the app that hosts the task. The information should be
    trimmed if the caller app doesn't have the privilege.
    
    Bug: 243130512
    Test: verified locally
    Test: atest RecentTasksTest
    Change-Id: Ia343ac70e5bb9aeae718fca6674e1ca491a14512
    (cherry picked from commit 401e782b244bf84fd5aab371f60c2e52d6226fb3)
    (cherry picked from commit 17df433a3d96fb6c00dc7078fd0f8048645ace6a)
    Merged-In: Ia343ac70e5bb9aeae718fca6674e1ca491a14512

--- services/core/java/com/android/server/wm/RecentTasks.java
+++ services/core/java/com/android/server/wm/RecentTasks.java
@@ -961,7 +961,7 @@ class RecentTasks {
                 continue;
             }
 
-            res.add(createRecentTaskInfo(task, true /* stripExtras */));
+            res.add(createRecentTaskInfo(task, true /* stripExtras */, getTasksAllowed));
         }
         return res;
     }
@@ -1833,12 +1833,16 @@ class RecentTasks {
     /**
      * Creates a new RecentTaskInfo from a Task.
      */
-    ActivityManager.RecentTaskInfo createRecentTaskInfo(Task tr, boolean stripExtras) {
+    ActivityManager.RecentTaskInfo createRecentTaskInfo(Task tr, boolean stripExtras,
+            boolean getTasksAllowed) {
         ActivityManager.RecentTaskInfo rti = new ActivityManager.RecentTaskInfo();
         tr.fillTaskInfo(rti, stripExtras);
         // Fill in some deprecated values
         rti.id = rti.isRunning ? rti.taskId : INVALID_TASK_ID;
         rti.persistentId = rti.taskId;
+        if (!getTasksAllowed) {
+            Task.trimIneffectiveInfo(tr, rti);
+        }
         return rti;
     }
 
