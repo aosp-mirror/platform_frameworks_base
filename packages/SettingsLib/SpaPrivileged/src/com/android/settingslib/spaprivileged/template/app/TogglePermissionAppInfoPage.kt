18a595d08eb2d9dc083a38451e44d8941d9dc679
Zekan Qian <pierreqian@google.com>
2022-09-30 11:44:40 +0800

    Improve runtime parameter support. 1. Define runtime parameter as prefixed by "rt_" 2. Move entry highlight parameter into intent as Extra, instead of a runtime parameter.
    Bug: 244122804
    Test: manual - build Gallery
    Change-Id: Ic8945d32868173f30c31afd5fe54b2ba12fad6e4

--- packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/TogglePermissionAppInfoPage.kt
+++ packages/SettingsLib/SpaPrivileged/src/com/android/settingslib/spaprivileged/template/app/TogglePermissionAppInfoPage.kt
@@ -46,8 +46,8 @@ import kotlinx.coroutines.Dispatchers
 
 private const val ENTRY_NAME = "AllowControl"
 private const val PERMISSION = "permission"
-private const val PACKAGE_NAME = "packageName"
-private const val USER_ID = "userId"
+private const val PACKAGE_NAME = "rt_packageName"
+private const val USER_ID = "rt_userId"
 private const val PAGE_NAME = "TogglePermissionAppInfoPage"
 private val PAGE_PARAMETER = listOf(
     navArgument(PERMISSION) { type = NavType.StringType },
