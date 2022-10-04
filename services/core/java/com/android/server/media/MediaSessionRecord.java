e50b9c899341fbeba2858cb8a5ebaf7d82e41625
Iván Budnik <ivanbuper@google.com>
2022-10-04 15:52:30 +0000

    Enforce MediaButtonReceiver extracted component name matches session package name
    This change makes sure that the extracted component name in a
    MediaButtonReceiverHolder matches the Media Session owner's package
    name. This avoids incorrectly routing media button events and potential
    security issues.
    
    Bug: 244312001
    Bug: 238177121
    Test: atest CtsMediaBetterTogetherTestCases
    Change-Id: Ifac9cf53889222e31d18c14c1e096ee68c0a346c
    (cherry picked from commit 185c3e252397bfa37592edbb5b2f5ae97db92eda)
    Merged-In: Ifac9cf53889222e31d18c14c1e096ee68c0a346c
    (cherry picked from commit 48c388277880e56ab5cc29e145e4d00aa383ce01)
    Merged-In: Ifac9cf53889222e31d18c14c1e096ee68c0a346c

--- services/core/java/com/android/server/media/MediaSessionRecord.java
+++ services/core/java/com/android/server/media/MediaSessionRecord.java
@@ -936,8 +936,7 @@ public class MediaSessionRecord implements IBinder.DeathRecipient, MediaSessionR
         }
 
         @Override
-        public void setMediaButtonReceiver(PendingIntent pi, String sessionPackageName)
-                throws RemoteException {
+        public void setMediaButtonReceiver(PendingIntent pi) throws RemoteException {
             final long token = Binder.clearCallingIdentity();
             try {
                 if ((mPolicies & MediaSessionPolicyProvider.SESSION_POLICY_IGNORE_BUTTON_RECEIVER)
@@ -945,7 +944,7 @@ public class MediaSessionRecord implements IBinder.DeathRecipient, MediaSessionR
                     return;
                 }
                 mMediaButtonReceiverHolder =
-                        MediaButtonReceiverHolder.create(mContext, mUserId, pi, sessionPackageName);
+                        MediaButtonReceiverHolder.create(mUserId, pi, mPackageName);
                 mService.onMediaButtonReceiverChanged(MediaSessionRecord.this);
             } finally {
                 Binder.restoreCallingIdentity(token);
