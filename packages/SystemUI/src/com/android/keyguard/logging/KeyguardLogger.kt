2455184cfedaf66ab81274686bbe4b4f7fe38030
Michał Brzeziński <brzezinski@google.com>
2022-10-05 16:57:05 +0000

    Adding class comment and license info to KeyguardLogger
    Bug: 237743330
    Change-Id: I2b3c62d3cdc81d5284d74f71fd30f11231c18788
    Test: no need

--- packages/SystemUI/src/com/android/keyguard/logging/KeyguardLogger.kt
+++ packages/SystemUI/src/com/android/keyguard/logging/KeyguardLogger.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.android.keyguard.logging
 
 import com.android.systemui.log.LogBuffer
@@ -14,6 +30,11 @@ import javax.inject.Inject
 
 private const val TAG = "KeyguardLog"
 
+/**
+ * Generic logger for keyguard that's wrapping [LogBuffer]. This class should be used for adding
+ * temporary logs or logs for smaller classes when creating whole new [LogBuffer] wrapper might be
+ * an overkill.
+ */
 class KeyguardLogger @Inject constructor(@KeyguardLog private val buffer: LogBuffer) {
     fun d(@CompileTimeConstant msg: String) = log(msg, DEBUG)
 
