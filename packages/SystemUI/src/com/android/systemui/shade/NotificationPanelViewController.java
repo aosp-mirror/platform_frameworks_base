4be95bac2eb1f9ec1122aa34ae87993f6d33f62e
Chandru <chandruis@google.com>
2022-10-11 12:29:13 +0000

    Fix lockscreen not bouncing up when face auth is running or locked out
    Earlier, either face auth will run on tapping lockscreen or keyguard
    will bounce up
    
    Bug: 247694441
    Fixes: 248044652
    Test: atest NotificationPanelViewControllerTest
    
    Change-Id: I05ed138755ceca51d2a2b5ce5ae17d8a2fa7ff6e

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -3872,17 +3872,17 @@ public final class NotificationPanelViewController {
         switch (mBarState) {
             case KEYGUARD:
                 if (!mDozingOnDown) {
-                    if (mUpdateMonitor.isFaceEnrolled()
-                            && !mUpdateMonitor.isFaceDetectionRunning()
-                            && !mUpdateMonitor.getUserCanSkipBouncer(
-                            KeyguardUpdateMonitor.getCurrentUser())) {
-                        mUpdateMonitor.requestFaceAuth(true,
-                                FaceAuthApiRequestReason.NOTIFICATION_PANEL_CLICKED);
-                    } else {
-                        mLockscreenGestureLogger.write(MetricsEvent.ACTION_LS_HINT,
-                                0 /* lengthDp - N/A */, 0 /* velocityDp - N/A */);
-                        mLockscreenGestureLogger
-                                .log(LockscreenUiEvent.LOCKSCREEN_LOCK_SHOW_HINT);
+                    mShadeLog.v("onMiddleClicked on Keyguard, mDozingOnDown: false");
+                    // Try triggering face auth, this "might" run. Check
+                    // KeyguardUpdateMonitor#shouldListenForFace to see when face auth won't run.
+                    mUpdateMonitor.requestFaceAuth(true,
+                            FaceAuthApiRequestReason.NOTIFICATION_PANEL_CLICKED);
+
+                    mLockscreenGestureLogger.write(MetricsEvent.ACTION_LS_HINT,
+                            0 /* lengthDp - N/A */, 0 /* velocityDp - N/A */);
+                    mLockscreenGestureLogger
+                            .log(LockscreenUiEvent.LOCKSCREEN_LOCK_SHOW_HINT);
+                    if (!mUpdateMonitor.isFaceDetectionRunning()) {
                         startUnlockHintAnimation();
                     }
                     if (mUpdateMonitor.isFaceEnrolled()) {
