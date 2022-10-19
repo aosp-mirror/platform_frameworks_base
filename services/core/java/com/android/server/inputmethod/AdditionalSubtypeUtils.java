8457cc2b438a8322f951461cbf16dae04d30e7d9
Shu Chen <shuchen@google.com>
2022-10-19 15:35:45 +0800

    Adds the new API - InputMethodSubtypeBuilder#setSubtypeNameOverride.
    So the IME can use it to provide display names for additional subtypes.
    
    Bug: 253531463
    Test: Locally verified with Gboard.
    
    Change-Id: Ia8c5654f92519ad39b54086b093fd97cc242e0cd

--- services/core/java/com/android/server/inputmethod/AdditionalSubtypeUtils.java
+++ services/core/java/com/android/server/inputmethod/AdditionalSubtypeUtils.java
@@ -57,6 +57,7 @@ final class AdditionalSubtypeUtils {
     private static final String NODE_IMI = "imi";
     private static final String ATTR_ID = "id";
     private static final String ATTR_LABEL = "label";
+    private static final String ATTR_NAME_OVERRIDE = "nameOverride";
     private static final String ATTR_ICON = "icon";
     private static final String ATTR_IME_SUBTYPE_ID = "subtypeId";
     private static final String ATTR_IME_SUBTYPE_LOCALE = "imeSubtypeLocale";
@@ -160,6 +161,7 @@ final class AdditionalSubtypeUtils {
                     }
                     out.attributeInt(null, ATTR_ICON, subtype.getIconResId());
                     out.attributeInt(null, ATTR_LABEL, subtype.getNameResId());
+                    out.attribute(null, ATTR_NAME_OVERRIDE, subtype.getNameOverride().toString());
                     out.attribute(null, ATTR_IME_SUBTYPE_LOCALE, subtype.getLocale());
                     out.attribute(null, ATTR_IME_SUBTYPE_LANGUAGE_TAG,
                             subtype.getLanguageTag());
@@ -242,6 +244,8 @@ final class AdditionalSubtypeUtils {
                     }
                     final int icon = parser.getAttributeInt(null, ATTR_ICON);
                     final int label = parser.getAttributeInt(null, ATTR_LABEL);
+                    final String untranslatableName = parser.getAttributeValue(null,
+                            ATTR_NAME_OVERRIDE);
                     final String imeSubtypeLocale =
                             parser.getAttributeValue(null, ATTR_IME_SUBTYPE_LOCALE);
                     final String languageTag =
@@ -257,6 +261,7 @@ final class AdditionalSubtypeUtils {
                     final InputMethodSubtype.InputMethodSubtypeBuilder
                             builder = new InputMethodSubtype.InputMethodSubtypeBuilder()
                             .setSubtypeNameResId(label)
+                            .setSubtypeNameOverride(untranslatableName)
                             .setSubtypeIconResId(icon)
                             .setSubtypeLocale(imeSubtypeLocale)
                             .setLanguageTag(languageTag)
