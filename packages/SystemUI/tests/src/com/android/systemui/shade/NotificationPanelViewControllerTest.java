e929d785a2e8de6f9b519888a06e6ba8aee52065
Jeff DeCew <jeffdq@google.com>
2022-10-20 20:22:53 +0000

    Re-land "Dump NotificationWakeUpCoordinator state to bugreports."
    This reverts commit e50e2c4875f33e2df54003351713fac8810e5896.
    
    Reason for revert: b/241591738#comment11 -- Previously reverted CL was not the true cause of the regression.
    
    Bug: 234979264
    Test: dumpsysui NotificationWakeUpCoordinator
    Change-Id: I21bab10d62fbdf8f3bab63fbfd255dabc2749c55

--- packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/shade/NotificationPanelViewControllerTest.java
@@ -377,6 +377,7 @@ public class NotificationPanelViewControllerTest extends SysuiTestCase {
 
         NotificationWakeUpCoordinator coordinator =
                 new NotificationWakeUpCoordinator(
+                        mDumpManager,
                         mock(HeadsUpManagerPhone.class),
                         new StatusBarStateControllerImpl(new UiEventLoggerFake(), mDumpManager,
                                 mInteractionJankMonitor),
