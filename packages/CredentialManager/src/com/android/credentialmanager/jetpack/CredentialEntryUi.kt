ab4b22a3b58e1f6c9997f095ea47d5df47e47a3e
Helen Qin <helenqin@google.com>
2022-10-21 01:54:36 +0000

    Define all the entry-level display info in Entry.
    Note that the UI has not started using the
    new jetpack entry classes (defined under
    packages/CredentialManager/src/com/android/credentialmanager/jetpack).
    Next, the existing *UI.kt files under
    packages/CredentialManager/src/com/android/credentialmanager needs to be
    removed and the UI begin adopting these new structures.
    
    Test: deployed locally
    Bug: 247855226
    Bug: 253156958
    Change-Id: I35c48a4a224fad019efc10b841a1910ae940185c

---
+++ packages/CredentialManager/src/com/android/credentialmanager/jetpack/CredentialEntryUi.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.credentialmanager.jetpack
+
+import android.app.slice.Slice
+import android.graphics.drawable.Icon
+
+/**
+ * UI representation for a credential entry used during the get credential flow.
+ *
+ * TODO: move to jetpack.
+ */
+abstract class CredentialEntryUi(
+  val credentialTypeIcon: Icon,
+  val profileIcon: Icon?,
+  val lastUsedTimeMillis: Long?,
+  val note: CharSequence?,
+) {
+  companion object {
+    fun fromSlice(slice: Slice): CredentialEntryUi {
+      return when (slice.spec?.type) {
+        TYPE_PUBLIC_KEY_CREDENTIAL -> PasskeyCredentialEntryUi.fromSlice(slice)
+        TYPE_PASSWORD_CREDENTIAL -> PasswordCredentialEntryUi.fromSlice(slice)
+        else -> throw IllegalArgumentException("Unexpected type: ${slice.spec?.type}")
+      }
+    }
+
+    const val TYPE_PUBLIC_KEY_CREDENTIAL: String =
+      "androidx.credentials.TYPE_PUBLIC_KEY_CREDENTIAL"
+    const val TYPE_PASSWORD_CREDENTIAL: String = "androidx.credentials.TYPE_PASSWORD"
+  }
+}
