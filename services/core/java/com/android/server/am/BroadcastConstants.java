cd334a0864b2b77b8c70c404f5baf9e56dae299c
Jeff Sharkey <jsharkey@android.com>
2022-10-21 16:06:59 -0600

    Enable "modern" BroadcastQueue by default; take three.
    We've completed several weeks of teamfooding which has helped us
    gain confidence in the "modern" queue, so let's pivot it enabling it
    by default.
    
    Bug: 253906105
    Test: atest FrameworksMockingServicesTests:BroadcastRecordTest
    Test: atest FrameworksMockingServicesTests:BroadcastQueueTest
    Test: atest FrameworksMockingServicesTests:BroadcastQueueModernImplTest
    Change-Id: Ic51abda906a37c0fa19729e45ec1d8e35fcc95b6

--- services/core/java/com/android/server/am/BroadcastConstants.java
+++ services/core/java/com/android/server/am/BroadcastConstants.java
@@ -133,7 +133,7 @@ public class BroadcastConstants {
      */
     public boolean MODERN_QUEUE_ENABLED = DEFAULT_MODERN_QUEUE_ENABLED;
     private static final String KEY_MODERN_QUEUE_ENABLED = "modern_queue_enabled";
-    private static final boolean DEFAULT_MODERN_QUEUE_ENABLED = false;
+    private static final boolean DEFAULT_MODERN_QUEUE_ENABLED = true;
 
     /**
      * For {@link BroadcastQueueModernImpl}: Maximum number of process queues to
