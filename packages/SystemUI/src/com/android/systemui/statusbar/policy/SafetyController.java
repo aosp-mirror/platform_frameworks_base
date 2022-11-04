38f3f8f21e1aded7231a4f482baac885a5e15656
Michael Groover <mpgroover@google.com>
2022-11-04 11:34:37 -0500

    Remove unaudited flag from SafetyController receiver
    The package change receiver in SafetyController was updated with an
    unaudited exported flag to ensure the receiver meets the new
    requirement that apps targeting U+ and registering a receiver for
    non-system broadcasts must have a flag indicating the exported state
    of the receiver. This receiver may have been incorrectly reported
    as requiring a flag due to it including a data scheme; since this
    receiver is registering for the system broadcast PACKAGE_CHANGED,
    a flag is not required.
    
    Fixes: 257410439
    Test: atest SystemUITests:com.android.systemui.statusbar.policy.\
      SafetyControllerTest#addingFirstListenerRegistersReceiver
    Change-Id: Ia63eed1761aec7b0b7a4781f9ad6d6e995a625a2

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/SafetyController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/SafetyController.java
@@ -78,8 +78,7 @@ public class SafetyController implements
         synchronized (mListeners) {
             mListeners.add(listener);
             if (mListeners.size() == 1) {
-                mContext.registerReceiver(mPermControllerChangeReceiver, PKG_CHANGE_INTENT_FILTER,
-                        Context.RECEIVER_EXPORTED_UNAUDITED);
+                mContext.registerReceiver(mPermControllerChangeReceiver, PKG_CHANGE_INTENT_FILTER);
                 mBgHandler.post(() -> {
                     mSafetyCenterEnabled = mSafetyCenterManager.isSafetyCenterEnabled();
                     listener.onSafetyCenterEnableChanged(isSafetyCenterEnabled());
