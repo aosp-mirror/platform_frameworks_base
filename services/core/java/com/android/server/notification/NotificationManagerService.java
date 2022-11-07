2772a3371451697f7e650394ebac67febe664ff4
Yuri Lin <yurilin@google.com>
2022-11-07 19:07:55 +0000

    [DO NOT MERGE] Revert "Fix system zen rules by using owner package name if caller is system"
    This reverts commit 2ceaaebb3cdcba2fba8a293492b3c50e51004cf6.
    
    Reason for revert: broke DND schedules in multi-user mode b/257477671
    
    Change-Id: I5b0b521c15a86a67a7aad8692a532026d2670088
    (cherry picked from commit 9631f58f21fbe10572687c85b6cbc0d3ddda1452)
    Merged-In: I5b0b521c15a86a67a7aad8692a532026d2670088

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -4486,16 +4486,7 @@ public class NotificationManagerService extends SystemService {
             }
             enforcePolicyAccess(Binder.getCallingUid(), "addAutomaticZenRule");
 
-            // If the caller is system, take the package name from the rule's owner rather than
-            // from the caller's package.
-            String rulePkg = pkg;
-            if (isCallingUidSystem()) {
-                if (automaticZenRule.getOwner() != null) {
-                    rulePkg = automaticZenRule.getOwner().getPackageName();
-                }
-            }
-
-            return mZenModeHelper.addAutomaticZenRule(rulePkg, automaticZenRule,
+            return mZenModeHelper.addAutomaticZenRule(pkg, automaticZenRule,
                     "addAutomaticZenRule");
         }
 
