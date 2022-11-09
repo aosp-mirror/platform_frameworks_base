5fde7717bce2e98b657b049d9ae07bbe326b4ebc
Sarah Chin <sarahchin@google.com>
2022-11-08 22:04:28 -0800

    Add toString for RadioAccessSpecifier
    Test: atest TelephonyManagerTest
    Bug: 255821693
    Change-Id: I9a1d6d541bbb64620d1f2f108b659af1aee68742

--- telephony/java/android/telephony/RadioAccessSpecifier.java
+++ telephony/java/android/telephony/RadioAccessSpecifier.java
@@ -161,9 +161,17 @@ public final class RadioAccessSpecifier implements Parcelable {
     }
 
     @Override
-    public int hashCode () {
+    public int hashCode() {
         return ((mRadioAccessNetwork * 31)
                 + (Arrays.hashCode(mBands) * 37)
                 + (Arrays.hashCode(mChannels)) * 39);
+    }
+
+    @Override
+    public String toString() {
+        return "RadioAccessSpecifier[mRadioAccessNetwork="
+                + AccessNetworkConstants.AccessNetworkType.toString(mRadioAccessNetwork)
+                + ", mBands=" + Arrays.toString(mBands)
+                + ", mChannels=" + Arrays.toString(mChannels) + "]";
     }
 }
