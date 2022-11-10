15a6f672ddf06e67cf946f89000afcab054257c9
Nikhil Kumar <nykkumar@google.com>
2022-11-10 14:26:58 +0000

    Allow admin users to have bugreport icon in the power menu
    Existing implementation only allows primary user to have bugreport
    icon in power menu (options displayed on device power off shortcut)
    
    With headless implementation primary/system (user 0) user will not
    be visible and there will be secondary admin users which are
    allowed to take bugreports.
    
    Refactor the implementation to allow admin users to have
    bug report icon in the power menu.
    
    Test: manually checked icon is visible on power menu for admin user
    
    Bug: 258647848
    Change-Id: I60a5e1aec3cd463bc085ec899e00564fe11a8371

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
@@ -733,7 +733,7 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
     @VisibleForTesting
     boolean shouldDisplayBugReport(UserInfo currentUser) {
         return mGlobalSettings.getInt(Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0
-                && (currentUser == null || currentUser.isPrimary());
+                && (currentUser == null || currentUser.isAdmin());
     }
 
     @Override
@@ -1058,8 +1058,9 @@ public class GlobalActionsDialogLite implements DialogInterface.OnDismissListene
 
         @Override
         public boolean showBeforeProvisioning() {
-            return Build.isDebuggable() && mGlobalSettings.getInt(
-                    Settings.Global.BUGREPORT_IN_POWER_MENU, 0) != 0;
+            return Build.isDebuggable() && mGlobalSettings.getIntForUser(
+                    Settings.Global.BUGREPORT_IN_POWER_MENU, 0, getCurrentUser().id) != 0
+                    && getCurrentUser().isAdmin();
         }
     }
 
