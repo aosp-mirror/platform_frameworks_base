d9b11fe421751469f66c5dc37e9779544560b1bd
Josh Tsuji <tsuji@google.com>
2022-11-11 14:35:04 -0500

    Update scrim controller directly with occlude animation status.
    updateIsKeyguard() short circuits in some cases which was preventing
    the scrim state from updating after the occlude animation ends. This
    fixes that by delivering the occlusion animation status directly
    (similar to clipsQsScrim and other params in ScrimController) and
    updating the scrims.
    
    Fixes: 258700577
    Test: launch camera in both light and dark mode
    Test: pull down shade before/after launch to confirm notif scrim is not stuck
    Change-Id: Ib3762df8d51f9347c56347cc235b8c97637991c0

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -137,6 +137,7 @@ import com.android.systemui.statusbar.phone.CentralSurfaces;
 import com.android.systemui.statusbar.phone.DozeParameters;
 import com.android.systemui.statusbar.phone.KeyguardBypassController;
 import com.android.systemui.statusbar.phone.ScreenOffAnimationController;
+import com.android.systemui.statusbar.phone.ScrimController;
 import com.android.systemui.statusbar.policy.KeyguardStateController;
 import com.android.systemui.statusbar.policy.UserSwitcherController;
 import com.android.systemui.util.DeviceConfigProxy;
@@ -851,6 +852,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                 @Override
                 public void onLaunchAnimationStart(boolean isExpandingFullyAbove) {
                     mOccludeAnimationPlaying = true;
+                    mScrimControllerLazy.get().setOccludeAnimationPlaying(true);
                 }
 
                 @Override
@@ -861,6 +863,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
                     // Ensure keyguard state is set correctly if we're cancelled.
                     mCentralSurfaces.updateIsKeyguard();
+                    mScrimControllerLazy.get().setOccludeAnimationPlaying(false);
                 }
 
                 @Override
@@ -874,6 +877,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                     // Hide the keyguard now that we're done launching the occluding activity over
                     // it.
                     mCentralSurfaces.updateIsKeyguard();
+                    mScrimControllerLazy.get().setOccludeAnimationPlaying(false);
 
                     mInteractionJankMonitor.end(CUJ_LOCKSCREEN_OCCLUSION);
                 }
@@ -1131,6 +1135,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
     private ScreenOnCoordinator mScreenOnCoordinator;
 
     private Lazy<ActivityLaunchAnimator> mActivityLaunchAnimator;
+    private Lazy<ScrimController> mScrimControllerLazy;
 
     /**
      * Injected constructor. See {@link KeyguardModule}.
@@ -1161,7 +1166,8 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             DreamOverlayStateController dreamOverlayStateController,
             Lazy<ShadeController> shadeControllerLazy,
             Lazy<NotificationShadeWindowController> notificationShadeWindowControllerLazy,
-            Lazy<ActivityLaunchAnimator> activityLaunchAnimator) {
+            Lazy<ActivityLaunchAnimator> activityLaunchAnimator,
+            Lazy<ScrimController> scrimControllerLazy) {
         mContext = context;
         mUserTracker = userTracker;
         mFalsingCollector = falsingCollector;
@@ -1206,6 +1212,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         mDreamOverlayStateController = dreamOverlayStateController;
 
         mActivityLaunchAnimator = activityLaunchAnimator;
+        mScrimControllerLazy = scrimControllerLazy;
 
         mPowerButtonY = context.getResources().getDimensionPixelSize(
                 R.dimen.physical_power_button_center_screen_location_y);
