a9ee1ca5c4a4bab197d5a73cfb296ddebb702a73
Zekan Qian <pierreqian@google.com>
2022-11-25 11:23:09 +0800

    Move EntryHighlight to framework/util
    Bug: 244122804
    Test: unit-test & local build gallery
    Change-Id: I0756816a3893fffb839844b2baf1216843258380

--- packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/framework/util/EntryHighlight.kt
+++ packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/framework/util/EntryHighlight.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.settingslib.spa.widget.util
+package com.android.settingslib.spa.framework.util
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.RepeatMode
