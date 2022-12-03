fa4b2dc6d8f75b84e398ef2db8134ce51fc80001
Pinyao Ting <pinyaoting@google.com>
2022-12-02 17:07:51 -0800

    Implement a global maximum on number of shortcuts an app can publish
    Currently there is a limit on the number of shortcuts an app can publish
    in respect to each launcher activity. This CL further implements a
    global maximum of total number of shortcuts an app can publish to
    mitigate from any potential system health issue.
    
    Bug: 250576066
    Test: manual
    Change-Id: I94b4e39475c7585ec67f15df1256fdf0b65f3bec

--- services/core/java/com/android/server/pm/ShortcutPackage.java
+++ services/core/java/com/android/server/pm/ShortcutPackage.java
@@ -1470,9 +1470,15 @@ class ShortcutPackage extends ShortcutPackageItem {
         }
 
         // Then make sure none of the activities have more than the max number of shortcuts.
+        int total = 0;
         for (int i = counts.size() - 1; i >= 0; i--) {
-            service.enforceMaxActivityShortcuts(counts.valueAt(i));
+            int count = counts.valueAt(i);
+            service.enforceMaxActivityShortcuts(count);
+            total += count;
         }
+
+        // Finally make sure that the app doesn't have more than the max number of shortcuts.
+        service.enforceMaxAppShortcuts(total);
     }
 
     /**
