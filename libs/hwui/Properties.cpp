49602f5ebf903fa8709dbbbe34ed73244f85450c
Matt Buckley <mattbuckley@google.com>
2022-12-06 22:03:30 +0000

    Disable HWUI adpf by default and enable in device.mk
     * Make HWUI disabled by default for vendors
     * Explicitly enable in supported devices
     * Consolidate flags for HWUI and SF
    
    Bug: b/261628396
    Test: manual
    Change-Id: I2d6b53459e4f9643e6b0bf2ba81eb4db36fb94b4

--- libs/hwui/Properties.cpp
+++ libs/hwui/Properties.cpp
@@ -82,7 +82,7 @@ bool Properties::isolatedProcess = false;
 int Properties::contextPriority = 0;
 float Properties::defaultSdrWhitePoint = 200.f;
 
-bool Properties::useHintManager = true;
+bool Properties::useHintManager = false;
 int Properties::targetCpuTimePercentage = 70;
 
 bool Properties::enableWebViewOverlays = true;
@@ -142,7 +142,7 @@ bool Properties::load() {
 
     runningInEmulator = base::GetBoolProperty(PROPERTY_IS_EMULATOR, false);
 
-    useHintManager = base::GetBoolProperty(PROPERTY_USE_HINT_MANAGER, true);
+    useHintManager = base::GetBoolProperty(PROPERTY_USE_HINT_MANAGER, false);
     targetCpuTimePercentage = base::GetIntProperty(PROPERTY_TARGET_CPU_TIME_PERCENTAGE, 70);
     if (targetCpuTimePercentage <= 0 || targetCpuTimePercentage > 100) targetCpuTimePercentage = 70;
 
