bf40e574d3061c9c5d72060fa22b152fc0c08500
Florence Yang <florenceyang@google.com>
2022-12-10 02:08:29 +0000

    Register Dumpables for Region Sampler
    Register dumpables where Region Sampler is implemented (lockscreen
    smartspace and clock) to log more information and help debug bug reports.
    
    Bug: 202758428
    Test: manual
    Change-Id: I83c4362e49521b843892f068aef19f487f430b86

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceControllerTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/lockscreen/LockscreenSmartspaceControllerTest.kt
@@ -33,6 +33,7 @@ import android.view.View
 import android.widget.FrameLayout
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
+import com.android.systemui.dump.DumpManager
 import com.android.systemui.flags.FeatureFlags
 import com.android.systemui.flags.Flags
 import com.android.systemui.plugins.ActivityStarter
@@ -117,6 +118,9 @@ class LockscreenSmartspaceControllerTest : SysuiTestCase() {
 
     @Mock
     private lateinit var configPlugin: BcSmartspaceConfigPlugin
+
+    @Mock
+    private lateinit var dumpManager: DumpManager
 
     @Mock
     private lateinit var controllerListener: SmartspaceTargetListener
@@ -209,6 +213,7 @@ class LockscreenSmartspaceControllerTest : SysuiTestCase() {
                 statusBarStateController,
                 deviceProvisionedController,
                 keyguardBypassController,
+                dumpManager,
                 execution,
                 executor,
                 bgExecutor,
