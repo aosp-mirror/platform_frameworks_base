9df37440158c73ae67cc254b17b1962096fa0b69
Marco Loaiza <loaizamontoya@google.com>
2022-12-14 16:41:46 +0000

    Update deviceId when display changes in context
    Whenever a DisplayContext is created or a displayId
    is updated in Context, update the deviceId, if not
    a Context with an explicit device association.
    
    Test: atest DeviceAssociationTest
    Bug: 253201821
    Change-Id: Iaef039c6b1cf46fbb0ae020ba41735eb48c7d708

--- core/java/android/app/ContextImpl.java
+++ core/java/android/app/ContextImpl.java
@@ -2709,7 +2709,7 @@ class ContextImpl extends Context {
         context.setResources(createResources(mToken, mPackageInfo, mSplitName, displayId,
                 overrideConfig, display.getDisplayAdjustments().getCompatibilityInfo(),
                 mResources.getLoaders()));
-        context.mDisplay = display;
+        context.setDisplay(display);
         // Inherit context type if the container is from System or System UI context to bypass
         // UI context check.
         context.mContextType = mContextType == CONTEXT_TYPE_SYSTEM_OR_SYSTEM_UI
@@ -2723,6 +2723,13 @@ class ContextImpl extends Context {
         // anymore.
         context.mIsConfigurationBasedContext = false;
         return context;
+    }
+
+    private void setDisplay(Display display) {
+        mDisplay = display;
+        if (display != null) {
+            updateDeviceIdIfChanged(display.getDisplayId());
+        }
     }
 
     @Override
@@ -2862,8 +2869,8 @@ class ContextImpl extends Context {
         baseContext.setResources(windowContextResources);
         // Associate the display with window context resources so that configuration update from
         // the server side will also apply to the display's metrics.
-        baseContext.mDisplay = ResourcesManager.getInstance().getAdjustedDisplay(displayId,
-                windowContextResources);
+        baseContext.setDisplay(ResourcesManager.getInstance().getAdjustedDisplay(
+                displayId, windowContextResources));
 
         return baseContext;
     }
@@ -3007,11 +3014,24 @@ class ContextImpl extends Context {
 
     @Override
     public void updateDisplay(int displayId) {
-        mDisplay = mResourcesManager.getAdjustedDisplay(displayId, mResources);
+        setDisplay(mResourcesManager.getAdjustedDisplay(displayId, mResources));
         if (mContextType == CONTEXT_TYPE_NON_UI) {
             mContextType = CONTEXT_TYPE_DISPLAY_CONTEXT;
         }
-        // TODO(b/253201821): Update deviceId when display is updated.
+    }
+
+    private void updateDeviceIdIfChanged(int displayId) {
+        if (mIsExplicitDeviceId) {
+            return;
+        }
+        VirtualDeviceManager vdm = getSystemService(VirtualDeviceManager.class);
+        if (vdm != null) {
+            int deviceId = vdm.getDeviceIdForDisplayId(displayId);
+            if (deviceId != mDeviceId) {
+                mDeviceId = deviceId;
+                notifyOnDeviceChangedListeners(mDeviceId);
+            }
+        }
     }
 
     @Override
@@ -3306,8 +3326,8 @@ class ContextImpl extends Context {
                 classLoader,
                 packageInfo.getApplication() == null ? null
                         : packageInfo.getApplication().getResources().getLoaders()));
-        context.mDisplay = resourcesManager.getAdjustedDisplay(displayId,
-                context.getResources());
+        context.setDisplay(resourcesManager.getAdjustedDisplay(
+                displayId, context.getResources()));
         return context;
     }
 
