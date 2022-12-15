aa9682d5ac568ad7b77390a8042376e1d974a919
Yilin Cai <cyl@google.com>
2022-12-16 00:11:25 +0800

    Fix failed test `AppOpPermissionAppListTest`.
    Failure message: http://go/paste/4521294979661824
    
    Test: atest SpaPrivilegedLibTests:AppOpPermissionAppListTest
    Bug: 262206181
    Change-Id: I533a01d55cc29396a586c7d8eff3742e82c2b708

--- packages/SettingsLib/SpaPrivileged/tests/src/com/android/settingslib/spaprivileged/template/app/AppOpPermissionAppListTest.kt
+++ packages/SettingsLib/SpaPrivileged/tests/src/com/android/settingslib/spaprivileged/template/app/AppOpPermissionAppListTest.kt
@@ -21,7 +21,7 @@ import android.content.Context
 import android.content.pm.ApplicationInfo
 import androidx.compose.runtime.State
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.lifecycle.liveData
+import androidx.lifecycle.MutableLiveData
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.android.settingslib.spa.testutils.firstWithTimeoutOrNull
@@ -254,7 +254,7 @@ class AppOpPermissionAppListTest {
 private class FakeAppOpsController(private val fakeMode: Int) : IAppOpsController {
     var setAllowedCalledWith: Boolean? = null
 
-    override val mode = liveData { emit(fakeMode) }
+    override val mode = MutableLiveData(fakeMode)
 
     override fun setAllowed(allowed: Boolean) {
         setAllowedCalledWith = allowed
