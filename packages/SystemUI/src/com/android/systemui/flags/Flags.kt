2acadea65db80181e0831d2f75d3815773f79e84
Chandru S <chandruis@google.com>
2022-12-16 09:23:15 +0000

    Add flags for biometrics animation revamp and face auth refactor
    Bug: 244313043
    Bug: 262838215
    Test: NA
    Change-Id: I7c496ac9fdd7482a538001e04ee4469e851e6591
    Merged-In: I7c496ac9fdd7482a538001e04ee4469e851e6591

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -176,6 +176,14 @@ object Flags {
     @JvmField
     val LIGHT_REVEAL_MIGRATION = unreleasedFlag(218, "light_reveal_migration", teamfood = false)
 
+    /** Flag to control the migration of face auth to modern architecture. */
+    // TODO(b/262838215): Tracking bug
+    @JvmField val FACE_AUTH_REFACTOR = unreleasedFlag(220, "face_auth_refactor")
+
+    /** Flag to control the revamp of keyguard biometrics progress animation */
+    // TODO(b/244313043): Tracking bug
+    @JvmField val BIOMETRICS_ANIMATION_REVAMP = unreleasedFlag(221, "biometrics_animation_revamp")
+
     // 300 - power menu
     // TODO(b/254512600): Tracking Bug
     @JvmField val POWER_MENU_LITE = releasedFlag(300, "power_menu_lite")
