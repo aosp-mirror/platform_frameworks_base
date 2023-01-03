21a8866591e491a5f5109ff87a8ec0e1e0b7be53
Shen Lin <shhenlin@gmail.com>
2023-01-03 14:05:53 +0800

    Introduce isDeviceClassMatched() to align BluetoothClass comparison
    getBluetoothClass is nullable that caller needs paying attention to. This CL introduced a unified and safe method to BluetoothUtils to align this comparison.
    
    Bug: 264162859
    Test: atest CachedBluetoothDeviceTest
    Change-Id: I740defb7bc369f659103b09643c3e37050ea8bbe

--- packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
+++ packages/SettingsLib/src/com/android/settingslib/bluetooth/CachedBluetoothDevice.java
@@ -1020,12 +1020,11 @@ public class CachedBluetoothDevice implements Comparable<CachedBluetoothDevice> 
         if (BluetoothUuid.containsAnyUuid(uuids, PbapServerProfile.PBAB_CLIENT_UUIDS)) {
             // The pairing dialog now warns of phone-book access for paired devices.
             // No separate prompt is displayed after pairing.
-            final BluetoothClass bluetoothClass = mDevice.getBluetoothClass();
             if (mDevice.getPhonebookAccessPermission() == BluetoothDevice.ACCESS_UNKNOWN) {
-                if (bluetoothClass != null && (bluetoothClass.getDeviceClass()
-                        == BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE
-                        || bluetoothClass.getDeviceClass()
-                        == BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET)) {
+                if (BluetoothUtils.isDeviceClassMatched(mDevice,
+                        BluetoothClass.Device.AUDIO_VIDEO_HANDSFREE)
+                        || BluetoothUtils.isDeviceClassMatched(mDevice,
+                        BluetoothClass.Device.AUDIO_VIDEO_WEARABLE_HEADSET)) {
                     EventLog.writeEvent(0x534e4554, "138529441", -1, "");
                 }
             }
