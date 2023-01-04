324bade1bcc9f0a7e93c45eef4c02474a4312c01
Dmitry Vykochko <vykochko@google.com>
2023-01-04 10:07:59 +0000

    Fix a11y focus rendering on non-dirty root view.
    ViewRootImpl can receive TYPE_VIEW_ACCESSIBILITY_FOCUSED, which
    means it should draw the a11y focus. But without any other
    triggers (e.g. animation or scrolling) it is not being rendered.
    This event invalidates the renderer, which makes the focus rectangle
    dirty, but nothing make the draw() method of the view to be called.
    Fixed by adding the whole view invalidation.
    
    Fix: 264356970
    Test: manual
    Change-Id: I4073974551daf622a0c467bfb27df5f5a14ba82c

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -4856,19 +4856,11 @@ public final class ViewRootImpl implements ViewParent,
             dirty.offset(surfaceInsets.left, surfaceInsets.top);
         }
 
-        boolean accessibilityFocusDirty = false;
-        final Drawable drawable = mAttachInfo.mAccessibilityFocusDrawable;
-        if (drawable != null) {
-            final Rect bounds = mAttachInfo.mTmpInvalRect;
-            final boolean hasFocus = getAccessibilityFocusedRect(bounds);
-            if (!hasFocus) {
-                bounds.setEmpty();
-            }
-            if (!bounds.equals(drawable.getBounds())) {
-                accessibilityFocusDirty = true;
-                // Force recalculation of transparent regions
-                requestLayout();
-            }
+        boolean accessibilityFocusDirty = isAccessibilityFocusDirty();
+
+        // Force recalculation of transparent regions
+        if (accessibilityFocusDirty) {
+            requestLayout();
         }
 
         mAttachInfo.mDrawingTime =
@@ -5415,8 +5407,9 @@ public final class ViewRootImpl implements ViewParent,
         mAccessibilityFocusedVirtualView = node;
         updateKeepClearForAccessibilityFocusRect();
 
-        if (mAttachInfo.mThreadedRenderer != null) {
-            mAttachInfo.mThreadedRenderer.invalidateRoot();
+        requestInvalidateRootRenderNode();
+        if (isAccessibilityFocusDirty()) {
+            scheduleTraversals();
         }
     }
 
@@ -9779,6 +9772,21 @@ public final class ViewRootImpl implements ViewParent,
         final int accessibilityViewId = AccessibilityNodeInfo.getAccessibilityViewId(
                 sourceNodeId);
         return AccessibilityNodeIdManager.getInstance().findView(accessibilityViewId);
+    }
+
+    private boolean isAccessibilityFocusDirty() {
+        final Drawable drawable = mAttachInfo.mAccessibilityFocusDrawable;
+        if (drawable != null) {
+            final Rect bounds = mAttachInfo.mTmpInvalRect;
+            final boolean hasFocus = getAccessibilityFocusedRect(bounds);
+            if (!hasFocus) {
+                bounds.setEmpty();
+            }
+            if (!bounds.equals(drawable.getBounds())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
