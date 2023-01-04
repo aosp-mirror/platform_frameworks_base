fd857dcc5fe761feddae62103dbf8ee29ea1ecb4
Aishwarya Mallampati <amallampati@google.com>
2023-01-04 23:02:56 +0000

    Revert "Added permission check for notifying subscription changed"
    Revert submission 20813798-smsvc_bootup
    
    Reason for revert: b/263772382
    
    Reverted changes: /q/submissionid:20813798-smsvc_bootup
    
    Change-Id: Id944cc1d6b66bea76dec6b35869ee29bf362cfba

--- services/core/java/com/android/server/TelephonyRegistry.java
+++ services/core/java/com/android/server/TelephonyRegistry.java
@@ -1002,10 +1002,6 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
     @Override
     public void notifySubscriptionInfoChanged() {
         if (VDBG) log("notifySubscriptionInfoChanged:");
-        if (!checkNotifyPermission("notifySubscriptionInfoChanged()")) {
-            return;
-        }
-
         synchronized (mRecords) {
             if (!mHasNotifySubscriptionInfoChangedOccurred) {
                 log("notifySubscriptionInfoChanged: first invocation mRecords.size="
@@ -1032,10 +1028,6 @@ public class TelephonyRegistry extends ITelephonyRegistry.Stub {
     @Override
     public void notifyOpportunisticSubscriptionInfoChanged() {
         if (VDBG) log("notifyOpptSubscriptionInfoChanged:");
-        if (!checkNotifyPermission("notifyOpportunisticSubscriptionInfoChanged()")) {
-            return;
-        }
-
         synchronized (mRecords) {
             if (!mHasNotifyOpportunisticSubscriptionInfoChangedOccurred) {
                 log("notifyOpptSubscriptionInfoChanged: first invocation mRecords.size="
