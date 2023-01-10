83fe0d64cf5ac0952a3bf05b49b60ecc588b2ecf
Nikolas Havrikov <havrikov@google.com>
2023-01-10 13:45:21 +0100

    Inline getter in InputMethodBindingController
    Test: make
    Change-Id: Id513779a289dd06158a61804c3606907cd36608e

--- services/core/java/com/android/server/inputmethod/InputMethodBindingController.java
+++ services/core/java/com/android/server/inputmethod/InputMethodBindingController.java
@@ -469,11 +469,11 @@ final class InputMethodBindingController {
 
     @GuardedBy("ImfLock.class")
     private boolean bindCurrentInputMethodService(ServiceConnection conn, int flags) {
-        if (getCurIntent() == null || conn == null) {
+        if (mCurIntent == null || conn == null) {
             Slog.e(TAG, "--- bind failed: service = " + mCurIntent + ", conn = " + conn);
             return false;
         }
-        return mContext.bindServiceAsUser(getCurIntent(), conn, flags,
+        return mContext.bindServiceAsUser(mCurIntent, conn, flags,
                 new UserHandle(mSettings.getCurrentUserId()));
     }
 
