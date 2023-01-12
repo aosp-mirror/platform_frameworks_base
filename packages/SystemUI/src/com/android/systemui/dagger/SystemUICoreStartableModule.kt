58b6e0dfa296ac3babadfafeacbe6d9406f82e28
Bryce Lee <brycelee@google.com>
2023-01-12 15:52:45 -0800

    Publish dream state to OverviewProxyService.
    This change pipes the current dream state to OverviewProxyService,
    similar to how doze information is shared. A condition has been
    added to monitor the dream state, and a callback relays this
    information to the StatusBarStateController. A new CoreStartable
    maintains the monitor for this interaction.
    
    Test: atest StatusBarStateControllerImplTest#testSetDreamState_invokesCallback
    Bug: 265755700
    Change-Id: Ic927a7e55864453da026c14cd1c87e6a7cf22489
    Merged-In: Ic927a7e55864453da026c14cd1c87e6a7cf22489

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -27,6 +27,7 @@ import com.android.systemui.accessibility.WindowMagnification
 import com.android.systemui.biometrics.AuthController
 import com.android.systemui.clipboardoverlay.ClipboardListener
 import com.android.systemui.dagger.qualifiers.PerUser
+import com.android.systemui.dreams.DreamMonitor
 import com.android.systemui.globalactions.GlobalActionsComponent
 import com.android.systemui.keyboard.KeyboardUI
 import com.android.systemui.keyguard.KeyguardViewMediator
@@ -286,4 +287,10 @@ abstract class SystemUICoreStartableModule {
     @IntoMap
     @ClassKey(StylusUsiPowerStartable::class)
     abstract fun bindStylusUsiPowerStartable(sysui: StylusUsiPowerStartable): CoreStartable
+
+    /**Inject into DreamMonitor */
+    @Binds
+    @IntoMap
+    @ClassKey(DreamMonitor::class)
+    abstract fun bindDreamMonitor(sysui: DreamMonitor): CoreStartable
 }
