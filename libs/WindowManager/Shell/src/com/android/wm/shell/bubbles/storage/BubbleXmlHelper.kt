ab8c81bcce2599b1914832e1af48f04c544d695f
András Kurucz <kurucz@google.com>
2023-01-17 16:32:08 +0000

    Fix missing notifs after restoring Overflow Bubbles
    When you swipe away a notification in the shade and that notification has a restored Overflow Bubble, the shade does not update to fill the blank space.
    
    How does this happen:
    - ShadeListBuilder depends on BubbleCoordinator's NotifFilter to tell it which notifications to filter out
    - BubbleCoordinator checks BubbleController's CachedState to see which notifications to suppress
    - BubbleController's CachedState is not updated to suppress the Notification, because
    - Bubble #setSuppressNotification thinks that we should still display the Notif in the shade, because
    - In Bubble #showInShade always returned true, becase the #mIsClearable is set to false for all restored Overflow Bubbles
    
    Why doesn't it happen with Bubbles in the stack:
    - if a Bubble is in the Stack, we don't read its data from the Overflow Bubbles
    - after a restart, Bubbles in the Stack are freshly created from restored NotificationEntries in BubblesManager#mSysuiProxy#getShouldRestoredEntries
    - NotificationEntry#isDismissable is used to set BubbleEntry#isClearable here, which field is preserved
    
    This change
    - writes Bubble#mIsClearable to the bubble xml, so we won't have this problem with restoring Overflow Bubbles
    - fixes SparseArray comparison in BubbleXmlHelperTest and BubblePersistentRepositoryTest as previously we were not comparing values stored with the same key
    
    Fixes: 237897866
    
    Test: have some Notifications which belongs to Overflow Bubbles (the max number of Bubbles is 5, if we receive more Notifs, the oldest Bubble will be pushed to the Overflow)
    => restart SystemUI with `adb shell am crash com.android.systemui`
    => swipe away shade notification for the chats
    => see that notifs below move up to fill blank space
    
    Test: atest BubbleXmlHelperTest BubblePersistentRepositoryTest
    
    Change-Id: I621b72a54db0034e4c66c69838bd266401c02f9b

--- libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/storage/BubbleXmlHelper.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/bubbles/storage/BubbleXmlHelper.kt
@@ -44,6 +44,9 @@ private const val ATTR_TITLE = "t"
 private const val ATTR_TASK_ID = "tid"
 private const val ATTR_LOCUS = "l"
 
+// TODO rename it to dismissable to follow NotificationEntry namings
+private const val ATTR_CLEARABLE = "d"
+
 /**
  * Writes the bubbles in xml format into given output stream.
  */
@@ -84,6 +87,7 @@ private fun writeXmlEntry(serializer: XmlSerializer, bubble: BubbleEntity) {
         bubble.title?.let { serializer.attribute(null, ATTR_TITLE, it) }
         serializer.attribute(null, ATTR_TASK_ID, bubble.taskId.toString())
         bubble.locus?.let { serializer.attribute(null, ATTR_LOCUS, it) }
+        serializer.attribute(null, ATTR_CLEARABLE, bubble.isClearable.toString())
         serializer.endTag(null, TAG_BUBBLE)
     } catch (e: IOException) {
         throw RuntimeException(e)
@@ -142,7 +146,8 @@ private fun readXmlEntry(parser: XmlPullParser): BubbleEntity? {
             parser.getAttributeWithName(ATTR_DESIRED_HEIGHT_RES_ID)?.toInt() ?: return null,
             parser.getAttributeWithName(ATTR_TITLE),
             parser.getAttributeWithName(ATTR_TASK_ID)?.toInt() ?: INVALID_TASK_ID,
-            parser.getAttributeWithName(ATTR_LOCUS)
+            parser.getAttributeWithName(ATTR_LOCUS),
+            parser.getAttributeWithName(ATTR_CLEARABLE)?.toBoolean() ?: false
     )
 }
 
