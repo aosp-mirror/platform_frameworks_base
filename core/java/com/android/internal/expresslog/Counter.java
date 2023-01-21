24d8ef54d39f0fe5a203d41258c189334320e96b
Vova Sharaienko <sharaienko@google.com>
2023-01-21 04:31:16 +0000

    [TeX] Extracted Utils API for Telemetry Express Logging APIs
    - hashString API moved to utils to be used by Histogram metric APIs
    
    Bug: 262791652
    Test: m
    Change-Id: Ic049083c6e19c045e6f743e3bc96e1fc15ed4533

--- core/java/com/android/internal/expresslog/Counter.java
+++ core/java/com/android/internal/expresslog/Counter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 The Android Open Source Project
+ * Copyright (C) 2023 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,9 +39,7 @@ public final class Counter {
      * @hide
      */
     public static void logIncrement(@NonNull String metricId, long amount) {
-        final long metricIdHash = hashString(metricId);
+        final long metricIdHash = Utils.hashString(metricId);
         FrameworkStatsLog.write(FrameworkStatsLog.EXPRESS_EVENT_REPORTED, metricIdHash, amount);
     }
-
-    private static native long hashString(String stringToHash);
 }
