aa1d37f7be6f6d082b317bed2f14f11a83704e63
Xiaowen Lei <xilei@google.com>
2023-01-31 15:20:14 -0800

    Flip the SMARTSPACE_DATE_WEATHER_DECOUPLED flag (feature targeting U).
    Bug: 258517050
    Test: manual
    Change-Id: Ib423ec14da8e1497f516a49a3c4e66594696c286

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -235,7 +235,7 @@ object Flags {
     // TODO(b/258517050): Clean up after the feature is launched.
     @JvmField
     val SMARTSPACE_DATE_WEATHER_DECOUPLED =
-        sysPropBooleanFlag(403, "persist.sysui.ss.dw_decoupled", default = false)
+        sysPropBooleanFlag(403, "persist.sysui.ss.dw_decoupled", default = true)
 
     // 500 - quick settings
 
