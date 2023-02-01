a8b5e7bd893c21040dabe8f12deb5dd60d3d85c2
Riddle Hsu <riddlehsu@google.com>
2023-02-01 16:47:05 +0800

    Avoid changing resolution by display mode when animating
    There was a logic to use default refresh rate when animating (e.g.
    default is high refresh rate, but app requested low refresh rate).
    But since display mode can also affect display size, if the mode
    triggers the size change when animating, the animation may be
    interrupted or canceled. So it is better to keep the current mode,
    and the mode will be updated on the window traversal after the
    animation is finished.
    
    Also do not set display properties when display is seamless
    rotating, otherwise there will be obvious flickering because
    there won't be screenshot to cover the resolution change.
    
    Fixes: 238771010
    Bug: 255850221
    Bug: 267365057
    
    Test: RefreshRatePolicyTest#testAnimatingAppOverridePreferredModeId
    Change-Id: I5b3ddbc8278a95b4c5239812b2c4a293775c740a
    Merged-In: I5b3ddbc8278a95b4c5239812b2c4a293775c740a

--- services/core/java/com/android/server/wm/DisplayContent.java
+++ services/core/java/com/android/server/wm/DisplayContent.java
@@ -2119,6 +2119,10 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
                 w.seamlesslyRotateIfAllowed(transaction, oldRotation, rotation, rotateSeamlessly);
             }, true /* traverseTopToBottom */);
             mPinnedTaskController.startSeamlessRotationIfNeeded(transaction, oldRotation, rotation);
+            if (!mDisplayRotation.hasSeamlessRotatingWindow()) {
+                // Make sure DisplayRotation#isRotatingSeamlessly() will return false.
+                mDisplayRotation.cancelSeamlessRotation();
+            }
         }
 
         mWmService.mDisplayManagerInternal.performTraversal(transaction);
@@ -4827,7 +4831,7 @@ class DisplayContent extends RootDisplayArea implements WindowManagerPolicy.Disp
         mInsetsStateController.getImeSourceProvider().checkShowImePostLayout();
 
         mLastHasContent = mTmpApplySurfaceChangesTransactionState.displayHasContent;
-        if (!mWmService.mDisplayFrozen) {
+        if (!mWmService.mDisplayFrozen && !mDisplayRotation.isRotatingSeamlessly()) {
             mWmService.mDisplayManagerInternal.setDisplayProperties(mDisplayId,
                     mLastHasContent,
                     mTmpApplySurfaceChangesTransactionState.preferredRefreshRate,
