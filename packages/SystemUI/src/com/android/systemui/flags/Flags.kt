81948ff84ba60442bd696f1b016ad1ff4e6525e8
Matt Casey <mrcasey@google.com>
2023-02-03 18:53:05 +0000

    SCREENSHOT_METADATA -> teamfood
    Was blocked by required sharesheet features.
    
    Bug: 264916608
    Test: Builds
    Change-Id: Ie881050e3ee2c40e143988018733851559ebc437

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -514,7 +514,7 @@ object Flags {
         unreleasedFlag(1301, "screenshot_work_profile_policy", teamfood = true)
 
     // TODO(b/264916608): Tracking Bug
-    @JvmField val SCREENSHOT_METADATA = unreleasedFlag(1302, "screenshot_metadata")
+    @JvmField val SCREENSHOT_METADATA = unreleasedFlag(1302, "screenshot_metadata", teamfood = true)
 
     // TODO(b/266955521): Tracking bug
     @JvmField val SCREENSHOT_DETECTION = unreleasedFlag(1303, "screenshot_detection")
