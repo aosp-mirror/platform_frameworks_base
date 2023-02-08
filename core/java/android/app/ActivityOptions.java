e53984d4e7816131d32697ea8ec4ac89b07c6fb3
Winson Chung <winsonc@google.com>
2023-02-08 01:04:46 +0000

    Only allow NEW_TASK flag when adjusting pending intents
    Bug: 243794108
    Test: atest CtsSecurityBulletinHostTestCases:android.security.cts.CVE_2023_20918
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:c62d2e1021a030f4f0ae5fcfc8fe8e0875fa669f)
    Merged-In: I5d329beecef1902c36704e93d0bc5cb60d0e2f5b
    Change-Id: I5d329beecef1902c36704e93d0bc5cb60d0e2f5b

--- core/java/android/app/ActivityOptions.java
+++ core/java/android/app/ActivityOptions.java
@@ -20,6 +20,8 @@ import static android.Manifest.permission.CONTROL_REMOTE_APP_TRANSITION_ANIMATIO
 import static android.app.ActivityTaskManager.SPLIT_SCREEN_CREATE_MODE_TOP_OR_LEFT;
 import static android.app.WindowConfiguration.ACTIVITY_TYPE_UNDEFINED;
 import static android.app.WindowConfiguration.WINDOWING_MODE_UNDEFINED;
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+import static android.content.Intent.FLAG_RECEIVER_FOREGROUND;
 import static android.view.Display.INVALID_DISPLAY;
 
 import android.annotation.NonNull;
@@ -1372,7 +1374,9 @@ public class ActivityOptions extends ComponentOptions {
      * @hide
      */
     public int getPendingIntentLaunchFlags() {
-        return mPendingIntentLaunchFlags;
+        // b/243794108: Ignore all flags except the new task flag, to be reconsidered in b/254490217
+        return mPendingIntentLaunchFlags &
+                (FLAG_ACTIVITY_NEW_TASK | FLAG_RECEIVER_FOREGROUND);
     }
 
     /**
