028de77c85d23d27f9a4f387fcadf380c35f90da
Aaron Liu <aaronjli@google.com>
2023-02-08 10:34:02 -0800

    Destroy and reinflate bouncer before showing
    Due to various bugs and settingns changes, it makes sense to entirely
    refresh the bouncer view before showing. This should significantly
    reduce the number of incoming bugs where bouncer is not showing or is
    not responding to settings changes.
    
    Fixes: 260958175
    Fixes: 266840428
    Fixes: 268226007
    Fixes: 267363062
    Test: open sim pin, sim puk, password, pattern, pin.
    Test: change pattern visible to see changes reflected on screen.
    Test: change show password characters to see changes reflected on
    screen.
    
    Change-Id: I8d7a906866888ad292bd8f813cdba99920a56884

--- packages/SystemUI/src/com/android/keyguard/KeyguardHostViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardHostViewController.java
@@ -202,8 +202,11 @@ public class KeyguardHostViewController extends ViewController<KeyguardHostView>
         mKeyguardSecurityContainerController.onPause();
     }
 
-    public void resetSecurityContainer() {
-        mKeyguardSecurityContainerController.reset();
+    /**
+     * Reinflate the view flipper child view.
+     */
+    public void reinflateViewFlipper() {
+        mKeyguardSecurityContainerController.reinflateViewFlipper();
     }
 
     /**
@@ -232,23 +235,19 @@ public class KeyguardHostViewController extends ViewController<KeyguardHostView>
     /**
      * Starts the animation when the Keyguard gets shown.
      */
-    public void appear(int statusBarHeight) {
+    public void appear() {
         // We might still be collapsed and the view didn't have time to layout yet or still
         // be small, let's wait on the predraw to do the animation in that case.
-        if (mView.getHeight() != 0 && mView.getHeight() != statusBarHeight) {
-            mKeyguardSecurityContainerController.startAppearAnimation();
-        } else {
-            mView.getViewTreeObserver().addOnPreDrawListener(
-                    new ViewTreeObserver.OnPreDrawListener() {
-                        @Override
-                        public boolean onPreDraw() {
-                            mView.getViewTreeObserver().removeOnPreDrawListener(this);
-                            mKeyguardSecurityContainerController.startAppearAnimation();
-                            return true;
-                        }
-                    });
-            mView.requestLayout();
-        }
+        mView.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        mView.getViewTreeObserver().removeOnPreDrawListener(this);
+                        mKeyguardSecurityContainerController.startAppearAnimation();
+                        return true;
+                    }
+                });
+        mView.requestLayout();
     }
 
     /**
