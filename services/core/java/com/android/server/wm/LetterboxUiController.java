fbcf72fa1b228a2885733dbba11e645893f01493
Pablo Gamito <pablogamito@google.com>
2023-02-10 15:57:51 +0000

    Update condition for when to show letterbox
    Makes sure we show the letterbox even during the period a window is
    requesting to be redrawn because of a layout change (i.e. orientation
    change) or for any other reason, if the window's associated surface
    is still visible.
    
    This avoids seeing a letterbox flicker when this happens.
    
    Bug: 256052489
    Bug: 266635069
    Test: Launch the dialer for a phone call or Google wallet app on a large
    screen device and make sure the letterbox doesn't flicker on launch.
    
    Change-Id: I7911ddc45c1ff839cc22be09caa5cc7ddc022d45

--- services/core/java/com/android/server/wm/LetterboxUiController.java
+++ services/core/java/com/android/server/wm/LetterboxUiController.java
@@ -1001,7 +1001,7 @@ final class LetterboxUiController {
 
     @VisibleForTesting
     boolean shouldShowLetterboxUi(WindowState mainWindow) {
-        return isSurfaceReadyAndVisible(mainWindow) && mainWindow.areAppWindowBoundsLetterboxed()
+        return isSurfaceVisible(mainWindow) && mainWindow.areAppWindowBoundsLetterboxed()
                 // Check for FLAG_SHOW_WALLPAPER explicitly instead of using
                 // WindowContainer#showWallpaper because the later will return true when this
                 // activity is using blurred wallpaper for letterbox background.
@@ -1009,11 +1009,8 @@ final class LetterboxUiController {
     }
 
     @VisibleForTesting
-    boolean isSurfaceReadyAndVisible(WindowState mainWindow) {
-        boolean surfaceReady = mainWindow.isDrawn() // Regular case
-                // Waiting for relayoutWindow to call preserveSurface
-                || mainWindow.isDragResizeChanged();
-        return surfaceReady && (mActivityRecord.isVisible()
+    boolean isSurfaceVisible(WindowState mainWindow) {
+        return mainWindow.isOnScreen() && (mActivityRecord.isVisible()
                 || mActivityRecord.isVisibleRequested());
     }
 
