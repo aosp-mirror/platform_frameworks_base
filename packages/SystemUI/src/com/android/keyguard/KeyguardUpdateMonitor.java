c24efef46ab6bccc00987f921b63c7870a1b9378
Beverly <beverlyt@google.com>
2023-02-16 18:19:05 +0000

    Revert change that would detectFp when FP is locked out
    This reverts commit ff09044d12f936bab51ed9e7d33016616ce9e87e in
    addition to partially reverting ag/20141372.
    
    Reason for revert: b/269574598
    
    Change-Id: Iea66d7782b3365dea827b854783aeddaa5c6699c
    (cherry picked from commit 63e446209d59a264a2abcf45630f80153c48e409)
    Merged-In: Iea66d7782b3365dea827b854783aeddaa5c6699c

--- packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -2736,7 +2736,8 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener, Dumpab
 
         boolean shouldListen = shouldListenKeyguardState && shouldListenUserState
                 && shouldListenBouncerState && shouldListenUdfpsState
-                && shouldListenSideFpsState;
+                && shouldListenSideFpsState
+                && !isFingerprintLockedOut();
         logListenerModelData(
                 new KeyguardFingerprintListenModel(
                     System.currentTimeMillis(),
