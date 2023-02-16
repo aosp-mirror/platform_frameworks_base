8c14a8201665f81afe0a0e1983093e14c67feaaa
Jakub Pawlowski <jpawlowski@google.com>
2023-02-16 11:46:18 +0100

    AudioService: Add makeLeAudioDeviceUnavailableLater
    Currently, when LE Audio device becomes unavaliable due to BT adapter
    disable, user can hear audio from device speaker for a split second.
    This is because we make device unavaliable immediately for LE Audio.
    Instead, it should become unavaliable after AUDIO_BECOMING_NOISY intent
    is sent.
    
    Since this is working correctly for A2DP, fix is to copy solution from
    there for LE Audio code path, and split makeLeAudioDeviceUnavailable
    into *Now and *Later versions.
    
    With this fix, the device becomes unavaliable after the noisy intent,
    and audio glitch is not heard from speaker.
    
    Example dumpsys audio logs after fix is applied:
    
    02-16 11:25:43:022 broadcast ACTION_AUDIO_BECOMING_NOISY
    02-16 11:25:44:050 LE Audio device addr=14:3F:A6:60:0A:A5 made unavailable
    
    Bug: 263209656
    Test: play music in Spotfiy, listen on LE Audio device, then disable
    Bluetooth. Verify that audio is not heard from the phoen speaker
    
    Change-Id: I0ff85798c3672b2d6862460c2f2d914f705aac2c
    (cherry picked from commit 7fb3968aac65430d6ed3ad1a109a8a42c4c77bed)

--- services/core/java/com/android/server/audio/AudioDeviceInventory.java
+++ services/core/java/com/android/server/audio/AudioDeviceInventory.java
@@ -395,7 +395,7 @@ public class AudioDeviceInventory {
                 case BluetoothProfile.LE_AUDIO:
                 case BluetoothProfile.LE_AUDIO_BROADCAST:
                     if (switchToUnavailable) {
-                        makeLeAudioDeviceUnavailable(address, btInfo.mAudioSystemDevice);
+                        makeLeAudioDeviceUnavailableNow(address, btInfo.mAudioSystemDevice);
                     } else if (switchToAvailable) {
                         makeLeAudioDeviceAvailable(address, BtHelper.getName(btInfo.mDevice),
                                 streamType, btInfo.mVolume == -1 ? -1 : btInfo.mVolume * 10,
@@ -504,6 +504,12 @@ public class AudioDeviceInventory {
     /*package*/ void onMakeA2dpDeviceUnavailableNow(String address, int a2dpCodec) {
         synchronized (mDevicesLock) {
             makeA2dpDeviceUnavailableNow(address, a2dpCodec);
+        }
+    }
+
+    /*package*/ void onMakeLeAudioDeviceUnavailableNow(String address, int device) {
+        synchronized (mDevicesLock) {
+            makeLeAudioDeviceUnavailableNow(address, device);
         }
     }
 
@@ -1027,10 +1033,11 @@ public class AudioDeviceInventory {
             new MediaMetrics.Item(mMetricsId + "disconnectLeAudio")
                     .record();
             if (toRemove.size() > 0) {
-                final int delay = checkSendBecomingNoisyIntentInt(device, 0,
+                final int delay = checkSendBecomingNoisyIntentInt(device,
+                        AudioService.CONNECTION_STATE_DISCONNECTED,
                         AudioSystem.DEVICE_NONE);
                 toRemove.stream().forEach(deviceAddress ->
-                        makeLeAudioDeviceUnavailable(deviceAddress, device)
+                        makeLeAudioDeviceUnavailableLater(deviceAddress, device, delay)
                 );
             }
         }
@@ -1354,7 +1361,7 @@ public class AudioDeviceInventory {
     }
 
     @GuardedBy("mDevicesLock")
-    private void makeLeAudioDeviceUnavailable(String address, int device) {
+    private void makeLeAudioDeviceUnavailableNow(String address, int device) {
         if (device != AudioSystem.DEVICE_NONE) {
             AudioSystem.setDeviceConnectionState(new AudioDeviceAttributes(device, address),
                     AudioSystem.DEVICE_STATE_UNAVAILABLE,
@@ -1363,6 +1370,14 @@ public class AudioDeviceInventory {
         }
 
         setCurrentAudioRouteNameIfPossible(null, false /*fromA2dp*/);
+    }
+
+    @GuardedBy("mDevicesLock")
+    private void makeLeAudioDeviceUnavailableLater(String address, int device, int delayMs) {
+        // the device will be made unavailable later, so consider it disconnected right away
+        mConnectedDevices.remove(DeviceInfo.makeDeviceListKey(device, address));
+        // send the delayed message to make the device unavailable later
+        mDeviceBroker.setLeAudioTimeout(address, device, delayMs);
     }
 
     @GuardedBy("mDevicesLock")
