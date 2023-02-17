745db83b5e1231c069295a59b8889a6c38983ecf
Riddle Hsu <riddlehsu@google.com>
2023-02-18 00:48:15 +0800

    Handle animating exit windows with shell transition
    WindowState#isWinVisibleLw can only detect legacy transition.
    So when using shell transition, if an activity relayout to
    invisible before the transition is finished, its surface will
    be destroyed when animating, which looks bad.
    
    Since there were too may visibility methods, and isWinVisibleLw
    can be covered by isVisible()+isDisplayed() which is able to
    consider legacy and shell transition. So just delete it.
     Other usages of isWinVisibleLw:
     - shouldKeepVisibleDeadAppWindow: unused dead code
     - imeWindow: non-activity window so it equals to isVisible().
    
    Besides, make shell transition have the same behavior as legacy
    transition to call onExitAnimationDone when the animation is
    finished to destroy the surfaces.
    
    Also simplify tryStartExitingAnimation:
    - Remove returned focusMayChange because it should be already
      done by visibility change.
    - Remove condition for wallpaper target because it was added
      for lockscreen (commit 6136b7e) 10 years ago, but notification
      shade (now it is lockscreen) was excluded for other issue in
      recent version. That means the original problem was gone.
    
    Fix: 247005789
    Test: atest WindowManagerServiceTests#testRelayoutExitingWindow
    Change-Id: I2255fdad39560fd3dff589985687bea93a00a038

--- services/core/java/com/android/server/wm/TransitionController.java
+++ services/core/java/com/android/server/wm/TransitionController.java
@@ -102,6 +102,12 @@ class TransitionController {
      */
     private final ArrayList<Transition> mPlayingTransitions = new ArrayList<>();
 
+    /**
+     * The windows that request to be invisible while it is in transition. After the transition
+     * is finished and the windows are no longer animating, their surfaces will be destroyed.
+     */
+    final ArrayList<WindowState> mAnimatingExitWindows = new ArrayList<>();
+
     final Lock mRunningLock = new Lock();
 
     private final IBinder.DeathRecipient mTransitionPlayerDeath;
@@ -658,6 +664,15 @@ class TransitionController {
         mPlayingTransitions.remove(record);
         updateRunningRemoteAnimation(record, false /* isPlaying */);
         record.finishTransition();
+        for (int i = mAnimatingExitWindows.size() - 1; i >= 0; i--) {
+            final WindowState w = mAnimatingExitWindows.get(i);
+            if (w.mAnimatingExit && w.mHasSurface && !w.inTransition()) {
+                w.onExitAnimationDone();
+            }
+            if (!w.mAnimatingExit || !w.mHasSurface) {
+                mAnimatingExitWindows.remove(i);
+            }
+        }
         mRunningLock.doNotifyLocked();
     }
 
