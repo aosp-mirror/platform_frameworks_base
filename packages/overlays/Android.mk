c2e18524b632127d0e00c3535f9e340e5339f267
Tiger <tigerhuang@google.com>
2023-02-21 23:16:36 +0800

    Add an overlay package to enable config_navBarDefaultTransparent
    This is used by the developer option.
    
    Fix: 232195501
    Test: Enable transparent navigation bar from developer options and see
          if the navigation bar background is transparent in NetGrapher.
    Change-Id: I33addaeaec2a1f21f1a4ea03b62e090cca5600b9

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -31,6 +31,7 @@ LOCAL_REQUIRED_MODULES := \
 	NavigationBarModeGesturalOverlayNarrowBack \
 	NavigationBarModeGesturalOverlayWideBack \
 	NavigationBarModeGesturalOverlayExtraWideBack \
+	TransparentNavigationBarOverlay \
 	preinstalled-packages-platform-overlays.xml
 
 include $(BUILD_PHONY_PACKAGE)
