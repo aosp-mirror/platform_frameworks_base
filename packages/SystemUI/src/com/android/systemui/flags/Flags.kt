5b30ee3c9c453004a41a8d34e943bf62f7e25247
Darrell Shi <darrellshi@google.com>
2023-02-22 18:05:11 +0000

    Add flag to hide Smartspace on dream overlay.
    This flag when flipped on hides the Smartspace on dream overlay.
    
    Bug: 270223352
    Bug: 270169281
    Test: manual
    Change-Id: I44ac8b863c41be0617385ac3026f1ca2ec0b3d58

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -231,6 +231,10 @@ object Flags {
     val SMARTSPACE_DATE_WEATHER_DECOUPLED =
         sysPropBooleanFlag(403, "persist.sysui.ss.dw_decoupled", default = false)
 
+    // TODO(b/270223352): Tracking Bug
+    @JvmField
+    val HIDE_SMARTSPACE_ON_DREAM_OVERLAY = unreleasedFlag(404, "hide_smartspace_on_dream_overlay")
+
     // 500 - quick settings
 
     val PEOPLE_TILE = resourceBooleanFlag(502, R.bool.flag_conversations, "people_tile")
