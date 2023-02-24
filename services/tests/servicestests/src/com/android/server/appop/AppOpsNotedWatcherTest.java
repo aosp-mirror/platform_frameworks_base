c8b1395c627b779a1ef4fefe50a3bba96d171a1e
Evan Severson <evanseverson@google.com>
2023-02-24 23:52:42 +0000

    Fix package name for appop test
    Fixes: 270753591
    Change-Id: I6c695e88c3838df452c61b2ab0f1b05c2c68dadf

--- services/tests/servicestests/src/com/android/server/appop/AppOpsNotedWatcherTest.java
+++ services/tests/servicestests/src/com/android/server/appop/AppOpsNotedWatcherTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.server.appops;
+package com.android.server.appop;
 
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.inOrder;
