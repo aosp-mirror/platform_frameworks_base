6318c7e12c130be2e7767c49e956ae2f5e56c9e8
Eric Biggers <ebiggers@google.com>
2023-02-27 19:20:45 +0000

    locksettings: improve logging of LSKF verification
    Improve the logging related to verifying the LSKF.
    
    We generally don't want to be super verbose here, but it does make sense
    to have an INFO message at the beginning and end.  There was already a
    DEBUG message at the beginning and an INFO message near the end, but
    they were unclear, so replace them with clearer INFO messages.
    
    Bug: 268526331
    Change-Id: Iaccbbd0d5a297bf97ff6ef31630eeec19fe3277b
    Merged-In: Iaccbbd0d5a297bf97ff6ef31630eeec19fe3277b
    (cherry picked from commit 18045f36e80f80aee4968c6a91c643c419bd8c6b)

--- services/core/java/com/android/server/locksettings/SyntheticPasswordManager.java
+++ services/core/java/com/android/server/locksettings/SyntheticPasswordManager.java
@@ -1176,8 +1176,9 @@ class SyntheticPasswordManager {
             storedType = pwd.credentialType;
         }
         if (!credential.checkAgainstStoredType(storedType)) {
-            Slog.e(TAG, TextUtils.formatSimple("Credential type mismatch: expected %d actual %d",
-                    storedType, credential.getType()));
+            Slogf.e(TAG, "Credential type mismatch: stored type is %s but provided type is %s",
+                    LockPatternUtils.credentialTypeToString(storedType),
+                    LockPatternUtils.credentialTypeToString(credential.getType()));
             result.gkResponse = VerifyCredentialResponse.ERROR;
             return result;
         }
