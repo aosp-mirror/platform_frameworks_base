e1a0be11c3a9db17337a928b6ccf33d818d7dbcb
João Victor Mendes Freire <jvfreire@google.com>
2023-03-02 11:15:27 -0300

    Revert back to waiting 10s to cool down
    After the tests ran for a couple of weeks, no significant changes were
    detected. However, some tests started to crash more often. Since we are
    not sure if it is due to the testing platform or due to the change in
    wait time, we are reverting to the amount used on the original
    investigation.
    
    Bug: 261621873
    Test: atest UserLifecycleTests
    Change-Id: I0bf137503f153993d7067c71b4e6f303e58031a7

--- apct-tests/perftests/multiuser/src/android/multiuser/UserLifecycleTests.java
+++ apct-tests/perftests/multiuser/src/android/multiuser/UserLifecycleTests.java
@@ -1555,8 +1555,8 @@ public class UserLifecycleTests {
 
     private void waitCoolDownPeriod() {
         // Heuristic value based on local tests. Stability increased compared to no waiting.
-        final int fiveSeconds = 1000 * 5;
+        final int tenSeconds = 1000 * 10;
         waitForBroadcastIdle();
-        sleep(fiveSeconds);
+        sleep(tenSeconds);
     }
 }
