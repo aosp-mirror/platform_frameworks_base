42ec1d8ced755ee6a72e3ccabb04d504ac3adb54
Alex Johnston <acjohnston@google.com>
2023-03-08 22:28:28 +0000

    Enforce DevicePolicyManager.setUserControlDisabledPackages in AppStandbyController
    When deciding an app's standby bucket, check if the
    app has its user control disabled by an IT admin. If so,
    the app should be the exempted restricted bucket.
    
    Bug: 272042183
    Test: atest AppStandbyControllerTests
    (cherry picked from commit 269fcb6873dee199dd8023831f882aafff1f6291)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:552dbbc142324408ab8d53cfb5c82a9395f9532c)
    Merged-In: I4279dc37f0e17aedb1c2a87468478248443a253e
    Change-Id: I4279dc37f0e17aedb1c2a87468478248443a253e

--- services/usage/java/com/android/server/usage/UsageStatsService.java
+++ services/usage/java/com/android/server/usage/UsageStatsService.java
@@ -2303,6 +2303,11 @@ public class UsageStatsService extends SystemService implements
         }
 
         @Override
+        public void setAdminProtectedPackages(Set<String> packageNames, int userId) {
+            mAppStandby.setAdminProtectedPackages(packageNames, userId);
+        }
+
+        @Override
         public void onAdminDataAvailable() {
             mAppStandby.onAdminDataAvailable();
         }
