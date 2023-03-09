8d5384a5db5117d964af022f50e3670790a6701b
Marzia Favaro <marziana@google.com>
2023-03-09 14:15:10 +0000

    Keep track of two top wallpapers to improve the control over each.
    Bug: 268136910
    Test: WallpaperManagerTest
    Change-Id: Ibf9a8cee8a922880fd9fc61b173a6d60bb032a5b

--- services/core/java/com/android/server/wm/Transition.java
+++ services/core/java/com/android/server/wm/Transition.java
@@ -579,9 +579,10 @@ class Transition implements BLASTSyncEngine.TransactionReadyListener {
         recordDisplay(wc.getDisplayContent());
         if (info.mShowWallpaper) {
             // Collect the wallpaper token (for isWallpaper(wc)) so it is part of the sync set.
-            final WindowState wallpaper =
-                    wc.getDisplayContent().mWallpaperController.getTopVisibleWallpaper();
-            if (wallpaper != null) {
+            final List<WindowState> wallpapers =
+                    wc.getDisplayContent().mWallpaperController.getAllTopWallpapers();
+            for (int i = wallpapers.size() - 1; i >= 0; i--) {
+                WindowState wallpaper = wallpapers.get(i);
                 collect(wallpaper.mToken);
             }
         }
