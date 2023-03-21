1ee9c3e8667539e357801424aa08806cd9cb9b0c
Aaron Liu <aaronjli@google.com>
2023-03-21 11:25:03 -0700

    Launch feature to teamfood.
    Launch prevent bypass keyguard to teamfood.
    
    Bug: 273341787
    Test: Been testing locally
    Change-Id: I0396ae62d6764e80aececf48b22d35fa9dc490ae

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -231,7 +231,7 @@ object Flags {
     /** Whether to inflate the bouncer view on a background thread. */
     // TODO(b/273341787): Tracking Bug
     @JvmField
-    val PREVENT_BYPASS_KEYGUARD = unreleasedFlag(230, "prevent_bypass_keyguard")
+    val PREVENT_BYPASS_KEYGUARD = unreleasedFlag(230, "prevent_bypass_keyguard", teamfood = true)
 
     // 300 - power menu
     // TODO(b/254512600): Tracking Bug
