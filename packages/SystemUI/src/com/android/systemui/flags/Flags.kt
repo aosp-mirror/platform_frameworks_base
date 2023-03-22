50c1aaa24592060a0eded849c62104123c974a01
Aaron Liu <aaronjli@google.com>
2023-03-22 14:51:19 -0700

    Move async_bouncer to teamfood.
    Turning teamfood on in udc-dev after a fix has been made.
    
    Bug: 272091103
    Test: turn on bouncer with flag on/off
    Change-Id: I082c94f751e85034c703b795af6ff15ef4915ad3

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -226,7 +226,7 @@ object Flags {
     /** Whether to inflate the bouncer view on a background thread. */
     // TODO(b/272091103): Tracking Bug
     @JvmField
-    val ASYNC_INFLATE_BOUNCER = unreleasedFlag(229, "async_inflate_bouncer", teamfood = false)
+    val ASYNC_INFLATE_BOUNCER = unreleasedFlag(229, "async_inflate_bouncer", teamfood = true)
 
     /** Whether to inflate the bouncer view on a background thread. */
     // TODO(b/273341787): Tracking Bug
