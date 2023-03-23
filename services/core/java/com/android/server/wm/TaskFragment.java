d19ad2aa09fd27ec11e626ab239babb929c32fd6
Louis Chang <louischang@google.com>
2023-03-23 04:52:57 +0000

    Fix black screen while animating a closing and changing TF
    A TaskFragment could be resizing to a smaller bounds while changing
    to be invisible. A black screen was shown during the transition.
    So, skip unfreeze surface in that case.
    
    Also union the staring bounds to the whole closing screen bounds,
    or the leash will be cropped by the target screen bounds.
    
    Bug: 267044260
    Test: steps on the bug
    
    Change-Id: Ia037c5beaca12695fc6c00e38c454cb6dced1b33
    Merged-In: Ia037c5beaca12695fc6c00e38c454cb6dced1b33

--- services/core/java/com/android/server/wm/TaskFragment.java
+++ services/core/java/com/android/server/wm/TaskFragment.java
@@ -2498,13 +2498,18 @@ class TaskFragment extends WindowContainer<WindowContainer> {
         }
     }
 
-    /** Records the starting bounds of the closing organized TaskFragment. */
-    void setClosingChangingStartBoundsIfNeeded() {
+    /**
+     * Returns {@code true} if the starting bounds of the closing organized TaskFragment is
+     * recorded. Otherwise, return {@code false}.
+     */
+    boolean setClosingChangingStartBoundsIfNeeded() {
         if (isOrganizedTaskFragment() && mDisplayContent != null
                 && mDisplayContent.mChangingContainers.remove(this)) {
             mDisplayContent.mClosingChangingContainers.put(
                     this, new Rect(mSurfaceFreezer.mFreezeBounds));
+            return true;
         }
+        return false;
     }
 
     @Override
