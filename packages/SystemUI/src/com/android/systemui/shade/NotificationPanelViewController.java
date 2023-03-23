58cd612694eff84cb86fc161e9afa7afa78f9d8e
Alejandro Nijamkin <nijamkin@google.com>
2023-03-23 14:43:58 -0700

    [multi-shade] Bouncer touch integration.
    Connects multi-shade's touch interaction to the bouncer.
    
    On the lock screen such that dragging up when shades are collapsed brings up the bouncer.
    
    https://drive.google.com/file/d/1AiRelfhAg2QFY-EoHvHtG8V6KIzcST_g/view?usp=sharing
    
    Bug: 274159734
    Flag: DUAL_SHADE
    Test: unit tests expanded for MultiShadeEventInteractor
    Test: manually verified that:
    1. dragging up when shades are collapsed brings up the bouncer.
    2. back gesture hides the bouncer, allowing a drag down to show the dual
       shades again.
    3. unlocking the bouncer works as expected.
    
    Change-Id: I81118cb2ccba3bb2887def2568403198e7852c97

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -233,6 +233,8 @@ import com.android.systemui.util.Utils;
 import com.android.systemui.util.time.SystemClock;
 import com.android.wm.shell.animation.FlingAnimationUtils;
 
+import kotlin.Unit;
+
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -243,7 +245,6 @@ import java.util.function.Consumer;
 import javax.inject.Inject;
 import javax.inject.Provider;
 
-import kotlin.Unit;
 import kotlinx.coroutines.CoroutineDispatcher;
 
 @CentralSurfacesComponent.CentralSurfacesScope
@@ -387,6 +388,7 @@ public final class NotificationPanelViewController implements Dumpable {
     private KeyguardBottomAreaView mKeyguardBottomArea;
     private boolean mExpanding;
     private boolean mSplitShadeEnabled;
+    private final boolean mMultiShadeEnabled;
     /** The bottom padding reserved for elements of the keyguard measuring notifications. */
     private float mKeyguardNotificationBottomPadding;
     /**
@@ -851,6 +853,7 @@ public final class NotificationPanelViewController implements Dumpable {
         mFeatureFlags = featureFlags;
         mAnimateBack = mFeatureFlags.isEnabled(Flags.WM_SHADE_ANIMATE_BACK_GESTURE);
         mTrackpadGestureBack = mFeatureFlags.isEnabled(Flags.TRACKPAD_GESTURE_FEATURES);
+        mMultiShadeEnabled = mFeatureFlags.isEnabled(Flags.DUAL_SHADE);
         mFalsingCollector = falsingCollector;
         mPowerManager = powerManager;
         mWakeUpCoordinator = coordinator;
@@ -4018,8 +4021,11 @@ public final class NotificationPanelViewController implements Dumpable {
      *   {@link #updateVisibility()}? That would allow us to make this method private.
      */
     public void updatePanelExpansionAndVisibility() {
-        mShadeExpansionStateManager.onPanelExpansionChanged(
-                mExpandedFraction, isExpanded(), mTracking, mExpansionDragDownAmountPx);
+        if (!mMultiShadeEnabled) {
+            mShadeExpansionStateManager.onPanelExpansionChanged(
+                    mExpandedFraction, isExpanded(), mTracking, mExpansionDragDownAmountPx);
+        }
+
         updateVisibility();
     }
 
