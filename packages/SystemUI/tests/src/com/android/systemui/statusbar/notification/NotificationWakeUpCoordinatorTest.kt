6de9be1669aea06ede10c0b62b1edbd1a960725c
Jeff DeCew <jeffdq@google.com>
2023-03-28 20:52:51 +0000

    Create AnimatorTestRule2 which avoids the test interaction bugs with the original as it currently exists
    Bug: 269085199
    Bug: 275602127
    Test: atest SystemStatusAnimationSchedulerImplTest NotificationWakeUpCoordinatorTest
    Change-Id: Ib28b96ea7e19a57bf21ef66f6d7dd1e563a200ad

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/NotificationWakeUpCoordinatorTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/NotificationWakeUpCoordinatorTest.kt
@@ -18,7 +18,7 @@ package com.android.systemui.statusbar.notification
 
 import android.testing.AndroidTestingRunner
 import android.testing.TestableLooper
-import androidx.core.animation.AnimatorTestRule
+import androidx.core.animation.AnimatorTestRule2
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
 import com.android.systemui.dump.DumpManager
@@ -51,7 +51,7 @@ import org.mockito.Mockito.verifyNoMoreInteractions
 @TestableLooper.RunWithLooper(setAsMainLooper = true)
 class NotificationWakeUpCoordinatorTest : SysuiTestCase() {
 
-    @get:Rule val animatorTestRule = AnimatorTestRule()
+    @get:Rule val animatorTestRule = AnimatorTestRule2()
 
     private val dumpManager: DumpManager = mock()
     private val headsUpManager: HeadsUpManager = mock()
