6191809a4f99ee45ac1826d9b823e62c54c3b082
Prabir Pradhan <prabirmsp@google.com>
2023-03-30 14:03:31 +0000

    Deprecate PointerIcon#TYPE_DEFAULT API
    The PointerIcon#TYPE_DEFAULT constant has the same value as TYPE_ARROW,
    which makes it a redundant API. Especially now that styluses can also
    show pointer icons, this API is misleading, because not all pointer
    types have the same default icon. While the arrow might be the default
    for a mouse or trackpad, it will likely not be the default for styluses.
    Since this default is no longer correct, deprecate the API.
    
    The way to use the system's default icon is to return a null value for
    PointerIcon through the View APIs. This is not to be confused with
    TYPE_NULL, which shows no icon.
    
    Bug: 215436642
    Test: Build
    Change-Id: I89f55685b588dc39a0d2b34619e4187d9f041a56

--- core/java/android/view/PointerIcon.java
+++ core/java/android/view/PointerIcon.java
@@ -146,7 +146,14 @@ public final class PointerIcon implements Parcelable {
     // conflicts with any system types that may be defined in the future.
     private static final int TYPE_OEM_FIRST = 10000;
 
-    /** The default pointer icon. */
+    /**
+     * The default pointer icon.
+     * @deprecated This is the same as using {@link #TYPE_ARROW}. Use {@link #TYPE_ARROW} to
+     *     explicitly show an arrow, or use a {@code null} {@link PointerIcon} with
+     *     {@link View#setPointerIcon(PointerIcon)} or
+     *     {@link View#onResolvePointerIcon(MotionEvent, int)} instead to show
+     *     the default pointer icon.
+     */
     public static final int TYPE_DEFAULT = TYPE_ARROW;
 
     private static final PointerIcon gNullIcon = new PointerIcon(TYPE_NULL);
