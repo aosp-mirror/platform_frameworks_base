6f6ee8a55f37c2b8c0df041b2bd53ec928764597
Michael Groover <mpgroover@google.com>
2023-03-31 21:31:22 +0000

    Limit the number of supported v1 and v2 signers
    The v1 and v2 APK Signature Schemes support multiple signers; this
    was intended to allow multiple entities to sign an APK. Previously,
    the platform had no limits placed on the number of signers supported
    in an APK, but this commit sets a hard limit of 10 supported signers
    for these signature schemes to ensure a large number of signers
    does not place undue burden on the platform.
    
    Bug: 266580022
    Test: Manually verified the platform only allowed an APK with the
           maximum number of supported signers.
    Change-Id: I6aa86b615b203cdc69d58a593ccf8f18474ca091

--- core/java/android/util/apk/ApkSignatureSchemeV2Verifier.java
+++ core/java/android/util/apk/ApkSignatureSchemeV2Verifier.java
@@ -76,6 +76,11 @@ public class ApkSignatureSchemeV2Verifier {
     private static final int APK_SIGNATURE_SCHEME_V2_BLOCK_ID = 0x7109871a;
 
     /**
+     * The maximum number of signers supported by the v2 APK signature scheme.
+     */
+    private static final int MAX_V2_SIGNERS = 10;
+
+    /**
      * Returns {@code true} if the provided APK contains an APK Signature Scheme V2 signature.
      *
      * <p><b>NOTE: This method does not verify the signature.</b>
@@ -183,6 +188,11 @@ public class ApkSignatureSchemeV2Verifier {
         }
         while (signers.hasRemaining()) {
             signerCount++;
+            if (signerCount > MAX_V2_SIGNERS) {
+                throw new SecurityException(
+                        "APK Signature Scheme v2 only supports a maximum of " + MAX_V2_SIGNERS
+                                + " signers");
+            }
             try {
                 ByteBuffer signer = getLengthPrefixedSlice(signers);
                 X509Certificate[] certs = verifySigner(signer, contentDigests, certFactory);
