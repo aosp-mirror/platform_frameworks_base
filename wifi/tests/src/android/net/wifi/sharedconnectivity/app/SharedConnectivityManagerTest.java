6547db707a817c76b8d78a341515661b0cd7de65
Isaac Katzenelson <isaackatz@google.com>
2023-04-07 22:46:40 +0000

    Create returns null when resource strings are empty
    When resource string for the shared connectivity service or intent
    are empty, the create method now returns null instead of creating
    the manager.
    
    Bug: 277383431
    Test: atest SharedConnectivityManagerTest
    Change-Id: Ib192821ed2311fb80689cef3c9adc31bf2c21f5b

--- wifi/tests/src/android/net/wifi/sharedconnectivity/app/SharedConnectivityManagerTest.java
+++ wifi/tests/src/android/net/wifi/sharedconnectivity/app/SharedConnectivityManagerTest.java
@@ -105,6 +105,13 @@ public class SharedConnectivityManagerTest {
     }
 
     @Test
+    public void resourceStringsAreEmpty_createShouldReturnNull() {
+        when(mResources.getString(anyInt())).thenReturn("");
+
+        assertThat(SharedConnectivityManager.create(mContext)).isNull();
+    }
+
+    @Test
     public void bindingToServiceOnFirstCallbackRegistration() {
         SharedConnectivityManager manager = SharedConnectivityManager.create(mContext);
         manager.registerCallback(mExecutor, mClientCallback);
