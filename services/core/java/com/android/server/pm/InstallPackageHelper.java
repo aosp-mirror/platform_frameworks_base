c9a8baf585e8eb0f3272443930301a61331b65c1
Song Chun Fan <schfan@google.com>
2023-04-12 00:19:00 +0000

    Revert "Revert "Revert "[pm] reject data apps using privileged sharedUserId"""
    This reverts commit 9e8324160028c1a7e8bc01bde062fb04ed63728e.
    
    Reason for revert: b/277810794
    
    Change-Id: I1506e6ceba9b238716006eff0be481ccf2399555

--- services/core/java/com/android/server/pm/InstallPackageHelper.java
+++ services/core/java/com/android/server/pm/InstallPackageHelper.java
@@ -994,7 +994,7 @@ final class InstallPackageHelper {
                     reconciledPackages = ReconcilePackageUtils.reconcilePackages(
                             requests, Collections.unmodifiableMap(mPm.mPackages),
                             versionInfos, mSharedLibraries, mPm.mSettings.getKeySetManagerService(),
-                            mPm.mSettings, mContext);
+                            mPm.mSettings);
                 } catch (ReconcileFailure e) {
                     for (InstallRequest request : requests) {
                         request.setError("Reconciliation failed...", e);
@@ -3930,7 +3930,7 @@ final class InstallPackageHelper {
                                 mPm.mPackages, Collections.singletonMap(pkgName,
                                         mPm.getSettingsVersionForPackage(parsedPackage)),
                                 mSharedLibraries, mPm.mSettings.getKeySetManagerService(),
-                                mPm.mSettings, mContext);
+                                mPm.mSettings);
                 if ((scanFlags & SCAN_AS_APEX) == 0) {
                     appIdCreated = optimisticallyRegisterAppId(installRequest);
                 } else {
