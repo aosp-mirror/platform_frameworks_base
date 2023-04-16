2a6cec96471db8775b3782040fc4fb9daf0d3754
Alex Buynytskyy <alexbuy@google.com>
2023-04-16 22:18:56 +0000

    More robust tests.
    https://android-build.corp.google.com/test_investigate/?invocationId=I89000010151050377&testResultId=TR75528603005220067
    
    Bug: 290699552
    Test: atest PackageManagerTests
    
    Merged-In: Id68a28e9dc12f71286e29822d4b88be88ab03653
    Change-Id: Id68a28e9dc12f71286e29822d4b88be88ab03653
    (cherry picked from commit 18e9fa7232de34b0f106b895818f4b4991a0da68)

--- services/tests/PackageManagerServiceTests/server/src/com/android/server/PackageManagerTests.java
+++ services/tests/PackageManagerServiceTests/server/src/com/android/server/PackageManagerTests.java
@@ -197,10 +197,11 @@ public class PackageManagerTests extends AndroidTestCase {
         }
 
         public Intent getResult() {
-            try {
-                return mResult.take();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            while (true) {
+                try {
+                    return mResult.take();
+                } catch (InterruptedException e) {
+                }
             }
         }
     }
