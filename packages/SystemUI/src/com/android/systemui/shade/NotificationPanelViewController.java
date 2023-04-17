301078629d95fc3e61aa51cf9be047632491a203
Shawn Lee <syeonlee@google.com>
2023-04-17 15:05:23 -0700

    DO NOT MERGE Hide EmptyShadeView when on bouncer
    Added a check to make sure EmptyShadeView is hidden when bouncer is showing.
    
    Bug: 267060171
    Test: manually verified EmptyShadeView visibility
    (cherry picked from commit bef2be6757eec92391189c106e89b63176cd16c0)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2e640447d16c4e893927e2f76bda8b1073bb18f8)
    Merged-In: I2bfdc7801cec1b3aaa44f841d8a821214c6cb801
    Change-Id: I2bfdc7801cec1b3aaa44f841d8a821214c6cb801

--- packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/shade/NotificationPanelViewController.java
@@ -2843,6 +2843,7 @@ public final class NotificationPanelViewController implements Dumpable {
     /** Set whether the bouncer is showing. */
     public void setBouncerShowing(boolean bouncerShowing) {
         mBouncerShowing = bouncerShowing;
+        mNotificationStackScrollLayoutController.updateShowEmptyShadeView();
         updateVisibility();
     }
 
