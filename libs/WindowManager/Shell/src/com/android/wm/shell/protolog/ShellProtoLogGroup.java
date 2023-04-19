72bb2e580da150c9516d6895c850281814bc991c
Winson Chung <winsonc@google.com>
2023-04-19 06:39:48 +0000

    Cancel existing recents animations if a no-op transitition is detected
    - This is not ideal behavior, but prevents a worse case where gesture
      nav is stuck due to an orphan recents animation runner not getting
      any callbacks.
    - Also tweak logs to print controller instance
    
    Bug: 275561141
    Test: Quickswitch and swipe up repeatedly
    Change-Id: Iea074fffe4d1b7442a2a3b1304fb727adf4fda68

--- libs/WindowManager/Shell/src/com/android/wm/shell/protolog/ShellProtoLogGroup.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/protolog/ShellProtoLogGroup.java
@@ -33,7 +33,7 @@ public enum ShellProtoLogGroup implements IProtoLogGroup {
     WM_SHELL_TRANSITIONS(Consts.ENABLE_DEBUG, Consts.ENABLE_LOG_TO_PROTO_DEBUG, true,
             Consts.TAG_WM_SHELL),
     WM_SHELL_RECENTS_TRANSITION(Consts.ENABLE_DEBUG, Consts.ENABLE_LOG_TO_PROTO_DEBUG, true,
-            Consts.TAG_WM_SHELL),
+            "ShellRecents"),
     WM_SHELL_DRAG_AND_DROP(Consts.ENABLE_DEBUG, Consts.ENABLE_LOG_TO_PROTO_DEBUG, true,
             Consts.TAG_WM_SHELL),
     WM_SHELL_STARTING_WINDOW(Consts.ENABLE_DEBUG, Consts.ENABLE_LOG_TO_PROTO_DEBUG, false,
