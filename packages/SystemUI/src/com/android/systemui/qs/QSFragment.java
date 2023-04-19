3b9787579c77098522f39e350b948aab5a5f6602
Jordan Demeulenaere <jdemeulenaere@google.com>
2023-04-19 17:30:38 +0200

    Fix flicker of QS footer actions when restarting SystemUI (1/2)
    This CL fixes a flicker that could happen with the footer actions View
    when SystemUI is starting. This CL is changing the default visibility of
    that View to INVISIBLE instead of VISIBLE.
    
    I also had to make FooterActionsViewBinder a class instead of a static
    object to make it mockable in QSFragmentTest.
    
    Bug: 263699931
    Test: atest FooterActionsViewModelTest
    Test: Manual, crash SystemUI
    Change-Id: I0f4344cf52dcb00a8b5b38d8a259c0188f13433d

--- packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFragment.java
@@ -119,6 +119,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
     private final QSLogger mLogger;
     private final FooterActionsController mFooterActionsController;
     private final FooterActionsViewModel.Factory mFooterActionsViewModelFactory;
+    private final FooterActionsViewBinder mFooterActionsViewBinder;
     private final ListeningAndVisibilityLifecycleOwner mListeningAndVisibilityLifecycleOwner;
     private boolean mShowCollapsedOnKeyguard;
     private boolean mLastKeyguardAndExpanded;
@@ -176,6 +177,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
             DumpManager dumpManager, QSLogger qsLogger,
             FooterActionsController footerActionsController,
             FooterActionsViewModel.Factory footerActionsViewModelFactory,
+            FooterActionsViewBinder footerActionsViewBinder,
             LargeScreenShadeInterpolator largeScreenShadeInterpolator,
             FeatureFlags featureFlags) {
         mRemoteInputQuickSettingsDisabler = remoteInputQsDisabler;
@@ -192,6 +194,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
         mDumpManager = dumpManager;
         mFooterActionsController = footerActionsController;
         mFooterActionsViewModelFactory = footerActionsViewModelFactory;
+        mFooterActionsViewBinder = footerActionsViewBinder;
         mListeningAndVisibilityLifecycleOwner = new ListeningAndVisibilityLifecycleOwner();
     }
 
@@ -284,7 +287,7 @@ public class QSFragment extends LifecycleFragment implements QS, CommandQueue.Ca
 
         if (!ComposeFacade.INSTANCE.isComposeAvailable()) {
             Log.d(TAG, "Binding the View implementation of the QS footer actions");
-            FooterActionsViewBinder.bind(footerActionsView, mQSFooterActionsViewModel,
+            mFooterActionsViewBinder.bind(footerActionsView, mQSFooterActionsViewModel,
                     mListeningAndVisibilityLifecycleOwner);
             return;
         }
