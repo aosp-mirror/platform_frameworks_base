e3d243d3e7740bd856e2ba5ce53802c39b3cc341
Riddle Hsu <riddlehsu@google.com>
2023-04-25 12:28:05 +0000

    Update config according to insets of status bar
    Previously, only cutout and navbar are included, so only the change
    of decor frame may trigger config change.
    But screenWidthDp and screenHeightDp are calculated according to
    config (stable) frame, which includes the insets from status bar.
    
    So this change makes the display configuration consistent with the
    latest state of the display decor insets.
    
    Fix: 259693044
    Test: DisplayPolicyTests#testUpdateDisplayConfigurationByDecor
    
    Change-Id: Idaa7280ed23076b3282c987a249259e0c38b6630

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1831,7 +1831,7 @@ public class WindowManagerService extends IWindowManager.Stub
 
             boolean needToSendNewConfiguration =
                     win.isVisibleRequestedOrAdding() && displayContent.updateOrientation();
-            if (win.providesNonDecorInsets()) {
+            if (win.providesDisplayDecorInsets()) {
                 needToSendNewConfiguration |= displayPolicy.updateDecorInsetsInfo();
             }
             if (needToSendNewConfiguration) {
@@ -2274,7 +2274,7 @@ public class WindowManagerService extends IWindowManager.Stub
                         & WindowManager.LayoutParams.SYSTEM_UI_VISIBILITY_CHANGED) != 0) {
                     win.mLayoutNeeded = true;
                 }
-                if (layoutChanged && win.providesNonDecorInsets()) {
+                if (layoutChanged && win.providesDisplayDecorInsets()) {
                     configChanged = displayPolicy.updateDecorInsetsInfo();
                 }
                 if (win.mActivityRecord != null && ((flagChanges & FLAG_SHOW_WHEN_LOCKED) != 0
