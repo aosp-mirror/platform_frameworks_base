61e037a7b732f3f83378036ede164a4423407085
Riddle Hsu <riddlehsu@google.com>
2023-04-26 22:20:42 +0800

    Set fixed transform hint on app level rotated surface
    This is similar to [1], but the transform is applied when
    switching activities with different requested orientations.
    Then the transform will be applied on ActivityRecord before
    display rotation happens.
    
    [1]: Id4ad45adb9719b078f46040a09f786092c325a8a
    
    Bug: 270282500
    Test: atest SurfaceControlTests#testSurfaceChangedOnRotation
    Change-Id: I6650eef0b12d84f7014e5afc280c1cb5e907d7b2

--- services/core/java/com/android/server/wm/WindowToken.java
+++ services/core/java/com/android/server/wm/WindowToken.java
@@ -596,6 +596,7 @@ class WindowToken extends WindowContainer<WindowState> {
                 .build();
         t.setPosition(leash, mLastSurfacePosition.x, mLastSurfacePosition.y);
         t.reparent(getSurfaceControl(), leash);
+        t.setFixedTransformHint(leash, getWindowConfiguration().getDisplayRotation());
         mFixedRotationTransformLeash = leash;
         updateSurfaceRotation(t, rotation, mFixedRotationTransformLeash);
         return mFixedRotationTransformLeash;
