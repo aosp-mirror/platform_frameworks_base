f6c1afbf31f0435cced1d442f9846d4c72e3ecf9
Ian Baker <ibaker@google.com>
2023-04-27 13:52:54 +0000

    Revert "Notify/bind app after the phone is unlocked"
    This reverts commit 0b1ebd7cda9b062b45ba2708bd5cf9e73ebcfe00.
    
    Reason for revert: DroidMonitor: b/279841572
    
    Change-Id: Ieab5c60d55f47fa862aba4b3a9f4f4dce759bbab

--- services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
+++ services/companion/java/com/android/server/companion/CompanionDeviceManagerService.java
@@ -53,7 +53,6 @@ import android.app.ActivityManagerInternal;
 import android.app.AppOpsManager;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.bluetooth.BluetoothDevice;
 import android.companion.AssociationInfo;
 import android.companion.AssociationRequest;
 import android.companion.DeviceNotAssociatedException;
@@ -228,7 +227,7 @@ public class CompanionDeviceManagerService extends SystemService {
         loadAssociationsFromDisk();
         mAssociationStore.registerListener(mAssociationStoreChangeListener);
 
-        mDevicePresenceMonitor = new CompanionDevicePresenceMonitor(mUserManager,
+        mDevicePresenceMonitor = new CompanionDevicePresenceMonitor(
                 mAssociationStore, mDevicePresenceCallback);
 
         mAssociationRequestsProcessor = new AssociationRequestsProcessor(
@@ -312,21 +311,6 @@ public class CompanionDeviceManagerService extends SystemService {
         BackgroundThread.getHandler().sendMessageDelayed(
                 obtainMessage(CompanionDeviceManagerService::maybeGrantAutoRevokeExemptions, this),
                 MINUTES.toMillis(10));
-    }
-
-    @Override
-    public void onUserUnlocked(@NonNull TargetUser user) {
-        // Notify and bind the app after the phone is unlocked.
-        final int userId = user.getUserIdentifier();
-        final Set<BluetoothDevice> blueToothDevices =
-                mDevicePresenceMonitor.getPendingReportConnectedDevices().get(userId);
-        for (BluetoothDevice bluetoothDevice : blueToothDevices) {
-            for (AssociationInfo ai:
-                    mAssociationStore.getAssociationsByAddress(bluetoothDevice.getAddress())) {
-                Slog.i(TAG, "onUserUnlocked, device id( " + ai.getId() + " ) is connected");
-                mDevicePresenceMonitor.onBluetoothCompanionDeviceConnected(ai.getId());
-            }
-        }
     }
 
     @NonNull
