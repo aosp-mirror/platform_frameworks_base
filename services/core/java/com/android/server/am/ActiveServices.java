51e431932ac8fa73d4900c8bbe6c4841779118e3
Makoto Onuki <omakoto@google.com>
2023-04-28 14:08:13 -0700

    Add UID states and capabiliites to FGS atom
    Bug: 270963079
    Test: statsd_testdrive 60
      Then start an FGS and make sure the new fields are filled in.
    Change-Id: I0388b53a85b0c9dbf6089a662fac47cb60685a60

--- services/core/java/com/android/server/am/ActiveServices.java
+++ services/core/java/com/android/server/am/ActiveServices.java
@@ -8217,7 +8217,11 @@ public final class ActiveServices {
                         : ForegroundServiceDelegationOptions.DELEGATION_SERVICE_DEFAULT,
                 0 /* api_sate */,
                 null /* api_type */,
-                null /* api_timestamp */);
+                null /* api_timestamp */,
+                mAm.getUidStateLocked(r.appInfo.uid),
+                mAm.getUidProcessCapabilityLocked(r.appInfo.uid),
+                mAm.getUidStateLocked(r.mRecentCallingUid),
+                mAm.getUidProcessCapabilityLocked(r.mRecentCallingUid));
 
         int event = 0;
         if (state == FOREGROUND_SERVICE_STATE_CHANGED__STATE__ENTER) {
