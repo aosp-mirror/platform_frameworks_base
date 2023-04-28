e173d498732c0a0a68e8aa7de81754f1f8376cce
Ioana Alexandru <aioana@google.com>
2023-04-28 14:11:04 +0000

    DO NOT MERGE Increase notification channel limit.
    This was previously reduced because it can cause an out of memory error or be abused to trigger a permanent denial of service, but it breaks some messages apps in Android 11, as they are creating too many channels. Rolling it back until the apps are fixed to reduce user impact.
    
    Bug: 279447569
    Bug: 261723753
    Test: N/A, this was previously 50k
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:de7c43eb607fbc33677c4391586075861950747a)
    Merged-In: Id8da382f812d4abb8db723c40a61366a7402da4f
    Change-Id: Id8da382f812d4abb8db723c40a61366a7402da4f

--- services/core/java/com/android/server/notification/PreferencesHelper.java
+++ services/core/java/com/android/server/notification/PreferencesHelper.java
@@ -92,7 +92,7 @@ public class PreferencesHelper implements RankingConfig {
     private static final String NON_BLOCKABLE_CHANNEL_DELIM = ":";
 
     @VisibleForTesting
-    static final int NOTIFICATION_CHANNEL_COUNT_LIMIT = 5000;
+    static final int NOTIFICATION_CHANNEL_COUNT_LIMIT = 50000;
     @VisibleForTesting
     static final int NOTIFICATION_CHANNEL_GROUP_COUNT_LIMIT = 6000;
 
