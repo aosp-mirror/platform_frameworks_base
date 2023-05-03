cd8f5d8b18fdd66ebb992b2a6ee3a587b6fd5e42
Tomislav Novak <tnovak@meta.com>
2023-05-03 14:36:51 -0700

    Ignore BIND_ABOVE_CLIENT for same-process connections
    Binding to a service using BIND_ABOVE_CLIENT affects the OOM adjustment
    of both the client and the service; client's value is dropped by one
    level in modifyRawOomAdj() to make sure it's lower than the service's.
    
    Doing this unconditionally, however, means that the process' OOM score
    will be reduced if binding to a service that lives in the same process
    as the client. For example, a top app would have its oom_score_adj set
    to 100 (visible) rather than 0 (foreground).
    
    Test: atest MockingOomAdjusterTests
    Signed-off-by: Tomislav Novak <tnovak@meta.com>
    Merged-In: Iee5024b8f13771bc98a39a20e5f96a89a4c79b4e
    Change-Id: Iee5024b8f13771bc98a39a20e5f96a89a4c79b4e

--- services/core/java/com/android/server/am/ProcessServiceRecord.java
+++ services/core/java/com/android/server/am/ProcessServiceRecord.java
@@ -258,7 +258,8 @@ final class ProcessServiceRecord {
         mHasAboveClient = false;
         for (int i = mConnections.size() - 1; i >= 0; i--) {
             ConnectionRecord cr = mConnections.valueAt(i);
-            if ((cr.flags & Context.BIND_ABOVE_CLIENT) != 0) {
+            if (cr.binding.service.app.mServices != this
+                    && (cr.flags & Context.BIND_ABOVE_CLIENT) != 0) {
                 mHasAboveClient = true;
                 break;
             }
