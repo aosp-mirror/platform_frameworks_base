8f00d3e9152838197afd8a87f75f14a7f18fbe5d
Ioana Alexandru <aioana@google.com>
2023-05-08 18:39:35 +0000

    Verify URI permissions for EXTRA_REMOTE_INPUT_HISTORY_ITEMS.
    Also added a step to serialize & deserialize the notification in the
    test, to prevent exceptions about not being able to cast e.g.
    Parcelable[] to RemoteInputHistoryItem[].
    
    Test: atest NotificationManagerServiceTest & tested with POC from bug
    Bug: 276729064
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:4e19431a60300c6ea6c7f7dd64299916e4eb09bc)
    Merged-In: I7053ca59f9c7f1df5226418594109cfb8b609b1e
    Change-Id: I7053ca59f9c7f1df5226418594109cfb8b609b1e

--- services/tests/uiservicestests/src/com/android/server/notification/NotificationManagerServiceTest.java
+++ services/tests/uiservicestests/src/com/android/server/notification/NotificationManagerServiceTest.java
@@ -129,6 +129,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Person;
 import android.app.RemoteInput;
+import android.app.RemoteInputHistoryItem;
 import android.app.StatsManager;
 import android.app.admin.DevicePolicyManagerInternal;
 import android.app.usage.UsageStatsManagerInternal;
@@ -5415,6 +5416,12 @@ public class NotificationManagerServiceTest extends UiServiceTestCase {
                 .setName("People List Person 2")
                 .setIcon(personIcon3)
                 .build();
+        final Uri historyUri1 = Uri.parse("content://com.example/history1");
+        final Uri historyUri2 = Uri.parse("content://com.example/history2");
+        final RemoteInputHistoryItem historyItem1 = new RemoteInputHistoryItem(null, historyUri1,
+                "a");
+        final RemoteInputHistoryItem historyItem2 = new RemoteInputHistoryItem(null, historyUri2,
+                "b");
 
         Bundle extras = new Bundle();
         extras.putParcelable(Notification.EXTRA_AUDIO_CONTENTS_URI, audioContents);
@@ -5422,6 +5429,8 @@ public class NotificationManagerServiceTest extends UiServiceTestCase {
         extras.putParcelable(Notification.EXTRA_MESSAGING_PERSON, person1);
         extras.putParcelableArrayList(Notification.EXTRA_PEOPLE_LIST,
                 new ArrayList<>(Arrays.asList(person2, person3)));
+        extras.putParcelableArray(Notification.EXTRA_REMOTE_INPUT_HISTORY_ITEMS,
+                new RemoteInputHistoryItem[]{historyItem1, historyItem2});
 
         Notification n = new Notification.Builder(mContext, "a")
                 .setContentTitle("notification with uris")
@@ -5429,6 +5438,13 @@ public class NotificationManagerServiceTest extends UiServiceTestCase {
                 .setLargeIcon(largeIcon)
                 .addExtras(extras)
                 .build();
+
+        // Serialize and deserialize the notification to make sure nothing breaks in the process,
+        // since that's what will usually happen before we get to call visitUris.
+        Parcel parcel = Parcel.obtain();
+        n.writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        n = new Notification(parcel);
 
         Consumer<Uri> visitor = (Consumer<Uri>) spy(Consumer.class);
         n.visitUris(visitor);
@@ -5439,6 +5455,8 @@ public class NotificationManagerServiceTest extends UiServiceTestCase {
         verify(visitor, times(1)).accept(eq(personIcon1.getUri()));
         verify(visitor, times(1)).accept(eq(personIcon2.getUri()));
         verify(visitor, times(1)).accept(eq(personIcon3.getUri()));
+        verify(visitor, times(1)).accept(eq(historyUri1));
+        verify(visitor, times(1)).accept(eq(historyUri2));
     }
 
     @Test
