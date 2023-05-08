7fbd6b41a6a5700cddabfc23892f48b02d23d6bb
Brandon Liu <branliu@google.com>
2023-05-08 17:47:57 +0000

    Make ProtoSerialize using uint32_t for TYPE_INT_DEC
    Bug: b/276926301
    Test: Verified affected atests pass
    Change-Id: I0df005625a1c02956c5cf3a00c23944319104f9e

--- tools/aapt2/format/proto/ProtoDeserialize.cpp
+++ tools/aapt2/format/proto/ProtoDeserialize.cpp
@@ -916,7 +916,7 @@ std::unique_ptr<Item> DeserializeItemFromPb(const pb::Item& pb_item,
         } break;
         case pb::Primitive::kIntDecimalValue: {
           val.dataType = android::Res_value::TYPE_INT_DEC;
-          val.data = static_cast<uint32_t>(pb_prim.int_decimal_value());
+          val.data = static_cast<int32_t>(pb_prim.int_decimal_value());
         } break;
         case pb::Primitive::kIntHexadecimalValue: {
           val.dataType = android::Res_value::TYPE_INT_HEX;
