8e7b11c5db5cdc86beaad810beee468c8b9cf64f
Helen Qin <helenqin@google.com>
2023-05-10 01:46:52 +0000

    Bug fixes for the create flow auto select.
    1. Create flow auto select should trigger regardless of remote entry's presence
    2. Parsing of the password & passkey requires a jetpack update. For this
       current release, will manually parse.
    
    Test: manual (see bug for recording)
    Bug: 281726583
    Change-Id: I69d57f7ae38eb6f7dc3ba914ddc739888e83ef1e

--- packages/CredentialManager/src/com/android/credentialmanager/DataConverter.kt
+++ packages/CredentialManager/src/com/android/credentialmanager/DataConverter.kt
@@ -483,7 +483,10 @@ class CreateFlowUtils {
                     createCredentialRequestJetpack.preferImmediatelyAvailableCredentials,
                     appPreferredDefaultProviderId = appPreferredDefaultProviderId,
                     userSetDefaultProviderIds = requestInfo.defaultProviderIds.toSet(),
-                    isAutoSelectRequest = createCredentialRequestJetpack.isAutoSelectAllowed,
+                    // The jetpack library requires a fix to parse this value correctly for
+                    // the password type. For now, directly parse it ourselves.
+                    isAutoSelectRequest = createCredentialRequest.credentialData.getBoolean(
+                        Constants.BUNDLE_KEY_PREFER_IMMEDIATELY_AVAILABLE_CREDENTIALS, false),
                 )
                 is CreatePublicKeyCredentialRequest -> {
                     newRequestDisplayInfoFromPasskeyJson(
@@ -494,7 +497,10 @@ class CreateFlowUtils {
                         createCredentialRequestJetpack.preferImmediatelyAvailableCredentials,
                         appPreferredDefaultProviderId = appPreferredDefaultProviderId,
                         userSetDefaultProviderIds = requestInfo.defaultProviderIds.toSet(),
-                        isAutoSelectRequest = createCredentialRequestJetpack.isAutoSelectAllowed,
+                        // The jetpack library requires a fix to parse this value correctly for
+                        // the passkey type. For now, directly parse it ourselves.
+                        isAutoSelectRequest = createCredentialRequest.credentialData.getBoolean(
+                            Constants.BUNDLE_KEY_PREFER_IMMEDIATELY_AVAILABLE_CREDENTIALS, false),
                     )
                 }
                 is CreateCustomCredentialRequest -> {
