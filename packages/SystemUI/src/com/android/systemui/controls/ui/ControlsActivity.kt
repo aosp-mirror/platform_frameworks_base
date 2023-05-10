b7adc6843c2f236b237bf2314b9f6094e3c086b0
Brandon Liu <branliu@google.com>
2023-05-10 23:04:30 +0000

    Revert "Lifecycle has to use kotlin syntax"
    Revert submission 22208109-lifecycle_9778738
    
    Reason for revert: <ACA identified this is the culprit of build failure b/281883862>
    
    Reverted changes: /q/submissionid:22208109-lifecycle_9778738
    
    Change-Id: I9a5249529dd9b7a8d4c9c59a61b3a780371d7e6b

--- packages/SystemUI/src/com/android/systemui/controls/ui/ControlsActivity.kt
+++ packages/SystemUI/src/com/android/systemui/controls/ui/ControlsActivity.kt
@@ -71,7 +71,7 @@ open class ControlsActivity @Inject constructor(
 
         setContentView(R.layout.controls_fullscreen)
 
-        lifecycle.addObserver(
+        getLifecycle().addObserver(
             ControlsAnimations.observerForAnimations(
                 requireViewById(R.id.control_detail_root),
                 window,
