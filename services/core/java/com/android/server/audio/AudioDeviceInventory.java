7282eb5e2e1a3d32cbe2b886e71c64ee0dbd9828
Eric Laurent <elaurent@google.com>
2023-05-11 13:33:51 +0200

    Revert "AudioService: synchronize BT_SCO on and A2DP or LE suspended states"
    This reverts commit c0184adb64974c8a1f947a923832909ecec465db.
    Bug 281786703
    Test: make
    
    Change-Id: I81bda9127c951850adcda43c010fe3d560395dd4

--- services/core/java/com/android/server/audio/AudioDeviceInventory.java
+++ services/core/java/com/android/server/audio/AudioDeviceInventory.java
@@ -1477,7 +1477,7 @@ public class AudioDeviceInventory {
         }
 
         // Reset A2DP suspend state each time a new sink is connected
-        mDeviceBroker.clearA2dpSuspended();
+        mAudioSystem.setParameters("A2dpSuspended=false");
 
         // The convention for head tracking sensors associated with A2DP devices is to
         // use a UUID derived from the MAC address as follows:
@@ -1750,8 +1750,7 @@ public class AudioDeviceInventory {
     private void makeA2dpDeviceUnavailableLater(String address, int delayMs) {
         // prevent any activity on the A2DP audio output to avoid unwanted
         // reconnection of the sink.
-        mDeviceBroker.setA2dpSuspended(
-                true /*enable*/, true /*internal*/, "makeA2dpDeviceUnavailableLater");
+        mAudioSystem.setParameters("A2dpSuspended=true");
         // retrieve DeviceInfo before removing device
         final String deviceKey =
                 DeviceInfo.makeDeviceListKey(AudioSystem.DEVICE_OUT_BLUETOOTH_A2DP, address);
@@ -1898,7 +1897,7 @@ public class AudioDeviceInventory {
                         "LE Audio device addr=" + address + " now available").printLog(TAG));
             }
             // Reset LEA suspend state each time a new sink is connected
-            mDeviceBroker.clearLeAudioSuspended();
+            mAudioSystem.setParameters("LeAudioSuspended=false");
 
             UUID sensorUuid = UuidUtils.uuidFromAudioDeviceAttributes(ada);
             mConnectedDevices.put(DeviceInfo.makeDeviceListKey(device, address),
@@ -1953,8 +1952,7 @@ public class AudioDeviceInventory {
     private void makeLeAudioDeviceUnavailableLater(String address, int device, int delayMs) {
         // prevent any activity on the LEA output to avoid unwanted
         // reconnection of the sink.
-        mDeviceBroker.setLeAudioSuspended(
-                true /*enable*/, true /*internal*/, "makeLeAudioDeviceUnavailableLater");
+        mAudioSystem.setParameters("LeAudioSuspended=true");
         // the device will be made unavailable later, so consider it disconnected right away
         mConnectedDevices.remove(DeviceInfo.makeDeviceListKey(device, address));
         // send the delayed message to make the device unavailable later
