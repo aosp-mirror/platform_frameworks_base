378447f6948edf1b1f66b0711f9bdb76c2d3721a
Fabian Kozynski <kozynski@google.com>
2023-05-11 19:54:54 +0000

    Support restoring TileService before app install
    Before, CustomTile were destroyed if the app was not installed (before
    that, they were kept as UNAVAILABLE, but that caused other issues). This
    was an issue when restoring from a backup, as the setting would include
    custom tiles that were restored before the app was installed and
    therefore immediately removed (and never brought back).
    
    With this change:
    * Do not create CustomTile that don't have a corresponding valid
      component. Instead, keep them in the setting and mark them as
      NotInstalled internally.
    * If the component later appears, actually create the tile, and send
      `onTileAdded`.
    
    Note that if a component for a TileService disappears (it's
    uninstalled), the tile will be automatically removed and would need to
    be manually added. This is existing behavior.
    
    Finally, in order to prevent race conditions when a package with
    multiple tiles is uninstalled, use a mutex whenever making changes to
    the setting.
    
    Fixes: 282003318
    Test: atest com.android.systemui.qs
    Test: atest CtsTileServiceTestCases
    Test: manual, add custom tiles through adb to settings and later install
    app
    Test: atest android.host.systemui
    Flag: QS_PIPELINE_NEW_HOST
    Change-Id: I0a774969ad20611103b09ce4ace6175ad53350ce
    
    Change-Id: I8b48f45124928702fcdcf88484ccf3b501ea86ed

---
+++ packages/SystemUI/src/com/android/systemui/util/kotlin/PackageManagerExt.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.util.kotlin
+
+import android.annotation.WorkerThread
+import android.content.pm.ComponentInfo
+import android.content.pm.PackageManager
+import com.android.systemui.util.Assert
+
+@WorkerThread
+fun PackageManager.isComponentActuallyEnabled(componentInfo: ComponentInfo): Boolean {
+    Assert.isNotMainThread()
+    return when (getComponentEnabledSetting(componentInfo.componentName)) {
+        PackageManager.COMPONENT_ENABLED_STATE_ENABLED -> true
+        PackageManager.COMPONENT_ENABLED_STATE_DISABLED -> false
+        PackageManager.COMPONENT_ENABLED_STATE_DEFAULT -> componentInfo.isEnabled
+        else -> false
+    }
+}
