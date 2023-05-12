4111de7b391253c0c54035fa880fc860207c8f2c
Ioana Alexandru <aioana@google.com>
2023-05-12 15:41:09 +0000

    Implement visitUris for RemoteViews ViewGroupActionAdd.
    This is to prevent a vulnerability where notifications can show
    resources belonging to other users, since the URI in the nested views
    was not being checked.
    
    Bug: 277740082
    Test: atest RemoteViewsTest NotificationVisitUrisTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:850fd984e5f346645b5a941ed7307387c7e4c4de)
    Merged-In: I5c71f0bad0a6f6361eb5ceffe8d1e47e936d78f8
    Change-Id: I5c71f0bad0a6f6361eb5ceffe8d1e47e936d78f8

--- core/java/android/widget/RemoteViews.java
+++ core/java/android/widget/RemoteViews.java
@@ -2560,6 +2560,11 @@ public class RemoteViews implements Parcelable, Filter {
         public int getActionTag() {
             return VIEW_GROUP_ACTION_ADD_TAG;
         }
+
+        @Override
+        public final void visitUris(@NonNull Consumer<Uri> visitor) {
+            mNestedViews.visitUris(visitor);
+        }
     }
 
     /**
