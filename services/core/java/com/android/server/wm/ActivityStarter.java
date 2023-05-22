c8b220a6287829d0e67afd7f79c4d436600591e1
Hani Kazmi <hanikazmi@google.com>
2023-05-22 15:19:10 +0000

    Update Pip launches to not enter pinned task if in background.
    Addresses a BAL bypass where Pip could be started without the launcher
    being visible.
    
    Bug: 271576718
    Test: atest CtsWindowManagerDeviceTestCases:PinnedStackTests
    Test: atest android.server.wm.BackgroundActivityLaunchTest#testPipCannotStartFromBackground
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b263d3beed7a412ac342c63956f213b70d6e2679)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:291d124257f6e46862a813305892b02b9a8f851c)
    Merged-In: Icfe0a17d7f6f127acaae8400a97e8bdc53fcc9ad
    Change-Id: Icfe0a17d7f6f127acaae8400a97e8bdc53fcc9ad

--- services/core/java/com/android/server/wm/ActivityStarter.java
+++ services/core/java/com/android/server/wm/ActivityStarter.java
@@ -1709,7 +1709,8 @@ class ActivityStarter {
         // If Activity's launching into PiP, move the mStartActivity immediately to pinned mode.
         // Note that mStartActivity and source should be in the same Task at this point.
         if (mOptions != null && mOptions.isLaunchIntoPip()
-                && sourceRecord != null && sourceRecord.getTask() == mStartActivity.getTask()) {
+                && sourceRecord != null && sourceRecord.getTask() == mStartActivity.getTask()
+                && balCode != BAL_BLOCK) {
             mRootWindowContainer.moveActivityToPinnedRootTask(mStartActivity,
                     sourceRecord, "launch-into-pip");
         }
