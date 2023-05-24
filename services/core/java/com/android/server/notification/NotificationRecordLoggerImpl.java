4f2c84485c037c5eb03246c89d13a274b553ad1b
Lyn <lynhan@google.com>
2023-05-24 21:46:29 +0000

    Log FSI fields on NotificationReported
    Bug: 280345058
    Test: NotificationRecordLoggerTest
    Test: adb shell setprop persist.sysui.notification.show_sticky_hun_for_denied_fsi 1
    m statsd_testdrive && $ANDROID_HOST_OUT/bin/statsd_testdrive -terse 90 244
    
    Change-Id: Ib6677f58973c24ee747b7ad9b031f66971841619

--- services/core/java/com/android/server/notification/NotificationRecordLoggerImpl.java
+++ services/core/java/com/android/server/notification/NotificationRecordLoggerImpl.java
@@ -75,7 +75,9 @@ class NotificationRecordLoggerImpl implements NotificationRecordLogger {
                 notificationReported.is_foreground_service,
                 notificationReported.timeout_millis,
                 notificationReported.is_non_dismissible,
-                notificationReported.post_duration_millis);
+                notificationReported.post_duration_millis,
+                notificationReported.fsi_state,
+                notificationReported.is_locked);
     }
 
     @Override
