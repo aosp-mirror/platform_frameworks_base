4bf71d74fc21cd9389dbe00fb750e2f9802eb789
Hongwei Wang <hwwang@google.com>
2023-05-24 20:19:43 -0700

    Disallow loading icon from content URI to PipMenu
    Bug: 278246904
    Test: manually, with the PoC app attached to the bug
    Merged-In: Ib3f5b8b6b9ce644fdf1173548d9078e4d969ae2e
    Change-Id: Idbd4081bf464e2b3420d4c3fd22ca37867d26bc0

--- libs/WindowManager/Shell/src/com/android/wm/shell/pip/phone/PipMenuView.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/pip/phone/PipMenuView.java
@@ -41,6 +41,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -462,13 +463,19 @@ public class PipMenuView extends FrameLayout {
                     final PipMenuActionView actionView =
                             (PipMenuActionView) mActionsGroup.getChildAt(i);
 
-                    // TODO: Check if the action drawable has changed before we reload it
-                    action.getIcon().loadDrawableAsync(mContext, d -> {
-                        if (d != null) {
-                            d.setTint(Color.WHITE);
-                            actionView.setImageDrawable(d);
-                        }
-                    }, mMainHandler);
+                    final int iconType = action.getIcon().getType();
+                    if (iconType == Icon.TYPE_URI || iconType == Icon.TYPE_URI_ADAPTIVE_BITMAP) {
+                        // Disallow loading icon from content URI
+                        actionView.setImageDrawable(null);
+                    } else {
+                        // TODO: Check if the action drawable has changed before we reload it
+                        action.getIcon().loadDrawableAsync(mContext, d -> {
+                            if (d != null) {
+                                d.setTint(Color.WHITE);
+                                actionView.setImageDrawable(d);
+                            }
+                        }, mMainHandler);
+                    }
                     actionView.setContentDescription(action.getContentDescription());
                     if (action.isEnabled()) {
                         actionView.setOnClickListener(v -> {
