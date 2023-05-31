4b8bfd8e716b9dc829f4157aad6c3740b22c14c0
Aishwarya Mallampati <amallampati@google.com>
2023-05-31 23:36:49 +0000

    Add isNonTerrestrialNetwork apis in NetworkRegistrationInfo for satellite connectivity
    Bug: 285189512
    Test: Flashed build on raven-userdebug and performed basic functionality
    tests,
    atest NetworkRegistrationInfoTest,
    atest ServiceStateTest
    
    Change-Id: Idc106f26209c805cee898e7358574523617a3365

--- telephony/java/android/telephony/ServiceState.java
+++ telephony/java/android/telephony/ServiceState.java
@@ -2250,4 +2250,18 @@ public class ServiceState implements Parcelable {
             return false;
         }
     }
+
+    /**
+     * Get whether device is connected to a non-terrestrial network.
+     *
+     * @return {@code true} if device is connected to a non-terrestrial network else {@code false}.
+     */
+    public boolean isUsingNonTerrestrialNetwork() {
+        synchronized (mNetworkRegistrationInfos) {
+            for (NetworkRegistrationInfo nri : mNetworkRegistrationInfos) {
+                if (nri.isNonTerrestrialNetwork()) return true;
+            }
+        }
+        return false;
+    }
 }
