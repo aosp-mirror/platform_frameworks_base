23e806a89cc213acaaee169e64553431b372505d
Aurélien Pomini <pomini@google.com>
2023-06-07 09:31:31 +0000

    Persist wallpaper changes after migration
    After a migration (e.g. set a wallpaper on home when having a live
    wallpaper on home+lock), the changes need to be persisted in
    wallpaper_info.xml. Otherwise after reboot, the wallpapers won't be
    rendered correctly.
    
    The complete() method of this CL is only called when the lockscreen
    livewallpaper flag is enabled, so this change is under flag.
    
    Cherry picked I704761c20e66e279a0e080e29dff3fa788d2ecf1 to master due to b/286408867.
    
    Code was merged via `-s ours` and is missing even though the sha exists on the target branch. Cherry pick is required to bring in the code.
    
    Manually remove the "Merged in" directive to allow for downstream propagation.
    
    Flag: lockscreen lwp
    Bug: 286061806
    Test: atest WallpaperManagerTest
    Test: manually check that the bug is fixed
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:900b78a92cd67d9760d1b65cbef0196108d0fc45)
    Change-Id: I601115e16b91b421c880f36e53a80c3c2cf83045

--- services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
+++ services/core/java/com/android/server/wallpaper/WallpaperManagerService.java
@@ -1463,6 +1463,10 @@ public class WallpaperManagerService extends IWallpaperManager.Stub
                 }
             }
 
+            synchronized (mLock) {
+                saveSettingsLocked(mNewWallpaper.userId);
+            }
+
             if (DEBUG) {
                 Slog.v(TAG, "--- wallpaper changed --");
                 Slog.v(TAG, "new sysWp: " + mWallpaperMap.get(mCurrentUserId));
