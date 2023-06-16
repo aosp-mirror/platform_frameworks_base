6283942d55b0d2209c4f79c9394cc526ee05c721
Nan Wu <wnan@google.com>
2023-06-16 14:42:24 +0000

    RESTRICT AUTOMERGE Fix BAL via notification.publicVersion
    We stripped the token that allows app to retrieve their own notification
    and fire their own PI to launch activities from background. But we
    forgot to strip the token from notification.publicVersion
    
    Bug: 278558814
    Test: NotificationManagerTest#testActivityStartFromRetrievedNotification_isBlocked
    Merged-In: I8f25d7a5e47890a0496af023149717e1df482f98
    Change-Id: I8f25d7a5e47890a0496af023149717e1df482f98

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -3485,8 +3485,11 @@ public class Notification implements Parcelable
      *
      * @hide
      */
-    public void setAllowlistToken(@Nullable IBinder token) {
-        mAllowlistToken = token;
+    public void clearAllowlistToken() {
+        mAllowlistToken = null;
+        if (publicVersion != null) {
+            publicVersion.clearAllowlistToken();
+        }
     }
 
     /**
