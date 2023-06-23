aec3145e0bec9fa4457b048b5aad2f0e6f9aabb3
Nataniel Borges <natanieljr@google.com>
2023-06-23 12:55:07 +0000

    Simplify pip tests
    Reuse split screen code to avoid duplication and increase stCUJ stability
    
    Test: atest WMShellFlickerTestsPip
    Bug: 288353930
    Bug: 285400227
    Change-Id: I3fb849be299a9a2c54e3ca9d983c7a1dde4bb9a1

--- libs/WindowManager/Shell/tests/flicker/src/com/android/wm/shell/flicker/splitscreen/benchmark/DragDividerToResizeBenchmark.kt
+++ libs/WindowManager/Shell/tests/flicker/src/com/android/wm/shell/flicker/splitscreen/benchmark/DragDividerToResizeBenchmark.kt
@@ -24,7 +24,7 @@ import android.tools.device.flicker.legacy.LegacyFlickerTest
 import android.tools.device.flicker.legacy.LegacyFlickerTestFactory
 import androidx.test.filters.RequiresDevice
 import com.android.wm.shell.flicker.splitscreen.SplitScreenBase
-import com.android.wm.shell.flicker.splitscreen.SplitScreenUtils
+import com.android.wm.shell.flicker.SplitScreenUtils
 import org.junit.Assume
 import org.junit.Before
 import org.junit.FixMethodOrder
