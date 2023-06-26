dd25b71157a0ccc0965f0580a55c49fa45ca0977
Dipen Pradhan <dipenp@google.com>
2023-06-26 15:51:50 -0700

    Add config for "Share" action in TextView context menu
    "Share" text action is not applicable to all platforms, this config
    allows disabling the action item.
    Example - Automotive (AAOS) does not support it
    
    Bug: 162463255
    
    Test: atest TextViewTest EditTextTest, manual check
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:712f9d38e3ea2b42fad56b52fbaed83442f2856e)
    Merged-In: I825a85bdf566c0a319bd91b523284757af6be68e
    Change-Id: I825a85bdf566c0a319bd91b523284757af6be68e

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -14996,7 +14996,9 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
     }
 
     boolean canShare() {
-        if (!getContext().canStartActivityForResult() || !isDeviceProvisioned()) {
+        if (!getContext().canStartActivityForResult() || !isDeviceProvisioned()
+                || !getContext().getResources().getBoolean(
+                com.android.internal.R.bool.config_textShareSupported)) {
             return false;
         }
         return canCopy();
