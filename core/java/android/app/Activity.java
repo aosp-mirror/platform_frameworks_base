1250f17dffab41880b2f7c4b78d9754a83dab84a
Roshan Pius <rpius@google.com>
2023-06-28 13:50:00 -0700

    activity(api): Expose isResumed for mainline module libs
    This is used by a bunch of NFC API classes. As a part of NFC mainline
    modularization, expose this API formally for mainline module libs.
    
    Bug: 263565738
    Test: Compiles
    Change-Id: I0377b317b204ffbf778078e20994439fc1e525b8

--- core/java/android/app/Activity.java
+++ core/java/android/app/Activity.java
@@ -8637,6 +8637,7 @@ public class Activity extends ContextThemeWrapper
      * @hide
      */
     @UnsupportedAppUsage
+    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
     public final boolean isResumed() {
         return mResumed;
     }
