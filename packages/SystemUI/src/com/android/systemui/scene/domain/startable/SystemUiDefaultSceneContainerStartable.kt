778b918db21efd1a7c3358c7e54cff71e1cba8c5
Alejandro Nijamkin <nijamkin@google.com>
2023-07-10 16:43:15 -0700

    [flexiglass] Centralizes automatic scene changing business logic.
    We had automatic scene changing business logic spread out amongst
    various scene-specific interactors. This was suboptimal for a few reasons:
    1. Hard to reason about what changes the scenes and when, when it's all
       distributed
    2. Hard to gate this logic based on the feature flag; causing memory and
       boot time regressions as seen in the attached files, because more
       resources were being used at startup even when the feature was off
    
    This CL moves business logic from LockScreenSceneInteractor and
    BouncerInteractor into SystemUiDefaultSceneContainerStartable and makes
    sure it's all gated behind the feature flag.
    
    Fix: 280883900, 289353099, 289726647
    Test: new unit tests added
    Test: manually verified in system UI that locking the device moves back
    to the Lockscreen scene and unlocking the device moves to the Gone scene
    
    Change-Id: I1dc46fc2d1d71f1b9baddc822ef69f5ea7b21cfc

--- packages/SystemUI/src/com/android/systemui/scene/domain/startable/SystemUiDefaultSceneContainerStartable.kt
+++ packages/SystemUI/src/com/android/systemui/scene/domain/startable/SystemUiDefaultSceneContainerStartable.kt
@@ -17,6 +17,7 @@
 package com.android.systemui.scene.domain.startable
 
 import com.android.systemui.CoreStartable
+import com.android.systemui.authentication.domain.interactor.AuthenticationInteractor
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.dagger.qualifiers.Application
 import com.android.systemui.flags.FeatureFlags
@@ -24,9 +25,11 @@ import com.android.systemui.flags.Flags
 import com.android.systemui.scene.domain.interactor.SceneInteractor
 import com.android.systemui.scene.shared.model.SceneContainerNames
 import com.android.systemui.scene.shared.model.SceneKey
+import com.android.systemui.scene.shared.model.SceneModel
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
@@ -41,17 +44,19 @@ class SystemUiDefaultSceneContainerStartable
 constructor(
     @Application private val applicationScope: CoroutineScope,
     private val sceneInteractor: SceneInteractor,
+    private val authenticationInteractor: AuthenticationInteractor,
     private val featureFlags: FeatureFlags,
 ) : CoreStartable {
 
     override fun start() {
         if (featureFlags.isEnabled(Flags.SCENE_CONTAINER)) {
-            keepVisibilityUpdated()
+            hydrateVisibility()
+            automaticallySwitchScenes()
         }
     }
 
-    /** Drives visibility of the scene container. */
-    private fun keepVisibilityUpdated() {
+    /** Updates the visibility of the scene container based on the current scene. */
+    private fun hydrateVisibility() {
         applicationScope.launch {
             sceneInteractor
                 .currentScene(CONTAINER_NAME)
@@ -59,6 +64,48 @@ constructor(
                 .distinctUntilChanged()
                 .collect { sceneKey ->
                     sceneInteractor.setVisible(CONTAINER_NAME, sceneKey != SceneKey.Gone)
+                }
+        }
+    }
+
+    /** Switches between scenes based on ever-changing application state. */
+    private fun automaticallySwitchScenes() {
+        applicationScope.launch {
+            authenticationInteractor.isUnlocked
+                .map { isUnlocked ->
+                    val currentSceneKey = sceneInteractor.currentScene(CONTAINER_NAME).value.key
+                    val isBypassEnabled = authenticationInteractor.isBypassEnabled.value
+                    when {
+                        isUnlocked ->
+                            when (currentSceneKey) {
+                                // When the device becomes unlocked in Bouncer, go to the Gone.
+                                is SceneKey.Bouncer -> SceneKey.Gone
+                                // When the device becomes unlocked in Lockscreen, go to Gone if
+                                // bypass is enabled.
+                                is SceneKey.Lockscreen -> SceneKey.Gone.takeIf { isBypassEnabled }
+                                // We got unlocked while on a scene that's not Lockscreen or
+                                // Bouncer, no need to change scenes.
+                                else -> null
+                            }
+                        // When the device becomes locked, to Lockscreen.
+                        !isUnlocked ->
+                            when (currentSceneKey) {
+                                // Already on lockscreen or bouncer, no need to change scenes.
+                                is SceneKey.Lockscreen,
+                                is SceneKey.Bouncer -> null
+                                // We got locked while on a scene that's not Lockscreen or Bouncer,
+                                // go to Lockscreen.
+                                else -> SceneKey.Lockscreen
+                            }
+                        else -> null
+                    }
+                }
+                .filterNotNull()
+                .collect { targetSceneKey ->
+                    sceneInteractor.setCurrentScene(
+                        containerName = CONTAINER_NAME,
+                        scene = SceneModel(targetSceneKey),
+                    )
                 }
         }
     }
