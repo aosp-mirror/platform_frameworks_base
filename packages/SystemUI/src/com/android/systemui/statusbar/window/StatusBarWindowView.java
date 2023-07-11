b95f742c965a564a02d9d4ff4164d3bd30a47e55
Johannes Gallmann <gallmann@google.com>
2023-07-11 16:49:14 +0200

    Fix Statusbar Chips clipped by 3 Button Nav in landscape mode
    Bug: 289875579
    Test: Manual, i.e. enabling 3 button nav and verifying chip animations in landscape and seascape mode.
    Change-Id: I3e3ade1f20597ca176ed553839076ad101ad3d9f

--- packages/SystemUI/src/com/android/systemui/statusbar/window/StatusBarWindowView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/window/StatusBarWindowView.java
@@ -46,6 +46,7 @@ public class StatusBarWindowView extends FrameLayout {
 
     public StatusBarWindowView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setClipChildren(false);
     }
 
     @Override
