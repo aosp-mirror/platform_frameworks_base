abf91abaefa6291cce2436b525a76d6fe057d5c8
Louis Chang <louischang@google.com>
2023-07-12 02:43:13 +0000

    Trims Recent Task when Task windowing mode changed after dismiss split-screen
    While starting an activity in split-screen, a Task was created in
    multi-window mode and added to the RecentTasks. Once the activity is
    finished, the same Task was reparented to TDA and became fullscreen
    windowing mode before the activity is removed.
    
    The next time starting the same activity in split-screen, a multi-window
    mode Task was added to the RecentTasks again without trimming the legacy
    recent Task because the windowing modes are incompatible (multi-window
    vs. fullscreen). Repeating the steps could resulted in redundant tasks
    in Recents.
    
    Bug: 289317239
    Test: atest RecentTasksTest
    Change-Id: I7af0acaa6d528d7a45018b1b7f57dbe6d57f1585

--- services/core/java/com/android/server/wm/WindowOrganizerController.java
+++ services/core/java/com/android/server/wm/WindowOrganizerController.java
@@ -1608,6 +1608,7 @@ class WindowOrganizerController extends IWindowOrganizerController.Stub
         final int count = tasksToReparent.size();
         for (int i = 0; i < count; ++i) {
             final Task task = tasksToReparent.get(i);
+            final int prevWindowingMode = task.getWindowingMode();
             if (syncId >= 0) {
                 addToSyncSet(syncId, task);
             }
@@ -1620,6 +1621,12 @@ class WindowOrganizerController extends IWindowOrganizerController.Stub
                 task.reparent((Task) newParent,
                         hop.getToTop() ? POSITION_TOP : POSITION_BOTTOM,
                         false /*moveParents*/, "processChildrenTaskReparentHierarchyOp");
+            }
+            // Trim the compatible Recent task (if any) after the Task is reparented and now has
+            // a different windowing mode, in order to prevent redundant Recent tasks after
+            // reparenting.
+            if (prevWindowingMode != task.getWindowingMode()) {
+                mService.mTaskSupervisor.mRecentTasks.removeCompatibleRecentTask(task);
             }
         }
 
