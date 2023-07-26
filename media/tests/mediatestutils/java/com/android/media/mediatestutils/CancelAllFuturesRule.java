b9dbeea2082db978118c3cf59c6c921fe9c43b4a
Atneya Nair <atneya@google.com>
2023-07-26 15:22:57 -0700

    Add mediatestutils and CancelAllFuturesRule
    Add new utils library for media tests.
    
    Add a CancelAllFuturesRule which cancels all registered futures on test
    completion (regardless of success).
    
    Add unit tests.
    
    Bug: 294636572
    Bug: 288333346
    Test: atest CancelAllFuturesRuleTest
    Change-Id: If270a6b7683515157ef356c41ea53ed872bd85d8

---
+++ media/tests/mediatestutils/java/com/android/media/mediatestutils/CancelAllFuturesRule.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.media.mediatestutils;
+
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Future;
+
+/**
+ *
+ */
+public class CancelAllFuturesRule implements TestRule {
+    private List<Future> mRegisteredFutures = new ArrayList<>();
+
+    public <T extends Future<?>> T registerFuture(T future) {
+        mRegisteredFutures.add(future);
+        return future;
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable {
+                try {
+                    base.evaluate();
+                } finally {
+                    mRegisteredFutures.forEach(f -> f.cancel(false /* shouldInterrupt */));
+                }
+            }
+        };
+    }
+}
