9c114782e6094b113c923111a77f6149d7efda10
Eric Biggers <ebiggers@google.com>
2023-07-28 22:03:03 +0000

    RESTRICT AUTOMERGE: SettingsProvider: exclude secure_frp_mode from resets
    When RescueParty detects that a system process is crashing frequently,
    it tries to recover in various ways, such as by resetting all settings.
    Unfortunately, this included resetting the secure_frp_mode setting,
    which is the means by which the system keeps track of whether the
    Factory Reset Protection (FRP) challenge has been passed yet.  With this
    setting reset, some FRP restrictions went away and it became possible to
    bypass FRP by setting a new lockscreen credential.
    
    Fix this by excluding secure_frp_mode from resets.
    
    Note: currently this bug isn't reproducible on 'main' due to ag/23727749
    disabling much of RescueParty, but that is a temporary change.
    
    Bug: 253043065
    Test: With ag/23727749 reverted and with my fix to prevent
          com.android.settings from crashing *not* applied, tried repeatedly
          setting lockscreen credential while in FRP mode, using the
          smartlock setup activity launched by intent via adb.  Verified
          that although RescueParty is still triggered after 5 attempts,
          secure_frp_mode is no longer reset (its value remains "1").
    Test: Verified that secure_frp_mode still gets changed from 1 to 0 when
          FRP is passed legitimately.
    Test: atest com.android.providers.settings.SettingsProviderTest
    Test: atest android.provider.SettingsProviderTest
    (cherry picked from commit 9890dd7f15c091f7d1a09e4fddb9f85d32015955)
    (changed Global.SECURE_FRP_MODE to Secure.SECURE_FRP_MODE,
     needed because this setting was moved in U)
    (removed static keyword from shouldExcludeSettingFromReset(),
     needed for compatibility with Java 15 and earlier)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:a7ea34724f1bdebde64d9e9a1391c92dc2e189b5)
    Merged-In: Id95ed43b9cc2208090064392bcd5dc012710af93
    Change-Id: Id95ed43b9cc2208090064392bcd5dc012710af93

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -3075,6 +3075,15 @@ public class SettingsProvider extends ContentProvider {
             return settingsState.getSettingLocked(name);
         }
 
+        private boolean shouldExcludeSettingFromReset(Setting setting, String prefix) {
+            // If a prefix was specified, exclude settings whose names don't start with it.
+            if (prefix != null && !setting.getName().startsWith(prefix)) {
+                return true;
+            }
+            // Never reset SECURE_FRP_MODE, as it could be abused to bypass FRP via RescueParty.
+            return Secure.SECURE_FRP_MODE.equals(setting.getName());
+        }
+
         public void resetSettingsLocked(int type, int userId, String packageName, int mode,
                 String tag) {
             resetSettingsLocked(type, userId, packageName, mode, tag, /*prefix=*/
@@ -3097,7 +3106,7 @@ public class SettingsProvider extends ContentProvider {
                         Setting setting = settingsState.getSettingLocked(name);
                         if (packageName.equals(setting.getPackageName())) {
                             if ((tag != null && !tag.equals(setting.getTag()))
-                                    || (prefix != null && !setting.getName().startsWith(prefix))) {
+                                    || shouldExcludeSettingFromReset(setting, prefix)) {
                                 continue;
                             }
                             if (settingsState.resetSettingLocked(name)) {
@@ -3117,7 +3126,7 @@ public class SettingsProvider extends ContentProvider {
                         Setting setting = settingsState.getSettingLocked(name);
                         if (!SettingsState.isSystemPackage(getContext(),
                                 setting.getPackageName())) {
-                            if (prefix != null && !setting.getName().startsWith(prefix)) {
+                            if (shouldExcludeSettingFromReset(setting, prefix)) {
                                 continue;
                             }
                             if (settingsState.resetSettingLocked(name)) {
@@ -3137,7 +3146,7 @@ public class SettingsProvider extends ContentProvider {
                         Setting setting = settingsState.getSettingLocked(name);
                         if (!SettingsState.isSystemPackage(getContext(),
                                 setting.getPackageName())) {
-                            if (prefix != null && !setting.getName().startsWith(prefix)) {
+                            if (shouldExcludeSettingFromReset(setting, prefix)) {
                                 continue;
                             }
                             if (setting.isDefaultFromSystem()) {
@@ -3160,7 +3169,7 @@ public class SettingsProvider extends ContentProvider {
                     for (String name : settingsState.getSettingNamesLocked()) {
                         Setting setting = settingsState.getSettingLocked(name);
                         boolean someSettingChanged = false;
-                        if (prefix != null && !setting.getName().startsWith(prefix)) {
+                        if (shouldExcludeSettingFromReset(setting, prefix)) {
                             continue;
                         }
                         if (setting.isDefaultFromSystem()) {
