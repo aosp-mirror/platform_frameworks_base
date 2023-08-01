766cef076547b5d757e14a7ea147e291204274b4
Sherry Zhou <yuandizhou@google.com>
2023-08-01 22:13:08 +0000

    Enable lockscreen_custom_transit_clock for Droidfood
    Bug: 287268101
    Test: manual
    Change-Id: I96fd91a09f23e67a8f53f5fdb8e53ab9f470b899

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -266,7 +266,7 @@ object Flags {
     @JvmField val KEYGUARD_TALKBACK_FIX = releasedFlag(238, "keyguard_talkback_fix")
 
     // TODO(b/287268101): Tracking bug.
-    @JvmField val TRANSIT_CLOCK = unreleasedFlag(239, "lockscreen_custom_transit_clock", teamfood = true)
+    @JvmField val TRANSIT_CLOCK = releasedFlag(239, "lockscreen_custom_transit_clock")
 
     /** Migrate the lock icon view to the new keyguard root view. */
     // TODO(b/286552209): Tracking bug.
