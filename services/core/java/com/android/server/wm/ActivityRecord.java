dea7e1e6513e35074126ea2d21c429b6ee6886cc
Riddle Hsu <riddlehsu@google.com>
2023-08-08 10:27:41 +0000

    Add removal time of starting window in transition trace proto
    The transition can play with the starting window, and even if the
    transition is finished, the app window may still not draw yet.
    Then at the timestamp of removing the starting window, this log
    can provide the info for flicker detection to check that the app
    is drawn or not.
    
    Bug: 284302118
    Test: The app surface is visible at timestamp of remove_time_ns
    Change-Id: I869b5a1213b7b64c4a875a9ac33c8245fcccf40f

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -2785,6 +2785,9 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
             } else if (isEmbedded()) {
                 associateStartingWindowWithTaskIfNeeded();
             }
+            if (mTransitionController.isCollecting()) {
+                mStartingData.mTransitionId = mTransitionController.getCollectingTransitionId();
+            }
         }
     }
 
