7a350b8d14d10f563ad98c7af18f1bfaa01f1255
William Loh <wloh@google.com>
2023-08-17 00:35:02 +0000

    Move PackageManagerTests to postsubmit
    Bug: 295273296
    Change-Id: I294718f4586347bac8c1acd52c29532682091c0b
    Test: presubmit
    Merged-In: I23e471ec8a0ddc34c36064a9b32eece7a209ec59

--- services/tests/PackageManagerServiceTests/server/src/com/android/server/PackageManagerTests.java
+++ services/tests/PackageManagerServiceTests/server/src/com/android/server/PackageManagerTests.java
@@ -56,7 +56,7 @@ import android.os.Process;
 import android.os.RemoteException;
 import android.os.StatFs;
 import android.os.SystemClock;
-import android.platform.test.annotations.Presubmit;
+import android.platform.test.annotations.Postsubmit;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
 import android.system.ErrnoException;
@@ -94,7 +94,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.TimeUnit;
 
-@Presubmit
+@Postsubmit
 public class PackageManagerTests extends AndroidTestCase {
     private static final boolean localLOGV = true;
 
