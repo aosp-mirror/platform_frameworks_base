9455659b80885e93320ba0827c97739b2b34ca0f
Songchun Fan <schfan@google.com>
2023-08-21 11:56:31 -0700

    [SettingsProvider] do not crash the system if config settings files are broken
    Tolerate config flags' serialization/deserialization failures because they can be sync'd after boot.
    
    BUG: 295555884
    Test: manually push a broken config file onto device and reboot
    Merged-In: I9851991b4a225f2941708d37d2ad9507d7c432f8
    Change-Id: I9851991b4a225f2941708d37d2ad9507d7c432f8
    (cherry picked from commit 2ba72c0611a4f1089dcd918c712ad95259f6da5a)

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsState.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsState.java
@@ -1090,7 +1090,10 @@ final class SettingsState {
         } catch (FileNotFoundException fnfe) {
             final String message = "No fallback file found for: " + mStatePersistFile;
             Slog.wtf(LOG_TAG, message);
-            throw new IllegalStateException(message);
+            if (!isConfigSettingsKey(mKey)) {
+                // Allow partially deserialized config settings because they can be updated later
+                throw new IllegalStateException(message);
+            }
         }
         if (parseStateFromXmlStreamLocked(in)) {
             // Parsed state from fallback file. Restore original file with fallback file
@@ -1102,7 +1105,10 @@ final class SettingsState {
         } else {
             final String message = "Failed parsing settings file: " + mStatePersistFile;
             Slog.wtf(LOG_TAG, message);
-            throw new IllegalStateException(message);
+            if (!isConfigSettingsKey(mKey)) {
+                // Allow partially deserialized config settings because they can be updated later
+                throw new IllegalStateException(message);
+            }
         }
     }
 
