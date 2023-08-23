960185865090a85706d9225613aa0a4fbf76fdf1
Josh Tsuji <tsuji@google.com>
2023-08-23 14:50:52 -0400

    Check isInteractive after traversal before hiding keyguard.
    The Runnable passed to postAfterTraversal is capable of
    unilaterally hiding the keyguard, but it's run at an
    indeterminate time due to the postAfterTraversal. To be
    safe, check the earliest source of truth (PowerManager)
    to see if we're still interactive before proceeding.
    
    Fixes: 296886973
    Test: lock the device during the unlock animation from bouncer to an app
    Test: atest SystemUITests
    Change-Id: Iaef7cff8366015c3e66aac4d740f6240e7c183db

--- packages/SystemUI/tests/src/com/android/systemui/keyguard/KeyguardViewMediatorTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/keyguard/KeyguardViewMediatorTest.java
@@ -182,6 +182,7 @@ public class KeyguardViewMediatorTest extends SysuiTestCase {
 
         when(mLockPatternUtils.getDevicePolicyManager()).thenReturn(mDevicePolicyManager);
         when(mPowerManager.newWakeLock(anyInt(), any())).thenReturn(mock(WakeLock.class));
+        when(mPowerManager.isInteractive()).thenReturn(true);
         when(mInteractionJankMonitor.begin(any(), anyInt())).thenReturn(true);
         when(mInteractionJankMonitor.end(anyInt())).thenReturn(true);
         final ViewRootImpl testViewRoot = mock(ViewRootImpl.class);
@@ -707,8 +708,6 @@ public class KeyguardViewMediatorTest extends SysuiTestCase {
         mViewMediator.setShowingLocked(true);
         when(mKeyguardStateController.isShowing()).thenReturn(true);
         TestableLooper.get(this).processAllMessages();
-
-        when(mPowerManager.isInteractive()).thenReturn(true);
 
         mViewMediator.onSystemReady();
         TestableLooper.get(this).processAllMessages();
