b8331285ad4fbc62ef5f5486eee2fbd8aed29df2
Tetiana Meronyk <tetianameronyk@google.com>
2023-08-24 16:27:30 +0000

    Truncate user data to a limit of 500 characters
    Fix vulnerability that allows creating users with no restrictions. This is done by creating an intent to create a user and putting extras that are too long to be serialized. It causes IOException and the restrictions are not written in the file.
    
    By truncating the string values when writing them to the file, we ensure that the exception does not happen and it can be recorded correctly.
    
    Bug: 293602317
    Test: install app provided in the bug, open app and click add. Check logcat to see there is no more IOException. Reboot the device by either opening User details page or running adb shell dumpsys user | grep -A12 heen and see that the restrictions are in place.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:1bc8e28626843225b09b2b070685f81fbadefc08)
    Merged-In: I633dc10974a64ef2abd07e67ff2d209847129989
    Change-Id: I633dc10974a64ef2abd07e67ff2d209847129989

--- services/tests/mockingservicestests/src/com/android/server/pm/UserManagerServiceTest.java
+++ services/tests/mockingservicestests/src/com/android/server/pm/UserManagerServiceTest.java
@@ -15,9 +15,12 @@
  */
 package com.android.server.pm;
 
+import static android.os.UserManager.USER_TYPE_FULL_SECONDARY;
+
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.doNothing;
 import static com.android.dx.mockito.inline.extended.ExtendedMockito.doReturn;
 
+import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import static org.junit.Assert.assertThrows;
@@ -323,6 +326,24 @@ public final class UserManagerServiceTest {
 
         assertThrows(UserManager.CheckedUserOperationException.class,
                 () -> mUmi.getBootUser(/* waitUntilSet= */ false));
+    }
+
+    @Test
+    public void testCreateUserWithLongName_TruncatesName() {
+        UserInfo user = mUms.createUserWithThrow(generateLongString(), USER_TYPE_FULL_SECONDARY, 0);
+        assertThat(user.name.length()).isEqualTo(500);
+        UserInfo user1 = mUms.createUserWithThrow("Test", USER_TYPE_FULL_SECONDARY, 0);
+        assertThat(user1.name.length()).isEqualTo(4);
+    }
+
+    private String generateLongString() {
+        String partialString = "Test Name Test Name Test Name Test Name Test Name Test Name Test "
+                + "Name Test Name Test Name Test Name "; //String of length 100
+        StringBuilder resultString = new StringBuilder();
+        for (int i = 0; i < 660; i++) {
+            resultString.append(partialString);
+        }
+        return resultString.toString();
     }
 
     private void mockCurrentUser(@UserIdInt int userId) {
