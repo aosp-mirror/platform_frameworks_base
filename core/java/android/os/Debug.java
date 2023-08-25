7aa6d65722f142c8d813d84855d33bc75a7e85c3
Kevin Jeon <kevinjeon@google.com>
2023-08-25 15:23:21 -0400

    Add Debug.getRss()
    This change adds the getRss() method, which will return RSS (as
    estimated by /proc/<pid>/status) and memtrack graphics, memtrack gl, and
    memtrack other. This is intended to be used a drop-in replacement for
    getPss() where efficiency is a higher priority than accuracy. Unlike
    getPss(), this method does not provide the option for collecting USS or
    SwapPss as these are not emitted in status.
    
    There are some formatting changes included here that were required for
    'repo upload'.
    
    Test: Added method call in CTS's DebugTest; included unit test for
          backing ProcMemInfo::StatusRss method.
    Bug: 296454553
    Change-Id: I20e7999dce46f6cdf01cb740ab9576937711dfef

--- core/java/android/os/Debug.java
+++ core/java/android/os/Debug.java
@@ -16,6 +16,7 @@
 
 package android.os;
 
+import android.annotation.FlaggedApi;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.app.AppGlobals;
@@ -1952,6 +1953,23 @@ public final class Debug
      * @hide
      */
     public static native long getPss(int pid, long[] outUssSwapPssRss, long[] outMemtrack);
+
+    /**
+     * Retrieves the RSS memory used by the process as given by the status file.
+     */
+    @FlaggedApi(Flags.FLAG_REMOVE_APP_PROFILER_PSS_COLLECTION)
+    public static native long getRss();
+
+    /**
+     * Retrieves the RSS memory used by the process as given by the status file. Optionally supply a
+     * long array of up to 4 entries to retrieve the total memtrack reported size, memtrack
+     * graphics, memtrack gl, and memtrack other.
+     *
+     * @return The RSS memory usage, or 0 if retrieval failed (i.e. the PID is gone).
+     * @hide
+     */
+    @FlaggedApi(Flags.FLAG_REMOVE_APP_PROFILER_PSS_COLLECTION)
+    public static native long getRss(int pid, long[] outMemtrack);
 
     /** @hide */
     public static final int MEMINFO_TOTAL = 0;
