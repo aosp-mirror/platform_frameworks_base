fd8b0fff4016f708629b221de715b0b9ea868afd
Jordan Demeulenaere <jdemeulenaere@google.com>
2023-08-25 16:56:21 +0200

    Add logs to help investigate b/288507023
    Bug: 288507023
    Test: Manual, checked the logs while opening notifications
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:f78f0c02fdedce6f73a5d3f5f4993940a9554852)
    Merged-In: Ief31f7d3075eb1b9dc384cefdaeef371cfdf6691
    Change-Id: Ief31f7d3075eb1b9dc384cefdaeef371cfdf6691

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/data/repository/NotificationExpansionRepository.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/data/repository/NotificationExpansionRepository.kt
@@ -16,11 +16,14 @@
 
 package com.android.systemui.statusbar.notification.data.repository
 
+import android.util.Log
 import com.android.systemui.dagger.SysUISingleton
 import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+
+private const val TAG = "NotificationExpansionRepository"
 
 /** A repository tracking the status of notification expansion animations. */
 @SysUISingleton
@@ -37,6 +40,8 @@ class NotificationExpansionRepository @Inject constructor() {
 
     /** Sets whether the notification expansion animation is currently running. */
     fun setIsExpandAnimationRunning(running: Boolean) {
+        // TODO(b/288507023): Remove this log.
+        Log.d(TAG, "setIsExpandAnimationRunning(running=$running)")
         _isExpandAnimationRunning.value = running
     }
 }
