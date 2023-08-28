461f259ffaa5e6d71cb6f1d7b16b1fccafa1cb35
axfordjc <axfordjc@google.com>
2023-08-28 16:26:05 +0000

    Pattern bouncer refactor: motion layout - change constraints on rotate
    - refactored Pattern bouncer into a single motion layout
    - transition between landscape or portrait constrains on rotate
    
    why are we doing this?
    - to add support for landscape bouncers on small screens
    
    "Split" bouncer constraints (https://hsv.googleplex.com/5351602346000384) will be used on small landscape screens only
    
    "Single" bouncer constraints (https://hsv.googleplex.com/6007067992129536) are used otherwise, such as for large screen port/land and small screens port - this is the same as the previous behaviour.
    
    Use of "split or single" bouncer constraints is not strictly related to device orientation.
    
    Flag:
    WHEN FLAG OFF - old portrait layout always used
    WHEN FLAG ON - new layout uses, constraints updated to match current orientation (once per rotate, once on device posture change, and once after inflate)
    
    Bug: 293252410
    Bug: 296567469
    Bug: 297863911
    
    Test: KeyguardPatternViewControllerTest
    Change-Id: If57b60482377eaf9ac3131fff9a136c92af67da0

--- packages/SystemUI/src/com/android/keyguard/KeyguardPatternViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPatternViewController.java
@@ -18,6 +18,7 @@ package com.android.keyguard;
 
 import static com.android.internal.util.LatencyTracker.ACTION_CHECK_CREDENTIAL;
 import static com.android.internal.util.LatencyTracker.ACTION_CHECK_CREDENTIAL_UNLOCKED;
+import static com.android.systemui.flags.Flags.LOCKSCREEN_ENABLE_LANDSCAPE;
 
 import android.content.res.ColorStateList;
 import android.os.AsyncTask;
@@ -205,6 +206,8 @@ public class KeyguardPatternViewController
         mLatencyTracker = latencyTracker;
         mFalsingCollector = falsingCollector;
         mEmergencyButtonController = emergencyButtonController;
+        view.setIsLockScreenLandscapeEnabled(
+                featureFlags.isEnabled(LOCKSCREEN_ENABLE_LANDSCAPE));
         mLockPatternView = mView.findViewById(R.id.lockPatternView);
         mPostureController = postureController;
     }
