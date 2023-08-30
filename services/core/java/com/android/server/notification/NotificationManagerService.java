c0da04b7a98c4985073fc4e9831addf57e23bcb3
Tej Singh <singhtejinder@google.com>
2023-08-30 00:27:29 -0700

    Move system server pullers off background thread
    The background thread is prone to severe delays in processing time near
    boot, which causes pull timeouts and event queue overflow in statsd. The
    vast majority of pullers use the DIRECT_EXECUTOR to execute pulls in the
    incoming binder thread, so move these pulled atoms to do the same thing.
    More context is in the bug and linked docs.
    
    Bug: 298133385
    Test: verified via boot traces that the pull durations are shorter and
    that the pulls no longer happen in the background thread
    
    Change-Id: I67a3d583cc13d9547dfe7108897e95f4a5238d26
    Merged-In: I67a3d583cc13d9547dfe7108897e95f4a5238d26
    (cherry picked from commit 5c85f29bb88459057735956afdae37354c1556a8)

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -2632,7 +2632,7 @@ public class NotificationManagerService extends SystemService {
         mStatsManager.setPullAtomCallback(
                 DND_MODE_RULE,
                 null, // use default PullAtomMetadata values
-                BackgroundThread.getExecutor(),
+                ConcurrentUtils.DIRECT_EXECUTOR,
                 mPullAtomCallback
         );
     }
