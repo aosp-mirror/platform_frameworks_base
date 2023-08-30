80841e1b172523d5ce9e3b19a7e42015c8eb864c
Chris Li <lihongyu@google.com>
2023-08-30 14:18:44 +0800

    Disable AE component for device that doesn't support multi window
    Low memory device that doesn't support multi window will not support AE
    either.
    
    Bug: 298139092
    Test: atest WMJetpackUnitTests:WindowExtensionsTest
    Change-Id: I54656fecfcd022d80d03464e111b582ac64808e8

--- libs/WindowManager/Jetpack/src/androidx/window/extensions/WindowExtensionsImpl.java
+++ libs/WindowManager/Jetpack/src/androidx/window/extensions/WindowExtensionsImpl.java
@@ -16,12 +16,14 @@
 
 package androidx.window.extensions;
 
+import android.app.ActivityTaskManager;
 import android.app.ActivityThread;
 import android.app.Application;
 import android.content.Context;
 import android.window.TaskFragmentOrganizer;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.window.common.DeviceStateManagerFoldingFeatureProducer;
 import androidx.window.common.RawFoldingFeatureProducer;
 import androidx.window.extensions.area.WindowAreaComponent;
@@ -111,9 +113,13 @@ public class WindowExtensionsImpl implements WindowExtensions {
      * {@link WindowExtensions#getWindowLayoutComponent()}.
      * @return {@link ActivityEmbeddingComponent} OEM implementation.
      */
-    @NonNull
+    @Nullable
     public ActivityEmbeddingComponent getActivityEmbeddingComponent() {
         if (mSplitController == null) {
+            if (!ActivityTaskManager.supportsMultiWindow(getApplication())) {
+                // Disable AE for device that doesn't support multi window.
+                return null;
+            }
             synchronized (mLock) {
                 if (mSplitController == null) {
                     mSplitController = new SplitController(
