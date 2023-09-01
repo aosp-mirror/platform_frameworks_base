447abc81d5d7253d7ce5175bbfc689d5f4c3084f
Eric Biggers <ebiggers@google.com>
2023-09-01 20:49:15 +0000

    Include TrustState in TrustManagerService dumpUser
    TrustManagerService dumps the raw aggregated trustable value for each
    user, which is not useful because it is not the authoritative trust
    state for the user, and it's a computed value (i.e., not real state)
    that is computed from other state already dumped.  Therefore, dump the
    TrustState instead which is real state that is much more useful.
    
    Test: adb shell dumpsys trust
    Flag: exempt, straightforward fix that only affects bugreports
    Change-Id: I17103be4d0cc2545e3bdcb1656b35f2d287b01d5

--- services/core/java/com/android/server/trust/TrustManagerService.java
+++ services/core/java/com/android/server/trust/TrustManagerService.java
@@ -814,6 +814,12 @@ public class TrustManagerService extends SystemService {
         }
     }
 
+    private TrustState getUserTrustStateInner(int userId) {
+        synchronized (mUserTrustState) {
+            return mUserTrustState.get(userId, TrustState.UNTRUSTED);
+        }
+    }
+
     boolean isDeviceLockedInner(int userId) {
         synchronized (mDeviceLockedForUser) {
             return mDeviceLockedForUser.get(userId, true);
@@ -1628,7 +1634,7 @@ public class TrustManagerService extends SystemService {
             if (isCurrent) {
                 fout.print(" (current)");
             }
-            fout.print(": trusted=" + dumpBool(aggregateIsTrusted(user.id)));
+            fout.print(": trustState=" + getUserTrustStateInner(user.id));
             fout.print(", trustManaged=" + dumpBool(aggregateIsTrustManaged(user.id)));
             fout.print(", deviceLocked=" + dumpBool(isDeviceLockedInner(user.id)));
             fout.print(", strongAuthRequired=" + dumpHex(
