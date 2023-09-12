77011b6f41c4bf43ed0a179782443357da5d4bba
Mady Mellor <madym@google.com>
2023-09-12 16:15:50 -0700

    Share the scrim value with launcher
    When I changed the scrim in ag/24343831 I didn't do it in taskbar as
    well, extract the value into common class so it can be shared with
    launcher in one place.
    
    Test: manual - expand a bubble and check the scrim
    Bug: 294396107
    Change-Id: I810889ee81b70c90f1bfad646cb4c6854f6d7a01

---
+++ libs/WindowManager/Shell/src/com/android/wm/shell/common/bubbles/BubbleConstants.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.wm.shell.common.bubbles;
+
+/**
+ * Constants shared between bubbles in shell & things we have to do for bubbles in launcher.
+ */
+public class BubbleConstants {
+
+    /** The alpha for the scrim shown when bubbles are expanded. */
+    public static float BUBBLE_EXPANDED_SCRIM_ALPHA = .32f;
+}
