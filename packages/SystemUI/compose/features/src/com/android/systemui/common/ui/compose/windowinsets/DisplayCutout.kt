a16ae9a2fa688199d1fca10a822db4d52b29d3c0
Shawn Lee <syeonlee@google.com>
2023-09-18 15:37:02 -0700

    Added display cutout handling to Shade Header in Flexiglass
    The collapsed Shade Header is now split into two halves that are repositioned around the display cutout, like the old impl. Compose does not yet expose the actual bounding rectangle for the display cutout, so this needs to be derived from android.view.DisplayCutout and piped in from the root view for now. The display cutout bounds are provided via CompositionLocal to the entire Compose tree.
    
    Bug: 298525212
    Test: Manually verified header resizes and truncates content properly with different font/display sizes and cutout locations.
    Change-Id: I8fa7fef39dcc920ce01c4ebb29e45aae310cdd87

---
+++ packages/SystemUI/compose/features/src/com/android/systemui/common/ui/compose/windowinsets/DisplayCutout.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.systemui.common.ui.compose.windowinsets
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlin.math.abs
+
+/** Represents the global position of the bounds for the display cutout for this display */
+data class DisplayCutout(
+    val left: Dp = 0.dp,
+    val top: Dp = 0.dp,
+    val right: Dp = 0.dp,
+    val bottom: Dp = 0.dp,
+    val location: CutoutLocation = CutoutLocation.NONE,
+) {
+    fun width() = abs(right.value - left.value).dp
+}
+
+enum class CutoutLocation {
+    NONE,
+    CENTER,
+    LEFT,
+    RIGHT,
+}
--- packages/SystemUI/src/com/android/systemui/compose/BaseComposeFacade.kt
+++ packages/SystemUI/src/com/android/systemui/compose/BaseComposeFacade.kt
@@ -19,6 +19,7 @@ package com.android.systemui.compose
 
 import android.content.Context
 import android.view.View
+import android.view.WindowInsets
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.LifecycleOwner
 import com.android.systemui.people.ui.viewmodel.PeopleViewModel
@@ -26,6 +27,8 @@ import com.android.systemui.qs.footer.ui.viewmodel.FooterActionsViewModel
 import com.android.systemui.scene.shared.model.Scene
 import com.android.systemui.scene.shared.model.SceneKey
 import com.android.systemui.scene.ui.viewmodel.SceneContainerViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.StateFlow
 
 /**
  * A facade to interact with Compose, when it is available.
@@ -63,8 +66,10 @@ interface BaseComposeFacade {
 
     /** Create a [View] to represent [viewModel] on screen. */
     fun createSceneContainerView(
+        scope: CoroutineScope,
         context: Context,
         viewModel: SceneContainerViewModel,
+        windowInsets: StateFlow<WindowInsets?>,
         sceneByKey: Map<SceneKey, Scene>,
     ): View
 }
