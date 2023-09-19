d9c7c85c52c007fdedb177b9f5f98821d0a76090
Beverly Tai <beverlyt@google.com>
2023-09-19 20:54:47 +0000

    Updated: always show the keyguard on device lockdown
    Additionally, don't hide keyguard when it's disabled if the user has locked
    down the device.
    
    Manual test steps:
        1. Enable app pinning and disable "Ask for PIN before unpinning" setting
        2. Pin an app (ie: Settings)
        3. Lockdown from the power menu
        4. Observe: user is brought to the keyguard, primary auth is
           required to enter the device.
           => After entering correct credential, the device is still in
              app pinning mode.
           => After entering an incorrect credential, the keyguard remains
              showing and the user can attempt again up to the limit
    
    Bug: 300463732
    Bug: 218495634
    Test: atest KeyguardViewMediatorTest
    Test: manual
    Change-Id: I70fdae80f717712b3dfc9df54b9649959b4bb8f0
    Merged-In: I70fdae80f717712b3dfc9df54b9649959b4bb8f0

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -768,6 +768,13 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                 }
             }
         }
+
+        @Override
+        public void onStrongAuthStateChanged(int userId) {
+            if (mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+                doKeyguardLocked(null);
+            }
+        }
     };
 
     ViewMediatorCallback mViewMediatorCallback = new ViewMediatorCallback() {
@@ -1932,6 +1939,10 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             mExternallyEnabled = enabled;
 
             if (!enabled && mShowing) {
+                if (mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
+                    Log.d(TAG, "keyguardEnabled(false) overridden by user lockdown");
+                    return;
+                }
                 // hiding keyguard that is showing, remember to reshow later
                 if (DEBUG) Log.d(TAG, "remembering to reshow, hiding keyguard, "
                         + "disabling status bar expansion");
@@ -2152,8 +2163,9 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
      * Enable the keyguard if the settings are appropriate.
      */
     private void doKeyguardLocked(Bundle options) {
-        // if another app is disabling us, don't show
-        if (!mExternallyEnabled) {
+        // if another app is disabling us, don't show unless we're in lockdown mode
+        if (!mExternallyEnabled
+                && !mLockPatternUtils.isUserInLockdown(KeyguardUpdateMonitor.getCurrentUser())) {
             if (DEBUG) Log.d(TAG, "doKeyguard: not showing because externally disabled");
 
             mNeedToReshowWhenReenabled = true;
