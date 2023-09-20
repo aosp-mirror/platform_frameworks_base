47fa2f79584b0a4e9ca7e9c6b237c4e5cf699032
Ioana Alexandru <aioana@google.com>
2023-09-20 19:31:22 +0200

    Visit Person.getUri().
    This is to prevent a potential security issue.
    
    Flag: ACONFIG android.app.VISIT_RISKY_URIS DEVELOPMENT
    Bug: 281044385
    Test: atest NotificationVisitUrisTest
    
    Change-Id: Ic882e91da9ab250162d5404f417eb62bd0d34c49

--- core/java/android/app/Person.java
+++ core/java/android/app/Person.java
@@ -189,6 +189,11 @@ public final class Person implements Parcelable {
      */
     public void visitUris(@NonNull Consumer<Uri> visitor) {
         visitor.accept(getIconUri());
+        if (Flags.visitRiskyUris()) {
+            if (mUri != null && !mUri.isEmpty()) {
+                visitor.accept(Uri.parse(mUri));
+            }
+        }
     }
 
     /** Builder for the immutable {@link Person} class. */
