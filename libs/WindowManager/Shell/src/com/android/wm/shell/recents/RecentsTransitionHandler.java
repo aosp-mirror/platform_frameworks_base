8a4a2efa5070b01005ba0cb8898fe0ae02e66900
Evan Rosky <erosky@google.com>
2023-09-20 21:02:18 +0000

    Revert "Keep z-order of tasks when merged into recents transition"
    This reverts commit 5742d6b06221b7fcc71b20b52f732eb1d48f6f73.
    
    Reason for revert: Messes up the order for restoration at end.
    
    Change-Id: I391aa73da259652b4200040bcecb8fd1cf4a7106

--- libs/WindowManager/Shell/src/com/android/wm/shell/recents/RecentsTransitionHandler.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/recents/RecentsTransitionHandler.java
@@ -581,9 +581,7 @@ public class RecentsTransitionHandler implements Transitions.TransitionHandler {
             final TransitionUtil.LeafTaskFilter leafTaskFilter =
                     new TransitionUtil.LeafTaskFilter();
             boolean hasTaskChange = false;
-            // Walk backwards so that higher z-order changes are recorded *last* in the assorted
-            // task lists. This way, when the are added, the on-top tasks are drawn on top.
-            for (int i = info.getChanges().size() - 1; i >= 0; --i) {
+            for (int i = 0; i < info.getChanges().size(); ++i) {
                 final TransitionInfo.Change change = info.getChanges().get(i);
                 final ActivityManager.RunningTaskInfo taskInfo = change.getTaskInfo();
                 if (taskInfo != null
