a91cd0ae432c903a2701777b2e8d3be25847f98a
Michal Brzezinski <brzezinski@google.com>
2023-09-26 16:20:10 +0100

    Fixing condition for logging wtf
    WTF log should happen only when we're in wrong state but current condition was triggered also on lockscreen when shade is kind of always expanded.
    Changing so its triggered only in unlocked state.
    
    Bug: 297709179
    Test: just changing logging
    Change-Id: Ia6900145849b36586309470788d28d2f1a77dde3

--- packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
+++ packages/SystemUI/src/com/android/systemui/shade/QuickSettingsController.java
@@ -791,7 +791,8 @@ public class QuickSettingsController implements Dumpable {
     /** update Qs height state */
     public void setExpansionHeight(float height) {
         // TODO(b/277909752): remove below log when bug is fixed
-        if (mSplitShadeEnabled && mShadeExpandedFraction == 1.0f && height == 0) {
+        if (mSplitShadeEnabled && mShadeExpandedFraction == 1.0f && height == 0
+                && mBarState == SHADE) {
             Log.wtf(TAG,
                     "setting QS height to 0 in split shade while shade is open(ing). "
                             + "Value of mExpandImmediate = " + mExpandImmediate);
