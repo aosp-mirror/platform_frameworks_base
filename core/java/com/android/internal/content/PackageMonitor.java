da12a3dd1175e9cae11546bd562566f65a7e3d04
Joanne Chung <joannechung@google.com>
2023-09-27 11:58:41 +0000

    Remove onHandleUserStop from PackageMonitor
    Remove onHandleUserStop from PackageMonitor, no real usage for this
    callback.
    
    Bug: 300309050
    Test: atest PackageMonitorTest
    Test: atest atest PackageUpdateMonitorTest
    
    Change-Id: I24fb43de14ef1ee3e70a32ed93d6a2d3751c878b

--- core/java/com/android/internal/content/PackageMonitor.java
+++ core/java/com/android/internal/content/PackageMonitor.java
@@ -48,7 +48,6 @@ public abstract class PackageMonitor extends android.content.BroadcastReceiver {
     static final String TAG = "PackageMonitor";
 
     final IntentFilter mPackageFilt;
-    final IntentFilter mNonDataFilt;
 
     Context mRegisteredContext;
     Handler mRegisteredHandler;
@@ -78,13 +77,6 @@ public abstract class PackageMonitor extends android.content.BroadcastReceiver {
         if (isCore) {
             mPackageFilt.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
         }
-
-        mNonDataFilt = new IntentFilter();
-        // UserController sends the broadcast
-        mNonDataFilt.addAction(Intent.ACTION_USER_STOPPED);
-        if (isCore) {
-            mNonDataFilt.setPriority(IntentFilter.SYSTEM_HIGH_PRIORITY);
-        }
     }
 
     @UnsupportedAppUsage
@@ -111,10 +103,8 @@ public abstract class PackageMonitor extends android.content.BroadcastReceiver {
         mRegisteredHandler = Objects.requireNonNull(handler);
         if (user != null) {
             context.registerReceiverAsUser(this, user, mPackageFilt, null, mRegisteredHandler);
-            context.registerReceiverAsUser(this, user, mNonDataFilt, null, mRegisteredHandler);
         } else {
             context.registerReceiver(this, mPackageFilt, null, mRegisteredHandler);
-            context.registerReceiver(this, mNonDataFilt, null, mRegisteredHandler);
         }
         if (mPackageMonitorCallback == null) {
             PackageManager pm = mRegisteredContext.getPackageManager();
@@ -214,9 +204,6 @@ public abstract class PackageMonitor extends android.content.BroadcastReceiver {
 
     public boolean onHandleForceStop(Intent intent, String[] packages, int uid, boolean doit) {
         return false;
-    }
-
-    public void onHandleUserStop(Intent intent, int userHandle) {
     }
 
     public void onUidRemoved(int uid) {
@@ -463,10 +450,6 @@ public abstract class PackageMonitor extends android.content.BroadcastReceiver {
                     intent.getIntExtra(Intent.EXTRA_UID, 0), true);
         } else if (Intent.ACTION_UID_REMOVED.equals(action)) {
             onUidRemoved(intent.getIntExtra(Intent.EXTRA_UID, 0));
-        } else if (Intent.ACTION_USER_STOPPED.equals(action)) {
-            if (intent.hasExtra(Intent.EXTRA_USER_HANDLE)) {
-                onHandleUserStop(intent, intent.getIntExtra(Intent.EXTRA_USER_HANDLE, 0));
-            }
         } else if (Intent.ACTION_EXTERNAL_APPLICATIONS_AVAILABLE.equals(action)) {
             String[] pkgList = intent.getStringArrayExtra(Intent.EXTRA_CHANGED_PACKAGE_LIST);
             mAppearingPackages = pkgList;
