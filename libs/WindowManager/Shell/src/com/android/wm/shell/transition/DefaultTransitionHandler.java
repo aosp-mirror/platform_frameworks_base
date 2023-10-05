57e14c67603bd4f507bee857f219bf0c8495d436
Louis Chang <louischang@google.com>
2023-10-05 08:26:08 +0000

    Revert "Explicitly release all surfaces after transition completed"
    This reverts commit 59cc423e76481bb279b6df3b30129a2b1bed3b5d.
    
    Reason for revert: 303160557
    
    Change-Id: I82e1fb5786b21da59ce0823b9d4fc8eea41216d3

--- libs/WindowManager/Shell/src/com/android/wm/shell/transition/DefaultTransitionHandler.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/transition/DefaultTransitionHandler.java
@@ -323,7 +323,6 @@ public class DefaultTransitionHandler implements Transitions.TransitionHandler {
         final Runnable onAnimFinish = () -> {
             if (!animations.isEmpty()) return;
             mAnimations.remove(transition);
-            info.releaseAllSurfaces();
             finishCallback.onTransitionFinished(null /* wct */);
         };
 
