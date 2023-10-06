228d742fd00781bed7fbae5df99f4d121ae475c0
Peter Kalauskas <peskal@google.com>
2023-10-06 21:04:53 +0000

    Fix AOD flicker during lockscreen transition
    When config_displayBlanksAfterDoze=true, disable performance
    optimization for LOCKSCREEN_TRANSITION_FROM_AOD; otherwise the clock
    will flicker.
    
    Bug: 301913237
    Test: Enable AOD, turn screen off and on, clock should not flicker
    Change-Id: I989455111cbde31991364118c29372038c5b15b0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/CentralSurfacesImpl.java
@@ -2900,7 +2900,10 @@ public class CentralSurfacesImpl implements CoreStartable, CentralSurfaces {
 
                 updateVisibleToUser();
                 updateIsKeyguard();
+                // TODO(b/301913237): can't delay transition if config_displayBlanksAfterDoze=true,
+                // otherwise, the clock will flicker during LOCKSCREEN_TRANSITION_FROM_AOD
                 mShouldDelayLockscreenTransitionFromAod = mDozeParameters.getAlwaysOn()
+                        && !mDozeParameters.getDisplayNeedsBlanking()
                         && mFeatureFlags.isEnabled(
                                 Flags.ZJ_285570694_LOCKSCREEN_TRANSITION_FROM_AOD);
                 if (!mShouldDelayLockscreenTransitionFromAod) {
