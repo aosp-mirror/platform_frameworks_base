a176edb7e13b3597bced1a7de87edbd2113bc91f
Fabián Kozynski <kozynski@google.com>
2023-10-13 16:19:27 -0400

    Unbind TileService onNullBinding
    Test: atest TileLifecycleManagerTest
    Test: manual: adb shell dumpsys activity service
    Test: sts test
    Bug: 300903792
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:3b93880c0b0052fe03c781a9768b81b098a353c0)
    Merged-In: Ia8126ac65432b124683960e3ebf47301ba6172a1
    Change-Id: Ia8126ac65432b124683960e3ebf47301ba6172a1

--- packages/SystemUI/src/com/android/systemui/qs/external/TileLifecycleManager.java
+++ packages/SystemUI/src/com/android/systemui/qs/external/TileLifecycleManager.java
@@ -243,6 +243,11 @@ public class TileLifecycleManager extends BroadcastReceiver implements
     }
 
     @Override
+    public void onNullBinding(ComponentName name) {
+        setBindService(false);
+    }
+
+    @Override
     public void onServiceDisconnected(ComponentName name) {
         if (DEBUG) Log.d(TAG, "onServiceDisconnected " + name);
         handleDeath();
