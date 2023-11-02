e6820deca4b93585dffe0e6f03dbf60dae412cfe
chelseahao <chelseahao@google.com>
2023-11-02 17:08:06 +0800

    Launch BLUETOOTH_QS_TILE_DIALOG to droidfood.
    Bug: 296821508
    
    Test: In teamfood for several weeks, no bug filed
    Change-Id: I711dae58ba4e602569590ddb3823d59066d6f900

--- packages/SystemUI/src/com/android/systemui/flags/Flags.kt
+++ packages/SystemUI/src/com/android/systemui/flags/Flags.kt
@@ -802,7 +802,7 @@ object Flags {
 
     /** Enable showing a dialog when clicking on Quick Settings bluetooth tile. */
     @JvmField
-    val BLUETOOTH_QS_TILE_DIALOG = unreleasedFlag("bluetooth_qs_tile_dialog", teamfood = true)
+    val BLUETOOTH_QS_TILE_DIALOG = unreleasedFlag("bluetooth_qs_tile_dialog")
 
     // TODO(b/300995746): Tracking Bug
     /** A resource flag for whether the communal service is enabled. */
