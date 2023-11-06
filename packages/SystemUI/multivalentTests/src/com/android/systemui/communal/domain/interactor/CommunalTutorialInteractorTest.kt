07dbee8abea6b50423ba7e195687c5a409e34fc2
Darrell Shi <darrellshi@google.com>
2023-11-06 14:35:56 -0800

    Move CommunalTutorialInteractorTest to multivalentTests
    Bug: 309471456
    Test: atest CommunalTutorialInteractorTest
    Flag: ACONFIG com.android.systemui.communal_hub DEVELOPMENT
    Change-Id: Iaddf56eb149f1b1174a11172aecc8c8893d15490

--- packages/SystemUI/multivalentTests/src/com/android/systemui/communal/domain/interactor/CommunalTutorialInteractorTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/communal/domain/interactor/CommunalTutorialInteractorTest.kt
@@ -19,6 +19,7 @@ package com.android.systemui.communal.domain.interactor
 import android.provider.Settings.Secure.HUB_MODE_TUTORIAL_COMPLETED
 import android.provider.Settings.Secure.HUB_MODE_TUTORIAL_NOT_STARTED
 import android.provider.Settings.Secure.HUB_MODE_TUTORIAL_STARTED
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
 import com.android.systemui.communal.data.repository.FakeCommunalRepository
@@ -43,12 +44,11 @@ import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.junit.runners.JUnit4
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 
 @SmallTest
-@RunWith(JUnit4::class)
+@RunWith(AndroidJUnit4::class)
 class CommunalTutorialInteractorTest : SysuiTestCase() {
 
     @Mock private lateinit var userTracker: UserTracker
