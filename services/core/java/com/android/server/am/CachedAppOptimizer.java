4f7bfa5c6dbf43f8b4e46ef3a7979dac47c2ad0c
Li Li <dualli@google.com>
2023-11-06 16:43:41 -0800

    Skip binder error handler if freezer is disabled
    Although freezer has been enforced to be enabled, it can be disabled on
    legacy vendor freeze project. And it can be temporarily disabled when
    taking a bugreport.
    
    Bug: 309448826
    Test: Disable freezer and boot Android
    Change-Id: I380c892a6cb92adfe739fb9cafca3c902772a384
    Merged-In: I380c892a6cb92adfe739fb9cafca3c902772a384

--- services/core/java/com/android/server/am/CachedAppOptimizer.java
+++ services/core/java/com/android/server/am/CachedAppOptimizer.java
@@ -2575,7 +2575,7 @@ public final class CachedAppOptimizer {
 
         // Do nothing if the binder error callback is not enabled.
         // That means the frozen apps in a wrong state will be killed when they are unfrozen later.
-        if (!mFreezerBinderCallbackEnabled) {
+        if (!mUseFreezer || !mFreezerBinderCallbackEnabled) {
             return;
         }
 
