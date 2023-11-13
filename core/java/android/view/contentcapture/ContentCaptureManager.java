fa4334fc33f548857981cd280e6c2403f95ceb36
MingWei <mingweiliao@google.com>
2023-11-13 17:49:26 +0000

    Enable content capture running on background thread
    1. Make sure works inside MainContentCaptureSession are delegated to the
       assigned thread. So even when the works are not done in UiThread, the
       synchronization is still guaranteed.
    2. Remove requirement of ContentProtectionEventProcessor running on
       UiThread. MainContentCaptureSession is the only caller to the
       processor. And it is guaranteed that only the assigned thread will be
       used. So no need to restrict it on UiThread.
    3. Move notifyContentCaptureEvents work away from ViewRootImpl and let
       MainContentCaptureSession decide how to process this.
    
    Test: CtsContentCaptureServiceTestCases, MainContentCaptureSessionTest.
    BUG: 309411951
    Change-Id: I13ce958da2280171fb2eb85b3321e3c7a7c5d0aa

--- core/java/android/view/contentcapture/ContentCaptureManager.java
+++ core/java/android/view/contentcapture/ContentCaptureManager.java
@@ -18,6 +18,7 @@ package android.view.contentcapture;
 import static android.view.contentcapture.ContentCaptureHelper.sDebug;
 import static android.view.contentcapture.ContentCaptureHelper.sVerbose;
 import static android.view.contentcapture.ContentCaptureHelper.toSet;
+import static android.view.contentcapture.flags.Flags.runOnBackgroundThreadEnabled;
 
 import android.annotation.CallbackExecutor;
 import android.annotation.IntDef;
@@ -52,6 +53,7 @@ import android.view.contentcapture.ContentCaptureSession.FlushReason;
 
 import com.android.internal.annotations.GuardedBy;
 import com.android.internal.annotations.VisibleForTesting;
+import com.android.internal.os.BackgroundThread;
 import com.android.internal.util.RingBuffer;
 import com.android.internal.util.SyncResultReceiver;
 
@@ -495,10 +497,9 @@ public final class ContentCaptureManager {
     @GuardedBy("mLock")
     private int mFlags;
 
-    // TODO(b/119220549): use UI Thread directly (as calls are one-way) or a shared thread / handler
-    // held at the Application level
-    @NonNull
-    private final Handler mHandler;
+    @Nullable
+    @GuardedBy("mLock")
+    private Handler mHandler;
 
     @GuardedBy("mLock")
     private MainContentCaptureSession mMainSession;
@@ -562,11 +563,6 @@ public final class ContentCaptureManager {
 
         if (sVerbose) Log.v(TAG, "Constructor for " + context.getPackageName());
 
-        // TODO(b/119220549): we might not even need a handler, as the IPCs are oneway. But if we
-        // do, then we should optimize it to run the tests after the Choreographer finishes the most
-        // important steps of the frame.
-        mHandler = Handler.createAsync(Looper.getMainLooper());
-
         mDataShareAdapterResourceManager = new LocalDataShareAdapterResourceManager();
 
         if (mOptions.contentProtectionOptions.enableReceiver
@@ -594,11 +590,25 @@ public final class ContentCaptureManager {
     public MainContentCaptureSession getMainContentCaptureSession() {
         synchronized (mLock) {
             if (mMainSession == null) {
-                mMainSession = new MainContentCaptureSession(mContext, this, mHandler, mService);
+                mMainSession = new MainContentCaptureSession(
+                        mContext, this, prepareContentCaptureHandler(), mService);
                 if (sVerbose) Log.v(TAG, "getMainContentCaptureSession(): created " + mMainSession);
             }
             return mMainSession;
         }
+    }
+
+    @NonNull
+    @GuardedBy("mLock")
+    private Handler prepareContentCaptureHandler() {
+        if (mHandler == null) {
+            if (runOnBackgroundThreadEnabled()) {
+                mHandler = BackgroundThread.getHandler();
+            } else {
+                mHandler = Handler.createAsync(Looper.getMainLooper());
+            }
+        }
+        return mHandler;
     }
 
     /** @hide */
