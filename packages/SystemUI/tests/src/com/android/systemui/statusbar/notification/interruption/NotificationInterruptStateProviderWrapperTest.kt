118c107309269e13b66b542d959441f417e9b3d1
Julia Tuttle <juliatuttle@google.com>
2023-11-20 14:20:37 -0500

    Force refactor flag disabled in some unittests
    BubblesTest and CentralSurfacesImplTest use the old implementation
    explicitly as a dependency, so they fail if the refactor flag is
    enabled.
    
    This should fix the presubmit failure in ag/25426992, allowing the flag
    to advance to STAGING.
    
    Long-term, I plan to update these tests to check the flag and create the
    old or new implementation, but short-term, forcing it disabled is easier
    and won't hurt test coverage, so I'm doing it first.
    
    While I'm here, now that I know SysuiTestCase creates a SetFlagsRule,
    stop creating my own redundant one in the refactor's own tests.
    
    Bug: 261728888
    Test: atest BubblesTest CentralSurfacesImplTest
    Test: atest NotificationInterruptStateProviderWrapperTest
    Test: atest VisualInterruptionDecisionProviderImplTest
    Flag: ACONFIG com.android.systemui.visual_interruptions_refactor DEVELOPMENT
    Change-Id: I22afb5c8e6e263b5000cc2e256d4a16fe9806b44

--- packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderWrapperTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/statusbar/notification/interruption/NotificationInterruptStateProviderWrapperTest.kt
@@ -18,7 +18,6 @@ package com.android.systemui.statusbar.notification.interruption
 
 import android.testing.AndroidTestingRunner
 import androidx.test.filters.SmallTest
-import com.android.systemui.Flags
 import com.android.systemui.statusbar.notification.collection.NotificationEntryBuilder
 import com.android.systemui.statusbar.notification.interruption.NotificationInterruptStateProvider.FullScreenIntentDecision
 import com.android.systemui.statusbar.notification.interruption.NotificationInterruptStateProvider.FullScreenIntentDecision.FSI_DEVICE_NOT_INTERACTIVE
@@ -37,7 +36,7 @@ import org.junit.runner.RunWith
 @RunWith(AndroidTestingRunner::class)
 class NotificationInterruptStateProviderWrapperTest : VisualInterruptionDecisionProviderTestBase() {
     init {
-        setFlagsRule.disableFlags(Flags.FLAG_VISUAL_INTERRUPTIONS_REFACTOR)
+        mSetFlagsRule.disableFlags(VisualInterruptionRefactor.FLAG_NAME)
     }
 
     override val provider by lazy {
