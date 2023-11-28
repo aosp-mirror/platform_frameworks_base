b597c3a7d91ea5273fbd80ec1941d7b57edd7075
Vladimir Komsiyski <vladokom@google.com>
2023-11-28 14:47:13 +0100

    VDM IME 3/n: Respect VD IME in IMMS
     - Write default device IME to settings when we switch away from it
     - Fallback to NO_IME if the VDM custom IME is not available
    
    Adding an overload of setInputMethodLocked because we don't want to
    change the default IME if we're currently on a virtual device but we
    do want to update the default device IME setting so the correct one
    is restored once we move back to the default device.
    
    Bug: 287269288
    Test: CTS
    Change-Id: Id05de00da4cc6fce284697acaf76237b0c7b7715

--- packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
+++ packages/SettingsProvider/test/src/android/provider/SettingsBackupTest.java
@@ -737,6 +737,7 @@ public class SettingsBackupTest {
                  Settings.Secure.CONNECTIVITY_RELEASE_PENDING_INTENT_DELAY_MS,
                  Settings.Secure.CONTENT_CAPTURE_ENABLED,
                  Settings.Secure.DEFAULT_INPUT_METHOD,
+                 Settings.Secure.DEFAULT_DEVICE_INPUT_METHOD,
                  Settings.Secure.DEVICE_PAIRED,
                  Settings.Secure.DIALER_DEFAULT_APPLICATION,
                  Settings.Secure.DISABLED_PRINT_SERVICES,
