be0ed8981f187e45c229fb462eb124993cfd9dc4
Tracy Zhou <tracyzhou@google.com>
2023-12-06 13:18:12 -0800

    Do not set isImmersive in TaskbarDelegate
    allow_gesture and immersive_mode are opposite of each other, and currently we set one or another, check one or another in different places. We should consolidate the two. Hence, we are removing all references of immersive_mode (allow_gesture has more usages).
    
    Bug: 313033827
    Test: N/A
    Change-Id: I39e4eb64c47a4caab43959bc7a1434ebb94707e4

--- packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
+++ packages/SystemUI/shared/src/com/android/systemui/shared/system/QuickStepContract.java
@@ -103,8 +103,6 @@ public class QuickStepContract {
     public static final int SYSUI_STATE_BACK_DISABLED = 1 << 22;
     // The bubble stack is expanded AND the mange menu for bubbles is expanded on top of it.
     public static final int SYSUI_STATE_BUBBLES_MANAGE_MENU_EXPANDED = 1 << 23;
-    // The current app is in immersive mode
-    public static final int SYSUI_STATE_IMMERSIVE_MODE = 1 << 24;
     // The voice interaction session window is showing
     public static final int SYSUI_STATE_VOICE_INTERACTION_WINDOW_SHOWING = 1 << 25;
     // Freeform windows are showing in desktop mode
@@ -162,7 +160,6 @@ public class QuickStepContract {
             SYSUI_STATE_DEVICE_DOZING,
             SYSUI_STATE_BACK_DISABLED,
             SYSUI_STATE_BUBBLES_MANAGE_MENU_EXPANDED,
-            SYSUI_STATE_IMMERSIVE_MODE,
             SYSUI_STATE_VOICE_INTERACTION_WINDOW_SHOWING,
             SYSUI_STATE_FREEFORM_ACTIVE_IN_DESKTOP_MODE,
             SYSUI_STATE_DEVICE_DREAMING,
@@ -246,9 +243,6 @@ public class QuickStepContract {
         }
         if ((flags & SYSUI_STATE_BUBBLES_MANAGE_MENU_EXPANDED) != 0) {
             str.add("bubbles_mange_menu_expanded");
-        }
-        if ((flags & SYSUI_STATE_IMMERSIVE_MODE) != 0) {
-            str.add("immersive_mode");
         }
         if ((flags & SYSUI_STATE_VOICE_INTERACTION_WINDOW_SHOWING) != 0) {
             str.add("vis_win_showing");
