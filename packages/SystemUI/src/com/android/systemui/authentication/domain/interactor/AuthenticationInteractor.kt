10f024bb577d91c471ac6efff6c6a6d94160a5d4
Danny Burakov <burakov@google.com>
2023-12-06 17:51:54 +0000

    Revert "[flexiglass] Force garbage collection after successful authentication."
    This reverts commit 0180d5bf02b515237f01856364afbcea080557d6.
    
    Reason for revert: breaks a unit test.
    
    Change-Id: Ia654605710d47b5c6d6bef5ae2b175a325727f86

--- packages/SystemUI/src/com/android/systemui/authentication/domain/interactor/AuthenticationInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/authentication/domain/interactor/AuthenticationInteractor.kt
@@ -45,7 +45,6 @@ import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 /**
  * Hosts application business logic related to user authentication.
@@ -228,10 +227,6 @@ constructor(
             // Since authentication succeeded, we should refresh throttling to make sure that our
             // state is completely reflecting the upstream source of truth.
             refreshThrottling()
-
-            // Force a garbage collection in an attempt to erase any credentials left in memory.
-            // Do it after a 5-sec delay to avoid making the bouncer dismiss animation janky.
-            initiateGarbageCollection(delayMs = 5000)
         }
 
         return if (authenticationResult.isSuccessful) {
@@ -312,15 +307,6 @@ constructor(
                         .map { LockPatternView.Cell.of(it.y, it.x) }
                 )
             else -> null
-        }
-    }
-
-    private suspend fun initiateGarbageCollection(delayMs: Long) {
-        withContext(backgroundDispatcher) {
-            delay(delayMs)
-            System.gc()
-            System.runFinalization()
-            System.gc()
         }
     }
 
