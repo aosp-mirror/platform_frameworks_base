64aea7fe5f8d8b806448fdbcf6558620cd00f01c
Beverly <beverlyt@google.com>
2023-12-06 20:57:40 +0000

    Accessibility updates for UDFPS device entry
    Adds a UDFPS a11y overlay on the lockscreen that takes
    up the bottom half of the screen. If there are hover
    events over this area when touchExploration is enabled,
    there will be a11y announcements guiding the user to
    the UDFPS sensor.
    
    This CL integrates this functionality on the lockscreen
    only and does not yet support the alternate bouncer.
    
    Updates Kosmos & testing.
    
    Flag: NONE
    Bug: 310044681
    Test: atest UdfpsAccessibilityOverlayViewModelTest
    Change-Id: I9b5f680edc9901ba4c06bdecccef0bac7b1cb424

--- packages/SystemUI/tests/src/com/android/systemui/keyguard/ui/view/layout/blueprints/DefaultKeyguardBlueprintTest.kt
+++ packages/SystemUI/tests/src/com/android/systemui/keyguard/ui/view/layout/blueprints/DefaultKeyguardBlueprintTest.kt
@@ -37,6 +37,7 @@ import com.android.systemui.keyguard.ui.view.layout.sections.DefaultSettingsPopu
 import com.android.systemui.keyguard.ui.view.layout.sections.DefaultShortcutsSection
 import com.android.systemui.keyguard.ui.view.layout.sections.DefaultStatusBarSection
 import com.android.systemui.keyguard.ui.view.layout.sections.DefaultStatusViewSection
+import com.android.systemui.keyguard.ui.view.layout.sections.DefaultUdfpsAccessibilityOverlaySection
 import com.android.systemui.keyguard.ui.view.layout.sections.SmartspaceSection
 import com.android.systemui.keyguard.ui.view.layout.sections.SplitShadeGuidelines
 import com.android.systemui.util.mockito.whenever
@@ -70,7 +71,8 @@ class DefaultKeyguardBlueprintTest : SysuiTestCase() {
     @Mock private lateinit var communalTutorialIndicatorSection: CommunalTutorialIndicatorSection
     @Mock private lateinit var clockSection: ClockSection
     @Mock private lateinit var smartspaceSection: SmartspaceSection
-
+    @Mock
+    private lateinit var udfpsAccessibilityOverlaySection: DefaultUdfpsAccessibilityOverlaySection
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
@@ -90,6 +92,7 @@ class DefaultKeyguardBlueprintTest : SysuiTestCase() {
                 communalTutorialIndicatorSection,
                 clockSection,
                 smartspaceSection,
+                udfpsAccessibilityOverlaySection,
             )
     }
 
