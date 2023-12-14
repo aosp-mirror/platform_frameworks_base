ad6a32fe34c2ceb29eb4bdbae3ca34a640fe4418
Chiachang Wang <chiachangwang@google.com>
2023-12-14 11:49:47 +0000

    Replace calls for updating permission to protect sockets from VPN
    NetworkManagementService is deprecated and the calls to NMS are
    expected to move to INetd if possible. The implementation for
    denyProtect() and allowProtect() redirect the calls to INetd.
    The permission enforcement checks the context inside NMS which
    is irrevalent to the caller. Thus, this commit acts as a no-op
    change.
    
    Bug: 316253925
    Test: atest FrameworksNetTests
    Change-Id: I2febc7124ef182f544407015bae0765bc55d7cde

--- services/core/java/com/android/server/connectivity/Vpn.java
+++ services/core/java/com/android/server/connectivity/Vpn.java
@@ -1431,7 +1431,7 @@ public class Vpn {
             }
 
             try {
-                mNms.denyProtect(mOwnerUID);
+                mNetd.networkSetProtectDeny(mOwnerUID);
             } catch (Exception e) {
                 Log.wtf(TAG, "Failed to disallow UID " + mOwnerUID + " to call protect() " + e);
             }
@@ -1441,7 +1441,7 @@ public class Vpn {
             mOwnerUID = getAppUid(mContext, newPackage, mUserId);
             mIsPackageTargetingAtLeastQ = doesPackageTargetAtLeastQ(newPackage);
             try {
-                mNms.allowProtect(mOwnerUID);
+                mNetd.networkSetProtectAllow(mOwnerUID);
             } catch (Exception e) {
                 Log.wtf(TAG, "Failed to allow UID " + mOwnerUID + " to call protect() " + e);
             }
