4f04d21066c3a558dedd1e8242e1b4cd2d173531
Xiaowen Lei <xilei@google.com>
2023-12-14 21:49:58 +0000

    Rename non-Dream-specific precondition and filter.
    Context:
      https://googleplex-android-review.git.corp.google.com/c/platform/frameworks/base/+/25615612/comment/e0746cce_b560e013/
    
    Flag: None
    Bug: 314203588
    Test: LockscreenTargetFilterTest
    Change-Id: Ic961addf9200294c1323e24618fb3150ef5cc85d

--- packages/SystemUI/multivalentTests/src/com/android/systemui/smartspace/LockscreenTargetFilterTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/smartspace/LockscreenTargetFilterTest.kt
@@ -27,7 +27,7 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.android.systemui.SysuiTestCase
 import com.android.systemui.settings.UserTracker
-import com.android.systemui.smartspace.filters.LockscreenAndDreamTargetFilter
+import com.android.systemui.smartspace.filters.LockscreenTargetFilter
 import com.android.systemui.util.concurrency.Execution
 import com.android.systemui.util.mockito.any
 import com.android.systemui.util.mockito.eq
@@ -52,55 +52,52 @@ import org.mockito.MockitoAnnotations
 @SmallTest
 @TestableLooper.RunWithLooper
 @RunWith(AndroidJUnit4::class)
-class LockscreenAndDreamTargetFilterTest : SysuiTestCase() {
-    @Mock
-    private lateinit var secureSettings: SecureSettings
+class LockscreenTargetFilterTest : SysuiTestCase() {
+    @Mock private lateinit var secureSettings: SecureSettings
 
-    @Mock
-    private lateinit var userTracker: UserTracker
+    @Mock private lateinit var userTracker: UserTracker
 
-    @Mock
-    private lateinit var execution: Execution
+    @Mock private lateinit var execution: Execution
 
-    @Mock
-    private lateinit var handler: Handler
+    @Mock private lateinit var handler: Handler
 
-    @Mock
-    private lateinit var contentResolver: ContentResolver
+    @Mock private lateinit var contentResolver: ContentResolver
 
-    @Mock
-    private lateinit var uiExecution: Executor
+    @Mock private lateinit var uiExecution: Executor
 
-    @Mock
-    private lateinit var userHandle: UserHandle
+    @Mock private lateinit var userHandle: UserHandle
 
-    @Mock
-    private lateinit var listener: SmartspaceTargetFilter.Listener
+    @Mock private lateinit var listener: SmartspaceTargetFilter.Listener
 
-    @Mock
-    private lateinit var lockScreenAllowPrivateNotificationsUri: Uri
+    @Mock private lateinit var lockScreenAllowPrivateNotificationsUri: Uri
 
     @Before
     fun setup() {
         MockitoAnnotations.initMocks(this)
         `when`(userTracker.userHandle).thenReturn(userHandle)
-        `when`(secureSettings
-                .getUriFor(eq(Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS)))
-                .thenReturn(lockScreenAllowPrivateNotificationsUri)
+        `when`(
+                secureSettings.getUriFor(
+                    eq(Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS)
+                )
+            )
+            .thenReturn(lockScreenAllowPrivateNotificationsUri)
     }
 
-    /**
-     * Ensures {@link Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS} is
-     * tracked.
-     */
+    /** Ensures {@link Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS} is tracked. */
     @Test
     fun testLockscreenAllowPrivateNotifications() {
         var setting = Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS
-        `when`(secureSettings
-                .getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
-                .thenReturn(0)
-        var filter = LockscreenAndDreamTargetFilter(secureSettings, userTracker, execution, handler,
-                contentResolver, uiExecution)
+        `when`(secureSettings.getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
+            .thenReturn(0)
+        var filter =
+            LockscreenTargetFilter(
+                secureSettings,
+                userTracker,
+                execution,
+                handler,
+                contentResolver,
+                uiExecution
+            )
 
         filter.addListener(listener)
         var smartspaceTarget = mock(SmartspaceTarget::class.java)
@@ -110,12 +107,16 @@ class LockscreenAndDreamTargetFilterTest : SysuiTestCase() {
 
         var settingCaptor = ArgumentCaptor.forClass(ContentObserver::class.java)
 
-        verify(contentResolver).registerContentObserver(eq(lockScreenAllowPrivateNotificationsUri),
-                anyBoolean(), settingCaptor.capture(), anyInt())
+        verify(contentResolver)
+            .registerContentObserver(
+                eq(lockScreenAllowPrivateNotificationsUri),
+                anyBoolean(),
+                settingCaptor.capture(),
+                anyInt()
+            )
 
-        `when`(secureSettings
-                .getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
-                .thenReturn(1)
+        `when`(secureSettings.getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
+            .thenReturn(1)
 
         clearInvocations(listener)
         settingCaptor.value.onChange(false, mock(Uri::class.java))
@@ -123,23 +124,28 @@ class LockscreenAndDreamTargetFilterTest : SysuiTestCase() {
         assertThat(filter.filterSmartspaceTarget(smartspaceTarget)).isTrue()
     }
 
-    /**
-     * Ensures user switches are tracked.
-     */
+    /** Ensures user switches are tracked. */
     @Test
     fun testUserSwitchCallback() {
         var setting = Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS
-        `when`(secureSettings
-                .getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
-                .thenReturn(0)
-        var filter = LockscreenAndDreamTargetFilter(secureSettings, userTracker, execution, handler,
-                contentResolver, uiExecution)
+        `when`(secureSettings.getIntForUser(eq(setting) ?: setting, anyInt(), anyInt()))
+            .thenReturn(0)
+        var filter =
+            LockscreenTargetFilter(
+                secureSettings,
+                userTracker,
+                execution,
+                handler,
+                contentResolver,
+                uiExecution
+            )
 
         filter.addListener(listener)
 
-        var userTrackerCallback = withArgCaptor<UserTracker.Callback> {
-            verify(userTracker).addCallback(capture(), any())
-        }
+        var userTrackerCallback =
+            withArgCaptor<UserTracker.Callback> {
+                verify(userTracker).addCallback(capture(), any())
+            }
 
         clearInvocations(listener)
         userTrackerCallback.onUserChanged(0, context)
