0f742eb1d49d50bd998dd3669a341090acec72de
Kiran Ramachandra <kiranmr@google.com>
2023-12-19 21:33:56 +0000

    RESTRICT AUTOMERGE Added limitations for attributions to handle invalid cases
    Bug: 304983146
    Test: Modified and introduced new tests to verify change -> atest CtsAppOpsTestCases:AttributionTest
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:63d122cf0e18ff6d8e77b7bcc8f0f3f8d4e4a018)
    Merged-In: Iee26fdb9cf1ca0fa8905e22732c32ec7d9b80fea
    Change-Id: Iee26fdb9cf1ca0fa8905e22732c32ec7d9b80fea

--- services/core/java/com/android/server/pm/pkg/component/ParsedAttributionImpl.java
+++ services/core/java/com/android/server/pm/pkg/component/ParsedAttributionImpl.java
@@ -38,7 +38,7 @@ import java.util.List;
 public class ParsedAttributionImpl implements ParsedAttribution, Parcelable {
 
     /** Maximum amount of attributions per package */
-    static final int MAX_NUM_ATTRIBUTIONS = 10000;
+    static final int MAX_NUM_ATTRIBUTIONS = 1000;
 
     /** Tag of the attribution */
     private @NonNull String tag;
