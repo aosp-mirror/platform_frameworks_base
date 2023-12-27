893184a6c0385cf2ba17f0dd54fc492e08a54bc6
Riddle Hsu <riddlehsu@google.com>
2023-12-27 23:53:03 +0800

    Ignore orientation of finishing activity
    There is a special case that the invisible activity can still
    report orientation for who uses orientation_behind.
    e.g.
     Top activity: orientation_behind
     The occluded invisible activity behind: orientation_sensor
    Then it will pick sensor for top activity.
    
    But if the top activity is already finishing, it should stop
    to search the behind activities and keep current orientation.
    Otherwise an intermediate orientation may be chosen which
    may cause unnecessary orientation change.
    
    The case may happen when launching orientation_behind activity
    with clearTaskOnLaunch or intent flag CLEAR_TASK that will finish
    the original activity at the same time.
    
    Fix: 317680642
    Test: ActivityRecordTests#testGetOrientation
    Change-Id: I3c288ebd79cfe5b780d1f6427432d3c0bb0e568b

--- services/tests/wmtests/src/com/android/server/wm/ActivityRecordTests.java
+++ services/tests/wmtests/src/com/android/server/wm/ActivityRecordTests.java
@@ -2640,6 +2640,9 @@ public class ActivityRecordTests extends WindowTestsBase {
         // Can specify orientation if the current orientation candidate is orientation behind.
         assertEquals(SCREEN_ORIENTATION_LANDSCAPE,
                 activity.getOrientation(SCREEN_ORIENTATION_BEHIND));
+        activity.makeFinishingLocked();
+        assertEquals("Finishing activity must not report orientation",
+                SCREEN_ORIENTATION_UNSET, activity.getOrientation(SCREEN_ORIENTATION_BEHIND));
 
         final ActivityRecord translucentActivity = new ActivityBuilder(mAtm)
                 .setActivityTheme(android.R.style.Theme_Translucent)
