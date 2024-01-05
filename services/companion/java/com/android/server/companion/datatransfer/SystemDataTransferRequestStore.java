e20da0d99f75b2c0375930e3eeb8d2d6d2c5ebd7
Alex Greer <alexgreer@google.com>
2024-01-05 22:38:44 +0000

    Revert "[CDM] Remove userId from SystemDataTransferRequest xml"
    This reverts commit 200476e6ace8220b0905c29a6485800cd6da924f.
    
    Reason for revert: Android build monitor - suspected build break.
    
    Change-Id: If6e1ff5ee1c49cff77c7662fbe8f2dc5a2480b54

--- services/companion/java/com/android/server/companion/datatransfer/SystemDataTransferRequestStore.java
+++ services/companion/java/com/android/server/companion/datatransfer/SystemDataTransferRequestStore.java
@@ -67,6 +67,7 @@ import java.util.concurrent.TimeoutException;
  *   <request
  *     association_id="1"
  *     data_type="1"
+ *     user_id="12"
  *     is_user_consented="true"
  *   </request>
  * </requests>
@@ -83,6 +84,7 @@ public class SystemDataTransferRequestStore {
 
     private static final String XML_ATTR_ASSOCIATION_ID = "association_id";
     private static final String XML_ATTR_DATA_TYPE = "data_type";
+    private static final String XML_ATTR_USER_ID = "user_id";
     private static final String XML_ATTR_IS_USER_CONSENTED = "is_user_consented";
 
     private static final int READ_FROM_DISK_TIMEOUT = 5; // in seconds
@@ -195,7 +197,7 @@ public class SystemDataTransferRequestStore {
                 final TypedXmlPullParser parser = Xml.resolvePullParser(in);
                 XmlUtils.beginDocument(parser, XML_TAG_REQUESTS);
 
-                return readRequestsFromXml(userId, parser);
+                return readRequestsFromXml(parser);
             } catch (XmlPullParserException | IOException e) {
                 Slog.e(LOG_TAG, "Error while reading requests file", e);
                 return new ArrayList<>();
@@ -204,7 +206,7 @@ public class SystemDataTransferRequestStore {
     }
 
     @NonNull
-    private ArrayList<SystemDataTransferRequest> readRequestsFromXml(int userId,
+    private ArrayList<SystemDataTransferRequest> readRequestsFromXml(
             @NonNull TypedXmlPullParser parser) throws XmlPullParserException, IOException {
         if (!isStartOfTag(parser, XML_TAG_REQUESTS)) {
             throw new XmlPullParserException("The XML doesn't have start tag: " + XML_TAG_REQUESTS);
@@ -218,15 +220,14 @@ public class SystemDataTransferRequestStore {
                 break;
             }
             if (isStartOfTag(parser, XML_TAG_REQUEST)) {
-                requests.add(readRequestFromXml(userId, parser));
+                requests.add(readRequestFromXml(parser));
             }
         }
 
         return requests;
     }
 
-    private SystemDataTransferRequest readRequestFromXml(int userId,
-            @NonNull TypedXmlPullParser parser)
+    private SystemDataTransferRequest readRequestFromXml(@NonNull TypedXmlPullParser parser)
             throws XmlPullParserException, IOException {
         if (!isStartOfTag(parser, XML_TAG_REQUEST)) {
             throw new XmlPullParserException("XML doesn't have start tag: " + XML_TAG_REQUEST);
@@ -234,6 +235,7 @@ public class SystemDataTransferRequestStore {
 
         final int associationId = readIntAttribute(parser, XML_ATTR_ASSOCIATION_ID);
         final int dataType = readIntAttribute(parser, XML_ATTR_DATA_TYPE);
+        final int userId = readIntAttribute(parser, XML_ATTR_USER_ID);
         final boolean isUserConsented = readBooleanAttribute(parser, XML_ATTR_IS_USER_CONSENTED);
 
         switch (dataType) {
@@ -290,6 +292,7 @@ public class SystemDataTransferRequestStore {
 
         writeIntAttribute(serializer, XML_ATTR_ASSOCIATION_ID, request.getAssociationId());
         writeIntAttribute(serializer, XML_ATTR_DATA_TYPE, request.getDataType());
+        writeIntAttribute(serializer, XML_ATTR_USER_ID, request.getUserId());
         writeBooleanAttribute(serializer, XML_ATTR_IS_USER_CONSENTED, request.isUserConsented());
 
         serializer.endTag(null, XML_TAG_REQUEST);
