bae00621c4c00f7ba6ad99133c3dfb3430751e14
burakov <burakov@google.com>
2024-01-15 13:30:37 +0000

    [flexiglass] Force garbage collection after successful authentication.
    This reverts commit 10f024bb577d91c471ac6efff6c6a6d94160a5d4.
    
    Reason for revert: unit test passes.
    
    Fix: 308826153
    Test: Unit tests pass.
    Flag: ACONFIG com.android.systemui.scene_container DEVELOPMENT
    Change-Id: Ie097ae15f5308a6b0cc1e62e5eaac0c347a4a039

--- packages/SystemUI/tests/utils/src/com/android/systemui/authentication/domain/interactor/AuthenticationInteractorKosmos.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/authentication/domain/interactor/AuthenticationInteractorKosmos.kt
@@ -19,12 +19,14 @@ package com.android.systemui.authentication.domain.interactor
 import com.android.systemui.authentication.data.repository.authenticationRepository
 import com.android.systemui.kosmos.Kosmos
 import com.android.systemui.kosmos.applicationCoroutineScope
+import com.android.systemui.kosmos.testDispatcher
 import com.android.systemui.user.domain.interactor.selectedUserInteractor
 
 val Kosmos.authenticationInteractor by
     Kosmos.Fixture {
         AuthenticationInteractor(
             applicationScope = applicationCoroutineScope,
+            backgroundDispatcher = testDispatcher,
             repository = authenticationRepository,
             selectedUserInteractor = selectedUserInteractor,
         )
