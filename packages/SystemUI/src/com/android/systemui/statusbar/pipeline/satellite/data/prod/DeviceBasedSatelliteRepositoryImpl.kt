6ef29c5003eec058447a5244969b6fdc5aa74dfd
Priyanka Advani <padvani@google.com>
2024-01-16 21:09:55 +0000

    Revert "[Satellite] Input logging for the repository layer"
    Revert submission 25841762
    
    Reason for revert: Likely culprit for b/320529189. Will be running through ABTD before submitting the revert.
    
    Reverted changes: /q/submissionid:25841762
    
    Change-Id: Iac4d26ccc33d37e3023804429f6a8c9e2b9e02e9

--- packages/SystemUI/src/com/android/systemui/statusbar/pipeline/satellite/data/prod/DeviceBasedSatelliteRepositoryImpl.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/pipeline/satellite/data/prod/DeviceBasedSatelliteRepositoryImpl.kt
@@ -25,11 +25,6 @@ import com.android.systemui.common.coroutine.ConflatedCallbackFlow.conflatedCall
 import com.android.systemui.dagger.SysUISingleton
 import com.android.systemui.dagger.qualifiers.Application
 import com.android.systemui.dagger.qualifiers.Background
-import com.android.systemui.log.LogBuffer
-import com.android.systemui.log.core.LogLevel
-import com.android.systemui.log.core.MessageInitializer
-import com.android.systemui.log.core.MessagePrinter
-import com.android.systemui.statusbar.pipeline.dagger.OemSatelliteInputLog
 import com.android.systemui.statusbar.pipeline.satellite.data.DeviceBasedSatelliteRepository
 import com.android.systemui.statusbar.pipeline.satellite.data.prod.SatelliteSupport.Companion.whenSupported
 import com.android.systemui.statusbar.pipeline.satellite.data.prod.SatelliteSupport.NotSupported
@@ -129,7 +124,6 @@ constructor(
     satelliteManagerOpt: Optional<SatelliteManager>,
     @Background private val bgDispatcher: CoroutineDispatcher,
     @Application private val scope: CoroutineScope,
-    @OemSatelliteInputLog private val logBuffer: LogBuffer,
     private val systemClock: SystemClock,
 ) : DeviceBasedSatelliteRepository {
 
@@ -152,11 +146,6 @@ constructor(
                 ensureMinUptime(systemClock, MIN_UPTIME)
                 satelliteSupport.value = satelliteManager.checkSatelliteSupported()
 
-                logBuffer.i(
-                    { str1 = satelliteSupport.value.toString() },
-                    { "Checked for system support. support=$str1" },
-                )
-
                 // We only need to check location availability if this mode is supported
                 if (satelliteSupport.value is Supported) {
                     isSatelliteAllowedForCurrentLocation.subscriptionCount
@@ -171,9 +160,6 @@ constructor(
                                  * connection might cause more frequent checks.
                                  */
                                 while (true) {
-                                    logBuffer.i {
-                                        "requestIsSatelliteCommunicationAllowedForCurrentLocation"
-                                    }
                                     checkIsSatelliteAllowed()
                                     delay(POLLING_INTERVAL_MS)
                                 }
@@ -182,8 +168,6 @@ constructor(
                 }
             }
         } else {
-            logBuffer.i { "Satellite manager is null" }
-
             satelliteSupport.value = NotSupported
         }
     }
@@ -198,7 +182,6 @@ constructor(
     private fun connectionStateFlow(sm: SupportedSatelliteManager): Flow<SatelliteConnectionState> =
         conflatedCallbackFlow {
                 val cb = SatelliteModemStateCallback { state ->
-                    logBuffer.i({ int1 = state }) { "onSatelliteModemStateChanged: state=$int1" }
                     trySend(SatelliteConnectionState.fromModemState(state))
                 }
 
@@ -209,7 +192,7 @@ constructor(
                         sm.registerForSatelliteModemStateChanged(bgDispatcher.asExecutor(), cb)
                     registered = res == SATELLITE_RESULT_SUCCESS
                 } catch (e: Exception) {
-                    logBuffer.e("error registering for modem state", e)
+                    // Logging is in next commit
                 }
 
                 awaitClose { if (registered) sm.unregisterForSatelliteModemStateChanged(cb) }
@@ -223,9 +206,6 @@ constructor(
     private fun signalStrengthFlow(sm: SupportedSatelliteManager) =
         conflatedCallbackFlow {
                 val cb = NtnSignalStrengthCallback { signalStrength ->
-                    logBuffer.i({ int1 = signalStrength.level }) {
-                        "onNtnSignalStrengthChanged: level=$int1"
-                    }
                     trySend(signalStrength.level)
                 }
 
@@ -234,7 +214,7 @@ constructor(
                     sm.registerForNtnSignalStrengthChanged(bgDispatcher.asExecutor(), cb)
                     registered = true
                 } catch (e: Exception) {
-                    logBuffer.e("error registering for signal strength", e)
+                    // Logging is in next commit
                 }
 
                 awaitClose { if (registered) sm.unregisterForNtnSignalStrengthChanged(cb) }
@@ -248,15 +228,11 @@ constructor(
                 bgDispatcher.asExecutor(),
                 object : OutcomeReceiver<Boolean, SatelliteManager.SatelliteException> {
                     override fun onError(e: SatelliteManager.SatelliteException) {
-                        logBuffer.e(
-                            "Found exception when checking availability",
-                            e,
-                        )
+                        android.util.Log.e(TAG, "Found exception when checking for satellite: ", e)
                         isSatelliteAllowedForCurrentLocation.value = false
                     }
 
                     override fun onResult(allowed: Boolean) {
-                        logBuffer.i { allowed.toString() }
                         isSatelliteAllowedForCurrentLocation.value = allowed
                     }
                 }
@@ -278,12 +254,6 @@ constructor(
                     }
 
                     override fun onError(error: SatelliteManager.SatelliteException) {
-                        logBuffer.e(
-                            "Exception when checking for satellite support. " +
-                                "Assuming it is not supported for this device.",
-                            error,
-                        )
-
                         // Assume that an error means it's not supported
                         continuation.resume(NotSupported)
                     }
@@ -309,19 +279,5 @@ constructor(
                 delay(timeTilMinUptime)
             }
         }
-
-        /** A couple of convenience logging methods rather than a whole class */
-        private fun LogBuffer.i(
-            initializer: MessageInitializer = {},
-            printer: MessagePrinter,
-        ) = this.log(TAG, LogLevel.INFO, initializer, printer)
-
-        private fun LogBuffer.e(message: String, exception: Throwable? = null) =
-            this.log(
-                tag = TAG,
-                level = LogLevel.ERROR,
-                message = message,
-                exception = exception,
-            )
     }
 }
