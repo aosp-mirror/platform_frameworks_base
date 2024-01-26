0452f131013479db763b2f76190ca47d77d28aab
chihtinglo <chihtinglo@google.com>
2024-01-27 02:45:00 +0800

    Mark WindowMagnificationAnimationControllerTest as flaky
    The WindowMagnificationAnimationControllerTest is flaky on presubmit, but the tests are passing both on local atest and the remote testing system. We would like to mark the test as flaky to avoid blocking in the test monitor dashboard and would like to remove the filter after checking another animation test related issue.
    
    Bug: 308501761
    Flag: N/A
    Test: N/A
    Change-Id: I3c946003c020e5568227146030da002865b405ac

--- packages/SystemUI/tests/src/com/android/systemui/accessibility/WindowMagnificationAnimationControllerTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/accessibility/WindowMagnificationAnimationControllerTest.java
@@ -43,6 +43,7 @@ import android.view.WindowManagerGlobal;
 import android.view.accessibility.IRemoteMagnificationAnimationCallback;
 import android.view.animation.AccelerateInterpolator;
 
+import androidx.test.filters.FlakyTest;
 import androidx.test.filters.LargeTest;
 
 import com.android.internal.graphics.SfVsyncFrameCallbackProvider;
@@ -67,6 +68,7 @@ import java.util.concurrent.atomic.AtomicReference;
 
 @LargeTest
 @RunWith(AndroidTestingRunner.class)
+@FlakyTest(bugId = 308501761)
 public class WindowMagnificationAnimationControllerTest extends SysuiTestCase {
 
     @Rule
