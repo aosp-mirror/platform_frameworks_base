f3cb555e758227f40fde4d8f541e55095e056863
harinirajan <harinirajan@google.com>
2024-01-29 07:19:43 +0000

    Multi credential flattened screen changes
    Bug: 310986916
    Test: Manual. See go/credential-selector-ui
    Change-Id: If91f1792dec5357075e11a9c5d866a661c5310dd

--- packages/CredentialManager/wear/src/com/android/credentialmanager/ui/screens/multiple/MultiCredentialsFlattenViewModel.kt
+++ packages/CredentialManager/wear/src/com/android/credentialmanager/ui/screens/multiple/MultiCredentialsFlattenViewModel.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0N
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,25 +14,26 @@
  * limitations under the License.
  */
 
-package com.android.credentialmanager.ui.screens.single.password
+package com.android.credentialmanager.ui.screens.multiple
 
 import android.content.Intent
-import android.credentials.selection.UserSelectionDialogResult
 import android.credentials.selection.ProviderPendingIntentResponse
-import androidx.annotation.MainThread
+import android.credentials.selection.UserSelectionDialogResult
 import androidx.lifecycle.ViewModel
+import com.android.credentialmanager.client.CredentialManagerClient
 import com.android.credentialmanager.ktx.getIntentSenderRequest
 import com.android.credentialmanager.model.Request
-import com.android.credentialmanager.client.CredentialManagerClient
+import com.android.credentialmanager.model.get.ActionEntryInfo
 import com.android.credentialmanager.model.get.CredentialEntryInfo
-import com.android.credentialmanager.ui.screens.single.UiState
+import com.android.credentialmanager.ui.screens.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
+/** ViewModel for [MultiCredentialsFlattenScreen].*/
 @HiltViewModel
-class SinglePasswordScreenViewModel @Inject constructor(
+class MultiCredentialsFlattenViewModel @Inject constructor(
     private val credentialManagerClient: CredentialManagerClient,
 ) : ViewModel() {
 
@@ -43,25 +44,18 @@ class SinglePasswordScreenViewModel @Inject constructor(
         MutableStateFlow<UiState>(UiState.CredentialScreen)
     val uiState: StateFlow<UiState> = _uiState
 
-    @MainThread
-    fun initialize(entryInfo: CredentialEntryInfo) {
+    fun onCredentialClicked(entryInfo: CredentialEntryInfo) {
         this.entryInfo = entryInfo
-    }
-
-    fun onDismissClick() {
-        _uiState.value = UiState.Cancel
-    }
-
-    fun onContinueClick() {
         _uiState.value = UiState.CredentialSelected(
             intentSenderRequest = entryInfo.getIntentSenderRequest()
         )
     }
 
-    fun onSignInOptionsClick() {
+    fun onCancelClicked() {
+        _uiState.value = UiState.Cancel
     }
 
-    fun onPasswordInfoRetrieved(
+    fun onInfoRetrieved(
         resultCode: Int? = null,
         resultData: Intent? = null,
     ) {
@@ -73,5 +67,9 @@ class SinglePasswordScreenViewModel @Inject constructor(
             if (resultCode != null) ProviderPendingIntentResponse(resultCode, resultData) else null
         )
         credentialManagerClient.sendResult(userSelectionDialogResult)
+    }
+
+    fun onActionEntryClicked(actionEntryInfo: ActionEntryInfo) {
+        // TODO(b/322797032)to be filled out
     }
 }
