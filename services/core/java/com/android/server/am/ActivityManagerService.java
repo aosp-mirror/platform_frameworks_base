8ca5a04051c28cda09ce886e85e42c88541e043b
Tetiana Meronyk <tetianameronyk@google.com>
2024-02-02 21:41:58 +0000

    Implement user start before alarms go off
    Before this change alarms in multiuser environment were not consistent. If the user was stopped, their alarm did not go off. Since user stops happen without user's explicit interaction and their status is not available to regular users, if the alarm was set on background user, there was no certainty that the alarm would ring.
    
    After the change a list of users with alarms scheduled is stored to start them in background shortly before their alarm. This ensures consistency in alarms going off even if the user gets stopped. Persistence of this list was added to save this list even after device reboots.
    
    Bug: 314907186
    Test: atest AlarmManagerServiceTest && atest UserWakeupStoreTest
    Change-Id: I5a75813d76f505383909ac6a281902c54784a1ed

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -18136,6 +18136,11 @@ public class ActivityManagerService extends IActivityManager.Stub
         }
 
         @Override
+        public boolean startUserInBackground(final int userId) {
+            return ActivityManagerService.this.startUserInBackground(userId);
+        }
+
+        @Override
         public void killForegroundAppsForUser(@UserIdInt int userId) {
             final ArrayList<ProcessRecord> procs = new ArrayList<>();
             synchronized (mProcLock) {
