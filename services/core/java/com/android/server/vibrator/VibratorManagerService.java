173774c92c5c0d8da15f7bad693e38cd990b7c72
Lais Andrade <lsandrade@google.com>
2024-02-29 18:35:20 +0000

    Fix vibration finished before HAL callback
    Update TurnOffVibratorStep so it will be considered part of the main
    vibration steps when it's waiting for the HAL completion callback.
    
    This keeps the ramp down steps outside the main vibration duration,
    but it makes sure the vibration will be completed after a callback is
    received, even if delayed compared to the expected duration.
    
    Also fix the VibratorManagerService command line to wait for the
    vibration ramp down and clean up before ending the command, to prevent
    vibrations from being cancelled because of binder death.
    
    Fix: 327608323
    Test: atest VibrationThreadTest
    Change-Id: I9cf0e1263f73b719de16782a85df87f740c9c5d0

--- services/core/java/com/android/server/vibrator/VibratorManagerService.java
+++ services/core/java/com/android/server/vibrator/VibratorManagerService.java
@@ -16,7 +16,6 @@
 
 package com.android.server.vibrator;
 
-import static android.os.ExternalVibrationScale.ScaleLevel.SCALE_MUTE;
 import static android.os.VibrationEffect.VibrationParameter.targetAmplitude;
 import static android.os.VibrationEffect.VibrationParameter.targetFrequency;
 
@@ -2146,6 +2145,7 @@ public class VibratorManagerService extends IVibratorManagerService.Stub {
     /** Provide limited functionality from {@link VibratorManagerService} as shell commands. */
     private final class VibratorManagerShellCommand extends ShellCommand {
         public static final String SHELL_PACKAGE_NAME = "com.android.shell";
+        public static final long VIBRATION_END_TIMEOUT_MS = 500; // Clean up shouldn't be too long.
 
         private final class CommonOptions {
             public boolean force = false;
@@ -2521,6 +2521,9 @@ public class VibratorManagerService extends IVibratorManagerService.Stub {
                     // Waits for the client vibration to finish, but the VibrationThread may still
                     // do cleanup after this.
                     vib.waitForEnd();
+                    // Wait for vibration clean up and possible ramp down before ending.
+                    mVibrationThread.waitForThreadIdle(
+                            mVibrationSettings.getRampDownDuration() + VIBRATION_END_TIMEOUT_MS);
                 } catch (InterruptedException e) {
                 }
             }
