9c14c9d94bbe8a7701cade7c2a5e2bb3b69f4102
Julia Tuttle <juliatuttle@google.com>
2024-02-29 15:14:30 -0500

    NSSL: Log actual orientation on updateSidePadding
    Bug: 326447757
    Test: NA
    Flag: NA
    Change-Id: I97375eacf9e566f68a440ed019a559693dd51450

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/stack/NotificationStackScrollLayout.java
@@ -1109,12 +1109,11 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
     }
 
     void updateSidePadding(int viewWidth) {
-        final boolean portrait =
-                getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+        final int orientation = getResources().getConfiguration().orientation;
 
         mLastUpdateSidePaddingDumpString = "viewWidth=" + viewWidth
                 + " skinnyNotifsInLandscape=" + mSkinnyNotifsInLandscape
-                + " portrait=" + portrait;
+                + " orientation=" + orientation;
 
         if (DEBUG_UPDATE_SIDE_PADDING) {
             Log.v(TAG, "updateSidePadding: " + mLastUpdateSidePaddingDumpString);
@@ -1126,7 +1125,7 @@ public class NotificationStackScrollLayout extends ViewGroup implements Dumpable
         }
 
         // Portrait is easy, just use the dimen for paddings
-        if (portrait) {
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
             mSidePaddings = mMinimumPaddings;
             return;
         }
