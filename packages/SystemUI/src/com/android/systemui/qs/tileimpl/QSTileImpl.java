4db275f0e5709bf94c13e6fee06cd90d83e1c988
Dave Mankoff <mankoff@google.com>
2024-03-05 20:49:58 +0000

    Remove Dependency#get call from ToggleSeekBar.
    RestrictedLockUtils#getShowAdminSupportDetailsIntent signature change
    such that it no longer requires a context.
    
    Flag: NA
    Bug: 138786270
    Test: built and flashed
    Change-Id: If4ab3030cfa1f9faed5d099077e5e3bab2cba622

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -587,7 +587,7 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                     name = "handleClick";
                     if (mState.disabledByPolicy) {
                         Intent intent = RestrictedLockUtils.getShowAdminSupportDetailsIntent(
-                                mContext, mEnforcedAdmin);
+                                mEnforcedAdmin);
                         mActivityStarter.postStartActivityDismissingKeyguard(intent, 0);
                     } else {
                         mQSLogger.logHandleClick(mTileSpec, msg.arg1);
