658799b7f7a34a58ab255981a95268b8ca02ab20
Emilian Peev <epeev@google.com>
2024-03-11 19:21:44 +0000

    Camera: Skip extension processing if session is not valid
    The preview request handler can run in a client supplied executor.
    The corresponding thread can continue to be active and process
    result callbacks after the camera device and session are no longer
    valid which can have effect on the pending images.
    To avoid accessing closed images, check whether the session is
    still initialized.
    
    Bug: 311634576
    Test: atest -c -d cts/tests/camera/src/android/hardware/camera2/cts/CameraExtensionSessionTest.java
    Change-Id: Ic6079f4eddc07a58b82039c59b46c1a1166a2c12

--- core/java/android/hardware/camera2/impl/CameraExtensionSessionImpl.java
+++ core/java/android/hardware/camera2/impl/CameraExtensionSessionImpl.java
@@ -1731,7 +1731,8 @@ public final class CameraExtensionSessionImpl extends CameraExtensionSession {
                                 mCallbacks, result.getSequenceId());
                     }
                     if ((!mSingleCapture) && (mPreviewProcessorType ==
-                            IPreviewExtenderImpl.PROCESSOR_TYPE_REQUEST_UPDATE_ONLY)) {
+                            IPreviewExtenderImpl.PROCESSOR_TYPE_REQUEST_UPDATE_ONLY)
+                            && mInitialized) {
                         CaptureStageImpl captureStage = null;
                         try {
                             captureStage = mPreviewRequestUpdateProcessor.process(
@@ -1754,8 +1755,8 @@ public final class CameraExtensionSessionImpl extends CameraExtensionSession {
                         } else {
                             mRequestUpdatedNeeded = false;
                         }
-                    } else if (mPreviewProcessorType ==
-                            IPreviewExtenderImpl.PROCESSOR_TYPE_IMAGE_PROCESSOR) {
+                    } else if ((mPreviewProcessorType ==
+                            IPreviewExtenderImpl.PROCESSOR_TYPE_IMAGE_PROCESSOR) && mInitialized) {
                         int idx = mPendingResultMap.indexOfKey(timestamp);
 
                         if ((idx >= 0) && (mPendingResultMap.get(timestamp).first == null)) {
@@ -1802,7 +1803,7 @@ public final class CameraExtensionSessionImpl extends CameraExtensionSession {
                     } else {
                         // No special handling for PROCESSOR_TYPE_NONE
                     }
-                    if (notifyClient) {
+                    if (notifyClient && mInitialized) {
                         final long ident = Binder.clearCallingIdentity();
                         try {
                             if (processStatus) {
