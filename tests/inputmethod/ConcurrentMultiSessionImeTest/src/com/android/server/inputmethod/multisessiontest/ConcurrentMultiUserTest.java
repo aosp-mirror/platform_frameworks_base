fba1cf2dc25b3f244b30f12d1e95cabcb9019602
Antonio Kantek <kanant@google.com>
2024-03-14 17:59:01 +0000

    Add bedstead dependency
    Bug: 327704045
    Test: atest ConcurrentMultiUserTest
    Change-Id: I5382cb3d19b086b034556de9d0386695bda26b37

--- tests/inputmethod/ConcurrentMultiSessionImeTest/src/com/android/server/inputmethod/multisessiontest/ConcurrentMultiUserTest.java
+++ tests/inputmethod/ConcurrentMultiSessionImeTest/src/com/android/server/inputmethod/multisessiontest/ConcurrentMultiUserTest.java
@@ -23,13 +23,19 @@ import android.content.pm.PackageManager;
 
 import androidx.test.platform.app.InstrumentationRegistry;
 
+import com.android.bedstead.harrier.BedsteadJUnit4;
+import com.android.bedstead.harrier.DeviceState;
+
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 
-@RunWith(JUnit4.class)
+@RunWith(BedsteadJUnit4.class)
 public final class ConcurrentMultiUserTest {
+
+    @Rule
+    public static final DeviceState sDeviceState = new DeviceState();
 
     @Before
     public void doBeforeEachTest() {
