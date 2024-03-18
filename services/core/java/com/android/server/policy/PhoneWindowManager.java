8e7ef1726f27e0fd54af8293f76094ce1b681bc4
Josep del Rio <joseprio@google.com>
2024-03-18 20:22:04 +0000

    Remove Meta+T shortcut
    At the moment there is no way to interact with a open taskbar from
    shortcut, so it has been decided to remove this shortcut for now.
    
    Bug: 311676243
    Flag: NA
    Test: Flashed on device
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:0310424e42e4ff9ce1bd08a84c53ab40f7c13b54)
    Merged-In: I9c30335c28c96a2fe47cb5a4c355e4a68c44cee6
    Change-Id: I9c30335c28c96a2fe47cb5a4c355e4a68c44cee6

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -3486,13 +3486,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     return true;
                 }
                 break;
-            case KeyEvent.KEYCODE_T:
-                if (firstDown && event.isMetaPressed()) {
-                    toggleTaskbar();
-                    logKeyboardSystemsEvent(event, KeyboardLogEvent.TOGGLE_TASKBAR);
-                    return true;
-                }
-                break;
             case KeyEvent.KEYCODE_DEL:
             case KeyEvent.KEYCODE_ESCAPE:
                 if (firstDown && event.isMetaPressed()) {
