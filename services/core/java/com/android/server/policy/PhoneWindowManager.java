34ad507bfebd5a65bde5858fe760c58bd1a665eb
Josep del Rio <joseprio@google.com>
2024-03-18 20:49:25 +0000

    Hide shortcut helper on all apps
    At the moment, triggering all apps won't close the shortcut
    helper. This CL addresses it.
    
    Bug: 324420929
    Flag: NA
    Test: Flashed on device
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:2f0ffaa6f13e96c84eba99b527df59f1e4f0853a)
    Merged-In: Ib979cf45b68aad8bac4a73582e794351b15f4721
    Change-Id: Ib979cf45b68aad8bac4a73582e794351b15f4721
    24D1-dev is based on 24Q2-release. Therefore, we merged this CL to 24D1-dev.

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1922,6 +1922,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             accessibilityManager.performSystemAction(
                     AccessibilityService.GLOBAL_ACTION_ACCESSIBILITY_ALL_APPS);
         }
+        dismissKeyboardShortcutsMenu();
     }
 
     private void toggleNotificationPanel() {
