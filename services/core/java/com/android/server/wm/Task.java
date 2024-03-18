c244307e28b9abced762e43cd5fcd9bc0a612021
Louis Chang <louischang@google.com>
2024-03-18 10:30:11 +0000

    Fixes ActivityLifecycleTests flakiness
    The Launcher Task has a empty child TaskFragment, which makes the
    Launcher delayed to be paused and sometimes making lifecycle test
    failed.
    
    The direct child resumed activity should be paused immediately if
    it shouldn't be stayed in the RESUMED state.
    
    Bug: 329953510
    Test: ActivityLifecycleTests
    Test: TaskTests
    
    Change-Id: I1a3331fd17f683896e39cc10b6e0f27fe76def0a

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -1274,7 +1274,8 @@ class Task extends TaskFragment {
         if (!isLeafTaskFragment()) {
             final ActivityRecord top = topRunningActivity();
             final ActivityRecord resumedActivity = getResumedActivity();
-            if (resumedActivity != null && top.getTaskFragment() != this) {
+            if (resumedActivity != null
+                    && (top.getTaskFragment() != this || !canBeResumed(resuming))) {
                 // Pausing the resumed activity because it is occluded by other task fragment.
                 if (startPausing(false /* uiSleeping*/, resuming, reason)) {
                     someActivityPaused[0]++;
