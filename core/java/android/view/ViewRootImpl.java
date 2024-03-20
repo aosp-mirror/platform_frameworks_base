a2c4477a146ca63d3087369e15cf14427d8deac8
Shane <shanewong@google.com>
2024-03-20 21:59:13 +0000

    Disable toolkit dVRR feature
    Disable toolkid dVRR feature
    
    Bug: 330596920
    Test: atest ViewRootImplTest / ViewTest / PhoneWindowTest / WindowTest /
    HorizontalScrollViewFunctionalTest / ScrollViewFunctionalTest
    
    Change-Id: I77b3e8320501f68c500f5010013f7a5c479d5715

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -12715,7 +12715,10 @@ public final class ViewRootImpl implements ViewParent,
     }
 
     private boolean shouldEnableDvrr() {
-        return sToolkitSetFrameRateReadOnlyFlagValue && mIsFrameRatePowerSavingsBalanced;
+        // uncomment this when we are ready for enabling dVRR
+        // return sToolkitSetFrameRateReadOnlyFlagValue && mIsFrameRatePowerSavingsBalanced;
+        return false;
+
     }
 
     private void checkIdleness() {
