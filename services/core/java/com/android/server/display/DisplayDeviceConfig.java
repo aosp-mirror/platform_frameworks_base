a6c764c76a44cd40dd48695512032b807092e19a
Chris Kuiper <ckuiper@google.com>
2024-03-19 17:00:23 -0700

    DisplayConfig: Add flag-protected option to select Fusion Proximity sensor.
    This adds to capability to add a second Proximity Sensor
    configuration that is selected based on an aconfig flag.
    
    In detail:
    
    1) Adds a new flag "useFusionProxSensor" that controls whether or not
       to use a Fusion Proximity sensor if configured.
    
    2) Adds a new attribute "featureFlag" to the sensorDetails XML
       element that marks the element to be used if a corresponding
       aconfig flag is set.
    
    3) Allows up to two "proxSensor" elements to be given instead of one.
       This allows to provide two different Proximity sensor
       configurations, one to be used if both the useFusionProxSensor
       flag and featureFlag attribute are set and matching, and another
       if not.
    
    Bug: 306203895
    Test: Pushing different XML config files, confirming via dumpsys and
          manual testing.
    
    Change-Id: Icb7a1c93d38385b48ba27d9747cae9cd36c4dc03

--- services/core/java/com/android/server/display/DisplayDeviceConfig.java
+++ services/core/java/com/android/server/display/DisplayDeviceConfig.java
@@ -2140,7 +2140,7 @@ public class DisplayDeviceConfig {
                 mAmbientLightSensor = SensorData.loadAmbientLightSensorConfig(config,
                         mContext.getResources());
                 mScreenOffBrightnessSensor = SensorData.loadScreenOffBrightnessSensorConfig(config);
-                mProximitySensor = SensorData.loadProxSensorConfig(config);
+                mProximitySensor = SensorData.loadProxSensorConfig(mFlags, config);
                 mTempSensor = SensorData.loadTempSensorConfig(mFlags, config);
                 loadAmbientHorizonFromDdc(config);
                 loadBrightnessChangeThresholds(config);
