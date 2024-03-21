fcc24eca9a94b9f1264457b041d8a04e247b41b3
Chaohui Wang <chaohuiw@google.com>
2024-03-21 14:02:36 +0800

    [Spa] Fix LoadingBarPage
    Also upgrade the libraries.
    
    Bug: 318810660
    Test: manual - with gallery
    Change-Id: I6a458ca98fc8b4fdb0b63b858dba4976a6330314

--- packages/SettingsLib/Spa/build.gradle.kts
+++ packages/SettingsLib/Spa/build.gradle.kts
@@ -29,7 +29,7 @@ val androidTop: String = File(rootDir, "../../../../..").canonicalPath
 
 allprojects {
     extra["androidTop"] = androidTop
-    extra["jetpackComposeVersion"] = "1.7.0-alpha04"
+    extra["jetpackComposeVersion"] = "1.7.0-alpha05"
 }
 
 subprojects {
