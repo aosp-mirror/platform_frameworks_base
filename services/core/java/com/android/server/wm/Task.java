13eeb8e1cba0db3f286aa20d4d9222e399a1625a
Riddle Hsu <riddlehsu@google.com>
2024-03-21 15:53:49 +0800

    Allow to collect moving back task in open transition
    Simulation:
     Task A starts(NEW_TASK) Task B, finishes
     Task B starts(NEW_TASK) Task C
       Task B calls moveTaskToBack in onPause
     Task C (declare as translucent and sleep 200ms in
             onCreate to delay transition)
       starts(NEW_TASK) Task B -> attempt to move task B to front
       finishes
    
    Because the initial open transition may be done until the last
    step, then moveTaskToBack for B will run after the last step.
    Then expect B should be on top but the result is on home screen
    because A and C are finished, and B is moved to back.
    
    This change is kinda a workaround to mitigate the case of
    deferred move-to-back. Because it is regular to have closing
    targets in an open transition, the moving task can run with
    the collecting transition directly.
    
    Fix: 325645759
    Test: atest TransitionTests#testDeferredMoveTaskToBack
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:7079ba9aad0012ae53cb689dc23740b99ed27d15)
    Merged-In: I46d8e26110ebefea74e5dd06c85c003b12693416
    Change-Id: I46d8e26110ebefea74e5dd06c85c003b12693416

--- services/core/java/com/android/server/wm/Task.java
+++ services/core/java/com/android/server/wm/Task.java
@@ -5770,7 +5770,7 @@ class Task extends TaskFragment {
         // If we have a watcher, preflight the move before committing to it.  First check
         // for *other* available tasks, but if none are available, then try again allowing the
         // current task to be selected.
-        if (isTopRootTaskInDisplayArea() && mAtmService.mController != null) {
+        if (mAtmService.mController != null && isTopRootTaskInDisplayArea()) {
             ActivityRecord next = topRunningActivity(null, task.mTaskId);
             if (next == null) {
                 next = topRunningActivity(null, INVALID_TASK_ID);
@@ -5814,6 +5814,15 @@ class Task extends TaskFragment {
                 + tr.mTaskId);
 
         if (mTransitionController.isShellTransitionsEnabled()) {
+            // TODO(b/277838915): Consider to make it concurrent to eliminate the special case.
+            final Transition collecting = mTransitionController.getCollectingTransition();
+            if (collecting != null && collecting.mType == TRANSIT_OPEN) {
+                // It can be a CLOSING participate of an OPEN transition. This avoids the deferred
+                // transition from moving task to back after the task was moved to front.
+                collecting.collect(tr);
+                moveTaskToBackInner(tr, collecting);
+                return true;
+            }
             final Transition transition = new Transition(TRANSIT_TO_BACK, 0 /* flags */,
                     mTransitionController, mWmService.mSyncEngine);
             // Guarantee that this gets its own transition by queueing on SyncEngine
@@ -5842,7 +5851,7 @@ class Task extends TaskFragment {
         return true;
     }
 
-    private boolean moveTaskToBackInner(@NonNull Task task, @Nullable Transition transition) {
+    private void moveTaskToBackInner(@NonNull Task task, @Nullable Transition transition) {
         final Transition.ReadyCondition movedToBack =
                 new Transition.ReadyCondition("moved-to-back", task);
         if (transition != null) {
@@ -5857,7 +5866,7 @@ class Task extends TaskFragment {
 
             if (inPinnedWindowingMode()) {
                 mTaskSupervisor.removeRootTask(this);
-                return true;
+                return;
             }
 
             mRootWindowContainer.ensureVisibilityAndConfig(null /* starting */,
@@ -5880,7 +5889,6 @@ class Task extends TaskFragment {
         } else {
             mRootWindowContainer.resumeFocusedTasksTopActivities();
         }
-        return true;
     }
 
     boolean willActivityBeVisible(IBinder token) {
