21c1459a07788b7772cf717e15f56560bc036eb6
Alex Dadukin <adadukin@google.com>
2024-03-28 15:46:00 +0000

    Add transfer reason meta data to default session
    Bug: b/330479175
    Test: tested manually
    Change-Id: Ib568cb0cfef4a19ef808790829abc04001f76b99

--- services/core/java/com/android/server/media/SystemMediaRoute2Provider.java
+++ services/core/java/com/android/server/media/SystemMediaRoute2Provider.java
@@ -474,6 +474,10 @@ class SystemMediaRoute2Provider extends MediaRoute2Provider {
                                 .setProviderId(mUniqueId)
                                 .setSystemSession(true)
                                 .addSelectedRoute(MediaRoute2Info.ROUTE_ID_DEFAULT)
+                                .setTransferReason(newSessionInfo.getTransferReason())
+                                .setTransferInitiator(
+                                        newSessionInfo.getTransferInitiatorUserHandle(),
+                                        newSessionInfo.getTransferInitiatorPackageName())
                                 .build();
                 return true;
             }
