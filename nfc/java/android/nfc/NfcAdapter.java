897900ae1eb731ce918a0ebe01943c0f5c9d6da3
Brad Lassey <lassey@google.com>
2024-04-10 17:35:50 -0400

    Use PollingFrame class instead of Bundles to represent polling frames in NFC stack
    Bug: 333768122
    Test: Tested by CTS
    Merged-In: I86ebecd530126d477cae1e7066a4908ccb02744e
    Change-Id: I86ebecd530126d477cae1e7066a4908ccb02744e

--- nfc/java/android/nfc/NfcAdapter.java
+++ nfc/java/android/nfc/NfcAdapter.java
@@ -2800,12 +2800,11 @@ public final class NfcAdapter {
     @TestApi
     @FlaggedApi(Flags.FLAG_NFC_READ_POLLING_LOOP)
     public void notifyPollingLoop(@NonNull PollingFrame pollingFrame) {
-        Bundle frame = pollingFrame.toBundle();
         try {
             if (sService == null) {
                 attemptDeadServiceRecovery(null);
             }
-            sService.notifyPollingLoop(frame);
+            sService.notifyPollingLoop(pollingFrame);
         } catch (RemoteException e) {
             attemptDeadServiceRecovery(e);
             // Try one more time
@@ -2814,7 +2813,7 @@ public final class NfcAdapter {
                 return;
             }
             try {
-                sService.notifyPollingLoop(frame);
+                sService.notifyPollingLoop(pollingFrame);
             } catch (RemoteException ee) {
                 Log.e(TAG, "Failed to recover NFC Service.");
             }
