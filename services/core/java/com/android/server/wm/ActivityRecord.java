992031103ca412b3f6fd743fb3009bfccea8bb37
tanxiaoyan <tanxiaoyan@xiaomi.com>
2024-04-19 19:21:16 +0800

    Should set visibleRequest false when destroy activity
    When destroy activity from Task#removeActivities or Task#remove, it may directly call destroyIfPossible rather than finishIfPossible which may not set visibleRequest false.
    However the activity may be collect in a transition with visibleRequest true, and all of this cause sync timeout.
    
    Bug: 330823160
    
    Change-Id: I35ed0a807e54004e6228f720803d146a7f1a2acc
    Signed-off-by: tanxiaoyan <tanxiaoyan@xiaomi.com>

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -3903,6 +3903,7 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         boolean removedFromHistory = false;
 
         cleanUp(false /* cleanServices */, false /* setState */);
+        setVisibleRequested(false);
 
         if (hasProcess()) {
             app.removeActivity(this, true /* keepAssociation */);
