520fabe3860a6958f4ebac07ad8d757aab017b83
Amith Yamasani <yamasani@google.com>
2024-04-20 14:59:06 -0700

    Improvements to app restriction reason reporting
    Improve documentation with examples
    Minor cleanup
    Avoid extra logging for unhibernate
    
    Bug: 333882527
    Test: statsd_testdrive 866
    Change-Id: Ieafb709b8e904f33f23a62f249ebaf9f1ec4caba

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -20256,7 +20256,7 @@ public class ActivityManagerService extends IActivityManager.Stub
         final int userId = UserHandle.getCallingUserId();
         final long callingId = Binder.clearCallingIdentity();
         try {
-            if (uid == -1) {
+            if (uid == INVALID_UID) {
                 uid = mPackageManagerInt.getPackageUid(packageName, 0, userId);
             }
             mAppRestrictionController.noteAppRestrictionEnabled(packageName, uid, restrictionType,
