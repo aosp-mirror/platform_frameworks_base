fa74cd7671ee7286f66f6434a6bfa7f72e43cee1
Fabian Kozynski <kozynski@google.com>
2024-04-24 16:21:07 +0000

    DO NOT MERGE ANYWHERE Revert "Add the Finder shutdown message to AOSP"
    Revert submission 26334207-finder_shutdown
    
    Reason for revert: Revert in 24d1
    
    Reverted changes: /q/submissionid:26334207-finder_shutdown
    
    Change-Id: I20b3d485a00109060a6ed206503aa3c12c812969

--- packages/SystemUI/tests/src/com/android/systemui/globalactions/ShutdownUiTest.java
+++ packages/SystemUI/tests/src/com/android/systemui/globalactions/ShutdownUiTest.java
@@ -19,13 +19,7 @@ package com.android.systemui.globalactions;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
 
-import static org.mockito.Mockito.when;
-
-import android.nearby.NearbyManager;
-import android.net.platform.flags.Flags;
 import android.os.PowerManager;
-import android.platform.test.annotations.DisableFlags;
-import android.platform.test.annotations.EnableFlags;
 import android.testing.AndroidTestingRunner;
 
 import androidx.test.filters.SmallTest;
@@ -38,7 +32,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 
 
 @SmallTest
@@ -48,13 +41,10 @@ public class ShutdownUiTest extends SysuiTestCase {
     ShutdownUi mShutdownUi;
     @Mock
     BlurUtils mBlurUtils;
-    @Mock
-    NearbyManager mNearbyManager;
 
     @Before
     public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        mShutdownUi = new ShutdownUi(getContext(), mBlurUtils, mNearbyManager);
+        mShutdownUi = new ShutdownUi(getContext(), mBlurUtils);
     }
 
     @Test
@@ -92,53 +82,4 @@ public class ShutdownUiTest extends SysuiTestCase {
         String message = mShutdownUi.getReasonMessage("anything-else");
         assertNull(message);
     }
-
-    @EnableFlags(Flags.FLAG_POWERED_OFF_FINDING_PLATFORM)
-    @Test
-    public void getDialog_whenPowerOffFindingModeEnabled_returnsFinderDialog() {
-        when(mNearbyManager.getPoweredOffFindingMode()).thenReturn(
-                NearbyManager.POWERED_OFF_FINDING_MODE_ENABLED);
-
-        int actualLayout = mShutdownUi.getShutdownDialogContent(false);
-
-        int expectedLayout = com.android.systemui.res.R.layout.shutdown_dialog_finder_active;
-        assertEquals(actualLayout, expectedLayout);
-    }
-
-    @DisableFlags(Flags.FLAG_POWERED_OFF_FINDING_PLATFORM)
-    @Test
-    public void getDialog_whenPowerOffFindingModeEnabledFlagDisabled_returnsFinderDialog() {
-        when(mNearbyManager.getPoweredOffFindingMode()).thenReturn(
-                NearbyManager.POWERED_OFF_FINDING_MODE_ENABLED);
-
-        int actualLayout = mShutdownUi.getShutdownDialogContent(false);
-
-        int expectedLayout = R.layout.shutdown_dialog;
-        assertEquals(actualLayout, expectedLayout);
-    }
-
-    @EnableFlags(Flags.FLAG_POWERED_OFF_FINDING_PLATFORM)
-    @Test
-    public void getDialog_whenPowerOffFindingModeDisabled_returnsDefaultDialog() {
-        when(mNearbyManager.getPoweredOffFindingMode()).thenReturn(
-                NearbyManager.POWERED_OFF_FINDING_MODE_DISABLED);
-
-        int actualLayout = mShutdownUi.getShutdownDialogContent(false);
-
-        int expectedLayout = R.layout.shutdown_dialog;
-        assertEquals(actualLayout, expectedLayout);
-    }
-
-    @EnableFlags(Flags.FLAG_POWERED_OFF_FINDING_PLATFORM)
-    @Test
-    public void getDialog_whenPowerOffFindingModeEnabledAndIsReboot_returnsDefaultDialog() {
-        when(mNearbyManager.getPoweredOffFindingMode()).thenReturn(
-                NearbyManager.POWERED_OFF_FINDING_MODE_ENABLED);
-
-        int actualLayout = mShutdownUi.getShutdownDialogContent(true);
-
-        int expectedLayout = R.layout.shutdown_dialog;
-        assertEquals(actualLayout, expectedLayout);
-    }
-
 }
