065ddcc7592b7ffd4b9e9986d69b72c8d942c9ff
Nick Chameyev <nickchameyev@google.com>
2024-04-29 14:34:25 +0000

    Make haptics during unfold animation configurable
    Adds a resource property to control if we should
    play haptics effect during unfold animation.
    
    Bug: 336388573
    Test: atest UnfoldHapticsPlayerTest
    Test: manual test that haptics could be disabled
    Merged-In: I379472097f46cab8eac82febe6eace2d45791c9c
    Change-Id: I379472097f46cab8eac82febe6eace2d45791c9c

--- packages/SystemUI/tests/utils/src/android/os/VibratorKosmos.kt
+++ packages/SystemUI/tests/utils/src/android/os/VibratorKosmos.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,11 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.systemui.unfold.config
 
-interface UnfoldTransitionConfig {
-    val isEnabled: Boolean
-    val isHingeAngleEnabled: Boolean
-    val isHapticsEnabled: Boolean
-    val halfFoldedTimeoutMillis: Int
-}
+package android.os
+
+import com.android.systemui.kosmos.Kosmos
+import com.android.systemui.util.mockito.mock
+
+var Kosmos.vibrator by Kosmos.Fixture { mock<Vibrator>() }
