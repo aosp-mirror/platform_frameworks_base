da979e9a769397b3b0ccf8516d39592585dc7e81
Mayank Garg <gargmayank@google.com>
2024-05-05 20:24:32 -0700

    Rename the newly added maps variable to be more descriptive
    Bug: 338665345
    Test: atest InputTests
    Change-Id: I3760407b2e9a67aed30ba92b20307c08a1c074d4

--- services/core/jni/com_android_server_input_InputManagerService.cpp
+++ services/core/jni/com_android_server_input_InputManagerService.cpp
@@ -602,7 +602,7 @@ void NativeInputManager::getReaderConfiguration(InputReaderConfiguration* outCon
     // Original data: [{'inputPort1': '1'}, {'inputPort2': '2'}]
     // Received data: ['inputPort1', '1', 'inputPort2', '2']
     // So we unpack accordingly here.
-    outConfig->portAssociations.clear();
+    outConfig->inputPortToDisplayPortAssociations.clear();
     jobjectArray portAssociations = jobjectArray(env->CallObjectMethod(mServiceObj,
             gServiceClassInfo.getInputPortAssociations));
     if (!checkAndClearExceptionFromCallback(env, "getInputPortAssociations") && portAssociations) {
@@ -619,16 +619,16 @@ void NativeInputManager::getReaderConfiguration(InputReaderConfiguration* outCon
                     displayPortStr.c_str());
                 continue;
             }
-            outConfig->portAssociations.insert({inputPort, displayPort});
+            outConfig->inputPortToDisplayPortAssociations.insert({inputPort, displayPort});
         }
         env->DeleteLocalRef(portAssociations);
     }
 
-    outConfig->uniqueIdAssociationsByPort = readMapFromInterleavedJavaArray<
+    outConfig->inputPortToDisplayUniqueIdAssociations = readMapFromInterleavedJavaArray<
             std::string>(gServiceClassInfo.getInputUniqueIdAssociationsByPort,
                          "getInputUniqueIdAssociationsByPort");
 
-    outConfig->uniqueIdAssociationsByDescriptor = readMapFromInterleavedJavaArray<
+    outConfig->inputDeviceDescriptorToDisplayUniqueIdAssociations = readMapFromInterleavedJavaArray<
             std::string>(gServiceClassInfo.getInputUniqueIdAssociationsByDescriptor,
                          "getInputUniqueIdAssociationsByDescriptor");
 
