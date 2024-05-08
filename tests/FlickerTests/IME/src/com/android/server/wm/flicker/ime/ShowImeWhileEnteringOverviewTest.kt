40c1df0fc2003c62366acd8688a8558be65f93b9
Chris Li <lihongyu@google.com>
2024-05-08 08:40:49 +0000

    Update flicker app targetSDK
    So that AE is enabled on phone target as well
    
    Update the following:
    1. PendingIntent requires FLAG_MUTABLE
    2. Bubble/Notification requires POST_NOTIFICATIONS
    3. registerReceiver requires RECEIVER_EXPORTED
    4. Opt-out force edge-to-edge
    
    Bug: 306666082
    Test: atest FlickerTestsOther
    Change-Id: Ib1c310795ee3723656c9ebd0557831d189eddb6c

--- tests/FlickerTests/IME/src/com/android/server/wm/flicker/ime/ShowImeWhileEnteringOverviewTest.kt
+++ tests/FlickerTests/IME/src/com/android/server/wm/flicker/ime/ShowImeWhileEnteringOverviewTest.kt
@@ -37,8 +37,8 @@ import org.junit.runners.MethodSorters
 import org.junit.runners.Parameterized
 
 /**
- * Test IME window layer will be associated with the app task when going to the overview screen. To
- * run this test: `atest FlickerTests:OpenImeWindowToOverViewTest`
+ * Test IME window layer will be associated with the app task when going to the overview screen.
+ * To run this test: `atest FlickerTestsIme2:ShowImeWhileEnteringOverviewTest`
  */
 @RunWith(Parameterized::class)
 @Parameterized.UseParametersRunnerFactory(FlickerParametersRunnerFactory::class)
