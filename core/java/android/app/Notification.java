786f1c2ef0fe054cefab90eb3557c86646687288
Ibrahim Yilmaz <iyz@google.com>
2024-05-08 21:45:46 +0000

    [Minimal HUN] strip subText in  Minimal HUN
    subText(aka summaryText) can be available in Notification. We dont want to have this text in minimal HUN.
    This CL strips this text from minimal hun content:
    
    Bug: 335236810
    Test: SystemUITests and manual
    Flag: ACONFIG android.app.Flags.compact_heads_up_notification DISABLED
    Change-Id: I9d00bbc1742c1ff080289f849910d736d47b07f6

--- core/java/android/app/Notification.java
+++ core/java/android/app/Notification.java
@@ -6611,7 +6611,7 @@ public class Notification implements Parcelable
             // Notification text is shown as secondary header text
             // for the minimal hun when it is provided.
             // Time(when and chronometer) is not shown for the minimal hun.
-            p.headerTextSecondary(p.mText).text(null).hideTime(true);
+            p.headerTextSecondary(p.mText).text(null).hideTime(true).summaryText("");
 
             return applyStandardTemplate(
                     getCompactHeadsUpBaseLayoutResource(), p,
