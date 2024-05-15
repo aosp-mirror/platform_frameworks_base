c21feaa18e5445994081878b68d0056a21f20699
Robin Lee <rgl@google.com>
2024-05-15 17:34:48 +0200

    Add keyguard-appearing transition handler
    This lets the SysUI implementation combine the keyguard appearing and
    occluding events into a single transition after it has called
    setLockScreenShown, so that it has all the information needed to play
    the right animation in one step.
    
    At the moment we won't play any animation and always just jump cut.
    
    Flag: com.android.window.flags:keyguard_appear_transition
    Bug: 337917376
    Bug: 338606332
    Bug: 338008315
    Test: KeyguardManagerTest
    Test: Crystalball run b/338008315#comment7
    Change-Id: Id8e8518614386f8a3d92d4587701a96deb77fcef

--- packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
+++ packages/SystemUI/src/com/android/systemui/keyguard/KeyguardViewMediator.java
@@ -1077,6 +1077,33 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
         }
     };
 
+    /**
+     * For now, the keyguard-appearing animation is a no-op, because we assume that this is
+     * happening while the screen is already off or turning off.
+     *
+     * TODO(b/278086361): create an animation for keyguard appearing over a non-showWhenLocked
+     * activity.
+     */
+    private final IRemoteAnimationRunner.Stub mAppearAnimationRunner =
+            new IRemoteAnimationRunner.Stub() {
+        @Override
+        public void onAnimationStart(@WindowManager.TransitionOldType int transit,
+                RemoteAnimationTarget[] apps,
+                RemoteAnimationTarget[] wallpapers,
+                RemoteAnimationTarget[] nonApps,
+                IRemoteAnimationFinishedCallback finishedCallback) {
+            try {
+                finishedCallback.onAnimationFinished();
+            } catch (RemoteException e) {
+                Log.e(TAG, "Failed to finish transition", e);
+            }
+        }
+
+        @Override
+        public void onAnimationCancelled() {
+        }
+    };
+
     private final IRemoteAnimationRunner mOccludeAnimationRunner =
             new OccludeActivityLaunchRemoteAnimationRunner(mOccludeAnimationController);
 
@@ -1165,7 +1192,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
                                     finishedCallback.onAnimationFinished();
                                     mOccludeByDreamAnimator = null;
                                 } catch (RemoteException e) {
-                                    e.printStackTrace();
+                                    Log.e(TAG, "Failed to finish transition", e);
                                 }
                             }
                         });
@@ -1280,7 +1307,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
                                     mInteractionJankMonitor.end(CUJ_LOCKSCREEN_OCCLUSION);
                                 } catch (RemoteException e) {
-                                    e.printStackTrace();
+                                    Log.e(TAG, "Failed to finish transition", e);
                                 }
                             }
                         });
@@ -1546,6 +1573,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
         mKeyguardTransitions.register(
                 KeyguardService.wrap(this, getExitAnimationRunner()),
+                KeyguardService.wrap(this, getAppearAnimationRunner()),
                 KeyguardService.wrap(this, getOccludeAnimationRunner()),
                 KeyguardService.wrap(this, getOccludeByDreamAnimationRunner()),
                 KeyguardService.wrap(this, getUnoccludeAnimationRunner()));
@@ -2122,6 +2150,10 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
 
     public IRemoteAnimationRunner getExitAnimationRunner() {
         return validatingRemoteAnimationRunner(mExitAnimationRunner);
+    }
+
+    public IRemoteAnimationRunner getAppearAnimationRunner() {
+        return validatingRemoteAnimationRunner(mAppearAnimationRunner);
     }
 
     public IRemoteAnimationRunner getOccludeAnimationRunner() {
@@ -3357,7 +3389,7 @@ public class KeyguardViewMediator implements CoreStartable, Dumpable,
             }
         } catch (RemoteException e) {
             mSurfaceBehindRemoteAnimationRequested = false;
-            e.printStackTrace();
+            Log.e(TAG, "Failed to report keyguardGoingAway", e);
         }
     }
 
