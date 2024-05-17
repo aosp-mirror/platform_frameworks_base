275e2758aa0b6739f0e92972fd66a3627cf75685
Riddle Hsu <riddlehsu@google.com>
2024-05-17 17:02:09 +0800

    Narrow down the condition of wallpaper target for occluded keyguard
    Previously, keyguard will be selected as the backup wallpaper target
    if there keyguard is locked and there is an active transition. That
    is to avoid wallpaper from being show/hide in a short time when
    running recents transition to close app on keyguard.
    
    The condition was a bit broad so the regular transition will also
    keep wallpaper target on keyguard. Which could make the transition
    change mode of wallpaper become CHANGE rather than TO_BACK when
    launching an occlude-keyguard app and then display rotates. Which
    could make the condition of seamless rotation rejects the seamless
    effect because it found wallpaper is a changing operation.
    
    To avoid such case, only enable the condition if there is a transient
    (recents) transition.
    
    Bug: 333557024
    Test: Enable auto rotation. Set a secured lock. Turn off screen.
          Double click power key to launch camera when holding the
          device in landscape. There should not have a rotation animation.
    Test: On a secured keyguard. Double click power key to launch camera.
          Swipe from bottom to dismiss camera. The wallpaper target
          should only change to notification shade once.
    Test: atest WallpaperControllerTests#testShowWhenLockedWallpaperTarge
    
    Change-Id: I43a29e828798f36f39d38af130f98de7c5859ec6

--- services/tests/wmtests/src/com/android/server/wm/WallpaperControllerTests.java
+++ services/tests/wmtests/src/com/android/server/wm/WallpaperControllerTests.java
@@ -314,6 +314,18 @@ public class WallpaperControllerTests extends WindowTestsBase {
         // Wallpaper is invisible because the lowest show-when-locked activity is opaque.
         assertNull(wallpaperController.getWallpaperTarget());
 
+        // Only transient-launch transition will make notification shade as last resort target.
+        // This verifies that regular transition won't choose invisible keyguard as the target.
+        final WindowState keyguard = createWindow(null /* parent */,
+                WindowManager.LayoutParams.TYPE_NOTIFICATION_SHADE, "keyguard");
+        keyguard.mAttrs.flags |= FLAG_SHOW_WALLPAPER;
+        registerTestTransitionPlayer();
+        final Transition transition = wallpaperWindow.mTransitionController.createTransition(
+                WindowManager.TRANSIT_CHANGE);
+        transition.collect(keyguard);
+        wallpaperController.adjustWallpaperWindows();
+        assertNull(wallpaperController.getWallpaperTarget());
+
         // A show-when-locked wallpaper is used for lockscreen. So the top wallpaper should
         // be the one that is not show-when-locked.
         final WindowState wallpaperWindow2 = createWallpaperWindow(mDisplayContent);
