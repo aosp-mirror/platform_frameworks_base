d1d3f8e203200cfc61d89975887eb6c1f83fc4ec
Beverly Tai <beverlyt@google.com>
2024-05-20 19:03:09 +0000

    Revert "Always emit latest propertiesInitialized value onStart"
    This reverts commit ced63861b833d2c771e5cd18abba3955c6ec9807.
    
    Reason for revert: Testing if this is the cause of triggering
    b/335759591 even more
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:b4766153db5681ffe6d549f4da7816f36e555fe5)
    Merged-In: I53d30dd0e926c96d9b4252ecfc65a17216528e57
    Change-Id: I53d30dd0e926c96d9b4252ecfc65a17216528e57

--- packages/SystemUI/src/com/android/systemui/keyguard/domain/interactor/KeyguardBlueprintInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/domain/interactor/KeyguardBlueprintInteractor.kt
@@ -20,7 +20,6 @@
 package com.android.systemui.keyguard.domain.interactor
 
 import android.content.Context
-import android.util.Log
 import com.android.systemui.biometrics.domain.interactor.FingerprintPropertyInteractor
 import com.android.systemui.common.ui.domain.interactor.ConfigurationInteractor
 import com.android.systemui.dagger.SysUISingleton
@@ -43,7 +42,6 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.merge
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 @SysUISingleton
@@ -80,15 +78,7 @@ constructor(
     private val refreshEvents: Flow<Unit> =
         merge(
             configurationInteractor.onAnyConfigurationChange,
-            fingerprintPropertyInteractor.propertiesInitialized
-                .filter { it }
-                .map { Unit }
-                .onEach {
-                    Log.d(
-                        "KeyguardBlueprintInteractor",
-                        "triggering refreshEvent from fpPropertiesInitialized"
-                    )
-                },
+            fingerprintPropertyInteractor.propertiesInitialized.filter { it }.map { Unit },
         )
 
     init {
