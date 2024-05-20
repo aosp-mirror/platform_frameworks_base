4e116dc472d52eb901a29d75397f52abb7d18377
Nan Wu <wnan@google.com>
2024-05-20 18:17:36 +0000

    [conflict] RESTRICT AUTOMERGE Backport preventing BAL bypass via bound service am: 95b71ad87d am: 0876fa0374
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/base/+/27155205
    
    Signed-off-by: Automerger Merge Worker <android-build-automerger-merge-worker@system.gserviceaccount.com>
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:fb7f9058517545a8ecc48b7d72b4de9e47268413)
    Merged-In: If6728bb9d056805856df76031840719d7aa0d871
    Change-Id: If6728bb9d056805856df76031840719d7aa0d871

--- services/texttospeech/java/com/android/server/texttospeech/TextToSpeechManagerPerUserService.java
+++ services/texttospeech/java/com/android/server/texttospeech/TextToSpeechManagerPerUserService.java
@@ -95,7 +95,8 @@ final class TextToSpeechManagerPerUserService extends
                 ITextToSpeechSessionCallback callback) {
             super(context,
                     new Intent(TextToSpeech.Engine.INTENT_ACTION_TTS_SERVICE).setPackage(engine),
-                    Context.BIND_AUTO_CREATE | Context.BIND_SCHEDULE_LIKE_TOP_APP,
+                    Context.BIND_AUTO_CREATE | Context.BIND_SCHEDULE_LIKE_TOP_APP 
+                            | Context.BIND_DENY_ACTIVITY_STARTS_PRE_34,
                     userId,
                     ITextToSpeechService.Stub::asInterface);
             mEngine = engine;
