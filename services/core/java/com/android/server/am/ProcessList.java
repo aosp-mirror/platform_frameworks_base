4fd29b7eefd13eb78c9c5ba1d99bf1d0c697df33
Priyanka Advani <padvani@google.com>
2024-05-20 17:46:39 +0000

    Revert "Change position of isSdkSandbox{Audit,Next} input selectors"
    Revert submission 27360739-sdk_sandbox_audit
    
    Reason for revert: Droidmonitor triggered revert due to b/341740133. Will be verifying through ABTD before submission.
    
    Reverted changes: /q/submissionid:27360739-sdk_sandbox_audit
    
    Change-Id: I9e31d24a571a3fb102352163b29baa072b0edfe6

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -37,7 +37,6 @@ import static android.os.Process.startWebView;
 import static android.system.OsConstants.EAGAIN;
 
 import static com.android.sdksandbox.flags.Flags.selinuxSdkSandboxAudit;
-import static com.android.sdksandbox.flags.Flags.selinuxSdkSandboxInputSelector;
 import static com.android.server.am.ActivityManagerDebugConfig.DEBUG_LRU;
 import static com.android.server.am.ActivityManagerDebugConfig.DEBUG_NETWORK;
 import static com.android.server.am.ActivityManagerDebugConfig.DEBUG_PROCESSES;
@@ -2066,14 +2065,10 @@ public final class ProcessList {
             }
         }
 
-        if (selinuxSdkSandboxInputSelector()) {
-            return app.info.seInfo + extraInfo + TextUtils.emptyIfNull(app.info.seInfoUser);
-        } else {
-            return app.info.seInfo
-                    + (TextUtils.isEmpty(app.info.seInfoUser) ? "" : app.info.seInfoUser)
-                    + extraInfo;
-        }
+        return app.info.seInfo
+                + (TextUtils.isEmpty(app.info.seInfoUser) ? "" : app.info.seInfoUser) + extraInfo;
     }
+
 
     @GuardedBy("mService")
     boolean startProcessLocked(HostingRecord hostingRecord, String entryPoint, ProcessRecord app,
