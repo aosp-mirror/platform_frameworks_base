ccef5befce03b0c44245cdae4257edf2a92ac302
Naomi Musgrave <nmusgrave@google.com>
2024-05-23 07:55:29 +0000

    Revert "Introduce provider for drag resize handle sizes."
    This reverts commit 8ac937409fe4af9342075cff62fbd548afd0608e.
    
    Reason for revert: b/341921013
    
    Change-Id: If91db88e0b9310753af5e3c2b4b791a828bafbac

--- libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecorationTests.java
+++ libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecorationTests.java
@@ -115,8 +115,6 @@ public class DesktopModeWindowDecorationTests extends ShellTestCase {
     private WindowDecoration.SurfaceControlViewHostFactory mMockSurfaceControlViewHostFactory;
     @Mock
     private TypedArray mMockRoundedCornersRadiusArray;
-    @Mock
-    private ResizeHandleSizeRepository mMockResizeHandleSizeRepository;
 
     private final Configuration mConfiguration = new Configuration();
 
@@ -348,8 +346,8 @@ public class DesktopModeWindowDecorationTests extends ShellTestCase {
         return new DesktopModeWindowDecoration(mContext, mMockDisplayController,
                 mMockShellTaskOrganizer, taskInfo, mMockSurfaceControl,
                 mMockHandler, mMockChoreographer, mMockSyncQueue, mMockRootTaskDisplayAreaOrganizer,
-                mMockResizeHandleSizeRepository, SurfaceControl.Builder::new,
-                mMockTransactionSupplier, WindowContainerTransaction::new, SurfaceControl::new,
+                SurfaceControl.Builder::new, mMockTransactionSupplier,
+                WindowContainerTransaction::new, SurfaceControl::new,
                 mMockSurfaceControlViewHostFactory);
     }
 
