69d74b4e2c3bdf77fd8347d62cfbbfd29353513a
William Xiao <wxyz@google.com>
2024-05-23 16:51:47 -0700

    Add lock screen shortcut for opening glanceable hub
    This is added temporarily behind a flag for UX testing purposes and is
    not intended to be a proper, full implementation. The flag will not be
    advanced, only modified locally via adb command.
    
    Shortcut is on the right by default, it can be moved to the left,
    replacing the home controls shortcut, by changing the system setting
    open_hub_chip_replace_home_controls to 1.
    
    Bug: 339667383
    Test: N/A
    Flag: com.android.systemui.glanceable_hub_shortcut_button
    Change-Id: I51df40df8274a6f77418543783e1b3014bec473f

--- packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/data/quickaffordance/KeyguardQuickAffordanceLegacySettingSyncerTest.kt
+++ packages/SystemUI/multivalentTests/src/com/android/systemui/keyguard/data/quickaffordance/KeyguardQuickAffordanceLegacySettingSyncerTest.kt
@@ -90,6 +90,7 @@ class KeyguardQuickAffordanceLegacySettingSyncerTest : SysuiTestCase() {
                             .thenReturn(FakeSharedPreferences())
                     },
                 userTracker = FakeUserTracker(),
+                systemSettings = FakeSettings(),
                 broadcastDispatcher = fakeBroadcastDispatcher,
             )
         settings = FakeSettings()
