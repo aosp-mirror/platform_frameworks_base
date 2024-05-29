903e019c88ebce87c727a7c47b2c9758b0b715ce
Chaohui Wang <chaohuiw@google.com>
2024-05-29 16:28:16 +0800

    [Spa] Fix the Title color
    Not hard code one, use the default on container color.
    
    Fix: 343040341
    Test: visual
    Merged-In: If938962b91775206f32f0107b3b4702f9b5f88d1
    Change-Id: I411e468425e5b811ba5c76c72aa158932ef2b087

--- packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/ui/Text.kt
+++ packages/SettingsLib/Spa/spa/src/com/android/settingslib/spa/widget/ui/Text.kt
@@ -40,7 +40,6 @@ fun SettingsTitle(title: String, useMediumWeight: Boolean = false) {
     Text(
         text = title,
         modifier = Modifier.padding(vertical = SettingsDimension.paddingTiny),
-        color = MaterialTheme.colorScheme.onSurface,
         style = MaterialTheme.typography.titleMedium.withWeight(useMediumWeight),
     )
 }
