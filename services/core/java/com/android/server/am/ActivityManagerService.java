2618484c8190ba61b4afa47ba0aa6425685df1e3
Lydia Tse <lydiatse@google.com>
2024-05-29 18:31:32 +0000

    Added trace event to broadcastIntentWithFeature.
    Test: follow the steps below to test
    1. Collect a boot trace using the trace config from company/teams/auto/embedded/framework/performance/tools/src/perfetto_trace_config.textproto.
    2. Open the trace in Perfetto and query for a slice name with 'broadcastIntent' using
    SELECT * FROM slice WHERE name glob '%broadcastIntent:%';
    
    Change-Id: Idce039d2b055f08a710e8f47ad90348cd9537b3a

--- services/core/java/com/android/server/am/ActivityManagerService.java
+++ services/core/java/com/android/server/am/ActivityManagerService.java
@@ -15770,6 +15770,7 @@ public class ActivityManagerService extends IActivityManager.Stub
             String[] excludedPackages, int appOp, Bundle bOptions,
             boolean serialized, boolean sticky, int userId) {
         enforceNotIsolatedCaller("broadcastIntent");
+
         synchronized(this) {
             intent = verifyBroadcastLocked(intent);
 
@@ -15783,6 +15784,12 @@ public class ActivityManagerService extends IActivityManager.Stub
             // Permission regimes around sender-supplied broadcast options.
             enforceBroadcastOptionPermissionsInternal(bOptions, callingUid);
 
+            final ComponentName cn = intent.getComponent();
+
+            Trace.traceBegin(
+                    Trace.TRACE_TAG_ACTIVITY_MANAGER,
+                    "broadcastIntent:" + (cn != null ? cn.toString() : intent.getAction()));
+
             final long origId = Binder.clearCallingIdentity();
             try {
                 return broadcastIntentLocked(callerApp,
@@ -15793,6 +15800,7 @@ public class ActivityManagerService extends IActivityManager.Stub
                         callingPid, userId, BackgroundStartPrivileges.NONE, null, null);
             } finally {
                 Binder.restoreCallingIdentity(origId);
+                Trace.traceEnd(Trace.TRACE_TAG_ACTIVITY_MANAGER);
             }
         }
     }
