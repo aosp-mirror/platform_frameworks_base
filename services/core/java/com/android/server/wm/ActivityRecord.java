ebfda4b19d311eb08dbfbc05506ae9d481031cc0
Riddle Hsu <riddlehsu@google.com>
2024-06-06 16:52:34 +0800

    Reduce unnecessary visibility update after resumed
    When starting/resuming activity, ensureVisibilityAndConfig has
    already done the update. The invocation in completeResumeLocked
    is just duplicated.
    
    Bug: 297502610
    Test: CtsWindowManagerDeviceActivity
    Change-Id: Ic99513dab200c7a514e516a67468bf38316e6d4a

--- services/core/java/com/android/server/wm/ActivityRecord.java
+++ services/core/java/com/android/server/wm/ActivityRecord.java
@@ -6541,7 +6541,10 @@ final class ActivityRecord extends WindowToken implements WindowManagerService.A
         // Schedule an idle timeout in case the app doesn't do it for us.
         mTaskSupervisor.scheduleIdleTimeout(this);
 
-        mTaskSupervisor.reportResumedActivityLocked(this);
+        mTaskSupervisor.mStoppingActivities.remove(this);
+        if (getDisplayArea().allResumedActivitiesComplete()) {
+            mRootWindowContainer.executeAppTransitionForAllDisplay();
+        }
 
         resumeKeyDispatchingLocked();
         final Task rootTask = getRootTask();
