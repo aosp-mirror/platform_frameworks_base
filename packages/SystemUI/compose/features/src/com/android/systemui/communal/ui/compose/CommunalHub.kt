eb700ed24b673b598e8eef5a52f21b728893cf03
Anvesh Renikindi <renikindi@google.com>
2024-06-25 04:26:13 +0000

    animateItemPlacement() is deprecated and needs to be updated to animateItem().
    Change-Id: I02a8d1426812b8379a130cbcd15f64f6d325e9e9
    Test: Treehugger
    Flag: bugfix b/348734945

--- packages/SystemUI/compose/features/src/com/android/systemui/communal/ui/compose/CommunalHub.kt
+++ packages/SystemUI/compose/features/src/com/android/systemui/communal/ui/compose/CommunalHub.kt
@@ -589,7 +589,7 @@ private fun BoxScope.CommunalHubLazyGrid(
                 }
             } else {
                 CommunalContent(
-                    modifier = cardModifier.animateItemPlacement(),
+                    modifier = cardModifier.animateItem(),
                     model = list[index],
                     viewModel = viewModel,
                     size = size,
