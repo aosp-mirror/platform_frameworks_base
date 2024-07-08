3a2316852ef267387e8f368b186c4b03d3d39336
Pablo Gamito <pablogamito@google.com>
2024-07-08 08:20:23 +0000

    Revert "Revert "Handle potentially null strings passed as ProtoL..."
    Revert submission 28207894-revert-28147757-no-processing-protolog-IJZXUZOQGO
    
    Reason for revert: Re-landing change
    
    Reverted changes: /q/submissionid:28207894-revert-28147757-no-processing-protolog-IJZXUZOQGO
    
    Change-Id: I0805cb9be59ba01d504ac292a831e4fa8d903c34

--- core/java/com/android/internal/protolog/PerfettoProtoLogImpl.java
+++ core/java/com/android/internal/protolog/PerfettoProtoLogImpl.java
@@ -91,6 +91,7 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class PerfettoProtoLogImpl implements IProtoLog {
     private static final String LOG_TAG = "ProtoLog";
+    public static final String NULL_STRING = "null";
     private final AtomicInteger mTracingInstances = new AtomicInteger();
 
     private final ProtoLogDataSource mDataSource = new ProtoLogDataSource(
@@ -320,8 +321,14 @@ public class PerfettoProtoLogImpl implements IProtoLog {
             StringBuilder builder = new StringBuilder("UNKNOWN MESSAGE");
             if (args != null) {
                 builder.append(" args = (");
-                builder.append(String.join(", ", Arrays.stream(args).map(
-                        Object::toString).toList()));
+                builder.append(String.join(", ", Arrays.stream(args)
+                        .map(it -> {
+                            if (it == null) {
+                                return "null";
+                            } else {
+                                return it.toString();
+                            }
+                        }).toList()));
                 builder.append(")");
             }
             messageString = builder.toString();
@@ -412,7 +419,11 @@ public class PerfettoProtoLogImpl implements IProtoLog {
                 for (Object o : args) {
                     int type = LogDataType.bitmaskToLogDataType(message.getMessageMask(), argIndex);
                     if (type == LogDataType.STRING) {
-                        internStringArg(ctx, o.toString());
+                        if (o == null) {
+                            internStringArg(ctx, NULL_STRING);
+                        } else {
+                            internStringArg(ctx, o.toString());
+                        }
                     }
                     argIndex++;
                 }
@@ -455,7 +466,12 @@ public class PerfettoProtoLogImpl implements IProtoLog {
                     try {
                         switch (type) {
                             case LogDataType.STRING:
-                                final int internedStringId = internStringArg(ctx, o.toString());
+                                final int internedStringId;
+                                if (o == null) {
+                                    internedStringId = internStringArg(ctx, NULL_STRING);
+                                } else {
+                                    internedStringId = internStringArg(ctx, o.toString());
+                                }
                                 os.write(STR_PARAM_IIDS, internedStringId);
                                 needsIncrementalState = true;
                                 break;
