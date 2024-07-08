d1e0d2e642d47e61d3bb6c28cc2dd4a13a79a400
Pablo Gamito <pablogamito@google.com>
2024-07-08 08:20:23 +0000

    Revert^2 "Handle null args list"
    3220edea88aac96468bf2a5000d85820628f596d
    
    Change-Id: I5aad0af971b987784eb4574de1402b04b9049ec8

--- core/java/com/android/internal/protolog/PerfettoProtoLogImpl.java
+++ core/java/com/android/internal/protolog/PerfettoProtoLogImpl.java
@@ -170,7 +170,7 @@ public class PerfettoProtoLogImpl implements IProtoLog {
     @VisibleForTesting
     @Override
     public void log(LogLevel level, IProtoLogGroup group, long messageHash, int paramsMask,
-            Object[] args) {
+            @Nullable Object[] args) {
         Trace.traceBegin(Trace.TRACE_TAG_WINDOW_MANAGER, "log");
 
         try {
@@ -297,7 +297,8 @@ public class PerfettoProtoLogImpl implements IProtoLog {
         os.end(outMessagesToken);
     }
 
-    private void logToLogcat(String tag, LogLevel level, long messageHash, Object[] args) {
+    private void logToLogcat(String tag, LogLevel level, long messageHash,
+            @Nullable Object[] args) {
         Trace.traceBegin(Trace.TRACE_TAG_WINDOW_MANAGER, "logToLogcat");
         try {
             doLogToLogcat(tag, level, messageHash, args);
@@ -306,7 +307,8 @@ public class PerfettoProtoLogImpl implements IProtoLog {
         }
     }
 
-    private void doLogToLogcat(String tag, LogLevel level, long messageHash, Object[] args) {
+    private void doLogToLogcat(String tag, LogLevel level, long messageHash,
+            @Nullable Object[] args) {
         String message = null;
         String messageString = mViewerConfigReader.getViewerString(messageHash);
         if (messageString != null) {
@@ -322,8 +324,10 @@ public class PerfettoProtoLogImpl implements IProtoLog {
         }
         if (message == null) {
             StringBuilder builder = new StringBuilder("UNKNOWN MESSAGE (" + messageHash + ")");
-            for (Object o : args) {
-                builder.append(" ").append(o);
+            if (args != null) {
+                for (Object o : args) {
+                    builder.append(" ").append(o);
+                }
             }
             message = builder.toString();
         }
