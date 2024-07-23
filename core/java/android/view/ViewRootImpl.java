6afe004904bbb7b7d1145636546488d572eb3fde
Shane <shanewong@google.com>
2024-07-23 00:10:37 +0000

    [VRR] Remove intermittentUpdateState check in setCategoryFromCategoryCounts
    Since we no longer apply intermittent update in ViewRootImpl, remove the check in setCategoryFromCategoryCounts to ensure it has no effect.
    
    Flag: EXEMPT bugfix
    Test: atest ViewFrameRateTest / atest ViewRootImplTest
    Bug: 352976358
    Change-Id: Iae6bff15189289ed2e68b02d738a2ad1a0ed13bb

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -12882,11 +12882,6 @@ public final class ViewRootImpl implements ViewParent,
                     mFrameRateCategoryHighCount = FRAME_RATE_CATEGORY_COUNT;
         }
 
-        // If it's currently an intermittent update,
-        // we should keep mPreferredFrameRateCategory as NORMAL
-        if (intermittentUpdateState() == INTERMITTENT_STATE_INTERMITTENT) {
-            return;
-        }
 
         if (mFrameRateCategoryHighCount > 0) {
             mPreferredFrameRateCategory = FRAME_RATE_CATEGORY_HIGH;
