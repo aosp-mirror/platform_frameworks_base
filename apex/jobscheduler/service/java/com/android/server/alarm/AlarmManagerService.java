7fe2d011062be5ff098c9b855efb5911d64354ab
Jorge De la Torre <jorgetm@google.com>
2024-07-24 18:30:36 +0000

    Revert "AlarmManager: Store the next dst transition in system properties"
    This reverts commit 2dd606d7ad5ae7588259ad8faf549e8a389397c1.
    
    Reason for revert: Updating how sys props are stored b/340482112
    
    Change-Id: I7ec8ba1c6535aaecaeed35a913ed8af6f00464fe

--- apex/jobscheduler/service/java/com/android/server/alarm/AlarmManagerService.java
+++ apex/jobscheduler/service/java/com/android/server/alarm/AlarmManagerService.java
@@ -179,9 +179,6 @@ import java.io.PrintWriter;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.text.SimpleDateFormat;
-import java.time.Instant;
-import java.time.zone.ZoneOffsetTransition;
-import java.time.zone.ZoneRules;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -193,7 +190,6 @@ import java.util.Set;
 import java.util.TimeZone;
 import java.util.TreeSet;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
 /**
@@ -234,12 +230,8 @@ public class AlarmManagerService extends SystemService {
 
     private static final long TEMPORARY_QUOTA_DURATION = INTERVAL_DAY;
 
-    // System properties read on some device configurations to initialize time properly and
-    // perform DST transitions at the bootloader level.
+    // System property read on some device configurations to initialize time properly.
     private static final String TIMEOFFSET_PROPERTY = "persist.sys.time.offset";
-    private static final String DST_TRANSITION_PROPERTY = "persist.sys.time.dst_transition";
-    private static final String DST_OFFSET_PROPERTY = "persist.sys.time.dst_offset";
-
 
     private final Intent mBackgroundIntent
             = new Intent().addFlags(Intent.FLAG_FROM_BACKGROUND);
@@ -2157,19 +2149,6 @@ public class AlarmManagerService extends SystemService {
 
             final int gmtOffset = newZone.getOffset(mInjector.getCurrentTimeMillis());
             SystemProperties.set(TIMEOFFSET_PROPERTY, String.valueOf(gmtOffset));
-
-            final ZoneRules rules = newZone.toZoneId().getRules();
-            final ZoneOffsetTransition transition = rules.nextTransition(Instant.now());
-            if (null != transition) {
-                // Get the offset between the time after the DST transition and before.
-                final long transitionOffset = TimeUnit.SECONDS.toMillis((
-                        transition.getOffsetAfter().getTotalSeconds()
-                        - transition.getOffsetBefore().getTotalSeconds()));
-                // Time when the next DST transition is programmed.
-                final long nextTransition = TimeUnit.SECONDS.toMillis(transition.toEpochSecond());
-                SystemProperties.set(DST_TRANSITION_PROPERTY, String.valueOf(nextTransition));
-                SystemProperties.set(DST_OFFSET_PROPERTY, String.valueOf(transitionOffset));
-            }
         }
 
         // Clear the default time zone in the system server process. This forces the next call
