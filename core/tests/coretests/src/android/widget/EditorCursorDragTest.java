514e7881902c790d725b42be1fdf12809b59e79d
Eric Lin <ericth@google.com>
2024-07-25 13:05:34 +0800

    Replace deprecated AndroidJUnit4.
    AndroidJUnit4 from `androidx.test.runner` is deprecated, use
    `androidx.test.ext.junit.runners.AndroidJUnit4` instead. See https://
    developer.android.com/reference/androidx/test/runner/AndroidJUnit4 for
    more details.
    
    Bug: 354063239
    Test: atest FrameworksCoreTests --\
     --module-arg FrameworksCoreTests:include-filter:android.widget,com.android.internal.widget \
     --module-arg FrameworksCoreTests:instrumentation-arg:annotation:=android.platform.test.annotations.Presubmit \
     --module-arg FrameworksCoreTests:instrumentation-arg:notAnnotation:=android.support.test.filters.FlakyTest,androidx.test.filters.FlakyTest
    Flag: TEST_ONLY
    Change-Id: Ifd2a99ad51f6030ad7d61fb70977b72e83e4e41a

--- core/tests/coretests/src/android/widget/EditorCursorDragTest.java
+++ core/tests/coretests/src/android/widget/EditorCursorDragTest.java
@@ -48,10 +48,10 @@ import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.test.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.MediumTest;
 import androidx.test.filters.Suppress;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
 
 import com.android.frameworks.coretests.R;
 
