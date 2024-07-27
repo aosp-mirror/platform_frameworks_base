44a859a61eebc24f995c73eb0af96abfe82cf176
Yohei Yukawa <yukawa@google.com>
2024-07-27 10:34:53 -0700

    Revert "Remove UserLifecycleListener deps from SecureSettingsWrapper"
    This reverts commit 1a7311fe9a04a840f730144d72f12d74f2005511.
    
    Reason for revert:
    While it is highly unlikely, reverting this is the fastest and easiest
    way to to demonstrate that this CL is not the real culprit of the SysUI
    perf regression discussed in Bug 355334507.
    
    Once it is confirmed that this CL is not the culprit, we will re-land
    this as this CL should indeed improve our memory consumption.
    
    Bug: 352354308
    Bug: 355334507
    Flag: EXEMPT refactor
    Test: presubmit
    Change-Id: Ib4ce3cb283f5165cff0e66c9a6ddc29910abd911

--- services/core/java/com/android/server/inputmethod/SecureSettingsWrapper.java
+++ services/core/java/com/android/server/inputmethod/SecureSettingsWrapper.java
@@ -20,7 +20,10 @@ import android.annotation.AnyThread;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
+import android.app.ActivityManagerInternal;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.UserInfo;
 import android.provider.Settings;
 import android.util.ArrayMap;
 import android.util.ArraySet;
@@ -318,13 +321,30 @@ final class SecureSettingsWrapper {
     }
 
     /**
-     * Called when the system is starting.
+     * Called when {@link InputMethodManagerService} is starting.
      *
-     * @param contentResolver the {@link ContentResolver} to be used
+     * @param context the {@link Context} to be used.
      */
     @AnyThread
-    static void setContentResolver(@NonNull ContentResolver contentResolver) {
-        sContentResolver = contentResolver;
+    static void onStart(@NonNull Context context) {
+        sContentResolver = context.getContentResolver();
+
+        final int userId = LocalServices.getService(ActivityManagerInternal.class)
+                .getCurrentUserId();
+        final UserManagerInternal userManagerInternal =
+                LocalServices.getService(UserManagerInternal.class);
+        putOrGet(userId, createImpl(userManagerInternal, userId));
+
+        userManagerInternal.addUserLifecycleListener(
+                new UserManagerInternal.UserLifecycleListener() {
+                    @Override
+                    public void onUserRemoved(UserInfo user) {
+                        synchronized (sMutationLock) {
+                            sUserMap = sUserMap.cloneWithRemoveOrSelf(user.id);
+                        }
+                    }
+                }
+        );
     }
 
     /**
@@ -354,18 +374,6 @@ final class SecureSettingsWrapper {
         }
         final ReaderWriter readerWriter = new UnlockedUserImpl(userId, sContentResolver);
         putOrGet(userId, readerWriter);
-    }
-
-    /**
-     * Called when a user is being removed.
-     *
-     * @param userId the ID of the user whose storage is being removed.
-     */
-    @AnyThread
-    static void onUserRemoved(@UserIdInt int userId) {
-        synchronized (sMutationLock) {
-            sUserMap = sUserMap.cloneWithRemoveOrSelf(userId);
-        }
     }
 
     /**
