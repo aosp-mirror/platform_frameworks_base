2379f39061deb1df55cda8684f62e85d5a353c3c
Winson Chung <winsonc@google.com>
2024-08-03 03:14:42 +0000

    Only cancel pending resize transition if it still exists
    - Canceling the running animation can reset the pending resize which can
      lead to a NPE when trying to cancel it directly
    
    Flag: EXEMPT bugfix
    Fixes: 340380540
    Test: NPE fix
    Change-Id: I2b8037fc81c55aad3a080802cf4cbb89d56c47e6

--- libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/SplitScreenTransitions.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/splitscreen/SplitScreenTransitions.java
@@ -417,9 +417,9 @@ class SplitScreenTransitions {
         ProtoLog.d(WM_SHELL_SPLIT_SCREEN, "setResizeTransition: hasPendingResize=%b",
                 mPendingResize != null);
         if (mPendingResize != null) {
+            mPendingResize.cancel(null);
             mainDecor.cancelRunningAnimations();
             sideDecor.cancelRunningAnimations();
-            mPendingResize.cancel(null);
             mAnimations.clear();
             onFinish(null /* wct */);
         }
