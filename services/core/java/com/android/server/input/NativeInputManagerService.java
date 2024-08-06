e98ecc5888975d65114071a9c0cd9136d5e9768f
Omar Abdelmonem <omarrr@google.com>
2024-08-06 09:45:59 +0000

    Create getTouchpadHardwareProperties() and link it to native code
    Added a new method getHardwareProperties that returns an instance of a
    new class, HardwareProperties, in Java. This method is also linked to
    its native implementation.
    
    Bug: 286551975
    
    Test: Verified that new method correctly calls its native counterpart.
    Flag: com.android.hardware.input.touchpad_visualizer
    Change-Id: I1fe1553bf19bcc71f679c3cdf243219da61ff333

--- services/core/java/com/android/server/input/NativeInputManagerService.java
+++ services/core/java/com/android/server/input/NativeInputManagerService.java
@@ -214,6 +214,9 @@ interface NativeInputManagerService {
 
     InputSensorInfo[] getSensorList(int deviceId);
 
+    @Nullable
+    TouchpadHardwareProperties getTouchpadHardwareProperties(int deviceId);
+
     boolean flushSensor(int deviceId, int sensorType);
 
     boolean enableSensor(int deviceId, int sensorType, int samplingPeriodUs,
@@ -505,6 +508,9 @@ interface NativeInputManagerService {
 
         @Override
         public native InputSensorInfo[] getSensorList(int deviceId);
+
+        @Override
+        public native TouchpadHardwareProperties getTouchpadHardwareProperties(int deviceId);
 
         @Override
         public native boolean flushSensor(int deviceId, int sensorType);
