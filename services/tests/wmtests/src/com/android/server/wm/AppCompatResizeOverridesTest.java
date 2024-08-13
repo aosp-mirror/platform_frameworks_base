b9d3ffd185a0349cbb86914941ee84c3142c8e7f
Massimo Carli <mcarli@google.com>
2024-08-13 09:55:23 +0000

    [33/n] Remove redundant spyOn()
    spyOn() invocations on AppCompatConfiguration is redundant because
    already done in the AppCompatConfigurationRobot.
    
    Flag: EXEMPT refactor
    Bug: 351763164
    Test: atest WmTests:AppCompatCameraOverridesTest
    Test: atest WmTests:AppCompatFocusOverridesTest
    Test: atest WmTests:AppCompatOrientationPolicyTest
    Test: atest WmTests:AppCompatReachabilityOverridesTest
    Test: atest WmTests:AppCompatReachabilityPolicyTest
    Test: atest WmTests:AppCompatResizeOverridesTest
    
    Change-Id: Ib2755c1be7c9ffa73f4cb72dbed13c6ed454c6cf

--- services/tests/wmtests/src/com/android/server/wm/AppCompatResizeOverridesTest.java
+++ services/tests/wmtests/src/com/android/server/wm/AppCompatResizeOverridesTest.java
@@ -20,8 +20,6 @@ import static android.content.pm.ActivityInfo.FORCE_NON_RESIZE_APP;
 import static android.content.pm.ActivityInfo.FORCE_RESIZE_APP;
 import static android.view.WindowManager.PROPERTY_COMPAT_ALLOW_RESIZEABLE_ACTIVITY_OVERRIDES;
 
-import static com.android.dx.mockito.inline.extended.ExtendedMockito.spyOn;
-
 import android.compat.testing.PlatformCompatChangeRule;
 import android.platform.test.annotations.Presubmit;
 
@@ -171,7 +169,6 @@ public class AppCompatResizeOverridesTest extends WindowTestsBase {
      * Runs a test scenario providing a Robot.
      */
     void runTestScenario(@NonNull Consumer<ResizeOverridesRobotTest> consumer) {
-        spyOn(mWm.mAppCompatConfiguration);
         final ResizeOverridesRobotTest robot = new ResizeOverridesRobotTest(mWm, mAtm, mSupervisor);
         consumer.accept(robot);
     }
