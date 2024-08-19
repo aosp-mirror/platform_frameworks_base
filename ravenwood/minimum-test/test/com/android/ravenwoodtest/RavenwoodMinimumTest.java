e935950bdfad2299f5ed716114bd3c7284ffdf2a
Makoto Onuki <omakoto@google.com>
2024-08-19 12:18:28 -0700

    Change tests' package names
    When we apply a new ravenizer tool on test jars, we want to avoid making
    changes to ravenwood code, which is under `com.android.ravenood`,
    `com.android.platform.test.ravenwood`, etc. (it's not consistent
    unfortunately)
    
    But we still want to apply it to ravenwood test code.
    
    So let's make sure ravenwood tests don't share the same packages
    as non-test code.
    
    Flag: EXEMPT host side test change only
    Bug: 292141694
    Test: $ANDROID_BUILD_TOP/frameworks/base/ravenwood/scripts/run-ravenwood-tests.sh
    Change-Id: I193f2a0bd715ffde0e28709b8b919e7d8738d7ab

--- ravenwood/minimum-test/test/com/android/ravenwoodtest/RavenwoodMinimumTest.java
+++ ravenwood/minimum-test/test/com/android/ravenwoodtest/RavenwoodMinimumTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.ravenwood;
+package com.android.ravenwoodtest;
 
 import android.platform.test.annotations.IgnoreUnderRavenwood;
 import android.platform.test.ravenwood.RavenwoodRule;
