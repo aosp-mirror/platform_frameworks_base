4a7f61e46acbed942789b4bbfebedcdb1eb60b56
Torne (Richard Coles) <torne@google.com>
2024-08-30 15:14:15 -0400

    webview: remove MATCH_DEBUG_TRIAGED_MISSING.
    MATCH_DEBUG_TRIAGED_MISSING is a deprecated alias for
    MATCH_DIRECT_BOOT_AUTO, which has no effect here as we aren't fetching
    component information, so just remove it.
    
    Bug: 310653407
    Test: atest CtsWebkitTestCases; atest WebViewUpdateServiceTest
    Flag: NONE removing obsolete hidden API dependency
    Change-Id: I6a6d49a9f14bb7163c80a6d092080bf564bc0ced

--- core/java/android/webkit/WebViewFactory.java
+++ core/java/android/webkit/WebViewFactory.java
@@ -318,7 +318,7 @@ public final class WebViewFactory {
         String libraryFileName;
         try {
             PackageInfo packageInfo = packageManager.getPackageInfo(packageName,
-                    PackageManager.GET_META_DATA | PackageManager.MATCH_DEBUG_TRIAGED_MISSING);
+                    PackageManager.GET_META_DATA);
             libraryFileName = getWebViewLibrary(packageInfo.applicationInfo);
         } catch (PackageManager.NameNotFoundException e) {
             Log.e(LOGTAG, "Couldn't find package " + packageName);
@@ -479,7 +479,6 @@ public final class WebViewFactory {
                 newPackageInfo = pm.getPackageInfo(
                     response.packageInfo.packageName,
                     PackageManager.GET_SHARED_LIBRARY_FILES
-                    | PackageManager.MATCH_DEBUG_TRIAGED_MISSING
                     // Make sure that we fetch the current provider even if its not
                     // installed for the current user
                     | PackageManager.MATCH_UNINSTALLED_PACKAGES
