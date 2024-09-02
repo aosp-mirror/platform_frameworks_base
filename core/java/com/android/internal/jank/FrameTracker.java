494e9f8a850e12fb66bd87b3675da9ef9ff326d4
Pechetty Sravani (xWF) <pechetty@google.com>
2024-09-02 06:26:08 +0000

    Revert "Use the expected frame duration from SF instead of HWUI."
    Revert submission 28899062-frame_duration
    
    Reason for revert: Droid monitor created revert due to this b/362835127.
    
    Reverted changes: /q/submissionid:28899062-frame_duration
    
    Change-Id: I969312c37cf9836b95b86106aa6cd80aa395f043

--- core/java/com/android/internal/jank/FrameTracker.java
+++ core/java/com/android/internal/jank/FrameTracker.java
@@ -158,17 +158,12 @@ public class FrameTracker implements HardwareRendererObserver.OnFrameMetricsAvai
             this.surfaceControlCallbackFired = true;
             this.jankType = jankStat.jankType;
             this.refreshRate = DisplayRefreshRate.getRefreshRate(jankStat.frameIntervalNs);
-            if (Flags.useSfFrameDuration()) {
-                this.totalDurationNanos = jankStat.actualAppFrameTimeNs;
-            }
             return this;
         }
 
         private JankInfo update(long totalDurationNanos, boolean isFirstFrame) {
             this.hwuiCallbackFired = true;
-            if (!Flags.useSfFrameDuration()) {
-                this.totalDurationNanos = totalDurationNanos;
-            }
+            this.totalDurationNanos = totalDurationNanos;
             this.isFirstFrame = isFirstFrame;
             return this;
         }
