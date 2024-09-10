2073b73d917d07997d93132beb480a6cfbbbe02a
Amith Yamasani <yamasani@google.com>
2024-09-10 16:20:52 -0700

    Settings override for use_app_info_not_launched flag
    Override the default false flag using global setting
    
    Bug: 362516211
    Test: settings put global use_app_info_not_launched 1
          Manually test restore of app and ensure BOOT_COMPLETED is not
          delivered to restoring app
    Flag: EXEMPT bugfix
    Change-Id: I60c1e3a48312eb2982c629f18cc2b375d46795b4

--- services/core/java/com/android/server/am/ProcessList.java
+++ services/core/java/com/android/server/am/ProcessList.java
@@ -3398,7 +3398,8 @@ public final class ProcessList {
         // Check if we should mark the processrecord for first launch after force-stopping
         if (wasStopped) {
             boolean wasEverLaunched = false;
-            if (android.app.Flags.useAppInfoNotLaunched()) {
+            if (android.app.Flags.useAppInfoNotLaunched()
+                    || mService.mConstants.mFlagUseAppInfoNotLaunched) {
                 wasEverLaunched = !info.isNotLaunched();
             } else {
                 try {
@@ -3419,7 +3420,8 @@ public final class ProcessList {
                         : STOPPED_STATE_FIRST_LAUNCH;
                 r.getWindowProcessController().setStoppedState(stoppedState);
             } else {
-                if (android.app.Flags.useAppInfoNotLaunched()) {
+                if (android.app.Flags.useAppInfoNotLaunched()
+                        || mService.mConstants.mFlagUseAppInfoNotLaunched) {
                     // If it was launched before, then it must be a force-stop
                     r.setWasForceStopped(wasEverLaunched);
                 } else {
