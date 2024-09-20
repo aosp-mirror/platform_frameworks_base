937542a5838a165a766b8fd8c1e048e7ec629b8e
Winson Chung <winsonc@google.com>
2024-09-20 22:32:14 +0000

    Add temporary bbq merge path to SCR logs
    - Until we get some native-side logs, at least surface these to SCR so
      we don't lose them when trying to trace locally
    
    Flag: EXEMPT dev logging
    Bug: 349601805
    Bug: 366484871
    Test: adb shell setprop persist.wm.debug.sc.tx.log_match_call setAlpha,merge,apply
    Change-Id: I9a3860f894f3649d1a7942a9cc43f6e693610a5a

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -12402,6 +12402,7 @@ public final class ViewRootImpl implements ViewParent,
         transaction.setBlurRegions(surfaceControl, regionCopy);
 
         if (mBlastBufferQueue != null) {
+            transaction.onMergeWithNextTransaction(getTitle());
             mBlastBufferQueue.mergeWithNextTransaction(transaction, frameNumber);
         }
     }
@@ -12428,6 +12429,9 @@ public final class ViewRootImpl implements ViewParent,
      */
     public void mergeWithNextTransaction(Transaction t, long frameNumber) {
         if (mBlastBufferQueue != null) {
+            if (t != null) {
+                t.onMergeWithNextTransaction(getTitle());
+            }
             mBlastBufferQueue.mergeWithNextTransaction(t, frameNumber);
         } else {
             t.apply();
