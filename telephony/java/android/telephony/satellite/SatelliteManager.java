a4408495642e27a12385bae997529434e9a3e4eb
Sooraj Sasindran <sasindran@google.com>
2024-09-27 17:26:04 +0000

    Add a new datagram type for checking pending incoming SMS.
    Change-Id: I106b9e339dd4175cf4289a4dcf5581fa96ff44a1
    Test: build
    Flag: EXEMPT hidden enum change
    Bug: 370045189

--- telephony/java/android/telephony/satellite/SatelliteManager.java
+++ telephony/java/android/telephony/satellite/SatelliteManager.java
@@ -1113,6 +1113,12 @@ public final class SatelliteManager {
      * @hide
      */
     public static final int DATAGRAM_TYPE_SMS = 6;
+    /**
+     * Datagram type indicating that the message to be sent is an SMS checking
+     * for pending incoming SMS.
+     * @hide
+     */
+    public static final int DATAGRAM_TYPE_CHECK_PENDING_INCOMING_SMS = 7;
 
     /** @hide */
     @IntDef(prefix = "DATAGRAM_TYPE_", value = {
@@ -1122,7 +1128,8 @@ public final class SatelliteManager {
             DATAGRAM_TYPE_KEEP_ALIVE,
             DATAGRAM_TYPE_LAST_SOS_MESSAGE_STILL_NEED_HELP,
             DATAGRAM_TYPE_LAST_SOS_MESSAGE_NO_HELP_NEEDED,
-            DATAGRAM_TYPE_SMS
+            DATAGRAM_TYPE_SMS,
+            DATAGRAM_TYPE_CHECK_PENDING_INCOMING_SMS
     })
     @Retention(RetentionPolicy.SOURCE)
     public @interface DatagramType {}
