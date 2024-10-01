bd5ccf06921b1aa87b88383d7c22bdb2b55ad18f
mattsziklay <mattsziklay@google.com>
2024-10-01 15:15:50 -0700

    Determine indicator type for input outside of display.
    Removes explicit TO_DESKTOP region calculation in favor of using
    defaults for inputs outside display bounds. Namely:
    
    - Inputs with x < 0 will return a TO_SPLIT_LEFT indicator.
    - Inputs with x > display width will return TO_SPLIT_RIGHT indicator.
    - Inputs anywhere else not in an explicitly defined region will default
      to NO_INDICATOR for freeform task drags and TO_DESKTOP for all others.
    - Fullscreen region is updated to have the top be Short.MIN_VALUE to
      serve as an arbitrarily low number.
    
    This fixes an issue where certain transitions could crash when receiving
    input outside of display bounds.
    
    Indicator test is updated accordingly.
    
    Fix: 370613574
    Test: Manual
    Flag: EXEMPT bug fix
    Change-Id: I5ad1aceab557f4546c68e841e5a8998b834143c6

--- libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
+++ libs/WindowManager/Shell/src/com/android/wm/shell/desktopmode/DesktopTasksController.kt
@@ -1574,7 +1574,7 @@ class DesktopTasksController(
             return
         }
 
-        val indicator = visualIndicator ?: return
+        val indicator = getVisualIndicator() ?: return
         val indicatorType =
             indicator.updateIndicatorType(
                 PointF(inputCoordinate.x, currentDragBounds.top.toFloat()),
