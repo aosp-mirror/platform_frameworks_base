2b2a7d6a31dab0b97de7599a6f32d374584c9d0e
Dmitry Dementyev <dementyev@google.com>
2024-10-01 14:57:44 -0700

    Update checkKeyIntent
    1) Explicityly set  component after target activity check.
    2) Update Intent subclass check.
    
    Bug: 360846772
    Test: manual
    Flag: EXEMPT bugfix
    (cherry picked from commit cde345a7ee06db716e613e12a2c218ce248ad1c4)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:d04cea5ea08f91f55a14209c1e424e163e2d50ab)
    Merged-In: Ied7961c73299681aa5b523cf3f00fd905893116f
    Change-Id: Ied7961c73299681aa5b523cf3f00fd905893116f

--- services/core/java/com/android/server/accounts/AccountManagerService.java
+++ services/core/java/com/android/server/accounts/AccountManagerService.java
@@ -4978,6 +4978,8 @@ public class AccountManagerService
                     Log.e(TAG, String.format(tmpl, activityName, pkgName, mAccountType));
                     return false;
                 }
+                intent.setComponent(targetActivityInfo.getComponentName());
+                bundle.putParcelable(AccountManager.KEY_INTENT, intent);
                 return true;
             } finally {
                 Binder.restoreCallingIdentity(bid);
@@ -4999,14 +5001,15 @@ public class AccountManagerService
             Bundle simulateBundle = p.readBundle();
             p.recycle();
             Intent intent = bundle.getParcelable(AccountManager.KEY_INTENT, Intent.class);
-            if (intent != null && intent.getClass() != Intent.class) {
-                return false;
-            }
             Intent simulateIntent = simulateBundle.getParcelable(AccountManager.KEY_INTENT,
                     Intent.class);
             if (intent == null) {
                 return (simulateIntent == null);
             }
+            if (intent.getClass() != Intent.class || simulateIntent.getClass() != Intent.class) {
+                return false;
+            }
+
             if (!intent.filterEquals(simulateIntent)) {
                 return false;
             }
