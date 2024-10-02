93849f0b3027c83641e439ba7b41e1bb3e888e0d
Marzia Favaro <marziana@google.com>
2024-10-02 10:04:07 +0000

    Ignore geometry parent when !useTasksDimOnly
    ag/28428783 accidentally introduced a regression on the old unflagged
    code
    
    Bug: 366132859
    Test: manual, verify bug is fixed when flag off
    Flag: EXEMPT fix for old code not protected by com.android.window.flags.use_tasks_dim_only
    Change-Id: Ifb788c9cdbdfbafff0f784dcb4eb77595d73ff2a

--- services/core/java/com/android/server/wm/Dimmer.java
+++ services/core/java/com/android/server/wm/Dimmer.java
@@ -21,6 +21,7 @@ import static com.android.server.wm.WindowManagerDebugConfig.TAG_WITH_CLASS_NAME
 import static com.android.server.wm.WindowManagerDebugConfig.TAG_WM;
 
 import android.annotation.NonNull;
+import android.annotation.Nullable;
 import android.graphics.Rect;
 import android.util.Log;
 import android.view.Surface;
@@ -128,7 +129,7 @@ class Dimmer {
         /**
          * Set the parameters to prepare the dim to be relative parented to the dimming container
          */
-        void prepareReparent(@NonNull WindowContainer<?> geometryParent,
+        void prepareReparent(@Nullable WindowContainer<?> geometryParent,
                 @NonNull WindowState relativeParent) {
             mAnimationHelper.setRequestedRelativeParent(relativeParent);
             mAnimationHelper.setRequestedGeometryParent(geometryParent);
@@ -221,7 +222,7 @@ class Dimmer {
      * @param dimmingContainer      The container that is dimming. The dim layer will be rel-z
      *                              parented below it
      */
-    public void adjustPosition(@NonNull WindowContainer<?> geometryParent,
+    public void adjustPosition(@Nullable WindowContainer<?> geometryParent,
                                     @NonNull WindowState dimmingContainer) {
         if (mDimState != null) {
             mDimState.prepareReparent(geometryParent, dimmingContainer);
