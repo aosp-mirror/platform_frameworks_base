8741b15afb240c773ffc6b845275f7317faaeae8
Jordan Demeulenaere <jdemeulenaere@google.com>
2024-10-08 14:12:00 +0200

    Rename SystemUIThemeTest to PlatformThemeTest
    Bug: 370422346
    Test: atest PlatformThemeTest
    Flag: EXEMPT test change
    Change-Id: I018a972a5e6b0a22ea2d437516711896a8118102

--- packages/SystemUI/compose/core/tests/src/com/android/compose/theme/PlatformThemeTest.kt
+++ packages/SystemUI/compose/core/tests/src/com/android/compose/theme/PlatformThemeTest.kt
@@ -27,7 +27,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class SystemUIThemeTest {
+class PlatformThemeTest {
     @get:Rule val composeRule = createComposeRule()
 
     @Test
