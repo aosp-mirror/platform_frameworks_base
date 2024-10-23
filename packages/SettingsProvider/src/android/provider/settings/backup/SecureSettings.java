b08920abb03d390117a8acab0473d62ec771af93
lbill <lbill@google.com>
2024-10-23 08:50:26 +0000

    [Screen off unlock UDFPS] Settings integration 1/2
    1. Add a settings key SCREEN_OFF_UNLOCK_UDFPS_ENABLED
    2. Integrate the key to AmbientDisplayConfiguration.java
    
    Bug: 373792870
    Bug: 369939804
    Bug: 369938501
    Flag: android.hardware.biometrics.screen_off_unlock_udfps
    Test: atest DozeSensorsTest
    Test: adb shell settings put secure
          screen_off_unlock_udfps <1|0>
    Change-Id: If3655a0492469d5048c87c0e032d678c2e764393

--- packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SecureSettings.java
@@ -133,6 +133,7 @@ public class SecureSettings {
         Settings.Secure.FINGERPRINT_SIDE_FPS_ENROLL_TAP_WINDOW,
         Settings.Secure.FINGERPRINT_SIDE_FPS_AUTH_DOWNTIME,
         Settings.Secure.SFPS_PERFORMANT_AUTH_ENABLED,
+        Settings.Secure.SCREEN_OFF_UNLOCK_UDFPS_ENABLED,
         Settings.Secure.ACTIVE_UNLOCK_ON_WAKE,
         Settings.Secure.ACTIVE_UNLOCK_ON_UNLOCK_INTENT,
         Settings.Secure.ACTIVE_UNLOCK_ON_UNLOCK_INTENT_LEGACY,
