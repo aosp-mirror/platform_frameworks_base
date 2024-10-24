7dc9b7b77170cadc09686ac8ad63bcd722d1a790
Johannes Gallmann <gallmann@google.com>
2024-10-24 11:27:06 +0200

    Fix back gesture not being handled by BackAnimationController
    Bug: 375342507
    Flag: EXEMPT bugfix
    Test: BackAnimationControllerTest
    Test: Manual, i.e. verify that issue of back gesture touch events being sent to app is not reproducible with fix.
    Change-Id: I45b46e5aca3bc42db2fbaf518f65061bb11062b4

--- libs/WindowManager/Shell/src/com/android/wm/shell/back/BackAnimationController.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/back/BackAnimationController.java
@@ -985,7 +985,6 @@ public class BackAnimationController implements RemoteCallable<BackAnimationCont
         ProtoLog.d(WM_SHELL_BACK_PREVIEW, "BackAnimationController: finishBackNavigation()");
         mActiveCallback = null;
         mApps = null;
-        mShouldStartOnNextMoveEvent = false;
         mOnBackStartDispatched = false;
         mThresholdCrossed = false;
         mPointersPilfered = false;
