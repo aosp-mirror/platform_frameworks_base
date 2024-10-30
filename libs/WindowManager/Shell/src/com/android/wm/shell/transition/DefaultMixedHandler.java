7b1ef4dab830be6be57ce3863253df3658c5e701
Charles Chen <charlesccchen@google.com>
2024-10-30 11:27:07 +0800

    Fix NPE when attach thumbnail
    Test: presubmit
    Fixes: 376368044
    Flag: EXEMPT bugfix
    Change-Id: Ica76fc7f2b66d0859235013f401f0bc68bbf5fdb

--- libs/WindowManager/Shell/src/com/android/wm/shell/transition/DefaultMixedHandler.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/transition/DefaultMixedHandler.java
@@ -40,6 +40,7 @@ import android.window.WindowContainerToken;
 import android.window.WindowContainerTransaction;
 
 import com.android.internal.protolog.ProtoLog;
+import com.android.window.flags.Flags;
 import com.android.wm.shell.ShellTaskOrganizer;
 import com.android.wm.shell.activityembedding.ActivityEmbeddingController;
 import com.android.wm.shell.common.split.SplitScreenUtils;
@@ -419,7 +420,9 @@ public class DefaultMixedHandler implements MixedTransitionHandler,
         for (int i = 0; i < info.getRootCount(); ++i) {
             out.addRoot(info.getRoot(i));
         }
-        out.setAnimationOptions(info.getAnimationOptions());
+        if (!Flags.moveAnimationOptionsToChange()) {
+            out.setAnimationOptions(info.getAnimationOptions());
+        }
         return out;
     }
 
