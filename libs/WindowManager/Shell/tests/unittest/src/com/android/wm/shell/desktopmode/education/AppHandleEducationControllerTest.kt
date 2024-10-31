50f92255d1f0045baa355f61ae90248f1c769aef
Jorge Gil <jorgegil@google.com>
2024-10-31 16:14:19 +0000

    Revert "Ignore AppHandleEducationControllerTest"
    This reverts commit f44963d537a317fdd26d74f11f81b1c1f1386f1c.
    
    Reason for revert: not causing the crash
    
    Change-Id: I5ff8e8eb206522a14a66ee0846fd6134cbb7d29e

--- libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/desktopmode/education/AppHandleEducationControllerTest.kt
+++ libs/WindowManager/Shell/tests/unittest/src/com/android/wm/shell/desktopmode/education/AppHandleEducationControllerTest.kt
@@ -68,7 +68,6 @@ import org.mockito.kotlin.whenever
 @SmallTest
 @RunWith(AndroidTestingRunner::class)
 @OptIn(ExperimentalCoroutinesApi::class)
-@Ignore("b/374328725")
 class AppHandleEducationControllerTest : ShellTestCase() {
   @JvmField
   @Rule
