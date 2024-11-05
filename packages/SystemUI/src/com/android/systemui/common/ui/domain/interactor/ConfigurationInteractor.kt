cb51880b9f399d000bd348da7d9d7cd9e82f0ccb
Daniel Chapin <chapin@google.com>
2024-11-05 15:09:54 +0000

    Revert "Provide @ShadeDisplayAware ConfigurationInteractor"
    This reverts commit 5b337f42d6de027d430e808e480cf361f28ab090.
    
    Reason for revert: b/377407253
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ca337dfba89a980d370fd056dbd4c97ee54ae650)
    Merged-In: Icbf104977cd616e8591d10f7531a0b062df29487
    Change-Id: Icbf104977cd616e8591d10f7531a0b062df29487

--- packages/SystemUI/src/com/android/systemui/common/ui/domain/interactor/ConfigurationInteractor.kt
+++ packages/SystemUI/src/com/android/systemui/common/ui/domain/interactor/ConfigurationInteractor.kt
@@ -21,6 +21,8 @@ import android.content.res.Configuration
 import android.graphics.Rect
 import android.view.Surface
 import com.android.systemui.common.ui.data.repository.ConfigurationRepository
+import com.android.systemui.dagger.SysUISingleton
+import javax.inject.Inject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
@@ -30,52 +32,14 @@ import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.onStart
 
 /** Business logic related to configuration changes. */
-interface ConfigurationInteractor {
+// TODO: b/374267505 - Create a @ShadeDisplayWindow annotated version of this.
+@SysUISingleton
+class ConfigurationInteractor @Inject constructor(private val repository: ConfigurationRepository) {
     /**
      * Returns screen size adjusted to rotation, so returned screen size is stable across all
      * rotations
      */
-    val Configuration.naturalScreenBounds: Rect
-
-    /** Returns the unadjusted screen size. */
-    val maxBounds: Flow<Rect>
-
-    /**
-     * Returns screen size adjusted to rotation, so returned screen sizes are stable across all
-     * rotations, could be useful if you need to react to screen resize (e.g. fold/unfold on
-     * foldable devices)
-     */
-    val naturalMaxBounds: Flow<Rect>
-
-    /**
-     * The layout direction. Will be either `View#LAYOUT_DIRECTION_LTR` or
-     * `View#LAYOUT_DIRECTION_RTL`.
-     */
-    val layoutDirection: Flow<Int>
-
-    /** Emit an event on any config change */
-    val onAnyConfigurationChange: Flow<Unit>
-
-    /** Emits the new configuration on any configuration change */
-    val configurationValues: Flow<Configuration>
-
-    /** Emits the current resolution scaling factor */
-    val scaleForResolution: Flow<Float>
-
-    /** Given [resourceId], emit the dimension pixel size on config change */
-    fun dimensionPixelSize(resourceId: Int): Flow<Int>
-
-    /** Emits the dimensional pixel size of the given resource, inverting it for RTL if necessary */
-    fun directionalDimensionPixelSize(originLayoutDirection: Int, resourceId: Int): Flow<Int>
-
-    /** Given a set of [resourceId]s, emit Map<ResourceId, DimensionPixelSize> on config change */
-    fun dimensionPixelSize(resourceIds: Set<Int>): Flow<Map<Int, Int>>
-}
-
-class ConfigurationInteractorImpl(private val repository: ConfigurationRepository) :
-    ConfigurationInteractor {
-
-    override val Configuration.naturalScreenBounds: Rect
+    private val Configuration.naturalScreenBounds: Rect
         get() {
             val rotation = windowConfiguration.displayRotation
             val maxBounds = windowConfiguration.maxBounds
@@ -86,40 +50,53 @@ class ConfigurationInteractorImpl(private val repository: ConfigurationRepositor
             }
         }
 
-    override val maxBounds: Flow<Rect> =
+    /** Returns the unadjusted screen size. */
+    val maxBounds: Flow<Rect> =
         repository.configurationValues
             .map { Rect(it.windowConfiguration.maxBounds) }
             .distinctUntilChanged()
 
-    override val naturalMaxBounds: Flow<Rect> =
+    /**
+     * Returns screen size adjusted to rotation, so returned screen sizes are stable across all
+     * rotations, could be useful if you need to react to screen resize (e.g. fold/unfold on
+     * foldable devices)
+     */
+    val naturalMaxBounds: Flow<Rect> =
         repository.configurationValues.map { it.naturalScreenBounds }.distinctUntilChanged()
 
-    override val layoutDirection: Flow<Int> =
+    /**
+     * The layout direction. Will be either `View#LAYOUT_DIRECTION_LTR` or
+     * `View#LAYOUT_DIRECTION_RTL`.
+     */
+    val layoutDirection: Flow<Int> =
         repository.configurationValues.map { it.layoutDirection }.distinctUntilChanged()
 
-    override fun dimensionPixelSize(resourceId: Int): Flow<Int> {
+    /** Given [resourceId], emit the dimension pixel size on config change */
+    fun dimensionPixelSize(resourceId: Int): Flow<Int> {
         return onAnyConfigurationChange.mapLatest { repository.getDimensionPixelSize(resourceId) }
     }
 
-    override fun directionalDimensionPixelSize(
-        originLayoutDirection: Int,
-        resourceId: Int,
-    ): Flow<Int> {
+    /** Emits the dimensional pixel size of the given resource, inverting it for RTL if necessary */
+    fun directionalDimensionPixelSize(originLayoutDirection: Int, resourceId: Int): Flow<Int> {
         return dimensionPixelSize(resourceId).combine(layoutDirection) { size, direction ->
             if (originLayoutDirection == direction) size else -size
         }
     }
 
-    override fun dimensionPixelSize(resourceIds: Set<Int>): Flow<Map<Int, Int>> {
+    /** Given a set of [resourceId]s, emit Map<ResourceId, DimensionPixelSize> on config change */
+    fun dimensionPixelSize(resourceIds: Set<Int>): Flow<Map<Int, Int>> {
         return onAnyConfigurationChange.mapLatest {
             resourceIds.associateWith { repository.getDimensionPixelSize(it) }
         }
     }
 
-    override val onAnyConfigurationChange: Flow<Unit> =
+    /** Emit an event on any config change */
+    val onAnyConfigurationChange: Flow<Unit> =
         repository.onAnyConfigurationChange.onStart { emit(Unit) }
 
-    override val configurationValues: Flow<Configuration> = repository.configurationValues
+    /** Emits the new configuration on any configuration change */
+    val configurationValues: Flow<Configuration> = repository.configurationValues
 
-    override val scaleForResolution: Flow<Float> = repository.scaleForResolution
+    /** Emits the current resolution scaling factor */
+    val scaleForResolution: Flow<Float> = repository.scaleForResolution
 }
