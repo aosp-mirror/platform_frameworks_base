0cd1b994c9d7c00fb1cf05e1187f6f6ee7d132a9
Michael Mikhail <michaelmikhil@google.com>
2024-11-06 15:21:36 +0000

    Add toast and timeout logic in ringer
    Flag: com.android.systemui.volume_redesign
    Bug: 369993851
    Test: atest VolumeDialogRingerDrawerViewModelTest
    Change-Id: I10527a07db808eb0fa4c32db1fade37126611166

--- packages/SystemUI/src/com/android/systemui/volume/dialog/dagger/VolumeDialogComponent.kt
+++ packages/SystemUI/src/com/android/systemui/volume/dialog/dagger/VolumeDialogComponent.kt
@@ -16,6 +16,7 @@
 
 package com.android.systemui.volume.dialog.dagger
 
+import com.android.systemui.volume.dialog.dagger.module.VolumeDialogModule
 import com.android.systemui.volume.dialog.dagger.scope.VolumeDialog
 import com.android.systemui.volume.dialog.dagger.scope.VolumeDialogScope
 import dagger.BindsInstance
@@ -27,7 +28,7 @@ import kotlinx.coroutines.CoroutineScope
  * [com.android.systemui.volume.dialog.VolumeDialogPlugin] and lives alongside it.
  */
 @VolumeDialogScope
-@Subcomponent(modules = [])
+@Subcomponent(modules = [VolumeDialogModule::class])
 interface VolumeDialogComponent {
 
     /**
