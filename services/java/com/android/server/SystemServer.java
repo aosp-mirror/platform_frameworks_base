68ab059aeac70bb54ff7e683fbed32ff292c6abd
Pechetty Sravani (xWF) <pechetty@google.com>
2024-11-13 04:52:05 +0000

    Revert "Migrate WristOrientationService"
    Revert submission 29447950-cherrypicker-L94600030006457400:N79600030103499368
    
    Reason for revert: <DroidMonitor created a revert due to b/378787763.Will be verified through ABTD for standard investigation.>
    
    Reverted changes: /q/submissionid:29447950-cherrypicker-L94600030006457400:N79600030103499368
    
    Change-Id: I5acb28639f9c15bc18039a76dfeca676029db8a1

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -2737,9 +2737,8 @@ public final class SystemServer implements Dumpable {
             mSystemServiceManager.startService(WEAR_MODE_SERVICE_CLASS);
             t.traceEnd();
 
-            boolean enableWristOrientationService =
-                    !android.server.Flags.migrateWristOrientation()
-                    && SystemProperties.getBoolean("config.enable_wristorientation", false);
+            boolean enableWristOrientationService = SystemProperties.getBoolean(
+                    "config.enable_wristorientation", false);
             if (enableWristOrientationService) {
                 t.traceBegin("StartWristOrientationService");
                 mSystemServiceManager.startService(WRIST_ORIENTATION_SERVICE_CLASS);
