bf915ec856e213447bbb800e680168b1fe096548
Paul Duffin <paulduffin@google.com>
2024-11-15 12:32:13 +0000

    Remove TODO(b/347269120)s as they will no longer be done
    Change https://r.android.com/3139200 added the TODOs as the intent was
    to add `@Nullable` back on the methods. However, doing so would break
    too many applications so it is not considered worth the effort so this
    change is cleaning up the unnecessary TODOs.
    
    Bug: 347269120
    Test: m checkapi
    Merged-In: Ibeb0380f8d14ebb9a4c05f7b9a26632e38affd2f
    Change-Id: Ibeb0380f8d14ebb9a4c05f7b9a26632e38affd2f

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -4484,7 +4484,6 @@ public abstract class Context {
      * @see #DISPLAY_HASH_SERVICE
      * @see android.view.displayhash.DisplayHashManager
      */
-    // TODO(b/347269120): Re-add @Nullable
     public abstract Object getSystemService(@ServiceName @NonNull String name);
 
     /**
@@ -4529,7 +4528,6 @@ public abstract class Context {
      * <b>never</b> throw a {@link RuntimeException} if the name is not supported.
      */
     @SuppressWarnings("unchecked")
-    // TODO(b/347269120): Re-add @Nullable
     public final <T> T getSystemService(@NonNull Class<T> serviceClass) {
         // Because subclasses may override getSystemService(String) we cannot
         // perform a lookup by class alone.  We must first map the class to its
