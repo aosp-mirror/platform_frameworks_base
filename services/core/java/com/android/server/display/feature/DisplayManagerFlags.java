418c40b5480d29d4b9521a55f077f8313bb3a6ad
Manasi Navare <navaremanasi@google.com>
2024-11-20 20:04:20 +0000

    Handle onModeRejected display event in DisplayModeDirector
    Define a new ModeChangeObserver class to listen to the
    onModeRejected() display event from SF.
    Define ForbiddenModesVote to filter out the rejected mode
    from the modelist and set the new DesiredModeSpecs and retry the
    modeset in the scheduled next traversal.
    
    Bug: 377702802
    Test: atest
    Flag: com.android.graphics.surfaceflinger.flags.display_config_error_hal
    
    Change-Id: Icb5ff755181eeace787c4fe13c4c2fa373eaa666
    Signed-off-by: Manasi Navare <navaremanasi@google.com>

--- services/core/java/com/android/server/display/feature/DisplayManagerFlags.java
+++ services/core/java/com/android/server/display/feature/DisplayManagerFlags.java
@@ -93,6 +93,10 @@ public class DisplayManagerFlags {
             com.android.graphics.surfaceflinger.flags.Flags.FLAG_ENABLE_SMALL_AREA_DETECTION,
             com.android.graphics.surfaceflinger.flags.Flags::enableSmallAreaDetection);
 
+    private final FlagState mDisplayConfigErrorHalFlagState = new FlagState(
+            com.android.graphics.surfaceflinger.flags.Flags.FLAG_DISPLAY_CONFIG_ERROR_HAL,
+            com.android.graphics.surfaceflinger.flags.Flags::displayConfigErrorHal);
+
     private final FlagState mBrightnessIntRangeUserPerceptionFlagState = new FlagState(
             Flags.FLAG_BRIGHTNESS_INT_RANGE_USER_PERCEPTION,
             Flags::brightnessIntRangeUserPerception);
@@ -357,6 +361,10 @@ public class DisplayManagerFlags {
         return mSmallAreaDetectionFlagState.isEnabled();
     }
 
+    public boolean isDisplayConfigErrorHalEnabled() {
+        return mDisplayConfigErrorHalFlagState.isEnabled();
+    }
+
     public boolean isBrightnessIntRangeUserPerceptionEnabled() {
         return mBrightnessIntRangeUserPerceptionFlagState.isEnabled();
     }
@@ -583,6 +591,7 @@ public class DisplayManagerFlags {
         pw.println(" " + mPowerThrottlingClamperFlagState);
         pw.println(" " + mEvenDimmerFlagState);
         pw.println(" " + mSmallAreaDetectionFlagState);
+        pw.println(" " + mDisplayConfigErrorHalFlagState);
         pw.println(" " + mBrightnessIntRangeUserPerceptionFlagState);
         pw.println(" " + mRestrictDisplayModes);
         pw.println(" " + mBrightnessWearBedtimeModeClamperFlagState);
