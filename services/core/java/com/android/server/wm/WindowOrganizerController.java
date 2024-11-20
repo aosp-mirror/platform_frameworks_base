ProjectNirwana/frameworks_base

Pass SafeActivityOptions with actual caller for startActivityInTF
We clearCallingUid before apply the WCT, but SafeActivityOptions will
query the Binder Uid when construct. Update to pass in the actual
caller.

Flag: EXEMPT bug fix
Bug: 369103643
Test: atest WmTests:WindowOrganizerTests#
      testStartActivityInTaskFragment_checkCallerPermission
Change-Id: I873ae576de0bc4a7402c2f522b45853bce48a0c5

services/core/java/com/android/server/wm/WindowOrganizerController.java
@@ -1517,8 +1517,10 @@ class WindowOrganizerController extends IWindowOrganizerController.Stub
                 final IBinder callerActivityToken = operation.getActivityToken();
                 final Intent activityIntent = operation.getActivityIntent();
                 final Bundle activityOptions = operation.getBundle();
+                final SafeActivityOptions safeOptions =
+                        SafeActivityOptions.fromBundle(activityOptions, caller.mPid, caller.mUid);
                 final int result = waitAsyncStart(() -> mService.getActivityStartController()
-                        .startActivityInTaskFragment(taskFragment, activityIntent, activityOptions,
+                        .startActivityInTaskFragment(taskFragment, activityIntent, safeOptions,
                                 callerActivityToken, caller.mUid, caller.mPid,
                                 errorCallbackToken));
                 if (!isStartResultSuccessful(result)) {

aosp-mirror/platform_frameworks_base

(cherry picked from commit 20c568e77eae5d469cd5e594b644d8645d830dbd)
(cherry picked from https://googleplex-android-review.googlesource.com/q/commit:703bc57a6fa008fb43d9daddd863525affc44070)
Merged-In: I873ae576de0bc4a7402c2f522b45853bce48a0c5

services/core/java/com/android/server/wm/WindowOrganizerController.java
@@ -919,8 +919,10 @@ class WindowOrganizerController extends IWindowOrganizerController.Stub
                 }
                 final Intent activityIntent = hop.getActivityIntent();
                 final Bundle activityOptions = hop.getLaunchOptions();
+                final SafeActivityOptions safeOptions =
+                        SafeActivityOptions.fromBundle(activityOptions, caller.mPid, caller.mUid);
                 final int result = mService.getActivityStartController()
-                        .startActivityInTaskFragment(tf, activityIntent, activityOptions,
+                        .startActivityInTaskFragment(tf, activityIntent, safeOptions,
                                 hop.getCallingActivity(), caller.mUid, caller.mPid,
                                 errorCallbackToken);
                 if (!isStartResultSuccessful(result)) {