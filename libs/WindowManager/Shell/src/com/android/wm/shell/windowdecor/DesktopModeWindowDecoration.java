f023f4477739af37abbba8d0e8b92851b43a9af9
Jorge Gil <jorgegil@google.com>
2024-11-20 01:08:24 +0000

    Remove duplicate to-desktop call from HandleMenu
    Bug: None
    Test: m
    Flag: EXEMPT bug fix
    Change-Id: I3722e99bc30d1b335b248c39c93749105f76f9e8

--- libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecoration.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecoration.java
@@ -1415,7 +1415,6 @@ public class DesktopModeWindowDecoration extends WindowDecoration<WindowDecorLin
         mHandleMenu.show(
                 /* onToDesktopClickListener= */ () -> {
                     mOnToDesktopClickListener.accept(APP_HANDLE_MENU_BUTTON);
-                    mOnToDesktopClickListener.accept(APP_HANDLE_MENU_BUTTON);
                     return Unit.INSTANCE;
                 },
                 /* onToFullscreenClickListener= */ mOnToFullscreenClickListener,
