ef388b9ec42e7e57c49e1c1d757bff5517bf72c1
Ben Lin <linben@google.com>
2024-12-04 22:29:11 +0000

    DesktopmodeWindowDecorViewModel: Use Display Context.
    Instead of using the default Context supplied, use the displayContext
    from DisplayController associated with the TaskInfo's displayId.
    
    Bug: 382023296
    Test: Build
    Flag: com.android.window.flags.enable_bug_fixes_for_secondary_display
    Change-Id: I5493598ea4bce49c6b53a67526dd622abdc785d1

--- libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecorViewModel.java
+++ libs/WindowManager/Shell/src/com/android/wm/shell/windowdecor/DesktopModeWindowDecorViewModel.java
@@ -1618,7 +1618,9 @@ public class DesktopModeWindowDecorViewModel implements WindowDecorViewModel,
         }
         final DesktopModeWindowDecoration windowDecoration =
                 mDesktopModeWindowDecorFactory.create(
-                        mContext,
+                        Flags.enableBugFixesForSecondaryDisplay()
+                                ? mDisplayController.getDisplayContext(taskInfo.displayId)
+                                : mContext,
                         mContext.createContextAsUser(UserHandle.of(taskInfo.userId), 0 /* flags */),
                         mDisplayController,
                         mSplitScreenController,
