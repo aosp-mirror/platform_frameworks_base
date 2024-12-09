d615298466085c4a88c6733804160e0c1ee7e31e
Vaibhav Devmurari <vdevmurari@google.com>
2024-12-09 13:15:03 +0000

    Fix: Block opening any apps on keyguard without user auth
    Test: atest KeyGestureEventTests
    Bug: 378900798
    Flag: EXEMPT bugfix
    Change-Id: I89d43872108710d20e0c4ef7e652d389896155d7

--- services/tests/wmtests/src/com/android/server/policy/TestPhoneWindowManager.java
+++ services/tests/wmtests/src/com/android/server/policy/TestPhoneWindowManager.java
@@ -571,6 +571,10 @@ class TestPhoneWindowManager {
         doNothing().when(mPhoneWindowManager).launchHomeFromHotKey(anyInt());
     }
 
+    void overrideKeyguardOn(boolean isKeyguardOn) {
+        doReturn(isKeyguardOn).when(mPhoneWindowManager).keyguardOn();
+    }
+
     void overrideIsUserSetupComplete(boolean isCompleted) {
         doReturn(isCompleted).when(mPhoneWindowManager).isUserSetupComplete();
     }
@@ -731,6 +735,11 @@ class TestPhoneWindowManager {
     void assertSearchManagerLaunchAssist() {
         mTestLooper.dispatchAll();
         verify(mSearchManager).launchAssist(any());
+    }
+
+    void assertSearchManagerDoesntLaunchAssist() {
+        mTestLooper.dispatchAll();
+        verify(mSearchManager, never()).launchAssist(any());
     }
 
     void assertLaunchSystemSettings() {
@@ -936,5 +945,11 @@ class TestPhoneWindowManager {
     void assertKeyGestureEventSentToKeyGestureController(int gestureType) {
         verify(mInputManagerInternal)
                 .handleKeyGestureInKeyGestureController(anyInt(), any(), anyInt(), eq(gestureType));
+    }
+
+    void assertNoActivityLaunched() {
+        mTestLooper.dispatchAll();
+        verify(mContext, never()).startActivityAsUser(any(), any(), any());
+        verify(mContext, never()).startActivityAsUser(any(), any());
     }
 }
