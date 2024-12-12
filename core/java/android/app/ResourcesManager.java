12e49e7a4e56df4dce97f80051063f45e8366329
Jeremy Meyer <jakmcbane@google.com>
2024-12-12 13:05:11 -0800

    Only allow load frro drawables from frro directory
    Fixes: 383080440
    Test: Used test app to verify drawable frros still work
    Flag: EXEMPT bugfix
    Change-Id: I4247e54f3ea0d9e4bf0d9f404e0272751a258ba2

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -77,6 +77,7 @@ import java.util.function.Function;
 public class ResourcesManager {
     static final String TAG = "ResourcesManager";
     private static final boolean DEBUG = false;
+    public static final String RESOURCE_CACHE_DIR = "/data/resource-cache/";
 
     private static volatile ResourcesManager sResourcesManager;
 
@@ -581,7 +582,7 @@ public class ResourcesManager {
     }
 
     private static String overlayPathToIdmapPath(String path) {
-        return "/data/resource-cache/" + path.substring(1).replace('/', '@') + "@idmap";
+        return RESOURCE_CACHE_DIR + path.substring(1).replace('/', '@') + "@idmap";
     }
 
     /**
