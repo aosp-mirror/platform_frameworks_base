0d87404ea3544f34378fcc162a9449648c7ece6b
András Kurucz <kurucz@google.com>
2024-12-17 15:44:32 +0000

    Convert StatusBarNotificationPresenterTest to Kotlin
    Use Mockito-Kotlin. Use kosmos wherever we can.
    
    Bug: 377795836
    Test: atest StatusBarNotificationPresenterTest
    Flag: TEST_ONLY
    Change-Id: I8cf2f38576876ff205a55c56cc6c36c3aa87c0cf

--- packages/SystemUI/tests/utils/src/com/android/systemui/statusbar/notification/VisualInterruptionDecisionProviderKosmos.kt
+++ packages/SystemUI/tests/utils/src/com/android/systemui/statusbar/notification/VisualInterruptionDecisionProviderKosmos.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Android Open Source Project
+ * Copyright (C) 2024 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,10 +14,11 @@
  * limitations under the License.
  */
 
-package com.android.systemui.statusbar.policy
+package com.android.systemui.statusbar.notification
 
 import com.android.systemui.kosmos.Kosmos
+import com.android.systemui.statusbar.notification.interruption.VisualInterruptionDecisionProvider
 import org.mockito.kotlin.mock
 
-var Kosmos.keyguardStateController: KeyguardStateController by
-    Kosmos.Fixture { mock<KeyguardStateController>() }
+val Kosmos.visualInterruptionDecisionProvider by
+    Kosmos.Fixture { mock<VisualInterruptionDecisionProvider>() }
