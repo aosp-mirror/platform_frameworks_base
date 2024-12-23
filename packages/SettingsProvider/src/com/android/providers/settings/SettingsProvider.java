02dc1fb067add8549264a99c1fe175f2efc2e7fc
Michael Groover <mpgroover@google.com>
2024-12-23 11:59:32 -0600

    Enforce adb shell DeviceConfig flags restrictions
    Android 16 limits the DeviceConfig flags that can be written by
    the shell user to those that have been allowlisted. Since a
    large portion of tests adopt the shell user's permission identity
    to modify DeviceConfig flags, this change would impact these tests,
    and the flags modified during tests would need to be allowlisted.
    The feature initially rolled out in logging mode to capture all
    flags that need to be allowlisted; now that the allowlist has
    been updated, this commit switches the feature to enforcing mode
    to prevent the adb shell user from writing flags that have not
    been allowlisted.
    
    Bug: 364083026
    Flag: android.security.protect_device_config_flags
    Test: atest DeviceConfigApiTest
    Change-Id: I376fa6a57685bf27d76b9ac48ab60bf440b384c1

--- packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
+++ packages/SettingsProvider/src/com/android/providers/settings/SettingsProvider.java
@@ -2469,7 +2469,7 @@ public class SettingsProvider extends ContentProvider {
         boolean isRestrictedShell = android.security.Flags.protectDeviceConfigFlags()
                 && hasAllowlistPermission;
 
-        if (!isRestrictedShell && hasWritePermission) {
+        if (hasWritePermission) {
             assertCallingUserDenyList(flags);
         } else if (hasAllowlistPermission) {
             Set<String> allowlistedDeviceConfigNamespaces = null;
@@ -2499,7 +2499,7 @@ public class SettingsProvider extends ContentProvider {
                 }
 
                 if (!namespaceAllowed && !DeviceConfig.getAdbWritableFlags().contains(flag)) {
-                    Slog.wtf(LOG_TAG, "Permission denial for flag '" + flag
+                    throw new SecurityException("Permission denial for flag '" + flag
                             + "'; allowlist permission granted, but must add flag to the "
                             + "allowlist");
                 }
