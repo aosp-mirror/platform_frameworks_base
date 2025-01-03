751e11aca8764f94393f418f59de1d18e479cd69
Chandru S <chandruis@google.com>
2025-01-03 03:11:26 +0000

    Separates primary bouncer transition module into 2 separate modules (decl & impl)
    This allows installing the decl module to allow injecting empty set of PrimaryBouncerTransition
    
    Bug: 385364857
    Flag: EXEMPT module changes
    Test: everything builds
    Change-Id: I7fcdb103ad619c6480d175a372f33ce2e708ce88

--- packages/SystemUI/src/com/android/systemui/keyguard/ui/transitions/PrimaryBouncerTransition.kt
+++ packages/SystemUI/src/com/android/systemui/keyguard/ui/transitions/PrimaryBouncerTransition.kt
@@ -16,37 +16,15 @@
 
 package com.android.systemui.keyguard.ui.transitions
 
-import android.content.res.Resources
-import android.util.MathUtils
-import com.android.systemui.dagger.SysUISingleton
-import com.android.systemui.dagger.qualifiers.Main
-import com.android.systemui.keyguard.ui.viewmodel.AlternateBouncerToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.AodToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.DozingToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.GlanceableHubToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.LockscreenToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.OccludedToPrimaryBouncerTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToAodTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToDozingTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToGlanceableHubTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToGoneTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToLockscreenTransitionViewModel
-import com.android.systemui.keyguard.ui.viewmodel.PrimaryBouncerToOccludedTransitionViewModel
-import com.android.systemui.res.R
-import com.android.systemui.window.flag.WindowBlurFlag
-import dagger.Binds
-import dagger.Module
-import dagger.Provides
-import dagger.multibindings.IntoSet
-import javax.inject.Inject
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import android.util.MathUtils.lerp
 import kotlinx.coroutines.flow.Flow
 
 /**
  * Each PrimaryBouncerTransition is responsible for updating various UI states based on the nature
  * of the transition.
  *
- * MUST list implementing classes in dagger module [PrimaryBouncerTransitionModule].
+ * MUST list implementing classes in dagger module
+ * [com.android.systemui.keyguard.dagger.PrimaryBouncerTransitionImplModule].
  */
 interface PrimaryBouncerTransition {
     /** Radius of blur applied to the window's root view. */
@@ -56,93 +34,5 @@ interface PrimaryBouncerTransition {
         starBlurRadius: Float,
         endBlurRadius: Float,
         transitionProgress: Float,
-    ): Float {
-        return MathUtils.lerp(starBlurRadius, endBlurRadius, transitionProgress)
-    }
-}
-
-/**
- * Module that installs all the transitions from different keyguard states to and away from the
- * primary bouncer.
- */
-@ExperimentalCoroutinesApi
-@Module
-interface PrimaryBouncerTransitionModule {
-    @Binds
-    @IntoSet
-    fun fromAod(impl: AodToPrimaryBouncerTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun fromAlternateBouncer(
-        impl: AlternateBouncerToPrimaryBouncerTransitionViewModel
-    ): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun fromDozing(impl: DozingToPrimaryBouncerTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun fromLockscreen(
-        impl: LockscreenToPrimaryBouncerTransitionViewModel
-    ): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun fromGlanceableHub(
-        impl: GlanceableHubToPrimaryBouncerTransitionViewModel
-    ): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun fromOccluded(impl: OccludedToPrimaryBouncerTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toAod(impl: PrimaryBouncerToAodTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toLockscreen(impl: PrimaryBouncerToLockscreenTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toDozing(impl: PrimaryBouncerToDozingTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toGlanceableHub(
-        impl: PrimaryBouncerToGlanceableHubTransitionViewModel
-    ): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toGone(impl: PrimaryBouncerToGoneTransitionViewModel): PrimaryBouncerTransition
-
-    @Binds
-    @IntoSet
-    fun toOccluded(impl: PrimaryBouncerToOccludedTransitionViewModel): PrimaryBouncerTransition
-
-    companion object {
-        @Provides
-        @SysUISingleton
-        fun provideBlurConfig(@Main resources: Resources): BlurConfig {
-            val minBlurRadius = resources.getDimensionPixelSize(R.dimen.min_window_blur_radius)
-            val maxBlurRadius =
-                if (WindowBlurFlag.isEnabled) {
-                    resources.getDimensionPixelSize(R.dimen.max_shade_window_blur_radius)
-                } else {
-                    resources.getDimensionPixelSize(R.dimen.max_window_blur_radius)
-                }
-            return BlurConfig(minBlurRadius.toFloat(), maxBlurRadius.toFloat())
-        }
-    }
-}
-
-/** Config that provides the max and min blur radius for the window blurs. */
-data class BlurConfig(val minBlurRadiusPx: Float, val maxBlurRadiusPx: Float) {
-    // No-op config that will be used by dagger of other SysUI variants which don't blur the
-    // background surface.
-    @Inject constructor() : this(0.0f, 0.0f)
+    ): Float = lerp(starBlurRadius, endBlurRadius, transitionProgress)
 }
