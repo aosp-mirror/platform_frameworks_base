c80b4e9020175e62632532cd1a2c10e5ef974a54
Fan Wu <cechkahn@google.com>
2025-01-09 14:33:07 +0800

    [Catalyst] Add BooleanPreference
    Bug: 388167302
    Test: atest
    Flag: EXEMPT refactor
    
    Change-Id: Icd1735c77e269cd68372173a7e66ad2da0d7750d

--- packages/SettingsLib/Graph/src/com/android/settingslib/graph/PreferenceGraphBuilder.kt
+++ packages/SettingsLib/Graph/src/com/android/settingslib/graph/PreferenceGraphBuilder.kt
@@ -40,7 +40,6 @@ import com.android.settingslib.graph.proto.PreferenceProto
 import com.android.settingslib.graph.proto.PreferenceProto.ActionTarget
 import com.android.settingslib.graph.proto.PreferenceScreenProto
 import com.android.settingslib.graph.proto.TextProto
-import com.android.settingslib.metadata.BooleanValue
 import com.android.settingslib.metadata.FloatPersistentPreference
 import com.android.settingslib.metadata.PersistentPreference
 import com.android.settingslib.metadata.PreferenceAvailabilityProvider
@@ -410,18 +409,20 @@ fun PreferenceMetadata.toProto(
             val storage = metadata.storage(context)
             value = preferenceValueProto {
                 when (metadata) {
-                    is BooleanValue -> storage.getBoolean(metadata.key)?.let { booleanValue = it }
                     is RangeValue -> storage.getInt(metadata.key)?.let { intValue = it }
                     is FloatPersistentPreference ->
                         storage.getFloat(metadata.key)?.let { floatValue = it }
                     else -> {}
+                }
+                when (metadata.valueType) {
+                    Boolean::class.javaObjectType ->
+                        storage.getBoolean(metadata.key)?.let { booleanValue = it }
                 }
             }
         }
         if (flags.includeValueDescriptor()) {
             valueDescriptor = preferenceValueDescriptorProto {
                 when (metadata) {
-                    is BooleanValue -> booleanType = true
                     is RangeValue -> rangeValue = rangeValueProto {
                             min = metadata.getMinValue(context)
                             max = metadata.getMaxValue(context)
@@ -429,6 +430,11 @@ fun PreferenceMetadata.toProto(
                         }
                     is FloatPersistentPreference -> floatType = true
                     else -> {}
+                }
+                if (metadata is PersistentPreference<*>) {
+                    when (metadata.valueType) {
+                        Boolean::class.javaObjectType -> booleanType = true
+                    }
                 }
             }
         }
