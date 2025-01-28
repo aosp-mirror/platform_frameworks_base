0d1c02433722d5d28bccdfd2ac757c80d79616d9
Orhan Uysal <uysalorhan@google.com>
2025-01-28 02:33:45 -0800

    Add root task tracking to dev options.
    Bug: 391626384
    Change-Id: I372c8e59df256c2f232bdf7b052375bb3ffcf409
    Test: Presubmit
    Flag: com.android.window.flags.enable_top_visible_root_task_per_user_tracking

--- services/core/java/com/android/server/wm/RootWindowContainer.java
+++ services/core/java/com/android/server/wm/RootWindowContainer.java
@@ -138,6 +138,7 @@ import android.view.Display;
 import android.view.DisplayInfo;
 import android.view.SurfaceControl;
 import android.view.WindowManager;
+import android.window.DesktopModeFlags;
 import android.window.TaskFragmentAnimationParams;
 import android.window.WindowContainerToken;
 
@@ -1908,7 +1909,7 @@ class RootWindowContainer extends WindowContainer<DisplayContent>
         // appropriate.
         removeRootTasksInWindowingModes(WINDOWING_MODE_PINNED);
 
-        if (Flags.enableTopVisibleRootTaskPerUserTracking()) {
+        if (DesktopModeFlags.ENABLE_TOP_VISIBLE_ROOT_TASK_PER_USER_TRACKING.isTrue()) {
             final IntArray visibleRootTasks = new IntArray();
             forAllRootTasks(rootTask -> {
                 if ((mCurrentUser == rootTask.mUserId || rootTask.showForAllUsers())
