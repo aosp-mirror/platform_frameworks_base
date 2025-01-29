8bfd577d5e024e09e23c8f6045232a5a00d6fbc7
Maryam Dehaini <mdehaini@google.com>
2025-01-28 17:38:53 -0800

    Add app-to-web flag to developer options
    Bug: 349695493
    Test: m
    Flag: com.android.window.flags.enable_desktop_windowing_app_to_web
    Change-Id: I7c15fb3abdc9c7cefc18fdf0500e92770c0ace39

--- core/java/android/window/DesktopModeFlags.java
+++ core/java/android/window/DesktopModeFlags.java
@@ -63,6 +63,7 @@ public enum DesktopModeFlags {
     DISABLE_NON_RESIZABLE_APP_SNAP_RESIZE(Flags::disableNonResizableAppSnapResizing, true),
     ENABLE_WINDOWING_SCALED_RESIZING(Flags::enableWindowingScaledResizing, true),
     ENABLE_DESKTOP_WINDOWING_TASK_LIMIT(Flags::enableDesktopWindowingTaskLimit, true),
+    ENABLE_DESKTOP_WINDOWING_APP_TO_WEB(Flags::enableDesktopWindowingAppToWeb, true),
     ENABLE_DESKTOP_WINDOWING_BACK_NAVIGATION(Flags::enableDesktopWindowingBackNavigation, true),
     ENABLE_WINDOWING_EDGE_DRAG_RESIZE(Flags::enableWindowingEdgeDragResize, true),
     ENABLE_DESKTOP_WINDOWING_TASKBAR_RUNNING_APPS(
