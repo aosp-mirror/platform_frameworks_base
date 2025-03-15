ab709b240fe3149b0db38152b859474d4267deef
lijilou <lijilou@xiaomi.corp-partner.google.com>
2025-03-15 14:57:24 +0800

    Remove useless mContext var for TaskFpsCallbackController.
    Bug: none
    Flag: EXEMPT minor optimization
    Change-Id: I39e5798fbdb6869db1cc3ee78fb14c97e56915aa

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -1452,7 +1452,7 @@ public class WindowManagerService extends IWindowManager.Stub
         mStartingSurfaceController = new StartingSurfaceController(this);
 
         mBlurController = new BlurController(mContext, mPowerManager);
-        mTaskFpsCallbackController = new TaskFpsCallbackController(mContext);
+        mTaskFpsCallbackController = new TaskFpsCallbackController();
         mAccessibilityController = new AccessibilityController(this);
         mScreenRecordingCallbackController = new ScreenRecordingCallbackController(this);
         mSystemPerformanceHinter = new SystemPerformanceHinter(mContext, displayId -> {
